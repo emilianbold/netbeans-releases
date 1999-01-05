6f9f1e47b4dcd4a3484e220082da1a3a06c239a3
Ian_Formanek <devnull@localhost>
1999-01-05 17:37:09 +0000

    #

---
+++ o.n.core/src/org/netbeans/beaninfo/FileSystemBeanInfo.java
@@ -0,0 +1,67 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is Forte for Java, Community Edition. The Initial
+ * Developer of the Original Code is Sun Microsystems, Inc. Portions
+ * Copyright 1997-2000 Sun Microsystems, Inc. All Rights Reserved.
+ */
+
+package com.netbeans.ide.filesystems;
+
+import java.beans.*;
+import java.util.ResourceBundle;
+import com.netbeans.ide.util.NbBundle;
+
+/** Object that provides beaninfo for file systems.
+*
+* @author Ian Formanek
+*/
+public class FileSystemBeanInfo extends SimpleBeanInfo {
+
+  /** Array of property descriptors. */
+  private static PropertyDescriptor[] desc;
+
+  // initialization of the array of descriptors
+  static {
+    try {
+      desc = new PropertyDescriptor[] {
+        new PropertyDescriptor ("readOnly", FileSystem.class, "isReadOnly", null), // 0
+        new PropertyDescriptor ("valid", FileSystem.class, "isValid", null), // 1
+        new PropertyDescriptor ("hidden", FileSystem.class, "isHidden", "setHidden") // 2
+      };
+      ResourceBundle bundle = NbBundle.getBundle("com.netbeans.developer.impl.locales.BaseFilesystemsBundle");
+      desc[0].setDisplayName (bundle.getString("PROP_readOnly"));
+      desc[0].setShortDescription (bundle.getString("HINT_readOnly"));
+      desc[1].setDisplayName (bundle.getString("PROP_valid"));
+      desc[1].setShortDescription (bundle.getString("HINT_valid"));
+      desc[2].setDisplayName (bundle.getString("PROP_hidden"));
+      desc[2].setShortDescription (bundle.getString("HINT_hidden"));
+    } catch (IntrospectionException ex) {
+      //throw new InternalError ();
+      ex.printStackTrace ();
+    }
+  }
+
+
+  /** Descriptor of valid properties
+  * @return array of properties
+  */
+  public PropertyDescriptor[] getPropertyDescriptors () {
+    return desc;
+  }
+
+}
+
+
+/*
+ * Log
+ *  1    Gandalf   1.0         1/5/99   Ian Formanek    
+ * $
+ * Beta Change History:
+ *  0    Tuborg    0.11        --/--/98 Jaroslav Tulach added hidden property
+ */
