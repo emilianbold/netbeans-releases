cf2cc8f992721e57affa2e5f7df212d8ae91ffd1
Ian_Formanek <devnull@localhost>
1999-03-16 10:32:57 +0000

    Added listening to icon and displayName changes on roots, support for ExplorerActions (Cut/Copy/...)

--- o.n.core/src/org/netbeans/core/NbMainExplorer.java
+++ o.n.core/src/org/netbeans/core/NbMainExplorer.java
@@ -25,16 +25,15 @@ import javax.swing.border.EmptyBorder;
 
 import com.netbeans.ide.*;
 import com.netbeans.ide.actions.*;
-import com.netbeans.ide.explorer.ExplorerPanel;
-import com.netbeans.ide.explorer.ExplorerManager;
-import com.netbeans.ide.explorer.view.BeanTreeView;
-import com.netbeans.ide.explorer.propertysheet.PropertySheetView;
 import com.netbeans.ide.awt.SplittedPanel;
 import com.netbeans.ide.awt.ToolbarToggleButton;
-import com.netbeans.ide.windows.TopComponent;
+import com.netbeans.ide.explorer.*;
+import com.netbeans.ide.explorer.view.BeanTreeView;
+import com.netbeans.ide.explorer.propertysheet.PropertySheetView;
 import com.netbeans.ide.nodes.Node;
 import com.netbeans.ide.util.NbBundle;
 import com.netbeans.ide.util.actions.SystemAction;
+import com.netbeans.ide.windows.TopComponent;
 
 /** Default explorer which contains toolbar with cut/copy/paste,
 * switchable property sheet and menu view actions in the toolbar.
@@ -45,12 +44,27 @@ public final class NbMainExplorer extends TopComponent implements ItemListener {
   /** The message formatter for Explorer title */
   private static MessageFormat formatExplorerTitle;
 
+  /** The root nodes displayed as tabs - acquired from Places.roots() */
+  private transient Node[] roots;
   /** ExplorerManagers for roots */
   private transient ExplorerManager[] managers;
+  /** ExplorerManagers for property sheet */
   private transient ExplorerManager sheetManager;
+  /** ExplorerManagers of the currently selected root (tab) */
   private transient ExplorerManager currentManager;
 
-  private transient PropertyChangeListener listener;
+  /** Listener which tracks changes on the managers for each tab and provides synchronization
+  * of rootContext, exploredContext and selectedNodes with property sheet and updating the title of the Explorer */
+  private transient PropertyChangeListener managersListener;
+
+  /** Listener which tracks changes on the root nodes (which are displayed as tabs) */
+  private transient PropertyChangeListener rootsListener;
+
+  /** action handler for cut/copy/paste/delete */
+  private static ExplorerActions actions;
+
+  /** Boolean flag - true, if this component is currently activated (and attached to ExplorerActions), false otherwise */
+  private boolean activated = false;
 
   /** Switchable property view panel */
   private transient ExplorerPanel sheetPanel;
@@ -62,8 +76,9 @@ public final class NbMainExplorer extends TopComponent implements ItemListener {
   private transient ToolbarToggleButton sheetSwitcher;
   /** Flag specifying if property sheet is visible */
   private boolean sheetVisible = false;
-  /** the width of the property sheet pane */
+  /** the default width of the property sheet pane */
   private int sheetWidth = 250;
+  /** the default height of the property sheet pane */
   private int sheetHeight = 400;
 
   /** Default constructor
@@ -75,7 +90,7 @@ public final class NbMainExplorer extends TopComponent implements ItemListener {
     final JTabbedPane tabs = new JTabbedPane ();
     tabs.setTabPlacement (SwingConstants.BOTTOM);
 
-    listener = new PropertyChangeListener () {
+    managersListener = new PropertyChangeListener () {
       public void propertyChange (PropertyChangeEvent evt) {
         if (evt.getSource () == currentManager) {
           if (sheetVisible && (sheetManager != null)) {
@@ -97,26 +112,53 @@ public final class NbMainExplorer extends TopComponent implements ItemListener {
       }
     };
 
-    Node[] roots = getRoots ();
+    rootsListener = new PropertyChangeListener () {
+      public void propertyChange (PropertyChangeEvent evt) {
+        for (int i = 0; i < roots.length; i++) {
+          if (roots[i] == evt.getSource ()) {
+            if (Node.PROP_DISPLAY_NAME.equals (evt.getPropertyName ())) {
+              tabs.setTitleAt (i, roots[i].getDisplayName ());
+            } else if (Node.PROP_ICON.equals (evt.getPropertyName ())) {
+              tabs.setIconAt (i, new ImageIcon (roots [i].getIcon (BeanInfo.ICON_COLOR_16x16)));
+            }
+/* [IAN] - this is just waiting for Sun to fix bug #4158286 : no way to change ToolTip text on tabs in JTabbedPane
+            else if (PROP_SHORT_DESCRIPTION.equals (evt.getPropertyName ())) {
+              tabs.setTooltipAt (i, roots[i].getShortDescription ());
+            } */
+            break;
+          }
+        }
+      }
+    };
+
+
+    roots = getRoots ();
     managers = new ExplorerManager[roots.length];
     for (int i = 0; i < roots.length; i++) {
       ExplorerPanel panel = new ExplorerPanel ();
       managers[i] = panel.getExplorerManager ();
       managers[i].setRootContext (roots[i]);
-      managers[i].addPropertyChangeListener (listener); // synchronization of property sheet, activated nodes, title
+      managers[i].addPropertyChangeListener (managersListener); // synchronization of property sheet, activated nodes, title
       BeanTreeView treeView = new BeanTreeView ();
       panel.setLayout (new BorderLayout ());
       panel.add (treeView);
-      tabs.addTab (roots[i].getDisplayName (), new ImageIcon (roots [i].getIcon (BeanInfo.ICON_COLOR_16x16)), panel); // [PENDING - hint]
+      tabs.addTab (roots[i].getDisplayName (), new ImageIcon (roots [i].getIcon (BeanInfo.ICON_COLOR_16x16)), panel, roots[i].getShortDescription ());
+
+      // add listeners to changes on the roots
+      roots[i].addPropertyChangeListener (rootsListener);
     }
     currentManager = managers[0]; // [PENDING]
 
     // [PENDING - addIconChangeListener]
+    // [PENDING - addNameChangeListener]
 
     tabs.addChangeListener (new javax.swing.event.ChangeListener () { 
         public void stateChanged (javax.swing.event.ChangeEvent evt) {
           int index = tabs.getSelectedIndex ();
           currentManager = managers[index];
+          if (activated) {
+            actions.attach (currentManager);
+          }
           if (sheetVisible && (sheetManager != null)) {
             sheetManager.setRootContext (currentManager.getRootContext ());
             sheetManager.setExploredContext (currentManager.getExploredContext ());
@@ -259,25 +301,34 @@ public final class NbMainExplorer extends TopComponent implements ItemListener {
   /** Removes listeners.
   */
   public boolean close () {
-    for (int i = 0; i < managers.length; i++)
-      managers[i].removePropertyChangeListener (listener);
-    super.close ();
-    return true;
+    if (super.close ()) {
+      for (int i = 0; i < managers.length; i++) {
+        managers[i].removePropertyChangeListener (managersListener);
+      }
+      for (int i = 0; i < roots.length; i++) {
+       roots[i].removePropertyChangeListener (rootsListener);
+      }
+      return true;
+    } else {
+      return false;
+    }
   }
 
   /** Activates copy/cut/paste actions.
   */
   protected void componentActivated () {
-//    if (actions == null) {
-//      actions = new ExplorerActions ();
-//    }
-//    actions.attach (explorer);
+    if (actions == null) {
+      actions = new ExplorerActions ();
+    }
+    actions.attach (currentManager);
+    activated = true;
   }
 
   /** Deactivates copy/cut/paste actions.
   */
   protected void componentDeactivated () {
-//    actions.detach ();
+    activated = false;
+    actions.detach ();
   }
   
 
@@ -300,6 +351,9 @@ public final class NbMainExplorer extends TopComponent implements ItemListener {
 
 /*
 * Log
+*  3    Gandalf   1.2         3/16/99  Ian Formanek    Added listening to icon 
+*       and displayName changes on roots, support for ExplorerActions 
+*       (Cut/Copy/...)
 *  2    Gandalf   1.1         3/15/99  Ian Formanek    Added formatting of 
 *       title, updating activatedNodes
 *  1    Gandalf   1.0         3/14/99  Ian Formanek    
