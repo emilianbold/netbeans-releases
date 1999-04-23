8bcdaf114bb6aa08f099f972b3803108583b9df8
Petr_Hrebejk <devnull@localhost>
1999-04-23 17:20:27 +0000

    #

---
+++ javadoc/src/org/netbeans/modules/javadoc/JavadocModule.java
@@ -0,0 +1,129 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is Forte for Java, Community Edition. The Initial
+ * Developer of the Original Code is Sun Microsystems, Inc. Portions
+ * Copyright 1997-2000 Sun Microsystems, Inc. All Rights Reserved.
+ */
+
+package com.netbeans.developer.modules.javadoc;
+ 
+import java.io.*;
+
+// IDE imports ------------------
+
+import com.netbeans.ide.util.actions.SystemAction;
+import com.netbeans.ide.modules.ModuleInstall;
+import com.netbeans.ide.loaders.DataFolder;
+import com.netbeans.ide.loaders.DataLoader;
+import com.netbeans.ide.TopManager;
+
+import javax.swing.event.*;
+
+// MODULE imports ---------------
+
+import com.netbeans.developer.modules.loaders.java.JavaDataObject;
+import com.netbeans.developer.modules.loaders.java.JavaNode;
+
+/** Class for initializing Javadoc module on IDE startup.
+
+ @author Petr Hrebejk
+*/
+public class JavadocModule implements ModuleInstall {
+  
+  static {
+  }
+
+  /** By first install of module in the IDE do nothing. 
+  */
+  public void installed() {
+  }
+
+  /** By uninstalling module from the IDE do nothing. 
+  */
+  public void uninstalled () {
+  }
+  
+  /** Called on IDE startup. Registers actions for generating documentation 
+  * on DataFolder and JavaDataObject.
+  */
+  public void restored() {
+    
+    // Install DataFolder action
+    installActions( TopManager.getDefault().getLoaderPool().firstProducerOf( DataFolder.class ),
+      2, new SystemAction[] { null, SystemAction.get( GenerateDocAction.class ) } );
+
+    // Install JavaDataObject action
+    
+    DataLoader javaLoader = TopManager.getDefault().getLoaderPool().firstProducerOf( JavaDataObject.class );
+    if (javaLoader != null) {
+      installActions(javaLoader, 2, new SystemAction[] { null, SystemAction.get( GenerateDocAction.class ) } );
+    }
+    else {
+      TopManager.getDefault().getLoaderPool().addChangeListener(new ChangeListener() {
+        public void stateChanged(ChangeEvent e) {
+          DataLoader javaL = TopManager.getDefault().getLoaderPool().firstProducerOf( JavaDataObject.class );
+          if (javaL != null) {
+            installActions(javaL, 2, new SystemAction[] { null, SystemAction.get( GenerateDocAction.class ) } );
+            TopManager.getDefault().getLoaderPool().removeChangeListener(this);
+          }
+        }
+      });
+    }
+
+    // System.out.println ( "Javadoc module installed .." );
+  }
+	
+  /** Called before exiting IDE. 
+  * @return Allways <CODE>true</CODE>.
+  */
+  public boolean closing() {
+    return true;
+  }
+
+ 
+
+  /** Installs sa[] actions on position <code>index</code> bottom of menu 
+  */
+  private void installActions ( DataLoader dl, int index, SystemAction sa[] ) {
+    int          i, j; 
+    SystemAction old_sa[], new_sa[]; 
+
+    //System.out.println ( dl );
+
+    old_sa = dl.getActions();
+
+    new_sa = new SystemAction[ old_sa.length + sa.length ];
+     
+    // Old actions up to index
+    for (i = 0; i < old_sa.length - index; i++) {
+      new_sa[i] = old_sa[i];
+    } 
+
+    // New actions
+    for (j = 0; i < old_sa.length - index + sa.length ; i++, j++ ) {
+      new_sa[i] = sa[j];
+      }
+    
+    // Rest of old actions
+    for (; i < old_sa.length + sa.length; i++ ) {
+      new_sa[i] = old_sa[i - index];
+    }
+
+    dl.setActions( new_sa );
+  }
+
+
+
+}
+
+/* 
+ * Log
+ *  1    Gandalf   1.0         4/23/99  Petr Hrebejk    
+ * $ 
+ */ 
---
+++ javadoc/src/org/netbeans/modules/javadoc/settings/DocumentationSettings.java
@@ -0,0 +1,95 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is Forte for Java, Community Edition. The Initial
+ * Developer of the Original Code is Sun Microsystems, Inc. Portions
+ * Copyright 1997-2000 Sun Microsystems, Inc. All Rights Reserved.
+ */
+
+package com.netbeans.developer.modules.javadoc.settings;
+
+import java.io.File;
+
+import com.netbeans.ide.options.ContextSystemOption;
+import com.netbeans.ide.util.NbBundle;
+
+/** Options for applets - which applet viewer use ...
+*
+* @author Petr Hrebejk
+* @version 0.1, Apr 15, 1999
+*/
+public class DocumentationSettings extends ContextSystemOption //implements ViewerConstants 
+  {
+  /** generated Serialized Version UID */
+  //static final long serialVersionUID = 605615362662343329L;
+
+  /** generation */
+  private static boolean externalJavadoc = false;
+
+  /** searchpath */
+  private static String[] searchPath = new String[] {"c:/Jdk1.2/doc" };
+  
+
+  // Private attributes for option's children
+
+  private static JavadocSettings javadocSettings;
+  private static StdDocletSettings stdDocletSettings;
+
+
+  static {
+    // Create option's children
+    javadocSettings  = new JavadocSettings ();
+    stdDocletSettings =  new StdDocletSettings ();
+  }
+
+  /** Constructor for DocumentationSettings adds optipn's children */
+  public DocumentationSettings () {
+    addOption( javadocSettings );
+    addOption( stdDocletSettings );
+  }
+    
+  
+  /** @return human presentable name */
+  public String displayName() {
+    return NbBundle.getBundle(JavadocSettings.class).getString("CTL_Documentation_settings");
+  }
+
+  /** getter for type of generation 
+  */
+  public boolean isExternalJavadoc () {
+    return externalJavadoc;
+  }
+
+  /** setter for viewer */
+  public void setExternalJavadoc(boolean b) {
+    externalJavadoc = b;
+    /*
+    if (v.equals(INTERNAL_BROWSER) || v.equals(APPLETVIEWER) || v.equals(EXTERNAL))
+      viewer = v;
+    */
+  }
+
+  /** Getter for documentation search path
+  */
+  public String[] getSearchPath() {
+    return searchPath;
+  }
+
+  /** Setter for documentation search path
+  */
+  public void setSearchPath(String[] s) {
+    searchPath = s;
+  }
+
+}
+
+/*
+ * Log
+ *  1    Gandalf   1.0         4/23/99  Petr Hrebejk    
+ * $
+ */
