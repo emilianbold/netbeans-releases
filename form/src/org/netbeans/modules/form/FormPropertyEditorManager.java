516f46bc7b7d136dfeb25ce41c933de7d448045b
Ian_Formanek <devnull@localhost>
1999-05-30 15:59:39 +0000

    minor changes, editors cache disabled

--- form/src/org/netbeans/modules/form/FormPropertyEditorManager.java
+++ form/src/org/netbeans/modules/form/FormPropertyEditorManager.java
@@ -16,7 +16,6 @@ package com.netbeans.developer.modules.loaders.form;
 import com.netbeans.ide.TopManager;
 
 import java.beans.PropertyEditor;
-import java.beans.PropertyEditorManager;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -25,11 +24,11 @@ import java.util.HashMap;
 * @author   Ian Formanek
 */
 final public class FormPropertyEditorManager extends Object {
-  private FormLoaderSettings formSettings = new FormLoaderSettings ();
+  private static FormLoaderSettings formSettings = new FormLoaderSettings ();
   private static HashMap editorsCache = new HashMap (30);
 
-  public static synchronized PropertyEditor getDefaultEditor (Class type) {
-    PropertyEditor[] eds = getAllEditors (type);
+  public static synchronized PropertyEditor findEditor (Class type) {
+    PropertyEditor[] eds = getAllEditors (type, false);
     if (eds.length > 0) {
       return eds[0];
     } else {
@@ -37,7 +36,7 @@ final public class FormPropertyEditorManager extends Object {
     }
   }
   
-  public static synchronized PropertyEditor[] getAllEditors (Class type) {
+  public static synchronized PropertyEditor[] getAllEditors (Class type, boolean allFromSearchPath) {
     PropertyEditor[] eds = (PropertyEditor[])editorsCache.get (type);
     if (eds != null) {
       return eds;
@@ -46,24 +45,36 @@ final public class FormPropertyEditorManager extends Object {
     ArrayList editorsList = new ArrayList (5);
 
     // First use explicitly registered editors
-/*    Map registered = formSettings.getRegisteredEditors ();
-    String[] names = registered.get (type.getName ());
-    if ((names != null) && (names.length > 0)) {
-      for (int i = 0; i < names.length; i++) {
-        try {
-          editorsList.add (Class.forName (names[i], true, TopManager.getDefault ().systemClassLoader ()).newInstance ());
-        } catch (Exception e) {
-          // Silently ignore any errors.
-        } catch (Throwable t) {
-          if (t instanceof ThreadDeath) {
-            throw (ThreadDeath)t;
+    String [][] registered = formSettings.getRegisteredEditors ();
+    for (int i = 0; i < registered.length; i++) {
+      if (registered[i].length > 0) {
+        String typeName = type.getName ();
+        if (type.isPrimitive ()) {
+          if (Byte.TYPE.equals (type)) typeName = "byte";
+          else if (Short.TYPE.equals (type)) typeName = "short";
+          else if (Integer.TYPE.equals (type)) typeName = "integer";
+          else if (Long.TYPE.equals (type)) typeName = "long";
+          else if (Boolean.TYPE.equals (type)) typeName = "boolean";
+          else if (Float.TYPE.equals (type)) typeName = "float";
+          else if (Double.TYPE.equals (type)) typeName = "double";
+        }
+        if (registered[i][0].equals (typeName)) {
+          for (int j = 1; j < registered[i].length; j++) {
+            try {
+              editorsList.add (Class.forName (registered[i][j], true, TopManager.getDefault ().systemClassLoader ()).newInstance ());
+            } catch (Exception e) {
+              // Silently ignore any errors.
+            } catch (Throwable t) { // [PENDING] should not be necessary
+              if (t instanceof ThreadDeath) {
+                throw (ThreadDeath)t;
+              }
+              t.printStackTrace ();
+              // Silently ignore any errors.
+            }
           }
-          t.printStackTrace ();
-          // Silently ignore any errors.
         }
       }
     }
-*/      // [PENDING]
 
     // Second try adding "Editor" to the class name.
     String editorName = type.getName() + "Editor";
@@ -80,7 +91,7 @@ final public class FormPropertyEditorManager extends Object {
     }
 
     // Third try looking for <searchPath>.fooEditor
-    String[] searchPath = PropertyEditorManager.getEditorSearchPath (); // formSettings.getEditorSearchPath (); // [PENDING]
+    String[] searchPath = formSettings.getEditorSearchPath ();
     
     editorName = type.getName();
     while (editorName.indexOf('.') > 0) {
@@ -90,6 +101,9 @@ final public class FormPropertyEditorManager extends Object {
       String name = searchPath[i] + "." + editorName + "Editor";
       try {
         editorsList.add (Class.forName (name, true, TopManager.getDefault ().systemClassLoader ()).newInstance ());
+        if (!allFromSearchPath) {
+          break; // stop on first found editor if allFromSearchPath is false
+        }
       } catch (Exception e) {
         // Silently ignore any errors.
       } catch (Throwable t) {
@@ -106,12 +120,12 @@ final public class FormPropertyEditorManager extends Object {
     editorsList.toArray (eds);
     
     // Cache the list for future reuse
-    editorsCache.put (type, eds);
+    //editorsCache.put (type, eds);
     
     return eds;
   }
   
-  synchronized void clearEditorsCache () {
+  synchronized static void clearEditorsCache () {
     editorsCache.clear ();
   }
   
@@ -119,6 +133,8 @@ final public class FormPropertyEditorManager extends Object {
 
 /*
  * Log
+ *  2    Gandalf   1.1         5/30/99  Ian Formanek    minor changes, editors 
+ *       cache disabled
  *  1    Gandalf   1.0         5/24/99  Ian Formanek    
  * $
  */
