42d7700c33f1bdc3081c142c24ad8384c4aae442
Jan_Jancura <devnull@localhost>
1999-09-02 15:08:26 +0000

    #

--- debugger.jpda/src/org/netbeans/modules/debugger/jpda/util/Operator.java
+++ debugger.jpda/src/org/netbeans/modules/debugger/jpda/util/Operator.java
@@ -13,9 +13,9 @@
 
 package com.netbeans.developer.modules.debugger.jpda.util;
 
+import com.sun.jdi.VirtualMachine;
 import com.sun.jdi.event.*;
 import com.sun.jdi.request.EventRequest;
-
 
 /**
 * Operator listens on debugger events and redirects them to the proper Executor.
@@ -24,12 +24,19 @@ import com.sun.jdi.request.EventRequest;
 */
 public class Operator {
 
+  private boolean           resume = false;
+  
   /**
-  * Creates operator.
+  * Creates operator for given event queue.
   */
-  public Operator (final EventQueue queue, final Runnable finalizer) {
+  public Operator (
+    final VirtualMachine virtualMachine, 
+    final Runnable starter,
+    final Runnable finalizer
+  ) {
     new Thread (new Runnable () {
       public void run () {
+        EventQueue queue = virtualMachine.eventQueue ();
         try {
           for (;;) {
             EventSet set = queue.remove ();
@@ -40,35 +47,84 @@ public class Operator {
                   (e instanceof VMDisconnectEvent)
               ) {
                 if (finalizer != null) finalizer.run ();
-System.out.println ("EVENT: " + e);          
-System.out.println ("Operator end");          
+                //S ystem.out.println ("EVENT: " + e);          
+                //S ystem.out.println ("Operator end");          
                 return;
               }    
-              if (e.request () == null) {
-System.out.println ("EVENT: " + e);          
+              if ((e instanceof VMStartEvent) && (starter != null)) {
+                starter.run ();
+                //S ystem.out.println ("Operator.start VM");          
                 continue;
-              }  
-              Executor exec = (Executor) e.request ().getProperty ("executor");
-System.out.println ("EVENT: " + e + " : " + exec);          
-              if (exec != null) exec.exec (e);
+              }
+              Executor exec = null;
+              if (e.request () == null) {
+                //S ystem.out.println ("EVENT: " + e + " REQUEST: null");          
+              } else 
+                exec = (Executor) e.request ().getProperty ("executor");
+              
+              //printEvent (e, exec);
+
+              // safe invocation of user action
+              if (exec != null) 
+                try {
+                  exec.exec (e);
+                } catch (Exception ex) {
+                  ex.printStackTrace ();
+                } catch (Error ex) {
+                  ex.printStackTrace ();
+                }
             }
-System.out.println ("END (" + set.suspendPolicy () + ") ===========================================================================");
+            //S ystem.out.println ("END (" + set.suspendPolicy () + ") ===========================================================================");
+            if (resume) {
+              resume = false;
+              virtualMachine.resume ();
+            }
           }
         } catch (InterruptedException e) {
         }
         if (finalizer != null) finalizer.run ();
-System.out.println ("Operator end");          
+        //S ystem.out.println ("Operator end");          
       }
     }, "Debugger operator thread").start ();
   }
   
-  public static void register (EventRequest req, Executor e) {
+  public void register (EventRequest req, Executor e) {
     req.putProperty ("executor", e);
+  }
+  
+  /**
+  * Calls resume after curent event set dispatch.
+  */
+  public void resume () {
+    resume = true;
+  }
+  
+  private void printEvent (Event e, Executor exec) {
+    if (e instanceof ClassPrepareEvent) {
+      System.out.println ("EVENT: ClassPrepareEvent " + ((ClassPrepareEvent) e).referenceType ());
+    } else
+    if (e instanceof ClassUnloadEvent) {
+      System.out.println ("EVENT: ClassUnloadEvent " + ((ClassUnloadEvent) e).className ());
+    } else
+    if (e instanceof ThreadStartEvent) {
+      System.out.println ("EVENT: ThreadStartEvent " + ((ThreadStartEvent) e).thread ());
+    } else
+    if (e instanceof ThreadDeathEvent) {
+      System.out.println ("EVENT: ThreadDeathEvent " + ((ThreadDeathEvent) e).thread ());
+    } else
+    if (e instanceof BreakpointEvent) {
+      System.out.println ("EVENT: BreakpointEvent " + ((BreakpointEvent) e).thread () + " : " + ((BreakpointEvent) e).location ());
+    } else
+    if (e instanceof StepEvent) {
+      System.out.println ("EVENT: BreakpointEvent " + ((StepEvent) e).thread () + " : " + ((StepEvent) e).location ());
+    } else
+      System.out.println ("EVENT: " + e + " : " + exec);          
   }
 }
 
 /*
  * Log
+ *  2    Gandalf   1.1         9/2/99   Jan Jancura     
  *  1    Gandalf   1.0         7/13/99  Jan Jancura     
  * $
  */
