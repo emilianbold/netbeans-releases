0898fdb024c98af51b6773d8f7d6579033f2655c
Jesse_Glick <devnull@localhost>
1999-10-01 18:53:19 +0000

    #

---
+++ o.n.core/src/org/netbeans/beaninfo/ServiceTypeBeanInfo.java
@@ -0,0 +1,42 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is Forte for Java, Community Edition. The Initial
+ * Developer of the Original Code is Sun Microsystems, Inc. Portions
+ * Copyright 1997-2000 Sun Microsystems, Inc. All Rights Reserved.
+ */
+
+package com.netbeans.developer.impl.beaninfo;
+
+import java.beans.*;
+
+import org.openide.ServiceType;
+import org.openide.util.NbBundle;
+
+/** BeanInfo for ServiceType. Has name property.
+* @author Jesse Glick
+*/
+public class ServiceTypeBeanInfo extends SimpleBeanInfo {
+  
+  public PropertyDescriptor[] getPropertyDescriptors () {
+    try {
+      PropertyDescriptor name = new PropertyDescriptor ("name", ServiceType.class);
+      name.setDisplayName (NbBundle.getBundle (ServiceTypeBeanInfo.class).getString ("PROP_ServiceType_name"));
+      name.setShortDescription (NbBundle.getBundle (ServiceTypeBeanInfo.class).getString ("HINT_ServiceType_name"));
+      // Is there an easier way to prevent this from appearing??
+      PropertyDescriptor helpCtx = new PropertyDescriptor ("helpCtx", ServiceType.class, "getHelpCtx", null);
+      helpCtx.setHidden (true);
+      return new PropertyDescriptor[] { name, helpCtx };
+    } catch (IntrospectionException ie) {
+      if (Boolean.getBoolean ("netbeans.debug.exceptions"))
+        ie.printStackTrace ();
+      return null;
+    }
+  }
+  
+}
