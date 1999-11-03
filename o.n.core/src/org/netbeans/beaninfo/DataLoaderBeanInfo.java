57a85c848af28c6acc426863ea1c904b8a892749
Jesse_Glick <devnull@localhost>
1999-11-03 13:09:56 +0000

    #

---
+++ o.n.core/src/org/netbeans/beaninfo/DataLoaderBeanInfo.java
@@ -0,0 +1,36 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is Forte for Java, Community Edition. The Initial
+ * Developer of the Original Code is Sun Microsystems, Inc. Portions
+ * Copyright 1997-2000 Sun Microsystems, Inc. All Rights Reserved.
+ */
+
+package com.netbeans.developer.impl.beaninfo;
+
+import java.beans.*;
+
+import org.openide.loaders.DataLoader;
+import org.openide.util.NbBundle;
+
+/** BeanInfo for {@link DataLoader}. */
+public class DataLoaderBeanInfo extends SimpleBeanInfo {
+  
+  public PropertyDescriptor[] getPropertyDescriptors () {
+    return new PropertyDescriptor[] { };
+    // [PENDING] maybe later "actions" should be included--once
+    // it has a decent property editor, that is
+  }
+  
+}
+
+/*
+ * Log
+ *  1    Gandalf   1.0         11/3/99  Jesse Glick     
+ * $
+ */
---
+++ o.n.core/src/org/netbeans/beaninfo/UniFileLoaderBeanInfo.java
@@ -0,0 +1,43 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is Forte for Java, Community Edition. The Initial
+ * Developer of the Original Code is Sun Microsystems, Inc. Portions
+ * Copyright 1997-2000 Sun Microsystems, Inc. All Rights Reserved.
+ */
+
+package com.netbeans.developer.impl.beaninfo;
+
+import java.beans.*;
+
+import org.openide.loaders.UniFileLoader;
+import org.openide.util.NbBundle;
+
+/** BeanInfo for {@link UniFileLoader}. */
+public class UniFileLoaderBeanInfo extends SimpleBeanInfo {
+  
+  public PropertyDescriptor[] getPropertyDescriptors () {
+    try {
+      PropertyDescriptor extensions = new PropertyDescriptor ("extensions", UniFileLoader.class);
+      extensions.setDisplayName (NbBundle.getBundle (UniFileLoaderBeanInfo.class).getString ("PROP_UniFileLoader_extensions"));
+      extensions.setShortDescription (NbBundle.getBundle (UniFileLoaderBeanInfo.class).getString ("HINT_UniFileLoader_extensions"));
+      return new PropertyDescriptor[] { extensions };
+    } catch (IntrospectionException ie) {
+      if (Boolean.getBoolean ("netbeans.debug.exceptions"))
+        ie.printStackTrace ();
+      return null;
+    }
+  }
+  
+}
+
+/*
+ * Log
+ *  1    Gandalf   1.0         11/3/99  Jesse Glick     
+ * $
+ */
