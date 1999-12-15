252d66463ad5a7202651360b9e057d8517988880
Radko_Najman <devnull@localhost>
1999-12-15 17:43:45 +0000

    #

---
+++ db/libsrc/org/netbeans/lib/ddl/DriverProductNotFoundException.java
@@ -0,0 +1,61 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2001 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package com.netbeans.ddl;
+
+/** 
+* System is not able to locate appropriate resources to create DriverSpecification object
+* (object describing the driver). It means that driver product is not supported by system.
+* You can write your own description file. If you are sure that it is, please check
+* location of description files.
+*
+* @author Radko Najman
+*/
+public class DriverProductNotFoundException extends Exception 
+{
+  static final long serialVersionUID =-1108211224066947350L;
+  
+	/** Driver name */
+	private String drvName;
+
+	/** Creates new exception
+	* @param desc The text describing the exception
+	*/
+	public DriverProductNotFoundException(String spec) {
+		super ();
+		drvName = spec;    
+	}
+
+	/** Creates new exception with text specified string.
+	* @param spec Driver name
+	* @param desc The text describing the exception
+	*/
+	public DriverProductNotFoundException(String spec, String desc) {
+		super (desc);
+		drvName = spec;
+	}
+	
+	/** Returns driver name.
+	* This driver is not supported by system. You can write your own description file.
+	*/
+	public String getDriverName()
+	{
+		return drvName;
+	}
+}
+
+/*
+ * <<Log>>
+ *  1    Gandalf   1.0         12/15/99 Radko Najman    
+ * $
+ */
---
+++ db/libsrc/org/netbeans/lib/ddl/DriverSpecificationFactory.java
@@ -0,0 +1,54 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2001 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package com.netbeans.ddl;
+
+import java.sql.*;
+import java.util.Set;
+import com.netbeans.ddl.*;
+
+/** 
+* The factory interface used for creating instances of DriverSpecification class. 
+* DriverSpecificationFactory collects information about available driver 
+* description files. Then it's able to specify if system can control 
+* the driver (specified by product name). It also provides a list of supported
+* drivers.
+*
+* @author Radko Najman
+*/
+public interface DriverSpecificationFactory {
+
+	/** Returns array of driver products supported by system. It returns
+	* string array only, not the DriverSpecification array.
+	*/
+	public Set supportedDrivers();
+	
+	/** Returns true if driver (specified by driverName) is 
+	* supported by system. Does not throw exception if it doesn't.
+	* @param ddriverName Driver product name as given from DatabaseMetaData
+	* @return True if driver product is supported.
+	*/	
+	public boolean isDriverSupported(String driverName);
+	
+	/** Creates instance of DriverSpecification class; a driver-specification
+	* class. This object knows about used driver.
+	* @param driverName Driver name
+	* @return DriverSpecification object.
+	*/
+	public DriverSpecification createDriverSpecification(String driverName) throws DriverProductNotFoundException;
+
+}
+
+/*
+* <<Log>>
+*/
