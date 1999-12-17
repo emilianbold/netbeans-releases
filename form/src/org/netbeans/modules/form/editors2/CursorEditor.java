0fa17ea509ae9072c11ebd0df4cf6e196e34245d
Pavel_Buzek <devnull@localhost>
1999-12-17 15:51:45 +0000

    support for saving into XML

--- form/src/org/netbeans/modules/form/editors2/CursorEditor.java
+++ form/src/org/netbeans/modules/form/editors2/CursorEditor.java
@@ -26,9 +26,10 @@ import javax.swing.*;
  * @version 
  */
 
-public class CursorEditor extends PropertyEditorSupport  implements EnhancedPropertyEditor {
+public class CursorEditor extends PropertyEditorSupport  implements EnhancedPropertyEditor, org.openide.explorer.propertysheet.editors.XMLPropertyEditor {
 
   private static HashMap CURSOR_TYPES = new HashMap ();
+  private static HashMap CURSOR_CONSTANTS = new HashMap ();
   static {
     CURSOR_TYPES.put (new Cursor (Cursor.CROSSHAIR_CURSOR).getName(), new Integer (Cursor.CROSSHAIR_CURSOR));
     CURSOR_TYPES.put (new Cursor (Cursor.DEFAULT_CURSOR).getName(), new Integer (Cursor.DEFAULT_CURSOR));
@@ -44,6 +45,21 @@ public class CursorEditor extends PropertyEditorSupport  implements EnhancedProp
     CURSOR_TYPES.put (new Cursor (Cursor.TEXT_CURSOR).getName(), new Integer (Cursor.TEXT_CURSOR));
     CURSOR_TYPES.put (new Cursor (Cursor.W_RESIZE_CURSOR).getName(), new Integer (Cursor.W_RESIZE_CURSOR));
     CURSOR_TYPES.put (new Cursor (Cursor.WAIT_CURSOR).getName(), new Integer (Cursor.WAIT_CURSOR));
+
+    CURSOR_CONSTANTS.put (new Integer (Cursor.CROSSHAIR_CURSOR), "java.awt.Cursor.CROSSHAIR_CURSOR");
+    CURSOR_CONSTANTS.put (new Integer (Cursor.DEFAULT_CURSOR), "java.awt.Cursor.DEFAULT_CURSOR");
+    CURSOR_CONSTANTS.put (new Integer (Cursor.E_RESIZE_CURSOR), "java.awt.Cursor.E_RESIZE_CURSOR");
+    CURSOR_CONSTANTS.put (new Integer (Cursor.HAND_CURSOR), "java.awt.Cursor.HAND_CURSOR");
+    CURSOR_CONSTANTS.put (new Integer (Cursor.MOVE_CURSOR), "java.awt.Cursor.MOVE_CURSOR");
+    CURSOR_CONSTANTS.put (new Integer (Cursor.N_RESIZE_CURSOR), "java.awt.Cursor.N_RESIZE_CURSOR");
+    CURSOR_CONSTANTS.put (new Integer (Cursor.NE_RESIZE_CURSOR), "java.awt.Cursor.NE_RESIZE_CURSOR");
+    CURSOR_CONSTANTS.put (new Integer (Cursor.NW_RESIZE_CURSOR), "java.awt.Cursor.NW_RESIZE_CURSOR");
+    CURSOR_CONSTANTS.put (new Integer (Cursor.S_RESIZE_CURSOR), "java.awt.Cursor.S_RESIZE_CURSOR");
+    CURSOR_CONSTANTS.put (new Integer (Cursor.SE_RESIZE_CURSOR), "java.awt.Cursor.SE_RESIZE_CURSOR");
+    CURSOR_CONSTANTS.put (new Integer (Cursor.SW_RESIZE_CURSOR), "java.awt.Cursor.SW_RESIZE_CURSOR");
+    CURSOR_CONSTANTS.put (new Integer (Cursor.TEXT_CURSOR), "java.awt.Cursor.TEXT_CURSOR");
+    CURSOR_CONSTANTS.put (new Integer (Cursor.W_RESIZE_CURSOR), "java.awt.Cursor.W_RESIZE_CURSOR");
+    CURSOR_CONSTANTS.put (new Integer (Cursor.WAIT_CURSOR), "java.awt.Cursor.WAIT_CURSOR");
   }
   
   Cursor current;
@@ -111,11 +127,11 @@ public class CursorEditor extends PropertyEditorSupport  implements EnhancedProp
   } 
 
   public String getJavaInitializationString () {
-    if (current == null) {
-      return null; // no code to generate
-    } else {     
-      return "new java.awt.Cursor ("+current.getType()+")";
-    }
+    if (current == null) return null; // no code to generate
+    String cursorName = (String) CURSOR_CONSTANTS.get (new Integer (current.getType()));
+    if (cursorName != null)
+      return "new java.awt.Cursor ("+cursorName+")";
+    return "new java.awt.Cursor ("+current.getType()+")";
   }
 
   class CursorPanel extends JPanel implements EnhancedCustomPropertyEditor {
@@ -152,10 +168,50 @@ public class CursorEditor extends PropertyEditorSupport  implements EnhancedProp
 
     private JList list;
   }
+  
+//--------------------------------------------------------------------------
+// XMLPropertyEditor implementation
+
+  public static final String XML_CURSOR = "Color";
+
+  public static final String ATTR_ID = "id";
+
+  /** Called to load property value from specified XML subtree. If succesfully loaded, 
+  * the value should be available via the getValue method.
+  * An IOException should be thrown when the value cannot be restored from the specified XML element
+  * @param element the XML DOM element representing a subtree of XML from which the value should be loaded
+  * @exception IOException thrown when the value cannot be restored from the specified XML element
+  */
+  public void readFromXML (org.w3c.dom.Node element) throws java.io.IOException {
+    if (!XML_CURSOR.equals (element.getNodeName ())) {
+      throw new java.io.IOException ();
+    }
+    org.w3c.dom.NamedNodeMap attributes = element.getAttributes ();
+    try {
+      String id = attributes.getNamedItem (ATTR_ID).getNodeValue ();
+      setAsText (id);
+    } catch (NullPointerException e) {
+      throw new java.io.IOException ();
+    }
+  }
+  
+  /** Called to store current property value into XML subtree. The property value should be set using the
+  * setValue method prior to calling this method.
+  * @param doc The XML document to store the XML in - should be used for creating nodes only
+  * @return the XML DOM element representing a subtree of XML from which the value should be loaded
+  */
+  public org.w3c.dom.Node storeToXML(org.w3c.dom.Document doc) {
+    org.w3c.dom.Element el = doc.createElement (XML_CURSOR);
+    el.setAttribute (ATTR_ID, getAsText ());
+    return el;
+  }
+  
 }
 
 /*
  * Log
+ *  3    Gandalf   1.2         12/17/99 Pavel Buzek     support for saving into 
+ *       XML
  *  2    Gandalf   1.1         12/17/99 Pavel Buzek     
  *  1    Gandalf   1.0         12/17/99 Pavel Buzek     
  * $
