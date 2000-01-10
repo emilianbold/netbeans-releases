5cad0e69e8cadd7af2fc02453d931291ba19aeec
Ian_Formanek <devnull@localhost>
2000-01-10 10:02:14 +0000

    Removed Look&Feel property

--- o.n.core/src/org/netbeans/core/IDESettingsBeanInfo.java
+++ o.n.core/src/org/netbeans/core/IDESettingsBeanInfo.java
@@ -17,8 +17,6 @@ import java.awt.Image;
 import java.beans.*;
 import java.util.Hashtable;
 import java.util.Vector;
-import javax.swing.LookAndFeel;
-import javax.swing.UIManager;
 
 /** A BeanInfo for global IDE settings.
 *
@@ -36,8 +34,6 @@ public class IDESettingsBeanInfo extends SimpleBeanInfo {
   static {
     try {
       desc = new PropertyDescriptor[] {
-        new PropertyDescriptor (IDESettings.PROP_LOOK_AND_FEEL, IDESettings.class,
-                                "getLookAndFeel", "setLookAndFeel"),
         new PropertyDescriptor (IDESettings.PROP_SHOW_TIPS_ON_STARTUP, IDESettings.class,
                                 "getShowTipsOnStartup", "setShowTipsOnStartup"),
         new PropertyDescriptor (IDESettings.PROP_LAST_TIP, IDESettings.class,
@@ -56,31 +52,27 @@ public class IDESettingsBeanInfo extends SimpleBeanInfo {
                                 "getProxyPort", "setProxyPort"),
       };
 
-      desc[0].setDisplayName (Main.getString ("PROP_LOOK_AND_FEEL"));
-      desc[0].setShortDescription (Main.getString ("HINT_LOOK_AND_FEEL"));
-      desc[0].setPropertyEditorClass (IDESettingsBeanInfo.LookAndFeelPropertyEditor.class);
+      desc[0].setDisplayName (Main.getString ("PROP_SHOW_TIPS_ON_STARTUP"));
+      desc[0].setShortDescription (Main.getString ("HINT_SHOW_TIPS_ON_STARTUP"));
 
-      desc[1].setDisplayName (Main.getString ("PROP_SHOW_TIPS_ON_STARTUP"));
-      desc[1].setShortDescription (Main.getString ("HINT_SHOW_TIPS_ON_STARTUP"));
+      desc[1].setHidden (true);
 
-      desc[2].setHidden (true);
+      desc[2].setDisplayName (Main.getString ("PROP_CONFIRM_DELETE"));
+      desc[2].setShortDescription (Main.getString ("HINT_CONFIRM_DELETE"));
 
-      desc[3].setDisplayName (Main.getString ("PROP_CONFIRM_DELETE"));
-      desc[3].setShortDescription (Main.getString ("HINT_CONFIRM_DELETE"));
+      desc[3].setHidden(true);
 
-      desc[4].setHidden(true);
-
-      desc[5].setDisplayName (Main.getString ("PROP_HOME_PAGE"));
-      desc[5].setShortDescription (Main.getString ("HINT_HOME_PAGE"));
+      desc[4].setDisplayName (Main.getString ("PROP_HOME_PAGE"));
+      desc[4].setShortDescription (Main.getString ("HINT_HOME_PAGE"));
       
-      desc[6].setDisplayName (Main.getString ("PROP_USE_PROXY"));
-      desc[6].setShortDescription (Main.getString ("HINT_USE_PROXY"));
+      desc[5].setDisplayName (Main.getString ("PROP_USE_PROXY"));
+      desc[5].setShortDescription (Main.getString ("HINT_USE_PROXY"));
 
-      desc[7].setDisplayName (Main.getString ("PROP_PROXY_HOST"));
-      desc[7].setShortDescription (Main.getString ("HINT_PROXY_HOST"));
+      desc[6].setDisplayName (Main.getString ("PROP_PROXY_HOST"));
+      desc[6].setShortDescription (Main.getString ("HINT_PROXY_HOST"));
 
-      desc[8].setDisplayName (Main.getString ("PROP_PROXY_PORT"));
-      desc[8].setShortDescription (Main.getString ("HINT_PROXY_PORT"));
+      desc[7].setDisplayName (Main.getString ("PROP_PROXY_PORT"));
+      desc[7].setShortDescription (Main.getString ("HINT_PROXY_PORT"));
 
     } catch (IntrospectionException ex) {
       if (System.getProperty ("netbeans.debug.exceptions") != null) ex.printStackTrace();
@@ -104,72 +96,13 @@ public class IDESettingsBeanInfo extends SimpleBeanInfo {
       return icon32;
   }
 
-  public static class LookAndFeelPropertyEditor extends PropertyEditorSupport {
-    /** The LookAndFeels. */
-    private static java.util.Vector lfs;
-    /** Names for LookAndFeels. */
-    private static String[] plafNames;
-    /** mapping LookAndFeel -> index in the table */
-    private static Hashtable lafToIndex;
-
-    static {
-      // populate the list of available and supported Look&Feels
-      UIManager.LookAndFeelInfo[] plafInfos = UIManager.getInstalledLookAndFeels();
-
-      lfs = new java.util.Vector();
-      lafToIndex = new Hashtable ();
-
-      for (int i=0; i<plafInfos.length; i++) {
-        try {
-          Class lfClass = Class.forName(plafInfos[i].getClassName());
-          LookAndFeel laf = (LookAndFeel)lfClass.newInstance();
-          if (laf.isSupportedLookAndFeel ()) {
-            lfs.addElement (laf);
-          }
-        } catch (Exception e) {
-          // problem with L&F - ignore that L&F
-        }
-      }
-      plafNames = new String[lfs.size()];
-      for (int i=0; i < lfs.size (); i++) {
-        LookAndFeel laf = (LookAndFeel)lfs.elementAt (i);
-        plafNames [i] = laf.getName ();
-        lafToIndex.put (laf.getClass (), new Integer (i));
-      }
-    }
-
-    /** @return names of the supported LookAndFeels */
-    public String[] getTags () {
-      return plafNames;
-    }
-
-    /** @return text for the current value */
-    public String getAsText () {
-      Object obj = getValue ();
-      if (obj != null) {
-        Integer index = (Integer) lafToIndex.get (obj.getClass ());
-        if (index != null)
-          return plafNames [index.intValue ()];
-      }
-      return "";
-    }
-
-    /** @param text A text for the current value. */
-    public void setAsText (String text) {
-      for (int i=0; i < plafNames.length; i++)
-        if (plafNames[i].equals (text)) {
-          setValue (lfs.elementAt (i));
-          return;
-        }
-      throw new IllegalArgumentException ();
-    }
-
-  }
 }
 
 
 /*
  * Log
+ *  13   Gandalf   1.12        1/10/00  Ian Formanek    Removed Look&Feel 
+ *       property
  *  12   Gandalf   1.11        10/22/99 Ian Formanek    NO SEMANTIC CHANGE - Sun
  *       Microsystems Copyright in File Comment
  *  11   Gandalf   1.10        8/7/99   Ian Formanek    Cleaned loading of icons
@@ -189,6 +122,4 @@ public class IDESettingsBeanInfo extends SimpleBeanInfo {
  *  2    Gandalf   1.1         1/7/99   Ian Formanek    fixed resource names
  *  1    Gandalf   1.0         1/5/99   Ian Formanek    
  * $
- * Beta Change History:
- *  0    Tuborg    0.11        --/--/98 Jan Palka       add property ShowTipsOnStartup nad lastTip
  */
