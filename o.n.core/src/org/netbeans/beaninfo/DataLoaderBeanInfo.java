2070f42391fcbc9417a176881e35520b0d65bf78
Jesse_Glick <devnull@localhost>
2000-01-12 23:55:41 +0000

    DataLoader.actions now editable.

--- o.n.core/src/org/netbeans/beaninfo/DataLoaderBeanInfo.java
+++ o.n.core/src/org/netbeans/beaninfo/DataLoaderBeanInfo.java
@@ -13,6 +13,8 @@
 
 package com.netbeans.developer.impl.beaninfo;
 
+import java.awt.Component;
+import java.awt.Image;
 import java.beans.*;
 
 import org.openide.loaders.DataLoader;
@@ -27,9 +29,11 @@ public class DataLoaderBeanInfo extends SimpleBeanInfo {
       representationClass.setDisplayName (NbBundle.getBundle (DataLoaderBeanInfo.class).getString ("PROP_representationClass"));
       representationClass.setShortDescription (NbBundle.getBundle (DataLoaderBeanInfo.class).getString ("HINT_representationClass"));
       representationClass.setExpert (true);
-      // [PENDING] maybe later "actions" should be included--once
-      // it has a decent property editor, that is
-      return new PropertyDescriptor[] { representationClass };
+      PropertyDescriptor actions = new PropertyDescriptor ("actions", DataLoader.class);
+      actions.setDisplayName (NbBundle.getBundle (DataLoaderBeanInfo.class).getString ("PROP_actions"));
+      actions.setShortDescription (NbBundle.getBundle (DataLoaderBeanInfo.class).getString ("HINT_actions"));
+      actions.setPropertyEditorClass (ActionsEditor.class);
+      return new PropertyDescriptor[] { actions, representationClass };
     } catch (IntrospectionException ie) {
       if (Boolean.getBoolean ("netbeans.debug.exceptions"))
         ie.printStackTrace ();
@@ -37,10 +41,36 @@ public class DataLoaderBeanInfo extends SimpleBeanInfo {
     }
   }
   
+  private static Image icon;
+  private static Image icon32;
+  public Image getIcon (int type) {
+    if ((type == BeanInfo.ICON_COLOR_16x16) || (type == BeanInfo.ICON_MONO_16x16)) {
+      if (icon == null) icon = loadImage ("/com/netbeans/developer/impl/resources/objectTypes.gif");
+      return icon;
+    } else {
+      if (icon32 == null) icon32 = loadImage ("/com/netbeans/developer/impl/resources/objectTypes32.gif");
+      return icon32;
+    }
+  }
+  
+  public static class ActionsEditor extends PropertyEditorSupport {
+    
+    public boolean supportsCustomEditor () {
+      return true;
+    }
+    
+    public Component getCustomEditor () {
+      return new LoaderActionsPanel (this);
+    }
+    
+  }
+  
 }
 
 /*
  * Log
+ *  3    Gandalf   1.2         1/13/00  Jesse Glick     DataLoader.actions now 
+ *       editable.
  *  2    Gandalf   1.1         11/25/99 Jesse Glick     representationClass 
  *       expert loader property.
  *  1    Gandalf   1.0         11/3/99  Jesse Glick     
