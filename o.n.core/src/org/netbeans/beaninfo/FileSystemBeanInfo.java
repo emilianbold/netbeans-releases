5509283840b040b0786ad11c7998f98d5896a859
Jesse_Glick <devnull@localhost>
2000-02-25 14:05:21 +0000

    Nicer BeanInfo for API implementors.

--- o.n.core/src/org/netbeans/beaninfo/FileSystemBeanInfo.java
+++ o.n.core/src/org/netbeans/beaninfo/FileSystemBeanInfo.java
@@ -34,7 +34,11 @@ public class FileSystemBeanInfo extends SimpleBeanInfo {
       desc = new PropertyDescriptor[] {
         new PropertyDescriptor ("readOnly", FileSystem.class, "isReadOnly", null), // 0 // NOI18N
         new PropertyDescriptor ("valid", FileSystem.class, "isValid", null), // 1 // NOI18N
-        new PropertyDescriptor ("hidden", FileSystem.class, "isHidden", "setHidden") // 2 // NOI18N
+        new PropertyDescriptor ("hidden", FileSystem.class, "isHidden", "setHidden"), // 2 // NOI18N
+        new PropertyDescriptor ("actions", FileSystem.class, "getActions", null), // 3 // NOI18N
+        new PropertyDescriptor ("displayName", FileSystem.class, "getDisplayName", null), // 4 // NOI18N
+        new PropertyDescriptor ("root", FileSystem.class, "getRoot", null), // 5 // NOI18N
+        new PropertyDescriptor ("status", FileSystem.class, "getStatus", null), // 6 // NOI18N
       };
       ResourceBundle bundle = NbBundle.getBundle(FileSystemBeanInfo.class);
       desc[0].setDisplayName (bundle.getString("PROP_readOnly"));
@@ -43,6 +47,10 @@ public class FileSystemBeanInfo extends SimpleBeanInfo {
       desc[1].setShortDescription (bundle.getString("HINT_valid"));
       desc[2].setDisplayName (bundle.getString("PROP_hidden"));
       desc[2].setShortDescription (bundle.getString("HINT_hidden"));
+      desc[3].setHidden (true);
+      desc[4].setHidden (true);
+      desc[5].setHidden (true);
+      desc[6].setHidden (true);
     } catch (IntrospectionException ex) {
       if (System.getProperty ("netbeans.debug.exceptions") != null) ex.printStackTrace();
     }
@@ -61,6 +69,8 @@ public class FileSystemBeanInfo extends SimpleBeanInfo {
 
 /*
  * Log
+ *  12   Jaga      1.10.1.0    2/25/00  Jesse Glick     Nicer BeanInfo for API 
+ *       implementors.
  *  11   Gandalf   1.10        1/13/00  Jaroslav Tulach I18N
  *  10   Gandalf   1.9         10/22/99 Ian Formanek    NO SEMANTIC CHANGE - Sun
  *       Microsystems Copyright in File Comment
