83a1576ccad20721274878ee358e69140bf9af1a
Jaroslav_Tulach <devnull@localhost>
2000-03-06 15:16:31 +0000

    #

---
+++ httpserver/src/org/netbeans/modules/httpserver/GrantAccessEvent.java
@@ -0,0 +1,75 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is Forte for Java, Community Edition. The Initial
+ * Developer of the Original Code is Sun Microsystems, Inc. Portions
+ * Copyright 1997-2000 Sun Microsystems, Inc. All Rights Reserved.
+ */
+
+package com.netbeans.developer.modules.httpserver;
+
+import java.util.EventObject;
+import java.net.InetAddress;
+
+// TODO:
+// The idea is that the HttpServer will have listeners
+// and when request arrives, it creates this event
+// for (l in listeners) {
+//   l.grantAccess (accessEvent);
+//   if (accessEvent.isGranted ()) {
+//     ...
+//     return;
+//   };
+// }
+
+
+
+/** This event is sent to access listeners to
+* ask them, whether the access to specified resource is
+* allowed.
+*
+* @author Jaroslav Tulach
+*/
+public abstract class GrantAccessEvent extends EventObject {
+  /** is access granted */
+  private boolean granted = false;
+
+  /** Creates new AccessEvent. Used only in this package by
+  * the HttpServer to create new access event when a resource
+  * is requested.
+  *
+  * @param httpServer the server 
+  */
+  GrantAccessEvent(HttpServerModule httpServer) {
+    super (httpServer);
+  }
+  
+  /** The Inet address that initiated the connection.
+  * @return the inet address
+  */
+  public abstract InetAddress getClientAddress ();
+  
+  // JST:
+  // It could be useful to also somehow describe the resource 
+  // requested, like resourceString or FileObject name, etc.
+  // I do not know about all details, so add it if think it is useful
+  
+  /** Allows access. The listener can use this method to grant
+  * access the client and resource.
+  */
+  public void grantAccess () {
+    granted = true;
+  }
+  
+  /** Getter to test whether the access has been granted.
+  * @return true if a listener granted the access
+  */
+  boolean isGranted () {
+    return true;
+  }
+}
---
+++ httpserver/src/org/netbeans/modules/httpserver/GrantAccessListener.java
@@ -0,0 +1,31 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is Forte for Java, Community Edition. The Initial
+ * Developer of the Original Code is Sun Microsystems, Inc. Portions
+ * Copyright 1997-2000 Sun Microsystems, Inc. All Rights Reserved.
+ */
+
+package com.netbeans.developer.modules.httpserver;
+
+import java.util.EventListener;
+
+/** Listener that can grant access when a client is asking for a 
+* resource.
+*
+* @author Jaroslav Tulach
+*/
+public interface GrantAccessListener {
+  /** Listener is notified about a request from a client and
+  * can grant access to specified resource. This can be done
+  * by calling <CODE>ev.grantAccess ()</CODE>.
+  *
+  * @param ev the event describing the request
+  */
+  public void grantAccess (GrantAccessEvent ev);
+}
