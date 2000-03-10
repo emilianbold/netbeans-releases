d3eddcd6ee243cb96e25c277d2dc2f26b7fb3787
Tran_Duc_Trung <devnull@localhost>
2000-03-10 14:22:16 +0000

    FIX: getJavaInitializationString() properly escapes int'l charaters if needed (\u1234)

--- o.n.core/src/org/netbeans/beaninfo/editors/StringEditor.java
+++ o.n.core/src/org/netbeans/beaninfo/editors/StringEditor.java
@@ -20,8 +20,8 @@ import org.openide.util.Utilities;
 * @author   Ian Formanek
 * @version  1.00, 18 Sep, 1998
 */
-public class StringEditor extends PropertyEditorSupport {
-
+public class StringEditor extends PropertyEditorSupport
+{
   /** sets new value */
   public void setAsText(String s) {
     setValue(s);
@@ -29,11 +29,7 @@ public class StringEditor extends PropertyEditorSupport {
 
   public String getJavaInitializationString () {
     String s = (String) getValue ();
-    s = Utilities.replaceString (s, "\\", "\\\\"); // NOI18N
-    s = Utilities.replaceString (s, "\"", "\\\""); // NOI18N
-    s = Utilities.replaceString (s, "\n", "\\n"); // NOI18N
-    s = Utilities.replaceString (s, "\t", "\\t"); // NOI18N
-    return "\""+s+"\""; // NOI18N
+    return "\"" + toAscii(s) + "\""; // NOI18N
   }
   
   public boolean supportsCustomEditor () {
@@ -44,10 +40,41 @@ public class StringEditor extends PropertyEditorSupport {
     return new StringCustomEditor (this);
   }
 
+  private static String toAscii(String str) {
+    StringBuffer buf = new StringBuffer(str.length() * 6); // x -> \u1234
+    char[] chars = str.toCharArray();
+    for (int i = 0; i < chars.length; i++) {
+      char c = chars[i];
+      switch (c) {
+        case '\b': buf.append("\\b"); break; // NOI18N
+        case '\t': buf.append("\\t"); break; // NOI18N
+        case '\n': buf.append("\\n"); break; // NOI18N
+        case '\f': buf.append("\\f"); break; // NOI18N
+        case '\r': buf.append("\\r"); break; // NOI18N
+        case '\"': buf.append("\\\""); break; // NOI18N
+//        case '\'': buf.append("\\'"); break; // NOI18N
+        case '\\': buf.append("\\\\"); break; // NOI18N
+        default:
+          if (c >= 0x0020 && c <= 0x007f)
+            buf.append(c);
+          else {
+            buf.append("\\u"); // NOI18N
+            String hex = Integer.toHexString(c);
+            for (int j = 0; j < 4 - hex.length(); j++)
+              buf.append('0');
+            buf.append(hex);
+          }
+      }
+    }
+    return buf.toString();
+  }
 }
 
 /*
  * Log
+ *  6    Gandalf-post-FCS1.4.1.0     3/10/00  Tran Duc Trung  FIX: 
+ *       getJavaInitializationString() properly escapes int'l charaters if 
+ *       needed (\u1234)
  *  5    Gandalf   1.4         1/13/00  Petr Jiricka    i18n
  *  4    Gandalf   1.3         10/22/99 Ian Formanek    NO SEMANTIC CHANGE - Sun
  *       Microsystems Copyright in File Comment
