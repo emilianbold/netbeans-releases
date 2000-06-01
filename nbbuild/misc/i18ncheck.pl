4adac4fcfb1be2697e5cd01c7f776eb726cddb51
jglick <jglick@netbeans.org>
2000-06-01 15:25:29 +0000

    license

--- nbbuild/misc/i18ncheck.pl
+++ nbbuild/misc/i18ncheck.pl
@@ -1,15 +1,21 @@
 #!/usr/bin/env perl
 # -*- perl -*-
+#                 Sun Public License Notice
+# 
+# The contents of this file are subject to the Sun Public License
+# Version 1.0 (the "License"). You may not use this file except in
+# compliance with the License. A copy of the License is available at
+# http://www.sun.com/
+# 
+# The Original Code is NetBeans. The Initial Developer of the Original
+# Code is Sun Microsystems, Inc. Portions Copyright 1997-2000 Sun
+# Microsystems, Inc. All Rights Reserved.
+
 #
 # i18ncheck.pl - checks java source for internationalizable strings
 #                not 100% foolproof !!
 #
-# SUN PROPRIETARY/CONFIDENTIAL:  INTERNAL USE ONLY.
 #
-# Copyright © 2000 Sun Microsystems, Inc. All rights reserved.
-# Use is subject to license terms.
-#
-
 require 5.005;
 use File::Find;
 
