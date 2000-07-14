f1dcd6cb8d721fa0553582dc31922b00fde88aff
rnajman <rnajman@netbeans.org>
2000-07-14 12:28:55 +0000

    new action

---
+++ db/src/org/netbeans/modules/db/explorer/actions/CaptureSchemaAction.java
@@ -0,0 +1,30 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2001 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.modules.db.explorer.actions;
+
+import org.openide.*;
+import org.openide.nodes.*;
+
+public class CaptureSchemaAction extends DatabaseAction
+{
+    static final long serialVersionUID = 2330200042238652177L;
+    protected boolean enable(Node[] activatedNodes) {
+//NOT IMPLEMENTED YET
+        return false;
+    }
+
+    public void performAction (Node[] activatedNodes) {
+//NOT IMPLEMENTED YET
+    }
+}
