2bb199b2c187b5a3de68784df69eaec689d2ac93
rnajman <rnajman@netbeans.org>
2000-07-14 15:57:48 +0000

    Weblogic (prefered) and "not installed driver" icons

--- db/src/org/netbeans/modules/db/explorer/infos/DatabaseNodeInfo.java
+++ db/src/org/netbeans/modules/db/explorer/infos/DatabaseNodeInfo.java
@@ -484,18 +484,38 @@ public class DatabaseNodeInfo extends Hashtable implements Node.Cookie
         put("table", nam);
     }
 
-    public String getIconBase()
-    {
-        if (getName().equals("Oracle thin") || getName().equals("PointBase Mobile Edition") || getName().equals("PointBase Server Edition"))
-            return (String)get("iconbaseprefered");
+    public String getIconBase() {
+        if (getCode().equals("driver"))
+            try {
+                Class.forName(getURL());
+                if (getName().startsWith("Microsoft SQL Server (Weblogic driver)") || getName().startsWith("Oracle thin") || getName().startsWith("PointBase"))
+                    return (String) get("iconbaseprefered");
+                else
+                    return (String) get("iconbase");
+            } catch (ClassNotFoundException exc) {
+                if (getName().startsWith("Microsoft SQL Server (Weblogic driver)") || getName().startsWith("Oracle thin") || getName().startsWith("PointBase"))
+                    return (String) get("iconbasepreferednotinstalled");
+                else
+                    return (String) get("iconbasenotinstalled");
+            }
         else
-            return (String)get("iconbase");
+            return (String) get("iconbase");
     }
 
-    public void setIconBase(String base)
-    {
-        if (getName().equals("Oracle thin") || getName().equals("PointBase Mobile Edition") || getName().equals("PointBase Server Edition"))
-            put("iconbaseprefered", base);
+    public void setIconBase(String base) {
+        if (getCode().equals("driver"))
+            try {
+                Class.forName(getURL());
+                if (getName().startsWith("Microsoft SQL Server (Weblogic driver)") || getName().startsWith("Oracle thin") || getName().startsWith("PointBase"))
+                    put("iconbaseprefered", base);
+                else
+                    put("iconbase", base);
+            } catch (ClassNotFoundException exc) {
+                if (getName().startsWith("Microsoft SQL Server (Weblogic driver)") || getName().startsWith("Oracle thin") || getName().startsWith("PointBase"))
+                    put("iconbasepreferednotinstalled", base);
+                else
+                    put("iconbasenotinstalled", base);
+            }
         else
             put("iconbase", base);
     }
@@ -681,35 +701,3 @@ public class DatabaseNodeInfo extends Hashtable implements Node.Cookie
         put(DRIVER_SPECIFICATION, driverSpecification);
     }
 }
-/*
- * <<Log>>
- *  23   Gandalf   1.22        1/25/00  Radko Najman    new driver adaptor 
- *       version
- *  22   Gandalf   1.21        12/22/99 Radko Najman    debug message
- *  21   Gandalf   1.20        12/15/99 Radko Najman    driver adaptor
- *  20   Gandalf   1.19        11/27/99 Patrik Knakal   
- *  19   Gandalf   1.18        11/15/99 Radko Najman    
- *  18   Gandalf   1.17        11/15/99 Radko Najman    
- *  17   Gandalf   1.16        11/8/99  Radko Najman    ConnectionNode name 
- *       refresh
- *  16   Gandalf   1.15        10/23/99 Ian Formanek    NO SEMANTIC CHANGE - Sun
- *       Microsystems Copyright in File Comment
- *  15   Gandalf   1.14        9/13/99  Slavek Psenicka 
- *  14   Gandalf   1.13        9/13/99  Slavek Psenicka 
- *  13   Gandalf   1.12        9/13/99  Slavek Psenicka 
- *  12   Gandalf   1.11        9/8/99   Slavek Psenicka adaptor changes
- *  11   Gandalf   1.10        9/2/99   Slavek Psenicka ReadOnly and permissions
- *       features
- *  10   Gandalf   1.9         8/19/99  Slavek Psenicka English
- *  9    Gandalf   1.8         8/18/99  Slavek Psenicka debug log removed
- *  8    Gandalf   1.7         8/18/99  Slavek Psenicka debug logs removed
- *  7    Gandalf   1.6         7/21/99  Slavek Psenicka don't remember password
- *  6    Gandalf   1.5         6/10/99  Slavek Psenicka new folder icon
- *  5    Gandalf   1.4         6/9/99   Ian Formanek    ---- Package Change To 
- *       org.openide ----
- *  4    Gandalf   1.3         5/21/99  Slavek Psenicka new version
- *  3    Gandalf   1.2         5/14/99  Slavek Psenicka new version
- *  2    Gandalf   1.1         4/23/99  Slavek Psenicka Debug mode
- *  1    Gandalf   1.0         4/23/99  Slavek Psenicka 
- * $
- */
