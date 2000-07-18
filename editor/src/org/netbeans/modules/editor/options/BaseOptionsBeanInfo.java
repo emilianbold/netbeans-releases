6022b3f099fbf41a1d50da31e7d12a3e6f5d9bae
mmetelka <mmetelka@netbeans.org>
2000-07-18 20:26:47 +0000

    improved property-descriptors creation

--- editor/src/org/netbeans/modules/editor/options/BaseOptionsBeanInfo.java
+++ editor/src/org/netbeans/modules/editor/options/BaseOptionsBeanInfo.java
@@ -17,10 +17,12 @@ import java.beans.*;
 import java.awt.Image;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.lang.reflect.Method;
 import java.util.ResourceBundle;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.Iterator;
 
@@ -53,6 +55,8 @@ public class BaseOptionsBeanInfo extends SimpleBeanInfo {
     /** Icons for compiler settings objects. */
     private Image icon;
     private Image icon32;
+
+    private HashMap names2PD;
 
     /** Propertydescriptors */
     PropertyDescriptor[] descriptors;
@@ -100,30 +104,99 @@ public class BaseOptionsBeanInfo extends SimpleBeanInfo {
     public PropertyDescriptor[] getPropertyDescriptors () {
         if (descriptors == null) {
             String[] propNames = getPropNames();
-            try {
-                descriptors = new PropertyDescriptor[propNames.length];
+            PropertyDescriptor[] pds = new PropertyDescriptor[propNames.length];
 
-                for (int i = 0; i < propNames.length; i++) {
-                    descriptors[i] = new PropertyDescriptor(propNames[i], getBeanClass());
-                    descriptors[i].setDisplayName(getString("PROP_" + bundlePrefix + propNames[i])); // NOI18N
-                    descriptors[i].setShortDescription(getString("HINT_" + bundlePrefix + propNames[i])); // NOI18N
-                }
+            for (int i = 0; i < propNames.length; i++) {
+                pds[i] = createPropertyDescriptor(propNames[i]);
+                // Set display-name and short-description
+                pds[i].setDisplayName(getString("PROP_" + bundlePrefix + propNames[i])); // NOI18N
+                pds[i].setShortDescription(getString("HINT_" + bundlePrefix + propNames[i])); // NOI18N
 
-                setPropertyEditor(BaseOptions.ABBREV_MAP_PROP, AbbrevsEditor.class);
-                setPropertyEditor(BaseOptions.CARET_TYPE_INSERT_MODE_PROP, CaretTypeEditor.class);
-                setPropertyEditor(BaseOptions.CARET_TYPE_OVERWRITE_MODE_PROP, CaretTypeEditor.class);
-                setPropertyEditor(BaseOptions.KEY_BINDING_LIST_PROP, KeyBindingsEditor.class);
-                setPropertyEditor(BaseOptions.COLORING_MAP_PROP, ColoringArrayEditor.class);
-                setPropertyEditor(BaseOptions.SCROLL_JUMP_INSETS_PROP, ScrollInsetsEditor.class);
-                setPropertyEditor(BaseOptions.SCROLL_FIND_INSETS_PROP, ScrollInsetsEditor.class);
-
-                setExpert(EXPERT_PROP_NAMES);
-
-            } catch (IntrospectionException e) {
-                descriptors = new PropertyDescriptor[0];
             }
+
+            descriptors = pds; // now the array are inited
+
+            // Now various properties of the descriptors can be updated
+            updatePropertyDescriptors();
         }
         return descriptors;
+    }
+
+    /** Create property descriptor for a particular property-name. */
+    protected PropertyDescriptor createPropertyDescriptor(String propName) {
+        PropertyDescriptor pd;
+        try {
+            pd = new PropertyDescriptor(propName, getBeanClass());
+
+        } catch (IntrospectionException e) {
+            try {
+                // Create property without read/write methods
+                pd = new PropertyDescriptor(propName, null, null);
+            } catch (IntrospectionException e2) {
+                throw new IllegalStateException("Invalid property name=" + propName);
+            }
+
+            // Try a simple search for get/set methods - just by name
+            // Successor can customize it if necessary
+            String cap = capitalize(propName);
+            Method m = findMethod("get" + cap);
+            if (m != null) {
+                try {
+                    pd.setReadMethod(m);
+                } catch (IntrospectionException e2) {
+                }
+            }
+            m = findMethod("set" + cap);
+            if (m != null) {
+                try {
+                    pd.setWriteMethod(m);
+                } catch (IntrospectionException e2) {
+                }
+            }
+        }
+
+        return pd;
+    }
+
+    private Method findMethod(String name) {
+        try {
+            Method[] ma = getBeanClass().getDeclaredMethods();
+            for (int i = 0; i < ma.length; i++) {
+                if (name.equals(ma[i].getName())) {
+                    return ma[i];
+                }
+            }
+        } catch (SecurityException e) {
+        }
+        return null;
+    }
+
+    private static String capitalize(String s) {
+	if (s.length() == 0) {
+ 	    return s;
+	}
+	char chars[] = s.toCharArray();
+	chars[0] = Character.toUpperCase(chars[0]);
+	return new String(chars);
+    }
+
+    /** Update various properties of the property descriptors. */
+    protected void updatePropertyDescriptors() {
+        setPropertyEditor(BaseOptions.ABBREV_MAP_PROP, AbbrevsEditor.class);
+        setPropertyEditor(BaseOptions.CARET_TYPE_INSERT_MODE_PROP, CaretTypeEditor.class);
+        setPropertyEditor(BaseOptions.CARET_TYPE_OVERWRITE_MODE_PROP, CaretTypeEditor.class);
+        setPropertyEditor(BaseOptions.KEY_BINDING_LIST_PROP, KeyBindingsEditor.class);
+        setPropertyEditor(BaseOptions.COLORING_MAP_PROP, ColoringArrayEditor.class);
+        setPropertyEditor(BaseOptions.SCROLL_JUMP_INSETS_PROP, ScrollInsetsEditor.class);
+        setPropertyEditor(BaseOptions.SCROLL_FIND_INSETS_PROP, ScrollInsetsEditor.class);
+
+        setExpert(EXPERT_PROP_NAMES);
+
+        setHidden(new String[] {
+            BaseOptions.EXPAND_TABS_PROP,
+            BaseOptions.SPACES_PER_TAB_PROP
+        });
+
     }
 
     protected Class getBeanClass() {
@@ -134,14 +207,15 @@ public class BaseOptionsBeanInfo extends SimpleBeanInfo {
         return BaseOptions.BASE_PROP_NAMES;
     }
 
-    protected PropertyDescriptor getPD(String prop) {
-        String[] propNames = getPropNames();
-        for (int i = 0; i < descriptors.length; i++) {
-            if (prop.equals(propNames[i])) {
-                return descriptors[i];
+    protected synchronized PropertyDescriptor getPD(String propName) {
+        if (names2PD == null) {
+            names2PD = new HashMap(37);
+            PropertyDescriptor[] pds = getPropertyDescriptors();
+            for (int i = pds.length - 1; i >= 0; i--) {
+                names2PD.put(pds[i].getName(), pds[i]);
             }
         }
-        return null;
+        return (PropertyDescriptor)names2PD.get(propName);
     }
 
     protected void setPropertyEditor(String propName, Class propEditor) {
