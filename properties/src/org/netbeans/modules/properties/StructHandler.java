ee9296cdced59fbc56789e2cf4c52d99a2fcdf69
pzavadsky <pzavadsky@netbeans.org>
2000-07-18 14:11:09 +0000

    Dirty flag away.

--- properties/src/org/netbeans/modules/properties/StructHandler.java
+++ properties/src/org/netbeans/modules/properties/StructHandler.java
@@ -1,16 +1,15 @@
 /*
- *                 Sun Public License Notice
- * 
- * The contents of this file are subject to the Sun Public License
- * Version 1.0 (the "License"). You may not use this file except in
- * compliance with the License. A copy of the License is available at
- * http://www.sun.com/
- * 
- * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2000 Sun
- * Microsystems, Inc. All Rights Reserved.
+ * SourceElementImpl.java -- synopsis. 
+ *
+ *
+ * <<Date>>
+ * <<Revision>>
+ *
+ * SUN PROPRIETARY/CONFIDENTIAL:  INTERNAL USE ONLY.
+ *
+ * Copyright © 1997-1999 Sun Microsystems, Inc. All rights reserved.
+ * Use is subject to license terms.
  */
-
 package org.netbeans.modules.properties;
 
 import java.io.*;
@@ -48,12 +47,6 @@ public class StructHandler extends Element /*implements TaskListener*/ {
     /** SoftReference is GC-ed too often, I wonder if I should keep a hard reference */
     DataRef hardReference;
 
-    /** This flag is set when somebody is editing the document and it is
-    * cleared after reparsing.
-    * It is used by parser to decide if parsing is necessary.
-    */
-    boolean dirty = false;
-
     // ======================== Public part ====================================
 
     static final long serialVersionUID =-3367087822606643886L;
@@ -78,7 +71,7 @@ public class StructHandler extends Element /*implements TaskListener*/ {
 
     /** If necessary parses the file, blocks until the thing is finished */
     private synchronized void getParsedDataBlocking() {
-        if (isDirty() || (getReferenceData() == null)) {
+        if (getReferenceData() == null) {
             reparseNowBlocking();
         }
     }
@@ -91,7 +84,6 @@ public class StructHandler extends Element /*implements TaskListener*/ {
         catch (IOException e) {
             setPropertiesStructure(null);
         }
-        setDirty(false);
     }
 
     /** Entry for use in this package */
@@ -136,51 +128,13 @@ public class StructHandler extends Element /*implements TaskListener*/ {
 
     // ======================== Package private part ================================
 
-    /** Sets the dirty flag - if the document was modified after last parsing. */
-    void setDirty(boolean b) {
-        if (dirty == b)
-            return;
-        synchronized (this) {
-            // another check inside the synchronized block
-            if (dirty == b)
-                return;
-            dirty = b;
-        }
-    }
-
-    /** Tests the dirty flag. This indicates whether the document and the structure match. */
-    boolean isDirty() {
-        return dirty;
-    }
-
     /** Starts the parsing if the this class is 'dirty' and status is true
     * and parsing is not running yet.
     */
     void autoParse() {
-        if (dirty && getStatus())
-            getParsedDataBlocking();
-        /*    Children.MUTEX.writeAccess(new Runnable() {
-              public void run() {
-                if (dirty && (parsingTask == null) && getStatus()) {
-                  prepareParsing(Thread.MIN_PRIORITY);
-                }
-              }
-            });*/
+        if (getStatus())
+            reparseNowBlocking();
     }
-
-    /** This method invokes the parsing only. It could be used by
-    * editor during saving.
-    */
-    /*  Task prepareParsing(final int priority) {
-        return (Task) Children.MUTEX.writeAccess(new Mutex.Action() {
-          public Object run() {
-            if (parsingTask == null) {
-              parsingTask = createParsingTask(priority);
-            }
-            return parsingTask;
-          }
-        });
-      }*/
 
     /** When parser finishes its job, it has to call this method to inform
     * everyone about the result. Must be called under mutex.writeaccess
@@ -207,30 +161,7 @@ public class StructHandler extends Element /*implements TaskListener*/ {
             // update calls notification methods according to changes
             data.ps.update(res);
         }
-
-        setDirty(false);
     }
-
-
-    /** Create parsing task.
-    * May be called only under mutex.writeAccess()
-    */
-    /*  private Task createParsingTask(final int priority) {
-        Runnable parseRunnable = new Runnable() {
-          public void run() {
-            try {
-              PropertiesParser parser = new PropertiesParser(pfe);
-              parser.parseFile();   
-            }
-            catch (IOException e) {
-              setPropertiesStructure(null);
-            }
-          }
-        };
-        RequestProcessor.Task t = RequestProcessor.postRequest(parseRunnable, 0, priority);
-        t.addTaskListener(this);
-        return t;
-      }                             */
 
     /** Gets the referenced object from the dataRef
     */
@@ -241,15 +172,6 @@ public class StructHandler extends Element /*implements TaskListener*/ {
                     }
                 });
     }
-
-    /** Clear the parsing task variable */
-    /*  public void taskFinished(final Task task) {
-        Children.MUTEX.writeAccess(new Runnable() {
-          public void run() {
-            parsingTask = null;
-          }
-        });
-      }*/
 
     /** Returns the structure */
     public PropertiesStructure getStructure() {
@@ -332,45 +254,4 @@ public class StructHandler extends Element /*implements TaskListener*/ {
             super.finalize();
         }
     }
-
-    // ======================== Utility - DataTask ==========================
-
-    /** Task which is used for holding the reference to the given data.
-    * It prevents them from being garbage collected.
-    */
-    /*  private static class DataTask extends Task {
-        private DataRef data;
-
-        public DataTask(DataRef data) {
-          super(null);
-          this.data = data;
-        }
-      }*/
 }
-
-
-
-
-/*
- * <<Log>>
- *  15   Gandalf   1.14        11/27/99 Patrik Knakal   
- *  14   Gandalf   1.13        10/23/99 Ian Formanek    NO SEMANTIC CHANGE - Sun
- *       Microsystems Copyright in File Comment
- *  13   Gandalf   1.12        9/23/99  Petr Jiricka    Fixed calling 
- *       super.finalize() - throws Throwable (JLint)
- *  12   Gandalf   1.11        9/23/99  Petr Jiricka    Finalize calls 
- *       super.finalize() (JLint warning)
- *  11   Gandalf   1.10        9/10/99  Petr Jiricka    
- *  10   Gandalf   1.9         8/18/99  Petr Jiricka    Debug prints
- *  9    Gandalf   1.8         8/9/99   Petr Jiricka    Removed debug prints
- *  8    Gandalf   1.7         7/24/99  Petr Jiricka    
- *  7    Gandalf   1.6         6/9/99   Ian Formanek    ---- Package Change To 
- *       org.openide ----
- *  6    Gandalf   1.5         6/8/99   Petr Jiricka    
- *  5    Gandalf   1.4         6/6/99   Petr Jiricka    
- *  4    Gandalf   1.3         5/16/99  Petr Jiricka    
- *  3    Gandalf   1.2         5/14/99  Petr Jiricka    
- *  2    Gandalf   1.1         5/13/99  Petr Jiricka    
- *  1    Gandalf   1.0         5/12/99  Petr Jiricka    
- * $
- */
