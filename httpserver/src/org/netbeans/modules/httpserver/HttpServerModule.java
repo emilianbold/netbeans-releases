72a4e063d8401fd7db62e0c5532f0000769b0a38
pjiricka <pjiricka@netbeans.org>
2000-08-07 21:21:25 +0000

    Upgraded to version of Jakarta of 7-Aug-2000. This is the version between Tomcat 3.2beta2 and FCS
    The HttpServer module now uses EmbededTomcat class to launch the server. This should be less error-prone and less sesnitive to Jakarta code changes than the previously used mode.
    All functionality is preserved, no new functionality added.

--- httpserver/src/org/netbeans/modules/httpserver/HttpServerModule.java
+++ httpserver/src/org/netbeans/modules/httpserver/HttpServerModule.java
@@ -27,16 +27,18 @@ import org.openide.util.NbBundle;
 import org.openide.TopManager;
 import org.openide.NotifyDescriptor;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.tomcat.startup.EmbededTomcat;
 import org.apache.tomcat.core.ContextManager;
 import org.apache.tomcat.core.ServerConnector;
 import org.apache.tomcat.core.ServletWrapper;
+import org.apache.tomcat.core.FacadeManager;
 import org.apache.tomcat.core.Context;
 import org.apache.tomcat.logging.TomcatLogger;
 import org.apache.tomcat.context.*;
-import org.apache.tomcat.service.SimpleTcpConnector;
 
 /**
 * Module installation class for Http Server
@@ -175,6 +177,22 @@ public class HttpServerModule extends ModuleInstall implements Externalizable {
             }
         }
     }
+    
+    
+    private static ContextManager getContextManager(EmbededTomcat tc) {
+        try {
+            java.lang.reflect.Field fm = EmbededTomcat.class.getDeclaredField("contextM");
+            fm.setAccessible(true);
+            return (ContextManager)fm.get(tc);
+        }
+        catch (NoSuchFieldException e) {
+            return null;
+        }
+        catch (IllegalAccessException e) {
+            return null;
+        }
+    }
+    
 
     private static ContextManager buildServer() throws Exception {
         HttpServerSettings op = HttpServerSettings.OPTIONS;
@@ -182,59 +200,29 @@ public class HttpServerModule extends ModuleInstall implements Externalizable {
         NbLogger logger = new NbLogger();
         logger.setName("tc_log");
 
-        ContextManager cm = new ContextManager();
-        cm.setDebug(0);
+        EmbededTomcat tc=new EmbededTomcat();
+        
+        File wd = NbClassPath.toFile(
+                      TopManager.getDefault().getRepository().getDefaultFileSystem().getRoot());
+        wd = new File(wd, "httpwork"); // NOI18N
+        tc.setWorkDir(wd.getAbsolutePath());
+        
+        // hack - force initialization of default interceptors, so our interceptor is after them
+        tc.addApplicationAdapter(null);
 
-        cm.addContextInterceptor(new LogEvents());
-        //cm.addContextInterceptor(new AutoSetup());
-        cm.addContextInterceptor(new NbCMSetter());
-        cm.addContextInterceptor(new WorkDirInterceptor());
-        //cm.addContextInterceptor(new WebXmlReader());
-        cm.addContextInterceptor(new LoadOnStartupInterceptor());
-
-        cm.setDefaults();
-        // set the HTTP connector port
-        SimpleTcpConnector con = (SimpleTcpConnector)cm.getConnectors().nextElement();
-        con.setPort(op.getPort());
-
-        Context ctxt = new Context();
-        ctxt.setContextManager(cm);
-        cm.addContext(ctxt);
-
-        // NotFoundServlet
-        ServletWrapper nf = new ServletWrapper();
-        nf.setServletClass("org.netbeans.modules.httpserver.NotFoundServlet");
-        nf.setServletName("NotFoundServlet");
-        ctxt.addServlet(nf);
-        ctxt.addServletMapping("/", "NotFoundServlet");
-        nf.setContext(ctxt);
-
-        // RepositoryServlet
-        ServletWrapper repo = new ServletWrapper();
-        repo.setServletClass("org.netbeans.modules.httpserver.RepositoryServlet");
-        repo.setServletName("RepositoryServlet");
-        ctxt.addServlet(repo);
-        ctxt.addServletMapping(op.getRepositoryBaseURL() + "*", "RepositoryServlet");
-        repo.setContext(ctxt);
-
-        // ClassPathServlet
-        ServletWrapper claz = new ServletWrapper();
-        claz.setServletClass("org.netbeans.modules.httpserver.ClasspathServlet");
-        claz.setServletName("ClasspathServlet");
-        ctxt.addServlet(claz);
-        ctxt.addServletMapping(op.getClasspathBaseURL() + "*", "ClasspathServlet");
-        claz.setContext(ctxt);
-
-        // InvokerServlet
-        ServletWrapper invoker = new ServletWrapper();
-        invoker.setServletClass("org.apache.tomcat.servlets.InvokerServlet");
-        invoker.setServletName("invoker");
-        ctxt.addServlet(invoker);
-        ctxt.addServletMapping("/servlet/*", "invoker");
-        invoker.setContext(ctxt);
-
-        cm.init();
+	NbServletsInterceptor nbI =new NbServletsInterceptor();
+	tc.addContextInterceptor( nbI );
+        
+        ServletContext sctx;
+        sctx=tc.addContext("", wd.toURL());
+        tc.initContext( sctx );
+        //ctxt.getServletLoader().setParentLoader(TopManager.getDefault().systemClassLoader());
+        
+        tc.addEndpoint( op.getPort(), null, null);
+        
+        ContextManager cm = getContextManager(tc);
         return cm;
+        
     }
 
     private static class NbLogger extends TomcatLogger {
@@ -254,66 +242,3 @@ public class HttpServerModule extends ModuleInstall implements Externalizable {
 
 }
 
-/*
- * Log
- *  41   Jaga      1.37.1.2    3/24/00  Petr Jiricka    Fixing main servlets, 
- *       grant access listeners
- *  40   Jaga      1.37.1.1    3/24/00  Petr Jiricka    Implemented 
- *       Jakarta-based webserver
- *  39   Jaga      1.37.1.0    3/22/00  Petr Jiricka    Fixed compilation 
- *       errors.
- *  38   Gandalf   1.37        1/13/00  Petr Jiricka    More i18n
- *  37   Gandalf   1.36        1/12/00  Petr Jiricka    i18n
- *  36   Gandalf   1.35        1/11/00  Petr Jiricka    Cleanup
- *  35   Gandalf   1.34        1/9/00   Petr Jiricka    Cleanup
- *  34   Gandalf   1.33        12/1/99  Ales Novak      bugfix - working 
- *       directory is not system - system is deleted no more
- *  33   Gandalf   1.32        11/27/99 Patrik Knakal   
- *  32   Gandalf   1.31        11/25/99 Petr Jiricka    - Another 
- *       security-related fix - for URLs starting with an allowed substring but 
- *       continuing further on.  - Relaxed security to allow executing arbitrary
- *       servlets  - Fixed undesirable creation of "work" directory
- *  31   Gandalf   1.30        11/24/99 Ian Formanek    Fixed bug 4767 - 
- *       Security problem:  Internal HTTP Server allows access to directories 
- *       including and below root directory under NT.  Could default Project 
- *       Settings | HTTP Server | Grant Access To to 127.0.0.0?
- *  30   Gandalf   1.29        10/23/99 Ian Formanek    NO SEMANTIC CHANGE - Sun
- *       Microsystems Copyright in File Comment
- *  29   Gandalf   1.28        10/10/99 Petr Hamernik   console debug messages 
- *       removed.
- *  28   Gandalf   1.27        10/7/99  Petr Jiricka    
- *  27   Gandalf   1.26        10/6/99  Petr Jiricka    Removed module 
- *       (de)serialization
- *  26   Gandalf   1.25        10/6/99  Petr Jiricka    Removed debug DumpStack
- *  25   Gandalf   1.24        10/5/99  Petr Jiricka    
- *  24   Gandalf   1.23        10/1/99  Petr Hrebejk    org.openide.modules.ModuleInstall
- *        changed to class + some methods added
- *  23   Gandalf   1.22        9/30/99  Petr Jiricka    Jetty -> JSWDK (TomCat)
- *  22   Gandalf   1.21        9/8/99   Petr Jiricka    SecurityException fix
- *  21   Gandalf   1.20        9/8/99   Petr Jiricka    Fixed 
- *       NullPointerException at startup
- *  20   Gandalf   1.19        8/17/99  Petr Jiricka    Externalization - server
- *       startup during the first IDE start
- *  19   Gandalf   1.18        8/9/99   Petr Jiricka    Fixed bug with multiple 
- *       restarts of the server on IDE startup
- *  18   Gandalf   1.17        7/24/99  Petr Jiricka    
- *  17   Gandalf   1.16        7/3/99   Petr Jiricka    
- *  16   Gandalf   1.15        7/3/99   Petr Jiricka    
- *  15   Gandalf   1.14        6/23/99  Petr Jiricka    
- *  14   Gandalf   1.13        6/22/99  Petr Jiricka    
- *  13   Gandalf   1.12        6/11/99  Jaroslav Tulach System.out commented
- *  12   Gandalf   1.11        6/9/99   Ian Formanek    ---- Package Change To 
- *       org.openide ----
- *  11   Gandalf   1.10        6/8/99   Petr Jiricka    
- *  10   Gandalf   1.9         6/1/99   Petr Jiricka    
- *  9    Gandalf   1.8         6/1/99   Petr Jiricka    
- *  8    Gandalf   1.7         5/31/99  Petr Jiricka    
- *  7    Gandalf   1.6         5/28/99  Petr Jiricka    
- *  6    Gandalf   1.5         5/25/99  Petr Jiricka    
- *  5    Gandalf   1.4         5/17/99  Petr Jiricka    
- *  4    Gandalf   1.3         5/17/99  Petr Jiricka    
- *  3    Gandalf   1.2         5/11/99  Petr Jiricka    
- *  2    Gandalf   1.1         5/10/99  Petr Jiricka    
- *  1    Gandalf   1.0         5/7/99   Petr Jiricka    
- * $
- */
---
+++ httpserver/src/org/netbeans/modules/httpserver/NbServletsInterceptor.java
@@ -0,0 +1,64 @@
+package org.netbeans.modules.httpserver;
+
+import org.apache.tomcat.core.*;
+import java.beans.*;
+import java.io.*;
+import java.io.IOException;
+import java.lang.reflect.*;
+import java.util.*;
+import java.util.StringTokenizer;
+
+import org.openide.TopManager;
+
+/**
+ * @author costin@dnt.ro
+ */
+public class NbServletsInterceptor extends BaseInterceptor {
+
+    public NbServletsInterceptor() {
+    }
+
+    private ServletWrapper addServlet( Context ctx, String name, String classN )
+	throws TomcatException {
+	ServletWrapper sw=new ServletWrapper();
+	sw.setContext(ctx);
+	sw.setServletName( name );
+	sw.setServletClass( classN);
+	ctx.addServlet( sw );
+	sw.setLoadOnStartUp(0);
+	return sw;
+    }
+    
+    private void addNbServlets( Context ctx ) throws TomcatException {
+        HttpServerSettings op = HttpServerSettings.OPTIONS;
+        
+        ctx.getServletLoader().setParentLoader(TopManager.getDefault().systemClassLoader());
+        
+        ServletWrapper sw;
+	//sw=addServlet( ctx, "NotFoundServlet", "org.netbeans.modules.httpserver.NotFoundServlet");
+	//ctx.addServletMapping("/*", "NotFoundServlet");
+        
+	sw=addServlet( ctx, "RepositoryServlet", "org.netbeans.modules.httpserver.RepositoryServlet");
+	ctx.addServletMapping(op.getRepositoryBaseURL() + "*", "RepositoryServlet");
+        
+	sw=addServlet( ctx, "ClasspathServlet", "org.netbeans.modules.httpserver.ClasspathServlet");
+	ctx.addServletMapping(op.getClasspathBaseURL() + "*", "ClasspathServlet");
+    }
+    
+    public void contextInit(Context ctx) throws TomcatException {
+	if( ctx.getDebug() > 0 ) ctx.log("NbServletsInterceptor - init  " + ctx.getPath() + " " + ctx.getDocBase() );
+	ContextManager cm=ctx.getContextManager();
+	
+	try {
+	    // Default init
+	    addNbServlets( ctx );
+
+	} catch (Exception e) {
+            String msg = "NbServletsInterceptor failed";
+	    System.out.println(msg);
+	}
+
+    }
+
+}
+
