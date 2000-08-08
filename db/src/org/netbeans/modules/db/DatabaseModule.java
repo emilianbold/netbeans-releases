049aae05a41a042682b5a69a4e1fae86b71c377a
rnajman <rnajman@netbeans.org>
2000-08-08 11:11:33 +0000

    fixed problem with file separator in pointbase.ini property

--- db/src/org/netbeans/modules/db/DatabaseModule.java
+++ db/src/org/netbeans/modules/db/DatabaseModule.java
@@ -34,7 +34,7 @@ public class DatabaseModule extends ModuleInstall {
     static final long serialVersionUID =5426465356344170725L;
 
     public void installed() {
-        System.setProperty("pointbase.ini", System.getProperty("netbeans.home") + java.io.File.separator + "pointbase\\pointbase.ini");
+        System.setProperty("pointbase.ini", System.getProperty("netbeans.home") + java.io.File.separator + "pointbase" + java.io.File.separator + "pointbase.ini");
 
         TopManager tm = TopManager.getDefault();
 
@@ -59,11 +59,11 @@ public class DatabaseModule extends ModuleInstall {
     }
     
     public void uninstalled() {
-        System.setProperty("pointbase.ini", System.getProperty("netbeans.home") + java.io.File.separator + "pointbase\\pointbase.ini");
+        System.setProperty("pointbase.ini", System.getProperty("netbeans.home") + java.io.File.separator + "pointbase" + java.io.File.separator + "pointbase.ini");
     }
     
     public void restored() {
-        System.setProperty("pointbase.ini", System.getProperty("netbeans.home") + java.io.File.separator + "pointbase\\pointbase.ini");
+        System.setProperty("pointbase.ini", System.getProperty("netbeans.home") + java.io.File.separator + "pointbase" + java.io.File.separator + "pointbase.ini");
     }
 
 }
