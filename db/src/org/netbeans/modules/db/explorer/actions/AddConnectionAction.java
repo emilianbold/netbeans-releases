ba7554b2452b6463a9e1c42e5b81e4d31ee7e73c
rnajman <rnajman@netbeans.org>
2000-08-15 13:21:54 +0000

    I18N

--- db/src/org/netbeans/modules/db/explorer/actions/AddConnectionAction.java
+++ db/src/org/netbeans/modules/db/explorer/actions/AddConnectionAction.java
@@ -13,12 +13,13 @@
 
 package org.netbeans.modules.db.explorer.actions;
 
-import java.io.*;
 import java.beans.*;
+import java.io.*;
 import java.util.*;
 import java.sql.*;
 import java.text.MessageFormat;
 import java.util.ResourceBundle;
+
 import org.openide.*;
 import org.openide.util.*;
 import org.openide.util.actions.*;
@@ -30,17 +31,18 @@ import org.netbeans.modules.db.explorer.nodes.*;
 import org.netbeans.modules.db.explorer.dlg.*;
 import org.netbeans.modules.db.explorer.infos.*;
 
-public class AddConnectionAction extends DatabaseAction
-{
-    private final static String CLASS_NOT_FOUND = "EXC_ClassNotFound";
-    private final static String BUNDLE_PATH = "org.netbeans.modules.db.resources.Bundle";
+public class AddConnectionAction extends DatabaseAction {
+    private final static String CLASS_NOT_FOUND = "EXC_ClassNotFound"; // NOI18N
+    private final static String BUNDLE_PATH = "org.netbeans.modules.db.resources.Bundle"; // NOI18N
 
     static final long serialVersionUID =5370365696803042542L;
-    public void performAction (Node[] activatedNodes)
-    {
+    public void performAction (Node[] activatedNodes) {
         Node node;
-        if (activatedNodes != null && activatedNodes.length>0) node = activatedNodes[0];
-        else return;
+        if (activatedNodes != null && activatedNodes.length>0)
+            node = activatedNodes[0];
+        else
+            return;
+        
         try {
             DatabaseNodeInfo info = (DatabaseNodeInfo)node.getCookie(DatabaseNodeInfo.class);
             ConnectionOwnerOperations nfo = (ConnectionOwnerOperations)info.getParent(nodename);
@@ -54,29 +56,15 @@ public class AddConnectionAction extends DatabaseAction
             }
 
             NewConnectionDialog cdlg = new NewConnectionDialog(drvs, cinfo);
-            if (cdlg.run()) nfo.addConnection((DBConnection)cinfo);
+            if (cdlg.run())
+                nfo.addConnection((DBConnection)cinfo);
 
-        } catch (ClassNotFoundException ex) {
-            String message = MessageFormat.format(NbBundle.getBundle(BUNDLE_PATH).getString(CLASS_NOT_FOUND), new String[] {ex.getMessage()});
+        } catch (ClassNotFoundException exc) {
+            String message = MessageFormat.format(NbBundle.getBundle(BUNDLE_PATH).getString(CLASS_NOT_FOUND), new String[] {exc.getMessage()});
             TopManager.getDefault().notify(new NotifyDescriptor.Message(message, NotifyDescriptor.ERROR_MESSAGE));
-        } catch (Exception e) {
-            TopManager.getDefault().notify(new NotifyDescriptor.Message("Unable to perform action, "+e.getMessage(), NotifyDescriptor.ERROR_MESSAGE));
+        } catch (Exception exc) {
+            String message = MessageFormat.format(NbBundle.getBundle(BUNDLE_PATH).getString("ERR_UnableToPerformAction"), new String[] {exc.getMessage()}); // NOI18N
+            TopManager.getDefault().notify(new NotifyDescriptor.Message(message, NotifyDescriptor.ERROR_MESSAGE));
         }
     }
 }
-/*
- * <<Log>>
- *  9    Gandalf   1.8         11/27/99 Patrik Knakal   
- *  8    Gandalf   1.7         10/23/99 Ian Formanek    NO SEMANTIC CHANGE - Sun
- *       Microsystems Copyright in File Comment
- *  7    Gandalf   1.6         9/8/99   Slavek Psenicka adaptor changes
- *  6    Gandalf   1.5         7/21/99  Slavek Psenicka 
- *  5    Gandalf   1.4         6/9/99   Ian Formanek    ---- Package Change To 
- *       org.openide ----
- *  4    Gandalf   1.3         5/21/99  Slavek Psenicka new version
- *  3    Gandalf   1.2         5/14/99  Slavek Psenicka new version
- *  2    Gandalf   1.1         4/23/99  Slavek Psenicka oprava activatedNode[0] 
- *       check
- *  1    Gandalf   1.0         4/23/99  Slavek Psenicka 
- * $
- */
