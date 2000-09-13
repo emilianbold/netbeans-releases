16f127e27a66fc78ef69cdee1656167e28c1b60d
pkeegan <pkeegan@netbeans.org>
2000-09-13 18:16:12 +0000

    more changes to activate the new help

---
+++ usersguide/javahelp/org/netbeans/modules/usersguide/ide.hs
@@ -0,0 +1,35 @@
+<?xml version='1.0' encoding='ISO-8859-1' ?>
+<!DOCTYPE helpset
+  PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 1.0//EN"
+         "http://java.sun.com/products/javahelp/helpset_1_0.dtd">
+
+<helpset version="1.0">
+
+ <title>NetBeans Help*</title>
+
+  <maps>
+     <homeID>ide.welcome</homeID>
+     <mapref location="Map.jhm" />
+  </maps>
+  <view>
+    <name>TOC</name>
+    <label>Table Of Contents</label>
+    <type>javax.help.TOCView</type>
+    <data>ide-toc.xml</data>
+  </view>
+  <view>
+    <name>Index</name>
+    <label>Index</label>
+    <type>javax.help.IndexView</type>
+    <data>ide-idx.xml</data>
+  </view>
+  <view>
+    <name>Search</name>
+    <label>Search</label>
+    <type>javax.help.SearchView</type>
+    <data engine="com.sun.java.help.search.DefaultSearchEngine">
+      JavaHelpSearch
+    </data>
+  </view>
+
+</helpset>
