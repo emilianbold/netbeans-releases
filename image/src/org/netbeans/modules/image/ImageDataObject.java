fddc4a9505eeb5ee89b74ef2771d5c4a07c57f34
pzavadsky <pzavadsky@netbeans.org>
2000-10-05 16:06:32 +0000

    Lazy initialization of cookies, for better performance made by Ales Novak.

--- image/src/org/netbeans/modules/image/ImageDataObject.java
+++ image/src/org/netbeans/modules/image/ImageDataObject.java
@@ -11,9 +11,12 @@
  * Microsystems, Inc. All Rights Reserved.
  */
 
+
 package org.netbeans.modules.image;
 
+
 import java.io.*;
+import java.net.URL;
 
 import org.openide.*;
 import org.openide.filesystems.*;
@@ -23,28 +26,33 @@ import org.openide.actions.OpenAction;
 import org.openide.util.actions.*;
 import org.openide.nodes.*;
 
-/** Object that represents one file containing an image.
-*
-* @author Petr Hamernik, Jaroslav Tulach, Ian Formanek
-*/
+
+/** 
+ * Object that represents one file containing an image.
+ * @author Petr Hamernik, Jaroslav Tulach, Ian Formanek
+ */
 public class ImageDataObject extends MultiDataObject {
-    /** generated Serialized Version UID */
+    
+    /** Generated serialized version UID. */
     static final long serialVersionUID = -6035788991669336965L;
 
-    private static final String IMAGE_ICON_BASE =
-        "org/netbeans/modules/image/imageObject"; // NOI18N
+    /** Base for image resource. */
+    private static final String IMAGE_ICON_BASE = "org/netbeans/modules/image/imageObject"; // NOI18N
 
-    /** New instance.
+    /** Helper variable. Speeds up <code>DataObject</code> recognition. */
+    private boolean shouldInitCookieSet = true;
+    
+    
+    /** Constructor.
     * @param pf primary file object for this data object
     * @param loader the data loader creating it
     * @exception DataObjectExistsException if there was already a data object for it 
     */
     public ImageDataObject(FileObject pf, MultiFileLoader loader) throws DataObjectExistsException {
         super(pf, loader);
-        // Support OpenCookie.
-        getCookieSet ().add (new ImageOpenSupport (getPrimaryEntry ()));
     }
 
+    
     /** Help context for this object.
     * @return the help context
     */
@@ -89,117 +97,40 @@ public class ImageDataObject extends MultiDataObject {
         node.setDefaultAction (SystemAction.get (OpenAction.class));
         return node;
     }
+    
+    /** Overrides superclass method. */
+    public CookieSet getCookieSet() {
+        if (shouldInitCookieSet) {
+            initCookieSet();
+        }
+        return super.getCookieSet();
+    }
 
-    /** Inner class. OpenSupport flavored with little EditorSupport" features like 
-    * listening on changes of image file and renames on dataobject, 
-    * so it can work appropriate in Editor window.
-    */
-//    class ImageOpenSupport extends OpenSupport implements OpenCookie {
-
-        /** Saves last modified time
-        */
-//        private long lastSaveTime;
-
-        /** Listens for changes on file
-        */
-//        private FileChangeListener fileChangeL; 
-
-
-        /** Constructs ImageOpenSupportObject on given MultiDataObject.Entry
-        */
-//        public ImageOpenSupport (MultiDataObject.Entry ent) {
-//            super (ent);
-//        }
-
-        /** Creates the CloenableTOPComponent viewer of image
-        */
-//        public CloneableTopComponent createCloneableTopComponent () {
-//            prepareViewer();
-//            return new ImageViewer(ImageDataObject.this);
-//        }
-
-        /**  Set listener for changes on image file
-        */
-//        void prepareViewer() {
-            // listen for changes on the image file
-//            if(fileChangeL == null) {
-//                fileChangeL = new FileChangeAdapter() {
-//                    public void fileChanged(final FileEvent evt) {
-//                        if (evt.getTime() > lastSaveTime) {
-//                            lastSaveTime = System.currentTimeMillis();
-                            // post in AWT event thread because of possible dialog popup
-//                            SwingUtilities.invokeLater(
-//                                new Runnable() {
-//                                    public void run() {
-//                                        reload(evt);
-//                                   }
-//                                }
-//                            );
-//                        }
-//                    }
-//                };
-//            }
-//            entry.getFile().addFileChangeListener(fileChangeL);
-//            lastSaveTime = System.currentTimeMillis();
-//        }
+    /**
+     * Overrides superclass method. 
+     * Look for a cookie in the current cookie set matching the requested class.
+     * @param type the class to look for
+     * @return an instance of that class, or <code>null</code> if this class of cookie
+     *    is not supported
+     */
+    public Node.Cookie getCookie(Class type) {
+        if (org.openide.cookies.CompilerCookie.class.isAssignableFrom(type)) {
+            return null;
+        }
         
-        /** Ask and reload/close image views
-        */
-//        private void reload(FileEvent evt) {
-            // ask if reload?
-//            MessageFormat fmt = new MessageFormat(NbBundle.getBundle(EditorSupport.class).getString("FMT_External_change")); // NOI18N
-//            String msg = fmt.format(new Object[] { entry.getFile().getPackageNameExt('/', '.')});
-//            NotifyDescriptor nd = new NotifyDescriptor.Confirmation(msg, NotifyDescriptor.YES_NO_OPTION);
-//            Object ret = TopManager.getDefault().notify(nd);
-
-//            if (NotifyDescriptor.YES_OPTION.equals(ret)) {
-//                RequestProcessor.postRequest(new Runnable() {
-                    // load icon from file
-//                    public void run() {
-//                        final Icon icon1; // accessory vraiable cause javac don't compile following code with final icon variable
-//                        try {
-//                            icon1 = new NBImageIcon(ImageDataObject.this);
-//                        } catch (IOException ioe) {
-//                            icon1 = new ImageIcon(new byte[0]); // empty icon
-//                        }
-                        
-//                        final Icon icon = icon1;
-
-//                        Enumeration e = allEditors.getComponents();
-//                        while(e.hasMoreElements()) {
-//                            final Object pane = e.nextElement();
-//                            SwingUtilities.invokeLater(new Runnable() {
-//                                public void run() {
-//                                    ((ImageViewer)pane).reloadIcon(icon);
-//                                }
-//                            });
-//                        }
-//                    }
-//                });
-//            }
-//        }
-//    } // end of inner ImageOpenSupport class
+        if (shouldInitCookieSet) {
+            initCookieSet();
+        }
+        return super.getCookie(type);
+    }
+    
+    /** Initializes cookie set. */
+    private synchronized void initCookieSet() {
+        if (!shouldInitCookieSet) {
+            return;
+        }
+        CookieSet cookies = super.getCookieSet();
+        cookies.add(new ImageOpenSupport (getPrimaryEntry ()));
+        shouldInitCookieSet = false;
+    }
 }
-
-/*
- * Log
- *  14   Gandalf   1.13        1/5/00   Ian Formanek    NOI18N
- *  13   Gandalf   1.12        10/23/99 Ian Formanek    NO SEMANTIC CHANGE - Sun
- *       Microsystems Copyright in File Comment
- *  12   Gandalf   1.11        6/24/99  Jesse Glick     Gosh-honest HelpID's.
- *  11   Gandalf   1.10        6/22/99  Ian Formanek    employed DEFAULT_HELP
- *  10   Gandalf   1.9         6/9/99   Ian Formanek    ---- Package Change To 
- *       org.openide ----
- *  9    Gandalf   1.8         4/13/99  Jesse Glick     Clean-ups of comments 
- *       and such for public perusal.
- *  8    Gandalf   1.7         3/22/99  Ian Formanek    Icons moved from 
- *       modules/resources to this package
- *  7    Gandalf   1.6         2/3/99   Jaroslav Tulach 
- *  6    Gandalf   1.5         1/22/99  Ian Formanek    
- *  5    Gandalf   1.4         1/15/99  Petr Hamernik   image source repaired
- *  4    Gandalf   1.3         1/7/99   Jaroslav Tulach Uses OpenSupport
- *  3    Gandalf   1.2         1/7/99   Ian Formanek    
- *  2    Gandalf   1.1         1/7/99   Ian Formanek    
- *  1    Gandalf   1.0         1/5/99   Ian Formanek    
- * $
- */
