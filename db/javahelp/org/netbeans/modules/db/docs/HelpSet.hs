8c8b9d83d30189f3dfc716ec5d4ff28c93b3df00
jhuth <jhuth@netbeans.org>
2000-12-26 22:07:56 +0000

    Syncing Files in Boston -> Pilsen

--- db/javahelp/org/netbeans/modules/db/docs/HelpSet.hs
+++ db/javahelp/org/netbeans/modules/db/docs/HelpSet.hs
@@ -12,10 +12,38 @@ Code is Sun Microsystems, Inc. Portions Copyright 1997-2001 Sun
 Microsystems, Inc. All Rights Reserved.
 -->
 
+<!DOCTYPE helpset
+  PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 1.0//EN"
+         "http://java.sun.com/products/javahelp/helpset_1_0.dtd">
+
 <helpset version="1.0">
-  <title>Database Explorer Module Documentation</title>
+
+ <title>Database Explorer Module Documentation</title>
+
   <maps>
-     <homeID>org.netbeans.modules.db.HOMEID</homeID>
-     <mapref location="Map.jhm"/>
+     <homeID>dbexpovew</homeID>
+     <mapref location="Map.jhm" />
   </maps>
+  <view>
+    <name>TOC</name>
+    <label>Table Of Contents</label>
+    <type>javax.help.TOCView</type>
+    <data>toc.xml</data>
+  </view>
+  <view>
+    <name>Index</name>
+    <label>Index</label>
+    <type>javax.help.IndexView</type>
+    <data>db-idx.xml</data>
+  </view>
+  <view>
+    <name>Search</name>
+   <label>Search</label>
+   <type>javax.help.SearchView</type>
+ 
+  <data engine="com.sun.java.help.search.DefaultSearchEngine">
+      JavaHelpSearch
+   </data>
+  </view> 
+
 </helpset>
