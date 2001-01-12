9216f52cbea6c782ea70a296dd10a1a0bdcf6ac7
jrechtacek <jrechtacek@netbeans.org>
2001-01-12 14:18:02 +0000

    class for Foreign key

---
+++ db/src/org/netbeans/modules/db/explorer/nodes/ForeignKeyNode.java
@@ -0,0 +1,34 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2001 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.modules.db.explorer.nodes;
+
+import java.awt.datatransfer.Transferable;
+import java.io.IOException;
+import java.sql.*;
+import java.text.MessageFormat;
+import java.util.*;
+
+import org.openide.nodes.Children;
+import org.openide.nodes.NodeTransfer;
+import org.openide.nodes.Node;
+import org.openide.util.datatransfer.PasteType;
+
+import org.netbeans.lib.ddl.*;
+import org.netbeans.lib.ddl.impl.*;
+import org.netbeans.modules.db.*;
+import org.netbeans.modules.db.explorer.*;
+import org.netbeans.modules.db.explorer.infos.*;
+
+public class ForeignKeyNode extends DatabaseNode {
+}
