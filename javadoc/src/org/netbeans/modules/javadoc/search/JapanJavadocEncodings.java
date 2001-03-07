574eb7ca6b2928c187b21a857d582d108568f600
psuchomel <psuchomel@netbeans.org>
2001-03-07 17:58:14 +0000

    Added support for japanese search engine

---
+++ javadoc/src/org/netbeans/modules/javadoc/search/JapanJavadocEncodings.java
@@ -0,0 +1,55 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2000 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+/* Base class providing search for JDK1.2/1.3 documentation
+ * Jdk12SearchType.java
+ *
+ * Created on 19. ?or 2001, 17:14
+ * @author Petr Hrebejk, Petr Suchomel
+ */
+
+/*
+ * JapanJavadocEncodings.java
+ *
+ * Created on March 7, 2001, 5:37 PM
+ */
+
+package org.netbeans.modules.javadoc.search;
+
+import java.beans.*;
+
+/**
+ *
+ * @author  Petr Suchomel
+ * @version 
+ */
+
+public class JapanJavadocEncodings extends PropertyEditorSupport {
+    
+    private static final String[] tags = { "KOI8", "UTF-8", "EUC-JP", "SJIS", "PC-KANJI"};
+    
+    /** @return names of the supported encodings */
+    public String[] getTags() {
+        return tags;
+    }
+
+    /** @return text for the current value */
+    public String getAsText () {
+        return ((String)getValue());
+    }
+
+    /** @param text A text for the current value. */
+    public void setAsText (String text) {
+        setValue( text );
+    }
+}
---
+++ javadoc/src/org/netbeans/modules/javadoc/search/Jdk12SearchType_japan.java
@@ -0,0 +1,133 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2000 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+/* Base class providing search for JDK1.2/1.3 documentation
+ * Jdk12SearchType.java
+ *
+ * Created on 19. ?or 2001, 17:14
+ * @author Petr Hrebejk, Petr Suchomel
+ */
+
+package org.netbeans.modules.javadoc.search;
+
+import java.util.*;
+import java.io.File;
+
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileSystem;
+import org.openide.ServiceType;
+import org.openide.util.HelpCtx;
+import org.openide.util.NbBundle;
+import org.openide.TopManager;
+import org.openide.NotifyDescriptor;
+
+public class Jdk12SearchType_japan extends JavadocSearchType {
+
+    private boolean caseSensitive = true;
+    private String  japanEncoding;
+    
+    /** generated Serialized Version UID */
+    static final long serialVersionUID =-2453877778724454324L;
+                                         
+    /** Returns human presentable name
+     * @return human presentable name
+    */
+    public String displayName() {
+        return NbBundle.getBundle( Jdk12SearchType_japan.class ).getString("CTL_Jdk12_search_eng_ja");
+    }
+
+    /** Returns HelpCtx
+     * @return help
+     */    
+    public HelpCtx getHelpCtx () {
+        return new HelpCtx (Jdk12SearchType_japan.class);
+    }
+    
+    /** Getter for property caseSensitive.
+     * @return Value of property caseSensitive.
+     */
+    public boolean isCaseSensitive() {
+        return caseSensitive;
+    }
+    
+    /** Setter for property caseSensitive.
+     * @param caseSensitive New value of property caseSensitive.
+    */
+    public void setCaseSensitive(boolean caseSensitive) {
+        boolean oldVal = caseSensitive;
+        this.caseSensitive = caseSensitive;
+        this.firePropertyChange("caseSensitive", new Boolean(oldVal), new Boolean(caseSensitive));
+    }
+
+    /** Getter for property encoding.
+     * @return Value of property encoding.
+    */
+    public java.lang.String getJapanEncoding() {
+        return ( japanEncoding != null ) ? japanEncoding : "KOI8";
+    }
+    
+    /** Setter for property encoding.
+     * @param encoding New value of property encoding.
+    */
+    public void setJapanEncoding(java.lang.String japanEncoding) {
+        this.japanEncoding = japanEncoding;
+    }    
+    
+    /** default returns null, must be overriden
+     * @param fs File system where to find index files
+     * @param rootOffset offset , position of index files in file system, normally null
+     * @return File object containing index-files
+    */
+    public FileObject getDocFileObject( FileSystem fs , String rootOffset ) {
+        //System.out.println("in jdk 12");        
+        if( rootOffset != null && rootOffset.length() != 0 ){
+            rootOffset = ( rootOffset.replace('/', '.') + '.' );
+        }
+        else{
+            rootOffset = "";
+        }            
+        FileObject fo = fs.find( rootOffset + "index-files", null, null ); // NOI18N
+        if ( fo != null ) {
+            return fo;
+        }
+
+        fo = fs.find( rootOffset + "", "index-all", "html" ); // NOI18N
+        if ( fo != null ) {
+            return fo;
+        }
+
+        fo = fs.find( rootOffset + "api.index-files", null, null ); // NOI18N
+        if ( fo != null ) {
+            return fo;
+        }
+
+        fo = fs.find( rootOffset + "api", "index-all", "html" ); // NOI18N
+        if ( fo != null ) {
+            return fo;
+        }
+
+        return null;
+    }    
+    
+    /** Returns Java doc search thread for doument
+     * @param toFind String to find
+     * @param fo File object containing index-files
+     * @param diiConsumer consumer for parse events
+     * @return IndexSearchThread
+     * @see IndexSearchThread
+     */    
+    public IndexSearchThread getSearchThread( String toFind, FileObject fo, IndexSearchThread.DocIndexItemConsumer diiConsumer ){
+        //here you can send one more parameter .. getJapanEncoding
+        return new SearchThreadJdk12_japan ( toFind, fo, diiConsumer, isCaseSensitive() );
+    }    
+}
