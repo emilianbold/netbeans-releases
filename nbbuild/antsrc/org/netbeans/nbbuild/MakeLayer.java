449c5d6fbd68dac74696006acc1d76b5168dd754
jglick <jglick@netbeans.org>
2001-03-09 11:28:54 +0000

    [Javadoc] Trivial.

--- nbbuild/antsrc/org/netbeans/nbbuild/MakeLayer.java
+++ nbbuild/antsrc/org/netbeans/nbbuild/MakeLayer.java
@@ -1,7 +1,14 @@
 /*
- * MakeLayer.java
- *
- * Created on February 1, 2001, 4:51 PM
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2000 Sun
+ * Microsystems, Inc. All Rights Reserved.
  */
 
 package org.netbeans.nbbuild;
@@ -15,10 +22,9 @@ import org.apache.tools.ant.FileScanner;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.taskdefs.MatchingTask;
 
-/**
+/** Create a fragment of a module's XML layer.
  *
  * @author  Michal Zlamal
- * @version 
  */
 public class MakeLayer extends MatchingTask {
 
