298b45e1f9820ff92f9013b6b9954631ea17be9a
jrechtacek <jrechtacek@netbeans.org>
2001-03-14 11:06:16 +0000

    INF#716 - change setting pointbase.ini system property to respect setting from pointbase module

--- db/src/org/netbeans/modules/db/DatabaseModule.java
+++ db/src/org/netbeans/modules/db/DatabaseModule.java
@@ -32,9 +32,35 @@ public class DatabaseModule extends ModuleInstall {
 
     static final long serialVersionUID =5426465356344170725L;
     
+    /* setting overtaken from PointBaseModule.java */
+    /** name of pointbase directory name in shared installation 
+     */
+    private static final String POINTBASE_HOME_DIR = "pointbase"; //NOI18N
+    
+    /** Name of pointbase directory in user's home directory 
+     */
+    private static final String POINTBASE_USER_DIR = "pointbasedb"; //NOI18N
+
+    /** Name of directory where are databases located
+     */
+    private static final String POINTBASE_DB_DIR = "databases"; //NOI18N
+    
+    /** The name of pointbase ini file
+     */
+    private static final String INI_FILE = "pointbase.ini"; //NOI18N
+
+    /** File separator */
+    private static final String SEPARATOR = java.io.File.separator;
+    
+    private static final String user = System.getProperty("netbeans.user"); //NOI18N
+    private static final String home = System.getProperty("netbeans.home"); //NOI18N
+        
     public void installed() {
 
-        System.setProperty("pointbase.ini", System.getProperty("netbeans.home") + java.io.File.separator + "pointbase" + java.io.File.separator + "pointbase.ini"); //NOI18N
+        // test if the ppointbase module is installed
+        // if not then ini file is set in the default place
+        if(System.getProperty(INI_FILE)==null)
+            System.setProperty(INI_FILE, home + java.io.File.separator + POINTBASE_HOME_DIR + java.io.File.separator + INI_FILE); //NOI18N
 
         TopManager tm = TopManager.getDefault();
 
@@ -60,7 +86,10 @@ public class DatabaseModule extends ModuleInstall {
     
     public void uninstalled() {
 
-        System.setProperty("pointbase.ini", System.getProperty("netbeans.home") + java.io.File.separator + "pointbase" + java.io.File.separator + "pointbase.ini"); //NOI18N
+        // test if the ppointbase module is installed
+        // if not then ini file is set in the default place
+        if(System.getProperty(INI_FILE)==null)
+            System.setProperty(INI_FILE, home + java.io.File.separator + POINTBASE_HOME_DIR + java.io.File.separator + INI_FILE); //NOI18N
 
         // closing all open connection
         try {
@@ -75,7 +104,11 @@ public class DatabaseModule extends ModuleInstall {
     }
     
     public void restored() {
-        System.setProperty("pointbase.ini", System.getProperty("netbeans.home") + java.io.File.separator + "pointbase" + java.io.File.separator + "pointbase.ini"); //NOI18N
+        // test if the ppointbase module is installed
+        // if not then ini file is set in the default place
+        if(System.getProperty(INI_FILE)==null)
+            System.setProperty(INI_FILE, home + java.io.File.separator + POINTBASE_HOME_DIR + java.io.File.separator + INI_FILE); //NOI18N
+
     }
 
     public boolean closing () {
