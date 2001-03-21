f054197cdd3c0f64ba848acae9e627e62d9b23cb
pzavadsky <pzavadsky@netbeans.org>
2001-03-21 18:10:08 +0000

    Bug fix #10538: IllegalArgument excpetions when switching sort in search result window while root node selected.

--- utilities/src/org/netbeans/modules/search/ResultModel.java
+++ utilities/src/org/netbeans/modules/search/ResultModel.java
@@ -11,39 +11,49 @@
  * Microsystems, Inc. All Rights Reserved.
  */
 
+
 package org.netbeans.modules.search;
 
-import java.awt.*;
-import java.util.*;
-import java.text.*;
-import java.beans.*;
 
-import javax.swing.event.*;
+import java.awt.Image;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.text.MessageFormat;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
-import org.openide.nodes.*;
-import org.openide.util.*;
-
-import org.openidex.search.*;
-
-import org.netbeans.modules.search.res.*;
+import org.netbeans.modules.search.res.Res;
 import org.netbeans.modules.search.types.DetailHandler;
+
+import org.openide.nodes.AbstractNode;
+import org.openide.nodes.Children;
+import org.openide.nodes.Node;
+import org.openide.nodes.NodeAcceptor;
+import org.openide.util.TaskListener;
+import org.openidex.search.SearchTask;
+import org.openidex.search.SearchType;
+
 
 /**
  * Holds search result data.
  * 
  * @author  Petr Kuzel
- * @version 1.0
  */
-public class ResultModel implements NodeAcceptor, TaskListener
-{
+public class ResultModel implements NodeAcceptor, TaskListener {
+    
     /** ChangeEvent object being used to notify about the search task finish. */
     private final ChangeEvent EVENT;
 
-    public final String PROP_SORTED = "sorted";
+    /** Property name of sorted proerty. */
+    public final String PROP_SORTED = "sorted"; // NOI18N
 
     /** Node representing root of found nodes.
-    * Its children hold all found nodes.
-    */
+     * Its children hold all found nodes. */
     private ResultRootNode root;
 
     /** Whether the nodes are sorted. */
@@ -51,26 +61,33 @@ public class ResultModel implements NodeAcceptor, TaskListener
 
     /** Unsorted list of found nodes. */
     private ArrayList unsortedNodes;
+    
     /** Sorted list of found nodes. */
     private ArrayList sortedNodes;
 
+    /** Search task. */
     private SearchTask task = null;
 
     /** Search state field. */
     private boolean done = false;
 
+    /** Set of listeners. */
     private HashSet listeners = new HashSet();
 
     /** Which criteria have produced this result. */
     private CriteriaModel criteria;
 
+    /** Indicates if to use search displayer. */
     private boolean useDisp = false;
+    
+    /** Search displayer. */
     private SearchDisplayer disp = null;
 
+    /** Porperty change support. */
     private PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
 
 
-    /** Creates new ResultModel */
+    /** Creates new <code>ResultModel</code>. */
     public ResultModel(CriteriaModel model) {
         EVENT = new ChangeEvent(this);
 
@@ -81,8 +98,8 @@ public class ResultModel implements NodeAcceptor, TaskListener
         criteria = model;
     }
 
-    /** Some nodes were found by engine.
-    */
+    
+    /** Accept nodes. Some nodes were found by engine. */
     public synchronized boolean acceptNodes(Node[] nodes) {
 
         root.getChildren().add(nodes);
@@ -102,8 +119,7 @@ public class ResultModel implements NodeAcceptor, TaskListener
     public void fillOutput () {
         if (useDisp) {
             disp.resetOutput();
-        }
-        else {
+        } else {
             disp = new SearchDisplayer();
             useDisp = true;
         }
@@ -112,9 +128,8 @@ public class ResultModel implements NodeAcceptor, TaskListener
     }
 
     /** Does used criteria allow filling output window?
-    * Currently it checks for presence of DetailHandler.
-    * @return true it it can be used.
-    */
+     * Currently it checks for presence of DetailHandler.
+     * @return true it it can be used. */
     synchronized boolean canFillOutput() {
 
         SearchType[] crs = getCriteriaModel().getCustomizedCriteria();
@@ -133,31 +148,30 @@ public class ResultModel implements NodeAcceptor, TaskListener
         return false;
     }
 
-    /** Is search engine still running?  */
+    /** Is search engine still running? */
     public boolean isDone() {
         return done;
     }
 
-    /**
-    */
+    /** Sets search task. */
     public void setTask (SearchTask task) {
         this.task = task;
         this.task.addTaskListener(this);
     }
 
-    /** @return root node of result
-    */
+    /** Gets result root node.
+     * @return root node of result. */
     public Node getRoot() {
         return root;
     }
 
-    /**
-    * @return criteria model that produces these results.
-    */
+    /** Gets criteria model.
+     * @return criteria model that produces these results. */
     public CriteriaModel getCriteriaModel() {
         return criteria;
     }
 
+    /** Gets number of found nodes. */
     public int getFound() {
         return unsortedNodes.size();
     }
@@ -168,16 +182,15 @@ public class ResultModel implements NodeAcceptor, TaskListener
     }
 
     /** Sort or unsort found nodes. (Display name is used for sorting.)
-      * A new root node is created. Should not be called until search is finished.
-      * @return the new root node with (un)sorted subnodes.
-      */
+     * A new root node is created. Should not be called until search is finished.
+     * @return the new root node with (un)sorted subnodes.
+     */
     public Node sortNodes(boolean sort) {
-        if (sort == sorted) return root;
+        if(sort == sorted)
+            return root;
 
-        Children ch = new Children.Array();
-        Node oldRoot = root;
-        root = new ResultRootNode(ch, getRootDisplayName());
-
+        root.setDisplayName(getRootDisplayName());
+        
         // copy one array of nodes to another array of nodes which is
         // of type Node[] ...
         Object[] objects;
@@ -186,21 +199,23 @@ public class ResultModel implements NodeAcceptor, TaskListener
 
         objects = sortedNodes.toArray();
         sortedN = new Node[objects.length];
+        
         for (int i=0; i < objects.length; i++)
             sortedN[i] = (Node)objects[i];
 
         objects = unsortedNodes.toArray();
         unsortedN = new Node[objects.length];
+        
         for (int i=0; i < objects.length; i++)
             unsortedN[i] = (Node)objects[i];
 
         if (sort) {
-            oldRoot.getChildren().remove(unsortedN);
-            ch.add(sortedN);
+            root.getChildren().remove(unsortedN);
+            root.getChildren().add(sortedN);
         }
         else {
-            oldRoot.getChildren().remove(sortedN);
-            ch.add(unsortedN);
+            root.getChildren().remove(sortedN);
+            root.getChildren().add(unsortedN);
         }
 
         sorted = sort;
@@ -210,8 +225,7 @@ public class ResultModel implements NodeAcceptor, TaskListener
     }
 
 
-    /** Search task finished. Notify all listeners.
-    */
+    /** Search task finished. Notify all listeners. */
     public void taskFinished(final org.openide.util.Task task) {
         done = true;
         root.setDisplayName(getRootDisplayName());
@@ -219,6 +233,7 @@ public class ResultModel implements NodeAcceptor, TaskListener
         fireChange();
     }
 
+    /** Gets display name for root node. Utilitty method. */
     private String getRootDisplayName() {
         if (!isDone()) {
             return Res.text("SEARCHING___"); // NOI18N
@@ -229,30 +244,30 @@ public class ResultModel implements NodeAcceptor, TaskListener
         if (found == 1) {
             return MessageFormat.format(Res.text("MSG_FOUND_A_NODE"), // NOI18N
                                         new Object[] { new Integer(found) } );
-        } 
-        else if (found > 1) {
+        } else if (found > 1) {
             return MessageFormat.format(Res.text("MSG_FOUND_X_NODES"), // NOI18N
                                         new Object[] { new Integer(found) } );
-        } 
-        else { // <1
+        } else { // <1
             return Res.text("MSG_NO_NODE_FOUND"); // NOI18N
         }
     }
 
+    /** Stops serach task. */
     public void stop() {
         if (task != null) task.stop();
     }
 
+    /** Adds change listsner. */
     public void addChangeListener(ChangeListener lis) {
         listeners.add(lis);
     }
 
+    /** Removes change listener. */
     public void removeChangeListener(ChangeListener lis) {
         listeners.remove(lis);
     }
 
-    /** Fire event to all listeners.
-    */
+    /** Fires change event to all listeners. */
     private void fireChange() {
         Iterator it = listeners.iterator();
 
@@ -263,24 +278,20 @@ public class ResultModel implements NodeAcceptor, TaskListener
     }
 
     /** Adds a <code>PropertyChangeListener</code> to the listener list.
-     * @param l The listener to add.
-     */
+     * @param l The listener to add. */
     public void addPropertyChangeListener(PropertyChangeListener l) {
         propertyChangeSupport.addPropertyChangeListener(l);
     }
 
     /** Removes a <code>PropertyChangeListener</code> from the listener list.
-     * @param l The listener to remove.
-     */
-    public void removePropertyChangeListener(java.beans.PropertyChangeListener l) {
+     * @param l The listener to remove. */
+    public void removePropertyChangeListener(PropertyChangeListener l) {
         propertyChangeSupport.removePropertyChangeListener(l);
     }
 
-    /** Search Result root node. May contain some statistic properties.
-    */
-    private static class ResultRootNode extends AbstractNode //implements Comparable
-    {
-        /** Create a new node with no content. */
+    /** Search Result root node. May contain some statistic properties. */
+    private static class ResultRootNode extends AbstractNode {
+        /** Creates a new node with no content. */
         public ResultRootNode() {
             super(new Children.Array());
 
@@ -288,46 +299,49 @@ public class ResultModel implements NodeAcceptor, TaskListener
             setDisplayName(Res.text("SEARCHING___")); // NOI18N
         }
 
-        /** Create a new node with subnodes. */
+        /** Creates a new node with subnodes. */
         public ResultRootNode(Children ch, String dispName) {
             super(ch);
             setDisplayName(dispName);
         }
 
-        /** @return universal search icon.
-        */
+        
+        /** Gets icon. Overrides superclass method.
+         * @return universal search icon. */
         public Image getIcon(int type) {
             return Res.image("SEARCH"); // NOI18N
         }
 
+        /** Gets opened icon. Overrides superclass method. */
         public Image getOpenedIcon(int type) {
             return getIcon(type);
         }
+    } // End of ResultRoorNode class.
+    
 
-//        public int compareTo(Object o) {
-//            Node node = (Node) o;
-//            return getDisplayName().compareTo(node.getDisplayName());
-//        }
-    }
-
-    private static class NodeNameComparator implements Comparator
-    {
+    /** Node name comparator, */
+    private static class NodeNameComparator implements Comparator {
+        /** Comparator. */
         private static Comparator comparator = null;
 
-        /** Compare two nodes according to their display names. */
+        /** Compare two nodes according to their display names.
+         * Implements <code>Comparator</code> interface method. */
         public int compare(Object o1,Object o2) {
             return ((Node)o1).getDisplayName().compareTo(((Node)o2).getDisplayName());
         }
 
+        /** Implements <code>Comparator</code> interface method. */
         public boolean equals(Object obj) {
             return obj instanceof NodeNameComparator;
         }
 
-        /** @return the instance of <code>NodeNameComparator</code> */
+        /** Gets the instance pf comparator. 
+         * @return the instance of <code>NodeNameComparator</code> */
         public static Comparator getComparator() {
             if (comparator == null)
                 comparator = new NodeNameComparator();
             return comparator;
         }
-    }
+    } // End of NodeNameComparator class.
+    
 }
