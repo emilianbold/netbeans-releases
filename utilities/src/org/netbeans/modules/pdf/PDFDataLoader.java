cc627d134222d659ad7d224053b6e5f367a27f37
pzavadsky <pzavadsky@netbeans.org>
2001-04-02 09:47:58 +0000

    Bug fix #10933: Data loader performance.

--- utilities/src/org/netbeans/modules/pdf/PDFDataLoader.java
+++ utilities/src/org/netbeans/modules/pdf/PDFDataLoader.java
@@ -11,7 +11,9 @@
  * Microsystems, Inc. All Rights Reserved.
  */
 
+
 package org.netbeans.modules.pdf;
+
 
 import java.io.IOException;
 
@@ -21,49 +23,61 @@ import org.openide.loaders.*;
 import org.openide.util.NbBundle;
 import org.openide.util.actions.SystemAction;
 
+
 /** Loader for PDF files (Portable Document Format).
  * Permits simple viewing of them.
  * @author Jesse Glick
  */
 public class PDFDataLoader extends UniFileLoader {
 
+    /** Generated serial version UID. */
     private static final long serialVersionUID = -4354042385752587850L;
-    
+
+    /** Creates loader. */
     public PDFDataLoader () {
         this (PDFDataObject.class);
     }
 
+    /** Creates loader for specified recognized class. */
     public PDFDataLoader (Class recognizedObject) {
         super (recognizedObject);
     }
 
+    
+    /** Initizalized loader, i.e. its extension list. Overrides superclass method. */
     protected void initialize () {
-        
-        setDisplayName (NbBundle.getMessage (PDFDataLoader.class, "LBL_loaderName"));
-
         ExtensionList extensions = new ExtensionList ();
         extensions.addExtension ("pdf"); // NOI18N
         setExtensions (extensions);
-
-        setActions (new SystemAction[] {
-                        SystemAction.get (OpenAction.class),
-                        SystemAction.get (FileSystemAction.class),
-                        null,
-                        SystemAction.get (CutAction.class),
-                        SystemAction.get (CopyAction.class),
-                        SystemAction.get (PasteAction.class),
-                        null,
-                        SystemAction.get (DeleteAction.class),
-                        SystemAction.get (RenameAction.class),
-                        null,
-                        SystemAction.get (SaveAsTemplateAction.class),
-                        null,
-                        SystemAction.get (ToolsAction.class),
-                        SystemAction.get (PropertiesAction.class),
-                    });
-
+    }
+    
+    /** Gets default display name. Overrides superclass method. */
+    protected String defaultDisplayName() {
+        return NbBundle.getMessage (PDFDataLoader.class, "LBL_loaderName");
+    }
+    
+    /** Gets default system actions. Overrides superclass method. */
+    protected SystemAction[] defaultActions() {
+        return new SystemAction[] {
+            SystemAction.get (OpenAction.class),
+            SystemAction.get (FileSystemAction.class),
+            null,
+            SystemAction.get (CutAction.class),
+            SystemAction.get (CopyAction.class),
+            SystemAction.get (PasteAction.class),
+            null,
+            SystemAction.get (DeleteAction.class),
+            SystemAction.get (RenameAction.class),
+            null,
+            SystemAction.get (SaveAsTemplateAction.class),
+            null,
+            SystemAction.get (ToolsAction.class),
+            SystemAction.get (PropertiesAction.class),
+        };
     }
 
+    /** Creates multi data objcte for specified primary file.
+     * Implements superclass abstract method. */
     protected MultiDataObject createMultiObject (FileObject primaryFile)
     throws DataObjectExistsException, IOException {
         return new PDFDataObject (primaryFile, this);
