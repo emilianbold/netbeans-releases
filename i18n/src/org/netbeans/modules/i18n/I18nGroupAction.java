71e9a9c17ff89efb7d5d16343fd2fc93b676f7ff
pzavadsky <pzavadsky@netbeans.org>
2001-04-06 09:33:30 +0000

    Performance. Lazy popup init of i18n group actions.

--- i18n/src/org/netbeans/modules/i18n/I18nGroupAction.java
+++ i18n/src/org/netbeans/modules/i18n/I18nGroupAction.java
@@ -16,10 +16,20 @@ package org.netbeans.modules.i18n;
 
 
 import java.awt.event.ActionEvent;
+import javax.swing.event.PopupMenuEvent;
+import javax.swing.event.PopupMenuListener;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.SwingUtilities;
 
 import org.netbeans.modules.i18n.wizard.I18nWizardAction;
 import org.netbeans.modules.i18n.wizard.I18nTestWizardAction;
 
+import org.openide.awt.JInlineMenu;
+import org.openide.awt.JMenuPlus;
+import org.openide.util.actions.Presenter;
 import org.openide.util.actions.SystemAction;
 import org.openide.util.HelpCtx;
 
@@ -33,6 +43,9 @@ import org.openide.util.HelpCtx;
  */
 public abstract class I18nGroupAction extends SystemAction {
 
+    /** Icon. */
+    private static Icon icon = null;
+    
     /** Array of i18n actions. */
     protected static final SystemAction[] i18nActions = new SystemAction[] {
         SystemAction.get(I18nWizardAction.class),
@@ -40,6 +53,7 @@ public abstract class I18nGroupAction extends SystemAction {
         SystemAction.get(I18nAction.class),
         SystemAction.get(InsertI18nStringAction.class)
     };
+    
 
     /** Does nothing. Shouldn't be called. Implements superclass abstract method. */
     public void actionPerformed(ActionEvent ev) {
@@ -59,5 +73,77 @@ public abstract class I18nGroupAction extends SystemAction {
     public HelpCtx getHelpCtx () {
         return new HelpCtx(I18nUtil.HELP_ID_I18N);
     }
+    
+    /** Cretates menu item with lazy popup.
+     * @param isMenu flag if the item os part of normal menu (not popup). */
+    protected JMenuItem createLazyPopup(boolean isMenu) {
+        return new LazyPopup(isMenu);
+    }
+    
+    
+    /** Menu item which will create its items lazilly when the popup will becomming visible.
+     * Performance savings.*/
+    private class LazyPopup extends JInlineMenu implements PopupMenuListener {
+
+        /** Sub menu. */
+        private final JMenu menu;
+
+        /** Indicates if is part of menu, i.e. if should have icons. */
+        private boolean isMenu;
+        
+        /** Indicates whether menu items were created. */
+        private boolean created = false;
+        
+
+        /** Constructor. */
+        public LazyPopup(boolean isMenu) {
+            this.menu = new JMenuPlus(I18nGroupAction.this.getName());
+            this.isMenu = isMenu;
+            
+            if(isMenu) {
+                // Binary-incompatible across SystemAction.icon ImageIcon -> Icon change:
+                //menu.setIcon (getIcon ());
+                if(icon == null) 
+                    icon = new ImageIcon(I18nGroupAction.class.getResource(I18nGroupAction.this.iconResource()));
+
+                menu.setIcon(icon);
+            }
+            
+            menu.getPopupMenu().addPopupMenuListener(this);
+            setMenuItems(new JMenuItem[] {menu});
+        }
+
+
+        /** Dummy implementation of <code>PopupMenuListener</code>. */
+        public void popupMenuCanceled(PopupMenuEvent evt) {}
+
+        /** Dummy implementation of <code>PopupMenuListener</code>. */
+        public void popupMenuWillBecomeInvisible(PopupMenuEvent evt) {}
+
+        /** Implemsnts <code>PopupMenuListener</code> method. */
+        public void popupMenuWillBecomeVisible(PopupMenuEvent evt) {
+            // We are AWT-Event-queue.
+            if(!created)
+                createMenuItems(); 
+        }
+
+        /** Creates items when actually needed. */
+        private void createMenuItems() {
+            created = true;
+            menu.removeAll();
+
+            for(int i=0; i<i18nActions.length; i++) {
+                SystemAction action = i18nActions[i];
+
+                if(action == null) {
+                    menu.addSeparator ();
+                } else if(!isMenu && action instanceof Presenter.Popup) {
+                    menu.add(((Presenter.Popup)action).getPopupPresenter());
+                } else if(isMenu && action instanceof Presenter.Menu) {
+                    menu.add(((Presenter.Menu)action).getMenuPresenter());
+                }
+            }
+        }
+    } // End of class Popup.
 
 }
