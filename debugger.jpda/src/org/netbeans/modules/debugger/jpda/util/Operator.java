024dc3ba8ab10050ec3cee2f212872db6d948424
jjancura <jjancura@netbeans.org>
2001-05-09 12:57:27 +0000

    Optimalizations & Memory Leaks

--- debugger.jpda/src/org/netbeans/modules/debugger/jpda/util/Operator.java
+++ debugger.jpda/src/org/netbeans/modules/debugger/jpda/util/Operator.java
@@ -39,59 +39,59 @@ public class Operator {
         final Runnable finalizer
     ) {
         thread = new Thread (new Runnable () {
-                                 public void run () {
-                                     EventQueue queue = virtualMachine.eventQueue ();
-                                     try {
-                                         for (;;) {
-                                             EventSet set = queue.remove ();
-                                             resume = false;
-                                             stopRequest = false;
-                                             EventIterator i = set.eventIterator ();
-                                             while (i.hasNext ()) {
-                                                 Event e = i.nextEvent ();
-                                                 if ((e instanceof VMDeathEvent) ||
-                                                         (e instanceof VMDisconnectEvent)
-                                                    ) {
-                                                     disconnected = true;
-                                                     if (finalizer != null) finalizer.run ();
-                                                     //S ystem.out.println ("EVENT: " + e); // NOI18N
-                                                     //S ystem.out.println ("Operator end"); // NOI18N
-                                                     return;
-                                                 }
-                                                 if ((e instanceof VMStartEvent) && (starter != null)) {
-                                                     starter.run ();
-                                                     //S ystem.out.println ("Operator.start VM"); // NOI18N
-                                                     continue;
-                                                 }
-                                                 Executor exec = null;
-                                                 if (e.request () == null) {
-                                                     //S ystem.out.println ("EVENT: " + e + " REQUEST: null"); // NOI18N
-                                                 } else
-                                                     exec = (Executor) e.request ().getProperty ("executor");
+         public void run () {
+             EventQueue queue = virtualMachine.eventQueue ();
+             try {
+                 for (;;) {
+                     EventSet set = queue.remove ();
+                     resume = false;
+                     stopRequest = false;
+                     EventIterator i = set.eventIterator ();
+                     while (i.hasNext ()) {
+                         Event e = i.nextEvent ();
+                         if ((e instanceof VMDeathEvent) ||
+                                 (e instanceof VMDisconnectEvent)
+                            ) {
+                             disconnected = true;
+                             if (finalizer != null) finalizer.run ();
+                             //S ystem.out.println ("EVENT: " + e); // NOI18N
+                             //S ystem.out.println ("Operator end"); // NOI18N
+                             return;
+                         }
+                         if ((e instanceof VMStartEvent) && (starter != null)) {
+                             starter.run ();
+                             //S ystem.out.println ("Operator.start VM"); // NOI18N
+                             continue;
+                         }
+                         Executor exec = null;
+                         if (e.request () == null) {
+                             //S ystem.out.println ("EVENT: " + e + " REQUEST: null"); // NOI18N
+                         } else
+                             exec = (Executor) e.request ().getProperty ("executor");
 
-                                                 // printEvent (e, exec);
+                         // printEvent (e, exec);
 
-                                                 // safe invocation of user action
-                                                 if (exec != null)
-                                                     try {
-                                                         exec.exec (e);
-                                                     } catch (Exception ex) {
-                                                         ex.printStackTrace ();
-                                                     }
-                                             }
-                                             //            S ystem.out.println ("END (" + set.suspendPolicy () + ") ==========================================================================="); // NOI18N
-                                             if (resume && !stopRequest)
-                                                 virtualMachine.resume ();
-                                         }
-                                     } catch (VMDisconnectedException e) {   
-                                     } catch (InterruptedException e) {
-                                     } catch (Exception e) {
-                                         e.printStackTrace ();
-                                     }
-                                     if (finalizer != null) finalizer.run ();
-                                     //S ystem.out.println ("Operator end"); // NOI18N
-                                 }
-                             }, "Debugger operator thread"); // NOI18N
+                         // safe invocation of user action
+                         if (exec != null)
+                             try {
+                                 exec.exec (e);
+                             } catch (Exception ex) {
+                                 ex.printStackTrace ();
+                             }
+                     }
+                     //            S ystem.out.println ("END (" + set.suspendPolicy () + ") ==========================================================================="); // NOI18N
+                     if (resume && !stopRequest)
+                         virtualMachine.resume ();
+                 }
+             } catch (VMDisconnectedException e) {   
+             } catch (InterruptedException e) {
+             } catch (Exception e) {
+                 e.printStackTrace ();
+             }
+             if (finalizer != null) finalizer.run ();
+             //S ystem.out.println ("Operator end"); // NOI18N
+         }
+     }, "Debugger operator thread"); // NOI18N
     }
 
     /**
@@ -103,6 +103,10 @@ public class Operator {
 
     public void register (EventRequest req, Executor e) {
         req.putProperty ("executor", e); // NOI18N
+    }
+
+    public void unregister (EventRequest req) {
+        req.putProperty ("executor", null); // NOI18N
     }
 
     /**
@@ -163,26 +167,3 @@ public class Operator {
         }
     }
 }
-
-/*
- * Log
- *  15   Jaga      1.9.3.4     3/30/00  Jan Jancura     Comments out!
- *  14   Jaga      1.9.3.3     3/22/00  Jan Jancura     
- *  13   Jaga      1.9.3.2     3/6/00   Jan Jancura     Non java languages 
- *       support  + JDK1.2 patches
- *  12   Jaga      1.9.3.1     3/2/00   Jan Jancura     
- *  11   Jaga      1.9.3.0     2/25/00  Daniel Prusa    post 1.0 changes
- *  10   Gandalf   1.9         1/14/00  Daniel Prusa    NOI18N
- *  9    Gandalf   1.8         1/13/00  Daniel Prusa    NOI18N
- *  8    Gandalf   1.7         1/4/00   Jan Jancura     Use trim () on user 
- *       input.
- *  7    Gandalf   1.6         11/9/99  Jan Jancura     sout commented out.
- *  6    Gandalf   1.5         11/8/99  Jan Jancura     Somma classes renamed
- *  5    Gandalf   1.4         10/23/99 Ian Formanek    NO SEMANTIC CHANGE - Sun
- *       Microsystems Copyright in File Comment
- *  4    Gandalf   1.3         9/28/99  Jan Jancura     
- *  3    Gandalf   1.2         9/28/99  Jan Jancura     
- *  2    Gandalf   1.1         9/2/99   Jan Jancura     
- *  1    Gandalf   1.0         7/13/99  Jan Jancura     
- * $
- */
