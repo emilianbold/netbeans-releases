d28be6a1e4b30c55ce699165a09f51ec41f859ac
pnejedly <pnejedly@netbeans.org>
2001-05-14 07:14:52 +0000

    Added the missing license headers

--- performance/test/perf/src/org/netbeans/performance/platform/ExceptionConstruct.java
+++ performance/test/perf/src/org/netbeans/performance/platform/ExceptionConstruct.java
@@ -1,7 +1,14 @@
 /*
- * ExceptionConstruct.java
- *
- * Created on 28. bre 2001, 17:44
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2000 Sun
+ * Microsystems, Inc. All Rights Reserved.
  */
 
 package org.netbeans.performance.platform;
