616755a817c873648344c5480b1e95538240abfd
phamernik <phamernik@netbeans.org>
2001-05-29 14:25:59 +0000

    SPL added

--- nbbuild/misc/bugcompare/ChangelogParslet.java
+++ nbbuild/misc/bugcompare/ChangelogParslet.java
@@ -1,13 +1,14 @@
 /*
- * ChangelogParslet.java
- *
- * Created on 28. kvìten 2001, 18:26
- */
-
-/**
- *
- * @author  ph97928
- * @version 
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2001 Sun
+ * Microsystems, Inc. All Rights Reserved.
  */
 public class ChangelogParslet {
 
