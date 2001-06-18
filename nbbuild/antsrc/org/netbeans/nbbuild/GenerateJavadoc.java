a1c24dbbfc155c0aa0d3ebfd1be8c65feaa50160
jglick <jglick@netbeans.org>
2001-06-18 10:21:47 +0000

    Adding missing Sun Public License notice.

--- nbbuild/antsrc/org/netbeans/nbbuild/GenerateJavadoc.java
+++ nbbuild/antsrc/org/netbeans/nbbuild/GenerateJavadoc.java
@@ -1,3 +1,16 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2001 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
 package org.netbeans.nbbuild;
 
 import java.io.File;
@@ -11,9 +24,8 @@ import org.apache.tools.ant.taskdefs.*;
 import org.apache.tools.ant.types.*;
 
 /**
- * @author  Michal Zlamal
- * @version
  * This class generating javadoc
+ * @author  Michal Zlamal
  */
 public class GenerateJavadoc extends Task
 {
