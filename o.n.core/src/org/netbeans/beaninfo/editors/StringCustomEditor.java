a09a9183e46e084c4af27dd4c547ff2463d3db4a
dstrupl <dstrupl@netbeans.org>
2001-06-21 13:38:01 +0000

    #9219: disable property string editor for disabled properties

--- o.n.core/src/org/netbeans/beaninfo/editors/StringCustomEditor.java
+++ o.n.core/src/org/netbeans/beaninfo/editors/StringCustomEditor.java
@@ -38,6 +38,7 @@ public class StringCustomEditor extends javax.swing.JPanel implements EnhancedCu
         
         // bugfix# 9219 sets the text editable or not
         textArea.setEditable(editor.isEditable());
+        textArea.setEnabled(editor.isEditable());
         
         textArea.setText (s);
         setBorder (new javax.swing.border.EmptyBorder (new java.awt.Insets(8, 8, 8, 8)));
@@ -80,26 +81,3 @@ public class StringCustomEditor extends javax.swing.JPanel implements EnhancedCu
     // End of variables declaration//GEN-END:variables
 
 }
-
-/*
- * Log
- *  11   Gandalf   1.10        1/13/00  Petr Jiricka    i18n
- *  10   Gandalf   1.9         10/22/99 Ian Formanek    NO SEMANTIC CHANGE - Sun
- *       Microsystems Copyright in File Comment
- *  9    Gandalf   1.8         8/18/99  Ian Formanek    Fixed bug 2322 - Some PE
- *       couldn't be initialized - en exception is issued
- *  8    Gandalf   1.7         8/9/99   Ian Formanek    Generated Serial Version
- *       UID
- *  7    Gandalf   1.6         7/15/99  Ian Formanek    Fixed preferred size of 
- *       the custom property editor
- *  6    Gandalf   1.5         7/8/99   Jesse Glick     Context help.
- *  5    Gandalf   1.4         6/30/99  Ian Formanek    Reflecting changes in 
- *       editors packages and enhanced property editor interfaces
- *  4    Gandalf   1.3         6/8/99   Ian Formanek    ---- Package Change To 
- *       org.openide ----
- *  3    Gandalf   1.2         5/31/99  Ian Formanek    Updated to X2 format
- *  2    Gandalf   1.1         4/3/99   Ian Formanek    Fixed bug 1320 - 
- *       Connecion wizard does not work. No wizard window is opened.
- *  1    Gandalf   1.0         1/5/99   Ian Formanek    
- * $
- */
