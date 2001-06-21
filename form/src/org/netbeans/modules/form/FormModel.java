94180180d94b8f92539395af0f92e0cb8883b1a2
jkozak <jkozak@netbeans.org>
2001-06-21 08:20:41 +0000

    #13037: small cosmetic change

--- form/src/org/netbeans/modules/form/FormModel.java
+++ form/src/org/netbeans/modules/form/FormModel.java
@@ -329,7 +329,8 @@ public class FormModel
             ((RADMenuItemComponent)comp).freeMenu();
         
         deleteEventHandlersRecursively(comp);
-        if (eventHandlers!=null) eventHandlers.setNeed2Notify(true);
+        if (eventHandlers!=null) 
+            eventHandlers.setNeed2Notify(true);
         deleteVariables(comp);
     }
 
