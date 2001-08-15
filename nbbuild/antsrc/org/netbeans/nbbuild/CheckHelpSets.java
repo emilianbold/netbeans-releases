a6313fee9ac16b949987e1115feddea307973b50
jglick <jglick@netbeans.org>
2001-08-15 23:26:04 +0000

    #13931: Ant task to sanity-check helpsets.

---
+++ nbbuild/antsrc/org/netbeans/nbbuild/CheckHelpSets.java
@@ -0,0 +1,223 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2001 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+// See #13931.
+
+package org.netbeans.nbbuild;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+import javax.swing.tree.DefaultMutableTreeNode;
+
+import org.apache.tools.ant.*;
+import org.apache.tools.ant.types.FileSet;
+
+import javax.help.*;
+
+import javax.xml.parsers.*;
+import org.xml.sax.*;
+import org.xml.sax.helpers.*;
+
+/** Task to check various aspects of JavaHelp helpsets.
+ * <ol>
+ * <li>General parsability as far as JavaHelp is concerned.
+ * <li>Map IDs are not duplicated.
+ * <li>Map IDs point to real HTML files (and anchors where specified).
+ * <li>TOC/Index navigators refer to real map IDs.
+ * <li>HTML links in reachable HTML files point to valid places (including anchors).
+ * </ol>
+ * @author Jesse Glick
+ */
+public class CheckHelpSets extends Task {
+    
+    private List filesets = new ArrayList(); // List<FileSet>
+    
+    /** Add a fileset with one or more helpsets in it.
+     * <strong>Only</strong> the <samp>*.hs</samp> should match!
+     * All other files will be found from it.
+     */
+    public void addFileset(FileSet fs) {
+        filesets.add(fs);
+    }
+    
+    public void execute() throws BuildException {
+        Iterator it = filesets.iterator();
+        while (it.hasNext()) {
+            FileSet fs = (FileSet)it.next();
+            FileScanner scanner = fs.getDirectoryScanner(project);
+            File dir = scanner.getBasedir();
+            String[] files = scanner.getIncludedFiles();
+            for (int i = 0; i < files.length; i++) {
+                File helpset = new File(dir, files[i]);
+                try {
+                    checkHelpSet(helpset);
+                } catch (BuildException be) {
+                    throw be;
+                } catch (Exception e) {
+                    throw new BuildException("Error checking helpset", e, new Location(helpset.getAbsolutePath()));
+                }
+            }
+        }
+    }
+    
+    private void checkHelpSet(File hsfile) throws Exception {
+        log("Checking helpset: " + hsfile);
+        HelpSet hs = new HelpSet(null, hsfile.toURL());
+        javax.help.Map map = hs.getCombinedMap();
+        log("Parsed helpset, checking map IDs in TOC/Index navigators...");
+        NavigatorView[] navs = hs.getNavigatorViews();
+        for (int i = 0; i < navs.length; i++) {
+            String name = navs[i].getName();
+            File navfile = new File(hsfile.getParentFile(), (String)navs[i].getParameters().get("data"));
+            if (! navfile.exists()) throw new BuildException("Navigator " + name + " not found", new Location(navfile.getAbsolutePath()));
+            if (navs[i] instanceof IndexView) {
+                log("Checking index navigator " + name, Project.MSG_VERBOSE);
+                IndexView.parse(navfile.toURL(), hs, Locale.getDefault(), new VerifyTIFactory(hs, map, navfile, false));
+            } else if (navs[i] instanceof TOCView) {
+                log("Checking TOC navigator " + name, Project.MSG_VERBOSE);
+                TOCView.parse(navfile.toURL(), hs, Locale.getDefault(), new VerifyTIFactory(hs, map, navfile, true));
+            } else {
+                log("Skipping non-TOC/Index view: " + name, Project.MSG_VERBOSE);
+            }
+        }
+        log("Checking for duplicate map IDs...");
+        HelpSet.parse(hsfile.toURL(), null, new VerifyHSFactory());
+        log("Checking links from help map and between HTML files...");
+        Enumeration e = map.getAllIDs();
+        Set okurls = new HashSet(1000); // Set<URL>
+        Set badurls = new HashSet(1000); // Set<URL>
+        Set openableurls = new HashSet(1000); // Set<URL>
+        while (e.hasMoreElements()) {
+            javax.help.Map.ID id = (javax.help.Map.ID)e.nextElement();
+            URL u = map.getURLFromID(id);
+            if (u == null) {
+                throw new BuildException("Bogus map ID: " + id.id, new Location(hsfile.getAbsolutePath()));
+            }
+            log("Checking ID " + id.id, Project.MSG_VERBOSE);
+            CheckLinks.scan(this, id.id, u, openableurls, okurls, badurls, false, 2);
+        }
+    }
+    
+    private final class VerifyTIFactory implements TreeItemFactory {
+        
+        private final HelpSet hs;
+        private final javax.help.Map map;
+        private final File navfile;
+        private final boolean toc;
+        public VerifyTIFactory(HelpSet hs, javax.help.Map map, File navfile, boolean toc) {
+            this.hs = hs;
+            this.map = map;
+            this.navfile = navfile;
+            this.toc = toc;
+        }
+        
+        // The useful method:
+        
+        public TreeItem createItem(String str, Hashtable hashtable, HelpSet helpSet, Locale locale) {
+            String target = (String)hashtable.get("target");
+            if (target != null) {
+                if (! map.isValidID(target, hs)) {
+                    log(navfile + ": invalid map ID: " + target, Project.MSG_WARN);
+                } else {
+                    log("OK map ID: " + target, Project.MSG_VERBOSE);
+                }
+            }
+            return createItem();
+        }
+        
+        // Filler methods:
+        
+        public java.util.Enumeration listMessages() {
+            return Collections.enumeration(Collections.EMPTY_LIST);
+        }
+        
+        public void processPI(HelpSet helpSet, String str, String str2) {
+        }
+        
+        public void reportMessage(String str, boolean param) {
+            log(str, param ? Project.MSG_VERBOSE : Project.MSG_WARN);
+        }
+        
+        public void processDOCTYPE(String str, String str1, String str2) {
+        }
+        
+        public void parsingStarted(URL uRL) {
+        }
+        
+        public DefaultMutableTreeNode parsingEnded(DefaultMutableTreeNode defaultMutableTreeNode) {
+            return defaultMutableTreeNode;
+        }
+        
+        public TreeItem createItem() {
+            if (toc) {
+                return new TOCItem();
+            } else {
+                return new IndexItem();
+            }
+        }
+        
+    }
+    
+    private final class VerifyHSFactory extends HelpSet.DefaultHelpSetFactory {
+        
+        private Set ids = new HashSet(1000); // Set<String>
+        
+        public void processMapRef(HelpSet hs, Hashtable attrs) {
+            try {
+                URL map = new URL(hs.getHelpSetURL(), (String)attrs.get("location"));
+                SAXParserFactory factory = SAXParserFactory.newInstance();
+                factory.setValidating(false);
+                factory.setNamespaceAware(false);
+                SAXParser parser = factory.newSAXParser();
+                parser.parse(new InputSource(map.toExternalForm()), new Handler(map.getFile()));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        
+        // Using SAX 1 because Ant does not always ship with SAX 2:
+        private final class Handler extends HandlerBase {
+            
+            private final String map;
+            public Handler(String map) {
+                this.map = map;
+            }
+            
+            public void startElement(String name, AttributeList attr) {
+                if (name.equals("mapID")) {
+                    String target = attr.getValue("target");
+                    if (target != null) {
+                        if (ids.add(target)) {
+                            log("Found map ID: " + target, Project.MSG_DEBUG);
+                        } else {
+                            log(map + ": duplicated ID: " + target, Project.MSG_WARN);
+                        }
+                    }
+                }
+            }
+            
+            public InputSource resolveEntity(String pub, String sys) throws SAXException {
+                if (pub.equals("-//Sun Microsystems Inc.//DTD JavaHelp Map Version 1.0//EN")) {
+                    // Ignore.
+                    return new InputSource(new ByteArrayInputStream(new byte[0]));
+                } else {
+                    return super.resolveEntity(pub, sys);
+                }
+            }
+            
+        }
+        
+    }
+    
+}
--- nbbuild/antsrc/org/netbeans/nbbuild/CheckLinks.java
+++ nbbuild/antsrc/org/netbeans/nbbuild/CheckLinks.java
@@ -20,6 +20,7 @@ import java.util.*;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.FileScanner;
 import org.apache.tools.ant.Project;
+import org.apache.tools.ant.Task;
 import org.apache.tools.ant.taskdefs.MatchingTask;
 
 import org.apache.regexp.*;
@@ -31,7 +32,6 @@ import org.apache.regexp.*;
 
 /** Task to check for broken links in HTML.
  * Note that this is a matching task and you must give it a list of things to match.
- * It will assume that any matching file is in HTML format.
  * @author Jesse Glick
  */
 public class CheckLinks extends MatchingTask {
@@ -65,15 +65,8 @@ public class CheckLinks extends MatchingTask {
         Set okurls = new HashSet (1000); // Set<URL>
         // Set of known-bad URLs.
         Set badurls = new HashSet (100); // Set<URL>
-        RE hrefs, anchors;
-        try {
-            hrefs = new RE ("<a\\s+href=\"([^\"#]+)(#[^\"]+)?\">");
-            anchors = new RE ("<a\\s+name=\"([^\"]+)\">");
-        } catch (RESyntaxException rese) {
-            throw new BuildException (rese, location);
-        }
-        hrefs.setMatchFlags (RE.MATCH_CASEINDEPENDENT);
-        anchors.setMatchFlags (RE.MATCH_CASEINDEPENDENT);
+        // Set of base URLs known to be at least openable.
+        Set openableurls = new HashSet(100); // Set<URL>
         for (int i = 0; i < files.length; i++) {
             File file = new File (basedir, files[i]);
             URL fileurl;
@@ -84,79 +77,108 @@ public class CheckLinks extends MatchingTask {
             }
             log ("Scanning " + file, Project.MSG_VERBOSE);
             try {
-                // Intentionally using InputStream, not Reader, for speed:
-                InputStream rd = new FileInputStream (file);
-                try {
-                    CharacterIterator it = new StreamCharacterIterator (rd);
-                    int idx = 0;
-                    while (hrefs.match (it, idx)) {
-                        // Advance match position past end of expression:
-                        idx = hrefs.getParenEnd (0);
-                        // Get the URL involved:
-                        String base = hrefs.getParen (1);
-                        String anchor = hrefs.getParen (2);
-                        String baseAnchor = ((anchor == null) ? base : base + anchor);
-                        URL u1 = new URL (fileurl, base);
-                        URL u2 = new URL (fileurl, baseAnchor);
-                        if (! checkexternal && ! "file".equals (u2.getProtocol ())) {
-                            log ("" + file + ": skipping external link: " + baseAnchor, Project.MSG_VERBOSE);
-                            continue;
-                        }
-                        // Now categorize the URL according to whether it is good/bad, known/unknown, ...
-                        if (badurls.contains (u1) || badurls.contains (u2)) {
-                            log ("" + file + ": " + baseAnchor + " again bad", Project.MSG_WARN);
-                            continue;
-                        }
-                        if (okurls.contains (u2)) {
-                            continue;
-                        }
-                        if (okurls.contains (u1)) {
-                            log ("" + file + ": " + base + " exists but it has no anchor " + anchor.substring (1), Project.MSG_WARN);
-                            badurls.add (u2);
-                            continue;
-                        }
-                        InputStream is;
-                        String mimeType;
-                        try {
-                            URLConnection conn = u1.openConnection ();
-                            conn.connect ();
-                            mimeType = conn.getContentType ();
-                            is = conn.getInputStream ();
-                        } catch (IOException ioe) {
-                            badurls.add (u1);
-                            // u2 will automatically be considered bad too
-                            log ("" + file + ": " + base + " does not exist", Project.MSG_WARN);
-                            continue;
-                        }
-                        try {
-                            okurls.add (u1);
-                            if ("text/html".equals (mimeType)) {
-                                CharacterIterator it2 = new StreamCharacterIterator (is);
-                                int idx2 = 0;
-                                while (anchors.match (it2, idx2)) {
-                                    idx2 = anchors.getParenEnd (0);
-                                    String name = anchors.getParen (1);
-                                    okurls.add (new URL (u1.toString () + '#' + name));
-                                }
-                                if (! okurls.contains (u2)) {
-                                    log ("" + file + ": " + base + " exists but it has no anchor " + anchor.substring (1), Project.MSG_WARN);
-                                    badurls.add (u2);
-                                }
-                            } else if (! u1.equals (u2)) {
-                                log ("" + file + ": " + baseAnchor + " pointed to resource with type " + mimeType + "; not checking anchors");
-                                badurls.add (u2);
-                            }
-                        } finally {
-                            is.close ();
-                        }
-                    }
-                } finally {
-                    rd.close ();
-                }
+                scan(this, file.getAbsolutePath(), fileurl, openableurls, okurls, badurls, checkexternal, 1);
             } catch (IOException ioe) {
                 throw new BuildException ("Could not scan " + file, ioe, location);
             }
         }
     }
-
+    
+    static RE hrefOrAnchor;
+    static {
+        try {
+            hrefOrAnchor = new RE("<a\\s+(href|name)=\"([^\"#]+)(#[^\"]+)?\">");
+        } catch (RESyntaxException rese) {
+            throw new Error (rese.toString());
+        }
+        hrefOrAnchor.setMatchFlags (RE.MATCH_CASEINDEPENDENT);
+    }
+    
+    // recurse:
+    // 0 - just check that it can be opened
+    // 1 - check also that any links from it can be opened
+    // 2 - recurse
+    public static void scan(Task task, String referrer, URL u, Set openableurls, Set okurls, Set badurls, boolean checkexternal, int recurse) throws IOException {
+        if (okurls.contains(u)) {
+            // Already checked, done.
+            return;
+        }
+        URL base = new URL(u, "");
+        String frag = u.getRef();
+        if (badurls.contains(u) || badurls.contains(base)) {
+            task.log(referrer + ": broken link (already reported): " + u, Project.MSG_WARN);
+            return;
+        }
+        if (openableurls.contains(base) && recurse == 0 && frag == null) {
+            // We are just checking opening, done.
+            return;
+        }
+        if (! checkexternal && ! "file".equals(u.getProtocol())) {
+            task.log("Skipping external link: " + base, Project.MSG_VERBOSE);
+            openableurls.add(base);
+            okurls.add(base);
+            okurls.add(u);
+            return;
+        }
+        task.log("Checking " + u, Project.MSG_VERBOSE);
+        InputStream rd;
+        String mimeType;
+        try {
+            URLConnection conn = base.openConnection ();
+            conn.connect ();
+            mimeType = conn.getContentType ();
+            rd = conn.getInputStream ();
+        } catch (IOException ioe) {
+            task.log(referrer + ": broken link: " + base, Project.MSG_WARN);
+            badurls.add(u);
+            return;
+        }
+        Set others = new HashSet(100); // Set<URL>
+        try {
+            openableurls.add(base);
+            if (recurse == 0 && frag == null) {
+                // That is all we wanted to check.
+                return;
+            }
+            okurls.add(base);
+            if ("text/html".equals(mimeType)) {
+                task.log("Parsing " + base, Project.MSG_VERBOSE);
+                CharacterIterator it = new StreamCharacterIterator (rd);
+                int idx = 0;
+                while (hrefOrAnchor.match (it, idx)) {
+                    // Advance match position past end of expression:
+                    idx = hrefOrAnchor.getParenEnd (0);
+                    // Get the stuff involved:
+                    String type = hrefOrAnchor.getParen(1);
+                    if (type.equalsIgnoreCase("name")) {
+                        // We have an anchor, therefore refs to it are valid.
+                        String name = hrefOrAnchor.getParen(2);
+                        okurls.add(new URL(base, "#" + name));
+                    } else if (type.equalsIgnoreCase("href")) {
+                        // A link to some other document.
+                        if (recurse > 0) {
+                            String otherbase = hrefOrAnchor.getParen (2);
+                            String otheranchor = hrefOrAnchor.getParen (3);
+                            others.add(new URL(base, (otheranchor == null) ? otherbase : otherbase + otheranchor));
+                        } // else we are only checking that this one has right anchors
+                    } else {
+                        throw new IllegalStateException(type);
+                    }
+                }
+            } else {
+                task.log("Not checking contents of " + base, Project.MSG_VERBOSE);
+            }
+        } finally {
+            rd.close();
+        }
+        if (! okurls.contains(u)) {
+            task.log(referrer + ": broken link: " + u, Project.MSG_WARN);
+        }
+        Iterator it = others.iterator();
+        while (it.hasNext()) {
+            URL other = (URL)it.next();
+            scan(task, u.getPath(), other, openableurls, okurls, badurls, checkexternal, recurse == 1 ? 0 : 2);
+        }
+    }
+    
 }
