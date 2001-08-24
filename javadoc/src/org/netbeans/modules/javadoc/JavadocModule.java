c044821e88eab62409d239ab6062497b1a9c7543
psuchomel <psuchomel@netbeans.org>
2001-08-24 15:56:58 +0000

    Removed unneded code, class itself stays for compability

--- javadoc/src/org/netbeans/modules/javadoc/JavadocModule.java
+++ javadoc/src/org/netbeans/modules/javadoc/JavadocModule.java
@@ -52,108 +52,10 @@ public class JavadocModule extends ModuleInstall {
     /** serialVersionUID */
     private static final long serialVersionUID = 984124010415492146L;
 
-    //private static final String PROP_INSTALL_COUNT = "installCount"; // NOI18N
-    
-    public static final ErrorManager err = TopManager.getDefault ().getErrorManager ().getInstance ("org.apache.tools.ant.module"); // NOI18N
-
-    /** By first install of module in the IDE, check whether standard documentation folder
-    * exists. If not creates it.
-    */
-    public void installed() {
-        // Install Search Action 
-        restored();
-    }
-
-    /** By uninstalling module from the IDE do nothing.
-    */
-    public void uninstalled () {
-        // Unmount docs (AutoUpdate should handle actually removing the file):
-    }
-
-    /** Called on IDE startup. Registers actions for generating documentation
-    * on DataFolder and JavaDataObject.
-    */
-    public void restored() {
-       
-        // Install the factory for adding JavaDoc property to nodes
-        /*
-        invokeDynamic( "org.netbeans.modules.java.JavaDataObject", // NOI18N
-                       "addExplorerFilterFactory", // NOI18N
-                       new JavaDocPropertySupportFactory() );
-        invokeDynamic( "org.netbeans.modules.java.JavaDataObject", // NOI18N
-                       "addBrowserFilterFactory", // NOI18N
-                       new JavaDocPropertySupportFactory() );
-        */
-    }
-
-    /** Invoked on update */
-    public void updated(int release, String specVersion) {
-        restored();
-    }
-
-    // UTILITY METHODS ----------------------------------------------------------------------
-
-    /** Dynamicaly invokes a method
-     */
-    /*
-    private void invokeDynamic( String className, String methodName, FilterFactory factory ) {
-
-        try {
-            Class dataObject = TopManager.getDefault().systemClassLoader().loadClass( className );
-
-            if ( dataObject == null )
-                return;
-
-            Method method = dataObject.getDeclaredMethod( methodName, new Class[] { FilterFactory.class }  );
-            if ( method == null )
-                return;
-
-            method.invoke( null, new Object[] { factory } );
-        }
-        catch ( java.lang.ClassNotFoundException e ) {
-            notify (e);
-        }
-        catch ( java.lang.NoSuchMethodException e ) {
-            notify (e);
-        }
-        catch ( java.lang.IllegalAccessException e ) {
-            notify (e);
-        }
-        catch ( java.lang.reflect.InvocationTargetException e ) {
-            notify (e);
-        }
-    }
-    */
-    // Implementation of java.io.Externalizable ------------------
-/*
-    public void readExternal(final java.io.ObjectInput objectInput )
-    throws java.io.IOException, java.lang.ClassNotFoundException {
-        super.readExternal( objectInput );
-        //putProperty (PROP_INSTALL_COUNT, new Integer(objectInput.readInt()));
-        //numberOfStarts = objectInput.readInt();
-    }
-
-    public void writeExternal(final java.io.ObjectOutput objectOutput )
-    throws java.io.IOException {
-        super.writeExternal( objectOutput );
-        //objectOutput.writeObject (getProperty (PROP_INSTALL_COUNT));
-        //Integer i = (Integer)getProperty(PROP_INSTALL_COUNT);
-        //objectOutput.writeInt(i != null ? i.intValue() : 0);
-    }
-*/
     protected Object writeReplace(){
         return null;
     }
     
-    private static void notify (Exception e) {        
-        TopManager.getDefault ().getErrorManager ().notify (ErrorManager.INFORMATIONAL, e);
-    }
-
-    private static void notify (String s) {
-        if (Boolean.getBoolean ("netbeans.debug.javadoc")) // NOI18N
-            TopManager.getDefault ().getErrorManager ().log (ErrorManager.INFORMATIONAL, s);
-    }
-  
     /** Exists only for the sake of its bean info.
      * @deprecated Exists only for compability reasons
      */    
@@ -180,7 +82,6 @@ public class JavadocModule extends ModuleInstall {
             try {
                 return new BeanInfo[] { Introspector.getBeanInfo (LocalFileSystem.class) };
             } catch (IntrospectionException ie) {
-                err.notify (ie);
                 return null;
             }
         }
@@ -188,7 +89,6 @@ public class JavadocModule extends ModuleInstall {
             try {
                 return Introspector.getBeanInfo (LocalFileSystem.class).getIcon (kind);
             } catch (IntrospectionException ie) {
-                err.notify (ie);
                 return null;
             }
         }
