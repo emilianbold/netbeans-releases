c98ff74402bf6543f6afedd80f7f27e34bf8ab3c
vstejskal <vstejskal@netbeans.org>
2001-10-10 15:41:49 +0000

    opended project can be renamed (bugfix #15986)

---
+++ o.n.core/src/org/netbeans/core/projects/FilterFileSystem.java
@@ -0,0 +1,45 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is Forte for Java, Community Edition. The Initial
+ * Developer of the Original Code is Sun Microsystems, Inc. Portions
+ * Copyright 1997-2000 Sun Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.core.projects;
+
+import org.openide.filesystems.*;
+
+/**
+ *
+ * @author  Petr Jiricka, Vita Stejskal
+ * @version 1.0
+ */
+public final class FilterFileSystem extends MultiFileSystem {
+	
+    private final FileObject root;
+    private final FileSystem del;
+
+    public FilterFileSystem (FileObject root) throws FileStateInvalidException {
+        super (new FileSystem [] { root.getFileSystem () });
+        this.root = root;
+        this.del = root.getFileSystem ();
+    }
+
+    public String getDisplayName () {
+        return del.getSystemName () + " : " + root.getPackageNameExt ('/', '.'); //NOI18N
+    }
+
+    public final FileObject getRootFileObject () {
+        return root;
+    }
+
+    protected FileObject findResourceOn (FileSystem fs, String res) {
+        return fs.findResource (root.getPackageNameExt ('/', '.') + "/" + res); //NOI18N
+    }
+}
