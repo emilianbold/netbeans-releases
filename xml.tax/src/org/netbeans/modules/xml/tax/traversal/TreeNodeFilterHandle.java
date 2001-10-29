b7de07d6c66748e329f851e3d696c3061d756e2d
lkramolis <lkramolis@netbeans.org>
2001-10-29 12:33:54 +0000

    Revert changes.

--- xml.tax/src/org/netbeans/modules/xml/tax/traversal/TreeNodeFilterHandle.java
+++ xml.tax/src/org/netbeans/modules/xml/tax/traversal/TreeNodeFilterHandle.java
@@ -36,15 +36,6 @@ public final class TreeNodeFilterHandle implements Serializable {
     transient private TreeNodeFilter nodeFilter;
 
 
-    // Stream replacing
-    private static final String NODE_TYPE_NAMES_FIELD = "nodeTypeNames"; // NOI18N
-    private static final String ACCEPT_POLICY_FIELD   = "acceptPolicy"; // NOI18N
-    private static final ObjectStreamField[] serialPersistentFields = {
-        new ObjectStreamField (NODE_TYPE_NAMES_FIELD, Vector.class),
-        new ObjectStreamField (ACCEPT_POLICY_FIELD, short.class),
-    };
-
-
     //
     // init
     //
@@ -94,76 +85,12 @@ public final class TreeNodeFilterHandle implements Serializable {
     }
 
 
-//      /**
-//       */
-//      private void writeObject (ObjectOutputStream oos) throws IOException {
-//          initFields();
-
-//          oos.defaultWriteObject();
-//      }
-
-
     /**
      */
     private void writeObject (ObjectOutputStream oos) throws IOException {
         initFields();
 
-        ObjectOutputStream.PutField putField = oos.putFields();
-
-        Vector names = new Vector();
-        for (int i = 0; i < nodeTypes.length; i++) {
-            names.add (nodeTypes[i].getName());
-        }
-        putField.put (NODE_TYPE_NAMES_FIELD, names);
-
-        putField.put (ACCEPT_POLICY_FIELD, acceptPolicy);
-        
-        oos.writeFields();
-    }
-
-
-    /**
-     */
-    private void readObject (ObjectInputStream ois) throws IOException, ClassNotFoundException {
-        if ( DEBUG ) {
-            Util.debug ("\n* TreeNodeFilter::readObject: this = " + this, new RuntimeException()); // NOI18N
-        }
-
-        ObjectInputStream.GetField getField = ois.readFields();
-        
-        if ( DEBUG ) {
-            Util.debug ("*               ::readObject: getField = " + getField); // NOI18N
-        }
-        
-        try {
-            Vector nodeTypeNames = (Vector)getField.get
-                (NODE_TYPE_NAMES_FIELD,
-                 new Vector (Arrays.asList (new String[] { "org.netbeans.modules.xml.tree.TreeNode" }))); // NOI18N
-
-            if ( DEBUG ) {
-                Util.debug ("*               ::readObject: [new] nodeTypeNames = " + nodeTypeNames); // NOI18N
-            }
-            
-            Vector types = new Vector();
-            Iterator it = nodeTypeNames.iterator();
-            while (it.hasNext()) {
-                try {
-                    types.add (Class.forName((String)it.next()));
-                } catch (ClassNotFoundException ex) {
-                    //let it be
-                }
-            }
-            nodeTypes = (Class[])types.toArray (new Class[0]);
-        } catch (IllegalArgumentException ex) {
-            if ( DEBUG ) {
-                Util.debug ("*               ::readObject: [old] nodeTypes = " + nodeTypes); // NOI18N
-            }
-
-            // old version: private Class[] whatToShow;
-            nodeTypes = (Class[])getField.get (WHAT_TO_SHOW_FIELD, new Class[] { TreeNode.class });
-        }
-        
-        acceptPolicy = getField.get (ACCEPT_POLICY_FIELD, ACCEPT_TYPES);
+        oos.defaultWriteObject();
     }
 
 }
