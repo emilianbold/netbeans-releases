323ef82e8e4067e691d6c7eb7f5751f8b3c3f42a
pkuzel <pkuzel@netbeans.org>
2001-11-13 17:20:49 +0000

    Remap customized token colors.

--- xml.text/src/org/netbeans/modules/xml/text/syntax/XMLOptions.java
+++ xml.text/src/org/netbeans/modules/xml/text/syntax/XMLOptions.java
@@ -12,6 +12,8 @@
  */
 package org.netbeans.modules.xml.text.syntax;
 
+import java.util.*;
+
 /**
  * Options for the xml editor kit
  *
@@ -22,7 +24,7 @@ public class XMLOptions extends AbstractBaseOptions {
     /** Serial Version UID */
     private static final long serialVersionUID = 2347735706857337892L;
 
-
+    
     //
     // init
     //
@@ -32,4 +34,43 @@ public class XMLOptions extends AbstractBaseOptions {
         super (XMLKit.class, "xml"); // NOI18N
     }
 
+    
+    // remap old XMLTokenContext to new XMLDefaultTokenContext
+    private static final String[][] TRANSLATE_COLORS = {
+        { "xml-comment", "xml-block-comment" },
+        { "xml-ref", "xml-character" },
+        { "xml-string", "xml-value" },
+        { "xml-attribute", "xml-argument" },
+        { "xml-symbol", "xml-operator" },
+//        { "xml-tag", "xml-tag" },
+        { "xml-keyword", "xml-sgml-declaration" },
+        { "xml-plain", "xml-text"},
+    };
+    
+    /**
+     * Get coloring, possibly remap setting from previous versions
+     * to new one.
+     */
+    public synchronized Map getColoringMap() {
+        Map colors = super.getColoringMap();
+        
+        // get old customized colors and map them to new token IDs
+        // the map will contain only such old colors that was customized AFAIK
+        // because current initializer does not create them
+        
+        for (int i = 0; i<TRANSLATE_COLORS.length; i++) {
+            String oldKey = TRANSLATE_COLORS[i][0];
+            Object color = colors.get(oldKey);
+            if (color != null) {
+                colors.remove(oldKey);
+                String newKey = TRANSLATE_COLORS[i][1];
+                colors.put(newKey, color);
+            }
+        }
+
+        // do not save it explicitly if the user will do a customization
+        // it get saved automatically (i.e.old keys removal will apply)
+        
+        return colors;
+    }
 }
