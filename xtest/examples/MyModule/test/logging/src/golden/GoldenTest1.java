40eed16ec95e2f0341444eb5f103474eef22c5a0
jskrivanek <jskrivanek@netbeans.org>
2001-11-19 17:29:06 +0000

    Examples to demonstrate usage of unique workdir, logging facility and golden files

---
+++ xtest/examples/MyModule/test/logging/src/golden/GoldenTest1.java
@@ -0,0 +1,66 @@
+package golden;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import org.netbeans.junit.NbTestSuite;
+import org.netbeans.junit.NbTestCase;
+
+import java.io.PrintWriter;
+import java.io.File;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.io.FileOutputStream;
+
+/** Example of golden file approach.
+ * Next to this GoldenTest1.java file there should exist GoldenTest1.pass file
+ * containing expected output (AKA golden file)
+ *
+ */
+public class GoldenTest1 extends NbTestCase {
+    
+    
+    public GoldenTest1(String testName) {
+        super(testName);
+    }
+    
+    /** Use for execution inside IDE */
+    public static void main(java.lang.String[] args) {
+        junit.textui.TestRunner.run(new NbTestSuite(GoldenTest1.class));
+    }
+    
+    /** Uses golden files approach. Messages are written to the reference file
+     * by the ref() method. At the end tearDown() method satisfy comparing with
+     * golden file.<br>
+     * Of course, you can also use fail() and assert() methods within the test to indicate
+     * a test failure.
+     */
+    public void testPart1() throws Exception {
+        // print path where log and ref files will be stored
+        System.out.println("WORKDIR="+getWorkDirPath());
+        ref("Output to ref file called testPart1.refddd");
+        log("message to log file testPart1.log");
+        System.out.println("AFTERRRRRR");
+        log("mySpecialLog.log", "message to log file mySpecialLog.log");
+        // here write body of the test
+    }
+    
+    
+    /** This method should fail because contents of reference file and
+     * golden file will differ.
+     */
+    public void testPart2() throws Exception {
+        ref("Output to ref file called testPart12.ref");
+        log("message to log file testPart2.log");
+        // here write body of the test
+        log("Finished.");
+    }
+    
+    /* Use tearDown to compare reference files after each test method is finished.
+     *  assertFile() method compares <testname>.ref file againts <testname>.pass
+     */
+    protected void tearDown() {
+        compareReferenceFiles();
+    }
+    
+}
---
+++ xtest/examples/MyModule/test/logging/src/golden/GoldenTest2.java
@@ -0,0 +1,47 @@
+package golden;
+
+import org.netbeans.junit.NbTestSuite;
+import org.netbeans.junit.NbTestCase;
+
+
+/** Example of golden file approach.
+ * Next to this GoldenTest1.java file there should exist GoldenTest1.pass file
+ * containing expected output (AKA golden file)
+ *
+ */
+public class GoldenTest2 extends NbTestCase {
+    
+    
+    public GoldenTest2(String testName) {
+        super(testName);
+    }
+    
+    /** Use for execution inside IDE */
+    public static void main(java.lang.String[] args) {
+        junit.textui.TestRunner.run(new NbTestSuite(GoldenTest2.class));
+    }
+    
+    /** This method use golden files approach and you need to use compareReferenceFiles()
+     * method explicitly to compare generated reference file against golden file.
+     */
+    public void testPart1() {
+        log("message to log file testPart1.log");
+        ref("Output to ref file called testPart1.ref");
+        // here write body of the test
+        boolean somethingWrong = false;
+        // You can also use assert() or fail() method in the body of the test 
+        // to indicate a failure
+        if(somethingWrong) fail();
+        assert(!somethingWrong);
+        log("mySpecialLog.log", "message to log file mySpecialLog.log");
+        compareReferenceFiles();
+    }
+    
+    
+    /** This method doesn't use golden file approach */
+    public void testPart2() throws Exception {
+        log("message to log file testPart2.log");
+        // here write body of the test
+    }
+    
+}
