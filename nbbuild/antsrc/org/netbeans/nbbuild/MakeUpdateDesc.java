f8765eabcf848853bb3428793ae1539c1235ffc2
rbalada <rbalada@netbeans.org>
2001-12-17 13:34:19 +0000

    Added support for xml-entity-includes. Improved catching of wrong filenames in <nbm file="">

--- nbbuild/antsrc/org/netbeans/nbbuild/MakeUpdateDesc.java
+++ nbbuild/antsrc/org/netbeans/nbbuild/MakeUpdateDesc.java
@@ -54,13 +54,28 @@ public class MakeUpdateDesc extends Task {
 	    file = f;
 	}
     }
+    
+    public /*static*/ class Include {
+        public String file;
+        /** Path to the entity file.
+         * It included as an xml-entity pointer in master .xml file.
+         */
+	public void setFile (String f) {
+	    file = f;
+	}
+    }
 
+    private List includes = new LinkedList();
     private List groups = new LinkedList ();
+
+    private String desc_name;
     private File desc;
+    private File desc_ent;
 
     /** Description file to create. */
-    public void setDesc (File d) {
-	desc = d;
+    public void setDesc (String d) {
+	desc_name = d;
+        desc = new File (desc_name);
     }
     public Group createGroup () {
 	Group g = new Group ();
@@ -68,6 +83,12 @@ public class MakeUpdateDesc extends Task {
 	return g;
     }
 
+    public Include createInclude () {
+        Include i = new Include ();
+        includes.add (i);
+        return i;
+    }
+    
     public void execute () throws BuildException {
 	if (desc.exists ()) {
 	    // Simple up-to-date check.
@@ -95,13 +116,52 @@ public class MakeUpdateDesc extends Task {
 	try {
 	    OutputStream os = new FileOutputStream (desc);
 	    try {
-		PrintWriter pw = new PrintWriter (new OutputStreamWriter (os));
+                String ent_name=desc_name.toString();                
+                int xml_idx = ent_name.indexOf(".xml");
+                if (xml_idx != -1) {
+                    ent_name = ent_name.substring (0, xml_idx) + ".ent";
+                } else {
+                    ent_name = ent_name + ".ent";
+                }
+                desc_ent = new File(ent_name);               
+                
+                PrintWriter pw = new PrintWriter (new OutputStreamWriter (os));
 		pw.println ("<?xml version='1.0'?>");
 		pw.println ();
+                pw.println ("<!DOCTYPE module_updates [");
+                // Would be better to follow order of groups and includes
+                pw.println ("    <!ENTITY entity SYSTEM \"" + desc_ent.getName() + "\">");
+                Iterator ic = includes.iterator();
+                int inc_num=0;
+                while (ic.hasNext ()) {
+                    inc_num+=1;
+                    Include i = (Include) ic.next ();                    
+                    pw.println ("    <!ENTITY include" + inc_num + " SYSTEM \"" + i.file + "\">");
+                }                
+                pw.println ("]>");
+		pw.println ();
+                ic = null;
+
 		SimpleDateFormat format = new SimpleDateFormat ("ss/mm/HH/dd/MM/yyyy");
 		format.setTimeZone (TimeZone.getTimeZone ("GMT"));
 		String date = format.format (new Date ());
 		pw.println ("<module_updates timestamp=\"" + date + "\">");
+                pw.println ("    &entity;");
+                ic = includes.iterator();
+                inc_num=0;
+                while (ic.hasNext ()) {
+                    inc_num+=1;
+                    ic.next ();                    
+                    pw.println ("    &include" + inc_num + ";");
+                }
+                ic = null;
+
+                pw.println ("</module_updates>");
+		pw.println ();
+		pw.close ();
+                
+                os = new FileOutputStream (desc_ent);
+                pw = new PrintWriter (new OutputStreamWriter (os));
 		StringBuffer licenses = new StringBuffer ();
 		Set licenseNames = new HashSet (); // Set<String>
 		Iterator it = groups.iterator ();
@@ -112,62 +172,65 @@ public class MakeUpdateDesc extends Task {
 		    Iterator it2 = g.nbms.iterator ();
 		    while (it2.hasNext ()) {
 			Nbm n = (Nbm) it2.next ();
-			long size = n.file.length ();
-			ZipFile zip = new ZipFile (n.file);
 			try {
-			    ZipEntry entry = zip.getEntry ("Info/info.xml");
-			    if (entry == null)
-				throw new BuildException ("NBM " + n.file + " was malformed: no Info/info.xml", location);
-			    InputStream is = zip.getInputStream (entry);
-			    try {
-				BufferedReader r = new BufferedReader (new InputStreamReader (is));
-				String line = r.readLine ();
-				if (! line.startsWith ("<?xml"))
-				    throw new BuildException ("Strange info.xml line: " + line, location);
-				// next line probably blank, no problem though
-			    INFOXML:
-				while ((line = r.readLine ()) != null) {
-				    String dummyDownloadSize = "downloadsize=\"0\"";
-				    int idx = line.indexOf (dummyDownloadSize);
-				    if (idx != -1) {
-					line = line.substring (0, idx) +
-					    "downloadsize=\"" + size + "\"" +
-					    line.substring (idx + dummyDownloadSize.length ());
-				    }
-				    String licenseMarker = "<license name=\"";
-				    idx = line.indexOf (licenseMarker);
-				    if (idx != -1) {
-					int idx2 = line.indexOf ("\"", idx + licenseMarker.length ());
-					if (idx2 == -1)
-					    throw new BuildException ("Strange info.xml line: " + line, location);
-					String name = line.substring (idx + licenseMarker.length (), idx2);
-					// [PENDING] ideally would compare the license texts to make sure they actually match up
-					boolean copy = ! licenseNames.contains (name);
-					licenseNames.add (name);
-					do {
-					    if (copy) {
-						licenses.append (line);
-						licenses.append ('\n');
-					    }
-					    if (line.indexOf ("</license>") != -1)
-						continue INFOXML;
-					} while ((line = r.readLine ()) != null);
-				    } else {
-					// Non-license line.
-					pw.println (line);
-				    }
-				}
-			    } finally {
-				is.close ();
-			    }
-			} finally {
-			    zip.close ();
+			    long size = n.file.length ();
+ 			    ZipFile zip = new ZipFile (n.file);
+                            try {
+                                ZipEntry entry = zip.getEntry ("Info/info.xml");
+                                if (entry == null)
+                                    throw new BuildException ("NBM " + n.file + " was malformed: no Info/info.xml", location);
+                                InputStream is = zip.getInputStream (entry);
+                                try {
+                                    BufferedReader r = new BufferedReader (new InputStreamReader (is));
+                                    String line = r.readLine ();
+                                    if (! line.startsWith ("<?xml"))
+                                        throw new BuildException ("Strange info.xml line: " + line, location);
+                                    // next line probably blank, no problem though
+                                INFOXML:
+                                    while ((line = r.readLine ()) != null) {
+                                        String dummyDownloadSize = "downloadsize=\"0\"";
+                                        int idx = line.indexOf (dummyDownloadSize);
+                                        if (idx != -1) {
+                                            line = line.substring (0, idx) +
+                                                "downloadsize=\"" + size + "\"" +
+                                                line.substring (idx + dummyDownloadSize.length ());
+                                        }
+                                        String licenseMarker = "<license name=\"";
+                                        idx = line.indexOf (licenseMarker);
+                                        if (idx != -1) {
+                                            int idx2 = line.indexOf ("\"", idx + licenseMarker.length ());
+                                            if (idx2 == -1)
+                                                throw new BuildException ("Strange info.xml line: " + line, location);
+                                            String name = line.substring (idx + licenseMarker.length (), idx2);
+                                            // [PENDING] ideally would compare the license texts to make sure they actually match up
+                                            boolean copy = ! licenseNames.contains (name);
+                                            licenseNames.add (name);
+                                            do {
+                                                if (copy) {
+                                                    licenses.append (line);
+                                                    licenses.append ('\n');
+                                                }
+                                                if (line.indexOf ("</license>") != -1)
+                                                continue INFOXML;
+                                            } while ((line = r.readLine ()) != null);
+                                        } else {
+                                            // Non-license line.
+                                            pw.println (line);
+                                        }
+                                    }
+                                } finally {
+                                    is.close ();
+                                }
+                            } finally {
+                                zip.close ();
+                            }
+			} catch (IOException ioe) {
+			    throw new BuildException("Cannot access nbm file: " + n.file, ioe, location);
 			}
 		    }
 		    pw.println ("</module_group>");
 		}
 		pw.print (licenses.toString ());
-		pw.println ("</module_updates>");
 		pw.close ();
 	    } finally {
 		os.close ();
