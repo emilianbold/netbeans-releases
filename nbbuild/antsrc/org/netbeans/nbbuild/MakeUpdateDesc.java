41da71cdfbd1cc098984032c8b2a9aaa1941ab09
rbalada <rbalada@netbeans.org>
2002-01-04 10:57:07 +0000

    NB Ant extension task makeupdatedesc switched to use filesets.

--- nbbuild/antsrc/org/netbeans/nbbuild/MakeUpdateDesc.java
+++ nbbuild/antsrc/org/netbeans/nbbuild/MakeUpdateDesc.java
@@ -13,49 +13,44 @@
 
 package org.netbeans.nbbuild;
 
-import java.io.*;
-import java.text.SimpleDateFormat;
-import java.util.*;
-import java.util.zip.*;
-
+import org.apache.tools.ant.types.FileSet;
 import org.apache.tools.ant.BuildException;
-import org.apache.tools.ant.Location;
-import org.apache.tools.ant.Task;
+import org.apache.tools.ant.taskdefs.MatchingTask;
+import org.apache.tools.ant.DirectoryScanner;
+
+import org.netbeans.nbbuild.MakeUpdateDesc.Group;
+import org.netbeans.nbbuild.MakeUpdateDesc.Entityinclude;
+import java.io.File;
+import java.util.Vector;
+
 
 /** Makes an XML file representing update information from NBMs.
  *
  * @author Jesse Glick
  */
-public class MakeUpdateDesc extends Task {
+public class MakeUpdateDesc extends MatchingTask {
 
+    protected boolean usedMatchingTask = false;
     /** Set of NBMs presented as a folder in the Update Center. */
     public /*static*/ class Group {
-	public List nbms = new LinkedList ();
+        public Vector nbms = new Vector ();
+        public Vector filesets = new Vector ();
+        public Vector scanners = new Vector ();
 	public String name;
+
         /** Displayed name of the group. */
 	public void setName (String s) {
 	    name = s;
 	}
-        // [PENDING] should support nested <fileset>s to match *.nbm?
-	public Nbm createNbm () {
-	    Nbm n = new Nbm ();
-	    nbms.add (n);
-	    return n;
-	}
-    }
 
-    /** One NBM file in the group. */
-    public /*static*/ class Nbm {
-	public File file;
-        /** Path to the NBM file.
-         * Its <samp>Info/info.xml</samp> will be parsed for information to include.
-         */
-	public void setFile (File f) {
-	    file = f;
-	}
+        /** Add fileset to the group of NetBeans modules **/
+        public void addFileSet (FileSet set) {
+            filesets.add(set);
+        }
     }
     
-    public /*static*/ class Include {
+    /** pointer to another xml entity to include **/
+    public class Entityinclude {
         public String file;
         /** Path to the entity file.
          * It included as an xml-entity pointer in master .xml file.
@@ -65,8 +60,9 @@ public class MakeUpdateDesc extends Task {
 	}
     }
 
-    private List includes = new LinkedList();
-    private List groups = new LinkedList ();
+    private Vector entityincludes = new Vector();
+    private Vector groups = new Vector ();
+    private Vector filesets = new Vector ();
 
     private String desc_name;
     private File desc;
@@ -77,33 +73,49 @@ public class MakeUpdateDesc extends Task {
 	desc_name = d;
         desc = new File (desc_name);
     }
+
     public Group createGroup () {
 	Group g = new Group ();
 	groups.add (g);
 	return g;
     }
 
-    public Include createInclude () {
-        Include i = new Include ();
-        includes.add (i);
+    public Entityinclude createEntityinclude () {
+        Entityinclude i = new Entityinclude ();
+        entityincludes.add (i);
         return i;
     }
+
+   /**
+     * Adds a set of files (nested fileset attribute).
+     */
+    public void addFileset(FileSet set) {
+        filesets.addElement(set);
+    }
+ 
     
     public void execute () throws BuildException {
 	if (desc.exists ()) {
 	    // Simple up-to-date check.
 	    long time = desc.lastModified ();
 	    boolean uptodate = true;
-	    Iterator it = groups.iterator ();
+
 	CHECK:
-	    while (it.hasNext ()) {
-		Group g = (Group) it.next ();
-		Iterator it2 = g.nbms.iterator ();
-		while (it2.hasNext ()) {
-		    Nbm n = (Nbm) it2.next ();
-		    if (n.file.lastModified () > time) {
-			uptodate = false;
-			break CHECK;
+            for (int i=0; i<groups.size(); i++) {
+		Group g = (Group) groups.elementAt(i);
+                for (int j=0; j<g.filesets.size(); j++) {
+		    FileSet n = (FileSet) g.filesets.elementAt(j);
+                    if ( n != null ) {
+                        DirectoryScanner ds = n.getDirectoryScanner(project);
+                        String[] files = ds.getIncludedFiles();
+                        File bdir = ds.getBasedir();
+                        for (int k=0; k <files.length; k++) {
+                            File n_file = new File(bdir, files[k]);
+                            if (n_file.lastModified () > time) {
+                                uptodate = false;
+                                break CHECK;
+                            }
+                        }
 		    }
 		}
 	    }
@@ -114,128 +126,158 @@ public class MakeUpdateDesc extends Task {
 	// unfortunately I don't have docs for any handy at the moment.
 	// So e.g. it is assumed that <license> is on its own line(s), etc.
 	try {
-	    OutputStream os = new FileOutputStream (desc);
+            desc.delete();
+	    java.io.OutputStream os = new java.io.FileOutputStream (desc);
 	    try {
-                String ent_name=desc_name.toString();                
-                int xml_idx = ent_name.indexOf(".xml");
-                if (xml_idx != -1) {
-                    ent_name = ent_name.substring (0, xml_idx) + ".ent";
-                } else {
-                    ent_name = ent_name + ".ent";
-                }
-                desc_ent = new File(ent_name);               
                 
-                PrintWriter pw = new PrintWriter (new OutputStreamWriter (os));
+                java.io.PrintWriter pw = new java.io.PrintWriter (new java.io.OutputStreamWriter (os));
 		pw.println ("<?xml version='1.0'?>");
 		pw.println ();
-                pw.println ("<!DOCTYPE module_updates [");
-                // Would be better to follow order of groups and includes
-                pw.println ("    <!ENTITY entity SYSTEM \"" + desc_ent.getName() + "\">");
-                Iterator ic = includes.iterator();
-                int inc_num=0;
-                while (ic.hasNext ()) {
-                    inc_num+=1;
-                    Include i = (Include) ic.next ();                    
-                    pw.println ("    <!ENTITY include" + inc_num + " SYSTEM \"" + i.file + "\">");
-                }                
-                pw.println ("]>");
-		pw.println ();
-                ic = null;
-
-		SimpleDateFormat format = new SimpleDateFormat ("ss/mm/HH/dd/MM/yyyy");
-		format.setTimeZone (TimeZone.getTimeZone ("GMT"));
-		String date = format.format (new Date ());
-		pw.println ("<module_updates timestamp=\"" + date + "\">");
-                pw.println ("    &entity;");
-                ic = includes.iterator();
-                inc_num=0;
-                while (ic.hasNext ()) {
-                    inc_num+=1;
-                    ic.next ();                    
-                    pw.println ("    &include" + inc_num + ";");
-                }
-                ic = null;
-
-                pw.println ("</module_updates>");
-		pw.println ();
-		pw.close ();
+		java.text.SimpleDateFormat format = new java.text.SimpleDateFormat ("ss/mm/HH/dd/MM/yyyy");
+		format.setTimeZone (java.util.TimeZone.getTimeZone ("GMT"));
+		String date = format.format (new java.util.Date ());
                 
-                os = new FileOutputStream (desc_ent);
-                pw = new PrintWriter (new OutputStreamWriter (os));
+                if ( entityincludes.size() > 0 ) {
+                    // prepare .ent file
+                    String ent_name=desc_name.toString();                
+                    int xml_idx = ent_name.indexOf(".xml");
+                    if (xml_idx != -1) {
+                        ent_name = ent_name.substring (0, xml_idx) + ".ent";
+                    } else {
+                        ent_name = ent_name + ".ent";
+                    }
+                    desc_ent = new File(ent_name);               
+                    desc_ent.delete();
+                    pw.println ("<!DOCTYPE module_updates [");
+                    // Would be better to follow order of groups and includes
+                    pw.println ("    <!ENTITY entity SYSTEM \"" + desc_ent.getName() + "\">");
+                    int inc_num=0;
+                    for (int i=0; i<entityincludes.size(); i++) {
+                        Entityinclude ei = (Entityinclude) entityincludes.elementAt(i);
+                        pw.println ("    <!ENTITY include" + i + " SYSTEM \"" + ei.file + "\">");
+                    }
+                    pw.println ("]>");
+                    pw.println ();
+                    pw.println ("<module_updates timestamp=\"" + date + "\">");
+                    pw.println ("    &entity;");
+                    for (int i=0; i<entityincludes.size(); i++) {
+                        pw.println ("    &include" + i + ";");
+                    }
+                    pw.println ("</module_updates>");
+                    pw.println ();
+                    pw.close ();
+                
+                    os = new java.io.FileOutputStream (desc_ent);
+                    pw = new java.io.PrintWriter (new java.io.OutputStreamWriter (os));
+                    
+                } else {
+                    pw.println ("<module_updates timestamp=\"" + date + "\">");
+                    pw.println ();
+                }
 		StringBuffer licenses = new StringBuffer ();
-		Set licenseNames = new HashSet (); // Set<String>
-		Iterator it = groups.iterator ();
-		while (it.hasNext ()) {
-		    Group g = (Group) it.next ();
+		java.util.Set licenseNames = new java.util.HashSet (); // Set<String>
+                Group root = new Group();
+                root.setName("root"); //NOI18N
+                for (int i=0; i < filesets.size(); i++) {
+                    root.addFileSet((FileSet) filesets.elementAt(i));
+                }
+                groups.addElement(root);
+                
+                for (int gi=0; gi < groups.size(); gi++) {
+		    Group g = (Group) groups.elementAt(gi);
 		    // Don't indent; embedded descriptions would get indented otherwise.
-		    pw.println ("<module_group name=\"" + g.name + "\">");
-		    Iterator it2 = g.nbms.iterator ();
-		    while (it2.hasNext ()) {
-			Nbm n = (Nbm) it2.next ();
-			try {
-			    long size = n.file.length ();
- 			    ZipFile zip = new ZipFile (n.file);
+                    log ("Creating group \"" + g.name + "\"");
+                    if ( ! g.name.equals("root")) {
+                        pw.println ("<module_group name=\"" + g.name + "\">");
+                        pw.println ();
+                    }
+                    for (int fsi=0; fsi < g.filesets.size(); fsi++) {
+                        FileSet fs = (FileSet) g.filesets.elementAt(fsi);
+                        DirectoryScanner ds = fs.getDirectoryScanner(project);
+                        String[] files = ds.getIncludedFiles();
+                        for (int fid=0; fid < files.length; fid++) {
+                            File n_file = new File(fs.getDir(project), files[fid]);
                             try {
-                                ZipEntry entry = zip.getEntry ("Info/info.xml");
-                                if (entry == null)
-                                    throw new BuildException ("NBM " + n.file + " was malformed: no Info/info.xml", location);
-                                InputStream is = zip.getInputStream (entry);
+                                long size = n_file.length ();
+                                java.util.zip.ZipFile zip = new java.util.zip.ZipFile (n_file);
                                 try {
-                                    BufferedReader r = new BufferedReader (new InputStreamReader (is));
-                                    String line = r.readLine ();
-                                    if (! line.startsWith ("<?xml"))
-                                        throw new BuildException ("Strange info.xml line: " + line, location);
-                                    // next line probably blank, no problem though
-                                INFOXML:
-                                    while ((line = r.readLine ()) != null) {
-                                        String dummyDownloadSize = "downloadsize=\"0\"";
-                                        int idx = line.indexOf (dummyDownloadSize);
-                                        if (idx != -1) {
-                                            line = line.substring (0, idx) +
-                                                "downloadsize=\"" + size + "\"" +
-                                                line.substring (idx + dummyDownloadSize.length ());
+                                    java.util.zip.ZipEntry entry = zip.getEntry ("Info/info.xml");
+                                    if (entry == null)
+                                        throw new BuildException ("NBM " + n_file + " was malformed: no Info/info.xml", location);
+                                    java.io.InputStream is = zip.getInputStream (entry);
+                                    try {
+                                        java.io.BufferedReader r = new java.io.BufferedReader (new java.io.InputStreamReader (is));
+                                        String line = r.readLine ();
+                                        if (! line.startsWith ("<?xml"))
+                                            throw new BuildException ("Strange info.xml line: " + line, location);
+                                        // next line probably blank, no problem though
+                                    INFOXML:
+                                        while ((line = r.readLine ()) != null) {
+                                            String dummyDownloadSize = "downloadsize=\"0\"";
+                                            int idx = line.indexOf (dummyDownloadSize);
+                                            if (idx != -1) {
+                                                line = line.substring (0, idx) +
+                                                    "downloadsize=\"" + size + "\"" +
+                                                    line.substring (idx + dummyDownloadSize.length ());
+                                            }
+                                            String dummyModuleName = "OpenIDE-Module-Name=\"";
+                                            idx = line.indexOf(dummyModuleName);
+                                            if (idx != -1) {
+                                                String mn = line.substring (idx + dummyModuleName.length () - 1);
+                                                log (" Adding module   " + mn + " (" + n_file.getAbsolutePath() + ")");
+                                            }
+                                            String licenseMarker = "<license name=\"";
+                                            idx = line.indexOf (licenseMarker);
+                                            if (idx != -1) {
+                                                int idx2 = line.indexOf ("\"", idx + licenseMarker.length ());
+                                                if (idx2 == -1)
+                                                    throw new BuildException ("Strange info.xml line: " + line, location);
+                                                String name = line.substring (idx + licenseMarker.length (), idx2);
+                                                // [PENDING] ideally would compare the license texts to make sure they actually match up
+                                                boolean copy = ! licenseNames.contains (name);
+                                                licenseNames.add (name);
+                                                do {
+                                                    if (copy) {
+                                                        licenses.append (line);
+                                                        licenses.append ('\n');
+                                                    }
+                                                    if (line.indexOf ("</license>") != -1) {
+                                                        licenses.append ('\n');
+                                                        continue INFOXML;
+                                                    }
+                                                } while ((line = r.readLine ()) != null);
+                                            } else {
+                                                // Non-license line.
+                                                pw.println (line);
+                                            }
                                         }
-                                        String licenseMarker = "<license name=\"";
-                                        idx = line.indexOf (licenseMarker);
-                                        if (idx != -1) {
-                                            int idx2 = line.indexOf ("\"", idx + licenseMarker.length ());
-                                            if (idx2 == -1)
-                                                throw new BuildException ("Strange info.xml line: " + line, location);
-                                            String name = line.substring (idx + licenseMarker.length (), idx2);
-                                            // [PENDING] ideally would compare the license texts to make sure they actually match up
-                                            boolean copy = ! licenseNames.contains (name);
-                                            licenseNames.add (name);
-                                            do {
-                                                if (copy) {
-                                                    licenses.append (line);
-                                                    licenses.append ('\n');
-                                                }
-                                                if (line.indexOf ("</license>") != -1)
-                                                continue INFOXML;
-                                            } while ((line = r.readLine ()) != null);
-                                        } else {
-                                            // Non-license line.
-                                            pw.println (line);
-                                        }
+                                    } finally {
+                                        is.close ();
+                                        pw.println ();
                                     }
                                 } finally {
-                                    is.close ();
+                                    zip.close ();
                                 }
-                            } finally {
-                                zip.close ();
+                            } catch (java.io.IOException ioe) {
+                                throw new BuildException("Cannot access nbm file: " + n_file, ioe, location);
                             }
-			} catch (IOException ioe) {
-			    throw new BuildException("Cannot access nbm file: " + n.file, ioe, location);
-			}
+                        }
 		    }
-		    pw.println ("</module_group>");
+                    if ( ! g.name.equals("root")) {
+                        pw.println ("</module_group>");
+                        pw.println ();
+                    }
 		}
 		pw.print (licenses.toString ());
+                if ( entityincludes.size() <= 0 ) {
+                    pw.println ("</module_updates>");
+                    pw.println ();
+                }
 		pw.close ();
 	    } finally {
 		os.close ();
 	    }
-	} catch (IOException ioe) {
+	} catch (java.io.IOException ioe) {
 	    desc.delete ();
 	    throw new BuildException ("Cannot create update description", ioe, location);
 	}
