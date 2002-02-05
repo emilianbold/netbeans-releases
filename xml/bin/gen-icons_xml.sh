9177ef814c44c0615a25e65c0c490c47198ef4a4
lkramolis <lkramolis@netbeans.org>
2002-02-05 16:11:16 +0000

    - new bin dir (currently contains gen-icons_xml.sh and gen-l10n_list.sh)

---
+++ xml/bin/gen-icons_xml.sh
@@ -0,0 +1,118 @@
+#!/bin/sh
+
+# 
+#                 Sun Public License Notice
+# 
+# The contents of this file are subject to the Sun Public License
+# Version 1.0 (the "License"). You may not use this file except in
+# compliance with the License. A copy of the License is available at
+# http://www.sun.com/
+# 
+# The Original Code is NetBeans. The Initial Developer of the Original
+# Code is Sun Microsystems, Inc. Portions Copyright 1997-2001 Sun
+# Microsystems, Inc. All Rights Reserved.
+# 
+
+#set -x
+
+#
+# It is used for generating xml/www/*/data/icons.xml files.
+# When you set GENERATE to true, default xml/www/*/images/icons/description.properties files are generated.
+#
+# Run it from xml directory.
+#
+
+
+# 'true' or anything else
+GENERATE=false
+
+BASE=`pwd`
+WWW=$BASE/www
+
+for dir in catalog core css tax text-edit tools tree-edit; do
+    cd $BASE/$dir
+
+    ICONS=$WWW/$dir/images/icons
+    mkdir -p $ICONS
+
+    if [ "$GENERATE" == "true" ]; then
+        DESC_FILE=$ICONS/description.properties
+        echo "# 
+#                 Sun Public License Notice
+# 
+# The contents of this file are subject to the Sun Public License
+# Version 1.0 (the "License"). You may not use this file except in
+# compliance with the License. A copy of the License is available at
+# http://www.sun.com/
+# 
+# The Original Code is NetBeans. The Initial Developer of the Original
+# Code is Sun Microsystems, Inc. Portions Copyright 1997-2001 Sun
+# Microsystems, Inc. All Rights Reserved.
+# 
+" > $DESC_FILE
+    fi
+
+    . $ICONS/description.properties
+
+    DATA=$WWW/$dir/data/icons.xml    
+        echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<!--
+                Sun Public License Notice
+
+The contents of this file are subject to the Sun Public License
+Version 1.0 (the "License"). You may not use this file except in
+compliance with the License. A copy of the License is available at
+http://www.sun.com/
+
+The Original Code is NetBeans. The Initial Developer of the Original
+Code is Sun Microsystems, Inc. Portions Copyright 1997-2001 Sun
+Microsystems, Inc. All Rights Reserved.
+-->
+" > $DATA
+
+    echo "<list module=\"$dir\">" >> $DATA
+
+    echo
+    echo "== $dir"
+
+    for file in `find src -name "*.gif" | grep -v www | grep -v testbed | grep -v javahelp`; do
+        echo "-- $file"
+        NAME=`basename $file`
+        KEY=`echo $file | tr \/.- ___`
+        CMD="echo \$$KEY"
+        DESC=`eval $CMD`
+
+        cp $file $ICONS
+
+        echo "    <icon name=\"$NAME\" file=\"$file\">" >> $DATA
+        echo "        <description>$DESC</description>" >> $DATA
+#        echo "        <screen-shot name=\"ss-$NAME\"/>" >> $DATA
+        echo "        <screen-shot name=\"$NAME\"/>" >> $DATA
+        echo "        <usage>" >> $DATA
+
+        for usage in `grep -r -l "$NAME\""   ../ | grep -v ".form$" | grep -v CVS | grep -v "~$" | grep -v ".#" | grep -v "/www/" | grep -v "/javahelp/" | grep -v "/testbed/" | grep -v "TODO.xml" | grep -v "build.xml" | grep -v "/test/"`; do
+            echo " - $usage"
+            REF_FILE=`basename $usage`
+            echo "            <source name=\"$REF_FILE\" file=\"$usage\"/>" >> $DATA
+        done
+
+        NO_EXT=`basename $file .gif`
+        for usage in `grep -r -l "$NO_EXT\"" ../ | grep -v ".form$" | grep -v CVS | grep -v "~$" | grep -v ".#" | grep -v "/www/" | grep -v "/javahelp/" | grep -v "/testbed/" | grep -v "TODO.xml" | grep -v "build.xml" | grep -v "/test/"`; do
+            echo " - $usage"
+            REF_FILE=`basename $usage`
+            echo "            <source name=\"$REF_FILE\" file=\"$usage\"/>" >> $DATA
+        done
+
+        echo "        </usage>" >> $DATA
+        echo "    </icon>" >> $DATA
+
+
+        if [ "$GENERATE" == "true" ]; then
+            echo "$KEY=\"It is used for \"" >> $DESC_FILE
+        fi
+
+    done
+
+    echo "</list>" >> $DATA
+
+done
---
+++ xml/bin/gen-l10n_list.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# 
+#                 Sun Public License Notice
+# 
+# The contents of this file are subject to the Sun Public License
+# Version 1.0 (the "License"). You may not use this file except in
+# compliance with the License. A copy of the License is available at
+# http://www.sun.com/
+# 
+# The Original Code is NetBeans. The Initial Developer of the Original
+# Code is Sun Microsystems, Inc. Portions Copyright 1997-2001 Sun
+# Microsystems, Inc. All Rights Reserved.
+# 
+
+#set -x
+
+#
+# It is used for generating xml/*/l10n.list files.
+#
+# Run it from xml/.. directory.
+#
+
+# reset old list
+echo -n > xml/l10n.list
+
+for module in catalog compat core css tax text-edit tools tree-edit; do
+    find xml/${module} -name "Bundle.properties" > xml/${module}/l10n.list
+    find xml/${module} -name "*.html" | grep -v "_ja.html" | grep -v "package.html" >> xml/${module}/l10n.list
+
+    cat xml/${module}/l10n.list >> xml/l10n.list
+done
