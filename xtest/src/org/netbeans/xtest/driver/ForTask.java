e73345d037463b81790a9603ed29ec31ba6da481
jskrivanek <jskrivanek@netbeans.org>
2002-02-06 10:48:17 +0000

    Updated ForTask to support count attribute (run something 'count' times).

--- xtest/src/org/netbeans/xtest/driver/ForTask.java
+++ xtest/src/org/netbeans/xtest/driver/ForTask.java
@@ -1,11 +1,11 @@
 /*
  *                 Sun Public License Notice
- * 
+ *
  * The contents of this file are subject to the Sun Public License
  * Version 1.0 (the "License"). You may not use this file except in
  * compliance with the License. A copy of the License is available at
  * http://www.sun.com/
- * 
+ *
  * The Original Code is NetBeans. The Initial Developer of the Original
  * Code is Sun Microsystems, Inc. Portions Copyright 1997-2000 Sun
  * Microsystems, Inc. All Rights Reserved.
@@ -25,16 +25,19 @@ import java.util.StringTokenizer;
 import java.util.LinkedList;
 import java.util.List;
 import org.apache.tools.ant.taskdefs.Ant;
-import org.apache.tools.ant.taskdefs.Property; 
+import org.apache.tools.ant.taskdefs.Property;
 import java.util.Iterator;
 import java.io.File;
 import org.apache.tools.ant.taskdefs.CallTarget;
 
-/** Task which behave as for command. Task has two required attributes (list,property) 
- * and one optional (delimiters) and have nested elements ant and antcall.
+/** Task which behave as for command. Task has two required attributes (list,property)
+ * and one optional (delimiters) or one attribute (count). 
+ * It has nested elements ant and antcall.
  * 'For' task divides value of list attribute by delimiters and for every part
- * calls all nested elements with property set to the devided part of the list. 
+ * calls all nested elements with property set to the devided part of the list.
  * Name of this property is specified by property attribute of for task.
+ * When 'count' attribute is used, it calls nested elements several times 
+ * depending on count value.
  *
  * Example:
  *
@@ -45,15 +48,24 @@ import org.apache.tools.ant.taskdefs.CallTarget;
  *      </ant>
  *  </for>
  *
+ * <!-- or -->
+ * 
+ *  <for count="3">
+ *      <antcall target="my_target"/>
+ *      <ant target="sometarget">
+ *        <property name="some.property" value="somevalue"/>
+ *      </ant>
+ *  </for>
  * @author lm97939
  */
 public class ForTask extends Task {
-
+    
     private String list;
     private String property;
     private String delim = ",;: \t\n\r\f";
-    private List antlist = new LinkedList(); 
-
+    private String count;
+    private List antlist = new LinkedList();
+    
     
     public void setList(String list) {
         this.list = list;
@@ -66,13 +78,17 @@ public class ForTask extends Task {
     public void setDelimiters(String delim) {
         this.delim = delim;
     }
-
+    
+    public void setCount(String count) {
+        this.count = count;
+    }
+    
     public static class MyAnt {
-        String antfile; 
+        String antfile;
         String target;
         File dir;
-        List properties = new LinkedList(); 
-
+        List properties = new LinkedList();
+        
         public void setTarget(String t) {
             target = t;
         }
@@ -89,27 +105,27 @@ public class ForTask extends Task {
     
     public static class MyAntCall {
         String target;
-        List properties = new LinkedList(); 
-
+        List properties = new LinkedList();
+        
         public void setTarget(String t) {
             target = t;
         }
-
+        
         public void addParam(MyProperty p) {
             properties .add(p);
         }
     }
     
     public static class MyProperty {
-            String name;
-            String value;
-            
-            public void setName(String n) {
-                name = n;
-            }
-            public void setValue(String v) {
-                value = v;
-            }
+        String name;
+        String value;
+        
+        public void setName(String n) {
+            name = n;
+        }
+        public void setValue(String v) {
+            value = v;
+        }
     }
     
     public void addAnt(MyAnt a) {
@@ -119,35 +135,87 @@ public class ForTask extends Task {
     public void addAntcall(MyAntCall a) {
         antlist.add(a);
     }
-
+    
     public void execute() throws BuildException {
-        if (list == null) throw new BuildException("Attribute 'list' is empty.");
-        if (property == null) throw new BuildException("Attribute 'property' is empty.");
         
-        StringTokenizer tokens = new StringTokenizer(list,delim);
-        while (tokens.hasMoreTokens()) {
-            String name = tokens.nextToken();
-            Iterator it = antlist.iterator();
-            while (it.hasNext()) {
-                Object o = it.next();
-                if (o instanceof MyAnt) {
-                    MyAnt a = (MyAnt)o;
-                    Iterator prop = a.properties.iterator();
-                    Ant ant = (Ant) project.createTask ("ant");
-                    ant.init ();                
-                    ant.setLocation (location);
-                    ant.setTarget(a.target);
-                    ant.setDir(a.dir);
-                    ant.setAntfile(a.antfile);
+        if(list == null && count == null) {
+            throw new BuildException("Attribute 'list' or 'count' must be defined.");
+        }
+        if(count != null && (list != null || property != null)) {
+            throw new BuildException("Only attribute 'count' must be defined.");
+        }
+        if(list != null && property == null) {
+            throw new BuildException("Attribute 'property' is empty.");
+        }
+        
+        if(count != null) {
+            int intCount;
+            try {
+                intCount = Integer.valueOf(count).intValue();
+            } catch (NumberFormatException e) {
+                throw new BuildException("Attribute 'count' must be a valid integer.");
+            }
+            for(int i=0; i<intCount;i++) {
+                runNested(null);
+            }
+        } else {
+            StringTokenizer tokens = new StringTokenizer(list,delim);
+            while (tokens.hasMoreTokens()) {
+                String name = tokens.nextToken();
+                runNested(name);
+            }
+        }
+    }
+    
+    public void runNested(String name) {
+        Iterator it = antlist.iterator();
+        while (it.hasNext()) {
+            Object o = it.next();
+            if (o instanceof MyAnt) {
+                MyAnt a = (MyAnt)o;
+                Iterator prop = a.properties.iterator();
+                Ant ant = (Ant) project.createTask("ant");
+                ant.init();
+                ant.setLocation(location);
+                ant.setTarget(a.target);
+                ant.setDir(a.dir);
+                ant.setAntfile(a.antfile);
+                if(name!=null) {
                     Property p1 = ant.createProperty();
                     p1.setName(property);
                     p1.setValue(name);
+                }
+                while (prop.hasNext()) {
+                    MyProperty myproperty = (MyProperty) prop.next();
+                    Property p = ant.createProperty();
+                    StringBuffer buff = new StringBuffer(myproperty.value);
+                    int index = buff.toString().indexOf("${"+property+"}");
+                    if (index != -1 && name != null)
+                        buff.replace(index,index+property.length()+3,name);
+                    p.setName(myproperty.name);
+                    p.setValue(buff.toString());
+                }
+                ant.execute();
+            }
+            else {
+                if (o instanceof MyAntCall) {
+                    MyAntCall a = (MyAntCall)o;
+                    Iterator prop = a.properties.iterator();
+                    CallTarget ant = (CallTarget) project.createTask("antcall");
+                    ant.init();
+                    ant.setLocation(location);
+                    ant.setTarget(a.target);
+                    if(name != null) {
+                        Property p1 = ant.createParam();
+                        p1.setName(property);
+                        p1.setValue(name);
+                    }
                     while (prop.hasNext()) {
                         MyProperty myproperty = (MyProperty) prop.next();
-                        Property p = ant.createProperty();
+                        Property p = ant.createParam();
                         StringBuffer buff = new StringBuffer(myproperty.value);
                         int index = buff.toString().indexOf("${"+property+"}");
-                        if (index != -1) 
+                        if (index != -1 && name != null)
                             buff.replace(index,index+property.length()+3,name);
                         p.setName(myproperty.name);
                         p.setValue(buff.toString());
@@ -155,34 +223,10 @@ public class ForTask extends Task {
                     ant.execute();
                 }
                 else {
-                  if (o instanceof MyAntCall) {
-                      MyAntCall a = (MyAntCall)o;
-                    Iterator prop = a.properties.iterator();
-                    CallTarget ant = (CallTarget) project.createTask ("antcall");
-                    ant.init ();                
-                    ant.setLocation (location);
-                    ant.setTarget(a.target);
-                    Property p1 = ant.createParam();
-                    p1.setName(property);
-                    p1.setValue(name);
-                    while (prop.hasNext()) {
-                        MyProperty myproperty = (MyProperty) prop.next();
-                        Property p = ant.createParam();
-                        StringBuffer buff = new StringBuffer(myproperty.value);
-                        int index = buff.toString().indexOf("${"+property+"}");
-                        if (index != -1) 
-                            buff.replace(index,index+property.length()+3,name);
-                        p.setName(myproperty.name);
-                        p.setValue(buff.toString());
-                    }
-                    ant.execute();
-                  }
-                  else {
-                      throw new BuildException("Unknown nested element.");
-                  }
+                    throw new BuildException("Unknown nested element.");
                 }
             }
         }
     }
-
+    
 }
