ea3b913f83ddc685064fe057aff42587c251179d
mmirilovic <mmirilovic@netbeans.org>
2002-04-02 14:19:35 +0000

    Remove files that cause errros after chaeckout on Win and add the same tests with new names..

---
+++ o.n.core/test/qa-functional/src/gui/propertyeditors/PropertyType_BooleanO.java
@@ -0,0 +1,78 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2002 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package gui.propertyeditors;
+
+import org.netbeans.junit.NbTestSuite;
+
+
+/**
+ * Test of Boolean Property Editor.
+ * 
+ * @author  Marian.Mirilovic@Sun.Com
+ */
+public class PropertyType_BooleanO extends PropertyEditorsTest {
+    
+    public String propertyName_L;
+    public String propertyValue_L;
+    
+    /** Creates a new instance of PropertyType_BooleanO */
+    public PropertyType_BooleanO(String testName) {
+        super(testName);
+    }
+    
+    
+    public void setUp(){
+        propertyName_L = "p_Boolean";
+        useForm = true;
+        super.setUp();
+    }
+    
+    public static NbTestSuite suite() {
+        NbTestSuite suite = new NbTestSuite();
+        suite.addTest(new PropertyType_BooleanO("testByComboTrue"));
+        suite.addTest(new PropertyType_BooleanO("testByComboFalse"));
+        return suite;
+    }
+    
+    public void testByComboFalse(){
+        //propertyValue_L = new Boolean(false).toString();
+        propertyValue_L = "False";
+        lastTest = true;
+        setByCombo(propertyName_L, propertyValue_L, true);
+    }
+    
+    public void testByComboTrue(){
+        //propertyValue_L = new Boolean(true).toString();
+        propertyValue_L = "True";
+        setByCombo(propertyName_L, propertyValue_L, true);
+    }
+    
+    public void setCustomizerValue() {
+    }
+    
+    public void verifyPropertyValue(boolean expectation) {
+        verifyExpectationValue(propertyName_L,expectation, propertyValue_L, propertyValue_L, false);
+    }
+    
+    
+    /** Test could be executed internaly in Forte without XTest
+     * @param args arguments from command line
+     */
+    public static void main(String[] args) {
+        //junit.textui.TestRunner.run(new NbTestSuite(PropertyType_BooleanO.class));
+        junit.textui.TestRunner.run(suite());
+    }
+    
+    
+}
---
+++ o.n.core/test/qa-functional/src/gui/propertyeditors/PropertyType_FloatO.java
@@ -0,0 +1,83 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2002 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package gui.propertyeditors;
+
+import org.netbeans.junit.NbTestSuite;
+
+
+/**
+ * Tests for Float Property Editor.
+ *
+ * @author  Marian.Mirilovic@Sun.Com
+ */
+public class PropertyType_FloatO extends PropertyEditorsTest {
+    
+    public String propertyName_L;
+    public String propertyValue_L;
+    public String propertyValueExpectation_L;
+    
+    public boolean waitDialog = false;
+    
+    /** Creates a new instance of PropertyType_FloatO */
+    public PropertyType_FloatO(String testName) {
+        super(testName);
+    }
+    
+    
+    public void setUp(){
+        propertyName_L = "p_Float";
+        useForm = true;
+        super.setUp();
+    }
+    
+    public static NbTestSuite suite() {
+        NbTestSuite suite = new NbTestSuite();
+        suite.addTest(new PropertyType_FloatO("testByInPlace"));
+        suite.addTest(new PropertyType_FloatO("testByInPlaceInvalid"));
+        return suite;
+    }
+    
+    public void testByInPlace(){
+        propertyValue_L = "123456789123456789123456789";
+        propertyValueExpectation_L = "1.2345679E26";
+        waitDialog = false;
+        setByInPlace(propertyName_L, propertyValue_L, true);
+    }
+    
+    public void testByInPlaceInvalid(){
+        propertyValue_L = "1 2";
+        propertyValueExpectation_L = propertyValue_L;
+        waitDialog = true;
+        lastTest = true;
+        setByInPlace(propertyName_L, propertyValue_L, false);
+    }
+    
+    public void setCustomizerValue() {
+    }
+    
+    public void verifyPropertyValue(boolean expectation) {
+        verifyExpectationValue(propertyName_L,expectation, propertyValueExpectation_L, propertyValue_L, waitDialog);
+    }
+    
+    
+    /** Test could be executed internaly in Forte without XTest
+     * @param args arguments from command line
+     */
+    public static void main(String[] args) {
+        //junit.textui.TestRunner.run(new NbTestSuite(PropertyType_FloatO.class));
+        junit.textui.TestRunner.run(suite());
+    }
+    
+    
+}
