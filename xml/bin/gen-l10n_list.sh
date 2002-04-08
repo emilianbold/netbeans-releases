07f1f768f01cface017246e2d6878788af509665
lkramolis <lkramolis@netbeans.org>
2002-04-08 11:05:08 +0000

    Fixed bug #22185 - l10n.list does not have javahelp files except .html.

--- xml/bin/gen-l10n_list.sh
+++ xml/bin/gen-l10n_list.sh
@@ -22,11 +22,39 @@
 #
 
 # reset old list
-echo -n > xml/l10n.list
+L10N_LIST="xml/l10n.list"
+echo -n > $L10N_LIST
 
 for module in catalog compat core css tax text-edit tools tree-edit; do
-    find xml/${module} -name "Bundle.properties" | sort > xml/${module}/l10n.list
-    find xml/${module} -name "*.html" | grep -v "_ja.html" | grep -v "package.html" | sort >> xml/${module}/l10n.list
+    MODULE_L10N_LIST="xml/${module}/l10n.list"
+    MODULE_L10N_LIST_TEMP="${MODULE_L10N_LIST}.temp"
 
-    cat xml/${module}/l10n.list >> xml/l10n.list
+    rm -f ${MODULE_L10N_LIST_TEMP}
+
+    ## src
+    for sub in / /compat/ /lib/ /lib/compat/; do
+        MODULE_SRC=xml/${module}${sub}src
+        if [ -e $MODULE_SRC ]; then
+            find $MODULE_SRC -name "Bundle.properties" >> ${MODULE_L10N_LIST_TEMP}
+            find $MODULE_SRC -name "*.html" | grep -v "_ja.html" | grep -v "package.html" >> ${MODULE_L10N_LIST_TEMP}
+            find $MODULE_SRC -name "*.template" >> ${MODULE_L10N_LIST_TEMP}
+        fi
+    done
+    
+    ## javahelp
+    MODULE_JAVAHELP=xml/${module}/javahelp
+    if [ -e $MODULE_JAVAHELP ]; then
+        for ext in html gif xml hs jhm; do
+            find $MODULE_JAVAHELP -name "*.${ext}" | grep -v "_ja.${ext}" >> ${MODULE_L10N_LIST_TEMP}
+        done
+    fi
+
+    ## sort
+    cat ${MODULE_L10N_LIST_TEMP} | sort > ${MODULE_L10N_LIST}
+    rm ${MODULE_L10N_LIST_TEMP}
+
+#    find xml/${module} -name "Bundle.properties" | sort > ${MODULE_L10N_LIST}
+#    find xml/${module} -name "*.html" | grep -v "_ja.html" | grep -v "package.html" | sort >> ${MODULE_L10N_LIST}
+
+    cat ${MODULE_L10N_LIST} >> $L10N_LIST
 done
