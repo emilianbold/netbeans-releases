1cf6ed73ecd285e79984b32f84be73d49cf3f7b5
asotona <asotona@netbeans.org>
2002-04-12 15:12:49 +0000

    added jemmyTest template and templates transformation

--- testtools/src/org/netbeans/modules/testtools/wizards/WizardIterator.java
+++ testtools/src/org/netbeans/modules/testtools/wizards/WizardIterator.java
@@ -19,9 +19,21 @@ package org.netbeans.modules.testtools.wizards;
  * Created on April 10, 2002, 1:51 PM
  */
 
-import org.openide.loaders.TemplateWizard;
-import org.openide.WizardDescriptor;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.lang.reflect.Modifier;
 import javax.swing.event.ChangeListener;
+
+import org.openide.WizardDescriptor;
+import org.openide.src.Type;
+import org.openide.src.Identifier;
+import org.openide.src.ClassElement;
+import org.openide.src.MethodElement;
+import org.openide.src.SourceException;
+import org.openide.cookies.SaveCookie;
+import org.openide.loaders.TemplateWizard;
+
+import org.netbeans.modules.java.JavaDataObject;
 
 /**
  *
@@ -67,6 +79,41 @@ public abstract class WizardIterator implements TemplateWizard.Iterator {
     public void uninitialize(TemplateWizard wizard) {
         panels=null;
         names=null;
+    }
+    
+    private static MethodElement[] getTemplateMethods(JavaDataObject source) {
+        ClassElement clel = source.getSource().getClass(Identifier.create(source.getName()));
+        MethodElement[] methods = clel.getMethods();
+        ArrayList templates = new ArrayList();
+        for (int i=0; i<methods.length; i++)
+            if ((methods[i].getName().getName().startsWith("test"))&&
+                (methods[i].getModifiers()==Modifier.PUBLIC)&&
+                (methods[i].getParameters().length==0)&&
+                (methods[i].getReturn().equals(Type.VOID)))
+                templates.add(methods[i]);
+        return (MethodElement[])templates.toArray(new MethodElement[templates.size()]);
+    }
+            
+    private static void transformTemplateMethods(JavaDataObject source, String[] newNames,  int[] indexes, MethodElement[] templates) throws SourceException, IOException {
+        ClassElement clel = source.getSource().getClass(Identifier.create(source.getName()));
+        clel.removeMethods(getTemplateMethods(source));
+        for (int i=0; i<newNames.length; i++) {
+            clel.addMethod(templates[indexes[i]]);
+            clel.getMethod(templates[indexes[i]].getName(), null).setName(Identifier.create(newNames[i]));
+        }
+        String className=source.getName();
+        StringBuffer suite = new StringBuffer();
+        suite.append("\n        TestSuite suite = new NbTestSuite();\n");
+        for (int i=0; i<newNames.length; i++) {
+            suite.append("        suite.addTest(new ");
+            suite.append(className);
+            suite.append("(\"");
+            suite.append(newNames[i]);
+            suite.append("\"));\n");
+        }
+        suite.append("        return suite;\n");
+        clel.getMethod(Identifier.create("suite"), null).setBody(suite.toString());
+        ((SaveCookie)source.getCookie(SaveCookie.class)).save();
     }
     
 }
