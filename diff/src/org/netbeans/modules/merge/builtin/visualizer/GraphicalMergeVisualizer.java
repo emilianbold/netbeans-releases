6f111c6d30a434674f85874d8fcf7da6052dc2c0
mentlicher <mentlicher@netbeans.org>
2002-04-23 16:03:27 +0000

    Removed unneeded stuff, added display name and short description.

--- diff/src/org/netbeans/modules/merge/builtin/visualizer/GraphicalMergeVisualizer.java
+++ diff/src/org/netbeans/modules/merge/builtin/visualizer/GraphicalMergeVisualizer.java
@@ -23,6 +23,7 @@ import java.io.Reader;
 import java.io.Writer;
 import java.io.Serializable;
 
+import org.openide.util.NbBundle;
 import org.openide.util.WeakListener;
 
 import org.netbeans.api.diff.Difference;
@@ -51,88 +52,18 @@ public class GraphicalMergeVisualizer extends MergeVisualizer implements Seriali
     }
     
     /**
-     * Show the visual representation of the diff between two sources.
-     * @param diffs The list of differences (instances of {@link Difference}).
-     *       may be <code>null</code> in case that it does not need diff provider.
-     * @param name1 the name of the first source
-     * @param title1 the title of the first source
-     * @param r1 the first source
-     * @param name2 the name of the second source
-     * @param title2 the title of the second source
-     * @param r2 the second resource compared with the first one.
-     * @param MIMEType the mime type of these sources
-     * @param actionListener
-     * @return The Component representing the diff visual representation
-     *        or null, when the representation is outside the IDE.
-     *
-    public Component createView(Difference[] diffs, String name1, String title1, Reader r1,
-                                String name2, String title2, Reader r2,
-                                String name3, String title3, Writer w3,
-                                String MIMEType, ActionListener actionListener) {
-        /*
-        merge = new MergeDialogDescriptor(diffs, name1+" <> "+name2, MIMEType,
-            name1, name2, name3, title1, title2, title3, r1, r2, w3,
-            new Color[] { colorMissing, colorAdded, colorChanged }, mergeListener);
-        return org.openide.TopManager.getDefault().createDialog(merge);
-         *
-        synchronized (this) {
-            if (merge == null) {
-                merge = new MergeDialogComponent();
-                merge.open();
-            }
-        }
-        MergePanel panel = new MergePanel();
-        MergeControl control = new MergeControl(/*merge, *panel);
-        control.initialize(diffs, name1, title1, r1, name2, title2, r2,
-                           name3, title3, w3, MIMEType, colorAdded, colorChanged, colorMissing);
-        merge.addMergePanel(panel, actionListener);
-        //control.addActionListener(actionL);
-        return merge;
-    }
+     * Get the display name of this merge visualizer.
      */
+    public String getDisplayName() {
+        return NbBundle.getMessage(GraphicalMergeVisualizer.class, "GraphicalMergeVisualizer.displayName");
+    }
     
     /**
-     * Show the visual representation of the merging process of two sources.
-     * The result of the merging process can be saved into a Writer even
-     * before all conflicts are actually resolved.
-     *
-     * @param diffs The list of conflicts.
-     * @param name1 the name of the first source
-     * @param title1 the title of the first source
-     * @param r1 the first source
-     * @param name2 the name of the second source
-     * @param title2 the title of the second source
-     * @param r2 the second resource compared with the first one.
-     * @param name3 the name of the result source
-     * @param title3 the title of the result source
-     * @param wi the information about the writer, that is used to write the result
-     * @param MIMEType the mime type of these sources
-     * @return The Component representing the diff visual representation
-     *        or null, when the representation is outside the IDE.
-     * @throws IOException when the reading from input streams fails.
-     *
-    public Component createView(Difference[] diffs, String name1, String title1, Reader r1,
-                                String name2, String title2, Reader r2,
-                                String name3, String title3, MergeVisualizer.WriterInfo wi,
-                                String MIMEType) throws IOException {
-        synchronized (this) {
-            System.out.println("createView(): merge = "+merge);
-            if (merge == null) {
-                merge = new MergeDialogComponent();
-                merge.open();
-            }
-        }
-        MergePanel panel = new MergePanel();
-        MergeControl control = new MergeControl(/*merge, *panel);
-        control.initialize(diffs, name1, title1, r1, name2, title2, r2,
-                           name3, title3, wi, MIMEType, colorUnresolvedConflict,
-                           colorResolvedConflict, colorOtherConflict);
-        merge.addVetoableChangeListener(WeakListener.vetoableChange(control, merge));
-        merge.addMergePanel(panel/*, actionListener*);
-        //control.addActionListener(actionL);
-        return merge;
-    }
+     * Get a short description of this merge visualizer.
      */
+    public String getShortDescription() {
+        return NbBundle.getMessage(GraphicalMergeVisualizer.class, "GraphicalMergeVisualizer.shortDescription");
+    }
     
     /**
      * Show the visual representation of the merging process of two sources.
@@ -157,13 +88,12 @@ public class GraphicalMergeVisualizer extends MergeVisualizer implements Seriali
             }
         }
         MergePanel panel = new MergePanel();
-        MergeControl control = new MergeControl(/*merge, */panel);
+        MergeControl control = new MergeControl(panel);
         control.initialize(diffs, source1, source2, result,
                            colorUnresolvedConflict, colorResolvedConflict,
                            colorOtherConflict);
         merge.addVetoableChangeListener(WeakListener.vetoableChange(control, merge));
-        merge.addMergePanel(panel/*, actionListener*/);
-        //control.addActionListener(actionL);
+        merge.addMergePanel(panel);
         return merge;
     }
     
