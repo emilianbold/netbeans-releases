2a1c0bb3909b1cf5d1b77782dba203c3ea2cef7c
jemmy <jemmy@netbeans.org>
2002-05-02 09:25:48 +0000

    *** empty log message ***

---
+++ jemmy/testsuite/src/org/netbeans/jemmy/testing/jemmy_038.java
@@ -0,0 +1,53 @@
+package org.netbeans.jemmy.testing;
+
+import org.netbeans.jemmy.*;
+
+import org.netbeans.jemmy.operators.*;
+
+import java.awt.Container;
+import java.awt.Frame;
+import java.awt.Window;
+
+import java.io.PrintWriter;
+
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+import javax.swing.text.JTextComponent;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.netbeans.jemmy.demo.Demonstrator;
+import org.netbeans.jemmy.util.Dumper;
+
+public class jemmy_038 extends JemmyTest {
+    public int runIt(Object obj) {
+
+	try {
+	    (new ClassReference("org.netbeans.jemmy.testing.Application_001")).startApplication();
+
+	    JFrame win =JFrameOperator.waitJFrame("Application_001", true, true);
+
+	    JFrameOperator fo = new JFrameOperator(win);
+
+	    fo.activate();
+	    fo.resize(400, 400);
+	    fo.move(200, 200);
+	    fo.maximize();
+	    fo.demaximize();
+	    fo.iconify();
+	    fo.deiconify();
+	    fo.close();
+
+	} catch(Exception e) {
+	    finalize();
+	    throw(new TestCompletedException(1, e));
+	}
+
+	finalize();
+
+	return(0);
+    }
+}
