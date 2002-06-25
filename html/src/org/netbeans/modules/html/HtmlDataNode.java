5782f8b4e2148564a80662dcc25a7317d102492d
rkubacki <rkubacki@netbeans.org>
2002-06-25 09:31:29 +0000

    Providing short description for HTML objects

--- html/src/org/netbeans/modules/html/HtmlDataNode.java
+++ html/src/org/netbeans/modules/html/HtmlDataNode.java
@@ -7,13 +7,12 @@
  * http://www.sun.com/
  * 
  * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2000 Sun
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2002 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
 package org.netbeans.modules.html;
 
-import java.lang.reflect.InvocationTargetException;
 import org.openide.actions.InstantiateAction;
 import org.openide.actions.OpenAction;
 import org.openide.actions.ViewAction;
@@ -24,26 +23,22 @@ import org.openide.util.NbBundle;
 import org.openide.util.actions.SystemAction;
 
 /**
- * Node that represents HTML data object
+ * Node that represents HTML data object.
  *
- * @author  rkubacki
- * @version 
+ * @author  Radim Kubacki
  */
 public class HtmlDataNode extends org.openide.loaders.DataNode {
     
-    // private static final String PROP_FOR_EDIT = "ForEdit";  // NOI18N
-
     /** Creates new HtmlDataNode */
     public HtmlDataNode (DataObject dobj, Children ch) {
         super (dobj, ch);
+        setShortDescription (NbBundle.getMessage(HtmlDataNode.class, "LBL_htmlNodeShortDesc"));
     }
 
     /** Get the default action for this node - 
-     *  ViewAction when data object ismounted on javadoc filesystem,
+     *  InstantiateAction for templates, 
+     *  ViewAction when data object is mounted on javadoc filesystem,
      *  OpenAction otherwise.
-     * This action can but need not be one from the list returned
-     * from {@link #getActions}. If so, the popup menu returned from {@link #getContextMenu}
-     * is encouraged to highlight the action.
      *
      * @return default action, or <code>null</code> if there should be none
      */
