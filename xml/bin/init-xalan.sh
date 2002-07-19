c876515104209463c1364b6d42394843a7bb19a2
lkramolis <lkramolis@netbeans.org>
2002-07-19 15:13:02 +0000

    Add JAXP and Xalan from nbextra to classpath.

---
+++ xml/bin/init-xalan.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# 
+#                 Sun Public License Notice
+# 
+# The contents of this file are subject to the Sun Public License
+# Version 1.0 (the "License"). You may not use this file except in
+# compliance with the License. A copy of the License is available at
+# http://www.sun.com/
+# 
+# The Original Code is NetBeans. The Initial Developer of the Original
+# Code is Sun Microsystems, Inc. Portions Copyright 1997-2002 Sun
+# Microsystems, Inc. All Rights Reserved.
+# 
+
+#set -x
+
+#
+# Extend classpath with JAXP and Xalan
+#
+# Run it from this (xml/bin) directory: '. init-xalan.sh'.
+#
+
+XMLBINDIR=`pwd -P`
+XML_ROOT=$XMLBINDIR/..
+NBROOT=${XML_ROOT}/..
+BINROOT=${NBROOT}/../nbextra
+
+XML_APIS=${BINROOT}/core/release/lib/ext/xml-apis.jar
+XALAN=${BINROOT}/libs/xalan/release/modules/autoload/ext/xalan-2.3.1.jar
+
+##<debug>
+#ls -la ${XML_APIS}
+#ls -la ${XALAN}
+##</debug>
+
+export CLASSPATH=${CLASSPATH}:${XML_APIS}:${XALAN}
