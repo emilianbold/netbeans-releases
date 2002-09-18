90f383363b041ddcda3743c9ea9702a9d103694d
asgeir <asgeir@netbeans.org>
2002-09-18 17:16:37 +0000

    Attributes are now editable for in the properties panel, if an element is selected in the text editor

--- xml.text/src/org/netbeans/modules/xml/text/completion/NodeSelector.java
+++ xml.text/src/org/netbeans/modules/xml/text/completion/NodeSelector.java
@@ -1,11 +1,11 @@
 /*
  *                 Sun Public License Notice
- * 
+ *
  * The contents of this file are subject to the Sun Public License
  * Version 1.0 (the "License"). You may not use this file except in
  * compliance with the License. A copy of the License is available at
  * http://www.sun.com/
- * 
+ *
  * The Original Code is NetBeans. The Initial Developer of the Original
  * Code is Sun Microsystems, Inc. Portions Copyright 1997-2000 Sun
  * Microsystems, Inc. All Rights Reserved.
@@ -15,6 +15,7 @@ package org.netbeans.modules.xml.text.completion;
 
 import java.awt.Container;
 import java.awt.event.ActionListener;
+import java.lang.reflect.InvocationTargetException;
 import javax.swing.JEditorPane;
 import javax.swing.Timer;
 import javax.swing.event.CaretEvent;
@@ -44,13 +45,13 @@ public class NodeSelector {
     
     /** Listener on caret movements */
     private CaretListener caretListener;
-
+    
     /** Timer which countdowns the "update selected element node" time. */ // NOI18N
     private Timer timerSelNodes;
     
     /** The last caret offset position. */
     private int lastCaretOffset = -1;
-        
+    
     /** Default delay between cursor movement and updating selected element nodes. */
     private static final int SELECTED_NODES_DELAY = 500;
     
@@ -59,29 +60,29 @@ public class NodeSelector {
     private XMLSyntaxSupport syntaxSupport;
     
     private Node originalUINode;
-            
+    
     HintContext hintContext;
-
+    
     /** Creates a new instance of NodeSelector */
     public NodeSelector(final JEditorPane pane) {
         this.pane = pane;
         
         caretListener = new CaretListener() {
-                public void caretUpdate (CaretEvent e) {
-                    restartTimerSelNodes(e.getDot());
-                }
-            };
-            
+            public void caretUpdate(CaretEvent e) {
+                restartTimerSelNodes(e.getDot());
+            }
+        };
+        
         timerSelNodes = new Timer(100, new java.awt.event.ActionListener() {
-                                      public void actionPerformed(java.awt.event.ActionEvent e) {
-                                          if (lastCaretOffset == -1 && pane != null) {
-                                              Caret caret = pane.getCaret();
-                                              if (caret != null)
-                                                lastCaretOffset = caret.getDot();
-                                          }
-                                          selectElementsAtOffset(lastCaretOffset);
-                                      }
-                                  });
+            public void actionPerformed(java.awt.event.ActionEvent e) {
+                if (lastCaretOffset == -1 && pane != null) {
+                    Caret caret = pane.getCaret();
+                    if (caret != null)
+                        lastCaretOffset = caret.getDot();
+                }
+                selectElementsAtOffset(lastCaretOffset);
+            }
+        });
         timerSelNodes.setInitialDelay(100);
         timerSelNodes.setRepeats(false);
         timerSelNodes.restart();
@@ -90,14 +91,14 @@ public class NodeSelector {
     }
     
     /** Restart the timer which updates the selected nodes after the specified delay from
-    * last caret movement.
-    */
+     * last caret movement.
+     */
     void restartTimerSelNodes(int pos) {
         timerSelNodes.setInitialDelay(SELECTED_NODES_DELAY);
         lastCaretOffset = pos;
         timerSelNodes.restart();
     }
-        
+    
     /** Selects element at the given position. */
     void selectElementsAtOffset(final int offset) {
         if (syntaxSupport == null) {
@@ -127,12 +128,12 @@ public class NodeSelector {
         if (originalUINode == null) {
             originalUINode = activeNodes[0];
         }
-
+        
         GrammarQuery grammarQuery = XMLCompletionQuery.getPerformer(pane.getDocument(), syntaxSupport);
         if (grammarQuery == null) {
             return;
         }
-                        
+        
         SyntaxQueryHelper helper = null;
         try {
             helper = new SyntaxQueryHelper(syntaxSupport, offset);
@@ -141,11 +142,11 @@ public class NodeSelector {
             return;
         }
         
-        Node newUiNode = new DelegatingNode(originalUINode, grammarQuery, helper.getContext());        
+        Node newUiNode = new DelegatingNode(originalUINode, grammarQuery, helper.getContext());
         
         topComp.setActivatedNodes(new Node[]{newUiNode});
     }
-        
+    
     private class DelegatingNode extends PeerNode {
         
         GrammarQuery grammarQuery;
@@ -167,7 +168,7 @@ public class NodeSelector {
                 return grammarQuery.getCustomizer(hintContext);
             }
         }
-
+        
         public boolean hasCustomizer() {
             if (grammarQuery == null || hintContext == null) {
                 return super.hasCustomizer();
@@ -175,24 +176,46 @@ public class NodeSelector {
                 return grammarQuery.hasCustomizer(hintContext);
             }
         }
-
+        
         public Node.PropertySet[] getPropertySets() {
             if (nodePropertySet == null) {
-                nodePropertySet = new Node.PropertySet("Node properties", "Node properties", 
-                        "Shows properties specific for the selected node in the text editor") {
-                        public Node.Property[] getProperties() {
-                            if (grammarQuery == null || hintContext == null) {
-                                return new Node.Property[0];
-                            } else {
-                                Node.Property[] nodeProperties = grammarQuery.getProperties(hintContext);
-                                if (nodeProperties != null) {
-                                    return nodeProperties;
-                                } else {
-                                    return new Node.Property[0];
-                                }
+                nodePropertySet = new Node.PropertySet("Node properties", "Node properties",
+                "Shows properties specific for the selected node in the text editor") {
+                    public Node.Property[] getProperties() {
+                        if (grammarQuery != null && hintContext != null) {
+                            Node.Property[] nodeProperties = grammarQuery.getProperties(hintContext);
+                            if (nodeProperties != null && nodeProperties.length > 0) {
+                                // The GrammarQuery controls the properties
+                                return nodeProperties;
                             }
                         }
-                    };
+                        
+                        // By default, we try to create properties from the attributes of the
+                        // selected element.
+                        org.w3c.dom.Element attributeOwningElem = null;
+                        if (hintContext != null) {
+                            if (hintContext.getNodeType() == org.w3c.dom.Node.ELEMENT_NODE) {
+                                attributeOwningElem = (org.w3c.dom.Element)hintContext;
+                            } else if (hintContext.getNodeType() == org.w3c.dom.Node.ATTRIBUTE_NODE) {
+                                attributeOwningElem = (org.w3c.dom.Element)((org.w3c.dom.Attr)hintContext).getOwnerElement();
+                            }
+                        }
+                        
+                        if (attributeOwningElem != null) {
+                            // We have a selected element that might have attributes 
+                            org.w3c.dom.NamedNodeMap attributes = attributeOwningElem.getAttributes();
+                            Node.Property[] nodeProperties = new Node.Property[attributes.getLength()];
+                            for (int ind = 0; ind < attributes.getLength(); ind++) {
+                                org.w3c.dom.Node node = attributes.item(ind);
+                                nodeProperties[ind] = new AttributeProperty(attributeOwningElem, node.getNodeName());          
+                            }
+                            
+                            return nodeProperties;
+                        }
+                        
+                        return new Node.Property[0];
+                    }
+                };
             }
             
             Node.PropertySet[] parentPropSets = super.getPropertySets();
@@ -202,6 +225,25 @@ public class NodeSelector {
             }
             newPropSets[parentPropSets.length] = nodePropertySet;
             return newPropSets;
+        }
+    }
+    
+    private class AttributeProperty extends org.openide.nodes.PropertySupport {
+        String propName;
+        org.w3c.dom.Element ownerElem;
+        
+        public AttributeProperty(org.w3c.dom.Element ownerElem, String propName) {
+            super(propName, String.class, propName, propName, true, true);
+            this.ownerElem = ownerElem;
+            this.propName = propName;
+        }
+        
+        public void setValue(Object value) {
+            ownerElem.setAttribute(propName, (String)value);
+        }
+        
+        public Object getValue() {
+            return ownerElem.getAttribute(propName);
         }
     }
 }
