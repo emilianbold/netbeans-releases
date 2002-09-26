71c130c9441d7595fcd29e03ed95bc19fbbbe083
jbecicka <jbecicka@netbeans.org>
2002-09-26 13:21:30 +0000

    Template file

---
+++ beans/test/qa-func/src/gui/data/TestFile.java
@@ -0,0 +1,17 @@
+/*
+ * TestFile.java
+ *
+ * Created on September 26, 2002, 9:37 AM
+ */
+
+/**
+ *
+ * @author  jb105785
+ */
+public class TestFile {
+    
+    /** Creates a new instance of TestFile */
+    public TestFile() {
+    }
+    
+}
