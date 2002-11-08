a5783e2ef2ff410cad04edba70b214ed03cb5544
jskrivanek <jskrivanek@netbeans.org>
2002-11-08 13:19:49 +0000

    Implemented 28457: - added inner class StatusTextTracer to be able to check even history of Main Window status bar messages - changed waitStatusText method to use StatusTextTracer

--- jellytools/src/org/netbeans/jellytools/MainWindowOperator.java
+++ jellytools/src/org/netbeans/jellytools/MainWindowOperator.java
@@ -14,11 +14,16 @@ package org.netbeans.jellytools;
 
 import java.awt.Component;
 import java.awt.Container;
+import java.util.ArrayList;
 import javax.swing.JComponent;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import org.openide.awt.Toolbar;
+import org.openide.awt.StatusDisplayer;
+import org.openide.windows.WindowManager;
 import org.netbeans.core.awt.TabControl;
 import org.netbeans.core.windows.UIModeManager;
 import org.netbeans.core.windows.WindowManagerImpl;
@@ -34,11 +39,12 @@ import org.netbeans.jemmy.operators.JButtonOperator;
 import org.netbeans.jemmy.operators.JFrameOperator;
 import org.netbeans.jemmy.operators.JMenuBarOperator;
 import org.netbeans.jemmy.operators.JPopupMenuOperator;
-import org.openide.windows.WindowManager;
 
 /**
- * Handle NetBeans main window. It manipulates with toolbars and workspaces. 
- * You can get text from status bar as well. To invoke menu use actions.
+ * Handle NetBeans main window. It manipulates with toolbars and workspaces.
+ * You can get text from status bar as well. To invoke menu items use
+ * {@link org.netbeans.jellytools.actions actions}.
+ * <br>
  * It is singleton, so to get instance use static method <code>getDefault()</code>.
  * <p>
  * Usage:<br>
@@ -59,7 +65,7 @@ import org.openide.windows.WindowManager;
  *      Thread.sleep(2000);
  *      new NbDialogOperator("About").close();
  * </pre>
-
+ *
  * @author Jiri.Skrivanek@sun.com
  */
 public class MainWindowOperator extends JFrameOperator {
@@ -70,14 +76,16 @@ public class MainWindowOperator extends JFrameOperator {
     private JMenuBarOperator _menuBar;
     /** Special tab component for workspace switching */
     private Component _tabControl;
+    /** Instance of StatusTextTracer for this MainWindowOperator instance */
+    private static StatusTextTracer statusTextTracer = null;
     
     /** Creates new instance of MainWindowOperator. Waits for JFrame with
      * title "NetBeans IDE..." or "Forte...".
      */
     private MainWindowOperator() {
         super(Bundle.getString("org.netbeans.core.windows.Bundle",
-            "CTL_MainWindow_Title",
-            new String[] {System.getProperty("netbeans.buildnumber")}));
+                               "CTL_MainWindow_Title",
+                               new String[] {System.getProperty("netbeans.buildnumber")}));
     }
     
     /** Returns instance of MainWindowOperator. It is singleton, so this method
@@ -107,20 +115,20 @@ public class MainWindowOperator extends JFrameOperator {
      */
     public static boolean isMDI() {
         return ((WindowManagerImpl)WindowManager.getDefault()).
-                    uiModeManager().getUIMode() == UIModeManager.MDI_MODE;
+                            uiModeManager().getUIMode() == UIModeManager.MDI_MODE;
     }
     
     /** Makes IDE to switch to MDI (full screen) mode. */
     public static void setMDI() {
         ((WindowManagerImpl)WindowManager.getDefault()).
-                    uiModeManager().setUIMode(UIModeManager.MDI_MODE);
+                            uiModeManager().setUIMode(UIModeManager.MDI_MODE);
         new EventTool().waitNoEvent(1000);
     }
     
     /** Makes IDE to switch to SDI (multiple smaller windows) mode. */
     public static void setSDI() {
         ((WindowManagerImpl)WindowManager.getDefault()).
-                    uiModeManager().setUIMode(UIModeManager.SDI_MODE);
+                            uiModeManager().setUIMode(UIModeManager.SDI_MODE);
         new EventTool().waitNoEvent(1000);
     }
     
@@ -131,22 +139,30 @@ public class MainWindowOperator extends JFrameOperator {
         return org.openide.awt.StatusDisplayer.getDefault().getStatusText();
     }
     
+    /** Returns singleton instance of StatusTextTracer.
+     * @return singleton instance of StatusTextTracer
+     */
+    public synchronized StatusTextTracer getStatusTextTracer() {
+        if(statusTextTracer == null) {
+            statusTextTracer = new StatusTextTracer();
+        }
+        return statusTextTracer;
+    }
+    
     /** Waits until given text appears in the main window status bar.
+     * If you want to trace status messages during an operation is proceed,
+     * use {@link StatusTextTracer}.
      * @param text  a text to wait for
      */
     public void waitStatusText(final String text) {
+        getStatusTextTracer().start();
         try {
-            new Waiter(new Waitable() {
-                public Object actionProduced(Object aText) {
-                    return getComparator().equals(getStatusText(), aText.toString()) 
-                            ? Boolean.TRUE : null;
-                }
-                public String getDescription() {
-                    return("Wait status text equals to "+text);
-                }
-            }).waitAction(text);
-        } catch (InterruptedException e) {
-            throw new JemmyException("Interrupted.", e);
+            // not wait in case status text was already printed out
+            if(!getComparator().equals(getStatusText(), text)) {
+                getStatusTextTracer().waitText(text);
+            }
+        } finally {
+            getStatusTextTracer().stop();
         }
     }
     
@@ -162,21 +178,21 @@ public class MainWindowOperator extends JFrameOperator {
     
     /** Switches to GUIEditing workspace. */
     public void switchToGUIEditingWorkspace() {
-        String label = Bundle.getStringTrimmed("org.netbeans.modules.form.Bundle", 
+        String label = Bundle.getStringTrimmed("org.netbeans.modules.form.Bundle",
                                                "CTL_GuiEditingWorkspaceName");
         switchToWorkspace(label);
     }
     
     /** Switches to Debugging workspace. */
     public void switchToDebuggingWorkspace() {
-        String label = Bundle.getString("org.netbeans.modules.debugger.resources.Bundle", 
+        String label = Bundle.getString("org.netbeans.modules.debugger.resources.Bundle",
                                         "CTL_Debugging_workspace");
         switchToWorkspace(label);
     }
     
     /** Switches to Editing workspace. */
     public void switchToEditingWorkspace() {
-        String label = Bundle.getStringTrimmed("org.netbeans.core.windows.Bundle", 
+        String label = Bundle.getStringTrimmed("org.netbeans.core.windows.Bundle",
                                                "CTL_Workspace_Editing");
         switchToWorkspace(label);
     }
@@ -269,7 +285,7 @@ public class MainWindowOperator extends JFrameOperator {
      */
     public ContainerOperator getToolbar(int index) {
         ComponentChooser chooser = new ToolbarChooser();
-        return new ContainerOperator((Container)waitComponent((Container)getSource(), 
+        return new ContainerOperator((Container)waitComponent((Container)getSource(),
                                      chooser, index));
     }
     
@@ -286,7 +302,7 @@ public class MainWindowOperator extends JFrameOperator {
      * @return number of toolbars
      */
     public int getToolbarCount() {
-        ToolbarChooser chooser = new ToolbarChooser("Non sense name - @#$%^&*", 
+        ToolbarChooser chooser = new ToolbarChooser("Non sense name - @#$%^&*", //NOI18N
                                                     getComparator());
         findComponent((Container)getSource(), chooser);
         return chooser.getCount();
@@ -434,11 +450,166 @@ public class MainWindowOperator extends JFrameOperator {
             return "Toolbar button with tooltip \""+buttonTooltip+"\".";
         }
     }
-
-    /** Performs verification by accessing all sub-components */    
+    
+    /** Performs verification by accessing all sub-components */
     public void verify() {
         menuBar();
         getTabControl();
     }
-}
+    
+    /** Class to trace messages printed to status bar of the Main Window.
+     * <p>
+     * Usage:<br>
+     * <pre>
+     *      MainWindowOperator.StatusTextTracer stt = MainWindowOperator.getDefault().getStatusTextTracer();
+     *      // start tracing
+     *      stt.start();
+     *      // compile action will produce at least two messages: "Compiling ...",
+     *      // "Finished ..."
+     *      new CompileAction().performAPI();
 
+     *      // waits for "Compiling" status text
+     *      stt.waitText("Compiling");
+     *      // waits for "Finished" status text
+     *      stt.waitText("Finished");
+     *
+     *      // order is not significant => following works as well
+     *      stt.waitText("Finished");
+     *      stt.waitText("Compiling");
+     *
+     *      // to be order significant, set removedCompared parameter to true
+     *      stt.waitText("Compiling", true);
+     *      stt.waitText("Finished", true);
+     *
+     *      // history was removed by above methods => need to produce a new messages
+     *      new CompileAction().performAPI();
+     *
+     *      // order is significant if removedCompared parameter is true =>
+     *      // => following fails because Finished is shown as second
+     *      stt.waitText("Finished", true);
+     *      stt.waitText("Compiling", true);
+     *
+     *      // stop tracing
+     *      stt.stop();
+     * </pre>
+     */
+    public class StatusTextTracer implements ChangeListener {
+        /** List of all messages */
+        private ArrayList statusTextHistory;
+        
+        /** Creates new instance. */
+        public StatusTextTracer() {
+            this.statusTextHistory = new ArrayList();
+        }
+        
+        /** Starts to register all status messages into history array.
+         * Exactly, it adds the listener to org.openide.awt.StatusDisplayer.
+         * It clears possible previously filled history array before.
+         */
+        public void start() {
+            stop();
+            clear();
+            StatusDisplayer.getDefault().addChangeListener(this);
+        }
+        
+        /** Stops registering of status messages. Exactly, it removes the
+         * listener from org.openide.awt.StatusDisplayer.
+         */
+        public void stop() {
+            StatusDisplayer.getDefault().removeChangeListener(this);
+        }
+        
+        /** Called when status text was changed. It adds status text to history
+         * array.
+         * @param evt change event - not used
+         */
+        public void stateChanged(ChangeEvent evt) {
+            synchronized (this) {
+                statusTextHistory.add(StatusDisplayer.getDefault().getStatusText());
+            }
+        }
+        
+        /** Clears status text history array. */
+        public void clear() {
+            synchronized (this) {
+                statusTextHistory.clear();
+            }
+        }
+        
+        /** Checks whether given text equals to any of messages in the history
+         * array. Comparator of this MainWindowOperator instance is used.
+         * If <tt>removeCompared</tt> parameter is set to <tt>true</tt>,
+         * messages already compared are removed from history array. Otherwise
+         * messages are not removed until {@link #clear} or {@link #start} are
+         * called.
+         * @param text a text to be compared
+         * @param removeCompared whether to remove already compared messages from
+         * history array
+         * @return true if text matches any of messages in the history array;
+         * false otherwise
+         */
+        public boolean contains(String text, boolean removeCompared) {
+            StringComparator comparator = getComparator();
+            synchronized (this) {
+                if(removeCompared) {
+                    while(!statusTextHistory.isEmpty()) {
+                        String status = (String)statusTextHistory.remove(0);
+                        if(comparator.equals(status, text)) {
+                            return true;
+                        }
+                    }
+                } else {
+                    for (int i = 0; i < statusTextHistory.size(); i ++) {
+                        if(comparator.equals((String)statusTextHistory.get(i), text)) {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+        }
+        
+        /** Waits for text to be shown in the Main Window status bar not 
+         * removing any message from history.
+         * Comparator of this MainWindowOperator instance is used.
+         * It throws TimeoutExpiredException if timeout expires.
+         * @param text a text to wait for
+         */
+        public void waitText(final String text) {
+            waitText(text, false);
+        }
+        
+        /** Waits for text to be shown in the Main Window status bar.
+         * Comparator of this MainWindowOperator instance is used.
+         * If <tt>removeCompared</tt> parameter is set to <tt>true</tt>,
+         * messages already compared are removed from history array. It satisfies
+         * that order of messages is significant when this method is called
+         * more than once.
+         * If <tt>removeCompared</tt> parameter is set to <tt>false</tt>,
+         * messages are not removed until {@link #clear} or {@link #start} are
+         * called and its order is not taken into account.
+         * @param text a text to wait for
+         * @param removeCompared whether to remove already compared messages from
+         * history array
+         */
+        public void waitText(final String text, final boolean removeCompared) {
+            try {
+                new Waiter(new Waitable() {
+                    public Object actionProduced(Object anObject) {
+                        return contains(text, removeCompared) ? Boolean.TRUE : null;
+                    }
+                    public String getDescription() {
+                        return("Wait status text equals to "+text);
+                    }
+                }).waitAction(null);
+            } catch (InterruptedException e) {
+                throw new JemmyException("Interrupted.", e);
+            }
+        }
+        
+        /** Calls {@link #stop} at the end of life cycle of this class. */
+        public void finalize() {
+            stop();
+        }
+    }
+}
