dbedd8b8a88f5c3c8107033eb9711577201a86bf
ehucka <ehucka@netbeans.org>
2002-11-11 14:33:53 +0000

    Rewritten conformable with IDE deprecations.

---
+++ editor/test/qa-functional/src/org/netbeans/test/editor/app/tests/CallTestGeneric.java
@@ -0,0 +1,125 @@
+/*
+ *                 Sun Public License Notice
+ *
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ *
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2001 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+package org.netbeans.test.editor.app.tests;
+
+//import experimental.Utils;
+import java.io.*;
+//import javasoft.sqe.javatest.*;
+import org.netbeans.test.editor.app.util.WriterOutputStream;
+import org.openide.loaders.DataObject;
+
+import java.security.PermissionCollection;
+import java.security.Permissions;
+import java.security.AllPermission;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Constructor;
+import org.openide.filesystems.Repository;
+
+public class CallTestGeneric {//implements Test {
+    
+    private static final boolean debug = true;
+    
+    private PrintWriter log = null;
+    
+    protected void log(String what) {
+        if (log != null) {
+            log.println(what);
+            log.flush();
+        } else {
+            System.err.println(what);
+            System.err.flush();
+        }
+    }
+    
+    public void runTest(String[] args, final PrintWriter log, final PrintWriter ref) throws Exception {
+        //	log("log!");
+        //	ref.println("ref!");
+        //	log.flush();
+        //	ref.flush();
+   /*     PrintStream oout = System.out;
+        PrintStream oerr = System.err;
+        
+        try {
+            if (debug)
+                System.err.println("Testing internal execution!");
+            if (Repository.getDefault() == null) {
+                throw new IllegalStateException("Repository.getDefault() == null, probably not internal execution.");
+            }
+            
+            if (debug)
+                System.err.println("Redirecting System.err and System.out.");
+            System.setErr(new PrintStream(new WriterOutputStream(log)));
+            System.setOut(new PrintStream(new WriterOutputStream(ref)));
+            */
+/*	if (debug)
+            log("Mounting editortest.jar:");
+        if (debug)
+            log("Unmounting old editortest.jar.");
+        Utils.unmountJar("editortest");
+ 
+        if (debug)
+            log("Mounting new editortest.jar.");
+        log.flush();
+        if (!Utils.mountJar("experimental", "editortest_new", "jar")) {
+            log("Unable to mount editortest.jar!");
+            return Status.failed("Unable to mount editortest.jar!");
+        };
+ 
+        if (debug)
+            log.print("Mounting editortest.jar done.");*/
+    /*        System.err.println("Before trying to execute:");
+            NbClassLoader cl = new NbClassLoader();
+            
+            PermissionCollection pcoll = new Permissions();
+            pcoll.add(new AllPermission());
+            cl.setDefaultPermissions(pcoll);
+            
+            Class callTest = cl.loadClass("org.netbeans.test.editor.app.gui.Main");
+            Method method = callTest.getMethod("main", new Class[] {args.getClass()});
+            
+            System.err.println(method.getReturnType());
+            
+            Object obj = method.invoke(null, new Object[] {args});
+        } catch (Exception e) {
+            e.printStackTrace(log);
+            throw e;
+        } finally {*/
+/*	    if (debug)
+                log ("Unmounting editortest.jar.");
+            Utils.unmountJar ("editortest_new");
+            if (debug)
+                log ("done.");*/
+    /*        System.err.flush();
+            System.out.flush();
+            System.setOut(oout);
+            System.setErr(oerr);
+        }
+    }
+    
+    public static final void main(String[] args) throws Exception {
+        String[] arguments = new String[] {
+            "/org/netbeans/test/editor/app/tests/javadoc_test.xml",
+            "Javadoc_writting.Common_Java_settings",
+            "/org/netbeans/test/editor/app/tests/javadoc_test.xml",
+            "Javadoc_writting.JavaDoc_inside",
+        };
+        PrintWriter log = new PrintWriter(System.err);
+        PrintWriter ref = new PrintWriter(System.out);
+        
+        new CallTestGeneric().runTest(arguments, log, ref);
+        
+        log.close();
+        ref.close();*/
+    }
+}
---
+++ editor/test/qa-functional/src/org/netbeans/test/editor/app/tests/GenerateTests.java
@@ -0,0 +1,227 @@
+/*
+ *                 Sun Public License Notice
+ *
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ *
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2001 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+package org.netbeans.test.editor.app.tests;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileLock;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+import org.xml.sax.InputSource;
+
+import org.netbeans.test.editor.app.core.Test;
+import java.io.InputStream;
+import java.util.Enumeration;
+import org.openide.filesystems.Repository;
+
+/**
+ *
+ * @author  jlahoda
+ * @version
+ */
+public class GenerateTests extends Object {
+    
+    private String masterPackage = "org.netbeans.test.editor.app.tests";
+    
+    /** Creates new GenerateTests */
+    public GenerateTests() {
+    }
+    
+    private void generateGoldenFile(Element test, FileObject file) throws IOException {
+        String content = Test.loadString(test, "Golden");
+        
+        if (content == null) {
+            content = "";
+        }
+        FileLock lock = null;
+        PrintWriter write = null;
+        try {
+            lock = file.lock();
+            write = new PrintWriter(file.getOutputStream(lock));
+            write.print(content);
+            write.flush();
+            write.close();
+            write = null;
+        } finally {
+            if (lock != null) {
+                lock.releaseLock();
+            }
+            if (write != null) {
+                write.close();
+            }
+        }
+    }
+    
+    private void generateCall(Element call, PrintWriter result) {
+        String file = call.getAttribute("File");
+        String action = call.getAttribute("Action");
+        
+        if (file != null) {
+            result.println("\"" + file + "\",");
+        } else {
+            result.println("\"\",");
+        }
+        if (action != null) {
+            result.println("\"" + action + "\",");
+        } else {
+            result.println("\"\",");
+        }
+    }
+    
+    private boolean isCVS(FileObject fo) {
+        return fo.isFolder() && "CVS".equals(fo.getName());
+    }
+    
+    private void removeContentExceptCVS() throws IOException {
+        FileObject toRemove = Repository.getDefault().findResource(masterPackage+".AppTestPerformer.java");
+        
+        if (toRemove != null)
+            toRemove.delete();
+        
+        toRemove = Repository.getDefault().findResource(masterPackage+".data.goldenfiles.AppTestPerformer");
+        
+        FileObject[] fo = toRemove.getChildren();
+        
+        for (int cntr = 0; cntr < fo.length; cntr++) {
+            if (!isCVS(fo[cntr]))
+                fo[cntr].delete();
+        }
+    }
+    
+    private void generateTest(Element test, PrintWriter result) throws IOException {
+        String name = test.getAttribute("Name");
+        
+        System.err.println("name = " + name );
+        if (name == null || "".equals(name)) {
+            throw new IllegalArgumentException("Test element has empty Name attribute!");
+        }
+        
+        NodeList nodes = test.getElementsByTagName("Call");
+        StringBuffer params = new StringBuffer();
+        
+        result.println("public void test" + name + "() throws Exception {");
+        result.println("PrintWriter ref = null;");
+        result.println("PrintWriter log = null;");
+        result.println("String[] arguments = new String[] {");
+        
+        for (int cntr = 0; cntr < nodes.getLength(); cntr++) {
+            generateCall((Element)nodes.item(cntr), result);
+        }
+        
+        result.println("};");
+        
+        result.println("try {");
+        result.println("ref = new PrintWriter(getRef());");
+        result.println("log = new PrintWriter(getLog());");
+        result.println("new CallTestGeneric().runTest(arguments, log, ref);");
+        result.println("} finally {");
+        result.println("if (ref != null) ref.flush();");
+        result.println("if (log != null) log.flush();");
+        result.println("}");
+        result.println("}");
+        
+        FileObject goldenDir = Repository.getDefault().findResource(masterPackage+".data.goldenfiles.AppTestPerformer");
+        FileObject goldenFile = goldenDir.createData("test" + name + ".pass");
+        
+        generateGoldenFile(test, goldenFile);
+    }
+    
+    private void createAllTests(Element tests) throws IOException {
+        removeContentExceptCVS();
+        FileObject testFileDir  = Repository.getDefault().findResource(masterPackage);
+        FileObject testFile     = testFileDir.createData("AppTestPerformer", "java");
+        FileLock   testFileLock = null;
+        PrintWriter result      = null;
+        try {
+            testFileLock = testFile.lock();
+            result = new PrintWriter(testFile.getOutputStream(testFileLock));
+            
+            result.println("//This file was automaticaly generated. Do not modify.");
+            result.println("/*");
+            result.println(" *                 Sun Public License Notice");
+            result.println(" *");
+            result.println(" * The contents of this file are subject to the Sun Public License");
+            result.println(" * Version 1.0 (the \"License\"). You may not use this file except in");
+            result.println(" * compliance with the License. A copy of the License is available at");
+            result.println(" * http://www.sun.com/");
+            result.println(" *");
+            result.println(" * The Original Code is NetBeans. The Initial Developer of the Original");
+            result.println(" * Code is Sun Microsystems, Inc. Portions Copyright 1997-2001 Sun");
+            result.println(" * Microsystems, Inc. All Rights Reserved.");
+            result.println(" */");
+            result.println("");
+            result.println("package " + masterPackage + ";");
+            result.println("import java.io.PrintWriter;");
+            result.println("import org.openide.filesystems.*;");
+            result.println("import org.netbeans.junit.NbTestCase;");
+            result.println("import org.netbeans.junit.NbTestSuite;");
+            result.println("import java.io.File;");
+            result.println("import org.netbeans.test.editor.LineDiff;");
+            
+            result.println("public class AppTestPerformer extends NbTestCase {");
+            result.println("public AppTestPerformer(String name) {");
+            result.println("super(name);");
+            result.println("}");
+            result.println("public void tearDown() throws Exception {");
+            //            result.println("compareReferenceFiles();");
+            result.println("    assertFile(\"Output does not match golden file.\", getGoldenFile(), new File(getWorkDir(), this.getName() + \".ref\"), null, new LineDiff(false));");
+            result.println("}");
+            NodeList nodes = tests.getElementsByTagName("Test");
+            
+            for (int cntr = 0; cntr < nodes.getLength(); cntr++) {
+                generateTest((Element)nodes.item(cntr), result);
+            }
+            result.println("}");
+        } finally {
+            if (result != null)
+                result.close();
+            if (testFileLock != null)
+                testFileLock.releaseLock();
+        }
+    }
+    
+    public static final void main(String[] args) {
+        if (args.length < 1) {
+            System.err.println("FATAL - First argument has to be generator file!");
+            System.err.flush();
+            return;
+        }
+        
+        InputStream input = GenerateTests.class.getResourceAsStream(args[0]);
+        
+        if (input == null) {
+            System.err.println("FATAL - Generator file " + args[0] + " not found!");
+            System.err.flush();
+            return;
+        }
+        
+        try {
+            Document doc = org.openide.xml.XMLUtil.parse(new InputSource(input), false, false, null, null);
+            GenerateTests generator = new GenerateTests();
+            
+            generator.createAllTests((Element) doc.getElementsByTagName("TestList").item(0));
+        } catch (IOException e) {
+            e.printStackTrace(System.err);
+        } catch (SAXException e) {
+            e.printStackTrace(System.err);
+        }
+        System.err.flush();
+    }
+    
+}
