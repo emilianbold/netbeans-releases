3a613c3889e70827e2d1026a6225e71b4e70d807
av <av@netbeans.org>
2002-12-05 11:00:57 +0000

    settings: Added External Browser(Unix) tests. Updated 3 methods for EBCL

--- extbrowser/test/ExtBrowser/qa-functional/src/org/netbeans/test/gui/web/extbrowser/settings/BrowserRegistry.java
+++ extbrowser/test/ExtBrowser/qa-functional/src/org/netbeans/test/gui/web/extbrowser/settings/BrowserRegistry.java
@@ -18,6 +18,7 @@ import org.netbeans.test.gui.web.util.BrowserUtils;
 
 import org.netbeans.jemmy.operators.JButtonOperator;
 import org.netbeans.jemmy.operators.JTextFieldOperator;
+import org.netbeans.jemmy.operators.JTextComponentOperator;
 import org.netbeans.junit.NbTestSuite;
 import java.io.File;
 
@@ -126,7 +127,7 @@ public class BrowserRegistry extends JellyTestCase {
 	TextFieldProperty pr = new TextFieldProperty(psto, pnameBrowserExecutable);
 	pr.openEditor();
 	NbDialogOperator nbo = new NbDialogOperator(pnameBrowserExecutable);
-	if(!(new JTextFieldOperator(nbo, 1)).getText().equals("{URL}")) {
+	if(!(new JTextComponentOperator(nbo, 1)).getText().equals("{URL}")) {
 	    fail("Wrong default arguments field: " + (new JTextFieldOperator(nbo, 1)).getText());
 	}
 	String custom = "..."; //Correct bundle Not found currently
@@ -138,8 +139,8 @@ public class BrowserRegistry extends JellyTestCase {
 	String openTitle =  Bundle.getString("org.openide.explorer.propertysheet.editors.Bundle", "CTL_OpenButtonName");
 	new JButtonOperator(nbo1, openTitle).push();
 	nbo.ok();
-	if (!pr.getValue().equals(newExec + "  {URL}")) {
-	    fail("Browser Executable not set via editor. \"" + pr.getValue() + "\" instead of \"" + newExec + "  {URL}\"");
+	if (!pr.getValue().equals(newExec + " {URL}")) {
+	    fail("Browser Executable not set via editor. \"" + pr.getValue() + "\" instead of \"" + newExec + " {URL}\"");
 	}
 	oo.close();
 	oo = OptionsOperator.invoke();
@@ -147,8 +148,8 @@ public class BrowserRegistry extends JellyTestCase {
 	pso = PropertySheetOperator.invoke();
         psto = new PropertySheetTabOperator(pso);
 	pr = new TextFieldProperty(psto, pnameBrowserExecutable);
-	if (!pr.getValue().equals(newExec + "  {URL}")) {
-	    fail("Browser Executable field not saved, if set via editor. "+ pr.getValue() + " instead of \"" + newExec + "  {URL}\"");
+	if (!pr.getValue().equals(newExec + " {URL}")) {
+	    fail("Browser Executable field not saved, if set via editor. "+ pr.getValue() + " instead of \"" + newExec + " {URL}\"");
 	}
     }
     /**
@@ -172,8 +173,8 @@ public class BrowserRegistry extends JellyTestCase {
 	NbDialogOperator nbo = new NbDialogOperator(pnameBrowserExecutable);
 	new JTextFieldOperator(nbo, 0).setText(newExec);
 	nbo.cancel();
-	if (!pr.getValue().equals(origExec + "  {URL}")) {
-	    fail("\"" + pr.getValue() + "\" instead of \"" + origExec + "  {URL}\"");
+	if (!pr.getValue().equals(origExec)) {
+	    fail("\"" + pr.getValue() + "\" instead of \"" + origExec + "\"");
 	}
     }
 
@@ -196,12 +197,169 @@ public class BrowserRegistry extends JellyTestCase {
 	String origExec = pr.getValue();
 	pr.openEditor();
 	NbDialogOperator nbo = new NbDialogOperator(pnameBrowserExecutable);
-	new JTextFieldOperator(nbo, 1).setText(newArg);
+	new JTextComponentOperator(nbo, 1).setText(newArg);
 	nbo.cancel();
 	if (!pr.getValue().equals(origExec)) {
 	    fail("\"" + pr.getValue() + "\" instead of \"" + origExec + "\"");
 	}
     }
+
+    /********************************************
+       External Browser (Unix)  section
+    *********************************************/
+    /**
+       External Browser (Unix) : Change Browser Description 
+     **/
+    public void testChangeBrowserDescrEBU() {
+	String newDescr = "CL Browser";
+	OptionsOperator oo = OptionsOperator.invoke();
+	String ideConfiguration = Bundle.getString("org.netbeans.core.Bundle", "UI/Services/IDEConfiguration");
+	String sets = Bundle.getString("org.netbeans.core.Bundle", "UI/Services/IDEConfiguration/ServerAndExternalToolSettings");
+	String browsers = Bundle.getString("org.netbeans.core.Bundle", "Services/Browsers");
+	String cl = Bundle.getString("org.netbeans.modules.extbrowser.Bundle","CTL_UnixBrowserName");
+	oo.selectOption(ideConfiguration + iSep + sets + iSep + browsers + iSep + cl);
+	PropertySheetOperator pso = PropertySheetOperator.invoke();
+        PropertySheetTabOperator psto = new PropertySheetTabOperator(pso);
+	String pnameBrowserDescr = Bundle.getString("org.netbeans.modules.extbrowser.Bundle" ,"PROP_Description");
+	TextFieldProperty pr = new TextFieldProperty(psto, pnameBrowserDescr);
+	if (pr.isEditable()) {
+	    fail("Browser Description field editable");
+	}
+	oo.close();
+	
+    }
+   
+    /**
+       External Browser (Unix) : Set Process to Browser in Path
+     **/
+    
+    public void testSetProcessToBrowserInPathEBU() {
+	String newExec = "netscape {params}";
+	OptionsOperator oo = OptionsOperator.invoke();
+	String ideConfiguration = Bundle.getString("org.netbeans.core.Bundle", "UI/Services/IDEConfiguration");
+	String sets = Bundle.getString("org.netbeans.core.Bundle", "UI/Services/IDEConfiguration/ServerAndExternalToolSettings");
+	String browsers = Bundle.getString("org.netbeans.core.Bundle", "Services/Browsers");
+	String cl = Bundle.getString("org.netbeans.modules.extbrowser.Bundle","CTL_UnixBrowserName");
+	oo.selectOption(ideConfiguration + iSep + sets + iSep + browsers + iSep + cl);
+	PropertySheetOperator pso = PropertySheetOperator.invoke();
+        PropertySheetTabOperator psto = new PropertySheetTabOperator(pso);
+	String pnameBrowserExecutable = Bundle.getString("org.netbeans.modules.extbrowser.Bundle" ,"PROP_browserExecutable");
+	TextFieldProperty pr = new TextFieldProperty(psto, pnameBrowserExecutable);
+	pr.setValue(newExec);
+	if (!pr.getValue().equals(newExec)) {
+	    fail("Browser Executable field not editable");
+	}
+	oo.close();
+	oo = OptionsOperator.invoke();
+	oo.selectOption(ideConfiguration + iSep + sets + iSep + browsers + iSep + cl);
+	pso = PropertySheetOperator.invoke();
+        psto = new PropertySheetTabOperator(pso);
+	pr = new TextFieldProperty(psto, pnameBrowserExecutable);
+	if (!pr.getValue().equals(newExec)) {
+	    fail("Browser Executable field not saved");
+	  }
+    }
+    /**
+       External Browser (Unix) :  Set Process with Full Path to Browser
+                           &
+       External Browser (Unix) :  Use String '{params}' in Process    
+     **/
+
+    public void testSetProcessWithFullPathToBrowserEBU() {
+	String newExec = fullPathCommand();
+	System.out.println("##$$##Executable full path is \"" + newExec + "\"");
+	OptionsOperator oo = OptionsOperator.invoke();
+	String ideConfiguration = Bundle.getString("org.netbeans.core.Bundle", "UI/Services/IDEConfiguration");
+	String sets = Bundle.getString("org.netbeans.core.Bundle", "UI/Services/IDEConfiguration/ServerAndExternalToolSettings");
+	String browsers = Bundle.getString("org.netbeans.core.Bundle", "Services/Browsers");
+	String cl = Bundle.getString("org.netbeans.modules.extbrowser.Bundle","CTL_UnixBrowserName");
+	oo.selectOption(ideConfiguration + iSep + sets + iSep + browsers + iSep + cl);
+	PropertySheetOperator pso = PropertySheetOperator.invoke();
+        PropertySheetTabOperator psto = new PropertySheetTabOperator(pso);
+	String pnameBrowserExecutable = Bundle.getString("org.netbeans.modules.extbrowser.Bundle" ,"PROP_browserExecutable");
+	TextFieldProperty pr = new TextFieldProperty(psto, pnameBrowserExecutable);
+	pr.openEditor();
+	NbDialogOperator nbo = new NbDialogOperator(pnameBrowserExecutable);
+	if(!(new JTextComponentOperator(nbo, 1)).getText().equals("{params}")) {
+	    fail("Wrong default arguments field: " + (new JTextFieldOperator(nbo, 1)).getText());
+	}
+	String custom = "..."; //Correct bundle Not found currently
+	new JButtonOperator(nbo, custom).pushNoBlock();
+	System.out.println("Before wait for open");
+	NbDialogOperator nbo1 = new NbDialogOperator("Open");
+	System.out.println("After wait for open");
+	new JTextFieldOperator(nbo1, 0).setText(newExec);
+	String openTitle =  Bundle.getString("org.openide.explorer.propertysheet.editors.Bundle", "CTL_OpenButtonName");
+	new JButtonOperator(nbo1, openTitle).push();
+	nbo.ok();
+	if (!pr.getValue().equals(newExec + " {params}")) {
+	    fail("Browser Executable not set via editor. \"" + pr.getValue() + "\" instead of \"" + newExec + " {params}\"");
+	}
+	oo.close();
+	oo = OptionsOperator.invoke();
+	oo.selectOption(ideConfiguration + iSep + sets + iSep + browsers + iSep + cl);
+	pso = PropertySheetOperator.invoke();
+        psto = new PropertySheetTabOperator(pso);
+	pr = new TextFieldProperty(psto, pnameBrowserExecutable);
+	if (!pr.getValue().equals(newExec + " {params}")) {
+	    fail("Browser Executable field not saved, if set via editor. "+ pr.getValue() + " instead of \"" + newExec + " {params}\"");
+	}
+    }
+    /**
+	   External Browser (Unix) : Change process -- "Cancel" 
+    **/
+    
+    public void testChangeProcessCancelEBU() {
+	String newExec = "UnReal";
+	OptionsOperator oo = OptionsOperator.invoke();
+	String ideConfiguration = Bundle.getString("org.netbeans.core.Bundle", "UI/Services/IDEConfiguration");
+	String sets = Bundle.getString("org.netbeans.core.Bundle", "UI/Services/IDEConfiguration/ServerAndExternalToolSettings");
+	String browsers = Bundle.getString("org.netbeans.core.Bundle", "Services/Browsers");
+	String cl = Bundle.getString("org.netbeans.modules.extbrowser.Bundle","CTL_UnixBrowserName");
+	oo.selectOption(ideConfiguration + iSep + sets + iSep + browsers + iSep + cl);
+	PropertySheetOperator pso = PropertySheetOperator.invoke();
+        PropertySheetTabOperator psto = new PropertySheetTabOperator(pso);
+	String pnameBrowserExecutable = Bundle.getString("org.netbeans.modules.extbrowser.Bundle" ,"PROP_browserExecutable");
+	TextFieldProperty pr = new TextFieldProperty(psto, pnameBrowserExecutable);
+	String origExec = pr.getValue();
+	pr.openEditor();
+	NbDialogOperator nbo = new NbDialogOperator(pnameBrowserExecutable);
+	new JTextFieldOperator(nbo, 0).setText(newExec);
+	nbo.cancel();
+	if (!pr.getValue().equals(origExec)) {
+	    fail("\"" + pr.getValue() + "\" instead of \"" + origExec + "\"");
+	}
+    }
+
+    /**
+	   External Browser (Unix) : Change arguments -- "Cancel" 
+    **/
+    
+    public void testChangeArgumentsCancelEBU() {
+	String newArg = "UnRealArgs";
+	OptionsOperator oo = OptionsOperator.invoke();
+	String ideConfiguration = Bundle.getString("org.netbeans.core.Bundle", "UI/Services/IDEConfiguration");
+	String sets = Bundle.getString("org.netbeans.core.Bundle", "UI/Services/IDEConfiguration/ServerAndExternalToolSettings");
+	String browsers = Bundle.getString("org.netbeans.core.Bundle", "Services/Browsers");
+	String cl = Bundle.getString("org.netbeans.modules.extbrowser.Bundle","CTL_UnixBrowserName");
+	oo.selectOption(ideConfiguration + iSep + sets + iSep + browsers + iSep + cl);
+	PropertySheetOperator pso = PropertySheetOperator.invoke();
+        PropertySheetTabOperator psto = new PropertySheetTabOperator(pso);
+	String pnameBrowserExecutable = Bundle.getString("org.netbeans.modules.extbrowser.Bundle" ,"PROP_browserExecutable");
+	TextFieldProperty pr = new TextFieldProperty(psto, pnameBrowserExecutable);
+	String origExec = pr.getValue();
+	pr.openEditor();
+	NbDialogOperator nbo = new NbDialogOperator(pnameBrowserExecutable);
+	new JTextComponentOperator(nbo, 1).setText(newArg);
+	nbo.cancel();
+	if (!pr.getValue().equals(origExec)) {
+	    fail("\"" + pr.getValue() + "\" instead of \"" + origExec + "\"");
+	}
+    }
+
+
+
+
 
 
 
