8d1dbfdc3ff75bd2728b0c2a2b7dc8dbe14883aa
av <av@netbeans.org>
2002-12-19 10:36:25 +0000

    Updated BrowserUtils for "using"a tests

--- extbrowser/test/ExtBrowser/qa-functional/src/org/netbeans/test/gui/web/util/BrowserUtils.java
+++ extbrowser/test/ExtBrowser/qa-functional/src/org/netbeans/test/gui/web/util/BrowserUtils.java
@@ -26,6 +26,8 @@ import org.netbeans.jellytools.properties.PropertySheetTabOperator;
 import org.netbeans.jellytools.properties.PropertySheetOperator;
 
 
+import java.io.File;
+
 public class BrowserUtils {
     private static String iSep = "|";
     public BrowserUtils() {
@@ -63,6 +65,24 @@ public class BrowserUtils {
 	}
     }
     
+
+    public static void setEBUBrowserCommand(String command) {
+	OptionsOperator oo = OptionsOperator.invoke();
+	String ideConfiguration = Bundle.getString("org.netbeans.core.Bundle", "UI/Services/IDEConfiguration");
+	String sets = Bundle.getString("org.netbeans.core.Bundle", "UI/Services/IDEConfiguration/ServerAndExternalToolSettings");
+	String browsers = Bundle.getString("org.netbeans.core.Bundle", "Services/Browsers");
+	String cl = Bundle.getString("org.netbeans.modules.extbrowser.Bundle","CTL_UnixBrowserName");
+	oo.selectOption(ideConfiguration + iSep + sets + iSep + browsers + iSep + cl);
+	PropertySheetOperator pso = PropertySheetOperator.invoke();
+        PropertySheetTabOperator psto = new PropertySheetTabOperator(pso);
+	String pnameBrowserExecutable = Bundle.getString("org.netbeans.modules.extbrowser.Bundle" ,"PROP_browserExecutable");
+	TextFieldProperty pr = new TextFieldProperty(psto, pnameBrowserExecutable);
+	if (!pr.getValue().equals(command)) {
+	    pr.setValue(command);
+	}
+    }
+
+
     public static  void setBrowser(String name) {
 	//new OptionsViewAction().perform();
 	OptionsOperator oo = OptionsOperator.invoke();
@@ -89,5 +109,101 @@ public class BrowserUtils {
 	}
     }
 
+
+    /**
+       DDE servers section
+    */
+    public static void setDDEServerExplorer() {
+	System.out.println("#####\nNot implemented yet\n#####");
+    }
+    public static void setDDEServerNetscape() {
+	System.out.println("#####\nNot implemented yet\n#####");
+    }
+    
+    public static void setDDEServerNetscape6() {
+	System.out.println("#####\nNot implemented yet\n#####");
+    }
+    /* End of DDE servers section */
+
+    /**
+       Get Browsers section
+     */
+
+    public static String getIEInPath() {
+	System.out.println("#####\nNot implemented yet\n#####");
+	return null;
+    }
+    public static String getIEFullPath() {
+	System.out.println("#####\nNot implemented yet\n#####");
+	return null;
+    }
+    public static String getNetscapeFullPath() {
+	String[] paths = null;
+	String command = null;
+	if(System.getProperty("os.name").indexOf("Windows")!=-1) {
+            System.out.println("##########\nThis test must be extended for Windows platform\n#######");
+	    return null;
+        }else {
+	    paths = new String[] {"/usr/bin/netscape","/usr/local/bin/netscape","/bin/netscape"};
+	}
+	for(int i=0;i<paths.length;i++) {
+	    if((new File(paths[i])).exists()) {
+		command = paths[i] + " {URL}";
+		i = paths.length;
+	    }
+	}
+	if(command == null) {
+	    StringBuffer reason = new StringBuffer("Nothing of following commands found on your system : ");
+	    for(int i=0;i<paths.length;i++) {
+		reason.append(paths[i] + ";");
+	    }
+	    System.out.println("##########\n" + reason.toString() + "\n##########");
+	    return null;
+	}
+	return command;
+    }
+    public static String getNetscapeInPath() {
+	String netscapeWin = "netscape.exe";
+	String netscapeUx = "netscape";
+	if(System.getProperty("os.name").indexOf("Windows")!=-1) {
+	    return netscapeWin;
+        }else {
+	    return netscapeUx;
+	}
+    }
+    public static String getNetscape6FullPath() {
+	String[] paths = null;
+	String command = null;
+	if(System.getProperty("os.name").indexOf("Windows")!=-1) {
+            System.out.println("##########\nThis test must be extended for Windows platform\n#######");
+	    return null;
+        }else {
+	    paths = new String[] {"/usr/bin/netscape6","/usr/local/bin/netscape6","/bin/netscape6"};
+	}
+	for(int i=0;i<paths.length;i++) {
+	    if((new File(paths[i])).exists()) {
+		command = paths[i] + " {URL}";
+		i = paths.length;
+	    }
+	}
+	if(command == null) {
+	    StringBuffer reason = new StringBuffer("Nothing of following commands found on your system : ");
+	    for(int i=0;i<paths.length;i++) {
+		reason.append(paths[i] + ";");
+	    }
+	    System.out.println("##########\n" + reason.toString() + "\n##########");
+	    return null;
+	}
+	return command;
+    }
+    public static String getNetscape6InPath() {
+	String netscapeWin = "netscp6.exe";
+	String netscapeUx = "netscape6";
+	if(System.getProperty("os.name").indexOf("Windows")!=-1) {
+	    return netscapeWin;
+        }else {
+	    return netscapeUx;
+	}
+    }
 }
 
