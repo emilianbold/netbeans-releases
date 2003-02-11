f8e6b14afaf216844a38b2f5e0e8687784d8897e
mroskanin <mroskanin@netbeans.org>
2003-02-11 15:02:39 +0000

    bugfix of #30930 No code completion database for examples

---
+++ editor/src/org/netbeans/modules/editor/EditorWarmUpTask.java
@@ -0,0 +1,72 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2000 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.modules.editor;
+
+import java.io.File;
+import java.util.Enumeration;
+import org.netbeans.editor.BaseKit;
+import org.netbeans.modules.editor.java.JCStorage;
+import org.netbeans.modules.editor.java.JavaKit;
+import org.netbeans.modules.editor.plain.PlainKit;
+import org.openide.filesystems.FileSystem;
+import org.openide.filesystems.Repository;
+
+/**
+ * "Warm-up" task for editor. Executed after IDE startup, it should
+ * pre-initialize some suitable parts of the module to improve first time usage
+ * experience - which might suffer from long response time due to class loading
+ * and various initialization.
+ * See {@link org.netbeans.core.AfterStartWarmUp} for details about how the task is run.
+ *
+ * @author  Tomas Pavek, Martin Roskanin
+ */
+
+public class EditorWarmUpTask implements Runnable{
+
+    private static final boolean debug
+        = Boolean.getBoolean("netbeans.debug.editor.warmup"); // NOI18N
+    
+    
+    private void sampleDirParsing(){
+        File userdir = new File(System.getProperty("netbeans.user", ""),"sampledir"); //NOI18N
+        String fsName = userdir.getAbsolutePath().replace('\\','/');
+        JCStorage.getStorage().parseFSOnBackground(Repository.getDefault().findFileSystem(fsName));
+    }
+    
+    public void run() {
+        long startTime = System.currentTimeMillis();
+        
+        JCStorage.getStorage();
+        if (debug){
+            System.out.println("storage initialized:"+(System.currentTimeMillis()-startTime));
+            startTime = System.currentTimeMillis();
+        }
+        
+        sampleDirParsing();
+        if (debug){
+            System.out.println("sample dir parsed:"+(System.currentTimeMillis()-startTime));
+            startTime = System.currentTimeMillis();
+        }
+
+/*
+        EditorModule.init();
+
+        BaseKit javaKit = BaseKit.getKit(JavaKit.class);
+        javaKit.getActions();
+        javaKit.createDefaultDocument();
+
+        BaseKit.getKit(PlainKit.class);
+ */
+    }
+}
