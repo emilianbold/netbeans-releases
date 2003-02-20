6cbabb6c1ea86c9e573b67385985d2f0f9f0b0be
pkuzel <pkuzel@netbeans.org>
2003-02-20 15:31:52 +0000

    #30806 part 2. Removed openide-deprecated.jar dependency.

--- xsl/src/org/netbeans/modules/xsl/ui/TransformPanel.java
+++ xsl/src/org/netbeans/modules/xsl/ui/TransformPanel.java
@@ -26,11 +26,12 @@ import java.util.Vector;
 
 import javax.xml.transform.*;
 
-import org.openide.Places;
 import org.openide.nodes.Node;
 import org.openide.nodes.NodeAcceptor;
 import org.openide.nodes.NodeOperation;
 import org.openide.loaders.DataObject;
+import org.openide.loaders.RepositoryNodeFactory;
+import org.openide.loaders.DataFilter;
 import org.openide.filesystems.*;
 import org.openide.util.UserCancelException;
 import org.openide.util.Utilities;
@@ -637,11 +638,11 @@ public final class TransformPanel extends javax.swing.JPanel {
     private void browseXSLTButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_browseXSLTButtonActionPerformed
         // Add your handling code here:
         try {
-            Places places = (Places) Lookup.getDefault().lookup(Places.class);
+
             Node[] nodes = NodeOperation.getDefault().select
             (Util.THIS.getString("LBL_select_xslt_script"),
             Util.THIS.getString("LBL_select_node"),
-            places.nodes().repository(),
+            RepositoryNodeFactory.getDefault().repository(DataFilter.ALL),
             new NodeAcceptor() {
                 public boolean acceptNodes(Node[] nodes) {
                     if ( nodes.length != 1 ) {
@@ -681,11 +682,10 @@ public final class TransformPanel extends javax.swing.JPanel {
     private void browseInputButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_browseInputButtonActionPerformed
         // Add your handling code here:
         try {
-            Places places = (Places) Lookup.getDefault().lookup(Places.class);
             Node[] nodes = NodeOperation.getDefault().select
             (Util.THIS.getString("LBL_select_xml_document"),
             Util.THIS.getString("LBL_select_node"),
-            places.nodes().repository(),
+            RepositoryNodeFactory.getDefault().repository(DataFilter.ALL),
             new NodeAcceptor() {
                 public boolean acceptNodes(Node[] nodes) {
                     if ( nodes.length != 1 ) {
