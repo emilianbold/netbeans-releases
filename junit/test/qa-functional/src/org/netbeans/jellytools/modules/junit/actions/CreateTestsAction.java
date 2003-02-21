5a85c73b5382898bcacbf87ec195bd7f2f701150
dkaspar <dkaspar@netbeans.org>
2003-02-21 12:43:06 +0000

    i18n, licence

--- junit/test/qa-functional/src/org/netbeans/jellytools/modules/junit/actions/CreateTestsAction.java
+++ junit/test/qa-functional/src/org/netbeans/jellytools/modules/junit/actions/CreateTestsAction.java
@@ -1,21 +1,31 @@
 /*
- * CreateTestsAction.java
- *
- * Created on 2/6/03 2:21 PM
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2000 Sun
+ * Microsystems, Inc. All Rights Reserved.
  */
 
 package org.netbeans.jellytools.modules.junit.actions;
 
 import org.netbeans.jellytools.actions.*;
 import java.awt.event.KeyEvent;
+import org.netbeans.jellytools.Bundle;
 
 /** CreateTestsAction Class
  * @author dave
  */
 public class CreateTestsAction extends Action {
 
+    public static final String MENU = Bundle.getString("org.netbeans.core.Bundle", "Actions/Tools") + "|" + Bundle.getString("org.netbeans.modules.junit.Bundle", "LBL_Action_Tests") + "|" + Bundle.getString("org.netbeans.modules.junit.Bundle", "LBL_Action_CreateTest");
+    
     /** creates new CreateTestsAction instance */
     public CreateTestsAction() {
-        super("Tools|JUnit Tests|Create Tests", "Tools|JUnit Tests|Create Tests", new Action.Shortcut(KeyEvent.CTRL_MASK|KeyEvent.ALT_MASK, KeyEvent.VK_J));
+        super(MENU, MENU, new Action.Shortcut(KeyEvent.CTRL_MASK|KeyEvent.ALT_MASK, KeyEvent.VK_J));
     }
 }
--- junit/test/qa-functional/src/org/netbeans/jellytools/modules/junit/actions/ExecuteTestAction.java
+++ junit/test/qa-functional/src/org/netbeans/jellytools/modules/junit/actions/ExecuteTestAction.java
@@ -1,21 +1,31 @@
 /*
- * ExecuteTestAction.java
- *
- * Created on 2/6/03 2:21 PM
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2000 Sun
+ * Microsystems, Inc. All Rights Reserved.
  */
 
 package org.netbeans.jellytools.modules.junit.actions;
 
 import org.netbeans.jellytools.actions.*;
 import java.awt.event.KeyEvent;
+import org.netbeans.jellytools.Bundle;
 
 /** ExecuteTestAction Class
  * @author dave
  */
 public class ExecuteTestAction extends Action {
 
+    public static final String MENU = Bundle.getString("org.netbeans.core.Bundle", "Actions/Tools") + "|" + Bundle.getString("org.netbeans.modules.junit.Bundle", "LBL_Action_Tests") + "|" + Bundle.getString("org.netbeans.modules.junit.Bundle", "LBL_Action_RunTest");
+
     /** creates new ExecuteTestAction instance */
     public ExecuteTestAction() {
-        super("Tools|JUnit Tests|Execute Test", "Tools|JUnit Tests|Execute Test", new Action.Shortcut(KeyEvent.CTRL_MASK|KeyEvent.ALT_MASK, KeyEvent.VK_L));
+        super(MENU, MENU, new Action.Shortcut(KeyEvent.CTRL_MASK|KeyEvent.ALT_MASK, KeyEvent.VK_L));
     }
 }
