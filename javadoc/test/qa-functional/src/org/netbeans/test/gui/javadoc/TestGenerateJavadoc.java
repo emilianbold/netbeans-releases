f8ddaaacba6433d6cca4d13513c33180c115c4c4
mkubec <mkubec@netbeans.org>
2003-03-05 09:07:27 +0000

    added licenses

--- javadoc/test/qa-functional/src/org/netbeans/test/gui/javadoc/TestGenerateJavadoc.java
+++ javadoc/test/qa-functional/src/org/netbeans/test/gui/javadoc/TestGenerateJavadoc.java
@@ -1,4 +1,17 @@
 /*
+ *                 Sun Public License Notice
+ *
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ *
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 2003 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+/*
  * TestGenerateJavadoc.java
  *
  * Created on February 3, 2003, 3:56 PM
