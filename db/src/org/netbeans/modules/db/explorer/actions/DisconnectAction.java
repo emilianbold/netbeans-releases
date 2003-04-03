38ba30ab58f65b4f2d5f3888dc2a6b19e03e5350
rnajman <rnajman@netbeans.org>
2003-04-03 09:27:18 +0000

    Disconnect action reimplemented to run in RequestProcessor.

--- db/src/org/netbeans/modules/db/explorer/actions/DisconnectAction.java
+++ db/src/org/netbeans/modules/db/explorer/actions/DisconnectAction.java
@@ -15,11 +15,14 @@ package org.netbeans.modules.db.explorer.actions;
 
 import java.text.MessageFormat;
 
-import org.openide.*;
-import org.openide.nodes.*;
+import org.openide.DialogDisplayer;
+import org.openide.NotifyDescriptor;
+import org.openide.nodes.Node;
+import org.openide.util.RequestProcessor;
 
-import org.netbeans.modules.db.explorer.nodes.*;
-import org.netbeans.modules.db.explorer.infos.*;
+import org.netbeans.modules.db.explorer.infos.ConnectionNodeInfo;
+import org.netbeans.modules.db.explorer.infos.DatabaseNodeInfo;
+import org.netbeans.modules.db.explorer.nodes.DatabaseNode;
 
 public class DisconnectAction extends DatabaseAction {
     static final long serialVersionUID =-5994051723289754485L;
@@ -49,16 +52,21 @@ public class DisconnectAction extends DatabaseAction {
         if (activatedNodes == null || activatedNodes.length == 0)
             return;
         
-        for (int i = 0; i < activatedNodes.length; i++) {
-            Node node = activatedNodes[i];
-            try {
-                DatabaseNodeInfo info = (DatabaseNodeInfo)node.getCookie(DatabaseNodeInfo.class);
-                ConnectionNodeInfo nfo = (ConnectionNodeInfo)info.getParent(DatabaseNode.CONNECTION);
-                nfo.disconnect();
-            } catch(Exception exc) {
-                String message = MessageFormat.format(bundle.getString("ERR_UnableToDisconnect"), new String[] {node.getName(), exc.getMessage()}); // NOI18N
-                DialogDisplayer.getDefault().notify(new NotifyDescriptor.Message(message, NotifyDescriptor.ERROR_MESSAGE));
+        final Node[] nodes = activatedNodes;
+        RequestProcessor.getDefault().post(new Runnable() {
+            public void run () {
+                for (int i = 0; i < nodes.length; i++) {
+                    Node node = nodes[i];
+                    try {
+                        DatabaseNodeInfo info = (DatabaseNodeInfo) node.getCookie(DatabaseNodeInfo.class);
+                        ConnectionNodeInfo nfo = (ConnectionNodeInfo) info.getParent(DatabaseNode.CONNECTION);
+                        nfo.disconnect();
+                    } catch(Exception exc) {
+                        String message = MessageFormat.format(bundle.getString("ERR_UnableToDisconnect"), new String[] {node.getName(), exc.getMessage()}); // NOI18N
+                        DialogDisplayer.getDefault().notify(new NotifyDescriptor.Message(message, NotifyDescriptor.ERROR_MESSAGE));
+                    }
+                }
             }
-        }
+        }, 0);
     }
 }
