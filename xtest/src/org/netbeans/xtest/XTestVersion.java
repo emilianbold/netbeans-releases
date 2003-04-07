a8efa0fbe2f6064a7cbc5c45b281cc3e329f6a2f
breh <breh@netbeans.org>
2003-04-07 13:02:29 +0000

    Solaris fix on versioning

--- xtest/src/org/netbeans/xtest/XTestVersion.java
+++ xtest/src/org/netbeans/xtest/XTestVersion.java
@@ -25,31 +25,21 @@ public class XTestVersion  extends Task {
 
     private String xtestHomeProperty;
     
-    private Manifest getManifest() {
-        JarInputStream jis = null;
-        FileInputStream fis = null;
-        //final String info="org/netbeans/xtest/version_info";         
+    private Manifest getManifest() {             
         try {
             File xtestHome = new File(xtestHomeProperty);
             File xtestJar = new File(xtestHome,"lib/xtest.jar");
-            fis = new FileInputStream(xtestJar);
-            jis = new JarInputStream(fis);
-            return jis.getManifest();
-            //return new Manifest(XTestVersion.class.getClassLoader().getResourceAsStream(info));            
+            JarFile xtestJarFile = new JarFile(xtestJar);
+            Manifest man = xtestJarFile.getManifest();
+            if (man == null) {
+                throw new MissingResourceException("Cannot find manifest in xtest.jar",null,null);
+            } else {
+                return man;
+            }
         } catch (IOException e) {
             e.printStackTrace(System.err);
             throw new MissingResourceException("Version info not available",null,null);
-        } finally {
-            if (jis != null) {
-                try {
-                    jis.close();
-                } catch (IOException ioe) {}
-            } else if (fis != null) {
-                try {
-                    fis.close();
-                } catch (IOException ioe) {}
-            }
-        }
+        } 
     }
     
     private void printoutAttributes(Attributes atts) {
@@ -81,7 +71,7 @@ public class XTestVersion  extends Task {
         String version = "unknown";
         try {
             version = getMajorVersion()+"."+getMinorVersion()+" "+getBranch();
-        } catch (MissingResourceException mre) {
+        } catch (Exception mre) {
             // cannot find resource --- unkonwn version
         }
         log("XTest version: "+version);        
