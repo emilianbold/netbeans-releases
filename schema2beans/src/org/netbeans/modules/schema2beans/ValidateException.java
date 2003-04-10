7912a50cc31892c076bb38918ad8f82c79b21dfc
cliffwd <cliffwd@netbeans.org>
2003-04-10 23:16:09 +0000

    Clean up the internal data structure that schema's get compiled into.  It's now much more of an AST (Abstract Syntax Tree). Added a feature to GraphLink to be able to calculate mutually exclusive properties.  This works across multiple nested '|'s.  Validations and setter reseting take advantage of this.  addValue for indexed property will also reset mutually exclusive properties if it's the first element being added. Added a validation failure type.  This should make it easier to deal with validation errors programmatically.

--- schema2beans/src/org/netbeans/modules/schema2beans/ValidateException.java
+++ schema2beans/src/org/netbeans/modules/schema2beans/ValidateException.java
@@ -16,14 +16,38 @@ package org.netbeans.modules.schema2beans;
 public class ValidateException extends Exception {
     protected Object failedBean;
     protected String failedPropertyName;
-    
+    protected FailureType failureType;
+
     public ValidateException(String msg, String failedPropertyName, Object failedBean) {
         super(msg);
         this.failedBean = failedBean;
         this.failedPropertyName = failedPropertyName;
     }
 
+    public ValidateException(String msg, FailureType ft,
+                             String failedPropertyName, Object failedBean) {
+        super(msg);
+        this.failureType = ft;
+        this.failedBean = failedBean;
+        this.failedPropertyName = failedPropertyName;
+    }
+
     public String getFailedPropertyName() {return failedPropertyName;}
     public Object getFailedBean() {return failedBean;}
+    public FailureType getFailureType() {return failureType;}
+
+    public static class FailureType {
+        private final String name;
+
+        private FailureType(String name) {this.name = name;}
+
+        public String toString() { return name;}
+
+        public static final FailureType NULL_VALUE = new FailureType("NULL_VALUE");
+        public static final FailureType DATA_RESTRICTION = new FailureType("DATA_RESTRICTION");
+        public static final FailureType ENUM_RESTRICTION = new FailureType("ENUM_RESTRICTION");
+        public static final FailureType MUTUALLY_EXCLUSIVE = new FailureType("MUTUALLY_EXCLUSIVE");
+    }
+    
 }
 
