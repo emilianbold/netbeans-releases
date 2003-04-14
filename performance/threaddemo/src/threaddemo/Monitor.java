047ad9168afdadd9d86b06c2f488c2d3faafe085
jglick <jglick@netbeans.org>
2003-04-14 16:17:04 +0000

    Made immediate heap updates optional.

--- performance/threaddemo/src/threaddemo/Monitor.java
+++ performance/threaddemo/src/threaddemo/Monitor.java
@@ -14,6 +14,7 @@
 package threaddemo;
 
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.DecimalFormat;
@@ -38,9 +39,11 @@ final class Monitor extends JPanel implements ActionListener {
     private final JTextField heapField;
     private static final NumberFormat format = new DecimalFormat("0.00");
     private int tick = 0;
+    private JCheckBox heapLiveCheckBox;
+    private boolean heapLive;
     
     public Monitor() {
-        setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+        setLayout(new GridLayout(3, 1));
         t1 = new javax.swing.Timer(DELAY_CPU, this);
         t1.setRepeats(true);
         t2 = new java.util.Timer(true);
@@ -50,20 +53,24 @@ final class Monitor extends JPanel implements ActionListener {
             }
         };
         last = System.currentTimeMillis();
-        add(new JLabel("Last blockage above " + GRACE + "msec:"));
+        JPanel sub = new JPanel();
+        sub.add(new JLabel("Last blockage above " + GRACE + "msec:"));
         blockageField = new JTextField(10);
         Font font = new Font("Monospaced", Font.PLAIN, blockageField.getFont().getSize());
         blockageField.setFont(font);
         blockageField.setEditable(false);
-        add(Box.createHorizontalStrut(5));
-        add(blockageField);
-        add(Box.createHorizontalStrut(10));
-        add(new JLabel("Heap:"));
-        add(Box.createHorizontalStrut(5));
+        sub.add(blockageField);
+        add(sub);
+        sub = new JPanel();
+        sub.add(new JLabel("Heap:"));
         heapField = new JTextField(10);
         heapField.setFont(font);
         heapField.setEditable(false);
-        add(heapField);
+        sub.add(heapField);
+        heapLiveCheckBox = new JCheckBox("Immediate Heap Updates");
+        sub.add(heapLiveCheckBox);
+        add(sub);
+        sub = new JPanel();
         JButton gc = new JButton("GC");
         gc.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ev) {
@@ -72,8 +79,8 @@ final class Monitor extends JPanel implements ActionListener {
                 System.gc();
             }
         });
-        add(Box.createHorizontalStrut(10));
-        add(gc);
+        sub.add(gc);
+        add(sub);
         t1.start();
         t2.schedule(task, 0L, DELAY_HEAP);
     }
@@ -88,11 +95,19 @@ final class Monitor extends JPanel implements ActionListener {
             blockageField.paintImmediately(0, 0, blockageField.getWidth(), blockageField.getHeight());
         }
         last = now;
+        // Also update heapLive since we are in AWT:
+        heapLive = heapLiveCheckBox.isSelected();
     }
     
     private void updateHeap() {
+        // setText is thread-safe:
         heapField.setText(format.format(heapUsedMega()) + "Mb");
-        heapField.paintImmediately(0, 0, heapField.getWidth(), heapField.getHeight());
+        if (heapLive) {
+            // Show live updates even when AWT is blocked.
+            // Note that this can sometimes cause weird painting problems
+            // in other windows, so off by default.
+            heapField.paintImmediately(0, 0, heapField.getWidth(), heapField.getHeight());
+        }
     }
     
     private static float heapUsedMega() {
