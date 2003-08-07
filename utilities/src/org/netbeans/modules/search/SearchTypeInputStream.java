ba895d6ad3d70473c82f3124f6983b7f99ab45d6
mpetras <mpetras@netbeans.org>
2003-08-07 07:38:18 +0000

    second version fix of bug #33883 - "Find in files"->"Restore Saved"->"Search"->Exception

--- utilities/src/org/netbeans/modules/search/SearchTypeInputStream.java
+++ utilities/src/org/netbeans/modules/search/SearchTypeInputStream.java
@@ -17,30 +17,88 @@ import java.io.IOException;
 import java.io.InputStream;
 
 /**
- *
+ * Object input stream with a customized class resolution.
+ * 
+ * @see  #resolveClass
  * @author  Marian Petras
  */
 class SearchTypeInputStream extends java.io.ObjectInputStream {
     
+    /**
+     * holds the resolved <code>SearchType</code> class so that its classloader
+     * may be used for resolution of other classes
+     *
+     * @see  #extClassLoader
+     * @see  #resolveExtClass
+     */
+    private Class extSearchType = null;
+    /**
+     * classloader of the resolved <code>SearchType</code>
+     *
+     * @see  #extSearchType
+     */
+    private ClassLoader extClassLoader = null;
+
     /** Creates a new instance of SearchTypeInputStream */
     public SearchTypeInputStream(InputStream in) throws IOException {
         super(in);
     }
     
     /**
+     * Loads the local class equivalent of the specified stream class
+     * description. Uses the default
+     * {@link ObjectInputStream ObjectInputStream}'s method first and
+     * if it fails, tries to lookup the class among the registered
+     * <code>SearchType</code>s or to use their classloaders.
      */
     protected Class resolveClass(java.io.ObjectStreamClass objectStreamClass)
             throws IOException, ClassNotFoundException {
         try {
             return super.resolveClass(objectStreamClass);
         } catch (ClassNotFoundException ex) {
-            String className = objectStreamClass.getName();
-            Class searchTypeClass = Utils.searchTypeForName(className);
-            if (searchTypeClass != null) {
-                return searchTypeClass;
+            Class extClass = resolveExtClass(objectStreamClass.getName());
+            if (extClass != null) {
+                return extClass;
             } else {
                 throw ex;
             }
+        }
+    }
+
+    /**
+     * Resolves the specified class.
+     * If this method is used for the first time (on this instance), it just
+     * tries to lookup the class among the registed <code>SearchType</code>s.
+     * Next time this method is called, it uses the <code>SearchType</code>'s
+     * classloader for loading classes.
+     * 
+     * @param  className  name of a class to be resolved
+     * @return  local class matching the specified class name
+     *          or <code>null</code> if no matching class was found
+     */
+    private Class resolveExtClass(final String className) {
+        if (extSearchType == null) {
+
+            /* first time class resolution must be for the ext. SearchType */
+            extSearchType = Utils.searchTypeForName(className);
+            return extSearchType;
+        }
+
+        /*
+         * We expect that the classloader that was used for loading
+         * the Search Type class is suitable also for other classes:
+         */
+        if (extClassLoader == null) {
+            try {
+                extClassLoader = extSearchType.getClassLoader();
+            } catch (SecurityException ex) {
+                return null;
+            }
+        }
+        try {
+            return extClassLoader.loadClass(className);
+        } catch (ClassNotFoundException ex) {
+            return null;
         }
     }
     
