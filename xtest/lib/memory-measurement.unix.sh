5e9ab1d8629f52f8eabd577e52467234c30f9350
breh <breh@netbeans.org>
2003-08-12 12:56:56 +0000

    linux fix

--- xtest/lib/memory-measurement.unix.sh
+++ xtest/lib/memory-measurement.unix.sh
@@ -6,7 +6,8 @@ then
 fi
 if [ "`uname`" = "Linux" ]
 then
-    cat /proc/$pid/status | grep VmSize | sed -e 's/VmSize: *\t* *//' | sed -e 's/ .*//'
+#    cat /proc/$pid/status | grep VmSize | sed -e 's/VmSize: *\t* *//' | sed -e 's/ .*//'
+     cat /proc/$pid/status | grep VmSize | awk '{print $2}'
 elif [ "`uname`" = "SunOS" ]
 then
     pmap -x $pid | grep \^total | sed -e 's/.*Kb *//' | sed -e 's/ .*//'
