1228c88325b1dfd2fa9c210aa32ae70159081e51
tzezula <tzezula@netbeans.org>
2003-11-12 17:15:59 +0000

    Added print to HTML action

---
+++ editor/src/org/netbeans/modules/editor/ExportHtmlAction.java
@@ -0,0 +1,304 @@
+/*
+ *                 Sun Public License Notice
+ *
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ *
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2000 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+package org.netbeans.modules.editor;
+
+import org.openide.util.actions.CookieAction;
+import org.openide.util.HelpCtx;
+import org.openide.util.NbBundle;
+import org.openide.nodes.Node;
+import org.openide.cookies.EditorCookie;
+import org.openide.loaders.DataObject;
+import org.openide.DialogDisplayer;
+import org.openide.DialogDescriptor;
+import org.openide.ErrorManager;
+import org.openide.NotifyDescriptor;
+import org.openide.filesystems.FileObject;
+import org.openide.awt.HtmlBrowser;
+import org.netbeans.editor.*;
+
+import javax.swing.text.StyledDocument;
+import javax.swing.text.Document;
+import javax.swing.text.JTextComponent;
+import javax.swing.*;
+import java.io.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.net.URL;
+import java.net.MalformedURLException;
+import java.text.MessageFormat;
+
+public class ExportHtmlAction extends CookieAction {
+
+    private static final String HTML_EXT = ".html";  //NOI18N
+    private static final String FILE_PROTOCOL = "file://"; //NOI18N
+
+    private Dialog dlg;
+
+    public ExportHtmlAction () {
+    }
+
+    protected final int mode() {
+        return CookieAction.MODE_EXACTLY_ONE;
+    }
+
+
+
+    protected final Class[] cookieClasses() {
+        return new Class[] {EditorCookie.class, DataObject.class};
+    }
+
+    protected final void performAction(Node[] activatedNodes) {
+        EditorCookie ec = (EditorCookie) activatedNodes[0].getCookie (EditorCookie.class);
+        StyledDocument doc = ec.getDocument();
+        if (doc instanceof BaseDocument) {
+            BaseDocument bdoc = (BaseDocument) doc;
+            JTextComponent jtc = Utilities.getLastActiveComponent();
+            Presenter p = new Presenter ();
+            p.setFileName (System.getProperty("user.home")+File.separatorChar+
+                    ((DataObject)bdoc.getProperty (Document.StreamDescriptionProperty)).getPrimaryFile().getName()+HTML_EXT);
+            p.setShowLines (((Boolean)SettingsUtil.getValue (bdoc.getKitClass(),SettingsNames.LINE_NUMBER_VISIBLE,
+                    Boolean.FALSE)).booleanValue());
+            p.setSelectionActive ((jtc != null && jtc.getSelectionStart()!=jtc.getSelectionEnd()));
+            DialogDescriptor dd = new DialogDescriptor (p, NbBundle.getMessage(ExportHtmlAction.class, "CTL_ExportHtml"));
+            dlg = DialogDisplayer.getDefault().createDialog (dd);
+            dlg.setVisible (true);
+            dlg.dispose();
+            dlg = null;
+            if (dd.getValue() == DialogDescriptor.OK_OPTION) {
+                String file = p.getFileName();
+                boolean lineNumbers = p.isShowLines();
+                boolean open = p.isOpenHtml();
+                boolean selection = p.isSelection();
+                int selectionStart;
+                int selectionEnd;
+                if (selection) {
+                    selectionStart = jtc.getSelectionStart();
+                    selectionEnd = jtc.getSelectionEnd();
+                }
+                else {
+                    selectionStart = 0;
+                    selectionEnd = bdoc.getLength();
+                }
+                try {
+                    export (bdoc, file, lineNumbers, selectionStart, selectionEnd);
+                } catch (IOException ioe) {
+                    NotifyDescriptor nd = new NotifyDescriptor.Message (
+                            MessageFormat.format (NbBundle.getMessage(ExportHtmlAction.class,"ERR_IOError"),
+                                new Object[]{((DataObject)bdoc.getProperty(Document.StreamDescriptionProperty)).getPrimaryFile().getNameExt()
+                                +HTML_EXT,file}),    //NOI18N
+                            NotifyDescriptor.ERROR_MESSAGE);
+                    DialogDisplayer.getDefault().notify (nd);
+                    return;
+                }
+                if (open) {
+                    try {
+                        HtmlBrowser.URLDisplayer.getDefault().showURL (new URL (FILE_PROTOCOL+file));
+                    } catch (MalformedURLException mue) {
+                        ErrorManager.getDefault().notify (mue);
+                    }
+                }
+            }
+        }
+    }
+
+    public final String getName() {
+        return NbBundle.getMessage (ExportHtmlAction.class, "CTL_ExportHtmlAction");
+    }
+
+    public final HelpCtx getHelpCtx() {
+        return HelpCtx.DEFAULT_HELP;
+    }
+
+    protected final boolean asynchronous() {
+        return false;
+    }
+
+    private void export (final BaseDocument bdoc,  String fileName, boolean lineNumbers, int selectionStart, int selectionEnd) throws IOException {
+        Coloring coloring =  SettingsUtil.getColoring (bdoc.getKitClass(), SettingsNames.DEFAULT_COLORING, false);
+        Color bgColor = coloring.getBackColor();
+        Color fgColor = coloring.getForeColor();
+        Font font = coloring.getFont();
+        FileObject fo = ((DataObject)bdoc.getProperty (Document.StreamDescriptionProperty)).getPrimaryFile();
+        HtmlPrintContainer htmlPrintContainer = new HtmlPrintContainer();
+        htmlPrintContainer.begin (fo, font, fgColor, bgColor);
+        bdoc.print (htmlPrintContainer,false, lineNumbers, selectionStart, selectionEnd);
+        String result = htmlPrintContainer.end();
+        PrintWriter out = null;
+        try {
+            out = new PrintWriter (new FileWriter (fileName));
+            out.print (result);
+        } finally {
+            if (out != null)
+                out.close();
+        }
+    }
+
+
+    private class Presenter extends JPanel {
+
+        private JTextField fileName;
+        private JCheckBox showLineNumbers;
+        private JCheckBox openHtml;
+        private JCheckBox selection;
+
+        public Presenter () {
+            this.initGUI ();
+        }
+
+        public final String getFileName () {
+            return this.fileName.getText();
+        }
+
+        public final void setFileName (String name) {
+            this.fileName.setText (name);
+        }
+
+        public final boolean isShowLines () {
+            return this.showLineNumbers.isSelected();
+        }
+
+        public final void setShowLines (boolean value) {
+            this.showLineNumbers.setSelected (value);
+        }
+
+        public final boolean isSelection () {
+            return this.selection.isSelected();
+        }
+
+        public final boolean isOpenHtml () {
+            return this.openHtml.isSelected();
+        }
+
+        public final void setSelectionActive (boolean value) {
+            this.selection.setEnabled (value);
+        }
+
+        private void initGUI () {
+            this.setLayout ( new GridBagLayout ());
+            JLabel label = new JLabel (NbBundle.getMessage (ExportHtmlAction.class, "CTL_OutputDir"));
+            label.setDisplayedMnemonic (NbBundle.getMessage (ExportHtmlAction.class, "MNE_OutputDir").charAt(0));
+            label.getAccessibleContext().setAccessibleName(NbBundle.getMessage (ExportHtmlAction.class, "AN_OutputDir"));
+            label.getAccessibleContext().setAccessibleDescription(NbBundle.getMessage (ExportHtmlAction.class, "AD_OutputDir"));
+            GridBagConstraints c = new GridBagConstraints ();
+            c.gridx = 0;
+            c.gridy = 0;
+            c.gridwidth = 1;
+            c.gridheight = 1;
+            c.anchor = GridBagConstraints.WEST;
+            c.insets = new Insets (12,12,6,6);
+            ((GridBagLayout)this.getLayout()).setConstraints (label, c);
+            this.add (label);
+            fileName = new JTextField ();
+            fileName.setColumns (25);
+            c = new GridBagConstraints ();
+            c.gridx = 1;
+            c.gridy = 0;
+            c.gridwidth = 1;
+            c.gridheight = 1;
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.anchor = GridBagConstraints.WEST;
+            c.insets = new Insets (12,6,6,6);
+            c.weightx = 1.0;
+            ((GridBagLayout)this.getLayout()).setConstraints (fileName, c);
+            this.add (this.fileName);
+            label.setLabelFor (this.fileName);
+            JButton button = new JButton (NbBundle.getMessage(ExportHtmlAction.class,"CTL_Select"));
+            button.setMnemonic (NbBundle.getMessage (ExportHtmlAction.class, "MNE_Select").charAt(0));
+            button.getAccessibleContext().setAccessibleName(NbBundle.getMessage(ExportHtmlAction.class,"AN_Select"));
+            button.getAccessibleContext().setAccessibleDescription (NbBundle.getMessage(ExportHtmlAction.class,"AD_Select"));
+            button.addActionListener (new ActionListener () {
+                public void actionPerformed(ActionEvent e) {
+                    selectFile ();
+                }
+            });
+            c = new GridBagConstraints ();
+            c.gridx = 2;
+            c.gridy = 0;
+            c.gridwidth = 1;
+            c.gridheight = 1;
+            c.anchor = GridBagConstraints.WEST;
+            c.insets = new Insets (12,6,6,12);
+            ((GridBagLayout)this.getLayout()).setConstraints (button,c);
+            this.add (button);
+            selection = new JCheckBox (NbBundle.getMessage(ExportHtmlAction.class, "CTL_Selection"));
+            selection.setMnemonic(NbBundle.getMessage(ExportHtmlAction.class,"MNE_Selection").charAt(0));
+            selection.getAccessibleContext().setAccessibleName(NbBundle.getMessage(ExportHtmlAction.class,"AN_Selection"));
+            selection.getAccessibleContext().setAccessibleDescription(NbBundle.getMessage(ExportHtmlAction.class,"AD_Selection"));
+            c = new GridBagConstraints ();
+            c.gridx = 1;
+            c.gridy = 1;
+            c.gridwidth = GridBagConstraints.REMAINDER;
+            c.gridheight = 1;
+            c.anchor = GridBagConstraints.WEST;
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.insets = new Insets (6,6,6,12);
+            c.weightx = 1.0;
+            ((GridBagLayout)this.getLayout()).setConstraints (this.selection,c);
+            this.add (this.selection);
+            showLineNumbers = new JCheckBox (NbBundle.getMessage(ExportHtmlAction.class,"CTL_ShowLineNumbers"));
+            showLineNumbers.setMnemonic(NbBundle.getMessage(ExportHtmlAction.class,"MNE_ShowLineNumbers").charAt(0));
+            showLineNumbers.getAccessibleContext().setAccessibleName(NbBundle.getMessage(ExportHtmlAction.class,"AN_ShowLineNumbers"));
+            showLineNumbers.getAccessibleContext().setAccessibleDescription(NbBundle.getMessage(ExportHtmlAction.class,"AD_ShowLineNumbers"));
+            c = new GridBagConstraints();
+            c.gridx = 1;
+            c.gridy = 2;
+            c.gridwidth = GridBagConstraints.REMAINDER;
+            c.gridheight = 1;
+            c.anchor = GridBagConstraints.WEST;
+            c.fill   = GridBagConstraints.HORIZONTAL;
+            c.insets = new Insets (6,6,6,12);
+            c.weightx = 1.0;
+            ((GridBagLayout)this.getLayout()).setConstraints (this.showLineNumbers,c);
+            this.add (this.showLineNumbers);
+            openHtml = new JCheckBox (NbBundle.getMessage(ExportHtmlAction.class,"CTL_OpenHTML"));
+            openHtml.setMnemonic(NbBundle.getMessage(ExportHtmlAction.class,"MNE_OpenHTML").charAt(0));
+            openHtml.getAccessibleContext().setAccessibleName(NbBundle.getMessage(ExportHtmlAction.class,"AN_OpenHTML"));
+            openHtml.getAccessibleContext().setAccessibleDescription(NbBundle.getMessage(ExportHtmlAction.class,"AD_OpenHTML"));
+            c = new GridBagConstraints ();
+            c.gridx = 1;
+            c.gridy = 3;
+            c.gridwidth = GridBagConstraints.REMAINDER;
+            c.gridheight = 1;
+            c.anchor = GridBagConstraints.WEST;
+            c.fill = GridBagConstraints.HORIZONTAL;
+            c.insets = new Insets (6,6,12,12);
+            c.weightx = 1.0;
+            ((GridBagLayout)this.getLayout()).setConstraints (this.openHtml,c);
+            this.add (this.openHtml);
+        }
+
+
+        private void selectFile () {
+            JFileChooser chooser = new JFileChooser();
+            chooser.setFileFilter (new javax.swing.filechooser.FileFilter () {
+                public boolean accept(File f) {
+                    if (f.isFile() && f.getName().endsWith (HTML_EXT)) {
+                        return true;
+                    }
+                    else
+                      return false;
+                }
+
+                public String getDescription() {
+                    return NbBundle.getMessage (ExportHtmlAction.class, "TXT_HTMLFileType");
+                }
+            });
+            chooser.setSelectedFile (new File (this.fileName.getText()));
+            if (chooser.showOpenDialog (dlg) == JFileChooser.APPROVE_OPTION) {
+                this.fileName.setText (chooser.getSelectedFile().getAbsolutePath());
+            }
+        }
+    }
+
+}
---
+++ editor/src/org/netbeans/modules/editor/HtmlPrintContainer.java
@@ -0,0 +1,332 @@
+/*
+ *                 Sun Public License Notice
+ *
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ *
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2000 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+package org.netbeans.modules.editor;
+
+import org.netbeans.editor.PrintContainer;
+import org.openide.filesystems.FileObject;
+
+import java.awt.*;
+import java.text.MessageFormat;
+import java.util.*;
+
+public class HtmlPrintContainer implements PrintContainer {
+
+    private static final String T_HTML_S = "<HTML>";    //NOI18N
+    private static final String T_HTML_E = "</HTML>";   //NOI18N
+    private static final String T_HEAD_S = "<HEAD>";    //NOI18N
+    private static final String T_HEAD_E = "</HEAD>";   //NOI18N
+    private static final String T_BODY_S = "<BODY>";    //NOI18N
+    private static final String T_BODY_E = "</BODY>";   //NOI18N
+    private static final String T_TITLE = "<TITLE>{0}</TITLE>";    //NOI18N
+    private static final String T_PRE_S = "<PRE>";   //NOI18N
+    private static final String T_PRE_E = "</PRE>";  //NOI18N
+    private static final String T_BLOCK_S = "<FONT CLASS=\"{0}\">";  //NOI18N
+    private static final String T_BLOCK_E = "</FONT>";   //NOI18N
+    private static final String T_NAME_TABLE = "<TABLE BGCOLOR=\"#999999\" WIDTH=\"100%\"><TR><TD ALIGN=\"center\">{0}</TD></TR></TABLE>";    //NOI18N
+    private static final String T_STYLE_S = "<STYLE TYPE=\"text/css\">";    //NOI18N
+    private static final String T_STYLE_E = "</STYLE>"; //NOI18N
+    private static final String T_COMMENT_S = "<!--";   //NOI18N
+    private static final String T_COMMENT_E = "-->";    //NOI18N
+    private static final String ST_BODY = "BODY";       //NOI18N
+    private static final String ST_BEGIN = "{";        //NOI18N
+    private static final String ST_COLOR = "color: "; //NOI18N
+    private static final String ST_BGCOLOR = "background-color: ";    //NOI18N
+    private static final String ST_BOLD = "font-weight: bold";    //NOI18N
+    private static final String ST_ITALIC = "font-style: italic"; //NOI18N
+    private static final String ST_FONT_FAMILY = "font-family: ";    //NOI18N
+    private static final String ST_SEPARATOR = "; ";    //NOI18N
+    private static final String ST_END = "}";           //NOI18N
+    private static final String EOL = "\n";             //NOI18N
+    private static final String WS = " ";               //NOI18N
+    private static final String ESC_LT = "&lt;";        //NOI18N
+    private static final String ESC_GT = "&gt;";        //NOI18N
+    private static final String ESC_AMP = "&amp;";      //NOI18N
+    private static final String ESC_QUOT = "&quot;";    //NOI18N
+    private static final char   ZERO    = '0';          //NOI18N
+    private static final char   DOT = '.';              //NOI18N
+    private static final String STYLE_PREFIX = "ST";    //NOI18N
+
+    private Color defaultBackgroundColor;
+    private Color defaultForegroundColor;
+    private Font defaultFont;
+    private StringBuffer buffer;
+    private String fileName;
+    private Styles styles;
+    private boolean[] boolHolder;
+
+    public HtmlPrintContainer () {
+    }
+
+    public final void begin (FileObject fo, Font font, Color fgColor, Color bgColor) {
+        styles = new Styles ();
+        buffer = new StringBuffer();
+        fileName = fo.getPath();
+        boolHolder = new boolean [1];
+        this.defaultForegroundColor = fgColor;
+        this.defaultBackgroundColor = bgColor;
+        this.defaultFont = font;
+    }
+
+    public final void add(char[] chars, Font font, Color foreColor, Color backColor) {
+        String text = escape(chars, boolHolder);
+        String styleId = this.styles.getStyleId (font, foreColor, backColor);
+        boolHolder[0]&= (styleId!=null);
+        if (boolHolder[0]) {
+            buffer.append(MessageFormat.format(T_BLOCK_S,new Object[]{styleId}));
+        }
+        buffer.append (text);
+        if (boolHolder[0]) {
+            buffer.append(T_BLOCK_E);
+        }
+    }
+
+    public final void eol() {
+        buffer.append (EOL);
+    }
+
+    public final String end () {
+        StringBuffer result = new StringBuffer ();
+        result.append (T_HTML_S);
+        result.append (EOL);
+        result.append (T_HEAD_S);
+        result.append (EOL);
+        result.append (MessageFormat.format (T_TITLE, new Object[] {this.fileName}));
+        result.append (EOL);
+        result.append (T_STYLE_S);
+        result.append (EOL);
+        result.append (T_COMMENT_S);
+        result.append (EOL);
+        result.append (createStyle(ST_BODY,null,getDefaultFont(),getDefaultColor(),getDefaultBackgroundColor(),false));
+        result.append (EOL);
+        result.append (styles.toExternalForm());
+        result.append (T_COMMENT_E);
+        result.append (EOL);
+        result.append (T_STYLE_E);
+        result.append (EOL);
+        result.append (T_HEAD_E);
+        result.append (EOL);
+        result.append (T_BODY_S); //NOI18N
+        result.append (EOL);
+        result.append (MessageFormat.format (T_NAME_TABLE, new Object[] {this.fileName}));
+        result.append (EOL);
+        result.append (T_PRE_S);
+        result.append (EOL);
+        result.append (this.buffer);
+        result.append (T_PRE_E);
+        result.append (T_BODY_E);
+        result.append (EOL);
+        result.append (T_HTML_E);
+        result.append (EOL);
+        this.styles = null;
+        this.buffer = null;
+        this.fileName = null;
+        this.defaultBackgroundColor = null;
+        this.defaultForegroundColor = null;
+        this.defaultFont = null;
+        return result.toString();
+    }
+
+    public final boolean initEmptyLines() {
+        return false;
+    }
+
+    private String escape (char[] buffer, boolean[] boolHolder) {
+        StringBuffer result = new StringBuffer();
+        boolHolder[0] = false;
+        for (int i = 0; i < buffer.length; i++) {
+            if (buffer[i] == '<') {         //NOI18N
+                result.append(ESC_LT);
+                boolHolder[0]|=true;
+            }
+            else if (buffer[i] == '>') {    //NOI18N
+                result.append(ESC_GT);
+                boolHolder[0]|=true;
+            }
+            else if (buffer[i] =='&') {     //NOI18N
+                result.append(ESC_AMP);
+                boolHolder[0]|=true;
+            }
+            else if (buffer[i] =='\'') {    //NOI18N
+                result.append(ESC_QUOT);
+                boolHolder[0]|=true;
+            }
+            else if (Character.isWhitespace(buffer[i])) {
+                result.append (buffer[i]);
+            }
+            else {
+                result.append (buffer[i]);
+                boolHolder[0]|=true;
+            }
+        }
+        return result.toString();
+    }
+
+    private Color getDefaultColor () {
+        return this.defaultForegroundColor;
+    }
+
+    private Color getDefaultBackgroundColor () {
+        return this.defaultBackgroundColor;
+    }
+
+    private Font getDefaultFont () {
+        return this.defaultFont;
+    }
+
+    private String createStyle (String element, String selector, Font font, Color fg, Color bg, boolean useDefaults) {
+        StringBuffer sb = new StringBuffer();
+        if (element != null) {
+            sb.append (element);
+            sb.append (WS);
+        }
+
+        if (selector != null) {
+            sb.append (DOT);
+            sb.append (selector);
+            sb.append (WS);
+        }
+
+        sb.append (ST_BEGIN);
+        boolean first = true;
+        if (!useDefaults || !fg.equals(getDefaultColor())) {
+            sb.append (ST_COLOR);
+            sb.append (getHtmlColor(fg));
+            first = false;
+        }
+
+        if (!useDefaults || !bg.equals (getDefaultBackgroundColor())) {
+            if (!first) {
+                sb.append (ST_SEPARATOR);
+            }
+            sb.append (ST_BGCOLOR);
+            sb.append (getHtmlColor(bg));
+            first = false;
+        }
+
+        if (!useDefaults || !font.equals (getDefaultFont())) {
+            if (!first) {
+                sb.append (ST_SEPARATOR);
+            }
+            sb.append (ST_FONT_FAMILY);
+            sb.append (font.getFamily());   //TODO: Locale should go here
+            if (font.isBold()) {
+                sb.append (ST_SEPARATOR);
+                sb.append (ST_BOLD);
+            }
+            if (font.isItalic()) {
+                sb.append (ST_SEPARATOR);
+                sb.append (ST_ITALIC);
+            }
+        }
+        sb.append (ST_END);
+        return sb.toString();
+    }
+
+    private static String getHtmlColor (Color c) {
+        int r = c.getRed();
+        int g = c.getGreen();
+        int b = c.getBlue();
+        StringBuffer result = new StringBuffer();
+        result.append ("#");        //NOI18N
+        String rs = Integer.toHexString (r);
+        String gs = Integer.toHexString (g);
+        String bs = Integer.toHexString (b);
+        if (r < 0x10)
+            result.append(ZERO);
+        result.append(rs);
+        if (g < 0x10)
+            result.append (ZERO);
+        result.append(gs);
+        if (b < 0x10)
+            result.append (ZERO);
+        result.append(bs);
+        return result.toString();
+    }
+
+    private class Styles {
+        private Map descs;
+        private int sequence;
+
+        public Styles () {
+            this.descs = new HashMap();
+        }
+
+        public final String getStyleId (Font f, Color fc, Color bc) {
+            if (!fc.equals(getDefaultColor()) || !bc.equals(getDefaultBackgroundColor())) {
+                StyleDescriptor sd = new StyleDescriptor (f, fc, bc);
+                String id = (String) this.descs.get (sd);
+                if (id == null) {
+                    id = STYLE_PREFIX + this.sequence++;
+                    sd.name = id;
+                    this.descs.put (sd, id);
+                }
+                return id;
+            }
+            else {
+                return null;   //No style needed
+            }
+        }
+
+        public final String toExternalForm () {
+            StringBuffer result = new StringBuffer();
+            for (Iterator it = descs.keySet().iterator(); it.hasNext();) {
+                result.append (((StyleDescriptor)it.next()).toExternalForm());
+                result.append (EOL);
+            }
+            return result.toString();
+        }
+
+        public final String toString () {
+            return this.toExternalForm();
+        }
+
+        private class StyleDescriptor {
+
+            String name;
+            private Font font;
+            private Color fgColor;
+            private Color bgColor;
+
+            public StyleDescriptor (Font font, Color fgColor, Color bgColor) {
+                this.font = font;
+                this.fgColor = fgColor;
+                this.bgColor = bgColor;
+            }
+
+            public final String getName () {
+                return this.name;
+            }
+
+            public final String toExternalForm () {
+                return createStyle (null,name,font,fgColor,bgColor,true);
+            }
+
+            public final String toString () {
+                return this.toExternalForm();
+            }
+
+            public final boolean equals (Object object) {
+                if (!(object instanceof StyleDescriptor))
+                    return false;
+                StyleDescriptor od = (StyleDescriptor) object;
+                return font == null ? od.font == null : font.equals (od.font) &&
+                        fgColor == null ? od.fgColor == null : fgColor.equals (od.fgColor) &&
+                        bgColor == null ? od.bgColor == null : bgColor.equals (od.bgColor);
+            }
+
+            public final int hashCode () {
+                return this.fgColor.hashCode();
+            }
+        }
+    }
+}
