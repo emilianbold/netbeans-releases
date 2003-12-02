13abbd9e580b2bf8b7cad887a442e40765a2f9e8
jrechtacek <jrechtacek@netbeans.org>
2003-12-02 17:38:41 +0000

    rewrited test, it doesn't use robot more

--- openide/test/unit/src/org/openide/explorer/view/DefaultActionTest.java
+++ openide/test/unit/src/org/openide/explorer/view/DefaultActionTest.java
@@ -18,6 +18,7 @@ package org.openide.explorer.view;
 
 import java.awt.BorderLayout;
 import java.awt.Robot;
+import java.awt.event.ActionEvent;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
@@ -26,6 +27,7 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.swing.Action;
 import javax.swing.JScrollPane;
+import javax.swing.SwingUtilities;
 
 
 import junit.textui.TestRunner;
@@ -57,6 +59,23 @@ public class DefaultActionTest extends NbTestCase {
          TestRunner.run (new NbTestSuite (DefaultActionTest.class));
     }
     
+    /** Run all tests in AWT thread */
+    public final void run (final junit.framework.TestResult result) {
+        try {
+            // XXX ExplorerManager when updating selected nodes
+            // replanes all firing into AWT thread, therefore the test
+            // has to run in AWT.
+            javax.swing.SwingUtilities.invokeAndWait (new Runnable () {
+                public void run () {
+                    DefaultActionTest.super.run (result);
+                }
+            });
+        } catch (Exception ex) {
+            ex.printStackTrace ();
+            throw new IllegalStateException ();
+        }
+    }
+    
     private boolean performed;
     private Node root;
     private List fails = new ArrayList ();
@@ -64,37 +83,34 @@ public class DefaultActionTest extends NbTestCase {
     protected void setUp () {
         final Children children = new Children.Array ();
         root = new AbstractNode (children);
+        final NodeAction a = new NodeAction () {
+            protected void performAction (Node[] activatedNodes) {
+                log ("Action performed.");
+                performed = true;
+            }                 
+            
+            public boolean asynchronous () {
+                return false;
+            }
+
+            protected boolean enable (Node[] activatedNodes) {
+                return true;
+            }
+            
+            
+            public HelpCtx getHelpCtx () {
+                return null;
+            }
+            
+            public String getName () {
+                return "Test default action";
+            }
+        };
         children.add (new Node[] {new AbstractNode (Children.LEAF) {
             public String getName () {
                 return "Node with default action";
             }
             public Action getPreferredAction () {
-                NodeAction a = new NodeAction () {
-                    protected void performAction (Node[] activatedNodes) {
-                        if (activatedNodes != null && activatedNodes.length > 0) {
-                            log ("Action performed.");
-                            // commented out because this assert interups this method. Why?
-                            //assertTrue ("Default action performed twice.", performed);
-                            performed = true;
-                        } else {
-                            assertTrue ("Action performed on node.", true);
-                        }
-                    }
-                    protected boolean enable (Node[] activatedNodes) {
-                        return true;
-                    }
-                    
-                    
-                    public HelpCtx getHelpCtx () {
-                        return null;
-                    }
-                    
-                    public String getName () {
-                        return "Test default action";
-                    }
-                };
-                
-                
                 return a;
             }
         }});
@@ -105,16 +121,7 @@ public class DefaultActionTest extends NbTestCase {
         p.setSize (200, 200);
         p.add (view, BorderLayout.CENTER);
         p.getExplorerManager ().setRootContext (root);
-        p.open ();
-        
-        // wait for component will be showing
-        p.requestActive ();
-        try {
-            Thread.sleep (300);
-        } catch (Exception x) {
-            fail (x.getMessage ());
-        }
-        
+
         try {
             p.getExplorerManager ().setSelectedNodes (root.getChildren().getNodes ());
         } catch (PropertyVetoException pve) {
@@ -123,73 +130,52 @@ public class DefaultActionTest extends NbTestCase {
         
         return p;
     }
-    
-    private void invokeDefaultAction (TopComponent tc) {
+
+    private void invokeDefaultAction (final TopComponent tc) {
         performed = false;
         try {
-            Robot robot = new Robot ();
+            Node[] nodes = tc.getActivatedNodes ();
+            assertNotNull ("View has the active nodes.", nodes);
+            Node n = nodes.length > 0 ? nodes[0] : null;
+            assertNotNull ("View has a active node.", n);
             
-            robot.mouseMove (tc.getLocationOnScreen ().x + 100, tc.getLocationOnScreen ().y + 100);
-            //robot.mouseMove (100, 100);
-            robot.mousePress (InputEvent.BUTTON1_MASK);
-            robot.mouseRelease (InputEvent.BUTTON1_MASK);
+            final Action action = n.getPreferredAction ();
+            action.actionPerformed (new ActionEvent (n, ActionEvent.ACTION_PERFORMED, ""));
             
-            // wait to mouse event will be propagated
-            Thread.sleep (300);
-            robot.keyPress (KeyEvent.VK_ENTER);
-            robot.keyRelease (KeyEvent.VK_ENTER);
-            
-            // wait to key event will be propagated
+            // wait to invoke action is propagated
             Thread.sleep (300);
         } catch (Exception x) {
             fail (x.getMessage ());
         }
     }
     
-    public void testViews () {
-        doTestListView ();
-        doTestBeanTreeView ();
-        // default action in TTV failed - should be filed defect 
-        //doTestTreeTableView ();
-        doTestTreeContextView ();
-    }
-    
-    public void doTestListView () {
+    public void testListView () {
         TopComponent tc = prepareExplorerPanel (new ListView ());
         invokeDefaultAction (tc);
         assertDefaultActionWasPerformed ("ListView");
         tc.close ();
     }
     
-    public void doTestBeanTreeView () {
+    public void testBeanTreeView () {
         TopComponent tc = prepareExplorerPanel (new BeanTreeView ());
         invokeDefaultAction (tc);
         assertDefaultActionWasPerformed ("BeanTreeView");
     }
     
-    public void doTestTreeTableView () {
+    public void testTreeTableView () {
         TopComponent tc = prepareExplorerPanel (new TreeTableView ());
         invokeDefaultAction (tc);
         assertDefaultActionWasPerformed ("TreeTableView");
     }
     
-    public void doTestTreeContextView () {
+    public void testTreeContextView () {
         TopComponent tc = prepareExplorerPanel (new ContextTreeView ());
         invokeDefaultAction (tc);
         assertDefaultActionWasPerformed ("ContextTreeView");
     }
     
     void assertDefaultActionWasPerformed (String nameOfView) {
-        if (performed) {
-            log ("[" + nameOfView + "] DefaultAction was preformed.");
-        } else {
-            fails.add ("[" + nameOfView + "] DefaultAction was preformed.");
-        };
+        assertTrue ("[" + nameOfView + "] DefaultAction was preformed.", performed);
     }
     
-    void assertFails () {
-        if (!fails.isEmpty()) {
-            fail ((String)fails.get (0));
-        }
-    }
 }
