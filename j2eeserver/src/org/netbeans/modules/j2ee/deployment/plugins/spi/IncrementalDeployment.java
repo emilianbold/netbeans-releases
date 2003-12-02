539aa1a46e5bd767bb4fdcc99fc4618ef61b6e05
nnguyen <nnguyen@netbeans.org>
2003-12-02 23:26:54 +0000

    IN=18199 add methods for initial incremental deployment

--- j2eeserver/src/org/netbeans/modules/j2ee/deployment/plugins/spi/IncrementalDeployment.java
+++ j2eeserver/src/org/netbeans/modules/j2ee/deployment/plugins/spi/IncrementalDeployment.java
@@ -7,8 +7,10 @@
 package org.netbeans.modules.j2ee.deployment.plugins.spi;
 
 import org.netbeans.modules.j2ee.deployment.plugins.api.AppChangeDescriptor;
+import javax.enterprise.deploy.spi.Target;
 import javax.enterprise.deploy.spi.TargetModuleID;
 import javax.enterprise.deploy.spi.status.ProgressObject;
+import javax.enterprise.deploy.model.DeployableObject;
 
 import java.io.File;
 
@@ -18,12 +20,19 @@ import java.io.File;
  * @author  George Finklang
  */
 public interface IncrementalDeployment extends DeploymentManagerWrapper {
-    
+
+    /**
+     * First time deployment file distribution
+     * @param target target of deployment
+     * @param app the app to deploy
+     * @param dir the destination directory for the given deploy app
+     * @return the object for feedback on progress of deployment
+     */ 
+    public ProgressObject initialDeploy(Target target, DeployableObject app, File dir);
+
     /* Before this method is called, the on-disk representation of TargetModuleID
      * is updated, and AppChangeDescriptor describes what in the application changed. 
      **/
     public ProgressObject incrementalDeploy(TargetModuleID module, AppChangeDescriptor changes);
-    
-    // public ProgressObject initialDeploy(DeployableObject app, File dir);
     
 }
