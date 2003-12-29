2369a1790667efa225c4b7dcbd71505579bfb8a7
jglick <jglick@netbeans.org>
2003-12-29 02:59:11 +0000

    Working on testing property completion.

--- ant.grammar/test/unit/src/org/netbeans/modules/ant/grammar/AntGrammarTest.java
+++ ant.grammar/test/unit/src/org/netbeans/modules/ant/grammar/AntGrammarTest.java
@@ -70,40 +70,34 @@ public class AntGrammarTest extends NbTestCase {
     
     public void testTaskCompletion() throws Exception {
         String p = "<project default='x'><target name='x'><ecHERE/></target></project>";
-        HintContext c = TestUtil.createCompletion(p);
-        List l = TestUtil.grammarResultValues(g.queryElements(c));
+        List l = TestUtil.grammarResultValues(g.queryElements(TestUtil.createCompletion(p)));
         assertTrue("matched <echo>", l.contains("echo"));
         // XXX more...
     }
     
     public void testTypeCompletion() throws Exception {
         String p = "<project default='x'><target name='x'><paHERE/></target></project>";
-        HintContext c = TestUtil.createCompletion(p);
-        List l = TestUtil.grammarResultValues(g.queryElements(c));
+        List l = TestUtil.grammarResultValues(g.queryElements(TestUtil.createCompletion(p)));
         assertTrue("matched <path>", l.contains("path"));
         p = "<project default='x'><filHERE/><target name='x'/></project>";
-        c = TestUtil.createCompletion(p);
-        l = TestUtil.grammarResultValues(g.queryElements(c));
+        l = TestUtil.grammarResultValues(g.queryElements(TestUtil.createCompletion(p)));
         assertTrue("matched <fileset>", l.contains("fileset"));
         // XXX more...
     }
     
     public void testRegularAttrCompletion() throws Exception {
         String p = "<project default='x'><target name='x'><javac srcdHERE=''/></target></project>";
-        HintContext c = TestUtil.createCompletion(p);
-        List l = TestUtil.grammarResultValues(g.queryAttributes(c));
+        List l = TestUtil.grammarResultValues(g.queryAttributes(TestUtil.createCompletion(p)));
         assertTrue("matched srcdir on <javac>: " + l, l.contains("srcdir"));
         // XXX more...
     }
     
     public void testSpecialAttrCompletion() throws Exception {
         String p = "<project default='x'><target nHERE=''/></project>";
-        HintContext c = TestUtil.createCompletion(p);
-        List l = TestUtil.grammarResultValues(g.queryAttributes(c));
+        List l = TestUtil.grammarResultValues(g.queryAttributes(TestUtil.createCompletion(p)));
         assertEquals("matched name on <target>", l, Collections.singletonList("name"));
         p = "<project default='x'><target dHERE=''/></project>";
-        c = TestUtil.createCompletion(p);
-        l = TestUtil.grammarResultValues(g.queryAttributes(c));
+        l = TestUtil.grammarResultValues(g.queryAttributes(TestUtil.createCompletion(p)));
         Collections.sort(l);
         assertEquals("matched depends and description on <target>", l,
             Arrays.asList(new String[] {"depends", "description"}));
@@ -112,20 +106,86 @@ public class AntGrammarTest extends NbTestCase {
     
     public void testEnumeratedValueCompletion() throws Exception {
         String p = "<project default='x'><target><echo level='vHERE'/></target></project>";
-        HintContext c = TestUtil.createCompletion(p);
-        List l = TestUtil.grammarResultValues(g.queryValues(c));
+        List l = TestUtil.grammarResultValues(g.queryValues(TestUtil.createCompletion(p)));
         assertEquals("matched level='verbose' on <echo>", l, Collections.singletonList("verbose"));
     }
     
     public void testBooleanValueCompletion() throws Exception {
         String p = "<project default='x'><target><echo append='HERE'/></target></project>";
-        HintContext c = TestUtil.createCompletion(p);
-        List l = TestUtil.grammarResultValues(g.queryValues(c));
+        List l = TestUtil.grammarResultValues(g.queryValues(TestUtil.createCompletion(p)));
         Collections.sort(l);
         assertEquals("true or false for append on <echo>", l,
             Arrays.asList(new String[] {"false", "true"}));
     }
     
-    // XXX need tests for property completion... fairly complex
+    public void testStockProperties() throws Exception {
+        String p = "<project default='x'><target><echo message='${HERE'/></target></project>";
+        List l = TestUtil.grammarResultValues(g.queryValues(TestUtil.createCompletion(p)));
+        assertTrue("matched ${ant.home}: " + l, l.contains("${ant.home}"));
+        assertTrue("matched ${basedir}: " + l, l.contains("${basedir}"));
+        assertTrue("matched ${java.home}: " + l, l.contains("${java.home}"));
+    }
     
+    public void testPropertiesWithoutBrace() throws Exception {
+        String p = "<project default='x'><target><echo message='$HERE'/></target></project>";
+        List l = TestUtil.grammarResultValues(g.queryValues(TestUtil.createCompletion(p)));
+        assertTrue("matched ${basedir}: " + l, l.contains("${basedir}"));
+    }
+    
+    public void testPropertiesInText() throws Exception {
+        String p = "<project default='x'><target><echo>basedir=${baseHERE</echo></target></project>";
+        List l = TestUtil.grammarResultValues(g.queryValues(TestUtil.createCompletion(p)));
+        assertTrue("matched ${basedir}: " + l, l.contains("dir}"));
+    }
+    
+    public void testPropertiesInInterior() throws Exception {
+        String p = "<project default='x'><target><echo message='basedir=${baseHERE'/></target></project>";
+        List l = TestUtil.grammarResultValues(g.queryValues(TestUtil.createCompletion(p)));
+        assertTrue("matched ${basedir} after prefix: " + l, l.contains("basedir=${basedir}"));
+        p = "<project default='x'><target><echo message='foo=${foo} basedir=${baseHERE'/></target></project>";
+        l = TestUtil.grammarResultValues(g.queryValues(TestUtil.createCompletion(p)));
+        assertTrue("matched ${basedir} after other props: " + l, l.contains("foo=${foo} basedir=${basedir}"));
+        p = "<project default='x'><target><echo>foo=${foo} basedir=${baseHERE</echo></target></project>";
+        l = TestUtil.grammarResultValues(g.queryValues(TestUtil.createCompletion(p)));
+        assertTrue("matched ${basedir} after other props in text: " + l, l.contains("dir}"));
+    }
+    
+    public void testAlreadyUsedProperties() throws Exception {
+        String p = "<project default='x'><target><echo message='${foo}'/><echo message='${HERE'/></target></project>";
+        List l = TestUtil.grammarResultValues(g.queryValues(TestUtil.createCompletion(p)));
+        assertTrue("matched already used property ${foo}: " + l, l.contains("${foo}"));
+        p = "<project default='x'><target><echo message='${HERE'/></target><target><echo message='${foo}'/></target></project>";
+        l = TestUtil.grammarResultValues(g.queryValues(TestUtil.createCompletion(p)));
+        assertTrue("matched property ${foo} used later: " + l, l.contains("${foo}"));
+        p = "<project default='x'><target><echo message='${HERE'/></target><target><echo>${foo}</echo></target></project>";
+        l = TestUtil.grammarResultValues(g.queryValues(TestUtil.createCompletion(p)));
+        assertTrue("matched property ${foo} used in a text node: " + l, l.contains("${foo}"));
+        p = "<project default='x'><target><echo message='prefix${foo}suffix'/><echo message='${HERE'/></target></project>";
+        l = TestUtil.grammarResultValues(g.queryValues(TestUtil.createCompletion(p)));
+        assertTrue("matched property ${foo} used inside a value: " + l, l.contains("${foo}"));
+        p = "<project default='x'><target><echo message='${foo}:${bar}'/><echo message='${HERE'/></target></project>";
+        l = TestUtil.grammarResultValues(g.queryValues(TestUtil.createCompletion(p)));
+        assertTrue("matched property ${foo} used before another prop: " + l, l.contains("${foo}"));
+        assertTrue("matched property ${bar} used after another prop: " + l, l.contains("${bar}"));
+    }
+    
+    // XXX tests needed:
+    // - testSpecials
+    // adding <target> and <description> (esp. that <description> appears only once!)
+    // - testAddedProperties
+    // <property name='foo' .../> produces ${foo} etc.
+    // - testImpliedProperties
+    // <target if='foo'>, <junit errorproperty='foo'>, etc. produce ${foo}
+    // - testImplicitProperties
+    // <buildnumber> produces ${build.number} etc.
+    // - testIndirectProperties
+    // <property name='${foo}' .../> does not produce ${${foo}}
+    // - testNonProperties
+    // '${foo', '$${foo}', etc. do not produce ${foo}, and '${}' does not produce ${}
+    // - testNonCompleting
+    // '$${base' should not complete to $${basedir}
+    // '${basedir}' is already completed, no more completions allowed
+    // - testCompleteImpliedProperties
+    // "<target if='base", "<isset property='base", etc. complete just name
+
 }
