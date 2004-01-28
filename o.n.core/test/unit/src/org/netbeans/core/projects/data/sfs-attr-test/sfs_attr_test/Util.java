492d6b0f046610180349ff82411a994f94ea29a8
jglick <jglick@netbeans.org>
2004-01-28 04:09:49 +0000

    Providing some background diagnostics for #37861.

--- o.n.core/test/unit/src/org/netbeans/core/projects/data/sfs-attr-test/sfs_attr_test/Util.java
+++ o.n.core/test/unit/src/org/netbeans/core/projects/data/sfs-attr-test/sfs_attr_test/Util.java
@@ -13,9 +13,16 @@ public abstract class Util {
     private static Image mergeIcons(FileObject fo) throws IOException {
         int count = ((Integer)fo.getAttribute("iconCount")).intValue();
         if (count < 2) throw new IOException();
-        Image img = Toolkit.getDefaultToolkit().getImage((URL)fo.getAttribute("icon1"));
+        URL icon1 = (URL)fo.getAttribute("icon1");
+        System.out.println("Loading " + icon1 + " just to be sure...");
+        // Make sure it is really loadable:
+        icon1.openConnection().getInputStream().close();
+        Image img = Toolkit.getDefaultToolkit().getImage(icon1);
         for (int i = 2; i <= count; i++) {
-            Image added = Toolkit.getDefaultToolkit().getImage((URL)fo.getAttribute("icon" + i));
+            URL iconn = (URL)fo.getAttribute("icon" + i);
+            System.out.println("Loading " + iconn + " just to be sure...");
+            iconn.openConnection().getInputStream().close();
+            Image added = Toolkit.getDefaultToolkit().getImage(iconn);
             int x = ((Integer)fo.getAttribute("iconx" + count)).intValue();
             int y = ((Integer)fo.getAttribute("icony" + count)).intValue();
             img = Utilities.mergeImages(img, added, x, y);
