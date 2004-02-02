15af5c0d54271cef5450a3b250af96d78013a28e
pnejedly <pnejedly@netbeans.org>
2004-02-02 10:18:46 +0000

    Reverted the last change, the read lock is needed because of usual calling from Children.Keys.

--- openide.loaders/src/org/openide/loaders/DataObject.java
+++ openide.loaders/src/org/openide/loaders/DataObject.java
@@ -231,18 +231,24 @@ public abstract class DataObject extends Object implements Node.Cookie, Serializ
             Exception e = new IllegalStateException("The data object " + getPrimaryFile() + " is invalid; you may not call getNodeDelegate on it any more; see #17020 and please fix your code"); // NOI18N
             ErrorManager.getDefault().notify(ErrorManager.INFORMATIONAL, e);
         }
-        // synchronize on something private, so only one delegate can be created
-        // do not synchronize on this, because we could deadlock with
-        // subclasses could synchronize too.
-        synchronized(nodeCreationLock) {
-            if (nodeDelegate == null) {
-                nodeDelegate = createNodeDelegate();
-            }
-        }
-
-        // JST: debuging code
         if (nodeDelegate == null) {
-            throw new IllegalStateException("DataObject " + this + " has null node delegate"); // NOI18N
+            // synchronize on something private, so only one delegate can be created
+            // do not synchronize on this, because we could deadlock with
+            // subclasses could synchronize too.
+            Children.MUTEX.readAccess (new Runnable() {
+                public void run() {
+                    synchronized(nodeCreationLock) {
+                        if (nodeDelegate == null) {
+                            nodeDelegate = createNodeDelegate();
+                        }
+                    }
+                }
+            });
+
+            // JST: debuging code
+            if (nodeDelegate == null) {
+                throw new IllegalStateException("DataObject " + this + " has null node delegate"); // NOI18N
+            }
         }
         return nodeDelegate;
     }
@@ -438,9 +444,7 @@ public abstract class DataObject extends Object implements Node.Cookie, Serializ
             }
 
             // try to use the loaders machinery
-            DataLoaderPool p = (DataLoaderPool)Lookup.getDefault().lookup(DataLoaderPool.class);
-            assert p != null : "No DataLoaderPool found in " + Lookup.getDefault();
-            obj = p.findDataObject (fo);
+            obj = ((DataLoaderPool)Lookup.getDefault().lookup(DataLoaderPool.class)).findDataObject (fo);
             if (obj != null) {
                 return obj;
             }
@@ -557,7 +561,6 @@ public abstract class DataObject extends Object implements Node.Cookie, Serializ
             throw iae;
         }
         String oldName;
-        String newName;
         final FileObject[] files = new FileObject[2]; // [old, new]
         
         synchronized ( synchObject() ) {
@@ -575,11 +578,9 @@ public abstract class DataObject extends Object implements Node.Cookie, Serializ
                             item.changePrimaryFile (files[1]);
                     }
                 });
-             newName = getName ();
         }
         if (files[0] != files[1])
             firePropertyChange (PROP_PRIMARY_FILE, files[0], getPrimaryFile ());
-        firePropertyChange (PROP_NAME, oldName, newName);
         
         fireOperationEvent (new OperationEvent.Rename (this, oldName), OperationEvent.RENAME);
     }
@@ -967,7 +968,7 @@ public abstract class DataObject extends Object implements Node.Cookie, Serializ
         * @return array of objects
         */
         public DataObject[] getModified () {
-            return (DataObject[])modified.toArray (new DataObject[modified.size()]);
+            return (DataObject[])modified.toArray (new DataObject[0]);
         }
     }
 
@@ -1057,15 +1058,11 @@ public abstract class DataObject extends Object implements Node.Cookie, Serializ
         FileObject f = fae.getFile();
         if (f != null) {
             String attrFromFO = (String)f.getAttribute(EA_ASSIGNED_LOADER);
-            if (attrFromFO == null || (! attrFromFO.equals(getLoader().getClass().getName()))) {
-                java.util.HashSet single = new java.util.HashSet();
-                single.add(f);
-                if (!DataObjectPool.getPOOL().revalidate(single).isEmpty()) {
-                    ErrorManager.getDefault().log(ErrorManager.INFORMATIONAL, "It was not possible to invalidate data object: " + this); // NOI18N
-                } else {
-                    // we need to refresh parent folder if it is there 
-                    // this should be covered by DataLoaderPoolTest.testChangeIsAlsoReflectedInNodes
-                    FolderList.changedDataSystem (f.getParent());
+            if ((attrFromFO != null) && (! attrFromFO.equals(getLoader().getClass().getName()))) {
+                try {
+                    setValid (false);
+                } catch (java.beans.PropertyVetoException e) {
+                    ErrorManager.getDefault ().notify (ErrorManager.INFORMATIONAL, e);
                 }
             }
         }
