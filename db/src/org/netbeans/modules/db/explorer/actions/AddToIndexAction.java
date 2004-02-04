2a00bd34db4bac2c60e7ef194cf57bc4e573c1fe
rnajman <rnajman@netbeans.org>
2004-02-04 16:39:17 +0000

    Fixed issue #15413 - Action Add column to a index re-create the index as unique Unique flag wasn't recognized correctly. Reimplemented.

--- db/src/org/netbeans/modules/db/explorer/actions/AddToIndexAction.java
+++ db/src/org/netbeans/modules/db/explorer/actions/AddToIndexAction.java
@@ -7,34 +7,38 @@
  * http://www.sun.com/
  * 
  * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2003 Sun
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
 package org.netbeans.modules.db.explorer.actions;
 
-import java.sql.*;
+import java.sql.ResultSet;
 import java.text.MessageFormat;
-import java.util.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Vector;
 
-import org.openide.*;
-import org.openide.nodes.*;
+import org.openide.DialogDisplayer;
+import org.openide.NotifyDescriptor;
+import org.openide.nodes.Node;
 
-import org.netbeans.lib.ddl.*;
-import org.netbeans.lib.ddl.impl.*;
-import org.netbeans.modules.db.explorer.*;
-import org.netbeans.modules.db.explorer.dlg.*;
-import org.netbeans.modules.db.explorer.nodes.*;
-import org.netbeans.modules.db.explorer.infos.*;
+import org.netbeans.lib.ddl.impl.CreateIndex;
+import org.netbeans.lib.ddl.impl.DriverSpecification;
+import org.netbeans.lib.ddl.impl.DropIndex;
+import org.netbeans.lib.ddl.impl.Specification;
+
+import org.netbeans.modules.db.explorer.dlg.ColumnItem;
+import org.netbeans.modules.db.explorer.dlg.LabeledComboDialog;
+import org.netbeans.modules.db.explorer.nodes.DatabaseNode;
+import org.netbeans.modules.db.explorer.infos.DatabaseNodeInfo;
 
 public class AddToIndexAction extends DatabaseAction {
     static final long serialVersionUID =-1416260930649261633L;
     
     protected boolean enable(Node[] activatedNodes) {
-        if (activatedNodes != null && activatedNodes.length == 1)
-            return true;
-        else
-            return false;
+        return (activatedNodes != null && activatedNodes.length == 1);
     }
 
     public void performAction (Node[] activatedNodes) {
@@ -61,16 +65,25 @@ public class AddToIndexAction extends DatabaseAction {
             ResultSet rs = drvSpec.getResultSet();
             HashMap rset = new HashMap();
             boolean isUQ = false;
+            String ixname;
             while (rs.next()) {
                 rset = drvSpec.getRow();
-                String ixname = (String) rset.get(new Integer(6));
-                if (ixname != null) {
-                    String colname = (String) rset.get(new Integer(9));
-                    if (ixname.equals(index))
-                        ixrm.add(colname);
-                }
-                isUQ = !Boolean.valueOf( (String)rset.get( new Integer(4) ) ).booleanValue();
-                rset.clear();
+                ixname = (String) rset.get(new Integer(6));
+
+                if (!index.equals(ixname))
+                    continue;
+
+                String colname = (String) rset.get(new Integer(9));
+                ixrm.add(colname);
+
+                String val = (String) rset.get(new Integer(4));
+                if (val.equals("1"))
+                    isUQ = false;
+                else
+                    isUQ = !(Boolean.valueOf(val).booleanValue());
+                
+                rset.clear();                
+                break;
             }
             rs.close();
 
@@ -101,6 +114,7 @@ public class AddToIndexAction extends DatabaseAction {
                     icmd.setIndexType(ColumnItem.UNIQUE);
                 else
                     icmd.setIndexType(new String());
+                
                 Iterator enu = ixrm.iterator();
                 while (enu.hasNext())
                     icmd.specifyColumn((String)enu.next());
