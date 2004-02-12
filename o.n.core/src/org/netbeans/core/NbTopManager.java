af7a54dcd48823f11538dc897deadd7722e0df84
jpokorsky <jpokorsky@netbeans.org>
2004-02-12 21:31:48 +0000

    #39704: replaced NbBrowser.requestFocus with requestActive in order to  properly activate topcomponent with the internal browser

--- o.n.core/src/org/netbeans/core/NbTopManager.java
+++ o.n.core/src/org/netbeans/core/NbTopManager.java
@@ -7,7 +7,7 @@
  * http://www.sun.com/
  * 
  * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2003 Sun
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -683,7 +683,7 @@ public abstract class NbTopManager /*extends TopManager*/ {
 	 */
 	private void showUrl (URL url) {
             open ();
-            requestFocus ();
+            requestActive ();
             setURL (url);
 	}
         
