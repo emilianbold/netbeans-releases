853f1faaf0d46510eb9434d16c463b1ceef53419
jskrivanek <jskrivanek@netbeans.org>
2004-02-19 17:17:22 +0000

    Help sets submenu not used in NetBeans3.6.

--- jellytools/src/org/netbeans/jellytools/actions/HelpAction.java
+++ jellytools/src/org/netbeans/jellytools/actions/HelpAction.java
@@ -7,7 +7,7 @@
  * http://www.sun.com/
  * 
  * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2002 Sun
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 package org.netbeans.jellytools.actions;
@@ -15,7 +15,7 @@ package org.netbeans.jellytools.actions;
 import java.awt.event.KeyEvent;
 import org.netbeans.jellytools.Bundle;
 
-/** Used to call "Help|Contents" or "Help|Help Sets|{help_set} main menu item,
+/** Used to call "Help|Help Contents" main menu item,
  * "org.openide.actions.HelpAction" or F1 shortcut. It can also be used
  * to invoke help on a property sheet from popup menu.
  * @see Action
@@ -27,18 +27,22 @@ public class HelpAction extends Action {
     private static final String helpMenu = Bundle.getStringTrimmed("org.netbeans.core.Bundle", "Menu/Help")
                                          + "|" 
                                          + Bundle.getStringTrimmed("org.netbeans.modules.usersguide.Bundle", "Menu/Help/org-netbeans-modules-usersguide-master.xml");
+    /* --- Helpsets subdirectory not used in NetBeans 3.6 ---
     private static final String helpSetsMenu = Bundle.getStringTrimmed("org.netbeans.core.Bundle", "Menu/Help")
                                              + "|" 
                                              + Bundle.getStringTrimmed("org.netbeans.modules.javahelp.resources.Bundle", "Menu/Help/HelpShortcuts")
-                                             + "|"; 
+                                             + "|";
+     */
     private static final Shortcut helpShortcut = new Shortcut(KeyEvent.VK_F1);
 
     /** Creates new HelpAction instance for specific help set.
      * @param helpSet menu item of help set to be showed (e.g. "Core IDE Help")
      */
+    /* --- Helpsets subdirectory not used in NetBeans 3.6 ---
     public HelpAction(String helpSet) {
         super(helpSetsMenu+helpSet, null);
     }
+     */
 
     /** Creates new HelpAction instance for master help set (Help|Contents)
      * or for generic use e.g. in property sheets.
