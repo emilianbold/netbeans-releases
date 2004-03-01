3c95b3c5eca0cf349acf8ba2b7f1ea7efc0b3472
jrechtacek <jrechtacek@netbeans.org>
2004-03-01 09:53:17 +0000

    added a missed AccessibleDescription

--- openide.loaders/src/org/openide/loaders/TemplateWizard2.java
+++ openide.loaders/src/org/openide/loaders/TemplateWizard2.java
@@ -7,7 +7,7 @@
  * http://www.sun.com/
  * 
  * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2003 Sun
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -164,6 +164,9 @@ final class TemplateWizard2 extends javax.swing.JPanel implements DocumentListen
         super.addNotify();
         //Fix for issue 31086, initial focus on Back button 
         newObjectName.requestFocus();
+        getAccessibleContext().setAccessibleDescription(
+            NbBundle.getBundle(TemplateWizard2.class).getString ("ACSD_TemplateWizard2") // NOI18N
+        );
     }
     
     /** Helper implementation of WizardDescription.Panel for TemplateWizard.Panel2.
