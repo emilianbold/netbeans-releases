c3f1f53468d3f3bca053633469ff358be48961ae
jskrivanek <jskrivanek@netbeans.org>
2004-03-02 12:07:15 +0000

    Help button was removed from Documents dialog.

--- jellytools/src/org/netbeans/jellytools/DocumentsDialogOperator.java
+++ jellytools/src/org/netbeans/jellytools/DocumentsDialogOperator.java
@@ -7,7 +7,7 @@
  * http://www.sun.com/
  *
  * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2003 Sun
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -195,7 +195,6 @@ public class DocumentsDialogOperator extends NbDialogOperator {
         btCloseDocuments();
         btSaveDocuments();
         btClose();
-        btHelp();
         txtDescription();
         lstDocuments();
     }
