1fc64c3668082d63c34a4de497aa82337e69b41f
jskrivanek <jskrivanek@netbeans.org>
2004-03-17 10:24:52 +0000

    netbeans.dest.dir has to be set in driver and propagated to module build scripts.

--- xtest/src/org/netbeans/xtest/NbExecutor.java
+++ xtest/src/org/netbeans/xtest/NbExecutor.java
@@ -7,7 +7,7 @@
  * http://www.sun.com/
  * 
  * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2000 Sun
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -43,6 +43,7 @@ public class NbExecutor extends Task {
     File   testrun;
     
     private static final String[] propertiesToPass = {
+                                            "netbeans.dest.dir",
                                             "netbeans.home",
                                             "build.sysclasspath",
                                             "ant.home",
