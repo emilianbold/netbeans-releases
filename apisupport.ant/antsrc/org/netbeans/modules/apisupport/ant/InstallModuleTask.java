641a81faaf5963dd5af749eda1384b9bcafc4c96
jglick <jglick@netbeans.org>
2004-03-30 18:19:47 +0000

    Projectizing apisupport, apisupport/ant, and apisupport/lite. Cleaning a lot of old stuff out of these modules as well as apisupport/apidocs.

---
+++ apisupport.ant/antsrc/org/netbeans/modules/apisupport/ant/InstallModuleTask.java
@@ -0,0 +1,66 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.modules.apisupport.ant;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+import org.apache.tools.ant.types.EnumeratedAttribute;
+
+import org.openide.util.Lookup;
+
+import org.netbeans.core.modules.TestModuleDeployer;
+
+// Note that Ant tasks in general are not internationalized.
+
+public class InstallModuleTask extends Task {
+
+    private File module = null;
+    private String action = null;
+
+    public static class Action extends EnumeratedAttribute {
+        public String[] getValues () {
+            return new String[] { /*XXX: "install", "uninstall",*/ "reinstall" }; // NOI18N
+        }
+    }
+
+    public void setModule (File f) {
+        module = f;
+    }
+
+    public void setAction (Action a) {
+        action = a.getValue ();
+    }
+
+    public void execute () throws BuildException {
+        if (module == null) throw new BuildException ("Required attribute: module", getLocation()); // NOI18N
+        if (action == null) throw new BuildException ("Required attribute: action", getLocation()); // NOI18N
+        try {
+            if (action.equals ("reinstall")) { // NOI18N
+                TestModuleDeployer deployer = (TestModuleDeployer)Lookup.getDefault().lookup(TestModuleDeployer.class);
+                if (deployer == null) {
+                    throw new BuildException("No test module deployer found. Probably you are trying to run <nbinstaller> from the command-line. It can only be run from inside the IDE."); // NOI18N
+                }
+                deployer.deployTestModule(module);
+            } else {
+                throw new BuildException ("Unsupported action: " + action, getLocation()); // NOI18N
+            }
+        } catch (IOException ioe) {
+            throw new BuildException (ioe, getLocation());
+        }
+    }
+
+}
