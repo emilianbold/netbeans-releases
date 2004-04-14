0c703f1ca5cfedc22f5f067aaba14c8ff0bf3282
tzezula <tzezula@netbeans.org>
2004-04-14 18:30:33 +0000

    Added test for nbinst URL protocol handler.

---
+++ o.n.core/test/unit/src/org/netbeans/core/filesystems/NbinstURLMapperTest.java
@@ -0,0 +1,187 @@
+/*
+ *                 Sun Public License Notice
+ *
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ *
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+package org.netbeans.core.filesystems;
+
+
+import java.beans.PropertyVetoException;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.StringTokenizer;
+import org.openide.filesystems.*;
+import org.openide.util.Lookup;
+import org.openide.util.lookup.Lookups;
+import org.openide.util.lookup.ProxyLookup;
+import org.openide.modules.InstalledFileLocator;
+import org.netbeans.junit.NbTestCase;
+
+
+public class NbinstURLMapperTest extends NbTestCase {
+
+    private static final String FILE_NAME = "test.txt";     //NOI18N
+    private static final String FOLDER_NAME = "modules";    //NOI18N
+
+    private FileSystem fs;
+    private FileObject testFile;
+    private int expectedLength;
+
+    static {
+        System.setProperty("org.openide.util.Lookup", Lkp.class.getName());
+    }
+
+    public NbinstURLMapperTest (String testName) throws IOException {
+        super (testName);
+    }
+
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        File f = this.getWorkDir();
+        this.clearWorkDir();
+        Lookup.Result result = Lookup.getDefault().lookup (new Lookup.Template(InstalledFileLocator.class));
+        boolean found = false;
+        for (java.util.Iterator it = result.allInstances().iterator(); it.hasNext();) {
+            Object locator = it.next();
+            if (locator instanceof TestInstalledFileLocator) {
+                ((TestInstalledFileLocator)locator).setRoot(f);
+                found = true;
+            }
+        }
+        assertTrue("The TestInstalledFileLocator can not be found in the default lookup.",found);
+        f = new File (f,FOLDER_NAME);
+        f.mkdir();
+        f = new File (f,FILE_NAME);
+        f.createNewFile();
+        testFile = FileUtil.fromFile (f)[0];
+        PrintWriter pw = null;
+        try {
+            pw = new PrintWriter(new FileWriter(f));
+            pw.println(FILE_NAME);
+        } finally {
+            if (pw!=null) {
+                pw.close ();
+            }
+        }
+        this.expectedLength = (int) f.length();
+        this.fs = this.mountFs ();
+        assertNotNull ("Test was not able to mount filesystem.",this.fs);
+    }
+
+
+    protected void tearDown() throws Exception {
+        this.umountFs (this.fs);
+        super.tearDown();
+    }
+
+    public void testFindFileObject () throws MalformedURLException, IOException {
+        URL url = new URL ("nbinst:///modules/test.txt");  //NOI18N
+        FileObject[] fos = URLMapper.findFileObjects (url);
+        assertTrue ("URLMapper returned null, violation of API contract.",fos!=null);
+        assertTrue ("The nbinst URL was not resolved.",fos.length == 1);
+        assertEquals("URLMapper returned wrong file.",fos[0],testFile);
+        url = new URL ("nbinst://test-module/modules/test.txt");
+        fos = URLMapper.findFileObjects (url);
+        assertTrue ("URLMapper returned null, violation of API contract.",fos!=null);
+        assertTrue ("The nbinst URL was not resolved.",fos.length == 1);
+        assertEquals("URLMapper returned wrong file.",fos[0],testFile);
+        url = new URL ("nbinst://foo-module/modules/test.txt");
+        fos = URLMapper.findFileObjects (url);
+        assertTrue ("URLMapper returned null, violation of API contract.",fos!=null);
+        assertTrue ("The nbinst URL was resolved.",fos.length == 0);
+    }
+
+    public void testURLConenction () throws MalformedURLException, IOException {
+        URL url = new URL ("nbinst:///modules/test.txt");                //NOI18N
+        URLConnection connection = url.openConnection();
+        assertEquals ("URLConnection returned wrong content length.",connection.getContentLength(),expectedLength);
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader  ( new InputStreamReader (connection.getInputStream()));
+            String line = in.readLine();
+            assertTrue("URLConnection returned invalid InputStream",line.equals(FILE_NAME));
+        } finally {
+            if (in != null) {
+                in.close ();
+            }
+        }
+    }
+
+
+
+
+
+    private FileSystem mountFs () throws IOException {
+        File f = FileUtil.normalizeFile(this.getWorkDir());
+        String parentName;
+        while ((parentName=f.getParent())!=null) {
+            f = new File (parentName);
+        }
+        try {
+            LocalFileSystem fs = new LocalFileSystem ();
+            fs.setRootDirectory (f);
+            Repository.getDefault().addFileSystem(fs);
+            return fs;
+        } catch (PropertyVetoException pve) {
+            return null;
+        }
+    }
+
+    private void umountFs (FileSystem fs) {
+        assertNotNull ("umountFs called with null FileSystem.",fs);
+        Repository.getDefault().removeFileSystem(fs);
+    }
+
+    public static class Lkp extends ProxyLookup {
+        public Lkp () {
+            this.setLookups (new Lookup[] {
+                Lookups.fixed(new Object[] {new TestInstalledFileLocator(), new NbinstURLStreamHandlerFactory()})
+            });
+        }
+
+    }
+
+    public static class TestInstalledFileLocator extends InstalledFileLocator {
+
+        private File root;
+
+        public TestInstalledFileLocator () {
+        }
+
+
+        public void setRoot (File root) {
+            this.root = root;
+        }
+
+        public File locate(String relativePath, String codeNameBase, boolean localized) {
+            assert relativePath != null;
+            if (root == null) {
+                return null;
+            }
+            if (codeNameBase!= null && !"test-module".equals(codeNameBase)) {
+                return null;
+            }
+            StringTokenizer tk = new StringTokenizer(relativePath,"/");
+            File f = this.root;
+            while (tk.hasMoreTokens()) {
+                String part = tk.nextToken();
+                f = new File (f,part);
+                if (!f.exists()) {
+                    return null;
+                }
+            }
+            return f;
+        }
+    }
+
+}
