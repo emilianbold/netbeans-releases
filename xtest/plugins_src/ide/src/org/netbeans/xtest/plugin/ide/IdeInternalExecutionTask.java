c47307dc482e8061da768990a2ad4327c64fe3f7
jskrivanek <jskrivanek@netbeans.org>
2004-04-26 07:10:08 +0000

    SystemClassLoader find in threads hierarchy; better handling of exceptions from launched class; task accepts arguments.

--- xtest/plugins_src/ide/src/org/netbeans/xtest/plugin/ide/IdeInternalExecutionTask.java
+++ xtest/plugins_src/ide/src/org/netbeans/xtest/plugin/ide/IdeInternalExecutionTask.java
@@ -23,6 +23,8 @@ import java.security.PermissionCollection;
 import java.security.Permissions;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Task;
+import org.apache.tools.ant.types.Commandline;
+import org.apache.tools.ant.types.CommandlineJava;
 import org.apache.tools.ant.types.Path;
 import org.openide.util.Lookup;
 
@@ -38,18 +40,20 @@ public class IdeInternalExecutionTask extends Task {
     protected Path classpath;
     /** Fully qualified name of class to be run. */
     protected String classname;
+    /** Property to store arguments. */
+    private CommandlineJava cmdl = new CommandlineJava();
     
     
     /** Sets classpath property.
      * @param classpath new value
-     */    
+     */
     public void setClasspath(Path classpath) {
         this.classpath = classpath;
     }
     
     /** Creates classpath
      * @return created path
-     */    
+     */
     public Path createClasspath() {
         if (this.classpath == null) {
             this.classpath = new Path(getProject());
@@ -59,25 +63,55 @@ public class IdeInternalExecutionTask extends Task {
     
     /** Sets classname property.
      * @param classname fully-qualified class name
-     */    
+     */
     public void setClassname(String classname) {
         this.classname = classname;
     }
     
-    /** Gets IDE's system class loader, adds given classpath and invokes main 
+    /** Creates main method argument.
+     * @param arg arguments to be passed to main method.
+     * @return created argument
+     */
+    public Commandline.Argument createArg() {
+        return cmdl.createArgument();
+    }
+    
+    
+    /** Gets IDE's system class loader, adds given classpath and invokes main
      * method of given class.
      * @throws BuildException when something's wrong
-     */    
+     */
     public void execute() throws BuildException {
         try {
-            ClassLoader systemClassloader = (ClassLoader)Lookup.getDefault().lookup(ClassLoader.class);
+            // find NetBeans SystemClassLoader in threads hierarchy
+            ThreadGroup tg = Thread.currentThread().getThreadGroup();
+            ClassLoader systemClassloader = Thread.currentThread().getContextClassLoader();
+            while(!systemClassloader.getClass().getName().endsWith("SystemClassLoader")) { // NOI18N        
+                tg = tg.getParent();
+                if(tg == null) {
+                    throw new BuildException("NetBeans SystemClassLoader not found!");
+                }
+                Thread[] list = new Thread[tg.activeCount()];
+                tg.enumerate(list);
+                systemClassloader = list[0].getContextClassLoader();
+            }
             URL[] urls = classpathToURL(classpath);
             URLClassLoader testClassLoader = new TestClassLoader(urls, systemClassloader);
             Class classToRun = testClassLoader.loadClass(this.classname);
-            Method method = classToRun.getDeclaredMethod("main", new Class[] {String[].class});
-            method.invoke(null, new Object[] {null});
+            Method method = classToRun.getDeclaredMethod("main", new Class[] {String[].class}); // NOI18N
+            String[] args = cmdl.getJavaCommand().getArguments();
+            method.invoke(null, new Object[] {args});
         } catch (Exception e) {
-            throw new BuildException(e.getClass().getName()+": "+e.getMessage(), e);
+            Throwable cause = e.getCause();
+            if(cause != null) {
+                cause.printStackTrace();
+                throw new BuildException(cause.getClass().getName()+": "+
+                                         cause.getMessage()+" at "+
+                                         e.getCause().getStackTrace()[0], cause);
+            } else {
+                e.printStackTrace();
+                throw new BuildException(e.getClass().getName()+": "+e.getMessage(), e);
+            }
         }
     }
     
