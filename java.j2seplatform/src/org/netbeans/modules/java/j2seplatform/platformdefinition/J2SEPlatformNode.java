0e4191cf95e84ddb135aee6503c3819a5844cf70
tzezula <tzezula@netbeans.org>
2004-04-29 07:25:27 +0000

    Added setName.

--- java.j2seplatform/src/org/netbeans/modules/java/j2seplatform/platformdefinition/J2SEPlatformNode.java
+++ java.j2seplatform/src/org/netbeans/modules/java/j2seplatform/platformdefinition/J2SEPlatformNode.java
@@ -1,9 +1,14 @@
-/**
- * Created by IntelliJ IDEA.
- * User: tom
- * Date: Apr 27, 2004
- * Time: 8:06:47 PM
- * To change this template use Options | File Templates.
+/*
+ *                 Sun Public License Notice
+ *
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ *
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
+ * Microsystems, Inc. All Rights Reserved.
  */
 package org.netbeans.modules.java.j2seplatform.platformdefinition;
 
@@ -28,6 +33,14 @@ class J2SEPlatformNode extends AbstractNode {
 
     public String getName () {
         return this.getDisplayName();
+    }
+
+    public void setName (String name) {
+        this.platform.setDisplayName (name);
+    }
+
+    public void setDisplayName(String name) {
+        this.setName (name);
     }
 
     public boolean hasCustomizer () {
