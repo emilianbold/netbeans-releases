706ee48fe853aba65a5065b9de0fdd5440404235
mslama <mslama@netbeans.org>
2004-05-05 14:17:06 +0000

    #31148: Added subclass of URLConnection to provide special content when referenced module help is not available. (Module can be disabled or not installed.)

--- javahelp/src/org/netbeans/modules/javahelp/NbDocsStreamHandler.java
+++ javahelp/src/org/netbeans/modules/javahelp/NbDocsStreamHandler.java
@@ -18,6 +18,8 @@ import java.net.*;
 import java.util.*;
 
 import org.openide.modules.InstalledFileLocator;
+import org.openide.modules.ModuleInfo;
+import org.openide.util.Lookup;
 import org.openide.util.NbBundle;
 import org.openide.util.Utilities;
 
@@ -79,6 +81,32 @@ final class NbDocsStreamHandler extends URLStreamHandler {
                 throw e;
             }
             if (! connected) {
+                String host = url.getHost();
+                if (host.length() > 0) {
+                    ModuleInfo moduleInfo = findModule(host);
+                    if (moduleInfo != null) {
+                        if (!moduleInfo.isEnabled()) {
+                            URL info = new URL("nbdocs:/org/netbeans/modules/javahelp/resources/notEnabledModule.html"); // NOI18N
+                            String moduleName = moduleInfo.getDisplayName();
+                            real = new InfoURLConnection(info,moduleName);
+                            real.connect();
+                            connected = true;
+                            return;
+                        }
+                    } else {
+                        URL info = new URL("nbdocs:/org/netbeans/modules/javahelp/resources/notInstalledModule.html"); // NOI18N
+                        String moduleName = ""; // NOI18N
+                        try {
+                            moduleName = NbBundle.getMessage(NbDocsStreamHandler.class,host);
+                        } catch (MissingResourceException exc) {
+                            moduleName = host;
+                        }
+                        real = new InfoURLConnection(info,moduleName);
+                        real.connect();
+                        connected = true;
+                        return;
+                    }
+                }
                 String resource = url.getFile();
                 if (resource.startsWith("/")) resource = resource.substring(1); //NOI18N
                 URL target;
@@ -110,6 +138,30 @@ final class NbDocsStreamHandler extends URLStreamHandler {
                 real.connect();
                 connected = true;
             }
+        }
+        
+        /** Searches for module with given code name.
+         * @param codeNameBase unique string base name of the module
+         * (without release number)
+         *
+         * @return module info of found module or null if module is not found
+         * (not installed).
+         * @deprecated will be replaced by similar method in Modules Open APIs in
+         * future releases
+         */
+        private static ModuleInfo findModule (String codeNameBase) {
+            Lookup.Result modulesResult = 
+                Lookup.getDefault().lookup(new Lookup.Template(ModuleInfo.class));
+            Collection infos = modulesResult.allInstances();
+            ModuleInfo curInfo = null;
+            boolean equalsName = false;
+            for (Iterator iter = infos.iterator(); iter.hasNext(); ) {
+                curInfo = (ModuleInfo)iter.next();
+                if (curInfo.getCodeNameBase().equals(codeNameBase)) {
+                    return curInfo;
+                }
+            }
+            return null;
         }
         
         /** Maybe connect, if not keep track of the problem.
@@ -201,5 +253,115 @@ final class NbDocsStreamHandler extends URLStreamHandler {
         
     }
     
+    /** A URL connection that reads from the info files. It displays
+     * help page when referred module is not enabled or installed.
+     * It also takes module display name from bundle when available.
+     * Module base name is key to retrieve module display name
+     * eg.: org.netbeans.modules.web.monitor=HTTP Monitor
+     */
+    private static final class InfoURLConnection extends URLConnection {
+        /** Provides input stream for this connection. */
+        private ByteArrayInputStream stream;
+        /** Module display name */
+        private String moduleName;
+        
+        /** Make the connection.
+         * @param u URL to connect to
+         */
+        public InfoURLConnection (URL u, String moduleName) {
+            super(u);
+            this.moduleName = moduleName;
+        }
+        
+        /** Connect to the URL.
+         * Actually look up and open the underlying connection.
+         * @throws IOException for the usual reasons
+         */
+        public synchronized void connect() throws IOException {
+            if (!connected) {
+                //Prepare data
+                InputStream is = url.openStream();
+                if (is != null) {
+                    byte [] arr;
+                    arr = readData(is);
+                    String s1 = new String(arr,"UTF-8"); // NOI18N
+                    String s2 = s1.replaceAll("\\{0\\}",moduleName); // NOI18N
+                    arr = s2.getBytes("UTF-8");
+                    stream = new ByteArrayInputStream(arr);
+                } else {
+                    throw new IOException("Info file not found."); // NOI18N
+                }
+                connected = true;
+            }
+        }
+        
+        /** Reads all available data from input steram to byte array. It is workaround
+         * to avoid usage of InputStream.available which might be unreliable on URL. */
+        private byte [] readData (InputStream is) throws IOException {
+            int step = 4096;
+            byte[] buff = new byte[step];
+            byte[] sum = new byte[0];
+            byte[] result;
+            int len = -1, readLen = 0, allocLen = 0;
+            
+            for (;;) {
+                len = is.read(buff);
+                if (len == -1) {
+                    result = new byte[readLen];
+                    System.arraycopy(sum,0,result,0,readLen);
+                    return sum;
+                }
+                if (allocLen < (readLen + len)) {
+                    byte [] tmp = new byte[sum.length];
+                    System.arraycopy(sum,0,tmp,0,readLen);
+                    sum = new byte[allocLen + step];
+                    allocLen = allocLen + step;
+                    System.arraycopy(tmp,0,sum,0,readLen);
+                }
+                System.arraycopy(buff,0,sum,readLen,len);
+                readLen = readLen + len;
+            }
+        }
+        
+        /** Maybe connect, if not keep track of the problem.
+         */
+        private void tryToConnect() {
+            if (connected) {
+                return;
+            }
+            try {
+                connect();
+            } catch (IOException ioe) {
+            }
+        }
+        
+        /** Get an input stream on the connection.
+         * @throws IOException for the usual reasons
+         * @return a stream to the object
+         */
+        public InputStream getInputStream() throws IOException {
+            connect();
+            return stream;
+        }
+        
+        
+        /** Get the type of the content.
+         * @return the MIME type
+         */
+        public String getContentType() {
+            return "text/html"; // NOI18N
+        }
+        
+        /** Get the length of content.
+         * @return the length in bytes
+         */
+        public int getContentLength() {
+            tryToConnect();
+            if (connected) {
+                return stream.available();
+            } else {
+                return 0;
+            }
+        }
+    }
 }
-
