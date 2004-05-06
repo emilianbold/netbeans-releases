55ae22122bc5b7a9cf31e91f4e1651704965c45f
mgrebac <mgrebac@netbeans.org>
2004-05-06 15:19:33 +0000

    licence + isjsp method added

--- web.debug/src/org/netbeans/modules/web/debug/util/Utils.java
+++ web.debug/src/org/netbeans/modules/web/debug/util/Utils.java
@@ -1,13 +1,21 @@
 /*
- * Utils.java
- *
- * Created on November 1, 2003, 8:24 AM
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
+ * Microsystems, Inc. All Rights Reserved.
  */
 
 package org.netbeans.modules.web.debug.util;
 
 import java.util.*;
 
+import java.io.*;
 import javax.swing.*;
 import javax.swing.text.*;
 
@@ -78,6 +86,24 @@ public class Utils {
 //        return sorted;
 //    }
 
+    public static boolean isJsp(String url){
+        if (url != null) {
+            URI uri = null;
+            try {
+                uri = new URI(url);
+            } catch (Exception e) {};
+            if (uri != null) {
+                File f = new File(uri);
+                FileObject fo = FileUtil.toFileObject(f);
+                if (fo != null) {
+                   return "text/x-jsp".equals(fo.getMIMEType());   //NOI18N
+                }                 
+            }
+        }
+        return false;
+    }
+
+    
     public static String getServletClass(String jspUrl) {
         System.err.println("url : " + jspUrl);
         URI jspUri = URI.create(jspUrl);
