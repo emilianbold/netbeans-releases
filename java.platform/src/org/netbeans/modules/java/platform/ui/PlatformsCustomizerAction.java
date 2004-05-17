00b951f1879a7504f5a92ad8cc7772ed2cf40cbb
tzezula <tzezula@netbeans.org>
2004-05-17 16:32:45 +0000

    Fix of issue: 42705. Platforms and Libraries Manager accessible from menu.

---
+++ java.platform/src/org/netbeans/modules/java/platform/ui/PlatformsCustomizerAction.java
@@ -0,0 +1,35 @@
+/*
+ *                 Sun Public License Notice
+ *
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ *
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.modules.java.platform.ui;
+
+import org.openide.util.HelpCtx;
+import org.openide.util.NbBundle;
+import org.openide.util.actions.CallableSystemAction;
+
+
+
+public class PlatformsCustomizerAction extends CallableSystemAction {
+
+    public void performAction() {
+        org.netbeans.api.java.platform.PlatformsCustomizer.showCustomizer(null);
+    }
+
+    public String getName() {
+        return NbBundle.getMessage(PlatformsCustomizerAction.class,"CTL_PlatformManager");
+    }
+
+    public HelpCtx getHelpCtx() {
+        return HelpCtx.DEFAULT_HELP;
+    }
+}
---
+++ project.libraries/src/org/netbeans/modules/project/libraries/ui/LibrariesCustomizerAction.java
@@ -0,0 +1,36 @@
+/*
+ *                 Sun Public License Notice
+ *
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ *
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+package org.netbeans.modules.project.libraries.ui;
+
+import org.openide.util.HelpCtx;
+import org.openide.util.NbBundle;
+import org.openide.util.actions.CallableSystemAction;
+
+
+
+
+public class LibrariesCustomizerAction extends CallableSystemAction {
+
+
+    public void performAction() {
+        org.netbeans.api.project.libraries.LibrariesCustomizer.showCustomizer(null);
+    }
+
+    public String getName() {
+        return NbBundle.getMessage(LibrariesCustomizerAction.class,"CTL_LibrariesManager");
+    }
+
+    public HelpCtx getHelpCtx() {
+        return HelpCtx.DEFAULT_HELP;
+    }
+}
