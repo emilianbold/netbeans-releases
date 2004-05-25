3f323f43fa11ad20958d91cce58a355af33290e9
mkleint <mkleint@netbeans.org>
2004-05-25 13:30:54 +0000

    commented out the sliding effect.

--- core.windows/src/org/netbeans/core/windows/view/ui/slides/DefaultSlidingFx.java
+++ core.windows/src/org/netbeans/core/windows/view/ui/slides/DefaultSlidingFx.java
@@ -29,31 +29,33 @@ import javax.swing.JLayeredPane;
 public class DefaultSlidingFx  implements SlidingFx {
     
     public void showEffect(JLayeredPane pane, int layer, SlideOperation operation) {
-        Component comp = operation.getComponent();
-        Graphics2D gr2d = (Graphics2D)pane.getGraphics();
-        gr2d.setColor(Color.BLUE.darker().darker());
-        Rectangle start = operation.getStartBounds();
-        Rectangle finish = operation.getFinishBounds();
-        Rectangle current = start;
-        for (int i = 0; i < 6 /** magic constant **/; i++) {
-            Rectangle newRect;
-            if (i > 0) {
-                // wipe out old
-            } 
-            newRect = new Rectangle();
-            newRect.x = Math.abs((current.x + finish.x) / 2);
-            newRect.y = Math.abs((current.y + finish.y) / 2);
-            newRect.height = Math.abs((current.height + finish.height) / 2);
-            newRect.width = Math.abs((current.width + finish.width) / 2);
-            gr2d.drawRect(newRect.x, newRect.y, newRect.width, newRect.height);
-            gr2d.getColor().brighter();
-            current = newRect;
-        }
-//        try {
-//            Thread.sleep(5000);
-//        } catch (Throwable th) {
-//            
+//        Component comp = operation.getComponent();
+//        Graphics2D gr2d = (Graphics2D)pane.getGraphics();
+//        Color original = gr2d.getColor();
+//        gr2d.setColor(Color.BLUE.darker().darker());
+//        Rectangle start = operation.getStartBounds();
+//        Rectangle finish = operation.getFinishBounds();
+//        Rectangle current = start;
+//        for (int i = 0; i < 6 /** magic constant **/; i++) {
+//            Rectangle newRect;
+//            if (i > 0) {
+//                // wipe out old
+//            } 
+//            newRect = new Rectangle();
+//            newRect.x = Math.abs((current.x + finish.x) / 2);
+//            newRect.y = Math.abs((current.y + finish.y) / 2);
+//            newRect.height = Math.abs((current.height + finish.height) / 2);
+//            newRect.width = Math.abs((current.width + finish.width) / 2);
+//            gr2d.drawRect(newRect.x, newRect.y, newRect.width, newRect.height);
+//            gr2d.setColor(gr2d.getColor().brighter());
+//            current = newRect;
 //        }
+//        gr2d.setColor(original);
+////        try {
+////            Thread.sleep(5000);
+////        } catch (Throwable th) {
+////            
+////        }
     }
     
 }
