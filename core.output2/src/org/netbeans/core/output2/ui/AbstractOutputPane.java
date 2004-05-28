fcf3bda299817cce325c6b6a318b9a2bcf28ac78
tboudreau <tboudreau@netbeans.org>
2004-05-28 11:22:39 +0000

    #43887: Timing dependent rare AIOOBE in new output window - trying to send caret to a line that hasn't been created yet

--- core.output2/src/org/netbeans/core/output2/ui/AbstractOutputPane.java
+++ core.output2/src/org/netbeans/core/output2/ui/AbstractOutputPane.java
@@ -212,7 +212,7 @@ public abstract class AbstractOutputPane extends JScrollPane implements Document
         unlockScroll();
         caret.setVisible(true);
         caret.setSelectionVisible(true);
-        Element el = textView.getDocument().getDefaultRootElement().getElement(idx);
+        Element el = textView.getDocument().getDefaultRootElement().getElement(Math.min(idx, getLineCount()));
         int position = el.getStartOffset();
         if (Controller.log) Controller.log ("Send caret to line " + idx + " select: " + select + " position " + position);
         if (select) {
