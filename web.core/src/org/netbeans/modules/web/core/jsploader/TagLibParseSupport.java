a4373d28c0d0464f5cf32802cecfae82d20a0907
ppisl <ppisl@netbeans.org>
2004-05-28 08:52:46 +0000

    Better check, if the web module is not null

--- web.core/src/org/netbeans/modules/web/core/jsploader/TagLibParseSupport.java
+++ web.core/src/org/netbeans/modules/web/core/jsploader/TagLibParseSupport.java
@@ -88,9 +88,11 @@ public class TagLibParseSupport implements org.openide.nodes.Node.Cookie {
     }
     
     private WebModule getWebModule(FileObject fo){
-        FileObject wmRoot = WebModule.getWebModule(fo).getDocumentBase();
-        if (fo == wmRoot || FileUtil.isParentOf(wmRoot, fo)) {
-            return WebModule.getWebModule(fo);
+        if (WebModule.getWebModule(fo) != null){
+            FileObject wmRoot = WebModule.getWebModule(fo).getDocumentBase();
+            if (fo == wmRoot || FileUtil.isParentOf(wmRoot, fo)) {
+                return WebModule.getWebModule(fo);
+            }
         }
         return null;
     }
