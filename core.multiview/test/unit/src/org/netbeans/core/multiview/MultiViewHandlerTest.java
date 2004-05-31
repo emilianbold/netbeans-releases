caf6cb4408845800fbe5e1a97ad1a2ffeb711bb5
mkleint <mkleint@netbeans.org>
2004-05-31 13:24:43 +0000

    fixed copyright headers.

--- core.multiview/test/unit/src/org/netbeans/core/multiview/MultiViewHandlerTest.java
+++ core.multiview/test/unit/src/org/netbeans/core/multiview/MultiViewHandlerTest.java
@@ -7,9 +7,10 @@
  * http://www.sun.com/
  * 
  * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 2002 Sun
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
+
 
 package org.netbeans.core.multiview;
 
