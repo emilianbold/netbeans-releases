05cc66a9642ea538ff5e05cc98fa7620e7df8259
mkleint <mkleint@netbeans.org>
2004-06-03 06:43:27 +0000

    added license.

--- o.n.swing.tabcontrol/src/org/netbeans/swing/tabcontrol/plaf/AquaSlidingButtonUI.java
+++ o.n.swing.tabcontrol/src/org/netbeans/swing/tabcontrol/plaf/AquaSlidingButtonUI.java
@@ -1,8 +1,16 @@
 /*
- * AquaSlidingButtonUI.java
+ *                 Sun Public License Notice
  *
- * Created on May 28, 2004, 7:45 AM
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ *
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
+ * Microsystems, Inc. All Rights Reserved.
  */
+
 
 package org.netbeans.swing.tabcontrol.plaf;
 
