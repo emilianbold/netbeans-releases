300cb04ad3448a60dc882a6ad72c44acb4d8d43f
ttran <ttran@netbeans.org>
2004-06-19 12:09:36 +0000

    new NetBeans installer, using InstallShield MultiPlatform 5

---
+++ installer/coreide/src/org/netbeans/installer/InstallLocationPanel.java
@@ -0,0 +1,48 @@
+/*
+ *                 Sun Public License Notice
+ *
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ *
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.installer;
+
+import com.installshield.product.service.product.ProductService;
+import com.installshield.product.wizardbeans.DestinationPanel;
+import com.installshield.util.Log;
+import com.installshield.wizard.WizardBeanEvent;
+import com.installshield.wizard.service.ServiceException;
+
+import java.io.File;
+
+public class InstallLocationPanel extends DestinationPanel
+{
+    public boolean queryEnter(WizardBeanEvent event) {
+        try {
+            ProductService service = (ProductService) getService(ProductService.NAME);
+            String destination = (String) service.getProductBeanProperty(
+            ProductService.DEFAULT_PRODUCT_SOURCE, null, "installLocation");
+            
+            if (! System.getProperty("os.name").startsWith("Windows")) {
+                File root = new File("/");
+                if (! root.canWrite()) {
+                    service.setProductBeanProperty(
+                    ProductService.DEFAULT_PRODUCT_SOURCE,
+                    null,
+                    "installLocation",
+                    resolveString("$L(org.netbeans.installer.Bundle, Product.installLocationForNonRoot)"));
+                }
+            }
+        } catch (ServiceException e) {
+            logEvent(this, Log.ERROR, e);
+        }
+        
+        return super.queryEnter(event);
+    }
+}
---
+++ installer/coreide/src/org/netbeans/installer/NbSummaryPanel.java
@@ -0,0 +1,77 @@
+/*
+ *                 Sun Public License Notice
+ *
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ *
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.installer;
+
+import com.installshield.wizard.WizardBeanEvent;
+import com.installshield.product.service.product.ProductService;
+import com.installshield.wizard.service.ServiceException;
+import com.installshield.wizardx.panels.TextDisplayPanel;
+import com.installshield.util.Log;
+import com.installshield.product.GenericSoftwareObject;
+import com.installshield.product.ProductTree;
+
+import java.util.Properties;
+
+public class NbSummaryPanel extends TextDisplayPanel
+{
+    private int type = ProductService.PRE_INSTALL;
+    
+    public NbSummaryPanel() {
+        setTextSource(TEXT_PROPERTY);
+        setContentType(HTML_CONTENT_TYPE);
+        setDescription("");
+    }
+    
+    public int getType() {
+        return type;        
+    }
+    
+    public void setType(int type) {
+        this.type = type;
+    }
+    
+    public boolean queryEnter(WizardBeanEvent evt) {
+        boolean okay = super.queryEnter(evt);
+
+        try {
+            ProductService service = (ProductService) getService(ProductService.NAME);
+            
+            if (type == ProductService.POST_INSTALL) {
+                ProductTree pt = service.getSoftwareObjectTree(ProductService.DEFAULT_PRODUCT_SOURCE);
+                GenericSoftwareObject gso = (GenericSoftwareObject) pt.getRoot();
+                
+                if (gso.getInstallStatus() == gso.UNINSTALLED) {
+                    // intallation failed
+                    
+                    Properties summary = service.getProductSummary(
+                    ProductService.DEFAULT_PRODUCT_SOURCE,
+                    ProductService.POST_INSTALL,
+                    ProductService.HTML);
+                    setText(summary.getProperty(ProductService.SUMMARY_MSG));
+                } else {
+                    setText(resolveString("$L(org.netbeans.installer.Bundle, SummaryPanel.description)"));
+                }
+            } else {
+                Properties summary = service.getProductSummary(
+                ProductService.DEFAULT_PRODUCT_SOURCE,
+                type,
+                ProductService.HTML);
+                setText(summary.getProperty(ProductService.SUMMARY_MSG));
+            }
+        } catch (ServiceException e) {
+            logEvent(this, Log.ERROR, e);
+        }
+        return okay;
+    }
+}
