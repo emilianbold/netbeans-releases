3da3299dc9e01abdf14777b0225f6de94e42b8ab
jrechtacek <jrechtacek@netbeans.org>
2004-06-23 15:40:55 +0000

    added the missed I18N keys

--- projectui/src/org/netbeans/modules/project/ui/Hacks.java
+++ projectui/src/org/netbeans/modules/project/ui/Hacks.java
@@ -98,9 +98,9 @@ public class Hacks {
                             pname = ProjectUtils.getInformation((Project)projects.iterator().next()).getDisplayName();
                             assert pname != null;
                         } else if (projects.isEmpty()) {
-                            pname = "No Project"; // XXX I18N
+                            pname = NbBUndle.getMessage (Hacks.class, "LBL_NoProject"); // NOI18N
                         } else {
-                            pname = "Multiple Projects"; // XXX I18N
+                            pname = NbBUndle.getMessage (Hacks.class, "LBL_MultipleProjects"); // NOI18N
                         }
                         try {
                             setProjectName.invoke(windowSystemImpl, new Object[] {pname});
