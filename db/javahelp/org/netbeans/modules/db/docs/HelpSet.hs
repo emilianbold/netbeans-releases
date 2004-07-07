7b9ddebb2fcccbfe483da2197fa3c074d9460edb
jglick <jglick@netbeans.org>
2004-07-07 16:18:31 +0000

    Tweaks to be in line with projectized.xml's format for JavaHelp: uses "JavaHelpSearch" for the name of the search DB by default. Since this is now built outside the source tree, the old workaround of using "JavaHelpSearch2" to avoid triggering a CVS update bug is no longer relevant.

--- db/javahelp/org/netbeans/modules/db/docs/HelpSet.hs
+++ db/javahelp/org/netbeans/modules/db/docs/HelpSet.hs
@@ -42,7 +42,7 @@ Microsystems, Inc. All Rights Reserved.
    <type>javax.help.SearchView</type>
  
   <data engine="com.sun.java.help.search.DefaultSearchEngine">
-      JavaHelpSearch2
+      JavaHelpSearch
    </data>
   </view> 
 
