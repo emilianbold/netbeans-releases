db7b4786d20af0a7d74ce4fa6626f53908641773
rkubacki <rkubacki@netbeans.org>
2004-07-12 15:37:35 +0000

    Let the OpenIDE cache the icon

--- web.core/src/org/netbeans/modules/web/taglib/TLDLoaderBeanInfo.java
+++ web.core/src/org/netbeans/modules/web/taglib/TLDLoaderBeanInfo.java
@@ -7,7 +7,7 @@
  * http://www.sun.com/
  * 
  * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2002 Sun
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -22,18 +22,11 @@ import java.awt.Image;
 */
 public class TLDLoaderBeanInfo extends SimpleBeanInfo {
 
-    /** Icon for image data objects. */
-    private static Image icon;
-
     /** @param type Desired type of the icon
      * @return returns the Image loader's icon
      */
     public Image getIcon(int type) {
-        if (icon == null) {
-            icon = loadImage("/org/netbeans/modules/web/taglib/resources/tags.gif"); // NOI18N
-        }
-
-	return icon;
+        return org.openide.util.Utilities.loadImage("/org/netbeans/modules/web/taglib/resources/tags.gif"); // NOI18N
     }
 }
 
