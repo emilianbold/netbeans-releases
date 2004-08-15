683244adff5412459264431a6f71157dd701a2e7
tboudreau <tboudreau@netbeans.org>
2004-08-15 23:08:04 +0000

    Removing some junk from the testing app

--- core.output2/demosrc/org/netbeans/core/output2/TestFrame.java
+++ core.output2/demosrc/org/netbeans/core/output2/TestFrame.java
@@ -36,58 +36,6 @@
 //        io.setToolbarActions (new Action[] {new StopAction("STOP!!")});
         validate();
         System.err.println ("Writing content");
-//        io.getOut().println("This is an output window");
-        
-//        io.getOut().write ("This is using write()");
-//        io.getOut().write ("This is using println");
-//        io.getOut().write ("1 a line\n2 another line");
-        /*
-        for (int i=0; i < 200; i++) {
-            out.println (Thread.currentThread().getName() + i +  ": Wow, we will write a long line of text here.  Very long in fact - who knows just how long it" +
-                " might end up being?  Well, we'll have to see.");
-
-            if (i % 50 == 0) {
-                try {
-//                    out.println ("[deprecated] This is a hyperlink, hear it roar.  And roar, and roar!", new L());
-//                    Thread.currentThread().sleep(20);
-                } catch (Exception e) {}
-            }
-            try {
-//                Thread.currentThread().sleep(50);
-            } catch (Exception e) {}
-            System.err.println("Writing ");
-
-            io.getErr().println (i + ": This is a not so long line");
-            out.println (i + ": This, on the other hand, is a relatively short line");
-            if (stopped) {
-                out.println("Some dastardly individual has STOPPED me!");
-                out.flush();
-                out.close();
-                io.getErr().close();
-                return;
-            }
-        }
-         */
-/*        for (int i=0; i < 2000; i++) {
-            io.getErr().println("Scroll me " + i);
-            if (i %20 == 0) {
-                io.getErr().println("This is a longer line.  A much longer line.  A very long line.  You'd be surprised how long it would be - maybe long enought to wrap - or at least that's the idea and the master plan, right?  Well, we'll hope so");
-            }
-        }
- */
-        /*
-            for (int i=0; i < 100; i++) {
-                io.getErr().println("0 Scroll me " + i);
-                if (i %2 == 0) {
-                    io.getErr().println("1 This is a longer line.  A much longer line.  A very long line.  You'd be surprised how long it would be - maybe long enought to wrap - or at least that's the idea and the master plan, right?  Well, we'll hope so");
-                    io.getOut().println("2 This is a shorter line");
-                    io.getErr().println("3 This Well, this one isn't so bad.  But it could be long too.  I mean, then we'd have more long lines.  Are we sure that's a good idea?  I'm not.  So just go away, why don't you!  I don't want to do this anyways!");
-                }
-                if (i % 73 == 0) {
-                    io.getErr().println("Well, ain't this special - a multiple of 73!  Test for the user, where will the next one show up?  Ha ha, I don't know.  What the fuck's the point of this, anyway?  Beats the shit out of me.  What do you thingk?  Would you like to be a dog with a headdress?");
-                }
-            }
-         */
         for (int i=0; i < 100000; i++) {
             io.getErr().println("Scroll me " + i);
             if (i %20 == 0) {
@@ -95,7 +43,7 @@
                 io.getErr().println("This Well, this one isn't so bad.  But it could be long too.  I mean, then we'd have more long lines.  Are we sure that's a good idea?  I'm not.  So just go away, why don't you!  I don't want to do this anyways!");
             }
             if (i % 73 == 0) {
-                io.getErr().println("Well, ain't this special - a multiple of 73!  Test for the user, where will the next one show up?  Ha ha, I don't know.  What the fuck's the point of this, anyway?  Beats the shit out of me.  What do you thingk?  Would you like to be a dog with a headdress?");
+                io.getErr().println("Grumble, grumble, I am a multiple of 73");
 //                io.getErr().println("file:/tmp/file:23");
             }
         }
