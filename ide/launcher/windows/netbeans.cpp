223eb60bac170f602a93ba63b8169fee9f25cfb2
ttran <ttran@netbeans.org>
2004-09-03 15:43:14 +0000

    #48441: can't run IDE if JDK is in directory with space #48089: the IDE should recognize  ${userdir}/etc/netbeans.conf

--- ide/launcher/windows/netbeans.cpp
+++ ide/launcher/windows/netbeans.cpp
@@ -20,12 +20,21 @@
 #include <commdlg.h>
 
 static char* getUserHomeFromRegistry(char* userhome);
-static char * GetStringValue(HKEY key, const char *name);
+static char* GetStringValue(HKEY key, const char *name);
+static void parseConfigFile(const char* path);
+static void parseArgs(int argc, char *argv[]);
+
+static char userdir[MAX_PATH] = "c:\\nbuser";
+static char options[4098] = "";
+static char dirs[4098] = "", extradirs[4098];
+static char jdkswitch[MAX_PATH] = "";
 
 #ifdef WINMAIN
 
+static void parseCommandLine(char *argstr);
+
 int WINAPI
-    WinMain (HINSTANCE hSelf, HINSTANCE hPrev, LPSTR cmdline, int nShow) {
+    WinMain (HINSTANCE /* hSelf */, HINSTANCE /* hPrev */, LPSTR cmdline, int /* nShow */) {
 #else
     int main(int argc, char* argv[]) {
         char cmdline[10240] = "";
@@ -37,9 +46,7 @@ int WINAPI
         }
 #endif    
 
-    char topdir[MAX_PATH], userdir[MAX_PATH] = "c:\\nbuser", options[4098] = "";
-    char dirs[4098] = "", extradirs[4098];
-    char jdkswitch[MAX_PATH] = "";
+    char topdir[MAX_PATH];
     char buf[MAX_PATH], *pc;
   
     GetModuleFileName(0, buf, sizeof buf);
@@ -56,12 +63,130 @@ int WINAPI
     strcpy(topdir, buf);
 
     sprintf(buf, "%s\\etc\\netbeans.conf", topdir);
+    parseConfigFile(buf);
+
+#ifdef WINMAIN
+    parseCommandLine(cmdline);
+#else
+    parseArgs(argc - 1, argv + 1); // skip progname
+#endif    
+
+    char olduserdir[MAX_PATH];
+    strcpy(olduserdir, userdir);
+    sprintf(buf, "%s\\etc\\netbeans.conf", userdir);
+    parseConfigFile(buf);
+    strcpy(userdir, olduserdir);
     
-    FILE* fin = fopen(buf, "r");
-    if (fin == NULL)
+    char nbexec[MAX_PATH];
+    char cmdline2[10240];
+    
+    if (dirs[0] == '\0') {
+        WIN32_FIND_DATA ffd;
+        HANDLE ffh;
+        
+        sprintf(buf, "%s\\extra", topdir);
+    
+        memset(&ffd, 0, sizeof ffd);
+        ffd.dwFileAttributes = FILE_ATTRIBUTE_DIRECTORY;
+        ffh = FindFirstFile(buf, &ffd);
+        if (ffh != INVALID_HANDLE_VALUE) {
+            FindClose(ffh);
+            sprintf(dirs, "%s\\%s;%s\\%s;%s\\%s",
+                    topdir, "\\nb4.0", topdir, "\\ide4", topdir, "\\extra");
+        } else {
+            sprintf(dirs, "%s\\%s;%s\\%s",
+                    topdir, "\\nb4.0", topdir, "\\ide4" );
+        }
+    }
+    if (extradirs[0] != '\0') {
+        strcat(strcat(dirs, ";"), extradirs);
+    }
+    
+    sprintf(nbexec, "%s\\platform4\\lib\\nbexec.exe", topdir);
+    sprintf(cmdline2, "\"%s\" %s -J-Dnetbeans.importclass=org.netbeans.upgrade.AutoUpgrade --branding nb --clusters \"%s\" --userdir \"%s\" %s %s",
+            nbexec,
+            jdkswitch,
+            dirs,
+            userdir,
+            options,
+            cmdline);
+
+    STARTUPINFO start;
+    PROCESS_INFORMATION pi;
+
+    memset (&start, 0, sizeof (start));
+    start.cb = sizeof (start);
+
+#ifdef WINMAIN
+    start.dwFlags = STARTF_USESHOWWINDOW;
+    start.wShowWindow = SW_HIDE;
+#endif
+    
+    if (!CreateProcess (NULL, cmdline2,
+                        NULL, NULL, TRUE, NORMAL_PRIORITY_CLASS,
+                        NULL, NULL,
+                        &start,
+                        &pi)) {
+        MessageBox(NULL, "Cannot start the IDE", "Error", MB_ICONSTOP | MB_OK);
         exit(1);
+    } else {
+        // Wait until child process exits.
+        WaitForSingleObject( pi.hProcess, INFINITE );
+
+        // Close process and thread handles. 
+        CloseHandle( pi.hProcess );
+        CloseHandle( pi.hThread );        
+        exit(0);
+    }
+    return 0;
+}
     
-    char line[2048];
+char* getUserHomeFromRegistry(char* userhome)
+{
+    HKEY key;
+
+    if (RegOpenKeyEx(
+            HKEY_CURRENT_USER,
+            "Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Shell Folders",
+            0,
+            KEY_READ,
+            &key) != 0)
+        return NULL;
+
+    char *path = GetStringValue(key, "Desktop");
+    RegCloseKey(key);
+    
+    char *pc = strrchr(path, '\\');
+    if (pc == NULL) {
+	return NULL;
+    }
+    *pc = '\0';
+    strcpy(userhome, path);
+    return userhome;
+}
+
+char * GetStringValue(HKEY key, const char *name)
+{
+    DWORD type, size;
+    char *value = 0;
+
+    if (RegQueryValueEx(key, name, 0, &type, 0, &size) == 0 && type == REG_SZ) {
+        value = (char*) malloc(size);
+        if (RegQueryValueEx(key, name, 0, 0, (unsigned char*)value, &size) != 0) {
+            free(value);
+            value = 0;
+        }
+    }
+    return value;
+}
+
+void parseConfigFile(const char* path) {
+    FILE* fin = fopen(path, "r");
+    if (fin == NULL)
+        return;
+    
+    char line[2048], *pc;
+    
     while (NULL != fgets(line, sizeof line, fin)) {
         for (pc = line; *pc != '\0' && (*pc == ' ' || *pc == '\t' || *pc == '\n' || *pc == '\r'); pc++)
             ;
@@ -141,107 +266,139 @@ int WINAPI
         }
     }
     fclose(fin);
-
-    char nbexec[MAX_PATH];
-    char cmdline2[10240];
-    
-    if (dirs[0] == '\0') {
-        int rc = 0;
-        WIN32_FIND_DATA ffd;
-        HANDLE ffh;
-        
-        sprintf(buf, "%s\\extra", topdir);
-    
-        memset(&ffd, 0, sizeof ffd);
-        ffd.dwFileAttributes = FILE_ATTRIBUTE_DIRECTORY;
-        ffh = FindFirstFile(buf, &ffd);
-        if (ffh != INVALID_HANDLE_VALUE) {
-            FindClose(ffh);
-            sprintf(dirs, "%s\\%s;%s\\%s;%s\\%s",
-                    topdir, "\\nb4.0", topdir, "\\ide4", topdir, "\\extra");
-        } else {
-            sprintf(dirs, "%s\\%s;%s\\%s",
-                    topdir, "\\nb4.0", topdir, "\\ide4" );
-        }
-    }
-    if (extradirs[0] != '\0') {
-        strcat(strcat(dirs, ";"), extradirs);
-    }
-    
-    sprintf(nbexec, "%s\\platform4\\lib\\nbexec.exe", topdir);
-    sprintf(cmdline2, "\"%s\" %s -J-Dnetbeans.importclass=org.netbeans.upgrade.AutoUpgrade --branding nb --clusters \"%s\" --userdir \"%s\" %s %s",
-            nbexec,
-            jdkswitch,
-            dirs,
-            userdir,
-            options,
-            cmdline);
-
-    STARTUPINFO start;
-    PROCESS_INFORMATION pi;
-
-    memset (&start, 0, sizeof (start));
-    start.cb = sizeof (start);
+}
 
 #ifdef WINMAIN
-    start.dwFlags = STARTF_USESHOWWINDOW;
-    start.wShowWindow = SW_HIDE;
-#endif
+
+void parseCommandLine(char *argstr) {
+    char **argv = (char**) malloc(2048 * sizeof (char*));
+    int argc = 0;
+
+#define START 0
+#define NORMAL 1
+#define IN_QUOTES 2
+#define IN_APOS 3
+
+    char *p, *q;
+    int state = NORMAL;
+    char token[1024 * 64];
+    int eof;
+  
+    q = argstr;
+    p = token;
+    state = START;
+    eof = 0;
+  
+    while (!eof) {
+        if (*q == '\0')
+            eof = 1;
     
-    if (!CreateProcess (NULL, cmdline2,
-                        NULL, NULL, TRUE, NORMAL_PRIORITY_CLASS,
-                        NULL, NULL,
-                        &start,
-                        &pi)) {
-        MessageBox(NULL, "Cannot start the IDE", "Error", MB_ICONSTOP | MB_OK);
-        exit(1);
-    } else {
-        // Wait until child process exits.
-        WaitForSingleObject( pi.hProcess, INFINITE );
+        switch (state) {
+            case START:
+                if (*q == ' ' || *q == '\r' || *q == '\n' || *q == '\t') {
+                    q++;
+                    continue;
+                }
 
-        // Close process and thread handles. 
-        CloseHandle( pi.hProcess );
-        CloseHandle( pi.hThread );        
-        exit(0);
-    }
-    return 0;
-}
+                p = token;
+                *p = '\0';
+      
+                if (*q == '"') {
+                    state = IN_QUOTES;
+                    q++;
+                    continue;
+                }
+                if (*q == '\'') {
+                    state = IN_APOS;
+                    q++;
+                    continue;
+                }
+      
+                state = NORMAL;
+                continue;
 
-char* getUserHomeFromRegistry(char* userhome)
-{
-    HKEY key;
+            case NORMAL:
+                if (*q == ' ' || *q == '\r' || *q == '\n' || *q == '\t' || *q == '\0') {
+                    *p = '\0';
+                    argv[argc] = strdup(token);
+                    argc++;
+                    state = START;
+                } else {
+                    *p++ = *q++;
+                }
+                break;
 
-    if (RegOpenKeyEx(
-            HKEY_CURRENT_USER,
-            "Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Shell Folders",
-            0,
-            KEY_READ,
-            &key) != 0)
-        return NULL;
+            case IN_QUOTES:
+                if (*q == '"') {
+                    if (*(q+1) == '"') {
+                        *p++ = '"';
+                        q += 2;
+                    } else {
+                        *p = '\0';
+          
+                        argv[argc] = strdup(token);
+                        argc++;
 
-    char *path = GetStringValue(key, "Desktop");
-    RegCloseKey(key);
-    
-    char *pc = strrchr(path, '\\');
-    if (pc == NULL) {
-	return NULL;
-    }
-    *pc = '\0';
-    strcpy(userhome, path);
-    return userhome;
-}
+                        q++;
+                        state = START;
+                    }
+                } else if (*q == '\0') {
+                    *p = '\0';
+                    argv[argc] = strdup(token);
+                    argc++;
+                    state = START;
+                } else {
+                    *p++ = *q++;
+                }
+                break;
+      
+            case IN_APOS:
+                if (*q == '\'') {
+                    if (*(q+1) == '\'') {
+                        *p++ = '\'';
+                        q += 2;
+                    } else {
+                        *p = '\0';
+          
+                        argv[argc] = strdup(token);
+                        argc++;
 
-char * GetStringValue(HKEY key, const char *name)
-{
-    DWORD type, size;
-    char *value = 0;
-
-    if (RegQueryValueEx(key, name, 0, &type, 0, &size) == 0 && type == REG_SZ) {
-        value = (char*) malloc(size);
-        if (RegQueryValueEx(key, name, 0, 0, (unsigned char*)value, &size) != 0) {
-            free(value);
-            value = 0;
+                        q++;
+                        state = START;
+                    }
+                } else if (*q == '\0') {
+                    *p = '\0';
+                    argv[argc] = strdup(token);
+                    argc++;
+                    state = START;
+                } else {
+                    *p++ = *q++;
+                }
+                break;
         }
     }
-    return value;
+  
+    parseArgs(argc, argv);
+
+}
+#endif // WINMAIN
+
+void parseArgs(int argc, char *argv[]) {
+    char *arg;
+
+    while (argc > 0 && (arg = *argv) != 0) {
+        argv++;
+        argc--;
+
+        if (0 == strcmp("-userdir", arg) || 0 == strcmp("--userdir", arg)) {
+            if (argc > 0) {
+                arg = *argv;
+                argv++;
+                argc--;
+                if (arg != 0) {
+                    strcpy(userdir, arg);
+                }
+            }
+        }
+    }
 }
