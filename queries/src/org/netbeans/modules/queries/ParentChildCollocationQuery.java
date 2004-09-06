f923e287023e9f3d7a47a718a91da07fa1ef4a5a
dkonecny <dkonecny@netbeans.org>
2004-09-06 11:56:08 +0000

    #43793 - absolute links created when referencing JAR inside project folder. Moving implementation from projects/projectapi module to projects/queries. Adding unit test.

---
+++ queries/src/org/netbeans/modules/queries/ParentChildCollocationQuery.java
@@ -0,0 +1,50 @@
+/*
+ *                 Sun Public License Notice
+ *
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ *
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.modules.queries;
+
+import java.io.File;
+import org.netbeans.spi.queries.CollocationQueryImplementation;
+
+
+/**
+ * Tests whether files are in parent-child relationship. Such files are
+ * considered to be collocated.
+ *
+ * @author David Konecny
+ */
+public class ParentChildCollocationQuery implements CollocationQueryImplementation {
+    
+    /** Default constructor for lookup. */
+    public ParentChildCollocationQuery() {}
+
+    public boolean areCollocated(File file1, File file2) {
+        if (file1.equals(file2)) {
+            return false;
+        }
+        String f1 = file1.getAbsolutePath();
+        if ((file1.isDirectory() || !file1.exists()) && !f1.endsWith(File.separator)) {
+            f1 += File.separatorChar;
+        }
+        String f2 = file2.getAbsolutePath();
+        if ((file2.isDirectory() || !file2.exists()) && !f2.endsWith(File.separator)) {
+            f2 += File.separatorChar;
+        }
+        return f1.startsWith(f2) || f2.startsWith(f1);
+    }
+    
+    public File findRoot(File file) {
+        return null;
+    }
+    
+}
---
+++ queries/test/unit/src/org/netbeans/modules/queries/ParentChildCollocationQueryTest.java
@@ -0,0 +1,65 @@
+/*
+ *                 Sun Public License Notice
+ *
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ *
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.modules.queries;
+
+import java.io.File;
+import junit.framework.TestCase;
+import org.netbeans.junit.NbTestCase;
+
+/**
+ * Test of ParentChildCollocationQuery impl.
+ *
+ * @author David Konecny
+ */
+public class ParentChildCollocationQueryTest extends NbTestCase {
+    
+    public ParentChildCollocationQueryTest(String testName) {
+        super(testName);
+    }
+
+    public void testAreCollocated() throws Exception {
+        clearWorkDir();
+        File base = getWorkDir();
+        File proj1 = new File(base, "proj1");
+        proj1.mkdirs();
+        File proj3 = new File(proj1, "proj3");
+        proj3.mkdirs();
+        File proj2 = new File(base, "proj2");
+        proj2.mkdirs();
+        
+        ParentChildCollocationQuery query = new ParentChildCollocationQuery();
+        assertTrue("Must be collocated", query.areCollocated(proj1, proj3));
+        assertTrue("Must be collocated", query.areCollocated(proj3, proj1));
+        assertFalse("Cannot be collocated", query.areCollocated(proj1, proj2));
+        assertFalse("Cannot be collocated", query.areCollocated(proj2, proj1));
+        
+        // folder does not exist:
+        File proj4 = new File(base, "proj");
+        assertFalse("Cannot be collocated", query.areCollocated(proj1, proj4));
+        assertFalse("Cannot be collocated", query.areCollocated(proj4, proj1));
+        proj4.mkdirs();
+        assertFalse("Cannot be collocated", query.areCollocated(proj1, proj4));
+        assertFalse("Cannot be collocated", query.areCollocated(proj4, proj1));
+        
+        // files do not exist:
+        File file1 = new File(base, "file1.txt");
+        File file2 = new File(base, "file1");
+        assertFalse("Cannot be collocated", query.areCollocated(file1, file2));
+        assertFalse("Cannot be collocated", query.areCollocated(file2, file1));
+        
+        // passing the same parameter
+        assertFalse("Cannot be collocated", query.areCollocated(proj1, proj1));
+    }
+
+}
