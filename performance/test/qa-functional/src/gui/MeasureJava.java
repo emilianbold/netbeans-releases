fae3b32cb94ea174961a54cbf392d5fbd647e318
mmirilovic <mmirilovic@netbeans.org>
2004-09-23 10:24:28 +0000

    First attempt to create module related test config

--- performance/test/qa-functional/src/gui/MeasureJava.java
+++ performance/test/qa-functional/src/gui/MeasureJava.java
@@ -29,62 +29,62 @@ public class MeasureJava  {
     public static NbTestSuite suite() {
         NbTestSuite suite = new NbTestSuite();
 
-//        suite.addTest(new ExpandNodesProjectsView("testExpandProjectNode", "Expand Project node"));
-//        suite.addTest(new ExpandNodesProjectsView("testExpandSourcePackagesNode", "Expand Source Packages node"));
-//        suite.addTest(new ExpandNodesProjectsView("testExpandFolderWith50JavaFiles", "Expand folder with 50 java files"));
-//        suite.addTest(new ExpandNodesProjectsView("testExpandFolderWith100JavaFiles", "Expand folder with 100 java files"));
-//
-//        suite.addTest(new OpenFiles("testOpening20kBJavaFile", "Open Java file (20kB)"));
-//        suite.addTest(new OpenFilesWithOpenedEditor("testOpening20kBJavaFile", "Open Java file (20kB) if Editor opened"));
-//
-//        suite.addTest(new OpenJspFile("testOpening20kBJSPFile", "Open JSP file"));
-//        suite.addTest(new OpenJspFileWithOpenedEditor("testOpening20kBJSPFile", "Open JSP file if Editor opened"));
-//        
-//        suite.addTest(new PasteInEditor("measureTime", "Paste in the editor"));
-//        suite.addTest(new PageUpPageDownInEditor("measureTime", "Press Page Up in the editor", true));
-//        suite.addTest(new PageUpPageDownInEditor("measureTime", "Press Page Down in the editor", false));
-//        
-//        suite.addTest(new JavaCompletionInEditor("measureTime", "Invoke Code Completion dialog in Editor"));
-//        suite.addTest(new TypingInEditor("measureTime", "Type a character in Editor"));
-//
-//
-//        suite.addTest(new CloseEditor("testClosing20kBJavaFile", "Close Java file (20kB)"));
-//        
-//        suite.addTest(new CloseEditorModified("testClosingModifiedJavaFile", "Close modified Java file"));
-//        
-//        suite.addTest(new SaveModifiedFile("testSaveModifiedJavaFile", "Save modified Java file"));
-//
-//        suite.addTest(new RefreshFolder("testRefreshFolderWith50JavaFiles", "Refresh folder with 50 java files"));
-//        suite.addTest(new RefreshFolder("testRefreshFolderWith100JavaFiles", "Refresh folder with 100 java files"));
-//        
-//        suite.addTest(new DeleteFolder("testDeleteFolderWith50JavaFiles", "Delete folder with 50 java files"));
-//        suite.addTest(new DeleteFolder("testDeleteFolderWith100JavaFiles", "Delete folder with 100 java files"));
-//        
-//        suite.addTest(new CreateProject("testCreateJavaApplicationProject", "Create Java Application project"));
-//        suite.addTest(new CreateProject("testCreateJavaLibraryProject", "Create Java Library project"));
-//        suite.addTest(new CreateProject("testCreateWebApplicationProject", "Create Web Application project"));
-//        
-//        suite.addTest(new ProjectsViewPopupMenu("testPackagePopupMenuProjects", "Package node popup in Projects View"));
-//        suite.addTest(new ProjectsViewPopupMenu("testJavaFilePopupMenuProjects", "Java file node popup in Projects View"));
-//        suite.addTest(new ProjectsViewPopupMenu("testJspFilePopupMenuProjects", "Jsp file node popup in Projects View"));
-//        
-//        suite.addTest(new FilesViewPopupMenu("testProjectNodePopupMenuFiles", "Project node popup in Files View"));
-//        suite.addTest(new FilesViewPopupMenu("testPackagePopupMenuFiles", "Package node popup in Files View"));
-//        
-//        suite.addTest(new ToolsMenu("testJavaToolsMenu", "Tools main menu for Java node"));
-//
-//        suite.addTest(new SourceEditorPopupMenu("testPopupOnMethod", "Java Editor Method popup"));
-//        suite.addTest(new SourceEditorPopupMenu("testPopupOnClassName", "Java Editor Class Name popup"));
-//
-//        suite.addTest(new ToDoWindow("measureTime", "To Do window open"));
-//
-//        suite.addTest(new OverrideMethods("measureTime", "Override and Implement Methods dialog open"));
-//        
-//        suite.addTest(new AutoCommentWindow("measureTime", "Auto Comment Tool open"));
-//
-//        suite.addTest(new RefactorFindUsagesDialog("measureTime", "Refactor find usages dialog open"));
-//        suite.addTest(new RefactorRenameDialog("measureTime", "Refactor rename dialog open"));
-//        suite.addTest(new RefactorMoveClassDialog("measureTime", "Refactor move class dialog open"));
+        suite.addTest(new ExpandNodesProjectsView("testExpandProjectNode", "Expand Project node"));
+        suite.addTest(new ExpandNodesProjectsView("testExpandSourcePackagesNode", "Expand Source Packages node"));
+        suite.addTest(new ExpandNodesProjectsView("testExpandFolderWith50JavaFiles", "Expand folder with 50 java files"));
+        suite.addTest(new ExpandNodesProjectsView("testExpandFolderWith100JavaFiles", "Expand folder with 100 java files"));
+
+        suite.addTest(new OpenFiles("testOpening20kBJavaFile", "Open Java file (20kB)"));
+        suite.addTest(new OpenFilesWithOpenedEditor("testOpening20kBJavaFile", "Open Java file (20kB) if Editor opened"));
+
+        suite.addTest(new OpenJspFile("testOpening20kBJSPFile", "Open JSP file"));
+        suite.addTest(new OpenJspFileWithOpenedEditor("testOpening20kBJSPFile", "Open JSP file if Editor opened"));
+        
+        suite.addTest(new PasteInEditor("measureTime", "Paste in the editor"));
+        suite.addTest(new PageUpPageDownInEditor("measureTime", "Press Page Up in the editor", true));
+        suite.addTest(new PageUpPageDownInEditor("measureTime", "Press Page Down in the editor", false));
+        
+        suite.addTest(new JavaCompletionInEditor("measureTime", "Invoke Code Completion dialog in Editor"));
+        suite.addTest(new TypingInEditor("measureTime", "Type a character in Editor"));
+
+
+        suite.addTest(new CloseEditor("testClosing20kBJavaFile", "Close Java file (20kB)"));
+        
+        suite.addTest(new CloseEditorModified("testClosingModifiedJavaFile", "Close modified Java file"));
+        
+        suite.addTest(new SaveModifiedFile("testSaveModifiedJavaFile", "Save modified Java file"));
+
+        suite.addTest(new RefreshFolder("testRefreshFolderWith50JavaFiles", "Refresh folder with 50 java files"));
+        suite.addTest(new RefreshFolder("testRefreshFolderWith100JavaFiles", "Refresh folder with 100 java files"));
+        
+        suite.addTest(new DeleteFolder("testDeleteFolderWith50JavaFiles", "Delete folder with 50 java files"));
+        suite.addTest(new DeleteFolder("testDeleteFolderWith100JavaFiles", "Delete folder with 100 java files"));
+        
+        suite.addTest(new CreateProject("testCreateJavaApplicationProject", "Create Java Application project"));
+        suite.addTest(new CreateProject("testCreateJavaLibraryProject", "Create Java Library project"));
+        suite.addTest(new CreateProject("testCreateWebApplicationProject", "Create Web Application project"));
+        
+        suite.addTest(new ProjectsViewPopupMenu("testPackagePopupMenuProjects", "Package node popup in Projects View"));
+        suite.addTest(new ProjectsViewPopupMenu("testJavaFilePopupMenuProjects", "Java file node popup in Projects View"));
+        suite.addTest(new ProjectsViewPopupMenu("testJspFilePopupMenuProjects", "Jsp file node popup in Projects View"));
+        
+        suite.addTest(new FilesViewPopupMenu("testProjectNodePopupMenuFiles", "Project node popup in Files View"));
+        suite.addTest(new FilesViewPopupMenu("testPackagePopupMenuFiles", "Package node popup in Files View"));
+        
+        suite.addTest(new ToolsMenu("testJavaToolsMenu", "Tools main menu for Java node"));
+
+        suite.addTest(new SourceEditorPopupMenu("testPopupOnMethod", "Java Editor Method popup"));
+        suite.addTest(new SourceEditorPopupMenu("testPopupOnClassName", "Java Editor Class Name popup"));
+
+        suite.addTest(new ToDoWindow("measureTime", "To Do window open"));
+
+        suite.addTest(new OverrideMethods("measureTime", "Override and Implement Methods dialog open"));
+        
+        suite.addTest(new AutoCommentWindow("measureTime", "Auto Comment Tool open"));
+
+        suite.addTest(new RefactorFindUsagesDialog("measureTime", "Refactor find usages dialog open"));
+        suite.addTest(new RefactorRenameDialog("measureTime", "Refactor rename dialog open"));
+        suite.addTest(new RefactorMoveClassDialog("measureTime", "Refactor move class dialog open"));
         
         suite.addTest(new RefactorFindUsages("measureTime", "Refactor find usages"));
         
