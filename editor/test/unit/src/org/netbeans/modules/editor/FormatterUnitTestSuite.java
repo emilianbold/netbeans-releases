356c773d2e69ce9a52c4ecb6582f8e801676086c
mmetelka <mmetelka@netbeans.org>
2004-10-01 14:14:06 +0000

    Implemented #42364 - add asterisk-space in javadoc when text after caret is present

---
+++ editor/test/unit/src/org/netbeans/modules/editor/FormatterUnitTestSuite.java
@@ -0,0 +1,26 @@
+package org.netbeans.modules.editor;
+
+import junit.framework.TestSuite;
+import org.netbeans.junit.NbTestSuite;
+import org.netbeans.modules.editor.java.JavaFormatterUnitTest;
+
+
+/**
+ * Test behavior of smart brackets feature.
+ *
+ * @author Miloslav Metelka
+ */
+  public class FormatterUnitTestSuite extends NbTestSuite {
+      
+    public FormatterUnitTestSuite() {
+        super("Formatter");
+        
+        addTestSuite(JavaFormatterUnitTest.class);
+    }
+    
+
+    public static NbTestSuite suite() {
+        return new FormatterUnitTestSuite();
+    }
+    
+}
