d6c6defafb1bea6cdb27ce1f96bc05d1e5af4535
jskrivanek <jskrivanek@netbeans.org>
2004-10-13 16:01:06 +0000

    Added popup action.

--- jellytools/src/org/netbeans/jellytools/actions/CompileAction.java
+++ jellytools/src/org/netbeans/jellytools/actions/CompileAction.java
@@ -15,7 +15,7 @@ package org.netbeans.jellytools.actions;
 import java.awt.event.KeyEvent;
 import org.netbeans.jellytools.Bundle;
 
-/** Used to call "Build|Compile File" main menu item or F9 shortcut.
+/** Used to call "Build|Compile File" main menu item, "Compile File" popup menu or F9 shortcut.
  * @see Action
  * @author <a href="mailto:adam.sotona@sun.com">Adam Sotona</a> 
  */
@@ -25,9 +25,11 @@ public class CompileAction extends Action {
     private static final String compileMenu = Bundle.getStringTrimmed("org.netbeans.core.Bundle", "Menu/Build")+"|"
                                             +Bundle.getStringTrimmed("org.netbeans.modules.project.ui.actions.Bundle", "LBL_CompileSingleAction_Name");
     private static final Shortcut compileShortcut = new Shortcut(KeyEvent.VK_F9);
-
+    // Compile File
+    private static final String compilePopup = Bundle.getString("org.netbeans.modules.java.project.Bundle", "LBL_CompileFile_Action");
+    
     /** creates new CompileAction instance */    
     public CompileAction() {
-        super(compileMenu, null, null, compileShortcut);
+        super(compileMenu, compilePopup, null, compileShortcut);
     }
 }
