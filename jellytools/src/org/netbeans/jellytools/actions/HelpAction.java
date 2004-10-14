56e3debe22b29e7db587087c4cf1b12300ba6e93
jskrivanek <jskrivanek@netbeans.org>
2004-10-14 13:46:07 +0000

    Removed API action.

--- jellytools/src/org/netbeans/jellytools/actions/HelpAction.java
+++ jellytools/src/org/netbeans/jellytools/actions/HelpAction.java
@@ -16,7 +16,7 @@ import java.awt.event.KeyEvent;
 import org.netbeans.jellytools.Bundle;
 
 /** Used to call "Help|Help Contents" main menu item,
- * "org.openide.actions.HelpAction" or F1 shortcut. It can also be used
+ * or F1 shortcut. It can also be used
  * to invoke help on a property sheet from popup menu.
  * @see Action
  * @author <a href="mailto:adam.sotona@sun.com">Adam Sotona</a> */
@@ -27,27 +27,12 @@ public class HelpAction extends Action {
     private static final String helpMenu = Bundle.getStringTrimmed("org.netbeans.core.Bundle", "Menu/Help")
                                          + "|" 
                                          + Bundle.getStringTrimmed("org.netbeans.modules.usersguide.Bundle", "Menu/Help/org-netbeans-modules-usersguide-master.xml");
-    /* --- Helpsets subdirectory not used in NetBeans 3.6 ---
-    private static final String helpSetsMenu = Bundle.getStringTrimmed("org.netbeans.core.Bundle", "Menu/Help")
-                                             + "|" 
-                                             + Bundle.getStringTrimmed("org.netbeans.modules.javahelp.resources.Bundle", "Menu/Help/HelpShortcuts")
-                                             + "|";
-     */
     private static final Shortcut helpShortcut = new Shortcut(KeyEvent.VK_F1);
-
-    /** Creates new HelpAction instance for specific help set.
-     * @param helpSet menu item of help set to be showed (e.g. "Core IDE Help")
-     */
-    /* --- Helpsets subdirectory not used in NetBeans 3.6 ---
-    public HelpAction(String helpSet) {
-        super(helpSetsMenu+helpSet, null);
-    }
-     */
 
     /** Creates new HelpAction instance for master help set (Help|Contents)
      * or for generic use e.g. in property sheets.
      */
     public HelpAction() {
-        super(helpMenu, popupPath, "org.openide.actions.HelpAction", helpShortcut);
+        super(helpMenu, popupPath, null, helpShortcut);
     }
 }
