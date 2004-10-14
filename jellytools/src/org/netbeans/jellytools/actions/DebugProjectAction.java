5b0db8fc820c7b96c7699d4f5e14a4a264158c9d
jskrivanek <jskrivanek@netbeans.org>
2004-10-14 13:36:28 +0000

    API action removed.

--- jellytools/src/org/netbeans/jellytools/actions/DebugProjectAction.java
+++ jellytools/src/org/netbeans/jellytools/actions/DebugProjectAction.java
@@ -7,7 +7,7 @@
  * http://www.sun.com/
  * 
  * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2002 Sun
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 package org.netbeans.jellytools.actions;
@@ -16,20 +16,25 @@ import java.awt.event.KeyEvent;
 import org.netbeans.jellytools.Bundle;
 
 /** Used to call "Debug Project" popup menu item on project's root node,
- * "Project|Debug Project" main menu item,
- * "org.netbeans.modules.projects.actions.DebugProjectAction" or Ctrl+Shift+F5 shortcut.
+ * "Run|Debug Main Project" main menu item or F5 shortcut.
  * @see Action
  * @see org.netbeans.jellytools.nodes.ProjectRootNode
- * @author <a href="mailto:adam.sotona@sun.com">Adam Sotona</a> */
+ * @author <a href="mailto:adam.sotona@sun.com">Adam Sotona</a> 
+ * @author Jiri.Skrivanek@sun.com
+ */
 public class DebugProjectAction extends Action {
     
-    private static final String debugProjectPopup = Bundle.getStringTrimmed("org.netbeans.modules.projects.actions.Bundle", "ProjectDebug");
-    private static final String debugProjectMenu = Bundle.getStringTrimmed("org.netbeans.modules.projects.Bundle", "Menu/Project")
-                                            + "|" + debugProjectPopup;
-    private static final Shortcut debugProjectShortcut = new Shortcut(KeyEvent.VK_F5, KeyEvent.SHIFT_MASK | KeyEvent.CTRL_MASK);
+    // "Debug Project"
+    private static final String debugProjectPopup = Bundle.getString("org.netbeans.modules.java.j2seproject.ui.Bundle", "LBL_DebugAction_Name");
+    // "Run|Debug Main Project"
+    private static final String debugProjectMenu = 
+            Bundle.getStringTrimmed("org.netbeans.modules.project.ui.Bundle", "Menu/RunProject")+
+            "|"+
+            Bundle.getStringTrimmed("org.netbeans.modules.project.ui.actions.Bundle", "LBL_DebugMainProjectAction_Name");
+    private static final Shortcut debugProjectShortcut = new Shortcut(KeyEvent.VK_F5);
 
     /** creates new DebugProjectAction instance */    
     public DebugProjectAction() {
-        super(debugProjectMenu, debugProjectPopup, "org.netbeans.modules.projects.actions.DebugProjectAction", debugProjectShortcut);
+        super(debugProjectMenu, debugProjectPopup, null, debugProjectShortcut);
     }
 }
