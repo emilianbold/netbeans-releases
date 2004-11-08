718e25e42e4249d5f3c26ca96b118b83e1e7d935
cwebster <cwebster@netbeans.org>
2004-11-08 17:06:01 +0000

    IN=41978 - Add ability to generate message driven beans

--- j2ee.ejbjarproject/src/org/netbeans/modules/j2ee/ejbjarproject/EjbJarProject.java
+++ j2ee.ejbjarproject/src/org/netbeans/modules/j2ee/ejbjarproject/EjbJarProject.java
@@ -457,6 +457,7 @@ final class EjbJarProject implements Project, AntProjectListener {
             
             "Templates/J2EE/Session", // NOI18N
             "Templates/J2EE/RelatedCMP", // NOI18N
+            "Templates/J2EE/Message", //NOI18N
             "Templates/Classes/Class.java", // NOI18N
             "Templates/Other/Folder", // NOI18N
             "Templates/J2EE/WebService"
