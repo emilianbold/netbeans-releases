6891ffc527d6aa3dfc8213e5f5d358f4b930bb58
jrechtacek <jrechtacek@netbeans.org>
2004-11-16 08:56:19 +0000

    better clean of workdir after test-run

--- java.project/test/unit/src/org/netbeans/modules/java/project/JavaTargetChooserPanelTest.java
+++ java.project/test/unit/src/org/netbeans/modules/java/project/JavaTargetChooserPanelTest.java
@@ -14,7 +14,6 @@
 package org.netbeans.modules.java.project;
 
 import java.io.File;
-import junit.framework.*;
 import org.netbeans.junit.NbTestCase;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
@@ -24,49 +23,44 @@ import org.openide.filesystems.FileUtil;
  * @author Jiri Rechtacek
  */
 public class JavaTargetChooserPanelTest extends NbTestCase {
+    FileObject root = null;
     
     public JavaTargetChooserPanelTest (String testName) {
         super (testName);
     }
-
-    protected void setUp () throws java.lang.Exception {
+    
+    protected void tearDown () throws Exception {
+        // clean up test folders
+        if (root != null) {
+            root.delete ();
+        }
     }
-
+    
     public static junit.framework.Test suite () {
-        junit.framework.TestSuite suite = new junit.framework.TestSuite(JavaTargetChooserPanelTest.class);
+        junit.framework.TestSuite suite = new junit.framework.TestSuite (JavaTargetChooserPanelTest.class);
         
         return suite;
     }
-
+    
     // tests #51135: checked existence of multiple dotted package name
     public void testCanUseFileName () throws Exception {
-        FileObject root = null;
-        try {
-            File rootFile = getWorkDir ();
-            assertNotNull ("WorkDir exists.", rootFile);
-            root = FileUtil.toFileObject (rootFile);
-            if (!root.canWrite ()) {
-                fail ("Cannot create test folder.");
-            }
-
-            root = root.createFolder ("testCanUseFileName");
-
-            assertNotNull (root + " exists.", FileUtil.toFile(root));
-            assertTrue ("Package aaa.bbb.ccc can be created.", JavaTargetChooserPanel.canUseFileName (root, "", "aaa.bbb.ccc", "") == null);
-            
-            assertNotNull ("Package aaa.bbb.ccc was created.", root.createFolder ("aaa").createFolder ("bbb").createFolder ("ccc"));
-            assertTrue ("Package aaa cannot be created.", JavaTargetChooserPanel.canUseFileName (root, "", "aaa", "") != null);
-            assertTrue ("Package aaa.bbb cannot be created.", JavaTargetChooserPanel.canUseFileName (root, "", "aaa.bbb", "") != null);
-            assertTrue ("Package aaa.bbb.ccc cannot be created.", JavaTargetChooserPanel.canUseFileName (root, "", "aaa.bbb.ccc", "") != null);
-            assertTrue ("Package ddd can be created.", JavaTargetChooserPanel.canUseFileName (root, "", "ddd", "") == null);
-        } finally {
-            // clean up test folders
-            if (root != null) {
-                root.delete ();
-            }
+        File rootFile = getWorkDir ();
+        assertNotNull ("WorkDir exists.", rootFile);
+        root = FileUtil.toFileObject (rootFile);
+        if (!root.canWrite ()) {
+            fail ("Cannot create test folder.");
         }
-        
 
+        root = root.createFolder ("testCanUseFileName");
+
+        assertNotNull (root + " exists.", FileUtil.toFile (root));
+        assertTrue ("Package aaa.bbb.ccc can be created.", JavaTargetChooserPanel.canUseFileName (root, "", "aaa.bbb.ccc", "") == null);
+
+        assertNotNull ("Package aaa.bbb.ccc was created.", root.createFolder ("aaa").createFolder ("bbb").createFolder ("ccc"));
+        assertTrue ("Package aaa cannot be created.", JavaTargetChooserPanel.canUseFileName (root, "", "aaa", "") != null);
+        assertTrue ("Package aaa.bbb cannot be created.", JavaTargetChooserPanel.canUseFileName (root, "", "aaa.bbb", "") != null);
+        assertTrue ("Package aaa.bbb.ccc cannot be created.", JavaTargetChooserPanel.canUseFileName (root, "", "aaa.bbb.ccc", "") != null);
+        assertTrue ("Package ddd can be created.", JavaTargetChooserPanel.canUseFileName (root, "", "ddd", "") == null);
     }
-
+    
 }
