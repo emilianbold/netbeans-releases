ff2af67727e9b27c8a50834bc1d7e8baebdecf22
mkleint <mkleint@netbeans.org>
2004-11-18 09:12:19 +0000

    48339 - added a new method to api, allowing the api client to specify if a hyperlink in output is imporant or not. increased specification version, added changes document.

--- openide.io/src/org/openide/windows/OutputWriter.java
+++ openide.io/src/org/openide/windows/OutputWriter.java
@@ -37,6 +37,7 @@ public abstract class OutputWriter extends PrintWriter {
     /** Print a line which will be displayed as a hyperlink, calling the 
     * passed <code>OutputListener</code> if it is clicked, if the caret 
     * enters it, or if the enter key is pressed over it.
+    * 
     *
     * @param s a string to print to the tab
     * @param l a listener that will receive events about this line
@@ -44,6 +45,26 @@ public abstract class OutputWriter extends PrintWriter {
     */
     public abstract void println(String s, OutputListener l) throws IOException;
 
+    /** Print a line which will be displayed as a hyperlink, calling the 
+     * passed <code>OutputListener</code> if it is clicked, if the caret 
+     * enters it, or if the enter key is pressed over it. 
+     * 
+     * Implementors of this class 
+     * are encouraged to override this method, which is not abstract for backward 
+     * compatibility reasons only.
+     *
+     * @param s a string to print to the tab
+     * @param l a listener that will receive events about this line
+     * @param important mark the line as important. 
+     *        Makes the UI respond appropriately, eg. stop the automatic scrolling 
+     *        or highlight the hyperlink.
+     * @throws IOException if the string could not be printed
+     * @since 1.5
+     */
+    public void println(String s, OutputListener l, boolean important) throws IOException {
+        println(s, l);
+    }
+    
     /** Clear the output pane.
     * Expect this method to be deprecated in a future release and an 
     * equivalent created in <code>InputOutput</code>.  It is ambiguous what it means
