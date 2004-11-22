00d91956f7385c22f1b29b43055943d81ff72fbe
johnjullion <johnjullion@netbeans.org>
2004-11-22 13:02:20 +0000

    adding j2ee helpset to the j2ee/platform module

---
+++ j2ee.platform/javahelp/org/netbeans/modules/j2ee/platform/docs/j2ee.hs
@@ -0,0 +1,46 @@
+<?xml version='1.0' encoding='ISO-8859-1' ?>
+<!--
+*     Copyright © 2004 Sun Microsystems, Inc. All rights reserved.
+*     Use is subject to license terms.
+-->
+<!DOCTYPE helpset
+  PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN"
+         "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
+<helpset version="2.0">
+
+<!-- last updated 05Feb04-->
+
+  <!-- title -->
+  <title>J2EE Development Help</title>
+  
+  <!-- maps -->
+  <maps>
+     <homeID>j2ee_about</homeID>
+     <mapref location="j2eeMap.jhm" />
+  </maps>
+  
+  <!-- views -->
+  <view>
+    <name>TOC</name>
+    <label>Table Of Contents</label>
+    <type>javax.help.TOCView</type>
+    <data>j2ee-toc.xml</data> 
+  </view>
+
+  <view>
+    <name>Index</name>
+    <label>Index</label>
+    <type>javax.help.IndexView</type>
+    <data>j2ee-idx.xml</data>
+  </view>
+
+  <view>
+    <name>Search</name>
+    <label>Search</label>
+    <type>javax.help.SearchView</type> 
+    <data engine="com.sun.java.help.search.DefaultSearchEngine">      
+      JavaHelpSearch
+    </data>  
+  </view>
+
+</helpset>
