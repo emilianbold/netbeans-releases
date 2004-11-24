30393630d7ae70843cc2a789a3f6dff3e7f610c7
cwebster <cwebster@netbeans.org>
2004-11-24 02:56:47 +0000

    modify interface to support new functionality

--- j2ee.api.ejbmodule/src/org/netbeans/modules/j2ee/api/ejbjar/EjbReference.java
+++ j2ee.api.ejbmodule/src/org/netbeans/modules/j2ee/api/ejbjar/EjbReference.java
@@ -10,20 +10,36 @@ import org.openide.src.MethodElement;
 
 public interface EjbReference {
     public AntArtifact getClientJarTarget();
-    
     public boolean supportsLocalInvocation();
-    
     public boolean supportsRemoteInvocation();
     
     public EjbRef createRef();
-    
     public EjbLocalRef createLocalRef();
     
     public void populateReference(EjbRef ref);
-    
     public void populateReference(EjbLocalRef ref);
     
+    /**
+     * 
+     *@deprecated use generateJNDILookup(EjbRef, boolean) instead
+     */
     public MethodElement generateJNDILookup(EjbRef ref);
-    
+    /**
+     * 
+     *@deprecated use generateJNDILookup(EjbLocalRef, boolean) instead
+     */
     public MethodElement generateJNDILookup(EjbLocalRef ref);
+    
+    /**
+     * Create JNDI Lookup method and do no throw any checked exceptions in the
+     * generated method.
+     */
+    public MethodElement generateJNDILookup(EjbRef ref, boolean throwExceptions);
+    
+    /**
+     * Create JNDI Lookup method and do no throw any checked exceptions in the
+     * generated method.
+     */
+    public MethodElement generateJNDILookup(EjbLocalRef ref, boolean throwExceptions);
+
 }
