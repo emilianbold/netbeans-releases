c02b5b58fda2895da8acb5006074e8e301126e53
pcw <pcw@netbeans.org>
2004-12-09 08:05:33 +0000

    Added wscompile support and project settable stub support to web service module api

--- websvc.websvcapi/src/org/netbeans/modules/websvc/spi/webservices/WebServicesConstants.java
+++ websvc.websvcapi/src/org/netbeans/modules/websvc/spi/webservices/WebServicesConstants.java
@@ -20,18 +20,18 @@ public interface WebServicesConstants {
     public static final String MAPPING_FILE_SUFFIX = "-mapping.xml";//NOI18N
     public static final String WebServiceServlet_PREFIX = "WSServlet_";//NOI18N
 
-    public static final String WSDL_FOLDER = "wsdl";	// NOI18N
-    public static final String WEB_SERVICE_CLIENTS="web-service-clients"; //NOI18N
-    public static final String WEB_SERVICE_CLIENT="web-service-client"; //NOI18N
-    public static final String WEB_SERVICE_CLIENT_NAME="web-service-client-name"; //NOI18N
-    public static final String WSCOMPILE_CLASSPATH="wscompile.classpath"; //NOI18N
-    public static final String WSCOMPILE_TOOLS_CLASSPATH="wscompile.tools.classpath"; //NOI18N
-    public static final String WEBSVC_GENERATED_DIR="websvc.generated.dir"; // NOI18N
+    public static final String WSDL_FOLDER = "wsdl"; // NOI18N
+    public static final String WEB_SERVICE_CLIENTS = "web-service-clients"; //NOI18N
+    public static final String WEB_SERVICE_CLIENT = "web-service-client"; //NOI18N
+    public static final String WEB_SERVICE_CLIENT_NAME = "web-service-client-name"; //NOI18N
+    public static final String WEB_SERVICE_STUB_TYPE = "web-service-stub-type"; //NOI18N
+    public static final String WSCOMPILE_CLASSPATH = "wscompile.classpath"; //NOI18N
+    public static final String WSCOMPILE_TOOLS_CLASSPATH = "wscompile.tools.classpath"; //NOI18N
+    public static final String WEBSVC_GENERATED_DIR = "websvc.generated.dir"; // NOI18N
     public static final String [] WSCOMPILE_JARS = {
         "${libs.j2ee14.classpath}",
         "${libs.jaxrpc11.classpath}",
         "${libs.saaj12.classpath}",
         "${wscompile.tools.classpath}"
     };
-   
 }
