00962f991964409bdc69b6aaa5caa73a82de4c55
jjancura <jjancura@netbeans.org>
2005-01-05 17:12:09 +0000

    Default model for watches added.

---
+++ spi.debugger.ui/src/org/netbeans/modules/debugger/ui/models/WatchesTableModel.java
@@ -0,0 +1,83 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2000 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.modules.debugger.ui.models;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.Vector;
+import org.netbeans.api.debugger.Watch;
+import org.netbeans.spi.debugger.ContextProvider;
+import org.netbeans.spi.debugger.ui.Constants;
+import org.netbeans.spi.viewmodel.TableModel;
+import org.netbeans.spi.viewmodel.TreeModelListener;
+import org.netbeans.spi.viewmodel.UnknownTypeException;
+
+
+/**
+ *
+ * @author   Jan Jancura
+ */
+public class WatchesTableModel implements TableModel, Constants {
+
+    public Object getValueAt (Object row, String columnID) throws 
+    UnknownTypeException {
+        if (row instanceof Watch) {
+            if (columnID.equals (WATCH_TO_STRING_COLUMN_ID))
+                return "";
+            else
+            if (columnID.equals (WATCH_TYPE_COLUMN_ID))
+                return "";
+            else
+            if (columnID.equals (WATCH_VALUE_COLUMN_ID))
+                return "";
+        }
+        throw new UnknownTypeException (row);
+    }
+    
+    public boolean isReadOnly (Object row, String columnID) throws 
+    UnknownTypeException {
+        if (row instanceof Watch) {
+            if (columnID.equals (WATCH_TO_STRING_COLUMN_ID))
+                return true;
+            else
+            if (columnID.equals (WATCH_TYPE_COLUMN_ID))
+                return true;
+            else
+            if (columnID.equals (WATCH_VALUE_COLUMN_ID))
+                return true;
+        }
+        throw new UnknownTypeException (row);
+    }
+    
+    public void setValueAt (Object row, String columnID, Object value) 
+    throws UnknownTypeException {
+        throw new UnknownTypeException (row);
+    }
+    
+    /** 
+     * Registers given listener.
+     * 
+     * @param l the listener to add
+     */
+    public void addTreeModelListener (TreeModelListener l) {
+    }
+
+    /** 
+     * Unregisters given listener.
+     *
+     * @param l the listener to remove
+     */
+    public void removeTreeModelListener (TreeModelListener l) {
+    }
+}
