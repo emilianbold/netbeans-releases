d7e65b24976cb31dfa22706ec4f0809ccd6ca960
rstrobl <rstrobl@netbeans.org>
2005-01-07 14:22:54 +0000

    Initial commit.

---
+++ editor/test/qa-functional/data/projects/editor_test/src/search_replace/SearchTest/testFindDialogOpenClose.java
@@ -0,0 +1,13 @@
+package search_replace.SearchTest;
+
+/**
+ * Sample file for search and replace tests.
+ *
+ * @author  Roman Strobl
+ */
+public class testFindDialogOpenClose {
+    
+    public testFindDialogOpenClose () {
+    }
+    
+}
