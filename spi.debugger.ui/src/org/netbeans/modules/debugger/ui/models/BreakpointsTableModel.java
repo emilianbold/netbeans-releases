2a8db7264472fa46a7d82c5cf93443765951038f
jjancura <jjancura@netbeans.org>
2005-01-07 09:51:13 +0000

    TableModel for watches added.

---
+++ spi.debugger.ui/src/org/netbeans/modules/debugger/ui/models/BreakpointsTableModel.java
@@ -0,0 +1,79 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2000 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.modules.debugger.ui.models;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.Vector;
+import org.netbeans.api.debugger.Breakpoint;
+import org.netbeans.spi.debugger.ContextProvider;
+import org.netbeans.spi.debugger.ui.Constants;
+import org.netbeans.spi.viewmodel.TableModel;
+import org.netbeans.spi.viewmodel.TreeModelListener;
+import org.netbeans.spi.viewmodel.UnknownTypeException;
+
+
+/**
+ *
+ * @author   Jan Jancura
+ */
+public class BreakpointsTableModel implements TableModel, Constants {
+
+    public Object getValueAt (Object row, String columnID) throws 
+    UnknownTypeException {
+        if (row instanceof Breakpoint) {
+            if (columnID.equals (BREAKPOINT_ENABLED_COLUMN_ID))
+                return Boolean.valueOf (((Breakpoint) row).isEnabled ());
+        }
+        throw new UnknownTypeException (row);
+    }
+    
+    public boolean isReadOnly (Object row, String columnID) throws 
+    UnknownTypeException {
+        if (row instanceof Breakpoint) {
+            if (columnID.equals (BREAKPOINT_ENABLED_COLUMN_ID))
+                return false;
+        }
+        throw new UnknownTypeException (row);
+    }
+    
+    public void setValueAt (Object row, String columnID, Object value) 
+    throws UnknownTypeException {
+        if (row instanceof Breakpoint) {
+            if (columnID.equals (BREAKPOINT_ENABLED_COLUMN_ID)) {
+                if (((Boolean) value).booleanValue ())
+                    ((Breakpoint) row).enable ();
+                else
+                    ((Breakpoint) row).disable ();
+            }
+        }
+        throw new UnknownTypeException (row);
+    }
+    
+    /** 
+     * Registers given listener.
+     * 
+     * @param l the listener to add
+     */
+    public void addTreeModelListener (TreeModelListener l) {
+    }
+
+    /** 
+     * Unregisters given listener.
+     *
+     * @param l the listener to remove
+     */
+    public void removeTreeModelListener (TreeModelListener l) {
+    }
+}
