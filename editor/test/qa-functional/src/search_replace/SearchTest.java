7a62aac4a0cc7fd2fca0d53be93b57fe1a808744
rstrobl <rstrobl@netbeans.org>
2005-01-10 19:14:30 +0000

    Added new find tests.

--- editor/test/qa-functional/src/search_replace/SearchTest.java
+++ editor/test/qa-functional/src/search_replace/SearchTest.java
@@ -19,6 +19,8 @@ import lib.EditorTestCase;
 import org.netbeans.jellytools.HelpOperator;
 import org.netbeans.jellytools.actions.FindAction;
 import org.netbeans.jellytools.modules.editor.Find;
+import org.netbeans.jemmy.EventTool;
+import org.netbeans.jemmy.operators.JComboBoxOperator;
 import org.netbeans.jemmy.operators.JEditorPaneOperator;
 
 /**
@@ -27,7 +29,6 @@ import org.netbeans.jemmy.operators.JEditorPaneOperator;
  * @author Roman Strobl
  */
 public class SearchTest extends EditorTestCase {
-    EditorOperator editor;
     
     /** Creates a new instance of Main */
     public SearchTest(String testMethodName) {
@@ -37,26 +38,146 @@ public class SearchTest extends EditorTestCase {
     /**
      * TC1 - open and close find dialog
      */
-    public void testFindDialogOpenClose(){
+    public void testFindDialogOpenClose() {
         openDefaultProject();
         openDefaultSampleFile();
-        
-        editor = getDefaultSampleEditorOperator();
-        JEditorPaneOperator txtOper = editor.txtEditorPane();
-        
-        new FindAction().perform();
-        txtOper.pushKey(KeyEvent.VK_ESCAPE);
-        
-        txtOper.pushKey(KeyEvent.VK_F, KeyEvent.CTRL_MASK);
-        Find find = new Find();
-        find.btHelp().clickMouse();
-        
-        HelpOperator help = new HelpOperator();
-        help.close();
-        
-        find.btClose().clickMouse();
-        
-        closeFileWithDiscard();
+        try {
+            EditorOperator editor = getDefaultSampleEditorOperator();
+            JEditorPaneOperator txtOper = editor.txtEditorPane();
+
+            // open find and close
+            new FindAction().perform();
+            txtOper.pushKey(KeyEvent.VK_ESCAPE);
+            
+            // open find and open help
+            txtOper.pushKey(KeyEvent.VK_F, KeyEvent.CTRL_MASK);
+            Find find = new Find();
+            find.btHelp().clickMouse();
+            
+            // close help
+            HelpOperator help = new HelpOperator();
+            help.close();
+            
+            // close find
+            find.btClose().clickMouse();
+            
+        } finally {
+            closeFileWithDiscard();
+        }
     }
     
+    /**
+     * TC2 - Find Selection Repeated
+     */
+    public void testFindSelectionRepeated() {
+        openDefaultProject();
+        openDefaultSampleFile();
+        try {
+            EditorOperator editor = getDefaultSampleEditorOperator();
+            
+            // choose the "public" word
+            editor.select(13, 1, 6);
+            new FindAction().perform();
+            Find find = new Find();
+            String text = find.cboFindWhat().getTextField().getText();
+            // compare
+            assertEquals(text, "public");
+            find.find();
+            new EventTool().waitNoEvent(1000);
+            find.close();
+            // check position
+            assertEquals(editor.getLineNumber(), 16);
+            assertEquals(editor.txtEditorPane().getCaret().getDot(), 266);           
+
+            // choose the "testFindSelectionRepeated" word
+            editor.select(13, 14, 38);
+            new FindAction().perform();
+            Find find2 = new Find();
+            text = find.cboFindWhat().getTextField().getText();
+            // compare
+            assertEquals(text, "testFindSelectionRepeated");
+            find2.find();
+            new EventTool().waitNoEvent(1000);
+            find2.close();
+            // check position
+            assertEquals(editor.getLineNumber(), 15);
+            assertEquals(editor.txtEditorPane().getCaret().getDot(), 258);
+            
+        } finally {
+            closeFileWithDiscard();
+        }
+    }
+    
+    /**
+     * TC3 - Find Dialog Combo Box
+     */
+    public void testFindDialogComboBox() {
+        openDefaultProject();
+        openDefaultSampleFile();
+        try {
+            EditorOperator editor = getDefaultSampleEditorOperator();
+                       
+            // first search
+            editor.setCaretPosition(0);
+            new FindAction().perform();
+            Find find = new Find();
+            find.cboFindWhat().getTextField().setText("public");
+            find.find();
+            find.close();
+            
+            // second search
+            editor.setCaretPosition(0);
+            new FindAction().perform();
+            Find find2 = new Find();
+            find2.cboFindWhat().getTextField().setText("class");
+            find2.find();
+            find2.close();
+            
+            // search for an item from history - word "public"
+            editor.setCaretPosition(0);
+            new FindAction().perform();
+            Find find3 = new Find();
+            JComboBoxOperator cbo = find3.cboFindWhat();
+            cbo.selectItem(1);
+            find3.find();
+            new EventTool().waitNoEvent(1000);
+            find3.close();
+            // check position
+            assertEquals(editor.getLineNumber(), 16);
+            assertEquals(editor.txtEditorPane().getCaret().getDot(), 262);
+                        
+        } finally {
+            closeFileWithDiscard();
+        }        
+    }
+    
+    /**
+     * TC4 - Unselected All Options
+     */
+    public void testUnselectedAllOptions() {
+        openDefaultProject();
+        openDefaultSampleFile();
+        try {
+            EditorOperator editor = getDefaultSampleEditorOperator();
+            
+            // perform search with all options unselected
+            editor.setCaretPosition(0);
+            new FindAction().perform();
+            Find find = new Find();
+            JComboBoxOperator cbo = find.cboFindWhat();
+            cbo.getTextField().setText("cLaSs");
+            find.cbHighlightSearch().clickMouse();
+            find.cbIncrementalSearch().clickMouse();
+            find.cbWrapSearch().clickMouse();
+            find.find();
+            new EventTool().waitNoEvent(1000);
+            find.close();
+            // check position
+            assertEquals(editor.getLineNumber(), 13);
+            assertEquals(editor.txtEditorPane().getCaret().getDot(), 163);
+            
+        } finally {
+            closeFileWithDiscard();
+        }
+    }
 }
