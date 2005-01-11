87c27b458743aa1dcad1d860e073abc47f266e7c
rstrobl <rstrobl@netbeans.org>
2005-01-11 14:24:59 +0000

    Initial commit.

---
+++ editor/test/qa-functional/data/projects/editor_test/src/search_replace/SearchTest/testHighlightSearch.java
@@ -0,0 +1,19 @@
+/*
+ * testHighlightSearch.java
+ *
+ * Created on January 11, 2005, 2:51 PM
+ */
+
+package search_replace.SearchTest;
+
+/**
+ *
+ * @author rs155161
+ */
+public class testHighlightSearch {
+    
+    /** Creates a new instance of testHighlightSearch */
+    public testHighlightSearch() {
+    }
+    
+}
