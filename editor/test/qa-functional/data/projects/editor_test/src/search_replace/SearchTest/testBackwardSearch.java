38e79ab6bfbdea68a3b82e8c6a7b0642a0356f28
rstrobl <rstrobl@netbeans.org>
2005-01-11 15:43:34 +0000

    Initial commit.

---
+++ editor/test/qa-functional/data/projects/editor_test/src/search_replace/SearchTest/testBackwardSearch.java
@@ -0,0 +1,25 @@
+/*
+ * testBackwardSearch.java
+ *
+ * Created on January 11, 2005, 4:03 PM
+ */
+
+package search_replace.SearchTest;
+
+/**
+ *
+ * @author rs155161
+ */
+public class testBackwardSearch {
+    
+    /** Creates a new instance of testBackwardSearch */
+    public testBackwardSearch() {
+        /*
+         * fourth
+         * third
+         * second
+         * first
+         */
+    }
+    
+}
---
+++ editor/test/qa-functional/data/projects/editor_test/src/search_replace/SearchTest/testIncrementalSearch.java
@@ -0,0 +1,31 @@
+/*
+ * testIncrementalSearch.java
+ *
+ * Created on January 11, 2005, 3:42 PM
+ */
+
+package search_replace.SearchTest;
+
+/**
+ *
+ * @author rs155161
+ */
+public class testIncrementalSearch {
+    
+    /** Creates a new instance of testIncrementalSearch */
+    public testIncrementalSearch() {
+        /* searchedWord
+         * searchedWord
+         * searchedWord
+         * searchedWord
+         * searchedWord
+         * searchedWord
+         * searchedWord
+         * searchedWord
+         * searchedWord
+         * searchedWord
+         */
+    }
+    
+}
+
