fa2b2fe051887bb4c6d025d52733ba2a0e7e1a60
rstrobl <rstrobl@netbeans.org>
2005-01-11 18:46:42 +0000

    Initial commit.

---
+++ editor/test/qa-functional/data/projects/editor_test/src/search_replace/SearchTest/testWrapSearch.java
@@ -0,0 +1,25 @@
+/*
+ * testWrapSearch.java
+ *
+ * Created on January 11, 2005, 5:25 PM
+ */
+
+package search_replace.SearchTest;
+
+/**
+ *
+ * @author rs155161
+ */
+public class testWrapSearch {
+    
+    /** Creates a new instance of testWrapSearch */
+    public testWrapSearch() {
+        /*
+         * wrapWord
+         * wrapWord
+         * wrapWord
+         * wrapWord
+         */
+    }
+    
+}
