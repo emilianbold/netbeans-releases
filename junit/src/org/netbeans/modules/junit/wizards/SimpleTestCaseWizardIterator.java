c69d4ae97078658699330db2c577c6353051d111
mpetras <mpetras@netbeans.org>
2005-01-18 08:17:13 +0000

    corrected copyright notice - extended to year 2005

--- junit/src/org/netbeans/modules/junit/wizards/SimpleTestCaseWizardIterator.java
+++ junit/src/org/netbeans/modules/junit/wizards/SimpleTestCaseWizardIterator.java
@@ -7,7 +7,7 @@
  * http://www.sun.com/
  * 
  * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 2004 Sun
+ * Code is Sun Microsystems, Inc. Portions Copyright 2004-2005 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
