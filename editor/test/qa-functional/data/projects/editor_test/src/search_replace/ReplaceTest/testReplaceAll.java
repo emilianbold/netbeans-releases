7cb848eab02d513780843a619d4884f8fa3e21e3
rstrobl <rstrobl@netbeans.org>
2005-01-28 13:11:17 +0000

    Initial commit.

---
+++ editor/test/qa-functional/data/projects/editor_test/src/search_replace/ReplaceTest/testReplaceAll.java
@@ -0,0 +1,35 @@
+/*
+ * testReplaceAll.java
+ *
+ * Created on January 28, 2005, 2:02 PM
+ */
+
+package search_replace.ReplaceTest;
+
+/**
+ *
+ * @author rs155161
+ */
+public class testReplaceAll {
+    
+    /** Creates a new instance of testReplaceAll */
+    public testReplaceAll() {
+        /**
+         * testWord
+         * testWord
+         * testWord
+         * testWord
+         * testWord
+         * testWord
+         * testWord
+         * testWord
+         * testWord
+         * testWord
+         * testWord
+         * testWord
+         * testWord
+         * testWord
+         */
+    }
+    
+}
