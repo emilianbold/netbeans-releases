14e4f5a5ec2232a363edc006fe258fed13fc0392
rstrobl <rstrobl@netbeans.org>
2005-01-28 14:01:11 +0000

    Initial commit.

---
+++ editor/test/qa-functional/data/projects/editor_test/src/search_replace/ReplaceTest/testReplaceInSelectionOnly.java
@@ -0,0 +1,31 @@
+/*
+ * testReplaceInSelectionOnly.java
+ *
+ * Created on January 28, 2005, 2:32 PM
+ */
+
+package search_replace.ReplaceTest;
+
+/**
+ *
+ * @author rs155161
+ */
+public class testReplaceInSelectionOnly {
+    
+    /** Creates a new instance of testReplaceInSelectionOnly */
+    public testReplaceInSelectionOnly() {
+        /**
+         * testWord
+         * testWord
+         * testWord
+         * testWord
+         * testWord
+         * testWord
+         * testWord
+         * testWord
+         * testWord
+         * testWord
+         */
+    }
+    
+}
