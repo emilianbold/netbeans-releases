191842f63b439d11803f6273530a3f6d108c6e79
jtulach <jtulach@netbeans.org>
2005-02-07 16:38:53 +0000

    Next time better check the diff before commit

--- o.n.core/test/unit/src/org/netbeans/core/lookup/InstanceDataObjectModuleTest8.java
+++ o.n.core/test/unit/src/org/netbeans/core/lookup/InstanceDataObjectModuleTest8.java
@@ -106,7 +106,6 @@ implements FileChangeListener {
         assertSameDataObject ("same data object2", obj1, obj3);
         InstanceCookie inst3 = (InstanceCookie)obj3.getCookie(InstanceCookie.class);
         assertNull("Had instance", inst3);
-        fail ("KUK");
     }
     
     
