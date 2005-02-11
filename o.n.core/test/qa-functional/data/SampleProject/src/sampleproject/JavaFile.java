33e0196ba346305b082cdf1174addbaccd287350
mmirilovic <mmirilovic@netbeans.org>
2005-02-11 13:21:11 +0000

    add tests for Main Menu, Toolbars and Menus

---
+++ o.n.core/test/qa-functional/data/SampleProject/src/sampleproject/JavaFile.java
@@ -0,0 +1,19 @@
+/*
+ * JavaFile.java
+ *
+ * Created on February 3, 2005, 11:43 AM
+ */
+
+package sampleproject;
+
+/**
+ *
+ * @author marianm
+ */
+public class JavaFile {
+    
+    /** Creates a new instance of JavaFile */
+    public JavaFile() {
+    }
+    
+}
---
+++ o.n.core/test/qa-functional/data/SampleProject/src/sampleproject/Main.java
@@ -0,0 +1,26 @@
+/*
+ * Main.java
+ *
+ * Created on February 3, 2005, 11:07 AM
+ */
+
+package sampleproject;
+
+/**
+ *
+ * @author marianm
+ */
+public class Main {
+    
+    /** Creates a new instance of Main */
+    public Main() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
