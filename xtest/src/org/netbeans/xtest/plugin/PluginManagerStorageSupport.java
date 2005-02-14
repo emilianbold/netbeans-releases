d0100b75cce98917557a5d41461f2b09e4f19960
jskrivanek <jskrivanek@netbeans.org>
2005-02-14 14:07:37 +0000

    File name of serialized plugin manager has to be unique to not clash with other running XTest instances. The name is set to property which also indicates that plugin manager was previously stored.

--- xtest/src/org/netbeans/xtest/plugin/PluginManagerStorageSupport.java
+++ xtest/src/org/netbeans/xtest/plugin/PluginManagerStorageSupport.java
@@ -7,7 +7,7 @@
  * http://www.sun.com/
  * 
  * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 package org.netbeans.xtest.plugin;
@@ -26,10 +26,9 @@ import java.io.*;
  */
 public class PluginManagerStorageSupport {
     
-     private static final String PLUGIN_MANAGER_SERIALIZED_FILE_NAME = System.getProperty("java.io.tmpdir")+File.separator+"xtestPluginManager.ser";
-     // property name to indicate that PluginManager was not yet serialized in this JVM
-     private static final String PLUGIN_MANAGER_SYSTEM_PROPERTY_NAME="xtest.internal.serialized.pluginManager";
-
+    // property name to save file name and to indicate that PluginManager was not yet serialized in this JVM
+    private static final String PLUGIN_MANAGER_SYSTEM_PROPERTY_NAME="xtest.internal.serialized.pluginManager";
+     
     
     /** Creates a new instance of PluginManagerStorageSupport */
     private PluginManagerStorageSupport() {
@@ -37,37 +36,43 @@ public class PluginManagerStorageSupport {
     
      // static methods for storing/retrieving PluginManager, so it survives classloader change
     public static PluginManager retrievePluginManager() {
-        File serializedFile = new File(PLUGIN_MANAGER_SERIALIZED_FILE_NAME);
-        if(System.getProperty(PLUGIN_MANAGER_SYSTEM_PROPERTY_NAME) != null && serializedFile.exists()) {
-            try {
-                FileInputStream in = new FileInputStream(serializedFile);
-                ObjectInputStream s = new ObjectInputStream(in);
-                Object pluginManagerObject = s.readObject();
-                if (pluginManagerObject instanceof PluginManager) {
-                    return (PluginManager)pluginManagerObject;
+        String filename = System.getProperty(PLUGIN_MANAGER_SYSTEM_PROPERTY_NAME);
+        if(filename != null) {
+            File serializedFile = new File(filename);
+            if(serializedFile.exists()) {
+                try {
+                    FileInputStream in = new FileInputStream(serializedFile);
+                    ObjectInputStream s = new ObjectInputStream(in);
+                    Object pluginManagerObject = s.readObject();
+                    if (pluginManagerObject instanceof PluginManager) {
+                        return (PluginManager)pluginManagerObject;
+                    }
+                } catch (FileNotFoundException fnfe) {
+                    System.out.println("Exception when retrieving PluginManager: "+fnfe);
+                    fnfe.printStackTrace();
+                } catch (IOException ioe) {
+                    System.out.println("Exception when retrieving PluginManager: "+ioe);
+                    ioe.printStackTrace();
+                } catch (ClassNotFoundException cnfe) {
+                    System.out.println("Exception when retrieving PluginManager: "+cnfe);
+                    cnfe.printStackTrace();
                 }
-            } catch (FileNotFoundException fnfe) {
-                System.out.println("Exception when retrieving PluginManager: "+fnfe);
-                fnfe.printStackTrace();
-            } catch (IOException ioe) {
-                System.out.println("Exception when retrieving PluginManager: "+ioe);
-                ioe.printStackTrace();
-            } catch (ClassNotFoundException cnfe) {
-                System.out.println("Exception when retrieving PluginManager: "+cnfe);
-                cnfe.printStackTrace();
             }
         }
         return null;
     }
     
     public static void storePluginManager(PluginManager pluginManager) throws IOException {
-        FileOutputStream out = new FileOutputStream(PLUGIN_MANAGER_SERIALIZED_FILE_NAME);
+        String filename = System.getProperty("java.io.tmpdir")+File.separator+
+                          "xtestPluginManager"+System.currentTimeMillis()+".ser";
+        FileOutputStream out = new FileOutputStream(filename);
         ObjectOutputStream s = new ObjectOutputStream(out);
         s.writeObject(pluginManager);
         s.close();
         out.close();
-        // indicate the PluginManager was stored
-        System.setProperty(PLUGIN_MANAGER_SYSTEM_PROPERTY_NAME, "true");
+        // set property to unique file name to be used when retrieving it and to indicate 
+        // the PluginManager was stored
+        System.setProperty(PLUGIN_MANAGER_SYSTEM_PROPERTY_NAME, filename);
     }
     
 }
