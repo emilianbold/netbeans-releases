6c6f8dfbfc3cb7450b322064d21bc140e34d9a25
mpetras <mpetras@netbeans.org>
2005-02-18 07:50:36 +0000

    changed copyright comment - extended copyright period to 2005

--- properties/src/org/netbeans/modules/properties/LocalePanel.java
+++ properties/src/org/netbeans/modules/properties/LocalePanel.java
@@ -7,7 +7,7 @@
  * http://www.sun.com/
  * 
  * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2000 Sun
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -33,6 +33,7 @@ import org.openide.util.NbBundle;
  * Panel to allow user convenient to choose locale.
  *
  * @author  Peter Zavadsky
+ * @author  Marian Petras
  * @see java.util.Locale
  */
 public class LocalePanel extends JPanel {
