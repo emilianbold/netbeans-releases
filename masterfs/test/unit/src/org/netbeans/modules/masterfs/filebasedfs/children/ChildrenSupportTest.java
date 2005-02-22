166af5df8e5ad0738b87b4f1c84b73a81daae8c7
rmatous <rmatous@netbeans.org>
2005-02-22 12:56:19 +0000

    added test for ChildrenSupport

---
+++ masterfs/test/unit/src/org/netbeans/modules/masterfs/filebasedfs/children/ChildrenSupportTest.java
@@ -0,0 +1,153 @@
+/*
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.modules.masterfs.filebasedfs.children;
+
+import junit.framework.*;
+import org.netbeans.junit.NbTestCase;
+import org.netbeans.modules.masterfs.filebasedfs.naming.FileName;
+import org.netbeans.modules.masterfs.filebasedfs.naming.FileNaming;
+import org.netbeans.modules.masterfs.filebasedfs.naming.NamingFactory;
+
+import java.io.File;
+import java.util.*;
+
+/**
+ *
+ * @author Radek Matous
+ */
+public class ChildrenSupportTest extends NbTestCase {
+    ChildrenSupport folderItem;
+    File testFile;
+    
+    /*Just for testRefresh*/
+    private File fbase;
+    private File removed1;
+    private File removed2;
+    private File added1;
+    private File added2;
+    private FileNaming folderName;
+
+    public ChildrenSupportTest(String testName) {
+        super(testName);
+    }
+
+    protected void setUp() throws java.lang.Exception {
+        testFile = getWorkDir().getParentFile();
+        folderName = NamingFactory.fromFile(testFile);
+        folderItem = new ChildrenSupport ();
+        
+        if (getName().startsWith("testRefresh")) {
+            fbase = new File (testFile, "testrefresh");
+            removed1 = new File (fbase, "removed1/");
+            removed2 = new File (fbase, "removed2/");
+            added1 = new File (fbase, "added1/");
+            added2 = new File (fbase, "added2/");  
+            
+            assertTrue (testFile.exists());
+            assertTrue(testFile.isDirectory());
+            if (!fbase.exists()) assertTrue (fbase.getAbsoluteFile().mkdirs());
+            if (!removed1.exists())assertTrue (removed1.mkdir());
+            if (!removed2.exists())assertTrue (removed2.mkdir());
+            if (added1.exists()) assertTrue (added1.delete());
+            if (added2.exists()) assertTrue (added2.delete());        
+        }
+    }
+
+
+    /**
+     * Test of getFolderName method, of class org.netbeans.modules.masterfs.children.FolderPathItems.
+     */
+    public void testGetFolderItem() throws Exception {
+        assertEquals(testFile, folderName.getFile());
+    }
+
+
+    /**
+     * Test of getChildren method, of class org.netbeans.modules.masterfs.children.FolderPathItems.
+     */
+    public void testGetChildrenPathItems() throws Exception{
+        Set childItems = folderItem.getChildren(folderName, true);
+        List lst = Arrays.asList(testFile.listFiles());
+        Iterator it = childItems.iterator();
+        while (it.hasNext()) {
+            FileNaming pi = (FileNaming)it.next();
+            File f = pi.getFile();
+            assertTrue (lst.contains(f));
+        }
+    }
+
+    /**
+     * Test of getFileName method, of class org.netbeans.modules.masterfs.children.FolderPathItems.
+     */
+    public void testGetChildItem() throws Exception{
+        folderItem.getChildren(folderName, true);
+        List lst = Arrays.asList(testFile.listFiles());
+        Iterator it = lst.iterator();
+        while (it.hasNext()) {
+            File f = (File)it.next();
+            FileNaming item = folderItem.getChild(f.getName(), folderName,false);
+            assertNotNull(item);
+            assertTrue (item.getFile().equals(f));
+        }
+    }
+
+    public void testRefresh () throws Exception {
+        FileNaming fpiName = NamingFactory.fromFile(fbase);
+        ChildrenSupport fpi = new ChildrenSupport();
+        fpi.getChildren(fpiName, true);
+        assertTrue (removed1.delete());
+        assertTrue (removed2.delete());
+        assertTrue (added1.mkdirs());
+        assertTrue (added2.mkdirs());
+
+        List added = Arrays.asList(new String[] {"added1", "added2"});
+        List removed = Arrays.asList(new String[] {"removed1", "removed2"});
+        
+        Map changes = fpi.refresh(fpiName);
+        Iterator it = changes.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry entry = (Map.Entry) it.next();
+            FileNaming pItem = (FileNaming)entry.getKey();
+            Integer type = (Integer)entry.getValue();
+            if (type == ChildrenCache.ADDED_CHILD) {
+                assertTrue (added.contains(pItem.getName()));
+            }
+            if (type == ChildrenCache.REMOVED_CHILD) {
+                assertTrue (removed.contains(pItem.getName()));
+            }            
+        }
+        assertTrue (changes.size() == 4);        
+    }        
+    
+    public void testRefresh2 () throws Exception {
+        FileNaming fpiName = NamingFactory.fromFile(fbase);
+        ChildrenSupport fpi = new ChildrenSupport();
+        fpi.getChild("removed1", fpiName, false);
+        assertTrue (removed1.delete());
+        assertTrue (removed2.delete());
+        assertTrue (added1.mkdirs());
+        assertTrue (added2.mkdirs());
+        
+        Map changes = fpi.refresh(fpiName);
+        Iterator it = changes.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry entry = (Map.Entry) it.next();
+            FileNaming pItem = (FileNaming)entry.getKey();
+            Integer type = (Integer)entry.getValue();
+            assertEquals("removed1", pItem.getName());
+        }
+        assertTrue (changes.size() == 1);        
+    }        
+    
+}
