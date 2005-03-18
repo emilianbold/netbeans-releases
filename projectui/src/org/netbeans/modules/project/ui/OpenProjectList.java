1ea72f6180e52e294b0932b4f6e7bc7d0777f0ed
jrechtacek <jrechtacek@netbeans.org>
2005-03-18 13:45:25 +0000

    #56454: open saved files also in subprojects

--- projectui/src/org/netbeans/modules/project/ui/OpenProjectList.java
+++ projectui/src/org/netbeans/modules/project/ui/OpenProjectList.java
@@ -7,7 +7,7 @@
  * http://www.sun.com/
  *
  * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -428,6 +428,8 @@ public final class OpenProjectList {
             if ( !openProjects.contains( sp ) ) {
                 openProjects.add( sp );
                 recentProjectsChanged |= recentProjects.remove( sp );
+                // bugfix #56454: open saved files also in subprojects
+                ProjectUtilities.openProjectFiles (sp);
                 notifyOpened(sp);
             }
             recentProjectsChanged |= openSubprojects( sp );            
