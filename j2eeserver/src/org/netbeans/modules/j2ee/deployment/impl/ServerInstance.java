9916decd93b0c6f37dbc6b8fb5ca18cbaced0af6
lkotouc <lkotouc@netbeans.org>
2005-03-21 08:46:49 +0000

    #56631: Detecting servers running in debug mode.

--- j2eeserver/src/org/netbeans/modules/j2ee/deployment/impl/ServerInstance.java
+++ j2eeserver/src/org/netbeans/modules/j2ee/deployment/impl/ServerInstance.java
@@ -17,6 +17,7 @@ package org.netbeans.modules.j2ee.deployment.impl;
 import javax.enterprise.deploy.spi.*;
 import javax.enterprise.deploy.shared.*;
 import javax.enterprise.deploy.spi.status.*;
+import javax.swing.JButton;
 
 import org.netbeans.modules.j2ee.deployment.plugins.api.*;
 import org.netbeans.modules.j2ee.deployment.impl.ui.DeployProgressUI;
@@ -54,6 +55,19 @@ public class ServerInstance implements Node.Cookie {
     private InstancePropertiesImpl instanceProperties;
     private ServerStatusBar serverStatusBar = null;
     
+    private static class ConflictData {
+        private String transport;
+        private ServerInstance si;
+        public ConflictData(ServerInstance si, String transport) {
+            this.si = si;
+            assert transport.equals(ServerDebugInfo.TRANSPORT_SOCKET) ||
+                   transport.equals(ServerDebugInfo.TRANSPORT_SHMEM);
+            this.transport = transport;
+        }
+        public String getTransport() { return transport; }
+        public ServerInstance getServerInstance() { return si; }
+    };
+        
     // PENDING how to manage connected/disconnected servers with the same manager?
     // maybe concept of 'default unconnected instance' is broken?
     public ServerInstance(Server server, String url, DeploymentManager manager) {
@@ -315,6 +329,42 @@ public class ServerInstance implements Node.Cookie {
         return ss != null && ss.isDebuggable(target);
     }
     
+    /**
+     * @return conflict data instance for server instance running in debug mode with the same socket number 
+     * or shared memory id. If no such server instance exists then null is returned.
+     */
+    public ConflictData anotherServerDebuggable(Target target) {
+
+        ConflictData cd = null;
+        //get debug info for this instance
+        StartServer thisSS = getStartServer();
+        if (thisSS == null) //this case should not occur =>
+            return cd; //attempt to start server (serverInstance remains null)
+        ServerDebugInfo thisSDI = thisSS.getDebugInfo(target);
+        
+        //get all server instances
+        ServerInstance[] serverInstances = ServerRegistry.getInstance().getServerInstances();
+        //check existence of a server instance running in debug mode with the same parameters
+        for (int i = 0; cd == null && i < serverInstances.length; i++) {
+            ServerInstance si = serverInstances[i];
+            if (url.equalsIgnoreCase(si.getUrl())) continue;
+            if (si.isDebuggable(target)) { //running in debug mode
+                ServerDebugInfo sdi = si.getStartServer().getDebugInfo(target);
+                if (thisSDI.getTransport().equals(sdi.getTransport())) { //transport matches
+                    if (thisSDI.getTransport() == ServerDebugInfo.TRANSPORT_SOCKET) {
+                        if (thisSDI.getHost().equalsIgnoreCase(sdi.getHost())) //host matches
+                            if (thisSDI.getPort() == sdi.getPort()) //port matches
+                                cd = new ConflictData(si, ServerDebugInfo.TRANSPORT_SOCKET);
+                    }
+                    else if (thisSDI.getShmemName().equalsIgnoreCase(sdi.getShmemName()))
+                        cd = new ConflictData(si, ServerDebugInfo.TRANSPORT_SHMEM);
+                }
+            }
+        }
+        
+        return cd;
+    }
+    
     public boolean startedByIde() {
         return managerStartedByIde;
     }
@@ -429,8 +479,52 @@ public class ServerInstance implements Node.Cookie {
     }
     
     //------------------------------------------------------------
+    /**
+     * @return true if conflict was resolved successfully, i.e. current server was stopped.
+     */
+    private boolean _resolveServerConflict(Target target, DeployProgressUI ui, ConflictData cd) {
+        
+        StartServer ss = getStartServer();
+        assert(ss != null);
+        
+        ServerInstance si = cd.getServerInstance();
+        //inform a user and allow him to stop the running instance
+        NotifyDescriptor nd = new NotifyDescriptor.Confirmation(
+                NbBundle.getMessage(
+                            ServerInstance.class, 
+                            "MSG_AnotherServerRunning",
+                            new Object[] {
+                                si.getDisplayName(),
+                                ss.getDebugInfo(target).getHost(),
+                                cd.getTransport().equals(ServerDebugInfo.TRANSPORT_SOCKET) ?
+                                    "socket" : "shared memory",
+                                cd.getTransport().equals(ServerDebugInfo.TRANSPORT_SOCKET) ?
+                                    new Integer(ss.getDebugInfo(target).getPort()).toString() : ss.getDebugInfo(target).getShmemName()
+                            }),
+                NotifyDescriptor.QUESTION_MESSAGE
+                );
+        nd.setOptionType(NotifyDescriptor.OK_CANCEL_OPTION);
+        JButton yes = new JButton(NbBundle.getMessage(ServerInstance.class, "MSG_AnotherServerStopYes"));
+        JButton no = new JButton(NbBundle.getMessage(ServerInstance.class, "MSG_AnotherServerStopNo"));
+        yes.setDefaultCapable(true);
+        nd.setOptions(new Object[] { yes, no });
+        Object option = DialogDisplayer.getDefault().notify(nd);
+        if (option != yes) //user pressed No
+            return false;
+        //try to stop running server
+        if (si.getStartServer().supportsStartDeploymentManager()) {
+            if (!si._stop(ui)) //stopping unsuccessful =>
+                return false;  //starting target server impossible
+        } else {
+            return si.errorCannotControlAdmin(ui);
+        }
+        
+        return true;
+    }
+            
     // multiplexor state-machine core
     private boolean startTarget(Target target, DeployProgressUI ui, boolean debugMode) {
+        
         StartServer ss = getStartServer();
         
         // No StartServer, have to assume manually started
@@ -460,6 +554,11 @@ public class ServerInstance implements Node.Cookie {
                         return errorCannotControlAdmin(ui);
                     }
                 }
+                ConflictData cd = anotherServerDebuggable(target);
+                if (cd != null) //another server instance with the same parameters
+                    if (!_resolveServerConflict(target, ui, cd))
+                        return false;
+                
                 if (canControlAdmin) {
                     return _startDebug(target, ui);
                 } else {
@@ -504,6 +603,11 @@ public class ServerInstance implements Node.Cookie {
                         return false;
                     }
                 }
+                ConflictData cd = anotherServerDebuggable(target);
+                if (cd != null) //another server instance with the same parameters
+                    if (!_resolveServerConflict(target, ui, cd))
+                        return false;
+                
                 return _startDebug(target, ui);
             } else {
                 if (ss.isRunning(target)) {
