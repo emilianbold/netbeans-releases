e3357dfb7f93764e92664bcee0e66ebb0ac5f78f
tzezula <tzezula@netbeans.org>
2005-03-21 16:42:28 +0000

    #56696:Deadlock after clicking OK in project customizer

--- java.j2seproject/src/org/netbeans/modules/java/j2seproject/classpath/SourcePathImplementation.java
+++ java.j2seproject/src/org/netbeans/modules/java/j2seproject/classpath/SourcePathImplementation.java
@@ -44,15 +44,22 @@ final class SourcePathImplementation implements ClassPathImplementation, Propert
         this.sourceRoots.addPropertyChangeListener (this);
     }
 
-    public synchronized List /*<PathResourceImplementation>*/ getResources() {
-        if (this.resources == null) {
-            List result = new ArrayList ();
-            URL[] roots = this.sourceRoots.getRootURLs();
-            for (int i = 0; i < roots.length; i++) {
-                PathResourceImplementation res = ClassPathSupport.createResource(roots[i]);
-                result.add (res);
+    public List /*<PathResourceImplementation>*/ getResources() {
+        synchronized (this) {
+            if (this.resources != null) {
+                return this.resources;
             }
-            this.resources = Collections.unmodifiableList(result);
+        }        
+        URL[] roots = this.sourceRoots.getRootURLs();                                
+        synchronized (this) {
+            if (this.resources == null) {
+                List result = new ArrayList (roots.length);
+                for (int i = 0; i < roots.length; i++) {
+                    PathResourceImplementation res = ClassPathSupport.createResource(roots[i]);
+                    result.add (res);
+                }
+                this.resources = Collections.unmodifiableList(result);
+            }
         }
         return this.resources;
     }
