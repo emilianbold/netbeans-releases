f5b40fce9380744e4aef425f6fcb3d7a0285ac64
abadea <abadea@netbeans.org>
2005-03-28 09:40:44 +0000

    #56754: Deadlock after clicking OK in project customizer

--- web.project/src/org/netbeans/modules/web/project/classpath/JspSourcePathImplementation.java
+++ web.project/src/org/netbeans/modules/web/project/classpath/JspSourcePathImplementation.java
@@ -51,23 +51,32 @@ final class JspSourcePathImplementation implements ClassPathImplementation, Prop
     }
 
     public synchronized List /*<PathResourceImplementation>*/ getResources() {
-        if (this.resources == null) {
-            List result = new ArrayList ();
-            if (documentBaseDir != null) {
-                try {
-                    result.add(ClassPathSupport.createResource(documentBaseDir.getURL()));
-                }
-                catch (FileStateInvalidException e){
-                    ErrorManager.getDefault().notify(e);
-                }
+        synchronized (this) {
+            if (this.resources != null)
+                return resources;
+        }
+        URL[] roots = this.sourceRoots.getRootURLs();
+        PathResourceImplementation documentBaseDirRes = null;
+        if (documentBaseDir != null) {
+            try {
+                documentBaseDirRes = ClassPathSupport.createResource(documentBaseDir.getURL());
             }
-            URL[] roots = this.sourceRoots.getRootURLs();
-            for (int i = 0; i < roots.length; i++) {
-                PathResourceImplementation res = ClassPathSupport.createResource(roots[i]);
-                result.add (res);
+            catch (FileStateInvalidException e){
+                ErrorManager.getDefault().notify(e);
             }
-            
-            this.resources = Collections.unmodifiableList(result);
+        }
+        synchronized (this) {
+            if (this.resources == null) {
+                List result = new ArrayList ();
+                if (documentBaseDir != null) {
+                    result.add(documentBaseDir);
+                }
+                for (int i = 0; i < roots.length; i++) {
+                    PathResourceImplementation res = ClassPathSupport.createResource(roots[i]);
+                    result.add (res);
+                }
+                this.resources = Collections.unmodifiableList(result);
+            }
         }
         return this.resources;
     }
