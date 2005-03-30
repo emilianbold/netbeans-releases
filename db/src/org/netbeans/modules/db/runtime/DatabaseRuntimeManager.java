cab993e15c220ba0d9dd3280d80667f7d16d60ad
rkubacki <rkubacki@netbeans.org>
2005-03-30 13:43:30 +0000

    SPL

--- db/src/org/netbeans/modules/db/runtime/DatabaseRuntimeManager.java
+++ db/src/org/netbeans/modules/db/runtime/DatabaseRuntimeManager.java
@@ -1,7 +1,14 @@
 /*
- * DatabaseRuntimeManager.java
+ *                 Sun Public License Notice
  *
- * Created on June 22, 2004, 10:28 AM
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ *
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
+ * Microsystems, Inc. All Rights Reserved.
  */
 
 package org.netbeans.modules.db.runtime;
