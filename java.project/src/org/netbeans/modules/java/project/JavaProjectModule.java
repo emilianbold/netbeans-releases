c47672604f0e32503bb3d60e7796c1e6cdb5fcc5
phrebejk <phrebejk@netbeans.org>
2005-04-01 18:11:24 +0000

    #57260 Removing action wrappers.

--- java.project/src/org/netbeans/modules/java/project/JavaProjectModule.java
+++ java.project/src/org/netbeans/modules/java/project/JavaProjectModule.java
@@ -12,109 +12,37 @@
  */
 
 package org.netbeans.modules.java.project;
-
-import java.awt.event.ActionEvent;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
 import javax.swing.Action;
-import org.openide.util.Lookup;
 import org.netbeans.spi.project.ActionProvider;
 import org.netbeans.spi.project.ui.support.FileSensitiveActions;
-import org.openide.util.ContextAwareAction;
-import org.openide.util.HelpCtx;
 import org.openide.util.NbBundle;
-import org.openide.util.actions.CallableSystemAction;
+
 
 /**
  * Supplies project-specific file actions (e.g. compile/run/debug) for *.java files.
  * @author Petr Hrebejk
  */
 public class JavaProjectModule {
-            
-    private static class ActionWrapper extends CallableSystemAction implements ContextAwareAction, PropertyChangeListener {
-        
-        private Action action;
-        
-        public ActionWrapper( Action action ) {
-            this.action = action;            
-        }
-            
-        public String getName() {
-            return (String)action.getValue( Action.NAME );
-        }
-
-        public String iconResource() {
-            return null;
-        }
-
-        public HelpCtx getHelpCtx() {
-            return HelpCtx.DEFAULT_HELP;
-        }
-
-        protected boolean asynchronous() {
-            return false;
-        }
-
-        public void actionPerformed( ActionEvent ev ) {
-            action.actionPerformed(ev);
-        }
-        
-        public boolean isEnabled() {
-            return action.isEnabled();            
-        }
-
-        protected void addNotify() {
-            this.action.addPropertyChangeListener( this );
-            super.addNotify();
-        }
-        
-        protected void removeNotify() {
-            this.action.removePropertyChangeListener( this );
-            super.removeNotify();
-        }
-        
-        public void performAction() {
-            actionPerformed( new ActionEvent( this, 0, "" ) ); // NOI18N
-        }
-        
-        public Action createContextAwareInstance( Lookup actionContext ) {
-            return ((ContextAwareAction)action).createContextAwareInstance( actionContext );
-        }
-        
-        public void propertyChange( PropertyChangeEvent evt ) {
-            firePropertyChange( evt.getPropertyName(), evt.getOldValue(), evt.getNewValue() );
-        }
-        
-    }
-    
-    public static class CompileWrapper extends ActionWrapper {
-        
-        CompileWrapper() {
-            super( FileSensitiveActions.fileCommandAction( 
+          
+    public static Action compile() {
+        return FileSensitiveActions.fileCommandAction( 
                        ActionProvider.COMMAND_COMPILE_SINGLE, 
                        NbBundle.getMessage( JavaProjectModule.class, "LBL_CompileFile_Action" ), // NOI18N
-                       null ) );
-        }
-        
+                       null );
     }
-    
-    public static class RunWrapper extends ActionWrapper {
-        RunWrapper() {
-            super( FileSensitiveActions.fileCommandAction( 
+            
+    public static Action run() {
+        return FileSensitiveActions.fileCommandAction( 
                        ActionProvider.COMMAND_RUN_SINGLE, 
                        NbBundle.getMessage( JavaProjectModule.class, "LBL_RunFile_Action" ), // NOI18N
-                       null ) );
-            
-        }
+                       null );
     }
     
-    public static class DebugWrapper extends ActionWrapper {
-        DebugWrapper() {
-            super( FileSensitiveActions.fileCommandAction( 
+    public static Action debug() {
+        return FileSensitiveActions.fileCommandAction( 
                        ActionProvider.COMMAND_DEBUG_SINGLE, 
                        NbBundle.getMessage( JavaProjectModule.class, "LBL_DebugFile_Action" ), // NOI18N
-                       null ) );
-        }
+                       null );
     }
     
 }
