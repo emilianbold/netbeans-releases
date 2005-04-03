965f53ce587a1996c7151ad1269ba5e4950c8709
jglick <jglick@netbeans.org>
2005-04-03 01:36:38 +0000

    SPL.

--- openide.loaders/test/unit/src/org/openide/loaders/ConnectionSupportTest.java
+++ openide.loaders/test/unit/src/org/openide/loaders/ConnectionSupportTest.java
@@ -1,8 +1,14 @@
 /*
- * ConnectionSupportTest.java
- * JUnit based test
- *
- * Created on 7. únor 2005, 14:10
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
+ * Microsystems, Inc. All Rights Reserved.
  */
 
 package org.openide.loaders;
