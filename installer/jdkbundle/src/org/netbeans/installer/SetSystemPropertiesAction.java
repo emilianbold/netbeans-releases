733207dcbb52abf2b47c9750f8c54a88b2b10304
mslama <mslama@netbeans.org>
2005-04-20 13:15:29 +0000

    #51593: Resolve product bean properties at runtime. Move all version dependent properties to Bundle.properties.

--- installer/jdkbundle/src/org/netbeans/installer/SetSystemPropertiesAction.java
+++ installer/jdkbundle/src/org/netbeans/installer/SetSystemPropertiesAction.java
@@ -7,12 +7,14 @@
  * http://www.sun.com/
  *
  * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
 package org.netbeans.installer;
 
+import com.installshield.product.SoftwareObjectKey;
+import com.installshield.product.SoftwareVersion;
 import com.installshield.product.service.product.ProductService;
 import com.installshield.util.Log;
 import com.installshield.wizard.WizardAction;
@@ -50,6 +52,7 @@ public class SetSystemPropertiesAction extends WizardAction {
         //RunnableWizardBeanState state = getState();
         //String msg = resolveString("$L(com.sun.installer.InstallerResources,INIT_PROPS_MSG)");
         //state.setStatusDescription(msg);
+        resolveProductBeanProperties();
         checkStorageBuilder();
         setDesktopIconName();
         setAdminProperties();
@@ -259,6 +262,142 @@ public class SetSystemPropertiesAction extends WizardAction {
             ex.printStackTrace();
         }
         return jreHome;
+    }
+    
+    private void resolveProductBeanProperties () {
+        try {
+            ProductService service = (ProductService) getService(ProductService.NAME);
+            String prop;
+            String resolvedProp;
+            
+            prop = (String) service.getProductBeanProperty
+            (ProductService.DEFAULT_PRODUCT_SOURCE, Names.PRODUCT_ROOT_ID, "name");
+            resolvedProp = resolveString(prop);
+            logEvent(this, Log.DBG,"prop: " + prop + " resolvedProp: " + resolvedProp);
+            service.setRetainedProductBeanProperty
+            (ProductService.DEFAULT_PRODUCT_SOURCE, Names.PRODUCT_ROOT_ID, "name", resolvedProp);
+
+            prop = (String) service.getProductBeanProperty
+            (ProductService.DEFAULT_PRODUCT_SOURCE, Names.PRODUCT_ROOT_ID, "description");
+            resolvedProp = resolveString(prop);
+            logEvent(this, Log.DBG,"prop: " + prop + " resolvedProp: " + resolvedProp);
+            service.setRetainedProductBeanProperty
+            (ProductService.DEFAULT_PRODUCT_SOURCE, Names.PRODUCT_ROOT_ID, "description", resolvedProp);
+            
+            prop = (String) service.getProductBeanProperty
+            (ProductService.DEFAULT_PRODUCT_SOURCE, Names.PRODUCT_ROOT_ID, "productNumber");
+            resolvedProp = resolveString(prop);
+            logEvent(this, Log.DBG,"prop: " + prop + " resolvedProp: " + resolvedProp);
+            service.setRetainedProductBeanProperty
+            (ProductService.DEFAULT_PRODUCT_SOURCE, Names.PRODUCT_ROOT_ID, "productNumber", resolvedProp);
+            
+            prop = (String) service.getProductBeanProperty
+            (ProductService.DEFAULT_PRODUCT_SOURCE, Names.PRODUCT_ROOT_ID, "vendor");
+            resolvedProp = resolveString(prop);
+            logEvent(this, Log.DBG,"prop: " + prop + " resolvedProp: " + resolvedProp);
+            service.setRetainedProductBeanProperty
+            (ProductService.DEFAULT_PRODUCT_SOURCE, Names.PRODUCT_ROOT_ID, "vendor", resolvedProp);
+            
+            prop = (String) service.getProductBeanProperty
+            (ProductService.DEFAULT_PRODUCT_SOURCE, Names.PRODUCT_ROOT_ID, "vendorWebsite");
+            resolvedProp = resolveString(prop);
+            logEvent(this, Log.DBG,"prop: " + prop + " resolvedProp: " + resolvedProp);
+            service.setRetainedProductBeanProperty
+            (ProductService.DEFAULT_PRODUCT_SOURCE, Names.PRODUCT_ROOT_ID, "vendorWebsite", resolvedProp);
+            
+            SoftwareObjectKey keyObject;
+            SoftwareVersion version;
+            String major, minor, maintenance, key;
+
+            // ---------------------- Product ---------------------------
+            keyObject = (SoftwareObjectKey) service.getProductBeanProperty
+            (ProductService.DEFAULT_PRODUCT_SOURCE, Names.PRODUCT_ROOT_ID, "key");
+            key = resolveString("$L(org.netbeans.installer.Bundle,Product.UID)");
+            logEvent(this, Log.DBG,"Product UID: " + key);
+            keyObject.setUID(key);
+            
+            major = resolveString("$L(org.netbeans.installer.Bundle,Product.major)");
+            minor = resolveString("$L(org.netbeans.installer.Bundle,Product.minor)");
+            maintenance = resolveString("$L(org.netbeans.installer.Bundle,Product.maintenance)");
+            
+            version = new SoftwareVersion();
+            version.setMajor(major);
+            version.setMinor(minor);
+            version.setMaintenance(maintenance);
+            logEvent(this, Log.DBG,"Product version: " + getStringForm(version));
+            keyObject.setVersion(version);
+            
+            service.setRetainedProductBeanProperty
+            (ProductService.DEFAULT_PRODUCT_SOURCE, Names.PRODUCT_ROOT_ID, "key", keyObject);
+            
+            // ------------------ Core IDE -----------------------------
+            keyObject = (SoftwareObjectKey) service.getProductBeanProperty
+            (ProductService.DEFAULT_PRODUCT_SOURCE, Names.CORE_IDE_ID, "key");
+            key = resolveString("$L(org.netbeans.installer.Bundle,CoreIDE.UID)");
+            logEvent(this, Log.DBG,"Core IDE UID: " + key);
+            keyObject.setUID(key);
+            
+            version = new SoftwareVersion();
+            version.setMajor(major);
+            version.setMinor(minor);
+            version.setMaintenance(maintenance);
+            logEvent(this, Log.DBG,"Core IDE version: " + getStringForm(version));
+            keyObject.setVersion(version);
+            
+            service.setRetainedProductBeanProperty
+            (ProductService.DEFAULT_PRODUCT_SOURCE, Names.CORE_IDE_ID, "key", keyObject);
+            
+            // ------------------ J2SE -----------------------------
+            keyObject = (SoftwareObjectKey) service.getProductBeanProperty
+            (ProductService.DEFAULT_PRODUCT_SOURCE, Names.J2SE_ID, "key");
+            key = resolveString("$L(org.netbeans.installer.Bundle,J2SE.UID)");
+            logEvent(this, Log.DBG,"J2SE UID: " + key);
+            keyObject.setUID(key);
+            
+            service.setRetainedProductBeanProperty
+            (ProductService.DEFAULT_PRODUCT_SOURCE, Names.J2SE_ID, "key", keyObject);
+            
+            // ----------------- Storage Builder ------------------------
+            keyObject = (SoftwareObjectKey) service.getProductBeanProperty
+            (ProductService.DEFAULT_PRODUCT_SOURCE, Names.STORAGE_BUILDER_ID, "key");
+            key = resolveString("$L(org.netbeans.installer.Bundle,StorageBuilder.UID)");
+            logEvent(this, Log.DBG,"Storage Builder UID: " + key);
+            keyObject.setUID(key);
+            
+            version = new SoftwareVersion();
+            version.setMajor(major);
+            version.setMinor(minor);
+            version.setMaintenance(maintenance);
+            logEvent(this, Log.DBG,"Storage Builder version: " + getStringForm(version));
+            keyObject.setVersion(version);
+            
+            service.setRetainedProductBeanProperty
+            (ProductService.DEFAULT_PRODUCT_SOURCE, Names.STORAGE_BUILDER_ID, "key", keyObject);
+        } catch (ServiceException ex) {
+            ex.printStackTrace();
+            Util.logStackTrace(this,ex);
+        }
+    }
+    
+    /** Simplified conversion of SoftwareVersion instance to String. Just for logging. */
+    private String getStringForm (SoftwareVersion version) {
+        String ret = "";
+        if (version.getMajor().length() == 0) {
+            return ret;
+        } else {
+            ret = version.getMajor();
+        }
+        if (version.getMinor().length() == 0) {
+            return ret;
+        } else {
+            ret += "." + version.getMinor();
+        }
+        if (version.getMaintenance().length() == 0) {
+            return ret;
+        } else {
+            ret += "." + version.getMaintenance();
+        }
+        return ret;
     }
     
 }
