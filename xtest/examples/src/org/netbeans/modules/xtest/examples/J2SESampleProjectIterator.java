4a6c7537bbd28c42e31a24063700dd06c5cbe8a1
jskrivanek <jskrivanek@netbeans.org>
2005-05-18 09:50:54 +0000

    XTest examples added.

---
+++ xtest/examples/src/org/netbeans/modules/xtest/examples/J2SESampleProjectIterator.java
@@ -0,0 +1,115 @@
+/*
+ *                 Sun Public License Notice
+ *
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ *
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.modules.xtest.examples;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.NoSuchElementException;
+import javax.swing.JComponent;
+import org.openide.WizardDescriptor;
+import org.openide.filesystems.FileObject;
+import org.openide.loaders.DataObject;
+import org.openide.loaders.TemplateWizard;
+import org.openide.util.NbBundle;
+
+/**
+ * @author Martin Grebac
+ */
+public class J2SESampleProjectIterator implements TemplateWizard.Iterator {
+    
+    private static final long serialVersionUID = 4L;
+    
+    int currentIndex;
+    PanelConfigureProject basicPanel;
+    private transient WizardDescriptor wiz;
+
+    static Object create() {
+        return new J2SESampleProjectIterator();
+    }
+    
+    public J2SESampleProjectIterator () {
+    }
+    
+    public void addChangeListener (javax.swing.event.ChangeListener changeListener) {
+    }
+    
+    public void removeChangeListener (javax.swing.event.ChangeListener changeListener) {
+    }
+    
+    public org.openide.WizardDescriptor.Panel current () {
+        return basicPanel;
+    }
+    
+    public boolean hasNext () {
+        return false;
+    }
+    
+    public boolean hasPrevious () {
+        return false;
+    }
+    
+    public void initialize (org.openide.loaders.TemplateWizard templateWizard) {
+        this.wiz = templateWizard;
+        String name = templateWizard.getTemplate().getNodeDelegate().getDisplayName();
+        if (name != null) {
+            name = name.replaceAll(" ", ""); //NOI18N
+        }
+        templateWizard.putProperty (WizardProperties.NAME, name);
+        basicPanel = new PanelConfigureProject();
+        currentIndex = 0;
+        updateStepsList ();
+    }
+    
+    public void uninitialize (org.openide.loaders.TemplateWizard templateWizard) {
+        basicPanel = null;
+        currentIndex = -1;
+        this.wiz.putProperty("projdir",null);           //NOI18N
+        this.wiz.putProperty("name",null);          //NOI18N
+    }
+    
+    public java.util.Set instantiate (org.openide.loaders.TemplateWizard templateWizard) throws java.io.IOException {
+        File projectLocation = (File) wiz.getProperty(WizardProperties.PROJECT_DIR);
+        String name = (String) wiz.getProperty(WizardProperties.NAME);
+                        
+        FileObject prjLoc = null;
+        prjLoc = J2SESampleProjectGenerator.createProjectFromTemplate(templateWizard.getTemplate().getPrimaryFile(), projectLocation, name);
+        return Collections.singleton(DataObject.find(prjLoc));
+    }
+    
+    public String name() {
+        return current().getComponent().getName();
+    }
+    
+    public void nextPanel() {
+        throw new NoSuchElementException ();
+    }
+    
+    public void previousPanel() {
+        throw new NoSuchElementException ();
+    }
+    
+    void updateStepsList() {
+        JComponent component = (JComponent) current ().getComponent ();
+        if (component == null) {
+            return;
+        }
+        String[] list;
+        list = new String[] {
+            NbBundle.getMessage(PanelConfigureProject.class, "LBL_NWP1_ProjectTitleName"), // NOI18N
+        };
+        component.putClientProperty ("WizardPanel_contentData", list); // NOI18N
+        component.putClientProperty ("WizardPanel_contentSelectedIndex", new Integer (currentIndex)); // NOI18N
+    }
+    
+}
---
+++ xtest/examples/src/org/netbeans/modules/xtest/examples/SettingsPanel.java
@@ -0,0 +1,27 @@
+/*
+ *                 Sun Public License Notice
+ *
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ *
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.modules.xtest.examples;
+
+import javax.swing.JPanel;
+import org.openide.WizardDescriptor;
+
+
+abstract class SettingsPanel extends JPanel {
+
+    abstract void store (WizardDescriptor settings);
+
+    abstract void read (WizardDescriptor settings);
+
+    abstract boolean valid (WizardDescriptor settings);
+}
