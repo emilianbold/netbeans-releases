831e299fac259f9ab6edc0c090492371d53bfd21
mkleint <mkleint@netbeans.org>
2005-06-01 13:44:40 +0000

    #59223 - remove antialiasing hints..

--- openide.awt/src/org/openide/awt/HtmlLabelUI.java
+++ openide.awt/src/org/openide/awt/HtmlLabelUI.java
@@ -162,7 +162,6 @@ class HtmlLabelUI extends LabelUI {
         if (hintsMap == null) {
             hintsMap = new HashMap();
             hintsMap.put(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-            hintsMap.put(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         }
 
         return hintsMap;
