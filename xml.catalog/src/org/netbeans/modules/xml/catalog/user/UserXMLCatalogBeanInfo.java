8eab33abd704fa1bd4713b2612e4dff1004223bc
mkuchtiak <mkuchtiak@netbeans.org>
2005-06-03 11:00:07 +0000

    Correct BeanInfo implementation

--- xml.catalog/src/org/netbeans/modules/xml/catalog/user/UserXMLCatalogBeanInfo.java
+++ xml.catalog/src/org/netbeans/modules/xml/catalog/user/UserXMLCatalogBeanInfo.java
@@ -15,17 +15,32 @@ package org.netbeans.modules.xml.catalog.user;
 
 import java.beans.PropertyDescriptor;
 import java.beans.SimpleBeanInfo;
+import org.openide.util.NbBundle;
 
 /**
- * (Non-)description of {@link ProjectXMLCatalogReader}.
- * @author Jesse Glick
+ * description of {@link UserXMLCatalog}.
+ * @author Milan Kuchtiak
  */
 public class UserXMLCatalogBeanInfo extends SimpleBeanInfo {
     
     public UserXMLCatalogBeanInfo() {}
-
+    
+    private static final int PROPERTY_displayName = 0;
+    private static final int PROPERTY_shortDescription = 1;
+    
     public PropertyDescriptor[] getPropertyDescriptors() {
-        return new PropertyDescriptor[0];
+        PropertyDescriptor[] properties = new PropertyDescriptor[2];
+    
+        try {
+            properties[PROPERTY_displayName] = new PropertyDescriptor ("displayName", UserXMLCatalog.class, "getDisplayName", null);
+            properties[PROPERTY_displayName].setDisplayName (NbBundle.getMessage(UserXMLCatalog.class,"PROP_catalog_name"));
+            properties[PROPERTY_displayName].setShortDescription (NbBundle.getMessage(UserXMLCatalog.class,"HINT_catalog_name"));
+            properties[PROPERTY_shortDescription] = new PropertyDescriptor ( "shortDescription", UserXMLCatalog.class, "getShortDescription", null );
+            properties[PROPERTY_shortDescription].setDisplayName (NbBundle.getMessage(UserXMLCatalog.class,"PROP_catalog_desc"));
+            properties[PROPERTY_shortDescription].setShortDescription (NbBundle.getMessage(UserXMLCatalog.class,"HINT_catalog_desc"));
+        } catch( java.beans.IntrospectionException e) {
+        }
+        return properties;
     }
     
 }
