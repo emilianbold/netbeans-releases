c0ffe10dd8698b1ad040911547a99734c3c3ef94
mkuchtiak <mkuchtiak@netbeans.org>
2005-06-10 11:57:39 +0000

    Improving code completion for XML schema. Now GarmmarProvider reads the prefixes and build grammar that correspons to these prefixes.

--- xml.schema/src/org/netbeans/modules/xml/xsd/SchemaGrammarQueryManager.java
+++ xml.schema/src/org/netbeans/modules/xml/xsd/SchemaGrammarQueryManager.java
@@ -13,24 +13,20 @@
 
 package org.netbeans.modules.xml.xsd;
 import java.util.Enumeration;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.xml.sax.*;
+import org.w3c.dom.*;
+import org.xml.sax.InputSource;
 import org.netbeans.modules.xml.dtd.grammar.DTDParser;
 import org.netbeans.api.xml.services.UserCatalog;
+import java.io.*;
 
 /** XML Schema Grammar provided code completion for XML Schema file.
  *
- * @author  mk115033
+ * @author  Milan Kuchtiak
  */
 public class SchemaGrammarQueryManager extends org.netbeans.modules.xml.api.model.GrammarQueryManager
 {
     // actually code completion works only for xsd: and xs: prefixes
-    private static final String SCHEMA_ROOT_XSD="xsd:schema"; //NOI18N
-    private static final String SCHEMA_ROOT_XS="xs:schema"; //NOI18N
-
-    private static final String NS_JAXB="xmlns:jaxb"; //NOI18N
-    private static final String NS_JXB="xmlns:jxb"; //NOI18N
+    private static final String SCHEMA="schema"; //NOI18N
     private static final String PUBLIC_JAXB="http://java.sun.com/xml/ns/jaxb"; //NOI18N
     
     private String prefix, ns_jaxb;
@@ -38,6 +34,8 @@ public class SchemaGrammarQueryManager extends org.netbeans.modules.xml.api.mode
     public java.util.Enumeration enabled(org.netbeans.modules.xml.api.model.GrammarEnvironment ctx) {
         if (ctx.getFileObject() == null) return null;
         Enumeration en = ctx.getDocumentChildren();
+        prefix=null;
+        ns_jaxb=null;
         while (en.hasMoreElements()) {
             Node next = (Node) en.nextElement();
             if (next.getNodeType() == next.DOCUMENT_TYPE_NODE) {
@@ -45,35 +43,23 @@ public class SchemaGrammarQueryManager extends org.netbeans.modules.xml.api.mode
             } else if (next.getNodeType() == next.ELEMENT_NODE) {
                 Element element = (Element) next;
                 String tagName = element.getTagName();
-                if (SCHEMA_ROOT_XSD.equals(tagName)) {  // NOI18N
-                    prefix = SCHEMA_ROOT_XSD;
-                    String attr = element.getAttribute(NS_JAXB);
-                    if (PUBLIC_JAXB.equals(attr)) {
-                        ns_jaxb=NS_JAXB;
-                    } else {
-                        attr = element.getAttribute(NS_JXB);
-                        if (PUBLIC_JAXB.equals(attr)) {
-                            ns_jaxb=NS_JXB;
-                        }
-                    }
-                    return org.openide.util.Enumerations.singleton (next);
-                } else if (SCHEMA_ROOT_XS.equals(tagName)) {  // NOI18N
-                    prefix = SCHEMA_ROOT_XS;
-                    String attr = element.getAttribute(NS_JAXB);
-                    if (PUBLIC_JAXB.equals(attr)) {
-                        ns_jaxb=NS_JAXB;
-                    } else {
-                        attr = element.getAttribute(NS_JXB);
-                        if (PUBLIC_JAXB.equals(attr)) {
-                            ns_jaxb=NS_JXB;
-                        }
-                    }
-                    return org.openide.util.Enumerations.singleton (next);
+                if (tagName.endsWith(":"+SCHEMA)) { //NOI18N
+                    prefix = tagName.substring(0,tagName.indexOf(":"+SCHEMA));
+                } else if (tagName.equals(SCHEMA)) {
+                    prefix = "";
                 }
-                
+                if (prefix==null) return null;
+                NamedNodeMap map = element.getAttributes();
+                for (int i=0; i<map.getLength();i++) {
+                    Attr attr = (Attr)map.item(i);
+                    String name = attr.getName();
+                    if (PUBLIC_JAXB.equals(attr.getValue())) {
+                        if (name.startsWith("xmlns:")) ns_jaxb=name.substring(6); //NOI18N
+                    }
+                }
+                return org.openide.util.Enumerations.singleton (next);
             }
         }
-        
         return null;
     }
     
@@ -84,27 +70,40 @@ public class SchemaGrammarQueryManager extends org.netbeans.modules.xml.api.mode
     /** Returns DTD for code completion
     */
     public org.netbeans.modules.xml.api.model.GrammarQuery getGrammar(org.netbeans.modules.xml.api.model.GrammarEnvironment ctx) {
+        if (prefix==null) return null;
         InputSource inputSource = null;
-        if (SCHEMA_ROOT_XSD.equals(prefix)) {
-            if (NS_JAXB.equals(ns_jaxb))
-                inputSource = new InputSource("nbres:/org/netbeans/modules/xml/schema/resources/XMLSchema_xsd_jaxb.dtd"); //NOI18N
-            else if (NS_JXB.equals(ns_jaxb))
-                inputSource = new InputSource("nbres:/org/netbeans/modules/xml/schema/resources/XMLSchema_xsd_jxb.dtd"); //NOI18N
-            else
-                inputSource = new InputSource("nbres:/org/netbeans/modules/xml/schema/resources/XMLSchema_xsd.dtd"); //NOI18N
-        } else if (SCHEMA_ROOT_XS.equals(prefix)) {
-            if (NS_JAXB.equals(ns_jaxb))
-                inputSource = new InputSource("nbres:/org/netbeans/modules/xml/schema/resources/XMLSchema_xs_jaxb.dtd"); //NOI18N
-            else if (NS_JXB.equals(ns_jaxb))
-                inputSource = new InputSource("nbres:/org/netbeans/modules/xml/schema/resources/XMLSchema_xs_jxb.dtd"); //NOI18N
-            else
-            inputSource = new InputSource("nbres:/org/netbeans/modules/xml/schema/resources/XMLSchema_xs.dtd"); //NOI18N
+        StringBuffer buffer=new StringBuffer();
+        if (prefix.length()==0) {
+            buffer.append("<!ENTITY % p ''><!ENTITY % s ''>"); //NOI18N
+        } else {
+            buffer.append("<!ENTITY % p '"+prefix+":'><!ENTITY % s ':"+prefix+"'>"); //NOI18N
         }
+        java.io.InputStream is = null;
+        if (ns_jaxb==null) {
+            is = getClass().getResourceAsStream("/org/netbeans/modules/xml/schema/resources/XMLSchema.dtd"); //NOI18N
+        } else {
+            is = getClass().getResourceAsStream("/org/netbeans/modules/xml/schema/resources/XMLSchema_jaxb.dtd"); //NOI18N
+            buffer.append("<!ENTITY % jaxb '"+ns_jaxb+"'>"); //NOI18N
+        }
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        String line = null;
+        try {
+            while ((line=br.readLine())!=null) {
+                buffer.append(line);
+            }
+            br.close();
+        } catch (IOException ex) {
+            return null;
+        }
+        inputSource = new InputSource(new StringReader(buffer.toString()));
+        if (ns_jaxb==null)
+            inputSource.setSystemId("nbres:/org/netbeans/modules/xml/schema/resources/XMLSchema.dtd"); //NOI18N
+        else
+            inputSource.setSystemId("nbres:/org/netbeans/modules/xml/schema/resources/XMLSchema_jaxb.dtd"); //NOI18N
         if (inputSource!=null) {
             DTDParser dtdParser = new DTDParser(true);
             return dtdParser.parse(inputSource);
         }
         return null;
-    }
-    
+    }    
 }
