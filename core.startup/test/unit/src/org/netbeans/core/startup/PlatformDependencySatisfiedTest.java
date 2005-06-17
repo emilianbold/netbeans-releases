794935190fecf1df9833c40856980c99302ce9b5
jtulach <jtulach@netbeans.org>
2005-06-17 10:46:34 +0000

    Rewriting the test, so it fakes the platforms and tests more than one at once.

--- core.startup/test/unit/src/org/netbeans/core/startup/PlatformDependencySatisfiedTest.java
+++ core.startup/test/unit/src/org/netbeans/core/startup/PlatformDependencySatisfiedTest.java
@@ -44,30 +44,79 @@ public class PlatformDependencySatisfiedTest extends SetupHid {
         
         File tmp = File.createTempFile ("PlatformDependencySatisfiedModule", ".jar");
         moduleJarFile = tmp;
+
+        // clean the operatingSystem field
+        java.lang.reflect.Field f;
+        f = org.openide.util.Utilities.class.getDeclaredField("operatingSystem");
+        f.setAccessible(true);
+        f.set(null, new Integer(-1));
+    }
+    
+    public void testWindows2000() throws Exception {
+        System.setProperty("os.name", "Windows 2000");
+        assertTrue("We are on windows", org.openide.util.Utilities.isWindows());
         
+        assertEnableModule("org.openide.modules.os.Windows", true);
+        assertEnableModule("org.openide.modules.os.MacOSX", false);
+        assertEnableModule("org.openide.modules.os.Unix", false);
+        assertEnableModule("org.openide.modules.os.Garbage", false);
+    }
+    
+    public void testMacOSX() throws Exception {
+        System.setProperty("os.name", "Mac OS X");
+        assertTrue("We are on mac", (org.openide.util.Utilities.getOperatingSystem() & org.openide.util.Utilities.OS_MAC) != 0);
+        
+        assertEnableModule("org.openide.modules.os.Windows", false);
+        assertEnableModule("org.openide.modules.os.MacOSX", true);
+        assertEnableModule("org.openide.modules.os.Unix", true);
+        assertEnableModule("org.openide.modules.os.Garbage", false);
+    }
+
+    public void testDarwin() throws Exception {
+        System.setProperty("os.name", "Darwin");
+        assertTrue("We are on mac", (org.openide.util.Utilities.getOperatingSystem() & org.openide.util.Utilities.OS_MAC) != 0);
+        
+        assertEnableModule("org.openide.modules.os.Windows", false);
+        assertEnableModule("org.openide.modules.os.MacOSX", true);
+        assertEnableModule("org.openide.modules.os.Unix", true);
+        assertEnableModule("org.openide.modules.os.Garbage", false);
+    }
+    
+    public void testLinux() throws Exception {
+        System.setProperty("os.name", "Fedora Linux");
+        assertTrue("We are on linux", (org.openide.util.Utilities.getOperatingSystem() & org.openide.util.Utilities.OS_LINUX) != 0);
+        
+        assertEnableModule("org.openide.modules.os.Windows", false);
+        assertEnableModule("org.openide.modules.os.MacOSX", false);
+        assertEnableModule("org.openide.modules.os.Unix", true);
+        assertEnableModule("org.openide.modules.os.Garbage", false);
+    }
+
+    public void testBSD() throws Exception {
+        System.setProperty("os.name", "FreeBSD X1.4");
+        assertTrue("We are on unix", org.openide.util.Utilities.isUnix());
+        
+        assertEnableModule("org.openide.modules.os.Windows", false);
+        assertEnableModule("org.openide.modules.os.MacOSX", false);
+        assertEnableModule("org.openide.modules.os.Unix", true);
+        assertEnableModule("org.openide.modules.os.Garbage", false);
+    }
+    
+    /**  */
+    private void assertEnableModule(String req, boolean enable) throws Exception {
         Manifest man = new Manifest ();
         man.getMainAttributes ().putValue ("Manifest-Version", "1.0");
         man.getMainAttributes ().putValue ("OpenIDE-Module", "org.test.PlatformDependency/1");
         
-        String req = null;
-        if (org.openide.util.Utilities.isWindows ()) {
-            req = "org.openide.modules.os.Windows";
-        } else if ((org.openide.util.Utilities.getOperatingSystem () & org.openide.util.Utilities.OS_MAC) != 0) {
-            req = "org.openide.modules.os.MacOSX";
-        } else if (org.openide.util.Utilities.isUnix ()) {
-            req = "org.openide.modules.os.Unix";
-        }
         assertNotNull ("This test is supposed to run on windows, unix and macosx, fail otherwise on this line", req);
         
         man.getMainAttributes ().putValue ("OpenIDE-Module-Requires", req);
         
-        JarOutputStream os = new JarOutputStream (new FileOutputStream (tmp), man);
+        JarOutputStream os = new JarOutputStream (new FileOutputStream (moduleJarFile), man);
         os.putNextEntry (new JarEntry ("empty/test.txt"));
         os.close ();
-    }
-    
-    /**  */
-    public void testTryToInstallTheModuleWhichRequiresTheOS () throws Exception {
+        
+        
         Main.getModuleSystem (); // init module system
         final FakeEvents ev = new FakeEvents();
         org.netbeans.core.startup.NbInstaller installer = new org.netbeans.core.startup.NbInstaller(ev);
@@ -77,9 +126,15 @@ public class PlatformDependencySatisfiedTest extends SetupHid {
         mgr.mutexPrivileged().enterWriteAccess();
         try {
             Module m1 = mgr.create(moduleJarFile, null, false, false, false);
-            assertEquals(Collections.EMPTY_SET, m1.getProblems());
-            mgr.enable(m1);
-            mgr.disable(m1);
+            
+            
+            if (enable) {
+                assertEquals(Collections.EMPTY_SET, m1.getProblems());
+                mgr.enable(m1);
+                mgr.disable(m1);
+            } else {
+                assertFalse("We should not be able to enable the module", m1.getProblems().isEmpty());
+            }
             mgr.delete(m1);
         } finally {
             mgr.mutexPrivileged().exitWriteAccess();
