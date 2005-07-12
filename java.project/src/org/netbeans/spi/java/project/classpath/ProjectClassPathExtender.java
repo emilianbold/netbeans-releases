311a445cd324577b79e833a35544f30acf53d7b1
jglick <jglick@netbeans.org>
2005-07-12 04:06:10 +0000

    Some Javadoc.

--- java.project/src/org/netbeans/spi/java/project/classpath/ProjectClassPathExtender.java
+++ java.project/src/org/netbeans/spi/java/project/classpath/ProjectClassPathExtender.java
@@ -10,55 +10,51 @@
  * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
+
 package org.netbeans.spi.java.project.classpath;
 
 import java.io.IOException;
 import java.net.URI;
-import org.openide.filesystems.FileObject;
-import org.netbeans.api.project.libraries.Library;
 import org.netbeans.api.project.ant.AntArtifact;
-
-
-
-
+import org.netbeans.api.project.libraries.Library;
+import org.openide.filesystems.FileObject;
 
 /**
- * Interface for project's compile classpath extension
- * Project can provide this interface in its lookup to
+ * Interface for project's compile classpath extension.
+ * A project can provide this interface in its {@link org.netbeans.api.project.Project#getLookup lookup} to
  * allow clients to extend its compilation classpath
- * by new classpath element (jar, folder, dependent project, library).
+ * by a new classpath element (JAR, folder, dependent project, or library).
  * @since org.netbeans.modules.java.project/1 1.3 
  */
 public interface ProjectClassPathExtender {
 
     /**
-     * Adds Library into project's compile classpath if the
+     * Adds a library into the project's compile classpath if the
      * library is not already included.
-     * @param library to be added, not null.
+     * @param library to be added
      * @return true in case the classpath was changed
-     * @exception IOException in case the project metadata can not be stored.
+     * @exception IOException in case the project metadata cannot be changed
      */
-    public boolean addLibrary (Library library) throws IOException;
+    boolean addLibrary(Library library) throws IOException;
 
     /**
-     * Adds archive file into project's compile classpath if the
-     * file is not already on it.
-     * @param archiveFile to be added, can't be null.
+     * Adds an archive file or folder into the project's compile classpath if the
+     * entry is not already there.
+     * @param archiveFile ZIP/JAR file to be added
      * @return true in case the classpath was changed
-     * @exception IOException in case the project metadata can not be stored.
+     * @exception IOException in case the project metadata cannot be changed
      */
-    public boolean addArchiveFile (FileObject archiveFile) throws IOException;
-
+    boolean addArchiveFile(FileObject archiveFile) throws IOException;
 
     /**
-     * Adds AntArtifact into project's compile classpath if the
+     * Adds an artifact (e.g. subproject) into project's compile classpath if the
      * artifact is not already on it.
-     * @param artifact to be added, can't be null.
-     * @param artifactElement the URI of the build output, the artifactElement
-     * has to be owned by the artifact and must be relative to it.
+     * @param artifact to be added
+     * @param artifactElement the URI of the build output
+     *                        (must be owned by the artifact and be relative to it)
      * @return true in case the classpath was changed
-     * @exception IOException in case the project metadata can not be stored.
+     * @exception IOException in case the project metadata cannot be changed
      */
-    public boolean addAntArtifact (AntArtifact artifact, URI artifactElement) throws IOException;
+    boolean addAntArtifact(AntArtifact artifact, URI artifactElement) throws IOException;
 
 }
