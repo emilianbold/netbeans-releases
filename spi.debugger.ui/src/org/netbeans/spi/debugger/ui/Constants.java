3c88d0dcd2ae0f94096d53cbb1d786c0ecb5d74d
mentlicher <mentlicher@netbeans.org>
2005-07-19 16:44:46 +0000

    Fixing javadoc & arch questions.

--- spi.debugger.ui/src/org/netbeans/spi/debugger/ui/Constants.java
+++ spi.debugger.ui/src/org/netbeans/spi/debugger/ui/Constants.java
@@ -7,7 +7,7 @@
  * http://www.sun.com/
  * 
  * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2000 Sun
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -24,27 +24,27 @@ public interface Constants {
     /**
      * Thread State column id.
      *
-     * @see org.netbeans.spi.treeview.ColumnModel#getID
-     * @see org.netbeans.spi.treeview.ColumnModel#getPreviuosColumnID
-     * @see org.netbeans.spi.treeview.ColumnModel#getNextColumnID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getPreviuosColumnID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getNextColumnID
      */
     public static final String THREAD_STATE_COLUMN_ID = "ThreadState";
 
     /**
      * Thread Suspended column id.
      *
-     * @see org.netbeans.spi.treeview.ColumnModel#getID
-     * @see org.netbeans.spi.treeview.ColumnModel#getPreviuosColumnID
-     * @see org.netbeans.spi.treeview.ColumnModel#getNextColumnID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getPreviuosColumnID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getNextColumnID
      */
     public static final String THREAD_SUSPENDED_COLUMN_ID = "ThreadSuspended";
 
     /**
      * Breakpoint Enabled column id.
      *
-     * @see org.netbeans.spi.treeview.ColumnModel#getID
-     * @see org.netbeans.spi.treeview.ColumnModel#getPreviuosColumnID
-     * @see org.netbeans.spi.treeview.ColumnModel#getNextColumnID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getPreviuosColumnID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getNextColumnID
      */
     public static final String BREAKPOINT_ENABLED_COLUMN_ID = 
         "BreakpointEnabled";
@@ -52,9 +52,9 @@ public interface Constants {
     /**
      * CallStackFrame Location column id.
      *
-     * @see org.netbeans.spi.treeview.ColumnModel#getID
-     * @see org.netbeans.spi.treeview.ColumnModel#getPreviuosColumnID
-     * @see org.netbeans.spi.treeview.ColumnModel#getNextColumnID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getPreviuosColumnID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getNextColumnID
      */
     public static final String CALL_STACK_FRAME_LOCATION_COLUMN_ID = 
         "CallStackFrameLocation";
@@ -62,81 +62,81 @@ public interface Constants {
     /**
      * Locals toString () column id.
      *
-     * @see org.netbeans.spi.treeview.ColumnModel#getID
-     * @see org.netbeans.spi.treeview.ColumnModel#getPreviuosColumnID
-     * @see org.netbeans.spi.treeview.ColumnModel#getNextColumnID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getPreviuosColumnID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getNextColumnID
      */
     public static final String LOCALS_TO_STRING_COLUMN_ID = "LocalsToString";
 
     /**
      * Locals Tyoe column id.
      *
-     * @see org.netbeans.spi.treeview.ColumnModel#getID
-     * @see org.netbeans.spi.treeview.ColumnModel#getPreviuosColumnID
-     * @see org.netbeans.spi.treeview.ColumnModel#getNextColumnID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getPreviuosColumnID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getNextColumnID
      */
     public static final String LOCALS_TYPE_COLUMN_ID = "LocalsType";
 
     /**
      * Locals Value column id.
      *
-     * @see org.netbeans.spi.treeview.ColumnModel#getID
-     * @see org.netbeans.spi.treeview.ColumnModel#getPreviuosColumnID
-     * @see org.netbeans.spi.treeview.ColumnModel#getNextColumnID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getPreviuosColumnID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getNextColumnID
      */
     public static final String LOCALS_VALUE_COLUMN_ID = "LocalsValue";
 
     /**
      * Watch toString () column id.
      *
-     * @see org.netbeans.spi.treeview.ColumnModel#getPreviuosColumnID
-     * @see org.netbeans.spi.treeview.ColumnModel#getNextColumnID
-     * @see org.netbeans.spi.treeview.ColumnModel#getID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getPreviuosColumnID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getNextColumnID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getID
      */
     public static final String WATCH_TO_STRING_COLUMN_ID = "WatchToString";
 
     /**
      * Watch Tyoe column id.
      *
-     * @see org.netbeans.spi.treeview.ColumnModel#getID
-     * @see org.netbeans.spi.treeview.ColumnModel#getPreviuosColumnID
-     * @see org.netbeans.spi.treeview.ColumnModel#getNextColumnID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getPreviuosColumnID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getNextColumnID
      */
     public static final String WATCH_TYPE_COLUMN_ID = "WatchType";
 
     /**
      * Watch Value column id.
      *
-     * @see org.netbeans.spi.treeview.ColumnModel#getID
-     * @see org.netbeans.spi.treeview.ColumnModel#getPreviuosColumnID
-     * @see org.netbeans.spi.treeview.ColumnModel#getNextColumnID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getPreviuosColumnID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getNextColumnID
      */
     public static final String WATCH_VALUE_COLUMN_ID = "WatchValue";
 
     /**
      * Session Host Name column id.
      *
-     * @see org.netbeans.spi.treeview.ColumnModel#getID
-     * @see org.netbeans.spi.treeview.ColumnModel#getPreviuosColumnID
-     * @see org.netbeans.spi.treeview.ColumnModel#getNextColumnID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getPreviuosColumnID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getNextColumnID
      */
     public static final String SESSION_HOST_NAME_COLUMN_ID = "SessionHostName";
 
     /**
      * Session State column id.
      *
-     * @see org.netbeans.spi.treeview.ColumnModel#getID
-     * @see org.netbeans.spi.treeview.ColumnModel#getPreviuosColumnID
-     * @see org.netbeans.spi.treeview.ColumnModel#getNextColumnID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getPreviuosColumnID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getNextColumnID
      */
     public static final String SESSION_STATE_COLUMN_ID = "SessionState";
 
     /**
      * Session Language column id.
      *
-     * @see org.netbeans.spi.treeview.ColumnModel#getID
-     * @see org.netbeans.spi.treeview.ColumnModel#getPreviuosColumnID
-     * @see org.netbeans.spi.treeview.ColumnModel#getNextColumnID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getPreviuosColumnID
+     * @see org.netbeans.spi.viewmodel.ColumnModel#getNextColumnID
      */
     public static final String SESSION_LANGUAGE_COLUMN_ID = "SessionLanguage";
 }
