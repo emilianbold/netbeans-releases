1b36dcea79ee27b4b307f964ec1517a23df06a90
msandor <msandor@netbeans.org>
2005-07-25 10:02:28 +0000

    Adding javahelp helpset

---
+++ versioning.system.cvss/javahelp/org/netbeans/modules/versioning/system/cvss/docs/cvsmodule.hs
@@ -0,0 +1,47 @@
+﻿<?xml version='1.0' encoding='ISO-8859-1' ?>
+<!--
+*     Copyright © 2005 Sun Microsystems, Inc. All rights reserved.
+*     Use is subject to license terms.
+-->
+<!DOCTYPE helpset
+  PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN"
+         "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
+<helpset version="2.0">
+
+<!-- last updated 08feb02-->
+
+  <!-- title -->
+  <title>CVS</title>
+  
+  <!-- maps -->
+  <maps>
+     <homeID>ctx_cvsmoduleintro</homeID>
+     <mapref location="cvsmodule.jhm" />
+  </maps>
+  
+  <!-- views -->
+  <view>
+    <name>TOC</name>
+    <label>Table Of Contents</label>
+    <type>javax.help.TOCView</type>
+    <data>cvsmodule-toc.xml</data> 
+ </view>
+ 
+  <view>
+    <name>Index</name>
+    <label>Index</label>
+    <type>javax.help.IndexView</type>
+    <data>cvsmodule-idx.xml</data>
+  </view>
+
+  <view>
+    <name>Search</name>
+   <label>Search</label>
+   <type>javax.help.SearchView</type>
+   <data engine="com.sun.java.help.search.DefaultSearchEngine">
+      JavaHelpSearch
+   </data>
+  </view>
+
+
+</helpset>
