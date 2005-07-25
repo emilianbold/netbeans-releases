6a03523d0363ed35edd21170643e9085fb8ffa28
msandor <msandor@netbeans.org>
2005-07-25 08:07:13 +0000

    #61273: NPE wokaround

--- versioning.system.cvss/src/org/netbeans/modules/versioning/system/cvss/ui/actions/commit/CommitTable.java
+++ versioning.system.cvss/src/org/netbeans/modules/versioning/system/cvss/ui/actions/commit/CommitTable.java
@@ -70,6 +70,7 @@ class CommitTable implements MouseListener, AncestorListener {
     private void setDefaultColumnSizes() {
         int width = table.getWidth();
         TableColumnModel columnModel = table.getColumnModel();
+        if (columnModel == null) return; 
         if (columnModel.getColumnCount() != columns.length) return; 
         if (columns.length == 4) {
             for (int i = 0; i < columns.length; i++) {
