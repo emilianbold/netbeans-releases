0de60b0ce230b712761cfb288abcdb9b5b01f00f
msandor <msandor@netbeans.org>
2005-07-25 14:08:21 +0000

    Initial History UI code

--- versioning.system.cvss/src/org/netbeans/modules/versioning/system/cvss/ui/actions/log/SearchHistoryAction.java
+++ versioning.system.cvss/src/org/netbeans/modules/versioning/system/cvss/ui/actions/log/SearchHistoryAction.java
@@ -14,9 +14,13 @@
 package org.netbeans.modules.versioning.system.cvss.ui.actions.log;
 
 import org.netbeans.modules.versioning.system.cvss.ui.actions.AbstractSystemAction;
+import org.netbeans.modules.versioning.system.cvss.ui.history.SearchHistoryTopComponent;
 import org.netbeans.modules.versioning.system.cvss.FileInformation;
+import org.openide.util.NbBundle;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
+import java.io.File;
 
 /**
  * Search History action.
@@ -34,6 +38,18 @@ public class SearchHistoryAction extends AbstractSystemAction  {
     }
 
     public void actionPerformed(ActionEvent ev) {
+        String title = NbBundle.getMessage(SearchHistoryAction.class, "CTL_SearchHistory_Title", getContextDisplayName());
+        openHistory(getFilesToProcess(), title);
     }
 
+    private void openHistory(final File [] roots, final String title) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                SearchHistoryTopComponent tc = new SearchHistoryTopComponent(roots);
+                tc.setName(title);
+                tc.open();
+                tc.requestActive();
+            }
+        });
+    }
 }
---
+++ versioning.system.cvss/src/org/netbeans/modules/versioning/system/cvss/ui/history/SearchHistoryTopComponent.java
@@ -0,0 +1,64 @@
+/*
+ *                 Sun Public License Notice
+ *
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ *
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.modules.versioning.system.cvss.ui.history;
+
+import org.openide.windows.TopComponent;
+import org.openide.util.NbBundle;
+
+import java.awt.BorderLayout;
+import java.io.File;
+
+/**
+ * Search History component.
+ *  
+ * @author Maros Sandor
+ */
+public class SearchHistoryTopComponent extends TopComponent {
+    
+    private File[] roots;
+
+    public SearchHistoryTopComponent() {
+    }
+    
+    public SearchHistoryTopComponent(File [] roots) {
+        this.roots = roots;
+        initComponents();
+        getAccessibleContext().setAccessibleName(NbBundle.getMessage(SearchHistoryTopComponent.class, "ACSN_SearchHistoryT_Top_Component")); // NOI18N
+        getAccessibleContext().setAccessibleDescription(NbBundle.getMessage(SearchHistoryTopComponent.class, "ACSD_SearchHistoryT_Top_Component")); // NOI18N
+    }
+
+    
+    private void initComponents() {
+        setLayout(new BorderLayout());
+        
+        SearchCriteriaPanel scp = new SearchCriteriaPanel();
+        
+        SearchHistoryPanel shp = new SearchHistoryPanel();
+        shp.setTopPanel(scp);
+        add(shp);
+    }
+
+    public int getPersistenceType(){
+       return TopComponent.PERSISTENCE_NEVER;
+    }
+    
+    protected void componentClosed() {
+//       ((DiffMainPanel) getComponent(0)).componentClosed();
+       super.componentClosed();
+    }
+    
+    protected String preferredID(){
+       return "SearchHistoryTopComponent";    //NOI18N       
+    }
+}
