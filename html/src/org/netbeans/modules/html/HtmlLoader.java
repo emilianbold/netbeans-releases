932c7f7c009e7882628d7451334a599172e97aa5
jglick <jglick@netbeans.org>
2005-07-26 02:48:20 +0000

    #61386: basic support for XHTML. Also using MIMEResolver for recognizing plain old text/html.

--- html/src/org/netbeans/modules/html/HtmlLoader.java
+++ html/src/org/netbeans/modules/html/HtmlLoader.java
@@ -1,13 +1,13 @@
 /*
  *                 Sun Public License Notice
- * 
+ *
  * The contents of this file are subject to the Sun Public License
  * Version 1.0 (the "License"). You may not use this file except in
  * compliance with the License. A copy of the License is available at
  * http://www.sun.com/
- * 
+ *
  * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2001 Sun
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -21,39 +21,37 @@ import org.openide.filesystems.FileObject;
 import org.openide.util.NbBundle;
 
 /**
-* Loader for Html DataObjects.
-*
-* @author Jan Jancura
-*/
+ * Loader for Html DataObjects.
+ *
+ * @author Jan Jancura
+ */
 public class HtmlLoader extends UniFileLoader {
-
-    static final long serialVersionUID =-5809935261731217882L;
+    
+    private static final long serialVersionUID = -5809935261731217882L;
     
     public HtmlLoader() {
-        super ("org.netbeans.modules.html.HtmlDataObject"); // NOI18N
+        super("org.netbeans.modules.html.HtmlDataObject"); // NOI18N
     }
-
-    protected void initialize () {
+    
+    protected void initialize() {
         super.initialize();
-        getExtensions ().addExtension ("html"); // NOI18N
-        getExtensions ().addExtension ("htm"); // NOI18N
-        getExtensions ().addExtension ("shtml"); // NOI18N
+        getExtensions().addMimeType("text/html"); // NOI18N
     }
-
-    protected MultiDataObject createMultiObject (final FileObject primaryFile)
+    
+    protected MultiDataObject createMultiObject(final FileObject primaryFile)
     throws DataObjectExistsException, IOException {
-        return new HtmlDataObject (primaryFile, this);
+        return new HtmlDataObject(primaryFile, this);
     }
     
     /** Get the default display name of this loader.
      * @return default display name
      */
-    protected String defaultDisplayName () {
-        return NbBundle.getBundle(HtmlLoader.class).
-                       getString("PROP_HtmlLoader_Name");
+    protected String defaultDisplayName() {
+        return NbBundle.getMessage(HtmlLoader.class, "PROP_HtmlLoader_Name");
     }
     
     protected String actionsContext() {
         return "Loaders/text/html/Actions/"; // NOI18N
     }
+    
 }
