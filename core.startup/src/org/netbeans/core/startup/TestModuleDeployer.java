da710de724737096daed5ba8e2125fece2c584dc
jglick <jglick@netbeans.org>
2005-07-29 03:52:20 +0000

    Be sure to normalize incoming JAR paths.

--- core.startup/src/org/netbeans/core/startup/TestModuleDeployer.java
+++ core.startup/src/org/netbeans/core/startup/TestModuleDeployer.java
@@ -1,13 +1,13 @@
 /*
  *                 Sun Public License Notice
- * 
+ *
  * The contents of this file are subject to the Sun Public License
  * Version 1.0 (the "License"). You may not use this file except in
  * compliance with the License. A copy of the License is available at
  * http://www.sun.com/
- * 
+ *
  * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2001 Sun
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -15,14 +15,17 @@ package org.netbeans.core.startup;
 
 import java.io.File;
 import java.io.IOException;
+import org.openide.filesystems.FileUtil;
 
-/** Interface for deploying test modules.
- * Find an instance via lookup and deploy away.
+/**
+ * Interface for deploying test modules.
  * @author Jesse Glick
  * @since org.netbeans.core/1 1.1
  */
 public final class TestModuleDeployer {
-    /** Deploy a module in test mode.
+    
+    /**
+     * Deploy a module in test mode.
      * You pass the JAR file.
      * Module system figures out the rest (i.e. whether it needs
      * to be installed, reinstalled, etc.).
@@ -34,7 +37,7 @@ public final class TestModuleDeployer {
      * @throws IOException if there is some error in the process
      */
     public static void deployTestModule(File jar) throws IOException {
-        org.netbeans.core.startup.Main.getModuleSystem ().deployTestModule(jar);
+        Main.getModuleSystem().deployTestModule(FileUtil.normalizeFile(jar));
     }
     
 }
