640a7cf81317d1dbfd2927de4be6eaa0987c14ac
levequet <levequet@netbeans.org>
2005-08-09 12:55:12 +0000

    added tests for Add MBean attributes action

--- jmx/test/qa-functional/src/org/netbeans/modules/jmx/test/actions/CreateActionsProject.java
+++ jmx/test/qa-functional/src/org/netbeans/modules/jmx/test/actions/CreateActionsProject.java
@@ -81,4 +81,7 @@ public class CreateActionsProject extends JellyTestCase {
     
     public static final String PACKAGE = "com.foo.bar";
     public static final String PROJECT = "ActionsFunctionalTest";
+    public static final String MENU = "Management";
+    public static final String DYNAMIC = "DynamicSupport";
+    public static final String MBEAN = "MBean";
 }
