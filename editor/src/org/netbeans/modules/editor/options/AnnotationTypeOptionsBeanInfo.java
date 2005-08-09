16a658d8aee74b3159c66c84050adf67f6915373
rkubacki <rkubacki@netbeans.org>
2005-08-09 15:23:09 +0000

    optimize for size rather than speed by not caching property descriptors

--- editor/src/org/netbeans/modules/editor/options/AnnotationTypeOptionsBeanInfo.java
+++ editor/src/org/netbeans/modules/editor/options/AnnotationTypeOptionsBeanInfo.java
@@ -7,16 +7,14 @@
  * http://www.sun.com/
  *
  * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2001 Sun
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
 package org.netbeans.modules.editor.options;
 
 import java.beans.SimpleBeanInfo;
-import java.awt.Image;
 import java.beans.PropertyDescriptor;
-import java.beans.BeanInfo;
 import java.util.ResourceBundle;
 import org.openide.util.NbBundle;
 
@@ -27,46 +25,43 @@ import org.openide.util.NbBundle;
  */
 public class AnnotationTypeOptionsBeanInfo extends SimpleBeanInfo {
 
-    /** Propertydescriptors */
-    private static PropertyDescriptor[] descriptors;
-
     /* PropertyDescriptotrs
     * @return Returns an array of PropertyDescriptors
     * describing the editable properties supported by this bean.
     */
     public PropertyDescriptor[] getPropertyDescriptors () {
-        if (descriptors == null) {
-            try {
-                descriptors = new PropertyDescriptor[] {
-                                  new PropertyDescriptor("highlightColor", AnnotationTypeOptions.class), // NOI18N
-                                  new PropertyDescriptor("useHighlightColor", AnnotationTypeOptions.class), // NOI18N
-                                  new PropertyDescriptor("foregroundColor", AnnotationTypeOptions.class), // NOI18N
-                                  new PropertyDescriptor("inheritForegroundColor", AnnotationTypeOptions.class), // NOI18N
-                                  new PropertyDescriptor("waveUnderlineColor", AnnotationTypeOptions.class), // NOI18N
-                                  new PropertyDescriptor("useWaveUnderlineColor", AnnotationTypeOptions.class), // NOI18N
-                                  new PropertyDescriptor("wholeLine", AnnotationTypeOptions.class, "isWholeLine", null) // NOI18N
-                              };
-                ResourceBundle bundle;
-                bundle = NbBundle.getBundle(AnnotationTypeOptionsBeanInfo.class);
+        PropertyDescriptor[] descriptors;
+        
+        try {
+            descriptors = new PropertyDescriptor[] {
+                              new PropertyDescriptor("highlightColor", AnnotationTypeOptions.class), // NOI18N
+                              new PropertyDescriptor("useHighlightColor", AnnotationTypeOptions.class), // NOI18N
+                              new PropertyDescriptor("foregroundColor", AnnotationTypeOptions.class), // NOI18N
+                              new PropertyDescriptor("inheritForegroundColor", AnnotationTypeOptions.class), // NOI18N
+                              new PropertyDescriptor("waveUnderlineColor", AnnotationTypeOptions.class), // NOI18N
+                              new PropertyDescriptor("useWaveUnderlineColor", AnnotationTypeOptions.class), // NOI18N
+                              new PropertyDescriptor("wholeLine", AnnotationTypeOptions.class, "isWholeLine", null) // NOI18N
+                          };
+            ResourceBundle bundle;
+            bundle = NbBundle.getBundle(AnnotationTypeOptionsBeanInfo.class);
 
-                descriptors[0].setDisplayName(bundle.getString("PROP_AT_HIGHLIGHT")); // NOI18N
-                descriptors[0].setShortDescription(bundle.getString("HINT_AT_HIGHLIGHT")); // NOI18N
-                descriptors[1].setDisplayName(bundle.getString("PROP_AT_USE_HIGHLIGHT")); // NOI18N
-                descriptors[1].setShortDescription(bundle.getString("HINT_AT_USE_HIGHLIGHT")); // NOI18N
-                descriptors[2].setDisplayName(bundle.getString("PROP_AT_FOREGROUND")); // NOI18N
-                descriptors[2].setShortDescription(bundle.getString("HINT_AT_FOREGROUND")); // NOI18N
-                descriptors[3].setDisplayName(bundle.getString("PROP_AT_INHERIT_FOREGROUND")); // NOI18N
-                descriptors[3].setShortDescription(bundle.getString("HINT_AT_INHERIT_FOREGROUND")); // NOI18N
-                descriptors[4].setDisplayName(bundle.getString("PROP_AT_WAVEUNDERLINE")); // NOI18N
-                descriptors[4].setShortDescription(bundle.getString("HINT_AT_WAVEUNDERLINE")); // NOI18N
-                descriptors[5].setDisplayName(bundle.getString("PROP_AT_USE_WAVEUNDERLINE")); // NOI18N
-                descriptors[5].setShortDescription(bundle.getString("HINT_AT_USE_WAVEUNDERLINE")); // NOI18N
-                descriptors[6].setDisplayName(bundle.getString("PROP_AT_WHOLELINE")); // NOI18N
-                descriptors[6].setShortDescription(bundle.getString("HINT_AT_WHOLELINE")); // NOI18N
-                              
-            } catch (Exception e) {
-                descriptors = new PropertyDescriptor[0];
-            }
+            descriptors[0].setDisplayName(bundle.getString("PROP_AT_HIGHLIGHT")); // NOI18N
+            descriptors[0].setShortDescription(bundle.getString("HINT_AT_HIGHLIGHT")); // NOI18N
+            descriptors[1].setDisplayName(bundle.getString("PROP_AT_USE_HIGHLIGHT")); // NOI18N
+            descriptors[1].setShortDescription(bundle.getString("HINT_AT_USE_HIGHLIGHT")); // NOI18N
+            descriptors[2].setDisplayName(bundle.getString("PROP_AT_FOREGROUND")); // NOI18N
+            descriptors[2].setShortDescription(bundle.getString("HINT_AT_FOREGROUND")); // NOI18N
+            descriptors[3].setDisplayName(bundle.getString("PROP_AT_INHERIT_FOREGROUND")); // NOI18N
+            descriptors[3].setShortDescription(bundle.getString("HINT_AT_INHERIT_FOREGROUND")); // NOI18N
+            descriptors[4].setDisplayName(bundle.getString("PROP_AT_WAVEUNDERLINE")); // NOI18N
+            descriptors[4].setShortDescription(bundle.getString("HINT_AT_WAVEUNDERLINE")); // NOI18N
+            descriptors[5].setDisplayName(bundle.getString("PROP_AT_USE_WAVEUNDERLINE")); // NOI18N
+            descriptors[5].setShortDescription(bundle.getString("HINT_AT_USE_WAVEUNDERLINE")); // NOI18N
+            descriptors[6].setDisplayName(bundle.getString("PROP_AT_WHOLELINE")); // NOI18N
+            descriptors[6].setShortDescription(bundle.getString("HINT_AT_WHOLELINE")); // NOI18N
+
+        } catch (Exception e) {
+            descriptors = new PropertyDescriptor[0];
         }
         return descriptors;
     }
