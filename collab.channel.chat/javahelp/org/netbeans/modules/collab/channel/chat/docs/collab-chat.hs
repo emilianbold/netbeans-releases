9c2104ae463dc83f535e04814ee98ed93852e322
pnejedly <pnejedly@netbeans.org>
2005-08-09 14:03:21 +0000

    Added helpset

---
+++ collab.channel.chat/javahelp/org/netbeans/modules/collab/channel/chat/docs/collab-chat.hs
@@ -0,0 +1,42 @@
+<?xml version='1.0' encoding='ISO-8859-1'?>
+<!--
+                Sun Public License Notice
+
+The contents of this file are subject to the Sun Public License
+Version 1.0 (the "License"). You may not use this file except in
+compliance with the License. A copy of the License is available at
+http://www.sun.com/
+
+The Original Code is NetBeans. The Initial Developer of the Original
+Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
+Microsystems, Inc. All Rights Reserved.
+-->
+
+<!DOCTYPE helpset PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN"
+                         "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
+
+<helpset version="2.0">
+	<title>Collaboration Chat Help</title>
+	<maps>
+		<homeID>collab_chat_overview</homeID>
+		<mapref location="collab-chat-map.jhm"/>
+	</maps>
+	<view>
+		<name>TOC</name>
+		<label>Table of Contents</label>
+		<type>javax.help.TOCView</type>
+		<data>collab-chat-toc.xml</data>
+	</view>
+	<view>
+		<name>Index</name>
+		<label>Index</label>
+		<type>javax.help.IndexView</type>
+		<data>collab-chat-idx.xml</data>
+	</view>
+        <view>
+            <name>Search</name>
+            <label>Search</label>
+            <type>javax.help.SearchView</type>
+            <data engine="com.sun.java.help.search.DefaultSearchEngine">JavaHelpSearch</data>
+        </view>
+</helpset>
