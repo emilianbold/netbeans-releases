ed140fcbf18b9e1a0c6a1ffbef49c5b9bae34426
alex_neubert <devnull@localhost>
2005-08-10 07:15:20 +0000

    Added constants for functional tests

---
+++ jmx/test/qa-functional/src/org/netbeans/modules/jmx/test/helpers/JellyConstants.java
@@ -0,0 +1,29 @@
+/*
+ * JellyConstants.java
+ *
+ * Created on August 10, 2005, 9:09 AM
+ *
+ * To change this template, choose Tools | Options and locate the template under
+ * the Source Creation and Management node. Right-click the template and choose
+ * Open. You can then make changes to the template in the Source Editor.
+ */
+
+package org.netbeans.modules.jmx.test.helpers;
+
+/**
+ *
+ * @author alex
+ */
+public class JellyConstants {
+    
+   //package
+   public static final String PROJECT_NAME = "MBeanFunctionalTest";
+   public static final String PACKAGE_NAME = "com.foo.bar";
+   public static final String CATEGORY = "JMX MBean";
+   
+   //MBean type
+   public static final String STDMBEAN = "StandardMBean";
+   public static final String EXTSTDMBEAN = "ExtendedStandardMBean";
+   public static final String DYNMBEAN = "DynamicMBean";
+   
+}
