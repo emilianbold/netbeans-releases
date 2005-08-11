d3e859da42d07c3efc66dcc66c3668bc4c6914fd
jskrivanek <jskrivanek@netbeans.org>
2005-08-11 08:04:44 +0000

    #61300 - on Mac invoke Options by system action instead of main menu. The item Tools|Options was moved to NetBeans|Preferences.

--- jellytools/src/org/netbeans/jellytools/actions/OptionsViewAction.java
+++ jellytools/src/org/netbeans/jellytools/actions/OptionsViewAction.java
@@ -7,16 +7,17 @@
  * http://www.sun.com/
  * 
  * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2002 Sun
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 package org.netbeans.jellytools.actions;
 
-import java.awt.event.KeyEvent;
 import org.netbeans.jellytools.Bundle;
 
 /** Used to call "Tools|Options" main menu item or
- * "org.netbeans.core.actions.OptionsAction".
+ * "org.netbeans.core.actions.OptionsAction". If called on MAC it uses IDE API to
+ * open Options. Apple system menu is unreachable for Jemmy 
+ * (http://www.netbeans.org/issues/show_bug.cgi?id=61300).
  * @see Action 
  */
 public class OptionsViewAction extends Action {
@@ -30,5 +31,18 @@ public class OptionsViewAction extends Action {
     /** Creates new instance. */    
     public OptionsViewAction() {
         super(menu, null, "org.netbeans.core.actions.OptionsAction");
+    }
+    
+    /** performs action through main menu. If called on MAC it uses IDE API to
+     * open Options. Apple system menu is unreachable for Jemmy 
+     * (http://www.netbeans.org/issues/show_bug.cgi?id=61300).
+     * @throws UnsupportedOperationException when action does not support menu mode 
+     */
+    public void performMenu() {
+        if(System.getProperty("os.name").toLowerCase().indexOf("mac") > -1) { // NOI18N
+            performAPI();
+        } else {
+            super.performMenu();
+        }
     }
 }
