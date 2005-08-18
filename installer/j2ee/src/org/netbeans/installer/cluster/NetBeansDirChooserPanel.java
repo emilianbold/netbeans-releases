92477daa7d2484148660f04ae83dc0a02a8a4b05
mslama <mslama@netbeans.org>
2005-08-18 15:26:55 +0000

    Use shared library.

--- installer/j2ee/src/org/netbeans/installer/cluster/NetBeansDirChooserPanel.java
+++ installer/j2ee/src/org/netbeans/installer/cluster/NetBeansDirChooserPanel.java
@@ -7,7 +7,7 @@
  * http://www.sun.com/
  *
  * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -25,6 +25,8 @@ import com.installshield.wizard.service.file.FileService;
 
 import java.io.File;
 import java.io.IOException;
+
+import org.netbeans.installer.Util;
 
 public class NetBeansDirChooserPanel extends WizardPanel {
     private String nbdir = "";
