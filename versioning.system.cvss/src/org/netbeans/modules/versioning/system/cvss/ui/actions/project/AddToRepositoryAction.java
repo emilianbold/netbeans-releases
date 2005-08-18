78591d8e5585f9b7307d821036630ea013735b46
pkuzel <pkuzel@netbeans.org>
2005-08-18 13:07:24 +0000

    Import into Repository action enabled for any node representing non-versioned folder.

--- versioning.system.cvss/src/org/netbeans/modules/versioning/system/cvss/ui/actions/project/AddToRepositoryAction.java
+++ versioning.system.cvss/src/org/netbeans/modules/versioning/system/cvss/ui/actions/project/AddToRepositoryAction.java
@@ -16,10 +16,13 @@ package org.netbeans.modules.versioning.system.cvss.ui.actions.project;
 import org.openide.util.actions.NodeAction;
 import org.openide.util.HelpCtx;
 import org.openide.util.NbBundle;
+import org.openide.util.Lookup;
 import org.openide.nodes.Node;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
 import org.openide.*;
+import org.openide.loaders.DataObject;
+import org.openide.loaders.DataShadow;
 import org.openide.xml.XMLUtil;
 import org.netbeans.api.project.Project;
 import org.netbeans.api.project.Sources;
@@ -80,144 +83,153 @@ public final class AddToRepositoryAction extends NodeAction implements ChangeLis
 
     protected void performAction(Node[] nodes) {
         if (nodes.length == 1) {
-            Project project = (Project) nodes[0].getLookup().lookup(Project.class);
-            if (project != null) {
-                Sources sources = (Sources) project.getLookup().lookup(Sources.class);
-                if (sources != null) {
-                    SourceGroup[] groups = sources.getSourceGroups(Sources.TYPE_GENERIC);
-                    if (groups.length == 1) {
-                        FileObject root = groups[0].getRootFolder();
-                        File importDirectory = FileUtil.toFile(root);
-                        if (importDirectory != null) {
+            File importDirectory = lookupImportDirectory(nodes[0]);
+            if (importDirectory != null) {
 
-                            // try to detect some resonable defaults for cvs root and repositoryStep
+                // try to detect some resonable defaults for cvs root and repositoryStep
 
-                            File parent = importDirectory.getParentFile();
-                            File parent_cvsRoot = new File(parent, "CVS/Root");  // NOI18N
-                            File parent_cvsRepo = new File(parent, "CVS/Repository");  // NOI18N
-                            String cvsRoot = null;
-                            String cvsRepository = null;
-                            if (parent_cvsRepo.isFile() && parent_cvsRoot.isFile()) {
-                                BufferedReader r = null;
-                                try {
-                                    r = new BufferedReader((new FileReader(parent_cvsRoot)));
-                                    cvsRoot = r.readLine();
-                                } catch (IOException e) {
-                                    ErrorManager err = ErrorManager.getDefault();
-                                    err.annotate(e, "Cannot read CVS/Root");
-                                    err.notify(e);
-                                } finally {
-                                    if (r != null) {
-                                        try {
-                                            r.close();
-                                        } catch (IOException alreadyClosed) {
-                                        }
-                                    }
-                                }
-
-                                try {
-                                    r = new BufferedReader((new FileReader(parent_cvsRepo)));
-                                    cvsRepository = r.readLine();
-                                } catch (IOException e) {
-                                    ErrorManager err = ErrorManager.getDefault();
-                                    err.annotate(e, "Cannot read CVS/Repository");
-                                    err.notify(e);
-                                } finally {
-                                    if (r != null) {
-                                        try {
-                                            r.close();
-                                        } catch (IOException alreadyClosed) {
-                                        }
-                                    }
-                                }
+                File parent = importDirectory.getParentFile();
+                File parent_cvsRoot = new File(parent, "CVS/Root");  // NOI18N
+                File parent_cvsRepo = new File(parent, "CVS/Repository");  // NOI18N
+                String cvsRoot = null;
+                String cvsRepository = null;
+                if (parent_cvsRepo.isFile() && parent_cvsRoot.isFile()) {
+                    BufferedReader r = null;
+                    try {
+                        r = new BufferedReader((new FileReader(parent_cvsRoot)));
+                        cvsRoot = r.readLine();
+                    } catch (IOException e) {
+                        ErrorManager err = ErrorManager.getDefault();
+                        err.annotate(e, "Cannot read CVS/Root");
+                        err.notify(e);
+                    } finally {
+                        if (r != null) {
+                            try {
+                                r.close();
+                            } catch (IOException alreadyClosed) {
                             }
-
-                            String prefRoot;
-                            if (cvsRoot != null) {
-                                prefRoot = cvsRoot;
-                            } else {
-                                prefRoot = NbBundle.getMessage(AddToRepositoryAction.class, "BK0008");
-                            }
-
-                            String prefModule;
-                            if (cvsRepository != null) {
-                                prefModule = cvsRepository + "/" + root.getName();
-                            } else {
-                                prefModule = root.getName();
-                            }
-
-                            wizardIterator = panelIterator(prefRoot, prefModule, importDirectory.getAbsolutePath());
-                            wizard = new WizardDescriptor(wizardIterator);
-                            wizard.putProperty("WizardPanel_contentData",  // NOI18N
-                                    new String[] {
-                                        NbBundle.getMessage(AddToRepositoryAction.class, "BK0015"),
-                                        NbBundle.getMessage(AddToRepositoryAction.class, "BK0014")
-                                    }
-                            );
-                            wizard.putProperty("WizardPanel_contentDisplayed", Boolean.TRUE);  // NOI18N
-                            wizard.putProperty("WizardPanel_autoWizardStyle", Boolean.TRUE);  // NOi18N
-                            wizard.putProperty("WizardPanel_contentNumbered", Boolean.TRUE);  // NOi18N
-                            wizard.setTitleFormat(new MessageFormat("{0}"));
-                            String title = NbBundle.getMessage(AddToRepositoryAction.class, "BK0007");
-                            wizard.setTitle(title);
-
-                            Object result = DialogDisplayer.getDefault().notify(wizard);
-                            if (result == DialogDescriptor.OK_OPTION) {
-
-                                boolean checkout = importStep.getCheckout();
-                                String logMessage = importStep.getMessage();
-                                String module = importStep.getModule();
-                                String vendorTag = "default_vendor";
-                                String releaseTag = "default_release";
-                                String selectedRoot = repositoryStep.getCvsRoot();
-                                String folder = importStep.getFolder();
-                                File dir = new File(folder);
-
-                                HistorySettings.addRecent(HistorySettings.PROP_CVS_ROOTS, selectedRoot);
-
-                                try {
-                                    prepareIgnore(dir);
-                                } catch (IOException e) {
-                                    ErrorManager err = ErrorManager.getDefault();
-                                    err.annotate(e, "Can not generate .cvsignore for unshareable files!");
-                                    err.notify(e);
-                                }
-
-                                GlobalOptions gtx = new GlobalOptions();
-                                gtx.setCVSRoot(selectedRoot);
-                                ImportCommand importCommand = new ImportCommand();
-                                importCommand.setModule(module);
-                                importCommand.setLogMessage(logMessage);
-                                importCommand.setVendorTag(vendorTag);
-                                importCommand.setReleaseTag(releaseTag);
-                                importCommand.setImportDirectory(importDirectory.getPath());
-
-                                ImportExecutor executor = new ImportExecutor(importCommand, gtx, checkout, folder);
-                                executor.execute();
-                            }
-
                         }
-                    } else {
-                        StringBuffer paths = new StringBuffer();
-                        paths.append("<dl>"); // NOi18N
-                        for (int i = 0; i < groups.length; i++) {
-                            SourceGroup group = groups[i];
-                            FileObject fo = group.getRootFolder();
-                            String name = group.getDisplayName();
-                            paths.append("<dt>").append(escape(name)).append("</dt>");  // NOi18N
-                            paths.append("<dd>").append(escape(fo.getPath())).append("</dd>");  // NOI18N
-                        }
-                        paths.append("</dl>"); // NOi18N
-                        String msg = NbBundle.getMessage(AddToRepositoryAction.class, "BK0004", paths.toString());
-                        NotifyDescriptor desc = new NotifyDescriptor.Message(msg);
-                        DialogDisplayer.getDefault().notify(desc);
-
-                        // implementation sketch ...
-                        // MetadataAttic.scheduleFolder();
                     }
+
+                    try {
+                        r = new BufferedReader((new FileReader(parent_cvsRepo)));
+                        cvsRepository = r.readLine();
+                    } catch (IOException e) {
+                        ErrorManager err = ErrorManager.getDefault();
+                        err.annotate(e, "Cannot read CVS/Repository");
+                        err.notify(e);
+                    } finally {
+                        if (r != null) {
+                            try {
+                                r.close();
+                            } catch (IOException alreadyClosed) {
+                            }
+                        }
+                    }
+                }
+
+                String prefRoot;
+                if (cvsRoot != null) {
+                    prefRoot = cvsRoot;
+                } else {
+                    prefRoot = NbBundle.getMessage(AddToRepositoryAction.class, "BK0008");
+                }
+
+                String prefModule;
+                if (cvsRepository != null) {
+                    prefModule = cvsRepository + "/" + importDirectory.getName();
+                } else {
+                    prefModule = importDirectory.getName();
+                }
+
+                wizardIterator = panelIterator(prefRoot, prefModule, importDirectory.getAbsolutePath());
+                wizard = new WizardDescriptor(wizardIterator);
+                wizard.putProperty("WizardPanel_contentData",  // NOI18N
+                        new String[] {
+                            NbBundle.getMessage(AddToRepositoryAction.class, "BK0015"),
+                            NbBundle.getMessage(AddToRepositoryAction.class, "BK0014")
+                        }
+                );
+                wizard.putProperty("WizardPanel_contentDisplayed", Boolean.TRUE);  // NOI18N
+                wizard.putProperty("WizardPanel_autoWizardStyle", Boolean.TRUE);  // NOi18N
+                wizard.putProperty("WizardPanel_contentNumbered", Boolean.TRUE);  // NOi18N
+                wizard.setTitleFormat(new MessageFormat("{0}"));
+                String title = NbBundle.getMessage(AddToRepositoryAction.class, "BK0007");
+                wizard.setTitle(title);
+
+                Object result = DialogDisplayer.getDefault().notify(wizard);
+                if (result == DialogDescriptor.OK_OPTION) {
+
+                    boolean checkout = importStep.getCheckout();
+                    String logMessage = importStep.getMessage();
+                    String module = importStep.getModule();
+                    String vendorTag = "default_vendor";
+                    String releaseTag = "default_release";
+                    String selectedRoot = repositoryStep.getCvsRoot();
+                    String folder = importStep.getFolder();
+                    File dir = new File(folder);
+
+                    HistorySettings.addRecent(HistorySettings.PROP_CVS_ROOTS, selectedRoot);
+
+                    try {
+                        prepareIgnore(dir);
+                    } catch (IOException e) {
+                        ErrorManager err = ErrorManager.getDefault();
+                        err.annotate(e, "Can not generate .cvsignore for unshareable files!");
+                        err.notify(e);
+                    }
+
+                    GlobalOptions gtx = new GlobalOptions();
+                    gtx.setCVSRoot(selectedRoot);
+                    ImportCommand importCommand = new ImportCommand();
+                    importCommand.setModule(module);
+                    importCommand.setLogMessage(logMessage);
+                    importCommand.setVendorTag(vendorTag);
+                    importCommand.setReleaseTag(releaseTag);
+                    importCommand.setImportDirectory(importDirectory.getPath());
+
+                    ImportExecutor executor = new ImportExecutor(importCommand, gtx, checkout, folder);
+                    executor.execute();
                 }
             }
         }
+    }
+
+    private File lookupImportDirectory(Node node) {
+        File importDirectory = null;
+        Project project = (Project) node.getLookup().lookup(Project.class);
+        if (project != null) {
+            Sources sources = (Sources) project.getLookup().lookup(Sources.class);
+            if (sources != null) {
+                SourceGroup[] groups = sources.getSourceGroups(Sources.TYPE_GENERIC);
+                if (groups.length == 1) {
+                    FileObject root = groups[0].getRootFolder();
+                    importDirectory = FileUtil.toFile(root);
+                } else {
+                    importDirectory = FileUtil.toFile(project.getProjectDirectory());
+                }
+            }
+        } else {
+            FileObject fo = null;
+            Collection fileObjects = node.getLookup().lookup(new Lookup.Template(FileObject.class)).allInstances();
+            if (fileObjects.size() > 0) {
+                fo = (FileObject) fileObjects.iterator().next();
+            } else {
+                DataObject dataObject = (DataObject) node.getCookie(DataObject.class);
+                if (dataObject instanceof DataShadow) {
+                    dataObject = ((DataShadow) dataObject).getOriginal();
+                }
+                if (dataObject != null) {
+                    fo = dataObject.getPrimaryFile();
+                }
+            }
+
+            File f = FileUtil.toFile(fo);
+            if (f.isDirectory()) {
+                importDirectory = f;
+            }
+        }
+        return importDirectory;
     }
 
     private WizardDescriptor.Iterator panelIterator(String root, String module, String folder) {
@@ -404,26 +416,15 @@ public final class AddToRepositoryAction extends NodeAction implements ChangeLis
         if (!valid) return "Folder must exist";
 
         valid &= importPanel.commentTextArea.getText().trim().length() > 0;
-        if (!valid) return "Message required";
+        if (!valid) return "Import message required";
 
         String module = importPanel.moduleTextField.getText().trim();
         valid &= module.length() > 0;
-        if (!valid) return "Specify module";
+        if (!valid) return "Specify repository module";
         valid &= module.indexOf(" ") == -1;  // NOI18N
-        if (!valid) return "Invalid module name";
+        if (!valid) return "Invalid repository module name";
 
         return null;
-    }
-
-    private static String escape(String path) {
-        try {
-            return XMLUtil.toElementContent(path);
-        } catch (CharConversionException e) {
-            ErrorManager err = ErrorManager.getDefault();
-            err.annotate(e, "Can not HTML escape '" + path + "'");  // NOI18N
-            err.notify(e);
-        }
-        return NbBundle.getMessage(AddToRepositoryAction.class, "BK0005");
     }
 
     private void prepareIgnore(File dir) throws IOException {
@@ -472,15 +473,11 @@ public final class AddToRepositoryAction extends NodeAction implements ChangeLis
     }
 
     protected boolean enable(Node[] nodes) {
-        if (nodes.length > 0) {
-            for (int i = 0; i < nodes.length; i++) {
-                Node node = nodes[i];
-                if (node.getLookup().lookup(Project.class) == null) return false;
-                if (Utils.isVersionedProject(node)) {
-                    return false;
-                }
+        if (nodes.length == 1) {
+            File dir = lookupImportDirectory(nodes[0]);
+            if (dir.isDirectory()) {
+                return new File(dir, "CVS").exists() == false;  // NOI18N
             }
-            return true;
         }
         return false;
     }
