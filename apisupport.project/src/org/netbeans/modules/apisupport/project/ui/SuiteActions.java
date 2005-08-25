d919b578f8eae5e0c1ff2ef38fe8d76118567e98
jglick <jglick@netbeans.org>
2005-08-25 02:39:18 +0000

    Offer a menu item to build ZIP distros.

--- apisupport.project/src/org/netbeans/modules/apisupport/project/ui/SuiteActions.java
+++ apisupport.project/src/org/netbeans/modules/apisupport/project/ui/SuiteActions.java
@@ -57,6 +57,8 @@ public final class SuiteActions implements ActionProvider {
         actions.add(ProjectSensitiveActions.projectCommandAction(ActionProvider.COMMAND_RUN, "Run", null));
         actions.add(ProjectSensitiveActions.projectCommandAction(ActionProvider.COMMAND_DEBUG, "Debug", null));
         actions.add(null);
+        actions.add(ProjectSensitiveActions.projectCommandAction("build-zip", "Build ZIP Distribution", null));
+        actions.add(null);
         actions.add(ProjectSensitiveActions.projectCommandAction("build-jnlp", "Build JNLP Application", null));
         actions.add(ProjectSensitiveActions.projectCommandAction("run-jnlp", "Run JNLP Application", null));
         actions.add(ProjectSensitiveActions.projectCommandAction("debug-jnlp", "Debug JNLP Application", null));
@@ -114,6 +116,7 @@ public final class SuiteActions implements ActionProvider {
             ActionProvider.COMMAND_REBUILD,
             ActionProvider.COMMAND_RUN,
             ActionProvider.COMMAND_DEBUG,
+            "build-zip", // NOI18N
             "build-jnlp", // NOI18N
             "run-jnlp", // NOI18N
             "debug-jnlp", // NOI18N
@@ -140,6 +143,8 @@ public final class SuiteActions implements ActionProvider {
             targetNames = new String[] {"run"}; // NOI18N
         } else if (command.equals(ActionProvider.COMMAND_DEBUG)) {
             targetNames = new String[] {"debug"}; // NOI18N
+        } else if (command.equals("build-zip")) { // NOI18N
+            targetNames = new String[] {"build-zip"}; // NOI18N
         } else if (command.equals("build-jnlp")) { // NOI18N
             targetNames = new String[] {"build-jnlp"}; // NOI18N
         } else if (command.equals("run-jnlp")) { // NOI18N
