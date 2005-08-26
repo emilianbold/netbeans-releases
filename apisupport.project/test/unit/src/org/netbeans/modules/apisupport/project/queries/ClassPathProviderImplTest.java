76855219984826ec9c68f792abd6a53202014ce9
jglick <jglick@netbeans.org>
2005-08-26 02:53:42 +0000

    Updated to match current deps.

--- apisupport.project/test/unit/src/org/netbeans/modules/apisupport/project/queries/ClassPathProviderImplTest.java
+++ apisupport.project/test/unit/src/org/netbeans/modules/apisupport/project/queries/ClassPathProviderImplTest.java
@@ -113,7 +113,10 @@ public class ClassPathProviderImplTest extends TestBase {
         expectedRoots.add(urlForJar("nbbuild/netbeans/platform6/modules/org-openide-loaders.jar"));
         expectedRoots.add(urlForJar("nbbuild/netbeans/platform6/modules/org-openide-explorer.jar"));
         expectedRoots.add(urlForJar("nbbuild/netbeans/ide6/modules/org-netbeans-spi-navigator.jar"));
-        assertEquals("right COMPILE classpath", expectedRoots, urlsOfCp(cp));
+        expectedRoots.add(urlForJar("nbbuild/netbeans/ide6/modules/org-netbeans-libs-formlayout.jar"));
+        expectedRoots.add(urlForJar("libs/external/forms-1.0.5.jar"));
+        expectedRoots.add(urlForJar("nbbuild/netbeans/ide6/modules/org-netbeans-modules-options-api.jar"));
+        assertEquals("right COMPILE classpath for ant/src", expectedRoots.toString(), urlsOfCp(cp).toString());
         cp = ClassPath.getClassPath(src, ClassPath.EXECUTE);
         assertNotNull("have an EXECUTE classpath", cp);
         // #48099: need to include build/classes here too
@@ -200,7 +203,10 @@ public class ClassPathProviderImplTest extends TestBase {
         expectedRoots.add(urlForJar("nbbuild/netbeans/platform6/modules/org-openide-loaders.jar"));
         expectedRoots.add(urlForJar("nbbuild/netbeans/platform6/modules/org-openide-explorer.jar"));
         expectedRoots.add(urlForJar("nbbuild/netbeans/ide6/modules/org-netbeans-spi-navigator.jar"));
-        assertEquals("right COMPILE classpath", expectedRoots, urlsOfCp(cp));
+        expectedRoots.add(urlForJar("nbbuild/netbeans/ide6/modules/org-netbeans-libs-formlayout.jar"));
+        expectedRoots.add(urlForJar("libs/external/forms-1.0.5.jar"));
+        expectedRoots.add(urlForJar("nbbuild/netbeans/ide6/modules/org-netbeans-modules-options-api.jar"));
+        assertEquals("right COMPILE classpath for ant/src-bridge", expectedRoots.toString(), urlsOfCp(cp).toString());
         cp = ClassPath.getClassPath(srcbridge, ClassPath.EXECUTE);
         assertNotNull("have an EXECUTE classpath", cp);
         expectedRoots.add(urlForDir("ant/build/bridge-classes"));
