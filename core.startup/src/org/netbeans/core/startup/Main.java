37d87975174bf712e22d80aed649aeef167806c5
mslama <mslama@netbeans.org>
2005-08-29 12:38:00 +0000

    Rollback fix of #62831.

--- core.startup/src/org/netbeans/core/startup/Main.java
+++ core.startup/src/org/netbeans/core/startup/Main.java
@@ -323,8 +323,7 @@ public final class Main extends Object {
     // -----------------------------------------------------------------------------------------------------
     // License check
     try {
-        if ((System.getProperty ("netbeans.full.hack") == null) && (System.getProperty ("netbeans.close") == null) &&
-            (System.getProperty ("netbeans.skip_license_check") == null)) {
+        if ((System.getProperty ("netbeans.full.hack") == null) && (System.getProperty ("netbeans.close") == null)) {
             if (!handleLicenseCheck()) {
                 org.netbeans.TopSecurityManager.exit(0);
             }
