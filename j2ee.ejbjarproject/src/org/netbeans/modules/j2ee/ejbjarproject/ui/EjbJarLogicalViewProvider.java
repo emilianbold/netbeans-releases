74f9d40914c88646cc05cd3752042cc2a4bd0f40
abadea <abadea@netbeans.org>
2005-09-07 09:25:13 +0000

    #58627: Missing support for CVS project actions

--- j2ee.ejbjarproject/src/org/netbeans/modules/j2ee/ejbjarproject/ui/EjbJarLogicalViewProvider.java
+++ j2ee.ejbjarproject/src/org/netbeans/modules/j2ee/ejbjarproject/ui/EjbJarLogicalViewProvider.java
@@ -19,11 +19,19 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.io.CharConversionException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import javax.swing.AbstractAction;
 import javax.swing.Action;
+import javax.swing.JSeparator;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import org.netbeans.api.java.platform.JavaPlatformManager;
@@ -50,6 +58,8 @@ import org.netbeans.spi.project.ui.LogicalViewProvider;
 import org.netbeans.spi.project.ui.support.ProjectSensitiveActions;
 import org.netbeans.spi.java.project.support.ui.BrokenReferencesSupport;
 import org.netbeans.api.java.project.JavaProjectConstants;
+import org.netbeans.api.project.SourceGroup;
+import org.netbeans.api.project.Sources;
 import org.netbeans.modules.j2ee.ejbjarproject.ui.customizer.EjbJarProjectProperties;
 import org.netbeans.modules.j2ee.ejbjarproject.UpdateHelper;
 import org.openide.filesystems.FileObject;
@@ -61,6 +71,16 @@ import org.netbeans.modules.j2ee.api.ejbjar.EjbProjectConstants;
 import org.netbeans.modules.j2ee.deployment.devmodules.spi.InstanceListener;
 import org.netbeans.modules.j2ee.ejbjarproject.EjbJarProject;
 import org.netbeans.spi.project.ui.support.DefaultProjectOperations;
+import org.openide.ErrorManager;
+import org.openide.filesystems.FileStateInvalidException;
+import org.openide.filesystems.FileStatusEvent;
+import org.openide.filesystems.FileStatusListener;
+import org.openide.filesystems.FileSystem;
+import org.openide.filesystems.FileUtil;
+import org.openide.filesystems.Repository;
+import org.openide.loaders.DataObject;
+import org.openide.loaders.DataObjectNotFoundException;
+import org.openide.loaders.FolderLookup;
 
 /**
  * Support for creating logical views.
@@ -188,12 +208,23 @@ public class EjbJarLogicalViewProvider implements LogicalViewProvider {
 
     /** Filter node containin additional features for the J2SE physical
      */
-    private final class WebLogicalViewRootNode extends AbstractNode {
+    private final class WebLogicalViewRootNode extends AbstractNode implements Runnable, FileStatusListener, ChangeListener, PropertyChangeListener {
 
         private Action brokenLinksAction;
         private BrokenServerAction brokenServerAction;
         private boolean broken;
         private static final String BROKEN_PROJECT_BADGE = "org/netbeans/modules/j2ee/ejbjarproject/ui/resources/brokenProjectBadge.gif"; // NOI18N
+        
+        // icon badging >>>
+        private Set files;
+        private Map fileSystemListeners;
+        private RequestProcessor.Task task;
+        private final Object privateLock = new Object();
+        private boolean iconChange;
+        private boolean nameChange;
+        private ChangeListener sourcesListener;
+        private Map groupsListeners;
+        // icon badging <<<
         
         public WebLogicalViewRootNode() {
             super( new LogicalViewChildren( project, updateHelper, evaluator, resolver ), createLookup( project ) ); 
@@ -207,23 +238,209 @@ public class EjbJarLogicalViewProvider implements LogicalViewProvider {
             J2eeModuleProvider moduleProvider = (J2eeModuleProvider)project.getLookup().lookup(J2eeModuleProvider.class);
             moduleProvider.addInstanceListener((InstanceListener)WeakListeners.create(
                         InstanceListener.class, brokenServerAction, moduleProvider));
+            setProjectFiles(project);
         }
         
-        public Image getIcon(int type) {
+        protected final void setProjectFiles(Project project) {
+            Sources sources = ProjectUtils.getSources(project);  // returns singleton
+            if (sourcesListener == null) {                
+                sourcesListener = WeakListeners.change(this, sources);
+                sources.addChangeListener(sourcesListener);                                
+            }
+            setGroups(Arrays.asList(sources.getSourceGroups(Sources.TYPE_GENERIC)));
+        }
+
+
+        private final void setGroups(Collection groups) {
+            if (groupsListeners != null) {
+                Iterator it = groupsListeners.keySet().iterator();
+                while (it.hasNext()) {
+                    SourceGroup group = (SourceGroup) it.next();
+                    PropertyChangeListener pcl = (PropertyChangeListener) groupsListeners.get(group);
+                    group.removePropertyChangeListener(pcl);
+                }
+            }
+            groupsListeners = new HashMap();
+            Set roots = new HashSet();
+            Iterator it = groups.iterator();
+            while (it.hasNext()) {
+                SourceGroup group = (SourceGroup) it.next();
+                PropertyChangeListener pcl = WeakListeners.propertyChange(this, group);
+                groupsListeners.put(group, pcl);
+                group.addPropertyChangeListener(pcl);
+                FileObject fo = group.getRootFolder();
+                roots.add(fo);
+            }
+            setFiles(roots);
+        }
+
+        protected final void setFiles(Set files) {
+            if (fileSystemListeners != null) {
+                Iterator it = fileSystemListeners.keySet().iterator();
+                while (it.hasNext()) {
+                    FileSystem fs = (FileSystem) it.next();
+                    FileStatusListener fsl = (FileStatusListener) fileSystemListeners.get(fs);
+                    fs.removeFileStatusListener(fsl);
+                }
+            }
+                        
+            fileSystemListeners = new HashMap();
+            this.files = files;
+            if (files == null) return;
+
+            Iterator it = files.iterator();
+            Set hookedFileSystems = new HashSet();
+            while (it.hasNext()) {
+                FileObject fo = (FileObject) it.next();
+                try {
+                    FileSystem fs = fo.getFileSystem();
+                    if (hookedFileSystems.contains(fs)) {
+                        continue;
+                    }
+                    hookedFileSystems.add(fs);
+                    FileStatusListener fsl = FileUtil.weakFileStatusListener(this, fs);
+                    fs.addFileStatusListener(fsl);
+                    fileSystemListeners.put(fs, fsl);
+                } catch (FileStateInvalidException e) {
+                    ErrorManager err = ErrorManager.getDefault();
+                    err.annotate(e, "Can not get " + fo + " filesystem, ignoring...");  // NO18N
+                    err.notify(ErrorManager.INFORMATIONAL, e);
+                }
+            }
+        }
+        
+        public String getDisplayName () {
+            String s = super.getDisplayName ();
+
+            if (files != null && files.iterator().hasNext()) {
+                try {
+                    FileObject fo = (FileObject) files.iterator().next();
+                    s = fo.getFileSystem ().getStatus ().annotateName (s, files);
+                } catch (FileStateInvalidException e) {
+                    ErrorManager.getDefault().notify(ErrorManager.INFORMATIONAL, e);
+                }
+            }
+
+            return s;
+        }
+
+         public String getHtmlDisplayName() {
+             if (files != null && files.iterator().hasNext()) {
+                 try {
+                     FileObject fo = (FileObject) files.iterator().next();
+                     FileSystem.Status stat = fo.getFileSystem().getStatus();
+                     if (stat instanceof FileSystem.HtmlStatus) {
+                         FileSystem.HtmlStatus hstat = (FileSystem.HtmlStatus) stat;
+
+                         String result = hstat.annotateNameHtml (
+                             getMyHtmlDisplayName(), files);
+
+                         //Make sure the super string was really modified
+                         if (result != null && !result.equals(getMyHtmlDisplayName())) {
+                             return result;
+                         }
+                     }
+                 } catch (FileStateInvalidException e) {
+                     ErrorManager.getDefault().notify(ErrorManager.INFORMATIONAL, e);
+                 }
+             }
+             return getMyHtmlDisplayName();
+         }
+
+        public java.awt.Image getIcon (int type) {
+            java.awt.Image img = getMyIcon (type);
+
+            if (files != null && files.iterator().hasNext()) {
+                try {
+                    FileObject fo = (FileObject) files.iterator().next();
+                    img = fo.getFileSystem ().getStatus ().annotateIcon (img, type, files);
+                } catch (FileStateInvalidException e) {
+                    ErrorManager.getDefault().notify(ErrorManager.INFORMATIONAL, e);
+                }
+            }
+
+            return img;
+        }
+
+        public java.awt.Image getOpenedIcon (int type) {
+            java.awt.Image img = getMyOpenedIcon(type);
+
+            if (files != null && files.iterator().hasNext()) {
+                try {
+                    FileObject fo = (FileObject) files.iterator().next();
+                    img = fo.getFileSystem ().getStatus ().annotateIcon (img, type, files);
+                } catch (FileStateInvalidException e) {
+                    ErrorManager.getDefault().notify(ErrorManager.INFORMATIONAL, e);
+                }
+            }
+
+            return img;
+        }
+
+        public void run() {
+            boolean fireIcon;
+            boolean fireName;
+            synchronized (privateLock) {
+                fireIcon = iconChange;
+                fireName = nameChange;
+                iconChange = false;
+                nameChange = false;
+            }
+            if (fireIcon) {
+                fireIconChange();
+                fireOpenedIconChange();
+            }
+            if (fireName) {
+                fireDisplayNameChange(null, null);
+            }
+        }
+
+        public void annotationChanged(FileStatusEvent event) {
+            if (task == null) {
+                task = RequestProcessor.getDefault().create(this);
+            }
+
+            synchronized (privateLock) {
+                if ((iconChange == false && event.isIconChange())  || (nameChange == false && event.isNameChange())) {
+                    Iterator it = files.iterator();
+                    while (it.hasNext()) {
+                        FileObject fo = (FileObject) it.next();
+                        if (event.hasChanged(fo)) {
+                            iconChange |= event.isIconChange();
+                            nameChange |= event.isNameChange();
+                        }
+                    }
+                }
+            }
+
+            task.schedule(50);  // batch by 50 ms
+         }
+ 
+        // sources change
+        public void stateChanged(ChangeEvent e) {
+            setProjectFiles(project);
+        }
+
+        // group change
+        public void propertyChange(PropertyChangeEvent evt) {
+            setProjectFiles(project);
+        }
+        
+        public Image getMyIcon(int type) {
             Image original = super.getIcon( type );                
             return broken || brokenServerAction.isEnabled() 
                     ? Utilities.mergeImages(original, Utilities.loadImage(BROKEN_PROJECT_BADGE), 8, 0) 
                     : original;
         }
 
-        public Image getOpenedIcon(int type) {
+        public Image getMyOpenedIcon(int type) {
             Image original = super.getOpenedIcon(type);                
             return broken || brokenServerAction.isEnabled() 
                     ? Utilities.mergeImages(original, Utilities.loadImage(BROKEN_PROJECT_BADGE), 8, 0) 
                     : original;
         }            
 
-        public String getHtmlDisplayName() {
+        public String getMyHtmlDisplayName() {
             String dispName = super.getDisplayName();
             try {
                 dispName = XMLUtil.toElementContent(dispName);
@@ -280,6 +497,36 @@ public class EjbJarLogicalViewProvider implements LogicalViewProvider {
             actions.add(CommonProjectActions.deleteProjectAction());
             actions.add(null);
             actions.add(SystemAction.get( org.openide.actions.FindAction.class ));
+            
+            // honor 57874 contract
+            
+            try {
+                Repository repository  = Repository.getDefault();
+                FileSystem sfs = repository.getDefaultFileSystem();
+                FileObject fo = sfs.findResource("Projects/Actions");  // NOI18N
+                if (fo != null) {
+                    DataObject dobj = DataObject.find(fo);
+                    FolderLookup actionRegistry = new FolderLookup((DataFolder)dobj);
+                    Lookup.Template query = new Lookup.Template(Object.class);
+                    Lookup lookup = actionRegistry.getLookup();
+                    Iterator it = lookup.lookup(query).allInstances().iterator();
+                    if (it.hasNext()) {
+                        actions.add(null);
+                    }
+                    while (it.hasNext()) {
+                        Object next = it.next();
+                        if (next instanceof Action) {
+                            actions.add(next);
+                        } else if (next instanceof JSeparator) {
+                            actions.add(null);
+                        }
+                    }
+                }
+            } catch (DataObjectNotFoundException ex) {
+                // data folder for existing fileobject expected
+                ErrorManager.getDefault().notify(ex);
+            }
+            
             actions.add(null);
             if (brokenLinksAction != null && brokenLinksAction.isEnabled()) {
                 actions.add(brokenLinksAction);
