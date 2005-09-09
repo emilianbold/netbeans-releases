d3b2f87b55028b89f425779e1fcab9b000d98f1b
jjancura <jjancura@netbeans.org>
2005-09-09 12:54:19 +0000

    title component colors changed.

--- options.api/src/org/netbeans/modules/options/OptionsPanel.java
+++ options.api/src/org/netbeans/modules/options/OptionsPanel.java
@@ -26,6 +26,7 @@ import java.awt.Insets;
 import java.awt.BorderLayout;
 import java.awt.Font;
 import java.awt.KeyboardFocusManager;
+import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -167,15 +168,15 @@ public class OptionsPanel extends JPanel {
         // title bar
         JPanel pTitle = new JPanel (new BorderLayout ());
         lTitle = new JLabel ();
-        lTitle.setBackground (new Color (0, 85, 229));
-        lTitle.setForeground (Color.white);
+        lTitle.setBackground (SystemColor.activeCaption); // new Color (0, 85, 229));
+        lTitle.setForeground (SystemColor.activeCaptionText);
         Font f = lTitle.getFont ();
         lTitle.setFont (new Font (f.getName (), Font.BOLD, 16));
         lTitle.setIconTextGap (8);
         lTitle.setOpaque (true);
         pTitle.setBorder (new CompoundBorder (
             new LoweredBorder (),
-            new LineBorder (new Color (0, 85, 229), 1)
+            new LineBorder (SystemColor.activeCaption, 1)
         ));
         pTitle.add ("Center", lTitle);
 
