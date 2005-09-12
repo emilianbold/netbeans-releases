832da5cbedc29a8c83f5c0c0d34efe1a5da994f4
jskrivanek <jskrivanek@netbeans.org>
2005-09-12 09:25:34 +0000

    This panel can be either in Checkout wizard or Import wizard.

--- jellytools/src/org/netbeans/jellytools/modules/javacvs/CVSRootStepOperator.java
+++ jellytools/src/org/netbeans/jellytools/modules/javacvs/CVSRootStepOperator.java
@@ -1,7 +1,20 @@
+/*
+ *                 Sun Public License Notice
+ *
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ *
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
 package org.netbeans.jellytools.modules.javacvs;
 
 import javax.swing.JTextField;
 import org.netbeans.jellytools.Bundle;
+import org.netbeans.jellytools.WizardOperator;
 import org.netbeans.jemmy.operators.JButtonOperator;
 import org.netbeans.jemmy.operators.JCheckBoxOperator;
 import org.netbeans.jemmy.operators.JComboBoxOperator;
@@ -11,7 +24,7 @@ import org.netbeans.jemmy.operators.JRadioButtonOperator;
 import org.netbeans.jemmy.operators.JTextFieldOperator;
 
 /** Class implementing all necessary methods for handling "CVS Root" panel of
- * Checkout wizard.
+ * Checkout or Import wizard.
  * <br>
  * Usage:<br>
  * <pre>
@@ -27,16 +40,18 @@ import org.netbeans.jemmy.operators.JTextFieldOperator;
  *      moduleOper.finish();
  * </pre>
  * @see CheckoutWizardOperator
+ * @see ImportWizardOperator
  * @see EditCVSRootOperator
  * @see ProxyConfigurationOperator
  *
  * @author Jiri.Skrivanek@sun.com
  */
-public class CVSRootStepOperator extends CheckoutWizardOperator {
+public class CVSRootStepOperator extends WizardOperator {
 
     /** Waits for CVS Root panel. */
     public CVSRootStepOperator() {
-        super();
+        // It can be in either Checkout or Import Project Options wizards
+        super("");
         stepsWaitSelectedValue(Bundle.getString(
                 "org.netbeans.modules.versioning.system.cvss.ui.wizards.Bundle", 
                 "BK0006"));
