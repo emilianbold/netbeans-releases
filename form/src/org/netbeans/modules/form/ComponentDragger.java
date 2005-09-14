3b711374710ca942a0b9eaa6a231c41da8171626
jstola <jstola@netbeans.org>
2005-09-14 16:19:19 +0000

    64342: Selection sometimes contains temporarily removed nodes.

--- form/src/org/netbeans/modules/form/ComponentDragger.java
+++ form/src/org/netbeans/modules/form/ComponentDragger.java
@@ -410,6 +410,7 @@ class ComponentDragger
         if (undoRedoOn) // turn on undo/redo monitoring again
             formModel.setUndoRedoRecording(true);
 
+        formDesigner.clearSelection(); // Issue 64342
         // select dropped components in designer (after everything updates)
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
