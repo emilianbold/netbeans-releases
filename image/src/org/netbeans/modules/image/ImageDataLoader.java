ac157d901a58ad247ee615f8be915672a66f4848
mpetras <mpetras@netbeans.org>
2005-09-15 12:51:30 +0000

    fixed bug #59052 - "No reaction on new extension of the Image Object."

--- image/src/org/netbeans/modules/image/ImageDataLoader.java
+++ image/src/org/netbeans/modules/image/ImageDataLoader.java
@@ -13,8 +13,6 @@
 
 package org.netbeans.modules.image;
 
-import java.util.HashMap;
-import java.util.Map;
 import javax.imageio.ImageIO;
 import org.openide.actions.*;
 import org.openide.filesystems.FileObject;
@@ -24,8 +22,6 @@ import org.openide.loaders.MultiDataObject;
 import org.openide.loaders.UniFileLoader;
 import org.openide.util.actions.SystemAction;
 import org.openide.util.NbBundle;
-import org.openide.ErrorManager;
-import java.util.Iterator;
 
 /** 
  * Data loader which recognizes image files.
@@ -37,36 +33,41 @@ public class ImageDataLoader extends UniFileLoader {
     /** Generated serial version UID. */
     static final long serialVersionUID =-8188309025795898449L;
     
-    /** holds information about supported and unsupported MIME-types */
-    private static final Map imageTypeSupport = new HashMap(7, .75f);
+    /** MIME-type of BMP files */
+    private static final String BMP_MIME_TYPE = "image/bmp";            //NOI18N
+    /** is BMP format support status known? */
+    private static boolean bmpSupportStatusKnown = false;
     
     /** Creates new image loader. */
     public ImageDataLoader() {
         // Set the representation class.
         super("org.netbeans.modules.image.ImageDataObject"); // NOI18N
+        
+        ExtensionList ext = new ExtensionList();
+        ext.addMimeType("image/gif");                                   //NOI18N
+        ext.addMimeType("image/jpeg");                                  //NOI18N
+        ext.addMimeType("image/png");                                   //NOI18N
+        setExtensions(ext);
     }
     
     protected FileObject findPrimaryFile(FileObject fo){
-        if (fo.isFolder()) {
-            return null;
+        FileObject primFile = super.findPrimaryFile(fo);
+        
+        if ((primFile == null)
+                && !bmpSupportStatusKnown 
+                && !fo.isFolder()
+                && fo.getMIMEType().equals(BMP_MIME_TYPE)) {
+            try {
+                if (ImageIO.getImageReadersByMIMEType(BMP_MIME_TYPE).hasNext()){
+                    getExtensions().addMimeType(BMP_MIME_TYPE);
+                    primFile = fo;
+                }
+            } finally {
+                bmpSupportStatusKnown = true;
+            }
         }
         
-        final String mimeType = fo.getMIMEType();
-        if (mimeType.startsWith("image/") && mimeType.length() > 6) {   //NOI18N
-            final String imgType = mimeType.substring(6);
-            Object supported = imageTypeSupport.get(imgType);
-            if (supported != null) {
-                return (supported == Boolean.TRUE) ? fo : null;
-            } else {
-                boolean hasReader = ImageIO.getImageReadersByMIMEType(mimeType)
-                                    .hasNext();
-                imageTypeSupport.put(new String(imgType),
-                                     Boolean.valueOf(hasReader));
-                return hasReader ? fo : null;
-            }
-        } else {
-            return null;
-        }
+        return primFile;
     }
     
     /** Gets default display name. Overrides superclass method. */
