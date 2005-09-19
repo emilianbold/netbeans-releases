f54c113e4cb260ce8045878672f09edfb99adbee
mfukala <mfukala@netbeans.org>
2005-09-19 12:20:18 +0000

    #61755 - [cc] Confusing completion behind optional end tags

--- html.editor.lib/src/org/netbeans/editor/ext/html/HTMLSyntaxSupport.java
+++ html.editor.lib/src/org/netbeans/editor/ext/html/HTMLSyntaxSupport.java
@@ -543,7 +543,8 @@ public class HTMLSyntaxSupport extends ExtSyntaxSupport implements InvalidateLis
         
         for( ; elem != null; elem = elem.getPrevious() ) {
             if( elem.getType() == SyntaxElement.TYPE_ENDTAG && elem.getText().endsWith(">") ) { // NOI18N
-                stack.push( ((SyntaxElement.Named)elem).getName().toUpperCase() );
+                DTD.Element tag = dtd.getElement( ((SyntaxElement.Named)elem).getName().toUpperCase() );
+                if(!tag.isEmpty()) stack.push( ((SyntaxElement.Named)elem).getName().toUpperCase() );
             } else if( (elem.getType() == SyntaxElement.TYPE_TAG) && (elem.getText().indexOf("<") == -1)) { //now </ and > are returned as SyntaxElement.TAG so I need to filter them  NOI18N
                 DTD.Element tag = dtd.getElement( ((SyntaxElement.Tag)elem).getName().toUpperCase() );
 
