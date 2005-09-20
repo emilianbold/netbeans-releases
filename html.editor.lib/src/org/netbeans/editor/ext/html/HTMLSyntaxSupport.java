eb190f447c4cef283782b1ec42928fad6afca76d
mfukala <mfukala@netbeans.org>
2005-09-20 13:00:37 +0000

    #64736 - NPE on end tags completion behind unknown tags

--- html.editor.lib/src/org/netbeans/editor/ext/html/HTMLSyntaxSupport.java
+++ html.editor.lib/src/org/netbeans/editor/ext/html/HTMLSyntaxSupport.java
@@ -544,7 +544,7 @@ public class HTMLSyntaxSupport extends ExtSyntaxSupport implements InvalidateLis
         for( ; elem != null; elem = elem.getPrevious() ) {
             if( elem.getType() == SyntaxElement.TYPE_ENDTAG && elem.getText().endsWith(">") ) { // NOI18N
                 DTD.Element tag = dtd.getElement( ((SyntaxElement.Named)elem).getName().toUpperCase() );
-                if(!tag.isEmpty()) stack.push( ((SyntaxElement.Named)elem).getName().toUpperCase() );
+                if(tag == null || !tag.isEmpty()) stack.push( ((SyntaxElement.Named)elem).getName().toUpperCase() );
             } else if( (elem.getType() == SyntaxElement.TYPE_TAG) && (elem.getText().indexOf("<") == -1)) { //now </ and > are returned as SyntaxElement.TAG so I need to filter them  NOI18N
                 DTD.Element tag = dtd.getElement( ((SyntaxElement.Tag)elem).getName().toUpperCase() );
 
