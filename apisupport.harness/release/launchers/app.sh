56d663c8672a948525ab7f493e0b7a1cf3f68b52
jglick <jglick@netbeans.org>
2005-09-21 04:18:19 +0000

    #61551: permit spaces in suite paths.

--- apisupport.harness/release/launchers/app.sh
+++ apisupport.harness/release/launchers/app.sh
@@ -19,47 +19,47 @@ done
 progdir=`dirname "$PRG"`
 APPNAME=`basename "$0"`
 
-if [ -f "$progdir"/../etc/"$APPNAME".conf ] ; then
-    . "$progdir"/../etc/"$APPNAME".conf
+if [ -f "$progdir/../etc/$APPNAME".conf ] ; then
+    . "$progdir/../etc/$APPNAME".conf
 fi
 
 args="$@"
 
 case "`uname`" in
     Darwin*)
-        userdir=${default_mac_userdir}
+        userdir="${default_mac_userdir}"
         ;;
     *)
-        userdir=${default_userdir}
+        userdir="${default_userdir}"
         ;;
 esac
 while [ $# -gt 0 ] ; do
     case "$1" in
-        --userdir) shift; if [ $# -gt 0 ] ; then userdir=$1; fi
+        --userdir) shift; if [ $# -gt 0 ] ; then userdir="$1"; fi
             ;;
     esac
     shift
 done
 
-if [ -f "${userdir}"/etc/"$APPNAME".conf ] ; then
-    . "${userdir}"/etc/"$APPNAME".conf
+if [ -f "${userdir}/etc/$APPNAME".conf ] ; then
+    . "${userdir}/etc/$APPNAME".conf
 fi
 
 readClusters() {
   sep=""
   while read X; do 
     echo -n $sep
-    echo -n $progdir/../$X
+    echo -n "$progdir/../$X"
     sep=":"
   done
 }
-clusters=`cat $progdir/../etc/"$APPNAME".clusters | readClusters`
+clusters=`cat "$progdir/../etc/$APPNAME".clusters | readClusters`
 
 if [ ! -z "$extraclusters" ] ; then
     clusters="$clusters:$extraclusters"
 fi
 
-nbexec="$progdir"/../platform6/lib/nbexec
+nbexec="$progdir/../platform6/lib/nbexec"
 
 case "`uname`" in
     Darwin*)
