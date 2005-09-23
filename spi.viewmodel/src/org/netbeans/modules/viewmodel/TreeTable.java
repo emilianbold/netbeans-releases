00ec5803d59b027a7d924cca299c4a281d8c9cf5
mentlicher <mentlicher@netbeans.org>
2005-09-23 10:43:46 +0000

    #64856 - Define automatically column mnemonics.

--- spi.viewmodel/src/org/netbeans/modules/viewmodel/TreeTable.java
+++ spi.viewmodel/src/org/netbeans/modules/viewmodel/TreeTable.java
@@ -212,12 +212,32 @@ ExplorerManager.Provider, PropertyChangeListener, TreeExpansionListener {
             if (cs [i].getType () == null)
                 addDefaultColumn = false;
         }
-        if (!addDefaultColumn) return columns;
+        if (!addDefaultColumn) {
+            setMnemonics(columns);
+            return columns;
+        }
         PropertySupport.ReadWrite[] columns2 = 
             new PropertySupport.ReadWrite [columns.length + 1];
         System.arraycopy (columns, 0, columns2, 1, columns.length);
         columns2 [0] = new DefaultColumn ();
+        setMnemonics(columns2);
         return columns2;
+    }
+    
+    private void setMnemonics(Node.Property properties[]) {
+        HashSet mnemonicsUsed = new HashSet();
+        for (int i = 0; i < properties.length; i++) {
+            String name = properties[i].getDisplayName();
+            for (int j = 0; j < name.length(); j++) {
+                String c = name.substring(j, j + 1);
+                String lc = c.toLowerCase();
+                if (!mnemonicsUsed.contains(lc)) {
+                    mnemonicsUsed.add(lc);
+                    properties[i].setValue("ColumnMnemonicCharTTV", c); // NOI18N
+                    break;
+                }
+            }
+        }
     }
 
     void updateColumnWidths () {
