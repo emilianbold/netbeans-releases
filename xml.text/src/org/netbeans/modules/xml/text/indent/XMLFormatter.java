798cd2d5d1f9a52490bda17f911282ef9d727abe
mfukala <mfukala@netbeans.org>
2005-10-03 14:49:10 +0000

    #65446 -   Wrong CC for comments

--- xml.text/src/org/netbeans/modules/xml/text/indent/XMLFormatter.java
+++ xml.text/src/org/netbeans/modules/xml/text/indent/XMLFormatter.java
@@ -211,7 +211,7 @@ public class XMLFormatter extends ExtFormatter {
                 TokenItem token = sup.getTokenChain(dotPos-1, dotPos);
                 TokenItem origToken = token;
                 int start = token.getOffset();
-                if (token.getTokenContextPath().contains(XMLDefaultTokenContext.contextPath) &&
+                if (token.getTokenID() == XMLDefaultTokenContext.TAG &&
                         !token.getImage().endsWith("/>")) {
                     // > is a xml token which interests us
                     
