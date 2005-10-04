da351fec55d0fe7981b29c0773c93fd2a2ca71a9
mmetelka <mmetelka@netbeans.org>
2005-10-04 16:05:43 +0000

    Fixed 63212 - code templates now shown below java completion items

--- editor.codetemplates/src/org/netbeans/lib/editor/codetemplates/CodeTemplateCompletionItem.java
+++ editor.codetemplates/src/org/netbeans/lib/editor/codetemplates/CodeTemplateCompletionItem.java
@@ -172,7 +172,7 @@ public final class CodeTemplateCompletionItem implements CompletionItem {
     }
 
     public int getSortPriority() {
-        return 50;
+        return 800;
     }        
     
     public CharSequence getSortText() {
