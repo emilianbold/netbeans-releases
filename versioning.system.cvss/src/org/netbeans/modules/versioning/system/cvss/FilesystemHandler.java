3cdb9c7b2e0dcab70c68aedc11b306583359f777
msandor <msandor@netbeans.org>
2005-10-17 15:30:39 +0000

    #66974: Test for invalid or deleted folders added

--- versioning.system.cvss/src/org/netbeans/modules/versioning/system/cvss/FilesystemHandler.java
+++ versioning.system.cvss/src/org/netbeans/modules/versioning/system/cvss/FilesystemHandler.java
@@ -112,6 +112,7 @@ class FilesystemHandler implements FileChangeListener, InterceptionListener {
 
     private void saveRecursively(File root) {
         File [] files = root.listFiles();
+        if (files == null) return;   // invalid or deleted folder
         for (int i = 0; i < files.length; i++) {
             File file = files[i];
             if (file.isDirectory()) {
