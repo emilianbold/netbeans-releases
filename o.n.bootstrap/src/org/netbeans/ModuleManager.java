9e760980f8aeda8b9d167fa3411207d5f34b8cda
jskrivanek <jskrivanek@netbeans.org>
2005-10-17 14:59:02 +0000

    Report file name when IOException is caught.

--- o.n.bootstrap/src/org/netbeans/ModuleManager.java
+++ o.n.bootstrap/src/org/netbeans/ModuleManager.java
@@ -7,7 +7,7 @@
  * http://www.sun.com/
  * 
  * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2003 Sun
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -87,7 +87,7 @@ public final class ModuleManager {
                     try {
                         classLoaderPatches.add(new JarFile(f));
                     } catch (IOException ioe) {
-                        Util.err.notify(ioe);
+                        Util.err.notify(new Exception("Problematic file: "+f, ioe));
                     }
                 }
             }
