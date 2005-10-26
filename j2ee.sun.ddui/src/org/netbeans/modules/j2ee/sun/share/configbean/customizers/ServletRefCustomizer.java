3caf85190558d5500ab3e814ff3e4e3cadfa5bff
pcw <pcw@netbeans.org>
2005-10-26 00:33:42 +0000

    Cleaned up extraneous imports that were not cleaned up in previous webservice endpoint removal checkin.

--- j2ee.sun.ddui/src/org/netbeans/modules/j2ee/sun/share/configbean/customizers/ServletRefCustomizer.java
+++ j2ee.sun.ddui/src/org/netbeans/modules/j2ee/sun/share/configbean/customizers/ServletRefCustomizer.java
@@ -17,37 +17,18 @@
  */
 
 package org.netbeans.modules.j2ee.sun.share.configbean.customizers;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
-
-import java.beans.Customizer;
-import java.beans.PropertyVetoException;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
-import java.awt.event.ItemEvent;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
-import javax.swing.event.TableModelEvent;
-import javax.swing.event.TableModelListener;
-
-import org.netbeans.modules.j2ee.sun.dd.api.CommonDDBean;
-import org.netbeans.modules.j2ee.sun.dd.api.common.WebserviceEndpoint;
 
 import org.netbeans.modules.j2ee.sun.share.configbean.ServletRef;
 import org.netbeans.modules.j2ee.sun.share.configbean.ServletVersion;
-import org.netbeans.modules.j2ee.sun.share.configbean.ErrorMessageDB;
-import org.netbeans.modules.j2ee.sun.share.configbean.StorageBeanFactory;
-import org.netbeans.modules.j2ee.sun.share.configbean.customizers.common.GenericTableModel;
-import org.netbeans.modules.j2ee.sun.share.configbean.customizers.common.GenericTablePanel;
-import org.netbeans.modules.j2ee.sun.share.configbean.customizers.common.CustomizerErrorPanel;
-import org.netbeans.modules.j2ee.sun.share.configbean.customizers.common.CustomizerTitlePanel;
 import org.netbeans.modules.j2ee.sun.share.configbean.customizers.common.BaseCustomizer;
-import org.netbeans.modules.j2ee.sun.share.configbean.customizers.common.HelpContext;
 
 
 /**
