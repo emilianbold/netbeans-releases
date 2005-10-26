ecabbd22af650ee67d9a9776d4eca2dbaf657d6f
lkotouc <lkotouc@netbeans.org>
2005-10-26 09:27:20 +0000

    #67305: Too eager init of palettes [artefacts loading]

--- spi.palette/src/org/netbeans/modules/palette/PaletteItemNode.java
+++ spi.palette/src/org/netbeans/modules/palette/PaletteItemNode.java
@@ -17,16 +17,19 @@ import java.awt.Image;
 import java.awt.datatransfer.Transferable;
 import java.beans.BeanInfo;
 import java.io.IOException;
+import org.openide.ErrorManager;
 import org.openide.loaders.DataNode;
 import org.openide.nodes.Children;
 import org.openide.nodes.FilterNode;
 import org.openide.nodes.Node;
 import org.openide.text.ActiveEditorDrop;
 import org.openide.util.Lookup;
+import org.openide.util.NbBundle;
+import org.openide.util.Utilities;
 import org.openide.util.datatransfer.ExTransferable;
 import org.openide.util.lookup.AbstractLookup;
 import org.openide.util.lookup.InstanceContent;
-import org.openide.util.lookup.ProxyLookup;
+
 
 
 
@@ -37,23 +40,40 @@ import org.openide.util.lookup.ProxyLookup;
 public final class PaletteItemNode extends FilterNode {
     
     private static final Node.PropertySet[] NO_PROPERTIES = new Node.PropertySet[0];
-    
+
     private String name;
+    private String bundleName;
+    private String displayNameKey;
+    private String className;
+    private String tooltipKey; 
+    private String icon16URL;
+    private String icon32URL; 
+    
     private String displayName;
     private String description;
     private Image icon16;
     private Image icon32;
-    private Lookup localLookup;
     
-    PaletteItemNode(DataNode original, String name, String displayName, String description, Image icon16, Image icon32, InstanceContent content ) {
+    PaletteItemNode(DataNode original, 
+                    String name, 
+                    String bundleName, 
+                    String displayNameKey, 
+                    String className, 
+                    String tooltipKey, 
+                    String icon16URL, 
+                    String icon32URL, 
+                    InstanceContent content) 
+    {
         super(original, Children.LEAF, new AbstractLookup(content));
         
         content.add( this );
         this.name = name;
-        this.displayName = displayName;
-        this.description = description;
-        this.icon16 = icon16;
-        this.icon32 = icon32;
+        this.bundleName = bundleName; 
+        this.displayNameKey = displayNameKey;
+        this.className = className;
+        this.tooltipKey = tooltipKey;
+        this.icon16URL = icon16URL;
+        this.icon32URL = icon32URL;
     }
  
     public String getName() {
@@ -61,19 +81,39 @@ public final class PaletteItemNode extends FilterNode {
     }
 
     public String getDisplayName() {
+        if (displayName == null)
+            displayName = _getDisplayName(bundleName, displayNameKey, className);
+        
         return displayName;
     }
 
     public String getShortDescription() {
+        if (description == null)
+            description = _getShortDescription(bundleName, tooltipKey, className, displayNameKey);
+        
         return description;
     }
 
     public Image getIcon(int type) {
 
-        Image icon = icon16;
+        Image icon = null;
         
-        if (type == BeanInfo.ICON_COLOR_32x32 || type == BeanInfo.ICON_MONO_32x32)
+        if (type == BeanInfo.ICON_COLOR_16x16 || type == BeanInfo.ICON_MONO_16x16) {
+            if (icon16 == null) {
+                icon16 = _getIcon(icon16URL);
+                if (icon16 == null)
+                    icon16 = Utilities.loadImage("org/netbeans/modules/palette/resources/unknown16.gif"); // NOI18N
+            }
+            icon = icon16;
+        }
+        else if (type == BeanInfo.ICON_COLOR_32x32 || type == BeanInfo.ICON_MONO_32x32) {
+            if (icon32 == null) {
+                icon32 = _getIcon(icon32URL);
+                if (icon32 == null)
+                    icon32 = Utilities.loadImage("org/netbeans/modules/palette/resources/unknown32.gif"); // NOI18N
+            }
             icon = icon32;
+        }
         
         return icon;
     }
@@ -118,6 +158,82 @@ public final class PaletteItemNode extends FilterNode {
             return drop;
         }
         
+    }
+    
+    public String _getDisplayName(
+            String bundleName, 
+            String displayNameKey, 
+            String instanceName) 
+    {
+
+        String displayName = null;
+        try {
+            displayName = NbBundle.getBundle(bundleName).getString(displayNameKey);
+
+            if (displayName == null && displayNameKey != null)
+                displayName = displayNameKey;
+
+            if (displayName == null) {//derive name from the instance name
+                if (instanceName != null && instanceName.trim().length() > 0) {
+                    int dotIndex = instanceName.lastIndexOf('.'); // NOI18N
+                    displayName = instanceName.substring(dotIndex);
+                }
+            }
+
+            if (displayName == null) // no name derived from the item
+                displayName = name;
+
+        }
+        catch (Exception ex) {
+            ErrorManager.getDefault().notify(ErrorManager.INFORMATIONAL, ex);
+        }
+
+        return (displayName == null ? "" : displayName);
+    }
+
+    public String _getShortDescription(
+            String bundleName, 
+            String tooltipKey, 
+            String instanceName, 
+            String displayNameKey) 
+    {
+
+        String tooltip = null;
+        try {
+            tooltip = NbBundle.getBundle(bundleName).getString(tooltipKey);
+
+            if (tooltip == null && tooltipKey != null)
+                tooltip = tooltipKey;
+
+            if (tooltip == null) {//derive name from instance name
+                if (instanceName != null && instanceName.trim().length() > 0) {
+                    int dotIndex = instanceName.indexOf('.'); // NOI18N
+                    tooltip = instanceName.substring(0, dotIndex).replace('-', '.'); // NOI18N
+                }
+            }
+
+            if (tooltip == null) // no tooltip derived from the item
+                tooltip = _getDisplayName(bundleName, displayNameKey, instanceName);
+
+        }
+        catch (Exception ex) {
+            ErrorManager.getDefault().notify(ErrorManager.INFORMATIONAL, ex);
+        }
+
+        return (tooltip == null ? "" :  tooltip);
+    }
+
+    public Image _getIcon(String iconURL) {
+
+        Image icon = null;
+        try {
+            icon = Utilities.loadImage(iconURL);
+        }
+        catch (Exception ex) {
+            ErrorManager.getDefault().notify(ErrorManager.INFORMATIONAL, ex);
+        }
+
+        return icon;
     }
     
 }
