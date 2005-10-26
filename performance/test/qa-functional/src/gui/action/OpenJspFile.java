b4421f3058818ed3d6d1bb649947cf478556447b
mmirilovic <mmirilovic@netbeans.org>
2005-10-26 12:26:28 +0000

    try to fix failed JSP open tests

--- performance/test/qa-functional/src/gui/action/OpenJspFile.java
+++ performance/test/qa-functional/src/gui/action/OpenJspFile.java
@@ -7,7 +7,7 @@
  * http://www.sun.com/
  *
  * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -59,6 +59,7 @@ public class OpenJspFile extends OpenFiles {
     
     public void prepare(){
         this.openNode = new Node(new ProjectsTabOperator().getProjectRootNode(fileProject),WEB_PAGES + '|' + fileName);
+        this.openNode.select();
         
         if (this.openNode == null) {
             throw new Error("Cannot find node [" + WEB_PAGES + '|' + fileName + "] in project [" + fileProject + "]");
