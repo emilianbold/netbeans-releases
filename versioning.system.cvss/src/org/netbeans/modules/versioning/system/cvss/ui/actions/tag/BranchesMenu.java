b78bb88717b3ef97b65794e2db6b239dd51941a3
msandor <msandor@netbeans.org>
2005-11-02 13:19:15 +0000

    #67980: Fixing popup menu

--- versioning.system.cvss/src/org/netbeans/modules/versioning/system/cvss/ui/actions/tag/BranchesMenu.java
+++ versioning.system.cvss/src/org/netbeans/modules/versioning/system/cvss/ui/actions/tag/BranchesMenu.java
@@ -13,15 +13,12 @@
 
 package org.netbeans.modules.versioning.system.cvss.ui.actions.tag;
 
-import javax.swing.event.ChangeListener;
-import org.openide.util.actions.Presenter;
 import org.openide.util.actions.SystemAction;
 import org.openide.util.NbBundle;
 import org.openide.awt.Actions;
+import org.openide.awt.DynamicMenuContent;
 
 import javax.swing.*;
-import javax.swing.event.PopupMenuListener;
-import javax.swing.event.PopupMenuEvent;
 import java.awt.event.ActionEvent;
 
 /**
@@ -29,10 +26,18 @@ import java.awt.event.ActionEvent;
  *
  * @author Maros Sandor
  */
-public class BranchesMenu extends AbstractAction implements Presenter.Menu, Presenter.Popup {
+public class BranchesMenu extends AbstractAction implements DynamicMenuContent {
 
     public BranchesMenu() {
         super(NbBundle.getMessage(BranchesMenu.class, "CTL_MenuItem_BranchesMenu"));
+    }
+
+    public JComponent[] getMenuPresenters() {
+        return new JComponent [] { createMenu() };
+    }
+
+    public JComponent[] synchMenuPresenters(JComponent[] items) {
+        return new JComponent [] { createMenu() };
     }
 
     public boolean isEnabled() {
@@ -43,42 +48,12 @@ public class BranchesMenu extends AbstractAction implements Presenter.Menu, Pres
         // no operation
     }
 
-    public JMenuItem getMenuPresenter() {
-        return createMenu();
-    }
-
-    public JMenuItem getPopupPresenter() {
-        return createMenu();
-    }
-
     private JMenu createMenu() {
         JMenu menu = new JMenu(this);
-	// change listener seems to be the only thing to work correctly on macosx.
-	// popup listener is not..
-        menu.getModel().addChangeListener(new Change(menu));
-	
-        menu.setMnemonic(NbBundle.getMessage(BranchesMenu.class, "MNE_MenuItem_BranchesMenu").charAt(0));
-        return menu;
-    }
-
-    private static void fillMenu(JPopupMenu menu) {
-        menu.removeAll();
         menu.add(new Actions.MenuItem(SystemAction.get(BranchAction.class), true));
         menu.add(new Actions.MenuItem(SystemAction.get(SwitchBranchAction.class), true));
         menu.add(new Actions.MenuItem(SystemAction.get(MergeBranchAction.class), true));
+        menu.setMnemonic(NbBundle.getMessage(BranchesMenu.class, "MNE_MenuItem_BranchesMenu").charAt(0));
+        return menu;
     }
-
-
-    private static class Change implements ChangeListener {
-	private JMenu menu;
-	
-	public Change(JMenu menu) {
-	    this.menu = menu;
-	}
-        public void stateChanged(javax.swing.event.ChangeEvent e) {
-	    fillMenu(menu.getPopupMenu());
-        }
-    }
-    
 }
-
