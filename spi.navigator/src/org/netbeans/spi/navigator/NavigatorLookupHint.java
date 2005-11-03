c440f5bd4030939a4cbc91ab4215ec9c23e927bc
dsimonek <dsimonek@netbeans.org>
2005-11-03 17:54:44 +0000

    #68026: fixed and improved navigator javadoc

--- spi.navigator/src/org/netbeans/spi/navigator/NavigatorLookupHint.java
+++ spi.navigator/src/org/netbeans/spi/navigator/NavigatorLookupHint.java
@@ -13,19 +13,21 @@
 
 package org.netbeans.spi.navigator;
 
-/** Hint for Navigator clients to link their Node's Lookup with 
- * Navigator content type.  
+/** Hint for Navigator clients to link <code>Lookup</code> of their
+ * <code>TopComponent</code> with Navigator content type.<p></p>  
  * 
  * Usage: Implementation of this interface should be inserted into
- * client's specific node lookup (see Node.getLookup()).
- * When mentioned node gets selected, system will ask for content type
- * to show in Navigator UI.  
+ * client's specific topComponent's lookup, see 
+ * <a href="@org-openide-windows@/org/openide/windows/TopComponent.html#getLookup()">TopComponent.getLookup()</a>
+ * method. When mentioned <code>TopComponent</code> gets active in the system, system will
+ * ask <code>NavigatorLookupHint</code> implementation for content type
+ * to show in Navigator UI.
  *
  * @author Dafe Simonek
  */
 public interface NavigatorLookupHint {
 
-    /** Hitn for content type that should be used in Navigator 
+    /** Hint for content type that should be used in Navigator 
      * 
      * @return String representation of content type (in mime-type style)
      */
