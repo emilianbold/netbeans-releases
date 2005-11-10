a4d0b29db22e9e24b418431289f8a3082e49aea6
jskrivanek <jskrivanek@netbeans.org>
2005-11-10 09:41:23 +0000

    #67854 - added test to check trimming of log files.

--- nbjunit/test/unit/src/org/netbeans/junit/LoggingTest.java
+++ nbjunit/test/unit/src/org/netbeans/junit/LoggingTest.java
@@ -14,11 +14,7 @@
 package org.netbeans.junit;
 
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.io.PrintStream;
-import java.io.Writer;
-import org.netbeans.junit.NbTestCase;
 
 /** Checks that we can do proper logging.
  *
@@ -27,18 +23,61 @@ import org.netbeans.junit.NbTestCase;
 public class LoggingTest extends NbTestCase {
     
     public LoggingTest(String testName) {
-    	super(testName);
+        super(testName);
     }
     
+    public static NbTestSuite suite() {
+        NbTestSuite suite = new NbTestSuite();
+        // you can define order of test cases here
+        suite.addTest(new LoggingTest("testLogFileName"));
+        suite.addTest(new LoggingTest("testLogFileNameEqualsToNameOfTest"));
+        suite.addTest(new LoggingTest("testTrimLogFiles"));
+        suite.addTest(new LoggingTest("testCheckTrimLogFiles"));
+        return suite;
+    }
     
     public void testLogFileName() throws Exception {
         PrintStream ps = getLog("ahoj");
         File f = new File(getWorkDir(), "ahoj");
         assertEquals("Log file exists", true, f.exists());
     }
+    
     public void testLogFileNameEqualsToNameOfTest() throws Exception {
         PrintStream ps = getLog();
         File f = new File(getWorkDir(), getName() + ".log");
         assertEquals("Log file exists", true, f.exists());
+    }
+    
+    /** Test of NbTestCase#trimLogFiles method. It should trim size of all files
+     * in test case workdir to 1 MB. The method is called at the end of 
+     * NbTestCase#run method.
+     */
+    public void testTrimLogFiles() {
+        StringBuffer buff = new StringBuffer(1024);
+        for(int i=0;i<1024;i++) {
+            buff.append('A');
+        }
+        String string1kB = buff.toString();
+        for(int i=0;i<2024;i++) {
+            log(string1kB);
+            log("myLog", string1kB);
+            ref(string1kB);
+        }
+    }
+    
+    /** Check log files created by previous test case were correctly trimmed. */
+    public void testCheckTrimLogFiles() throws Exception {
+        File trimmedDir = new File(getWorkDir().getParentFile(), "testTrimLogFiles");
+        String[] filenames = {"testTrimLogFiles.log", "testTrimLogFiles.ref", "myLog" };
+        for(int i=0;i<filenames.length;i++) {
+            File file = new File(trimmedDir, "TRIMMED_"+filenames[i]);
+            assertTrue(file.getName()+" not exists.", file.exists());
+            assertTrue(file.getName()+" not trimmed to 1 MB.", file.length() < 2097152L);
+            file = new File(trimmedDir, filenames[i]);
+            if(file.exists()) {
+                // original file exists only if cannot be deleted. Then it has minimal size.
+                assertTrue(file.getName()+" not trimmed.", file.length() < 1024L);
+            }
+        }
     }
 }
