6c7ee605831cc611a87d1ed04bd8bb8f42f1b4c2
mkuchtiak <mkuchtiak@netbeans.org>
2005-11-22 13:06:34 +0000

    NB License added

--- websvc.registry/extsrc/org/netbeans/modules/websvc/registry/actions/AddWSGroupPanel.java
+++ websvc.registry/extsrc/org/netbeans/modules/websvc/registry/actions/AddWSGroupPanel.java
@@ -1,16 +1,24 @@
 /*
- * AddWSGroupPanel.java
+ *                 Sun Public License Notice
  *
- * Created on 22. listopad 2005, 10:54
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ *
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
+ * Microsystems, Inc. All Rights Reserved.
  */
 
 package org.netbeans.modules.websvc.registry.actions;
 
 import javax.swing.text.Document;
 
-/**
+/** AddWSGroupPanel.java - simple panel with one text field for Web Service Group name
  *
  * @author  mkuchtiak
+ * Created on 22. November 2005, 10:54
  */
 public class AddWSGroupPanel extends javax.swing.JPanel {
     
