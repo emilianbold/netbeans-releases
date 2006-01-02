aa2f79fdae7c081f6a80e3ff911d73805694e106
tpavek <tpavek@netbeans.org>
2006-01-02 16:51:45 +0000

    new test

---
+++ form/test/unit/src/org/netbeans/modules/form/layoutdesign/ALT_DefaultSize01Test.java
@@ -0,0 +1,114 @@
+/*
+ *                 Sun Public License Notice
+ *
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ *
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.modules.form.layoutdesign;
+
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+import org.openide.filesystems.FileUtil;
+
+// Tests mouse resizing of component (not container) to default size.
+public class ALT_DefaultSize01Test extends LayoutTestCase {
+        
+    public ALT_DefaultSize01Test(String name) {
+        super(name);
+        try {
+	    className = this.getClass().getName();
+	    className = className.substring(className.lastIndexOf('.') + 1, className.length());	    
+            startingFormFile = FileUtil.toFileObject(new File(url.getFile() + goldenFilesPath + className + "-StartingForm.form").getCanonicalFile());
+        } catch (IOException ioe) {
+            fail(ioe.toString());
+        }
+    }
+
+    // Resize (shrink) button from right to default size.
+    public void doChanges0() {
+        ld.externalSizeChangeHappened();
+        // > UPDATE CURRENT STATE
+        contInterior.put("Form", new Rectangle(0, 0, 400, 300));
+        compBounds.put("jButton1", new Rectangle(10, 11, 137, 23));
+        baselinePosition.put("jButton1-137-23", new Integer(15));
+        compPrefSize.put("jButton1", new Dimension(75, 23));
+        contInterior.put("Form", new Rectangle(0, 0, 400, 300));
+        compBounds.put("jButton1", new Rectangle(10, 11, 137, 23));
+        baselinePosition.put("jButton1-137-23", new Integer(15));
+        ld.updateCurrentState();
+        // < UPDATE CURRENT STATE
+        // > START RESIZING
+        baselinePosition.put("jButton1-137-23", new Integer(15));
+        compPrefSize.put("jButton1", new Dimension(75, 23));
+        {
+            String[] compIds = new String[] {
+                "jButton1"
+                };
+            Rectangle[] bounds = new Rectangle[] {
+                new Rectangle(10, 11, 137, 23)
+                };
+            Point hotspot = new Point(148,21);
+            int[] resizeEdges = new int[] {
+                1,
+                    -1
+                };
+            boolean inLayout = true;
+            ld.startResizing(compIds, bounds, hotspot, resizeEdges, inLayout);
+        }
+        // < START RESIZING
+        prefPaddingInParent.put("Form-jButton1-0-1", new Integer(10)); // parentId-compId-dimension-compAlignment
+        // > MOVE
+        {
+            Point p = new Point(89,25);
+            String containerId= "Form";
+            boolean autoPositioning = true;
+            boolean lockDimension = false;
+            Rectangle[] bounds = new Rectangle[] {
+                new Rectangle(10, 11, 75, 23)
+                };
+            ld.move(p, containerId, autoPositioning, lockDimension, bounds);
+        }
+        // < MOVE
+        prefPaddingInParent.put("Form-jButton1-0-1", new Integer(10)); // parentId-compId-dimension-compAlignment
+        // > MOVE
+        {
+            Point p = new Point(88,25);
+            String containerId= "Form";
+            boolean autoPositioning = true;
+            boolean lockDimension = false;
+            Rectangle[] bounds = new Rectangle[] {
+                new Rectangle(10, 11, 75, 23)
+                };
+            ld.move(p, containerId, autoPositioning, lockDimension, bounds);
+        }
+        // < MOVE
+        // > END MOVING
+        prefPaddingInParent.put("Form-jButton1-0-1", new Integer(10)); // parentId-compId-dimension-compAlignment
+        prefPaddingInParent.put("Form-jButton1-1-1", new Integer(11)); // parentId-compId-dimension-compAlignment
+        ld.endMoving(true);
+        // < END MOVING
+        ld.externalSizeChangeHappened();
+        // > UPDATE CURRENT STATE
+        contInterior.put("Form", new Rectangle(0, 0, 400, 300));
+        compBounds.put("jButton1", new Rectangle(10, 11, 75, 23));
+        baselinePosition.put("jButton1-75-23", new Integer(15));
+        compPrefSize.put("jButton1", new Dimension(75, 23));
+        contInterior.put("Form", new Rectangle(0, 0, 400, 300));
+        compBounds.put("jButton1", new Rectangle(10, 11, 75, 23));
+        baselinePosition.put("jButton1-75-23", new Integer(15));
+        ld.updateCurrentState();
+        // < UPDATE CURRENT STATE
+    }
+    
+}
