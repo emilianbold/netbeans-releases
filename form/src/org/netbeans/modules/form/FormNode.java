795886001ccf65e4470dea38643ab4f6177153c3
tpavek <tpavek@netbeans.org>
2006-01-05 15:40:18 +0000

    #70984: serialVersionUID for FormNode.Handle

--- form/src/org/netbeans/modules/form/FormNode.java
+++ form/src/org/netbeans/modules/form/FormNode.java
@@ -143,6 +143,7 @@ public class FormNode extends AbstractNode implements FormCookie {
     }
 
     static class Handle implements Node.Handle {
+        static long serialVersionUID = 1;
         public Node getNode() throws java.io.IOException {
             return new ClosingNode();
         }
