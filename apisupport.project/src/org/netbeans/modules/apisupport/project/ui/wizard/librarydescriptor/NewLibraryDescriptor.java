b53ba3c0548e2c8d9e9beb5c79e6b1657ed98e0e
jglick <jglick@netbeans.org>
2006-01-06 02:16:16 +0000

    Comment only.

--- apisupport.project/src/org/netbeans/modules/apisupport/project/ui/wizard/librarydescriptor/NewLibraryDescriptor.java
+++ apisupport.project/src/org/netbeans/modules/apisupport/project/ui/wizard/librarydescriptor/NewLibraryDescriptor.java
@@ -7,7 +7,7 @@
  * http://www.sun.com/
  *
  * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -95,6 +95,7 @@ final class NewLibraryDescriptor extends BasicWizardIterator {
         }
 
         public boolean isValidLibraryName() {
+            // XXX may need additional conditions, TBD (would need new message in that case)
             return getLibraryName() != null && 
                     getLibraryName().trim().length() != 0;
         }
