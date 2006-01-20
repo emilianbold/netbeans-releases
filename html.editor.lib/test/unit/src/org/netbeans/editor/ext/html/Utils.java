a38c839e8b307241470066121bd07eabec3e2d37
mfukala <mfukala@netbeans.org>
2006-01-20 12:35:24 +0000

    added SUN license

--- html.editor.lib/test/unit/src/org/netbeans/editor/ext/html/Utils.java
+++ html.editor.lib/test/unit/src/org/netbeans/editor/ext/html/Utils.java
@@ -1,12 +1,15 @@
 /*
- * Utils.java
+ *                 Sun Public License Notice
  *
- * Created on January 19, 2006, 10:34 AM
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
  *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
+ * Microsystems, Inc. All Rights Reserved.
  */
-
 package org.netbeans.editor.ext.html;
 
 import java.io.BufferedReader;
