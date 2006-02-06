9943896e97de0f3926b75349b764033987234399
mkrauskopf <mkrauskopf@netbeans.org>
2006-02-06 13:45:41 +0000

    #71994: Add module dependency dlg size issues

--- apisupport.project/src/org/netbeans/modules/apisupport/project/ui/customizer/CustomizerLibraries.java
+++ apisupport.project/src/org/netbeans/modules/apisupport/project/ui/customizer/CustomizerLibraries.java
@@ -15,6 +15,7 @@ package org.netbeans.modules.apisupport.project.ui.customizer;
 
 import java.awt.BorderLayout;
 import java.awt.Dialog;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
@@ -48,6 +49,8 @@ import org.openide.util.NbBundle;
  * @author mkrauskopf
  */
 public class CustomizerLibraries extends NbPropertyPanel.Single {
+    
+    private static Rectangle lastSize;
     
     /** Creates new form CustomizerLibraries */
     public CustomizerLibraries(final SingleModuleProperties props) {
@@ -506,7 +509,12 @@ public class CustomizerLibraries extends NbPropertyPanel.Single {
                 d.dispose();
             }
         });
+        if (lastSize != null) {
+            d.setBounds(lastSize);
+        }
         d.setVisible(true);
+        lastSize = d.getBounds();
+        d.dispose();
         if (descriptor.getValue().equals(DialogDescriptor.OK_OPTION)) {
             ModuleDependency[] newDeps = addPanel.getSelectedDependencies();
             for (int i = 0; i < newDeps.length; i++) {
@@ -514,7 +522,6 @@ public class CustomizerLibraries extends NbPropertyPanel.Single {
                 dependencyList.setSelectedValue(newDeps[i], true);
             }
         }
-        d.dispose();
         dependencyList.requestFocusInWindow();
     }//GEN-LAST:event_addModuleDependency
     
