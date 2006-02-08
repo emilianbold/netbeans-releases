49e0b8393308941c5b7f1645d698c1c83d602ada
mfukala <mfukala@netbeans.org>
2006-02-08 12:57:20 +0000

    added forgotten package

---
+++ html.editor/test/unit/src/org/netbeans/modules/html/editor/test/Utils.java
@@ -0,0 +1,45 @@
+/*
+ *                 Sun Public License Notice
+ *
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ *
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.modules.html.editor.test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author Marek Fukala
+ */
+public class Utils {
+    
+    public static String readFileContentToString(File file) throws IOException {
+        StringBuffer buff = new StringBuffer();
+        
+        BufferedReader rdr = new BufferedReader(new FileReader(file));
+        
+        String line;
+        
+        try{
+            while ((line = rdr.readLine()) != null){
+                buff.append(line + "\n");
+            }
+        } finally{
+            rdr.close();
+        }
+        
+        return buff.toString();
+    }
+    
+}
