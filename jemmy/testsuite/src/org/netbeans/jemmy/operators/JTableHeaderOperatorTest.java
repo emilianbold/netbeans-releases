9d0760095c2658012009bf64d488209297912aab
mriem <mriem@netbeans.org>
2006-02-11 02:38:17 +0000

    Added proper header

--- jemmy/testsuite/src/org/netbeans/jemmy/operators/JTableHeaderOperatorTest.java
+++ jemmy/testsuite/src/org/netbeans/jemmy/operators/JTableHeaderOperatorTest.java
@@ -1,10 +1,21 @@
 /*
- * JTableHeaderOperatorTest.java
- * JUnit based test
+ * $Id$
  *
- * Created on February 9, 2006, 6:08 PM
+ * ---------------------------------------------------------------------------
+ *
+ * Sun Public License Notice
+ *
+ * The contents of this file are subject to the Sun Public License Version 1.0
+ * (the "License"). You may not use this file except in compliance with the
+ * License. A copy of the License is available at http://www.sun.com/.
+ *
+ * The Original Code is the Jemmy library. The Initial Developer of the
+ * Original Code is Alexandre Iline. All Rights Reserved.
+ *
+ * ---------------------------------------------------------------------------
+ *
+ * Contributor(s): Manfred Riem (mriem@netbeans.org).
  */
-
 package org.netbeans.jemmy.operators;
 
 import java.awt.Point;
