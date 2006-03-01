757d636aa6aeaf60ce91d47ba06e8946f1e97653
awegscheider <awegscheider@netbeans.org>
2006-03-01 05:28:51 +0000

    Adding WebSphere 6 plugin help files

---
+++ j2ee.websphere6/javahelp/org/netbeans/modules/j2ee/websphere6/docs/websphere-javahelp.hs
@@ -0,0 +1,29 @@
+<?xml version='1.0' encoding='ISO-8859-1'?>
+<!DOCTYPE helpset PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN"
+                         "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
+
+<helpset version="2.0">
+	<title>IBM WebSphere Application Server Plugin</title>
+	<maps>
+		<homeID>j2eeplugins_registering_app_server_websphere</homeID>
+		<mapref location="websphere-javahelp-map.jhm"/>
+	</maps>
+	<view>
+		<name>TOC</name>
+		<label>Table Of Contents</label>
+		<type>javax.help.TOCView</type>
+		<data>websphere-javahelp-toc.xml</data>
+	</view>
+	<view>
+		<name>Index</name>
+		<label>Index</label>
+		<type>javax.help.IndexView</type>
+		<data>websphere-javahelp-idx.xml</data>
+	</view>
+	<view>
+		<name>Search</name>
+		<label>Search</label>
+		<type>javax.help.SearchView</type>
+		<data engine="com.sun.java.help.search.DefaultSearchEngine">JavaHelpSearch</data>
+	</view>
+</helpset>
