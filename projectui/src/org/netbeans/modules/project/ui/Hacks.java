5e65212f720483a95cb5c09c6fc153e388bfce87
jglick <jglick@netbeans.org>
2006-03-05 21:08:10 +0000

    Put updating of main window title on a delay and do work off EQ. Should make explorer navigation a bit faster and avoid pauses.

--- projectui/src/org/netbeans/modules/project/ui/Hacks.java
+++ projectui/src/org/netbeans/modules/project/ui/Hacks.java
@@ -7,12 +7,13 @@
  * http://www.sun.com/
  *
  * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
 package org.netbeans.modules.project.ui;
 
+import java.awt.EventQueue;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.lang.reflect.Method;
@@ -27,6 +28,7 @@ import org.openide.loaders.TemplateWizard;
 import org.openide.nodes.Node;
 import org.openide.util.Lookup;
 import org.openide.util.NbBundle;
+import org.openide.util.RequestProcessor;
 import org.openide.windows.TopComponent;
 
 /**
@@ -56,42 +58,51 @@ public class Hacks {
         }
         if (setProjectName != null) {
             final TopComponent.Registry r = TopComponent.getRegistry();
-            r.addPropertyChangeListener(new PropertyChangeListener() {
-                public void propertyChange(PropertyChangeEvent ev) {
-                    if (TopComponent.Registry.PROP_ACTIVATED_NODES.equals(ev.getPropertyName())) {
-                        Node[] sel = r.getActivatedNodes();
-                        Set/*<Project>*/ projects = new HashSet();
-                        for (int i = 0; i < sel.length; i++) {
-                            Lookup l = sel[i].getLookup();
-                            Project p = (Project)l.lookup(Project.class);
-                            if (p != null) {
-                                projects.add(p);
-                            } else {
-                                DataObject d = (DataObject)l.lookup(DataObject.class);
-                                if (d != null) {
-                                    FileObject f = d.getPrimaryFile();
-                                    p = FileOwnerQuery.getOwner(f);
-                                    if (p != null) {
-                                        projects.add(p);
-                                    }
+            final RequestProcessor.Task task = RequestProcessor.getDefault().create(new Runnable() {
+                public void run() {
+                    Node[] sel = r.getActivatedNodes();
+                    Set/*<Project>*/ projects = new HashSet();
+                    for (int i = 0; i < sel.length; i++) {
+                        Lookup l = sel[i].getLookup();
+                        Project p = (Project) l.lookup(Project.class);
+                        if (p != null) {
+                            projects.add(p);
+                        } else {
+                            DataObject d = (DataObject) l.lookup(DataObject.class);
+                            if (d != null) {
+                                FileObject f = d.getPrimaryFile();
+                                p = FileOwnerQuery.getOwner(f);
+                                if (p != null) {
+                                    projects.add(p);
                                 }
                             }
                         }
-                        String pname;
-                        if (projects.size() == 1) {
-                            Project p = (Project) projects.iterator().next();
-                            pname = ProjectUtils.getInformation(p).getDisplayName();
-                            assert pname != null : p;
-                        } else if (projects.isEmpty()) {
-                            pname = null;
-                        } else {
-                            pname = NbBundle.getMessage (Hacks.class, "LBL_MultipleProjects"); // NOI18N
+                    }
+                    final String pname;
+                    if (projects.size() == 1) {
+                        Project p = (Project) projects.iterator().next();
+                        pname = ProjectUtils.getInformation(p).getDisplayName();
+                        assert pname != null : p;
+                    } else if (projects.isEmpty()) {
+                        pname = null;
+                    } else {
+                        pname = NbBundle.getMessage(Hacks.class, "LBL_MultipleProjects"); // NOI18N
+                    }
+                    EventQueue.invokeLater(new Runnable() {
+                        public void run() {
+                            try {
+                                setProjectName.invoke(windowSystemImpl, new Object[] {pname});
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
                         }
-                        try {
-                            setProjectName.invoke(windowSystemImpl, new Object[] {pname});
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
+                    });
+                }
+            });
+            r.addPropertyChangeListener(new PropertyChangeListener() {
+                public void propertyChange(PropertyChangeEvent ev) {
+                    if (TopComponent.Registry.PROP_ACTIVATED_NODES.equals(ev.getPropertyName())) {
+                        task.schedule(200);
                     }
                 }
             });
