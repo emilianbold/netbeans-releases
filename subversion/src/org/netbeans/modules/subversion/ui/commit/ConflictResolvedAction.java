ad5cad4a0bf30c942599e75f070f89496e3ec1cc
pkuzel <pkuzel@netbeans.org>
2006-03-09 14:48:25 +0000

    Mark resolved.

--- subversion/src/org/netbeans/modules/subversion/ui/commit/ConflictResolvedAction.java
+++ subversion/src/org/netbeans/modules/subversion/ui/commit/ConflictResolvedAction.java
@@ -13,17 +13,58 @@
 
 package org.netbeans.modules.subversion.ui.commit;
 
-import org.netbeans.modules.subversion.ui.actions.PlaceholderAction;
+import java.io.*;
+import org.netbeans.modules.subversion.*;
+import org.netbeans.modules.subversion.client.*;
+import org.netbeans.modules.subversion.ui.actions.ContextAction;
+import org.netbeans.modules.subversion.util.*;
+import org.openide.*;
+import org.openide.nodes.Node;
+import org.tigris.subversion.svnclientadapter.*;
 
 /**
+ * Represnts <tt>svn resolved</tt> command.
  *
  * @author Petr Kuzel
  */
-public class ConflictResolvedAction extends PlaceholderAction {
+public class ConflictResolvedAction extends ContextAction {
     
-    /** Creates a new instance of ConflistResolvedAction */
-    public ConflictResolvedAction() {
-        name = "Conflict Resolved";
+    protected String getBaseName(Node[] activatedNodes) {
+        return "resolve";  // NOI18N
     }
-    
+
+    protected void performContextAction(Node[] nodes) {
+        Context ctx = getContext(nodes);
+        SvnClient client = null;;
+        try {
+            client = Subversion.getInstance().getClient(ctx);
+        } catch (SVNClientException ex) {
+            ErrorManager.getDefault().notify(ErrorManager.INFORMATIONAL, ex);
+            ErrorManager.getDefault().notify(ErrorManager.USER, ex);
+        }
+        FileStatusCache cache = Subversion.getInstance().getStatusCache();
+
+        if (client == null) {
+            return;
+        }
+
+        // FIXME move out of AWT
+
+        Object pair = startProgress(nodes);
+        try {
+            File[] files = ctx.getFiles();
+            for (int i = 0; i<files.length; i++) {
+                File file = files[1];
+                try {
+                    client.resolved(file);
+                    cache.refresh(file, FileStatusCache.REPOSITORY_STATUS_UNKNOWN);
+                } catch (SVNClientException ex) {
+                    ErrorManager.getDefault().notify(ErrorManager.INFORMATIONAL, ex);
+                }
+            }
+        } finally {
+            finished(pair);
+        }
+    }
+
 }
