e86687b9b891004f9065c38f83c635dac0af703b
rkubacki <rkubacki@netbeans.org>
2006-03-23 10:48:49 +0000

    more work to set properly defaults and make comparing simple

--- performance/test/reports/src/org/netbeans/performance/results/AnalyzeResultsTask.java
+++ performance/test/reports/src/org/netbeans/performance/results/AnalyzeResultsTask.java
@@ -7,7 +7,7 @@
  * http://www.sun.com/
  *
  * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2004 Sun
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -29,9 +29,6 @@ import org.xml.sax.helpers.DefaultHandler;
  * @author Radim Kubacki
  */
 public class AnalyzeResultsTask extends Task {
-    
-    // TODO customize method names to match custom task
-    // property and type (handled by inner class) names
     
     /* For a simple option:
     private boolean opt;
@@ -56,13 +53,6 @@ public class AnalyzeResultsTask extends Task {
         refFile = f;
     }
     
-    /** Output destination. */
-    private File destFile;
-    
-    public void setdestFile(File f) {
-        destFile = f;
-    }
-    
     /* Custom nested elements:
     public static class Nestme {
         String val; // accessible from execute()
@@ -85,65 +75,10 @@ public class AnalyzeResultsTask extends Task {
     // </customtask>
      */
     
-    private List resultsReports = new LinkedList(); // List<FileSet>
+    private List<FileSet> resultsReports = new LinkedList();
     public void addFileset(FileSet fs) {
         resultsReports.add(fs);
     }
-    
-    /* For nested text:
-    private StringBuffer text;
-    public void addText(String raw) {
-        String s = getProject().replaceProperties(raw.trim());
-        if (text == null) {
-            text = new StringBuffer(s);
-        } else {
-            text.append(s);
-        }
-    }
-    // <customtask>
-    //     Some text...
-    // </customtask>
-     */
-    
-    /* Some sort of path (like classpath or similar):
-    private Path path;
-    public void setPath(Path p) {
-        if (path == null) {
-            path = p;
-        } else {
-            path.append(p);
-        }
-    }
-    public Path createPath () {
-        if (path == null) {
-            path = new Path(project);
-        }
-        return path.createPath();
-    }
-    public void setPathRef(Reference r) {
-        createPath().setRefid(r);
-    }
-    // <customtask path="foo:bar"/>
-    // <customtask>
-    //     <path>
-    //         <pathelement location="foo"/>
-    //     </path>
-    // </customtask>
-    // Etc.
-     */
-    
-    /* One of a fixed set of choices:
-    public static class FooBieBletch extends EnumeratedAttribute {
-        public String[] getValues() {
-            return new String[] {"foo", "bie", "bletch"};
-        }
-    }
-    private String mode = "foo";
-    public void setMode(FooBieBletch m) {
-        mode = m.getValue();
-    }
-    // <customtask mode="bletch"/>
-     */
     
     public void execute() throws BuildException {
         // TODO code here what the task actually does:
@@ -152,60 +87,37 @@ public class AnalyzeResultsTask extends Task {
         // log("Some message");
         // log("Serious message", Project.MSG_WARN);
         log("AnalyzeResultsTask running", Project.MSG_VERBOSE);
-        
-        if (refFile == null || !refFile.exists() || !refFile.isFile()) {
-            throw new BuildException ("Missing reference file. refFile attribute must be set.");
-        }
-        if (destFile == null) {
-            throw new BuildException ("Missing destination file. distFile attribute must be set.");
-        }
+
+        checkArgs();
         OutputStreamWriter ow = null;
         try {
-            log("Processing "+refFile, Project.MSG_VERBOSE);
-            ResultsHandler handler = new ResultsHandler ();
+            log("Processing reference results from "+refFile, Project.MSG_VERBOSE);
+            ResultsHandler handler = new ResultsHandler();
             SAXParser p = SAXParserFactory.newInstance().newSAXParser();
             p.parse(refFile, handler);
             
-            Map refCases = handler.getCases();
+            Map<TestResult,Collection<Integer>> refCases = handler.getCases();
             
-                ow = new OutputStreamWriter(new FileOutputStream(destFile));
-                ow.write("<testresults>\n<referenceresults>\n");
-                printCases (ow, refCases.keySet().iterator(), null);
-                ow.write("</referenceresults>\n");
-                
-	            Iterator it = resultsReports.iterator();
-	            while (it.hasNext()) {
-	                FileSet fs = (FileSet)it.next();
-	                DirectoryScanner ds = fs.getDirectoryScanner(project);
-	                File basedir = ds.getBasedir();
-	                String[] files = ds.getIncludedFiles();
-	                for (int i = 0; i<files.length; i++) {
-	                    log("Processing "+files[i], Project.MSG_VERBOSE);
-	                    ResultsHandler handler2 = new ResultsHandler ();
-	                    p.parse(new File(basedir, files[i]), handler2);
-	                    
-	                    Map newCases = handler2.getCases();
-	                    Iterator it2 = newCases.keySet().iterator();
-	                    while (it2.hasNext()) {
-	                        TestCaseResults res = (TestCaseResults)it2.next();
-	                        Object o = refCases.get(res);
-	                        if (o != null) {
-	                            TestCaseResults refResult = (TestCaseResults)o;
-	                            if (refResult.getCount() <= 2 || res.getCount() <= 2) {
-	                                log("ttest skipped for "+refResult.getName()+" "+refResult.getOrder(), Project.MSG_VERBOSE);
-	                                continue;
-	                            }
-	                            log("Compare "+refResult.getName()+" "+refResult.getOrder(), Project.MSG_VERBOSE);
-	                            log("\t"+refResult.getCount()+" against "+res.getCount()+" values", Project.MSG_VERBOSE);
-	                            TestCaseResults.TTestValue tt = TestCaseResults.ttest(res, refResult);
-	                            res.setTTest(tt);
-	                        }
-	                    }
-		            printCases (ow, newCases.keySet().iterator(), refCases);
-	                }
-	                // process them...
-	            }
-                ow.write("</testresults>\n");
+            for (FileSet fs: resultsReports) {
+                DirectoryScanner ds = fs.getDirectoryScanner(project);
+                File basedir = ds.getBasedir();
+                String[] files = ds.getIncludedFiles();
+                for (int i = 0; i<files.length; i++) {
+                    log("Processing "+files[i], Project.MSG_VERBOSE);
+                    ResultsHandler handler2 = new ResultsHandler();
+                    File srcFile = new File(basedir, files[i]);
+                    File destFile = new File( srcFile.getParentFile(), "testreport-perf-comparision.xml");
+                    p.parse(srcFile, handler2);
+                    
+                    Map<TestResult,Collection<Integer>> newCases = handler2.getCases();
+//                    printTTest(refCases, newCases);
+                    ow = new OutputStreamWriter(new FileOutputStream(destFile));
+                    ow.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<testresults>\n");
+                    printCases(ow, newCases, refCases);
+                    ow.write("</testresults>\n");
+                }
+                // process them...
+            }
             
         }
         catch (ParserConfigurationException ex) {
@@ -224,11 +136,6 @@ public class AnalyzeResultsTask extends Task {
             try { if (ow != null) ow.close(); } catch (IOException ioe) {}
         }
         
-        // To signal an error:
-        // throw new BuildException("Problem", location);
-        // throw new BuildException(someThrowable, location);
-        // throw new BuildException("Problem", someThrowable, location);
-        
         // You can call other tasks too:
         // Zip zip = (Zip)project.createTask("zip");
         // zip.setZipfile(zipFile);
@@ -239,32 +146,55 @@ public class AnalyzeResultsTask extends Task {
         // zip.setLocation(location);
         // zip.execute();
     }
+
+    private void printTTest(final Map refCases, final Map newCases) {
+        Iterator it2 = newCases.keySet().iterator();
+        while (it2.hasNext()) {
+            TestCaseResults res = (TestCaseResults)it2.next();
+            Object o = refCases.get(res);
+            if (o != null) {
+                TestCaseResults refResult = (TestCaseResults)o;
+                if (refResult.getCount() <= 2 || res.getCount() <= 2) {
+                    log("ttest skipped for "+refResult.getName()+" "+refResult.getOrder(), Project.MSG_VERBOSE);
+                    continue;
+                }
+                log("Compare "+refResult.getName()+" "+refResult.getOrder(), Project.MSG_VERBOSE);
+                log("\t"+refResult.getCount()+" against "+res.getCount()+" values", Project.MSG_VERBOSE);
+                TestCaseResults.TTestValue tt = TestCaseResults.ttest(res, refResult);
+                res.setTTest(tt);
+            }
+        }
+    }
     
     /** Outputs the results of all cases in passed iterator.
      * When refCases is supplied it also prints the difference and result of ttest.
      */
-    private void printCases (OutputStreamWriter ow, Iterator it, Map refCases) throws IOException {
+    private void printCases (OutputStreamWriter ow,
+            Map<TestResult, Collection<Integer>> actual,
+            Map<TestResult, Collection<Integer>> reference) throws IOException {
         NumberFormat f = new DecimalFormat ("#######0.00");
         NumberFormat f2 = new DecimalFormat ("#######0.0000");
-        while (it.hasNext()) {
-            TestCaseResults oneCase = (TestCaseResults)it.next();
+        Set<TestResult> keys = actual.keySet();
+        for (TestResult oneCase : keys) {
+            Collection<Integer> values = actual.get(oneCase);
+            TestResult.Statistics stat = TestResult.computeStatistics(values);
             ow.write("<testcase");
             ow.write(" name=\""+oneCase.getName()+"\"\n");
             ow.write(" threshold=\""+oneCase.getThreshold()+"\"");
             ow.write(" unit=\""+oneCase.getUnit()+"\"");
             ow.write(" order=\""+oneCase.getOrder()+"\"\n");
-            ow.write(" average=\""+f.format(oneCase.getAverage())+"\"");
-            ow.write(" stddev=\""+f.format(oneCase.getStdDev())+"\"");
-            ow.write(" variance=\""+f.format(oneCase.getVariance())+"\">\n");
-            Iterator it2 = oneCase.getValues().iterator();
-            while (it2.hasNext()) {
-                ow.write("\t<result value=\""+it2.next().toString()+"\"/>\n");
+            ow.write(" average=\""+f.format(stat.getAverage())+"\"");
+            ow.write(" stddev=\""+f.format(stat.getStdDev())+"\"");
+            ow.write(" variance=\""+f.format(stat.getVariance())+"\">\n");
+            for (int val : values) {
+                ow.write("\t<result value=\""+val+"\"/>\n");
             }
-            if (refCases != null) {
-                Object o = refCases.get(oneCase);
-                if (o != null) {
-                    TestCaseResults refResult = (TestCaseResults)o;
-                    ow.write("<difference value=\""+f.format(oneCase.getAverage()/refResult.getAverage()*100-100)+"\"/>\n");
+            if (reference != null) {
+                Collection<Integer> refValues = reference.get(oneCase);
+                if (refValues != null) {
+                    ow.write("<difference value=\""+
+                            f.format(stat.getAverage()/TestResult.computeStatistics(refValues).getAverage()*100-100)+
+                            "\"/>\n");
                     
 		    /*
             	TestCaseResults.TTestValue tt = oneCase.getTTest();
@@ -279,10 +209,45 @@ public class AnalyzeResultsTask extends Task {
             ow.write("</testcase>\n");
         }
     }
+
+    private void checkArgs() throws BuildException {
+        if (refFile == null && resultsReports.isEmpty()) {
+            // try to find last performance results and use them as a reference
+            // look for one of results/testrun_??????-??????/xmlresults/testrun-performance.xml
+            File dir = new File (getProject().getBaseDir(), "results");
+            if (dir.isDirectory()) {
+                File [] testrunDirs = dir.listFiles(new FilenameFilter() {
+                    public boolean accept(File dir, String name) {
+                         return name.startsWith("testrun_") && name.length() == 21;
+                    }
+                });
+                Arrays.sort(testrunDirs);
+                if (testrunDirs.length > 0) {
+                    refFile = new File(testrunDirs[testrunDirs.length-1], "xmlresults"+File.separator+"testrun-performance.xml");
+                    log("Using default reference file "+refFile, Project.MSG_VERBOSE);
+                }
+                for (int i = 0; i < testrunDirs.length - 1; i++) {
+                    File runFile = new File(testrunDirs[i], "xmlresults"+File.separator+"testrun-performance.xml");
+                    log("Adding files for comparision: "+runFile, Project.MSG_VERBOSE);
+                    FileSet fs = new FileSet();
+                    fs.setFile(runFile);
+                    addFileset(fs);
+                }
+            }
+            // find results to compare with
+
+        }
+        if (refFile == null || !refFile.exists() || !refFile.isFile()) {
+            throw new BuildException ("Missing reference file. refFile attribute must be set.");
+        }
+//        if (destFile == null) {
+//            throw new BuildException ("Missing destination file. destFile attribute must be set.");
+//        }
+    }
     
     private class ResultsHandler extends DefaultHandler {
         
-        private Map cases;
+        private Map<TestResult, Collection<Integer>> cases;
         
         private String currSuite;
         
@@ -290,7 +255,7 @@ public class AnalyzeResultsTask extends Task {
             cases = new TreeMap (); // TestCaseResults
         }
         
-        public Map getCases () {
+        public Map<TestResult, Collection<Integer>> getCases () {
             return cases;
         }
         
@@ -308,13 +273,13 @@ public class AnalyzeResultsTask extends Task {
                     String unit = atts.getValue("unit");
                     int value = Integer.parseInt(atts.getValue("value"));
                     
-                    TestCaseResults oneCase = new TestCaseResults (name, threshold, unit, order, currSuite);
-                    TestCaseResults theCase = (TestCaseResults)cases.get(oneCase);
-                    if (theCase == null) {
-                        cases.put(oneCase, oneCase);
-                        theCase = oneCase;
+                    TestResult oneCase = new TestResult (name, threshold, unit, order, currSuite);
+                    Collection<Integer> values = cases.get(oneCase);
+                    if (values == null) {
+                        values = new ArrayList ();
+                        cases.put(oneCase, values);
                     }
-                    theCase.addValue(value);
+                    values.add(value);
                 }
                 catch (NumberFormatException e) {
                     
