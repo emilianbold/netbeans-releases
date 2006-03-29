2ad34fdf374673ba73b0b59e527317e0c6c5efd4
dprusa <dprusa@netbeans.org>
2006-03-29 12:40:03 +0000

    #71987: compare names instead of displaynames

--- options.editor/src/org/netbeans/modules/options/colors/CategoryComparator.java
+++ options.editor/src/org/netbeans/modules/options/colors/CategoryComparator.java
@@ -15,7 +15,7 @@ package org.netbeans.modules.options.colors;
 
 import java.util.Comparator;
 import javax.swing.text.AttributeSet;
-import org.netbeans.api.editor.settings.EditorStyleConstants;
+import javax.swing.text.StyleConstants;
 
 
 /**
@@ -23,16 +23,19 @@ import org.netbeans.api.editor.settings.EditorStyleConstants;
  * @author Jan Jancura
  */
 public class CategoryComparator implements Comparator {
+    
     public int compare (Object o1, Object o2) {
-	if (name (o1).startsWith ("Default")) 
-	    return name (o2).startsWith ("Default") ? 0 : -1;
-        if (name (o2).startsWith ("Default"))
+        String name_1 = name(o1);
+        String name_2 = name(o2);
+	if (name_1.startsWith ("default")) // NOI18N
+	    return name_2.startsWith ("default") ? 0 : -1; // NOI18N
+        if (name_2.startsWith ("default")) // NOI18N
             return 1;
-	return name (o1).compareTo (name (o2));
+	return name_1.compareTo (name_2);
     }
     
     private static String name (Object o) {
-        return (String) ((AttributeSet) o).getAttribute 
-            (EditorStyleConstants.DisplayName);
+        return (String) ((AttributeSet) o).getAttribute(StyleConstants.NameAttribute);
     }
+    
 }
