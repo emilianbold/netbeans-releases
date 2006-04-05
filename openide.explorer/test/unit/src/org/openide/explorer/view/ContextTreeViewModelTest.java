5b28a8e209c8223d2741f9e7ea88a7784635df8d
jtulach <jtulach@netbeans.org>
2006-04-05 11:06:55 +0000

    Much more logging

--- openide.explorer/test/unit/src/org/openide/explorer/view/ContextTreeViewModelTest.java
+++ openide.explorer/test/unit/src/org/openide/explorer/view/ContextTreeViewModelTest.java
@@ -13,6 +13,8 @@
 
 package org.openide.explorer.view;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JPanel;
 import javax.swing.event.TreeModelEvent;
 import javax.swing.event.TreeModelListener;
@@ -29,6 +31,9 @@ import org.openide.nodes.Node;
 public class ContextTreeViewModelTest extends NbTestCase {
     
     private static final int NO_OF_NODES = 3;
+
+
+    private Logger LOG = Logger.getLogger("TEST-" + getName());
     
     public ContextTreeViewModelTest(String name) {
         super(name);
@@ -37,105 +42,178 @@ public class ContextTreeViewModelTest extends NbTestCase {
     protected boolean runInEQ() {
         return true;
     }
+
+    protected Level logLevel() {
+        return Level.ALL;
+    }
     
     public void testCheckThatTheModelFiresChangesAboutOnlyVisibleNodes() throws Throwable {
         final AbstractNode root = new AbstractNode(new Children.Array());
         root.setName("test root");
+
+        LOG.info("node created");
         
         root.getChildren().add(new Node[] {
             createNode("kuk", true),
             createNode("huk", true),
         });
+
+        LOG.info("two subnodes added");
         
         
         NodeTreeModel m = new ContextTreeView.NodeContextModel();
+
+        LOG.info("model created");
+
         m.setNode(root);
+
+        LOG.info("its root node set");
+
         TreeNode visual = (TreeNode)m.getRoot();
+
+        LOG.info("getting the root: " + visual);
+        
         waitEQ();
         
         
         assertEquals("Leaf nodes are not counted", 0, m.getChildCount(visual));
-        
+
+
         Listener listener = new Listener();
         m.addTreeModelListener(listener);
-        
+
+        LOG.info("listener added");
+
         Node n = createNode("blik", true);
+
+        LOG.info("node created");
         
         root.getChildren().add(new Node[] { n });
+
+        LOG.info("node added");
+
         assertEquals("Leaf nodes are not counted even when added", 0, m.getChildCount(visual));
         assertEquals("Really added", n.getParentNode(), root);
         listener.assertEvents("No events", 0);
-        
+
+        LOG.info("events ok");
+
         root.getChildren().remove(new Node[] { n });
         listener.assertEvents("Still no events", 0);
         assertNull("Removed", n.getParentNode());
+
+        LOG.info("node removed");
         
         Node nonLeaf = createNode("nonleaf", false);
         root.getChildren().add(new Node[] { nonLeaf });
+
+        LOG.info("non-leaf added");
+
         assertEquals("One child is there", 1, m.getChildCount(visual));
         listener.assertEvents("This node is visible there", 1);
         listener.assertIndexes("Added at position zero", new int[] { 0 });
         assertEquals("Really added", nonLeaf.getParentNode(), root);
+
+        LOG.info("all checked");
         
         root.getChildren().remove(new Node[] { nonLeaf });
+
+        LOG.info("non-leaf removed");
+
         assertEquals("One child is away", 0, m.getChildCount(visual));
         assertNull("Removed", nonLeaf.getParentNode());
         listener.assertEvents("And it has been removed", 1);
         listener.assertIndexes("Removed from position zero", new int[] { 0 });
-        
+
+        LOG.info("all ok");
     }
     
     public void testABitMoreComplexAddAndRemoveEventCheck() throws Throwable {
         final AbstractNode root = new AbstractNode(new Children.Array());
         root.setName("test root");
-        
+
+        LOG.info("root created");
+
         root.getChildren().add(new Node[] {
             createNode("kuk", false),
             createNode("huk", false),
         });
-        
+
+
+        LOG.info("two nodes added");
         
         NodeTreeModel m = new ContextTreeView.NodeContextModel();
         m.setNode(root);
+
+        LOG.info("root set");
+
         TreeNode visual = (TreeNode)m.getRoot();
         waitEQ();
+
+        LOG.info("visual is here: " + visual);
         
         
         assertEquals("Initial size is two", 2, m.getChildCount(visual));
         
         Listener listener = new Listener();
         m.addTreeModelListener(listener);
+
+        LOG.info("listener added");
         
         Node[] arr = {
             createNode("add1", false), createNode("add2", false)
         };
-        
+
+        LOG.info("adding children");
         root.getChildren().add(arr);
+
+        LOG.info("children added");
+
         listener.assertEvents("One addition", 1);
         listener.assertIndexes("after the two first", new int[] { 2, 3 });
-        
+
+        LOG.info("removing children");
         root.getChildren().remove(arr);
+        LOG.info("children removed");
+
         listener.assertEvents("One removal", 1);
         listener.assertIndexes("from end", new int[] { 2, 3 });
-        
+
+        LOG.info("all well");
     }
     
     public void testRemoveInMiddle() throws Throwable {
         final AbstractNode root = new AbstractNode(new Children.Array());
         root.setName("test root");
+
+
+        LOG.info("root added");
+
         root.getChildren().add(new Node[] { createNode("Ahoj", false) });
         
+        LOG.info("a node added");
+
         Node[] first = {
             createNode("kuk", false),
             createNode("huk", false),
         };
+
+
+        LOG.info("adding more nodes");
         
         root.getChildren().add(first);
+
+        LOG.info("nodes added");
         
         
         NodeTreeModel m = new ContextTreeView.NodeContextModel();
         m.setNode(root);
+
+        LOG.info("root set");
         TreeNode visual = (TreeNode)m.getRoot();
+
+        LOG.info("visual is here: " + visual);
+
         waitEQ();
         
         
@@ -143,19 +221,32 @@ public class ContextTreeViewModelTest extends NbTestCase {
         
         Listener listener = new Listener();
         m.addTreeModelListener(listener);
+
+
+        LOG.info("listener added");
         
         Node[] arr = {
             createNode("add1", false), createNode("add2", false)
         };
         
         root.getChildren().add(arr);
+
+        LOG.info("arr added");
+
         listener.assertEvents("One addition", 1);
         listener.assertIndexes("after the three first", new int[] { 3, 4 });
-        
+
+        LOG.info("arr ok");
+
         root.getChildren().remove(first);
+
+
+        LOG.info("arr removed");
+
         listener.assertEvents("One removal", 1);
         listener.assertIndexes("from end", new int[] { 1, 2 });
-        
+
+        LOG.info("all ok");
     }
     
     private static Node createNode(String name, boolean leaf) {
