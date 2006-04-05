8cd1ecd2767d64b97e172a2587a1ce3801bef06a
jtulach <jtulach@netbeans.org>
2006-04-05 14:50:28 +0000

    This test needs to use Log.enable as well as it did on its original place

--- openide.util/test/unit/src/org/openide/util/actions/AsynchronousTest.java
+++ openide.util/test/unit/src/org/openide/util/actions/AsynchronousTest.java
@@ -20,9 +20,11 @@ import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
 import java.awt.image.PixelGrabber;
 import java.util.Date;
+import java.util.logging.Level;
 import javax.swing.Icon;
 import javax.swing.JButton;
 import junit.textui.TestRunner;
+import org.netbeans.junit.Log;
 import org.netbeans.junit.NbTestCase;
 import org.openide.ErrorManager;
 import org.openide.util.HelpCtx;
@@ -36,19 +38,19 @@ import org.openide.util.lookup.InstanceContent;
  * @author Jesse Glick
  */
 public class AsynchronousTest extends NbTestCase {
+
+    private CharSequence err;
     
     public AsynchronousTest(String name) {
         super(name);
     }
     
-    protected void setUp() {
-        System.setProperty("org.openide.util.Lookup", "org.openide.util.actions.AsynchronousTest$Lkp");
-        assertNotNull("ErrManager has to be in lookup", Lookup.getDefault().lookup(ErrManager.class));
-        ErrManager.messages.delete(0, ErrManager.messages.length());
-    }
-    
     protected boolean runInEQ() {
         return true;
+    }
+
+    protected void setUp() {
+        err = Log.enable("", Level.ALL);
     }
     
     public void testExecutionOfActionsThatDoesNotOverrideAsynchronousIsAsynchronousButWarningIsPrinted() throws Exception {
@@ -62,8 +64,8 @@ public class AsynchronousTest extends NbTestCase {
             assertTrue("The asynchronous action is finished", action.finished);
         }
         
-        if (ErrManager.messages.toString().indexOf(DoesNotOverride.class.getName() + " should override") < 0) {
-            fail("There should be warning about not overriding asynchronous: " + ErrManager.messages);
+        if (err.toString().indexOf(DoesNotOverride.class.getName() + " should override") < 0) {
+            fail("There should be warning about not overriding asynchronous: " + err);
         }
     }
     
@@ -78,8 +80,8 @@ public class AsynchronousTest extends NbTestCase {
             assertTrue("The asynchronous action is finished", action.finished);
         }
         
-        if (ErrManager.messages.toString().indexOf(DoesOverrideAndReturnsTrue.class.getName()) >= 0) {
-            fail("No warning about the class: " + ErrManager.messages);
+        if (err.toString().indexOf(DoesOverrideAndReturnsTrue.class.getName()) >= 0) {
+            fail("No warning about the class: " + err);
         }
     }
     
@@ -91,8 +93,8 @@ public class AsynchronousTest extends NbTestCase {
             assertTrue("The synchronous action is finished immediatelly", action.finished);
         }
         
-        if (ErrManager.messages.toString().indexOf(DoesOverrideAndReturnsTrue.class.getName()) >= 0) {
-            fail("No warning about the class: " + ErrManager.messages);
+        if (err.toString().indexOf(DoesOverrideAndReturnsTrue.class.getName()) >= 0) {
+            fail("No warning about the class: " + err);
         }
     }
     
@@ -104,8 +106,8 @@ public class AsynchronousTest extends NbTestCase {
             assertTrue("When asked for synchronous the action is finished immediatelly", action.finished);
         }
         
-        if (ErrManager.messages.toString().indexOf(DoesOverrideAndReturnsTrue.class.getName()) >= 0) {
-            fail("No warning about the class: " + ErrManager.messages);
+        if (err.toString().indexOf(DoesOverrideAndReturnsTrue.class.getName()) >= 0) {
+            fail("No warning about the class: " + err);
         }
     }
     
@@ -137,46 +139,5 @@ public class AsynchronousTest extends NbTestCase {
         public boolean asynchronous() {
             return false;
         }
-    }
-    
-    
-    public static final class Lkp extends AbstractLookup {
-        public Lkp() {
-            this(new InstanceContent());
-        }
-        
-        private Lkp(InstanceContent ic) {
-            super(ic);
-            ic.add(new ErrManager());
-        }
-    }
-    
-    private static final class ErrManager extends ErrorManager {
-        public static final StringBuffer messages = new StringBuffer();
-        
-        public Throwable annotate(Throwable t, int severity, String message, String localizedMessage, Throwable stackTrace, Date date) {
-            return t;
-        }
-        
-        public Throwable attachAnnotations(Throwable t, ErrorManager.Annotation[] arr) {
-            return t;
-        }
-        
-        public ErrorManager.Annotation[] findAnnotations(Throwable t) {
-            return null;
-        }
-        
-        public ErrorManager getInstance(String name) {
-            return this;
-        }
-        
-        public void log(int severity, String s) {
-            messages.append(s);
-            messages.append('\n');
-        }
-        
-        public void notify(int severity, Throwable t) {
-        }
-        
     }
 }
