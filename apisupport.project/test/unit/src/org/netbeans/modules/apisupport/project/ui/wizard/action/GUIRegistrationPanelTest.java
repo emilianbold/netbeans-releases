42c9a79c92c670df1ad23f5397c5effad60342ad
mkrauskopf <mkrauskopf@netbeans.org>
2006-04-05 19:20:11 +0000

    #73852: GUIRegistrationPanelTest.testMemoryLeak_70032 failure s/JLabel/JTextField to workaround the JTextComponent.focusedComponent also moving all realization code to the EDT

--- apisupport.project/test/unit/src/org/netbeans/modules/apisupport/project/ui/wizard/action/GUIRegistrationPanelTest.java
+++ apisupport.project/test/unit/src/org/netbeans/modules/apisupport/project/ui/wizard/action/GUIRegistrationPanelTest.java
@@ -14,12 +14,13 @@
 package org.netbeans.modules.apisupport.project.ui.wizard.action;
 
 import java.awt.BorderLayout;
-import java.awt.Component;
+import java.awt.EventQueue;
 import java.lang.ref.WeakReference;
+import java.lang.reflect.InvocationTargetException;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
+import javax.swing.JTextField;
 import org.netbeans.modules.apisupport.project.TestBase;
 import org.netbeans.modules.apisupport.project.layers.LayerTestBase;
 import org.netbeans.modules.project.uiapi.ProjectChooserFactory;
@@ -30,62 +31,76 @@ import org.openide.WizardDescriptor;
  */
 public class GUIRegistrationPanelTest extends LayerTestBase {
     
+    private JFrame frame;
+    private JPanel outerPane;
+    private GUIRegistrationPanel regPane;
+    private WeakReference regPaneWR;
+    private boolean valid;
+    
     public GUIRegistrationPanelTest(String testName) {
         super(testName);
     }
     
     public void testMemoryLeak_70032() throws Exception {
         TestBase.initializeBuildProperties(getWorkDir());
-        WizardDescriptor wd = new WizardDescriptor(new WizardDescriptor.Panel[] {});
+        final WizardDescriptor wd = new WizardDescriptor(new WizardDescriptor.Panel[] {});
         wd.putProperty(ProjectChooserFactory.WIZARD_KEY_PROJECT,
                 TestBase.generateStandaloneModule(getWorkDir(), "module"));
-        GUIRegistrationPanel regPane = new GUIRegistrationPanel(wd, new DataModel(wd));
-        // let's force checkValidity failed until all data are loaded
-        regPane.editorContext.setSelected(true);
-        regPane.fileTypeContext.setSelected(true);
-        regPane.globalMenuItem.setSelected(true);
-        regPane.globalToolbarButton.setSelected(true);
-        
-        // workaround for inability to GC JFrame/JDialog itself
-        JPanel outerPane = new JPanel();
-        outerPane.add(regPane);
-        
-        final WeakReference compWr = new WeakReference(regPane);
-        final WeakReference outerWr = new WeakReference(outerPane);
-        
-        final JFrame frame = new JFrame("testMemoryLeak_70032");
-        frame.getContentPane().setLayout(new BorderLayout());
-        frame.getContentPane().add(new JLabel("<html><font color='blue'>I will close myself" +
-                " as soon as I load all data from the SystemFileSystem</font></html>"), BorderLayout.NORTH);
-        frame.getContentPane().add(outerPane, BorderLayout.CENTER);
-        SwingUtilities.invokeAndWait(new Runnable() {
+        EventQueue.invokeAndWait(new Runnable() {
             public void run() {
+                regPane = new GUIRegistrationPanel(wd, new DataModel(wd));
+                // let's force checkValidity failed until all data are loaded
+                regPane.editorContext.setSelected(true);
+                regPane.fileTypeContext.setSelected(true);
+                regPane.globalMenuItem.setSelected(true);
+                regPane.globalToolbarButton.setSelected(true);
+                
+                // workaround for inability to GC JFrame/JDialog itself
+                outerPane = new JPanel();
+                outerPane.add(regPane);
+                
+                regPaneWR = new WeakReference(regPane);
+                
+                frame = new JFrame("testMemoryLeak_70032");
+                frame.getContentPane().setLayout(new BorderLayout());
+                frame.getContentPane().add(new JLabel("<html><font color='blue'>I will close my self" +
+                        " as soos as I load all data from the SystemFileSystem</font></html>"), BorderLayout.NORTH);
+                frame.getContentPane().add(outerPane, BorderLayout.CENTER);
                 frame.pack();
                 // NOTE: we have to show the frame to take combobox renderers into the game
                 frame.setVisible(true);
             }
         });
-        while (!regPane.checkValidity()) {
+        while (!isRegPaneValid()) {
             Thread.sleep(200);
         }
-        SwingUtilities.invokeAndWait(new Runnable() {
+        EventQueue.invokeAndWait(new Runnable() {
             public void run() {
-                frame.getContentPane().remove((Component) outerWr.get());
+                frame.getContentPane().remove(outerPane);
                 
-                // prevents KeyboardFocusManager.permanentFocusOwner to hold us
-                JLabel dummyFocusEater = new JLabel();
+                // prevents KeyboardFocusManager.permanentFocusOwner and
+                // JTextComponent.focusedComponent to hold us
+                JTextField dummyFocusEater = new JTextField();
                 frame.getContentPane().add(dummyFocusEater);
                 dummyFocusEater.requestFocus();
-                
                 frame.setVisible(false);
                 frame.dispose();
             }
         });
+        
         outerPane = null;
         regPane = null;
         
-        assertGC("GCing comp", compWr);
-        assertNull("sanity check", compWr.get());
+        assertGC("GCing comp", regPaneWR);
+    }
+    
+    private boolean isRegPaneValid() throws InterruptedException, InvocationTargetException {
+        EventQueue.invokeAndWait(new Runnable() {
+            public void run() {
+                valid = regPane.checkValidity();
+            }
+        });
+        return valid;
     }
     
 }
