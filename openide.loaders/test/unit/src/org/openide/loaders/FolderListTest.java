fc7a7292c9cd39415d2120abb4518f63753fe865
jtulach <jtulach@netbeans.org>
2006-04-05 11:42:46 +0000

    #72678: Give SoftReferences a chance to survive

--- openide.loaders/test/unit/src/org/openide/loaders/FolderListTest.java
+++ openide.loaders/test/unit/src/org/openide/loaders/FolderListTest.java
@@ -84,23 +84,10 @@ public class FolderListTest extends NbTestCase {
     }
 
     public void testComputeChildrenListKeepsTheObject() throws Exception {
-        
-        DataObject[] objArr = list.getChildren();
-        FileObject[] arr = new FileObject[objArr.length];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = objArr[i].getPrimaryFile();
-        }
-        for (int i = 0; i < objArr.length; i++) {
-            WeakReference r = new WeakReference(objArr[i]);
-            objArr[i] = null;
-            assertGC(i + "-th can GC", r);
-        }
-        
-        
         class L implements FolderListListener {
             private HashSet hashes = new HashSet();
             private boolean check;
-            
+
             public void process(DataObject obj, List arr) {
                 if (!check) {
                     hashes.add(new Integer(obj.hashCode()));
@@ -108,32 +95,57 @@ public class FolderListTest extends NbTestCase {
                     Integer i = new Integer(obj.hashCode());
                     hashes.remove(i);
                 }
-                
+
             }
 
             public void finished(List arr) {
                 check = true;
             }
         }
-        
-        L listener = new L();       
-        RequestProcessor.Task t = list.computeChildrenList(listener);
-        t.waitFinished();
 
-        // if GC kicks in here, the objects can go away
-        for (int i = 0; i < 10; i++) {
-            System.gc();
+        L listener = null;
+
+
+        for(int j = 0; j < 10; j++) {
+            DataObject[] objArr = list.getChildren();
+            FileObject[] arr = new FileObject[objArr.length];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = objArr[i].getPrimaryFile();
+            }
+            for (int i = 0; i < objArr.length; i++) {
+                WeakReference r = new WeakReference(objArr[i]);
+                objArr[i] = null;
+                assertGC(i + "-th can GC", r);
+            }
+
+            listener = new L();
+            RequestProcessor.Task t = list.computeChildrenList(listener);
+            t.waitFinished();
+
+            // if GC kicks in here, the objects can go away
+            for (int i = 0; i < 10; i++) {
+                System.gc();
+            }
+
+
+            t = list.computeChildrenList(listener);
+            t.waitFinished();
+
+
+            if (listener.hashes.isEmpty()) {
+                break;
+            }
+
+            // make some room so soft references have a place to stay
+            byte[] tmp = new byte[10000 * j];
+            tmp = null;
         }
-        
-        
-        t = list.computeChildrenList(listener);
-        t.waitFinished();
-        
-        
+
+
+
         if (!listener.hashes.isEmpty()) {
             fail("There is new data object created, which is not in " + listener.hashes);
         }
-        
         
         assertEquals("Was checking", true, listener.check);
     }
@@ -158,18 +170,30 @@ public class FolderListTest extends NbTestCase {
             }
         }
         
-        L listener = new L();       
-        RequestProcessor.Task t = list.computeChildrenList(listener);
-        t.waitFinished();
+        L listener = null;
 
-        // if GC kicks in here, the objects can go away
-        for (int i = 0; i < 10; i++) {
-            System.gc();
+        for(int i = 0; i < 10; i++) {
+            listener = new L();
+            RequestProcessor.Task t = list.computeChildrenList(listener);
+            t.waitFinished();
+
+            // if GC kicks in here, the objects can go away
+            for (int j = 0; j < 10; j++) {
+                System.gc();
+            }
+
+
+            t = list.computeChildrenList(listener);
+            t.waitFinished();
+
+            if (listener.hashes.isEmpty()) {
+                break;
+            }
+
+            // make some room so soft references have a place to stay
+            byte[] tmp = new byte[10000 * i];
+            tmp = null;
         }
-        
-        
-        t = list.computeChildrenList(listener);
-        t.waitFinished();
         
         
         if (!listener.hashes.isEmpty()) {
