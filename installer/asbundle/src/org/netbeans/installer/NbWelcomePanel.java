d05487dda0b03d479b845eee6e6fc7e339374bce
mslama <mslama@netbeans.org>
2006-04-21 09:21:11 +0000

    #75018: Add check of JDK version at installer start.

--- installer/asbundle/src/org/netbeans/installer/NbWelcomePanel.java
+++ installer/asbundle/src/org/netbeans/installer/NbWelcomePanel.java
@@ -16,9 +16,14 @@ package org.netbeans.installer;
 import com.installshield.wizard.WizardBeanEvent;
 import com.installshield.wizardx.panels.TextDisplayPanel;
 import com.installshield.util.Log;
+import com.installshield.wizard.swing.SwingWizardUI;
+
+import java.awt.Component;
 
 public class NbWelcomePanel extends TextDisplayPanel {
-
+    
+    private static final String BUNDLE = "$L(org.netbeans.installer.Bundle,";
+    
     public NbWelcomePanel() {
         setTextSource(TEXT_PROPERTY);
         setContentType(HTML_CONTENT_TYPE);
@@ -27,10 +32,44 @@ public class NbWelcomePanel extends TextDisplayPanel {
     
     public boolean queryEnter(WizardBeanEvent evt) {
         boolean okay = super.queryEnter(evt);
-        setText(resolveString("$L(org.netbeans.installer.Bundle,InstallWelcomePanel.text,"
-        + "$L(org.netbeans.installer.Bundle,Product.displayName),"
-        + "$L(org.netbeans.installer.Bundle,AS.name))"));
+        if (isJDKVersionSupported()) {
+            setText(resolveString(BUNDLE + "InstallWelcomePanel.text,"
+            + BUNDLE + "Product.displayName),"
+            + BUNDLE + "AS.name))"));
+        } else {
+            if (Util.isMacOSX()) {
+                setText(resolveString(BUNDLE + "InstallWelcomePanel.error,"
+                + System.getProperty("java.version") + ","
+                + BUNDLE + "InstallWelcomePanel.jdkURLMacOSX))"));
+            } else {
+                setText(resolveString(BUNDLE + "InstallWelcomePanel.error,"
+                + System.getProperty("java.version") + ","
+                + BUNDLE + "InstallWelcomePanel.jdkURL))"));
+            }
+        }
         return okay;
+    }
+    
+    public boolean entered(WizardBeanEvent event) {
+        if (!isJDKVersionSupported()) {
+            if (event.getUserInterface() instanceof SwingWizardUI) {
+                SwingWizardUI ui = (SwingWizardUI) event.getUserInterface();
+                Component nextButton = ui.getNavigationController().next();
+                nextButton.setEnabled(false);
+                ui.getNavigationController().setCancelType(SwingWizardUI.NavigationController.CLOSE);
+            }
+        }
+        return true;
+    }
+    
+    private boolean isJDKVersionSupported () {
+        String javaSpecVer = System.getProperty("java.specification.version");
+        logEvent(this,Log.DBG,"java.specification.version: " + javaSpecVer);
+        if ("1.4".compareTo(javaSpecVer) >= 0) {
+            return false;
+        } else {
+            return true;
+        }
     }
     
 }
