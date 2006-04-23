83eae05a4fd85f8e40f4d9a3c2ec867f2e96d0ce
tstupka <tstupka@netbeans.org>
2006-04-23 23:39:34 +0000

    javadoc

--- subversion/src/org/netbeans/modules/subversion/config/SvnConfigFiles.java
+++ subversion/src/org/netbeans/modules/subversion/config/SvnConfigFiles.java
@@ -31,29 +31,43 @@ import org.openide.util.Utilities;
 
 /**
  *
- * XXX test for robustness
+ * Handles the Subversions <b>servers</b> and <b>config</b> configuration files.</br>
+ * Everytime the singleton instance is created are the values from the commandline clients
+ * configuration directory (and registry folder in case of windows) merged into the Subversion 
+ * modules configuration files. Already present proxy setting values wan't be changed, 
+ * the remaining values are always taken from the commandline clients configuration files. 
+ * The only exception is the 'store-auth-creds'key, which is always set to 'no'.
+ * 
  * @author Tomas Stupka
  */
 public class SvnConfigFiles {
     
+    /** the only SvnConfigFiles instance */
     private static SvnConfigFiles instance;
-
+    /** the Ini instance holding the configuration values stored in the <b>servers</b> 
+     * file used by the Subversion module */
     private Ini servers = null;
 
     private static final String UNIX_CONFIG_DIR = ".subversion";
     private static final String WINDOWS_CONFIG_DIR = "Application Data/Subversion/";
+    private static final String[] AUTH_FOLDERS = new String [] {"auth/svn.simple", "auth/svn.username", "auth/svn.username"};   
+    private static final String GROUPS = "groups";
 
-    private final String[] AUTH_FOLDERS = new String [] {"auth/svn.simple", "auth/svn.username", "auth/svn.username"};   
-
-    private String GROUPS = "groups";
-
+    /**
+     * Creates a new instance
+     */
     private SvnConfigFiles() {
         // copy config file        
-        copyToIDEConfigDir();        
+        copyConfigFileToIDEConfigDir();        
         // get the nb servers file merged with the systems servers files
         servers = loadNetbeansIniFile("servers"); 
     }
 
+    /**
+     * Returns a singleton instance.
+     *
+     * @return the SvnConfigFiles instance
+     */
     public static SvnConfigFiles getInstance() {
         if(instance==null) {
             instance = new SvnConfigFiles();                    
@@ -61,6 +75,16 @@ public class SvnConfigFiles {
         return instance;
     }
 
+    /**
+     * Returns a {@link org.netbeans.modules.subversion.config.ProxyDescriptor}.
+     * The ProxyDescritor is created from the proxy settings for the given 
+     * Hostname or IP address stored in the <b>servers</b> file used by the 
+     * Subversion module.
+     *
+     * @param host the host. May be the host name or an IP address
+     * @return a {@link org.netbeans.modules.subversion.config.ProxyDescriptor}
+     *
+     */
     public ProxyDescriptor getProxyDescriptor(String host) {
         Ini.Section group = getGroup(host);
         if(group==null) {
@@ -84,6 +108,14 @@ public class SvnConfigFiles {
         return new ProxyDescriptor(ProxyDescriptor.TYPE_HTTP, proxyHost, proxyPort, username, password);    
     }
 
+    /**
+     * Stores the proxy host, port, username and password from the given  
+     * {@link org.netbeans.modules.subversion.config.ProxyDescriptor} in the 
+     * <b>servers</b> file used by the Subversion module.  
+     *
+     * @param pd the {@link org.netbeans.modules.subversion.config.ProxyDescriptor}
+     * @param host the host
+     */
     public void setProxy(ProxyDescriptor pd, String host) {
 
         if(pd != null && pd.getHost() != null) {
@@ -122,6 +154,45 @@ public class SvnConfigFiles {
         }
     }
 
+    /**
+     * Returns the path for the Sunbversion configuration dicectory used 
+     * by the systems Subversion commandline client.
+     *
+     * @return the path
+     *
+     */ 
+    public static String getUserConfigPath() {
+        String path = System.getProperty("user.home") ;
+        if(Utilities.isUnix()) {
+            return path + "/" + UNIX_CONFIG_DIR;
+        } else if (Utilities.isWindows()){
+            return path + "/" + WINDOWS_CONFIG_DIR;
+        } else {
+            // XXX
+        }
+        return "";
+    }
+
+    /**
+     * Returns the path for the Sunbversion configuration dicectory used 
+     * by the Netbeans Subversion module.
+     *
+     * @return the path
+     *
+     */ 
+    public static String getNBConfigDir() {
+        String nbHome = System.getProperty("netbeans.user");       
+        return nbHome + "/config/svn/config/";
+    }
+    
+    /** 
+     * Adds a new group into the <b>servers</b> config file used by the Subversion module. </br>
+     * The result is a new section with a generated name and an new entry in the groups section 
+     * assotiating the new group with the given host.
+     *
+     * @param host the host name 
+     * @return the Ini.Section newly created
+     */
     private Ini.Section addGroup(String host) {
         Ini.Section groups = getGroups();
         int idx = 0;
@@ -136,6 +207,13 @@ public class SvnConfigFiles {
         return group;
     }
 
+    /**
+     * Removes a group from the <b>servers</b> config file used by the Subversion module. </br>
+     * The entry in the groups section will always be removed. The according section holding the proxy settings will 
+     * be removed as long there is no more host assotiated with the group.
+     * 
+     * @param host the host which has to removed from the groups section
+     */
     private void removeFromGroup(String host) {
         Ini.Section group = getGroup(host);
         if(group != null) {
@@ -159,6 +237,11 @@ public class SvnConfigFiles {
         }
     }
 
+    /**
+     * Retruns the groups section from <b>servers</b> config file used by the Subversion module. </br>
+     *
+     * @return the groups section
+     */ 
     private Ini.Section getGroups() {
         Ini.Section groups = (Ini.Section) servers.get(GROUPS);
         if(groups==null) {
@@ -167,6 +250,13 @@ public class SvnConfigFiles {
         return groups;
     }
 
+    /**
+     * Returns the section from the <b>servers</b> config file used by the Subversion module which 
+     * is holding the proxy settings for the given host
+     *
+     * @param host the host
+     * @return the section holding the proxy settings for the given host
+     */ 
     private Ini.Section getGroup(String host) {
         Ini.Section groups = getGroups();
         for (Iterator it = groups.keySet().iterator(); it.hasNext();) {
@@ -191,6 +281,13 @@ public class SvnConfigFiles {
         return null;
     }
 
+   /**
+     * Returns the section from the <b>servers</b> config file used by the Subversion module which 
+     * is holding the proxy settings from the given {@link org.netbeans.modules.subversion.config.ProxyDescriptor}
+     *
+     * @param pd the {@link org.netbeans.modules.subversion.config.ProxyDescriptor}
+     * @return the section holding the proxy settings for the given {@link org.netbeans.modules.subversion.config.ProxyDescriptor}
+     */ 
     private Ini.Section getGroup(ProxyDescriptor pd) {
         for (Iterator it = servers.values().iterator(); it.hasNext();) {
             Ini.Section group = (Ini.Section) it.next();
@@ -208,7 +305,14 @@ public class SvnConfigFiles {
         return null;
     }
     
-    // XXX test me
+    /**
+     * Evaluates if the given hostaname or IP address is in the given value String.
+     *
+     * @param value the value String. A list of host names or IP addresses delimited by ",". 
+     *                          (e.g 192.168.0.1,*.168.0.1, some.domain.com, *.anything.com, ...)
+     * @param host the hostname or IP address
+     * @return true if the host name or IP address was found in the values String, otherwise false.
+     */
     private boolean match(String value, String host) {
         String[] values = value.split(",");
         for (int i = 0; i < values.length; i++) {
@@ -226,6 +330,14 @@ public class SvnConfigFiles {
         return false;
     }
 
+    /**
+     * Evaluates if the given hostaname or IP address matches with the given value String representing 
+     * a hostaname or IP adress with one or more "*" wildcards in it.
+     *
+     * @param value the value String. A host name or IP addresse with a "*" wildcard. (e.g *.168.0.1 or *.anything.com)
+     * @param host the hostname or IP address
+     * @return true if the host name or IP address matches with the values String, otherwise false.
+     */
     private boolean matchSegments(String value, String host) {
         String[] valueSegments = value.split(".");
         String[] hostSegments = host.split(".");
@@ -244,7 +356,15 @@ public class SvnConfigFiles {
         return false;
     }
 
-    private void copyToIDEConfigDir () {      
+    /**
+     * Copies the <b>config</b> configuration file from the Subversion commandline client 
+     * configuration directory into the configuration directory used by the Netbeans Subversion module. </br>
+     * The value for the 'store-auth-creds' key is alway set to 'no' so the commandline client wan't 
+     * create a file holding the authentication credentials when a svn command is called. 
+     * The reason for this is that the Subverion module holds the credentials in files with the 
+     * same format as the commandline client but with a different name. 
+     */
+    private void copyConfigFileToIDEConfigDir () {      
         Ini config = loadSystemIniFile("config");
 
         // patch store-auth-creds to "no"
@@ -262,6 +382,12 @@ public class SvnConfigFiles {
         }
     }
 
+    /**
+     * Loads the ini configuration file from the configuration directory used by the Netbeans Subversion module.
+     *
+     * @param fileName the file name
+     * @return an Ini instance holding the cofiguration file. 
+     */    
     private Ini loadNetbeansIniFile(String fileName) {
         File file = FileUtil.normalizeFile(new File(getNBConfigDir() + "/" + fileName));       
         Ini nbIni = null;
@@ -283,6 +409,20 @@ public class SvnConfigFiles {
         return nbIni;
     }
 
+    /**
+     * Loads the ini configuration file from the configurat settings used by 
+     * the Subversion commandline client. The settings are loaded and merged together in 
+     * in the folowing order:
+     * <ol>
+     *  <li> The per-user INI files
+     *  <li> The per-user Registry values (in case of Windows)
+     *  <li> The system-wide INI files
+     *  <li> The system-wide Registry values (in case of Windows)
+     * </ol> 
+     *
+     * @param fileName the file name
+     * @return an Ini instance holding the cofiguration file. 
+     */       
     private Ini loadSystemIniFile(String fileName) {
         // config files from userdir
         File file = FileUtil.normalizeFile(new File(getUserConfigPath() + "/config"));
@@ -320,9 +460,10 @@ public class SvnConfigFiles {
     }
 
     /**
-     *
-     * Merge only sections/keys/values into target which are not present in source
-     *
+     * Merges only sections/keys/values into target which are not already present in source
+     * 
+     * @param source the source ini file
+     * @param target the target ini file in which the values from the source file are going to be merged
      */
     private void merge(Ini source, Ini target) {
         for (Iterator itSections = source.keySet().iterator(); itSections.hasNext();) {
@@ -345,10 +486,11 @@ public class SvnConfigFiles {
     }
 
     /**
+     * Merges all sections/keys/values from source into target as long
+     * as they are no proxy configuration values. 
      *
-     * Merge all sections/keys/values from source into target as long
-     * as they are no proxy configuration values.
-     *
+     * @param source the source ini file
+     * @param target the target ini file in which the values from the source file are going to be merged
      */
     private void mergeWithoutProxyConfigurations(Ini source, Ini target) {
         // add changes from source
@@ -399,6 +541,12 @@ public class SvnConfigFiles {
         }        
     }
 
+    /**
+     * Evaluates if the section holds some proxy setting values.
+     *
+     * @param section the section
+     * @return true if the section holds some proxy setting values. Otherwise false
+     */    
     private boolean isProxyConfigurationSection(Ini.Section section) {
         Collection keys = section.keySet();
         return keys.contains("http-proxy-host")     ||
@@ -407,26 +555,23 @@ public class SvnConfigFiles {
                keys.contains("http-proxy-password");
     }
     
+    /**
+     * Evaluates if the value stored under the key is a proxy setting value.
+     *
+     * @param key the key
+     * @return true if the value stored under the key is a proxy setting value. Otherwise false
+     */
     private boolean isProxyConfigurationKey(String key) {
         return key.equals("http-proxy-host")     ||
                key.equals("http-proxy-port")     ||
                key.equals("http-proxy-username") ||
                key.equals("http-proxy-password");
     }
-
-    public static String getUserConfigPath() {
-        String path = System.getProperty("user.home") ;
-        if(Utilities.isUnix()) {
-            return path + "/" + UNIX_CONFIG_DIR;
-        } else if (Utilities.isWindows()){
-            return path + "/" + WINDOWS_CONFIG_DIR;
-        } else {
-            // XXX
-        }
-        return "";
-    }
-
-    public static String getGlobalConfigPath () {
+    
+    /**
+     * Return the path for the systemwide command lines configuration directory 
+     */
+    private static String getGlobalConfigPath () {
         if(Utilities.isUnix()) {
             return "/etc/subversion";
         } else if (Utilities.isWindows()){
@@ -437,11 +582,15 @@ public class SvnConfigFiles {
         return "";
     }
 
-    public static String getNBConfigDir() {
-        String nbHome = System.getProperty("netbeans.user");       
-        return nbHome + "/config/svn/config/";
-    }
-
+    /**
+     * Merges all sections/keys/values from the given registry folder into the given ini file. 
+     * The registry key name is keyPrefix + "\\Software\\Tigris.org\\Subversion\\" + svnFile
+     *
+     * @param keyPrefix the registry key prefix
+     * @param svnFile the last registry key folder
+     * @param iniFile the target ini file in which the values from the registry folder are going to be merged
+     */   
+    // XXX shouldn't be in this case also used the merge only the values witch aren't already present logic?
     private void mergeFromRegistry(String keyPrefix, String svnFile, Ini iniFile) {
         String key = keyPrefix + "\\Software\\Tigris.org\\Subversion\\" + svnFile;
         String tmpDirPath = System.getProperty("netbeans.user") + "/config/svn/tmp";  // XXX maybe an another location... XXX create temp file
