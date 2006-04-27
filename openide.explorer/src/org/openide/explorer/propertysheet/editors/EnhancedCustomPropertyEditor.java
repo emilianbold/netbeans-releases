307622572c317066ecd000e2dca592c906f78fff
jtulach <jtulach@netbeans.org>
2006-04-27 09:40:32 +0000

    Another example of possible rewrite

--- openide.explorer/src/org/openide/explorer/propertysheet/editors/EnhancedCustomPropertyEditor.java
+++ openide.explorer/src/org/openide/explorer/propertysheet/editors/EnhancedCustomPropertyEditor.java
@@ -24,6 +24,11 @@ import org.openide.explorer.propertysheet.PropertyEnv;
 * @deprecated Use {@link PropertyEnv} instead. An example of what needs to be
 *    done can be found in the rewrite of
 *    <a href="http://www.netbeans.org/source/browse/core/src/org/netbeans/beaninfo/editors/RectangleCustomEditor.java?r1=1.25&r2=1.26">RectangleCustomEditor</a>.
+*  Another example showing the changes in property editor as well as in its
+*  custom component can be found in
+*  <a href="http://core.netbeans.org/source/browse/core/execution/src/org/netbeans/core/execution/beaninfo/editors/NbProcessDescriptorEditor.java?r1=1.3&r2=1.4">NbProcessDescriptorEditor</a>
+*  and
+*   <a href="http://core.netbeans.org/source/browse/core/execution/src/org/netbeans/core/execution/beaninfo/editors/NbProcessDescriptorCustomEditor.java?r1=1.3&r2=1.4">NbProcessDescriptorCustomEditor</a>.
 */
 public interface EnhancedCustomPropertyEditor {
     /** Get the customized property value.
