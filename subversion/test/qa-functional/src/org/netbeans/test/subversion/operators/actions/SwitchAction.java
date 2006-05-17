be6a9b6133725dc8a2a48caf69917de1053ec8ed
ppis <ppis@netbeans.org>
2006-05-17 20:49:19 +0000

    Switch test added.

---
+++ subversion/test/qa-functional/src/org/netbeans/test/subversion/operators/actions/SwitchAction.java
@@ -0,0 +1,31 @@
+/*
+ * SwitchAction.java
+ *
+ * Created on 17 May 2006, 22:09
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package org.netbeans.test.subversion.operators.actions;
+
+import org.netbeans.jellytools.actions.ActionNoBlock;
+
+/**
+ *
+ * @author peter
+ */
+public class SwitchAction extends ActionNoBlock {
+    
+    /** "Subversion" menu item. */
+    public static final String SVN_ITEM = "Subversion";
+            
+    /** "Switch" menu item. */
+    public static final String SWITCH_ITEM = "Switch";
+    
+    /** Creates a new instance of SwitchAction */
+    public SwitchAction() {
+        super(SVN_ITEM + "|" + SWITCH_ITEM, SVN_ITEM + "|" + SWITCH_ITEM);
+    }
+    
+}
