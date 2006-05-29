c4c9402c09515d841c52be603d62c0ed87b8d8d4
ksorokin <ksorokin@netbeans.org>
2006-05-29 08:01:35 +0000

    added license notices

--- j2ee.websphere6/src/org/netbeans/modules/j2ee/websphere6/dd/beans/ExtendedServletsType.java
+++ j2ee.websphere6/src/org/netbeans/modules/j2ee/websphere6/dd/beans/ExtendedServletsType.java
@@ -1,11 +1,15 @@
 /*
- * ExtendedServletExtensionsType.java
- *
- *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Microsystems, Inc. All Rights Reserved.
  */
-
 package org.netbeans.modules.j2ee.websphere6.dd.beans;
 
 import org.w3c.dom.*;
--- j2ee.websphere6/src/org/netbeans/modules/j2ee/websphere6/dd/loaders/ui/WSWebBndAttributesForm.java
+++ j2ee.websphere6/src/org/netbeans/modules/j2ee/websphere6/dd/loaders/ui/WSWebBndAttributesForm.java
@@ -1,12 +1,17 @@
 /*
- * WSWebBndAttributesForm.java
- *
- *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
+ *                 Sun Public License Notice
+ * 
+ * The contents of this file are subject to the Sun Public License
+ * Version 1.0 (the "License"). You may not use this file except in
+ * compliance with the License. A copy of the License is available at
+ * http://www.sun.com/
+ * 
+ * The Original Code is NetBeans. The Initial Developer of the Original
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Microsystems, Inc. All Rights Reserved.
  */
-
 package org.netbeans.modules.j2ee.websphere6.dd.loaders.ui;
+
 //import  org.netbeans.modules.j2ee.websphere6.ddloaders.multiview.*;
 import org.netbeans.modules.j2ee.websphere6.dd.beans.WSWebBnd;
 import org.netbeans.modules.j2ee.websphere6.dd.loaders.webbnd.WSWebBndDataObject;
