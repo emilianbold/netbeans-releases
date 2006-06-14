611abf798df40657d85f16c667a9b65af754bade
jglick <jglick@netbeans.org>
2006-06-14 01:13:11 +0000

    Simplified with Jackpot.

--- projectapi/test/unit/src/org/netbeans/api/project/TestUtil.java
+++ projectapi/test/unit/src/org/netbeans/api/project/TestUtil.java
@@ -348,23 +348,18 @@ public final class TestUtil extends ProxyLookup {
         }
         assert fo.isData();
         if (content != null || touch) {
-            FileLock lock = fo.lock();
+            OutputStream os = fo.getOutputStream();
             try {
-                OutputStream os = fo.getOutputStream(lock);
-                try {
-                    if (content != null) {
-                        InputStream is = content.openStream();
-                        try {
-                            FileUtil.copy(is, os);
-                        } finally {
-                            is.close();
-                        }
+                if (content != null) {
+                    InputStream is = content.openStream();
+                    try {
+                        FileUtil.copy(is, os);
+                    } finally {
+                        is.close();
                     }
-                } finally {
-                    os.close();
                 }
             } finally {
-                lock.releaseLock();
+                os.close();
             }
         }
         return fo;
