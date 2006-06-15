70c0eaa9355a481baa2ce5dfdc0bc655484f93fb
jglick <jglick@netbeans.org>
2006-06-15 13:48:11 +0000

    Updating to match reality.

--- o.n.core/test/unit/src/org/netbeans/core/ShortcutsFolder66845Test.java
+++ o.n.core/test/unit/src/org/netbeans/core/ShortcutsFolder66845Test.java
@@ -13,83 +13,42 @@
 
 package org.netbeans.core;
 
-import java.util.Date;
+import java.util.logging.Level;
 import javax.swing.text.Keymap;
 import org.netbeans.core.startup.Main;
-import org.netbeans.junit.MockServices;
+import org.netbeans.junit.Log;
 import org.netbeans.junit.NbTestCase;
-import org.openide.ErrorManager;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileSystem;
 import org.openide.filesystems.FileUtil;
 import org.openide.filesystems.Repository;
 import org.openide.util.Lookup;
 
-/**
- * Tests shortcuts folder to ensure it handles wildcard keystrokes correctly. 
- */
 public class ShortcutsFolder66845Test extends NbTestCase {
-    private ErrorManager err;
-    private Keymap keymap;
 
-    /** Constructor required by JUnit.
-     * @param testName method name to be used as testcase
-     */
     public ShortcutsFolder66845Test(String s) {
         super(s);
     }
-    
+
+    private Keymap keymap;
+    private CharSequence logs;
+
     protected void setUp() throws Exception {
-        MockServices.setServices(EM.class, NbKeymap.class);
         Main.initializeURLFactory ();
         keymap = Lookup.getDefault().lookup(Keymap.class);
-        
         assertNotNull("There is a keymap", keymap);
+        assertEquals("of correct type", NbKeymap.class, keymap.getClass());
         ShortcutsFolder.initShortcuts ();
-        
-        err = ErrorManager.getDefault().getInstance("TEST-" + getName());
+        logs = Log.enable(ShortcutsFolder.class.getName(), Level.WARNING);
     }
-    
-    public void testApplyChangeToFactoryActionIssue49597 () throws Exception {
+
+    public void testLogging() throws Exception {
         final FileSystem fs = Repository.getDefault ().getDefaultFileSystem ();
         final FileObject shortcuts = FileUtil.createData(fs.getRoot(), "Keymaps/NetBeans/org-nb-Neznam.instance");
 
         ShortcutsFolder.waitFinished ();
 
-
-        assertEquals("One notification to error manager", 1, EM.cnt);
-        assertEquals("Severity informational", EM.INFORMATIONAL, EM.sev);
+        assertTrue("got message in " + logs, logs.toString().contains("Neznam"));
     }
 
-    public static final class EM extends ErrorManager {
-        public static int cnt;
-        public static int sev;
-
-        public EM() {
-        }
-
-        public Throwable attachAnnotations(Throwable t, ErrorManager.Annotation[] arr) {
-            return t;
-        }
-
-        public ErrorManager.Annotation[] findAnnotations(Throwable t) {
-            return null;
-        }
-
-        public Throwable annotate(Throwable t, int severity, String message, String localizedMessage, Throwable stackTrace, Date date) {
-            return t;
-        }
-
-        public void notify(int severity, Throwable t) {
-            cnt++;
-            sev = severity;
-        }
-
-        public void log(int severity, String s) {
-        }
-
-        public ErrorManager getInstance(String name) {
-            return this;
-        }
-    }
 }
