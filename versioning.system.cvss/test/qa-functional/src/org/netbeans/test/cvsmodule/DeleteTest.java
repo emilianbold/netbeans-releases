b35fc7ee13c7b41cfaf1dff7b9fcaa341a1ab7d0
ppis <ppis@netbeans.org>
2006-06-27 20:04:17 +0000

    Test for incorrect notification for file deletion.

---
+++ versioning.system.cvss/test/qa-functional/src/org/netbeans/test/cvsmodule/DeleteTest.java
@@ -0,0 +1,208 @@
+/*
+ * DeleteTest.java
+ *
+ * Created on 27 June 2006, 20:01
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package org.netbeans.test.cvsmodule;
+
+import java.io.File;
+import java.io.InputStream;
+import junit.textui.TestRunner;
+import org.netbeans.jellytools.JellyTestCase;
+import org.netbeans.jellytools.NbDialogOperator;
+import org.netbeans.jellytools.OutputOperator;
+import org.netbeans.jellytools.OutputTabOperator;
+import org.netbeans.jellytools.modules.javacvs.CVSRootStepOperator;
+import org.netbeans.jellytools.modules.javacvs.CheckoutWizardOperator;
+import org.netbeans.jellytools.modules.javacvs.ModuleToCheckoutStepOperator;
+import org.netbeans.jellytools.modules.javacvs.ProxyConfigurationOperator;
+import org.netbeans.jellytools.modules.javacvs.VersioningOperator;
+import org.netbeans.jellytools.nodes.Node;
+import org.netbeans.jellytools.nodes.SourcePackagesNode;
+import org.netbeans.jemmy.JemmyProperties;
+import org.netbeans.jemmy.TimeoutExpiredException;
+import org.netbeans.jemmy.operators.JButtonOperator;
+import org.netbeans.jemmy.operators.JProgressBarOperator;
+import org.netbeans.jemmy.operators.JTableOperator;
+import org.netbeans.junit.NbTestSuite;
+import org.netbeans.junit.ide.ProjectSupport;
+
+/**
+ *
+ * @author peter
+ */
+public class DeleteTest extends JellyTestCase {
+    
+    String os_name;
+    static String sessionCVSroot;
+    final String projectName = "ForImport";
+    final String pathToMain = "forimport|Main.java";
+    final String PROTOCOL_FOLDER = "protocol";
+    static File cacheFolder;
+    
+    /** Creates a new instance of DeleteTest */
+    public DeleteTest(String name) {
+        super(name);
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        TestRunner.run(suite());
+    }
+    
+    public static NbTestSuite suite() {
+        NbTestSuite suite = new NbTestSuite();
+        suite.addTest(new DeleteTest("testCheckOutProject"));
+        suite.addTest(new DeleteTest("testDeleteFile"));
+        suite.addTest(new DeleteTest("removeAllData"));
+        return suite;
+     }
+    
+    protected void setUp() throws Exception {
+        
+        os_name = System.getProperty("os.name");
+        //System.out.println(os_name);
+        System.out.println("### "+getName()+" ###");
+        
+    }
+    
+    protected boolean isUnix() {
+        boolean unix = false;
+        if (os_name.indexOf("Windows") == -1) {
+            unix = true;
+        }
+        return unix;
+    }
+    
+    public void testCheckOutProject() throws Exception {
+        JemmyProperties.setCurrentTimeout("ComponentOperator.WaitComponentTimeout", 36000);
+        JemmyProperties.setCurrentTimeout("DialogWaiter.WaitDialogTimeout", 36000);
+        OutputOperator oo = OutputOperator.invoke();
+        CheckoutWizardOperator cwo = CheckoutWizardOperator.invoke();
+        CVSRootStepOperator crso = new CVSRootStepOperator();
+        //JComboBoxOperator combo = new JComboBoxOperator(crso, 0);
+        crso.setCVSRoot(":pserver:anoncvs@localhost:/cvs");
+        //crso.setPassword("");
+        ProxyConfigurationOperator pco = crso.proxyConfiguration();
+        pco.noProxyDirectConnection();
+        pco.ok();
+        //crso.setPassword("test");
+        
+        //prepare stream for successful authentification and run PseudoCVSServer
+        InputStream in = TestKit.getStream(getDataDir().getCanonicalFile().toString() + File.separator + PROTOCOL_FOLDER, "authorized.in");   
+        PseudoCvsServer cvss = new PseudoCvsServer(in);
+        new Thread(cvss).start();
+        cvss.ignoreProbe();
+        String CVSroot = cvss.getCvsRoot();
+        sessionCVSroot = CVSroot;
+        //System.out.println(sessionCVSroot);
+        crso.setCVSRoot(CVSroot);
+        System.setProperty("netbeans.t9y.cvs.connection.CVSROOT", CVSroot);
+        crso.next();
+              
+        try {
+           JProgressBarOperator progress = new JProgressBarOperator(crso);
+           JButtonOperator btnStop = new JButtonOperator(crso);
+        } catch (TimeoutExpiredException e) {
+            throw e;
+        }
+        
+        //second step of checkoutwizard
+        //2nd step of CheckOutWizard
+        
+        File tmp = new File("/tmp"); // NOI18N
+        File work = new File(tmp, "" + File.separator + System.currentTimeMillis());
+        cacheFolder = new File(work, projectName + File.separator + "src" + File.separator + "forimport" + File.separator + "CVS" + File.separator + "RevisionCache");
+        tmp.mkdirs();
+        work.mkdirs();
+        tmp.deleteOnExit();
+        ModuleToCheckoutStepOperator moduleCheck = new ModuleToCheckoutStepOperator();
+        cvss.stop();
+        in.close();
+        moduleCheck.setModule("ForImport");        
+        moduleCheck.setLocalFolder(work.getAbsolutePath()); // NOI18N
+        
+        //Pseudo CVS server for finishing check out wizard
+        in = TestKit.getStream(getDataDir().getCanonicalFile().toString() + File.separator + PROTOCOL_FOLDER, "checkout_finish_2.in");
+        cvss = new PseudoCvsServer(in);
+        new Thread(cvss).start();
+        CVSroot = cvss.getCvsRoot();
+        //cvss.ignoreProbe();
+        
+        //crso.setCVSRoot(CVSroot);
+        //combo.setSelectedItem(CVSroot);
+        System.setProperty("netbeans.t9y.cvs.connection.CVSROOT", CVSroot);
+        cwo.finish();
+        
+        //System.out.println(CVSroot);
+        
+        OutputTabOperator oto = new OutputTabOperator(sessionCVSroot); 
+        oto.getTimeouts().setTimeout("ComponentOperator.WaitStateTimeout", 30000);
+        oto.waitText("Checking out finished");
+        cvss.stop();
+        in.close();
+        NbDialogOperator nbdialog = new NbDialogOperator("Checkout Completed");
+        JButtonOperator open = new JButtonOperator(nbdialog, "Open Project");
+        open.push();
+        ProjectSupport.waitScanFinished();
+        //create new elements for testing
+        //TestKit.createNewElements(projectName);
+        System.setProperty("netbeans.t9y.cvs.connection.CVSROOT", "");
+    }
+    
+    public void testDeleteFile() throws Exception {
+        JemmyProperties.setCurrentTimeout("ComponentOperator.WaitComponentTimeout", 18000);   
+        PseudoCvsServer cvss, cvss2, cvss3;
+        InputStream in, in2, in3;
+        //OutputOperator oo;
+        OutputTabOperator oto;
+        org.openide.nodes.Node nodeIDE;
+        String color, CVSroot;
+        VersioningOperator vo;
+        JTableOperator table;
+        
+        oto = new OutputTabOperator(sessionCVSroot); 
+        oto.getTimeouts().setTimeout("ComponentOperator.WaitStateTimeout", 30000);
+        
+        TestKit.deleteRecursively(cacheFolder);
+        Node nodeMain = new Node(new SourcePackagesNode(projectName), "forimport");
+        in = TestKit.getStream(getDataDir().getCanonicalFile().toString() + File.separator + PROTOCOL_FOLDER, "show_changes_package_1.in");
+        cvss = new PseudoCvsServer(in);
+        new Thread(cvss).start();
+        CVSroot = cvss.getCvsRoot();
+        System.setProperty("netbeans.t9y.cvs.connection.CVSROOT", CVSroot);
+        nodeMain.performPopupAction("CVS|Show Changes");
+        Thread.sleep(1000);
+        vo = VersioningOperator.invoke();
+        cvss.stop();
+        
+        //delete file again and commit deletion
+        nodeMain = new Node(new SourcePackagesNode(projectName), pathToMain);
+        nodeMain.performPopupActionNoBlock("Delete");
+        NbDialogOperator dialog = new NbDialogOperator("Confirm Object Deletion");
+        dialog.yes();
+        Thread.sleep(1000);
+        Exception e = null;
+        try {
+            table = vo.tabFiles();
+            assertEquals("Files should have been [Locally Deleted]", "Locally Deleted", table.getValueAt(0, 1).toString());       
+        }
+        catch (Exception ex) {
+            e = ex;
+        }
+        assertNull("Unexpected behavior - file should appear in Versioning view!!!", e);
+    }
+    
+    public void removeAllData() {
+        TestKit.removeAllData(projectName);
+        System.setProperty("netbeans.t9y.cvs.connection.CVSROOT", "");
+    }
+}
+
