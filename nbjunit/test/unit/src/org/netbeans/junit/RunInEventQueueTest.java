586c7a557588c110708b9f31dd7973013e2f7851
jskrivanek <jskrivanek@netbeans.org>
2006-06-29 08:12:14 +0000

    #78892 - setUp() and tearDown() run also in AWT event thread if runInEQ(0 returns true. It is done in runBare method to which was also moved code from runTest method.

--- nbjunit/test/unit/src/org/netbeans/junit/RunInEventQueueTest.java
+++ nbjunit/test/unit/src/org/netbeans/junit/RunInEventQueueTest.java
@@ -7,7 +7,7 @@
  * http://www.sun.com/
  * 
  * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
+ * Code is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -26,9 +26,11 @@ public class RunInEventQueueTest extends NbTestCase {
     }
 
     protected void setUp () throws Exception {
+        assertTrue("setUp should be run in AWT thread.", SwingUtilities.isEventDispatchThread());
     }
 
     protected void tearDown () throws Exception {
+        assertTrue("tearDown should be run in AWT thread.", SwingUtilities.isEventDispatchThread());
     }
     
     protected boolean runInEQ () {
