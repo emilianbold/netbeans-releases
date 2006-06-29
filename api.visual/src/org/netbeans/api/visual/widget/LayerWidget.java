7df5e582c32257a25aa05c9531a2e48f3030fa5e
dkaspar <dkaspar@netbeans.org>
2006-06-29 07:57:19 +0000

    LayerWidget.reevaluateLayout and related methods added, DevolveTest added

--- api.visual/src/org/netbeans/api/visual/widget/LayerWidget.java
+++ api.visual/src/org/netbeans/api/visual/widget/LayerWidget.java
@@ -12,12 +12,18 @@
  */
 package org.netbeans.api.visual.widget;
 
+import org.netbeans.api.visual.animator.PreferredLocationAnimator;
+import org.netbeans.api.visual.layout.Layout;
+
 import java.awt.*;
 
 /**
  * @author David Kaspar
  */
+// TODO - reevaluateLayout
 public class LayerWidget extends Widget {
+
+    private Layout devolveLayout;
 
     public LayerWidget (Scene scene) {
         super (scene);
@@ -29,6 +35,56 @@ public class LayerWidget extends Widget {
 
     protected boolean isRepaintRequiredForRevalidating () {
         return false;
+    }
+
+    public Layout getDevolveLayout () {
+        return devolveLayout;
+    }
+
+    public void setDevolveLayout (Layout devolveLayout) {
+        this.devolveLayout = devolveLayout;
+    }
+
+    @Deprecated
+    public void reevaluateLayout (boolean animate) {
+        reevaluateLayout (getDevolveLayout (), animate);
+    }
+
+    /**
+     * Reevaluates layout of the layer widget using a specified layout.
+     * <p>
+     * Note: This method does not work corectly when the scene does not have Graphics2D instance assigned - means Scene is not shown on a display. This happens when a layout algorithm calculates with children boundaries only.
+     * <p>
+     * Note: Calling this method has side-effects.
+     * Best if this method is called alone (without any other library method) during a single scene validation - means during the initialization or AWT event processing.
+     * The layer widget will temporarily set different layout.
+     * Scene.validate method is called.
+     * Changes preferredLocation of children Widget.
+     * At the end the layer widget is marked for revalidation.
+     *
+     * @param layout the layout using for reevaluating
+     * @param animate
+     * @deprecated Because of many side-effects, this method could be redesigned later.
+     */
+    public void reevaluateLayout (Layout layout, boolean animate) {
+        if (layout == null)
+            return;
+
+        Layout oldLayout = getLayout ();
+        try {
+            setLayout (layout);
+            getScene ().validate ();
+            // The child Widgets have now had their 'location' set... Now, we need to set their preferredLocation.
+            PreferredLocationAnimator pla = getScene ().getSceneAnimator ().getPreferredLocationAnimator ();
+            for (Widget child : getChildren ()) {
+                if (animate)
+                    pla.setPreferredLocation (child, child.getLocation ());
+                else
+                    child.setPreferredLocation (child.getLocation ());
+            }
+        } finally {
+            setLayout (oldLayout);
+        }
     }
 
 }
