e0493a5ea6fd7671c95b61fbdbf2206c2f83035b
jtulach <jtulach@netbeans.org>
2006-06-30 21:21:22 +0000

    Updating to CDDL for module openidex

--- o.openidex.util/src/org/openidex/search/SearchPattern.java
+++ o.openidex.util/src/org/openidex/search/SearchPattern.java
@@ -1,38 +1,44 @@
 /*
- *                 Sun Public License Notice
- * 
- * The contents of this file are subject to the Sun Public License
- * Version 1.0 (the "License"). You may not use this file except in
- * compliance with the License. A copy of the License is available at
- * http://www.sun.com/
- * 
- * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 2005 Sun
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 2005 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
 package org.openidex.search;
 
 /**
- * Pattern describes the search conditions 
+ * Pattern describes the search conditions
  *
  * @since  org.openidex.util/3 3.5, NB 4.1
  * @author  Martin Roskanin
  */
 public final class SearchPattern {
-    
+
     /** SearchExpression - a text to search */
     private String searchExpression;
-    
+
     /** if true, only whole words were searched */
     private boolean wholeWords;
-    
+
     /** if true, case sensitive search was preformed */
     private boolean matchCase;
-    
+
     /** if true, regular expression search was performed */
     private boolean regExp;
-    
+
     /** Creates a new instance of SearchPattern 
      *  @param searchExpression a searched text
      *  @param wholeWords if true, only whole words were searched
