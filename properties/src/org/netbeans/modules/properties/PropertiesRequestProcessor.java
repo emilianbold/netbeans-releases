fb2ca35cf19c2f6c347c84c9a53450cd4f88be4b
jtulach <jtulach@netbeans.org>
2006-06-30 21:32:48 +0000

    Updating to CDDL for module properties

--- properties/src/org/netbeans/modules/properties/PropertiesRequestProcessor.java
+++ properties/src/org/netbeans/modules/properties/PropertiesRequestProcessor.java
@@ -1,20 +1,26 @@
 /*
- *                 Sun Public License Notice
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
  *
- * The contents of this file are subject to the Sun Public License
- * Version 1.0 (the "License"). You may not use this file except in
- * compliance with the License. A copy of the License is available at
- * http://www.sun.com/
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
  *
- * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
 package org.netbeans.modules.properties;
 
 import org.openide.util.RequestProcessor;
- 
+
 /**
  *
  * Provides an {@link org.openide.util.RequestProcessor} instance for the Properties module.
@@ -22,11 +28,11 @@ import org.openide.util.RequestProcessor;
  * @author Tomas Stupka
  */
 public class PropertiesRequestProcessor {
-    
+
      /** the static {@link org.openide.util.RequestProcessor} instance */
-    private final static RequestProcessor requestProcessor = 
+    private final static RequestProcessor requestProcessor =
         new RequestProcessor ("org.netbeans.modules.properties.PropertiesRequestProcessor");
-    
+
     /** Creates a new instance of PropertiesResourceProcessor */
     public PropertiesRequestProcessor() {        
     }
