3ed9c696328b6c11a20873b85186cf845fdf184b
jtulach <jtulach@netbeans.org>
2006-06-30 20:16:32 +0000

    Updating to CDDL for module javacvs

--- versioning.system.cvss/src/org/netbeans/modules/versioning/system/cvss/util/Context.java
+++ versioning.system.cvss/src/org/netbeans/modules/versioning/system/cvss/util/Context.java
@@ -1,13 +1,19 @@
 /*
- *                 Sun Public License Notice
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
  *
- * The contents of this file are subject to the Sun Public License
- * Version 1.0 (the "License"). You may not use this file except in
- * compliance with the License. A copy of the License is available at
- * http://www.sun.com/
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
  *
- * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -21,12 +27,12 @@ import java.util.*;
  * Encapsulates context of an action. There are two ways in which context may be defined:
  * - list of files (f/b.txt, f/c.txt, f/e.txt)
  * - list of roots (top folders) plus list of exclusions (f),(a.txt, d.txt)
- * 
+ *
  * @author Maros Sandor
  */
 public class Context implements Serializable {
 
-    public static final Context Empty = new Context(Collections.EMPTY_LIST, Collections.EMPTY_LIST, Collections.EMPTY_LIST); 
+    public static final Context Empty = new Context(Collections.EMPTY_LIST, Collections.EMPTY_LIST, Collections.EMPTY_LIST);
     
     private static final long serialVersionUID = 1L;
     
