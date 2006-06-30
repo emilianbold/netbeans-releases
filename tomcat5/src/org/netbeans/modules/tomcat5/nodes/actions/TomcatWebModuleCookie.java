d2f901450ea2ff0b4dcbbb9b28699ac5fef86a3e
jtulach <jtulach@netbeans.org>
2006-06-30 22:10:53 +0000

    Updating to CDDL for module tomcatint

--- tomcat5/src/org/netbeans/modules/tomcat5/nodes/actions/TomcatWebModuleCookie.java
+++ tomcat5/src/org/netbeans/modules/tomcat5/nodes/actions/TomcatWebModuleCookie.java
@@ -1,13 +1,19 @@
 /*
- *                 Sun Public License Notice
- * 
- * The contents of this file are subject to the Sun Public License
- * Version 1.0 (the "License"). You may not use this file except in
- * compliance with the License. A copy of the License is available at
- * http://www.sun.com/
- * 
- * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2000 Sun
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -16,7 +22,7 @@ package org.netbeans.modules.tomcat5.nodes.actions;
 import org.openide.nodes.Node;
 import org.netbeans.modules.tomcat5.TomcatModule;
 
-/** 
+/**
  * Tomcat web module cookie
  * @author Petr Pisl
  */
@@ -24,19 +30,19 @@ public interface TomcatWebModuleCookie extends Node.Cookie {
 
     /**
      * Undeploys the web module from the server.
-     */    
+     */
     public void undeploy();
 
     /**
      * Starts the web module.
-     */    
+     */
     public void start ();
-    
+
     /**
      * Stops the web module.
      */
     public void stop ();
-    
+
     /**
      * Returns <code>true</code> if the web module is started, <code>false</code>
      * otherwise.
