ba35f0865f600b3d82acaac24323f87a43fe33ad
jtulach <jtulach@netbeans.org>
2006-06-30 19:36:31 +0000

    Updating to CDDL for module html

--- html/src/org/netbeans/modules/html/palette/items/META.java
+++ html/src/org/netbeans/modules/html/palette/items/META.java
@@ -1,13 +1,19 @@
 /*
- *                 Sun Public License Notice
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
  *
- * The contents of this file are subject to the Sun Public License
- * Version 1.0 (the "License"). You may not use this file except in
- * compliance with the License. A copy of the License is available at
- * http://www.sun.com/
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
  *
- * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -23,10 +29,10 @@ import org.openide.text.ActiveEditorDrop;
  * @author Libor Kotouc
  */
 public class META implements ActiveEditorDrop {
-    
+
     public static final String TYPE_HEADERS = "HEADERS"; // NOI18N
     public static final String TYPE_ENGINES = "ENGINES"; // NOI18N
-    
+
     public static final String[] headers = new String[] { "Content-Type", "Content-Language", "Refresh", "Cache-Control", "Expires" }; // NOI18N
     public static final int HEADER_DEFAULT = 0;
     public static final String[] engines = new String[] { "robots", "description", "keywords" }; // NOI18N
