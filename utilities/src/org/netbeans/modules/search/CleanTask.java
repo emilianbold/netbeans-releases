d1049b1427e92516a1bd7b98726021601fafc8f2
jtulach <jtulach@netbeans.org>
2006-06-30 22:21:46 +0000

    Updating to CDDL for module utilities

--- utilities/src/org/netbeans/modules/search/CleanTask.java
+++ utilities/src/org/netbeans/modules/search/CleanTask.java
@@ -1,13 +1,19 @@
 /*
- *                 Sun Public License Notice
- * 
- * The contents of this file are subject to the Sun Public License
- * Version 1.0 (the "License"). You may not use this file except in
- * compliance with the License. A copy of the License is available at
- * http://www.sun.com/
- * 
- * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 2004 Sun
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 2004 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -20,10 +26,10 @@ package org.netbeans.modules.search;
  * @author  Marian Petras
  */
 class CleanTask implements Runnable {
-    
+
     /** result model to clean */
     ResultModel resultModel;
-    
+
     /**
      * Creates a new instance of CleanTask.
      *
@@ -32,11 +38,11 @@ class CleanTask implements Runnable {
     CleanTask(ResultModel resultModel) {
         this.resultModel = resultModel;
     }
-    
+
     /**
      */
     public void run() {
         resultModel.close();
     }
-    
+
 }
--- utilities/src/org/netbeans/modules/search/FindDialogMemory.java
+++ utilities/src/org/netbeans/modules/search/FindDialogMemory.java
@@ -1,13 +1,19 @@
 /*
- *                 Sun Public License Notice
- * 
- * The contents of this file are subject to the Sun Public License
- * Version 1.0 (the "License"). You may not use this file except in
- * compliance with the License. A copy of the License is available at
- * http://www.sun.com/
- * 
- * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 2005 Sun
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 2005 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -20,7 +26,7 @@ import org.netbeans.modules.search.types.FullTextType;
 import org.openidex.search.SearchHistory;
 import org.openidex.search.SearchType;
 
-/** 
+/**
  * Registry for everything related to the Find dialog.
  * It is <em>not</em> designed to be persistent across invocations
  * of the IDE.
@@ -28,7 +34,7 @@ import org.openidex.search.SearchType;
  * @author Marian Petras
  */
 public final class FindDialogMemory implements PropertyChangeListener {
-    
+
     /** singleton instance of this class */
     private static FindDialogMemory singleton;
     
