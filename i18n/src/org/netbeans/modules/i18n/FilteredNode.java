8335796d5f0d7779a2b609e5e24ff531b78cf6a4
jtulach <jtulach@netbeans.org>
2006-06-30 19:58:45 +0000

    Updating to CDDL for module i18n

--- i18n/src/org/netbeans/modules/i18n/FilteredNode.java
+++ i18n/src/org/netbeans/modules/i18n/FilteredNode.java
@@ -1,13 +1,19 @@
 /*
- *                 Sun Public License Notice
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
  *
- * The contents of this file are subject to the Sun Public License
- * Version 1.0 (the "License"). You may not use this file except in
- * compliance with the License. A copy of the License is available at
- * http://www.sun.com/
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
  *
- * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2000 Sun
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -24,9 +30,9 @@ import org.openide.nodes.Node;
  * This class implements a filter node that can be used to display a
  * hierarchy of nodes filtered by a criteriea defined by
  * <code>NodeFilter</code>. We don't use NodeAcceptor because of
- * efficiency - node acceptor requires an array as a parameter, 
+ * efficiency - node acceptor requires an array as a parameter,
  * which we don't want to create.
- */ 
+ */
 public class FilteredNode extends FilterNode {
 
   private NodeFilter filter;
--- i18n/src/org/netbeans/modules/i18n/ResourceHolder.java
+++ i18n/src/org/netbeans/modules/i18n/ResourceHolder.java
@@ -1,13 +1,19 @@
 /*
- *                 Sun Public License Notice
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
  *
- * The contents of this file are subject to the Sun Public License
- * Version 1.0 (the "License"). You may not use this file except in
- * compliance with the License. A copy of the License is available at
- * http://www.sun.com/
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
  *
- * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2003 Sun
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -21,12 +27,12 @@ import java.util.Arrays;
 import org.openide.loaders.DataObject;
 
 /**
- * Abstract class which implementation's are wrappers for holders of properties resources. 
+ * Abstract class which implementation's are wrappers for holders of properties resources.
  * Typically such object is <code>PropertiesDataObject</code> which represents bundle
  * of .properties files. But it can be also some object representing other type of resources
- * e.g. subclass of <code>ListResourceBundle</code> class or in case of i18n-ing JSP pages 
+ * e.g. subclass of <code>ListResourceBundle</code> class or in case of i18n-ing JSP pages
  * object representing tag library etc.
- * 
+ *
  * @author  Peter Zavadsky
  */
 public abstract class ResourceHolder {
