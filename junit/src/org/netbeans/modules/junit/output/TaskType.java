8af226e90aff37b007b406bc9f2b0641cd6bc586
jtulach <jtulach@netbeans.org>
2006-06-30 20:28:48 +0000

    Updating to CDDL for module junit

--- junit/src/org/netbeans/modules/junit/output/TaskType.java
+++ junit/src/org/netbeans/modules/junit/output/TaskType.java
@@ -1,13 +1,19 @@
 /*
- *                 Sun Public License Notice
- * 
- * The contents of this file are subject to the Sun Public License
- * Version 1.0 (the "License"). You may not use this file except in
- * compliance with the License. A copy of the License is available at
- * http://www.sun.com/
- * 
- * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 2006 Sun
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 2006 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -19,12 +25,12 @@ package org.netbeans.modules.junit.output;
  * @author  Marian Petras
  */
 enum TaskType {
-    
+
     /** denotes a JUnit test task */
     TEST_TASK,
     /** denotes a JUnit test task running in debugging mode */
     DEBUGGING_TEST_TASK,
     /** denotes other task type */
     OTHER_TASK
-    
+
 }
--- junit/test/unit/src/org/netbeans/modules/junit/ClassNameTextFieldTest.java
+++ junit/test/unit/src/org/netbeans/modules/junit/ClassNameTextFieldTest.java
@@ -1,13 +1,19 @@
 /*
- *                 Sun Public License Notice
- * 
- * The contents of this file are subject to the Sun Public License
- * Version 1.0 (the "License"). You may not use this file except in
- * compliance with the License. A copy of the License is available at
- * http://www.sun.com/
- * 
- * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 2005 Sun
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 2005 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -20,7 +26,7 @@ import junit.framework.TestCase;
  * @author  Marian Petras
  */
 public class ClassNameTextFieldTest extends TestCase {
-    
+
     public ClassNameTextFieldTest(String testName) {
         super(testName);
     }
@@ -33,7 +39,7 @@ public class ClassNameTextFieldTest extends TestCase {
             this.value = value;
         }
     }
-    
+
     /**
      * Test of determineStatus method, of class org.netbeans.modules.junit.ClassNameTextField.
      */
