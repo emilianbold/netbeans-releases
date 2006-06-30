27d50333811a6c68fa9cee1e54872424e352405c
jtulach <jtulach@netbeans.org>
2006-06-30 14:09:07 +0000

    Updating to CDDL for module which is not in standard build like management

--- jmx/src/org/netbeans/modules/jmx/MBeanOperationException.java
+++ jmx/src/org/netbeans/modules/jmx/MBeanOperationException.java
@@ -1,13 +1,19 @@
 /*
- *                 Sun Public License Notice
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
  *
- * The contents of this file are subject to the Sun Public License
- * Version 1.0 (the "License"). You may not use this file except in
- * compliance with the License. A copy of the License is available at
- * http://www.sun.com/
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
  *
- * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 2004-2005 Sun
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 2004-2005 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -15,21 +21,21 @@ package org.netbeans.modules.jmx;
 
 /**
  * Class which describes the structure of an MBean operation exception
- * 
+ *
  */
 public class MBeanOperationException {
-    
+
     private String excepClass = "";// NOI18N
     private String excepDescription = "";// NOI18N
-    
-    
+
+
     /**
      * Default constructor
      */
     public MBeanOperationException() {
-        
+
     }
-    
+
     /**
      * Constructor
      * @param excepClass the exception java class
