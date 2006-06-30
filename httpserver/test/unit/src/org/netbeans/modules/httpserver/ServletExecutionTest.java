b87e82483b00061173b971bcbeb43116164faf38
jtulach <jtulach@netbeans.org>
2006-06-30 19:41:56 +0000

    Updating to CDDL for module httpserver

--- httpserver/test/unit/src/org/netbeans/modules/httpserver/ServletExecutionTest.java
+++ httpserver/test/unit/src/org/netbeans/modules/httpserver/ServletExecutionTest.java
@@ -1,13 +1,19 @@
 /*
- *                 Sun Public License Notice
- * 
- * The contents of this file are subject to the Sun Public License
- * Version 1.0 (the "License"). You may not use this file except in
- * compliance with the License. A copy of the License is available at
- * http://www.sun.com/
- * 
- * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2005 Sun
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -24,13 +30,13 @@ import org.netbeans.junit.NbTestCase;
  * @author Radim Kubacki
  */
 public class ServletExecutionTest extends NbTestCase {
-    
+
     public ServletExecutionTest(String testName) {
         super (testName);
     }
-    
+
     public void testServletExecution() throws Exception {
-        HttpServerSettings settings = 
+        HttpServerSettings settings =
          (HttpServerSettings)HttpServerSettings.findObject (HttpServerSettings.class, true);
         log("Starting HTTP server");
         settings.setRunning(true);
