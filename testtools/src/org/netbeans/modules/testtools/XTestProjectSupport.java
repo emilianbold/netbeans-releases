dc5dca5bdf167bdd17250a96ce28a059dec1f257
jtulach <jtulach@netbeans.org>
2006-06-30 14:20:42 +0000

    Updating to CDDL for module which is not in standard build like testtools

--- testtools/src/org/netbeans/modules/testtools/XTestProjectSupport.java
+++ testtools/src/org/netbeans/modules/testtools/XTestProjectSupport.java
@@ -1,18 +1,25 @@
 /*
- *                         Sun Public License Notice
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
  *
- * The contents of this file are subject to the Sun Public License Version
- * 1.0 (the "License"). You may not use this file except in compliance with 
- * the License. A copy of the License is available at http://www.sun.com/
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
  *
- * The Original Code is the Ant module
- * The Initial Developer of the Original Code is Jayme C. Edwards.
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is the Ant module
+ * The Initial Developer of the Original Software is Jayme C. Edwards.
  * Portions created by Jayme C. Edwards are Copyright (c) 2002.
  * All Rights Reserved.
  *
  * Contributor(s): Jesse Glick.
  */
- 
+
 package org.netbeans.modules.testtools;
 
 import java.io.*;
