f7004ee848a2d2e0a46255afc3f50afbaf08c1ce
jtulach <jtulach@netbeans.org>
2006-06-30 21:27:07 +0000

    Updating to CDDL for module projects

--- projectui/src/org/netbeans/modules/project/ui/actions/LookupSensitiveAction.java
+++ projectui/src/org/netbeans/modules/project/ui/actions/LookupSensitiveAction.java
@@ -1,13 +1,19 @@
 /*
- *                 Sun Public License Notice
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
  *
- * The contents of this file are subject to the Sun Public License
- * Version 1.0 (the "License"). You may not use this file except in
- * compliance with the License. A copy of the License is available at
- * http://www.sun.com/
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
  *
- * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
@@ -23,12 +29,12 @@ import org.openide.util.Utilities;
 import org.openide.util.WeakListeners;
 
 /** Action sensitive to current project
- * 
- * @author Pet Hrebejk 
+ *
+ * @author Pet Hrebejk
  */
 public abstract class LookupSensitiveAction extends BasicAction implements LookupListener {
-    
-    private Lookup lookup;    
+
+    private Lookup lookup;
     private Class<?>[] watch;
     private Lookup.Result results[];
     private boolean needsRefresh = true;
