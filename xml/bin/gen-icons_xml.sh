e95fc70384584b13f5d1707a98fc9a0cba49f9b6
jtulach <jtulach@netbeans.org>
2006-07-01 07:20:31 +0000

    CDDL references instead of SPL ones

--- xml/bin/gen-icons_xml.sh
+++ xml/bin/gen-icons_xml.sh
@@ -49,17 +49,22 @@ for dir in api catalog core css tax text-edit tools tree-edit xsl schema; do
     if [ "$GENERATE" == "true" ]; then
         DESC_FILE=$ICONS/description.properties
         echo "# 
-#                 Sun Public License Notice
-# 
-# The contents of this file are subject to the Sun Public License
-# Version 1.0 (the "License"). You may not use this file except in
-# compliance with the License. A copy of the License is available at
-# http://www.sun.com/
-# 
-# The Original Code is NetBeans. The Initial Developer of the Original
-# Code is Sun Microsystems, Inc. Portions Copyright 1997-2002 Sun
+# The contents of this file are subject to the terms of the Common Development
+# and Distribution License (the License). You may not use this file except in
+# compliance with the License.
+#
+# You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+# or http://www.netbeans.org/cddl.txt.
+#
+# When distributing Covered Code, include this CDDL Header Notice in each file
+# and include the License file at http://www.netbeans.org/cddl.txt.
+# If applicable, add the following below the CDDL Header, with the fields
+# enclosed by brackets [] replaced by your own identifying information:
+# "Portions Copyrighted [year] [name of copyright owner]"
+#
+# The Original Software is NetBeans. The Initial Developer of the Original
+# Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
 # Microsystems, Inc. All Rights Reserved.
-# 
 " > $DESC_FILE
     fi
 
