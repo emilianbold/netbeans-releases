bb12088b52f4aa2473c5c3bf16b7c18302450af3
ppis <ppis@netbeans.org>
2006-07-03 14:03:48 +0000

    Diff test added.

---
+++ subversion/test/qa-functional/src/org/netbeans/test/subversion/main/diff/DiffTest.java
@@ -0,0 +1,172 @@
+/*
+ * DiffTest.java
+ *
+ * Created on July 3, 2006, 3:18 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package org.netbeans.test.subversion.main.diff;
+
+import java.io.File;
+import java.io.PrintStream;
+import junit.textui.TestRunner;
+import org.netbeans.jellytools.EditorOperator;
+import org.netbeans.jellytools.JellyTestCase;
+import org.netbeans.jellytools.NbDialogOperator;
+import org.netbeans.jellytools.OutputTabOperator;
+import org.netbeans.jellytools.nodes.Node;
+import org.netbeans.jellytools.nodes.SourcePackagesNode;
+import org.netbeans.jemmy.JemmyProperties;
+import org.netbeans.jemmy.TimeoutExpiredException;
+import org.netbeans.jemmy.operators.JButtonOperator;
+import org.netbeans.junit.NbTestSuite;
+import org.netbeans.test.subversion.operators.CheckoutWizardOperator;
+import org.netbeans.test.subversion.operators.RepositoryStepOperator;
+import org.netbeans.test.subversion.operators.VersioningOperator;
+import org.netbeans.test.subversion.operators.WorkDirStepOperator;
+import org.netbeans.test.subversion.utils.RepositoryMaintenance;
+import org.netbeans.test.subversion.utils.TestKit;
+import org.netbeans.junit.ide.ProjectSupport;
+import org.netbeans.test.subversion.operators.DiffOperator;
+
+/**
+ *
+ * @author pvcs
+ */
+public class DiffTest extends JellyTestCase {
+    
+    public static final String TMP_PATH = "/tmp";
+    public static final String REPO_PATH = "repo";
+    public static final String WORK_PATH = "work";
+    public static final String PROJECT_NAME = "JavaApp";
+    public File projectPath;
+    public PrintStream stream;
+    String os_name;
+    
+    /** Creates a new instance of DiffTest */
+    public DiffTest(String name) {
+        super(name);
+    }
+    
+    protected void setUp() throws Exception {        
+        os_name = System.getProperty("os.name");
+        //System.out.println(os_name);
+        System.out.println("### "+getName()+" ###");
+        
+    }
+    
+    protected boolean isUnix() {
+        boolean unix = false;
+        if (os_name.indexOf("Windows") == -1) {
+            unix = true;
+        }
+        return unix;
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        TestRunner.run(suite());
+    }
+    
+    public static NbTestSuite suite() {
+        NbTestSuite suite = new NbTestSuite();
+        suite.addTest(new DiffTest("testDiffFile"));
+        return suite;
+    }
+    
+    public void testDiffFile() throws Exception {
+        JemmyProperties.setCurrentTimeout("ComponentOperator.WaitComponentTimeout", 30000);
+        JemmyProperties.setCurrentTimeout("DialogWaiter.WaitDialogTimeout", 30000);    
+        TestKit.closeProject(PROJECT_NAME);
+        
+        stream = new PrintStream(new File(getWorkDir(), getName() + ".log"));
+        VersioningOperator vo = VersioningOperator.invoke();
+        CheckoutWizardOperator co = CheckoutWizardOperator.invoke();
+        RepositoryStepOperator rso = new RepositoryStepOperator();       
+        
+        //create repository... 
+        new File(TMP_PATH).mkdirs();
+        new File(TMP_PATH + File.separator + WORK_PATH).mkdirs();
+        RepositoryMaintenance.deleteFolder(new File(TMP_PATH + File.separator + REPO_PATH));
+        RepositoryMaintenance.deleteFolder(new File(TMP_PATH + File.separator + WORK_PATH));
+        RepositoryMaintenance.createRepository(TMP_PATH + File.separator + REPO_PATH);   
+        RepositoryMaintenance.loadRepositoryFromFile(TMP_PATH + File.separator + REPO_PATH, getDataDir().getCanonicalPath() + File.separator + "repo_dump");      
+        rso.setRepositoryURL(RepositoryStepOperator.ITEM_FILE + RepositoryMaintenance.changeFileSeparator(TMP_PATH + File.separator + REPO_PATH, false));
+        
+        rso.next();
+        WorkDirStepOperator wdso = new WorkDirStepOperator();
+        wdso.setRepositoryFolder("trunk/" + PROJECT_NAME);
+        wdso.setLocalFolder(TMP_PATH + File.separator + WORK_PATH);
+        wdso.checkCheckoutContentOnly(false);
+        OutputTabOperator oto = new OutputTabOperator("file:///tmp/repo");
+        oto.getTimeouts().setTimeout("ComponentOperator.WaitStateTimeout", 30000);
+        oto.clear();
+        wdso.finish();
+        //open project
+        oto.waitText("Checking out... finished.");
+        NbDialogOperator nbdialog = new NbDialogOperator("Checkout Completed");
+        JButtonOperator open = new JButtonOperator(nbdialog, "Open Project");
+        open.push();
+        ProjectSupport.waitScanFinished();
+        
+        //modify, save file and invoke Diff
+        oto = new OutputTabOperator("file:///tmp/repo");
+        oto.getTimeouts().setTimeout("ComponentOperator.WaitStateTimeout", 30000);
+        oto.clear();
+        Node node = new Node(new SourcePackagesNode(PROJECT_NAME), "javaapp|Main.java");    
+        node.performPopupAction("Open");
+        EditorOperator eo = new EditorOperator("Main.java");
+        eo.deleteLine(2);
+        eo.insert(" insert", 5, 1);
+        eo.insert("\tSystem.out.println(\"\");\n", 19, 1);
+        eo.save();
+        node.performPopupAction("Subversion|Show Changes");
+        Thread.sleep(1000);
+        vo = VersioningOperator.invoke();
+        //Save action should change the file annotations
+        org.openide.nodes.Node nodeIDE = (org.openide.nodes.Node) node.getOpenideNode();        
+        String color = TestKit.getColor(nodeIDE.getHtmlDisplayName());
+        String status = TestKit.getStatus(nodeIDE.getHtmlDisplayName());
+        assertEquals("Wrong color of node - file color should be new!!!", TestKit.MODIFIED_COLOR, color);
+        assertEquals("Wrong annotation of node - file status should be new!!!", TestKit.MODIFIED_STATUS, status);
+        assertEquals("Wrong number of records in Versioning view!!!", 1, vo.tabFiles().getRowCount());
+        
+        node.performPopupAction("Subversion|Diff");
+        System.out.println("DIFF action doesn't print information into OUTPUT!!!");
+        //oto.waitText("Annotating... finished.");
+        Thread.sleep(2000);
+        
+        DiffOperator diffOp = new DiffOperator("Main.java");
+        //
+        try {
+            TimeoutExpiredException afee = null;
+            diffOp.next();
+            diffOp.next();
+            try {
+                diffOp.next();
+            } catch (TimeoutExpiredException e) {
+                afee = e;
+            }
+            assertNotNull("TimeoutExpiredException was expected.", afee);
+
+            //verify previous button
+            afee = null;
+            diffOp.previous();
+            diffOp.previous();
+            try {
+                diffOp.previous();
+            } catch (TimeoutExpiredException e) {
+                afee = e;
+            }
+            assertNotNull("TimeoutExpiredException was expected.", afee);
+        } catch (Exception e) {
+            System.out.println("Problem with buttons of differences");
+        }    
+        
+        TestKit.removeAllData(PROJECT_NAME);
+        stream.flush();
+        stream.close();
+    }
+}
