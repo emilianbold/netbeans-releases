ddb03787243ed00ee42700f68225542ea8e7d968
jglick <jglick@netbeans.org>
2006-07-28 11:42:57 +0000

    #81139 cont'd: use new Ant API in 1.7.0.

--- o.apache.tools.ant.module/src-bridge/org/apache/tools/ant/module/bridge/impl/NbInputHandler.java
+++ o.apache.tools.ant.module/src-bridge/org/apache/tools/ant/module/bridge/impl/NbInputHandler.java
@@ -21,6 +21,7 @@ package org.apache.tools.ant.module.bridge.impl;
 
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
 import javax.swing.JComboBox;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
@@ -33,6 +34,7 @@ import org.apache.tools.ant.input.MultipleChoiceInputRequest;
 import org.openide.DialogDescriptor;
 import org.openide.DialogDisplayer;
 import org.openide.NotifyDescriptor;
+import org.openide.util.Exceptions;
 import org.openide.util.HelpCtx;
 import org.openide.util.NbBundle;
 
@@ -40,24 +42,24 @@ import org.openide.util.NbBundle;
  * @author David Konecny, Dusan Balek, Jesse Glick
  */
 final class NbInputHandler implements InputHandler {
-    
+
     private JComboBox combo = null;
     private JTextField input = null;
     private final Runnable interestingOutputCallback;
-    
+
     public NbInputHandler(Runnable interestingOutputCallback) {
         this.interestingOutputCallback = interestingOutputCallback;
     }
-    
+
     public void handleInput(InputRequest request) throws BuildException {
         interestingOutputCallback.run();
-        
+
         // #30196 - for one Ant script containing several <input> tasks there will be created
         // just one instance of the NbInputHandler. So it is necessary to cleanup the instance
         // used by the previous <input> task first.
         combo = null;
         input = null;
-        
+
         JPanel panel = createPanel(request);
         DialogDescriptor dlg = new DialogDescriptor(panel,
         NbBundle.getMessage(NbInputHandler.class, "TITLE_input_handler")); //NOI18N
@@ -75,58 +77,69 @@ final class NbInputHandler implements InputHandler {
             request.setInput(value);
         } while (!request.isInputValid());
     }
-    
+
     private JPanel createPanel(InputRequest request) {
-        
+
         JPanel pane = new JPanel();
         pane.setLayout(new GridBagLayout());
-        
-        JLabel jLabel1 = new javax.swing.JLabel(request.getPrompt());
-        GridBagConstraints gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
-        gridBagConstraints.insets = new java.awt.Insets(12, 12, 11, 6);
+
+        JLabel jLabel1 = new JLabel(request.getPrompt());
+        GridBagConstraints gridBagConstraints = new GridBagConstraints();
+        gridBagConstraints.anchor = GridBagConstraints.WEST;
+        gridBagConstraints.insets = new Insets(12, 12, 11, 6);
         pane.add(jLabel1, gridBagConstraints);
-        
+
         JComponent comp = null;
         if (request instanceof MultipleChoiceInputRequest) {
-            combo = new javax.swing.JComboBox(((MultipleChoiceInputRequest)request).getChoices());
+            combo = new JComboBox(((MultipleChoiceInputRequest)request).getChoices());
             if (defaultValue != null && defaultValue.length() > 0) {
-                combo.setSelectedItem(defaultValue);
+                combo.setSelectedItem(getDefaultValue(request));
             }
             comp = combo;
         } else {
-            input = new JTextField(defaultValue, 25);
+            input = new JTextField(getDefaultValue(request), 25);
             comp = input;
         }
-        
+
         comp.getAccessibleContext().setAccessibleDescription(
         NbBundle.getMessage(NbInputHandler.class, "ACSD_input_handler")); // NOI18N
-        
-        gridBagConstraints = new java.awt.GridBagConstraints();
+
+        gridBagConstraints = new GridBagConstraints();
         gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 0;
-        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.fill = GridBagConstraints.BOTH;
         gridBagConstraints.weightx = 1.0;
-        gridBagConstraints.insets = new java.awt.Insets(12, 6, 11, 6);
+        gridBagConstraints.insets = new Insets(12, 6, 11, 6);
         pane.add(comp, gridBagConstraints);
-        
+
         jLabel1.setLabelFor(comp);
         if (request.getPrompt().length() > 0)
             jLabel1.setDisplayedMnemonic(request.getPrompt().charAt(0));
-        
+
         pane.getAccessibleContext().setAccessibleName(
         NbBundle.getMessage(NbInputHandler.class, "TITLE_input_handler")); // NOI18N
         pane.getAccessibleContext().setAccessibleDescription(
         NbBundle.getMessage(NbInputHandler.class, "ACSD_input_handler")); // NOI18N
-        
+
         HelpCtx.setHelpIDString(pane, "org.apache.tools.ant.module.run.NBInputHandler"); // NOI18N
-        
+
         return pane;
     }
-    
+
     private static String defaultValue;
     static void setDefaultValue(String d) {
         defaultValue = d;
     }
-    
+    private static String getDefaultValue(InputRequest req) {
+        try {
+            // Ant 1.7.0+
+            return (String) InputRequest.class.getMethod("getDefaultValue").invoke(req); // NOI18N
+        } catch (NoSuchMethodException e) {
+            // OK, Ant 1.6.5 or earlier
+        } catch (Exception e) {
+            Exceptions.printStackTrace(e);
+        }
+        return defaultValue;
+    }
+
 }
