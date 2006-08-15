ee078e9d6d9b7ebfd6b1adb177404beddfd13b87
jtulach <jtulach@netbeans.org>
2006-08-15 05:48:19 +0000

    Pointer to the right bundle with the UI_ key

--- openide.windows/src/org/openide/windows/TopComponent.java
+++ openide.windows/src/org/openide/windows/TopComponent.java
@@ -888,7 +888,7 @@ public class TopComponent extends JComponent implements Externalizable, Accessib
             if (action.isEnabled()) {
                 LogRecord rec = new LogRecord(Level.FINER, "UI_ACTION_KEY_PRESS"); // NOI18N
                 rec.setParameters(new Object[] { ks, ks.toString(), action, action.getClass().getName(), action.getValue(Action.NAME) });
-                rec.setResourceBundle(NbBundle.getBundle(Actions.class));
+                rec.setResourceBundle(NbBundle.getBundle(TopComponent.class));
                 rec.setLoggerName(UILOG.getName());
                 UILOG.log(rec);
 
