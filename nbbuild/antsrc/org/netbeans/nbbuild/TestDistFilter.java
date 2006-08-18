fa925e64fb2294a4622dd6ac84fbbed7c4b4e6a5
pzajac <pzajac@netbeans.org>
2006-08-18 09:34:43 +0000

    removing

---
+++ nbbuild/antsrc/org/netbeans/nbbuild/TestDistFilter.java
@@ -0,0 +1,294 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.nbbuild;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.apache.tools.ant.*;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+/**
+ * It scans test distribution ant sets to property with name defined by param 
+ * 'testListProperty'path with filtered test. The TestDistFilter is used for running tests 
+ * in test distribution.
+ * <br>
+ * 
+ * Parameters :
+ * <ul>
+ *    <li>harness - type of harness (junit,xtest). For junit harness are 
+ *     scanned only tests with unit tes type and 'code' executor name inside 
+ *     cfg-unit.xml.
+ *    <li>testtype - unit|qa-functional|all testtype  
+ *    <li>attribs - xtest.attribs filter (attribs are declared in cfg-<xxx>.xml file  
+ *    <li>testlistproperty  - store to property path with test folders (separated by ':')
+ *    <li>testdistdir - root folder with test distribution
+ * </ul>
+ */
+public class TestDistFilter extends Task {
+    public static final String TYPE_ALL = "all";
+    public static final String TYPE_UNIT = "unit";
+    public static final String TYPE_QA_FUNCTIONAL = "qa-functional";
+    
+    public static final String HARNESS_JUNIT = "junit";
+    public static final String HARNESS_XTEST = "xtest";
+
+    private File testDistDir;
+    Set/*<TestConf>*/ possibleTests = new HashSet();
+    // "unit|qa_functional|all
+    // default value is all
+    private String testtype = TYPE_ALL;
+    private String harness = HARNESS_XTEST;
+    // xtest attribs
+    private String attribs ;
+    private String testListProperty;
+    // TODO customize method names to match custom task
+    // property and type (handled by inner class) names
+    
+    /** represents a test directory
+     */
+    private static class TestConf {
+        File moduleDir;
+        boolean unit;
+        TestConf(File moduleDir,boolean unit) {
+            this.moduleDir = moduleDir;
+            this.unit = unit;
+        }
+
+        public int hashCode() {
+            return moduleDir.hashCode();
+        }
+        public boolean  equals(Object obj) {
+            return  (obj instanceof TestConf) && moduleDir.equals(((TestConf)obj).moduleDir); 
+        }
+      
+        
+        /** check if cfg-<testtype>.xml contains xtest.attribs, 
+         *  ide executor is ignored for junit
+         */
+        boolean matchAttribs(String harness,String attribs) {
+            Element config;
+            try {
+                config = getConfig();
+            } catch (SAXException ex) {
+                throw new BuildException(ex);
+            } catch (ParserConfigurationException ex) {
+                throw new BuildException(ex);
+            } catch (IOException ex) {
+                throw new BuildException(ex);
+            }
+            if (config == null) {
+                return false;
+            }
+            boolean junit = HARNESS_JUNIT.equals(harness);
+            NodeList elements = config.getElementsByTagName("testbag");
+            for (int n  = 0 ; n < elements.getLength() ; n++) {
+                Element testbag = (Element) elements.item(n);
+                if (junit && "ide".equals(testbag.getAttribute("executor"))) {
+                    continue;
+                }
+                if (testAttr(testbag.getAttribute("testattribs"),attribs)) {
+                    return true;
+                }
+            } 
+            return false;
+        }
+        private static boolean testAttr(String xmlAttr,String userAttr) {
+            if (userAttr == null) {
+                return true;
+            }
+            if (xmlAttr == null) {
+                return false;
+            }
+            StringTokenizer tokenizer = new StringTokenizer(xmlAttr,"&|,");
+            while (tokenizer.hasMoreTokens()) {
+                if (tokenizer.nextToken().trim().equals(userAttr)) {
+                    return true;
+                } 
+            }
+            return false;
+        }
+        File getModuleDir() {
+            return moduleDir;
+        }
+
+        private Element getConfig() throws ParserConfigurationException, SAXException, IOException {
+            String name = (unit) ? "cfg-unit.xml" : "cfg-qa-functional.xml";
+            File xml = new File(getModuleDir(),name);
+            if (!xml.exists()) {
+                return null;
+            }
+            return getDocumentBuilder().parse(xml).getDocumentElement();
+        }
+        
+    }
+
+    
+    public void execute() throws BuildException {
+          possibleTests.clear();
+          if (getTestListProperty() == null) {
+              throw new BuildException("Param testlistproperty is not defined.");
+          }
+          if (getTestDistDir() == null || !getTestDistDir().exists()) {
+              throw new BuildException("Param testdistdir is not defined.");
+          }
+          if ("".equals(attribs)) {
+              attribs = null;
+          }
+          String tt = getTesttype();
+          if (getHarness().equals(HARNESS_JUNIT)) { 
+              findCodeTests(HARNESS_JUNIT,TYPE_UNIT,getAttribs());
+          } else {
+              if (tt.equals(TYPE_QA_FUNCTIONAL) || tt.equals(TYPE_ALL)) {
+                  findCodeTests(HARNESS_XTEST,TYPE_QA_FUNCTIONAL,getAttribs());
+              }
+              if (tt.equals(TYPE_UNIT) || tt.equals(TYPE_ALL)) {
+                  findCodeTests(HARNESS_XTEST,TYPE_UNIT,getAttribs());
+              }
+          }
+          define(getTestListProperty(),getTestList());
+          
+    }
+    /** get path with test dirs separated by :
+     */
+    private String getTestList() {
+        StringBuffer path = new StringBuffer();
+        for (Iterator it = possibleTests.iterator() ; it.hasNext() ; ) {
+            TestConf tc = (TestConf)it.next();
+            if (path.length() > 0) {
+                path.append(':');
+            }
+            path.append(tc.getModuleDir().getAbsolutePath());
+        }
+        return path.toString();
+    }
+    private void define(String prop, String val) {
+        log("Setting " + prop + "=" + val, Project.MSG_VERBOSE);
+        String old = getProject().getProperty(prop);
+        if (old != null && !old.equals(val)) {
+            getProject().log("Warning: " + prop + " was already set to " + old, Project.MSG_WARN);
+        }
+        getProject().setNewProperty(prop, val);
+    }
+
+
+    public String getTesttype() {
+        return testtype;
+    }
+
+    public void setTesttype(String testtype) {
+        this.testtype = testtype;
+    }
+
+    public String getHarness() {
+        return harness;
+    }
+
+    public void setHarness(String harness) {
+        this.harness = harness;
+    }
+
+    public String getAttribs() {
+        return attribs;
+    }
+
+    public void setAttribs(String attribs) {
+        this.attribs = attribs;
+    }
+
+    public String getTestListProperty() {
+        return testListProperty;
+    }
+
+    public void setTestListProperty(String testListProperty) {
+        this.testListProperty = testListProperty;
+    }
+
+    private void findCodeTests(String harness, String type, String string) {
+          List tests = getTestList(type);
+          for (int i = 0 ; i < tests.size() ; i++) {
+              TestConf test = (TestConf)tests.get(i);
+              if (test.matchAttribs(harness,attribs)) {
+                  possibleTests.add(test);
+              }
+          }
+    }
+
+    private List getTestList(String testtype) {
+        File root = new File (getTestDistDir(),testtype);
+        List /*<TestConf>*/ testList = new ArrayList();
+        if (!root.exists()) {
+            return Collections.EMPTY_LIST;
+        }
+        File clusters[] = root.listFiles();
+        for (int c = 0 ; c < clusters.length ; c++) {
+            File cluster = clusters[c];
+            if (cluster.isDirectory()) {
+                File modules[] = cluster.listFiles();
+                for (int m = 0 ; m < modules.length ; m++) {
+                    File module = modules[m];
+                    if (module.isDirectory()) {
+                        testList.add(new TestConf(module,testtype.equals(TYPE_UNIT)));
+                    }
+                }
+            }
+        }
+        return testList;
+    }
+
+    
+    // create document builder with empty EntityResolver
+    //
+    private static DocumentBuilder db;
+    private static DocumentBuilder getDocumentBuilder() throws ParserConfigurationException {
+        if (db == null) {
+           db = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+           db.setEntityResolver(new EntityResolver() {
+               public InputSource resolveEntity(String publicId, String systemId) throws SAXException,IOException {
+                   return new InputSource(new StringReader(""));
+               }
+            
+           });
+        }
+        return db;
+    }
+
+    public File getTestDistDir() {
+        return testDistDir;
+    }
+
+    public void setTestDistDir(File testDistDir) {
+        this.testDistDir = testDistDir;
+    }
+}
---
+++ nbbuild/test/unit/src/org/netbeans/nbbuild/TestDistFilterTest.java
@@ -0,0 +1,196 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * 'Portions Copyrighted [year] [name of copyright owner]'
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.nbbuild;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.HashSet;
+import java.util.Set;
+import junit.framework.*;
+import org.apache.tools.ant.Project;
+import org.netbeans.junit.*;
+
+/**
+ *
+ * @author pzajac
+ */
+public class TestDistFilterTest extends NbTestCase {
+    private static final String ORG_OPENIDE_UNIT = "unit/platform5/org-openide";
+    private static final String ORG_OPENIDE_FS = "unit/platform5/org-openide-fs";
+    private static final String ORG_OPENIDE_LOADERS = "unit/platform5/org-openide-loaders";
+    private static final String ORG_OPENIDE_FS_QA = "qa-functional/platform5/org-openide-fs";   
+    public TestDistFilterTest(java.lang.String testName) {
+        super(testName);
+    }
+    
+    public void testDistFilter() throws IOException {
+        TestDistFilter filter = new TestDistFilter();
+        filter.setTestDistDir(getWorkDir());
+        createModule(ORG_OPENIDE_UNIT,createConfig("unstable","code")); 
+        filter.setAttribs("stable");
+        filter.setHarness("junit");
+        Project prj = getProject();
+        filter.setProject(prj);
+        filter.setTesttype("unit");
+        
+        filter.setTestListProperty("list.prop");
+        filter.execute();
+        assertProperty(prj,"list.prop",new String[]{});
+        
+        createModule(ORG_OPENIDE_UNIT,createConfig("stable","code")); 
+        
+        filter.setTestListProperty("list.prop1");
+        filter.execute();
+        assertProperty(prj,"list.prop1",new String[]{ORG_OPENIDE_UNIT});
+        
+        createModule(ORG_OPENIDE_FS,createConfig("stable","ide")); 
+        filter.setTestListProperty("list.prop2");
+        filter.execute();
+        assertProperty(prj,"list.prop2",new String[]{ORG_OPENIDE_UNIT});
+ 
+        filter.setTestListProperty("list.prop3");
+        createModule(ORG_OPENIDE_LOADERS,createConfig("stable","code")); 
+        filter.execute();
+        assertProperty(prj,"list.prop3",new String[]{ORG_OPENIDE_UNIT,ORG_OPENIDE_LOADERS});
+
+        createModule(ORG_OPENIDE_FS_QA,createConfig("stable","code"));
+        filter.setTestListProperty("list.prop4");
+        filter.execute();
+        assertProperty(prj,"list.prop4",new String[]{ORG_OPENIDE_UNIT,ORG_OPENIDE_LOADERS});
+
+        filter.setTestListProperty("list.prop5");
+        createModule(ORG_OPENIDE_LOADERS,createConfig("unstable","code")); 
+        filter.execute();
+        assertProperty(prj,"list.prop5",new String[]{ORG_OPENIDE_UNIT});
+   
+        filter.setHarness("xtest");
+        filter.setTestListProperty("list.prop6");
+        filter.execute();
+        assertProperty(prj,"list.prop6",new String[]{ORG_OPENIDE_UNIT,ORG_OPENIDE_FS});
+
+        filter.setTestListProperty("list.prop7");
+        filter.setTesttype("qa-functional");
+        filter.execute();
+        assertProperty(prj,"list.prop7",new String[]{ORG_OPENIDE_FS_QA});        
+   }
+    
+   public void testAllTestTypes() throws Exception {
+        TestDistFilter filter = new TestDistFilter();
+        filter.setTestDistDir(getWorkDir());
+        Project prj = getProject();
+        filter.setProject(prj);
+        filter.setTesttype("all");
+        filter.setHarness("xtest");
+        createModule(ORG_OPENIDE_UNIT,createConfig("stable","code")); 
+        createModule(ORG_OPENIDE_FS_QA,createConfig("stable","code"));
+        filter.setTestListProperty("list.prop");
+        filter.execute();
+        assertProperty(prj,"list.prop",new String[]{ORG_OPENIDE_UNIT,ORG_OPENIDE_FS_QA});
+   }
+   
+   public void testComplexAttribs() throws Exception {
+        TestDistFilter filter = new TestDistFilter();
+        filter.setTestDistDir(getWorkDir());
+        Project prj = getProject();
+        createModule(ORG_OPENIDE_UNIT,createConfig("stable|xx , aa","code")); 
+        filter.setProject(prj);
+        filter.setTesttype("all");
+        filter.setHarness("xtest");
+        filter.setAttribs("stable");
+        filter.setTestListProperty("list.prop");
+        filter.execute();
+        assertProperty(prj,"list.prop",new String[]{ORG_OPENIDE_UNIT});
+       
+        filter.setAttribs("xx");
+        filter.setTestListProperty("list.prop2");
+        filter.execute();
+        assertProperty(prj,"list.prop2",new String[]{ORG_OPENIDE_UNIT});
+ 
+        filter.setAttribs("aa");
+        filter.setTestListProperty("list.prop3");
+        filter.execute();
+        assertProperty(prj,"list.prop3",new String[]{ORG_OPENIDE_UNIT});
+
+        filter.setAttribs("");
+        filter.setTestListProperty("list.prop4");
+        filter.execute();
+        assertProperty(prj,"list.prop4",new String[]{ORG_OPENIDE_UNIT});
+        
+        filter.setAttribs(null); 
+        filter.setTestListProperty("list.prop5");
+        filter.execute();
+        assertProperty(prj,"list.prop5",new String[]{ORG_OPENIDE_UNIT});
+
+        filter.setAttribs("nonsence");
+        filter.setTestListProperty("list.prop6");
+        filter.execute();
+        assertProperty(prj,"list.prop6",new String[]{});
+   }
+    private String createConfig(String attribs,String executor) {   
+        return "<?xml version='1.0' encoding='UTF-8'?>\n" +
+                "<!DOCTYPE mconfig PUBLIC '-//NetBeans//DTD XTest cfg 1.0//EN' 'http://www.netbeans.org/dtds/xtest-cfg-1_0.dtd'>\n" +
+                "<mconfig name='Configuration of refactoring unit tests'>\n" +
+                    "<testbag testattribs='" + attribs + "' executor='" + executor + "' name='Refactoring unit tests'>\n" +
+                        "<testset dir='unit/src'>\n" +
+                            "<patternset>\n" +
+                                "<include name='org/netbeans/test/refactoring/encapsulate/EncapsulateTest.class'/>\n" +
+                            "</patternset>\n" +
+                        "</testset>\n" +
+                    "</testbag>\n" +
+                "</mconfig>\n";
+    }
+
+    private void createModule(String path, String xml) throws IOException {
+        File dir = new File(getWorkDir(),path);
+        dir.mkdirs();
+        File cfg = new File(dir,(path.startsWith("unit")) ? "cfg-unit.xml" : "cfg-qa-functional.xml");
+        PrintStream ps = new PrintStream(cfg);
+        try {
+            ps.print(xml);
+        } finally {
+            ps.close();
+        }
+    }
+
+    private Project getProject() throws IOException {
+        Project project = new Project();
+        project.setBaseDir(getWorkDir());
+        return project;
+    }
+
+    private void assertProperty(Project prj, String propName, String modules[]) throws IOException {
+        String listModules = prj.getProperty(propName);
+        assertNotNull("prop " + propName + " was not defined",listModules);
+        log(" listModules " + listModules);
+        String arrayModules[] = (listModules.length() == 0) ? new String[0] :listModules.split(":");
+        Set set1 = new HashSet();
+        for (int i = 0 ; i < arrayModules.length ; i++) {
+            log(i + " = " +  arrayModules[i]);
+            set1.add(new File(arrayModules[i])); 
+        }
+        Set set2 = new HashSet();
+        for (int i = 0 ; i < modules.length ; i++) {
+            set2.add(new File(getWorkDir(),modules[i]));
+        }
+        assertEquals("paths length",set2.size(),set1.size());
+        assertEquals("Different paths: ", set2,set1);
+    }
+}
