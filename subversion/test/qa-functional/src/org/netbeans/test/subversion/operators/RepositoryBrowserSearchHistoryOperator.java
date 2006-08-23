30acdab11d7cf838172a703befa0ce8071861dd4
ppis <ppis@netbeans.org>
2006-08-23 16:12:37 +0000

    no message

---
+++ subversion/test/qa-functional/src/org/netbeans/test/subversion/operators/RepositoryBrowserSearchHistoryOperator.java
@@ -0,0 +1,135 @@
+/*
+ * RepositoryBrowserOperator.java
+ *
+ * Created on 19/04/06 13:26
+ */
+package org.netbeans.test.subversion.operators;
+
+import org.netbeans.jellytools.NbDialogOperator;
+import org.netbeans.jellytools.nodes.Node;
+import org.netbeans.jemmy.operators.*;
+
+/** Class implementing all necessary methods for handling "RepositoryBrowserOperator" NbDialog.
+ *
+ * @author peter
+ * @version 1.0
+ */
+public class RepositoryBrowserSearchHistoryOperator extends NbDialogOperator {
+
+    /** Creates new RepositoryBrowserOperator that can handle it.
+     */
+    public RepositoryBrowserSearchHistoryOperator() {
+        super("Browse Repository");
+    }
+
+    private JTreeOperator _tree;
+    private JLabelOperator _lblSpecifyFolderToCheckout;
+    private JButtonOperator _btOK;
+    private JButtonOperator _btCancel;
+    private JButtonOperator _btHelp;
+
+
+    //******************************
+    // Subcomponents definition part
+    //******************************
+
+    /** Tries to find null TreeView$ExplorerTree in this dialog.
+     * @return JTreeOperator
+     */
+    public JTreeOperator tree() {
+        if (_tree == null) {
+            _tree= new JTreeOperator(this);
+        }
+        return _tree;
+    }
+
+    /** Tries to find "Specify Folder to checkout:" JLabel in this dialog.
+     * @return JLabelOperator
+     */
+    public JLabelOperator lblSpecifyFolderToCheckout() {
+        if (_lblSpecifyFolderToCheckout==null) {
+            _lblSpecifyFolderToCheckout = new JLabelOperator(this, "Choose");
+        }
+        return _lblSpecifyFolderToCheckout;
+    }
+    
+    //****************************************
+    // Low-level functionality definition part
+    //****************************************
+
+    /** Selects a folder denoted by path.
+     * @param path path to folder without root (e.g. "folder|subfolder")
+     */
+    public void selectFolder(String path) {
+        new Node(tree(), path).select();
+    }
+    
+    /** Tries to find "OK" JButton in this dialog.
+     * @return JButtonOperator
+     */
+    public JButtonOperator btOK() {
+        if (_btOK==null) {
+            _btOK = new JButtonOperator(this, "OK");
+        }
+        return _btOK;
+    }
+
+    /** Tries to find "Cancel" JButton in this dialog.
+     * @return JButtonOperator
+     */
+    public JButtonOperator btCancel() {
+        if (_btCancel==null) {
+            _btCancel = new JButtonOperator(this, "Cancel");
+        }
+        return _btCancel;
+    }
+
+    /** Tries to find "Help" JButton in this dialog.
+     * @return JButtonOperator
+     */
+    public JButtonOperator btHelp() {
+        if (_btHelp==null) {
+            _btHelp = new JButtonOperator(this, "Help");
+        }
+        return _btHelp;
+    }
+
+
+    //****************************************
+    // Low-level functionality definition part
+    //****************************************
+
+    /** clicks on "OK" JButton
+     */
+    public void ok() {
+        btOK().push();
+    }
+
+    /** clicks on "Cancel" JButton
+     */
+    public void cancel() {
+        btCancel().push();
+    }
+
+    /** clicks on "Help" JButton
+     */
+    public void help() {
+        btHelp().push();
+    }
+
+
+    //*****************************************
+    // High-level functionality definition part
+    //*****************************************
+
+    /** Performs verification of RepositoryBrowserOperator by accessing all its components.
+     */
+    public void verify() {
+        tree();
+        lblSpecifyFolderToCheckout();
+        btOK();
+        btCancel();
+        btHelp();
+    }
+}
+
