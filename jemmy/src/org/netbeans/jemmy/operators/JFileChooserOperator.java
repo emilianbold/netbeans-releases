4b00274b589a43bb78ac6730e5b0c9262d86801e
jskrivanek <jskrivanek@netbeans.org>
2006-09-05 08:15:12 +0000

    Try-catch extended to catch NPE from both calls of list.getCellBounds().

--- jemmy/src/org/netbeans/jemmy/operators/JFileChooserOperator.java
+++ jemmy/src/org/netbeans/jemmy/operators/JFileChooserOperator.java
@@ -2,24 +2,24 @@
  * The contents of this file are subject to the terms of the Common Development
  * and Distribution License (the License). You may not use this file except in
  * compliance with the License.
- *
+ * 
  * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
  * or http://www.netbeans.org/cddl.txt.
- *
+ * 
  * When distributing Covered Code, include this CDDL Header Notice in each file
  * and include the License file at http://www.netbeans.org/cddl.txt.
  * If applicable, add the following below the CDDL Header, with the fields
  * enclosed by brackets [] replaced by your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- *
+ * 
  * The Original Software is the Jemmy library.
  * The Initial Developer of the Original Software is Alexandre Iline.
  * All Rights Reserved.
  *
  * Contributor(s): Alexandre Iline.
- *
+ * 
  * $Id$ $Revision$ $Date$
- *
+ * 
  */
 
 package org.netbeans.jemmy.operators;
@@ -1120,16 +1120,16 @@ public class JFileChooserOperator extends JComponentOperator
 			return("");
 		    }
 		    int current = (param != null) ? ((Integer)param).intValue() : 0;
-		    if(list.getCellBounds(current, current) != null) {
-                        try {
+                    try {
+                        if(list.getCellBounds(current, current) != null) {
                             return(list.getCellBounds(last_one, last_one));
-                        } catch(NullPointerException e) {
-                            //somtimes thrown when item exists but not painted
+                        } else {
                             return(null);
                         }
-		    } else {
-			return(null);
-		    }
+                    } catch(NullPointerException e) {
+                        //sometimes thrown from list.getCellBounds when item exists but not painted
+                        return(null);
+                    }
 		}
 		public String getDescription() {
 		    return("List drawed");
