e1e4cd604531aa4a859005762e490985124eda0f
alex_grk <devnull@localhost>
2006-09-08 19:47:16 +0000

    added  validate(); to removeNodeWithEdges (N node)

--- api.visual/src/org/netbeans/api/visual/graph/GraphScene.java
+++ api.visual/src/org/netbeans/api/visual/graph/GraphScene.java
@@ -65,8 +65,10 @@ public abstract class GraphScene<N, E> extends ObjectScene {
 
     public final void removeNodeWithEdges (N node) {
         for (E edge : findNodeEdges (node, true, true))
-            if (isEdge (edge))
+            if (isEdge (edge)){
                 removeEdge (edge);
+                validate();
+            }
         removeNode (node);
     }
 
