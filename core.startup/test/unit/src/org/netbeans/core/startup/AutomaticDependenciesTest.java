acc5a364cc0b6d1e1f81f9e569e3c7cbb2f439d5
rkubacki <rkubacki@netbeans.org>
2006-09-08 08:35:08 +0000

    less warnings when running tests

--- core.startup/test/unit/src/org/netbeans/core/startup/AutomaticDependenciesTest.java
+++ core.startup/test/unit/src/org/netbeans/core/startup/AutomaticDependenciesTest.java
@@ -45,12 +45,12 @@ public class AutomaticDependenciesTest extends NbTestCase {
     }
     
     public void testBasicOperation() throws Exception {
-        Set deps = new HashSet(); // Set<Dependency>
+        Set<Dependency> deps = new HashSet<Dependency>();
         deps.addAll(Dependency.create(Dependency.TYPE_JAVA, "Java > 1.3"));
         deps.addAll(Dependency.create(Dependency.TYPE_MODULE, "unrelated"));
         deps.addAll(Dependency.create(Dependency.TYPE_MODULE, "orig > 1.0"));
         deps.addAll(Dependency.create(Dependency.TYPE_MODULE, "orig2/1 > 1.5.1"));
-        Set _deps = new HashSet(deps);
+        Set<Dependency> _deps = new HashSet<Dependency>(deps);
         AutomaticDependencies.Report rep = ad.refineDependenciesAndReport("random", _deps);
         assertEquals("Java > 1.3, Nue, nue > 1.0, nue2 > 1.0, orig > 1.0, orig2/1 > 1.5.1, unrelated", normal(_deps));
         assertTrue(rep.isModified());
@@ -58,74 +58,74 @@ public class AutomaticDependenciesTest extends NbTestCase {
     }
     
     public void testExcludes() throws Exception {
-        Set deps = new HashSet(); // Set<Dependency>
+        Set<Dependency> deps = new HashSet<Dependency>();
         deps.addAll(Dependency.create(Dependency.TYPE_MODULE, "orig > 1.0"));
-        Set _deps = new HashSet(deps);
+        Set<Dependency> _deps = new HashSet<Dependency>(deps);
         ad.refineDependencies("a", _deps);
         assertEquals("orig > 1.0", normal(_deps));
-        _deps = new HashSet(deps);
+        _deps = new HashSet<Dependency>(deps);
         ad.refineDependencies("a.x", _deps);
         assertEquals("Nue, nue > 1.0, orig > 1.0", normal(_deps));
-        _deps = new HashSet(deps);
+        _deps = new HashSet<Dependency>(deps);
         ad.refineDependencies("b", _deps);
         assertEquals("orig > 1.0", normal(_deps));
-        _deps = new HashSet(deps);
+        _deps = new HashSet<Dependency>(deps);
         ad.refineDependencies("b.x", _deps);
         assertEquals("orig > 1.0", normal(_deps));
     }
     
     public void testVersionSensitivity() throws Exception {
-        Set deps = new HashSet(); // Set<Dependency>
+        Set<Dependency> deps = new HashSet<Dependency>();
         deps.addAll(Dependency.create(Dependency.TYPE_MODULE, "orig > 1.1"));
         deps.addAll(Dependency.create(Dependency.TYPE_MODULE, "orig2/2 > 0.1"));
-        Set _deps = new HashSet(deps);
+        Set<Dependency> _deps = new HashSet<Dependency>(deps);
         ad.refineDependencies("random", _deps);
         assertEquals("orig > 1.1, orig2/2 > 0.1", normal(_deps));
     }
     
     public void testCancellations() throws Exception {
-        Set deps = new HashSet(); // Set<Dependency>
+        Set<Dependency> deps = new HashSet<Dependency>();
         deps.addAll(Dependency.create(Dependency.TYPE_PACKAGE, "javax.death[AbstractCoffin] > 1.0"));
-        Set _deps = new HashSet(deps);
+        Set<Dependency> _deps = new HashSet<Dependency>(deps);
         AutomaticDependencies.Report rep = ad.refineDependenciesAndReport("random", _deps);
         assertEquals("javax.death/1 > 1.0", normal(_deps));
         assertEquals("message-1", normalMessages(rep));
     }
     
     public void testMerges() throws Exception {
-        Set deps = new HashSet(); // Set<Dependency>
+        Set<Dependency> deps = new HashSet<Dependency>();
         deps.addAll(Dependency.create(Dependency.TYPE_PACKAGE, "javax.death[AbstractCoffin] > 1.0"));
         deps.addAll(Dependency.create(Dependency.TYPE_MODULE, "javax.death/1 > 1.0"));
-        Set _deps = new HashSet(deps);
+        Set<Dependency> _deps = new HashSet<Dependency>(deps);
         ad.refineDependencies("random", _deps);
         assertEquals("javax.death/1 > 1.0", normal(_deps));
-        deps = new HashSet(); // Set<Dependency>
+        deps = new HashSet<Dependency>();
         deps.addAll(Dependency.create(Dependency.TYPE_PACKAGE, "javax.death[AbstractCoffin] > 1.0"));
         deps.addAll(Dependency.create(Dependency.TYPE_MODULE, "javax.death/1 > 0.3"));
-        _deps = new HashSet(deps);
+        _deps = new HashSet<Dependency>(deps);
         ad.refineDependencies("random", _deps);
         assertEquals("javax.death/1 > 1.0", normal(_deps));
-        deps = new HashSet(); // Set<Dependency>
+        deps = new HashSet<Dependency>(); // Set<Dependency>
         deps.addAll(Dependency.create(Dependency.TYPE_PACKAGE, "[javax.death.AbstractCoffin]"));
         deps.addAll(Dependency.create(Dependency.TYPE_MODULE, "javax.death/1 > 0.3"));
-        _deps = new HashSet(deps);
+        _deps = new HashSet<Dependency>(deps);
         ad.refineDependencies("random", _deps);
         assertEquals("javax.death/1 > 1.0", normal(_deps));
-        deps = new HashSet(); // Set<Dependency>
+        deps = new HashSet<Dependency>(); // Set<Dependency>
         deps.addAll(Dependency.create(Dependency.TYPE_PACKAGE, "javax.death[AbstractCoffin] > 1.0"));
         deps.addAll(Dependency.create(Dependency.TYPE_MODULE, "javax.death/1 > 1.3"));
-        _deps = new HashSet(deps);
+        _deps = new HashSet<Dependency>(deps);
         ad.refineDependencies("random", _deps);
         assertEquals("javax.death/1 > 1.3", normal(_deps));
     }
     
     public void testExclusionOfImplDeps() throws Exception {
         // Cf. #46961.
-        Set/*<Dependency>*/ deps = new HashSet();
+        Set<Dependency> deps = new HashSet<Dependency>();
         deps.addAll(Dependency.create(Dependency.TYPE_MODULE, "orig = some-orig-version"));
         deps.addAll(Dependency.create(Dependency.TYPE_MODULE, "orig2/1 = some-orig2-version"));
         deps.addAll(Dependency.create(Dependency.TYPE_MODULE, "orig2/0 = some-ancient-orig2-version"));
-        Set _deps = new HashSet(deps);
+        Set<Dependency> _deps = new HashSet<Dependency>(deps);
         AutomaticDependencies.Report rep = ad.refineDependenciesAndReport("random", _deps);
         assertEquals("orig = some-orig-version, orig2/0 = some-ancient-orig2-version, orig2/1 = some-orig2-version", normal(_deps));
         assertFalse(rep.isModified());
@@ -135,67 +135,67 @@ public class AutomaticDependenciesTest extends NbTestCase {
     public void testCanUpgradeMajorVersions() throws Exception {
         // Need to be able to say that anything using module/0 (w/ or w/o spec) should use module/1 instead.
         // Plain dep on old module is upgraded:
-        Set/*<Dependency>*/ deps = new HashSet();
+        Set<Dependency> deps = new HashSet<Dependency>();
         deps.addAll(Dependency.create(Dependency.TYPE_MODULE, "stabilized/0"));
-        Set _deps = new HashSet(deps);
+        Set<Dependency> _deps = new HashSet<Dependency>(deps);
         AutomaticDependencies.Report rep = ad.refineDependenciesAndReport("random", _deps);
         assertEquals("stabilized/1 > 1.0", normal(_deps));
         assertTrue(rep.isModified());
         assertEquals("message-1a", normalMessages(rep));
         // Spec dep on old module is upgraded:
-        deps = new HashSet();
+        deps = new HashSet<Dependency>();
         deps.addAll(Dependency.create(Dependency.TYPE_MODULE, "stabilized/0 > 0.5"));
-        _deps = new HashSet(deps);
+        _deps = new HashSet<Dependency>(deps);
         rep = ad.refineDependenciesAndReport("random", _deps);
         assertEquals("stabilized/1 > 1.0", normal(_deps));
         assertTrue(rep.isModified());
         assertEquals("message-1a", normalMessages(rep));
         // Impl dep on old module is left alone (will be useless anyway on new version):
-        deps = new HashSet();
+        deps = new HashSet<Dependency>();
         deps.addAll(Dependency.create(Dependency.TYPE_MODULE, "stabilized/0 = whatever"));
-        _deps = new HashSet(deps);
+        _deps = new HashSet<Dependency>(deps);
         rep = ad.refineDependenciesAndReport("random", _deps);
         assertEquals("stabilized/0 = whatever", normal(_deps));
         assertFalse(rep.isModified());
         assertEquals("", normalMessages(rep));
         // Plain dep on new module is left alone:
-        deps = new HashSet();
+        deps = new HashSet<Dependency>();
         deps.addAll(Dependency.create(Dependency.TYPE_MODULE, "stabilized/1"));
-        _deps = new HashSet(deps);
+        _deps = new HashSet<Dependency>(deps);
         rep = ad.refineDependenciesAndReport("random", _deps);
         assertEquals("stabilized/1", normal(_deps));
         assertFalse(rep.isModified());
         assertEquals("", normalMessages(rep));
         // Spec dep on new module is left alone:
-        deps = new HashSet();
+        deps = new HashSet<Dependency>();
         deps.addAll(Dependency.create(Dependency.TYPE_MODULE, "stabilized/1 > 1.1"));
-        _deps = new HashSet(deps);
+        _deps = new HashSet<Dependency>(deps);
         rep = ad.refineDependenciesAndReport("random", _deps);
         assertEquals("stabilized/1 > 1.1", normal(_deps));
         assertFalse(rep.isModified());
         assertEquals("", normalMessages(rep));
         // Impl dep on new module is left alone:
-        deps = new HashSet();
+        deps = new HashSet<Dependency>();
         deps.addAll(Dependency.create(Dependency.TYPE_MODULE, "stabilized/1 = whatever"));
-        _deps = new HashSet(deps);
+        _deps = new HashSet<Dependency>(deps);
         rep = ad.refineDependenciesAndReport("random", _deps);
         assertEquals("stabilized/1 = whatever", normal(_deps));
         assertFalse(rep.isModified());
         assertEquals("", normalMessages(rep));
     }
     
-    private static String normal(Set deps) {
-        SortedSet s = new TreeSet(); // Set<String>
-        Iterator it = deps.iterator();
+    private static String normal(Set<Dependency> deps) {
+        SortedSet<String> s = new TreeSet<String>(); // Set<String>
+        Iterator<Dependency> it = deps.iterator();
         while (it.hasNext()) {
-            Dependency d = (Dependency)it.next();
+            Dependency d = it.next();
             s.add(dep2String(d));
         }
         StringBuffer b = new StringBuffer();
-        it = s.iterator();
-        while (it.hasNext()) {
-            b.append(it.next());
-            if (it.hasNext()) {
+        Iterator<String> it2 = s.iterator();
+        while (it2.hasNext()) {
+            b.append(it2.next());
+            if (it2.hasNext()) {
                 b.append(", ");
             }
         }
@@ -203,7 +203,7 @@ public class AutomaticDependenciesTest extends NbTestCase {
     }
     
     private static String normalMessages(AutomaticDependencies.Report rep) {
-        SortedSet s = new TreeSet(); // Set<String>
+        SortedSet<String> s = new TreeSet<String>(); // Set<String>
         Iterator it = rep.getMessages().iterator();
         while (it.hasNext()) {
             s.add((String)it.next());
