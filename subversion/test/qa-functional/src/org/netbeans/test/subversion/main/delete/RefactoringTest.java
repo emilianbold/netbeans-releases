0ac294773f359ae9fb0e355a3da817fec8268797
ppis <ppis@netbeans.org>
2006-09-20 13:34:53 +0000

    Refactoring functional test added.

---
+++ subversion/test/qa-functional/src/org/netbeans/test/subversion/main/delete/RefactoringTest.java
@@ -0,0 +1,154 @@
+/*
+ * RefactoringTest.java
+ *
+ * Created on Streda, 2006, september 20, 11:25
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package org.netbeans.test.subversion.main.delete;
+
+import java.io.File;
+import java.io.PrintStream;
+import junit.textui.TestRunner;
+import org.netbeans.jellytools.JellyTestCase;
+import org.netbeans.jellytools.NbDialogOperator;
+import org.netbeans.jellytools.OutputTabOperator;
+import org.netbeans.jellytools.nodes.Node;
+import org.netbeans.jellytools.nodes.SourcePackagesNode;
+import org.netbeans.jemmy.JemmyProperties;
+import org.netbeans.jemmy.operators.JButtonOperator;
+import org.netbeans.jemmy.operators.JTextFieldOperator;
+import org.netbeans.junit.NbTestSuite;
+import org.netbeans.junit.ide.ProjectSupport;
+import org.netbeans.test.subversion.operators.CheckoutWizardOperator;
+import org.netbeans.test.subversion.operators.RefactoringOperator;
+import org.netbeans.test.subversion.operators.RepositoryStepOperator;
+import org.netbeans.test.subversion.operators.VersioningOperator;
+import org.netbeans.test.subversion.operators.WorkDirStepOperator;
+import org.netbeans.test.subversion.utils.RepositoryMaintenance;
+import org.netbeans.test.subversion.utils.TestKit;
+
+/**
+ *
+ * @author pvcs
+ */
+public class RefactoringTest extends JellyTestCase {
+    
+    public static final String TMP_PATH = "/tmp";
+    public static final String REPO_PATH = "repo";
+    public static final String WORK_PATH = "work";
+    public static final String PROJECT_NAME = "JavaApp";
+    public File projectPath;
+    public PrintStream stream;
+    String os_name;
+    
+    /** Creates a new instance of RefactoringTest */
+    public RefactoringTest(String name) {
+        super(name);
+    }
+    
+    protected void setUp() throws Exception {        
+        os_name = System.getProperty("os.name");
+        //System.out.println(os_name);
+        System.out.println("### "+getName()+" ###");
+        
+    }
+    
+    protected boolean isUnix() {
+        boolean unix = false;
+        if (os_name.indexOf("Windows") == -1) {
+            unix = true;
+        }
+        return unix;
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        TestRunner.run(suite());
+    }
+    
+    public static NbTestSuite suite() {
+        NbTestSuite suite = new NbTestSuite();
+        suite.addTest(new RefactoringTest("testRefactoring"));
+        return suite;
+    }
+    
+    public void testRefactoring() throws Exception {
+        JemmyProperties.setCurrentTimeout("ComponentOperator.WaitComponentTimeout", 30000);
+        JemmyProperties.setCurrentTimeout("DialogWaiter.WaitDialogTimeout", 30000);    
+        TestKit.closeProject(PROJECT_NAME);
+        
+        stream = new PrintStream(new File(getWorkDir(), getName() + ".log"));
+        VersioningOperator vo = VersioningOperator.invoke();
+        CheckoutWizardOperator co = CheckoutWizardOperator.invoke();
+        RepositoryStepOperator rso = new RepositoryStepOperator();       
+        
+        //create repository... 
+        File work = new File(TMP_PATH + File.separator + WORK_PATH + File.separator + "w" + System.currentTimeMillis());
+        new File(TMP_PATH).mkdirs();
+        work.mkdirs();
+        RepositoryMaintenance.deleteFolder(new File(TMP_PATH + File.separator + REPO_PATH));
+        //RepositoryMaintenance.deleteFolder(new File(TMP_PATH + File.separator + WORK_PATH));
+        RepositoryMaintenance.createRepository(TMP_PATH + File.separator + REPO_PATH);   
+        RepositoryMaintenance.loadRepositoryFromFile(TMP_PATH + File.separator + REPO_PATH, getDataDir().getCanonicalPath() + File.separator + "repo_dump");      
+        rso.setRepositoryURL(RepositoryStepOperator.ITEM_FILE + RepositoryMaintenance.changeFileSeparator(TMP_PATH + File.separator + REPO_PATH, false));
+        
+        rso.next();
+        WorkDirStepOperator wdso = new WorkDirStepOperator();
+        wdso.setRepositoryFolder("trunk/" + PROJECT_NAME);
+        wdso.setLocalFolder(work.getCanonicalPath());
+        wdso.checkCheckoutContentOnly(false);
+        OutputTabOperator oto = new OutputTabOperator("file:///tmp/repo");
+        oto.getTimeouts().setTimeout("ComponentOperator.WaitStateTimeout", 30000);
+        oto.clear();
+        wdso.finish();
+        //open project
+        oto.waitText("Checking out... finished.");
+        NbDialogOperator nbdialog = new NbDialogOperator("Checkout Completed");
+        JButtonOperator open = new JButtonOperator(nbdialog, "Open Project");
+        open.push();
+        ProjectSupport.waitScanFinished();
+        
+        oto = new OutputTabOperator("file:///tmp/repo");
+        oto.getTimeouts().setTimeout("ComponentOperator.WaitStateTimeout", 30000);
+        oto.clear();            
+        Node node = new Node(new SourcePackagesNode(PROJECT_NAME), "");
+        node.performPopupAction("Subversion|Show Changes");
+        oto.waitText("Refreshing... finished.");
+        node = new Node(new SourcePackagesNode(PROJECT_NAME), "javaapp");
+        
+        node.performPopupActionNoBlock("Refactor|Rename");
+        NbDialogOperator dialog = new NbDialogOperator("Rename");
+        JTextFieldOperator txt = new JTextFieldOperator(dialog);
+        txt.setText("javaapp_ren");
+        JButtonOperator btn = new JButtonOperator(dialog, "Next");
+        btn.push();
+
+        RefactoringOperator ro = new RefactoringOperator();
+        ro.doRefactoring();
+        //Thread.sleep(1000);
+        NbDialogOperator ndo = new NbDialogOperator("Refactoring");
+        ndo.getTimeouts().setTimeout("ComponentOperator.WaitStateTimeout", 30000);
+        ndo.waitClosed();
+        Thread.sleep(2000);
+        
+        vo = VersioningOperator.invoke();
+        String[] expected = new String[] {"Main.java", "Main.java", "javaapp_ren"};
+        String[] actual = new String[vo.tabFiles().getRowCount()];
+        for (int i = 0; i < vo.tabFiles().getRowCount(); i++) {
+            actual[i] = vo.tabFiles().getValueAt(i, 0).toString().trim();
+        }
+        int result = TestKit.compareThem(expected, actual, false);
+        assertEquals("Wrong files in Versioning View", 3, result);
+        
+        expected = new String[] {"Locally Deleted", "Locally New", "Locally Copied"};
+        actual = new String[vo.tabFiles().getRowCount()];
+        for (int i = 0; i < vo.tabFiles().getRowCount(); i++) {
+            actual[i] = vo.tabFiles().getValueAt(i, 1).toString().trim();
+        }
+        result = TestKit.compareThem(expected, actual, false);
+        assertEquals("Wrong status in Versioning View", 3, result);
+    }
+}
---
+++ subversion/test/qa-functional/src/org/netbeans/test/subversion/operators/RefactoringOperator.java
@@ -0,0 +1,174 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.test.subversion.operators;
+
+import java.awt.Component;
+import java.awt.Container;
+import javax.swing.JComponent;
+import org.netbeans.jellytools.OutputTabOperator;
+import org.netbeans.jellytools.TopComponentOperator;
+import org.netbeans.jellytools.actions.Action;
+import org.netbeans.jemmy.ComponentChooser;
+import org.netbeans.jemmy.ComponentSearcher;
+import org.netbeans.jemmy.operators.JButtonOperator;
+import org.netbeans.jemmy.operators.JTabbedPaneOperator;
+import org.netbeans.test.subversion.operators.actions.RefactoringAction;
+
+public class RefactoringOperator extends TopComponentOperator {
+    
+    private static final Action invokeAction = new RefactoringAction();
+    private JButtonOperator _btDoRefactoring;
+    private JButtonOperator _btCancel;
+    
+    public JButtonOperator btDoRefactoring() {
+        if (_btDoRefactoring == null) {
+            _btDoRefactoring = new JButtonOperator(this, "Do Refactoring");
+        }
+        return _btDoRefactoring;
+    }
+    
+    public JButtonOperator btCancel() {
+        if (_btCancel == null) {
+            _btCancel = new JButtonOperator(this, "Cancel");
+        }
+        return _btCancel;
+    }
+    
+    public void doRefactoring() {
+        btDoRefactoring().pushNoBlock();
+    }
+    
+    public void cancel() {
+        btCancel().pushNoBlock();
+    }
+    
+    /** Waits for refactoring window top component and creates a new operator for it. */
+    public RefactoringOperator() {
+        super(waitTopComponent(null, null, 0, renameChooser));
+    }
+    
+    /**
+     * Opens Output from main menu Window|Output and returns RefactoringOperator.
+     * 
+     * @return instance of RefactoringOperator
+     */
+    public static RefactoringOperator invoke() {
+        invokeAction.perform();
+        return new RefactoringOperator();
+    }
+    
+    /** Returns active OutputTabOperator instance regardless it is the only one in
+     * output or it is in tabbed pane.
+     * @return active OutputTabOperator instance
+     *
+    private OutputTabOperator getActiveOutputTab() {
+        OutputTabOperator outputTabOper;
+        if(null != JTabbedPaneOperator.findJTabbedPane((Container)getSource(), ComponentSearcher.getTrueChooser(""))) {
+            outputTabOper = new OutputTabOperator(((JComponent)new JTabbedPaneOperator(this).getSelectedComponent()));
+            outputTabOper.copyEnvironment(this);
+        } else {
+            outputTabOper = new OutputTabOperator("");
+        }
+        return outputTabOper;
+    }*/
+
+    /**
+     * Returns instance of OutputTabOperator of given name.
+     * It is activated by default.
+     * @param tabName name of tab to be selected
+     * @return instance of OutputTabOperator
+     *
+    public OutputTabOperator getOutputTab(String tabName) {
+        return new OutputTabOperator(tabName);
+    }*/
+    
+    /**
+     * Returns text from the active tab.
+     * @return text from the active tab
+     *
+    public String getText() {
+        return getActiveOutputTab().getText();
+    }*/
+    
+    /********************************** Actions ****************************/
+    
+    /** Performs copy action on active tab. 
+    public void copy() {
+        getActiveOutputTab().copy();
+    }*/
+    
+    /** Performs find action on active tab. 
+    public void find() {
+        getActiveOutputTab().find();
+    }*/
+    
+    /** Performs find next action on active tab. 
+    public void findNext() {
+        getActiveOutputTab().findNext();
+    }*/
+    
+    /** Performs select all action on active tab. 
+    public void selectAll() {
+        getActiveOutputTab().selectAll();
+    }*/    
+    
+    /** Performs next error action on active tab. 
+    public void nextError() {
+        getActiveOutputTab().nextError();
+    }*/
+    
+    /** Performs next error action on active tab. 
+    public void previousError() {
+        getActiveOutputTab().previousError();
+    }*/
+
+    /** Performs wrap text action on active tab. 
+    public void wrapText() {
+        getActiveOutputTab().wrapText();
+    }*/ 
+    
+    /** Performs clear action on active tab. 
+    public void clear() {
+        getActiveOutputTab().clear();
+    }*/
+
+    /** Performs save as action on active tab. 
+    public void saveAs() {
+        getActiveOutputTab().saveAs();
+    }*/
+    
+    /** Performs verification by accessing all sub-components. 
+    public void verify() {
+        // do nothing because output top component can be empty
+    }*/
+    
+    /** SubChooser to determine OutputWindow TopComponent
+     * Used in constructor.
+     */
+    private static final ComponentChooser renameChooser = new ComponentChooser() {
+        public boolean checkComponent(Component comp) {
+            return comp.getClass().getName().endsWith("RefactoringPanelContainer"); //NOI18N
+        }
+        
+        public String getDescription() {
+            return "component instanceof org.netbeans.refactoring.ui.RefactoringPanelContainer";// NOI18N
+        }
+    };
+}
