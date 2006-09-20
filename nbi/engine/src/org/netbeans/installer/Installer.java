938d8c7561e8ce72e52b2d30f950f95800050e6b
ksorokin <ksorokin@netbeans.org>
2006-09-20 05:53:36 +0000

    First step in supporting target component mode. Added support for custom look and feels.

--- nbi/engine/src/org/netbeans/installer/Installer.java
+++ nbi/engine/src/org/netbeans/installer/Installer.java
@@ -24,12 +24,18 @@ import java.io.File;
 import java.util.Properties;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
-import org.netbeans.installer.utils.ErrorLevel;
+import org.netbeans.installer.product.ProductRegistry;
 import org.netbeans.installer.utils.ErrorManager;
 import org.netbeans.installer.utils.LogManager;
 import org.netbeans.installer.wizard.Wizard;
 import org.netbeans.installer.wizard.components.actions.FinalizeRegistryAction;
 import org.netbeans.installer.wizard.components.actions.InitalizeRegistryAction;
+
+import static org.netbeans.installer.utils.ErrorLevel.DEBUG;
+import static org.netbeans.installer.utils.ErrorLevel.MESSAGE;
+import static org.netbeans.installer.utils.ErrorLevel.WARNING;
+import static org.netbeans.installer.utils.ErrorLevel.ERROR;
+import static org.netbeans.installer.utils.ErrorLevel.CRITICAL;
 
 /**
  * The main class of the NBBA installer framework. It represents the installer and
@@ -52,7 +58,6 @@ public class Installer {
         new Installer(arguments).start();        
     }
     
-    
     /////////////////////////////////////////////////////////////////////////////////
     // Constants
     public static final String DEFAULT_LOCAL_DIRECTORY_PATH =
@@ -60,6 +65,21 @@ public class Installer {
     
     public static final String LOCAL_DIRECTORY_PATH_PROPERTY =
             "nbi.local.directory.path";
+    
+    public static final String DEFAULT_NBI_LOOK_AND_FEEL_CLASS_NAME = 
+            UIManager.getSystemLookAndFeelClassName();
+    
+    public static final String NBI_LOOK_AND_FEEL_CLASS_NAME_PROPERTY = 
+            "nbi.look.and.feel";
+    
+    /** Errorcode to be used at normal exit */
+    public static final int NORMAL_ERRORCODE = 0;
+    
+    /** Errorcode to be used when the installer is canceled */
+    public static final int CANCEL_ERRORCODE = 1;
+    
+    /** Errorcode to be used when the installer exits because of a critical error */
+    public static final int CRITICAL_ERRORCODE = Integer.MAX_VALUE;
     
     /////////////////////////////////////////////////////////////////////////////////
     // Static
@@ -80,58 +100,38 @@ public class Installer {
     private File localDirectory =
             new File(DEFAULT_LOCAL_DIRECTORY_PATH).getAbsoluteFile();
     
-    private LogManager logManager = LogManager.getInstance();
+    private LogManager   logManager   = LogManager.getInstance();
+    private ErrorManager errorManager = ErrorManager.getInstance();
     
+    // Constructor //////////////////////////////////////////////////////////////////
     /**
      * The only private constructor - we need to hide the default one as
      * <code>Installer is a singleton.
      */
     private Installer(String[] arguments) {
-        logManager.log(ErrorLevel.MESSAGE, "initializing the installer engine");
+        logManager.log(MESSAGE, "initializing the installer engine");
         logManager.indent();
         
-        logManager.log(ErrorLevel.MESSAGE, "setting the look and feel");
-        String lfName = UIManager.getSystemLookAndFeelClassName();
-        try {
-            UIManager.setLookAndFeel(lfName);
-        } catch (ClassNotFoundException e) {
-            ErrorManager.getInstance().notify(ErrorLevel.WARNING, "Could " +
-                    "not set the Look And Feel to the system default.", e);
-        } catch (InstantiationException e) {
-            ErrorManager.getInstance().notify(ErrorLevel.WARNING, "Could " +
-                    "not set the Look And Feel to the system default.", e);
-        } catch (IllegalAccessException e) {
-            ErrorManager.getInstance().notify(ErrorLevel.WARNING, "Could " +
-                    "not set the Look And Feel to the system default.", e);
-        } catch (UnsupportedLookAndFeelException e) {
-            ErrorManager.getInstance().notify(ErrorLevel.WARNING, "Could " +
-                    "not set the Look And Feel to the system default.", e);
-        }
-        logManager.log(ErrorLevel.MESSAGE, "... look and feel set to " + lfName);
+        dumpSystemInfo();
         
-        logManager.log(ErrorLevel.MESSAGE, "parsing command-line arguments");
-        parseCommandLineArguments(arguments);
-        logManager.log(ErrorLevel.MESSAGE, "... command line arguments successfully parsed");
+        parseArguments(arguments);
+        
+        setLookAndFeel();
         
         instance = this;
         
-        logManager.log(ErrorLevel.MESSAGE, "initializing the local directory");
+        logManager.log(MESSAGE, "initializing the local directory");
         if (System.getProperty(LOCAL_DIRECTORY_PATH_PROPERTY) != null) {
             localDirectory = new File(System.getProperty(
                     LOCAL_DIRECTORY_PATH_PROPERTY)).getAbsoluteFile();
         }
         
         
-        logManager.log(ErrorLevel.MESSAGE, "system properties:");
-        Properties sysprops = System.getProperties();
-        
-        for (Object key: sysprops.keySet()) {
-            LogManager.getInstance().log(ErrorLevel.MESSAGE, key.toString() + " => " + sysprops.get(key).toString());
-        }
-        
         logManager.unindent();
+        logManager.log(MESSAGE, "... finished initializing the installer engine");
     }
     
+    // Life cycle control methods ///////////////////////////////////////////////////
     /**
      * Starts the installer. This method parses the passed-in command line arguments,
      * initializes the wizard and the components registry.
@@ -141,14 +141,14 @@ public class Installer {
     public void start() {
         if (!localDirectory.exists()) {
             if (!localDirectory.mkdirs()) {
-                ErrorManager.getInstance().notify(ErrorLevel.CRITICAL, "Cannot create local directory: " + localDirectory);
+                ErrorManager.getInstance().notify(CRITICAL, "Cannot create local directory: " + localDirectory);
             }
         } else if (localDirectory.isFile()) {
-            ErrorManager.getInstance().notify(ErrorLevel.CRITICAL, "Local directory exists and is a file: " + localDirectory);
+            ErrorManager.getInstance().notify(CRITICAL, "Local directory exists and is a file: " + localDirectory);
         } else if (!localDirectory.canRead()) {
-            ErrorManager.getInstance().notify(ErrorLevel.CRITICAL, "Cannot read local directory - not enought permissions");
+            ErrorManager.getInstance().notify(CRITICAL, "Cannot read local directory - not enought permissions");
         } else if (!localDirectory.canWrite()) {
-            ErrorManager.getInstance().notify(ErrorLevel.CRITICAL, "Cannot write to local directory - not enought permissions");
+            ErrorManager.getInstance().notify(CRITICAL, "Cannot write to local directory - not enought permissions");
         }
         
         
@@ -199,28 +199,110 @@ public class Installer {
         System.exit(CRITICAL_ERRORCODE);
     }
     
+    // Getters //////////////////////////////////////////////////////////////////////
     public File getLocalDirectory() {
         return localDirectory;
     }
     
+    // Private stuff ////////////////////////////////////////////////////////////////
     /**
      * Parses the command line arguments passed to the installer. All unknown
      * arguments are ignored.
      *
      * @param arguments The command line arguments
      */
-    private void parseCommandLineArguments(String[] arguments) {
-        // TODO
+    private void parseArguments(String[] arguments) {
+        logManager.log(MESSAGE, "parsing command-line arguments");
+        logManager.indent();
+        
+        for (int i = 0; i < arguments.length; i++) {
+            if (arguments[i].equalsIgnoreCase("--look-and-feel")) {
+                logManager.log(MESSAGE, "parsing command line parameter \"--look-and-feel\"");
+                if (i < arguments.length - 1) {
+                    String value = arguments[i + 1];
+                    System.setProperty(NBI_LOOK_AND_FEEL_CLASS_NAME_PROPERTY, value);
+                    
+                    i = i + 1;
+                    
+                    logManager.log(MESSAGE, "... class name: " + value);
+                } else {
+                    ErrorManager.getInstance().notify(WARNING, "Required parameter missing for command line argument \"--look-and-feel\". Should be \"--look-and-feel <look-and-feel-class-name>\".");
+                }
+                
+                continue;
+            }
+            
+            if (arguments[i].equalsIgnoreCase("--target")) {
+                logManager.log(MESSAGE, "parsing command line parameter \"--target\"");
+                if (i < arguments.length - 2) {
+                    String uid = arguments[i + 1];
+                    String version = arguments[i + 2];
+                    System.setProperty(ProductRegistry.TARGET_COMPONENT_UID_PROPERTY, uid);
+                    System.setProperty(ProductRegistry.TARGET_COMPONENT_VERSION_PROPERTY, version);
+                    
+                    i = i + 2;
+                    
+                    logManager.log(MESSAGE, "... uid:     " + uid);
+                    logManager.log(MESSAGE, "... version: " + version);
+                }
+                
+                continue;
+            }
+            
+        }
+        
+        if (arguments.length == 0) {
+            logManager.log(MESSAGE, "... no command line arguments were specified");
+        }
+        
+        logManager.unindent();
+        logManager.log(MESSAGE, "... finished parsing command line arguments");
     }
     
-    /////////////////////////////////////////////////////////////////////////////////
-    // Constants
-    /** Errorcode to be used at normal exit */
-    public static final int NORMAL_ERRORCODE = 0;
+    private void setLookAndFeel() {
+        logManager.log(MESSAGE, "setting the look and feel");
+        logManager.indent();
+        
+        String className = System.getProperty(NBI_LOOK_AND_FEEL_CLASS_NAME_PROPERTY);
+        if (className == null) {
+            logManager.log(MESSAGE, "custom look and feel class name was not specified, using system default");
+            className = DEFAULT_NBI_LOOK_AND_FEEL_CLASS_NAME;
+        }
+        
+        logManager.log(MESSAGE, "... class name: " + className);
+        
+        try {
+            UIManager.setLookAndFeel(className);
+        } catch (ClassNotFoundException e) {
+            ErrorManager.getInstance().notify(WARNING, "Could not set the look and feel.", e);
+        } catch (InstantiationException e) {
+            ErrorManager.getInstance().notify(WARNING, "Could not set the look and feel.", e);
+        } catch (IllegalAccessException e) {
+            ErrorManager.getInstance().notify(WARNING, "Could not set the look and feel.", e);
+        } catch (UnsupportedLookAndFeelException e) {
+            ErrorManager.getInstance().notify(WARNING, "Could not set the look and feel.", e);
+        }
+        
+        logManager.unindent();
+        logManager.log(MESSAGE, "... finished setting the look and feel");
+    }
     
-    /** Errorcode to be used when the installer is canceled */
-    public static final int CANCEL_ERRORCODE = 1;
-    
-    /** Errorcode to be used when the installer exits because of a critical error */
-    public static final int CRITICAL_ERRORCODE = Integer.MAX_VALUE;
+    private void dumpSystemInfo() {
+        logManager.log(MESSAGE, "dumping target system information");
+        logManager.indent();
+        
+        logManager.log(MESSAGE, "system properties");
+        logManager.indent();
+        
+        Properties properties = System.getProperties();
+        for (Object key: properties.keySet()) {
+            logManager.log(MESSAGE, key.toString() + " => " + properties.get(key).toString());
+        }
+        
+        logManager.unindent();
+        logManager.log(MESSAGE, "... end of system properties");
+        
+        logManager.unindent();
+        logManager.log(MESSAGE, "... end of target system information");
+    }
 }
