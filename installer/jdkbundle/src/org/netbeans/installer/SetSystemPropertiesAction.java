fa70d1cd6960138641e0a50330dfe5e4292a1a49
mslama <mslama@netbeans.org>
2006-10-11 09:24:07 +0000

    #86791: Fix reading of JDK and JRE keys from Windows registry.

--- installer/jdkbundle/src/org/netbeans/installer/SetSystemPropertiesAction.java
+++ installer/jdkbundle/src/org/netbeans/installer/SetSystemPropertiesAction.java
@@ -29,13 +29,7 @@ import com.installshield.wizard.WizardBuilderSupport;
 import com.installshield.wizard.platform.win32.Win32RegistryService;
 import com.installshield.wizard.service.ServiceException;
 import com.installshield.wizard.service.security.SecurityService;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
 import java.util.Locale;
-import java.util.StringTokenizer;
 
 /** This class is used to initialize some system properties at begining
  * of installation.
@@ -127,67 +121,33 @@ public class SetSystemPropertiesAction extends WizardAction {
     
     private String findJdkHome() {
         String jdkHome = null;
-        try{
-            logEvent(this, Log.DBG,"Checking Win32 Registry ... ");
-            File regFile = File.createTempFile("forte",".reg");
+        try {
+            Win32RegistryService regService = (Win32RegistryService) getService(Win32RegistryService.NAME);
             
-            String command = "regedit -e " + regFile.getAbsolutePath()
-            + " \"HKEY_LOCAL_MACHINE\\SOFTWARE\\JavaSoft\\Java Development Kit\"";
-            RunCommand runCommand = new RunCommand();
-            runCommand.execute(command);
-            runCommand.waitFor();
-            BufferedReader reader;
-
-            if (Util.isWindows98()) {
-                reader = new BufferedReader(new InputStreamReader(new FileInputStream(regFile.getAbsolutePath())));
-            } else if (Util.isWindowsNT()) {
-                reader = new BufferedReader(new InputStreamReader(new FileInputStream(regFile.getAbsolutePath())));
-            } else {
-                reader = new BufferedReader(new InputStreamReader(new FileInputStream(regFile.getAbsolutePath()),"UTF-16"));
+            int HKLM = Win32RegistryService.HKEY_LOCAL_MACHINE;
+            
+            String HKEY_JDK = "SOFTWARE\\JavaSoft\\Java Development Kit";
+            String [] keys = regService.getSubkeyNames(HKLM,HKEY_JDK);
+                        
+            for (int i = 0; i < keys.length; i++) {
+                logEvent(this, Log.DBG,"findJdkHome keys[" + i + "]: " + keys[i]);
             }
-            
             String bundledVersion = resolveString("$L(org.netbeans.installer.Bundle,JDK.version)");
-            String line;
-            while ((line = reader.readLine()) != null) {
-                //logEvent(this, Log.DBG,"findJdkHome line: " + line);
-                if (line.startsWith("[HKEY_LOCAL_MACHINE\\SOFTWARE\\JavaSoft\\Java Development Kit")){
-                    String version = line.substring(line.lastIndexOf("\\") + 1);
-                    //Strip off trailing square bracket "]"
-                    if (version.lastIndexOf("]") == (version.length() - 1)) {
-                        version = version.substring(0, version.length() - 1);
-                    }
-                    logEvent(this, Log.DBG,"findJdkHome version: '" + version + "'");
-                    //Must be equal to match "1.X.X" and not eg."1.X.X_01"
-                    if (bundledVersion.equals(version)) {
-                        line = reader.readLine();
-                        StringTokenizer st = new StringTokenizer(line,"\"");
-                        String firstToken="",lastToken="";
-                        if (st.hasMoreTokens())  firstToken = st.nextToken();
-                        if (firstToken.compareTo("JavaHome") == 0) {
-                            while (st.hasMoreTokens())  lastToken = st.nextToken();
-                            StringBuffer stringBuffer = new StringBuffer();
-                            int index=0;
-                            for (int i=0; i<lastToken.length(); i++) {
-                                if ((lastToken.charAt(i) == '\\') && (index==0)) {
-                                    index++;
-                                } else {
-                                    stringBuffer.append(lastToken.charAt(i));
-                                    if(index > 0) index=0;
-                                }
-                            }
-                            jdkHome=stringBuffer.toString();
-                            if (!Util.checkJdkHome(jdkHome)) { 
-				System.getProperties().put("corruptJdk",jdkHome);
-				logEvent(this, Log.DBG, "corruptJdk: " + System.getProperties().get("corruptJdk"));
-				jdkHome = null;
-			    }
-                        }
+            for (int i = 0; i < keys.length; i++) {
+                if (keys[i].equals(bundledVersion)) {
+                    String javaVersionKey = HKEY_JDK + "\\" + keys[i];
+                    jdkHome = regService.getStringValue(HKLM,javaVersionKey,"JavaHome",false);
+                    logEvent(this, Log.DBG,"jdkHome:" + jdkHome);
+                    if (!Util.checkJdkHome(jdkHome)) { 
+                        System.getProperties().put("corruptJdk",jdkHome);
+                        logEvent(this, Log.DBG, "corruptJdk: " + System.getProperties().get("corruptJdk"));
+                        jdkHome = null;
                     }
                 }
             }
-            reader.close();
-            regFile.delete();
-        } catch(Exception ex){
+            
+        } catch (ServiceException ex) {
+            System.out.println(ex.getLocalizedMessage());
             ex.printStackTrace();
         }
         return jdkHome;
@@ -196,66 +156,32 @@ public class SetSystemPropertiesAction extends WizardAction {
     private String findJreHome() {
         String jreHome = null;
         try {
-            logEvent(this, Log.DBG,"Checking Win32 Registry ... ");
-            File regFile = File.createTempFile("forte",".reg");
+            Win32RegistryService regService = (Win32RegistryService) getService(Win32RegistryService.NAME);
             
-            String command = "regedit -e " + regFile.getAbsolutePath()
-            + " \"HKEY_LOCAL_MACHINE\\SOFTWARE\\JavaSoft\\Java Runtime Environment\"";
-            RunCommand runCommand = new RunCommand();
-            runCommand.execute(command);
-            runCommand.waitFor();
-            BufferedReader reader;
-
-            if (Util.isWindows98()) {
-                reader = new BufferedReader(new InputStreamReader(new FileInputStream(regFile.getAbsolutePath())));
-            } else if (Util.isWindowsNT()) {
-                reader = new BufferedReader(new InputStreamReader(new FileInputStream(regFile.getAbsolutePath())));
-            } else {
-                reader = new BufferedReader(new InputStreamReader(new FileInputStream(regFile.getAbsolutePath()),"UTF-16"));
+            int HKLM = Win32RegistryService.HKEY_LOCAL_MACHINE;
+            
+            String HKEY_JRE = "SOFTWARE\\JavaSoft\\Java Runtime Environment";
+            String [] keys = regService.getSubkeyNames(HKLM,HKEY_JRE);
+                        
+            for (int i = 0; i < keys.length; i++) {
+                logEvent(this, Log.DBG,"findJdkHome keys[" + i + "]: " + keys[i]);
             }
-            
             String bundledVersion = resolveString("$L(org.netbeans.installer.Bundle,JRE.version)");
-            String line;
-            while ((line = reader.readLine()) != null) {
-                //logEvent(this, Log.DBG,"findJreHome line: " + line);
-                if (line.startsWith("[HKEY_LOCAL_MACHINE\\SOFTWARE\\JavaSoft\\Java Runtime Environment")){
-                    String version = line.substring(line.lastIndexOf("\\") + 1);
-                    //Strip off trailing square bracket "]"
-                    if (version.lastIndexOf("]") == (version.length() - 1)) {
-                        version = version.substring(0, version.length() - 1);
-                    }
-                    logEvent(this, Log.DBG,"findJreHome version: '" + version + "'");
-                    //Must be equal to match "1.X.X" and not eg."1.X.X_01"
-                    if (bundledVersion.equals(version)) {
-                        line = reader.readLine();
-                        StringTokenizer st = new StringTokenizer(line,"\"");
-                        String firstToken="",lastToken="";
-                        if (st.hasMoreTokens())  firstToken = st.nextToken();
-                        if (firstToken.compareTo("JavaHome") == 0){
-                            while (st.hasMoreTokens())  lastToken = st.nextToken();
-                            StringBuffer stringBuffer = new StringBuffer();
-                            int index=0;
-                            for (int i=0; i<lastToken.length(); i++) {
-                                if ((lastToken.charAt(i) == '\\') && (index==0)) {
-                                    index++;
-                                } else {
-                                    stringBuffer.append(lastToken.charAt(i));
-                                    if(index > 0) index=0;
-                                }
-                            }
-                            jreHome=stringBuffer.toString();
-                            if (!Util.checkJreHome(jreHome)) { 
-				System.getProperties().put("corruptJre",jreHome);
-				logEvent(this, Log.DBG, "corruptJre: " + System.getProperties().get("corruptJre"));
-				jreHome = null;
-			    }
-                        }
+            for (int i = 0; i < keys.length; i++) {
+                if (keys[i].equals(bundledVersion)) {
+                    String javaVersionKey = HKEY_JRE + "\\" + keys[i];
+                    jreHome = regService.getStringValue(HKLM,javaVersionKey,"JavaHome",false);
+                    logEvent(this, Log.DBG,"jreHome:" + jreHome);
+                    if (!Util.checkJreHome(jreHome)) { 
+                        System.getProperties().put("corruptJre",jreHome);
+                        logEvent(this, Log.DBG, "corruptJre: " + System.getProperties().get("corruptJre"));
+                        jreHome = null;
                     }
                 }
             }
-            reader.close();
-            regFile.delete();
-        } catch(Exception ex){
+            
+        } catch (ServiceException ex) {
+            System.out.println(ex.getLocalizedMessage());
             ex.printStackTrace();
         }
         return jreHome;
