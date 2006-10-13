f2ad97b07ee2de0c83e5da5080bdcfdd30098943
jglick <jglick@netbeans.org>
2006-10-13 20:42:03 +0000

    Added 'checkout' target to check out the exact set of CVS modules needed.

--- nbbuild/antsrc/org/netbeans/nbbuild/PrintCvsModules.java
+++ nbbuild/antsrc/org/netbeans/nbbuild/PrintCvsModules.java
@@ -20,12 +20,15 @@
 package org.netbeans.nbbuild;
 
 import java.io.File;
-import java.util.*;
-
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
 import org.apache.tools.ant.BuildException;
-import org.apache.tools.ant.Project;
-import org.apache.tools.ant.Target;
 import org.apache.tools.ant.Task;
+import org.apache.tools.ant.types.FileSet;
 
 /** Pseudo-task to unpack a set of modules.
  * Resolves build-time dependencies of modules in selected moduleconfig
@@ -36,28 +39,24 @@ import org.apache.tools.ant.Task;
  */
 public class PrintCvsModules extends Task {
     
-    private Vector modules = new Vector (); // list of modules defined by build.xml
-    private Vector buildmodules = new Vector (); // list of modules which will be built
-    private String targetprefix = "all-";    
-    private String dummyName;
-    private Hashtable targets;
+    private List/*<String>*/ modules; // list of modules defined by build.xml
     private String selectorId;
     private File dir;
+    private String cvsModulesProperty;
     
     /** Comma-separated list of modules to include. */
     public void setModules (String s) {
         StringTokenizer tok = new StringTokenizer (s, ", ");
-        modules = new Vector ();
+        modules = new ArrayList();
         while (tok.hasMoreTokens ())
-            modules.addElement (tok.nextToken ());
+            modules.add(tok.nextToken ());
     }
     
-    /** String which will have a module name appended to it.
-     * This will form a target in the same project which should
-     * create the <samp>netbeans/</samp> subdirectory.
+    /**
+     * Specify a property which will be given the list of CVS modules, separated by spaces.
      */
-    public void setTargetprefix (String s) {
-        targetprefix = s;
+    public void setCvsModulesProperty(String p) {
+        cvsModulesProperty = p;
     }
     
     /** Name of property to set the file set to.
@@ -66,93 +65,50 @@ public class PrintCvsModules extends Task {
         selectorId = s;
     }
     
-    /** Directory with sources */
+    /** Directory with sources (meaningful only with {@link #setId}) */
     public void setDir (File f) {
         dir = f;
     }
 
-    /** Mode - either sources or external binaries
-     */
-    public static final class Mode extends org.apache.tools.ant.types.EnumeratedAttribute {
-        public String[] getValues() {
-            return new String[] {
-                "sources",
-                "binaries"
-            };
-        }
-    }
-    
-    public void setMode (Mode m) {
-        mode = m.getValue ();
-    }
-    
-    private String mode = "sources";
-    
-    /**
-     * 
-     * @throws org.apache.tools.ant.BuildException 
-     */
     public void execute () throws BuildException {
-
-        buildmodules.addAll(modules);
-        Vector compiletime = new Vector();
-        Vector cvslist = new Vector();
+        Set/*<String>*/ cvslist = new TreeSet();
         cvslist.add("nbbuild");
-        for (int i = 0; i < buildmodules.size (); i++) {
-            String module = (String) buildmodules.elementAt (i);
-            log("module.indexOf(\"/\") is " + module.indexOf("/"), Project.MSG_DEBUG);
-            if (module.indexOf("/") > 0) {
-                module = module.substring(0,module.indexOf("/"));
-                log("cvs module name is " + module, Project.MSG_VERBOSE);
+        Iterator modulesIt = modules.iterator();
+        while (modulesIt.hasNext()) {
+            String module = (String) modulesIt.next();
+            int slash = module.indexOf('/');
+            if (slash > 0) {
+                module = module.substring(0, slash);
             }
-            if ( ! cvslist.contains(module) ) {
-                cvslist.add(module);
-            } else {
-                log("not adding cvs module name " + module, Project.MSG_VERBOSE);
-            }
+            cvslist.add(module);
         }
 
-        log("compiletime="+compiletime);
         log("selectedmodules="+modules);
         log("cvsmodules="+cvslist);    
+        if (cvsModulesProperty != null) {
+            StringBuffer cvslistSpaces = new StringBuffer();
+            Iterator cvsIt = cvslist.iterator();
+            while (cvsIt.hasNext()) {
+                if (cvslistSpaces.length() > 0) {
+                    cvslistSpaces.append(' ');
+                }
+                cvslistSpaces.append((String) cvsIt.next());
+            }
+            getProject().setNewProperty(cvsModulesProperty, cvslistSpaces.toString());
+        }
         
         if (selectorId != null) {
-            if ("sources".equals (mode)) {
-                org.netbeans.nbbuild.CvsFileSet set = new org.netbeans.nbbuild.CvsFileSet ();
-                set.setDir (dir);
-                Iterator it = cvslist.iterator ();
-                while (it.hasNext ()) {
-                    String modname = (String)it.next ();
-                    set.createInclude ().setName (modname + "/**/*");
-                    set.createExclude ().setName (modname + "/www/**/*");
-                    set.createExclude ().setName (modname + "/test/**/*");
-                }
-                set.createExclude ().setName ("*/*/test/**/*");
-                getProject ().addReference (selectorId, set);
-            } else {
-                // binaries
-                org.apache.tools.ant.types.FileSet set = new org.apache.tools.ant.types.FileSet ();
-                set.setDir (dir);
-                Iterator it = cvslist.iterator ();
-                while (it.hasNext ()) {
-                    String modname = (String)it.next ();
-                    set.createInclude ().setName (modname + "/external/*");
-                }
-                // XXX kill the below soon...
-                set.createExclude ().setName ("**/unscrambling.log");
-                // walk through selected files and explicitly exclude unscrambled files
-                org.apache.tools.ant.DirectoryScanner ds = set.getDirectoryScanner(this.getProject());
-                ds.scan();
-                String[] includedFiles = ds.getIncludedFiles();
-                for (int i=0; i < includedFiles.length; i++) {
-                    if (includedFiles[i].endsWith(".scrambled")) {
-                        String ne = includedFiles[i].substring(0,includedFiles[i].length()-10);
-                        log("Setting exclude for unscrambled file "+ne, Project.MSG_VERBOSE);
-                        set.createExclude().setName(ne);
-                    }
-                }
-                getProject ().addReference (selectorId, set);
+            FileSet set = new CvsFileSet();
+            set.setDir(dir);
+            Iterator it = cvslist.iterator();
+            while (it.hasNext()) {
+                String modname = (String) it.next();
+                set.createInclude().setName(modname + "/**/*");
+                set.createExclude().setName(modname + "/www/**/*");
+                set.createExclude().setName(modname + "/test/**/*");
             }
+            set.createExclude().setName("*/*/test/**/*");
+            getProject().addReference(selectorId, set);
         }
     }        
 
