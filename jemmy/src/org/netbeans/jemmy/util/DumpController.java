1ed658657e596209c56b13f8203235027e7dfab0
mrkam <mrkam@netbeans.org>
2006-10-17 08:47:58 +0000

    Removed unnecessary comments

--- jemmy/src/org/netbeans/jemmy/util/DumpController.java
+++ jemmy/src/org/netbeans/jemmy/util/DumpController.java
@@ -3,9 +3,6 @@
  *
  * Created on October 17, 2005, 11:52 AM
  *
- * To change this template, choose Tools | Options and locate the template under
- * the Source Creation and Management node. Right-click the template and choose
- * Open. You can then make changes to the template in the Source Editor.
  */
 
 package org.netbeans.jemmy.util;
