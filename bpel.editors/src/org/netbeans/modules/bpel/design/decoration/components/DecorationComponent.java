c039a0b5689596fa14239e5532bca65b3a29e5f3
yaroslavskiy <yaroslavskiy@netbeans.org>
2006-10-20 12:02:27 +0000

    Initial version in trunk

---
+++ bpel.editors/src/org/netbeans/modules/bpel/design/decoration/components/DecorationComponent.java
@@ -0,0 +1,18 @@
+/*
+ * DecorationComponent.java
+ *
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package org.netbeans.modules.bpel.design.decoration.components;
+
+/**
+ *
+ * @author Alexey
+ * marker interfce to identify components added to diagram as "decorations"
+ */
+public interface DecorationComponent {
+    
+}
