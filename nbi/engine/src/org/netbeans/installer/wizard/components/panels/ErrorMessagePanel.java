75c7ea387009c4472ca23e222a74895108c378f2
ksorokin <ksorokin@netbeans.org>
2006-11-12 15:48:45 +0000

    Fix for 87493. ErrorMessagePane now performs additional validation in a background thread.

--- nbi/engine/src/org/netbeans/installer/wizard/components/panels/ErrorMessagePanel.java
+++ nbi/engine/src/org/netbeans/installer/wizard/components/panels/ErrorMessagePanel.java
@@ -15,7 +15,7 @@
  * The Original Software is NetBeans. The Initial Developer of the Original
  * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
  * Microsystems, Inc. All Rights Reserved.
- *  
+ *
  * $Id$
  */
 package org.netbeans.installer.wizard.components.panels;
@@ -24,6 +24,9 @@ import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.SwingUtilities;
+import org.netbeans.installer.utils.ErrorLevel;
+import org.netbeans.installer.utils.ErrorManager;
 import org.netbeans.installer.utils.helper.swing.NbiLabel;
 
 /**
@@ -31,10 +34,30 @@ import org.netbeans.installer.utils.helper.swing.NbiLabel;
  * @author ks152834
  */
 public abstract class ErrorMessagePanel extends DefaultWizardPanel {
+    /////////////////////////////////////////////////////////////////////////////////
+    // Constants
+    public static final String ERROR_ICON = "org/netbeans/installer/wizard/components/panels/error.png";
+    public static final String EMPTY_ICON = "org/netbeans/installer/wizard/components/panels/empty.png";
+    
+    public static final long VALIDATION_DELAY = 1000;
+    
+    /////////////////////////////////////////////////////////////////////////////////
+    // Instance
     private Icon errorIcon = new ImageIcon(getClass().getClassLoader().getResource(ERROR_ICON));
     private Icon emptyIcon = new ImageIcon(getClass().getClassLoader().getResource(EMPTY_ICON));
     
-    private NbiLabel errorLabel;
+    private ValidatingThread validatingThread = new ValidatingThread();
+    private NbiLabel         errorLabel = null;
+    
+    public void defaultInitialize() {
+        super.defaultInitialize();
+        
+        if (validatingThread.isAlive()) {
+            validatingThread.play();
+        } else {
+            validatingThread.start();
+        }
+    }
     
     public void defaultInitComponents() {
         super.defaultInitComponents();
@@ -42,6 +65,30 @@ public abstract class ErrorMessagePanel extends DefaultWizardPanel {
         errorLabel = new NbiLabel();
         
         add(errorLabel, new GridBagConstraints(0, 99, 99, 1, 1.0, 0.0, GridBagConstraints.CENTER, GridBagConstraints.HORIZONTAL, new Insets(7, 11, 11, 11), 0, 0));
+    }
+    
+    public void evaluateNextButtonClick() {
+        super.evaluateNextButtonClick();
+        
+        validatingThread.pause();
+    }
+    
+    public void evaluateHelpButtonClick() {
+        super.evaluateHelpButtonClick();
+        
+        validatingThread.pause();
+    }
+    
+    public void evaluateCancelButtonClick() {
+        super.evaluateCancelButtonClick();
+        
+        validatingThread.pause();
+    }
+    
+    public void evaluateBackButtonClick() {
+        super.evaluateBackButtonClick();
+        
+        validatingThread.pause();
     }
     
     public void updateErrorMessage() {
@@ -60,6 +107,31 @@ public abstract class ErrorMessagePanel extends DefaultWizardPanel {
     
     public abstract String validateInput();
     
-    private static final String ERROR_ICON = "org/netbeans/installer/wizard/components/panels/error.png";
-    private static final String EMPTY_ICON = "org/netbeans/installer/wizard/components/panels/empty.png";
+    /////////////////////////////////////////////////////////////////////////////////
+    // Inner Classes
+    private class ValidatingThread extends Thread {
+        private boolean paused = false;
+        
+        public void run() {
+            while (true) {
+                if (!paused) {
+                    updateErrorMessage();
+                }
+                
+                try {
+                    sleep(VALIDATION_DELAY);
+                } catch (InterruptedException e) {
+                    ErrorManager.notify(ErrorLevel.DEBUG, e);
+                }
+            }
+        }
+        
+        public void pause() {
+            paused = true;
+        }
+        
+        public void play() {
+            paused = false;
+        }
+    }
 }
