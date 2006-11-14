d5b37795cce299733da8a708d7f9838f1002956f
dkaspar <dkaspar@netbeans.org>
2006-11-14 10:04:11 +0000

    SPL changed to CDDL license

--- api.visual/src/org/netbeans/modules/visual/border/FancyDashedBorder.java
+++ api.visual/src/org/netbeans/modules/visual/border/FancyDashedBorder.java
@@ -1,13 +1,19 @@
 /*
- *                 Sun Public License Notice
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
  *
- * The contents of this file are subject to the Sun Public License
- * Version 1.0 (the "License"). You may not use this file except in
- * compliance with the License. A copy of the License is available at
- * http://www.sun.com/
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
  *
- * The Original Code is NetBeans. The Initial Developer of the Original
- * Code is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 package org.netbeans.modules.visual.border;
@@ -28,7 +34,7 @@ public final class FancyDashedBorder extends DashedBorder {
     public FancyDashedBorder(Color color, int width, int height) {
         super (color, width, height);
     }
-    
+
     public void paint(Graphics2D g, Rectangle bounds) {
         int x=bounds.x,y=bounds.y,width=bounds.width,height=bounds.height;
         //x=x+halfRectSize;y=y+halfRectSize;width=width-rectSize;height=height-rectSize;
