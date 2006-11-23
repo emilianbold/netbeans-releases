268e301d26b893835c72a5eea778e4f26b20fadf
rkubacki <rkubacki@netbeans.org>
2006-11-23 15:58:59 +0000

    much simpler bean info

--- diff/src/org/netbeans/modules/diff/builtin/provider/BuiltInDiffProviderBeanInfo.java
+++ diff/src/org/netbeans/modules/diff/builtin/provider/BuiltInDiffProviderBeanInfo.java
@@ -27,77 +27,9 @@ import org.openide.util.*;
  */
 public class BuiltInDiffProviderBeanInfo extends SimpleBeanInfo {
 
-    // Bean descriptor//GEN-FIRST:BeanDescriptor
-    /*lazy BeanDescriptor*/
-    private static BeanDescriptor getBdescriptor(){
-        BeanDescriptor beanDescriptor = new BeanDescriptor  ( org.netbeans.modules.diff.builtin.provider.BuiltInDiffProvider.class , null ); // NOI18N//GEN-HEADEREND:BeanDescriptor
-
-        // Here you can add code for customizing the BeanDescriptor.
-        
-        return beanDescriptor;         }//GEN-LAST:BeanDescriptor
-    
-    
-    // Property identifiers//GEN-FIRST:Properties
     private static final int PROPERTY_trimLines = 0;
 
-    // Property array 
-    /*lazy PropertyDescriptor*/
-    private static PropertyDescriptor[] getPdescriptor(){
-        PropertyDescriptor[] properties = new PropertyDescriptor[1];
-    
-        try {
-            properties[PROPERTY_trimLines] = new PropertyDescriptor ( "trimLines", org.netbeans.modules.diff.builtin.provider.BuiltInDiffProvider.class, "isTrimLines", "setTrimLines" ); // NOI18N
-            properties[PROPERTY_trimLines].setDisplayName ( NbBundle.getMessage(BuiltInDiffProvider.class,"BK0001") );
-            properties[PROPERTY_trimLines].setShortDescription ( NbBundle.getMessage(BuiltInDiffProvider.class,"BK0002") );
-        }
-        catch(IntrospectionException e) {
-            e.printStackTrace();
-        }//GEN-HEADEREND:Properties
-        
-        // Here you can add code for customizing the properties array.
-        
-        return properties;         }//GEN-LAST:Properties
-    
-    // EventSet identifiers//GEN-FIRST:Events
-
-    // EventSet array
-    /*lazy EventSetDescriptor*/
-    private static EventSetDescriptor[] getEdescriptor(){
-        EventSetDescriptor[] eventSets = new EventSetDescriptor[0];//GEN-HEADEREND:Events
-        
-        // Here you can add code for customizing the event sets array.
-        
-        return eventSets;         }//GEN-LAST:Events
-    
-    // Method identifiers//GEN-FIRST:Methods
-
-    // Method array 
-    /*lazy MethodDescriptor*/
-    private static MethodDescriptor[] getMdescriptor(){
-        MethodDescriptor[] methods = new MethodDescriptor[0];//GEN-HEADEREND:Methods
-        
-        // Here you can add code for customizing the methods array.
-        
-        return methods;         }//GEN-LAST:Methods
-    
-    private static java.awt.Image iconColor16 = null;//GEN-BEGIN:IconsDef
-    private static java.awt.Image iconColor32 = null;
-    private static java.awt.Image iconMono16 = null;
-    private static java.awt.Image iconMono32 = null;//GEN-END:IconsDef
     private static String iconNameC16 = "/org/netbeans/modules/diff/diffSettingsIcon.gif";//GEN-BEGIN:Icons
-    private static String iconNameC32 = null;
-    private static String iconNameM16 = null;
-    private static String iconNameM32 = null;//GEN-END:Icons
-    
-    private static final int defaultPropertyIndex = -1;//GEN-BEGIN:Idx
-    private static final int defaultEventIndex = -1;//GEN-END:Idx
-    
-    
-//GEN-FIRST:Superclass
-    
-    // Here you can add code for customizing the Superclass BeanInfo.
-    
-//GEN-LAST:Superclass
     
     /**
      * Gets the bean's <code>BeanDescriptor</code>s.
@@ -107,7 +39,7 @@ public class BuiltInDiffProviderBeanInfo extends SimpleBeanInfo {
      * information should be obtained by automatic analysis.
      */
     public BeanDescriptor getBeanDescriptor() {
-        return getBdescriptor();
+        return new BeanDescriptor  ( BuiltInDiffProvider.class , null );
     }
     
     /**
@@ -123,7 +55,18 @@ public class BuiltInDiffProviderBeanInfo extends SimpleBeanInfo {
      * if a given PropertyDescriptor is an IndexedPropertyDescriptor.
      */
     public PropertyDescriptor[] getPropertyDescriptors() {
-        return getPdescriptor();
+        PropertyDescriptor[] properties = new PropertyDescriptor[1];
+    
+        try {
+            properties[PROPERTY_trimLines] = new PropertyDescriptor ( "trimLines", BuiltInDiffProvider.class, "isTrimLines", "setTrimLines" ); // NOI18N
+            properties[PROPERTY_trimLines].setDisplayName ( NbBundle.getMessage(BuiltInDiffProvider.class,"BK0001") );
+            properties[PROPERTY_trimLines].setShortDescription ( NbBundle.getMessage(BuiltInDiffProvider.class,"BK0002") );
+        }
+        catch(IntrospectionException e) {
+            Exceptions.printStackTrace(e);
+        }
+        
+        return properties;
     }
     
     /**
@@ -134,7 +77,7 @@ public class BuiltInDiffProviderBeanInfo extends SimpleBeanInfo {
      * should be obtained by automatic analysis.
      */
     public EventSetDescriptor[] getEventSetDescriptors() {
-        return getEdescriptor();
+        return new EventSetDescriptor[0];
     }
     
     /**
@@ -145,89 +88,11 @@ public class BuiltInDiffProviderBeanInfo extends SimpleBeanInfo {
      * should be obtained by automatic analysis.
      */
     public MethodDescriptor[] getMethodDescriptors() {
-        return getMdescriptor();
+        return new MethodDescriptor[0];
     }
     
-    /**
-     * A bean may have a "default" property that is the property that will
-     * mostly commonly be initially chosen for update by human's who are
-     * customizing the bean.
-     * @return  Index of default property in the PropertyDescriptor array
-     * 		returned by getPropertyDescriptors.
-     * <P>	Returns -1 if there is no default property.
-     */
-    public int getDefaultPropertyIndex() {
-        return defaultPropertyIndex;
-    }
-    
-    /**
-     * A bean may have a "default" event that is the event that will
-     * mostly commonly be used by human's when using the bean.
-     * @return Index of default event in the EventSetDescriptor array
-     *		returned by getEventSetDescriptors.
-     * <P>	Returns -1 if there is no default event.
-     */
-    public int getDefaultEventIndex() {
-        return defaultEventIndex;
-    }
-    
-    /**
-     * This method returns an image object that can be used to
-     * represent the bean in toolboxes, toolbars, etc.   Icon images
-     * will typically be GIFs, but may in future include other formats.
-     * <p>
-     * Beans aren't required to provide icons and may return null from
-     * this method.
-     * <p>
-     * There are four possible flavors of icons (16x16 color,
-     * 32x32 color, 16x16 mono, 32x32 mono).  If a bean choses to only
-     * support a single icon we recommend supporting 16x16 color.
-     * <p>
-     * We recommend that icons have a "transparent" background
-     * so they can be rendered onto an existing background.
-     *
-     * @param  iconKind  The kind of icon requested.  This should be
-     *    one of the constant values ICON_COLOR_16x16, ICON_COLOR_32x32,
-     *    ICON_MONO_16x16, or ICON_MONO_32x32.
-     * @return  An image object representing the requested icon.  May
-     *    return null if no suitable icon is available.
-     */
     public java.awt.Image getIcon(int iconKind) {
-        switch ( iconKind ) {
-            case ICON_COLOR_16x16:
-                if ( iconNameC16 == null )
-                    return null;
-                else {
-                    if( iconColor16 == null )
-                        iconColor16 = loadImage( iconNameC16 );
-                    return iconColor16;
-                }
-            case ICON_COLOR_32x32:
-                if ( iconNameC32 == null )
-                    return null;
-                else {
-                    if( iconColor32 == null )
-                        iconColor32 = loadImage( iconNameC32 );
-                    return iconColor32;
-                }
-            case ICON_MONO_16x16:
-                if ( iconNameM16 == null )
-                    return null;
-                else {
-                    if( iconMono16 == null )
-                        iconMono16 = loadImage( iconNameM16 );
-                    return iconMono16;
-                }
-            case ICON_MONO_32x32:
-                if ( iconNameM32 == null )
-                    return null;
-                else {
-                    if( iconMono32 == null )
-                        iconMono32 = loadImage( iconNameM32 );
-                    return iconMono32;
-                }
-            default: return null;
-        }
+        return Utilities.loadImage(iconNameC16);
     }
     
 }
--- j2ee.sun.appsrv/src/org/netbeans/modules/j2ee/sun/ide/sunresources/beans/JMSBeanBeanInfo.java
+++ j2ee.sun.appsrv/src/org/netbeans/modules/j2ee/sun/ide/sunresources/beans/JMSBeanBeanInfo.java
@@ -19,19 +19,12 @@
 package org.netbeans.modules.j2ee.sun.ide.sunresources.beans;
 
 import java.beans.*;
+import org.openide.util.Exceptions;
 import org.openide.util.NbBundle;
 import org.netbeans.modules.j2ee.sun.ide.editors.BooleanEditor;
 
 public class JMSBeanBeanInfo extends SimpleBeanInfo {
 
-    // Bean descriptor//GEN-FIRST:BeanDescriptor
-    /*lazy BeanDescriptor*/
-    private static BeanDescriptor getBdescriptor(){
-        BeanDescriptor beanDescriptor = new BeanDescriptor  ( JMSBean.class , null );//GEN-HEADEREND:BeanDescriptor
-
-        // Here you can add code for customizing the BeanDescriptor.
-        
-        return beanDescriptor;         }//GEN-LAST:BeanDescriptor
     
     static private String getLabel(String key){
         return NbBundle.getMessage(JMSBean.class,key);
@@ -44,9 +37,32 @@ public class JMSBeanBeanInfo extends SimpleBeanInfo {
     private static final int PROPERTY_name = 3;
     private static final int PROPERTY_resType = 4;
 
-    // Property array 
-    /*lazy PropertyDescriptor*/
-    private static PropertyDescriptor[] getPdescriptor(){
+    private static final int EVENT_propertyChangeListener = 0;
+
+    /**
+     * Gets the bean's <code>BeanDescriptor</code>s.
+     *
+     * @return BeanDescriptor describing the editable
+     * properties of this bean.  May return null if the
+     * information should be obtained by automatic analysis.
+     */
+    public BeanDescriptor getBeanDescriptor() {
+        return new BeanDescriptor  ( JMSBean.class , null );
+    }
+    
+    /**
+     * Gets the bean's <code>PropertyDescriptor</code>s.
+     *
+     * @return An array of PropertyDescriptors describing the editable
+     * properties supported by this bean.  May return null if the
+     * information should be obtained by automatic analysis.
+     * <p>
+     * If a property is indexed, then its entry in the result array will
+     * belong to the IndexedPropertyDescriptor subclass of PropertyDescriptor.
+     * A client of getPropertyDescriptors can use "instanceof" to check
+     * if a given PropertyDescriptor is an IndexedPropertyDescriptor.
+     */
+    public PropertyDescriptor[] getPropertyDescriptors() {
         PropertyDescriptor[] properties = new PropertyDescriptor[5];
     
         try {
@@ -66,121 +82,31 @@ public class JMSBeanBeanInfo extends SimpleBeanInfo {
             properties[PROPERTY_resType].setDisplayName ( getLabel("LBL_JMSResType") );
             properties[PROPERTY_resType].setShortDescription ( getLabel("DSC_JMSResType") );
         }
-        catch( IntrospectionException e) {}//GEN-HEADEREND:Properties
+        catch( IntrospectionException e) {
+            Exceptions.printStackTrace(e);
+        }
         
-        // Here you can add code for customizing the properties array.
-        
-        return properties;         }//GEN-LAST:Properties
+        return properties;
+    }
     
-    // EventSet identifiers//GEN-FIRST:Events
-    private static final int EVENT_propertyChangeListener = 0;
-
-    // EventSet array
-    /*lazy EventSetDescriptor*/
-    private static EventSetDescriptor[] getEdescriptor(){
+    public EventSetDescriptor[] getEventSetDescriptors() {
         EventSetDescriptor[] eventSets = new EventSetDescriptor[1];
     
             try {
-            eventSets[EVENT_propertyChangeListener] = new EventSetDescriptor ( org.netbeans.modules.j2ee.sun.ide.sunresources.beans.JMSBean.class, "propertyChangeListener", java.beans.PropertyChangeListener.class, new String[] {"propertyChange"}, "addPropertyChangeListener", "removePropertyChangeListener" );
+            eventSets[EVENT_propertyChangeListener] = new EventSetDescriptor ( JMSBean.class, "propertyChangeListener", java.beans.PropertyChangeListener.class, new String[] {"propertyChange"}, "addPropertyChangeListener", "removePropertyChangeListener" );
         }
-        catch( IntrospectionException e) {}//GEN-HEADEREND:Events
+        catch( IntrospectionException e) {
+            Exceptions.printStackTrace(e);
+        }
         
         // Here you can add code for customizing the event sets array.
         
-        return eventSets;         }//GEN-LAST:Events
-    
-    // Method identifiers//GEN-FIRST:Methods
-
-    // Method array 
-    /*lazy MethodDescriptor*/
-    private static MethodDescriptor[] getMdescriptor(){
-        MethodDescriptor[] methods = new MethodDescriptor[0];//GEN-HEADEREND:Methods
-        
-        // Here you can add code for customizing the methods array.
-        
-        return methods;         }//GEN-LAST:Methods
-    
-    
-    private static final int defaultPropertyIndex = -1;//GEN-BEGIN:Idx
-    private static final int defaultEventIndex = -1;//GEN-END:Idx
-    
-    
-//GEN-FIRST:Superclass
-    
-    // Here you can add code for customizing the Superclass BeanInfo.
-    
-//GEN-LAST:Superclass
-    
-    /**
-     * Gets the bean's <code>BeanDescriptor</code>s.
-     *
-     * @return BeanDescriptor describing the editable
-     * properties of this bean.  May return null if the
-     * information should be obtained by automatic analysis.
-     */
-    public BeanDescriptor getBeanDescriptor() {
-        return getBdescriptor();
+        return eventSets;
     }
     
-    /**
-     * Gets the bean's <code>PropertyDescriptor</code>s.
-     *
-     * @return An array of PropertyDescriptors describing the editable
-     * properties supported by this bean.  May return null if the
-     * information should be obtained by automatic analysis.
-     * <p>
-     * If a property is indexed, then its entry in the result array will
-     * belong to the IndexedPropertyDescriptor subclass of PropertyDescriptor.
-     * A client of getPropertyDescriptors can use "instanceof" to check
-     * if a given PropertyDescriptor is an IndexedPropertyDescriptor.
-     */
-    public PropertyDescriptor[] getPropertyDescriptors() {
-        return getPdescriptor();
-    }
-    
-    /**
-     * Gets the bean's <code>EventSetDescriptor</code>s.
-     *
-     * @return  An array of EventSetDescriptors describing the kinds of
-     * events fired by this bean.  May return null if the information
-     * should be obtained by automatic analysis.
-     */
-    public EventSetDescriptor[] getEventSetDescriptors() {
-        return getEdescriptor();
-    }
-    
-    /**
-     * Gets the bean's <code>MethodDescriptor</code>s.
-     *
-     * @return  An array of MethodDescriptors describing the methods
-     * implemented by this bean.  May return null if the information
-     * should be obtained by automatic analysis.
-     */
     public MethodDescriptor[] getMethodDescriptors() {
-        return getMdescriptor();
+        return new MethodDescriptor[0];
     }
     
-    /**
-     * A bean may have a "default" property that is the property that will
-     * mostly commonly be initially chosen for update by human's who are
-     * customizing the bean.
-     * @return  Index of default property in the PropertyDescriptor array
-     * 		returned by getPropertyDescriptors.
-     * <P>	Returns -1 if there is no default property.
-     */
-    public int getDefaultPropertyIndex() {
-        return defaultPropertyIndex;
-    }
-    
-    /**
-     * A bean may have a "default" event that is the event that will
-     * mostly commonly be used by human's when using the bean.
-     * @return Index of default event in the EventSetDescriptor array
-     *		returned by getEventSetDescriptors.
-     * <P>	Returns -1 if there is no default event.
-     */
-    public int getDefaultEventIndex() {
-        return defaultEventIndex;
-    }
 }
 
