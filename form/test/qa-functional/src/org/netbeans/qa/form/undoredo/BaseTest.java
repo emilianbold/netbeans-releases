c7e8e5cb4affd2fce1563c253d55d432c7e47e9d
janie <janie@netbeans.org>
2006-12-07 14:46:36 +0000

    fixed test edit redu, undo method

--- form/test/qa-functional/src/org/netbeans/qa/form/undoredo/BaseTest.java
+++ form/test/qa-functional/src/org/netbeans/qa/form/undoredo/BaseTest.java
@@ -43,7 +43,7 @@ import org.netbeans.jemmy.ComponentSearcher;
 import org.netbeans.jemmy.operators.JTabbedPaneOperator;
 import org.netbeans.jemmy.operators.Operator;
 import org.netbeans.jemmy.operators.JToggleButtonOperator;
-import org.netbeans.junit.ide.ProjectSupport;
+//import org.netbeans.junit.ide.ProjectSupport;
 import org.netbeans.qa.form.*;
 import java.io.*;
 
@@ -63,7 +63,7 @@ public class BaseTest extends JellyTestCase {
     ComponentInspectorOperator inspector;
     FormDesignerOperator formDesigner;
     ComponentPaletteOperator palette;
-
+    
     EditorOperator editor;
     EditorWindowOperator ewo;
     String fileName;
@@ -117,16 +117,16 @@ public class BaseTest extends JellyTestCase {
         Node inspectorRootNode = new Node(cio.treeComponents(), FRAME_ROOT);
         inspectorRootNode.select();
         inspectorRootNode.expand();
-      
+        
         palette = new ComponentPaletteOperator();
         inspector = new ComponentInspectorOperator();
         
         //init property sheet and select the proper "tab"
         PropertySheetOperator pso = cio.properties();
-//        selectPropertiesTab(pso);
-        new Action(null,"Add From Palette|Swing|JPanel").performPopup(new Node(inspector.treeComponents(),"[JFrame]"));
-//        selectPropertiesTab(pso);
-        new Action(null,"Add From Palette|Swing|JPanel").performPopup(new Node(inspector.treeComponents(),"[JFrame]"));
+        //        selectPropertiesTab(pso);
+        new Action(null,"Add From Palette|Swing Containers|Panel").performPopup(new Node(inspector.treeComponents(),"[JFrame]"));
+        //        selectPropertiesTab(pso);
+        new Action(null,"Add From Palette|Swing Containers|Panel").performPopup(new Node(inspector.treeComponents(),"[JFrame]"));
         
         
         
@@ -135,24 +135,26 @@ public class BaseTest extends JellyTestCase {
         inspector.selectComponent("[JFrame]|JPanel1 [JPanel]");
         selectPropertiesTab(pso);
         
-//        new ColorProperty(pso, "background").setRGBValue(202,234,223);
-        new ColorProperty(new PropertySheetOperator("jPanel1 [JPanel] - Properties"), "background").invokeCustomizer().setColorValue(new Color(202,234,223));
+        //        new ColorProperty(pso, "background").setRGBValue(202,234,223);
+        
+        new ColorProperty(new PropertySheetOperator("jPanel1 [JPanel] - Properties"), "background").setColorValue(new Color(202,234,223));
+        
         inspector.selectComponent("[JFrame]|JPanel2 [JPanel]");
         selectPropertiesTab(pso);
         new ColorProperty(new PropertySheetOperator("jPanel2 [JPanel] - Properties"), "background").setRGBValue(252,34,3);
         
         // add JButton1 to JPanel1
-        new Action(null,"Add From Palette|Swing|JButton").performPopup(new Node(inspector.treeComponents(),"[JFrame]|JPanel1 [JPanel]"));
+        new Action(null,"Add From Palette|Swing Controls|Button").performPopup(new Node(inspector.treeComponents(),"[JFrame]|JPanel1 [JPanel]"));
         
         // add JButton2 to JPanel2
-        new Action(null,"Add From Palette|Swing|JButton").performPopup(new Node(inspector.treeComponents(),"[JFrame]|JPanel2 [JPanel]"));
+        new Action(null,"Add From Palette|Swing Controls|Button").performPopup(new Node(inspector.treeComponents(),"[JFrame]|JPanel2 [JPanel]"));
         
         // cut-paste JButton1 from JPanel1 to JPanel2
         
         new Action(null,"Cut").performPopup(new Node(inspector.treeComponents(),"[JFrame]|JPanel1 [JPanel]|jButton1 [JButton]"));
         new Action(null,"Paste").performPopup(new Node(inspector.treeComponents(),"[JFrame]|JPanel2 [JPanel]"));
         
-
+        
         
         // change properties
         inspector.selectComponent("[JFrame]|JPanel2 [JPanel]|jButton1 [JButton]");
@@ -185,113 +187,147 @@ public class BaseTest extends JellyTestCase {
         
         Property prop = new Property(pso, "actionPerformed");
         prop.setValue("myAction");
-
-        openAction.perform(formnode);    
-        sleep(200) ;
-     //   selectPropertiesTab(pso);
         
-        //2x
+        
+        //   selectPropertiesTab(pso);
+        
+        
         for (int i=0;i<2;i++) {
-            // undo
-//            assertTrue("check in Editor 11b",checkEditor("private void myAction"));
+            // section undo testing
+            
+            openAction.perform(formnode);
+            sleep(2000) ;
+            
+            assertTrue("check in Editor 11b",checkEditor("private void myAction"));
+            
             undo(1);
-//            assertTrue("check in Editor 11a",!checkEditor("private void myAction"));
             
-//            assertTrue("check in Editor 10b",checkEditor("aaa,bbb"));
+            // check if aaa, bbb are generated
+            
+            assertTrue("check in Editor 10b",checkEditor("aaa,bbb"));
+            
             undo(1);
-//            assertTrue("check in Editor 10a",!checkEditor("aaa,bbb"));
             
-//            assertTrue("check in Editor 9b",checkEditor("jPanel2.add(jButton1,jPanel2.add(jButton2"));
+            // check if aaa, bbb are not in editor
+            assertTrue("check i// assertTrun Editor 10a",!checkEditor("aaa,bbb"));
+            
+            
+            //now it's not possible to check, in editore, there is different code (no on the same row)
+            assertTrue("check in Editor 9b",checkEditor("jPanel2.add(jButton1),jPanel2.add(jButton2)"));
+            
             undo(1);
-//            assertTrue("check in Editor 9a",checkEditor("jPanel2.add(jButton2,jPanel2.add(jButton1"));
+            //check if panel order was changed
+            assertTrue("check in Editor 9b",checkEditor("jPanel2.add(jButton2),jPanel2.add(jButton1)"));
             
-//            assertTrue("check in Editor 8b",checkEditor("html"));
+            
+            assertTrue("check in Editor 8b",checkEditor("<html>"));
+            
             undo(1);
-//            assertTrue("check in Editor 8a",!checkEditor("html"));
             
-//            assertTrue("check in Editor 7b",checkEditor("jPanel2.add(jButton1"));
+            assertTrue("check in Editor 8a",!checkEditor("<html>"));
+            
+            
+            //check if both buttons are in panel2
+            assertTrue("check in Editor 7a",checkEditor("jPanel2.add(jButton1"));
+            assertTrue("check in Editor 7b",checkEditor("jPanel2.add(jButton2"));
+            
             undo(1);
-//            assertTrue("check in Editor 7a",!checkEditor("jPanel2.add(jButton1"));
+            //check if panel2 has only button2 and panel1 has only button1 as well
+            assertTrue("check in Editor 7c",!checkEditor("jPanel2.add(jButton1"));
+            assertTrue("check in Editor 7d",checkEditor("jPanel1.add(jButton1"));
+            assertTrue("check in Editor 7e",checkEditor("jPanel2.add(jButton2"));
             
-//            assertTrue("check in Editor 6b",checkEditor("jPanel2.add(jButton2"));
             undo(1);
-//            assertTrue("check in Editor 6a",!checkEditor("jPanel2.add(jButton2"));
+            //check if button2 is not in jframe and background of panel2 is still set
+            assertTrue("check in Editor 4a",!checkEditor("jButton2"));
+            assertTrue("check in Editor 4b",checkEditor("jPanel2.setBackground"));
             
-//            assertTrue("check in Editor 5b",checkEditor("jPanel1.add(jButton1"));
             undo(1);
-//            assertTrue("check in Editor 5a",!checkEditor("jPanel1.add(jButton1"));
+            //check if button1 is not in jframe and background of panel1 is still set
+            assertTrue("check in Editor 3a",!checkEditor("jButton1"));
+            assertTrue("check in Editor 3b",checkEditor("jPanel1.setBackground"));
             
-//            assertTrue("check in Editor 4b",checkEditor("jPanel2.setBackground"));
             undo(1);
-//            assertTrue("check in Editor 4a",!checkEditor("jPanel2.setBackground"));
+            //check background if is not set on panel2
+            assertTrue("check in Editor 2a",!checkEditor("jPanel2.setBackground"));
             
-//            assertTrue("check in Editor 3b",checkEditor("jPanel1.setBackground"));
             undo(1);
-//            assertTrue("check in Editor 3a",!checkEditor("jPanel1.setBackground"));
+            //check background if is not set on panel1
+            assertTrue("check in Editor 1a",!checkEditor("jPanel1.setBackground"));
             
-//            assertTrue("check in Editor 2b",checkEditor("jPanel2"));
+            
             undo(1);
-//            assertTrue("check in Editor 2a",!checkEditor("jPanel2"));
             
-//            assertTrue("check in Editor 1b",checkEditor("jPanel1"));
+            //check if panel2 disappeared from jframe
+            assertTrue("check in Editor 0a",!checkEditor("jPanel2"));
+            
             undo(1);
-//            assertTrue("check in Editor 1a",!checkEditor("jPanel1"));
-            // redo
+            //check if panel1 disappeared from jframe
+            assertTrue("check in Editor 0b",!checkEditor("jPanel1"));
             
-//            assertTrue("check in Editor 101a",!checkEditor("jPanel1"));
+            
+            //redo
+            
             redo(1);
-//            assertTrue("check in Editor 101b",checkEditor("jPanel1"));
+            //check if panel1 is in jframe
+            assertTrue("check in Editor R01a",checkEditor("jPanel1"));
             
-//            assertTrue("check in Editor 102a",!checkEditor("jPanel2"));
             redo(1);
-//            assertTrue("check in Editor 102b",checkEditor("jPanel2"));
+            //check if panel1 is in jframe
+            assertTrue("check in Editor R02a",checkEditor("jPanel2"));
             
-//            assertTrue("check in Editor 103a",!checkEditor("jPanel1.setBackground"));
             redo(1);
-//            assertTrue("check in Editor 103b",checkEditor("jPanel1.setBackground"));
+            //check if background was set for panel1
+            assertTrue("check in Editor R03a",checkEditor("jPanel1.setBackground"));
             
-//            assertTrue("check in Editor 104a",!checkEditor("jPanel2.setBackground"));
             redo(1);
-//            assertTrue("check in Editor 104b",checkEditor("jPanel2.setBackground"));
+            //check if background was set for panel2
+            assertTrue("check in Editor R04a",checkEditor("jPanel2.setBackground"));
             
-//            assertTrue("check in Editor 105a",!checkEditor("jPanel1.add(jButton1"));
             redo(1);
-//            assertTrue("check in Editor 105b",checkEditor("jPanel1.add(jButton1"));
+            //check if button1 was added in panel1
+            assertTrue("check in Editor R05a",checkEditor("jPanel1.add(jButton1"));
             
-//            assertTrue("check in Editor 106a",!checkEditor("jPanel2.add(jButton2"));
             redo(1);
-//            assertTrue("check in Editor 106b",checkEditor("jPanel2.add(jButton2"));
+            //check if button2 was added in panel2
+            assertTrue("check in Editor R06a",checkEditor("jPanel2.add(jButton2"));
             
-//            assertTrue("check in Editor 107a",!checkEditor("jPanel2.add(jButton1"));
             redo(1);
-//            assertTrue("check in Editor 107b",checkEditor("jPanel2.add(jButton1"));
+            //check if panel2 contains both buttons and panel1 is empty
+            assertTrue("check in Editor R07a",checkEditor("jPanel2.add(jButton1"));
+            assertTrue("check in Editor R07b",checkEditor("jPanel2.add(jButton2"));
+            assertTrue("check in Editor R07c",!checkEditor("jPanel1.add(jButton2"));
             
-//            assertTrue("check in Editor 108a",!checkEditor("html"));
             redo(1);
-//            assertTrue("check in Editor 108b",checkEditor("html"));
+            //check if text in html was added
+            assertTrue("check in Editor R08a",checkEditor("<html>"));
+            assertTrue("check in Editor R08b",checkEditor("jPanel2.add(jButton2),jPanel2.add(jButton1)"));
             
-//            assertTrue("check in Editor 109a",checkEditor("jPanel2.add(jButton2,jPanel2.add(jButton1"));
             redo(1);
-//            assertTrue("check in Editor 109b",checkEditor("jPanel2.add(jButton1,jPanel2.add(jButton2"));
+            //check if buttons order was changed
+            assertTrue("check in Editor 9b",checkEditor("jPanel2.add(jButton1),jPanel2.add(jButton2)"));
             
-//            assertTrue("check in Editor 110a",!checkEditor("aaa,bbb"));
             redo(1);
-//            assertTrue("check in Editor 110b",checkEditor("aaa,bbb"));
+            //check if string aaa, bbb was added in editor
+            assertTrue("check in Editor 10b",checkEditor("aaa,bbb"));
             
-//            assertTrue("check in Editor 111a",!checkEditor("private void myAction"));
             redo(1);
-//            assertTrue("check in Editor 111b",checkEditor("private void myAction"));
+            //check if action "myAction" was added in editor
+            assertTrue("check in Editor 11b",checkEditor("private void myAction"));
             
+            
+            
+        
         }
         
-        undo(11);
-
+          undo(11);
         
-    
-    
-        Action saveAction;
-        saveAction = new Action("File|Save", null);
-        saveAction.perform();
+        
+        
+        
+        //        Action saveAction;
+        //        saveAction = new Action("File|Save", null);
+        //        saveAction.perform();
         
     }
     
@@ -301,9 +337,9 @@ public class BaseTest extends JellyTestCase {
     public void testFormFile() {
         try {
             getRef().print(
-            VisualDevelopmentUtil.readFromFile(
-            getDataDir().getAbsolutePath() + File.separatorChar + DATA_PROJECT_NAME +  File.separatorChar + "src" + File.separatorChar + PACKAGE_NAME + File.separatorChar + FILE_NAME + ".form")
-            );
+                    VisualDevelopmentUtil.readFromFile(
+                    getDataDir().getAbsolutePath() + File.separatorChar + DATA_PROJECT_NAME +  File.separatorChar + "src" + File.separatorChar + PACKAGE_NAME + File.separatorChar + FILE_NAME + ".form")
+                    );
         } catch (Exception e) {
             fail("Fail during create reffile: " + e.getMessage());
         }
@@ -321,21 +357,21 @@ public class BaseTest extends JellyTestCase {
     public void testJavaFile() {
         try {
             getRef().print(
-            VisualDevelopmentUtil.readFromFile(
-            getDataDir().getAbsolutePath() + File.separatorChar + DATA_PROJECT_NAME +  File.separatorChar + "src" + File.separatorChar + PACKAGE_NAME + File.separatorChar + FILE_NAME + ".java")
-            );
+                    VisualDevelopmentUtil.readFromFile(
+                    getDataDir().getAbsolutePath() + File.separatorChar + DATA_PROJECT_NAME +  File.separatorChar + "src" + File.separatorChar + PACKAGE_NAME + File.separatorChar + FILE_NAME + ".java")
+                    );
         } catch (Exception e) {
             fail("Fail during create reffile: " + e.getMessage());
         }
         compareReferenceFiles();
-       
+        
         ewo = new EditorWindowOperator();
         ewo.closeDiscard();
-
+        
         
         log("Test finished");
         
-    
+        
     }
     /** Run test.
      */
@@ -346,21 +382,26 @@ public class BaseTest extends JellyTestCase {
         //first switch to FormEditor tab
 /*OpenAction openAction = new OpenAction();
 openAction.perform(formnode);
-*/        
+ */
         MainWindowOperator mainWindow = MainWindowOperator.getDefault();
-        inspector.selectComponent("[JFrame]");
+        
+        OpenAction openAction = new OpenAction();
+        openAction.perform(formnode);
+        
+        sleep(500) ;
+        //inspector.selectComponent("[JFrame]");
         for (int i=0;i<n;i++) {
-            sleep(1000);
+            sleep(500);
             mainWindow.getToolbarButton(mainWindow.getToolbar("Edit"), "Undo").push();
-            sleep(1000);
+            sleep(500);
         }
     }
     
     void redo(int n) {
         //first switch to FormEditor tab
-/*OpenAction openAction = new OpenAction();
-openAction.perform(formnode);
-*/        
+        OpenAction openAction = new OpenAction();
+        openAction.perform(formnode);
+        
         MainWindowOperator mainWindow = MainWindowOperator.getDefault();
         inspector.selectComponent("[JFrame]");
         for (int i=0;i<n;i++) {
@@ -378,11 +419,11 @@ openAction.perform(formnode);
     boolean checkEditor(String regexp) {
         /*editor = ewo.getEditor("clear_JFrame");
         editor = new EditorOperator("clear_JFrame");
-        */
+         */
         EditAction editAction = new EditAction();
         editAction.perform(formnode);
         log("Source Editor window opened.");
-
+        
         
         editor = ewo.getEditor();
         sleep(300);
@@ -421,14 +462,14 @@ openAction.perform(formnode);
     public static NbTestSuite suite() {
         NbTestSuite suite = new NbTestSuite();
         suite.addTest(new BaseTest("testScenario"));
-        suite.addTest(new BaseTest("testFormFile"));
-        suite.addTest(new BaseTest("testJavaFile"));
-           return suite;
+        //suite.addTest(new BaseTest("testFormFile"));
+        //suite.addTest(new BaseTest("testJavaFile"));
+        return suite;
     }
    /* public static junit.framework.Test suite() {
         return new NbTestSuite(BaseTest.class);
     }
-*/    
+    */
     
 }
 
