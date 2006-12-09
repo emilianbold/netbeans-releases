6a0f408b69a57d044db609129c79163ab8ed583e
jglick <jglick@netbeans.org>
2006-12-09 23:36:27 +0000

    Adjusting test to account for #89131.

--- java.j2seplatform/test/unit/src/org/netbeans/modules/java/j2seplatform/platformdefinition/DefaultSourceLevelQueryImplTest.java
+++ java.j2seplatform/test/unit/src/org/netbeans/modules/java/j2seplatform/platformdefinition/DefaultSourceLevelQueryImplTest.java
@@ -16,67 +16,39 @@
  * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
+
 package org.netbeans.modules.java.j2seplatform.platformdefinition;
 
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-import junit.framework.*;
-import org.netbeans.api.java.platform.JavaPlatform;
-import org.netbeans.api.java.platform.JavaPlatformManager;
-import org.netbeans.api.project.TestUtil;
-import org.netbeans.core.startup.layers.ArchiveURLMapper;
+import org.netbeans.api.java.queries.SourceLevelQuery;
+import org.netbeans.junit.MockServices;
 import org.netbeans.junit.NbTestCase;
-import org.netbeans.modules.masterfs.MasterURLMapper;
-import org.netbeans.spi.java.queries.SourceLevelQueryImplementation;
 import org.openide.filesystems.FileLock;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
-import org.openide.util.Lookup;
-import org.openide.util.lookup.Lookups;
 
 /**
- *
  * @author Tomas Zezula
  */
-public class DefaultSourceLevelQueryImplTest extends NbTestCase implements Lookup.Provider {
-    
-    private Lookup lookup;
+public class DefaultSourceLevelQueryImplTest extends NbTestCase {
     
     public DefaultSourceLevelQueryImplTest(String testName) {
         super(testName);
-        TestUtil.setLookup(Lookups.proxy(this));
     }
 
     protected void setUp() throws Exception {
-        this.clearWorkDir();
-    }
-
-    protected void tearDown() throws Exception {
+        clearWorkDir();
+        MockServices.setServices(new Class[] {JavaPlatformProviderImpl.class});
     }
 
     public void testGetSourceLevel() throws Exception {
-        JavaPlatform jp = JavaPlatformManager.getDefault().getDefaultPlatform();
-        assertNotNull ("Default platform is null",jp);  //NOI18N
         FileObject root = FileUtil.toFileObject(this.getWorkDir());
         assertNotNull ("Cannot convert File to FileObject, missing master-fs?",root);    //NOI18N
         FileObject javaFile = createTestFile (root,"test","Test.java","package test;\n class Test {}");    //NOI18N
-        DefaultSourceLevelQueryImpl instance = new DefaultSourceLevelQueryImpl();        
-        String result = instance.getSourceLevel(javaFile);
-        assertEquals("DefaultSourceLevelQueryImpl returned wrong source level",jp.getSpecification().getVersion().toString(), result);  //NOI18N
+        assertEquals("1.5", SourceLevelQuery.getSourceLevel(javaFile));
     }        
-    
-    public Lookup getLookup () {
-        if (this.lookup == null) {
-            this.lookup = Lookups.fixed (
-                new Object[] {
-                    new ArchiveURLMapper (),
-                    new MasterURLMapper(),
-                    new JavaPlatformProviderImpl(),
-                });
-        }
-        return this.lookup;
-    }
     
     private FileObject createTestFile (FileObject root, String path, String fileName, String content) throws IOException {
         FileObject pkg = FileUtil.createFolder(root, path);
