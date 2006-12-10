769d841e72d8cbe2fab4a094363079efe182f5f1
ksorokin <ksorokin@netbeans.org>
2006-12-10 21:07:42 +0000

    Added RSS feeds.

--- nbi/infra/server/modules/war-client/src/java/org/netbeans/installer/infra/server/client/servlets/Feed.java
+++ nbi/infra/server/modules/war-client/src/java/org/netbeans/installer/infra/server/client/servlets/Feed.java
@@ -2,6 +2,7 @@ package org.netbeans.installer.infra.server.client.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
@@ -21,17 +22,53 @@ public class Feed extends HttpServlet {
     private Manager manager;
     
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        final PrintWriter out        = response.getWriter();
+        final String[]    registries = request.getParameterValues("registry");
+        
+        List<ProductComponent> components;
+        String                 feedType;
+        
+        // if the user did not specify any registry to look for the components,
+        // we cannot guess for him - will return an empty feed
+        if ((registries == null) || (registries.length == 0)) {
+            components = new ArrayList<ProductComponent>();
+        } else {
+            components = manager.getComponents(registries);
+        }
+        
+        feedType = request.getParameter("feed-type");
+        if (feedType == null) {
+            feedType = "rss";
+        }
+        
         response.setContentType("application/octet-stream");
         
-        final PrintWriter            out        = response.getWriter();
-        final String                 feedType   = request.getParameter("feed-type");
-        final String[]               registries = request.getParameterValues("registry");
-        final List<ProductComponent> components = manager.getComponents(registries);
+        out.println("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
         
-        out.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
-        
-        
+        if (feedType.equals("rss")) {
+            buildRss(components, out);
+        }
         
         out.close();
+    }
+    
+    private void buildRss(List<ProductComponent> components, PrintWriter out) throws IOException {
+        out.println("<rss version=\"2.0\">");
+        out.println("    <channel>");
+        
+        out.println("        <title><![CDATA[NetBeans Installer Components Feed]]></title>");
+        out.println("        <link>http://localhost/</link>");
+        out.println("        <description><![CDATA[NetBeans Installer Components Feed]]></description>");
+        
+        for (ProductComponent component: components) {
+            out.println("            <item>");
+            out.println("                <title><![CDATA[" + component.getDisplayName() + "]]></title>");
+            out.println("                <link>http://localhost/</link>");
+            out.println("                <description><![CDATA[" + component.getDescription() + "]]></description>");
+            out.println("            </item>");
+        }
+        
+        out.println("    </channel>");
+        out.println("</rss>");
     }
 }
