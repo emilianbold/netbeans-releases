2d20037656133bb52edc10a1b3499dfc6f36fc88
pflaska <pflaska@netbeans.org>
2006-12-13 15:14:29 +0000

    Test added.

---
+++ java.source/test/unit/src/org/netbeans/api/java/source/gen/BodyStatementTest.java
@@ -0,0 +1,128 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+package org.netbeans.api.java.source.gen;
+
+import java.io.File;
+import java.util.Collections;
+import com.sun.source.tree.BlockTree;
+import com.sun.source.tree.ClassTree;
+import com.sun.source.tree.ExpressionStatementTree;
+import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.MethodTree;
+import org.netbeans.api.java.source.CancellableTask;
+import org.netbeans.api.java.source.JavaSource;
+import static org.netbeans.api.java.source.JavaSource.*;
+import org.netbeans.api.java.source.TestUtilities;
+import org.netbeans.api.java.source.TreeMaker;
+import org.netbeans.api.java.source.WorkingCopy;
+import org.openide.filesystems.FileUtil;
+
+/**
+ * Test class add couple of body statements. It test statements creation and
+ * addition to body.
+ * 
+ * @author Pavel Flaska
+ */
+public class BodyStatementTest extends GeneratorTest {
+    
+    /** Creates a new instance of BodyStatementTest */
+    public BodyStatementTest(String name) {
+        super(name);
+    }
+    
+    /**
+     * Adds 'System.err.println(null);' statement to the method body. 
+     */
+    public void testNullLiteral() throws Exception {
+        testFile = new File(getWorkDir(), "Test.java");
+        TestUtilities.copyStringToFile(testFile, 
+            "package hierbas.del.litoral;\n\n" +
+            "import java.io.*;\n\n" +
+            "public class Test {\n" +
+            "    public void taragui() {\n" +
+            "        ;\n" +
+            "    }\n" +
+            "}\n"
+            );
+        String golden = 
+            "package hierbas.del.litoral;\n\n" +
+            "import java.io.*;\n\n" +
+            "public class Test {\n" +
+            "    public void taragui() {\n" +
+            "        ;\n" +
+            "    System.err.println(null);\n" + 
+            "}\n" +
+            "}\n";
+        JavaSource testSource = JavaSource.forFileObject(FileUtil.toFileObject(testFile));
+        
+        CancellableTask task = new CancellableTask<WorkingCopy>() {
+
+            public void run(WorkingCopy workingCopy) throws java.io.IOException {
+                workingCopy.toPhase(Phase.RESOLVED);
+                TreeMaker make = workingCopy.getTreeMaker();
+                
+                // finally, find the correct body and rewrite it.
+                ClassTree clazz = (ClassTree) workingCopy.getCompilationUnit().getTypeDecls().get(0);
+                MethodTree method = (MethodTree) clazz.getMembers().get(1);
+                ExpressionStatementTree statement = make.ExpressionStatement(
+                    make.MethodInvocation(
+                        Collections.<ExpressionTree>emptyList(),
+                        make.MemberSelect(
+                            make.MemberSelect(
+                                make.Identifier("System"),
+                                "err"
+                            ),
+                            "println"
+                        ),
+                        Collections.singletonList(
+                            make.Literal(null)
+                        )
+                    )
+                );
+                BlockTree copy = make.addBlockStatement(method.getBody(), statement);
+                workingCopy.rewrite(method.getBody(), copy);
+            }
+            
+            public void cancel() {
+            }
+        };
+        testSource.runModificationTask(task).commit();
+        String res = TestUtilities.copyFileToString(testFile);
+        System.err.println(res);
+        assertEquals(golden, res);
+        
+    }
+    
+    // methods not used in this test.
+    String getGoldenPckg() {
+        return "";
+    }
+    
+    String getSourcePckg() {
+        return "";
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
