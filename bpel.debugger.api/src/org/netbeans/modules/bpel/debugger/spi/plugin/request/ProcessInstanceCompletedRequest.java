b91dafc8aec027587abf08812a148a53c09156af
yaroslavskiy <yaroslavskiy@netbeans.org>
2006-12-29 12:04:23 +0000

    Sync trunk and gavotte

--- bpel.debugger.api/src/org/netbeans/modules/bpel/debugger/spi/plugin/request/ProcessInstanceCompletedRequest.java
+++
@@ -1,9 +0,0 @@
-package org.netbeans.modules.bpel.debugger.spi.plugin.request;
-
-/**
- *
- * @author Alexander Zgursky
- */
-public interface ProcessInstanceCompletedRequest extends EventRequest {
-    
-}
