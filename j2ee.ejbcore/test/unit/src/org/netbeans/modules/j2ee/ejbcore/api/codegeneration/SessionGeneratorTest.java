5fa5d8f9c291a97fb8a7ddd89935fa8d6461f1cf
madamek <madamek@netbeans.org>
2007-01-03 13:54:08 +0000

    Enhancing test

--- j2ee.ejbcore/test/unit/src/org/netbeans/modules/j2ee/ejbcore/api/codegeneration/SessionGeneratorTest.java
+++ j2ee.ejbcore/test/unit/src/org/netbeans/modules/j2ee/ejbcore/api/codegeneration/SessionGeneratorTest.java
@@ -19,20 +19,29 @@
 
 package org.netbeans.modules.j2ee.ejbcore.api.codegeneration;
 
+import java.io.EOFException;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.ExecutableElement;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.element.Name;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.VariableElement;
+import javax.lang.model.type.ArrayType;
+import javax.lang.model.type.TypeKind;
 import javax.lang.model.type.TypeMirror;
-import javax.swing.text.Document;
+import javax.lang.model.util.ElementFilter;
 import org.netbeans.api.java.source.CancellableTask;
 import org.netbeans.api.java.source.CompilationController;
 import org.netbeans.api.java.source.JavaSource;
 import org.netbeans.modules.j2ee.api.ejbjar.EjbProjectConstants;
 import org.netbeans.modules.j2ee.common.method.MethodModel;
+import org.netbeans.modules.j2ee.common.method.MethodModelSupport;
 import org.netbeans.modules.j2ee.common.source.AbstractTask;
 import org.netbeans.modules.j2ee.common.source.SourceUtils;
 import org.netbeans.modules.j2ee.ejbcore.naming.EJBNameOptions;
@@ -42,10 +51,8 @@ import org.netbeans.modules.j2ee.ejbcore.test.FakeJavaDataLoaderPool;
 import org.netbeans.modules.j2ee.ejbcore.test.FileOwnerQueryImpl;
 import org.netbeans.modules.j2ee.ejbcore.test.TestBase;
 import org.netbeans.modules.java.source.usages.IndexUtil;
-import org.openide.cookies.EditorCookie;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
-import org.openide.loaders.DataObject;
 
 /**
  *
@@ -119,31 +126,31 @@ public class SessionGeneratorTest extends TestBase {
                 TypeElement clazz = sourceUtils.getTypeElement();
                 assertTrue(clazz.getSimpleName().contentEquals(ejbNames.getSessionEjbClassPrefix() + name + ejbNames.getSessionEjbClassSuffix()));
                 assertDirectlyImplements(controller, clazz, new String[] {"javax.ejb.SessionBean"});
-                assertContains(controller, clazz, new MethodModel[] {
-                    MethodModel.create(
+                assertTrue(contains(controller, clazz, MethodModel.create(
                             "setSessionContext", "void", "", 
                             Collections.singletonList(MethodModel.Variable.create("javax.ejb.SessionContext", "aContext")), 
                             Collections.<String>emptyList(), 
-                            Collections.singleton(Modifier.PUBLIC)),
-                    MethodModel.create(
+                            Collections.singleton(Modifier.PUBLIC)
+                            )));
+                assertTrue(contains(controller, clazz, MethodModel.create(
                             "ejbActivate", "void", "", 
                             Collections.<MethodModel.Variable>emptyList(), 
                             Collections.<String>emptyList(), 
-                            Collections.singleton(Modifier.PUBLIC)),
-                    MethodModel.create(
+                            Collections.singleton(Modifier.PUBLIC)
+                            )));
+                assertTrue(contains(controller, clazz, MethodModel.create(
                             "ejbPassivate", "void", "", 
                             Collections.<MethodModel.Variable>emptyList(), 
                             Collections.<String>emptyList(), 
-                            Collections.singleton(Modifier.PUBLIC)),
-                    MethodModel.create(
+                            Collections.singleton(Modifier.PUBLIC)
+                            )));
+                assertTrue(contains(controller, clazz, MethodModel.create(
                             "ejbRemove", "void", "", 
                             Collections.<MethodModel.Variable>emptyList(), 
                             Collections.<String>emptyList(), 
-                            Collections.singleton(Modifier.PUBLIC)),
-                });
-                assertContains(controller, clazz, new MethodModel.Variable[] {
-                    MethodModel.Variable.create("javax.ejb.SessionContext", "context")
-                });
+                            Collections.singleton(Modifier.PUBLIC)
+                            )));
+                assertTrue(contains(controller, clazz, MethodModel.Variable.create("javax.ejb.SessionContext", "context")));
             }
         });
     }
@@ -198,19 +205,31 @@ public class SessionGeneratorTest extends TestBase {
     
     private static void assertDirectlyImplements(CompilationController controller, TypeElement typeElement, String[] interfaces) {
         List<? extends TypeMirror> foundInterfaces = typeElement.getInterfaces();
-        assertTrue(foundInterfaces.size() == interfaces.length);
+        assertTrue("Superclass or interface not found? Probably not on classpath?", foundInterfaces.size() == interfaces.length);
         for (TypeMirror typeMirror : foundInterfaces) {
             TypeElement element = (TypeElement) controller.getTypes().asElement(typeMirror);
             assertTrue(containsName(interfaces, element.getQualifiedName()));
         }
     }
     
-    private static void assertContains(CompilationController controller, TypeElement typeElement, MethodModel[] methods) {
-        
+    private static boolean contains(CompilationController controller, TypeElement typeElement, MethodModel methodModel) {
+        for (ExecutableElement executableElement : ElementFilter.methodsIn(typeElement.getEnclosedElements())) {
+            if (MethodModelSupport.isSameMethod(controller, executableElement, methodModel)) {
+                return true;
+            };
+        }
+        return false;
     }
     
-    private static void assertContains(CompilationController controller, TypeElement typeElement, MethodModel.Variable[] fields) {
+    private static boolean contains(CompilationController controller, TypeElement typeElement, MethodModel.Variable field) {
+        for (VariableElement variableElement : ElementFilter.fieldsIn(typeElement.getEnclosedElements())) {
+            if (getTypeName(controller, variableElement.asType()).equals(field.getType()) &&
+                    variableElement.getSimpleName().contentEquals(field.getName())) {
+                return true;
+            }
+        }
         
+        return false;
     }
     
     private static boolean containsName(String[] stringNames, Name name) {
@@ -227,4 +246,38 @@ public class SessionGeneratorTest extends TestBase {
         javaSource.runUserActionTask(taskToTest, true);
     }
     
+    // see #90968
+    private static String getTypeName(CompilationController controller, TypeMirror typeMirror) {
+        TypeKind typeKind = typeMirror.getKind();
+        switch (typeKind) {
+            case BOOLEAN : return "boolean"; // NOI18N
+            case BYTE : return "byte"; // NOI18N
+            case CHAR : return "char"; // NOI18N
+            case DOUBLE : return "double"; // NOI18N
+            case FLOAT : return "float"; // NOI18N
+            case INT : return "int"; // NOI18N
+            case LONG : return "long"; // NOI18N
+            case SHORT : return "short"; // NOI18N
+            case VOID : return "void"; // NOI18N
+            case DECLARED : 
+                Element element = controller.getTypes().asElement(typeMirror);
+                return ((TypeElement) element).getQualifiedName().toString();
+            case ARRAY : 
+                ArrayType arrayType = (ArrayType) typeMirror;
+                Element componentTypeElement = controller.getTypes().asElement(arrayType.getComponentType());
+                return ((TypeElement) componentTypeElement).getQualifiedName().toString() + "[]";
+            case ERROR :
+            case EXECUTABLE :
+            case NONE :
+            case NULL :
+            case OTHER :
+            case PACKAGE :
+            case TYPEVAR :
+            case WILDCARD :  
+                System.out.println("### getTypeName unknown type: " + typeMirror + ", " + typeKind + ", " + typeMirror.getClass().getName());
+                break;
+        }
+        return null;
+    }
+
 }
