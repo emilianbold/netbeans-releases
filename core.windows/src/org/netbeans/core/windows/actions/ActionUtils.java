2e9721fdd460019f3573587969bbc118ebdc460c
saubrecht <saubrecht@netbeans.org>
2007-01-09 10:56:54 +0000

    #90435 - Missing maximize command in slided out window's popup

--- core.windows/src/org/netbeans/core/windows/actions/ActionUtils.java
+++ core.windows/src/org/netbeans/core/windows/actions/ActionUtils.java
@@ -77,6 +77,9 @@ public abstract class ActionUtils {
             actions.add(new UndockWindowAction(tc));
         } else if (kind == Constants.MODE_KIND_SLIDING) {
             actions.add(new CloseWindowAction(tc));
+            if (mode.getState() == Constants.MODE_STATE_JOINED) {
+                actions.add(new MaximizeWindowAction(tc));
+            }
             actions.add(new UndockWindowAction(tc));
         }
         
