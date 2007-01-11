079c870eb6fa86599e7c9013f0beba3173f20ceb
mmirilovic <mmirilovic@netbeans.org>
2007-01-11 16:07:42 +0000

    END event is COMPONENT SHOWN

--- performance/test/qa-functional/src/gui/action/JavaCompletionInEditor.java
+++ performance/test/qa-functional/src/gui/action/JavaCompletionInEditor.java
@@ -13,26 +13,24 @@
  * "Portions Copyrighted [year] [name of copyright owner]"
  *
  * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
 package gui.action;
 
 import org.netbeans.performance.test.guitracker.LoggingRepaintManager.RegionFilter;
-
 import org.netbeans.modules.editor.java.JavaKit;
 import org.netbeans.modules.editor.options.BaseOptions;
 import org.netbeans.modules.java.editor.options.JavaOptions;
-
 import org.netbeans.jellytools.EditorOperator;
 import org.netbeans.jellytools.EditorWindowOperator;
 import org.netbeans.jellytools.nodes.Node;
 import org.netbeans.jellytools.ProjectsTabOperator;
 import org.netbeans.jellytools.actions.OpenAction;
 import org.netbeans.jellytools.modules.editor.CompletionJListOperator;
-
 import org.netbeans.jemmy.operators.ComponentOperator;
+import org.netbeans.performance.test.guitracker.ActionTracker;
 
 /**
  * Test of java completion in opened source editor.
@@ -43,6 +41,10 @@ public class JavaCompletionInEditor extends org.netbeans.performance.test.utilit
     
     private static final int lineNumber = 39;
     private static final String ccText = "        System";
+    
+    private EditorOperator editorOperator;
+    private int completionAutoPopupDelay, caretBlinkRate;
+    private boolean javaDocAutoPopup;
     
     /** Creates a new instance of JavaCompletionInEditor */
     public JavaCompletionInEditor(String testName) {
@@ -56,50 +58,14 @@ public class JavaCompletionInEditor extends org.netbeans.performance.test.utilit
         expectedTime = WINDOW_OPEN;
     }
     
-    private EditorOperator editorOperator;
-    private int completionAutoPopupDelay;
-    private boolean javaDocAutoPopup;
-    private int parsingErrors;
-    
-    protected void turnOff() {
-        // turn off the code completion popup delay and turn off the javadoc popup completely
-        Class kitClass = JavaKit.class;
-        BaseOptions options = BaseOptions.getOptions (kitClass);
-        if (options instanceof JavaOptions) {
-            completionAutoPopupDelay = ((JavaOptions)options).getCompletionAutoPopupDelay();
-            ((JavaOptions)options).setCompletionAutoPopupDelay(0);
-            javaDocAutoPopup = ((JavaOptions)options).getJavaDocAutoPopup();
-            ((JavaOptions)options).setJavaDocAutoPopup(false);
-        }
-        // turn off the error hightlighting feature
-        /* TODO doesn't work after retouche integration
-        parsingErrors = JavaSettings.getDefault().getParsingErrors();
-        JavaSettings.getDefault().setParsingErrors(0);        
-         */
-    }
-    
-    protected void turnBack() {
-        // set the modified properties back to default
-        Class kitClass = JavaKit.class;
-        BaseOptions options = BaseOptions.getOptions (kitClass);
-        if (options instanceof JavaOptions) {
-            ((JavaOptions)options).setCompletionAutoPopupDelay(completionAutoPopupDelay);
-            ((JavaOptions)options).setJavaDocAutoPopup(javaDocAutoPopup);
-        }
-        
-        /* TODO doesn't work after retouche integration
-        JavaSettings.getDefault().setParsingErrors(parsingErrors);        
-         */
-    }
-    
     public void initialize() {
+        MY_END_EVENT = ActionTracker.TRACK_COMPONENT_SHOW;
+        // prepare editor/completion for measuring
+        setCompletionForMeasuringOn();
+        
         // open a java file in the editor
         new OpenAction().performAPI(new Node(new ProjectsTabOperator().getProjectRootNode("PerformanceTestData"), gui.Utilities.SOURCE_PACKAGES + "|org.netbeans.test.performance|Main.java"));
         editorOperator = EditorWindowOperator.getEditor("Main.java");
-        turnOff();
-        
-        repaintManager().setRegionFilter(COMPLETION_FILTER);
-        setJavaEditorCaretFilteringOn();
         
         // scroll to the place where we start
         editorOperator.setCaretPositionToLine(lineNumber);
@@ -109,17 +75,6 @@ public class JavaCompletionInEditor extends org.netbeans.performance.test.utilit
     }
     
     public void prepare() {
-        /*
-        import org.netbeans.editor.BaseCaret;
-        
-        javax.swing.text.Caret thecaret = editorOperator.txtEditorPane().getCaret();
-        if (thecaret instanceof BaseCaret)
-        {
-            BaseCaret thebasecaret = (BaseCaret) thecaret;
-            setAreaToFilter (thebasecaret.x-20, thebasecaret.y-20, thebasecaret.width+40, thebasecaret.height+40);
-        }
-        */
-                
         // scroll to the place where we start
         EditorWindowOperator.getEditor("Main.java");
         editorOperator.setCaretPositionToEndOfLine(lineNumber);
@@ -132,7 +87,6 @@ public class JavaCompletionInEditor extends org.netbeans.performance.test.utilit
         // invoke the completion dialog
         editorOperator.pushKey('.');
         // wait for the completion window
-//        return new ComponentOperator(MainWindowOperator.getDefault(), new CodeCompletionSubchooser());
         return new CompletionJListOperator();
     }
     
@@ -143,12 +97,57 @@ public class JavaCompletionInEditor extends org.netbeans.performance.test.utilit
     }
     
     public void shutdown() {
-        turnBack();
-        repaintManager().setRegionFilter(null);
+        // set default values after measuring
+        setCompletionForMeasuringOff();
+        
         editorOperator.closeDiscard();
         super.shutdown();
     }
     
+    
+    private void setCompletionForMeasuringOn(){
+        // measure only paint events from QuietEditorPane
+        repaintManager().setRegionFilter(COMPLETION_FILTER);
+        
+        // set large font size for Editor
+        BaseOptions options = BaseOptions.getOptions (JavaKit.class);
+        if (options instanceof JavaOptions) {
+            completionAutoPopupDelay = ((JavaOptions)options).getCompletionAutoPopupDelay();
+            ((JavaOptions)options).setCompletionAutoPopupDelay(0);
+            javaDocAutoPopup = ((JavaOptions)options).getJavaDocAutoPopup();
+            ((JavaOptions)options).setJavaDocAutoPopup(false);
+        }
+        
+        caretBlinkRate = options.getCaretBlinkRate();
+        //disable caret blinkering
+        options.setCaretBlinkRate(0);
+        
+        // turn off the error hightlighting feature
+        /* TODO doesn't work after retouche integration
+        parsingErrors = JavaSettings.getDefault().getParsingErrors();
+        JavaSettings.getDefault().setParsingErrors(0);        
+         */
+    }
+    
+    private void setCompletionForMeasuringOff(){
+        // measure only paint events from QuietEditorPane
+        repaintManager().setRegionFilter(null);
+        
+        // set back the original font size for Editor
+        Class kitClass = JavaKit.class;
+        BaseOptions options = BaseOptions.getOptions (kitClass);
+        if (options instanceof JavaOptions) {
+            ((JavaOptions)options).setCompletionAutoPopupDelay(completionAutoPopupDelay);
+            ((JavaOptions)options).setJavaDocAutoPopup(javaDocAutoPopup);
+        }
+        
+        // set back the original blink rate
+        options.setCaretBlinkRate(caretBlinkRate);
+        
+        /* doesn't work after retouche integration
+        JavaSettings.getDefault().setParsingErrors(parsingErrors);        
+         */
+    }
     
     private static final RegionFilter COMPLETION_FILTER =
             new RegionFilter() {
