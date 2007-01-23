357180dc5feace812c644093d37979a69fd05f74
msauer <msauer@netbeans.org>
2007-01-23 15:44:33 +0000

    Adding tests for "Navigate | Go To Test"

--- junit/test/qa-functional/src/org/netbeans/test/junit/testcreation/GotoTest.java
+++ junit/test/qa-functional/src/org/netbeans/test/junit/testcreation/GotoTest.java
@@ -22,8 +22,11 @@ package org.netbeans.test.junit.testcreation;
 import org.netbeans.jellytools.Bundle;
 import org.netbeans.jellytools.EditorOperator;
 import org.netbeans.jellytools.MainWindowOperator;
+import org.netbeans.jellytools.ProjectsTabOperator;
 import org.netbeans.jellytools.nodes.Node;
 import org.netbeans.jemmy.operators.JMenuBarOperator;
+import org.netbeans.jemmy.operators.JMenuItemOperator;
+import org.netbeans.jemmy.operators.JMenuOperator;
 import org.netbeans.junit.NbTestCase;
 import org.netbeans.junit.NbTestSuite;
 import org.netbeans.test.junit.utils.Utilities;
@@ -51,22 +54,63 @@ public class GotoTest extends NbTestCase {
     }
     
     /**
-     * Test selecting appropriate test from menu
+     * Test selecting appropriate test from Main menu
      */
-    public void selectTest() {
+    public void testSelectTestFromMainMenu() {
         //open sample class
         Node n = Utilities.openFile(Utilities.SRC_PACKAGES_PATH +
                 "|" + TEST_PACKAGE_NAME+ "|" + Utilities.TEST_CLASS_NAME);
         
+        EditorOperator eos = new EditorOperator(Utilities.TEST_CLASS_NAME);
+        
         JMenuBarOperator jbo = new JMenuBarOperator(
                 MainWindowOperator.getDefault().getJMenuBar());
         String[] sf = {Bundle.getStringTrimmed("org.netbeans.core.Bundle",
-                "Menu/Window"),
+                "Menu/GoTo"),
                 Bundle.getStringTrimmed("org.netbeans.modules.junit.Bundle",
                 "LBL_Action_GoToTest")};
+        jbo.pushMenu(sf[0]);
+        JMenuItemOperator jmio = new JMenuItemOperator(new JMenuOperator(jbo, sf[0]).getItem(0));
+        //Check if goto test is enabled inside menu
+        assertTrue("Goto Test disabled when invoked from Editor!", jmio.isEnabled());
         jbo.pushMenu(sf);
-        EditorOperator eo = new EditorOperator(""); //TODO: finish
+        //Operator for opened TestClassTest
+        EditorOperator eo = new EditorOperator(Utilities.TEST_CLASS_NAME + "Test");
+        assertTrue("Test for \"" + TEST_PACKAGE_NAME +
+                Utilities.TEST_CLASS_NAME + "\" not opened!", eo.isVisible());
+        eo.close(false);
+        eos.close(false);
+    }
+    
+    /**
+     * Test selecting appropriate test from Edior's context menu
+     */
+    public void testSelectTestFromExplorer() {
+        //select sample class in explorer
+        Node pn = new ProjectsTabOperator().getProjectRootNode(
+                Utilities.TEST_PROJECT_NAME);
+        pn.select();
+        Node n = new Node(pn, Utilities.SRC_PACKAGES_PATH +
+                "|" + TEST_PACKAGE_NAME+ "|" + Utilities.TEST_CLASS_NAME);
+        n.select();
         
+        JMenuBarOperator jbo = new JMenuBarOperator(
+                MainWindowOperator.getDefault().getJMenuBar());
         
+        String[] sf = {Bundle.getStringTrimmed("org.netbeans.core.Bundle",
+                "Menu/GoTo"),
+                Bundle.getStringTrimmed("org.netbeans.modules.junit.Bundle",
+                "LBL_Action_GoToTest")};
+        jbo.pushMenu(sf[0]);
+        JMenuItemOperator jmio = new JMenuItemOperator(new JMenuOperator(jbo, sf[0]).getItem(0));
+        //Check if goto test is enabled inside menu
+        assertTrue("Goto Test disabled when invoked from Explorer!" +
+                "see: http://www.netbeans.org/issues/show_bug.cgi?id=88599",
+                jmio.isEnabled());
+        jbo.pushMenu(sf);
+        EditorOperator eot = new EditorOperator(Utilities.TEST_CLASS_NAME);
+        assertTrue("Test for \"" + TEST_PACKAGE_NAME +
+                Utilities.TEST_CLASS_NAME + "\" not opened!", eot.isVisible());
+        eot.close(false);
     }
 }
