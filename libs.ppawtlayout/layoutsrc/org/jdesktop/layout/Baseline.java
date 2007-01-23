bf5224ddf01342926d2c08b5db7d8917458c8918
luky <luky@netbeans.org>
2007-01-23 13:38:20 +0000

    Initial commit for CDC merge

---
+++ libs.ppawtlayout/layoutsrc/org/jdesktop/layout/Baseline.java
@@ -0,0 +1,341 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.jdesktop.layout;
+
+import java.awt.*;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Collections;
+import java.util.Dictionary;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Convenience class that can be used to determine the baseline of a
+ * particular component.  The static method <code>getBaseline</code> uses the
+ * following algorithm to determine the baseline:
+ * <ol>
+ * <li>If the component has a <code>getBaseline(Component,int,int)</code> 
+ *     method, invoke it.
+ * <li>If there is a <code>UIManager</code> property of the name
+ *     <code>Baseline.instance</code>, forward the call to that Baseline.
+ * <li>Otherwise use the built in support.
+ * </ol>
+ * <p>
+ * This class is primarily useful for JREs prior to 1.6.  In 1.6 API for this
+ * was added directly to Component, Component and the
+ * appropriate ComponentUIs.  When run on a JRE of 1.6 or greater this will directly
+ * call into the getBaseline method of Component.
+ *
+ * @version $Revision$
+ */
+public class Baseline {
+    //
+    // Used by button and label baseline code, cached to avoid excessive
+    // garbage.
+    //
+    private static final Rectangle viewRect = new Rectangle();
+    private static final Rectangle textRect = new Rectangle();
+    private static final Rectangle iconRect = new Rectangle();
+
+    // 
+    // These come from TitleBorder.  NOTE that these are NOT final in
+    // TitledBorder
+    //
+    private static final int EDGE_SPACING = 2;
+    private static final int TEXT_SPACING = 2;
+
+
+    private static final Insets EMPTY_INSETS = new Insets(0, 0, 0, 0);
+
+    // Prototype label for calculating baseline of tables.
+    private static Label TABLE_LABEL;
+
+    // Prototype label for calculating baseline of lists.
+    private static Label LIST_LABEL;
+
+    // Prototype label for calculating baseline of trees.
+    private static Label TREE_LABEL;
+
+    // Corresponds to com.sun.java.swing.plaf.windows.WindowsClassicLookAndFeel
+    private static Class CLASSIC_WINDOWS;
+    // Whether or not we've tried to load WindowsClassicLookAndFeel.
+    private static boolean checkedForClassic;
+
+    // Map<Class,Method> 
+    private static final Map BASELINE_MAP = Collections.
+            synchronizedMap(new HashMap());
+    private static Method COMPONENT_BASELINE_METHOD;
+
+    static {
+        COMPONENT_BASELINE_METHOD = null;
+        try {
+            COMPONENT_BASELINE_METHOD = Component.class.getMethod(
+                "getBaseline", new Class[] { int.class, int.class});
+        } catch (NoSuchMethodException nsme) {
+        }
+    }
+
+    /**
+     * Returns the baseline for the specified component, or -1 if the
+     * baseline can not be determined.  The baseline is measured from
+     * the top of the component.  This method returns the baseline based
+     * on the preferred size.
+     *
+     * @param component Component to calculate baseline for
+     * @return baseline for the specified component
+     */
+    public static int getBaseline(Component component) {
+        Dimension pref = component.getPreferredSize();
+        return getBaseline(component, pref.width, pref.height);
+    }
+
+    private static Method getBaselineMethod(Component component) {
+        if (COMPONENT_BASELINE_METHOD != null) {
+            return COMPONENT_BASELINE_METHOD;
+        }
+        Class klass = component.getClass();
+        while (klass != null) {
+            if (BASELINE_MAP.containsKey(klass)) {
+                Method method = (Method)BASELINE_MAP.get(klass);
+                return method;
+            }
+            klass = klass.getSuperclass();
+        }
+        klass = component.getClass();
+        Method[] methods = klass.getMethods();
+        for (int i = methods.length - 1; i >= 0; i--) {
+            Method method = methods[i];
+            if ("getBaseline".equals(method.getName())) {
+                Class[] params = method.getParameterTypes();
+                if (params.length == 2 && params[0] == int.class &&
+                        params[1] == int.class) {
+                    BASELINE_MAP.put(klass, method);
+                    return method;
+                }
+            }
+        }
+        BASELINE_MAP.put(klass, null);
+        return null;
+    }
+
+    private static int invokeBaseline(Method method, Component c, int width,
+            int height) {
+        int baseline = -1;
+        try {
+            baseline = ((Integer)method.invoke(c,
+                    new Object[] { new Integer(width),
+                            new Integer(height) })).intValue();
+        } catch (IllegalAccessException iae) {
+        } catch (IllegalArgumentException iae2) {
+        } catch (InvocationTargetException ite2) {
+        }
+        return baseline;
+    }
+    
+    /**
+     * Returns the baseline for the specified component, or a value less 
+     * than 0 if the baseline can not be determined.  The baseline is measured 
+     * from the top of the component.
+     *
+     * @param component Component to calculate baseline for
+     * @param width Width of the component to determine baseline for.
+     * @param height Height of the component to determine baseline for.
+     * @return baseline for the specified component
+     */
+    public static int getBaseline(Component component, int width, int height) {
+        if (1 == 1) return -1;
+        Method baselineMethod = getBaselineMethod(component);
+        if (baselineMethod != null) {
+            return invokeBaseline(baselineMethod, component, width, height);
+        }
+        Object baselineImpl = null; //UIManager.get("Baseline.instance");
+        if (baselineImpl != null && (baselineImpl instanceof Baseline)) {
+            return ((Baseline)baselineImpl).getComponentBaseline(
+                    component, width, height);
+        }
+        String uid = null;
+        if (component instanceof Button) uid = "ButtonUI";
+        if (component instanceof Label) uid = "LabelUI";
+        if (component instanceof TextField) uid = "TextFieldUI";
+        int baseline = -1;
+        if (uid == "ButtonUI" || uid == "CheckBoxUI" ||
+                uid == "RadioButtonUI" || uid == "ToggleButtonUI") {
+            baseline = getButtonBaseline((Button)component,
+                                         height);
+        }
+        else if (uid == "TextAreaUI") {
+            return getTextAreaBaseline((TextArea)component, height);
+        }
+        else if (uid == "FormattedTextFieldUI" ||
+                 uid == "PasswordFieldUI" ||
+                 uid == "TextFieldUI") {
+            baseline = getSingleLineTextBaseline((TextComponent)component,
+                                                 height);
+        }
+        else if (uid == "LabelUI") {
+            baseline = getLabelBaseline((Label)component, height);
+        }
+        else if (uid == "ListUI") {
+            baseline = getListBaseline((List)component, height);
+        }
+        else if (uid == "PanelUI") {
+            baseline = getPanelBaseline((Panel)component, height);
+        }
+        else if (uid == "ScrollPaneUI") {
+            baseline = getScrollPaneBaseline((ScrollPane)component, height);
+        }        
+        return Math.max(baseline, -1);
+    }
+
+    private static Insets rotateInsets(Insets topInsets, int targetPlacement) {
+          return new Insets(topInsets.top, topInsets.left,
+                            topInsets.bottom, topInsets.right);
+    }
+    
+    private static int getTextAreaBaseline(TextArea text, int height) {
+        FontMetrics fm = text.getFontMetrics(text.getFont());
+        return 6 + fm.getAscent();
+    }
+    
+    private static int getListBaseline(List list, int height) {
+        int rowHeight = -1;//list.getFixedCellHeight();
+        if (LIST_LABEL == null) {
+            LIST_LABEL = new Label("X");
+        }
+        Label label = LIST_LABEL;
+        label.setFont(list.getFont());
+        // JList actually has much more complex behavior here.
+        // If rowHeight != -1 the rowHeight is either the max of all cell
+        // heights (layout orientation != VERTICAL), or is variable depending
+        // upon the cell.  We assume a default size.
+        // We could theoretically query the real renderer, but that would
+        // not work for an empty model and the results may vary with 
+        // the content.
+        if (rowHeight == -1) {
+            rowHeight = label.getPreferredSize().height;
+        }
+        return getLabelBaseline(label, rowHeight) + 6;
+    }
+
+    private static int getScrollPaneBaseline(ScrollPane sp, int height) {
+//        Component view = sp.getViewport().getView();
+//        if (view instanceof Component) {
+//            int baseline = getBaseline((Component)view);
+//            if (baseline > 0) {
+//                return baseline + sp.getViewport().getY();
+//            }
+//        }
+        return -1;
+    }
+
+    private static int getPanelBaseline(Panel panel, int height) {
+        return -1;
+    }
+
+    private static int getLabelBaseline(Label label, int height) {
+        FontMetrics fm = label.getFontMetrics(label.getFont());
+
+        resetRects(label, height);
+
+        return textRect.y + fm.getAscent();
+    }
+
+    /**
+     * Returns the baseline for single line text components, like
+     * <code>JTextField</code>.
+     */
+    private static int getSingleLineTextBaseline(TextComponent textComponent,
+                                                 int h) {
+//        View rootView = textComponent.getUI().getRootView(textComponent);
+//        if (rootView.getViewCount() > 0) {
+//            Insets insets = textComponent.getInsets();
+//            int height = h - insets.top - insets.bottom;
+//            int y = insets.top;
+//            View fieldView = rootView.getView(0);
+//	    int vspan = (int)fieldView.getPreferredSpan(View.Y_AXIS);
+//	    if (height != vspan) {
+//		int slop = height - vspan;
+//		y += slop / 2;
+//	    }
+//            FontMetrics fm = textComponent.getFontMetrics(
+//                                 textComponent.getFont());
+//            y += fm.getAscent();
+//            return y;
+//        }
+        return -1;
+    }
+
+    /**
+     * Returns the baseline for buttons.
+     */
+    private static int getButtonBaseline(Button button, int height) {
+        FontMetrics fm = button.getFontMetrics(button.getFont());
+
+        resetRects(button, height);
+
+        String text = button.getLabel();
+        if (text != null && text.startsWith("<html>")) {
+            return -1;
+        }
+        return textRect.y + fm.getAscent();
+    }
+
+    private static void resetRects(Component c, int height) {
+        Insets insets = new Insets(5,5,5,5);//c.getInsets();
+        viewRect.x = insets.left;
+        viewRect.y = insets.top;
+        viewRect.width = c.getWidth() - (insets.right + viewRect.x);
+        viewRect.height = height - (insets.bottom + viewRect.y);
+        textRect.x = textRect.y = textRect.width = textRect.height = 0;
+        iconRect.x = iconRect.y = iconRect.width = iconRect.height = 0;
+    }
+   
+    /**
+     * Creates an instance of Baseline.  You typically don't create a
+     * Baseline.  The constructor is provided by look and feels that wish
+     * to provide baseline support.
+     * <p>
+     * A custom look and feel that wants to provide <code>Baseline</code>
+     * support should put the instance in the defaults returned
+     * from <code>getDefaults</code>.  If you want to override the 
+     * baseline suport for a look and feel place the instance in the defaults
+     * returned from UIManager.getLookAndFeelDefaults().  Tthis will ensure
+     * that if the look and feel changes the appropriate baseline can be used.
+     */
+    protected Baseline() {
+    }
+    
+    /**
+     * Returns the baseline for the specified component, or -1 if the
+     * baseline can not be determined.  The baseline is measured from
+     * the top of the component.
+     *
+     * @param component Component to calculate baseline for
+     * @param width Width of the component to determine baseline for.
+     * @param height Height of the component to determine baseline for.
+     * @return baseline for the specified component
+     */
+    public int getComponentBaseline(Component component, int width,
+            int height) {
+        return -1;
+    }
+}
---
+++ libs.ppawtlayout/layoutsrc/org/jdesktop/layout/LayoutStyle.java
@@ -0,0 +1,316 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.jdesktop.layout;
+
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Insets;
+
+/**
+ * LayoutStyle is used to determine how much space to place between components
+ * during layout.  LayoutStyle can be obtained for two components, or for
+ * a component relative to an edge of a parent container.  The amount of
+ * space can vary depending upon whether or not the components are
+ * logically grouped together (<code>RELATED</code>).
+ * <p>
+ * This class is primarily useful for JREs prior to 1.6.  In 1.6 API for this
+ * was added to Swing.  When run on a JRE of 1.6 or greater this will call into
+ * the appropriate methods in Swing.
+ *
+ * @version $Revision$
+ */
+public class LayoutStyle {
+    /**
+     * Possible argument to getPreferredGap.  Used to indicate the two componets
+     * are grouped together.
+     */
+    public static final int RELATED = 0;
+    /**
+     * Possible argument to getPreferredGap.  Used to indicate the two componets
+     * are not grouped together.
+     */
+    public static final int UNRELATED = 1;
+    
+    /**
+     * Possible argument to getPreferredGap.  Used to indicate the distance
+     * to indent a component is being requested.  To visually indicate
+     * a set of related components they will often times be horizontally
+     * indented, the <code>INDENT</code> constant for this.
+     * For example, to indent a check box relative to a label use this
+     * constant to <code>getPreferredGap</code>.
+     */
+    public static final int INDENT = 3;
+
+    private static LayoutStyle layoutStyle;
+
+    /**
+     * Sets the LayoutStyle instance to use for this look and feel.
+     * You generally don't need to invoke this, instead use the getter which
+     * will return the LayoutStyle appropriate for the current look and feel.
+     * 
+     * @param layoutStyle the LayoutStyle to use; a value of null indicates 
+     *        the default should be used
+     */
+    public static void setSharedInstance(LayoutStyle layoutStyle) {
+//        UIManager.getLookAndFeelDefaults().put("LayoutStyle.instance",
+//                layoutStyle);
+    }
+    
+    /**
+     * Factory methods for obtaining the current <code>LayoutStyle</code>
+     * object appropriate for the current look and feel.
+     * 
+     * @return the current LayoutStyle instance
+     */
+    public static LayoutStyle getSharedInstance() {
+        Object layoutImpl = null; //UIManager.get("LayoutStyle.instance");
+        if (layoutImpl != null && (layoutImpl instanceof LayoutStyle)) {
+            return (LayoutStyle)layoutImpl;
+        }
+        layoutStyle = new LayoutStyle();
+        return layoutStyle;
+    }
+
+    /**
+     * Returns the amount of space to use between two components.
+     * The return value indicates the distance to place
+     * <code>component2</code> relative to <code>component1</code>.
+     * For example, the following returns the amount of space to place
+     * between <code>component2</code> and <code>component1</code>
+     * when <code>component2</code> is placed vertically above
+     * <code>component1</code>:
+     * <pre>
+     *   int gap = getPreferredGap(component1, component2,
+     *                             LayoutStyle.RELATED,
+     *                             SwingConstants.NORTH, parent);
+     * </pre>
+     * The <code>type</code> parameter indicates the type 
+     * of gap being requested.  It can be one of the following values:
+     * <table>
+     * <tr><td><code>RELATED</code>
+     *     <td>If the two components will be contained in
+     *         the same parent and are showing similar logically related
+     *         items, use <code>RELATED</code>.
+     * <tr><td><code>UNRELATED</code>
+     *     <td>If the two components will be
+     *          contained in the same parent but show logically unrelated items
+     *          use <code>UNRELATED</code>.
+     * <tr><td><code>INDENT</code>
+     *     <td>Used to obtain the preferred distance to indent a component
+     *         relative to another.  For example, if you want to horizontally
+     *         indent a JCheckBox relative to a JLabel use <code>INDENT</code>.
+     *         This is only useful for the horizontal axis.
+     * </table>
+     * <p>
+     * It's important to note that some look and feels may not distinguish
+     * between <code>RELATED</code> and <code>UNRELATED</code>.
+     * <p>
+     * The return value is not intended to take into account the
+     * current size and position of <code>component2</code> or
+     * <code>component1</code>.  The return value may take into
+     * consideration various properties of the components.  For
+     * example, the space may vary based on font size, or the preferred
+     * size of the component.
+     * 
+     * @param component1 the <code>Component</code>
+     *               <code>component2</code> is being placed relative to
+     * @param component2 the <code>Component</code> being placed
+     * @param type how the two components are being placed
+     * @param position the position <code>component2</code> is being placed 
+     *        relative to <code>component1</code>; one of
+     *        <code>SwingConstants.NORTH</code>,
+     *        <code>SwingConstants.SOUTH</code>,
+     *        <code>SwingConstants.EAST</code> or
+     *        <code>SwingConstants.WEST</code>
+     * @param parent the parent of <code>component2</code>; this may differ
+     *        from the actual parent and may be null
+     * @return the amount of space to place between the two components
+     * @throws IllegalArgumentException if <code>position</code> is not
+     *         one of <code>SwingConstants.NORTH</code>,
+     *         <code>SwingConstants.SOUTH</code>,
+     *         <code>SwingConstants.EAST</code> or 
+     *         <code>SwingConstants.WEST</code>; <code>type</code> not one
+     *         of <code>INDENT</code>, <code>RELATED</code>
+     *         or <code>UNRELATED</code>; or <code>component1</code> or
+     *         <code>component2</code> is null
+     */
+    public int getPreferredGap(Component component1, Component component2,
+                          int type, int position, Container parent) {
+        if (position != 1 && //SwingConstants.NORTH &&
+              position != 5 && //SwingConstants.SOUTH &&
+              position != 7 && //SwingConstants.WEST &&
+              position != 3 ){ // SwingConstants.EAST) {
+            throw new IllegalArgumentException("Invalid position");
+        }
+        if (component1 == null || component2== null) {
+            throw new IllegalArgumentException("Components must be non-null");
+        }
+        if (type != RELATED && type != UNRELATED &&
+                type != INDENT) {
+            throw new IllegalArgumentException("Invalid type");
+        }
+        return 4;
+    }
+
+    /**
+     * Returns the amount of space to position a component inside its
+     * parent.
+     *
+     * @param component the <code>Component</code> being positioned
+     * @param position the position <code>component</code> is being placed 
+     *        relative to its parent; one of
+     *        <code>SwingConstants.NORTH</code>,
+     *        <code>SwingConstants.SOUTH</code>,
+     *        <code>SwingConstants.EAST</code> or
+     *        <code>SwingConstants.WEST</code>
+     * @param parent the parent of <code>component</code>; this may differ
+     *        from the actual parent and may be null
+     * @return the amount of space to place between the component and specified
+     *         edge
+     * @throws IllegalArgumentException if <code>position</code> is not
+     *         one of <code>SwingConstants.NORTH</code>,
+     *         <code>SwingConstants.SOUTH</code>,
+     *         <code>SwingConstants.EAST</code> or 
+     *         <code>SwingConstants.WEST</code>;
+     *         or <code>component</code> is null
+     */
+    public int getContainerGap(Component component, int position,
+            Container parent) {
+        if (position != 1 && //SwingConstants.NORTH &&
+              position != 5 && //SwingConstants.SOUTH &&
+              position != 7 && //SwingConstants.WEST &&
+              position != 3 ){ // SwingConstants.EAST) {
+            throw new IllegalArgumentException("Invalid position");
+        }
+        if (component == null) {
+            throw new IllegalArgumentException("Component must be non-null");
+        }
+        return 2;
+    }
+
+    /**
+     * Returns true if <code>component</code> should be treated as a dialog.
+     */
+    boolean isDialog(Component component) {
+        // PENDING: tag the content pane to make this easier to check for
+        String name = component.getName();
+        return (name != null && name.endsWith(".contentPane"));
+    }
+
+    /**
+     * For some look and feels check boxs and radio buttons have an empty
+     * border around them.  Look and feel guidelines generally don't include
+     * this space.  Use this method to subtract this space from the specified
+     * components.
+     *
+     * @param source First component
+     * @param target Second component
+     * @param position Position doing layout along.
+     * @param offset Ideal offset, not including border/margin
+     * @return offset - border/margin around the component.
+     */
+    int getCBRBPadding(Component source, Component target, int position,
+                       int offset) {
+        offset -= getCBRBPadding(source, position);
+        if (offset > 0) {
+            offset -= getCBRBPadding(target, flipDirection(position));
+        }
+        if (offset < 0) {
+            return 0;
+        }
+        return offset;
+    }
+
+    /**
+     * For some look and feels check boxs and radio buttons have an empty
+     * border around them.  Look and feel guidelines generally don't include
+     * this space.  Use this method to subtract this space from the specified
+     * components.
+     *
+     * @param source Component
+     * @param position Position doing layout along.
+     * @param offset Ideal offset, not including border/margin
+     * @return offset - border/margin around the component.
+     */
+    int getCBRBPadding(Component source, int position, int offset) {
+        offset -= getCBRBPadding(source, position);
+        return Math.max(offset, 0);
+    }
+
+    int flipDirection(int position) {
+        switch(position) {
+        case 1:
+            return 5;
+        case 5:
+            return 1;
+        case 3:
+            return 7;
+        case 7:
+            return 3;
+        }
+        return 0;
+    }
+
+    private int getCBRBPadding(Component c, int position) {
+//        if (c.getUIClassID() == "CheckBoxUI" ||
+//              c.getUIClassID() == "RadioButtonUI") {
+//            Border border = c.getBorder();
+//            if (border instanceof UIResource) {
+//                return getInset(c, position);
+//            }
+//        }
+        return 0;
+    }
+
+    private int getInset(Component c, int position) {
+        return 5;
+    }
+
+//    private boolean isLeftAligned(AbstractButton button, int position) {
+//        if (position == SwingConstants.WEST) {
+//            boolean ltr = button.getComponentOrientation().isLeftToRight();
+//            int hAlign = button.getHorizontalAlignment();
+//            return ((ltr && (hAlign == SwingConstants.LEFT ||
+//                             hAlign == SwingConstants.LEADING)) ||
+//                    (!ltr && (hAlign == SwingConstants.TRAILING)));
+//        }
+//        return false;
+//    }
+
+//    private boolean isRightAligned(AbstractButton button, int position) {
+//        if (position == SwingConstants.EAST) {
+//            boolean ltr = button.getComponentOrientation().isLeftToRight();
+//            int hAlign = button.getHorizontalAlignment();
+//            return ((ltr && (hAlign == SwingConstants.RIGHT ||
+//                             hAlign == SwingConstants.TRAILING)) ||
+//                    (!ltr && (hAlign == SwingConstants.LEADING)));
+//        }
+//        return false;
+//    }
+
+    /**
+     * Returns the amount to indent the specified component if it's
+     * a JCheckBox or JRadioButton.  If the component is not a JCheckBox or
+     * JRadioButton, 0 will be returned.
+     */
+    int getButtonChildIndent(Component c, int position) {
+        return 0;
+    }
+}
