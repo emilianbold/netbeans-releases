ddbd001cad9d95fe3c2825eaecab0c0bee6297c4
ksorokin <ksorokin@netbeans.org>
2007-01-27 20:04:34 +0000

    Fix for 90334.

--- nbi/engine/src/org/netbeans/installer/wizard/containers/SwingFrameContainer.java
+++ nbi/engine/src/org/netbeans/installer/wizard/containers/SwingFrameContainer.java
@@ -29,6 +29,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.net.URL;
+import javax.swing.ImageIcon;
 import javax.swing.SwingUtilities;
 import org.netbeans.installer.utils.helper.swing.NbiButton;
 import org.netbeans.installer.utils.helper.swing.NbiFrame;
