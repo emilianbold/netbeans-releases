ff8fec012162d9ba0d8c416e1036a0138d169a4d
luky <luky@netbeans.org>
2007-01-29 16:14:50 +0000

    Change in structure of ant tasks

--- j2me.cdc.project.savaje/antsrc/org/netbeans/modules/j2me/cdc/project/savaje/SavajePolicyBuilder.java
+++ j2me.cdc.project.savaje/antsrc/org/netbeans/modules/j2me/cdc/project/savaje/SavajePolicyBuilder.java
@@ -23,7 +23,6 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.StringTokenizer;
 
 import org.apache.tools.ant.*;
 import org.apache.tools.ant.types.*;
