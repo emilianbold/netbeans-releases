8c9e1769fd2a75ff90b3265c19650ec1baf482fc
dlipin <dlipin@netbeans.org>
2007-01-30 12:57:19 +0000

    Hope for final fix for not removing Dock icon (Issue 93537)

--- nbi/engine/src/org/netbeans/installer/utils/system/MacOsNativeUtils.java
+++ nbi/engine/src/org/netbeans/installer/utils/system/MacOsNativeUtils.java
@@ -287,9 +287,12 @@ public class MacOsNativeUtils extends UnixNativeUtils {
                         "    Not an array element");
                 return false;
             }
-            if(adding) {
+            if(adding) {                
                 LogManager.log(ErrorLevel.DEBUG,
-                        "    Adding shortcut");
+                        "Adding shortcut with the following properties: ");                
+                LogManager.log(ErrorLevel.DEBUG, "    executable = " + shortcut.getExecutablePath());
+                LogManager.log(ErrorLevel.DEBUG, "    name = " + shortcut.getName());
+                
                 dict = XMLUtils.addChildNode(array,"dict",null);
                 XMLUtils.addChildNode(dict,"key","tile-data");
                 Node dictChild = XMLUtils.addChildNode(dict,"dict",null);
@@ -304,42 +307,58 @@ public class MacOsNativeUtils extends UnixNativeUtils {
                 XMLUtils.addChildNode(dictChild,"key","file-type");
                 XMLUtils.addChildNode(dictChild,"integer","41");
                 XMLUtils.addChildNode(dict,"key","tile-type");
-                XMLUtils.addChildNode(dict, "string","file-tile");
+                XMLUtils.addChildNode(dict, "string","file-tile");                                
+                LogManager.log(ErrorLevel.DEBUG,
+                        "... adding shortcut to Dock XML finished");
             } else {
                 LogManager.log(ErrorLevel.DEBUG,
-                        "    Removing shortcut");
+                        "Removing shortcut with the following properties: ");
+                LogManager.indent();
+                LogManager.log(ErrorLevel.DEBUG,
+                        "executable = " + shortcut.getExecutablePath());
+                LogManager.log(ErrorLevel.DEBUG,
+                        "name = " + shortcut.getName());
+                
                 String location = shortcut.getExecutablePath();
                 List <Node> dcts = XMLUtils.getChildList(array,
                         "./dict/dict/dict/string");
                 index = 0;
                 Node dct = null;
                 LogManager.log(ErrorLevel.DEBUG,
-                        "    Total dict/dict/dict/string items = " + dcts.size());
+                        "Total dict/dict/dict/string items = " + dcts.size());
                 LogManager.log(ErrorLevel.DEBUG,
                         "        location = " + location);
+                
+                File locationFile = new File(location);
+                
                 while(index < dcts.size() && dcts.get(index)!=null) {
                     Node item = dcts.get(index);
-                    LogManager.log(ErrorLevel.DEBUG,
-                            "        content = " + item.getTextContent());
-                    
-                    if(location.equals(item.getTextContent())) {
-                        dct = item;
-                        break;
+                    String content = item.getTextContent();
+                    LogManager.log(ErrorLevel.DEBUG, "        content = " + content);
+                    if(content!=null && !content.equals("")) {                        
+                        File contentFile = new File(content);
+                        if(locationFile.equals(contentFile)) {
+                            dct = item;
+                            break;
+                        }
                     }
                     index++;
                 };
                 
                 if(dct!=null) {
                     LogManager.log(ErrorLevel.DEBUG,
-                            "    Shortcut exists in the dock.plist");
+                            "Shortcut exists in the dock.plist");
                     array.removeChild(dct.getParentNode().getParentNode().getParentNode());
                 } else {
                     LogManager.log(ErrorLevel.DEBUG,
-                            "    Shortcut doesn`t exist in the dock.plist");
+                            "hortcut doesn`t exist in the dock.plist");
                 }
+                LogManager.unindent();
+                LogManager.log(ErrorLevel.DEBUG,
+                        "... removing shortcut from Dock XML finished");
             }
             LogManager.log(ErrorLevel.DEBUG,
-                    "    Saving XML");
+                    "    Saving XML... ");
             XMLUtils.saveXMLDocument(document,dockFile);
             LogManager.log(ErrorLevel.DEBUG,
                     "    Done (saving xml)");
