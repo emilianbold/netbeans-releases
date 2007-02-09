528e09d5f8b4b5a108a71708d537c8a65398452b
dkaspar <dkaspar@netbeans.org>
2007-02-09 16:44:21 +0000

    MIDPNB: SVGImage code generator added

--- vmd.midpnb/src/org/netbeans/modules/vmd/midpnb/components/svg/SVGImageCD.java
+++ vmd.midpnb/src/org/netbeans/modules/vmd/midpnb/components/svg/SVGImageCD.java
@@ -19,30 +19,34 @@
 
 package org.netbeans.modules.vmd.midpnb.components.svg;
 
+import org.netbeans.modules.vmd.api.codegen.*;
 import org.netbeans.modules.vmd.api.model.*;
-import org.netbeans.modules.vmd.midp.components.MidpVersionDescriptor;
-import org.netbeans.modules.vmd.midpnb.codegen.MidpCustomCodePresenterSupport;
-import java.util.Arrays;
-import java.util.List;
 import org.netbeans.modules.vmd.api.properties.DefaultPropertiesPresenter;
+import org.netbeans.modules.vmd.midp.codegen.MidpCodeSupport;
 import org.netbeans.modules.vmd.midp.components.MidpTypes;
+import org.netbeans.modules.vmd.midp.components.MidpVersionDescriptor;
 import org.netbeans.modules.vmd.midp.components.MidpVersionable;
-import org.netbeans.modules.vmd.midp.components.resources.ResourceCD;
+import org.netbeans.modules.vmd.midp.components.general.ClassCD;
 import org.netbeans.modules.vmd.midp.propertyeditors.PropertiesCategories;
 import org.netbeans.modules.vmd.midp.propertyeditors.imagechooser.PropertyEditorImageChooser;
+import org.netbeans.modules.vmd.midpnb.codegen.MidpCustomCodePresenterSupport;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 /**
  *
  * @author Karol Harezlak
  */
 public class SVGImageCD extends ComponentDescriptor {
-    
+
     public static final TypeID TYPEID = new TypeID(TypeID.Kind.COMPONENT, "javax.microedition.m2g.SVGImage"); // NOI18N
 
     public static final String PROP_RESOURCE_PATH = "resourcePath";  // NOI18N
 
     public TypeDescriptor getTypeDescriptor() {
-        return new TypeDescriptor(ResourceCD.TYPEID, TYPEID, true, true);
+        return new TypeDescriptor(ClassCD.TYPEID, TYPEID, true, true);
     }
 
     public VersionDescriptor getVersionDescriptor() {
@@ -61,11 +65,53 @@ public class SVGImageCD extends ComponentDescriptor {
                 .addProperty("Resource Path", PropertyEditorImageChooser.create(), PROP_RESOURCE_PATH);
     }
 
+    private static Presenter createSetterPresenter () {
+        return new CodeSetterPresenter ()
+            .addSetters (new Setter () {
+                public boolean isConstructor () {
+                    return true;
+                }
+                public TypeID getConstructorRelatedTypeID () {
+                    return TYPEID;
+                }
+                public String getSetterName () {
+                    return null;
+                }
+                public Versionable getVersionable () {
+                    return MidpVersionable.MIDP_2;
+                }
+                public void generateSetterCode (MultiGuardedSection section, DesignComponent component, Map<String, Parameter> name2parameter) {
+                    CodeWriter writer = section.getWriter ();
+                    PropertyValue pathValue = component.readProperty (PROP_RESOURCE_PATH);
+                    String path = MidpTypes.getString (pathValue);
+                    if (path == null) {
+                        writer.write (CodeReferencePresenter.generateDirectAccessCode (component)).write (" = SVGImage.createEmptyImage (null);\n"); // NOI18N
+                    } else {
+                        writer.write ("try {\n"); // NOI18N
+                        writer.write (CodeReferencePresenter.generateDirectAccessCode (component)).write (" = "); // NOI18N
+                        writer.write ("(SVGImage) ScalableImage.createImage (");
+                        MidpCodeSupport.generateCodeForPropertyValue (writer, pathValue);
+                        writer.write (", null);\n"); // NOI18N
+
+                        writer.write ("} catch (java.io.IOException e) {\n").commit (); // NOI18N
+                        section.switchToEditable (component.getComponentID () + "-@java.io.IOException"); // NOI18N
+                        section.getWriter ().write ("e.printStackTrace ();\n").commit (); // NOI18N
+                        section.switchToGuarded ();
+                        writer.write ("}\n"); // NOI18N
+                    }
+                }
+                public List<String> getParameters () {
+                    return null;
+                }
+            });
+    }
+
     protected List<? extends Presenter> createPresenters() {
         return Arrays.asList(
             // properties
             createPropertiesPresenter(),
             // code
+            createSetterPresenter (),
             MidpCustomCodePresenterSupport.createAddImportPresenter ()
         );
     }
