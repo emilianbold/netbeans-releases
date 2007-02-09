062230f14b5e506d0ab8aafa30d375df010e9850
rkubacki <rkubacki@netbeans.org>
2007-02-09 11:39:49 +0000

    #95054: more friendly BeanInfos

--- xsl/src/org/netbeans/modules/xsl/XSLDataLoaderBeanInfo.java
+++ xsl/src/org/netbeans/modules/xsl/XSLDataLoaderBeanInfo.java
@@ -20,9 +20,9 @@ package org.netbeans.modules.xsl;
 
 import java.beans.*;
 import java.awt.Image;
-
 import org.openide.util.Utilities;
 import org.openide.loaders.MultiFileLoader;
+import org.openide.util.Exceptions;
 
 /**
  * Loader BeanInfo adding metadata missing in org.openide.loaders.MultiFileLoaderBeanInfo.
@@ -33,69 +33,6 @@ public class XSLDataLoaderBeanInfo extends SimpleBeanInfo {
     /* Icon base dir. */
     private static final String ICON_DIR_BASE = "org/netbeans/modules/xsl/resources/"; // NOI18N
 
-    // Bean descriptor //GEN-FIRST:BeanDescriptor
-    /*lazy BeanDescriptor*/;
-    private static BeanDescriptor getBdescriptor(){
-        BeanDescriptor beanDescriptor = new BeanDescriptor  ( XSLDataLoader.class , null );
-        beanDescriptor.setDisplayName ( Util.THIS.getString("NAME_XSLDataLoader") );//GEN-HEADEREND:BeanDescriptor
-        
-        // Here you can add code for customizing the BeanDescriptor.
-        
-        return beanDescriptor;         }//GEN-LAST:BeanDescriptor
-    
-    
-    // Property identifiers //GEN-FIRST:Properties
-    private static final int PROPERTY_extensions = 0;
-
-    // Property array 
-    /*lazy PropertyDescriptor*/;
-    private static PropertyDescriptor[] getPdescriptor(){
-        PropertyDescriptor[] properties = new PropertyDescriptor[1];
-    
-        try {
-            properties[PROPERTY_extensions] = new PropertyDescriptor ( "extensions", XSLDataLoader.class, "getExtensions", "setExtensions" );
-            properties[PROPERTY_extensions].setDisplayName ( Util.THIS.getString ("PROP_XSL_Extensions") );
-            properties[PROPERTY_extensions].setShortDescription ( Util.THIS.getString ("HINT_XSL_Extensions") );
-        }
-        catch( IntrospectionException e) {}//GEN-HEADEREND:Properties
-        
-        // Here you can add code for customizing the properties array.
-        
-        return properties;         }//GEN-LAST:Properties
-    
-    // EventSet identifiers//GEN-FIRST:Events
-
-    // EventSet array
-    /*lazy EventSetDescriptor*/;
-    private static EventSetDescriptor[] getEdescriptor(){
-        EventSetDescriptor[] eventSets = new EventSetDescriptor[0];//GEN-HEADEREND:Events
-        
-        // Here you can add code for customizing the event sets array.
-        
-        return eventSets;         }//GEN-LAST:Events
-    
-    // Method identifiers //GEN-FIRST:Methods
-
-    // Method array 
-    /*lazy MethodDescriptor*/;
-    private static MethodDescriptor[] getMdescriptor(){
-        MethodDescriptor[] methods = new MethodDescriptor[0];//GEN-HEADEREND:Methods
-        
-        // Here you can add code for customizing the methods array.
-        
-        return methods;         }//GEN-LAST:Methods
-    
-    
-    private static final int defaultPropertyIndex = -1;//GEN-BEGIN:Idx
-    private static final int defaultEventIndex = -1;//GEN-END:Idx
-    
-    
- //GEN-FIRST:Superclass
-    
-    // Here you can add code for customizing the Superclass BeanInfo.
-    
- //GEN-LAST:Superclass
-    
     /**
      * Gets the bean's <code>BeanDescriptor</code>s.
      *
@@ -104,7 +41,12 @@ public class XSLDataLoaderBeanInfo extends SimpleBeanInfo {
      * information should be obtained by automatic analysis.
      */
     public BeanDescriptor getBeanDescriptor() {
-        return getBdescriptor();
+        BeanDescriptor beanDescriptor = new BeanDescriptor  ( XSLDataLoader.class , null );
+        beanDescriptor.setDisplayName ( Util.THIS.getString("NAME_XSLDataLoader") );//GEN-HEADEREND:BeanDescriptor
+        
+        // Here you can add code for customizing the BeanDescriptor.
+        
+        return beanDescriptor;
     }
     
     /**
@@ -120,7 +62,18 @@ public class XSLDataLoaderBeanInfo extends SimpleBeanInfo {
      * if a given PropertyDescriptor is an IndexedPropertyDescriptor.
      */
     public PropertyDescriptor[] getPropertyDescriptors() {
-        return getPdescriptor();
+        int PROPERTY_extensions = 0;
+        PropertyDescriptor[] properties = new PropertyDescriptor[1];
+    
+        try {
+            properties[PROPERTY_extensions] = new PropertyDescriptor ( "extensions", XSLDataLoader.class, "getExtensions", "setExtensions" );
+            properties[PROPERTY_extensions].setDisplayName ( Util.THIS.getString ("PROP_XSL_Extensions") );
+            properties[PROPERTY_extensions].setShortDescription ( Util.THIS.getString ("HINT_XSL_Extensions") );
+        }
+        catch( IntrospectionException e) {
+            Exceptions.printStackTrace(e);
+        }
+        return properties;
     }
     
     /**
@@ -131,7 +84,7 @@ public class XSLDataLoaderBeanInfo extends SimpleBeanInfo {
      * should be obtained by automatic analysis.
      */
     public EventSetDescriptor[] getEventSetDescriptors() {
-        return getEdescriptor();
+        return new EventSetDescriptor[0];
     }
     
     /**
@@ -142,33 +95,9 @@ public class XSLDataLoaderBeanInfo extends SimpleBeanInfo {
      * should be obtained by automatic analysis.
      */
     public MethodDescriptor[] getMethodDescriptors() {
-        return getMdescriptor();
+        return new MethodDescriptor[0];
     }
     
-    /**
-     * A bean may have a "default" property that is the property that will
-     * mostly commonly be initially chosen for update by human's who are
-     * customizing the bean.
-     * @return  Index of default property in the PropertyDescriptor array
-     * 		returned by getPropertyDescriptors.
-     * <P>	Returns -1 if there is no default property.
-     */
-    public int getDefaultPropertyIndex() {
-        return defaultPropertyIndex;
-    }
-    
-    /**
-     * A bean may have a "default" event that is the event that will
-     * mostly commonly be used by human's when using the bean.
-     * @return Index of default event in the EventSetDescriptor array
-     *		returned by getEventSetDescriptors.
-     * <P>	Returns -1 if there is no default event.
-     */
-    public int getDefaultEventIndex() {
-        return defaultEventIndex;
-    }
-
-
     /** @param type Desired type of the icon
      * @return returns the Entity loader's icon
      */
@@ -188,7 +117,7 @@ public class XSLDataLoaderBeanInfo extends SimpleBeanInfo {
                 java.beans.Introspector.getBeanInfo (MultiFileLoader.class)
             };
         } catch (IntrospectionException e) {
-            // ignore
+            Exceptions.printStackTrace(e);
         }
         return super.getAdditionalBeanInfo();
     }
