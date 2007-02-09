ce1b5582cea7bf027a61f8cb99ad856e1fe1dafa
vkraemer <vkraemer@netbeans.org>
2007-02-09 07:10:33 +0000

    fix for 92624: from JFA

--- j2ee.sun.appsrv/src/org/netbeans/modules/j2ee/sun/appsrvapi/PortDetector.java
+++ j2ee.sun.appsrv/src/org/netbeans/modules/j2ee/sun/appsrvapi/PortDetector.java
@@ -39,18 +39,18 @@ import java.net.SocketTimeoutException;
 public class PortDetector {
     
     /**
-     *	This method accepts a hostname and port #.  It uses this information
-     *	to attempt to connect to the port, send a test query, analyze the
-     *	result to determine if the port is secure or unsecure (currently only
-     *	http / https is supported).
+     *  This method accepts a hostname and port #.  It uses this information
+     *  to attempt to connect to the port, send a test query, analyze the
+     *  result to determine if the port is secure or unsecure (currently only
+     *  http / https is supported).
      * it might emit a warning in the server log for GlassFish cases
      * No Harm, just an annoying warning, so we need to use this call only when really needed
      */
     public static boolean isSecurePort(String hostname, int port) throws IOException, ConnectException, SocketTimeoutException {
-	// Open the socket w/ a 4 second timeout
-	Socket socket = new Socket();
+        // Open the socket w/ a 4 second timeout
+        Socket socket = new Socket();
         try{
-            socket.connect(new InetSocketAddress(hostname, port), 4000);
+            socket.connect(new InetSocketAddress(hostname, port),4000);
         } catch (SocketException ex){// this could be bug 70020 due ot SOCKs proxy not having locahost
             String socksNonProxyHosts=System.getProperty("socksNonProxyHosts");
             if ((socksNonProxyHosts!=null) && (socksNonProxyHosts.indexOf("localhost")<0)) {
@@ -62,82 +62,78 @@ public class PortDetector {
                 throw ce; //status unknow at this point
                 //next call, we'll be ok and it will really detect if we are secure or not
             }
-
+            
             
         }
-
-	// Send an https query (w/ trailing http query)
-	java.io.OutputStream ostream = socket.getOutputStream();
-	ostream.write(TEST_QUERY);
-
-	// Get the result
-	java.io.InputStream istream = socket.getInputStream();
-	int count=0;
-	while (count<20) {
-	    // Wait up to 4 seconds
-	    try {
-		if (istream.available() > 0) {
-		    break;
-		}
-		Thread.sleep(200);
-	    } catch (InterruptedException ex) {
-	    }
-	    count++;
-	}
-
-	byte[] input = new byte[istream.available()];
-	istream.read(input);
-
-	// Close the socket
-	socket.close();
-
-	// Determine protocol from result
-	// Can't read https response w/ OpenSSL (or equiv), so use as
-	// default & try to detect an http response.
-	String response = new String(input).toLowerCase();
-	boolean isSecure = true;
-	if (response.length() == 0) {
-	    //isSecure = false; 
+        
+        // Send an https query (w/ trailing http query)
+        java.io.OutputStream ostream = socket.getOutputStream();
+        ostream.write(TEST_QUERY);
+        
+        // Get the result
+        java.io.InputStream istream = socket.getInputStream();
+        int count=0;
+        int byteRead = 0;
+        byte[] input = new byte[8192];
+        istream.read(input);
+        
+        // Close the socket
+        socket.close();
+        
+        // Determine protocol from result
+        // Can't read https response w/ OpenSSL (or equiv), so use as
+        // default & try to detect an http response.
+        String response = new String(input).toLowerCase();
+        boolean isSecure = true;
+        if (response.length() == 0) {
+            //isSecure = false;
             throw new ConnectException();
-	} else if (response.startsWith("http/1.")) {
-	    isSecure = false;
-	} else if (response.indexOf("<html") != -1) {
-	    isSecure = false;
-	} else if (response.indexOf("connection: ") != -1) {
-	    isSecure = false;
-	}
-	return isSecure;
+        } else if (response.startsWith("http/1.")) {
+            isSecure = false;
+        } else if (response.indexOf("<html") != -1) {
+            isSecure = false;
+        } else if (response.indexOf("connection: ") != -1) {
+            isSecure = false;
+        }
+        return isSecure;
     }
-
-
-
-
+    
+    
+    public static void main(String[] args) throws IOException{
+        String host = args[0];
+        int port = Integer.parseInt(args[1]);
+        System.out.println("host: " + " port: " + port);
+        System.out.println("isSecure: " + isSecurePort(host,port));
+    }
+    
+    
     /**
-     *	This is the test query used to ping the server in an attempt to
-     *	determine if it is secure or not.
+     *  This is the test query used to ping the server in an attempt to
+     *  determine if it is secure or not.
      */
     public static byte [] TEST_QUERY = new byte [] {
-	// The following SSL query is from nmap (http://www.insecure.org)
-	// This HTTPS request should work for most (all?) https servers
-	(byte)0x16, (byte)0x03, (byte)0x00, (byte)0x00, (byte) 'S', (byte)0x01,
-	(byte)0x00, (byte)0x00, (byte) 'O', (byte)0x03, (byte)0x00, (byte) '?',
-	(byte) 'G', (byte)0xd7, (byte)0xf7, (byte)0xba, (byte) ',', (byte)0xee,
-	(byte)0xea, (byte)0xb2, (byte) '`', (byte) '~', (byte)0xf3, (byte)0x00,
-	(byte)0xfd, (byte)0x82, (byte) '{', (byte)0xb9, (byte)0xd5, (byte)0x96,
-	(byte)0xc8, (byte) 'w', (byte)0x9b, (byte)0xe6, (byte)0xc4, (byte)0xdb,
-	(byte) '<', (byte) '=', (byte)0xdb, (byte) 'o', (byte)0xef, (byte)0x10,
-	(byte) 'n', (byte)0x00, (byte)0x00, (byte) '(', (byte)0x00, (byte)0x16,
-	(byte)0x00, (byte)0x13, (byte)0x00, (byte)0x0a, (byte)0x00, (byte) 'f',
-	(byte)0x00, (byte)0x05, (byte)0x00, (byte)0x04, (byte)0x00, (byte) 'e',
-	(byte)0x00, (byte) 'd', (byte)0x00, (byte) 'c', (byte)0x00, (byte) 'b',
-	(byte)0x00, (byte) 'a', (byte)0x00, (byte) '`', (byte)0x00, (byte)0x15,
-	(byte)0x00, (byte)0x12, (byte)0x00, (byte)0x09, (byte)0x00, (byte)0x14,
-	(byte)0x00, (byte)0x11, (byte)0x00, (byte)0x08, (byte)0x00, (byte)0x06,
-	(byte)0x00, (byte)0x03, (byte)0x01, (byte)0x00,
-	// The following is a HTTP request, some HTTP servers won't
-	// respond unless the following is also sent
-	(byte) 'G', (byte) 'E', (byte) 'T', (byte) ' ', (byte) '/', (byte) ' ',
-	(byte) 'H', (byte) 'T', (byte) 'T', (byte) 'P', (byte) '/', (byte) '1',
-	(byte) '.', (byte) '0', (byte)'\n', (byte)'\n'
-    };    
+        // The following SSL query is from nmap (http://www.insecure.org)
+        // This HTTPS request should work for most (all?) https servers
+        (byte)0x16, (byte)0x03, (byte)0x00, (byte)0x00, (byte) 'S', (byte)0x01,
+        (byte)0x00, (byte)0x00, (byte) 'O', (byte)0x03, (byte)0x00, (byte) '?',
+        (byte) 'G', (byte)0xd7, (byte)0xf7, (byte)0xba, (byte) ',', (byte)0xee,
+        (byte)0xea, (byte)0xb2, (byte) '`', (byte) '~', (byte)0xf3, (byte)0x00,
+        (byte)0xfd, (byte)0x82, (byte) '{', (byte)0xb9, (byte)0xd5, (byte)0x96,
+        (byte)0xc8, (byte) 'w', (byte)0x9b, (byte)0xe6, (byte)0xc4, (byte)0xdb,
+        (byte) '<', (byte) '=', (byte)0xdb, (byte) 'o', (byte)0xef, (byte)0x10,
+        (byte) 'n', (byte)0x00, (byte)0x00, (byte) '(', (byte)0x00, (byte)0x16,
+        (byte)0x00, (byte)0x13, (byte)0x00, (byte)0x0a, (byte)0x00, (byte) 'f',
+        (byte)0x00, (byte)0x05, (byte)0x00, (byte)0x04, (byte)0x00, (byte) 'e',
+        (byte)0x00, (byte) 'd', (byte)0x00, (byte) 'c', (byte)0x00, (byte) 'b',
+        (byte)0x00, (byte) 'a', (byte)0x00, (byte) '`', (byte)0x00, (byte)0x15,
+        (byte)0x00, (byte)0x12, (byte)0x00, (byte)0x09, (byte)0x00, (byte)0x14,
+        (byte)0x00, (byte)0x11, (byte)0x00, (byte)0x08, (byte)0x00, (byte)0x06,
+        (byte)0x00, (byte)0x03, (byte)0x01, (byte)0x00,
+        // The following is a HTTP request, some HTTP servers won't
+        // respond unless the following is also sent
+        (byte) 'G', (byte) 'E', (byte) 'T', (byte) ' ', (byte) '/', (byte) ' ',
+        (byte) 'H', (byte) 'T', (byte) 'T', (byte) 'P', (byte) '/', (byte) '1',
+        (byte) '.', (byte) '0', (byte)'\n', (byte)'\n'
+    };
 }
+
