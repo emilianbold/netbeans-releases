fc5cd75cd3aabecaa38f5df46668fa1a0cf70ad7
dkaspar <dkaspar@netbeans.org>
2007-02-12 13:27:45 +0000

    MIDPNB: Added missing SimpleCancellableTask.ExecutableParameter

--- vmd.midpnb/src/org/netbeans/modules/vmd/midpnb/components/resources/SimpleCancellableTaskCD.java
+++ vmd.midpnb/src/org/netbeans/modules/vmd/midpnb/components/resources/SimpleCancellableTaskCD.java
@@ -20,9 +20,12 @@
 package org.netbeans.modules.vmd.midpnb.components.resources;
 
 import org.netbeans.modules.vmd.api.codegen.CodeSetterPresenter;
+import org.netbeans.modules.vmd.api.codegen.CodeWriter;
 import org.netbeans.modules.vmd.api.codegen.MultiGuardedSection;
 import org.netbeans.modules.vmd.api.codegen.Parameter;
-import org.netbeans.modules.vmd.api.codegen.CodeWriter;
+import org.netbeans.modules.vmd.api.inspector.InspectorFolderPresenter;
+import org.netbeans.modules.vmd.api.inspector.InspectorPositionPresenter;
+import org.netbeans.modules.vmd.api.inspector.common.FolderPositionControllerFactory;
 import org.netbeans.modules.vmd.api.model.*;
 import org.netbeans.modules.vmd.api.properties.DefaultPropertiesPresenter;
 import org.netbeans.modules.vmd.api.properties.DesignEventFilterResolver;
@@ -33,35 +36,31 @@ import org.netbeans.modules.vmd.midp.components.MidpVersionDescriptor;
 import org.netbeans.modules.vmd.midp.components.MidpVersionable;
 import org.netbeans.modules.vmd.midp.components.general.ClassCD;
 import org.netbeans.modules.vmd.midp.propertyeditors.PropertiesCategories;
+import org.netbeans.modules.vmd.midp.inspector.controllers.ResourcePC;
+import org.netbeans.modules.vmd.midp.inspector.controllers.CategoryPC;
+import org.netbeans.modules.vmd.midpnb.codegen.MidpCustomCodePresenterSupport;
 import org.netbeans.modules.vmd.midpnb.components.displayables.AbstractInfoScreenCD;
 import org.netbeans.modules.vmd.midpnb.components.properteditors.PropertyEditorExecutableUserCode;
-import org.netbeans.modules.vmd.midpnb.codegen.MidpCustomCodePresenterSupport;
 
 import java.util.Arrays;
 import java.util.List;
-import org.netbeans.modules.vmd.api.inspector.InspectorFolderPresenter;
-import org.netbeans.modules.vmd.api.inspector.InspectorPositionPresenter;
-import org.netbeans.modules.vmd.api.inspector.common.FolderPositionControllerFactory;
-import org.netbeans.modules.vmd.midp.components.resources.ResourceCD;
-import org.netbeans.modules.vmd.midp.inspector.controllers.CategoryPC;
-import org.netbeans.modules.vmd.midp.inspector.controllers.ResourcePC;
 
 /**
  *
  * @author Karol Harezlak
  */
 public class SimpleCancellableTaskCD extends ComponentDescriptor {
-   
+
     public static final TypeID TYPEID = new TypeID(TypeID.Kind.COMPONENT, "org.netbeans.microedition.util.SimpleCancellableTask"); // NOI18N
-    
+
     public static final String ICON_PATH = "org/netbeans/modules/vmd/midpnb/resources/resource_16.png"; // NOI18N
-    
+
     private static final String PROP_CODE = "executableMethodBody"; //NOI18N
-    
+
     static {
         MidpTypes.registerIconResource(TYPEID, ICON_PATH);
     }
-    
+
     public SimpleCancellableTaskCD() {
     }
 
@@ -93,6 +92,7 @@ public class SimpleCancellableTaskCD extends ComponentDescriptor {
 
     private static Presenter createSetterPresenter () {
         return new CodeSetterPresenter ()
+            .addParameters (new ExecutableParameter ())
             .addSetters (MidpSetter.createConstructor (TYPEID, MidpVersionable.MIDP_2))
             .addSetters (MidpSetter.createSetter ("setExecutable", MidpVersionable.MIDP_2).addParameters (ExecutableParameter.PARAM_EXECUTABLE));
     }
@@ -106,9 +106,9 @@ public class SimpleCancellableTaskCD extends ComponentDescriptor {
             MidpCustomCodePresenterSupport.createAddImportPresenter (),
              // inspector
             InspectorFolderPresenter.create(true),
-            InspectorPositionPresenter.create(new ResourcePC(), FolderPositionControllerFactory.createHierarchical()),
-            InspectorPositionPresenter.create(new CategoryPC())
-                
+            InspectorPositionPresenter.create(new ResourcePC (), FolderPositionControllerFactory.createHierarchical()),
+            InspectorPositionPresenter.create(new CategoryPC ())
+
         );
     }
 
