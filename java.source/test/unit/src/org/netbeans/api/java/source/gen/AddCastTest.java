ae0c86d5b76c2d3c7035f6cbf4fbe779b61f0d76
pflaska <pflaska@netbeans.org>
2007-02-14 10:01:25 +0000

    #95279: Test for the issue.

--- java.source/test/unit/src/org/netbeans/api/java/source/gen/AddCastTest.java
+++ java.source/test/unit/src/org/netbeans/api/java/source/gen/AddCastTest.java
@@ -18,18 +18,15 @@
  */
 package org.netbeans.api.java.source.gen;
 
+import com.sun.source.tree.BlockTree;
 import com.sun.source.tree.ClassTree;
 import com.sun.source.tree.CompilationUnitTree;
-import com.sun.source.tree.ExpressionStatementTree;
 import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.ForLoopTree;
 import com.sun.source.tree.MethodTree;
-import com.sun.source.tree.StatementTree;
-import com.sun.source.tree.Tree;
 import com.sun.source.tree.VariableTree;
-import com.sun.source.util.TreePath;
 import java.io.File;
 import java.io.IOException;
-import javax.lang.model.element.TypeElement;
 import org.netbeans.api.java.source.CancellableTask;
 import org.netbeans.api.java.source.JavaSource;
 import org.netbeans.api.java.source.JavaSource.Phase;
@@ -51,7 +48,8 @@ public class AddCastTest extends GeneratorTestMDRCompat {
     }
     
     public static NbTestSuite suite() {
-        NbTestSuite suite = new NbTestSuite(AddCastTest.class);
+        NbTestSuite suite = new NbTestSuite();
+        suite.addTestSuite(AddCastTest.class);
         return suite;
     }
 
@@ -92,6 +90,58 @@ public class AddCastTest extends GeneratorTestMDRCompat {
                 MethodTree method = (MethodTree) clazz.getMembers().get(1);
                 VariableTree var = (VariableTree) method.getBody().getStatements().get(1);
                 ExpressionTree init = var.getInitializer();
+                ExpressionTree cast = make.TypeCast(make.Identifier("String"), init);
+                workingCopy.rewrite(init, cast);
+            }
+
+            public void cancel() {
+            }
+        };
+        src.runModificationTask(task).commit();
+        String res = TestUtilities.copyFileToString(testFile);
+        System.err.println(res);
+        assertEquals(golden, res);
+    }
+    
+    public void testAddCastInForWithoutSteps() throws Exception {
+        testFile = new File(getWorkDir(), "Test.java");
+        TestUtilities.copyStringToFile(testFile, 
+            "package hierbas.del.litoral;\n\n" +
+            "import java.util.*;\n\n" +
+            "public class Test<E> {\n" +
+            "    public void cast() {\n" +
+            "        Object o = null;\n" +
+            "        for (int i = 0; i < 5; ) {\n" +
+            "            String s = o;\n" +
+            "        }\n" +
+            "    }\n" +
+            "}\n"
+            );
+        String golden =
+            "package hierbas.del.litoral;\n\n" +
+            "import java.util.*;\n\n" +
+            "public class Test<E> {\n" +
+            "    public void cast() {\n" +
+            "        Object o = null;\n" +
+            "        for (int i = 0; i < 5; ) {\n" +
+            "            String s = (String) o;\n" +
+            "        }\n" +
+            "    }\n" +
+            "}\n";
+        JavaSource src = getJavaSource(testFile);
+        
+        CancellableTask task = new CancellableTask<WorkingCopy>() {
+
+            public void run(WorkingCopy workingCopy) throws IOException {
+                workingCopy.toPhase(Phase.RESOLVED);
+                CompilationUnitTree cut = workingCopy.getCompilationUnit();
+                TreeMaker make = workingCopy.getTreeMaker();
+                ClassTree clazz = (ClassTree) cut.getTypeDecls().get(0);
+                MethodTree method = (MethodTree) clazz.getMembers().get(1);
+                ForLoopTree forLoop = (ForLoopTree) method.getBody().getStatements().get(1);
+                BlockTree block = (BlockTree) forLoop.getStatement();
+                VariableTree vt = (VariableTree) block.getStatements().get(0);
+                ExpressionTree init = vt.getInitializer();
                 ExpressionTree cast = make.TypeCast(make.Identifier("String"), init);
                 workingCopy.rewrite(init, cast);
             }
