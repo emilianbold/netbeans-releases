52155322faa2b0c42942a29dc2243d847d033ec3
mfukala <mfukala@netbeans.org>
2007-02-14 13:58:23 +0000

    #95211 - HTML editor should show Ctrl+Alt+Shift completion

--- web.core.syntax/src/org/netbeans/modules/web/core/syntax/completion/JspCompletionProvider.java
+++ web.core.syntax/src/org/netbeans/modules/web/core/syntax/completion/JspCompletionProvider.java
@@ -62,10 +62,11 @@ public class JspCompletionProvider implements CompletionProvider {
     }
     
     public CompletionTask createTask(int type, JTextComponent component) {
-        if (type == COMPLETION_QUERY_TYPE)
+        if ((type & COMPLETION_QUERY_TYPE & COMPLETION_SMART_QUERY_TYPE & COMPLETION_ALL_QUERY_TYPE) != 0) {
             return new AsyncCompletionTask(new Query(component.getCaret().getDot()), component);
-        else if (type == DOCUMENTATION_QUERY_TYPE)
+        } else if (type == DOCUMENTATION_QUERY_TYPE) {
             return new AsyncCompletionTask(new DocQuery(null), component);
+        }
         return null;
     }
     
