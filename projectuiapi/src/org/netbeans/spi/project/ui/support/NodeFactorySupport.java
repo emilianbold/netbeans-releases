46237be9d7e5c0fa886faaf0d97cadea5cf45998
mkleint <mkleint@netbeans.org>
2007-02-21 07:09:19 +0000

    #89662 - use a more finegrained approach to refreshing items in the children list. no each child key from NodeList is handled separately to prevent unnecessary regeneration of nodes.

--- projectuiapi/src/org/netbeans/spi/project/ui/support/NodeFactorySupport.java
+++ projectuiapi/src/org/netbeans/spi/project/ui/support/NodeFactorySupport.java
@@ -21,7 +21,9 @@ package org.netbeans.spi.project.ui.support;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
@@ -94,13 +96,14 @@ public class NodeFactorySupport {
         }
     }
     
-    static class DelegateChildren extends Children.Keys<NodeList<?>> implements LookupListener, ChangeListener {
+    static class DelegateChildren extends Children.Keys<NodeListKeyWrapper> implements LookupListener, ChangeListener {
         
         private String folderPath;
         private Project project;
         private List<NodeList<?>> nodeLists = new ArrayList<NodeList<?>>();
         private List<NodeFactory> factories = new ArrayList<NodeFactory>();
         private Lookup.Result<NodeFactory> result;
+        private HashMap<NodeList<?>, List<NodeListKeyWrapper>> keys;
         
         public DelegateChildren(Project proj, String path) {
             folderPath = path;
@@ -114,43 +117,57 @@ public class NodeFactorySupport {
             return new FolderLookup(folder).getLookup();
         }
         
-        protected Node[] createNodes(NodeList<?> lst) {
-            return _createNodes(lst);
+       protected Node[] createNodes(NodeListKeyWrapper key) {
+           Node nd = key.nodeList.node(key.object);
+           if (nd != null) {
+               return new Node[] { nd };
+           }
+           return new Node[0];
         }
-        private static <K> Node[] _createNodes(NodeList<K> lst) {
-            List<Node> toRet = new ArrayList<Node>();
-            for (K elem : lst.keys()) {
-                Node nd = lst.node(elem);
-                if (nd != null) {
-                    toRet.add(nd);
-                }
-            }
-            return toRet.toArray(new Node[toRet.size()]);
-        }
-        
+       
+       private Collection<NodeListKeyWrapper> createKeys() {
+           Collection<NodeListKeyWrapper> col = new ArrayList<NodeListKeyWrapper>();
+           synchronized (keys) {
+               for (NodeList lst : nodeLists) {
+                   List<NodeListKeyWrapper> x = keys.get(lst);
+                   if (x != null) {
+                       col.addAll(x);
+                   }
+               }
+           }
+           return col;
+       }
+      
         protected void addNotify() {
             super.addNotify();
+            keys = new HashMap<NodeList<?>, List<NodeListKeyWrapper>>();
             result = createLookup().lookup(new Lookup.Template<NodeFactory>(NodeFactory.class));
             for (NodeFactory factory : result.allInstances()) {
                 NodeList<?> lst = factory.createNodes(project);
                 assert lst != null;
-                nodeLists.add(lst);
                 lst.addNotify();
+                synchronized (keys) {
+                    nodeLists.add(lst);
+                    addKeys(lst);
+                }
                 lst.addChangeListener(this);
                 factories.add(factory);
             }
             result.addLookupListener(this);
-            setKeys(nodeLists);
+            setKeys(createKeys());
         }
         
         protected void removeNotify() {
             super.removeNotify();
-            setKeys(Collections.<NodeList<?>>emptySet());
+            setKeys(Collections.<NodeListKeyWrapper>emptySet());
             for (NodeList elem : nodeLists) {
                 elem.removeChangeListener(this);
                 elem.removeNotify();
             }
-            nodeLists.clear();
+            synchronized (keys) {
+                keys.clear();
+                nodeLists.clear();
+            }
             factories.clear();
             if (result != null) {
                 result.removeLookupListener(this);
@@ -159,8 +176,29 @@ public class NodeFactorySupport {
         }
         
         public void stateChanged(ChangeEvent e) {
-            refreshKey((NodeList<?>) e.getSource());
+            NodeList list = (NodeList) e.getSource();
+            synchronized (keys) {
+                removeKeys(list);
+                addKeys(list);
+            }
+            setKeys(createKeys());
         }
+        
+        //to be called under lock.
+        private void addKeys(NodeList list) {
+            List<NodeListKeyWrapper> wrps = new ArrayList<NodeListKeyWrapper>();
+            for (Object key : list.keys()) {
+                wrps.add(new NodeListKeyWrapper(key, list));
+            }
+            keys.put(list, wrps);
+            
+        }
+        
+        //to be called under lock.
+        private void removeKeys(NodeList list) {
+            keys.remove(list);
+        }
+
 
         public void resultChanged(LookupEvent ev) {
             int index = 0;
@@ -169,20 +207,59 @@ public class NodeFactorySupport {
                     factories.add(index, factory);
                     NodeList<?> lst = factory.createNodes(project);
                     assert lst != null;
-                    nodeLists.add(index, lst);
+                    synchronized (keys) {
+                        nodeLists.add(index, lst);
+                        addKeys(lst);
+                    }
                     lst.addNotify();
                     lst.addChangeListener(this);
                 } else {
                     while (!factory.equals(factories.get(index))) {
                         factories.remove(index);
-                        NodeList<?> lst = nodeLists.remove(index);
-                        lst.removeNotify();
-                        lst.removeChangeListener(this);                            
+                        synchronized (keys) {
+                            NodeList<?> lst = nodeLists.remove(index);
+                            removeKeys(lst);
+                            lst.removeNotify();
+                            lst.removeChangeListener(this);                            
+                        }
                     }
                 }
                 index++;
             }
-            setKeys(nodeLists);
+            setKeys(createKeys());
+        }
+        
+
+    }
+    
+    /**
+     * this class makes sure the bond between the NodeList and individial
+     * items is not lost, prevents duplicates about different NodeLists
+     * while allowing for fine-grained updating of nodes on stateChange()
+     * 
+     */ 
+    private static class NodeListKeyWrapper  {
+        NodeList nodeList;
+        Object object;
+        NodeListKeyWrapper(Object obj, NodeList list) {
+            nodeList = list;
+            object = obj;
+        }
+        
+        public boolean equals(Object obj) {
+            if (! (obj instanceof NodeListKeyWrapper)) {
+                return false;
+            }
+            NodeListKeyWrapper other = (NodeListKeyWrapper)obj;
+            if (! nodeList.equals(other.nodeList)) {
+                return false;
+            }
+            return object.equals(other.object);
+        }
+        
+        public int hashCode() {
+            return (17 * 37 + nodeList.hashCode()) * 37 + object.hashCode();
+                    
         }
         
     }
