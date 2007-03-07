ae29cdcab88a871fe960dcb07d1336201abfe428
jtulach <jtulach@netbeans.org>
2007-03-07 10:36:53 +0000

    Forgotten license

--- o.n.core/test/unit/src/org/netbeans/core/NbSheetTest.java
+++ o.n.core/test/unit/src/org/netbeans/core/NbSheetTest.java
@@ -1,23 +1,31 @@
 /*
- * NbSheetTest.java
- * JUnit based test
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
  *
- * Created on 6. b\u0159ezen 2007, 17:48
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
+ * Microsystems, Inc. All Rights Reserved.
  */
 
 package org.netbeans.core;
 
 import java.awt.event.ActionEvent;
-import java.io.ObjectInput;
-import java.io.ObjectOutput;
 import javax.swing.SwingUtilities;
-import junit.framework.TestCase;
 import org.netbeans.core.actions.GlobalPropertiesAction;
 import org.netbeans.junit.NbTestCase;
 import org.openide.nodes.AbstractNode;
 import org.openide.nodes.Children;
 import org.openide.nodes.Node;
-import org.openide.util.HelpCtx;
 import org.openide.windows.TopComponent;
 
 /**
