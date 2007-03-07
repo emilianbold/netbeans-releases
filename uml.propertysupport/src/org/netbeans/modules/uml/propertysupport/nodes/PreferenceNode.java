07923752ccda1e35ef6b5462f23efad3972bbf26
sherylsu <sherylsu@netbeans.org>
2007-03-07 21:05:45 +0000

    IZ = 78547 - there is bad icon on UML node in advanced options use NB data folder icon to be consistent with rest of the option category display

--- uml.propertysupport/src/org/netbeans/modules/uml/propertysupport/nodes/PreferenceNode.java
+++ uml.propertysupport/src/org/netbeans/modules/uml/propertysupport/nodes/PreferenceNode.java
@@ -37,158 +37,193 @@ import javax.swing.UIManager;
  * It is plugged into the netbeans settings framework via {@link #factory()} method
  * registered in .../uml/propertysupport/resources/layer.xml
  */ 
-public final class PreferenceNode extends AbstractNode {
-
+public final class PreferenceNode extends AbstractNode
+{
+    /** Icon resource string for folder node */
+    static final String FOLDER_ICON_BASE =
+        "org/openide/loaders/defaultFolder"; // NOI18N
     private final IPropertyElement elment;
     private final PreferenceChildren prefChildren;
-
-	public PreferenceNode()
-	{
+    
+    public PreferenceNode()
+    {
         this(findRootElement());
         setDisplayName(NbBundle.getMessage(PreferenceNode.class, "LAB_PreferenceNodeDisplayName")); // NOI18N
-	}
-	
-    public PreferenceNode(IPropertyElement pe) {
+    }
+    
+    public PreferenceNode(IPropertyElement pe)
+    {
         this(pe, new PreferenceChildren(pe));
     }
-        
-    private PreferenceNode(IPropertyElement pe, PreferenceChildren ch) {
+    
+    private PreferenceNode(IPropertyElement pe, PreferenceChildren ch)
+    {
         // Children.LEAF forces nb explorer to paint the node as leaf
         super(ch.getChildren().isEmpty()? Children.LEAF: ch);
-            
+        
         this.elment = pe;
         this.prefChildren = ch;
         IPropertyDefinition pdef = pe.getPropertyDefinition();
         setDisplayName(pdef.getDisplayName());
-
         resolveIconBase(pdef);
     }
+    
+    private IPropertyElement getElement()
+    {
+        return elment;
+    }
+    /**
+     *
+     *
+     */
+    public Image getIcon(int type)
+    {
 
-	private IPropertyElement getElement()
-	{
-		return elment;
-	}
-	/**
-	 *
-	 *
-	 */
-	public Image getIcon(int type)
-	{
-		if (getElement() == findRootElement() )
-		{ 
-			Image image=(Image)UIManager.get(
-				"Nb.Explorer.Folder.icon"); // NOI18N
-			if (image!=null)
-			{
-				return image;
-			}
-			else
-			{
-				return super.getIcon(type);
-			}
-		}
-		return super.getIcon(type);
-	}
-	
-	
-	/**
-	 *
-	 *
-	 */
-	public Image getOpenedIcon(int type)
-	{
-		return getIcon(type);
-	}
-	
-    private void resolveIconBase(IPropertyDefinition pdef) {
+        if (getElement() == findRootElement() )
+        { 
+            Image img = (Image)UIManager.get("Nb.Explorer.Folder.icon");
+            if (img == null)
+            {
+                setIconBase(FOLDER_ICON_BASE);
+                img = super.getIcon(type);
+            }
+            return img;
+        }
+        return super.getIcon(type);
+    }
+    
+    
+    /**
+     *
+     *
+     */
+    public Image getOpenedIcon(int type)
+    {
+        
+         if (getElement() == findRootElement() )
+        { 
+             Image img = (Image)UIManager.get("Nb.Explorer.Folder.openedIcon");
+             if (img == null)
+             {
+                 setIconBase(FOLDER_ICON_BASE);
+                 img = super.getOpenedIcon(type);
+             }
+             return img;
+         }
+        return super.getOpenedIcon(type);
+    }
+    
+    private void resolveIconBase(IPropertyDefinition pdef)
+    {
         String imagePath = pdef.getImage();
-        if (imagePath != null && imagePath.length() > 0) { // ignore empty image path
+        if (imagePath != null && imagePath.length() > 0)
+        { // ignore empty image path
             int endOffset = imagePath.indexOf('.');
-            if (endOffset > 0) { // remove .gif or whatever suffix
+            if (endOffset > 0)
+            { // remove .gif or whatever suffix
                 imagePath = imagePath.substring(0, endOffset);
             }
             setIconBase(imagePath);
         }
     }
-
-    public String getShortDescription() {
+    
+    public String getShortDescription()
+    {
         return this.elment.getPropertyDefinition().getHelpDescription();
     }
-
-    protected Sheet createSheet() {
+    
+    protected Sheet createSheet()
+    {
         List<IPropertyElement> properties = prefChildren.getProperties();
         return PreferenceHelper.createNodeProperties(properties);
     }
     
-    public static Node factory() {
+    public static Node factory()
+    {
         IPropertyElement rootEl = findRootElement();
         AbstractNode n = new PreferenceNode(rootEl);
         n.setDisplayName(NbBundle.getMessage(PreferenceNode.class, "LAB_PreferenceNodeDisplayName")); // NOI18N
 //        n.setIconBase("org/netbeans/modules/uml/resources/uml"); // NOI18N
-		n.setIconBaseWithExtension("org/netbeans/modules/uml/resources/uml"); // NOI18N
+        n.setIconBaseWithExtension("org/netbeans/modules/uml/resources/uml"); // NOI18N
         return n;
     }
     
-    private static IPropertyElement findRootElement() {
+    private static IPropertyElement findRootElement()
+    {
         IPropertyElement retVal = null;
         IPreferenceManager2 prefMan = ProductRetriever.retrieveProduct().getPreferenceManager();
-        if (prefMan != null) {
+        if (prefMan != null)
+        {
             IPropertyElement[] pes = prefMan.getPropertyElements();
-            if (pes != null && pes.length == 1) {
+            if (pes != null && pes.length == 1)
+            {
                 retVal = pes[0];
             }
         }
         return retVal;
     }
-
-    private static final class PreferenceChildren extends Children.Keys {
-
+    
+    private static final class PreferenceChildren extends Children.Keys
+    {
+        
         private final List<IPropertyElement> children = new LinkedList<IPropertyElement>();
         private final List<IPropertyElement> properties = new LinkedList<IPropertyElement>();;
         private boolean initSubElements = true;
         private final IPropertyElement parentEl;
-
-        public PreferenceChildren(IPropertyElement parent) {
-            if (parent == null) {
+        
+        public PreferenceChildren(IPropertyElement parent)
+        {
+            if (parent == null)
+            {
                 throw new NullPointerException();
             }
             this.parentEl = parent;
         }
         
-        protected void addNotify() {
+        protected void addNotify()
+        {
             super.addNotify();
             setKeys(getChildren());
         }
-
-        protected Node[] createNodes(Object key) {
+        
+        protected Node[] createNodes(Object key)
+        {
             Node[] retVal;
-            if (key instanceof IPropertyElement) {
+            if (key instanceof IPropertyElement)
+            {
                 retVal = new Node[] {new PreferenceNode((IPropertyElement) key)};
-            } else {
+            }
+            else
+            {
                 retVal = new Node[0];
             }
             return retVal;
         }
-
-        public List<IPropertyElement> getChildren() {
+        
+        public List<IPropertyElement> getChildren()
+        {
             initSubElements();
             return children;
         }
-
-        public List<IPropertyElement> getProperties() {
+        
+        public List<IPropertyElement> getProperties()
+        {
             initSubElements();
             return properties;
         }
-
-        private void initSubElements() {
-            synchronized (this.children) {
-                if (this.initSubElements) {
+        
+        private void initSubElements()
+        {
+            synchronized (this.children)
+            {
+                if (this.initSubElements)
+                {
                     PreferenceHelper.analyzeSubElements(this.parentEl, children, properties);
                     this.initSubElements = false;
                 }
             }
         }
-
+        
     }
     
 }
