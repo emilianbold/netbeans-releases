1d5f891b69a09feb3db5cf5fbbc5fc1008458f5a
vstejskal <vstejskal@netbeans.org>
2007-03-07 01:36:31 +0000

    adding a basic test for KitsTracker

---
+++ editor/test/unit/src/org/netbeans/modules/editor/impl/KitsTrackerTest.java
@@ -0,0 +1,36 @@
+/*
+ * KitsTrackerTest.java
+ *
+ * Created on March 7, 2007, 2:17 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package org.netbeans.modules.editor.impl;
+
+import java.lang.reflect.Method;
+import javax.swing.text.EditorKit;
+import org.netbeans.junit.NbTestCase;
+
+/**
+ *
+ * @author vita
+ */
+public class KitsTrackerTest extends NbTestCase {
+    
+    /** Creates a new instance of KitsTrackerTest */
+    public KitsTrackerTest(String name) {
+        super(name);
+    }
+    
+    // o.n.editor.BaseKit uses similar code
+    public void testKitsTrackerCallable() throws Exception {
+        Class clazz = getClass().getClassLoader().loadClass("org.netbeans.modules.editor.impl.KitsTracker"); //NOI18N
+        Method getInstanceMethod = clazz.getDeclaredMethod("getInstance"); //NOI18N
+        Method findMimeTypeMethod = clazz.getDeclaredMethod("findMimeType", Class.class); //NOI18N
+        Object kitsTracker = getInstanceMethod.invoke(null);
+        String mimeType = (String) findMimeTypeMethod.invoke(kitsTracker, EditorKit.class);
+        assertNull("EditorKit.class should not have a mime type", mimeType);
+    }
+}
