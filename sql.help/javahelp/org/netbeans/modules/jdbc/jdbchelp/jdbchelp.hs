931c78d2e4700a33a6e78fead41a38fabab90b71
narayanap <narayanap@netbeans.org>
2007-03-15 12:17:06 +0000

    adding jdbchelp module

---
+++ sql.help/javahelp/org/netbeans/modules/jdbc/jdbchelp/jdbchelp.hs
@@ -0,0 +1,33 @@
+<?xml version='1.0' encoding='ISO-8859-1'?>
+<!--
+* Copyright 2007 Sun Microsystems, Inc. All rights reserved.
+* Use is subject to license terms.
+-->
+<!DOCTYPE helpset PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN"
+                         "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
+
+<helpset version="2.0">
+	<title>JDBC Wizard Help</title>
+	<maps>
+		<homeID>orch_intro</homeID>
+		<mapref location="jdbchelp-map.jhm"/>
+	</maps>
+	<view>
+		<name>TOC</name>
+		<label>Table of Contents</label>
+		<type>javax.help.TOCView</type>
+		<data>jdbchelp-toc.xml</data>
+	</view>
+	<view>
+		<name>Index</name>
+		<label>Index</label>
+		<type>javax.help.IndexView</type>
+		<data>jdbchelp-idx.xml</data>
+	</view>
+	<view>
+		<name>Search</name>
+		<label>Search</label>
+		<type>javax.help.SearchView</type>
+		<data engine="com.sun.java.help.search.DefaultSearchEngine">JavaHelpSearch</data>
+	</view>
+</helpset>
