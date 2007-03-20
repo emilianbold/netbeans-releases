7939fd4852e2f620a7055195f8656bb01aa6441b
msauer <msauer@netbeans.org>
2007-03-20 11:02:16 +0000

    Adding test for changing fonts & colors from option dialog

---
+++ editor/test/qa-functional/data/projects/editor_test/src/org/netbeans/test/editor/highlight/testCommentColor.java
@@ -0,0 +1,12 @@
+/*
+ */
+
+package org.netbeans.test.editor.highlight;
+
+public class testCommentColor {
+    int a = 3;
+    static int color; //comment
+    private int unused; 
+    public testCommentColor(int x) {                   
+    }
+}
