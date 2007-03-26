850ae422240ca52b8501d3ee8166011cce607279
sherylsu <sherylsu@netbeans.org>
2007-03-26 22:25:58 +0000

    IZ=98751 - diagrams are not closed after uml module is disabled port fix for 96473 to trunk

--- uml.project/src/org/netbeans/modules/uml/project/UMLProjectModule.java
+++ uml.project/src/org/netbeans/modules/uml/project/UMLProjectModule.java
@@ -5,7 +5,7 @@
  *
  * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
  * or http://www.netbeans.org/cddl.txt.
-
+ 
  * When distributing Covered Code, include this CDDL Header Notice in each file
  * and include the License file at http://www.netbeans.org/cddl.txt.
  * If applicable, add the following below the CDDL Header, with the fields
@@ -25,13 +25,13 @@
 
 package org.netbeans.modules.uml.project;
 
-import javax.swing.SwingUtilities;
 import org.netbeans.api.project.Project;
 import org.netbeans.api.project.ui.OpenProjects;
 import org.netbeans.modules.uml.core.IApplication;
 import org.netbeans.modules.uml.core.coreapplication.CoreProductManager;
 import org.netbeans.modules.uml.core.coreapplication.ICoreProductManager;
 import org.netbeans.modules.uml.core.coreapplication.IProductDescriptor;
+import org.netbeans.modules.uml.core.metamodel.diagrams.IProxyDiagram;
 import org.netbeans.modules.uml.core.support.umlutils.ETList;
 import org.netbeans.modules.uml.ui.products.ad.applicationcore.ADProduct;
 import org.netbeans.modules.uml.ui.products.ad.applicationcore.IADProduct;
@@ -42,6 +42,8 @@ import org.openide.modules.ModuleInstall;
 
 import org.netbeans.modules.uml.ui.products.ad.projecttreedefaultengine.ADProjectTreeEngine;
 import org.netbeans.modules.uml.project.ui.NetBeansUMLProjectTreeModel;
+import org.netbeans.modules.uml.ui.support.ProductHelper;
+import org.netbeans.modules.uml.ui.support.applicationmanager.IProductDiagramManager;
 
 /**
  *
@@ -49,132 +51,136 @@ import org.netbeans.modules.uml.project.ui.NetBeansUMLProjectTreeModel;
  */
 public class UMLProjectModule extends ModuleInstall
 {
-   private static ADProjectTreeEngine mEngine = new ADProjectTreeEngine();
+    private static ADProjectTreeEngine mEngine = new ADProjectTreeEngine();
 //	private static NetBeansUMLProjectTreeModel mModel = new NetBeansUMLProjectTreeModel();
-   private static NetBeansUMLProjectTreeModel mModel = null;
-   
-   /** Creates a new instance of UMLProjectModule */
-   public UMLProjectModule()
-   {
-      
-   }
-   
-   ////////////////////////////////////////////////////////////////////////////
-   // ModuleInstall Methods
-   
-   /**
-    * Loads the UML application and the initializes the Application Designer
-    * product.
-    */
-   public void restored()
-   {
-      final IADProduct product = getProduct();
-      mModel = new NetBeansUMLProjectTreeModel();
-      
-      if(product != null)
-      {         
+    private static NetBeansUMLProjectTreeModel mModel = null;
+    
+    /** Creates a new instance of UMLProjectModule */
+    public UMLProjectModule()
+    {
+        
+    }
+    
+    ////////////////////////////////////////////////////////////////////////////
+    // ModuleInstall Methods
+    
+    /**
+     * Loads the UML application and the initializes the Application Designer
+     * product.
+     */
+    public void restored()
+    {
+        final IADProduct product = getProduct();
+        mModel = new NetBeansUMLProjectTreeModel();
+        
+        if(product != null)
+        {
 //         RequestProcessor.getDefault().post(new Runnable()
 //         {
 //             public void run()
-             {
-                 IApplication app = product.initialize2(false);
-                 
-                 DispatchHelper helper = new DispatchHelper();
-                 helper.registerDrawingAreaEvents(mModel.getDrawingAreaListener());
-             }
+            {
+                IApplication app = product.initialize2(false);
+                
+                DispatchHelper helper = new DispatchHelper();
+                helper.registerDrawingAreaEvents(mModel.getDrawingAreaListener());
+            }
 //         });
+            
+            //product.setDiagramManager(new UMLDiagramManager());
+            
+            // Put the user interface proxy
+            product.setProxyUserInterface(new UMLUserInterface());
+            product.setProjectManager(new UMLProductProjectManager());
+            
+            // m_AcceleratorMgr = m_ADProduct.getAcceleratorManager();
+        }
+        else
+        {
+            DispatchHelper helper = new DispatchHelper();
+            helper.registerDrawingAreaEvents(mModel.getDrawingAreaListener());
+        }
         
-         //product.setDiagramManager(new UMLDiagramManager());
-                  
-         // Put the user interface proxy
-         product.setProxyUserInterface(new UMLUserInterface());
-         product.setProjectManager(new UMLProductProjectManager());
-         
-         // m_AcceleratorMgr = m_ADProduct.getAcceleratorManager();
-      }  
-      else
-      {
-          DispatchHelper helper = new DispatchHelper();
-          helper.registerDrawingAreaEvents(mModel.getDrawingAreaListener());
-      }      
-      
-      mEngine.initialize(mModel);
-      
-      
-   }
-      
-   /**
-    * Makes sure that all of the UML projects are saved.
-    */
-   public boolean closing()
-   {
-       return true;
-       // if project is modified user will be prompted, no need to save the project
-       // automatically
+        mEngine.initialize(mModel);
+        
+        
+    }
+    
+    /**
+     * Makes sure that all of the UML projects are saved.
+     */
+    public boolean closing()
+    {
+        return true;
+        // if project is modified user will be prompted, no need to save the project
+        // automatically
 //      boolean retVal = SaveProjectsDialog.saveProjects();
-      
-   }
-   
-   
-   public void uninstalled()
-   {
-		SwingUtilities.invokeLater(new Runnable()
-		{
-			public void run()
-			{
-				OpenProjects.getDefault().close(ProjectUtil.getOpenUMLProjects());
-			}
-		});        			
-
-   }
-   
-   
-   ////////////////////////////////////////////////////////////////////////////
-   // Data Access Methods
-   
-   public static ADProjectTreeEngine getProjectTreeEngine()
-   {
-      return mEngine;
-   }
-   
-   public static NetBeansUMLProjectTreeModel getProjectTreeModel()
-   {
-      return mModel;
-   }
-   
-   public static void addModelNode(UMLModelRootNode node, UMLProject project)
-   {
-      mModel.addModelRootNode(node, project);
-   }
-   
-   
-   //////////////////////////////////////////////////////////////////
-   // Helper Methods
-   
-   protected IADProduct getProduct()
-   {
-      IADProduct retVal = null;
-      
-      ICoreProductManager productManager = CoreProductManager.instance();
-      ETList<IProductDescriptor> pDesc = productManager.getProducts();
-      //ETList pDesc = productManager.getProducts();
-      
-      // Make sure that another project has not already created a product
-      if((pDesc == null) || (pDesc.size() == 0))
-      {
-         // Create a new ADProduct
-         retVal = new ADProduct();         
-         productManager.setCoreProduct(retVal);         
-      }
-      else
-      {
-         IProductDescriptor descriptor = (IProductDescriptor)pDesc.get(0);
-         if(descriptor.getCoreProduct() instanceof IADProduct)
-         {
-            retVal = (IADProduct)descriptor.getCoreProduct();
-         }
-      }
-      
-      return retVal;
-   }
+        
+    }
+    
+    
+    public void uninstalled()
+    {
+        Project[] projects = ProjectUtil.getOpenUMLProjects();
+        for (Project p: projects)
+        {
+            UMLProjectHelper h = (UMLProjectHelper)p.getLookup().lookup(UMLProjectHelper.class);
+            if (h != null)
+            {
+                IProductDiagramManager pDiaMgr =
+                            ProductHelper.getProductDiagramManager();
+                pDiaMgr.closeAllDiagrams();
+            }
+        }
+        OpenProjects.getDefault().close(projects);      
+    }
+    
+    
+    ////////////////////////////////////////////////////////////////////////////
+    // Data Access Methods
+    
+    public static ADProjectTreeEngine getProjectTreeEngine()
+    {
+        return mEngine;
+    }
+    
+    public static NetBeansUMLProjectTreeModel getProjectTreeModel()
+    {
+        return mModel;
+    }
+    
+    public static void addModelNode(UMLModelRootNode node, UMLProject project)
+    {
+        mModel.addModelRootNode(node, project);
+    }
+    
+    
+    //////////////////////////////////////////////////////////////////
+    // Helper Methods
+    
+    protected IADProduct getProduct()
+    {
+        IADProduct retVal = null;
+        
+        ICoreProductManager productManager = CoreProductManager.instance();
+        ETList<IProductDescriptor> pDesc = productManager.getProducts();
+        //ETList pDesc = productManager.getProducts();
+        
+        // Make sure that another project has not already created a product
+        if((pDesc == null) || (pDesc.size() == 0))
+        {
+            // Create a new ADProduct
+            retVal = new ADProduct();
+            productManager.setCoreProduct(retVal);
+        }
+        else
+        {
+            IProductDescriptor descriptor = (IProductDescriptor)pDesc.get(0);
+            if(descriptor.getCoreProduct() instanceof IADProduct)
+            {
+                retVal = (IADProduct)descriptor.getCoreProduct();
+            }
+        }
+        
+        return retVal;
+    }
 }
--- uml/src/org/netbeans/modules/uml/ui/support/applicationmanager/IProductDiagramManager.java
+++ uml/src/org/netbeans/modules/uml/ui/support/applicationmanager/IProductDiagramManager.java
@@ -5,7 +5,7 @@
  *
  * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
  * or http://www.netbeans.org/cddl.txt.
-
+ 
  * When distributing Covered Code, include this CDDL Header Notice in each file
  * and include the License file at http://www.netbeans.org/cddl.txt.
  * If applicable, add the following below the CDDL Header, with the fields
@@ -27,77 +27,82 @@ import org.netbeans.modules.uml.core.support.umlutils.ETList;
 
 public interface IProductDiagramManager
 {
-	/**
-	 * Tell the gui to open a diagram.  If bMaximized then the diagram is opened maximized so it draws.
-	*/
-	public IDiagram openDiagram( String sTOMFilename, boolean bMaximized, IDiagramCallback pDiagramCreatedCallback );
-
-	/**
-	 * Tell the gui to open a diagram
-	*/
-	public IDiagram openDiagram2( IProxyDiagram pProxyDiagram, boolean bMaximized, IDiagramCallback pDiagramCreatedCallback );
-
-	/**
-	 * Tell the gui to close a diagram
-	*/
-	public long closeDiagram( String sTOMFilename );
-
-	/**
-	 * Tell the gui to close a diagram
-	*/
-	public long closeDiagram2( IDiagram pDiagram );
-
-	/**
-	 * Tell the gui to close this diagram
-	*/
-	public long closeDiagram3( IProxyDiagram pProxyDiagram );
-
-	/**
-	 * Tell the gui to open the new diagram dialog
-	*/
-	public IDiagram newDiagramDialog( INamespace pNamespace, /* DiagramKind */ int nDefaultDiagram, int lAvailableDiagramKinds, IDiagramCallback pDiagramCreatedCallback );
-
-	/**
-	 * Bring this diagram to the front
-	*/
-	public long raiseWindow( IDiagram pOpenControl );
-
-	/**
-	 * Returns the currently active diagram.
-	*/
-	public IDiagram getCurrentDiagram();
-
-	/**
-	 * Returns the diagram with this name, returns 0 if the diagram is not open.
-	*/
-	public IDiagram getOpenDiagram( String sTOMFilename );
-
-	/**
-	 * Create a new diagram
-	*/
-	public IDiagram createDiagram( /* DiagramKind */ int nDiagramKind, INamespace pNamespace, String sDiagramName, IDiagramCallback pDiagramCreatedCallback );
-
-	/**
-	 * Returns all the open diagrams.
-	*/
-	public ETList<IProxyDiagram> getOpenDiagrams();
-
-	/**
-	 * Tell the gui to minimize a diagram
-	*/
-	public long minimizeDiagram( String sTOMFilename, boolean bMinimize );
-
-	/**
-	 * Tell the gui to minimize a diagram
-	*/
-	public long minimizeDiagram2( IDiagram pDiagram, boolean bMinimize );
-
-	/**
-	 * Tell the gui to minimize this diagram
-	*/
-	public long minimizeDiagram3( IProxyDiagram pProxyDiagram, boolean bMinimize );
-   
-   /** Refresh the diagram by reading in the contents of the diagram file. */
-   public void refresh(IProxyDiagram proxy);
-
+    /**
+     * Tell the gui to open a diagram.  If bMaximized then the diagram is opened maximized so it draws.
+     */
+    public IDiagram openDiagram( String sTOMFilename, boolean bMaximized, IDiagramCallback pDiagramCreatedCallback );
+    
+    /**
+     * Tell the gui to open a diagram
+     */
+    public IDiagram openDiagram2( IProxyDiagram pProxyDiagram, boolean bMaximized, IDiagramCallback pDiagramCreatedCallback );
+    
+    /**
+     * Tell the gui to close a diagram
+     */
+    public long closeDiagram( String sTOMFilename );
+    
+    /**
+     * Tell the gui to close a diagram
+     */
+    public long closeDiagram2( IDiagram pDiagram );
+    
+    /**
+     * Tell the gui to close this diagram
+     */
+    public long closeDiagram3( IProxyDiagram pProxyDiagram );
+    
+    /**
+     * Tell the gui to open the new diagram dialog
+     */
+    public IDiagram newDiagramDialog( INamespace pNamespace, /* DiagramKind */ int nDefaultDiagram, int lAvailableDiagramKinds, IDiagramCallback pDiagramCreatedCallback );
+    
+    /**
+     * Bring this diagram to the front
+     */
+    public long raiseWindow( IDiagram pOpenControl );
+    
+    /**
+     * Returns the currently active diagram.
+     */
+    public IDiagram getCurrentDiagram();
+    
+    /**
+     * Returns the diagram with this name, returns 0 if the diagram is not open.
+     */
+    public IDiagram getOpenDiagram( String sTOMFilename );
+    
+    /**
+     * Create a new diagram
+     */
+    public IDiagram createDiagram( /* DiagramKind */ int nDiagramKind, INamespace pNamespace, String sDiagramName, IDiagramCallback pDiagramCreatedCallback );
+    
+    /**
+     * Returns all the open diagrams.
+     */
+    public ETList<IProxyDiagram> getOpenDiagrams();
+    
+    /**
+     * Tell the gui to minimize a diagram
+     */
+    public long minimizeDiagram( String sTOMFilename, boolean bMinimize );
+    
+    /**
+     * Tell the gui to minimize a diagram
+     */
+    public long minimizeDiagram2( IDiagram pDiagram, boolean bMinimize );
+    
+    /**
+     * Tell the gui to minimize this diagram
+     */
+    public long minimizeDiagram3( IProxyDiagram pProxyDiagram, boolean bMinimize );
+    
+    /** Refresh the diagram by reading in the contents of the diagram file. */
+    public void refresh(IProxyDiagram proxy);
+    
+   /* close all diagrams
+    */
+    
+    public void closeAllDiagrams();
+    
 }
