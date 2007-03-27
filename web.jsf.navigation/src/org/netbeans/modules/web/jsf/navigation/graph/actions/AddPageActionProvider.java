c1c2082b4fea5beabad6a854f2e3cd9b63a8e450
JoelleLam <devnull@localhost>
2007-03-27 06:33:50 +0000

    Bug 98851 - Will need to double integrate if we want this fix in M8.  Allows AddPage action.

--- web.jsf.navigation/src/org/netbeans/modules/web/jsf/navigation/graph/actions/AddPageActionProvider.java
+++ web.jsf.navigation/src/org/netbeans/modules/web/jsf/navigation/graph/actions/AddPageActionProvider.java
@@ -33,63 +33,69 @@ import org.openide.util.NbBundle;
  * @author joelle
  */
 public class AddPageActionProvider extends AbstractAction implements ContextAwareAction {
-    PageFlowScene scene;
     
-    private static final String DEFAULT_DOC_BASE_FOLDER = "web"; //NOI18N
     
-    /** Creates a new instance of OpenPageAction */
-    public AddPageActionProvider(PageFlowScene scene) {
-        putValue(NAME, getDisplayName());
-        this.scene = scene;
-    }
-    
-    /**
-     *
-     * @return
-     */
-    protected String getDisplayName() {
-        return NbBundle.getMessage(AddPageActionProvider.class, "LBL_AddPage");
-    }
-    
-    public void actionPerformed(ActionEvent e) {
-        try {
-            PageFlowController pfc = scene.getPageFlowView().getPageFlowController();
-            
-            FileObject parentFolder = pfc.getProject().getProjectDirectory();
-            FileObject webFileObject = parentFolder.getFileObject(DEFAULT_DOC_BASE_FOLDER);
-            
-            String name = FileUtil.findFreeFileName(webFileObject, "page", "jsp");
-            name = JOptionPane.showInputDialog("Select Page Name", name);
-            
-            createIndexJSP(webFileObject, name);
-        } catch (IOException ex) {
-            Exceptions.printStackTrace(ex);
-        }
-        
-        //            }
-    }
     public Action createContextAwareInstance(Lookup lookup) {
         PageFlowScene scene = lookup.lookup(PageFlowScene.class);
-        return new AddPageActionProvider(scene);
+        return new AddPageAction(scene);
+    }
+  
+    public void actionPerformed(ActionEvent arg0) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
     
-    
-    /**
-     * Creates a JSP in the
-     * @param name
-     * @throws java.io.IOException
-     */
-    public void createIndexJSP(FileObject targetFolder, String name ) throws IOException {
+    private class AddPageAction extends AbstractAction {
         
-        FileObject jspTemplate = Repository.getDefault().getDefaultFileSystem().findResource( "Templates/JSP_Servlet/JSP.jsp" ); // NOI18N
+        private PageFlowScene scene;
+        private static final String DEFAULT_DOC_BASE_FOLDER = "web"; //NOI18N
         
-        if (jspTemplate == null)
-            return; // Don't know the template
+        /** Creates a new instance of OpenPageAction
+         * @param scene
+         */
+        public AddPageAction(PageFlowScene scene) {
+            putValue(NAME, getDisplayName());
+            this.scene = scene;
+        }
         
+        /**
+         *
+         * @return The Display Name of this option.
+         */
+        protected String getDisplayName() {
+            return NbBundle.getMessage(AddPageActionProvider.class, "LBL_AddPage");
+        }
         
-        DataObject mt = DataObject.find(jspTemplate);
-        DataFolder webDf = DataFolder.findFolder(targetFolder);
-        mt.createFromTemplate(webDf, name); // NOI18N
+        public void actionPerformed(ActionEvent e) {
+            try {
+                PageFlowController pfc = scene.getPageFlowView().getPageFlowController();
+                
+                FileObject parentFolder = pfc.getProject().getProjectDirectory();
+                FileObject webFileObject = parentFolder.getFileObject(DEFAULT_DOC_BASE_FOLDER);
+                
+                String name = FileUtil.findFreeFileName(webFileObject, "page", "jsp");
+                name = JOptionPane.showInputDialog("Select Page Name", name);
+                
+                createIndexJSP(webFileObject, name);
+            } catch (IOException ex) {
+                Exceptions.printStackTrace(ex);
+            }
+            
+            //            }
+        }
+        
+        private void createIndexJSP(FileObject targetFolder, String name ) throws IOException {
+            
+            FileObject jspTemplate = Repository.getDefault().getDefaultFileSystem().findResource( "Templates/JSP_Servlet/JSP.jsp" ); // NOI18N
+            
+            if (jspTemplate == null)
+                return; // Don't know the template
+            
+            
+            DataObject mt = DataObject.find(jspTemplate);
+            DataFolder webDf = DataFolder.findFolder(targetFolder);
+            mt.createFromTemplate(webDf, name); // NOI18N
+        }
     }
+    
     
 }
