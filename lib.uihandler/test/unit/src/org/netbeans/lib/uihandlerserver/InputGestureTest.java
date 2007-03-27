2254cb8221c4b4b0557145d8cdd6a6ceecbc21e0
jtulach <jtulach@netbeans.org>
2007-03-27 17:14:27 +0000

    page-down is not gesture action

--- lib.uihandler/test/unit/src/org/netbeans/lib/uihandlerserver/InputGestureTest.java
+++ lib.uihandler/test/unit/src/org/netbeans/lib/uihandlerserver/InputGestureTest.java
@@ -17,6 +17,7 @@
 
 package org.netbeans.lib.uihandlerserver;
 
+import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
@@ -61,6 +62,29 @@ public class InputGestureTest extends NbTestCase {
     }
 
     protected void tearDown() throws Exception {
+    }
+    
+    public void testIsLowLevelAction() throws Exception {
+        String s = "<record>n"
++ "<date>2007-03-27T18:45:54</date>n"
++ "<millis>1175013954455</millis>n"
++ "<sequence>1236</sequence>n"
++ "<level>FINE</level>n"
++ "<thread>13</thread>n"
++ "<message>UI_ACTION_EDITOR</message>n"
++ "<param>java.awt.event.ActionEvent[ACTION_PERFORMED,cmd=null,when=1175013954453,modifiers=] on org.openide.text.QuietEditorPane[,0,-2250,1086x12960,layout=javax.swing.plaf.basic.BasicTextUI$UpdateHandler,alignmentX=0.0,alignmentY=0.0,border=javax.swing.plaf.basic.BasicBorders$MarginBorder@1976f3a,flags=296,maximumSize=,minimumSize=,preferredSize=,caretColor=java.awt.Color[r=255,g=255,b=255],disabledTextColor=javax.swing.plaf.ColorUIResource[r=184,g=207,b=229],editable=true,margin=java.awt.Insets[top=0,left=0,bottom=0,right=0],selectedTextColor=sun.swing.PrintColorUIResource[r=51,g=51,b=51],selectionColor=javax.swing.plaf.ColorUIResource[r=184,g=207,b=229],kit=org.netbeans.modules.editor.java.JavaKit@b8df14,typeHandlers=]</param>n"
++ "<param>java.awt.event.ActionEvent[ACTION_PERFORMED,cmd=null,when=1175013954453,modifiers=] on org.openide.text.QuietEditorPane[,0,-1500,1086x12960,layout=javax.swing.plaf.basic.BasicTextUI$UpdateHandler,alignmentX=0.0,alignmentY=0.0,border=javax.swing.plaf.basic.BasicBorders$MarginBorder@1976f3a,flags=296,maximumSize=,minimumSize=,preferredSize=,caretColor=java.awt.Color[r=255,g=255,b=255],disabledTextColor=javax.swing.plaf.ColorUIResource[r=184,g=207,b=229],editable=true,margin=java.awt.Insets[top=0,left=0,bottom=0,right=0],selectedTextColor=sun.swing.PrintColorUIResource[r=51,g=51,b=51],selectionColor=javax.swing.plaf.ColorUIResource[r=184,g=207,b=229],kit=org.netbeans.modules.editor.java.JavaKit@b8df14,typeHandlers=]</param>n"
++ "<param>org.netbeans.editor.BaseKit$PageDownAction@596079</param>n"
++ "<param>org.netbeans.editor.BaseKit$PageDownAction@596079</param>n"
++ "<param>page-down</param>n"
++ "</record>";
+        
+        InputStream is = new ByteArrayInputStream(s.getBytes());
+        TestHandler records = new TestHandler(is);
+        LogRecord r = records.read();
+        assertNotNull(r);
+        
+        assertNull("No gesture", InputGesture.valueOf(r));
     }
 
     public void testReadALogAndTestInputGestures() throws Exception {
