b79b529196688d9b9e49b31398f9f9443b112a97
bhate <bhate@netbeans.org>
2007-03-27 23:56:33 +0000

    Cleanup of UI

--- websvc.design/src/org/netbeans/modules/websvc/design/view/widget/AbstractTitledWidget.java
+++ websvc.design/src/org/netbeans/modules/websvc/design/view/widget/AbstractTitledWidget.java
@@ -24,6 +24,10 @@ import java.awt.GradientPaint;
 import java.awt.Graphics2D;
 import java.awt.Paint;
 import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.geom.Arc2D;
+import java.awt.geom.GeneralPath;
+import java.awt.geom.RoundRectangle2D;
 import org.netbeans.api.visual.border.BorderFactory;
 import org.netbeans.api.visual.widget.Widget;
 import org.netbeans.api.visual.widget.Scene;
@@ -34,15 +38,11 @@ import org.netbeans.modules.websvc.design.view.layout.LeftRightLayout;
  */
 public abstract class AbstractTitledWidget extends Widget implements ExpandableWidget {
     
-    private static final Color FILL_COLOR_DARK = Color.WHITE;
-    private static final Color FILL_COLOR_LIGHT = Color.WHITE;
     private static final Color BORDER_COLOR = Color.GRAY;
     private static final int RADIUS = 10;
-    private static final int DEPTH = 2;
+    private static final int DEPTH = 3;
     private static final boolean raised = true;
     
-    private Color fillColorDark = FILL_COLOR_DARK;
-    private Color fillColorLight = FILL_COLOR_LIGHT;
     private Color borderColor = BORDER_COLOR;
     private int radius = RADIUS;
     
@@ -53,7 +53,9 @@ public abstract class AbstractTitledWidget extends Widget implements ExpandableW
     /**
      * Creates a new instance of RoundedRectangleWidget
      * with default rounded radius and gap and no gradient color
-     * @param scene
+     * @param scene scene this widget belongs to
+     * @param radius of the rounded arc
+     * @param color color of the border and gradient title
      */
     public AbstractTitledWidget(Scene scene, int radius, Color color) {
         super(scene);
@@ -83,37 +85,45 @@ public abstract class AbstractTitledWidget extends Widget implements ExpandableW
         Paint oldPaint = g.getPaint();
         if(raised) {
             g.setPaint(borderColor);
-            g.fillRoundRect(bounds.x+DEPTH, bounds.y+DEPTH, bounds.width-DEPTH,
-                    bounds.height-DEPTH, radius, radius);
+            RoundRectangle2D outerRect = new RoundRectangle2D.Double(
+                    bounds.x + DEPTH, bounds.y + DEPTH,
+                    bounds.width - DEPTH, bounds.height - DEPTH, radius, radius);
+            Shape oldClip = g.getClip();
+            g.setClip(bounds.x + bounds.width - DEPTH, bounds.y,
+                    DEPTH, bounds.height);
+            g.fill(outerRect);
+            g.setClip(bounds.x, bounds.y + bounds.height - DEPTH,
+                    bounds.width, DEPTH);
+            g.fill(outerRect);
+            g.setClip(oldClip);
+            Arc2D arc = new Arc2D.Double(bounds.x + bounds.width - DEPTH - radius,
+                    bounds.y + bounds.height - radius - DEPTH, radius, radius,0,-90,Arc2D.OPEN);
+            GeneralPath gpath = new GeneralPath(arc);
+            gpath.lineTo(bounds.x + bounds.width - DEPTH, bounds.y + bounds.height - DEPTH);
+            gpath.closePath();
+            g.fill(gpath);
         }
+        RoundRectangle2D rect = new RoundRectangle2D.Double(
+                bounds.x, bounds.y, bounds.width - (raised?DEPTH:0),
+                bounds.height - (raised?DEPTH:0), radius, radius);
         if(isExpanded()) {
-            int titleHeight = headerWidget.getBounds().height;
-            int bodyY = bounds.y + radius + titleHeight + radius/2 ;
-            int footerY = bounds.y + bounds.height - radius/2 - (raised?DEPTH:0)*2;
+            Shape oldClip = g.getClip();
+            int titleHeight = radius + headerWidget.getBounds().height + radius/2;
             GradientPaint gp = new GradientPaint(bounds.x, bounds.y, borderColor,
-                    bounds.x, bodyY, borderColor.brighter());
+                    bounds.x, bounds.y + titleHeight/2, borderColor.brighter(),true);
+            g.setClip(bounds.x, bounds.y, bounds.width, titleHeight);
             g.setPaint(gp);
-            g.fillRoundRect(bounds.x, bounds.y, bounds.width-(raised?DEPTH:0),
-                    bodyY-bounds.y+radius/2, radius, radius);
-            gp = new GradientPaint(bounds.x, footerY-radius, borderColor,
-                    bounds.x, bounds.y+bounds.height-(raised?DEPTH:0), borderColor.brighter());
-            g.setPaint(gp);
-            g.fillRoundRect(bounds.x, footerY-radius/2, bounds.width-(raised?DEPTH:0),
-                    bounds.height-footerY-(raised?DEPTH:0)-radius/2-(raised?DEPTH:0), radius, radius);
-            GradientPaint gpb = new GradientPaint(bounds.x, bodyY, fillColorDark,
-                    bounds.x, footerY, fillColorLight);
-            g.setPaint(gpb);
-            g.fillRect(bounds.x, bodyY, bounds.width-(raised?DEPTH:0),footerY-bodyY);
+            g.fill(rect);
+            g.setClip(oldClip);
         } else {
             GradientPaint gp = new GradientPaint(bounds.x, bounds.y, borderColor,
-                    bounds.x, bounds.y+bounds.height, borderColor.brighter());
+                    bounds.x, (bounds.y + bounds.height - (raised?DEPTH:0))/2,
+                    borderColor.brighter(),true);
             g.setPaint(gp);
-            g.fillRoundRect(bounds.x, bounds.y, bounds.width-(raised?DEPTH:0),
-                    bounds.height-(raised?DEPTH:0), radius, radius);
+            g.fill(rect);
         }
         g.setPaint(borderColor);
-        g.drawRoundRect(bounds.x, bounds.y, bounds.width-(raised?DEPTH:0),
-                bounds.height-(raised?DEPTH:0), radius, radius);
+        g.draw(rect);
         g.setPaint(oldPaint);
     }
     
