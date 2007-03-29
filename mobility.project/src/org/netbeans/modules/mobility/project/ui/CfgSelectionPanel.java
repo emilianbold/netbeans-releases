01df16d1d3b83f94566fd2aa1f7c3d48d75c17de
asotona <asotona@netbeans.org>
2007-03-29 13:37:36 +0000

    fixed license

--- mobility.project/src/org/netbeans/modules/mobility/project/ui/CfgSelectionPanel.java
+++ mobility.project/src/org/netbeans/modules/mobility/project/ui/CfgSelectionPanel.java
@@ -1,7 +1,20 @@
 /*
- * CfgSelectionPanel.java
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
  *
- * Created on 22. bøezen 2007, 22:27
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Microsystems, Inc. All Rights Reserved.
  */
 
 package org.netbeans.modules.mobility.project.ui;
@@ -9,8 +22,6 @@ package org.netbeans.modules.mobility.project.ui;
 import java.awt.Component;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.HashSet;
 import javax.swing.JCheckBox;
 import javax.swing.JPanel;
@@ -19,7 +30,7 @@ import org.openide.util.NbBundle;
 
 /**
  *
- * @author  Adam
+ * @author  Adam Sotona
  */
 public class CfgSelectionPanel extends JPanel {
     
