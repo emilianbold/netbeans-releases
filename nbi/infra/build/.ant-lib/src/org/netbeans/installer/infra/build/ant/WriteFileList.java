ac08ad4b9dfe072db5cb6fa3b35b55a4d3d44ce1
dlipin <dlipin@netbeans.org>
2007-04-02 17:07:48 +0000

    Some log on making engine content list

--- nbi/infra/build/.ant-lib/src/org/netbeans/installer/infra/build/ant/WriteFileList.java
+++ nbi/infra/build/.ant-lib/src/org/netbeans/installer/infra/build/ant/WriteFileList.java
@@ -2,17 +2,17 @@
  * The contents of this file are subject to the terms of the Common Development and
  * Distribution License (the License). You may not use this file except in compliance
  * with the License.
- * 
+ *
  * You can obtain a copy of the License at http://www.netbeans.org/cddl.html or
  * http://www.netbeans.org/cddl.txt.
- * 
+ *
  * When distributing Covered Code, include this CDDL Header Notice in each file and
  * include the License file at http://www.netbeans.org/cddl.txt. If applicable, add
  * the following below the CDDL Header, with the fields enclosed by brackets []
  * replaced by your own identifying information:
- * 
+ *
  *     "Portions Copyrighted [year] [name of copyright owner]"
- * 
+ *
  * The Original Software is NetBeans. The Initial Developer of the Original Software
  * is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun Microsystems, Inc. All
  * Rights Reserved.
@@ -92,7 +92,9 @@ public class WriteFileList extends Task{
         check(dir,"starting directory");
         check(output,"output file");
         check(mask,"file mask");
-        
+        log("Root directory : " + dir);
+        log("Output file : " + output);
+        log("Mask  : "        + mask);
         File root = new File(dir);
         File outFile = new File(output);
         FileOutputStream fos = null;
