989b3ee02bac1ff2d5367a4577ea2cdafaf60659
ksorokin <ksorokin@netbeans.org>
2007-04-04 07:38:43 +0000

    Correcting the SJSAM integration with the IDE.

--- installer/components/products/nb-soa/src/org/netbeans/installer/products/nb/soa/ConfigurationLogic.java
+++ installer/components/products/nb-soa/src/org/netbeans/installer/products/nb/soa/ConfigurationLogic.java
@@ -19,8 +19,19 @@
  */
 package org.netbeans.installer.products.nb.soa;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 import org.netbeans.installer.netbeans.product.components.NbClusterConfigurationLogic;
+import org.netbeans.installer.netbeans.utils.applications.GlassFishUtils;
+import org.netbeans.installer.netbeans.utils.applications.NetBeansUtils;
+import org.netbeans.installer.product.Registry;
+import org.netbeans.installer.product.components.Product;
 import org.netbeans.installer.utils.exceptions.InitializationException;
+import org.netbeans.installer.utils.exceptions.InstallationException;
+import org.netbeans.installer.utils.exceptions.UninstallationException;
+import org.netbeans.installer.utils.helper.Dependency;
+import org.netbeans.installer.utils.progress.Progress;
 
 /**
  *
@@ -33,10 +44,120 @@ public class ConfigurationLogic extends NbClusterConfigurationLogic {
         super(CLUSTER, ID);
     }
     
+    @Override
+    public void install(Progress progress) throws InstallationException {
+        final File soaLocation = getProduct().getInstallationLocation();
+        
+        // get the list of suitable netbeans ide installations
+        List<Dependency> dependencies = 
+                getProduct().getDependencyByUid(IDE_UID);
+        List<Product> sources = 
+                Registry.getInstance().getProducts(dependencies.get(0));
+        
+        // pick the first one and integrate with it
+        final Product nbProduct = sources.get(0);
+        final File nbLocation = nbProduct.getInstallationLocation();
+        
+        // get the list of suitable openesb installations
+        dependencies = 
+                getProduct().getDependencyByUid(OPENESB_UID);
+        sources = 
+                Registry.getInstance().getProducts(dependencies.get(0));
+        
+        // pick the first one and integrate with it
+        final Product openesbProduct = sources.get(0);
+        final File openesbLocation = openesbProduct.getInstallationLocation();
+        
+        // get the list of suitable access manager installations
+        dependencies = 
+                getProduct().getDependencyByUid(AM_UID);
+        sources = 
+                Registry.getInstance().getProducts(dependencies.get(0));
+        
+        // pick the first one and integrate with it
+        final Product amProduct = sources.get(0);
+        final File amLocation = amProduct.getInstallationLocation();
+        
+        // get the list of suitable glassfish installations
+        dependencies = 
+                amProduct.getDependencyByUid(GLASSFISH_UID);
+        sources = 
+                Registry.getInstance().getProducts(dependencies.get(0));
+        
+        // pick the first one and integrate with it
+        final Product glassfishProduct = sources.get(0);
+        final File glassfishLocation = glassfishProduct.getInstallationLocation();
+        
+        final File amConfigFile = new File(GlassFishUtils.getDomainConfig(
+                glassfishLocation, GlassFishUtils.DEFAULT_DOMAIN), AM_CONFIG_FILE);
+        
+        /////////////////////////////////////////////////////////////////////////////
+        try {
+            progress.setDetail(getString("CL.install.netbeans.conf.am")); // NOI18N
+            
+            NetBeansUtils.setJvmOption(
+                    nbLocation, 
+                    JVM_OPTION_AM_CONFIG, 
+                    amConfigFile.getAbsolutePath(), 
+                    true);
+        } catch (IOException e) {
+            throw new InstallationException(
+                    getString("CL.install.error.netbeans.conf.am"),  // NOI18N
+                    e);
+        }
+        
+        /////////////////////////////////////////////////////////////////////////////
+        super.install(progress);
+    }
+    
+    @Override
+    public void uninstall(Progress progress) throws UninstallationException {
+        final File soaLocation = getProduct().getInstallationLocation();
+        
+        // get the list of suitable netbeans ide installations
+        List<Dependency> dependencies = 
+                getProduct().getDependencyByUid(IDE_UID);
+        List<Product> sources = 
+                Registry.getInstance().getProducts(dependencies.get(0));
+        
+        // pick the first one and integrate with it
+        final Product nbProduct = sources.get(0);
+        final File nbLocation = nbProduct.getInstallationLocation();
+        
+        /////////////////////////////////////////////////////////////////////////////
+        try {
+            progress.setDetail(getString("CL.uninstall.netbeans.conf.am")); // NOI18N
+            
+            NetBeansUtils.removeJvmOption(nbLocation, JVM_OPTION_AM_CONFIG);
+        } catch (IOException e) {
+            throw new UninstallationException(
+                    getString("CL.uninstall.error.netbeans.conf.am"), // NOI18N
+                    e);
+        }
+        
+        /////////////////////////////////////////////////////////////////////////////
+        super.uninstall(progress);
+    }
+    
     /////////////////////////////////////////////////////////////////////////////////
     // Constants
     public static final String CLUSTER =
             "soa{soa-cluster-version}"; // NOI18N
     public static final String ID =
             "SOA"; // NOI18N
+    
+    public static final String GLASSFISH_UID =
+            "glassfish"; // NOI18N
+    public static final String OPENESB_UID =
+            "openesb"; // NOI18N
+    public static final String AM_UID = 
+            "sjsam"; // NOI18N
+    
+    public static final String JVM_OPTION_AM_CONFIG = 
+            "-DAM_CONFIG_FILE"; // NOI18N
+    public static final String JVM_OPTION_GLASSFISH = 
+            "-Dcom.sun.aas.installRoot"; // NOI18N
+            
+    public static final String AM_CONFIG_FILE = 
+            "AMConfig.properties"; // NOI18N
 }
