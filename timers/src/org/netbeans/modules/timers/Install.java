fd181fa314dcb8c93b2720d6668b7c9896cc95a1
pnejedly <pnejedly@netbeans.org>
2007-04-07 12:18:23 +0000

    Support logger-based API. No formal documentation yet, but usable. Given the number of leaks lately, we need to act quickly anyway.

---
+++ timers/src/org/netbeans/modules/timers/Install.java
@@ -0,0 +1,78 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.modules.timers;
+
+import java.util.logging.Handler;
+import java.util.logging.Level;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
+import org.openide.modules.ModuleInstall;
+
+/**
+ *
+ * @author nenik
+ */
+public class Install extends  ModuleInstall {
+    static Logger logger;
+    private static Handler timers = new TimerHandler();
+    
+    private static String INSTANCES = "Important instances";
+    
+    public void restored() {
+        Logger log = Logger.getLogger("TIMER"); // NOI18N
+        log.setUseParentHandlers(false);
+        log.setLevel(Level.FINEST);
+        log.addHandler(timers);
+    }
+    
+    private static class TimerHandler extends Handler {
+        TimerHandler() {}
+    
+        public void publish(LogRecord rec) {
+            String message = rec.getMessage();
+            Object[] args = rec.getParameters();
+            if (args == null || args[0] == null) return;
+            
+            if (args.length == 1) { // simplified instance logging
+                TimesCollectorPeer.getDefault().reportReference(
+                        INSTANCES, message, message, args[0]);
+                return;
+            }
+            
+            if (args.length != 2) return;
+            
+            Object key = args[0];
+
+            if (args[1] instanceof Number) { // time
+                TimesCollectorPeer.getDefault().reportTime(
+                        key, message, message, ((Number)args[1]).longValue());
+            } else if (args[1] instanceof Boolean) { // start/stop logic
+                // XXX - start/stop support
+            } else {
+                TimesCollectorPeer.getDefault().reportReference(
+                        key, message, message, args[1]);
+            }
+        }
+    
+        public void flush() {}
+        public void close() throws SecurityException {}
+    }
+   
+}
