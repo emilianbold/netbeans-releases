cfccbe10f59b71ebc08c394b8365857d729a30fd
yaroslavskiy <yaroslavskiy@netbeans.org>
2007-04-10 13:20:57 +0000

    [Issue 99960] Comapp can't be renamed: Can not deploy the Synchronouse Sample with name not SynchronouseSample

--- bpel.samples/resources/TravelReservationService/ReservationPartnerServices/src/java/partnerservices/AirlineReservationPortType_Impl.java
+++ bpel.samples/resources/TravelReservationService/ReservationPartnerServices/src/java/partnerservices/AirlineReservationPortType_Impl.java
@@ -1,21 +1,3 @@
-/*
- * The contents of this file are subject to the terms of the Common Development
- * and Distribution License (the License). You may not use this file except in
- * compliance with the License.
- * 
- * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
- * or http://www.netbeans.org/cddl.txt.
- * 
- * When distributing Covered Code, include this CDDL Header Notice in each file
- * and include the License file at http://www.netbeans.org/cddl.txt.
- * If applicable, add the following below the CDDL Header, with the fields
- * enclosed by brackets [] replaced by your own identifying information:
- * "Portions Copyrighted [year] [name of copyright owner]"
- * 
- * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
- * Microsystems, Inc. All Rights Reserved.
- */
 package partnerservices;
 
 import javax.ejb.SessionBean;
@@ -32,18 +14,11 @@ public class AirlineReservationPortType_Impl implements partnerservices.AirlineR
     public void reserveAirline(javax.xml.soap.SOAPElement itinerary) throws
             java.rmi.RemoteException {
         
-//        System.err.println("Enter AirlineReservationPortType_Impl.reserveAirline");
-//        System.err.println("Itinerary as SOAPElement: " + itinerary.toString() );
-        
         String uniqueID = PartnerUtils.getUniqueID(itinerary);
         
         PartnerUtils.sendJMSMessageToReservationCallbackProviderDestination(
                 ReservationCallbackProviderBean.AIRLINE_RESERVATION, uniqueID);
-        
-//        System.err.println("AirlineReservationPortType_Impl.reserveAirline after sendJMS");
-        
     }
-    
     
     public boolean cancelAirline(javax.xml.soap.SOAPElement itinerary) throws
             java.rmi.RemoteException {
@@ -51,9 +26,6 @@ public class AirlineReservationPortType_Impl implements partnerservices.AirlineR
         boolean _retVal = false;
         return _retVal;
     }
-    
-    
-    
     
     /**
      * @see javax.ejb.SessionBean#setSessionContext(javax.ejb.SessionContext)
