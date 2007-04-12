f2e7e26d5f229010645b6c5ef7859726a024138b
jpokorsky <jpokorsky@netbeans.org>
2007-04-12 14:28:41 +0000

    #100796: test added

---
+++ java.source/test/unit/src/org/netbeans/api/java/source/gen/InterfaceTest.java
@@ -0,0 +1,99 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ * 
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ * 
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ * 
+ * "Portions Copyrighted [2007] [Sun Microsystems, Inc]"
+ * 
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+package org.netbeans.api.java.source.gen;
+
+import com.sun.source.tree.ClassTree;
+import com.sun.source.tree.Tree;
+import java.io.File;
+import java.util.EnumSet;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.type.TypeKind;
+import org.netbeans.api.java.source.CancellableTask;
+import org.netbeans.api.java.source.JavaSource;
+import org.netbeans.api.java.source.TestUtilities;
+import org.netbeans.api.java.source.TreeMaker;
+import org.netbeans.api.java.source.WorkingCopy;
+import org.netbeans.junit.NbTestSuite;
+
+/**
+ *
+ * @author Jan Pokorsky
+ */
+public class InterfaceTest extends GeneratorTestMDRCompat {
+
+    public InterfaceTest(String name) {
+        super(name);
+    }
+
+    public static NbTestSuite suite() {
+        NbTestSuite suite = new NbTestSuite();
+        suite.addTestSuite(InterfaceTest.class);
+//        suite.addTest(new InterfaceTest("testAddField"));
+        return suite;
+    }
+    
+    // issue #100796
+    public void testAddField() throws Exception {
+        testFile = new File(getWorkDir(), "Test.java");
+        TestUtilities.copyStringToFile(testFile, 
+            "package hierbas.del.litoral;\n\n" +
+            "import java.util.*;\n\n" +
+            "public interface Test {\n" +
+            "}\n"
+            );
+        String golden =
+            "package hierbas.del.litoral;\n\n" +
+            "import java.util.*;\n\n" +
+            "public interface Test {\n" +
+            "   public static final int CONSTANT = 0;\n" +
+            "}\n";
+        JavaSource src = getJavaSource(testFile);
+        src.runModificationTask(new CancellableTask<WorkingCopy>() {
+            public void cancel() {
+            }
+
+            public void run(WorkingCopy wc) throws Exception {
+                wc.toPhase(JavaSource.Phase.RESOLVED);
+                ClassTree ct = (ClassTree) wc.getCompilationUnit().getTypeDecls().get(0);
+                TreeMaker make = wc.getTreeMaker();
+                Tree vt = make.Variable(
+                        make.Modifiers(EnumSet.<Modifier>of(Modifier.PUBLIC, Modifier.STATIC, Modifier.FINAL)),
+                        "CONSTANT",
+                        make.PrimitiveType(TypeKind.INT),
+                        make.Identifier("0")
+                        );
+                wc.rewrite(ct, make.addClassMember(ct, vt));
+            }
+        }).commit();
+        
+        String res = TestUtilities.copyFileToString(testFile);
+        System.err.println(res);
+        assertEquals(golden, res);
+    }
+
+    String getGoldenPckg() {
+        return "";
+    }
+
+    String getSourcePckg() {
+        return "";
+    }
+
+}
