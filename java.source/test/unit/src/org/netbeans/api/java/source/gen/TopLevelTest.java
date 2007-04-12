d55fd72b7ff59a7f5fac0b90cf952cc390abf330
pflaska <pflaska@netbeans.org>
2007-04-12 13:05:42 +0000

    Testing top level class insertion.

---
+++ java.source/test/unit/src/org/netbeans/api/java/source/gen/TopLevelTest.java
@@ -0,0 +1,169 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+package org.netbeans.api.java.source.gen;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.sun.source.tree.*;
+import java.util.Collections;
+import javax.lang.model.element.Modifier;
+
+import org.netbeans.api.java.source.*;
+import static org.netbeans.api.java.source.JavaSource.*;
+import org.netbeans.junit.NbTestSuite;
+
+/**
+ *
+ * @author Pavel Flaska
+ */
+public class TopLevelTest extends GeneratorTestMDRCompat {
+    
+    public TopLevelTest(String name) {
+        super(name);
+    }
+    
+    public static NbTestSuite suite() {
+        NbTestSuite suite = new NbTestSuite();
+        suite.addTestSuite(TopLevelTest.class);
+//        suite.addTest(new TopLevelTest(""));
+        return suite;
+    }
+    
+    public void testAddFirstTopLevel0() throws Exception {
+        testFile = new File(getWorkDir(), "Test.java");
+        TestUtilities.copyStringToFile(testFile, 
+            "package hierbas.del.litoral;\n" +
+            "\n");
+        String golden =
+            "package hierbas.del.litoral;\n" +
+            "\n" +
+            "public class Test {\n" +
+            "}\n";
+
+        JavaSource src = getJavaSource(testFile);
+        CancellableTask task = new CancellableTask<WorkingCopy>() {
+
+            public void run(WorkingCopy workingCopy) throws IOException {
+                workingCopy.toPhase(Phase.RESOLVED);
+                CompilationUnitTree cut = workingCopy.getCompilationUnit();
+                TreeMaker make = workingCopy.getTreeMaker();
+                ClassTree clazz = make.Class(
+                    make.Modifiers(Collections.<Modifier>singleton(Modifier.PUBLIC)),
+                    "Test",
+                    Collections.<TypeParameterTree>emptyList(),
+                    null,
+                    Collections.<Tree>emptyList(),
+                    Collections.<Tree>emptyList()
+                );
+                workingCopy.rewrite(cut, make.addCompUnitTypeDecl(cut, clazz));
+            }
+            
+            public void cancel() {
+            }
+        };
+        src.runModificationTask(task).commit();
+        String res = TestUtilities.copyFileToString(testFile);
+        System.err.println(res);
+        assertEquals(golden, res);
+    }
+    
+    public void testAddFirstTopLevel1() throws Exception {
+        testFile = new File(getWorkDir(), "Test.java");
+        TestUtilities.copyStringToFile(testFile, 
+            "package hierbas.del.litoral;\n");
+        String golden =
+            "package hierbas.del.litoral;\n" +
+            "\n" +
+            "public class Test {\n" +
+            "}\n";
+
+        JavaSource src = getJavaSource(testFile);
+        CancellableTask task = new CancellableTask<WorkingCopy>() {
+
+            public void run(WorkingCopy workingCopy) throws IOException {
+                workingCopy.toPhase(Phase.RESOLVED);
+                CompilationUnitTree cut = workingCopy.getCompilationUnit();
+                TreeMaker make = workingCopy.getTreeMaker();
+                ClassTree clazz = make.Class(
+                    make.Modifiers(Collections.<Modifier>singleton(Modifier.PUBLIC)),
+                    "Test",
+                    Collections.<TypeParameterTree>emptyList(),
+                    null,
+                    Collections.<Tree>emptyList(),
+                    Collections.<Tree>emptyList()
+                );
+                workingCopy.rewrite(cut, make.addCompUnitTypeDecl(cut, clazz));
+            }
+            
+            public void cancel() {
+            }
+        };
+        src.runModificationTask(task).commit();
+        String res = TestUtilities.copyFileToString(testFile);
+        System.err.println(res);
+        assertEquals(golden, res);
+    }
+    
+    public void testAddFirstTopLevel2() throws Exception {
+        testFile = new File(getWorkDir(), "Test.java");
+        TestUtilities.copyStringToFile(testFile, 
+            "package hierbas.del.litoral;");
+        String golden =
+            "package hierbas.del.litoral;\n" +
+            "\n" +
+            "public class Test {\n" +
+            "}\n";
+
+        JavaSource src = getJavaSource(testFile);
+        CancellableTask task = new CancellableTask<WorkingCopy>() {
+
+            public void run(WorkingCopy workingCopy) throws IOException {
+                workingCopy.toPhase(Phase.RESOLVED);
+                CompilationUnitTree cut = workingCopy.getCompilationUnit();
+                TreeMaker make = workingCopy.getTreeMaker();
+                ClassTree clazz = make.Class(
+                    make.Modifiers(Collections.<Modifier>singleton(Modifier.PUBLIC)),
+                    "Test",
+                    Collections.<TypeParameterTree>emptyList(),
+                    null,
+                    Collections.<Tree>emptyList(),
+                    Collections.<Tree>emptyList()
+                );
+                workingCopy.rewrite(cut, make.addCompUnitTypeDecl(cut, clazz));
+            }
+            
+            public void cancel() {
+            }
+        };
+        src.runModificationTask(task).commit();
+        String res = TestUtilities.copyFileToString(testFile);
+        System.err.println(res);
+        assertEquals(golden, res);
+    }
+
+    String getGoldenPckg() {
+        return "";
+    }
+
+    String getSourcePckg() {
+        return "";
+    }
+    
+}
