91572c4e0edf544f7de4854d10836a08bc45cdc8
bmay <bmay@netbeans.org>
2007-04-13 19:48:22 +0000

    no message

--- iep.help/javahelp/org/netbeans/modules/iep/help/help.hs
+++ iep.help/javahelp/org/netbeans/modules/iep/help/help.hs
@@ -1,15 +1,11 @@
 <?xml version='1.0' encoding='ISO-8859-1'?>
-<!--
-*     Copyright © 2007 Sun Microsystems, Inc. All rights reserved.
-*     Use is subject to license terms.
---> 
 <!DOCTYPE helpset PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN"
                          "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
 
 <helpset version="2.0">
 	<title>Intelligent Event Processor Help</title>
 	<maps>
-		<homeID></homeID>
+		<homeID>iep_aboutprojects</homeID>
 		<mapref location="help-map.jhm"/>
 	</maps>
 	<view mergetype="javax.help.AppendMerge">
