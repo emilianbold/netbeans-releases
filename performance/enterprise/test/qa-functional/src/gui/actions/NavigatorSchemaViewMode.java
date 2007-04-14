aa44b3a6d40a3cdb7a9ece4930da56a95806dda7
mmirilovic <mmirilovic@netbeans.org>
2007-04-14 21:04:54 +0000

    fix not compilable test ;(

--- performance/enterprise/test/qa-functional/src/gui/actions/NavigatorSchemaViewMode.java
+++ performance/enterprise/test/qa-functional/src/gui/actions/NavigatorSchemaViewMode.java
@@ -5,7 +5,7 @@
  *
  * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
  * or http://www.netbeans.org/cddl.txt.
-
+ 
  * When distributing Covered Code, include this CDDL Header Notice in each file
  * and include the License file at http://www.netbeans.org/cddl.txt.
  * If applicable, add the following below the CDDL Header, with the fields
@@ -19,93 +19,70 @@
 
 package gui.actions;
 
+import gui.EPUtilities;
 import org.netbeans.jellytools.ProjectsTabOperator;
 import org.netbeans.jellytools.TopComponentOperator;
 import org.netbeans.jellytools.nodes.Node;
-import org.netbeans.jemmy.EventTool;
+import org.netbeans.jellytools.actions.CloseAllDocumentsAction;
+
 import org.netbeans.jemmy.operators.ComponentOperator;
 import org.netbeans.jemmy.operators.JComboBoxOperator;
-import org.netbeans.junit.ide.ProjectSupport;
-import org.netbeans.jellytools.actions.CloseAllDocumentsAction;
-import gui.EPUtilities;
-import java.io.File;
+
 /**
  *
- * @author  rashid@netbeans.org
+ * @author  rashid@netbeans.org, mmirilovic@netbeans.org
  */
 public class NavigatorSchemaViewMode  extends org.netbeans.performance.test.utilities.PerformanceTestCase {
     
-  private SchemaOperator tco;
-  private Node pNode; 
-  private Node doc;
-  private Node doc1;
-
+    private Node processNode; 
+    
     /** Creates a new instance of SchemaDesignView */
     public NavigatorSchemaViewMode(String testName) {
         super(testName);
         expectedTime = UI_RESPONSE;
-     //   WAIT_AFTER_OPEN=4000; 
     }
+    
     public NavigatorSchemaViewMode(String testName, String performanceDataName) {
         super(testName, performanceDataName);
         expectedTime = UI_RESPONSE;
     }
-//    public void testNavigatorSchemaViewMode() {
-//        doMeasurement();
-//    }
-
-      protected void initialize() {
+    
+    protected void initialize() {
         log(":: initialize");
-        ProjectSupport.openProject(System.getProperty("xtest.tmpdir")+File.separator+testProjectName);
         new CloseAllDocumentsAction().performAPI();
-       Node pNode = new ProjectsTabOperator().getProjectRootNode(testProjectName);
-        Node doc1 = new Node(pNode,"Process Files"+"|"+testSchemaName1+".xsd");
+        
+        processNode = EPUtilities.getProcessFilesNode("SOATestProject");
+        Node doc1 = new Node(processNode,"batch.xsd");
         doc1.select();
-     
-       SchemaOperator tco = new SchemaOperator(testSchemaName1+".xsd");
-       JComboBoxOperator NavCombo = new JComboBoxOperator(tco,0); 
-             NavCombo.selectItem("Schema View");
+        
+        JComboBoxOperator combo = new JComboBoxOperator(new TopComponentOperator("Navigator")); // NOI18N
+        combo.selectItem("Schema View"); // NOI18N
     }
-
+    
     public void prepare() {
-     log(":: prepare");
-      
+        log(":: prepare");
     }
-
+    
     public ComponentOperator open() {
         log(":: open");
-    Node pNode = new ProjectsTabOperator().getProjectRootNode(testProjectName);
-        Node doc = new Node(pNode,"Process Files"+"|"+testSchemaName+".xsd");
+        Node doc = new Node(processNode,"fields.xsd");
         doc.select();
-      
-      return new SchemaOperator(testSchemaName+".xsd");
-
+        
+        return new TopComponentOperator("fields.xsd");
     }
-
-   
-
+    
     public void close() {
         log("::close");
-       Node pNode = new ProjectsTabOperator().getProjectRootNode(testProjectName);
-        Node doc1 = new Node(pNode,"Process Files"+"|"+testSchemaName1+".xsd");
+        Node doc1 = new Node(processNode,"batch.xsd");
         doc1.select();
- 
     }
-   
-
+    
     protected void shutdown() {
-        ProjectSupport.closeProject(testProjectName);
-
+        log(":: shutdown");
     }
+    
     public static void main(String[] args) {
         junit.textui.TestRunner.run(new NavigatorSchemaViewMode("measureTime"));
     }
     
-
-    private static String testProjectName = "SOATestProject";
-    private static String testSchemaName = "fields";  
-    private static String testSchemaName1 = "batch"; 
 }
-
-
-
