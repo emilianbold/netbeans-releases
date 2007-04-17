e1d706af7b3e13550e6f92b32ac1ccf9b2242636
mzlamal <mzlamal@netbeans.org>
2007-04-17 12:05:00 +0000

    Fix the chdir

--- nbbuild/newbuild/build-nbi.sh
+++ nbbuild/newbuild/build-nbi.sh
@@ -1,40 +1,13 @@
 #!/bin/bash
 set -x
 
-init() 
-{
-   if [ -z $CVS_STAMP ]; then
-       CVS_STAMP="00:00UTC today"
-       export DATESTAMP=`date -u +%Y%m%d0000`
-   fi
-
-   if [ -z $BASE_DIR ]; then
-       echo BASE_DIR variable not defined, using the default one: /space/NB-IDE
-       echo if you want to use another base dir for the whole build feel free
-       echo to define a BASE_DIR variable in your environment
-
-       export BASE_DIR=/space/NB-IDE
-   fi
-
-   if [ -z $DIST_SERVER ]; then
-       echo DIST_SERVER not defined: Upload will no work
-   fi
-
-   if [ -z $DIST_SERVER_PATH ]; then
-       echo DIST_SERVER_PATH not defined using default
-       DIST_SERVER_PATH=/releng/www/netbeans/6.0/nightly
-   fi
-
-   DIST=$BASE_DIR/dist
-   LOGS=$DIST/logs
-   BASENAME=netbeans-$BUILDNUM
-   INSTALLERS_LOG=$LOGS/$BASENAME-installers.log
-}
-init
+DIRNAME=`dirname $0`
+cd ${DIRNAME}
+source init.sh
 
 cd $BASE_DIR
 cvs -d :pserver:anoncvs@cvs.netbeans.org:/cvs checkout -D "$CVS_STAMP" -PA -d NBI installer/infra/build
 
-cd installer/infra/build
+cd NBI
 
 bash build.sh > $INSTALLERS_LOG 2>&1 
