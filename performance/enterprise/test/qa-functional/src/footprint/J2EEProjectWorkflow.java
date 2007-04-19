e07ead726ca8c73afdf02c20e251b1afa840ec0b
mmirilovic <mmirilovic@netbeans.org>
2007-04-19 09:00:25 +0000

    small fixed

--- performance/enterprise/test/qa-functional/src/footprint/J2EEProjectWorkflow.java
+++ performance/enterprise/test/qa-functional/src/footprint/J2EEProjectWorkflow.java
@@ -78,7 +78,7 @@ public class J2EEProjectWorkflow extends org.netbeans.performance.test.utilities
         j2eeproject_war = j2eeproject + "-war";
         j2eeproject_app = j2eeproject + "-app-client";
         
-// TODO temporary disabled - no index.jsp is created in War project        
+// TODO temporary disabled - no index.jsp is created in War project - IZ 101564     
 //        EPFootprintUtilities.openFile(new Node(new ProjectsTabOperator().getProjectRootNode(j2eeproject_war), EPFootprintUtilities.WEB_PAGES + "|index.jsp"),"index.jsp", true);
 //        EPFootprintUtilities.insertToFile("index.jsp", 23, "Hello World", true);
         
