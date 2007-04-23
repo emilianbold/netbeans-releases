6e6fd99cbf914e87320130efd2fb13f3305cbea6
dkaspar <dkaspar@netbeans.org>
2007-04-23 11:57:08 +0000

    MIDP: ImageItem.image injector

---
+++ vmd.midp/src/org/netbeans/modules/vmd/midp/screen/display/injector/ImageItemInjectorPresenter.java
@@ -0,0 +1,51 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ *
+ */
+
+package org.netbeans.modules.vmd.midp.screen.display.injector;
+
+import org.netbeans.modules.vmd.api.model.PropertyValue;
+import org.netbeans.modules.vmd.api.properties.common.PropertiesSupport;
+import org.netbeans.modules.vmd.api.screen.display.injector.ScreenButtonInjectorPresenter;
+import org.netbeans.modules.vmd.midp.components.items.ImageItemCD;
+
+import javax.swing.*;
+
+/**
+ * @author David Kaspar
+ */
+public class ImageItemInjectorPresenter extends ScreenButtonInjectorPresenter.Static {
+
+    public ImageItemInjectorPresenter () {
+        super ("Assign a new Image", null, 10);
+    }
+
+    public boolean isEnabled () {
+        return getComponent ().readProperty (ImageItemCD.PROP_IMAGE) == PropertyValue.createNull ();
+    }
+
+    protected void actionPerformed () {
+        SwingUtilities.invokeLater (new Runnable() {
+            public void run () {
+                PropertiesSupport.showPropertyEditorForCurrentComponent (getComponent (), ImageItemCD.PROP_IMAGE);
+            }
+        });
+    }
+
+}
