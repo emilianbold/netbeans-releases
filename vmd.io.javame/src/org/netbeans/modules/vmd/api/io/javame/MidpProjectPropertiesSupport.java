7428b95b719a4ae5e2616ca0ac7918908bad4326
dkaspar <dkaspar@netbeans.org>
2007-04-24 15:17:16 +0000

    IOJavaME: minor fix

--- vmd.io.javame/src/org/netbeans/modules/vmd/api/io/javame/MidpProjectPropertiesSupport.java
+++ vmd.io.javame/src/org/netbeans/modules/vmd/api/io/javame/MidpProjectPropertiesSupport.java
@@ -39,8 +39,8 @@ public final class MidpProjectPropertiesSupport {
         return MidpProjectPropertiesSupportImpl.evaluateProperty (ep, propertyName, configuration);
     }
 
-    public static void setProperty (EditableProperties ep, String propertyName, String configuration) {
-        MidpProjectPropertiesSupportImpl.setProperty (ep, propertyName, configuration);
+    public static void setProperty (EditableProperties ep, String propertyName, String configuration, String propertyValue) {
+        MidpProjectPropertiesSupportImpl.setProperty (ep, propertyName, configuration, propertyValue);
     }
 
     public static void addDeviceListener (DataObjectContext context, DeviceListener listener) {
