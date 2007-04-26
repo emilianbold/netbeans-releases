7e2187ec012e778f307db09e9f3ac82893c0aadf
dlipin <dlipin@netbeans.org>
2007-04-26 09:40:50 +0000

    Add Portlet container to the supported products

---
+++ installer/components/products/portletcontainer/src/org/netbeans/installer/products/portletcontainer/ConfigurationLogic.java
@@ -0,0 +1,131 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance
+ * with the License.
+ * 
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html or
+ * http://www.netbeans.org/cddl.txt.
+ * 
+ * When distributing Covered Code, include this CDDL Header Notice in each file and
+ * include the License file at http://www.netbeans.org/cddl.txt. If applicable, add
+ * the following below the CDDL Header, with the fields enclosed by brackets []
+ * replaced by your own identifying information:
+ * 
+ *     "Portions Copyrighted [year] [name of copyright owner]"
+ * 
+ * The Original Software is NetBeans. The Initial Developer of the Original Software
+ * is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun Microsystems, Inc. All
+ * Rights Reserved.
+ */
+package org.netbeans.installer.products.portletcontainer;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import org.netbeans.installer.utils.applications.GlassFishUtils;
+import org.netbeans.installer.product.Registry;
+import org.netbeans.installer.product.components.Product;
+import org.netbeans.installer.product.components.ProductConfigurationLogic;
+import org.netbeans.installer.utils.FileProxy;
+import org.netbeans.installer.utils.SystemUtils;
+import org.netbeans.installer.utils.applications.JavaUtils;
+import org.netbeans.installer.utils.exceptions.InitializationException;
+import org.netbeans.installer.utils.exceptions.InstallationException;
+import org.netbeans.installer.utils.exceptions.UninstallationException;
+import org.netbeans.installer.utils.helper.Dependency;
+import org.netbeans.installer.utils.progress.Progress;
+import org.netbeans.installer.wizard.Wizard;
+import org.netbeans.installer.wizard.components.WizardComponent;
+
+/**
+ *
+ * @author Kirill Sorokin
+ */
+public class ConfigurationLogic extends ProductConfigurationLogic {
+    /////////////////////////////////////////////////////////////////////////////////
+    // Constants
+    public static final String WIZARD_COMPONENTS_URI =
+	    FileProxy.RESOURCE_SCHEME_PREFIX + 
+            "org/netbeans/installer/products/portletcontainer/wizard.xml"; // NOI18N
+    
+    private static final String GLASSFISH_UID =
+            "glassfish"; // NOI18N
+    private static final String APPSERVER_UID =
+            "sjsas"; // NOI18N
+    
+    private static final String PC_INSTALLER =
+            "portlet-container-configurator.jar"; // NOI18N
+    
+    /////////////////////////////////////////////////////////////////////////////////
+    // Instance
+    private List<WizardComponent> wizardComponents;
+    
+    public ConfigurationLogic() throws InitializationException {
+        wizardComponents = Wizard.loadWizardComponents(
+                WIZARD_COMPONENTS_URI,
+                getClass().getClassLoader());
+    }
+    
+    public void install(Progress progress) throws InstallationException {
+        final File pcLocation = getProduct().getInstallationLocation();
+        
+        // get the list of suitable glassfish installations
+        final List<Dependency> dependencies = 
+                getProduct().getDependencyByUid(APPSERVER_UID);
+        final List<Product> sources = 
+                Registry.getInstance().getProducts(dependencies.get(0));
+        
+        // pick the first one and integrate with it
+        final File asLocation = sources.get(0).getInstallationLocation();
+        final File pcInstaller = new File(pcLocation, PC_INSTALLER);
+       
+        // resolve the dependency
+        dependencies.get(0).setVersionResolved(sources.get(0).getVersion());
+        
+        // stop the default domain //////////////////////////////////////////////////
+        try {
+            progress.setDetail(getString("CL.install.stop.as")); // NOI18N
+            
+            GlassFishUtils.stopDefaultDomain(asLocation);
+        } catch (IOException e) {
+            throw new InstallationException(
+                    getString("CL.install.error.stop.as"),  // NOI18N
+                    e);
+        }
+
+        // run the pc installer ////////////////////////////////////////////////
+        try {
+            progress.setDetail(getString("CL.install.portletcontainer.installer")); // NOI18N
+            
+            File javaExecutable = JavaUtils.getExecutable(
+                    GlassFishUtils.getJavaHome(asLocation));
+            
+            SystemUtils.executeCommand(asLocation,
+                    javaExecutable.getAbsolutePath(),
+                    "-jar",
+                    pcInstaller.getAbsolutePath(),
+                    asLocation.getAbsolutePath(),
+                    asLocation.getAbsolutePath() + File.separator + 
+                    "domains" +  File.separator + 
+                    GlassFishUtils.DEFAULT_DOMAIN);
+        } catch (IOException e) {
+            throw new InstallationException(
+                    getString("CL.install.error.portletcontainer.installer"),  // NOI18N
+                    e);
+        }
+        
+        /////////////////////////////////////////////////////////////////////////////
+        progress.setPercentage(Progress.COMPLETE);
+    }
+
+    public void uninstall(final Progress progress) throws UninstallationException {
+    }
+
+    public List<WizardComponent> getWizardComponents() {
+        return wizardComponents;
+    }
+    
+    public boolean registerInSystem() {
+        return false;
+    }
+}
