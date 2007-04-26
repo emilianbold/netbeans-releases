f77be95c27c84f5aa1da9c69ca74802f07edcfc5
dlipin <dlipin@netbeans.org>
2007-04-26 16:32:30 +0000

    Add javaeesdk blueprints

---
+++ installer/components/products/javaeesdk-blueprints/src/org/netbeans/installer/products/javaeesdk/blueprints/ConfigurationLogic.java
@@ -0,0 +1,79 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance
+ * with the License.
+ * 
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html or
+ * http://www.netbeans.org/cddl.txt.
+ * 
+ * When distributing Covered Code, include this CDDL Header Notice in each file and
+ * include the License file at http://www.netbeans.org/cddl.txt. If applicable, add
+ * the following below the CDDL Header, with the fields enclosed by brackets []
+ * replaced by your own identifying information:
+ * 
+ *     "Portions Copyrighted [year] [name of copyright owner]"
+ * 
+ * The Original Software is NetBeans. The Initial Developer of the Original Software
+ * is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun Microsystems, Inc. All
+ * Rights Reserved.
+ */
+package org.netbeans.installer.products.javaeesdk.blueprints;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import org.netbeans.installer.utils.applications.GlassFishUtils;
+import org.netbeans.installer.product.Registry;
+import org.netbeans.installer.product.components.Product;
+import org.netbeans.installer.product.components.ProductConfigurationLogic;
+import org.netbeans.installer.utils.FileProxy;
+import org.netbeans.installer.utils.SystemUtils;
+import org.netbeans.installer.utils.applications.JavaUtils;
+import org.netbeans.installer.utils.exceptions.InitializationException;
+import org.netbeans.installer.utils.exceptions.InstallationException;
+import org.netbeans.installer.utils.exceptions.UninstallationException;
+import org.netbeans.installer.utils.helper.Dependency;
+import org.netbeans.installer.utils.progress.Progress;
+import org.netbeans.installer.wizard.Wizard;
+import org.netbeans.installer.wizard.components.WizardComponent;
+
+/**
+ *
+ * @author Dmitry Lipin
+ */
+public class ConfigurationLogic extends ProductConfigurationLogic {
+    /////////////////////////////////////////////////////////////////////////////////
+    // Constants
+    public static final String WIZARD_COMPONENTS_URI =
+	    FileProxy.RESOURCE_SCHEME_PREFIX + 
+            "org/netbeans/installer/products/javaeesdk/blueprints/wizard.xml"; // NOI18N
+    
+    private static final String GLASSFISH_UID =
+            "glassfish"; // NOI18N
+    private static final String APPSERVER_UID =
+            "sjsas"; // NOI18N
+    
+    /////////////////////////////////////////////////////////////////////////////////
+    // Instance
+    private List<WizardComponent> wizardComponents;
+    
+    public ConfigurationLogic() throws InitializationException {
+        wizardComponents = Wizard.loadWizardComponents(
+                WIZARD_COMPONENTS_URI,
+                getClass().getClassLoader());
+    }
+    
+    public void install(Progress progress) throws InstallationException {
+    }
+
+    public void uninstall(final Progress progress) throws UninstallationException {
+    }
+
+    public List<WizardComponent> getWizardComponents() {
+        return wizardComponents;
+    }
+    
+    public boolean registerInSystem() {
+        return false;
+    }
+}
---
+++ installer/infra/build/.ant-lib/src/org/netbeans/installer/infra/build/ant/BuildNumberBluePrints.java
@@ -0,0 +1,147 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance
+ * with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html or
+ * http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file and
+ * include the License file at http://www.netbeans.org/cddl.txt. If applicable, add
+ * the following below the CDDL Header, with the fields enclosed by brackets []
+ * replaced by your own identifying information:
+ *
+ *     "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original Software
+ * is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun Microsystems, Inc. All
+ * Rights Reserved.
+ */
+package org.netbeans.installer.infra.build.ant;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Task;
+import org.netbeans.installer.infra.build.ant.utils.Utils;
+
+/**
+ * This class is an ant task which parses a given input file and devises the
+ * glassfish milestone number, build type (ea, beta, rc, etc) and build number from
+ * it.
+ *
+ * @author Dmitry Lipin
+ */
+public class BuildNumberBluePrints extends Task {
+    /////////////////////////////////////////////////////////////////////////////////
+    // Instance
+    /**
+     * The input file.
+     */
+    private File file;
+    
+    /**
+     * The properties' names prefix.
+     */
+    private String prefix;
+    
+    // setters //////////////////////////////////////////////////////////////////////
+    /**
+     * Setter for the <code>file</code> attribute.
+     *
+     * @param path The value of the <code>file</code> attribute.
+     */
+    public void setFile(String path) {
+        this.file = new File(path);
+    }
+    
+    /**
+     * Setter for the <code>prefix</code> attribute.
+     *
+     * @param prefix The value of the <code>prefix</code> attribute.
+     */
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+    
+    // execution ////////////////////////////////////////////////////////////////////
+    /**
+     * Executes the task. The input file is parsed and three properties identifying
+     * the glassfish build are set.
+     *
+     * @throws org.apache.tools.ant.BuildException if the input file cannot be
+     *      parsed for whatever reason.
+     */
+    @Override
+    public void execute() throws BuildException {
+        try {
+            final FileInputStream in = new FileInputStream(file);
+            final CharSequence contents = Utils.read(in);
+            
+            in.close();
+            
+            final Matcher matcher = PATTERN.matcher(contents);
+            
+            if (matcher.find()) {
+                final String milestoneNumber =
+                        matcher.group(1);                              // NOMAGI
+                
+                final String buildNumber = FORMAT_OUT.format(
+                        FORMAT_IN.parse(matcher.group(2))); // NOMAGI
+                
+                getProject().setProperty(
+                        prefix + MILESTONE_NUMBER_SUFFIX,
+                        milestoneNumber);
+                getProject().setProperty(
+                        prefix + BUILD_NUMBER_SUFFIX,
+                        buildNumber);
+            } else {
+                throw new BuildException(
+                        "Cannot parse the input file.");                    // NOI18N
+            }
+        } catch (IOException e) {
+            throw new BuildException(e);
+        } catch (ParseException e) {
+            throw new BuildException(e);
+        }
+    }
+    
+    /////////////////////////////////////////////////////////////////////////////////
+    // Constants
+    /**
+     * Pattern for which to look in the input file.
+     */
+    private static final Pattern PATTERN = Pattern.compile(
+            "java_ee_sdk-5_03-preview2-bin-b([0-9]+)-([A-Za-z0-9_]+).zip");//NOI18N
+    
+    /**
+     * Date format used in the input file.
+     */
+    private static final DateFormat FORMAT_IN =
+            new SimpleDateFormat("dd_MMM_yyyy", Locale.US);                 // NOI18N
+    
+    /**
+     * Date format to use in the output properties.
+     */
+    private static final DateFormat FORMAT_OUT =
+            new SimpleDateFormat("yyyyMMdd", Locale.US);                    // NOI18N
+    
+    /**
+     * Milestone number property suffix.
+     */
+    private static final String MILESTONE_NUMBER_SUFFIX =
+            ".milestone.number";                                            // NOI18N
+    
+    /**
+     * Build number property suffix.
+     */
+    private static final String BUILD_NUMBER_SUFFIX =
+            ".build.number";                                                // NOI18N
+}
