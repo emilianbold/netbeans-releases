7371da1a917b061d326d434058498603230b73a0
rcruz <rcruz@netbeans.org>
2007-04-27 23:47:11 +0000

    Fix IZ 102424: xendorsed flag in wsimport disappears after regeneration of build-impl.xml--Apply fix to App Client

--- j2ee.clientproject/src/org/netbeans/modules/j2ee/clientproject/wsclient/AppClientProjectJAXWSVersionProvider.java
+++ j2ee.clientproject/src/org/netbeans/modules/j2ee/clientproject/wsclient/AppClientProjectJAXWSVersionProvider.java
@@ -11,6 +11,7 @@ package org.netbeans.modules.j2ee.clientproject.wsclient;
 
 import java.io.File;
 import java.util.Map;
+import org.netbeans.modules.j2ee.clientproject.ui.customizer.AppClientProjectProperties;
 import org.netbeans.modules.j2ee.deployment.devmodules.api.Deployment;
 import org.netbeans.modules.j2ee.deployment.devmodules.api.J2eePlatform;
 import org.netbeans.modules.websvc.api.jaxws.project.JAXWSVersionProvider;
@@ -30,10 +31,19 @@ public class AppClientProjectJAXWSVersionProvider implements JAXWSVersionProvide
     }
     
     public String getJAXWSVersion(){
-         File appSvrRoot = null;
+        File appSvrRoot = null;
         Map properties = h.getStandardPropertyEvaluator().getProperties();
         if(properties != null){
             String serverInstance = (String)properties.get("j2ee.server.instance"); //NOI18N
+            if(serverInstance == null){
+                String serverType = h.getProperties(AntProjectHelper.PROJECT_PROPERTIES_PATH).getProperty(AppClientProjectProperties.J2EE_SERVER_TYPE);
+                if (serverType != null) {
+                    String[] servInstIDs = Deployment.getDefault().getInstancesOfServer(serverType);
+                    if (servInstIDs.length > 0) {
+                        serverInstance = servInstIDs[0];
+                    }
+                }
+            }
             if (serverInstance != null) {
                 J2eePlatform j2eePlatform = Deployment.getDefault().getJ2eePlatform(serverInstance);
                 if (j2eePlatform != null) {
