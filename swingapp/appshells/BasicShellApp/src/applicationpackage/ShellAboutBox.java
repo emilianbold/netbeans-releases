9949b467a90b63a69d5e3955a2f073bdde7e476f
tpavek <tpavek@netbeans.org>
2007-05-04 00:53:36 +0000

    another update of app templates, getting rid of toolbars, will bring back with a new document-like template

--- swingapp/appshells/BasicShellApp/src/applicationpackage/ShellAboutBox.java
+++ swingapp/appshells/BasicShellApp/src/applicationpackage/ShellAboutBox.java
@@ -28,35 +28,90 @@ public class ShellAboutBox extends javax.swing.JDialog {
     // <editor-fold defaultstate="collapsed" desc=" Generated Code ">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        aboutLabel = new javax.swing.JLabel();
         closeButton = new javax.swing.JButton();
+        javax.swing.JLabel appTitleLabel = new javax.swing.JLabel();
+        javax.swing.JLabel versionLabel = new javax.swing.JLabel();
+        javax.swing.JLabel appVersionLabel = new javax.swing.JLabel();
+        javax.swing.JLabel vendorLabel = new javax.swing.JLabel();
+        javax.swing.JLabel appVendorLabel = new javax.swing.JLabel();
+        javax.swing.JLabel homepageLabel = new javax.swing.JLabel();
+        javax.swing.JLabel appHomepageLabel = new javax.swing.JLabel();
+        javax.swing.JLabel appDescLabel = new javax.swing.JLabel();
+        javax.swing.JLabel imageLabel = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle(application.ApplicationContext.getInstance().getResourceMap(applicationpackage.ShellAboutBox.class).getString("title")); // NOI18N
         setName("aboutBox"); // NOI18N
         setResizable(false);
 
-        aboutLabel.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
-        aboutLabel.setIcon(application.ApplicationContext.getInstance().getResourceMap(applicationpackage.ShellAboutBox.class).getIcon("aboutLabel.icon")); // NOI18N
-        aboutLabel.setVerticalAlignment(javax.swing.SwingConstants.TOP);
+        closeButton.setAction(application.ApplicationContext.getInstance().getActionMap(ShellAboutBox.class, this).get("closeAboutBox"));
 
-        closeButton.setAction(application.ApplicationContext.getInstance().getActionMap(applicationpackage.ShellAboutBox.class, this).get("closeAboutBox"));
+        appTitleLabel.setFont(appTitleLabel.getFont().deriveFont(appTitleLabel.getFont().getStyle() | java.awt.Font.BOLD, appTitleLabel.getFont().getSize()+4));
+        appTitleLabel.setText(application.ApplicationContext.getInstance().getResourceMap(applicationpackage.ShellAboutBox.class).getString("Application.title")); // NOI18N
+
+        versionLabel.setFont(versionLabel.getFont().deriveFont(versionLabel.getFont().getStyle() | java.awt.Font.BOLD));
+        versionLabel.setText(application.ApplicationContext.getInstance().getResourceMap(applicationpackage.ShellAboutBox.class).getString("versionLabel.text")); // NOI18N
+
+        appVersionLabel.setText(application.ApplicationContext.getInstance().getResourceMap(applicationpackage.ShellAboutBox.class).getString("Application.version")); // NOI18N
+
+        vendorLabel.setFont(vendorLabel.getFont().deriveFont(vendorLabel.getFont().getStyle() | java.awt.Font.BOLD));
+        vendorLabel.setText(application.ApplicationContext.getInstance().getResourceMap(applicationpackage.ShellAboutBox.class).getString("vendorLabel.text")); // NOI18N
+
+        appVendorLabel.setText(application.ApplicationContext.getInstance().getResourceMap(applicationpackage.ShellAboutBox.class).getString("Application.vendor")); // NOI18N
+
+        homepageLabel.setFont(homepageLabel.getFont().deriveFont(homepageLabel.getFont().getStyle() | java.awt.Font.BOLD));
+        homepageLabel.setText(application.ApplicationContext.getInstance().getResourceMap(applicationpackage.ShellAboutBox.class).getString("homepageLabel.text")); // NOI18N
+
+        appHomepageLabel.setText(application.ApplicationContext.getInstance().getResourceMap(applicationpackage.ShellAboutBox.class).getString("Application.homepage")); // NOI18N
+
+        appDescLabel.setText(application.ApplicationContext.getInstance().getResourceMap(applicationpackage.ShellAboutBox.class).getString("appDescLabel.text")); // NOI18N
+
+        imageLabel.setIcon(application.ApplicationContext.getInstance().getResourceMap(applicationpackage.ShellAboutBox.class).getIcon("imageLabel.icon")); // NOI18N
 
         org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-            .add(aboutLabel, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 375, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-            .add(org.jdesktop.layout.GroupLayout.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(304, Short.MAX_VALUE)
-                .add(closeButton)
+            .add(layout.createSequentialGroup()
+                .add(imageLabel)
+                .add(18, 18, 18)
+                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.TRAILING)
+                    .add(org.jdesktop.layout.GroupLayout.LEADING, layout.createSequentialGroup()
+                        .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                            .add(versionLabel)
+                            .add(vendorLabel)
+                            .add(homepageLabel))
+                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                        .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                            .add(appVersionLabel)
+                            .add(appVendorLabel)
+                            .add(appHomepageLabel)))
+                    .add(org.jdesktop.layout.GroupLayout.LEADING, appTitleLabel)
+                    .add(org.jdesktop.layout.GroupLayout.LEADING, appDescLabel, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 266, Short.MAX_VALUE)
+                    .add(closeButton))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(imageLabel, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .add(layout.createSequentialGroup()
-                .add(aboutLabel, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 283, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, 14, Short.MAX_VALUE)
+                .addContainerGap()
+                .add(appTitleLabel)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(appDescLabel)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
+                    .add(versionLabel)
+                    .add(appVersionLabel))
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
+                    .add(vendorLabel)
+                    .add(appVendorLabel))
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
+                    .add(homepageLabel)
+                    .add(appHomepageLabel))
+                .add(19, 19, Short.MAX_VALUE)
                 .add(closeButton)
                 .addContainerGap())
         );
@@ -65,7 +120,6 @@ public class ShellAboutBox extends javax.swing.JDialog {
     }// </editor-fold>//GEN-END:initComponents
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel aboutLabel;
     private javax.swing.JButton closeButton;
     // End of variables declaration//GEN-END:variables
     
