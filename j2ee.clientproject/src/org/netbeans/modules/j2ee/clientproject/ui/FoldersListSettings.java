499b1e104dc0f507d58ab201852dfe301bb55c30
jungi <jungi@netbeans.org>
2007-05-06 21:43:04 +0000

    IZ #102083: Get rid of using org.openide.options

--- j2ee.clientproject/src/org/netbeans/modules/j2ee/clientproject/ui/FoldersListSettings.java
+++ j2ee.clientproject/src/org/netbeans/modules/j2ee/clientproject/ui/FoldersListSettings.java
@@ -19,156 +19,148 @@
 
 package org.netbeans.modules.j2ee.clientproject.ui;
 
-import org.openide.options.SystemOption;
 import org.openide.util.NbBundle;
 
 import java.io.File;
+import java.util.prefs.Preferences;
+import org.openide.util.NbPreferences;
 
 /**
- * Misnamed storage of information application to the new j2seproject wizard.
+ * Misnamed storage of information application to the new appclientproject wizard.
  */
-public class FoldersListSettings extends SystemOption {
+public class FoldersListSettings {
+
+    private static final FoldersListSettings INSTANCE = new FoldersListSettings();
 
     private static final long serialVersionUID = 2386225041150479082L;
 
     private static final String NEW_PROJECT_COUNT = "newProjectCount"; //NOI18N
-    
+
     private static final String NEW_APP_COUNT = "newApplicationCount";  //NOI18N
-    
+
     private static final String NEW_LIB_COUNT = "newLibraryCount"; //NOI18N
 
     private static final String LAST_USED_CP_FOLDER = "lastUsedClassPathFolder";    //NOI18N
 
     private static final String LAST_USED_ARTIFACT_FOLDER = "lastUsedArtifactFolder"; //NOI18N
-    
+
     private static final String AGREED_SET_JDK_14 = "agreeSetJdk14"; // NOI18N
-    
+
     private static final String AGREED_SET_SOURCE_LEVEL_14 = "agreeSetSourceLevel14"; // NOI18N
 
     private static final String AGREED_SET_JDK_15 = "agreeSetJdk15"; // NOI18N
-    
+
     private static final String AGREED_SET_SOURCE_LEVEL_15 = "agreeSetSourceLevel15"; // NOI18N
 
     private static final String SHOW_AGAIN_BROKEN_REF_ALERT = "showAgainBrokenRefAlert"; //NOI18N
-    
+
     private static final String SHOW_AGAIN_BROKEN_SERVER_ALERT = "showAgainBrokenServerAlert"; //NOI18N
-    
+
     private static final String LAST_USED_SERVER = "lastUsedServer"; // NOI18N
 
 
     public static FoldersListSettings getDefault () {
-        return (FoldersListSettings) SystemOption.findObject (FoldersListSettings.class, true);
+        return INSTANCE;
     }
-    
+
     public String displayName() {
         return NbBundle.getMessage(FoldersListSettings.class, "TXT_AppClientProjectFolderList");
     }
 
     public int getNewProjectCount () {
-        Integer value = (Integer) getProperty (NEW_PROJECT_COUNT);
-        return value == null ? 0 : value.intValue();
+        return getPreferences().getInt(NEW_PROJECT_COUNT, 0);
     }
 
     public void setNewProjectCount (int count) {
-        this.putProperty(NEW_PROJECT_COUNT, new Integer(count),true);
+        getPreferences().putInt(NEW_PROJECT_COUNT, count);
     }
-    
+
     public int getNewApplicationCount () {
-        Integer value = (Integer) getProperty (NEW_APP_COUNT);
-        return value == null ? 0 : value.intValue();
+        return getPreferences().getInt(NEW_APP_COUNT, 0);
     }
-    
+
     public void setNewApplicationCount (int count) {
-        this.putProperty(NEW_APP_COUNT, new Integer(count),true);
+        getPreferences().putInt(NEW_APP_COUNT, count);
     }
-    
+
     public int getNewLibraryCount () {
-        Integer value = (Integer) getProperty (NEW_LIB_COUNT);
-        return value == null ? 0 : value.intValue();
+        return getPreferences().getInt(NEW_LIB_COUNT, 0);
     }
-    
+
     public void setNewLibraryCount (int count) {
-        this.putProperty(NEW_LIB_COUNT, new Integer(count),true);
+        getPreferences().putInt(NEW_LIB_COUNT, count);
     }
 
     public File getLastUsedClassPathFolder () {
-        String lucpr = (String) this.getProperty (LAST_USED_CP_FOLDER);
-        if (lucpr == null) {
-            lucpr = System.getProperty("user.home");    //NOI18N
-        }
-        return new File (lucpr);
+        return new File (getPreferences().get(LAST_USED_CP_FOLDER, System.getProperty("user.home")));
     }
 
     public void setLastUsedClassPathFolder (File folder) {
         assert folder != null : "ClassPath root can not be null";
         String path = folder.getAbsolutePath();
-        this.putProperty(LAST_USED_CP_FOLDER, path, true);
+        getPreferences().put(LAST_USED_CP_FOLDER, path);
     }
 
     public File getLastUsedArtifactFolder () {
-        String folder = (String) this.getProperty (LAST_USED_ARTIFACT_FOLDER);
-        if (folder == null) {
-            folder = System.getProperty("user.home");    //NOI18N
-        }
-        return new File (folder);
+        return new File (getPreferences().get(LAST_USED_ARTIFACT_FOLDER, System.getProperty("user.home")));
     }
 
     public void setLastUsedArtifactFolder (File folder) {
         assert folder != null : "Folder can not be null";
         String path = folder.getAbsolutePath();
-        this.putProperty (LAST_USED_ARTIFACT_FOLDER, path, true);
-    }   
+        getPreferences().put(LAST_USED_ARTIFACT_FOLDER, path);
+    }
 
     public boolean isAgreedSetJdk14() {
-        Boolean b = (Boolean)getProperty(AGREED_SET_JDK_14);
-        return b == null ? true : b.booleanValue();
+        return getPreferences().getBoolean(AGREED_SET_JDK_14, true);
     }
-    
+
     public void setAgreedSetJdk14(boolean agreed) {
-        this.putProperty(AGREED_SET_JDK_14, Boolean.valueOf(agreed), true);
+        getPreferences().putBoolean(AGREED_SET_JDK_14, agreed);
     }
-    
+
     public boolean isAgreedSetSourceLevel14() {
-        Boolean b = (Boolean)getProperty(AGREED_SET_SOURCE_LEVEL_14);
-        return b == null ? true : b.booleanValue();
+        return getPreferences().getBoolean(AGREED_SET_SOURCE_LEVEL_14, true);
     }
-    
+
     public void setAgreedSetSourceLevel14(boolean agreed) {
-        this.putProperty(AGREED_SET_SOURCE_LEVEL_14, Boolean.valueOf(agreed), true);
+        getPreferences().putBoolean(AGREED_SET_SOURCE_LEVEL_14, agreed);
     }
 
     public boolean isAgreedSetJdk15() {
-        Boolean b = (Boolean)getProperty(AGREED_SET_JDK_15);
-        return b == null ? true : b.booleanValue();
+        return getPreferences().getBoolean(AGREED_SET_JDK_15, true);
     }
-    
+
     public void setAgreedSetJdk15(boolean agreed) {
-        this.putProperty(AGREED_SET_JDK_15, Boolean.valueOf(agreed), true);
+        getPreferences().putBoolean(AGREED_SET_JDK_15, agreed);
     }
-    
+
     public boolean isAgreedSetSourceLevel15() {
-        Boolean b = (Boolean)getProperty(AGREED_SET_SOURCE_LEVEL_15);
-        return b == null ? true : b.booleanValue();
+        return getPreferences().getBoolean(AGREED_SET_SOURCE_LEVEL_15, true);
     }
-    
+
     public void setAgreedSetSourceLevel15(boolean agreed) {
-        this.putProperty(AGREED_SET_SOURCE_LEVEL_15, Boolean.valueOf(agreed), true);
+        getPreferences().putBoolean(AGREED_SET_SOURCE_LEVEL_15, agreed);
     }
 
     public boolean isShowAgainBrokenServerAlert() {
-        Boolean b = (Boolean)getProperty(SHOW_AGAIN_BROKEN_SERVER_ALERT);
-        return b == null ? true : b.booleanValue();
+        return getPreferences().getBoolean(SHOW_AGAIN_BROKEN_SERVER_ALERT, true);
     }
-    
+
     public void setShowAgainBrokenServerAlert(boolean again) {
-        this.putProperty(SHOW_AGAIN_BROKEN_SERVER_ALERT, Boolean.valueOf(again), true);
+        getPreferences().putBoolean(SHOW_AGAIN_BROKEN_SERVER_ALERT, again);
     }
 
     public void setLastUsedServer(String serverInstanceID) {
-        putProperty(LAST_USED_SERVER, serverInstanceID, true);
+        getPreferences().put(LAST_USED_SERVER, serverInstanceID);
     }
 
     public String getLastUsedServer() {
-        return (String) getProperty(LAST_USED_SERVER);
+        return getPreferences().get(LAST_USED_SERVER, null);
     }
+
+    private static Preferences getPreferences() {
+        return NbPreferences.forModule(FoldersListSettings.class);
+    }
+
 }
