c8f178fc75289a19030e87bf7910ce3f32edf73b
saubrecht <saubrecht@netbeans.org>
2007-05-09 09:19:13 +0000

    dropped leading slash

--- java.editor/src/org/netbeans/modules/java/editor/imports/JavaFixAllImports.java
+++ java.editor/src/org/netbeans/modules/java/editor/imports/JavaFixAllImports.java
@@ -125,7 +125,7 @@ public class JavaFixAllImports {
                             variants[index][0] = NbBundle.getMessage(JavaFixAllImports.class, "FixDupImportStmts_CannotResolve"); //NOI18N
                             defaults[index] = variants[index][0];
                             icons[index] = new Icon[1];
-                            icons[index][0] = new ImageIcon( org.openide.util.Utilities.loadImage("/org/netbeans/modules/java/editor/resources/error-glyph.gif") );//NOI18N
+                            icons[index][0] = new ImageIcon( org.openide.util.Utilities.loadImage("org/netbeans/modules/java/editor/resources/error-glyph.gif") );//NOI18N
                         }
 
                         index++;
