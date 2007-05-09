e69b9b7bc9d189e600ab41d0b0208fe14ebcfc04
tstupka <tstupka@netbeans.org>
2007-05-09 14:52:44 +0000

    103323 - Can't connect to subversion

--- subversion/src/org/netbeans/modules/subversion/util/ProxySettings.java
+++ subversion/src/org/netbeans/modules/subversion/util/ProxySettings.java
@@ -26,18 +26,20 @@ import java.util.prefs.Preferences;
  */
 public class ProxySettings {   
 
-    private static final String PROXY_HTTP_HOST = "proxyHttpHost";
-    private static final String PROXY_HTTP_PORT = "proxyHttpPort";
-    private static final String PROXY_HTTPS_HOST = "proxyHttpsHost";
-    private static final String PROXY_HTTPS_PORT = "proxyHttpsPort";
-    private static final String NOT_PROXY_HOSTS = "proxyNonProxyHosts";
-    private static final String USE_PROXY_AUTHENTICATION = "useProxyAuthentication";
-    private static final String PROXY_AUTHENTICATION_USERNAME = "proxyAuthenticationUsername";
-    private static final String PROXY_AUTHENTICATION_PASSWORD = "proxyAuthenticationPassword";
+    private static final String PROXY_HTTP_HOST                 = "proxyHttpHost";
+    private static final String PROXY_HTTP_PORT                 = "proxyHttpPort";
+    private static final String PROXY_HTTPS_HOST                = "proxyHttpsHost";
+    private static final String PROXY_HTTPS_PORT                = "proxyHttpsPort";
+    private static final String NOT_PROXY_HOSTS                 = "proxyNonProxyHosts";
+    private static final String USE_PROXY_AUTHENTICATION        = "useProxyAuthentication";
+    private static final String PROXY_AUTHENTICATION_USERNAME   = "proxyAuthenticationUsername";
+    private static final String PROXY_AUTHENTICATION_PASSWORD   = "proxyAuthenticationPassword";
     
-    private static final String PROXY_TYPE = "proxyType";
-    private static final String DIRECT_CONNECTION = "0";            
-    
+    private static final String PROXY_TYPE                      = "proxyType";
+    private static final String DIRECT_CONNECTION               = "0";            
+    private static final String AUTO_DETECT_PROXY               = "1"; // as default
+    private static final String MANUAL_SET_PROXY                = "2";
+            
     private String username;
     private String password;
     private String notProxyHosts;
@@ -55,22 +57,50 @@ public class ProxySettings {
     };
         
     private void init() {
-        Preferences prefs = org.openide.util.NbPreferences.root ().node ("org/netbeans/core");                          // NOI18N    
-        useAuth             = prefs.getBoolean ( USE_PROXY_AUTHENTICATION,       false );                               // NOI18N            
-        username            = prefs.get        ( PROXY_AUTHENTICATION_USERNAME,  ""    );                               // NOI18N
-        password            = prefs.get        ( PROXY_AUTHENTICATION_PASSWORD,  ""    );                               // NOI18N                
-        notProxyHosts       = prefs.get        ( NOT_PROXY_HOSTS,                ""    ).replace("|", " ,");            // NOI18N                
-        httpHost            = prefs.get        ( PROXY_HTTP_HOST,                ""    );                               // NOI18N                
-        httpPort            = prefs.get        ( PROXY_HTTP_PORT,                ""    );                               // NOI18N                
-        httpsHost           = prefs.get        ( PROXY_HTTPS_HOST,               ""    );                               // NOI18N                
-        httpsPort           = prefs.get        ( PROXY_HTTPS_PORT,               ""    );                               // NOI18N                                
-        proxyType           = prefs.get        ( PROXY_TYPE,                     ""    );                               // NOI18N                                        
+        Preferences prefs = org.openide.util.NbPreferences.root ().node ("org/netbeans/core");                              // NOI18N    
+        proxyType           = prefs.get        ( PROXY_TYPE,                     ""    );                                   // NOI18N                                        
+
+        if(proxyType.equals(DIRECT_CONNECTION)) {
+            useAuth             = false;
+            username            = "";                                                                                       // NOI18N
+            password            = "";                                                                                       // NOI18N            
+        
+            notProxyHosts       = "";                                                                                       // NOI18N
+            httpHost            = "";                                                                                       // NOI18N
+            httpPort            = "";                                                                                       // NOI18N
+            httpsHost           = "";                                                                                       // NOI18N
+            httpsPort           = "";                                                                                       // NOI18N
+        } else if(isManualSetProxy()) {
+            useAuth             = prefs.getBoolean ( USE_PROXY_AUTHENTICATION,       false );                               // NOI18N            
+            username            = prefs.get        ( PROXY_AUTHENTICATION_USERNAME,  ""    );                               // NOI18N
+            password            = prefs.get        ( PROXY_AUTHENTICATION_PASSWORD,  ""    );                               // NOI18N                
+
+            notProxyHosts       = prefs.get        ( NOT_PROXY_HOSTS,                ""    ).replace("|", " ,");            // NOI18N                
+            httpHost            = prefs.get        ( PROXY_HTTP_HOST,                ""    );                               // NOI18N                
+            httpPort            = prefs.get        ( PROXY_HTTP_PORT,                ""    );                               // NOI18N                
+            httpsHost           = prefs.get        ( PROXY_HTTPS_HOST,               ""    );                               // NOI18N                
+            httpsPort           = prefs.get        ( PROXY_HTTPS_PORT,               ""    );                               // NOI18N                                            
+        } else { // AUTO_DETECT_PROXY or DEFAULT
+            useAuth             = false; // no way known yet!
+            username            = "";                                                                                       // NOI18N
+            password            = "";                                                                                       // NOI18N            
+        
+            notProxyHosts       = System.getProperty("http.nonProxyHosts");                                                 // NOI18N            
+            httpHost            = System.getProperty("http.proxyHost");                                                     // NOI18N            
+            httpPort            = System.getProperty("http.proxyPort");                                                     // NOI18N                
+            httpsHost           = System.getProperty("https.proxyHost");                                                    // NOI18N            
+            httpsPort           = System.getProperty("https.proxyPort");                                                    // NOI18N            
+        }
     }
     
     public boolean isDirect() {
         return proxyType.equals(DIRECT_CONNECTION);
     }
 
+    public boolean isManualSetProxy() {
+        return proxyType.equals(MANUAL_SET_PROXY);
+    }
+    
     public boolean hasAuth() {
         return useAuth;
     }
