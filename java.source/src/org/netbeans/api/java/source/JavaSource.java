bf42ef1f8d534fad15c0d221fedf0bce0c5978f2
tzezula <tzezula@netbeans.org>
2007-05-21 16:16:53 +0000

    Debug code.

--- java.source/src/org/netbeans/api/java/source/JavaSource.java
+++ java.source/src/org/netbeans/api/java/source/JavaSource.java
@@ -1921,7 +1921,12 @@ out:            for (Iterator<Collection<Request>> it = finishedRequests.values(
         
         boolean isInterruptJavac () {
             synchronized (INTERNAL_LOCK) {
-                return this.mayCancelJavac;
+                boolean ret = this.mayCancelJavac;
+                if (ret) {
+                    assert this.canceledReference != null;
+                    assert (this.canceledReference.javaSource.flags & INVALID) != 0;
+                }
+                return ret;
             }
         }
         
