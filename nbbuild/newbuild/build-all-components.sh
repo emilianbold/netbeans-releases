5adde8a09b3e4d18c819f4ebe58c44f3dbeb3359
mzlamal <mzlamal@netbeans.org>
2007-05-22 09:47:26 +0000

    Add the xml cluster to list of build clusters for the components

--- nbbuild/newbuild/build-all-components.sh
+++ nbbuild/newbuild/build-all-components.sh
@@ -25,7 +25,7 @@ fi
 #Components doesn't depend on each other, so no more error checking
 
 #VisualWeb component
-ant -f ant -f nbbuild/build.xml -Ddo-not-rebuild-clusters=true -Dnb.clusters.list=nb.cluster.profiler,nb.cluster.harness,nb.cluster.ide,nb.cluster.java,nb.cluster.apisupport,nb.cluster.j2ee,nb.cluster.nb,nb.cluster.platform,nb.cluster.visualweb build-nozip > $VISUALWEB_BUILD_LOG 2>&1
+ant -f ant -f nbbuild/build.xml -Ddo-not-rebuild-clusters=true -Dnb.clusters.list=nb.cluster.profiler,nb.cluster.harness,nb.cluster.ide,nb.cluster.java,nb.cluster.apisupport,nb.cluster.j2ee,nb.cluster.nb,nb.cluster.platform,nb.cluster.xml,nb.cluster.visualweb build-nozip > $VISUALWEB_BUILD_LOG 2>&1
 ERROR_CODE=$?
 
 if [ $ERROR_CODE != 0 ]; then
@@ -44,7 +44,7 @@ if [ $ERROR_CODE != 0 ]; then
 fi
 
 #UML component
-ant -f nbbuild/build.xml -Ddo-not-rebuild-clusters=true -Dnb.clusters.list=nb.cluster.profiler,nb.cluster.harness,nb.cluster.ide,nb.cluster.java,nb.cluster.apisupport,nb.cluster.j2ee,nb.cluster.nb,nb.cluster.platform,nb.cluster.uml build-nozip > $UML_BUILD_LOG 2>&1
+ant -f nbbuild/build.xml -Ddo-not-rebuild-clusters=true -Dnb.clusters.list=nb.cluster.profiler,nb.cluster.harness,nb.cluster.ide,nb.cluster.java,nb.cluster.apisupport,nb.cluster.j2ee,nb.cluster.nb,nb.cluster.platform,nb.cluster.xml,nb.cluster.uml build-nozip > $UML_BUILD_LOG 2>&1
 ERROR_CODE=$?
 
 if [ $ERROR_CODE != 0 ]; then
@@ -62,7 +62,7 @@ if [ $ERROR_CODE != 0 ]; then
 fi
 
 #SOA component
-ant -f nbbuild/build.xml -Ddo-not-rebuild-clusters=true -Dnb.clusters.list=nb.cluster.profiler,nb.cluster.harness,nb.cluster.ide,nb.cluster.java,nb.cluster.apisupport,nb.cluster.j2ee,nb.cluster.nb,nb.cluster.platform,nb.cluster.soa build-nozip > $SOA_BUILD_LOG 2>&1
+ant -f nbbuild/build.xml -Ddo-not-rebuild-clusters=true -Dnb.clusters.list=nb.cluster.profiler,nb.cluster.harness,nb.cluster.ide,nb.cluster.java,nb.cluster.apisupport,nb.cluster.j2ee,nb.cluster.nb,nb.cluster.platform,nb.cluster.xml,nb.cluster.soa build-nozip > $SOA_BUILD_LOG 2>&1
 ERROR_CODE=$?
 
 if [ $ERROR_CODE != 0 ]; then
