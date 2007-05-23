931692841a657f1a83eff7de238a1b103988a785
jglick <jglick@netbeans.org>
2007-05-23 21:06:29 +0000

    Use of varargs.

--- api.java/src/org/netbeans/spi/java/classpath/support/ClassPathSupport.java
+++ api.java/src/org/netbeans/spi/java/classpath/support/ClassPathSupport.java
@@ -103,7 +103,7 @@ public class ClassPathSupport {
      * archive must be used.  Cannot be null; can be empty array; array can contain nulls.
      * @return API classpath
      */
-    public static ClassPath createClassPath (FileObject[] roots) {
+    public static ClassPath createClassPath(FileObject... roots) {
         assert roots != null;
         List<PathResourceImplementation> l = new ArrayList<PathResourceImplementation> ();
         for (FileObject root : roots) {
@@ -128,7 +128,7 @@ public class ClassPathSupport {
      *   Cannot be null; can be empty array; array can contain nulls.
      * @return API classpath
      */
-    public static ClassPath createClassPath (URL[] roots) {
+    public static ClassPath createClassPath(URL... roots) {
         assert roots != null;
         List<PathResourceImplementation> l = new ArrayList<PathResourceImplementation> ();
         for (URL root : roots) {
@@ -146,7 +146,7 @@ public class ClassPathSupport {
      * @param delegates ClassPathImplementations to delegate to.
      * @return SPI classpath
      */
-    public static ClassPathImplementation createProxyClassPathImplementation (ClassPathImplementation[] delegates) {
+    public static ClassPathImplementation createProxyClassPathImplementation(ClassPathImplementation... delegates) {
         return new ProxyClassPathImplementation (delegates);
     }
 
@@ -157,7 +157,7 @@ public class ClassPathSupport {
      * @param delegates ClassPaths to delegate to.
      * @return API classpath
      */
-    public static ClassPath createProxyClassPath (ClassPath[] delegates) {
+    public static ClassPath createProxyClassPath(ClassPath... delegates) {
         assert delegates != null;
         ClassPathImplementation[] impls = new ClassPathImplementation [delegates.length];
         for (int i = 0; i < delegates.length; i++) {
