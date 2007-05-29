3e1ba86af26d4e3b11c161371566b3e4027bdce6
mmetelka <mmetelka@netbeans.org>
2007-05-29 12:32:01 +0000

    FIxed javadoc.

--- editor.codetemplates/src/org/netbeans/lib/editor/codetemplates/spi/CodeTemplateFilter.java
+++ editor.codetemplates/src/org/netbeans/lib/editor/codetemplates/spi/CodeTemplateFilter.java
@@ -23,14 +23,36 @@ import javax.swing.text.JTextComponent;
 import org.netbeans.lib.editor.codetemplates.api.CodeTemplate;
 
 /**
+ * Filter accepting code templates being displayed in a code completion popup.
+ * It is also used for editor hints (code templates) over a text selection.
  *
  * @author Dusan Balek
  */
 public interface CodeTemplateFilter {
-    
+  
+    /**
+     * Accept or reject the given code template.
+     * 
+     * @param template non-null template to accept or reject.
+     * @return true to accept the given code template or false to reject it.
+     */
     boolean accept(CodeTemplate template);
     
+    /**
+     * Factory for producing of the code template filters.
+     * <br/>
+     * It should be registered in the MimeLookup for a given mime-type.
+     */
     public interface Factory {
-        CodeTemplateFilter createFilter(JTextComponent component, int offset);        
+        
+        /**
+         * Create code template filter for the given context.
+         * 
+         * @param component non-null component for which the filter is being created.
+         * @param offset &gt;=0 offset for which the filter is being created.
+         * @return non-null code template filter instance.
+         */
+        CodeTemplateFilter createFilter(JTextComponent component, int offset);
+        
     }
 }
