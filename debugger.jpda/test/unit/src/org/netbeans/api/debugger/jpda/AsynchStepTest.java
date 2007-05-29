9b44c549feddccdf9f5ade974f36470c9a6659b5
mentlicher <mentlicher@netbeans.org>
2007-05-29 07:59:53 +0000

    Assert the validity of line breakpoint.

--- debugger.jpda/test/unit/src/org/netbeans/api/debugger/jpda/AsynchStepTest.java
+++ debugger.jpda/test/unit/src/org/netbeans/api/debugger/jpda/AsynchStepTest.java
@@ -52,6 +52,7 @@ public class AsynchStepTest extends NbTestCase {
             support = JPDASupport.attach
                 ("org.netbeans.api.debugger.jpda.testapps.AsynchStepApp");
             support.waitState (JPDADebugger.STATE_STOPPED);
+            assertEquals("Breakpoint "+lb+" is invalid, message: "+lb.getValidityMessage(), lb.getValidity(), LineBreakpoint.VALIDITY.VALID);
             dm.removeBreakpoint (lb);
             assertNotNull("No Current Thread.", support.getDebugger ().getCurrentThread());
             assertNotNull("No Current Call Stack Frame.", support.getDebugger ().getCurrentCallStackFrame());
