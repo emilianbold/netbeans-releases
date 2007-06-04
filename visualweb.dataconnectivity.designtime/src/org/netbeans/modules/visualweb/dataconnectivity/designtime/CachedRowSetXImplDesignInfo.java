3b447db60f74a9f85c58dc51e43ca27bff7f7df4
davidvc <davidvc@netbeans.org>
2007-06-04 17:03:18 +0000

    Fixed lower case 'x' in class name

--- visualweb.dataconnectivity.designtime/src/org/netbeans/modules/visualweb/dataconnectivity/designtime/CachedRowSetXImplDesignInfo.java
+++ visualweb.dataconnectivity.designtime/src/org/netbeans/modules/visualweb/dataconnectivity/designtime/CachedRowSetXImplDesignInfo.java
@@ -24,7 +24,7 @@ import com.sun.rowset.CachedRowSetImpl;
  *
  * @author David
  */
-public class CachedRowSetxImplDesignInfo implements DesignInfo {
+public class CachedRowSetXImplDesignInfo implements DesignInfo {
 
     public CachedRowSetxImplDesignInfo() {
         
