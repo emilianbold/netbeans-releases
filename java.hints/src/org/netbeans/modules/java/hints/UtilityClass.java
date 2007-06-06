f4012914588e9af47b87e6b685a6e99ff6349dbc
jtulach <jtulach@netbeans.org>
2007-06-06 08:23:23 +0000

    Utility class hint

---
+++ java.hints/src/org/netbeans/modules/java/hints/UtilityClass.java
@@ -0,0 +1,244 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * Portions Copyrighted 2007 Sun Microsystems, Inc.
+ */
+package org.netbeans.modules.java.hints;
+
+import com.sun.source.tree.ClassTree;
+import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.MethodTree;
+import com.sun.source.tree.ModifiersTree;
+import com.sun.source.tree.StatementTree;
+import com.sun.source.tree.Tree;
+import com.sun.source.tree.Tree.Kind;
+import com.sun.source.tree.TypeParameterTree;
+import com.sun.source.tree.VariableTree;
+import com.sun.source.util.TreePath;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Set;
+import java.util.prefs.Preferences;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementVisitor;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.PackageElement;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.TypeParameterElement;
+import javax.lang.model.element.VariableElement;
+import javax.swing.JComponent;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import org.netbeans.api.java.source.CancellableTask;
+import org.netbeans.api.java.source.CompilationInfo;
+import org.netbeans.api.java.source.JavaSource;
+import org.netbeans.api.java.source.ModificationResult;
+import org.netbeans.api.java.source.TreePathHandle;
+import org.netbeans.api.java.source.WorkingCopy;
+import org.netbeans.modules.java.editor.semantic.Utilities;
+import org.netbeans.modules.java.hints.spi.AbstractHint;
+import org.netbeans.spi.editor.hints.ChangeInfo;
+import org.netbeans.spi.editor.hints.ErrorDescription;
+import org.netbeans.spi.editor.hints.ErrorDescriptionFactory;
+import org.netbeans.spi.editor.hints.Fix;
+import org.openide.filesystems.FileObject;
+import org.openide.util.Exceptions;
+import org.openide.util.NbBundle;
+
+/**
+ *
+ * @author Jaroslav tulach
+ */
+public class UtilityClass extends AbstractHint implements ElementVisitor<Boolean,CompilationInfo> {
+    private boolean clazz;
+    
+    /** Creates a new instance of AddOverrideAnnotation */
+    private UtilityClass(boolean b) {
+        super( true, true, AbstractHint.HintSeverity.WARNING );
+        clazz = b;
+    }
+    
+    public Set<Kind> getTreeKinds() {
+        return EnumSet.of(clazz ? Kind.CLASS : Kind.METHOD); 
+    }
+    
+    public static UtilityClass withoutConstructor() {
+        return new UtilityClass(true);
+    }
+
+    public List<ErrorDescription> run(CompilationInfo compilationInfo,
+                                      TreePath treePath) {
+        try {
+            Document doc = compilationInfo.getDocument();
+            
+            if (doc == null) {
+                return null;
+            }
+            
+            Element e = compilationInfo.getTrees().getElement(treePath);
+            if (e == null) {
+                return null;
+            }
+            
+            if (clazz) {
+                for (Element m : e.getEnclosedElements()) {
+                    if (m.accept(this, compilationInfo)) {
+                        return null;
+                    }
+                }
+                List<Fix> fixes = Collections.<Fix>singletonList(new FixImpl(
+                    clazz,
+                    TreePathHandle.create(e, compilationInfo),
+                    compilationInfo.getFileObject()
+                    ));
+                
+                int[] span = Utilities.findIdentifierSpan(treePath, compilationInfo.getCompilationUnit(), compilationInfo.getTrees().getSourcePositions(), doc);
+                
+                ErrorDescription ed = ErrorDescriptionFactory.createErrorDescription(
+                    getSeverity().toEditorSeverity(),
+                    NbBundle.getMessage(UtilityClass.class, clazz ? "MSG_UtilityClass" : "MSG_PublicConstructor"), // NOI18N
+                    fixes,
+                    doc,
+                    doc.createPosition((int) span[0]),
+                    doc.createPosition((int) span[1])
+                    );
+                
+                return Collections.singletonList(ed);
+                
+            }
+        } catch (BadLocationException e) {
+            Exceptions.printStackTrace(e);
+        } catch (IOException e) {
+            Exceptions.printStackTrace(e);
+        }
+        
+        return null;
+    }
+
+    public String getId() {
+        return getClass().getName();
+    }
+
+    public String getDisplayName() {
+        return NbBundle.getMessage(UtilityClass.class, clazz ? "MSG_UtilityClass" : "MSG_PublicConstructor"); // NOI18N
+    }
+
+    public String getDescription() {
+        return NbBundle.getMessage(UtilityClass.class, clazz ? "HINT_UtilityClass" : "HINT_PublicConstructor"); // NOI18N
+    }
+
+    public void cancel() {
+        // XXX implement me 
+    }
+    
+    public Preferences getPreferences() {
+        return null;
+    }
+    
+    @Override
+    public JComponent getCustomizer(Preferences node) {
+        return null;
+    }    
+          
+    public Boolean visit(Element arg0, CompilationInfo arg1) {
+        return false;
+    }
+
+    public Boolean visit(Element arg0) {
+        return false;
+    }
+
+    public Boolean visitPackage(PackageElement arg0, CompilationInfo arg1) {
+        return false;
+    }
+
+    public Boolean visitType(TypeElement arg0, CompilationInfo arg1) {
+        return false;
+    }
+
+    public Boolean visitVariable(VariableElement v, CompilationInfo arg1) {
+        return !v.getModifiers().contains(Modifier.STATIC);
+    }
+
+    public Boolean visitExecutable(ExecutableElement m, CompilationInfo arg1) {
+        return !m.getModifiers().contains(Modifier.STATIC) && !arg1.getElementUtilities().isSynthetic(m);
+    }
+
+    public Boolean visitTypeParameter(TypeParameterElement arg0, CompilationInfo arg1) {
+        return false;
+    }
+
+    public Boolean visitUnknown(Element arg0, CompilationInfo arg1) {
+        return false;
+    }
+
+    private static final class FixImpl implements Fix, CancellableTask<WorkingCopy> {
+        private TreePathHandle handle;
+        private FileObject file;
+        private boolean clazz;
+        
+        public FixImpl(boolean clazz, TreePathHandle handle, FileObject file) {
+            this.handle = handle;
+            this.file = file;
+            this.clazz = clazz;
+        }
+        
+        public String getText() {
+            return NbBundle.getMessage(UtilityClass.class, clazz ? "MSG_UtilityClass" : "MSG_PublicConstructor"); // NOI18N
+        }
+        
+        public ChangeInfo implement() throws IOException {
+            ModificationResult result = JavaSource.forFileObject(file).runModificationTask(this);
+            result.commit();
+            return null;
+        }
+        
+        @Override public String toString() {
+            return "FixUtilityClass"; // NOI18N
+        }
+
+        public void cancel() {
+        }
+
+        public void run(WorkingCopy wc) throws Exception {
+            if (clazz) {
+                wc.toPhase(JavaSource.Phase.RESOLVED);
+                
+                Element e = handle.resolveElement(wc);
+                if (e == null) {
+                    return;
+                }
+                Tree outer = wc.getTrees().getTree(e);
+                if (outer == null || outer.getKind() != Kind.CLASS) {
+                    return;
+                }
+                ClassTree cls = (ClassTree)outer;
+                
+                ModifiersTree modifiers = wc.getTreeMaker().Modifiers(Collections.singleton(Modifier.PRIVATE));
+                MethodTree m = wc.getTreeMaker().Constructor(
+                    modifiers, 
+                    Collections.<TypeParameterTree>emptyList(), 
+                    Collections.<VariableTree>emptyList(), 
+                    Collections.<ExpressionTree>emptyList(), 
+                    wc.getTreeMaker().Block(Collections.<StatementTree>emptyList(), false)
+                );
+                wc.rewrite(cls, wc.getTreeMaker().addClassMember(cls, m));
+            }
+        }
+    }
+    
+}
---
+++ java.hints/test/unit/src/org/netbeans/modules/java/hints/UtilityClassTest.java
@@ -0,0 +1,99 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+package org.netbeans.modules.java.hints;
+
+import com.sun.source.util.TreePath;
+import java.util.List;
+import java.util.Locale;
+import org.netbeans.api.java.source.CompilationInfo;
+import org.netbeans.api.java.source.SourceUtilsTestUtil;
+import org.netbeans.modules.java.hints.infrastructure.TreeRuleTestBase;
+import org.netbeans.spi.editor.hints.ErrorDescription;
+
+/**
+ *
+ * @author Jaroslav Tulach
+ */
+public class UtilityClassTest extends TreeRuleTestBase {
+    
+    public UtilityClassTest(String testName) {
+        super(testName);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        Locale.setDefault(Locale.US);
+        SourceUtilsTestUtil.setLookup(new Object[0], getClass().getClassLoader());
+    }
+    
+    
+    
+    public void testClassWithOnlyStaticMethods() throws Exception {
+        String before = "package test; public class Te";
+        String after = "st extends Object {" +
+            " public static boolean isEventQueue() { return false; }" +
+            " public static String computeDiff(String x, String y) { return x + y; }" +
+            "}";
+        
+        performAnalysisTest("test/Test.java", before + after, before.length(), 
+            "0:27-0:31:verifier:Utility class without constructor"
+        );
+    }
+    public void tsestClassWithOnlyStaticMethodsAndFields() throws Exception {
+        String before = "package test; public class Te";
+        String after = "st extends Object {" +
+            " public static boolean isEventQueue() { return false; }" +
+            " public static String computeDiff(String x, String y) { return x + y; }" +
+            " public static final String PROP_X = null;";
+            ;
+        
+        String gold = before + after + " private Test() { } }";
+        performFixTest("test/Test.java", before + after + "}", before.length(), 
+            "0:27-0:31:verifier:Utility class without constructor",
+            "FixUtilityClass",
+            gold
+        );
+    }
+    public void testDisabledWhenMehtodIsThere() throws Exception {
+        String before = "package test; public class Te";
+        String after = "st extends Object {" +
+            " public boolean isEventQueue() { return false; }" +
+            " public static String computeDiff(String x, String y) { return x + y; }" +
+            "}";
+        
+        performAnalysisTest("test/Test.java", before + after, before.length());
+    }
+    public void testDisabledWhenConstructorIsThere() throws Exception {
+        String before = "package test; public class Te";
+        String after = "st extends Object {" +
+            " public Test() { }" +
+            " public static String computeDiff(String x, String y) { return x + y; }" +
+            "}";
+        
+        performAnalysisTest("test/Test.java", before + after, before.length());
+    }
+
+    protected List<ErrorDescription> computeErrors(CompilationInfo info, TreePath path) {
+        SourceUtilsTestUtil.setSourceLevel(info.getFileObject(), sourceLevel);
+        return UtilityClass.withoutConstructor().run(info, path);
+    }
+    
+    private String sourceLevel = "1.5";
+    
+}
