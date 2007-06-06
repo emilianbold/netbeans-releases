992d3cb6ce91b65082b16375fea166502bc0ed42
jtulach <jtulach@netbeans.org>
2007-06-06 05:49:14 +0000

    First API hint: Find leaking private types

---
+++ java.hints/src/org/netbeans/modules/java/hints/ExportNonAccessibleElement.java
@@ -0,0 +1,351 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * Portions Copyrighted 2007 Sun Microsystems, Inc.
+ */
+package org.netbeans.modules.java.hints;
+
+import com.sun.source.tree.ClassTree;
+import com.sun.source.tree.MethodTree;
+import com.sun.source.tree.ModifiersTree;
+import com.sun.source.tree.Tree;
+import com.sun.source.tree.Tree.Kind;
+import com.sun.source.tree.VariableTree;
+import com.sun.source.util.TreePath;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.prefs.Preferences;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementVisitor;
+import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.PackageElement;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.TypeParameterElement;
+import javax.lang.model.element.VariableElement;
+import javax.lang.model.type.ArrayType;
+import javax.lang.model.type.DeclaredType;
+import javax.lang.model.type.ErrorType;
+import javax.lang.model.type.ExecutableType;
+import javax.lang.model.type.NoType;
+import javax.lang.model.type.NullType;
+import javax.lang.model.type.PrimitiveType;
+import javax.lang.model.type.TypeMirror;
+import javax.lang.model.type.TypeVariable;
+import javax.lang.model.type.TypeVisitor;
+import javax.lang.model.type.WildcardType;
+import javax.swing.JComponent;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import org.netbeans.api.java.source.CancellableTask;
+import org.netbeans.api.java.source.CompilationInfo;
+import org.netbeans.api.java.source.JavaSource;
+import org.netbeans.api.java.source.ModificationResult;
+import org.netbeans.api.java.source.TreePathHandle;
+import org.netbeans.api.java.source.WorkingCopy;
+import org.netbeans.modules.java.editor.semantic.Utilities;
+import org.netbeans.modules.java.hints.spi.AbstractHint;
+import org.netbeans.spi.editor.hints.ChangeInfo;
+import org.netbeans.spi.editor.hints.ErrorDescription;
+import org.netbeans.spi.editor.hints.ErrorDescriptionFactory;
+import org.netbeans.spi.editor.hints.Fix;
+import org.openide.filesystems.FileObject;
+import org.openide.util.Exceptions;
+import org.openide.util.NbBundle;
+
+/**
+ *
+ * @author Jaroslav tulach
+ */
+public class ExportNonAccessibleElement extends AbstractHint 
+implements ElementVisitor<Boolean,Void>, TypeVisitor<Boolean,Void> {
+    
+    /** Creates a new instance of AddOverrideAnnotation */
+    public ExportNonAccessibleElement() {
+        super( true, true, AbstractHint.HintSeverity.WARNING );
+    }
+    
+    public Set<Kind> getTreeKinds() {
+        return EnumSet.of(Kind.METHOD, Kind.CLASS);
+    }
+
+    public List<ErrorDescription> run(CompilationInfo compilationInfo,
+                                      TreePath treePath) {
+        try {
+            Document doc = compilationInfo.getDocument();
+            
+            if (doc == null) {
+                return null;
+            }
+            
+            Element e = compilationInfo.getTrees().getElement(treePath);
+            if (e == null) {
+                return null;
+            }
+            Boolean b = e.accept(this, null);
+            
+            if (b) {
+
+                List<Fix> fixes = Collections.<Fix>singletonList(new FixImpl(
+                    "MSG_ExportNonAccessibleElement", // NOI18N
+                    TreePathHandle.create(e, compilationInfo), 
+                    compilationInfo.getFileObject()
+                ));
+
+                int[] span = Utilities.findIdentifierSpan(treePath, compilationInfo.getCompilationUnit(), compilationInfo.getTrees().getSourcePositions(), doc);
+
+                ErrorDescription ed = ErrorDescriptionFactory.createErrorDescription(
+                    getSeverity().toEditorSeverity(), 
+                    NbBundle.getMessage(ExportNonAccessibleElement.class, "MSG_ExportNonAccessibleElement"), 
+                    fixes, 
+                    doc, 
+                    doc.createPosition((int) span[0]), 
+                    doc.createPosition((int) span[1])
+                );
+
+                return Collections.singletonList(ed);
+            }
+        } catch (BadLocationException e) {
+            Exceptions.printStackTrace(e);
+        } catch (IOException e) {
+            Exceptions.printStackTrace(e);
+        }
+        
+        return null;
+    }
+
+    public String getId() {
+        return getClass().getName();
+    }
+
+    public String getDisplayName() {
+        return NbBundle.getMessage(MissingHashCode.class, "MSG_ExportNonAccessibleElement");
+    }
+
+    public String getDescription() {
+        return NbBundle.getMessage(MissingHashCode.class, "HINT_ExportNonAccessibleElement");
+    }
+
+    public void cancel() {
+        // XXX implement me 
+    }
+    
+    public Preferences getPreferences() {
+        return null;
+    }
+    
+    @Override
+    public JComponent getCustomizer(Preferences node) {
+        return null;
+    }    
+    public Boolean visit(Element arg0, Void arg1) {
+        // no need for hint
+        return false;
+    }
+
+    public Boolean visit(Element arg0) {
+        // no need for hint
+        return false;
+    }
+
+    public Boolean visitPackage(PackageElement arg0, Void arg1) {
+        return false;
+    }
+
+    public Boolean visitType(TypeElement arg0, Void arg1) {
+        for (TypeParameterElement e : arg0.getTypeParameters()) {
+            if (e.getGenericElement().accept(this, arg1)) {
+                return true;
+            }
+        }
+        return arg0.getSuperclass().accept(this, arg1);
+    }
+
+    public Boolean visitVariable(VariableElement field, Void arg1) {
+        if (!isVisible(field)) {
+            // no need for hint
+            return false;
+        }
+        return field.asType().accept(this, arg1);
+    }
+
+    public Boolean visitExecutable(ExecutableElement method, Void nothing) {
+        if (!isVisible(method)) {
+            // ok
+            return false;
+        }
+        for (VariableElement v : method.getParameters()) {
+            if (v.asType().accept(this, nothing)) {
+                return true;
+            }
+        }
+        return method.getReturnType().accept(this, nothing);
+    }
+
+    public Boolean visitTypeParameter(TypeParameterElement arg0, Void arg1) {
+        return false;
+    }
+
+    public Boolean visitUnknown(Element arg0, Void arg1) {
+        // ok, probably no need for hint
+        return false;
+    }
+
+
+    public Boolean visit(TypeMirror arg0, Void arg1) {
+        return false;
+    }
+
+    public Boolean visit(TypeMirror arg0) {
+        return false;
+    }
+
+    public Boolean visitPrimitive(PrimitiveType arg0, Void arg1) {
+        return false;
+    }
+
+    public Boolean visitNull(NullType arg0, Void arg1) {
+        return false;
+    }
+
+    public Boolean visitArray(ArrayType arg0, Void arg1) {
+        return arg0.getComponentType().accept(this, arg1);
+    }
+
+    public Boolean visitDeclared(DeclaredType arg0, Void arg1) {
+        if (!isVisible(arg0.asElement())) {
+            return true;
+        }
+        for (TypeMirror t : arg0.getTypeArguments()) {
+            if (t.accept(this, arg1)) {
+                return true;
+            }
+        }
+        return arg0.getEnclosingType().accept(this, arg1);
+    }
+
+    public Boolean visitError(ErrorType arg0, Void arg1) {
+        // no hint
+        return false;
+    }
+
+    public Boolean visitTypeVariable(TypeVariable arg0, Void arg1) {
+        return arg0.getLowerBound().accept(this, arg1) && arg0.getUpperBound().accept(this, arg1);
+    }
+
+    public Boolean visitWildcard(WildcardType arg0, Void arg1) {
+        return arg0.getExtendsBound().accept(this, arg1) &&
+            arg0.getSuperBound().accept(this, arg1);
+    }
+
+    public Boolean visitExecutable(ExecutableType arg0, Void arg1) {
+        return false;
+    }
+
+    public Boolean visitNoType(NoType arg0, Void arg1) {
+        return false;
+    }
+
+    public Boolean visitUnknown(TypeMirror arg0, Void arg1) {
+        return false;
+    }
+    
+    
+    private static boolean isVisible(Element... arr) {
+        return isVisible(Arrays.asList(arr));
+    }
+    private static boolean isVisible(Collection<? extends Element> arr) {
+        for (Element el : arr) {
+            if (el.getModifiers().contains(Modifier.PUBLIC) || 
+                el.getModifiers().contains(Modifier.PROTECTED)
+            ) {
+                return true;
+            }
+        }
+        return false;
+    }
+          
+    private static final class FixImpl implements Fix {
+        private TreePathHandle handle;
+        private FileObject file;
+        private String msg;
+        
+        public FixImpl(String type, TreePathHandle handle, FileObject file) {
+            this.handle = handle;
+            this.file = file;
+            this.msg = type;
+        }
+        
+        public String getText() {
+            return NbBundle.getMessage(MissingHashCode.class, msg);
+        }
+        
+        private static final Set<Kind> DECLARATION = EnumSet.of(Kind.CLASS, Kind.METHOD, Kind.VARIABLE);
+        
+        public ChangeInfo implement() throws IOException {
+            ModificationResult result = JavaSource.forFileObject(file).runModificationTask(new CancellableTask<WorkingCopy>() {
+                public void cancel() {
+                }
+
+                public void run(WorkingCopy wc) throws Exception {
+                    wc.toPhase(JavaSource.Phase.RESOLVED);
+                    Element e = handle.resolveElement(wc);
+                    
+                    Tree t = wc.getTrees().getTree(e);
+                    if (t.getKind() == Kind.CLASS) {
+                        ClassTree ct = (ClassTree)t;
+                        Set<Modifier> flags = new HashSet<Modifier>(ct.getModifiers().getFlags());
+                        flags.remove(Modifier.PUBLIC);
+                        flags.remove(Modifier.PROTECTED);
+                        ModifiersTree mt = wc.getTreeMaker().Modifiers(flags, ct.getModifiers().getAnnotations());
+                        wc.rewrite(ct.getModifiers(), mt);
+                        return;
+                    }
+                    if (t.getKind() == Kind.METHOD) {
+                        MethodTree mt = (MethodTree)t;
+                        Set<Modifier> flags = new HashSet<Modifier>(mt.getModifiers().getFlags());
+                        flags.remove(Modifier.PUBLIC);
+                        flags.remove(Modifier.PROTECTED);
+                        ModifiersTree modt = wc.getTreeMaker().Modifiers(flags, mt.getModifiers().getAnnotations());
+                        wc.rewrite(mt.getModifiers(), modt);
+                        return;
+                    }
+                    if (t.getKind() == Kind.VARIABLE) {
+                        VariableTree vt = (VariableTree)t;
+                        Set<Modifier> flags = new HashSet<Modifier>(vt.getModifiers().getFlags());
+                        flags.remove(Modifier.PUBLIC);
+                        flags.remove(Modifier.PROTECTED);
+                        ModifiersTree modt = wc.getTreeMaker().Modifiers(flags, vt.getModifiers().getAnnotations());
+                        wc.rewrite(vt.getModifiers(), modt);
+                        return;
+                    }
+                }
+            });
+
+            result.commit();
+            
+            return null;
+        }
+        
+        public String toString() {
+            return "FixExportNonAccessibleElement"; // NOI18N
+        }
+    }
+}
---
+++ java.hints/test/unit/src/org/netbeans/modules/java/hints/ExportNonAccessibleElementTest.java
@@ -0,0 +1,149 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+package org.netbeans.modules.java.hints;
+
+import com.sun.source.util.TreePath;
+import java.util.List;
+import java.util.Locale;
+import org.netbeans.api.java.source.CompilationInfo;
+import org.netbeans.api.java.source.SourceUtilsTestUtil;
+import org.netbeans.modules.java.hints.infrastructure.TreeRuleTestBase;
+import org.netbeans.spi.editor.hints.ErrorDescription;
+
+/**
+ *
+ * @author Jaroslav Tulach
+ */
+public class ExportNonAccessibleElementTest extends TreeRuleTestBase {
+    
+    public ExportNonAccessibleElementTest(String testName) {
+        super(testName);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        Locale.setDefault(Locale.US);
+        SourceUtilsTestUtil.setLookup(new Object[0], getClass().getClassLoader());
+    }
+    
+    
+    
+    public void testMethodReturningNonPublicInnerClass() throws Exception {
+        String before = "package test; public class Test extends Object {" +
+            " public Inner ";
+        String after = " ret() {" +
+            "  return null;" +
+            " }\n" +
+            " private class Inner { }" +
+            "}";
+        
+        performAnalysisTest("test/Test.java", before + after, before.length(), 
+            "0:63-0:66:verifier:Non Accessible Type"
+        );
+    }
+
+    public void testMethodTakingNonPublicInnerClassNotInMethod() throws Exception {
+        String before = "package test; public class Test extends Object {" +
+            " public void";
+        String after = " ret(Inner in) {" +
+            " }\n" +
+            " private class Inner { }" +
+            "}";
+        
+        performAnalysisTest("test/Test.java", before + after, before.length());
+    }
+    public void testMethodTakingNonPublicInnerClass() throws Exception {
+        String before = "package test; public class Test extends Object {" +
+            " public void ret(Inner in)";
+        String after = " {" +
+            " }" +
+            " private class Inner { }" +
+            "}";
+        
+        String res = (before + after).replace("public void", "void");
+        performFixTest("test/Test.java", before + after, before.length(), 
+            "0:61-0:64:verifier:Non Accessible Type",
+            "FixExportNonAccessibleElement",
+            res);
+    }
+    public void testNoProblemInNotPublicClasses() throws Exception {
+        String before = "package test; class Test extends Object {" +
+            " public void";
+        String after = " ret(Inner in) {" +
+            " }\n" +
+            " private class Inner { }" +
+            "}";
+        
+        performAnalysisTest("test/Test.java", before + after, before.length());
+    }
+    public void testOnFieldsAsWell() throws Exception {
+        String before = "package test; class Test extends Object {" +
+            " public Inner ";
+        String after = "in;" +
+            " private class Inner { }" +
+            "}";
+        
+        String res = (before + after).replace("public ", "");
+        performFixTest("test/Test.java", before + after, before.length(), 
+            "0:55-0:57:verifier:Non Accessible Type",
+            "FixExportNonAccessibleElement",
+            res);
+    }
+    public void testNonPublicSuperClass() throws Exception {
+        String before = "package test; " +
+            "class TestBase {} " +
+            "public class Test e"; String after = "xtends TestBase {" +
+            "}";
+        
+            
+        String res = (before + after).replace("public ", "");
+        performFixTest("test/Test.java", before + after, before.length(), 
+            "0:45-0:49:verifier:Non Accessible Type", 
+            "FixExportNonAccessibleElement",
+            res);
+    }
+    public void testWhyEachMethodIsInaccessible() throws Exception {
+        String before = "package test; " +
+            "public class Test {" +
+            "    public void testNonPublicSuperClass()";
+        String after = " throws Exception { " +
+            "\n}" +
+            "";
+        performAnalysisTest("test/Test.java", before + after, before.length());       
+    }
+
+
+    public void testNonVisibleMethod() throws Exception {
+        String before = "package test; " +
+            "public class Test {" +
+            "    private C testNonPublicSuperClass()";
+        String after = " throws Exception { " +
+            "\n}" +
+            "class C { }";
+        performAnalysisTest("test/Test.java", before + after, before.length());       
+    }
+    
+    protected List<ErrorDescription> computeErrors(CompilationInfo info, TreePath path) {
+        SourceUtilsTestUtil.setSourceLevel(info.getFileObject(), sourceLevel);
+        return new ExportNonAccessibleElement().run(info, path);
+    }
+    
+    private String sourceLevel = "1.5";
+    
+}
