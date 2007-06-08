65b812a4a59ddbe1f00e6085ac25996f2be974e6
mzlamal <mzlamal@netbeans.org>
2007-06-08 09:41:58 +0000

    Upload bits after the build at once

---
+++ nbbuild/newbuild/upload-bits.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -x
+
+#Initialize basic scructure
+DIRNAME=`dirname $0`
+cd ${DIRNAME}
+TRUNK_NIGHTLY_DIRNAME=`pwd`
+export BUILD_DESC=trunk-nightly
+source init.sh
+
+ssh -p 222 $DIST_SERVER mkdir -p $DIST_SERVER_PATH/$DATESTAMP
+scp -P 222 -q -r -v $DIST/* $DIST_SERVER:$DIST_SERVER_PATH/$DATESTAMP
+
+ssh -p 222 $DIST_SERVER rm $DIST_SERVER_PATH/latest.old
+ssh -p 222 $DIST_SERVER mv $DIST_SERVER_PATH/latest $DIST_SERVER_PATH/latest.old
+ssh -p 222 $DIST_SERVER ln -s $DIST_SERVER_PATH/$DATESTAMP $DIST_SERVER_PATH/latest
