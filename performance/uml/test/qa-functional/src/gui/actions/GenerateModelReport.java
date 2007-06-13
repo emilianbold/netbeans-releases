cf221ec94bac017f39072820aa2e67e64a189b0e
rashid <rashid@netbeans.org>
2007-06-13 16:26:39 +0000

    added new testcase GenerateModelReport

---
+++ performance/uml/test/qa-functional/src/gui/actions/GenerateModelReport.java
@@ -0,0 +1,102 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package gui.actions;
+
+
+import java.io.File;
+import org.netbeans.jellytools.ProjectsTabOperator;
+import org.netbeans.jellytools.actions.CloseAllDocumentsAction;
+import org.netbeans.jellytools.nodes.Node;
+import org.netbeans.jemmy.EventTool;
+import org.netbeans.jemmy.operators.ComponentOperator;
+import org.netbeans.junit.ide.ProjectSupport;
+import org.netbeans.jellytools.TopComponentOperator;
+import org.netbeans.jellytools.OutputOperator;
+import org.netbeans.jellytools.OutputTabOperator;
+
+/**
+ * Measure UI-RESPONSIVENES and WINDOW_OPENING.
+ *
+ * @author rashid@netbeans.org
+ *
+ */
+public class GenerateModelReport extends org.netbeans.performance.test.utilities.PerformanceTestCase {
+    
+    private static String testProjectName = "jEdit-Model";
+       
+   
+    /** Creates a new instance of GenerateModelReport */
+    public GenerateModelReport(String testName) {
+        super(testName);
+        //TODO: Adjust expectedTime value        
+        expectedTime = 300000;
+        WAIT_AFTER_OPEN=4000;        
+    }
+    public GenerateModelReport(String testName, String  performanceDataName) {
+        super(testName);
+        //TODO: Adjust expectedTime value
+        expectedTime = 300000;
+        WAIT_AFTER_OPEN=4000;                
+    }
+    
+    public void initialize(){
+        log(":: initialize");
+        
+        ProjectSupport.openProject(System.getProperty("xtest.tmpdir")+File.separator+testProjectName);
+        new CloseAllDocumentsAction().performAPI();
+        
+    }
+   
+    public void prepare() {
+        log(":: prepare");
+
+    }
+
+    public ComponentOperator open() {
+        log("::open");
+        Node pNode = new ProjectsTabOperator().getProjectRootNode(testProjectName);
+        pNode.performPopupActionNoBlock("Generate Model Report");
+        OutputOperator oot = new OutputOperator();
+        oot.getTimeouts().setTimeout("ComponentOperator.WaitStateTimeout",300000);
+        OutputTabOperator asot = oot.getOutputTab("UML Report Log");
+        asot.getTimeouts().setTimeout("ComponentOperator.WaitStateTimeout",300000);
+        asot.waitText("Report Successful");
+      
+ 
+      return null;
+    }
+    
+    protected void shutdown() {
+        log("::shutdown");
+        ProjectSupport.closeProject(testProjectName);
+    }
+   
+
+    public void close(){
+        log("::close");
+      new CloseAllDocumentsAction().performAPI();
+ 
+    } 
+    public static void main(java.lang.String[] args) {
+        junit.textui.TestRunner.run(new GenerateModelReport("measureTime"));
+    }      
+
+
+}
