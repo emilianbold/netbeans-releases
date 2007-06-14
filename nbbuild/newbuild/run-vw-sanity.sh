6f17c7d9711412b770a63b7abdfde976952bd1ad
am111535 <am111535@netbeans.org>
2007-06-14 19:36:48 +0000

    created seperate routines for each task

--- nbbuild/newbuild/run-vw-sanity.sh
+++ nbbuild/newbuild/run-vw-sanity.sh
@@ -20,104 +20,139 @@ TESTROOT="${SRCROOT}/visualweb/test"
 
 ###################################################################
 
-# Download Application Server
-if [ ! -d ${CACHEROOT} ]; then
-	mkdir -p $CACHEROOT
-fi
+download () {
+	# Download App Server
+	if [ ! -d ${CACHEROOT} ]; then
+		mkdir -p $CACHEROOT
+	fi
 
-if [ -f ${AS_KITSERVER}/${AS_BINARY} ]; then
-    cp ${AS_KITSERVER}/${AS_BINARY} ${CACHEROOT}
-    ERROR_CODE=$?
-else
-    echo "ERROR: Please set AS_KITSERVER and AS_BINARY - ${AS_KITSERVER}/${AS_BINARY}"
-    exit 2;
-fi
+	if [ -f ${AS_KITSERVER}/${AS_BINARY} ]; then
+    	cp ${AS_KITSERVER}/${AS_BINARY} ${CACHEROOT}
+    	ERROR_CODE=$?
+	else
+    	echo "ERROR: Please set AS_KITSERVER and AS_BINARY - ${AS_KITSERVER}/${AS_BINARY}"
+    	exit 2;
+	fi
 
-if [ $ERROR_CODE != 0 ]; then
-    echo "ERROR: $ERROR_CODE - Can't download Glassfish"
-    exit $ERROR_CODE;
-fi
-
-###################################################################
-
-# First be good and uninstall
-if [ -x $J2EE_HOME/bin/uninstall ]; then
-    $J2EE_HOME/bin/uninstall -silent
-fi
-# This is a temp hack as the "uninstall" command is broken
-if [ -d ${J2EE_HOME} -a ! -z ${J2EE_HOME} ]; then
-	rm -rf ${J2EE_HOME}
-fi
+	if [ $ERROR_CODE != 0 ]; then
+    	echo "ERROR: $ERROR_CODE - Can't download Glassfish"
+    	exit $ERROR_CODE;
+	fi
+}
 
 ###################################################################
 
-# Creating statefile
-rm -f ${AS_ROOT}/sunappserver_statefile
-echo "A" > ${AS_ROOT}/sunappserver_statefile
+uninstall() {
+	# First be good and uninstall
+	if [ -x $J2EE_HOME/bin/uninstall ]; then
+    	$J2EE_HOME/bin/uninstall -silent
+	fi
+
+	# This is a temp hack as the "uninstall" command is broken
+	if [ -d ${J2EE_HOME} -a ! -z ${J2EE_HOME} ]; then
+		rm -rf ${J2EE_HOME}
+	fi
+}
 
 ###################################################################
 
-# Install Application Server
-if [ ! -d ${AS_ROOT} ]; then
-	mkdir ${AS_ROOT}
-fi
-
-chmod a+x ${CACHEROOT}/${AS_BINARY}
-cd ${AS_ROOT}
-
-TEMP_DISPLAY="${DISPLAY}"
-unset DISPLAY
-
-java -Xmx256m -jar ${CACHEROOT}/${AS_BINARY} < ${AS_ROOT}/sunappserver_statefile
-
-ERROR_CODE=$?
-if [ $ERROR_CODE != 0 ]; then
-    echo "ERROR: $ERROR_CODE - Can't install Glassfish"
-    exit $ERROR_CODE;
-fi
-
-DISPLAY="${TEMP_DISPLAY}"
-export DISPLAY
+create_statefile() {
+	# Creating statefile
+	rm -f ${AS_ROOT}/sunappserver_statefile
+	echo "A" > ${AS_ROOT}/sunappserver_statefile
+}
 
 ###################################################################
 
-# Setup Application Server
-cd ${J2EE_HOME}
-ant -f setup.xml -Dinstance.port=28080
+install() {
+	# Install Application Server
+	if [ ! -d ${AS_ROOT} ]; then
+		mkdir ${AS_ROOT}
+	fi
+
+	chmod a+x ${CACHEROOT}/${AS_BINARY}
+	cd ${AS_ROOT}
+
+	TEMP_DISPLAY="${DISPLAY}"
+	unset DISPLAY
+
+	java -Xmx256m -jar ${CACHEROOT}/${AS_BINARY} < ${AS_ROOT}/sunappserver_statefile
+
+	ERROR_CODE=$?
+	if [ $ERROR_CODE != 0 ]; then
+    	echo "ERROR: $ERROR_CODE - Can't install Glassfish"
+    	exit $ERROR_CODE;
+	fi
+
+	DISPLAY="${TEMP_DISPLAY}"
+	export DISPLAY
+}
 
 ###################################################################
 
-# Setup properties file 
-cp $TESTROOT/data/DefaultDeploymentTargets.properties.template $TESTROOT/data/tmp.properties
-MODIFIED_J2EE_HOME=`echo ${J2EE_HOME} | sed 's/\//::/g'`
-sed -e "s/J2EE_HOME/${MODIFIED_J2EE_HOME}/g" -e "s/::/\//g" -e "s/8080/28080/g" $TESTROOT/data/tmp.properties > $TESTROOT/data/DefaultDeploymentTargets.properties
+setup_appserver() {
+	# Setup Application Server
+	cd ${J2EE_HOME}
+	ant -f setup.xml -Dinstance.port=8080
+}
+
+###################################################################
+
+setup_properties() {
+	# Setup properties file 
+	cp $TESTROOT/data/DefaultDeploymentTargets.properties.template $TESTROOT/data/tmp.properties
+	MODIFIED_J2EE_HOME=`echo ${J2EE_HOME} | sed 's/\//::/g'`
+	sed -e "s/J2EE_HOME/${MODIFIED_J2EE_HOME}/g" -e "s/::/\//g" -e "s/8080/8080/g" $TESTROOT/data/tmp.properties > $TESTROOT/data/DefaultDeploymentTargets.properties
    
-ERROR_CODE=$?
-if [ $ERROR_CODE != 0 ]; then
-	echo "ERROR: $ERROR_CODE - Can't setup Glassfish"
-	exit $ERROR_CODE;
-fi
+	ERROR_CODE=$?
+	if [ $ERROR_CODE != 0 ]; then
+		echo "ERROR: $ERROR_CODE - Can't setup Glassfish"
+		exit $ERROR_CODE;
+	fi
     
-rm -f $TESTROOT/data/tmp.properties
+	rm -f $TESTROOT/data/tmp.properties
+}
 
 ###################################################################
 
-# Run Sanity test on VisualWeb build
-cd $SRCROOT/visualweb/ravebuild
-ant build-test-tools -Dnetbeans.dist.dir="${J2EE_HOME}"
-ant commit-validation -Dnetbeans.dist.dir="${J2EE_HOME}"
+run_sanity() {
+	# Run Sanity test on VisualWeb build
+	cd $SRCROOT/visualweb/ravebuild
+	ant build-test-tools -Dnetbeans.dist.dir="${J2EE_HOME}"
+	ant commit-validation -Dnetbeans.dist.dir="${J2EE_HOME}"
 
-ERROR_CODE=$?
-if [ $ERROR_CODE != 0 ]; then
-    echo "ERROR: $ERROR_CODE - Error in running sanity test"
-    exit $ERROR_CODE;
-fi
+	ERROR_CODE=$?
+	if [ $ERROR_CODE != 0 ]; then
+    	echo "ERROR: $ERROR_CODE - Error in running sanity test"
+    	exit $ERROR_CODE;
+	fi
+}
 
 ###################################################################
 
-# Clean up
-if [ -d ${CACHEROOT} -a ! -z ${CACHEROOT} ]; then
-	rm -rf ${CACHEROOT}
-fi
+cleanup() {
+	# Clean up
+	if [ -d ${CACHEROOT} -a ! -z ${CACHEROOT} ]; then
+		rm -rf ${CACHEROOT}
+	fi
+}
 
-######################################## END #######################################
+############################# MAIN ################################
+
+#download
+
+#uninstall
+
+#create_statefile
+
+#install
+
+#setup_appserver
+
+setup_properties
+
+run_sanity
+
+cleanup
+
+############################## END ################################
