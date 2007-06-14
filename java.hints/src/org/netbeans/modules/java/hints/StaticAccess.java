af00f2fe46eaa793015f24bfc9277fb8e97afa5e
jtulach <jtulach@netbeans.org>
2007-06-14 09:59:34 +0000

    Adding halfly finished hint for static classes with test, so pflaska can debug why workingCopy.rewrite throws ISE

---
+++ java.hints/src/org/netbeans/modules/java/hints/StaticAccess.java
@@ -0,0 +1,213 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * Portions Copyrighted 2007 Sun Microsystems, Inc.
+ */
+package org.netbeans.modules.java.hints;
+
+import com.sun.source.tree.IdentifierTree;
+import com.sun.source.tree.MemberSelectTree;
+import com.sun.source.tree.Tree.Kind;
+import com.sun.source.util.TreePath;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.EnumSet;
+import java.util.List;
+import java.util.Set;
+import java.util.prefs.Preferences;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
+import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.TypeMirror;
+import javax.swing.JComponent;
+import javax.swing.JEditorPane;
+import javax.swing.SwingUtilities;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import org.netbeans.api.java.source.CancellableTask;
+import org.netbeans.api.java.source.CompilationInfo;
+import org.netbeans.api.java.source.JavaSource;
+import org.netbeans.api.java.source.ModificationResult;
+import org.netbeans.api.java.source.TreePathHandle;
+import org.netbeans.api.java.source.WorkingCopy;
+import org.netbeans.modules.java.editor.rename.InstantRenamePerformer;
+import org.netbeans.modules.java.editor.semantic.Utilities;
+import org.netbeans.modules.java.hints.spi.AbstractHint;
+import org.netbeans.spi.editor.hints.ChangeInfo;
+import org.netbeans.spi.editor.hints.ErrorDescription;
+import org.netbeans.spi.editor.hints.ErrorDescriptionFactory;
+import org.netbeans.spi.editor.hints.Fix;
+import org.openide.cookies.EditorCookie;
+import org.openide.filesystems.FileObject;
+import org.openide.loaders.DataObject;
+import org.openide.loaders.DataObjectNotFoundException;
+import org.openide.util.Exceptions;
+import org.openide.util.NbBundle;
+
+/**
+ *
+ * @author Jaroslav tulach
+ */
+public class StaticAccess extends AbstractHint {
+    /** Creates a new instance of AddOverrideAnnotation */
+    public StaticAccess() {
+        super( true, true, AbstractHint.HintSeverity.WARNING);
+    }
+    
+    public Set<Kind> getTreeKinds() {
+        return EnumSet.of(Kind.MEMBER_SELECT);
+    }
+
+    protected List<Fix> computeFixes(CompilationInfo info, TreePath treePath, Document doc, int[] bounds) {
+        MemberSelectTree mst = (MemberSelectTree)treePath.getLeaf();
+        TreePath expr = info.getTrees().getPath(info.getCompilationUnit(), mst.getExpression());
+        if (expr == null) {
+            return null;
+        }
+        
+        TypeMirror tm = info.getTrees().getTypeMirror(expr);
+        if (tm == null) {
+            return null;
+        }
+        Element el = info.getTypes().asElement(tm);
+        if (el == null || el.getKind() != ElementKind.CLASS) {
+            return null;
+        }
+        TypeElement type = (TypeElement)el;
+        if (mst.getExpression().getKind() == Kind.IDENTIFIER) {
+            IdentifierTree idt = (IdentifierTree)mst.getExpression();
+            if (idt.getName().equals(type.getSimpleName())) {
+                return null;
+            }
+            if (idt.getName().equals(type.getQualifiedName())) {
+                return null;
+            }
+        }
+        
+        TreePath exprPath = info.getTrees().getPath(info.getCompilationUnit(), mst.getExpression());
+        
+        int[] span = {
+            (int)info.getTrees().getSourcePositions().getStartPosition(info.getCompilationUnit(), mst.getExpression()),
+            (int)info.getTrees().getSourcePositions().getEndPosition(info.getCompilationUnit(), mst.getExpression()),
+        };
+        List<Fix> fixes = Collections.<Fix>singletonList(new FixImpl(
+            TreePathHandle.create(exprPath, info),
+            TreePathHandle.create(type, info),
+            info.getFileObject()
+        ));
+
+
+        bounds[0] = span[0];
+        bounds[1] = span[1];
+        return fixes;
+    }
+    
+    public List<ErrorDescription> run(CompilationInfo compilationInfo,
+                                      TreePath treePath) {
+        try {
+            Document doc = compilationInfo.getDocument();
+            
+            if (doc == null) {
+                return null;
+            }
+        
+            int[] span = new int[2];
+            List<Fix> fixes = computeFixes(compilationInfo, treePath, doc, span);
+            if (fixes == null) {
+                return null;
+            }
+
+            ErrorDescription ed = ErrorDescriptionFactory.createErrorDescription(
+                getSeverity().toEditorSeverity(),
+                NbBundle.getMessage(StaticAccess.class, "MSG_StaticAccess"), // NOI18N
+                fixes,
+                doc,
+                doc.createPosition(span[0]),
+                doc.createPosition(span[1]) // NOI18N
+            );
+
+            return Collections.singletonList(ed);
+        } catch (BadLocationException e) {
+            Exceptions.printStackTrace(e);
+        } catch (IOException e) {
+            Exceptions.printStackTrace(e);
+        }
+        
+        return null;
+    }
+
+    public String getId() {
+        return getClass().getName();
+    }
+
+    public String getDisplayName() {
+        return NbBundle.getMessage(DoubleCheck.class, "MSG_StaticAccessName"); // NOI18N
+    }
+
+    public String getDescription() {
+        return NbBundle.getMessage(DoubleCheck.class, "HINT_StaticAccess"); // NOI18N
+    }
+
+    public void cancel() {
+        // XXX implement me 
+    }
+    
+    public Preferences getPreferences() {
+        return null;
+    }
+    
+    @Override
+    public JComponent getCustomizer(Preferences node) {
+        return null;
+    }    
+
+    static final class FixImpl implements Fix, CancellableTask<WorkingCopy> {
+        private final TreePathHandle expr;
+        private final TreePathHandle type;
+        private final FileObject file;
+        
+        public FixImpl(TreePathHandle expr, TreePathHandle type, FileObject file) {
+            this.file = file;
+            this.type = type;
+            this.expr = expr;
+        }
+        
+        
+        public String getText() {
+            return NbBundle.getMessage(DoubleCheck.class, "MSG_StaticAccessText"); // NOI18N
+        }
+        
+        public ChangeInfo implement() throws IOException {
+            ModificationResult result = JavaSource.forFileObject(file).runModificationTask(this);
+            result.commit();
+            return null;
+        }
+        
+        @Override public String toString() {
+            return "FixStaticAccess"; // NOI18N
+        }
+
+        public void cancel() {
+        }
+
+        public void run(WorkingCopy copy) throws Exception {
+            copy.toPhase(JavaSource.Phase.RESOLVED);
+            TreePath path = expr.resolve(copy);
+            Element element = type.resolveElement(copy);
+            IdentifierTree idt = copy.getTreeMaker().Identifier(element);
+            copy.rewrite(path.getLeaf(), idt);
+        }
+    }
+    
+}
---
+++ java.hints/test/unit/src/org/netbeans/modules/java/hints/StaticAccessTest.java
@@ -0,0 +1,116 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+package org.netbeans.modules.java.hints;
+
+import com.sun.source.util.TreePath;
+import java.util.List;
+import java.util.Locale;
+import org.netbeans.api.java.source.CompilationInfo;
+import org.netbeans.api.java.source.SourceUtilsTestUtil;
+import org.netbeans.modules.java.hints.infrastructure.TreeRuleTestBase;
+import org.netbeans.spi.editor.hints.ErrorDescription;
+
+/**
+ *
+ * @author Jaroslav Tulach
+ */
+public class StaticAccessTest extends TreeRuleTestBase {
+    
+    public StaticAccessTest(String testName) {
+        super(testName);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        Locale.setDefault(Locale.US);
+        SourceUtilsTestUtil.setLookup(new Object[0], getClass().getClassLoader());
+    }
+    
+    
+    
+    public void testCallingStaticMethod() throws Exception {
+        String before = "package test; class Test {\n" +
+            "{\n" +
+            "Boolean b = null;\n" +
+            "b = b.valu";
+        String after = "eOf(true);\n" +
+            "}\n" +
+            "}\n";
+
+        String golden = "package test; class Test {" +
+            "{" +
+            "Boolean b = null;" +
+            "b = Boolean.valueOf(true);";
+        
+        
+        performFixTest("test/Test.java", before + after, before.length(), 
+            "3:4-3:5:verifier:Accessing static field",
+            "FixStaticAccess",
+            golden
+        );
+    }
+    public void testOkCallingStaticMethod() throws Exception {
+        String before = "package test; class Test {" +
+            "{" +
+            " Boolean b = null;" +
+            " b = Boolean.valu";
+        String after = "eOf(true);";
+        
+        performAnalysisTest("test/Test.java", before + after, before.length()); 
+    }
+    public void testAccessingStaticField() throws Exception {
+        String before = "package test; class Test {" +
+            "{" +
+            " Boolean b = null;" +
+            " b = b.TR";
+        String after = "UE;";
+        
+        performAnalysisTest("test/Test.java", before + after, before.length(), 
+            "0:50-0:51:verifier:Accessing static field"
+        );
+    }
+    public void testOkAccessingStaticField() throws Exception {
+        String before = "package test; class Test {" +
+            "{" +
+            " Boolean b = null;" +
+            " b = Boolean.TR";
+        String after = "UE;";
+        
+        performAnalysisTest("test/Test.java", before + after, before.length());
+    }
+    public void testAccessingStaticFieldViaMethod() throws Exception {
+        String before = "package test; class Test {" +
+            "static Boolean b() { return null; }" +
+            "{" +
+            " Object x = b().TR";
+        String after = "UE;";
+        
+        performAnalysisTest("test/Test.java", before + after, before.length(), 
+            "0:74-0:77:verifier:Accessing static field"
+        );
+    }
+
+    protected List<ErrorDescription> computeErrors(CompilationInfo info, TreePath path) {
+        SourceUtilsTestUtil.setSourceLevel(info.getFileObject(), sourceLevel);
+        return new StaticAccess().run(info, path);
+    }
+    
+    private String sourceLevel = "1.5";
+    
+}
