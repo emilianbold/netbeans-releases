c6c4adcbc83a3d4a9c6565e92bbe3fbbeb1d4bdc
amwilson <amwilson@netbeans.org>
2007-06-14 20:09:57 +0000

    IN=108586 - Update 6 Help with 551 changes

--- mobility.javahelp/javahelp/org/netbeans/modules/mobility/project/docs/mobility.hs
+++ mobility.javahelp/javahelp/org/netbeans/modules/mobility/project/docs/mobility.hs
@@ -1,27 +1,9 @@
 <?xml version='1.0' encoding='ISO-8859-1'?>
 <!DOCTYPE helpset PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN"
                          "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
-<!--
-The contents of this file are subject to the terms of the Common Development
-and Distribution License (the License). You may not use this file except in
-compliance with the License.
-
-You can obtain a copy of the License at http://www.netbeans.org/cddl.html
-or http://www.netbeans.org/cddl.txt.
-
-When distributing Covered Code, include this CDDL Header Notice in each file
-and include the License file at http://www.netbeans.org/cddl.txt.
-If applicable, add the following below the CDDL Header, with the fields
-enclosed by brackets [] replaced by your own identifying information:
-"Portions Copyrighted [year] [name of copyright owner]"
-
-The Original Software is NetBeans. The Initial Developer of the Original
-Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
-Microsystems, Inc. All Rights Reserved.
--->
 
 <helpset version="2.0">
-	<title>Java ME MIDP Development Help</title>
+	<title>Java ME Development Help</title>
 	<maps>
 		<homeID>me.welcome</homeID>
 		<mapref location="map.xml"/>
