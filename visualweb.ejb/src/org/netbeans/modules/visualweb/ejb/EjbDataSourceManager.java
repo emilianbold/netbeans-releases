fd0efbe9e18bd4a186f96241c634d1cc007cd9a5
edwingo <edwingo@netbeans.org>
2007-06-15 00:20:38 +0000

    Change module state directory in userdir to be part of NB filesystem and also migrate contents from the legacy Creator location to the new location. The new location is patterned after the NB library module.

--- visualweb.ejb/src/org/netbeans/modules/visualweb/ejb/EjbDataSourceManager.java
+++ visualweb.ejb/src/org/netbeans/modules/visualweb/ejb/EjbDataSourceManager.java
@@ -6,100 +6,144 @@
 
 package org.netbeans.modules.visualweb.ejb;
 
-import org.netbeans.modules.visualweb.ejb.datamodel.EjbDataModel;
-import org.netbeans.modules.visualweb.ejb.load.EjbDataSourceXmlCreator;
-import org.netbeans.modules.visualweb.ejb.load.EjbDataSourceXmlParser;
-import org.netbeans.modules.visualweb.ejb.load.EjbLoadException;
-import org.netbeans.modules.visualweb.ejb.nodes.EjbLibReferenceHelper;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.util.logging.Level;
+
 import org.netbeans.api.project.Project;
 import org.netbeans.api.project.ui.OpenProjects;
+import org.netbeans.modules.visualweb.ejb.datamodel.EjbDataModel;
+import org.netbeans.modules.visualweb.ejb.load.EjbDataSourceXmlCreator;
+import org.netbeans.modules.visualweb.ejb.load.EjbDataSourceXmlParser;
+import org.netbeans.modules.visualweb.ejb.load.EjbLoadException;
+import org.netbeans.modules.visualweb.ejb.nodes.EjbLibReferenceHelper;
+import org.netbeans.modules.visualweb.ejb.util.Util;
 import org.openide.ErrorManager;
-
+import org.openide.filesystems.FileUtil;
 
 /**
  * To load and save the data model to xml file
- * @author  cao
+ * 
+ * @author cao
  */
 public class EjbDataSourceManager {
-    private final String ejbDataSrcFileName = System.getProperty("netbeans.user") + "/ejb-datasource/ejbsources.xml"; // NOI18N
     public static final String EJB_DATA_SUB_DIR = "ejb-sources"; // NOI18N
-    
+
     // Singlton
     private static EjbDataSourceManager mgr = new EjbDataSourceManager();
-    
+
     private OpenProjectListKeeper openProjectList;
-    
-    
+
     public static EjbDataSourceManager getInstance() {
         return mgr;
     }
-    
-    private EjbDataSourceManager() {}
-    
+
+    private EjbDataSourceManager() {
+    }
+
     /**
      * Load the Ejb modules from the saved xml files
+     * 
+     * @throws IOException
      */
-    public void load() throws EjbLoadException {
+    public void load() throws EjbLoadException, IOException {
         // Load all the information from the xml file if there is one
-        
-        File file = new File( ejbDataSrcFileName );
-        if( file.exists() ) {
-            EjbDataSourceXmlParser parser = new EjbDataSourceXmlParser( ejbDataSrcFileName );
-            EjbDataModel.getInstance().addEjbGroups( parser.parse() );
+
+        String ejbDataSrcFileName = getUserDirDataSrcFileName();
+        File file = new File(ejbDataSrcFileName);
+        if (file.exists()) {
+            EjbDataSourceXmlParser parser = new EjbDataSourceXmlParser(ejbDataSrcFileName);
+            EjbDataModel.getInstance().addEjbGroups(parser.parse());
         }
-        
+
         // Since everything just loaded, reset the modified flag in the data model
         EjbDataModel.getInstance().resetModifiedFlag();
-        
-        openProjectList = new OpenProjectListKeeper( OpenProjects.getDefault().getOpenProjects() );
-        
+
+        openProjectList = new OpenProjectListKeeper(OpenProjects.getDefault().getOpenProjects());
+
         // Listen on the project open event so that we can check whehther any ejb related
         // jar files needed to be updated
-        OpenProjects.getDefault().addPropertyChangeListener( new OpenProjectListener() );
+        OpenProjects.getDefault().addPropertyChangeListener(new OpenProjectListener());
     }
-    
+
     /**
      * Save the data model to the xml files
      */
     public void save() {
         // Do a save only if there is a modification in the data model
-        if( !EjbDataModel.getInstance().isModified() )
+        if (!EjbDataModel.getInstance().isModified())
             return;
-        
+
+        String ejbDataSrcFileName;
         try {
-            File file = new File( ejbDataSrcFileName );
-            
-            if( !file.exists() )
+            ejbDataSrcFileName = getUserDirDataSrcFileName();
+        } catch (IOException e) {
+            Util.getLogger().log(Level.SEVERE, "Unable to save EJB datasource state", e);
+            return;
+        }
+
+        try {
+            File file = new File(ejbDataSrcFileName);
+
+            if (!file.exists())
                 file.createNewFile();
-            
-            OutputStreamWriter outputStreamWriter = new OutputStreamWriter( new FileOutputStream( file), "UTF-8" ); // No I18N
-            BufferedWriter bufferWriter = new BufferedWriter( outputStreamWriter );
-            
-            EjbDataSourceXmlCreator creator = new EjbDataSourceXmlCreator( EjbDataModel.getInstance(), bufferWriter );
+
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(new FileOutputStream(
+                    file), "UTF-8"); // No I18N
+            BufferedWriter bufferWriter = new BufferedWriter(outputStreamWriter);
+
+            EjbDataSourceXmlCreator creator = new EjbDataSourceXmlCreator(EjbDataModel
+                    .getInstance(), bufferWriter);
             creator.toXml();
             bufferWriter.flush();
             bufferWriter.close();
-        } catch( Exception ex ) {
-            ErrorManager.getDefault().getInstance( "org.netbeans.modules.visualweb.ejb.EjbDataSourceManager").log( ErrorManager.ERROR,
-                    "Failed to save ejb datasource to file: " + ejbDataSrcFileName );
+        } catch (Exception ex) {
+            ErrorManager.getDefault().getInstance(
+                    "org.netbeans.modules.visualweb.ejb.EjbDataSourceManager").log(
+                    ErrorManager.ERROR,
+                    "Failed to save ejb datasource to file: " + ejbDataSrcFileName);
             ex.printStackTrace();
         }
     }
-    
+
     private class OpenProjectListener implements PropertyChangeListener {
-        public void propertyChange( PropertyChangeEvent evt ) {
-            
-            if( evt.getPropertyName().equals(OpenProjects.PROPERTY_OPEN_PROJECTS) ) {
-                Project[] projects = openProjectList.getNewlyOpenedProjects( OpenProjects.getDefault().getOpenProjects() );
-                EjbLibReferenceHelper.syncArchiveRefs( projects );
+        public void propertyChange(PropertyChangeEvent evt) {
+
+            if (evt.getPropertyName().equals(OpenProjects.PROPERTY_OPEN_PROJECTS)) {
+                Project[] projects = openProjectList.getNewlyOpenedProjects(OpenProjects
+                        .getDefault().getOpenProjects());
+                EjbLibReferenceHelper.syncArchiveRefs(projects);
             }
         }
-        
+
+    }
+
+    private static String getUserDirDataSrcFileName() throws IOException {
+        File ejbDataSourceDir = new File(Util.getEjbStateDir(), "ejb-datasource");
+        if (!ejbDataSourceDir.isDirectory()) {
+            // Try to migrate EJB DataSources from old to new location
+            String userDir = System.getProperty("netbeans.user"); // NOI18N
+            if (userDir != null) {
+                File legacyDir = new File(userDir, "ejb-datasource"); // NOI18N
+                if (legacyDir.exists()) {
+                    Util.copyFileRecursive(legacyDir, ejbDataSourceDir);
+
+                    // Try to remove the old directory
+                    try {
+                        FileUtil.toFileObject(legacyDir).delete();
+                    } catch (Exception e) {
+                        Util.getLogger().log(Level.WARNING,
+                                "Unable to remove legacy ejb-datasource dir", e);
+                    }
+                }
+            }
+        }
+
+        return new File(ejbDataSourceDir, "ejbsources.xml").getAbsolutePath();
     }
 }
--- visualweb.ejb/src/org/netbeans/modules/visualweb/ejb/util/Util.java
+++ visualweb.ejb/src/org/netbeans/modules/visualweb/ejb/util/Util.java
@@ -7,17 +7,27 @@
 package org.netbeans.modules.visualweb.ejb.util;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
 import java.util.logging.Logger;
+
+import org.openide.filesystems.FileUtil;
+import org.openide.filesystems.Repository;
 
 /**
  * This class contains some utility methods
- *
- * @author  cao
+ * 
+ * @author cao
  */
 public class Util {
-    
+
     /** Logger for visualweb EJB module */
     private static final Logger logger;
+
+    /** Directory in userdir where this modules state is kept */
+    private static File ejbStateDir;
 
     static {
         String utilPkg = Util.class.getPackage().getName();
@@ -25,35 +35,32 @@ public class Util {
         String codeNameBase = utilPkg.substring(0, lastIndex);
         logger = Logger.getLogger(codeNameBase);
     }
-    
+
     /**
      * Get the file name out of the path
      */
-    public static String getFileName( String path )
-    {
+    public static String getFileName(String path) {
         return new File(path).getName();
     }
-    
-    public static String getClassName( String fullPackageClassName )
-    {
-        int i = fullPackageClassName.lastIndexOf( "." );
-        String beanClassName = fullPackageClassName.substring( i+1 );
-            
+
+    public static String getClassName(String fullPackageClassName) {
+        int i = fullPackageClassName.lastIndexOf(".");
+        String beanClassName = fullPackageClassName.substring(i + 1);
+
         return beanClassName;
     }
-    
-    public static String getPackageName( String fullPackageClassName )
-    {
-        int i = fullPackageClassName.lastIndexOf( "." );
-        
-        if( i == -1 ) // No package
+
+    public static String getPackageName(String fullPackageClassName) {
+        int i = fullPackageClassName.lastIndexOf(".");
+
+        if (i == -1) // No package
             i = 0;
-            
-        String packageName = fullPackageClassName.substring( 0, i );
-            
+
+        String packageName = fullPackageClassName.substring(0, i);
+
         return packageName;
     }
-    
+
     /*
      * Utility routine to paper over array type names
      */
@@ -72,49 +79,118 @@ public class Util {
                     sb.append("[]");
                 }
                 return sb.toString();
-            } catch (Throwable e) { /*FALLTHRU*/ }
+            } catch (Throwable e) { /* FALLTHRU */
+            }
         }
         return type.getName();
     }
-    
-    public static String capitalize(String name) 
-    {
-        if( name == null || name.length() == 0 ) {
+
+    public static String capitalize(String name) {
+        if (name == null || name.length() == 0) {
             return name;
         }
-        
+
         char chars[] = name.toCharArray();
         chars[0] = Character.toUpperCase(chars[0]);
         return new String(chars);
     }
-    
-    public static String decapitalize(String name) 
-    {
-        if( name == null || name.length() == 0 ) {
+
+    public static String decapitalize(String name) {
+        if (name == null || name.length() == 0) {
             return name;
         }
-        
+
         char chars[] = name.toCharArray();
         chars[0] = Character.toLowerCase(chars[0]);
         return new String(chars);
     }
-    
-    public static boolean isAcronyn( String name ) 
-    {
+
+    public static boolean isAcronyn(String name) {
         // The name is consider as acronyn if it starts with two or more upper-case letters in a row
-        
-        if( name == null || name.length() == 0 ) {
+
+        if (name == null || name.length() == 0) {
             return false;
         }
-        
+
         char chars[] = name.toCharArray();
-        if( Character.isUpperCase( chars[0] ) && Character.isUpperCase( chars[1] ) )
+        if (Character.isUpperCase(chars[0]) && Character.isUpperCase(chars[1]))
             return true;
         else
             return false;
     }
-    
+
     public static Logger getLogger() {
         return logger;
+    }
+
+    /**
+     * Returns the NetBeans module code name base.
+     * 
+     * @return
+     */
+    public static String getCodeNameBase() {
+        // Code name base should be same as the package name of this class minus the last component
+        String pkgName = Util.class.getPackage().getName();
+        return pkgName.substring(0, pkgName.lastIndexOf('.'));
+    }
+
+    /**
+     * Get the directory where state of this module is kept. This will be in the userdir and also
+     * part of the NetBeans filesystem so that it will make it easier to migrate to future IDE
+     * versions.
+     * 
+     * @return
+     * @throws IOException
+     */
+    public static File getEjbStateDir() {
+        if (ejbStateDir == null) {
+            File root = FileUtil.toFile(Repository.getDefault().getDefaultFileSystem().getRoot());
+            // Follow NetBeans convention of replacing dots with dashes
+            String stateDirName = getCodeNameBase().replace('.', '-');
+            ejbStateDir = new File(root, stateDirName);
+            if (!ejbStateDir.exists() && !ejbStateDir.mkdirs()) {
+                IllegalStateException ex = new IllegalStateException("Unable to create dir: "
+                        + ejbStateDir);
+                throw ex;
+            }
+        }
+        return ejbStateDir;
+    }
+
+    public static void copyFileRecursive(File source, File dest) throws IOException {
+        File newItem = null;
+        if (dest.isDirectory()) {
+            newItem = new File(dest, source.getName());
+        } else {
+            newItem = dest;
+        }
+
+        if (source.isDirectory()) {
+            newItem.mkdir();
+            File[] contents = source.listFiles();
+
+            for (int i = 0; i < contents.length; i++) {
+                copyFileRecursive(contents[i], newItem);
+            }
+        } else {
+            copyFile(source, newItem);
+        }
+    }
+
+    /**
+     * Copy files using nio
+     * 
+     * @param src
+     *            source file
+     * @param dst
+     *            destination file
+     * @throws IOException
+     */
+    public static void copyFile(File src, File dst) throws IOException {
+        FileChannel srcChannel = new FileInputStream(src).getChannel();
+        FileChannel dstChannel = new FileOutputStream(dst).getChannel();
+        dstChannel.transferFrom(srcChannel, 0, srcChannel.size());
+        srcChannel.close();
+        dstChannel.close();
     }
 }
