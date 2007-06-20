aae0998789c83efd2d775fcd9087550cea3ac853
msandor <msandor@netbeans.org>
2007-06-20 10:32:29 +0000

    Added missing tooltip

--- diff/src/org/netbeans/modules/diff/builtin/visualizer/editable/DiffSplitPaneDivider.java
+++ diff/src/org/netbeans/modules/diff/builtin/visualizer/editable/DiffSplitPaneDivider.java
@@ -66,8 +66,8 @@ class DiffSplitPaneDivider extends BasicSplitPaneDivider implements MouseMotionL
         mydivider = new DiffSplitDivider();
         add(mydivider);
         
-        addMouseListener(this);
-        addMouseMotionListener(this);
+        mydivider.addMouseListener(this);
+        mydivider.addMouseMotionListener(this);
     }
 
     public void mouseClicked(MouseEvent e) {
@@ -140,6 +140,7 @@ class DiffSplitPaneDivider extends BasicSplitPaneDivider implements MouseMotionL
         private Map renderingHints;
 
         public DiffSplitDivider() {
+            setToolTipText(""); // NOI18N
             setBackground(UIManager.getColor("SplitPane.background")); // NOI18N
             setOpaque(true);
             renderingHints = (Map)(Toolkit.getDefaultToolkit().getDesktopProperty(
