2f08b08777891d4cdd15c8ee5a43c05b9b8517d8
dkumar <dkumar@netbeans.org>
2007-06-20 03:59:58 +0000

    Port to 6.0

---
+++ j2ee.websphere6/src/org/netbeans/modules/j2ee/websphere6/config/WSModuleConfigurationFactory.java
@@ -0,0 +1,46 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ * 
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ * 
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ * 
+ * Portions Copyrighted 2007 Sun Microsystems, Inc.
+ */
+package org.netbeans.modules.j2ee.websphere6.config;
+
+/**
+ *
+ * @author dkumar
+ */
+
+import org.netbeans.modules.j2ee.deployment.common.api.ConfigurationException;import org.netbeans.modules.j2ee.deployment.devmodules.api.J2eeModule;
+import org.netbeans.modules.j2ee.deployment.plugins.spi.config.ModuleConfiguration;
+import org.netbeans.modules.j2ee.deployment.plugins.spi.config.ModuleConfigurationFactory;
+
+public class WSModuleConfigurationFactory implements ModuleConfigurationFactory {
+
+    /** Creates a new instance of JBModuleConfigurationFactory */
+    public WSModuleConfigurationFactory() {
+    }
+
+    public ModuleConfiguration create(J2eeModule j2eeModule) throws ConfigurationException {
+
+        if (J2eeModule.WAR == j2eeModule.getModuleType()) {
+            //Dileep -- return new WarDeploymentConfiguration(j2eeModule);
+        } else if (J2eeModule.EJB == j2eeModule.getModuleType()) {
+            //Dileep -- return new EjbDeploymentConfiguration(j2eeModule);
+        } else {
+            //Dileep -- return new EarDeploymentConfiguration(j2eeModule);
+        }
+        return null;
+    }
+
+}
