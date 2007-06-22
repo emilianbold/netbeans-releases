c06227e64e6e0e4ce2cc807ffba902b09c28c1fe
pflaska <pflaska@netbeans.org>
2007-06-22 13:48:47 +0000

    Added text method.

--- java.source/test/unit/src/org/netbeans/api/java/source/gen/FeatureAddingTest.java
+++ java.source/test/unit/src/org/netbeans/api/java/source/gen/FeatureAddingTest.java
@@ -13,38 +13,47 @@
  * "Portions Copyrighted [year] [name of copyright owner]"
  *
  * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 package org.netbeans.api.java.source.gen;
 
 import com.sun.source.tree.*;
 import java.io.File;
+import java.io.IOException;
 import java.util.Collections;
 import javax.lang.model.element.Modifier;
 import javax.lang.model.type.TypeKind;
+import org.netbeans.api.java.source.JavaSource;
+import static org.netbeans.api.java.source.JavaSource.*;
+import org.netbeans.api.java.source.Task;
+import org.netbeans.api.java.source.TreeMaker;
+import org.netbeans.api.java.source.WorkingCopy;
 import org.netbeans.jackpot.test.TestUtilities;
 import org.netbeans.modules.java.source.transform.Transformer;
 import org.netbeans.junit.NbTestSuite;
+import org.openide.filesystems.FileUtil;
 
 /**
  *
  * @author Pavel Flaska
  */
-public class FeatureAddingTest extends GeneratorTest {
-    
+public class FeatureAddingTest extends GeneratorTestMDRCompat {
+
     /** Creates a new instance of FeatureAddingTest */
     public FeatureAddingTest(String testName) {
         super(testName);
     }
-    
+
     public static NbTestSuite suite() {
         NbTestSuite suite = new NbTestSuite();
-        suite.addTest(new FeatureAddingTest("testAddFieldToBeginning"));
-        suite.addTest(new FeatureAddingTest("testAddFieldToEnd"));
+        suite.addTestSuite(FeatureAddingTest.class);
+//        suite.addTest(new FeatureAddingTest("testAddFieldToBeginning"));
+//        suite.addTest(new FeatureAddingTest("testAddFieldToEnd"));
+//        suite.addTest(new FeatureAddingTest("testAddFieldToEmpty"));
         return suite;
     }
-    
+
     public void testAddFieldToBeginning() throws Exception {
         testFile = new File(getWorkDir(), "Test.java");
         TestUtilities.copyStringToFile(testFile, 
@@ -77,14 +86,14 @@ public class FeatureAddingTest extends GeneratorTest {
                 public Void visitClass(ClassTree node, Object p) {
                     super.visitClass(node, p);
                     if ("Test".contentEquals(node.getSimpleName())) {
-                        VariableTree member = make.Variable(
-                            make.Modifiers(
-                                Collections.<Modifier>emptySet(),
-                                Collections.<AnnotationTree>emptyList()
-                            ),
-                            "a",
-                            make.PrimitiveType(TypeKind.INT), null
-                        );
+                            VariableTree member = make.Variable(
+                                make.Modifiers(
+                                    Collections.<Modifier>emptySet(),
+                                    Collections.<AnnotationTree>emptyList()
+                                ),
+                                "a",
+                                make.PrimitiveType(TypeKind.INT), null
+                            );
                         ClassTree copy = make.insertClassMember(node, 0, member);
                         changes.rewrite(node, copy);
                     }
@@ -95,7 +104,7 @@ public class FeatureAddingTest extends GeneratorTest {
         String res = TestUtilities.copyFileToString(testFile);
         assertEquals(golden, res);
     }
-    
+
     public void testAddFieldToEnd() throws Exception {
         testFile = new File(getWorkDir(), "Test.java");
         TestUtilities.copyStringToFile(testFile, 
@@ -130,14 +139,14 @@ public class FeatureAddingTest extends GeneratorTest {
                 public Void visitClass(ClassTree node, Object p) {
                     super.visitClass(node, p);
                     if ("Test".contentEquals(node.getSimpleName())) {
-                        VariableTree member = make.Variable(
-                            make.Modifiers(
-                                Collections.<Modifier>emptySet(),
-                                Collections.<AnnotationTree>emptyList()
-                            ),
-                            "a",
-                            make.PrimitiveType(TypeKind.INT), null
-                        );
+                            VariableTree member = make.Variable(
+                                make.Modifiers(
+                                    Collections.<Modifier>emptySet(),
+                                    Collections.<AnnotationTree>emptyList()
+                                ),
+                                "a",
+                                make.PrimitiveType(TypeKind.INT), null
+                            );
                         ClassTree copy = make.addClassMember(node, member);
                         changes.rewrite(node, copy);
                     }
@@ -149,6 +158,46 @@ public class FeatureAddingTest extends GeneratorTest {
         assertEquals(golden, res);
     }
     
+    public void testAddFieldToEmpty() throws Exception {
+        testFile = new File(getWorkDir(), "Test.java");
+        TestUtilities.copyStringToFile(testFile, 
+            "package hierbas.del.litoral;\n" +
+            "\n" +
+            "import java.io.File;\n" +
+            "\n" +
+            "public class Test {\n" +
+            "}\n"
+            );
+        String golden =
+            "package hierbas.del.litoral;\n" +
+            "\n" +
+            "import java.io.File;\n" +
+            "\n" +
+            "public class Test {\n" +
+            "    String s;\n" +
+            "}\n";
+
+        JavaSource src = JavaSource.forFileObject(FileUtil.toFileObject(testFile));
+        Task task = new Task<WorkingCopy>() {
+            
+            public void run(WorkingCopy workingCopy) throws IOException {
+                workingCopy.toPhase(Phase.RESOLVED);
+                BlockTree block = (BlockTree) workingCopy.getTreeUtilities().parseStatement("{ String s; }", null);
+                TreeMaker make = workingCopy.getTreeMaker();
+                CompilationUnitTree cut = workingCopy.getCompilationUnit();
+                ClassTree clazz = (ClassTree) cut.getTypeDecls().get(0);
+                VariableTree member = (VariableTree) block.getStatements().get(0);
+                ClassTree copy = make.insertClassMember(clazz, 0, member);
+                workingCopy.rewrite(clazz, copy);
+            }
+            
+        };
+        src.runModificationTask(task).commit();
+        String res = TestUtilities.copyFileToString(testFile);
+        assertEquals(golden, res);
+    }
+
+
     String getGoldenPckg() {
         return "";
     }
