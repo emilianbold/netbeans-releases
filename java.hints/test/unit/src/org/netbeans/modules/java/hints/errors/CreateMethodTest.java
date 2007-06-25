bbedd295a1ca200ff92c0e6af4288a0765853c57
jlahoda <jlahoda@netbeans.org>
2007-06-25 09:17:59 +0000

    #76498: adding test.

--- java.hints/test/unit/src/org/netbeans/modules/java/hints/errors/CreateMethodTest.java
+++ java.hints/test/unit/src/org/netbeans/modules/java/hints/errors/CreateMethodTest.java
@@ -104,6 +104,14 @@ public class CreateMethodTest extends ErrorHintsTestBase {
                        "package test; public class Test {public void test() {TopLevel.fff();}} class TopLevel { static void fff() { throw new UnsupportedOperationException(\"Not yet implemented\"); } }");
     }
     
+    public void testCreateMethod76498() throws Exception {
+        performFixTest("test/Test.java",
+                       "package test; public class Test {public static class T extends Test {public void test() {super.fff();}}}",
+                       122 - 25,
+                       "CreateMethodFix:fff()void:test.Test",
+                       "package test; public class Test {public static class T extends Test {public void test() {super.fff();}} private void fff() { throw new UnsupportedOperationException(\"Not yet implemented\"); } }");
+    }
+    
     protected List<Fix> computeFixes(CompilationInfo info, int pos, TreePath path) throws IOException {
         List<Fix> fixes = new CreateElement().analyze(info, pos);
         List<Fix> result=  new LinkedList<Fix>();
