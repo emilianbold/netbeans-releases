2d52b211954fccac31806e89d0d7e6e85649a795
krichard <krichard@netbeans.org>
2007-06-28 21:19:59 +0000

    fix for 108431 (partial) - slight changes to spacing and labels in options panels

--- uml.propertysupport/src/org/netbeans/modules/uml/propertysupport/options/panels/ShowMeDialogsListPanel.java
+++ uml.propertysupport/src/org/netbeans/modules/uml/propertysupport/options/panels/ShowMeDialogsListPanel.java
@@ -252,7 +252,7 @@ public class ShowMeDialogsListPanel extends javax.swing.JPanel {
                 .add(jPanel2Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
                     .add(mainLabel)
                     .add(jPanel1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(89, Short.MAX_VALUE))
+                .addContainerGap(127, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
