746ef7cd75926d30cc41f0d9023a86041c82172b
tstupka <tstupka@netbeans.org>
2007-06-28 23:28:23 +0000

    85257 - I18N - Local diff show ghost character if file encoding is not OS's native.

--- versioning.system.cvss/src/org/netbeans/modules/versioning/system/cvss/ui/actions/diff/DiffStreamSource.java
+++ versioning.system.cvss/src/org/netbeans/modules/versioning/system/cvss/ui/actions/diff/DiffStreamSource.java
@@ -166,6 +166,7 @@ public class DiffStreamSource extends StreamSource {
                             Utils.copyStreamsCloseAll(new FileOutputStream(newRemoteFile), new FileInputStream(rf));
                             if (isBase) {
                                 remoteFile = newRemoteFile;
+                                Utils.associateEncoding(file, newRemoteFile);                            
                             }
                         }
                     } catch (Exception e) {
