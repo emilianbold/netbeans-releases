45ad7ad4169c804e9112244276e1ed6528ae19f2
tstupka <tstupka@netbeans.org>
2007-06-29 13:11:49 +0000

    added javadoc

--- localhistory/src/org/netbeans/modules/localhistory/store/LocalHistoryStore.java
+++ localhistory/src/org/netbeans/modules/localhistory/store/LocalHistoryStore.java
@@ -30,14 +30,14 @@ public interface LocalHistoryStore {
     
     
     /**
-     * 
+     * Indicates that the storage has changed.
+     * First parameter: File which history has changed
      */
     public String PROPERTY_CHANGED = "LocalHistoryStore.changed";               // NOI18N  
-    
-    
-    // events
-    
+        
     /**
+     * Marks in the given files history that it was created with timestamp ts. The files content won't 
+     * be copied into the storage until a change is notified via fileChange(). 
      * 
      * @param file 
      * @param ts 
@@ -45,52 +45,52 @@ public interface LocalHistoryStore {
     public void fileCreate(File file, long ts);        
     
     /**
+     * Stores the files actual state under the given timestamp and marks it as deleted
      * 
-     * @param file 
-     * @param ts 
+     * @param file the file which has to be stored
+     * @param ts the timestamp under which the file has to be stored
      */
     public void fileDelete(File file, long ts);
     
-    // XXX merge fileCreateFromMove and fileDeleteFromMove into one call
     /**
+     * Marks in toFile-s history that it was created with timestamp ts as a result from
+     * being moved from fromFile. The toFile-s content won't 
+     * be copied into the storage until a change is notified via fileChange(). 
      * 
-     * @param from 
-     * @param to 
+     * @param fromFile
+     * @param toFile 
      * @param ts 
      */
-    public void fileCreateFromMove(File from, File to, long ts);
+    public void fileCreateFromMove(File fromFile, File toFile, long ts);
     
     /**
+     * Stores fromFile-s actual state under the given timestamp and 
+     * marks that it has been moved to the toFile
      * 
      * @param from 
      * @param to 
      * @param ts 
      */
-    public void fileDeleteFromMove(File from, File to, long ts);
+    public void fileDeleteFromMove(File fromFile, File toFile, long ts);
  
     /**
+     * Stores the files actual state under the given timestamp
      * 
-     * @param file 
-     * @param ts 
+     * @param file the file which has to be stored
+     * @param ts the timestamp under which the file has to be stored
      */
     public void fileChange(File file, long ts);                   
-    
-    
-    // edit 
-    
+        
     /**
+     * Sets a label for an entry represented by the given file and timestamp
      * 
-     * 
-     * @param file 
-     * @param ts 
-     * @param label 
+     * @param file the file for which entry the label has to be set
+     * @param ts timestamp
+     * @param label the label to be set 
      */ 
     public void setLabel(File file, long ts, String label);    
     
-    // listeners 
-    
     /**
-     * 
      * Adds a property change listener
      * 
      * @param l the property change listener
@@ -98,15 +98,11 @@ public interface LocalHistoryStore {
     public void addPropertyChangeListener(PropertyChangeListener l);
     
     /**
-     * 
      * Removes a property change listener
      * 
      * @param l the property change listener
      */
     public void removePropertyChangeListener(PropertyChangeListener l);
-    
-    
-    // data
     
     /**
      * Returns all entries for a file
@@ -127,25 +123,33 @@ public interface LocalHistoryStore {
     public StoreEntry getStoreEntry(File file, long ts);
     
     /**
+     * Return an StoreEntry array representing the given root folders state 
+     * in the history to the given timestamp ts. The files array contains the
+     * actually existing files under root.
      * 
+     * NOT REALY USED YET
      * 
-     * @param root 
-     * @param files 
-     * @param ts 
-     * @return 
+     * @param root the folder for which the StoreEntry array has to be returned
+     * @param files files which actually exist under root
+     * @param ts timestamp to which teh history has to be retrieved
+     * @return StoreEntry array representing the given root folders state 
      */ 
     public StoreEntry[] getFolderState(File root, File[] files, long ts);        
     
     /**
-     * 
+     * Returns StoreEntries for files which are directly 
+     * under the given root folder and:
+     * <ul>
+     *  <li> their youngest entry is marked as deleted
+     *  <li> or have an entry in the storage but don't exist under the given root anymore e.g. externaly deleted
+     * </ul>
      * 
      * @param root 
-     * @return 
+     * @return an array of StoreEntries
      */ 
     public StoreEntry[] getDeletedFiles(File root);    
     
     /**
-     * 
      * Deletes a StoreEntry from the storage represented by the given file and timestamp
      * 
      * @param file the file for which a StoreEntry has to be deleted
@@ -155,11 +159,9 @@ public interface LocalHistoryStore {
     public void deleteEntry(File file, long ts); 
         
     /**
+     * Removes all history information from the storage which is older than now - ttl. 
      * 
-     * Removes all entries, their labels and all tags from the storage 
-     * which are related to a time &lt; System.currentTimeMillis() - ttl;
-     * 
-     * @param ttl 
+     * @param ttl time to live
      */ 
     public void cleanUp(long ttl);    
 }
