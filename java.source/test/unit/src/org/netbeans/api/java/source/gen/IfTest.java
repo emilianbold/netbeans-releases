7b4dbfb02ef09baa7a0850b147978b04cee7f06d
pflaska <pflaska@netbeans.org>
2007-07-02 10:39:28 +0000

    #101672: Test provided in the issue sligthly modified and added to generator suite.

---
+++ java.source/test/unit/src/org/netbeans/api/java/source/gen/IfTest.java
@@ -0,0 +1,121 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+package org.netbeans.api.java.source.gen;
+
+import com.sun.source.tree.BlockTree;
+import com.sun.source.tree.ClassTree;
+import com.sun.source.tree.CompilationUnitTree;
+import com.sun.source.tree.IfTree;
+import com.sun.source.tree.MethodTree;
+import com.sun.source.tree.StatementTree;
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import org.netbeans.junit.NbTestSuite;
+import org.netbeans.api.java.source.JavaSource;
+import org.netbeans.api.java.source.JavaSource.Phase;
+import org.netbeans.api.java.source.Task;
+import org.netbeans.api.java.source.TestUtilities;
+import org.netbeans.api.java.source.TreeMaker;
+import org.netbeans.api.java.source.WorkingCopy;
+import org.netbeans.modules.java.source.transform.Transformer;
+
+/**
+ * Tests if statement creation.
+ */
+public class IfTest extends GeneratorTest {
+
+    public IfTest(String name) {
+        super(name);
+    }
+    
+    public static NbTestSuite suite() {
+        NbTestSuite suite = new NbTestSuite();
+        suite.addTest(new IfTest("testEmptyThenBlock"));
+        return suite;
+    }
+    
+    /**
+     * Test replacing then statement with empty block.
+     */
+    public void testEmptyThenBlock() throws Exception {
+        testFile = new File(getWorkDir(), "IfTest.java");        
+        TestUtilities.copyStringToFile(testFile, 
+            "package foo.bar;\n" +
+            "\n" +
+            "public class IfTest {\n" +
+            "    public void test(boolean b) {\n" +
+            "        if( b )\n" +
+            "            System.out.println();\n" +
+            "    }\n" +
+            "}\n"
+            );
+        String golden =
+            "package foo.bar;\n" +
+            "\n" +
+            "public class IfTest {\n" +
+            "    public void test(boolean b) {\n" +
+            "        if( b ) {\n" +
+            "        }\n" +
+            "    }\n" +
+            "}\n";
+        JavaSource src = getJavaSource(testFile);
+        
+        Task<WorkingCopy> task = new Task<WorkingCopy>() {
+
+            public void run(WorkingCopy workingCopy) throws IOException {
+                workingCopy.toPhase(Phase.RESOLVED);
+                CompilationUnitTree cut = workingCopy.getCompilationUnit();
+                TreeMaker make = workingCopy.getTreeMaker();
+                ClassTree clazz = (ClassTree) cut.getTypeDecls().get(0);
+                MethodTree method = (MethodTree) clazz.getMembers().get(1);
+                IfTree oldIf = (IfTree)method.getBody().getStatements().get(0);
+                BlockTree blk = make.Block(Collections.<StatementTree>emptyList(), false);
+                IfTree newIf = make.If(oldIf.getCondition(), blk, null);
+                workingCopy.rewrite(oldIf, newIf);
+            }
+
+            public void cancel() {
+            }
+        };
+        src.runModificationTask(task).commit();
+        String res = TestUtilities.copyFileToString(testFile);
+        System.err.println(res);
+        assertEquals(golden, res);
+    }
+    
+    class IfVisitor<Void, Object> extends Transformer<Void, Object> {
+        @Override
+        public Void visitIf(IfTree node, Object p) {
+            super.visitIf(node, p);
+            BlockTree emptyBlock = make.Block(Collections.<StatementTree>emptyList(), false);
+            IfTree copy = make.If(node.getCondition(), emptyBlock, null);
+            changes.rewrite(node, copy);
+            return null;
+        }
+    }
+    
+    String getGoldenPckg() {
+        return "";
+    }
+    
+    String getSourcePckg() {
+        return "";
+    }
+}
