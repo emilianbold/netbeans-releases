b3e7fcad5a8772d46d6ba026516421f2e0665778
bohemius <bohemius@netbeans.org>
2007-07-02 15:20:44 +0000

    IN=auto
    fixed tree view

--- mobility.end2end/src/org/netbeans/modules/mobility/end2end/profiles/alljava/AllJavaProfileProvider.java
+++ mobility.end2end/src/org/netbeans/modules/mobility/end2end/profiles/alljava/AllJavaProfileProvider.java
@@ -1,24 +1,38 @@
 package org.netbeans.modules.mobility.end2end.profiles.alljava;
 
+
+
 import org.netbeans.modules.mobility.javon.JavonProfileProvider;
 import org.netbeans.modules.mobility.javon.JavonTemplate;
 import org.netbeans.modules.mobility.javon.JavonSerializer;
 import org.netbeans.modules.mobility.e2e.mapping.JavonMappingImpl;
 import org.netbeans.modules.mobility.e2e.mapping.RealTypeSerializer;
 import org.netbeans.modules.mobility.e2e.mapping.PrimitiveTypeSerializer;
+import org.netbeans.modules.mobility.e2e.mapping.ArrayTypeSerializer;
+
 
 import java.util.List;
 import java.util.Collections;
 import java.util.Arrays;
 
+
+
 /**
+
  *
+
  * User: bohemius
+
  * Date: Apr 19, 2007
+
  * Time: 3:08:49 PM
+
  *
+
  */
+
 public class AllJavaProfileProvider implements JavonProfileProvider {
+
     public String getName() {
         return "alljava"; //NOI18N
     }
@@ -31,7 +45,10 @@ public class AllJavaProfileProvider implements JavonProfileProvider {
         return Collections.<JavonTemplate>emptyList();
     }
 
+
+
     public List<JavonSerializer> getSerializers() {
-        return Arrays.asList(new JavonSerializer[] {new RealTypeSerializer(), new PrimitiveTypeSerializer(), new AllJavaSerializer()});
+        return Arrays.asList(new JavonSerializer[] {new RealTypeSerializer(), new PrimitiveTypeSerializer(), new ArrayTypeSerializer(), new AllJavaSerializer()});
     }
 }
+
--- mobility.end2end/src/org/netbeans/modules/mobility/end2end/profiles/alljava/AllJavaSerializer.java
+++ mobility.end2end/src/org/netbeans/modules/mobility/end2end/profiles/alljava/AllJavaSerializer.java
@@ -1,31 +1,52 @@
 package org.netbeans.modules.mobility.end2end.profiles.alljava;
 
+
 import org.netbeans.modules.mobility.javon.JavonSerializer;
+
 import org.netbeans.modules.mobility.javon.Traversable;
+
 import org.netbeans.modules.mobility.javon.JavonMapping;
+
 import org.netbeans.modules.mobility.e2e.classdata.ClassData;
+
 import org.netbeans.modules.mobility.e2e.classdata.FieldData;
+
 import org.netbeans.modules.mobility.e2e.classdata.MethodParameter;
+
 import org.netbeans.modules.mobility.e2e.classdata.MethodData;
 
+
 import javax.lang.model.type.TypeMirror;
+
 import javax.lang.model.type.TypeKind;
+
 import javax.lang.model.type.DeclaredType;
+import javax.lang.model.type.ArrayType;
+
 import javax.lang.model.element.TypeElement;
+
 import javax.lang.model.element.VariableElement;
+
 import javax.lang.model.element.Modifier;
+
 import javax.lang.model.element.ExecutableElement;
+
 import javax.lang.model.util.ElementFilter;
+
 import java.util.Map;
+
 
 /**
  * User: bohemius
  * Date: Apr 19, 2007
  * Time: 3:09:09 PM
  */
+
 public class AllJavaSerializer implements JavonSerializer {
+
     public String getName() {
         return "All Java Serializer";//NOI18N
+
     }
 
     public boolean isTypeSupported( Traversable traversable, TypeMirror type, Map<String, ClassData> typeCache ) {
@@ -34,21 +55,62 @@ public class AllJavaSerializer implements JavonSerializer {
 
     public ClassData getType( Traversable traversable, TypeMirror type, Map<String, ClassData> typeCache ) {
         if ( TypeKind.DECLARED == type.getKind() ) {
-            TypeElement clazz = (TypeElement) ( (DeclaredType) type ).asElement();
-            String clsName = clazz.getSimpleName().toString();
-            String fqName = clazz.getQualifiedName().toString();
-            String pkgName = "";
-            if ( clsName.length() != fqName.length() )
-                pkgName = fqName.substring( 0, fqName.lastIndexOf( '.' ) );
-            if ( typeCache.containsKey( fqName ) ) {
-                return typeCache.get( fqName );
-            }
-            //add the new class info to the type cache, including its methods and fields
-            else {
-                ClassData clsData = new ClassData( pkgName, clsName, false, false );
-                typeCache.put( fqName, clsData );
+            return getDeclaredType( type, typeCache );
 
-                return clsData;
+        } /*else if ( TypeKind.ARRAY == type.getKind() ) {
+            return getArrayType( type, typeCache );
+
+        }*/
+        return null;
+    }
+
+    private ClassData getDeclaredType( TypeMirror type, Map<String, ClassData> typeCache ) {
+        assert type.getKind()==TypeKind.DECLARED;
+
+        TypeElement clazz = (TypeElement) ( (DeclaredType) type ).asElement();
+        String clsName = clazz.getSimpleName().toString();
+        String fqName = clazz.getQualifiedName().toString();
+        String pkgName = "";
+
+        if ( clsName.length() != fqName.length() )
+            pkgName = fqName.substring( 0, fqName.lastIndexOf( '.' ) );
+
+        if ( typeCache.containsKey( fqName ) ) {
+            return typeCache.get( fqName );
+        }
+        //add the new class info to the type cache, including its methods and fields
+        else {
+            ClassData clsData = new ClassData( pkgName, clsName, false, false );
+            typeCache.put( fqName, clsData );
+            return clsData;
+        }
+    }
+
+    private ClassData getArrayType( TypeMirror type, Map<String, ClassData> typeCache ) {
+        assert type.getKind()==TypeKind.ARRAY;
+
+        ArrayType array = (ArrayType) type;
+        TypeMirror componentType = array.getComponentType();
+
+        if (componentType.getKind()==TypeKind.ARRAY)
+            getArrayType( componentType, typeCache);
+        else {
+            TypeElement componentClazz= (TypeElement) ( ( DeclaredType) type ).asElement();
+            String componentClsName=componentClazz.getSimpleName().toString();
+            String componentFqName=componentClazz.getQualifiedName().toString();
+            String pkgName = "";
+
+            if (componentClsName.length()!= componentFqName.length())
+                pkgName=componentFqName.substring( 0, componentFqName.lastIndexOf( '.' ) );
+
+            if (typeCache.containsKey( componentFqName)) {
+                ClassData component=typeCache.get( componentFqName);
+                if (component.isArray())
+                    return component;
+            } else {
+                ClassData result=new ClassData (pkgName, componentClsName, false, true);
+                typeCache.put( componentFqName, result);
+                return result;
             }
         }
         return null;
@@ -80,6 +142,7 @@ public class AllJavaSerializer implements JavonSerializer {
         for ( FieldData fe : clsData.getFields() ) {
             result.append( fe.getModifier() + " " + fe.getType() + " " + fe.getName() + "\n" );
         }
+
         result.append( "\n" );
         for ( MethodData me : clsData.getMethods() ) {
             result.append( me.getReturnType() + " " + me.getName() + "(" );
@@ -95,3 +158,4 @@ public class AllJavaSerializer implements JavonSerializer {
         return result.toString();
     }
 }
+
