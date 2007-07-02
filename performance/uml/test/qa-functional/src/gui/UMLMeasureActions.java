4fe5aa17cf1c5105b6fc9bb39b958166d80bdc36
rashid <rashid@netbeans.org>
2007-07-02 14:43:04 +0000

    changed order of tests passing

--- performance/uml/test/qa-functional/src/gui/UMLMeasureActions.java
+++ performance/uml/test/qa-functional/src/gui/UMLMeasureActions.java
@@ -34,17 +34,17 @@ public class UMLMeasureActions  {
         NbTestSuite suite = new NbTestSuite();
              
         suite.addTest(new OpenUMLProject("measureTime", "Open UML Project"));
-        suite.addTest(new ReverseEngineering("measureTime", "Reverse Engineering"));
         suite.addTest(new OpenUMLDiagram("measureTime", "Open UML Diagram"));
         suite.addTest(new CreateEmptyDiagram("measureTime", "Create Empty UML Diagram"));
         suite.addTest(new SelectingMultipleNodes("measureTime", "Selecting Multiple Nodes"));
         suite.addTest(new CreateClassDiagramFromMultipleNodes("measureTime", "Create Class Diagram From Multiple Nodes")); 
         suite.addTest(new CreateSequenceDiagramFromMultipleNodes("measureTime", "Create Sequence Diagram From Multiple Nodes"));
-        suite.addTest(new GenerateModelReport("measureTime", "Generate Model Report"));
+        
         suite.addTest(new CodeGenerationFromUMLProject("measureTime", "Code Generation From UML Project"));
         suite.addTest(new ApplyDesignPattern("measureTime", "Apply Design Pattern"));
         suite.addTest(new GenerateDependencyDiagram("measureTime", "Generate Dependency Diagram"));
- 
+        suite.addTest(new ReverseEngineering("measureTime", "Reverse Engineering"));
+        suite.addTest(new GenerateModelReport("measureTime", "Generate Model Report"));
         return suite;
     }
     
