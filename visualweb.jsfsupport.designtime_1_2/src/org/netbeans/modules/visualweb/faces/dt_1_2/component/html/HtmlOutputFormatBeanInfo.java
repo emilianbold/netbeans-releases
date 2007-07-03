64d6dbe78cd753aa5693f9a55f910f6ba27119ce
pegah <pegah@netbeans.org>
2007-07-03 06:56:49 +0000

    Fix for issue # 94671. Modified the beaninfo of Formatted Output to make it inline editable.

--- visualweb.jsfsupport.designtime_1_2/src/org/netbeans/modules/visualweb/faces/dt_1_2/component/html/HtmlOutputFormatBeanInfo.java
+++ visualweb.jsfsupport.designtime_1_2/src/org/netbeans/modules/visualweb/faces/dt_1_2/component/html/HtmlOutputFormatBeanInfo.java
@@ -73,6 +73,8 @@ public class HtmlOutputFormatBeanInfo extends UIOutputBeanInfoBase {
         beanDescriptor.setValue(Constants.BeanDescriptor.TAG_NAME,"outputFormat");
         beanDescriptor.setValue(Constants.BeanDescriptor.TAGLIB_PREFIX,"h");
         beanDescriptor.setValue(Constants.BeanDescriptor.TAGLIB_URI,"http://java.sun.com/jsf/html");
+	beanDescriptor.setValue(Constants.BeanDescriptor.INLINE_EDITABLE_PROPERTIES,
+	        new String[] { "*value://span" }); // NOI18N
 
         return beanDescriptor;
 
