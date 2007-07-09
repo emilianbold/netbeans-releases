7ed052d1a9644182330f49154ef179506277a8ed
dlipin <dlipin@netbeans.org>
2007-07-09 07:19:19 +0000

    i18n work.

--- nbi/engine/src/org/netbeans/installer/utils/ErrorManager.java
+++ nbi/engine/src/org/netbeans/installer/utils/ErrorManager.java
@@ -2,17 +2,17 @@
  * The contents of this file are subject to the terms of the Common Development and
  * Distribution License (the License). You may not use this file except in compliance
  * with the License.
- * 
+ *
  * You can obtain a copy of the License at http://www.netbeans.org/cddl.html or
  * http://www.netbeans.org/cddl.txt.
- * 
+ *
  * When distributing Covered Code, include this CDDL Header Notice in each file and
  * include the License file at http://www.netbeans.org/cddl.txt. If applicable, add
  * the following below the CDDL Header, with the fields enclosed by brackets []
  * replaced by your own identifying information:
- * 
+ *
  *     "Portions Copyrighted [year] [name of copyright owner]"
- * 
+ *
  * The Original Software is NetBeans. The Initial Developer of the Original Software
  * is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun Microsystems, Inc. All
  * Rights Reserved.
@@ -94,40 +94,44 @@ public final class ErrorManager {
         }
         if (exception != null) {
             LogManager.log(level, exception);
-            dialogText += 
-                    "\n\nException:\n  " + 
-                    exception.getClass().getName() + ":\n  " + 
+            dialogText +=
+                    "\n\nException:\n  " +
+                    exception.getClass().getName() + ":\n  " +
                     exception.getMessage();
         }
         
-        dialogText += 
+        dialogText +=
                 "\n\nYou can get more details about the " +
-                "issue in the installer log file:\n" + 
+                "issue in the installer log file:\n" +
                 LogManager.getLogFile().getAbsolutePath();
         
         switch (level) {
             case ErrorLevel.MESSAGE:
                 UiUtils.showMessageDialog(
-                        dialogText, 
-                        "Message", 
+                        dialogText,
+                        ResourceUtils.getString(
+                        ErrorManager.class, ERROR_MESSAGE_KEY),
                         UiUtils.MessageType.INFORMATION);
                 return;
             case ErrorLevel.WARNING:
                 UiUtils.showMessageDialog(
-                        dialogText, 
-                        "Warning", 
+                        dialogText,
+                        ResourceUtils.getString(
+                        ErrorManager.class, ERROR_WARNING_KEY),
                         UiUtils.MessageType.WARNING);
                 return;
             case ErrorLevel.ERROR:
                 UiUtils.showMessageDialog(
-                        dialogText, 
-                        "Error", 
+                        dialogText,
+                         ResourceUtils.getString(
+                        ErrorManager.class, ERROR_ERROR_KEY),
                         UiUtils.MessageType.ERROR);
                 return;
             case ErrorLevel.CRITICAL:
                 UiUtils.showMessageDialog(
-                        dialogText, 
-                        "Critical Error", 
+                        dialogText,
+                        ResourceUtils.getString(
+                        ErrorManager.class, ERROR_CRITICAL_KEY),
                         UiUtils.MessageType.CRITICAL);
                 finishHandler.criticalExit();
                 return;
@@ -161,11 +165,23 @@ public final class ErrorManager {
     // Inner Classes
     public static class ExceptionHandler implements UncaughtExceptionHandler {
         public void uncaughtException(
-                final Thread thread, 
+                final Thread thread,
                 final Throwable exception) {
             ErrorManager.notifyCritical(
-                    "An unexpected exception happened in thread " + thread.getName(), 
+                    ResourceUtils.getString(ErrorManager.class,
+                    ERROR_UNEXPECTED_EXCEPTION_KEY,thread.getName()),
                     exception);
         }
     }
+    private static final String ERROR_UNEXPECTED_EXCEPTION_KEY =
+            "EM.ununexpected.exception";//NOI18N
+    private static final String ERROR_CRITICAL_KEY =
+            "EM.errortype.critical";//NOI18N
+    private static final String ERROR_WARNING_KEY =
+            "EM.errortype.warning";//NOI18N
+    private static final String ERROR_ERROR_KEY =
+            "EM.errortype.error";//NOI18N
+    private static final String ERROR_MESSAGE_KEY =
+            "EM.errortype.message";//NOI18N
+    
 }
