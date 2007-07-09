c848d046656fa8853efe67cbe9512d3e5e48b98f
jglick <jglick@netbeans.org>
2007-07-09 23:41:18 +0000

    #108686: support for JUnit 4.

--- apisupport.project/src/org/netbeans/modules/apisupport/project/Evaluator.java
+++ apisupport.project/src/org/netbeans/modules/apisupport/project/Evaluator.java
@@ -373,12 +373,14 @@ final class Evaluator implements PropertyEvaluator, PropertyChangeListener, AntP
                         // XXX this is ugly, try to look for the JAR using wildcards instead
                         "${netbeans.dest.dir}/ide6/modules/ext/junit-3.8.1.jar:" + // NOI18N
                         "${netbeans.dest.dir}/java1/modules/ext/junit-3.8.2.jar:" + // NOI18N
+                        "${netbeans.dest.dir}/java1/modules/ext/junit-4.1.jar:" + // NOI18N
                         "${netbeans.dest.dir}/testtools/modules/ext/nbjunit.jar:" + // NOI18N
                         "${netbeans.dest.dir}/testtools/modules/ext/insanelib.jar:" + // NOI18N
                         "${netbeans.dest.dir}/testtools/modules/org-netbeans-modules-nbjunit.jar:" + // NOI18N, new for 6.0
                         "${netbeans.dest.dir}/testtools/modules/org-netbeans-modules-nbjunit-ide.jar:" + // NOI18N, new for 6.0
                         "${netbeans.home}/../ide6/modules/ext/junit-3.8.1.jar:" + // NOI18N
                         "${netbeans.home}/../java1/modules/ext/junit-3.8.2.jar:" + // NOI18N
+                        "${netbeans.home}/../java1/modules/ext/junit-4.1.jar:" + // NOI18N
                         "${netbeans.home}/../testtools/modules/ext/nbjunit.jar:" + // NOI18N
                         "${netbeans.home}/../testtools/modules/ext/insanelib.jar:" + // NOI18N
                         "${netbeans.home}/../testtools/modules/org-netbeans-modules-nbjunit.jar:" + // NOI18N, new for 6.0
