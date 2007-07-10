14eb540d6b9e7da827d2d4ebdf586ee8926315a5
ksorokin <ksorokin@netbeans.org>
2007-07-10 16:21:03 +0000

    I18N + minor codestyle

--- nbi/engine/src/org/netbeans/installer/downloader/dispatcher/impl/Worker.java
+++ nbi/engine/src/org/netbeans/installer/downloader/dispatcher/impl/Worker.java
@@ -2,17 +2,17 @@
  * The contents of this file are subject to the terms of the Common Development and
  * Distribution License (the License). You may not use this file except in compliance
  * with the License.
- * 
+ *
  * You can obtain a copy of the License at http://www.netbeans.org/cddl.html or
  * http://www.netbeans.org/cddl.txt.
- * 
+ *
  * When distributing Covered Code, include this CDDL Header Notice in each file and
  * include the License file at http://www.netbeans.org/cddl.txt. If applicable, add
  * the following below the CDDL Header, with the fields enclosed by brackets []
  * replaced by your own identifying information:
- * 
+ *
  *     "Portions Copyrighted [year] [name of copyright owner]"
- * 
+ *
  * The Original Software is NetBeans. The Initial Developer of the Original Software
  * is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun Microsystems, Inc. All
  * Rights Reserved.
@@ -21,53 +21,57 @@
 package org.netbeans.installer.downloader.dispatcher.impl;
 
 import org.netbeans.installer.downloader.dispatcher.Process;
+import org.netbeans.installer.utils.ErrorManager;
 import org.netbeans.installer.utils.helper.NbiThread;
 
 /**
  * @author Danila_Dugurov
  */
 public class Worker extends NbiThread {
-  
-  /////////////////////////////////////////////////////////////////////////////////
-  // Instance
-  
-  Process current;
-  
-  public Worker() {
-    super();
-    setDaemon(true);
-  }
-  
-  //if worker busy return false
-  public synchronized boolean setCurrent(Process newCurrent) {
-    if (!isFree()) return false;
-    this.current = newCurrent;
-    notifyAll();
-    return true;
-  }
-  
-  public synchronized boolean isFree() {
-    return current == null;
-  }
-  
-  public void run() {
-    while (true) {
-      try {
-        Thread.interrupted();
-        synchronized (this) {
-          if (current == null){
-            wait();
-            if (isFree()) continue;
-          }
-        }
-        current.init();
-        current.run();
-      } catch (InterruptedException ignored) {
-      } finally {
-        synchronized (this) {
-          current = null;
-        }
-      }
+    /////////////////////////////////////////////////////////////////////////////////
+    // Instance
+    Process current;
+    
+    public Worker() {
+        setDaemon(true);
     }
-  }
+    
+    // if worker busy return false
+    public synchronized boolean setCurrent(Process newCurrent) {
+        if (!isFree()) {
+            return false;
+        }
+        this.current = newCurrent;
+        notifyAll();
+        return true;
+    }
+    
+    public synchronized boolean isFree() {
+        return current == null;
+    }
+    
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                Thread.interrupted();
+                synchronized (this) {
+                    if (current == null) {
+                        wait();
+                        if (isFree()) {
+                            continue;
+                        }
+                    }
+                }
+                current.init();
+                current.run();
+            } catch (InterruptedException e) {
+                ErrorManager.notifyDebug("Interrupted", e); // NOI18N
+            } finally {
+                synchronized (this) {
+                    current = null;
+                }
+            }
+        }
+    }
 }
--- nbi/engine/src/org/netbeans/installer/downloader/dispatcher/impl/WorkersPool.java
+++ nbi/engine/src/org/netbeans/installer/downloader/dispatcher/impl/WorkersPool.java
@@ -2,17 +2,17 @@
  * The contents of this file are subject to the terms of the Common Development and
  * Distribution License (the License). You may not use this file except in compliance
  * with the License.
- * 
+ *
  * You can obtain a copy of the License at http://www.netbeans.org/cddl.html or
  * http://www.netbeans.org/cddl.txt.
- * 
+ *
  * When distributing Covered Code, include this CDDL Header Notice in each file and
  * include the License file at http://www.netbeans.org/cddl.txt. If applicable, add
  * the following below the CDDL Header, with the fields enclosed by brackets []
  * replaced by your own identifying information:
- * 
+ *
  *     "Portions Copyrighted [year] [name of copyright owner]"
- * 
+ *
  * The Original Software is NetBeans. The Initial Developer of the Original Software
  * is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun Microsystems, Inc. All
  * Rights Reserved.
@@ -28,50 +28,56 @@ import java.util.LinkedList;
  * @author Danila_Dugurov
  */
 public class WorkersPool {
-  
-  /////////////////////////////////////////////////////////////////////////////////
-  // Instance
-  
-  private final int capacity;
-  
-  private int inUse;
-  private Queue<Worker> freeWorkers = new LinkedList<Worker>();
-  
-  public WorkersPool(int poolCapacity) {
-    this.capacity = poolCapacity;
-  }
-  
-  public int capacity() {
-    return capacity;
-  }
-  
-  public synchronized int remaining() {
-    return capacity - inUse;
-  }
-  
-  //noblocking
-  public synchronized Worker tryAcquire() {
-    if (inUse == capacity) return null;
-    inUse++;
-    final Worker worker = freeWorkers.poll();
-    return worker != null && worker.isAlive() ? worker : new Worker();
-  }
-  
-  public synchronized Worker acquire() throws InterruptedException {
-    while (true) {
-      final Worker worker = tryAcquire();
-      if (worker == null) wait();
-      else
-        return worker;
+    /////////////////////////////////////////////////////////////////////////////////
+    // Instance
+    private final int capacity;
+
+    private int inUse;
+    private Queue<Worker> freeWorkers = new LinkedList<Worker>();
+
+    public WorkersPool(final int poolCapacity) {
+        this.capacity = poolCapacity;
     }
-  }
-  
-  public synchronized void release(Worker worker) {
-    inUse--;
-    if (worker.isAlive()) freeWorkers.offer(worker);
-    else if (NEW == worker.getState()) freeWorkers.offer(worker);
-    notify();
-  }
-  
-  public synchronized void stopWaitingWorkers(){}
+
+    public int capacity() {
+        return capacity;
+    }
+
+    public synchronized int remaining() {
+        return capacity - inUse;
+    }
+
+    //noblocking
+    public synchronized Worker tryAcquire() {
+        if (inUse == capacity) {
+            return null;
+        }
+        inUse++;
+        final Worker worker = freeWorkers.poll();
+        return worker != null && worker.isAlive() ? worker : new Worker();
+    }
+
+    public synchronized Worker acquire() throws InterruptedException {
+        while (true) {
+            final Worker worker = tryAcquire();
+            if (worker == null) {
+                wait();
+            } else {
+                return worker;
+            }
+        }
+    }
+
+    public synchronized void release(final Worker worker) {
+        inUse--;
+        if (worker.isAlive()) {
+            freeWorkers.offer(worker);
+        } else if (NEW == worker.getState()) {
+            freeWorkers.offer(worker);
+        }
+        notify();
+    }
+
+    public synchronized void stopWaitingWorkers() {
+    }
 }
