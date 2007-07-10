76eaa0f704b7ec747f349c6ebe7a72eb11d5fb8d
phrebejk <phrebejk@netbeans.org>
2007-07-10 14:44:09 +0000

    #108736 Adding module for java support helpset

---
+++ java.helpset/javahelp/org/netbeans/modules/java/helpset/docs/java.hs
@@ -0,0 +1,16 @@
+<?xml version='1.0' encoding='ISO-8859-1' ?>
+<!--
+*     Copyright © 2006 Sun Microsystems, Inc. All rights reserved.
+*     Use is subject to license terms.
+-->
+<!DOCTYPE helpset
+  PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN"
+         "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
+<helpset version="2.0">
+
+
+  <!-- title -->
+  <title>NetBeans Java Support Modules Help</title>
+  
+
+</helpset>
