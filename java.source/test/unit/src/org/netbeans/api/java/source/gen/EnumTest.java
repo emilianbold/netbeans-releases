374a919a3181accebdd3ae1ce946c16018f1656c
pflaska <pflaska@netbeans.org>
2007-07-12 09:04:47 +0000

    #104839: Regression test, demonstrates the issue.

--- java.source/test/unit/src/org/netbeans/api/java/source/gen/EnumTest.java
+++ java.source/test/unit/src/org/netbeans/api/java/source/gen/EnumTest.java
@@ -20,6 +20,9 @@ package org.netbeans.api.java.source.gen;
 
 import java.io.*;
 import com.sun.source.tree.*;
+import java.util.Collections;
+import javax.lang.model.element.Modifier;
+import javax.lang.model.type.TypeKind;
 import org.netbeans.api.java.source.*;
 import org.netbeans.junit.NbTestSuite;
 import static org.netbeans.api.java.source.JavaSource.*;
@@ -104,6 +107,89 @@ public class EnumTest extends GeneratorTest {
         String res = TestUtilities.copyFileToString(testFile);
         System.err.println(res);
         assertEquals(golden, res);
+    }
+    
+    /**
+     * Test adds the method at the end of the class, demonstrates #104839
+     * 
+     * Original:
+     * 
+     * <code>
+     * public enum Test {
+     *     A, B, C;
+     * }
+     * </code>
+     * 
+     * Expected result:
+     * 
+     * <code>
+     * public enum Test {
+     *     A, B, C;
+     *    
+     *     public void enumMethod() {
+     *     }
+     * }
+     * </code>
+     */
+    public void testAddMethodAfterConstants() throws Exception {
+        testFile = new File(getWorkDir(), "Test.java");
+        TestUtilities.copyStringToFile(testFile, 
+            "package hierbas.del.litoral;\n" +
+            "\n" +
+            "import java.util.*;\n" +
+            "\n" +
+            "public enum Test {\n" +
+            "    A, B, C;\n" +
+            "}\n"
+            );
+        String golden =
+            "package hierbas.del.litoral;\n" +
+            "\n" +
+            "import java.util.*;\n" +
+            "\n" +
+            "public enum Test {\n" +
+            "    A, B, C;\n" +
+            "    \n" +
+            "    public void enumMethod() {\n" +
+            "    }\n" +
+            "}\n";
+        JavaSource src = getJavaSource(testFile);
+        
+        Task<WorkingCopy> task = new Task<WorkingCopy>() {
+
+            public void run(WorkingCopy workingCopy) throws IOException {
+                workingCopy.toPhase(Phase.RESOLVED);
+                CompilationUnitTree cut = workingCopy.getCompilationUnit();
+                TreeMaker make = workingCopy.getTreeMaker();
+                ClassTree clazz = (ClassTree) cut.getTypeDecls().get(0);
+                // create method.
+                ClassTree copy = make.addClassMember(clazz, m(make));
+                workingCopy.rewrite(clazz, copy);
+            }
+
+        };
+        src.runModificationTask(task).commit();
+        String res = TestUtilities.copyFileToString(testFile);
+        System.err.println(res);
+        assertEquals(golden, res);
+    }
+    
+    private MethodTree m(TreeMaker make) {
+        // create method
+        MethodTree newMethod = make.Method(
+            make.Modifiers( 
+                Collections.singleton(Modifier.PUBLIC), // modifiers
+                Collections.<AnnotationTree>emptyList() // annotations
+            ), // modifiers and annotations
+            "enumMethod", // name
+            make.PrimitiveType(TypeKind.VOID), // return type
+            Collections.<TypeParameterTree>emptyList(), // type parameters for parameters
+            Collections.<VariableTree>emptyList(), // parameters
+            Collections.<ExpressionTree>emptyList(),  // throws 
+            make.Block(Collections.<StatementTree>emptyList(), false), // empty statement block
+            null // default value - not applicable here, used by annotations
+        );
+        return newMethod;
     }
     
     String getGoldenPckg() {
