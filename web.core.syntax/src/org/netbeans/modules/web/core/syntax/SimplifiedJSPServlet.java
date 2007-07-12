af76f7ca7777d0fa5b7c1799519019833cda6ace
tslota <tslota@netbeans.org>
2007-07-12 11:47:02 +0000

    #109492: error message "exception must be caught or declared to be thrown" should not appear in scriptlet

--- web.core.syntax/src/org/netbeans/modules/web/core/syntax/SimplifiedJSPServlet.java
+++ web.core.syntax/src/org/netbeans/modules/web/core/syntax/SimplifiedJSPServlet.java
@@ -57,7 +57,10 @@ import static org.netbeans.api.jsp.lexer.JspTokenId.JavaCodeType;
 public class SimplifiedJSPServlet {
 
     private static final String CLASS_HEADER = "\nclass SimplifiedJSPServlet extends HttpServlet {\n" + "\tHttpServletRequest request;\n" + "\tHttpServletResponse response;\n" + "\tHttpSession session;\n" + "\tServletContext application;\n" + "\tJspWriter out;\n" + "\tServletConfig config;\n" + "\tJspContext jspContext;\n" + "\tObject page;\n" + "\tPageContext pageContext;\n"; //NOI18N
-    private static final String METHOD_HEADER = "\n\tvoid mergedScriptlets(){\n\n"; //NOI18N
+
+    private static final String METHOD_HEADER = "\n\tvoid mergedScriptlets() " +
+        "throws javax.servlet.ServletException, java.io.IOException {\n\n"; //NOI18N
+
     private static final String CLASS_FOOTER = "\n\t}\n}"; //NOI18N
     @Deprecated
     private final JspSyntaxSupport sup;
