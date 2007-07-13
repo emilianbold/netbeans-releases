e04f9e4c2866cca95b5eb4b64d55ca09432b53df
mkleint <mkleint@netbeans.org>
2007-07-13 10:13:42 +0000

    #109493 prevent "null" project display name for main projects.

--- projectui/src/org/netbeans/modules/project/ui/ProjectsRootNode.java
+++ projectui/src/org/netbeans/modules/project/ui/ProjectsRootNode.java
@@ -393,7 +393,7 @@ public class ProjectsRootNode extends AbstractNode {
 
                         String result = hstat.annotateNameHtml(super.getDisplayName(), Collections.singleton(fold.getPrimaryFile()));
                         //Make sure the super string was really modified
-                        if (!super.getDisplayName().equals(result)) {
+                        if (result != null && !super.getDisplayName().equals(result)) {
                            return isMain() ? "<b>" + (result) + "</b>" : result; //NOI18N
                         }
                     }
