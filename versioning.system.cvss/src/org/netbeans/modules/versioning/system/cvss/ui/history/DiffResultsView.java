5251931b6958f595b4b64da2585b2f28a8213772
tstupka <tstupka@netbeans.org>
2007-07-17 17:41:18 +0000

    reverting previous commit

--- versioning.system.cvss/src/org/netbeans/modules/versioning/system/cvss/ui/history/DiffResultsView.java
+++ versioning.system.cvss/src/org/netbeans/modules/versioning/system/cvss/ui/history/DiffResultsView.java
@@ -299,6 +299,7 @@ class DiffResultsView implements AncestorListener, PropertyChangeListener {
         private final String revision2;
         private boolean showLastDifference;
         private volatile boolean cancelled;
+        private Thread thread;
 
         public ShowDiffTask(LogInformation header, String revision1, String revision2, boolean showLastDifference) {
             this.header = header;
@@ -309,6 +310,7 @@ class DiffResultsView implements AncestorListener, PropertyChangeListener {
 
         public void run() {
             parent.getUndoRedo().setDiffView(null);            
+            thread = Thread.currentThread();
             final Diff diff = Diff.getDefault();
             final DiffStreamSource s1 = new DiffStreamSource(header.getFile(), revision1, revision1 == VersionsCache.REVISION_CURRENT ? 
                     NbBundle.getMessage(DiffResultsView.class, "LBL_DiffPanel_LocalCopy") : revision1);  // NOI18N
@@ -353,7 +355,10 @@ class DiffResultsView implements AncestorListener, PropertyChangeListener {
         }
 
         public boolean cancel() {
-            cancelled = true;            
+            cancelled = true;
+            if (thread != null) {
+                thread.interrupt();
+            }
             return true;
         }
     }
