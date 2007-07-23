d5db61928e7d951519c5619042fff0061f25179f
pflaska <pflaska@netbeans.org>
2007-07-23 14:13:52 +0000

    #106290: Replace if/then/else with only if/then. Regression test. (Moreover, condition is rewritten too.)

--- java.source/test/unit/src/org/netbeans/api/java/source/gen/IfTest.java
+++ java.source/test/unit/src/org/netbeans/api/java/source/gen/IfTest.java
@@ -22,6 +22,7 @@ import com.sun.source.tree.BinaryTree;
 import com.sun.source.tree.BlockTree;
 import com.sun.source.tree.ClassTree;
 import com.sun.source.tree.CompilationUnitTree;
+import com.sun.source.tree.ExpressionTree;
 import com.sun.source.tree.IfTree;
 import com.sun.source.tree.MemberSelectTree;
 import com.sun.source.tree.MethodTree;
@@ -29,6 +30,7 @@ import com.sun.source.tree.ParenthesizedTree;
 import com.sun.source.tree.StatementTree;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Collections;
 import org.netbeans.junit.NbTestSuite;
 import org.netbeans.api.java.source.JavaSource;
@@ -198,11 +200,71 @@ public class IfTest extends GeneratorTest {
         System.err.println(res);
         assertEquals(golden, res);
     }
-    
+
+    public void testModifyingIf() throws Exception {
+        testFile = new File(getWorkDir(), "Test.java");
+        TestUtilities.copyStringToFile(testFile, 
+            "package personal;\n" +
+            "\n" +
+            "public class Test {\n" +
+            "    public boolean method(int i) {\n" +
+            "        int y = 0;\n" +
+            "        if (i == 0) {\n" +
+            "            y = 2;\n" +
+            "        } else {y = 9;}\n" +
+            "        return y == 8;\n" +
+            "    }\n" +
+            "}\n");
+         String golden = 
+            "package personal;\n" +
+            "\n" +
+            "public class Test {\n" +
+            "    public boolean method(int i) {\n" +
+            "        int y = 0;\n" +
+            "        if (method(null)) {\n" + 
+            "            return true;\n" +
+            "        }\n" +
+            "        return y == 8;\n" +
+            "    }\n" +
+            "}\n";
+        JavaSource src = getJavaSource(testFile);
+        Task<WorkingCopy> task = new Task<WorkingCopy>() {
+
+            public void run(WorkingCopy workingCopy) throws IOException {
+                workingCopy.toPhase(Phase.RESOLVED);
+                TreeMaker make = workingCopy.getTreeMaker();
+                ClassTree clazz = (ClassTree) workingCopy.getCompilationUnit().getTypeDecls().get(0);
+                MethodTree method = (MethodTree) clazz.getMembers().get(1);
+                BlockTree block = method.getBody();
+                IfTree mit = (IfTree) block.getStatements().get(1);
+                IfTree nue = make.If(
+                    make.Parenthesized(make.MethodInvocation(
+                        Collections.<ExpressionTree>emptyList(),
+                        make.Identifier("method"),
+                        Arrays.asList(make.Literal(null)))
+                    ),
+                    make.Block(
+                        Collections.<StatementTree>singletonList(make.Return(make.Literal(true))),
+                        false
+                    ),
+                    null
+                );
+                workingCopy.rewrite(mit, nue);
+            }
+            
+            public void cancel() {
+            }
+        };
+        src.runModificationTask(task).commit();
+        String res = TestUtilities.copyFileToString(testFile);
+        System.err.println(res);
+        assertEquals(golden, res);
+    }
+
     String getGoldenPckg() {
         return "";
     }
-    
+
     String getSourcePckg() {
         return "";
     }
