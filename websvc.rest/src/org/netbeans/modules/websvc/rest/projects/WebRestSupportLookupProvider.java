9cb02f28a9f23782e4ba18d65ad7e8a4304d1563
PeterTLiu <devnull@localhost>
2007-07-23 19:28:40 +0000

    more rest model changes.

--- websvc.rest/src/org/netbeans/modules/websvc/rest/projects/WebRestSupportLookupProvider.java
+++ websvc.rest/src/org/netbeans/modules/websvc/rest/projects/WebRestSupportLookupProvider.java
@@ -59,7 +59,6 @@ public class WebRestSupportLookupProvider implements LookupProvider {
             PropertyChangeListener pcl;
             
             protected void projectOpened() {
-                System.out.println("REST web project opened");
                 RestSupport support = prj.getLookup().lookup(RestSupport.class);
                 
                 if (support!=null) {
@@ -68,13 +67,8 @@ public class WebRestSupportLookupProvider implements LookupProvider {
                         wsModel.runReadActionWhenReady(new MetadataModelAction<RestServicesMetadata, Void>() {
                             public Void run(final RestServicesMetadata metadata) {
                                 RestServices restServices = metadata.getRoot();
-                                System.out.println("restServices = " + restServices);
                                 pcl = new RestServicesChangeListener(wsModel, prj);
-                                restServices.addPropertyChangeListener(pcl);
-                                
-                                for (RestServiceDescription desc : restServices.getRestServiceDescription()) {
-                                    System.out.println("desc = " + desc);
-                                }
+                                restServices.addPropertyChangeListener(pcl);                  
                                 return null;
                             }
                         });
@@ -111,6 +105,7 @@ public class WebRestSupportLookupProvider implements LookupProvider {
     private class RestServicesChangeListener implements PropertyChangeListener {
         MetadataModel<RestServicesMetadata> wsModel;
         Project prj;
+        RestSupport support;
         
         private RequestProcessor.Task updateRestSvcTask = RequestProcessor.getDefault().create(new Runnable() {
             public void run() {
@@ -121,6 +116,7 @@ public class WebRestSupportLookupProvider implements LookupProvider {
         RestServicesChangeListener(MetadataModel<RestServicesMetadata> wsModel, Project prj) {
             this.wsModel=wsModel;
             this.prj=prj;
+            this.support = prj.getLookup().lookup(RestSupport.class);
         }
         
         public void propertyChange(PropertyChangeEvent evt) {
@@ -132,11 +128,17 @@ public class WebRestSupportLookupProvider implements LookupProvider {
             System.out.println("updating rest services");
             try {
                 wsModel.runReadAction(new MetadataModelAction<RestServicesMetadata, Void>() {
-                    public Void run(RestServicesMetadata metadata) {
+                    public Void run(RestServicesMetadata metadata) throws IOException {
                         RestServices root = metadata.getRoot();
                         for (RestServiceDescription desc : root.getRestServiceDescription()) {
-                            System.out.println("desc = " + desc);
-                           
+                            System.out.println("desc = " + desc);                          
+                        }
+                        
+                        if (root.getRestServiceDescription().length > 0) {
+                            System.out.println("Turning rest support on");
+                            support.ensureRestDevelopmentReady();
+                        } else {
+                            //TODO turn isRestSupportOn flag off.
                         }
                         
                         return null;
@@ -145,82 +147,6 @@ public class WebRestSupportLookupProvider implements LookupProvider {
             } catch (IOException ex) {
                 
             }
-//            try {
-//               Map<String,String> newServices = wsModel.runReadAction(new MetadataModelAction<RestServicesMetadata, Map<String,String>>() {
-//                    public Map<String,String> run(RestServicesMetadata metadata) {
-//                        Map<String,String> result = new HashMap<String,String>();
-//                        RestServices restServices = metadata.getRoot();
-//                        for (RestServiceDescription wsDesc:restServices.getRestServiceDescription()) {
-//                            PortComponent[] ports = wsDesc.getPortComponent();
-//                            for (PortComponent port:ports) {
-//                                result.put(port.getDisplayName(), wsDesc.getDisplayName());
-//                            }
-//                            
-//                        }
-//                        return result;
-//                    }
-//                });
-//                
-//                final JaxWsModel jaxWsModel = prj.getLookup().lookup(JaxWsModel.class);
-//                if (jaxWsModel!=null) {
-//                    // create list of all existing services (from java)
-//                    Map<String,String> oldServices = new HashMap<String,String>();
-//                    Service[] allServices = jaxWsModel.getServices();
-//                    
-//                    for (Service s:allServices) {
-//                        // add only services created from java
-//                        if (s.getWsdlUrl() == null) {
-//                            oldServices.put(s.getImplementationClass(),s.getName());
-//                        }
-//                    }
-//                    // compare new services with existing
-//                    
-//                    // looking for common services (implementationClass)
-//                    Set<String> commonServices = new HashSet<String>();
-//                    Set<String> keys1 = oldServices.keySet();
-//                    Set<String> keys2 = newServices.keySet();
-//                    for(String key:keys1) {
-//                        if (keys2.contains(key)) commonServices.add(key);
-//                    }
-//                    
-//                    for (String key:commonServices) {
-//                        oldServices.remove(key);
-//                        newServices.remove(key);
-//                    }
-//                    // remove old services
-//                    boolean needToSave =false;
-//                    for (String key:oldServices.keySet()) {
-//                        jaxWsModel.removeService(oldServices.get(key));
-//                        needToSave=true;
-//                    }
-//                    // add new services
-//                    for (String key:newServices.keySet()) {
-//                        // add only if doesn't exists
-//                        if (jaxWsModel.findServiceByImplementationClass(key) == null) {
-//                            try {
-//                                jaxWsModel.addService(newServices.get(key), key);
-//                                needToSave=true;
-//                            } catch (ServiceAlreadyExistsExeption ex) {
-//                                // TODO: need to handle this
-//                            }
-//                        }
-//                    }
-//                    if (needToSave) {
-//                        ProjectManager.mutex().writeAccess(new Runnable() {
-//                            public void run() {
-//                                try {
-//                                    jaxWsModel.write();
-//                                } catch (IOException ex) {
-//                                    ErrorManager.getDefault().notify(ex);
-//                                }
-//                            }
-//                            
-//                        });
-//                    }
-//                }
-//            } catch(java.io.IOException ioe) {
-//                ErrorManager.getDefault().notify(ioe);
-//            }
         }
     }
     
