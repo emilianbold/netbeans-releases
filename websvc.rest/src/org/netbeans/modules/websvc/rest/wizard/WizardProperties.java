7bfdbbdeb03eecc449e1ff90e896b74a6e1bfb0f
nnguyen <nnguyen@netbeans.org>
2007-07-27 14:55:03 +0000

    Allows user to specify non-default packages for converter and resource classes

--- websvc.rest/src/org/netbeans/modules/websvc/rest/wizard/WizardProperties.java
+++ websvc.rest/src/org/netbeans/modules/websvc/rest/wizard/WizardProperties.java
@@ -24,7 +24,8 @@ public class WizardProperties {
     public static final String DESCRIPTION = "Description"; // NOI18N
     public static final String SCOPE = "Scope"; // NOI18N
     public static final String ENTITY_CLASSES = "EntityClasses"; //NOI18N
-    public static final String REST_CLASSES_PACKAGE = "RESTBean"; //NOI18N
+    public static final String RESOURCE_PACKAGE = "resourcePackage"; //NOI18N
+    public static final String CONVERTER_PACKAGE = "converterPackage"; //NOI18N
     public static final String PERSISTENCE_UNIT_NAME = "PersistenceUnitName";       //NOI18N
     public static final String ENTITY_RESOURCE_MODEL = "EntityResourceModel";       //NOI18N
     
