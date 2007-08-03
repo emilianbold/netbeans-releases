e1573f0fbecd52158a7ac18780612ad59292182c
alexeyyarmolenko <alexeyyarmolenko@netbeans.org>
2007-08-03 13:37:30 +0000

    Fix for 105269 Added check to avoid endless recursion on recursive schemas

--- xslt.mapper/src/org/netbeans/modules/xslt/mapper/model/SchemaModelBridge.java
+++ xslt.mapper/src/org/netbeans/modules/xslt/mapper/model/SchemaModelBridge.java
@@ -2,16 +2,16 @@
  * The contents of this file are subject to the terms of the Common Development
  * and Distribution License (the License). You may not use this file except in
  * compliance with the License.
- * 
+ *
  * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
  * or http://www.netbeans.org/cddl.txt.
- * 
+ *
  * When distributing Covered Code, include this CDDL Header Notice in each file
  * and include the License file at http://www.netbeans.org/cddl.txt.
  * If applicable, add the following below the CDDL Header, with the fields
  * enclosed by brackets [] replaced by your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- * 
+ *
  * Portions Copyrighted 2007 Sun Microsystems, Inc.
  */
 package org.netbeans.modules.xslt.mapper.model;
@@ -21,26 +21,30 @@ import org.netbeans.modules.xslt.mapper.view.XsltMapper;
 /**
  *
  * @author Alexey
- * Keeps mapper trees in-sync with schema models 
+ * Keeps mapper trees in-sync with schema models
  * Subscribes on changes in schema model and reloads left and right trees
- * 
+ *
  */
-public class SchemaModelBridge extends ModelBridge{
+public class SchemaModelBridge extends ModelBridge {
 
     public SchemaModelBridge(XsltMapper mapper) {
         super(mapper);
-        super.subscribe(mapper.getContext().getSourceType().getModel());
-        super.subscribe(mapper.getContext().getTargetType().getModel());
+        if (mapper.getContext().getSourceType() != null) {
+            super.subscribe(mapper.getContext().getSourceType().getModel());
+        }
         
-                
+        if (mapper.getContext().getTargetType() != null) {
+            super.subscribe(mapper.getContext().getTargetType().getModel());
+        }
     }
+
     protected void onModelChanged() {
         if (!checkErrors()) {
             return;
         }
         super.reloadTree(getMapper().getMapperViewManager().getDestView().getTree());
         super.reloadTree(getMapper().getMapperViewManager().getSourceView().getTree());
-        
+
         getMapper().getBuilder().updateDiagram();
     }
 }
--- xslt.mapper/src/org/netbeans/modules/xslt/mapper/view/DiagramBuilder.java
+++ xslt.mapper/src/org/netbeans/modules/xslt/mapper/view/DiagramBuilder.java
@@ -32,6 +32,8 @@ import org.netbeans.modules.xslt.mapper.model.BuildExpressionVisitor;
 import org.netbeans.modules.xslt.mapper.model.nodes.LiteralCanvasNode;
 import org.netbeans.modules.xslt.mapper.model.nodes.Node;
 import org.netbeans.modules.xslt.mapper.model.nodes.TreeNode;
+import org.netbeans.modules.xslt.mapper.model.targettree.ElementDeclarationNode;
+import org.netbeans.modules.xslt.mapper.model.targettree.StylesheetNode;
 import org.netbeans.modules.xslt.model.XslComponent;
 import org.netbeans.modules.xslt.model.XslComponent;
 
@@ -40,24 +42,25 @@ import org.netbeans.modules.xslt.model.XslComponent;
  * @author Alexey
  */
 public class DiagramBuilder {
+
     boolean updating = false;
     private XsltMapper mapper;
     private boolean relayoutRequired;
     private PredicateFinderVisitor pfv;
-    
+
     public DiagramBuilder(XsltMapper mapper) {
         this.mapper = mapper;
         pfv = new PredicateFinderVisitor(mapper);
     }
-    
-    public void updateDiagram(){
+
+    public void updateDiagram() {
         TreeNode root = (TreeNode) mapper.getMapperViewManager()
                 .getDestView()
                 .getTree()
                 .getModel()
                 .getRoot();
-        
-        if (root != null){
+
+        if (root != null) {
             relayoutRequired = false;
             try {
                 updating = true;
@@ -65,98 +68,97 @@ public class DiagramBuilder {
             } finally {
                 updating = false;
             }
-            if (relayoutRequired){
+            if (relayoutRequired) {
                 //this flag indicates that at least 1 node was added to diagram
                 mapper.getMapperViewManager().getCanvasView().getAutoLayout().autoLayout();
             }
         }
     }
-    
-    public boolean isUpdating(){
+
+    public boolean isUpdating() {
         return this.updating;
     }
+
     /**
      * Detects if given target tree node contains xpath expression.
      * If so, builds expression node graph starting from this tree node
      **/
-    public void updateDiagram(TreeNode tree_node){
-        
+    public void updateDiagram(TreeNode tree_node) {
+
         Object data = tree_node.getDataObject();
-        
-        if (!(data instanceof XslComponent)){
+
+        if (!(data instanceof XslComponent)) {
             return;
         }
-        
+
         XslComponent xslc = (XslComponent) data;
-        
+
         GetExpressionVisitor visitor_ge = new GetExpressionVisitor();
-        
+
         xslc.accept(visitor_ge);
         XPathExpression new_expr = visitor_ge.getExpression();
-        
+
         // Look for predicates in the specified XPath expression
         if (new_expr != null) {
             pfv.setContextXslComponent(xslc);
             new_expr.accept(pfv);
         }
-        
+
         //first check, if current node is already connected to any grap
         List<Node> upstreams = tree_node.getPreviousNodes();
-        
+
         XPathExpression current_expr = null;
-        
-        
-        if (!upstreams.isEmpty()){
+
+
+        if (!upstreams.isEmpty()) {
             Node upstream = upstreams.get(0);
             if (upstream != null) {
-                BuildExpressionVisitor visitor_be =
-                        new BuildExpressionVisitor(mapper.getContext());
+                BuildExpressionVisitor visitor_be = new BuildExpressionVisitor(mapper.getContext());
 
                 upstream.accept(visitor_be);
                 current_expr = visitor_be.getResult();
             }
         }
-        
-        if (current_expr != null){
+
+        if (current_expr != null) {
             if (new_expr == null || !current_expr.toString().equals(new_expr.toString())) {
                 destroyDiagramRecursive(tree_node);
             }
-            
         }
-        if (new_expr != null){
+        if (new_expr != null) {
             if (current_expr == null || !new_expr.toString().equals(current_expr.toString())) {
-                
+
                 relayoutRequired = true;
-                
+
                 Node graph_root = buildDiagramRecursive(new_expr);
-                if (graph_root != null){
+                if (graph_root != null) {
                     mapper.addLink(graph_root, tree_node);
                 }
             }
         }
     }
-    public void destroyDiagramRecursive(Node node){
-        
+
+    public void destroyDiagramRecursive(Node node) {
+
         List<Node> upstreams = node.getPreviousNodes();
-        
-        for(Node upstream: upstreams){
+
+        for (Node upstream : upstreams) {
             if (upstream == null) {
                 continue;
             }
-            
-            IBasicViewModel model =
-                    mapper.getMapperViewManager().getMapperModel().getSelectedViewModel();
-            
+
+            IBasicViewModel model = mapper.getMapperViewManager().getMapperModel().getSelectedViewModel();
+
             mapper.removeDirectedChain(upstream.getMapperNode(), model);
         }
-        
-        
+
+
         List links = node.getMapperNode().getLinks();
         for (int i = 0; i < links.size(); i++) {
             mapper.removeLink((IMapperLink) links.get(i));
         }
-        
-        
+
+
         //        List<Node> upstreams = node.getPreviousNodes();
         //        List links = node.getOutputNode().getLinks();
         //
@@ -169,53 +171,52 @@ public class DiagramBuilder {
         //        }
         //
     }
-    
+
     /**
      * Perform recursion deep into expression.
      * Create mapper nodes for all subexpressions and add them to canvas
      **/
-    private Node buildDiagramRecursive(XPathExpression expression){
+    private Node buildDiagramRecursive(XPathExpression expression) {
         NodeCreatorVisitor visitor_cn = new NodeCreatorVisitor(mapper);
-        
+
         expression.accept(visitor_cn);
-        
+
         Node node = visitor_cn.getResult();
-        
-        if (node == null){
+
+        if (node == null) {
             return null;
         }
-        
+
         IMapperNode mapper_node = (IMapperNode) node.getMapperNode();
-        
-        if (!(mapper_node instanceof IMapperTreeNode)){
+
+        if (!(mapper_node instanceof IMapperTreeNode)) {
             mapper.addNode(mapper_node);
             //hack required by stupid Mapper framework.
             //aaddNode resets the literalName.
-            if (node instanceof LiteralCanvasNode){
+            if (node instanceof LiteralCanvasNode) {
                 IFieldNode field0 = (IFieldNode) node.getOutputNode();
                 field0.setLiteralName(expression.toString());
             }
-            
-            if (mapper_node instanceof IMethoidNode &&
-                    expression instanceof XPathOperationOrFuntion) {
+
+            if (mapper_node instanceof IMethoidNode && expression instanceof XPathOperationOrFuntion) {
                 IMethoidNode methoidNode = (IMethoidNode) mapper_node;
-                IMethoid methoid = (IMethoid)methoidNode.getMethoidObject();
+                IMethoid methoid = (IMethoid) methoidNode.getMethoidObject();
                 boolean isAccumulative = methoid.isAccumulative();
                 int fieldIndex = 0;
                 //
-                for (Object expr: ((XPathOperationOrFuntion) expression).getChildren()){
+                for (Object expr : ((XPathOperationOrFuntion) expression).getChildren()) {
                     List inputFields = methoidNode.getInputFieldNodes();
-                    if (fieldIndex >= inputFields.size() ){
+                    if (fieldIndex >= inputFields.size()) {
                         break;
                     }
-                    IFieldNode fn = (IFieldNode)inputFields.get(fieldIndex);
+                    IFieldNode fn = (IFieldNode) inputFields.get(fieldIndex);
                     assert fn != null : "Filed shouldn't be null anyway"; // NOI18N
-                    if(!expr.toString().startsWith(BuildExpressionVisitor.UNCONNECTED_INPUT)){
+                    if (!expr.toString().startsWith(BuildExpressionVisitor.UNCONNECTED_INPUT)) {
                         Node upstream_node = buildDiagramRecursive((XPathExpression) expr);
-                        
+
                         fn.setNodeObject(upstream_node);
-                        
-                        if (upstream_node != null){
+
+                        if (upstream_node != null) {
                             mapper.addLink(upstream_node, node);
                         }
                     }
@@ -225,11 +226,11 @@ public class DiagramBuilder {
         }
         return node;
     }
-    
+
     private boolean methoidHasField(IMethoidNode methoidNode, String fieldName) {
         for (Object fieldObj : methoidNode.getInputFieldNodes()) {
             if (fieldObj instanceof IFieldNode) {
-                IFieldNode tempFieldNode = (IFieldNode)fieldObj;
+                IFieldNode tempFieldNode = (IFieldNode) fieldObj;
                 String tempFieldName = tempFieldNode.getName();
                 if (fieldName.equals(tempFieldName)) {
                     return true;
@@ -238,17 +239,17 @@ public class DiagramBuilder {
         }
         return false;
     }
-    
+
     /**
      * Iterates over target tree nodes
      * calls updateDiagram() for each node
      **/
-    private void updateDiagramRecursive(TreeNode node){
-        updateDiagram(node);
-        for( TreeNode n: node.getChildren()){
-            updateDiagramRecursive(n);
+    private void updateDiagramRecursive(TreeNode node) {
+        if (node instanceof StylesheetNode) {
+            updateDiagram(node);
+            for (TreeNode n : node.getChildren()) {
+                updateDiagramRecursive(n);
+            }
         }
     }
-    
-    
 }
