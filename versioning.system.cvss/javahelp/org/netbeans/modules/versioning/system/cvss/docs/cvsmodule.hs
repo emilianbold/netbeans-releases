221d65ed5601789c38160564051087470f824291
tgiunipero <tgiunipero@netbeans.org>
2007-08-05 22:13:57 +0000

    modified copyright text. minor modifications.

--- versioning.system.cvss/javahelp/org/netbeans/modules/versioning/system/cvss/docs/cvsmodule.hs
+++ versioning.system.cvss/javahelp/org/netbeans/modules/versioning/system/cvss/docs/cvsmodule.hs
@@ -1,6 +1,6 @@
 <?xml version='1.0' encoding='ISO-8859-1' ?>
 <!--
-*     Copyright Â© 2006 Sun Microsystems, Inc. All rights reserved.
+*     Copyright © 2007 Sun Microsystems, Inc. All rights reserved.
 *     Use is subject to license terms.
 -->
 <!DOCTYPE helpset
