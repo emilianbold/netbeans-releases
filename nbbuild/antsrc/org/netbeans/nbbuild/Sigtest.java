46b2bb872d7ba4a540d843693afb410aefe48d29
mzlamal <mzlamal@netbeans.org>
2007-08-07 12:41:25 +0000

    Add CDDL

--- nbbuild/antsrc/org/netbeans/nbbuild/Sigtest.java
+++ nbbuild/antsrc/org/netbeans/nbbuild/Sigtest.java
@@ -1,18 +1,23 @@
 /*
- * Sigtest.java
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
  *
- * Created on August 1, 2007, 11:36 AM
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
+ * Microsystems, Inc. All Rights Reserved.
  */
 
 package org.netbeans.nbbuild;
-
-// IMPORTANT! You need to compile this class against ant.jar. So add the
-// JAR ide5/ant/lib/ant.jar from your IDE installation directory (or any
-// other version of Ant you wish to use) to your classpath. Or if
-// writing your own build target, use e.g.:
-// <classpath>
-//     <pathelement location="${ant.home}/lib/ant.jar"/>
-// </classpath>
 
 import java.io.File;
 import java.util.StringTokenizer;
