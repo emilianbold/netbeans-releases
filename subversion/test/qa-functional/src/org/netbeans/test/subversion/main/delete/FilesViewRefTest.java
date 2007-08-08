792204a4f328cdd6c2103993c4d1786f5c454487
novakm <novakm@netbeans.org>
2007-08-08 12:51:04 +0000

    New test - refactoring in files vies

---
+++ subversion/test/qa-functional/src/org/netbeans/test/subversion/main/delete/FilesViewRefTest.java
@@ -0,0 +1,151 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * Portions Copyrighted 2007 Sun Microsystems, Inc.
+ */
+package org.netbeans.test.subversion.main.delete;
+
+import java.io.File;
+import java.io.PrintStream;
+import junit.textui.TestRunner;
+import org.netbeans.jellytools.FilesTabOperator;
+import org.netbeans.jellytools.JellyTestCase;
+import org.netbeans.jellytools.NbDialogOperator;
+import org.netbeans.jellytools.OutputOperator;
+import org.netbeans.jellytools.OutputTabOperator;
+import org.netbeans.jellytools.nodes.Node;
+import org.netbeans.jellytools.nodes.SourcePackagesNode;
+import org.netbeans.jemmy.operators.JButtonOperator;
+import org.netbeans.jemmy.operators.JTableOperator;
+import org.netbeans.jemmy.operators.Operator;
+import org.netbeans.jemmy.operators.Operator.DefaultStringComparator;
+import org.netbeans.junit.NbTestSuite;
+import org.netbeans.test.subversion.operators.CheckoutWizardOperator;
+import org.netbeans.test.subversion.operators.CommitOperator;
+import org.netbeans.test.subversion.operators.RepositoryStepOperator;
+import org.netbeans.test.subversion.operators.VersioningOperator;
+import org.netbeans.test.subversion.operators.WorkDirStepOperator;
+import org.netbeans.test.subversion.utils.RepositoryMaintenance;
+import org.netbeans.test.subversion.utils.TestKit;
+
+/**
+ *
+ * @author novakm
+ */
+public class FilesViewRefTest extends JellyTestCase {
+
+    public static final String TMP_PATH = "/tmp";
+    public static final String REPO_PATH = "repo";
+    public static final String WORK_PATH = "work";
+    public static final String PROJECT_NAME = "JavaApp";
+    public File projectPath;
+    public PrintStream stream;
+    String os_name;
+    Operator.DefaultStringComparator comOperator;
+    Operator.DefaultStringComparator oldOperator;
+
+    /** Creates a new instance of FilesViewRefTest */
+    public FilesViewRefTest(String name) {
+        super(name);
+    }
+
+    protected void setUp() throws Exception {
+        os_name = System.getProperty("os.name");
+        //System.out.println(os_name);
+        System.out.println("### " + getName() + " ###");
+    }
+
+    protected boolean isUnix() {
+        boolean unix = false;
+        if (os_name.indexOf("Windows") == -1) {
+            unix = true;
+        }
+        return unix;
+    }
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        TestRunner.run(suite());
+    }
+
+    public static NbTestSuite suite() {
+        NbTestSuite suite = new NbTestSuite();
+        suite.addTest(new FilesViewRefTest("testFilesViewRefactoring"));
+        return suite;
+    }
+
+    public void testFilesViewRefactoring() throws Exception {
+        //JemmyProperties.setCurrentTimeout("ComponentOperator.WaitComponentTimeout", 30000);
+        //JemmyProperties.setCurrentTimeout("DialogWaiter.WaitDialogTimeout", 30000);
+        try {
+            TestKit.closeProject(PROJECT_NAME);
+            OutputOperator.invoke();
+            JTableOperator table;
+            stream = new PrintStream(new File(getWorkDir(), getName() + ".log"));
+            VersioningOperator vo = VersioningOperator.invoke();
+            comOperator = new Operator.DefaultStringComparator(true, true);
+            oldOperator = (DefaultStringComparator) Operator.getDefaultStringComparator();
+            Operator.setDefaultStringComparator(comOperator);
+            CheckoutWizardOperator co = CheckoutWizardOperator.invoke();
+            Operator.setDefaultStringComparator(oldOperator);
+            RepositoryStepOperator rso = new RepositoryStepOperator();
+
+            //create repository...
+            File work = new File(TMP_PATH + File.separator + WORK_PATH + File.separator + "w" + System.currentTimeMillis());
+            new File(TMP_PATH).mkdirs();
+            work.mkdirs();
+            RepositoryMaintenance.deleteFolder(new File(TMP_PATH + File.separator + REPO_PATH));
+            //RepositoryMaintenance.deleteFolder(new File(TMP_PATH + File.separator + WORK_PATH));
+            RepositoryMaintenance.createRepository(TMP_PATH + File.separator + REPO_PATH);
+            RepositoryMaintenance.loadRepositoryFromFile(TMP_PATH + File.separator + REPO_PATH, getDataDir().getCanonicalPath() + File.separator + "repo_dump");
+            rso.setRepositoryURL(RepositoryStepOperator.ITEM_FILE + RepositoryMaintenance.changeFileSeparator(TMP_PATH + File.separator + REPO_PATH, false));
+
+            rso.next();
+            WorkDirStepOperator wdso = new WorkDirStepOperator();
+            wdso.setRepositoryFolder("trunk/" + PROJECT_NAME);
+            wdso.setLocalFolder(work.getCanonicalPath());
+            wdso.checkCheckoutContentOnly(false);
+            wdso.finish();
+            //open project
+            OutputTabOperator oto = new OutputTabOperator("file:///tmp/repo");
+            oto.getTimeouts().setTimeout("ComponentOperator.WaitStateTimeout", 30000);
+//            oto.clear();
+            oto.waitText("Checking out... finished.");
+            NbDialogOperator nbdialog = new NbDialogOperator("Checkout Completed");
+            JButtonOperator open = new JButtonOperator(nbdialog, "Open Project");
+            open.push();
+
+            TestKit.waitForScanFinishedAndQueueEmpty();
+
+            oto = new OutputTabOperator("file:///tmp/repo");
+            oto.getTimeouts().setTimeout("ComponentOperator.WaitStateTimeout", 30000);
+            oto.clear();
+            TestKit.createNewPackage(PROJECT_NAME, "a.b.c");
+//            TestKit.createNewElement(PROJECT_NAME, "a.b.c", "NewClass");
+            Node node = new Node(new SourcePackagesNode(PROJECT_NAME), "");
+            CommitOperator cmo = CommitOperator.invoke(node);
+            cmo.commit();
+            node = new Node(new FilesTabOperator().tree(), PROJECT_NAME + "|src|a");
+            node.performPopupActionNoBlock("Cut");
+            node = new Node(new FilesTabOperator().tree(), PROJECT_NAME + "|src|javaapp");
+            node.performPopupActionNoBlock("Paste");
+//            NbDialogOperator dialog = new NbDialogOperator("Move Classes");
+
+        } catch (Exception e) {
+            throw new Exception("Test failed: " + e);
+        } finally {
+//            TestKit.closeProject(PROJECT_NAME);
+        }
+    }
+}
