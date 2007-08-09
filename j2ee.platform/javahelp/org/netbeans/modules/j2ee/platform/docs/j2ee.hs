eed1b22d64a44115905a3a7290c9174a1632e525
pkeegan <pkeegan@netbeans.org>
2007-08-09 21:28:10 +0000

    correcting copyright dates plus some html glitches

--- j2ee.platform/javahelp/org/netbeans/modules/j2ee/platform/docs/j2ee.hs
+++ j2ee.platform/javahelp/org/netbeans/modules/j2ee/platform/docs/j2ee.hs
@@ -1,6 +1,6 @@
 <?xml version='1.0' encoding='ISO-8859-1' ?>
 <!--
-*     Copyright © 2004 Sun Microsystems, Inc. All rights reserved.
+*     Copyright Â© 2007 Sun Microsystems, Inc. All rights reserved.
 *     Use is subject to license terms.
 -->
 <!DOCTYPE helpset
