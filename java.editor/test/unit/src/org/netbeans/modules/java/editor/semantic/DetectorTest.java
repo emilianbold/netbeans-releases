e2df4cd5342bb1829f80b7708eece118fe96113b
msauer <msauer@netbeans.org>
2007-08-13 13:56:29 +0000

    Issue 89356 - default constructor, serialVersionUID are grayed out - adding tests

--- java.editor/test/unit/src/org/netbeans/modules/java/editor/semantic/DetectorTest.java
+++ java.editor/test/unit/src/org/netbeans/modules/java/editor/semantic/DetectorTest.java
@@ -298,6 +298,10 @@ public class DetectorTest extends TestBase {
         performTest("UnusedImport111113");
     }
     
+    public void test89356() throws Exception {
+        performTest("SerialVersionUID89356");
+    }
+    
     private void performTest(String fileName) throws Exception {
         performTest(fileName, new Performer() {
             public void compute(CompilationController parameter, Document doc, ErrorDescriptionSetter setter) {
