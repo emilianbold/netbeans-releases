b3c9d4d321ff155bf6153bc9441ce00601f8c6f1
mkuchtiak <mkuchtiak@netbeans.org>
2007-08-14 08:07:31 +0000

    Issue 112681 : wsimport entries were removed from SourcePathImplementation

--- java.j2seproject/test/unit/src/org/netbeans/modules/java/j2seproject/classpath/SourcePathImplementationTest.java
+++ java.j2seproject/test/unit/src/org/netbeans/modules/java/j2seproject/classpath/SourcePathImplementationTest.java
@@ -104,7 +104,7 @@ public class SourcePathImplementationTest extends NbTestCase {
         ClassPath cp = cps[0];
         List<ClassPath.Entry> entries = cp.entries();
         assertNotNull ("Entries can not be null", entries);
-        assertEquals ("There must be 3 src entries",3, entries.size());
+        assertEquals ("There must be 2 src entries",2, entries.size());
         assertEquals("There must be src root", entries.get(0).getRoot(), sources);
         String buildDir = (String) J2SEProjectUtil.getEvaluatedProperty(pp,"${build.dir}");
         assertNotNull ("There is no build.dir property", buildDir);
@@ -114,13 +114,6 @@ public class SourcePathImplementationTest extends NbTestCase {
             url = new URL (url.toExternalForm() + "/");
         }
         assertEquals("There must be WSClient entry", entries.get(1).getURL(), url);
-        
-        f = new File (pp.getAntProjectHelper().resolveFile(buildDir),"generated/wsimport/client");
-        url = f.toURI().toURL();
-        if (!f.exists()) {
-            url = new URL (url.toExternalForm() + "/");
-        }
-        assertEquals("There must be WSImportClient entry", entries.get(2).getURL(), url);
         
         ProjectManager.mutex().writeAccess(new Mutex.ExceptionAction<Void>() {
             public Void run() throws Exception {
@@ -133,7 +126,7 @@ public class SourcePathImplementationTest extends NbTestCase {
         });                
         entries = cp.entries();
         assertNotNull ("Entries can not be null", entries);
-        assertEquals ("There must be 3 src entries",3, entries.size());
+        assertEquals ("There must be 2 src entries",2, entries.size());
         assertEquals("There must be src root", entries.get(0).getRoot(), sources);
         buildDir = (String) J2SEProjectUtil.getEvaluatedProperty(pp,"${build.dir}");
         assertNotNull ("There is no build.dir property", buildDir);
@@ -143,13 +136,6 @@ public class SourcePathImplementationTest extends NbTestCase {
             url = new URL (url.toExternalForm() + "/");
         }
         assertEquals("There must be WSClient entry", entries.get(1).getURL(), url);
-        
-        f = new File (pp.getAntProjectHelper().resolveFile(buildDir),"generated/wsimport/client");
-        url = f.toURI().toURL();
-        if (!f.exists()) {
-            url = new URL (url.toExternalForm() + "/");
-        }
-        assertEquals("There must be WSImportClient entry", entries.get(2).getURL(), url);
     }
 
     public void testIncludesExcludes() throws Exception {
