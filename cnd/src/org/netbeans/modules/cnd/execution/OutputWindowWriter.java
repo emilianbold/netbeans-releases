117b2f79507d45f7afc30f64a6760b530d6d01ee
thp <thp@netbeans.org>
2007-08-14 01:45:09 +0000

    111660 EmptyStackException when building

--- cnd/src/org/netbeans/modules/cnd/execution/OutputWindowWriter.java
+++ cnd/src/org/netbeans/modules/cnd/execution/OutputWindowWriter.java
@@ -328,7 +328,8 @@ public class OutputWindowWriter extends Writer {
 
     private static final Pattern GCC_ERROR_SCANNER = Pattern.compile("([a-zA-Z]:[^:\n]*|[^:\n]*):([^:\n]*):([^:\n]*):([^\n]*)"); // NOI18N
     private static final Pattern GCC_ERROR_SCANNER_ANOTHER = Pattern.compile("([^:\n]*):([0-9]+): ([a-zA-Z]*):*.*"); // NOI18N    
-    private static final Pattern GCC_DIRECTORY_ENTER_LEAVE = Pattern.compile("[gd]?make\\[([0-9]+)\\]: [\\w+\\s+]+`([^']*)'"); // NOI18N
+    private static final Pattern GCC_DIRECTORY_ENTER = Pattern.compile("[gd]?make\\[([0-9]+)\\]: Entering[\\w+\\s+]+`([^']*)'"); // NOI18N
+    private static final Pattern GCC_DIRECTORY_LEAVE = Pattern.compile("[gd]?make\\[([0-9]+)\\]: Leaving[\\w+\\s+]+`([^']*)'"); // NOI18N
     private static final Pattern GCC_DIRECTORY_CD    = Pattern.compile("cd\\s+([\\S]+)[\\s;]");// NOI18N
     private static final Pattern GCC_STACK_HEADER = Pattern.compile("In file included from ([A-Z]:[^:\n]*|[^:\n]*):([^:^,]*)"); // NOI18N
     private static final Pattern GCC_STACK_NEXT =   Pattern.compile("                 from ([A-Z]:[^:\n]*|[^:\n]*):([^:^,]*)"); // NOI18N
@@ -368,7 +369,7 @@ public class OutputWindowWriter extends Writer {
         
         public boolean handleLine(OutputWriter delegate, String line, Matcher m) throws IOException {
             
-            if (m.pattern() == GCC_DIRECTORY_ENTER_LEAVE) {
+            if (m.pattern() == GCC_DIRECTORY_ENTER || m.pattern() == GCC_DIRECTORY_LEAVE) {
                 int level = Integer.valueOf((m.group(1)));
                 int baseLavel = Integer.valueOf(this.relativeLevel.peek() + "");
                 String directory = m.group(2);
@@ -520,7 +521,7 @@ public class OutputWindowWriter extends Writer {
 
         
         public Pattern[] getPattern() {
-            return new Pattern[] {GCC_DIRECTORY_ENTER_LEAVE, GCC_DIRECTORY_CD, GCC_STACK_HEADER, GCC_STACK_NEXT, GCC_ERROR_SCANNER, GCC_ERROR_SCANNER_ANOTHER};
+            return new Pattern[] {GCC_DIRECTORY_ENTER, GCC_DIRECTORY_LEAVE, GCC_DIRECTORY_CD, GCC_STACK_HEADER, GCC_STACK_NEXT, GCC_ERROR_SCANNER, GCC_ERROR_SCANNER_ANOTHER};
         }
         
     }
