e1ebea4de2d52c194c716eda9db1a79451b9512d
jglick <jglick@netbeans.org>
2007-08-14 21:13:33 +0000

    Rewrote format for better readability and more information.

--- nbbuild/antsrc/org/netbeans/nbbuild/extlibs/CreateLicenseSummary.java
+++ nbbuild/antsrc/org/netbeans/nbbuild/extlibs/CreateLicenseSummary.java
@@ -33,6 +33,7 @@ import java.math.BigInteger;
 import java.nio.channels.FileChannel;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -77,44 +78,74 @@ public class CreateLicenseSummary extends Task {
     private Map<String,String> pseudoTests;
 
     public @Override void execute() throws BuildException {
-        // XXX deal with extra.license.files
         pseudoTests = new LinkedHashMap<String,String>();
         try {
-            Map<Long,String> crc2License = findCrc2LicenseMapping();
-            Map<String,Set<String>> license2Binaries = new TreeMap<String,Set<String>>();
+            Map<Long,Map<String,String>> crc2License = findCrc2LicenseHeaderMapping();
+            Map<String,Map<String,String>> binaries2LicenseHeaders = new TreeMap<String,Map<String,String>>();
             StringBuilder testBinariesAreUnique = new StringBuilder();
-            findBinaries(build, license2Binaries, crc2License, new HashMap<Long,String>(), "", testBinariesAreUnique);
+            findBinaries(build, binaries2LicenseHeaders, crc2License, new HashMap<Long,String>(), "", testBinariesAreUnique);
             pseudoTests.put("testBinariesAreUnique", testBinariesAreUnique.length() > 0 ? "Some binaries are duplicated" + testBinariesAreUnique : null);
             OutputStream os = new FileOutputStream(summary);
             try {
                 PrintWriter pw = new PrintWriter(new OutputStreamWriter(os, "UTF-8"));
                 pw.println("DO NOT TRANSLATE OR LOCALIZE.");
-                File licenses = new File(new File(nball, "nbbuild"), "licenses");
-                for (Map.Entry<String,Set<String>> entry : license2Binaries.entrySet()) {
-                    String licenseName = entry.getKey();
-                    pw.println();
-                    pw.println("=========== " + licenseName + " ===========");
-                    for (String binary : entry.getValue()) {
-                        File f = new File(build, binary.replace('/', File.separatorChar));
-                        MessageDigest digest;
-                        try {
-                            digest = MessageDigest.getInstance("SHA-1");
-                        } catch (NoSuchAlgorithmException x) {
-                            throw new BuildException(x, getLocation());
-                        }
-                        FileInputStream is = new FileInputStream(f);
-                        try {
-                            digest.update(is.getChannel().map(FileChannel.MapMode.READ_ONLY, 0, f.length()));
-                        } finally {
-                            is.close();
-                        }
-                        pw.printf("%s (size: %d; SHA-1: %040X)\n", binary, f.length(), new BigInteger(1, digest.digest()));
+                pw.println();
+                Set<String> licenseNames = new TreeSet<String>();
+                pw.printf("%-60s %10s %-40s %s\n", "NAME", "SIZE", "SHA-1 HASH", "LICENSE");
+                for (Map.Entry<String,Map<String,String>> entry : binaries2LicenseHeaders.entrySet()) {
+                    String binary = entry.getKey();
+                    File f = new File(build, binary.replace('/', File.separatorChar));
+                    MessageDigest digest;
+                    try {
+                        digest = MessageDigest.getInstance("SHA-1");
+                    } catch (NoSuchAlgorithmException x) {
+                        throw new BuildException(x, getLocation());
                     }
+                    FileInputStream is = new FileInputStream(f);
+                    try {
+                        digest.update(is.getChannel().map(FileChannel.MapMode.READ_ONLY, 0, f.length()));
+                    } finally {
+                        is.close();
+                    }
+                    Map<String,String> headers = entry.getValue();
+                    pw.printf("%-60s %10d %040X %s\n", binary, f.length(), new BigInteger(1, digest.digest()), getMaybeMissing(headers, "License"));
+                    String license = headers.get("License");
+                    if (license != null) {
+                        licenseNames.add(license);
+                    }
+                }
+                String[] otherHeaders = {"Name", "Version", "Description", "OSR", "Origin"};
+                Map<Map<String,String>,Set<String>> licenseHeaders2Binaries = new LinkedHashMap<Map<String,String>,Set<String>>();
+                for (Map.Entry<String,Map<String,String>> entry : binaries2LicenseHeaders.entrySet()) {
+                    Map<String,String> headers = new HashMap<String,String>(entry.getValue());
+                    headers.keySet().retainAll(Arrays.asList(otherHeaders));
+                    Set<String> binaries = licenseHeaders2Binaries.get(headers);
+                    if (binaries == null) {
+                        binaries = new TreeSet<String>();
+                        licenseHeaders2Binaries.put(headers, binaries);
+                    }
+                    binaries.add(entry.getKey());
+                }
+                for (Map.Entry<Map<String,String>,Set<String>> entry : licenseHeaders2Binaries.entrySet()) {
                     pw.println();
+                    for (String binary : entry.getValue()) {
+                        pw.println(binary);
+                    }
+                    for (String header : otherHeaders) {
+                        pw.printf("%s: %s\n", header, getMaybeMissing(entry.getKey(), header));
+                    }
+                }
+                File licenses = new File(new File(nball, "nbbuild"), "licenses");
+                for (String licenseName : licenseNames) {
+                    if (licenseName == null) {
+                        continue;
+                    }
                     File license = new File(licenses, licenseName);
                     if (!license.isFile()) {
                         continue;
                     }
+                    pw.println();
+                    pw.println("=========== " + licenseName + " ===========");
                     InputStream is = new FileInputStream(license);
                     try {
                         BufferedReader r = new BufferedReader(new InputStreamReader(is, "UTF-8"));
@@ -136,26 +167,33 @@ public class CreateLicenseSummary extends Task {
         }
         JUnitReportWriter.writeReport(this, reportFile, pseudoTests);
     }
+    private String getMaybeMissing(Map<String,String> headers, String headerName) {
+        if (headers.containsKey(headerName)) {
+            return headers.get(headerName);
+        } else {
+            return "<unknown>";
+        }
+    }
 
-    private Map<Long,String> findCrc2LicenseMapping() throws IOException {
-        Map<Long,String> crc2License = new HashMap<Long,String>();
+    private Map<Long,Map<String,String>> findCrc2LicenseHeaderMapping() throws IOException {
+        Map<Long,Map<String,String>> crc2LicenseHeaders = new HashMap<Long,Map<String,String>>();
         for (String cluster : getProject().getProperty("nb.clusters.list").split("[, ]+")) {
             for (String module : getProject().getProperty(cluster).split("[, ]+")) {
                 File d = new File(new File(nball, module), "external");
                 Set<String> cvsFiles = VerifyLibsAndLicenses.findCvsControlledFiles(d, false);
-                Map<String,String> binary2License = findBinary2LicenseMapping(cvsFiles, d);
+                Map<String,Map<String,String>> binary2License = findBinary2LicenseHeaderMapping(cvsFiles, d);
                 for (String n : cvsFiles) {
                     if (!n.endsWith(".jar") && !n.endsWith(".zip")) {
                         continue;
                     }
-                    String license = binary2License.get(n);
-                    if (license == null) {
+                    Map<String,String> headers = binary2License.get(n);
+                    if (headers == null) {
                         continue;
                     }
                     File f = new File(d, n);
                     InputStream is = new FileInputStream(f);
                     try {
-                        crc2License.put(computeCRC32(is), license);
+                        crc2LicenseHeaders.put(computeCRC32(is), headers);
                     } finally {
                         is.close();
                     }
@@ -170,7 +208,7 @@ public class CreateLicenseSummary extends Task {
                             }
                             is = zf.getInputStream(entry);
                             try {
-                                crc2License.put(computeCRC32(is), license);
+                                crc2LicenseHeaders.put(computeCRC32(is), headers);
                             } finally {
                                 is.close();
                             }
@@ -181,7 +219,7 @@ public class CreateLicenseSummary extends Task {
                 }
             }
         }
-        return crc2License;
+        return crc2LicenseHeaders;
     }
 
     private long computeCRC32(InputStream is) throws IOException {
@@ -194,8 +232,8 @@ public class CreateLicenseSummary extends Task {
         return crc32.getValue();
     }
 
-    private Map<String, String> findBinary2LicenseMapping(Set<String> cvsFiles, File d) throws IOException {
-        Map<String,String> binary2License = new HashMap<String,String>();
+    private Map<String,Map<String,String>> findBinary2LicenseHeaderMapping(Set<String> cvsFiles, File d) throws IOException {
+        Map<String,Map<String,String>> binary2LicenseHeaders = new HashMap<String,Map<String,String>>();
         for (String n : cvsFiles) {
             if (!n.endsWith("-license.txt")) {
                 continue;
@@ -214,42 +252,33 @@ public class CreateLicenseSummary extends Task {
             } finally {
                 is.close();
             }
-            String license = headers.get("License");
-            if (license == null) {
-                continue;
-            }
-            String files = headers.get("Files");
+            String files = headers.remove("Files");
             if (files != null) {
                 for (String file : files.split("[, ]+")) {
-                    binary2License.put(file, license);
+                    binary2LicenseHeaders.put(file, headers);
                 }
             } else {
-                binary2License.put(n.replaceFirst("-license\\.txt$", ".jar"), license);
-                binary2License.put(n.replaceFirst("-license\\.txt$", ".zip"), license);
+                binary2LicenseHeaders.put(n.replaceFirst("-license\\.txt$", ".jar"), headers);
+                binary2LicenseHeaders.put(n.replaceFirst("-license\\.txt$", ".zip"), headers);
             }
         }
-        return binary2License;
+        return binary2LicenseHeaders;
     }
 
-    private void findBinaries(File d, Map<String,Set<String>> license2Binaries, Map<Long,String> crc2License,
+    private void findBinaries(File d, Map<String,Map<String,String>> binaries2LicenseHeaders, Map<Long,Map<String,String>> crc2LicenseHeaders,
             Map<Long,String> crc2Binary, String prefix, StringBuilder testBinariesAreUnique) throws IOException {
         for (String n : d.list()) {
             File f = new File(d, n);
             if (f.isDirectory()) {
-                findBinaries(f, license2Binaries, crc2License, crc2Binary, prefix + n + "/", testBinariesAreUnique);
+                findBinaries(f, binaries2LicenseHeaders, crc2LicenseHeaders, crc2Binary, prefix + n + "/", testBinariesAreUnique);
             } else if (n.endsWith(".jar") || n.endsWith(".zip")) {
                 InputStream is = new FileInputStream(f);
                 try {
                     long crc = computeCRC32(is);
-                    String license = crc2License.get(crc);
-                    if (license != null) {
-                        Set<String> binaries = license2Binaries.get(license);
-                        if (binaries == null) {
-                            binaries = new TreeSet<String>();
-                            license2Binaries.put(license, binaries);
-                        }
+                    Map<String,String> headers = crc2LicenseHeaders.get(crc);
+                    if (headers != null) {
                         String path = prefix + n;
-                        binaries.add(path);
+                        binaries2LicenseHeaders.put(path, headers);
                         String otherPath = crc2Binary.put(crc, path);
                         if (otherPath != null) {
                             testBinariesAreUnique.append("\n" + otherPath + " and " + path + " are identical");
