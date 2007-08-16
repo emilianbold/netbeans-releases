3dbdccc1da578f1ab696f66339cf8328aab42223
vaysman <vaysman@netbeans.org>
2007-08-16 18:22:37 +0000

    tests has been updated according specification changes

---
+++ installer/test/qa-functional/src/org/netbeans/test/installer/TestInstallerAndUninstallerJavaEE.java
@@ -0,0 +1,70 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development and
+ * Distribution License (the License). You may not use this file except in compliance
+ * with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html or
+ * http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file and
+ * include the License file at http://www.netbeans.org/cddl.txt. If applicable, add
+ * the following below the CDDL Header, with the fields enclosed by brackets []
+ * replaced by your own identifying information:
+ *
+ *     "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original Software
+ * is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun Microsystems, Inc. All
+ * Rights Reserved.
+ *
+ * $Id$
+ *
+ */
+
+package org.netbeans.test.installer;
+
+import java.util.logging.Logger;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import org.netbeans.junit.NbTestCase;
+import org.netbeans.junit.NbTestSuite;
+
+/**
+ *
+ * @author Mikhail Vaysman
+ */
+public class TestInstallerAndUninstallerJavaEE extends NbTestCase {
+
+    public TestInstallerAndUninstallerJavaEE() {
+        super("Installer test");
+    }
+
+    public static Test suite() {
+        TestSuite suite = new NbTestSuite(TestInstallerAndUninstallerJavaEE.class);
+
+        return suite;
+    }
+
+    public void testInstaller() {
+        TestData data = new TestData(Logger.getLogger("global"));
+
+        Utils.phaseOne(this, data, "javaee");
+
+        //select apache
+        Utils.stepChooseComponet("Apache Tomcat");
+
+        //welcome, license and set NB dir
+        Utils.phaseTwo(data);
+
+        Utils.phaseThree(data);
+
+        Utils.phaseFour(data);
+
+        //TODO Dir removed test
+        //TODO Clean up work dir
+    }
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+}
