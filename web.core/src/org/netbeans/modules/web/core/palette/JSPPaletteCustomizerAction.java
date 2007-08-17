a5382a85868b1875f2244fa61992bd1bf2bf3ae6
ptymoshenko <ptymoshenko@netbeans.org>
2007-08-17 09:56:12 +0000

    #76955 - added missing license notices

--- web.core/src/org/netbeans/modules/web/core/palette/JSPPaletteCustomizerAction.java
+++ web.core/src/org/netbeans/modules/web/core/palette/JSPPaletteCustomizerAction.java
@@ -1,11 +1,20 @@
 /*
- * HTMLPaletteCustomizerAction.java
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
  *
- * Created on October 27, 2005, 10:49 AM
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
  *
- * To change this template, choose Tools | Options and locate the template under
- * the Source Creation and Management node. Right-click the template and choose
- * Open. You can then make changes to the template in the Source Editor.
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Microsystems, Inc. All Rights Reserved.
  */
 
 package org.netbeans.modules.web.core.palette;
