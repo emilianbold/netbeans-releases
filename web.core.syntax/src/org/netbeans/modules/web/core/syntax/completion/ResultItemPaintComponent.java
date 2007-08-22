8b9c434035cde62d9b34d57212a00682afe45a4f
mfukala <mfukala@netbeans.org>
2007-08-22 12:32:59 +0000

    #111446 - [cc] expression language code completion is too short

--- web.core.syntax/src/org/netbeans/modules/web/core/syntax/completion/ResultItemPaintComponent.java
+++ web.core.syntax/src/org/netbeans/modules/web/core/syntax/completion/ResultItemPaintComponent.java
@@ -177,15 +177,11 @@ public class ResultItemPaintComponent extends JPanel {
             drawString(g, "   "); // NOI18N
             drawString(g, s, c);
         } else {
-            int w = getWidth() - getWidth(s) - drawX;
             int spaceWidth = getWidth(" "); // NOI18N
-            if (w > spaceWidth * 2) {
-                drawX += getWidth() - 2 * spaceWidth - getWidth(s);
+            if(getWidth() - getWidth(s) < drawX) {
+                drawX += spaceWidth * 2;
             } else {
-                drawX += getWidth() - 2 * spaceWidth - getWidth(s) - getWidth("...   "); // NOI18N
-                g.setColor(getBackground());
-                g.fillRect(drawX, 0, getWidth() - drawX, getHeight());
-                drawString(g, "...   ", c); // NOI18N
+                drawX = getWidth() - getWidth(s) - 4; //4px between the type text end and the frame of the completion window
             }
             drawString(g, s, c);
         }
