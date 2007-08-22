ccb8275cac3b3e0aeb93ff0bf21d7906ac03ebad
mkrauskopf <mkrauskopf@netbeans.org>
2007-08-22 12:50:14 +0000

    Do not use getDataDir() for temporary data. Use getWorkDir instead.

--- ruby.platform/test/unit/src/org/netbeans/api/ruby/platform/RubyInstallationTest.java
+++ ruby.platform/test/unit/src/org/netbeans/api/ruby/platform/RubyInstallationTest.java
@@ -1,32 +1,35 @@
 /*
- * RubyInstallationTest.java
- * JUnit based test
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
  *
- * Created on June 26, 2007, 8:42 AM
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
+ * Microsystems, Inc. All Rights Reserved.
  */
+
 package org.netbeans.api.ruby.platform;
 
 import java.io.File;
 import java.util.Set;
-import org.netbeans.api.ruby.platform.RubyInstallation;
 import org.netbeans.junit.NbTestCase;
 
 /**
- *
  * @author Tor Norbye
  */
 public class RubyInstallationTest extends NbTestCase {
 
     public RubyInstallationTest(String testName) {
         super(testName);
-    }
-
-    protected void setUp() throws Exception {
-        super.setUp();
-    }
-
-    protected void tearDown() throws Exception {
-        super.tearDown();
     }
 
     public void testCompareGemVersions() {
@@ -44,7 +47,7 @@ public class RubyInstallationTest extends NbTestCase {
     }
 
     public void testChooseGems() throws Exception {
-        File home = getDataDir();
+        File home = getWorkDir();
 
         // Build a fake ruby structure
         File bin = new File(home, "bin");
