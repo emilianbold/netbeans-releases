e82269fcb12fac85eb551bb82ced89b83f64a4d4
vv159170 <vv159170@netbeans.org>
2007-08-23 16:16:09 +0000

    new  test cases and updated golden files

--- cnd.completion/test/unit/data/org/netbeans/modules/cnd/completion/CCBasicCompletionTestCase/file.h
+++ cnd.completion/test/unit/data/org/netbeans/modules/cnd/completion/CCBasicCompletionTestCase/file.h
@@ -7,3 +7,19 @@ private:
 public:
     void f2();
 };
+
+class B {
+protected:
+    void xxx();
+    
+};
+
+class C : public B {
+public:
+    C() {
+        
+    }
+    void test(){
+        
+    }
+};
