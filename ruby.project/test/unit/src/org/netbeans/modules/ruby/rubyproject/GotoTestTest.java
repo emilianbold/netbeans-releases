f3c67c7030e40a4f3836fe5c31fed5d271ce0532
mkrauskopf <mkrauskopf@netbeans.org>
2007-08-25 15:32:05 +0000

    licence

--- ruby.project/test/unit/src/org/netbeans/modules/ruby/rubyproject/GotoTestTest.java
+++ ruby.project/test/unit/src/org/netbeans/modules/ruby/rubyproject/GotoTestTest.java
@@ -1,8 +1,20 @@
 /*
- * GotoTestTest.java
- * JUnit based test
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
  *
- * Created on July 23, 2007, 10:48 AM
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
+ * Microsystems, Inc. All Rights Reserved.
  */
 
 package org.netbeans.modules.ruby.rubyproject;
@@ -13,7 +25,6 @@ import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
 
 /**
- *
  * @author Tor Norbye
  */
 public class GotoTestTest extends RubyProjectTestBase {
