e473ccd48ecb301f1d3d4ba5630da04d2a012c94
pkeegan <pkeegan@netbeans.org>
2007-08-27 18:18:00 +0000

    deleting index; correcting uncompleted comment

--- subversion/javahelp/org/netbeans/modules/subversion/docs/svnmodule.hs
+++ subversion/javahelp/org/netbeans/modules/subversion/docs/svnmodule.hs
@@ -1,6 +1,6 @@
 <?xml version='1.0' encoding='ISO-8859-1' ?>
 <!--
-*     Copyright © 2007 Sun Microsystems, Inc. All rights reserved.
+*     Copyright ï¿½ 2007 Sun Microsystems, Inc. All rights reserved.
 *     Use is subject to license terms.
 -->
 <!DOCTYPE helpset
@@ -25,14 +25,14 @@
     <type>javax.help.TOCView</type>
     <data>svnmodule-toc.xml</data> 
  </view>
- 
+ <!--
   <view>
     <name>Index</name>
     <label>Index</label>
     <type>javax.help.IndexView</type>
     <data>svnmodule-idx.xml</data>
   </view>
-
+-->
   <view>
     <name>Search</name>
    <label>Search</label>
