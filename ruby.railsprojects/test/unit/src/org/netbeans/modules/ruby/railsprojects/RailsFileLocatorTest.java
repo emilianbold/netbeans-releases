8550a1322767d46413535629f023264b1436d832
mkrauskopf <mkrauskopf@netbeans.org>
2007-08-28 09:36:39 +0000

    Forgotten tests (new files) for previous commit

---
+++ ruby.railsprojects/test/unit/src/org/netbeans/modules/ruby/railsprojects/RailsFileLocatorTest.java
@@ -0,0 +1,61 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.modules.ruby.railsprojects;
+
+import java.io.File;
+import org.netbeans.api.project.ProjectManager;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileUtil;
+import org.openide.util.lookup.Lookups;
+
+public class RailsFileLocatorTest extends RailsProjectTestBase {
+    
+    public RailsFileLocatorTest(String testName) {
+        super(testName);
+    }
+
+    private RailsFileLocator generateProject(String... paths) throws Exception {
+        RailsProject project = createTestProject("RubyProject1");
+        ProjectManager.getDefault().findProject(project.getProjectDirectory());
+        assertNotNull(project);
+        FileObject dirFO = project.getProjectDirectory();
+        assertNotNull(dirFO);
+        for (String path : paths) {
+            File file = new File(path);
+            File dir = new File(FileUtil.toFile(dirFO), file.getParent());
+            dir.mkdirs();
+            File testFile = new File(dir, file.getName());
+            testFile.createNewFile();
+        }
+        return new RailsFileLocator(Lookups.singleton(new Object()), project);
+    }
+
+    public void testRelativeToProjectDir() throws Exception { // # 112254
+        RailsFileLocator rfl = generateProject("./test/unit/http_phone/asterisk_cmd_test.rb");
+        assertNotNull(rfl.find("./test/unit/http_phone/asterisk_cmd_test.rb"));
+    }
+
+    public void testEdgeCases() throws Exception {
+        RailsFileLocator rfl = generateProject("./test/unit/http_phone/asterisk_cmd_test.rb");
+        assertNotNull(rfl.find("./http_phone/asterisk_cmd_test.rb"));
+        assertNotNull(rfl.find("asterisk_cmd_test.rb"));
+    }
+    
+}
---
+++ ruby.railsprojects/test/unit/src/org/netbeans/modules/ruby/railsprojects/RailsProjectTestBase.java
@@ -0,0 +1,41 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ * 
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ * 
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ * 
+ * Portions Copyrighted 2007 Sun Microsystems, Inc.
+ */
+package org.netbeans.modules.ruby.railsprojects;
+
+import java.io.File;
+import org.netbeans.api.project.ProjectManager;
+import org.netbeans.modules.ruby.RubyTestBase;
+import org.openide.filesystems.FileUtil;
+
+public class RailsProjectTestBase extends RubyTestBase {
+
+    public RailsProjectTestBase(String testName) {
+        super(testName);
+    }
+
+    protected @Override void setUp() throws Exception {
+        super.setUp();
+        clearWorkDir();
+    }
+
+    protected RailsProject createTestProject(String projectName) throws Exception {
+        File prjDirF = new File(getWorkDir(), projectName);
+        RailsProjectGenerator.createProject(prjDirF, projectName, false, null, false, false);
+        return (RailsProject) ProjectManager.getDefault().findProject(FileUtil.toFileObject(prjDirF));
+    }
+
+}
