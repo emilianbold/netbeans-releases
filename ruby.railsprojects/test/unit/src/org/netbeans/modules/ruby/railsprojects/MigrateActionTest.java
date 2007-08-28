2eaaad1b33e913c0839184e6fba7d7747187c61d
mkrauskopf <mkrauskopf@netbeans.org>
2007-08-28 09:45:02 +0000

    License, imports

--- ruby.railsprojects/test/unit/src/org/netbeans/modules/ruby/railsprojects/MigrateActionTest.java
+++ ruby.railsprojects/test/unit/src/org/netbeans/modules/ruby/railsprojects/MigrateActionTest.java
@@ -1,20 +1,26 @@
 /*
- * MigrateActionTest.java
- * JUnit based test
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
  *
- * Created on July 16, 2007, 2:18 PM
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
+ * Microsystems, Inc. All Rights Reserved.
  */
 
 package org.netbeans.modules.ruby.railsprojects;
 
 import java.io.File;
 import org.netbeans.api.project.FileOwnerQuery;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import org.netbeans.api.project.Project;
