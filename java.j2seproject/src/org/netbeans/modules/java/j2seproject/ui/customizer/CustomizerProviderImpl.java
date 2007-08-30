f0b76faedcbf5e7aca265b3aca863ffdce4beab0
mkubec <mkubec@netbeans.org>
2007-08-30 10:13:53 +0000

    #91291: running saving of j2seproject properties off AWT EQ

--- java.j2seproject/src/org/netbeans/modules/java/j2seproject/ui/customizer/CustomizerProviderImpl.java
+++ java.j2seproject/src/org/netbeans/modules/java/j2seproject/ui/customizer/CustomizerProviderImpl.java
@@ -101,7 +101,8 @@ public class CustomizerProviderImpl implements CustomizerProvider {
             });
 
             OptionListener listener = new OptionListener( project, uiProperties );
-            dialog = ProjectCustomizer.createCustomizerDialog( CUSTOMIZER_FOLDER_PATH, context, preselectedCategory, listener, null );
+            StoreListener storeListener = new StoreListener(uiProperties);
+            dialog = ProjectCustomizer.createCustomizerDialog( CUSTOMIZER_FOLDER_PATH, context, preselectedCategory, listener, storeListener, null );
             dialog.addWindowListener( listener );
             dialog.setTitle( MessageFormat.format(                 
                     NbBundle.getMessage( CustomizerProviderImpl.class, "LBL_Customizer_Title" ), // NOI18N 
@@ -111,6 +112,20 @@ public class CustomizerProviderImpl implements CustomizerProvider {
             dialog.setVisible(true);
         }
     }    
+    
+    private class StoreListener implements ActionListener {
+    
+        private J2SEProjectProperties uiProperties;
+        
+        StoreListener(J2SEProjectProperties uiProperties ) {
+            this.uiProperties = uiProperties;
+        }
+        
+        public void actionPerformed(ActionEvent e) {
+            uiProperties.save();
+        }
+        
+    }
     
     /** Listens to the actions on the Customizer's option buttons */
     private class OptionListener extends WindowAdapter implements ActionListener {
@@ -133,7 +148,6 @@ public class CustomizerProviderImpl implements CustomizerProvider {
 // as modified before the project customizer is shown. 
 //            assert !ProjectManager.getDefault().isModified(project) : 
 //                "Some of the customizer panels has written the changed data before OK Button was pressed. Please file it as bug."; //NOI18N
-            uiProperties.save();
             
             // Close & dispose the the dialog
             Dialog dialog = (Dialog)project2Dialog.get( project );
