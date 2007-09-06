b2ac3f27cb4f7037061f7154239a14685236a637
sergius <sergius@netbeans.org>
2007-09-06 14:33:29 +0000

    semantic highlighting: update on editing, "parse" task scheduling

---
+++ cnd.highlight/src/org/netbeans/modules/cnd/highlight/CppHighlightsLayerFactory.java
@@ -0,0 +1,44 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.modules.cnd.highlight;
+
+import org.netbeans.modules.cnd.highlight.semantic.ifdef.*;
+import org.netbeans.spi.editor.highlighting.HighlightsLayer;
+import org.netbeans.spi.editor.highlighting.HighlightsLayerFactory;
+import org.netbeans.spi.editor.highlighting.ZOrder;
+
+/**
+ *
+ * @author Sergey Grinev
+ */
+public class CppHighlightsLayerFactory implements HighlightsLayerFactory {
+    public HighlightsLayer[] createLayers(Context context) {
+        System.err.println("***** CppHighlightsLayerFactory is working");
+        InactiveCodeHighlighter h = new InactiveCodeHighlighter(context.getDocument());
+        return new HighlightsLayer[] {
+            HighlightsLayer.create(
+                    InactiveCodeHighlighter.class.getName(), 
+                    ZOrder.SYNTAX_RACK.forPosition(2000),
+                    true,
+                    h.getHighlightsBag(context.getDocument()))
+            };
+    }
+
+}
