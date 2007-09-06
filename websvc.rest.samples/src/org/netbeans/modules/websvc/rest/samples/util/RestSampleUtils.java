5617a545916669c9f7e4e5c9d8099fc74591178a
PeterTLiu <devnull@localhost>
2007-09-06 20:36:32 +0000

    Fix an issue in the unzip utitlity where a directory is not create if it is empty.

--- websvc.rest.samples/src/org/netbeans/modules/websvc/rest/samples/util/RestSampleUtils.java
+++ websvc.rest.samples/src/org/netbeans/modules/websvc/rest/samples/util/RestSampleUtils.java
@@ -5,7 +5,6 @@
  *
  * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
  * or http://www.netbeans.org/cddl.txt.
- 
  * When distributing Covered Code, include this CDDL Header Notice in each file
  * and include the License file at http://www.netbeans.org/cddl.txt.
  * If applicable, add the following below the CDDL Header, with the fields
@@ -49,22 +48,17 @@ import org.w3c.dom.Text;
 import org.xml.sax.InputSource;
 
 public class RestSampleUtils {
-    
+
     public static final String JERSEY_LIBRARY = "restlib"; //NOI18N
-    
-    public static String[] xlateFiles = {
-        "build-impl.xml", // NOI18N
-        "project.xml", // NOI18N
-        "project.properties", // NOI18N
-        "AssemblyInformation.xml" // NOI18N
-    };
-    
+    public static String[] xlateFiles = {"build-impl.xml", "project.xml", "project.properties", "AssemblyInformation.xml"};
+
     public static void unZipFile(InputStream source, FileObject rootFolder) throws IOException {
         try {
             ZipInputStream str = new ZipInputStream(source);
             ZipEntry entry;
             while ((entry = str.getNextEntry()) != null) {
                 if (entry.isDirectory()) {
+                    FileUtil.createFolder(rootFolder, entry.getName());
                     continue;
                 }
                 FileObject fo = FileUtil.createData(rootFolder, entry.getName());
@@ -84,13 +78,14 @@ public class RestSampleUtils {
             source.close();
         }
     }
-    
+
     public static void unZipFileTranslateProjectName(InputStream source, FileObject rootFolder, String name, String token) throws IOException {
         try {
             ZipInputStream str = new ZipInputStream(source);
             ZipEntry entry;
             while ((entry = str.getNextEntry()) != null) {
                 if (entry.isDirectory()) {
+                    FileUtil.createFolder(rootFolder, entry.getName());
                     continue;
                 }
                 String fname = entry.getName();
@@ -115,7 +110,7 @@ public class RestSampleUtils {
             source.close();
         }
     }
-    
+
     static boolean needTranslation(String fname) {
         for (int i = 0; i < xlateFiles.length; i++) {
             if (fname.endsWith(xlateFiles[i])) {
@@ -124,30 +119,29 @@ public class RestSampleUtils {
         }
         return false;
     }
-    
+
     static void translateProjectName(InputStream str, OutputStream out, String name, String token) throws IOException {
         ByteArrayOutputStream bo = new ByteArrayOutputStream();
         FileUtil.copy(str, bo);
         ByteArrayInputStream bi = new ByteArrayInputStream(bo.toString().replaceAll(token, name).getBytes());
         FileUtil.copy(bi, out);
     }
-    
+
     public static FileObject getStudioUserDir() {
-        Log.out("StudioUserDir: "+FileUtil.toFileObject(
-                new File(System.getProperty("netbeans.user"))).getPath()); // NOI18N
+        Log.out("StudioUserDir: " + FileUtil.toFileObject(new File(System.getProperty("netbeans.user"))).getPath()); // NOI18N
         return FileUtil.toFileObject(new File(System.getProperty("netbeans.user"))); // NOI18N
     }
-    
+
     public static FileObject getProjectFolder(FileObject parentDir, String projectDirName) {
-        assert parentDir != null : parentDir+"/"+projectDirName +"doesn't exist"; // NOI18N
-        assert projectDirName != null : "project name can't be empty" ; // NOI18N
+        assert parentDir != null : parentDir + "/" + projectDirName + "doesn't exist"; // NOI18N
+        assert projectDirName != null : "project name can't be empty"; // NOI18N
         return parentDir.getFileObject(projectDirName);
     }
-    
+
     private static FileObject getFolder(String relative) {
         return getFolder(getStudioUserDir(), relative);
     }
-    
+
     private static FileObject getFolder(FileObject parent, String relative) {
         FileObject folder = parent.getFileObject(relative);
         if (folder != null) {
@@ -155,27 +149,26 @@ public class RestSampleUtils {
         }
         try {
             folder = parent.createFolder(relative);
-        } catch(IOException ioe) {
+        } catch (IOException ioe) {
             ErrorManager.getDefault().notify(ErrorManager.EXCEPTION, ioe);
         }
         return folder;
     }
-    
+
     private static String getPropertiesPath(String name) {
         return RestSampleProjectProperties.getDefault().isPrivateProperty(name) ? AntProjectHelper.PRIVATE_PROPERTIES_PATH : AntProjectHelper.PROJECT_PROPERTIES_PATH;
     }
-    
+
     private static AntProjectHelper getAntProjectHelper(Project project) {
         return (AntProjectHelper) project.getLookup().lookup(AntProjectHelper.class);
     }
-    
+
     public static void setPrivateProperty(FileObject prjLoc, String name, String value) {
         Properties properties = new Properties();
         try {
             properties.setProperty(name, value);
-            
-            FileObject propFile = FileUtil.createData(prjLoc,
-                    AntProjectHelper.PRIVATE_PROPERTIES_PATH);
+
+            FileObject propFile = FileUtil.createData(prjLoc, AntProjectHelper.PRIVATE_PROPERTIES_PATH);
             FileLock lock = propFile.lock();
             OutputStream os = propFile.getOutputStream(lock);
             try {
@@ -188,18 +181,17 @@ public class RestSampleUtils {
             e.printStackTrace();
         }
     }
-    
+
     public static String getProperty(Project project, String name) {
         return getProperties(project, name).getProperty(name);
     }
-    
+
     private static EditableProperties getProperties(Project project, String name) {
         AntProjectHelper helper = getAntProjectHelper(project);
-        assert helper != null : "Can't get AntProjectHelper for project: " // NOI18N
-                + project;
+        assert helper != null : "Can't get AntProjectHelper for project: " + project;
         return helper.getProperties(getPropertiesPath(name));
     }
-    
+
     /**
      * @return SunApp default server instance location
      */
@@ -207,17 +199,15 @@ public class RestSampleUtils {
         String loc = null;
         String[] instances = InstanceProperties.getInstanceList();
         for (String instanceId : instances) {
-            if (instanceId.indexOf(
-                    RestSampleProjectProperties.SERVER_INSTANCE_SUN_APPSERVER) != -1) {
+            if (instanceId.indexOf(RestSampleProjectProperties.SERVER_INSTANCE_SUN_APPSERVER) != -1) {
                 int endIdx = instanceId.indexOf(']');
                 loc = instanceId.substring(1, endIdx);
                 break;
             }
-            
         }
         return loc;
     }
-    
+
     /**
      *  Method taken from NB anagram code.
      */
@@ -226,27 +216,24 @@ public class RestSampleUtils {
             // update project.xml
             File projXml = FileUtil.toFile(prjLoc.getFileObject(AntProjectHelper.PROJECT_XML_PATH));
             Document doc = XMLUtil.parse(new InputSource(projXml.toURI().toString()), false, true, null, null);
-            NodeList nlist = doc.getElementsByTagNameNS(projTypeName, "name");       //NOI18N
+            NodeList nlist = doc.getElementsByTagNameNS(projTypeName, "name"); //NOI18N
             if (nlist != null) {
-                for (int i=0; i < nlist.getLength(); i++) {
+                for (int i = 0; i < nlist.getLength(); i++) {
                     Node n = nlist.item(i);
                     if (n.getNodeType() != Node.ELEMENT_NODE) {
                         continue;
                     }
-                    Element e = (Element)n;
-                    
+                    Element e = (Element) n;
+
                     replaceText(e, name);
                 }
                 saveXml(doc, prjLoc, AntProjectHelper.PROJECT_XML_PATH);
             }
-            
         } catch (Exception e) {
             ErrorManager.getDefault().notify(e);
         }
-        
     }
-    
-    
+
     /**
      * Method taken from NB anagram game.
      * Extract nested text from an element.
@@ -257,13 +244,13 @@ public class RestSampleUtils {
         NodeList l = parent.getChildNodes();
         for (int i = 0; i < l.getLength(); i++) {
             if (l.item(i).getNodeType() == Node.TEXT_NODE) {
-                Text text = (Text)l.item(i);
+                Text text = (Text) l.item(i);
                 text.setNodeValue(name);
                 return;
             }
         }
     }
-    
+
     /**
      * Method taken from NB anagram game.
      * Save an XML config file to a named path.
@@ -283,34 +270,33 @@ public class RestSampleUtils {
             lock.releaseLock();
         }
     }
-    
-    
+
     public static void insertParameters(FileObject dir, String bpelProjDir) {
         Enumeration files = dir.getData(true);
-        
-        while(files.hasMoreElements()) {
+
+        while (files.hasMoreElements()) {
             FileObject fileObject = (FileObject) files.nextElement();
-            
-            if(fileObject.isFolder())
+
+            if (fileObject.isFolder()) {
                 continue;
-            
-            if(! ((fileObject.getExt().toLowerCase().equals("xml")  || // NOI18N
-                    fileObject.getExt().toLowerCase().equals("properties")))) // NOI18N
+            }
+            if (!((fileObject.getExt().toLowerCase().equals("xml") || fileObject.getExt().toLowerCase().equals("properties")))) {
+                // NOI18N
                 continue;
-            
+            }
             String line;
             StringBuffer buffer = new StringBuffer();
-            
+
             try {
                 InputStream inputStream = fileObject.getInputStream();
                 BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-                
-                while((line = reader.readLine()) != null) {
+
+                while ((line = reader.readLine()) != null) {
                     line = line.replace("__BPELPROJECTNAME__", bpelProjDir); // NOI18N
                     buffer.append(line);
                     buffer.append("\n"); // NOI18N
                 }
-                
+
                 File file = FileUtil.toFile(fileObject);
                 OutputStream outputStream = new FileOutputStream(file);
                 PrintWriter writer = new PrintWriter(outputStream);
@@ -322,26 +308,23 @@ public class RestSampleUtils {
             }
         }
     }
-    
+
     public static void addJerseyLibrary(Project project) {
         Library jerseyLibrary = LibraryManager.getDefault().getLibrary(JERSEY_LIBRARY);
-        
+
         if (jerseyLibrary == null) {
             System.out.println("no jersey library found");
             return;
         }
-        
+
         SourceGroup[] sgs = ProjectUtils.getSources(project).getSourceGroups(JavaProjectConstants.SOURCES_TYPE_JAVA);
         FileObject sourceRoot = sgs[0].getRootFolder();
-        String[] classPathTypes = new String[] {
-                ClassPath.COMPILE,
-                ClassPath.EXECUTE};
-        
+        String[] classPathTypes = new String[]{ClassPath.COMPILE, ClassPath.EXECUTE};
+
         for (String type : classPathTypes) {
             try {
-                ProjectClassPathModifier.addLibraries(new Library[] { jerseyLibrary }, sourceRoot, type);
-              
-            } catch(Exception ex) {
+                ProjectClassPathModifier.addLibraries(new Library[]{jerseyLibrary}, sourceRoot, type);
+            } catch (Exception ex) {
                 ex.printStackTrace();
             }
         }
