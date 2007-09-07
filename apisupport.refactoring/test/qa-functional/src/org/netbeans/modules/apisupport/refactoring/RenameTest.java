207225567e47adbc12b10b660cc3e87fde097c63
jglick <jglick@netbeans.org>
2007-09-07 16:10:15 +0000

    Commenting out tests as they have not been rewritten to Retouche.

--- apisupport.refactoring/test/qa-functional/src/org/netbeans/modules/apisupport/refactoring/RenameTest.java
+++ apisupport.refactoring/test/qa-functional/src/org/netbeans/modules/apisupport/refactoring/RenameTest.java
@@ -26,23 +26,8 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import org.netbeans.modules.refactoring.api.Problem;
-import org.netbeans.modules.refactoring.api.RefactoringElement;
-import org.netbeans.modules.refactoring.api.WhereUsedQuery;
-import org.netbeans.modules.refactoring.api.RefactoringSession;
-import org.netbeans.jmi.javamodel.*;
 import org.netbeans.junit.NbTestCase;
 import org.netbeans.junit.NbTestSuite;
-import org.netbeans.modules.refactoring.api.RenameRefactoring;
-import org.openide.filesystems.FileStateInvalidException;
-
  
 /**
  *
@@ -68,8 +53,8 @@ public class RenameTest extends NbTestCase {
     
     private String PATH_PREFIX = "";
     
-    private static TypeClass typeProxy;
-    private static JavaClass jc;
+//    private static TypeClass typeProxy;
+//    private static JavaClass jc;
 
     private PrintStream refPs;
 
@@ -81,62 +66,62 @@ public class RenameTest extends NbTestCase {
     
     public static NbTestSuite suite() {
         NbTestSuite suite = new NbTestSuite();
-        suite.addTestSuite(RenameTest.class);
+//        suite.addTestSuite(RenameTest.class);
         return suite;
     }
     
-    public void testRename() throws FileStateInvalidException, IOException {
-            jc = (JavaClass) TestUtility.findClass("testRename.MyAction");
-            typeProxy = ((JavaModelPackage) jc.refOutermostPackage()).getType();
-
-            jc = (JavaClass) typeProxy.resolve("testRename.MyAction");
-            RenameRefactoring refactoring = new RenameRefactoring(jc);
-            refactoring.setNewName("RenamedAction");
-            refactoring.checkParameters();
-            RefactoringSession result = RefactoringSession.create("rename class");
-            refactoring.prepare(result);
-            result.doRefactoring(true);
-
-            jc = (JavaClass) TestUtility.findClass("testRename.MyDataLoader");
-            typeProxy = ((JavaModelPackage) jc.refOutermostPackage()).getType();
-
-            jc = (JavaClass) typeProxy.resolve("testRename.MyDataLoader");
-             refactoring = new RenameRefactoring(jc);
-            refactoring.setNewName("RenamedLoader");
-            refactoring.checkParameters();
-            result = RefactoringSession.create("rename class"); 
-            refactoring.prepare(result);
-            result.doRefactoring(true);
-
-            // check modified files
-            for (int x = 0; x < resultFiles.length; x++) {
-                String fileName = PATH_PREFIX + resultFiles[x] ;
-                log("assertFile " + fileName);
-                File resF = TestUtility.getFile(getDataDir(),"testRename", fileName);
-                File goldenF = getGoldenFile(goldenFiles[x]);  
-                File f1 = writeToWorkDir(resF,resF.getName() + ".result");
-                File f2 = writeToWorkDir(goldenF,goldenF.getName() + ".pass");
-                assertFile(f1,f2);
-                f1.delete();
-                f2.delete();
-            }
-    } 
-    public void testWhereUsed() throws Exception {
-       
-        File f = new File(getWorkDir(),"whereUsed.ref" );
-        refPs = new PrintStream(new FileOutputStream(f)); 
-        jc = (JavaClass) TestUtility.findClass("testRename.WhereUsedDataLoader");
-        ref("testrename.MyDataLoader");
-        WhereUsedQuery wu= new WhereUsedQuery(jc);
-        wu.setSearchInComments(true);
-        findClass(wu);
-        wu = new WhereUsedQuery(TestUtility.findClass("testRename.WhereUsedAction"));
-        ref("testrename.WhereUsedAction");
-        findClass(wu);
-        refPs.close();
-        assertFile(f,  getGoldenFile("whereUsed.ref"));
-                
-    }
+//    public void testRename() throws FileStateInvalidException, IOException {
+//            jc = (JavaClass) TestUtility.findClass("testRename.MyAction");
+//            typeProxy = ((JavaModelPackage) jc.refOutermostPackage()).getType();
+//
+//            jc = (JavaClass) typeProxy.resolve("testRename.MyAction");
+//            RenameRefactoring refactoring = new RenameRefactoring(jc);
+//            refactoring.setNewName("RenamedAction");
+//            refactoring.checkParameters();
+//            RefactoringSession result = RefactoringSession.create("rename class");
+//            refactoring.prepare(result);
+//            result.doRefactoring(true);
+//
+//            jc = (JavaClass) TestUtility.findClass("testRename.MyDataLoader");
+//            typeProxy = ((JavaModelPackage) jc.refOutermostPackage()).getType();
+//
+//            jc = (JavaClass) typeProxy.resolve("testRename.MyDataLoader");
+//             refactoring = new RenameRefactoring(jc);
+//            refactoring.setNewName("RenamedLoader");
+//            refactoring.checkParameters();
+//            result = RefactoringSession.create("rename class"); 
+//            refactoring.prepare(result);
+//            result.doRefactoring(true);
+//
+//            // check modified files
+//            for (int x = 0; x < resultFiles.length; x++) {
+//                String fileName = PATH_PREFIX + resultFiles[x] ;
+//                log("assertFile " + fileName);
+//                File resF = TestUtility.getFile(getDataDir(),"testRename", fileName);
+//                File goldenF = getGoldenFile(goldenFiles[x]);  
+//                File f1 = writeToWorkDir(resF,resF.getName() + ".result");
+//                File f2 = writeToWorkDir(goldenF,goldenF.getName() + ".pass");
+//                assertFile(f1,f2);
+//                f1.delete();
+//                f2.delete();
+//            }
+//    } 
+//    public void testWhereUsed() throws Exception {
+//       
+//        File f = new File(getWorkDir(),"whereUsed.ref" );
+//        refPs = new PrintStream(new FileOutputStream(f)); 
+//        jc = (JavaClass) TestUtility.findClass("testRename.WhereUsedDataLoader");
+//        ref("testrename.MyDataLoader");
+//        WhereUsedQuery wu= new WhereUsedQuery(jc);
+//        wu.setSearchInComments(true);
+//        findClass(wu);
+//        wu = new WhereUsedQuery(TestUtility.findClass("testRename.WhereUsedAction"));
+//        ref("testrename.WhereUsedAction");
+//        findClass(wu);
+//        refPs.close();
+//        assertFile(f,  getGoldenFile("whereUsed.ref"));
+//                
+//    }
 //        
 //    
 //    public static String getAsString(String file) {
@@ -155,81 +140,81 @@ public class RenameTest extends NbTestCase {
 //        return result;
 //    }
     
-    protected void findClass(WhereUsedQuery wu) {
-        RefactoringSession result = RefactoringSession.create(null);
-        refProblems(wu.prepare(result));
-        refUsages(result);
-        ref("");
-    }
+//    protected void findClass(WhereUsedQuery wu) {
+//        RefactoringSession result = RefactoringSession.create(null);
+//        refProblems(wu.prepare(result));
+//        refUsages(result);
+//        ref("");
+//    }
 
     /**
      * Stores problems into ref file. Problems should be sorted.
      * @return true if problem is not null and one of them is fatal
      */
-    public boolean refProblems(Problem problem) {
-        Problem p=problem;
-        boolean ret=false;
-        if (p != null) {
-            ArrayList list=new ArrayList();
-            while (p != null) {
-                if (p.isFatal()) {
-                    ret=true;
-                    list.add("Problem fatal: "+p.getMessage());
-                } else {
-                    list.add("Problem: "+p.getMessage());
-                }
-                p=p.getNext();
-            }
-            Collections.sort(list);
-            for (int i=0;i < list.size();i++) {
-                ref(list.get(i));
-            }
-        }
-        return ret;
-    } 
-     protected void refUsages(RefactoringSession session) {
-        Collection result = session.getRefactoringElements();
-        ArrayList list=new ArrayList();
-        HashMap map=new HashMap();
-        for (Iterator it=result.iterator();it.hasNext();) {
-            Object o=it.next();
-            if (o instanceof RefactoringElement) {
-                RefactoringElement wue=(RefactoringElement) o;
-                Element el = wue.getJavaElement();
-                if (el != null && el.getResource() != null) {
-                    String s;
-                    s=el.getResource().getName().replace(File.separatorChar,'/');
-                    list=(ArrayList)(map.get(s));
-                    if (list == null) {
-                        list=new ArrayList();
-                        map.put(s, list);
-                    }
-                    list.add(getDisplayText(wue));
-                } else {
-                    log("refUsages without resource");
-                    log(getDisplayText(wue));
-                    map.put(getDisplayText(wue), "");
-                }
-            }
-        }
-        ref("Found "+String.valueOf(result.size())+" occurance(s).");
-        Object[] keys=map.keySet().toArray();
-        Arrays.sort(keys);
-        for (int i=0;i < keys.length;i++) {
-            ref("");
-            if (map.get(keys[i]) instanceof ArrayList) {
-                ref(keys[i]);
-                list=(ArrayList)(map.get(keys[i]));
-                Collections.sort(list);
-                for (int j=0;j < list.size();j++) {
-                    ref("      "+list.get(j));
-                }
-            } else {
-                ref(keys[i]);
-            }
-        }
-        ref("");
-    }
+//    public boolean refProblems(Problem problem) {
+//        Problem p=problem;
+//        boolean ret=false;
+//        if (p != null) {
+//            ArrayList list=new ArrayList();
+//            while (p != null) {
+//                if (p.isFatal()) {
+//                    ret=true;
+//                    list.add("Problem fatal: "+p.getMessage());
+//                } else {
+//                    list.add("Problem: "+p.getMessage());
+//                }
+//                p=p.getNext();
+//            }
+//            Collections.sort(list);
+//            for (int i=0;i < list.size();i++) {
+//                ref(list.get(i));
+//            }
+//        }
+//        return ret;
+//    } 
+//     protected void refUsages(RefactoringSession session) {
+//        Collection result = session.getRefactoringElements();
+//        ArrayList list=new ArrayList();
+//        HashMap map=new HashMap();
+//        for (Iterator it=result.iterator();it.hasNext();) {
+//            Object o=it.next();
+//            if (o instanceof RefactoringElement) {
+//                RefactoringElement wue=(RefactoringElement) o;
+//                Element el = wue.getJavaElement();
+//                if (el != null && el.getResource() != null) {
+//                    String s;
+//                    s=el.getResource().getName().replace(File.separatorChar,'/');
+//                    list=(ArrayList)(map.get(s));
+//                    if (list == null) {
+//                        list=new ArrayList();
+//                        map.put(s, list);
+//                    }
+//                    list.add(getDisplayText(wue));
+//                } else {
+//                    log("refUsages without resource");
+//                    log(getDisplayText(wue));
+//                    map.put(getDisplayText(wue), "");
+//                }
+//            }
+//        }
+//        ref("Found "+String.valueOf(result.size())+" occurance(s).");
+//        Object[] keys=map.keySet().toArray();
+//        Arrays.sort(keys);
+//        for (int i=0;i < keys.length;i++) {
+//            ref("");
+//            if (map.get(keys[i]) instanceof ArrayList) {
+//                ref(keys[i]);
+//                list=(ArrayList)(map.get(keys[i]));
+//                Collections.sort(list);
+//                for (int j=0;j < list.size();j++) {
+//                    ref("      "+list.get(j));
+//                }
+//            } else {
+//                ref(keys[i]);
+//            }
+//        }
+//        ref("");
+//    }
    public void ref(String s) {
        refPs.println(s);
     }
@@ -246,15 +231,15 @@ public class RenameTest extends NbTestCase {
         }
         br.close();
     } 
-     protected String getDisplayText(RefactoringElement elm) {
-        String app="";
-        if (elm.getStatus() == RefactoringElement.WARNING) {
-            app=" [ warning! ]";
-        } else if (elm.getStatus() == RefactoringElement.GUARDED) {
-            app=" [ error: code is in guarded block ]";
-        }
-        return elm.getDisplayText()+app;
-     }   
+//     protected String getDisplayText(RefactoringElement elm) {
+//        String app="";
+//        if (elm.getStatus() == RefactoringElement.WARNING) {
+//            app=" [ warning! ]";
+//        } else if (elm.getStatus() == RefactoringElement.GUARDED) {
+//            app=" [ error: code is in guarded block ]";
+//        }
+//        return elm.getDisplayText()+app;
+//     }   
 
     private File writeToWorkDir(File resF, String name) throws IOException {
        byte buff[] =  new byte[(int)resF.length()]; 
--- apisupport.refactoring/test/qa-functional/src/org/netbeans/modules/apisupport/refactoring/TestUtility.java
+++ apisupport.refactoring/test/qa-functional/src/org/netbeans/modules/apisupport/refactoring/TestUtility.java
@@ -16,72 +16,18 @@
  * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
+
 package org.netbeans.modules.apisupport.refactoring;
 
 import java.io.File;
-import org.netbeans.api.mdr.MDRepository;
-import org.netbeans.junit.NbTestCase;
-import org.netbeans.modules.javacore.internalapi.JavaMetamodel;
-import org.netbeans.jmi.javamodel.JavaModelPackage;
-import org.netbeans.jmi.javamodel.JavaClass;
-import org.netbeans.jmi.javamodel.UnresolvedClass;
 import org.openide.filesystems.FileStateInvalidException;
-import org.openide.filesystems.FileSystem;
-import org.openide.filesystems.Repository;
-
 
 public class TestUtility {
 
-    public static void beginTrans(boolean writeAccess) {
-        getDefaultRepository().beginTrans(writeAccess);
-    }
-        
-    public static void endTrans(boolean rollback) {
-        getDefaultRepository().endTrans(rollback);
-    }
-        
-    public static void endTrans() {
-        getDefaultRepository().endTrans();
-    }
-        
-    public static MDRepository getDefaultRepository() {
-        return JavaMetamodel.getDefaultRepository();
-    }
-        
     public static File getFile(File dataDir,String projectName, String fileName) throws FileStateInvalidException {
         String result = dataDir.getAbsolutePath() +"/" + projectName + "/" + fileName;
         System.out.println("looking for file: " + result);
         return new File(result);
     }
 
-    public static JavaModelPackage getJavaModelPackage(String pattern) {
-        throw new UnsupportedOperationException("Method is no longer supported. " +
-            "Rewrite your test to new project infrastructure!");
-    }
-    
-    public static FileSystem findFileSystem(String pattern) {
-        throw new UnsupportedOperationException("Method is no longer supported. " +
-            "Rewrite your test to new project infrastructure!");
-    }
-    
-    public static JavaClass findClass(String s) {
-        JavaClass result;
-        int i = 20;
-        do {
-            result = (JavaClass) JavaMetamodel.getManager().getDefaultExtent().getType().resolve(s);
-            if (result instanceof UnresolvedClass) {
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                    return null;
-                }
-            }
-            i--;
-        } while ((result instanceof UnresolvedClass) && i > 0);
-        if (result instanceof UnresolvedClass) {
-            throw new IllegalStateException("Class " + s + " not found.");
-        }
-        return result;
-    }
 }
