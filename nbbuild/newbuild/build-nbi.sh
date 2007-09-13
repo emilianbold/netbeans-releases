8e4b4b07368bf8e838fa4459114fdf36bc259cdd
mzlamal <mzlamal@netbeans.org>
2007-09-13 10:32:18 +0000

    Improve error handling for mac native installers

--- nbbuild/newbuild/build-nbi.sh
+++ nbbuild/newbuild/build-nbi.sh
@@ -12,7 +12,17 @@ cd NBI
 
 if [ ! -z $NATIVE_MAC_MACHINE ]; then
    ssh $NATIVE_MAC_MACHINE rm -f $MAC_PATH/zip/*
-   scp $DIST/zip/$BASENAME*.zip $NATIVE_MAC_MACHINE:$MAC_PATH/zip
+   EXIT_CODE=$?
+   if [ $ERROR_CODE != 0 ]; then
+       echo "ERROR: $ERROR_CODE - Connection to MAC machine $NATIVE_MAC_MACHINE failed, can't remove old bits"
+       exit $ERROR_CODE;
+   fi
+   scp -q -v $DIST/zip/$BASENAME*.zip $NATIVE_MAC_MACHINE:$MAC_PATH/zip
+   EXIT_CODE=$?
+   if [ $ERROR_CODE != 0 ]; then
+       echo "ERROR: $ERROR_CODE - Connection to MAC machine $NATIVE_MAC_MACHINE failed, can't put the zips"
+       exit $ERROR_CODE;
+   fi
    ssh $NATIVE_MAC_MACHINE $MAC_PATH/run-mac-installer.sh > $MAC_LOG 2>&1 &
 fi
 
@@ -41,13 +51,19 @@ rmdir $DIST/installers
 #Check if Mac installer was OK, 10 "BUILD SUCCESSFUL" messages should be in Mac log
 if [ ! -z $NATIVE_MAC_MACHINE ]; then
     IS_MAC_FAILED=`cat $MAC_LOG | grep "BUILD FAILED" | wc -l | tr " " "\n" | grep -v '^$'`
-    if [ $IS_MAC_FAILED -eq 0 ]; then
+    IS_MAC_CONNECT=`cat $MAC_LOG | grep "Connection timed out" | wc -l | tr " " "\n" | grep -v '^$'`
+    if [ $IS_MAC_FAILED -eq 0 -a $IS_MAC_CONNECT -eq 0 ]; then
         #copy the bits back
         mkdir -p $DIST/bundles
         scp $NATIVE_MAC_MACHINE:$MAC_PATH/installer/mac/dist/* $DIST/bundles
+        EXIT_CODE=$?
+        if [ $ERROR_CODE != 0 ]; then
+            echo "ERROR: $ERROR_CODE - Connection to MAC machine $NATIVE_MAC_MACHINE failed, can't get installers"
+            exit $ERROR_CODE;
+        fi    
     else
         tail -100 $MAC_LOG
         echo "ERROR: - Native Mac NBI installers build failed"
-#        exit 1;
+        exit 1;
     fi
 fi
