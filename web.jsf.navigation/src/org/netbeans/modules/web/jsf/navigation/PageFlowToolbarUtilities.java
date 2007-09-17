08564df1a2f2fab99d2a4f4694c671b2bada68e4
JoelleLam <devnull@localhost>
2007-09-17 23:35:47 +0000

    Fix 115385 for memory leak.

--- web.jsf.navigation/src/org/netbeans/modules/web/jsf/navigation/PageFlowToolbarUtilities.java
+++ web.jsf.navigation/src/org/netbeans/modules/web/jsf/navigation/PageFlowToolbarUtilities.java
@@ -32,6 +32,8 @@ import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.lang.ref.Reference;
+import java.lang.ref.WeakReference;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -49,24 +51,23 @@ import org.openide.util.NbBundle;
  * @author joelle
  */
 public class PageFlowToolbarUtilities {
-    
+
     private static final Logger LOGGER = Logger.getLogger("org.netbeans.modules.web.jsf.navigation");
     //    private static final int SCOPE_FACESCONFIG = 1;
     //    private static final int SCOPE_PROJECT = 2;
-    
+
     public static enum Scope {
+
         SCOPE_FACESCONFIG, SCOPE_PROJECT, SCOPE_ALL_FACESCONFIG
     }
-    
     private static Map<Scope, String> scope2String = new HashMap<Scope, String>();
     private static Map<String, Scope> string2Scope = new HashMap<String, Scope>();
     private static final String TT_SCOPE = NbBundle.getMessage(PageFlowToolbarUtilities.class, "TT_Scope_DropDown");
     private static final String LBL_SCOPE_FACESCONFIG = NbBundle.getMessage(PageFlowToolbarUtilities.class, "LBL_Scope_FacesConfig");
-    private static final String LBL_SCOPE_PROJECT = NbBundle.getMessage(PageFlowToolbarUtilities.class, "LBL_Scope_Project");      
-    private static final String LBL_SCOPE_ALL_PROJECT = NbBundle.getMessage(PageFlowToolbarUtilities.class, "LBL_Scope_All_FacesConfig"); 
-    
+    private static final String LBL_SCOPE_PROJECT = NbBundle.getMessage(PageFlowToolbarUtilities.class, "LBL_Scope_Project");
+    private static final String LBL_SCOPE_ALL_PROJECT = NbBundle.getMessage(PageFlowToolbarUtilities.class, "LBL_Scope_All_FacesConfig");
     private static final String TT_LAYOUTPAGES = NbBundle.getMessage(PageFlowToolbarUtilities.class, "TT_LayoutPages");
-    static  {        
+    static {
         /* Loading these for quick access */
         scope2String.put(Scope.SCOPE_FACESCONFIG, LBL_SCOPE_FACESCONFIG);
         scope2String.put(Scope.SCOPE_PROJECT, LBL_SCOPE_PROJECT);
@@ -75,44 +76,40 @@ public class PageFlowToolbarUtilities {
         string2Scope.put(LBL_SCOPE_PROJECT, Scope.SCOPE_PROJECT);
         string2Scope.put(LBL_SCOPE_ALL_PROJECT, Scope.SCOPE_ALL_FACESCONFIG);
     }
-    
-    
     private Scope currentScope;
     //    private PageFlowController pfc;
-    private final PageFlowView view;
-    
-    public static final String getScopeLabel( Scope scope ){
+
+    public static final String getScopeLabel(Scope scope) {
         return scope2String.get(scope);
     }
-    
-    public static final Scope getScope( String scopeStr ){
+
+    public static final Scope getScope(String scopeStr) {
         return string2Scope.get(scopeStr);
     }
-    
+
     /** Creates a new instance of PageFlowController
      * @param scene
      */
-    private PageFlowToolbarUtilities(PageFlowView view ) {
+    private PageFlowToolbarUtilities(PageFlowView view) {
         currentScope = Scope.SCOPE_PROJECT;
-        this.view = view;
+        setPageFlowView(view);
     }
-    
     private static PageFlowToolbarUtilities instance;
-    
-    private static final WeakHashMap<PageFlowView,PageFlowToolbarUtilities> map = new WeakHashMap<PageFlowView,PageFlowToolbarUtilities>();
+    private static final Map<PageFlowView, PageFlowToolbarUtilities> map = new WeakHashMap<PageFlowView, PageFlowToolbarUtilities>();
+
     /**
      *Accessor to get the instance. Singleton pattern
      * @return pageFlowUtilities instance
      */
-    public static PageFlowToolbarUtilities getInstance(PageFlowView view){
+    public static PageFlowToolbarUtilities getInstance(PageFlowView view) {
         PageFlowToolbarUtilities myInstance = map.get(view);
-        if (myInstance == null){
+        if (myInstance == null) {
             myInstance = new PageFlowToolbarUtilities(view);
             map.put(view, myInstance);
         }
         return myInstance;
     }
-    
+
     /**
      * Get the current page flow editor scope
      * @return currentScope (LBL_SCOPE_PROJECT,LBL_SCOPE_FACESCONFIG)
@@ -120,58 +117,55 @@ public class PageFlowToolbarUtilities {
     public Scope getCurrentScope() {
         return currentScope;
     }
-    
-   
-    public void setCurrentScope( Scope scope ){
+
+    public void setCurrentScope(Scope scope) {
         this.currentScope = scope;
-        if( scopeBox != null && !scopeBox.getSelectedItem().equals(getScopeLabel(currentScope))){
+        if (scopeBox != null && !scopeBox.getSelectedItem().equals(getScopeLabel(currentScope))) {
             scopeBox.setSelectedItem(getScopeLabel(currentScope));
         }
     }
-    
-    
     private static final Logger LOGGERUI = Logger.getLogger("org.netbeans.ui.web.jsf.navigation");
     private JComboBox scopeBox;
+
     /**
      * Creates a JComboBox for the user to select the scope type.
      * @param view
      * @param pfc
      * @return
      */
-    public JComboBox createScopeComboBox(){
+    public JComboBox createScopeComboBox() {
         JComboBox comboBox = new JComboBox();
         comboBox.addItem(getScopeLabel(Scope.SCOPE_FACESCONFIG));
         comboBox.addItem(getScopeLabel(Scope.SCOPE_PROJECT));
         comboBox.addItem(getScopeLabel(Scope.SCOPE_ALL_FACESCONFIG));
-        
+
         //Set the appropriate size of the combo box so it doesn't take up the whole page.
         Dimension prefSize = comboBox.getPreferredSize();
         comboBox.setMinimumSize(prefSize);
         comboBox.setMaximumSize(prefSize);
-        
+
         comboBox.setSelectedItem(getScopeLabel(currentScope));
-        
-        comboBox.addItemListener( new ItemListener() {
-            
-            public void itemStateChanged(ItemEvent event)  {
-                if ( event.getStateChange() == ItemEvent.SELECTED ) {
-                    
-                    String newScope = (String)event.getItem();
+
+        comboBox.addItemListener(new ItemListener() {
+
+            public void itemStateChanged(ItemEvent event) {
+                PageFlowView view = getPageFlowView();
+                if (event.getStateChange() == ItemEvent.SELECTED) {
+                    String newScope = (String) event.getItem();
                     /* Save Locations before switching scope */
                     view.saveLocations();
-                    
+
                     LogRecord record = new LogRecord(Level.FINE, "PageFLowEditor Scope Changed");
                     record.setSourceClassName("PageFlowUtilities.ItemListener");
                     record.setSourceMethodName("itemStateChanged");
-                    record.setParameters(new Object[] {newScope, new Date()});
+                    record.setParameters(new Object[]{newScope, new Date()});
                     LOGGERUI.log(record);
-                    
+
                     setCurrentScope(getScope(newScope));
                     //As we are setting the current scope, we should update the controller and update the scene.  But what happens with setup?
-                    
                     /* We don't want the background process to continue adding pins to the pages */
                     view.clearBackgroundPinAddingProcess();
-                    
+
                     /* You don't want to override the data you just stored */
                     view.getPageFlowController().setupGraphNoSaveData();
                 }
@@ -184,34 +178,46 @@ public class PageFlowToolbarUtilities {
     }
     private static final Image LAYOUT_ICON = org.openide.util.Utilities.loadImage("org/netbeans/modules/web/jsf/navigation/resources/navigation.gif"); // NOI18N
     private JButton layoutButton = null;
+
     /**
      * Creates a JComboBox for the user to select the scope type.
      * @param view
      * @param pfc
      * @return
      */
-    public JButton createLayoutButton(){
-        
-        if ( layoutButton != null ){
+    public JButton createLayoutButton() {
+
+        if (layoutButton != null) {
             return layoutButton;
         }
-        
+
         layoutButton = new JButton(new ImageIcon(LAYOUT_ICON));
         //Set the appropriate size of the combo box so it doesn't take up the whole page.
         //        Dimension prefSize = layoutButton.getPreferredSize();
         //        layoutButton.setMinimumSize(prefSize);
         //        layoutButton.setMaximumSize(prefSize);
         layoutButton.setToolTipText(TT_LAYOUTPAGES);
-        layoutButton.addMouseListener( new MouseAdapter() {
-            
+        layoutButton.addMouseListener(new MouseAdapter() {
+
+            final PageFlowView view = getPageFlowView();
+
             @Override
             public void mouseClicked(MouseEvent e) {
                 view.layoutNodes();
                 view.requestMultiViewActive();
             }
-            
         });
         return layoutButton;
     }
-        
+    private Reference<PageFlowView> pageFlowViewRef;
+
+    /* Get's the PageFlowView of the current utilities */
+    public PageFlowView getPageFlowView() {
+        return pageFlowViewRef.get();
+    }
+
+    /* Sets the PageFlowView in a WeakReference */
+    public void setPageFlowView(PageFlowView view) {
+        pageFlowViewRef = new WeakReference<PageFlowView>(view);
+    }
 }
