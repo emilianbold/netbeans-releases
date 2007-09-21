1b51f7f1bd164ef3c47375d098485fab08b8ec82
pcw <pcw@netbeans.org>
2007-09-21 00:40:03 +0000

    Fixed bug that caused virtual nodes implied by annotations in web project source files not to show up in sun-web.xml config editor.

--- j2ee.sun.ddui/src/org/netbeans/modules/j2ee/sun/ddloaders/multiview/common/CommonBeanReader.java
+++ j2ee.sun.ddui/src/org/netbeans/modules/j2ee/sun/ddloaders/multiview/common/CommonBeanReader.java
@@ -88,7 +88,7 @@ public abstract class CommonBeanReader
                 J2eeModule module = dc.getJ2eeModule();
                 if(module != null) {
                     if(J2eeModule.WAR == module.getModuleType()) {
-                        readWebAppMetadata(module.getMetadataModel(WebAppMetadata.class));
+                        result = readWebAppMetadata(module.getMetadataModel(WebAppMetadata.class));
                     } else if(J2eeModule.EJB == module.getModuleType()) {
                         result = readEjbJarMetadata(module.getMetadataModel(EjbJarMetadata.class));
                     } else if(J2eeModule.CLIENT == module.getModuleType()) {
