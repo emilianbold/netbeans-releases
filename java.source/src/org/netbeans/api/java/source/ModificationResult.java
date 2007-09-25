e418e09139059bd7709039cc5a1151688d1b6ead
pflaska <pflaska@netbeans.org>
2007-09-25 11:39:01 +0000

    #77710: Create file at when final commit() is called. File was accidently created when getResultingSource() was called to provide a preview. Method provides its Writer, but createUnit method does not respet this Writer and used Writer obtained from the output stream of a file.

--- java.source/src/org/netbeans/api/java/source/ModificationResult.java
+++ java.source/src/org/netbeans/api/java/source/ModificationResult.java
@@ -131,7 +131,7 @@ public final class ModificationResult {
                                     doc.insertString(diff.getStartPosition().getOffset(), diff.getNewText(), null);
                                     break;
                                 case CREATE:
-                                    createUnit(diff);
+                                    createUnit(diff, out);
                                     break;
                             }
                         } catch (BadLocationException ex) {
@@ -173,7 +173,7 @@ public final class ModificationResult {
                 if (diff.isExcluded())
                     continue;
                 if (Difference.Kind.CREATE == diff.getKind()) {
-                    createUnit(diff);
+                    createUnit(diff, out);
                     continue;
                 }
                 int pos = diff.getStartPosition().getOffset();
@@ -219,12 +219,14 @@ public final class ModificationResult {
         }            
     }
 
-    private void createUnit(Difference diff) {
+    private void createUnit(Difference diff, Writer out) {
         CreateChange change = (CreateChange) diff;
-        Writer w = null;
+        Writer w = out;
         try {
-            change.getFileObject().openOutputStream();
-            w = change.getFileObject().openWriter();
+            if (w == null) {
+                change.getFileObject().openOutputStream();
+                w = change.getFileObject().openWriter();
+            }
             w.append(change.getNewText());
         } catch (IOException e) {
             Logger.getLogger(WorkingCopy.class.getName()).log(Level.SEVERE, e.getMessage(), e);
