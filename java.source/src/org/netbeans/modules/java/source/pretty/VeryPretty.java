d9c66694577df35e7efd3dd767a7e5bf92fb9d57
jbecicka <jbecicka@netbeans.org>
2007-09-25 15:36:04 +0000

    #106804:  [Convert Anon to Inner] Wrong constructor parameter handling

--- java.source/src/org/netbeans/modules/java/source/pretty/VeryPretty.java
+++ java.source/src/org/netbeans/modules/java/source/pretty/VeryPretty.java
@@ -2009,7 +2009,7 @@ public final class VeryPretty extends JCTree.Visitor {
                 if (mit.meth.getKind() == Kind.IDENTIFIER) {
                     JCIdent it = (JCIdent) mit.getMethodSelect();                    
                     if (it.name == names._super) {
-                        return TreeInfo.getEndPos(tree, origUnit.endPositions) < 0;
+                        return TreeInfo.getEndPos(tree, origUnit.endPositions) < 0 && tree.pos!=NOPOS;
                     }
                 }
             }
