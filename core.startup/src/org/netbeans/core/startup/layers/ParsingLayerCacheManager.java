3cc332b2057cc28a3d331b1d54c6cbdee180a460
jglick <jglick@netbeans.org>
2007-09-25 17:04:55 +0000

    Removing reference to very old dep in warning.

--- core.startup/src/org/netbeans/core/startup/layers/ParsingLayerCacheManager.java
+++ core.startup/src/org/netbeans/core/startup/layers/ParsingLayerCacheManager.java
@@ -322,7 +322,7 @@ public abstract class ParsingLayerCacheManager extends LayerCacheManager impleme
             if (text.length() > 0) {
                 MemFile file = (MemFile)curr.peek();
                 if (file.ref != null) throw new SAXParseException("CDATA plus url= in <file>", locator);
-                LayerCacheManager.err.warning("use of inline CDATA text contents in <file name=\"" + file.name + "\"> deprecated for performance and charset safety at " + locator.getSystemId() + ":" + locator.getLineNumber() + ". Please use the 'url' attribute instead, or the file attribute 'originalFile' on *.shadow files (with IDE/1 > 2.23).");
+                LayerCacheManager.err.warning("use of inline CDATA text contents in <file name=\"" + file.name + "\"> deprecated for performance and charset safety at " + locator.getSystemId() + ":" + locator.getLineNumber() + ". Please use the 'url' attribute instead, or the file attribute 'originalFile' on *.shadow files.");
                 // Note: platform default encoding used. If you care about the encoding,
                 // you had better be using url= instead.
                 file.contents = text.getBytes();
