33b8c4b403d9bd29c9874eaf71c67171946b32a0
alexvsimon <alexvsimon@netbeans.org>
2007-09-26 10:52:27 +0000

    add unit test for IZ#97544 "Reformat Code" works wrong with class declarations

---
+++ cnd/test/unit/src/org/netbeans/modules/cnd/editor/cplusplus/CCFormatterUnitTestCase.java
@@ -0,0 +1,63 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ * 
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ * 
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ * 
+ * Portions Copyrighted 2007 Sun Microsystems, Inc.
+ */
+
+package org.netbeans.modules.cnd.editor.cplusplus;
+
+import javax.swing.text.BadLocationException;
+import javax.swing.text.EditorKit;
+import org.netbeans.editor.Formatter;
+import org.netbeans.modules.cnd.test.base.BaseDocumentUnitTestCase;
+
+/**
+ *
+ * @author Alexander Simon
+ */
+public class CCFormatterUnitTestCase extends BaseDocumentUnitTestCase {
+    
+    public CCFormatterUnitTestCase(String testMethodName) {
+        super(testMethodName);
+    }
+    
+    @Override
+    protected EditorKit createEditorKit() {
+        return new CCKit();
+    }
+
+    /**
+     * Perform new-line insertion followed by indenting of the new line
+     * by the formatter.
+     * The caret position should be marked in the document text by '|'.
+     */
+    protected void indentNewLine() {
+        Formatter f = getDocument().getFormatter();
+        int offset = f.indentNewLine(getDocument(), getCaretOffset());
+        getCaret().setDot(offset);
+    }
+    
+    /**
+     * Perform reformatting of the whole document's text.
+     */
+    protected void reformat() {
+        Formatter f = getDocument().getFormatter();
+        try {
+            f.reformat(getDocument(), 0, getDocument().getLength());
+        } catch (BadLocationException e) {
+            e.printStackTrace(getLog());
+            fail(e.getMessage());
+        }
+    }
+}
