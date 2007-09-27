5b93490d2c5de5d6a7847a818c1903569ae14e6a
jtulach <jtulach@netbeans.org>
2007-09-27 12:43:55 +0000

    #76728: No inter cluster dependencies on org.netbeans.core anymore

--- i18n/src/org/netbeans/modules/i18n/HelpStringCustomEditor.java
+++ i18n/src/org/netbeans/modules/i18n/HelpStringCustomEditor.java
@@ -23,15 +23,19 @@ package org.netbeans.modules.i18n;
 
 import java.awt.Component;
 import java.awt.event.KeyEvent;
+import java.beans.FeatureDescriptor;
+import java.beans.PropertyEditorSupport;
 import java.util.List;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.text.BadLocationException;
-import org.netbeans.beaninfo.editors.StringEditor;
 import org.openide.awt.Mnemonics;
 import org.openide.util.HelpCtx;
 import org.openide.ErrorManager;
+import org.openide.explorer.propertysheet.ExPropertyEditor;
+import org.openide.explorer.propertysheet.PropertyEnv;
+import org.openide.nodes.Node;
 
 
 /**
@@ -246,5 +250,84 @@ public class HelpStringCustomEditor extends JPanel {
             );
         }
     } // End of nested class I18nRegExpEditor.
+    private static abstract class StringEditor extends PropertyEditorSupport implements ExPropertyEditor {
+        private static boolean useRaw = Boolean.getBoolean("netbeans.stringEditor.useRawCharacters");
+
+       // bugfix# 9219 added editable field and isEditable() "getter" to be used in StringCustomEditor    
+        private boolean editable=true;   
+        /** gets information if the text in editor should be editable or not */
+        public boolean isEditable(){
+            return (editable);
+        }
+
+        /** sets new value */
+        public void setAsText(String s) {
+            if ( "null".equals( s ) && getValue() == null ) // NOI18N
+                return;
+            setValue(s);
+        }
+
+        public String getJavaInitializationString () {
+            String s = (String) getValue ();
+            return "\"" + toAscii(s) + "\""; // NOI18N
+        }
+
+        public boolean supportsCustomEditor () {
+            return customEd;
+        }
+
+        public abstract java.awt.Component getCustomEditor ();
+
+        private static String toAscii(String str) {
+            StringBuffer buf = new StringBuffer(str.length() * 6); // x -> \u1234
+            char[] chars = str.toCharArray();
+            for (int i = 0; i < chars.length; i++) {
+                char c = chars[i];
+                switch (c) {
+                case '\b': buf.append("\\b"); break; // NOI18N
+                case '\t': buf.append("\\t"); break; // NOI18N
+                case '\n': buf.append("\\n"); break; // NOI18N
+                case '\f': buf.append("\\f"); break; // NOI18N
+                case '\r': buf.append("\\r"); break; // NOI18N
+                case '\"': buf.append("\\\""); break; // NOI18N
+                    //        case '\'': buf.append("\\'"); break; // NOI18N
+                case '\\': buf.append("\\\\"); break; // NOI18N
+                default:
+                    if (c >= 0x0020 && (useRaw || c <= 0x007f))
+                        buf.append(c);
+                    else {
+                        buf.append("\\u"); // NOI18N
+                        String hex = Integer.toHexString(c);
+                        for (int j = 0; j < 4 - hex.length(); j++)
+                            buf.append('0');
+                        buf.append(hex);
+                    }
+                }
+            }
+            return buf.toString();
+        }
+
+        private String instructions=null;
+        private boolean oneline=false;
+        private boolean customEd=true;
+        private PropertyEnv env;
+
+        // bugfix# 9219 added attachEnv() method checking if the user canWrite in text box 
+        public void attachEnv(PropertyEnv env) {
+            this.env = env;
+
+            FeatureDescriptor desc = env.getFeatureDescriptor();
+            if (desc instanceof Node.Property){
+                Node.Property prop = (Node.Property)desc;
+                editable = prop.canWrite();
+                //enh 29294 - support one-line editor & suppression of custom
+                //editor
+                instructions = (String) prop.getValue ("instructions"); //NOI18N
+                oneline = Boolean.TRUE.equals (prop.getValue ("oneline")); //NOI18N
+                customEd = !Boolean.TRUE.equals (prop.getValue 
+                    ("suppressCustomEditor")); //NOI18N
+            }
+        }
+    }
     
 }
