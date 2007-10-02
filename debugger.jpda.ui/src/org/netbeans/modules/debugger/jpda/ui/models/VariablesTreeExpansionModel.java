44428d9d05fa045c735c65d4d7a96955727160cd
mentlicher <mentlicher@netbeans.org>
2007-10-02 15:21:30 +0000

    #90155 - Default expansions is set for Local Variables view.

---
+++ debugger.jpda.ui/src/org/netbeans/modules/debugger/jpda/ui/models/VariablesTreeExpansionModel.java
@@ -0,0 +1,95 @@
+/*
+ * The contents of this file are subject to the terms of the Common Development
+ * and Distribution License (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
+ * or http://www.netbeans.org/cddl.txt.
+ *
+ * When distributing Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at http://www.netbeans.org/cddl.txt.
+ * If applicable, add the following below the CDDL Header, with the fields
+ * enclosed by brackets [] replaced by your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ */
+
+package org.netbeans.modules.debugger.jpda.ui.models;
+
+import java.util.Set;
+import org.netbeans.spi.debugger.jpda.EditorContext.Operation;
+import org.netbeans.spi.viewmodel.TreeExpansionModel;
+import org.netbeans.spi.viewmodel.UnknownTypeException;
+import org.openide.util.WeakSet;
+
+
+/**
+ * @author   Jan Jancura
+ */
+public class VariablesTreeExpansionModel implements TreeExpansionModel {
+
+    private Set expandedNodes = new WeakSet();
+    private Set collapsedNodes = new WeakSet();
+
+    /**
+     * Defines default state (collapsed, expanded) of given node.
+     *
+     * @param node a node
+     * @return default state (collapsed, expanded) of given node
+     */
+    public boolean isExpanded (Object node) 
+    throws UnknownTypeException {
+        if (node instanceof String && ((String) node).startsWith("operationArguments ")) {
+            node = "operationArguments";
+        }
+        synchronized (this) {
+            if (expandedNodes.contains(node)) {
+                return true;
+            }
+            if (collapsedNodes.contains(node)) {
+                return false;
+            }
+        }
+        // Default behavior follows:
+        if ("lastOperations".equals(node)) {
+            return true;
+        }
+        if (node instanceof Operation) {
+            return true;
+        }
+        return false;
+    }
+    
+    /**
+     * Called when given node is expanded.
+     *
+     * @param node a expanded node
+     */
+    public void nodeExpanded (Object node) {
+        if (node instanceof String && ((String) node).startsWith("operationArguments ")) {
+            node = "operationArguments";
+        }
+        synchronized (this) {
+            expandedNodes.add(node);
+            collapsedNodes.remove(node);
+        }
+    }
+    
+    /**
+     * Called when given node is collapsed.
+     *
+     * @param node a collapsed node
+     */
+    public void nodeCollapsed (Object node) {
+        if (node instanceof String && ((String) node).startsWith("operationArguments ")) {
+            node = "operationArguments";
+        }
+        synchronized (this) {
+            collapsedNodes.add(node);
+            expandedNodes.remove(node);
+        }
+    }
+}
