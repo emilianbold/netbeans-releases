048e607457894409438cf85c52537ba80690aec1
musilt2 <musilt2@netbeans.org>
2007-10-02 13:57:04 +0000

    Added ruby debugger UI tests for basic  validation of functionality. Currently stabilized on linux.

---
+++ ruby.debugger/test/qa-functional/src/gui/ruby/debugger/BasicTests.java
@@ -0,0 +1,178 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ *
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ */
+package gui.ruby.debugger;
+
+import junit.textui.TestRunner;
+import org.netbeans.jellytools.Bundle;
+import org.netbeans.jellytools.EditorOperator;
+import org.netbeans.jellytools.JellyTestCase;
+import org.netbeans.jellytools.NbDialogOperator;
+import org.netbeans.jellytools.NewProjectNameLocationStepOperator;
+import org.netbeans.jellytools.NewProjectWizardOperator;
+import org.netbeans.jellytools.OutputTabOperator;
+import org.netbeans.jellytools.ProjectsTabOperator;
+import org.netbeans.jellytools.actions.Action;
+import org.netbeans.jemmy.JemmyProperties;
+import org.netbeans.jemmy.TimeoutExpiredException;
+import org.netbeans.junit.NbTestSuite;
+import org.netbeans.junit.ide.ProjectSupport;
+
+/**
+ * Basic ruby debugger test.
+ *
+ * @author Tomas.Musil@sun.com
+ */
+public class BasicTests extends JellyTestCase {
+    
+    
+    /** Need to be defined because of JUnit */
+    public BasicTests(String name) {
+        super(name);
+            }
+
+    public static NbTestSuite suite() {
+        NbTestSuite suite = new NbTestSuite();
+        suite.addTest(new BasicTests("testDebuggerStartStop"));
+        suite.addTest(new BasicTests("testStepInOutOver"));
+        suite.addTest(new BasicTests("testNativeRubyDebugging"));
+
+        return suite;
+    }
+    
+    /** Use for execution inside IDE */
+    public static void main(java.lang.String[] args) {
+        // run whole suite
+        TestRunner.run(suite());
+        // run only selected test case
+        //junit.textui.TestRunner.run(new RubyValidation("testCreateRubyProject"));
+    }
+    
+    /** Setup before every test case. */
+    public void setUp() {
+        System.out.println("########  "+getName()+"  #######");
+    }
+    
+    /** Teardown after every test case. */
+    public void tearDown() {
+    }
+    
+    // name of sample projects
+    private static final String SAMPLE_RUBY_PROJECT_NAME = "SampleRubyApplication";  //NOI18N
+    
+    public void testDebuggerStartStop() throws InterruptedException {
+        createRubyProject(SAMPLE_RUBY_PROJECT_NAME);
+        EditorOperator debuggeeOp = new EditorOperator("main.rb");
+        Util.putBreakpointToLine(debuggeeOp, 10);
+        Util.invokeDebugMainProject();
+        OutputTabOperator outOp = new OutputTabOperator(SAMPLE_RUBY_PROJECT_NAME + " (debug)");
+        outOp.waitText("ruby 1.8.5 debugger listens");
+        Util.invokeFinishDebuggerSession();
+        outOp.waitText("");
+        Thread.sleep(2000);
+        assertFalse("debugger is not running now", new Action("Run|Continue", null).isEnabled());
+    }
+
+    public void testStepInOutOver() throws InterruptedException{
+        EditorOperator debuggeeOp = new EditorOperator("main.rb");
+        debuggeeOp.setCaretPositionToLine(11);
+        debuggeeOp.insert("require 'date' \n d = Date.today \n puts d");
+        Util.invokeDebugMainProject();
+        Util.invokeStepOver(); //move to puts helloworld
+        Util.invokeStepOver(); //move to d = Date.new
+        Thread.sleep(500);
+        Util.invokeStepInto();
+        assertTrue("After step into PC should move to date.rb", Util.isOpenedEditorTab("date.rb"));
+        assertEquals("callstack size", 2, Util.getCallStackSize());
+        Util.invokeStepOut();
+        assertTrue("Local variables should contain d", Util.isLocalVariablePresent("d")); 
+        assertEquals("callstack size", 1, Util.getCallStackSize());
+        Util.invokeStepOver();
+    }
+
+    
+    public void testNativeRubyDebugging() throws InterruptedException{
+        String nativeRuby = Util.detectNativeRuby();
+        if (nativeRuby != null){
+            OutputTabOperator outOp = new OutputTabOperator(SAMPLE_RUBY_PROJECT_NAME + " (debug)");
+            Util.setRuby(nativeRuby);
+            Util.invokeDebugMainProject();
+            Util.invokeStepOver(); 
+            outOp.waitText("Hello World");
+            Util.invokeContinue();
+        } else {
+            fail ("did not found native ruby on $PATH;");
+        }
+    }
+    
+    //project generation method
+    private void createRubyProject(String projectName) {
+        // create new web application project
+        NewProjectWizardOperator npwo = NewProjectWizardOperator.invoke();
+        // "Ruby"
+        String rubyLabel = Bundle.getString("org.netbeans.modules.ruby.rubyproject.ui.wizards.Bundle", "Templates/Project/Ruby");
+        npwo.selectCategory(rubyLabel);
+        // "Ruby Application"
+        String rubyApplicationLabel = Bundle.getString("org.netbeans.modules.ruby.rubyproject.ui.wizards.Bundle", "TXT_NewJavaApp");
+        npwo.selectProject(rubyApplicationLabel);
+        npwo.btNext().pushNoBlock();
+        try {
+            // "Choose Ruby Interpreter"
+            String chooseRubyTitle = Bundle.getString("org.netbeans.api.ruby.platform.Bundle", "ChooseRuby");
+            new NbDialogOperator(chooseRubyTitle).cancel();
+        } catch (TimeoutExpiredException e) {
+            // ignore - Choose Ruby Interpreter dialog is opened only when native Ruby is available
+        }
+        NewProjectNameLocationStepOperator npnlso = new NewProjectNameLocationStepOperator();
+        npnlso.txtProjectName().setText(projectName);
+        npnlso.txtProjectLocation().setText(System.getProperty("netbeans.user")); // NOI18N
+        npnlso.finish();
+        // wait project appear in projects view
+        // wait 30 second
+        JemmyProperties.setCurrentTimeout("JTreeOperator.WaitNextNodeTimeout", 30000); // NOI18N
+        new ProjectsTabOperator().getProjectRootNode(projectName);
+        // wait classpath scanning finished
+        ProjectSupport.waitScanFinished();
+    }
+    
+    
+    
+    
+}
---
+++ ruby.debugger/test/qa-functional/src/gui/ruby/debugger/Util.java
@@ -0,0 +1,177 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ *
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ */
+package gui.ruby.debugger;
+
+import org.openide.util.Utilities;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+import javax.swing.JTextField;
+import org.netbeans.jellytools.EditorOperator;
+import org.netbeans.jellytools.OptionsOperator;
+import org.netbeans.jellytools.TopComponentOperator;
+import org.netbeans.jellytools.TreeTableOperator;
+import org.netbeans.jellytools.actions.Action;
+import org.netbeans.jellytools.modules.debugger.actions.ToggleBreakpointAction;
+import org.netbeans.jellytools.nodes.Node;
+import org.netbeans.jemmy.TimeoutExpiredException;
+import org.netbeans.jemmy.operators.JLabelOperator;
+import org.netbeans.jemmy.operators.JTableOperator;
+import org.netbeans.jemmy.operators.JTextFieldOperator;
+
+/**
+ * Utils for testing debugger
+ * @author Tomas.Musil@sun.com
+ */
+public final class Util {
+    
+    //collection of static methods
+    private Util(){}
+
+    public static String detectNativeRuby() {
+        String rubyPath = null;
+        String path = System.getenv("PATH"); // NOI18N
+        if (path == null) {
+            path = System.getenv("Path"); // NOI18N
+        }
+        if (path != null) {
+            final Set<String> rubies = new TreeSet<String>();
+            Set<String> dirs = new TreeSet<String>(Arrays.asList(path.split(File.pathSeparator)));
+            for (String dir : dirs) {
+                File f = null;
+                if (Utilities.isWindows()) {
+                    f = new File(dir, "ruby.exe");//NOI18N
+                } else {
+                    f = new File(dir, "ruby"); // NOI18N
+                    // Don't include /usr/bin/ruby on the Mac - it's no good
+                    if (Utilities.isMac() && "/usr/bin/ruby".equals(f.getPath())) {
+                            continue;
+                    }
+                }
+                if (f.exists()) {
+                    try {
+                        rubies.add(f.getCanonicalPath());
+                    } catch (IOException e) {
+                        rubies.add(f.getPath());
+                    }
+                }
+            }
+            
+            if(!rubies.isEmpty()) rubyPath = (String) rubies.toArray()[0]; //use first ruby on $PATH
+        }
+    return rubyPath;
+    }
+    
+    static void setRuby(String rubyPath) {
+        OptionsOperator optionsOper = OptionsOperator.invoke();
+        optionsOper.selectCategory("Ruby"); //NOI18N
+//        try{
+//            String chooseRubyTitle = Bundle.getString("org.netbeans.api.ruby.platform.Bundle", "ChooseRuby");
+//            new NbDialogOperator(chooseRubyTitle).closeByButton();
+//        } catch (TimeoutExpiredException exc) {
+//            // never mind
+//        }
+        JLabelOperator jloRI = new JLabelOperator(optionsOper, "Ruby Interpreter:");//NOI18N
+        new JTextFieldOperator((JTextField)jloRI.getLabelFor()).setText(rubyPath);
+        optionsOper.ok();
+    }
+
+    static boolean isLocalVariablePresent(String variableName) {
+        TopComponentOperator lvo = new TopComponentOperator("Local Variables");//NOI18N
+        JTableOperator lvtableOp = new JTableOperator(lvo);
+        TreeTableOperator tto = new TreeTableOperator((javax.swing.JTable) lvtableOp.getSource());
+
+        try {
+            Node varNode = new Node(tto.tree(), variableName);
+        } catch (TimeoutExpiredException timeoutExpiredException) {
+            return false;
+        }
+        return true;
+    }
+    
+    static boolean isOpenedEditorTab(String sourceName){
+        try {
+        EditorOperator eo = new EditorOperator(sourceName);
+        } catch (TimeoutExpiredException timeoutExpiredException) {
+            return false;
+        }
+        return true;
+
+    }
+    
+    static int getCallStackSize() {
+        TopComponentOperator callStackOp = new TopComponentOperator("Call Stack");//NOI18N
+        JTableOperator tableOp = new JTableOperator(callStackOp);
+        return tableOp.getRowCount();
+    }
+
+    
+    static void putBreakpointToLine(EditorOperator eo, int lineNumber) {
+        eo.setCaretPositionToLine(lineNumber);
+        new ToggleBreakpointAction().perform();
+    }
+
+    static void invokeDebugMainProject() {
+        new Action("Run|Debug Main Project", null).perform();//NOI18N
+    }
+
+    static void invokeFinishDebuggerSession() {
+        new Action("Run|Finish Debugger Session", null).perform();//NOI18N
+    }
+    
+    static void invokeStepOver(){
+        new Action("Run|Step Over", null).perform();//NOI18N
+    }
+    
+    static void invokeStepInto(){
+        new Action("Run|Step Into", null).perform();//NOI18N
+    }
+
+    static void invokeStepOut(){
+        new Action("Run|Step Out", null).perform();//NOI18N
+    }
+
+    static void invokeContinue() {
+        new Action("Run|Continue", null).perform();//NOI18N
+    }
+}
