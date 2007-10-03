e4f7fb14bd841ef731ad67e518247e667902cd1b
mkrauskopf <mkrauskopf@netbeans.org>
2007-10-03 10:17:12 +0000

    #115229: Showing javadoc for netbeans classes does not work

--- apisupport.project/src/org/netbeans/modules/apisupport/project/queries/GlobalJavadocForBinaryImpl.java
+++ apisupport.project/src/org/netbeans/modules/apisupport/project/queries/GlobalJavadocForBinaryImpl.java
@@ -44,6 +44,7 @@ package org.netbeans.modules.apisupport.project.queries;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -51,6 +52,10 @@ import java.util.Iterator;
 import java.util.List;
 import javax.swing.event.ChangeListener;
 import org.netbeans.api.java.queries.JavadocForBinaryQuery;
+import org.netbeans.api.java.queries.JavadocForBinaryQuery.Result;
+import org.netbeans.api.project.FileOwnerQuery;
+import org.netbeans.api.project.Project;
+import org.netbeans.modules.apisupport.project.NbModuleProject;
 import org.netbeans.modules.apisupport.project.Util;
 import org.netbeans.modules.apisupport.project.universe.NbPlatform;
 import org.netbeans.spi.java.queries.JavadocForBinaryQueryImplementation;
@@ -59,82 +64,122 @@ import org.openide.filesystems.URLMapper;
 import org.openide.modules.InstalledFileLocator;
 
 /**
- * Able to find javadoc in the appropriate NbPlatform for the given URL.
+ * Able to find Javadoc in the appropriate NbPlatform for the given URL.
  *
  * @author Jesse Glick, Martin Krauskopf
  */
 public final class GlobalJavadocForBinaryImpl implements JavadocForBinaryQueryImplementation {
     
-    public JavadocForBinaryQuery.Result findJavadoc(URL binaryRoot) {
+    public JavadocForBinaryQuery.Result findJavadoc(final URL root) {
         try {
-            if (!binaryRoot.getProtocol().equals("jar")) { // NOI18N
-                // XXX probably shouldn't just return null in this case
-                Util.err.log(binaryRoot + " is not an archive file."); // NOI18N
-                return null;
+            if (root.getProtocol().equals("jar")) { // NOI18N
+                return findForBinaryRoot(root);
+            } else {
+                return findForSourceRoot(root);
             }
-            URL jar = FileUtil.getArchiveFile(binaryRoot);
-            if (!jar.getProtocol().equals("file")) { // NOI18N
-                Util.err.log(binaryRoot + " is not an archive file."); // NOI18N
-                return null;
-            }
-            if (jar.toExternalForm().endsWith("/xtest/lib/junit.jar")) { // NOI18N
-                // #68685 hack - associate reasonable Javadoc with XTest's version of junit
-                File f = InstalledFileLocator.getDefault().locate("modules/ext/junit-3.8.2.jar", "org.netbeans.modules.junit", false); // NOI18N
-                if (f == null) {
-                    // For compat with NB 5.0.
-                    f = InstalledFileLocator.getDefault().locate("modules/ext/junit-3.8.1.jar", "org.netbeans.modules.junit", false); // NOI18N
-                }
-                if (f != null) {
-                    return JavadocForBinaryQuery.findJavadoc(FileUtil.getArchiveRoot(f.toURI().toURL()));
-                }
-            }
-            File binaryRootF = new File(URI.create(jar.toExternalForm()));
-            NbPlatform supposedPlaf = null;
-            for (Iterator it = NbPlatform.getPlatforms().iterator(); it.hasNext(); ) {
-                NbPlatform plaf = (NbPlatform) it.next();
-                if (binaryRootF.getAbsolutePath().startsWith(plaf.getDestDir().getAbsolutePath())) {
-                    supposedPlaf = plaf;
-                    break;
-                }
-            }
-            if (supposedPlaf == null) {
-                Util.err.log(binaryRootF + " does not correspond to a known platform"); // NOI18N
-                return null;
-            }
-            // XXX this will only work for modules following regular naming conventions:
-            String n = binaryRootF.getName();
-            if (!n.endsWith(".jar")) { // NOI18N
-                Util.err.log(binaryRootF + " is not a *.jar"); // NOI18N
-                return null;
-            }
-            String cnbdashes = n.substring(0, n.length() - 4);
-            final List<URL> candidates = new ArrayList<URL>();
-            URL[] roots = supposedPlaf.getJavadocRoots();
-            Util.err.log("Platform in " + supposedPlaf.getDestDir() + " claimed to have Javadoc roots " + Arrays.asList(roots));
-            for (int i = 0; i < roots.length; i++) {
-                // 1. user may insert directly e.g ...nbbuild/build/javadoc/org-openide-actions[.zip]
-                candidates.add(roots[i]);
-                // 2. or whole bunch of javadocs e.g. ...nbbuild/build/javadoc/
-                candidates.add(new URL(roots[i], cnbdashes + '/'));
-            }
-            Iterator it = candidates.iterator();
-            while (it.hasNext()) {
-                URL u = (URL) it.next();
-                if (URLMapper.findFileObject(u) == null) {
-                    Util.err.log("No such Javadoc candidate URL " + u);
-                    it.remove();
-                }
-            }
-            return new JavadocForBinaryQuery.Result() {
-                public URL[] getRoots() {
-                    return candidates.toArray(new URL[candidates.size()]);
-                }
-                public void addChangeListener(ChangeListener l) {}
-                public void removeChangeListener(ChangeListener l) {}
-            };
         } catch (MalformedURLException e) {
             throw new AssertionError(e);
         }
     }
-    
+
+    private Result findForBinaryRoot(final URL binaryRoot) throws MalformedURLException, MalformedURLException {
+        URL jar = FileUtil.getArchiveFile(binaryRoot);
+        if (!jar.getProtocol().equals("file")) { // NOI18N
+            Util.err.log(binaryRoot + " is not an archive file."); // NOI18N
+            return null;
+        }
+        if (jar.toExternalForm().endsWith("/xtest/lib/junit.jar")) { // NOI18N
+            // #68685 hack - associate reasonable Javadoc with XTest's version of junit
+            File f = InstalledFileLocator.getDefault().locate("modules/ext/junit-3.8.2.jar", "org.netbeans.modules.junit", false); // NOI18N
+            if (f == null) {
+                // For compat with NB 5.0.
+                f = InstalledFileLocator.getDefault().locate("modules/ext/junit-3.8.1.jar", "org.netbeans.modules.junit", false); // NOI18N
+            }
+            if (f != null) {
+                return JavadocForBinaryQuery.findJavadoc(FileUtil.getArchiveRoot(f.toURI().toURL()));
+            }
+        }
+        File binaryRootF = new File(URI.create(jar.toExternalForm()));
+        // XXX this will only work for modules following regular naming conventions:
+        String n = binaryRootF.getName();
+        if (!n.endsWith(".jar")) { // NOI18N
+            Util.err.log(binaryRootF + " is not a *.jar"); // NOI18N
+            return null;
+        }
+        String cnbdashes = n.substring(0, n.length() - 4);
+        NbPlatform supposedPlaf = null;
+        for (NbPlatform plaf : NbPlatform.getPlatforms()) {
+            if (binaryRootF.getAbsolutePath().startsWith(plaf.getDestDir().getAbsolutePath())) {
+                supposedPlaf = plaf;
+                break;
+            }
+        }
+        if (supposedPlaf == null) {
+            Util.err.log(binaryRootF + " does not correspond to a known platform"); // NOI18N
+            return null;
+        }
+        return findByDashedCNB(cnbdashes, supposedPlaf);
+    }
+
+    /**
+     * Go through all registered platforms and tries to find Javadoc for the
+     * given URL.
+     * <p>
+     * <em>TODO</em>: ideally should check module, or at least cluster, version.
+     */
+    private Result findForSourceRoot(final URL root) throws MalformedURLException {
+        Project p ;
+        try {
+            p = FileOwnerQuery.getOwner(root.toURI());
+        } catch (URISyntaxException e) {
+            throw new AssertionError(e);
+        }
+        if (p != null) {
+            NbModuleProject module = p.getLookup().lookup(NbModuleProject.class);
+            if (module != null) {
+                String cnb = module.getCodeNameBase();
+                for (NbPlatform plaf : NbPlatform.getPlatforms()) {
+                    Result r = findByDashedCNB(cnb.replace('.', '-'), plaf);
+                    if (r != null) {
+                        return r;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+   
+    private Result findByDashedCNB(final String cnbdashes, final NbPlatform plaf) throws MalformedURLException {
+        final List<URL> candidates = new ArrayList<URL>();
+        URL[] roots = plaf.getJavadocRoots();
+        Util.err.log("Platform in " + plaf.getDestDir() + " claimed to have Javadoc roots " + Arrays.asList(roots));
+        for (URL root : roots) {
+            // XXX: so should be checked, instead of always adding both?
+            // 1. user may insert directly e.g ...nbbuild/build/javadoc/org-openide-actions[.zip]
+            candidates.add(root);
+            // 2. or whole bunch of javadocs e.g. ...nbbuild/build/javadoc/
+            candidates.add(new URL(root, cnbdashes + '/'));
+        }
+        Iterator<URL> it = candidates.iterator();
+        while (it.hasNext()) {
+            URL u = it.next();
+            if (URLMapper.findFileObject(u) == null) {
+                Util.err.log("No such Javadoc candidate URL " + u);
+                it.remove();
+            }
+        }
+        if (candidates.isEmpty()) {
+            return null;
+        }
+        return new JavadocForBinaryQuery.Result() {
+            public URL[] getRoots() {
+                return candidates.toArray(new URL[candidates.size()]);
+            }
+            public void addChangeListener(ChangeListener l) {
+            }
+            public void removeChangeListener(ChangeListener l) {
+            }
+        };
+    }
+
 }
