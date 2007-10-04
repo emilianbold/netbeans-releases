7ec0cab1aadb1c2e7fe49f513d59a4b9b59079b3
tspiva <tspiva@netbeans.org>
2007-10-04 18:45:18 +0000

    Fixed issue 90355 - RE loses array parameter from derivation classifier We where not handling template instances with an array type.

--- uml/src/org/netbeans/modules/uml/core/reverseengineering/parsingfacilities/translation/statehandlers/TemplateInstantiationStateHandler.java
+++ uml/src/org/netbeans/modules/uml/core/reverseengineering/parsingfacilities/translation/statehandlers/TemplateInstantiationStateHandler.java
@@ -72,6 +72,14 @@ public class TemplateInstantiationStateHandler extends StateHandler
     
     private Stack < TemplateInstantiationStateHandler > childInstances = 
             new Stack < TemplateInstantiationStateHandler >();
+    
+    // Array Processing properties.  For a template all we care about
+    // is the array type and the number of dimensions.
+    
+//    private boolean processingArray = false;
+//    private Identifier arrayType = null;
+//    private int dimensions = 0;
+    MultiplicityIdentifier multiplicityIdentifier = null;
 
     public TemplateInstantiationStateHandler()
     {
@@ -208,6 +216,18 @@ public class TemplateInstantiationStateHandler extends StateHandler
            retVal.setDOMNode(getDOMNode());
            childInstances.push((TemplateInstantiationStateHandler)retVal);
         }
+        else if("Array Declarator".equals(stateName) == true)
+        {
+            if(multiplicityIdentifier == null)
+            {
+                multiplicityIdentifier = new MultiplicityIdentifier();
+            }
+            
+            // Array Declarators are recursive.  Therefore, if one already 
+            // exist, it means that we are simply defining a new dimension.
+            multiplicityIdentifier.addDimension();
+            retVal = this;
+        }
         else
         {
             retVal = super.createSubStateHandler(stateName, language);
@@ -265,11 +285,21 @@ public class TemplateInstantiationStateHandler extends StateHandler
         {
            if(mNameState == true)
            {
-              if(m_NameIdentifier == null)
-              {
-                 m_NameIdentifier = new Identifier();  
-              }
+               if(m_NameIdentifier == null)
+               {
+                  m_NameIdentifier = new Identifier();  
+               }
                m_NameIdentifier.addToken(pToken);
+           }
+           else if(multiplicityIdentifier != null)
+           {
+//               if(arrayType == null)
+//               {
+//                  arrayType = new Identifier();  
+//               }
+//               arrayType.addToken(pToken);
+               multiplicityIdentifier.addIdentifierToken(pToken);
+               
            }
            else
            {
@@ -283,6 +313,24 @@ public class TemplateInstantiationStateHandler extends StateHandler
                    createTokenDescriptor("IsPrimitive", -1, -1, -1, "true", -1);
                }
            }
+        }
+        else if("Array Start".equals(tokenType) == true)
+        {
+            multiplicityIdentifier.setStartDimensionToken(pToken);
+        }
+        else if("Array End".equals(tokenType) == true)
+        {
+            // Since the array declarator states are recursive the first
+            // array declarator will set multiplicityIdentifier to null.
+            // This is not a big deal since we have already got all the 
+            // information that we need.  
+            //
+            // Since the declarations are depth first, we have already found
+            // the last array end token.
+            if(multiplicityIdentifier != null)
+            {
+                multiplicityIdentifier.setEndDimensionToken(pToken);
+            }
         }
     }
 
@@ -320,7 +368,16 @@ public class TemplateInstantiationStateHandler extends StateHandler
                m_TypeIdentifiers.add(new TokenIdentifier(mCurrentTypeIdentifier));
            }
        }
-       
+       else if("Array Declarator".equals(stateName) == true)
+       {
+           if(multiplicityIdentifier != null)
+           {
+               m_TypeIdentifiers.add(multiplicityIdentifier);
+
+               multiplicityIdentifier = null;
+           }
+           
+       }
        super.stateComplete(stateName);
     }
 
@@ -345,6 +402,11 @@ public class TemplateInstantiationStateHandler extends StateHandler
     protected class TemplateIdentifier
     {
         private String value = "";
+        
+        public TemplateIdentifier()
+        {
+            
+        }
         
         public TemplateIdentifier(String data)
         {
@@ -406,5 +468,71 @@ public class TemplateInstantiationStateHandler extends StateHandler
         {
             return value.getLength();
         }
+    }
+    
+    protected class MultiplicityIdentifier extends TemplateIdentifier
+    {
+        private int dimensions = 0;
+        private ITokenDescriptor startDimToken = null;
+        private ITokenDescriptor endDimToken = null;
+        private Identifier type = new Identifier();
+        
+        public MultiplicityIdentifier()
+        {
+            super();
+        }
+        
+        public void addIdentifierToken(ITokenDescriptor token)
+        {
+            type.addToken(token);
+        }
+        
+        public String getValue()
+        {
+            String value = type.getIdentifierAsSource();
+            for(int dim = 0; dim < dimensions; dim ++)
+            {
+                value += startDimToken.getValue() + endDimToken.getValue();
+            }
+            
+            return value;
+        }
+        
+        public void addDimension()
+        {
+            dimensions++;
+        }
+        
+        public long getStartLine()
+        {
+            return type.getStartLine();
+        }
+        
+        public long getStartColumn()
+        {
+            return type.getStartColumn();
+        }
+        
+        public long getStartPosition()
+        {
+            return type.getStartPosition();
+        }
+        
+        public long getLength()
+        {
+            return endDimToken.getPosition() - getStartPosition();
+        }
+
+        public void setEndDimensionToken(ITokenDescriptor endDimToken)
+        {
+            this.endDimToken = endDimToken;
+        }
+
+        public void setStartDimensionToken(ITokenDescriptor startDimToken)
+        {
+            this.startDimToken = startDimToken;
+        }
+        
+        
     }
 }
