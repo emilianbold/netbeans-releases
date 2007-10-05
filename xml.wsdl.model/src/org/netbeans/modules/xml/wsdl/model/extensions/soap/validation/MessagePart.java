780e22589d2f7e878db6e8a3ea553e8b8f061eaf
yaroslavskiy <yaroslavskiy@netbeans.org>
2007-10-05 13:19:40 +0000

    Fix license header

--- xml.wsdl.model/src/org/netbeans/modules/xml/wsdl/model/extensions/soap/validation/MessagePart.java
+++ xml.wsdl.model/src/org/netbeans/modules/xml/wsdl/model/extensions/soap/validation/MessagePart.java
@@ -1,33 +1,3 @@
-/*
- * BEGIN_HEADER - DO NOT EDIT
- *
- * The contents of this file are subject to the terms
- * of the Common Development and Distribution License
- * (the "License").  You may not use this file except
- * in compliance with the License.
- *
- * You can obtain a copy of the license at
- * https://open-jbi-components.dev.java.net/public/CDDLv1.0.html.
- * See the License for the specific language governing
- * permissions and limitations under the License.
- *
- * When distributing Covered Code, include this CDDL
- * HEADER in each file and include the License file at
- * https://open-jbi-components.dev.java.net/public/CDDLv1.0.html.
- * If applicable add the following below this CDDL HEADER,
- * with the fields enclosed by brackets "[]" replaced with
- * your own identifying information: Portions Copyright
- * [year] [name of copyright owner]
- */
-
-/*
- * @(#)HttpSoapHeaderFaultValidator.java
- *
- * Copyright 2004-2007 Sun Microsystems, Inc. All Rights Reserved.
- *
- * END_HEADER - DO NOT EDIT
- */
-
 package org.netbeans.modules.xml.wsdl.model.extensions.soap.validation;
 
 import org.netbeans.modules.xml.wsdl.model.Message;
