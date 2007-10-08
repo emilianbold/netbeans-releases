5ea49ca35a37cb92709b46662c9b6d43eb82cda4
deva <deva@netbeans.org>
2007-10-08 18:26:34 +0000

    Fix for #115278: I18N - ide freezes after creating the second VWApp using win-31j encoding and ja_JP locale Removing the user action task which was used to model the newly created visual web project. Although it was used to improve performance, it is found that having task is more deadlock prone. Recent observation of performance of creating new project with a fresh user directory with and without task seems to be almost same.

--- visualweb.insync/src/org/netbeans/modules/visualweb/insync/models/FacesModelSet.java
+++ visualweb.insync/src/org/netbeans/modules/visualweb/insync/models/FacesModelSet.java
@@ -402,19 +402,7 @@ public class FacesModelSet extends ModelSet implements FacesDesignProject {
         facesConfigModel = new FacesConfigModel(this);
         setConfigModel(facesConfigModel);
 
-        FileObject anyJavaFile = getAnyJavaFile();
-        if (anyJavaFile == null || !SourceUtils.isScanInProgress()) {
-            ReadTaskWrapper.execute(new ReadTaskWrapper.Read() {
-                public Object run(CompilationInfo cinfo) {
-                    doModeling();
-                    return null;
-                }
-            }, anyJavaFile);
-        }else {
-            // Can't use wrapper task to ensure background scaning
-            // stopped while modeling.
-            doModeling();
-        }
+        doModeling();
         
         projectBuiltQueryStatus = ProjectBuiltQuery.getStatus(project);
         projectBuiltQueryStatusChangeListener  = new ChangeListener() {
@@ -483,22 +471,6 @@ public class FacesModelSet extends ModelSet implements FacesDesignProject {
     }
 
     //------------------------------------------------------------------------------------ Accessors
-
-
-    private FileObject getAnyJavaFile() {
-        FileObject sourceRootFileObject = JsfProjectUtils.getSourceRoot(project);
-        Enumeration<? extends FileObject> sourceFileObjects = sourceRootFileObject.getChildren(true);
-        FileObject anyJavaFile = null;
-        while (sourceFileObjects.hasMoreElements()) {
-            FileObject aSourceFileObject = sourceFileObjects.nextElement();
-            if (aSourceFileObject.getMIMEType().equals("text/x-java")) {
-                // NOI18N
-                anyJavaFile = aSourceFileObject;
-                break;
-            }
-        }
-        return anyJavaFile;
-    }
 
     /**
      * @return
