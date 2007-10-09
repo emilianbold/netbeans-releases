632797829e99ec634ac88768c7db04d0ec441667
manowar <manowar@netbeans.org>
2007-10-09 13:50:26 +0000

    MIDPNB - issue #79635 has been fixed

--- vmd.midpnb/src/org/netbeans/modules/vmd/midpnb/propertyeditors/table/PropertyEditorTableModel.java
+++ vmd.midpnb/src/org/netbeans/modules/vmd/midpnb/propertyeditors/table/PropertyEditorTableModel.java
@@ -37,7 +37,9 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
- */package org.netbeans.modules.vmd.midpnb.propertyeditors.table;
+ */
+
+package org.netbeans.modules.vmd.midpnb.propertyeditors.table;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
@@ -69,12 +71,12 @@ public class PropertyEditorTableModel extends DesignPropertyEditor implements Pr
     private PropertyEditorTableModel() {
         initComponents();
     }
-    
+
     public static PropertyEditorTableModel createInstance() {
         return new PropertyEditorTableModel();
     }
-    
-     private void initComponents() {
+
+    private void initComponents() {
         customEditor = new TableModelEditorElement();
         customEditor.addPropertyEditorResourceElementListener(this);
         customEditorPanel = new JPanel(new BorderLayout());
@@ -88,14 +90,14 @@ public class PropertyEditorTableModel extends DesignPropertyEditor implements Pr
             this.component = new WeakReference<DesignComponent>(component);
         }
     }
-        
+
     @Override
     public Component getCustomEditor() {
         //if (customEditorPanel.isShowing()) {
-            if (component != null && component.get() != null) {
-                customEditor.setDesignComponentWrapper(new DesignComponentWrapper(component.get()));
-            }
-            customEditor.setAllEnabled(true);
+        if (component != null && component.get() != null) {
+            customEditor.setDesignComponentWrapper(new DesignComponentWrapper(component.get()));
+        }
+        customEditor.setAllEnabled(true);
         //}
         return customEditorPanel;
     }
@@ -104,7 +106,7 @@ public class PropertyEditorTableModel extends DesignPropertyEditor implements Pr
     public Boolean canEditAsText() {
         return Boolean.FALSE;
     }
-    
+
     @Override
     public String getAsText() {
         return NbBundle.getMessage(PropertyEditorTableModel.class, "DISP_PE_TableModel_GetAsText"); //NOI18N
@@ -115,7 +117,7 @@ public class PropertyEditorTableModel extends DesignPropertyEditor implements Pr
         if (component == null || component.get() == null) {
             return false;
         }
-        
+
         DesignComponent _component = component.get();
         if (headers != null) {
             _component.writeProperty(SimpleTableModelCD.PROP_COLUMN_NAMES, headers);
@@ -125,17 +127,17 @@ public class PropertyEditorTableModel extends DesignPropertyEditor implements Pr
         }
         return false;
     }
-    
+
     @Override
     public boolean isExecuteInsideWriteTransactionUsed() {
         return true;
     }
-    
+
     @Override
     public boolean supportsDefaultValue() {
-        return false;
+        return true;
     }
-    
+
     public void elementChanged(PropertyEditorResourceElementEvent event) {
         PropertyValue propertyValue = event.getPropertyValue();
         String propertyName = event.getPropertyName();
@@ -145,6 +147,40 @@ public class PropertyEditorTableModel extends DesignPropertyEditor implements Pr
             values = propertyValue;
         } else {
             throw Debug.illegalArgument("Illegal property value has been passed"); // NOI18N
+        }
+    }
+
+    @Override
+    public boolean isDefaultValue() {
+        if (component == null || component.get() == null) {
+            return true;
+        }
+
+        final boolean[] isDefaultValue = new boolean[]{true};
+        component.get().getDocument().getTransactionManager().readAccess(new Runnable() {
+
+            public void run() {
+                DesignComponent _component = component.get();
+                boolean names = PropertyValue.createNull().equals(_component.readProperty(SimpleTableModelCD.PROP_COLUMN_NAMES));
+                boolean values = PropertyValue.createNull().equals(_component.readProperty(SimpleTableModelCD.PROP_VALUES));
+                isDefaultValue[0] = names && values;
+            }
+        });
+
+        return isDefaultValue[0];
+    }
+
+    @Override
+    public boolean isResetToDefaultAutomatically() {
+        return false;
+    }
+
+    @Override
+    public void customEditorResetToDefaultButtonPressed() {
+        if (component != null && component.get() != null) {
+            DesignComponent _component = component.get();
+            _component.writeProperty(SimpleTableModelCD.PROP_COLUMN_NAMES, PropertyValue.createNull());
+            _component.writeProperty(SimpleTableModelCD.PROP_VALUES, PropertyValue.createNull());
         }
     }
 }
