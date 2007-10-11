22f03d3e7f4e3ef38086f4005f5991499e847cad
lativ <lativ@netbeans.org>
2007-10-11 15:28:29 +0000

    fix 118111 - REGRESSION: some context menu commands are disabled after switching to Source View

--- bpel.core/src/org/netbeans/modules/bpel/core/multiview/BPELSourceMultiViewElement.java
+++ bpel.core/src/org/netbeans/modules/bpel/core/multiview/BPELSourceMultiViewElement.java
@@ -484,7 +484,8 @@ public class BPELSourceMultiViewElement extends CloneableEditor
                         ? null 
                         : myMultiViewObserver.getTopComponent();
                 if (tc != null) {
-                    tc.setActivatedNodes(new Node[] {node});
+                    tc.setActivatedNodes(new Node[] {node, getDataObject().getNodeDelegate()});
+                    setActivatedNodes(new Node[] {node, getDataObject().getNodeDelegate()});
                 }
             }
         });
