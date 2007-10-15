267547a486c9b1234bf9f096ab337da0038ea265
sergius <sergius@netbeans.org>
2007-10-15 06:44:05 +0000

    script update

--- cnd.modelimpl/test/scripts/repository.sh
+++ cnd.modelimpl/test/scripts/repository.sh
@@ -39,36 +39,68 @@
 # Version 2 license, then the option applies only if the new code is
 # made subject to such option by the copyright holder.
 
-TEST_PROJECT="$1"
+USETS=
 LOGPATH=${LOGPATH-/tmp}
+TEST_PROJECT=
+DOTIME=time
+
+while [ -n "$1" ]
+    do case "$1" in
+        -ignorets)
+		USETS=".1"
+		;;
+        -log)
+		shift
+		LOGPATH="$1"		
+		;;
+	-notime)
+		DOTIME=
+		;;
+	*)
+		TEST_PROJECT="${TEST_PROJECT} $1"
+		;;
+    esac
+    shift
+done
+
+test ! -d ${LOGPATH} && mkdir -p ${LOGPATH}
+
 PROJECT_TEMP_FILE=${LOGPATH}/repositorytest
+REPOSITORY_CACHE_PATH=${LOGPATH}/repositorytest-cache/
 echo Testing on project: ${TEST_PROJECT}
 echo Logs stored to: ${PROJECT_TEMP_FILE}
 echo "Running without repository..."
-./_parse_project.sh ${TEST_PROJECT} -fq  -J-Dcnd.repository.hardrefs=true  > ${PROJECT_TEMP_FILE}.original.txt 2>&1
+echo "./_parse_project.sh ${TEST_PROJECT} -fq  -J-Dcnd.repository.hardrefs=true  1> ${PROJECT_TEMP_FILE}.original.out 2>${PROJECT_TEMP_FILE}.original.err" > ${LOGPATH}/run.sh; chmod a+x ${LOGPATH}/run.sh
+${DOTIME} ${LOGPATH}/run.sh
+echo
 echo "Running with repository..."
-./_parse_project.sh ${TEST_PROJECT} -fq --cleanrepository> ${PROJECT_TEMP_FILE}.repository.txt 2>&1
+echo "./_parse_project.sh ${TEST_PROJECT} -fq --cl${PROJECT_TEMP_FILE}.original.erreanrepository -J-Dcnd.repository.cache.path=${REPOSITORY_CACHE_PATH} 1> ${PROJECT_TEMP_FILE}.repository.out 2>${PROJECT_TEMP_FILE}.repository.err" > ${LOGPATH}/run.sh; chmod a+x ${LOGPATH}/run.sh
+${DOTIME} ${LOGPATH}/run.sh
 
-diff ${PROJECT_TEMP_FILE}.original.txt ${PROJECT_TEMP_FILE}.repository.txt > ${PROJECT_TEMP_FILE}.diff.txt
-echo To see detailed results do
-echo cat ${PROJECT_TEMP_FILE}.diff.txt
+if [ -n "${USETS}" ]; then
+#rough dated logs filter, only for stderr
+DAYEAR=`date +%Y`
+cat ${PROJECT_TEMP_FILE}.original.err | grep -v ${DAYEAR} > ${PROJECT_TEMP_FILE}.original.err${USETS}
+cat ${PROJECT_TEMP_FILE}.repository.err | grep -v ${DAYEAR} > ${PROJECT_TEMP_FILE}.repository.err${USETS}
+fi
+
+diff ${PROJECT_TEMP_FILE}.original.out ${PROJECT_TEMP_FILE}.repository.out > ${PROJECT_TEMP_FILE}.diff.out
+diff ${PROJECT_TEMP_FILE}.original.err${USETS} ${PROJECT_TEMP_FILE}.repository.err${USETS} > ${PROJECT_TEMP_FILE}.diff.err
+echo 
 
 echo "Lines in diff file:"
-cat  ${PROJECT_TEMP_FILE}.diff.txt | wc -l
+cat ${PROJECT_TEMP_FILE}.diff.out ${PROJECT_TEMP_FILE}.diff.err  | wc -l
 
-if [ -s ${PROJECT_TEMP_FILE}.diff.txt ]; then
+if [ -s ${PROJECT_TEMP_FILE}.diff.out -o -s ${PROJECT_TEMP_FILE}.diff.err ]; then
     reptestresult="failed"
+    echo To see detailed results do
+    echo "echo \"*** OUT ***\"; cat ${PROJECT_TEMP_FILE}.diff.out; echo \"*** ERR ***\"; cat ${PROJECT_TEMP_FILE}.diff.err"
+    echo
 else
     reptestresult="passed"
 fi
-echo Repository correctness test: ${reptestresult}
 
-if [ -n "${XMLOUTPUT}" ]; then
-    echo "<log name='Model without repository'>repositorytest.original.txt</log>" >> ${XMLOUTPUT}
-    echo "<log name='Model with repository'>repositorytest.repository.txt</log>" >> ${XMLOUTPUT}
-    echo "<log name='Models diff'>repositorytest.diff.txt</log>" >> ${XMLOUTPUT}
-    echo "<result>${reptestresult}</result>" >> ${XMLOUTPUT}
-fi
+echo Repository correctness test: ${reptestresult}
 
 
 
