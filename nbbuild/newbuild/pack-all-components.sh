d42a5b891c2a3afcbdea79cff3b62f21b2817afc
mzlamal <mzlamal@netbeans.org>
2007-10-16 11:30:13 +0000

    Fix typo

--- nbbuild/newbuild/pack-all-components.sh
+++ nbbuild/newbuild/pack-all-components.sh
@@ -97,7 +97,7 @@ pack_all_components()
 
 pack_all_components $DIST $BASENAME
 
-if [ $ML_BUILD == 1]; then
+if [ $ML_BUILD == 1 ]; then
     rm -rf $NB_ALL/nbbuild/netbeans
     mv $NB_ALL/nbbuild/netbeans-ml $NB_ALL/nbbuild/netbeans
 
