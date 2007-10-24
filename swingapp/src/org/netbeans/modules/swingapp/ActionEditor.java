5e435589778146568031b06a65ed6b4822f838d2
jstola <jstola@netbeans.org>
2007-10-24 10:16:16 +0000

    119786: Making ActionEditor work with null value.

--- swingapp/src/org/netbeans/modules/swingapp/ActionEditor.java
+++ swingapp/src/org/netbeans/modules/swingapp/ActionEditor.java
@@ -368,27 +368,33 @@ public class ActionEditor extends PropertyEditorSupport implements FormAwareEdit
     public void readFromXML(Node element) throws IOException {
         if(element != null) {
             Element elem = (Element)element;
-            FileObject srcFile = getSourceFile();
-            String className = elem.getAttribute("class"); // NOI18N
-            String id = elem.getAttribute("id"); // NOI18N
-            String methodName = elem.getAttribute("methodName");
-            if(methodName == null) methodName = id;
-            ProxyAction action = new ProxyAction(className, id, methodName);
-            boolean appWide = !className.endsWith(srcFile.getName())
-                    || (className.length() != srcFile.getName().length()
-                    && className.charAt(className.length() - srcFile.getName().length() - 1) != '.');
-            action.setAppWide(appWide);
-            action.setResourceMap(ResourceUtils.getDesignResourceMap(srcFile, true));
-            action.loadFromResourceMap();
-            setValue(action);
+            if (elem.hasAttribute("id")) { // NOI18N
+                FileObject srcFile = getSourceFile();
+                String className = elem.getAttribute("class"); // NOI18N
+                String id = elem.getAttribute("id"); // NOI18N
+                String methodName = elem.getAttribute("methodName"); // NOI18N
+                if(methodName == null) methodName = id;
+                ProxyAction action = new ProxyAction(className, id, methodName);
+                boolean appWide = !className.endsWith(srcFile.getName())
+                        || (className.length() != srcFile.getName().length()
+                        && className.charAt(className.length() - srcFile.getName().length() - 1) != '.');
+                action.setAppWide(appWide);
+                action.setResourceMap(ResourceUtils.getDesignResourceMap(srcFile, true));
+                action.loadFromResourceMap();
+                setValue(action);
+            } else {
+                setValue(null);
+            }
         }
     }
 
     public Node storeToXML(Document doc) {
         Element elem = doc.createElement("action"); // NOI18N
-        elem.setAttribute("id",action.getId()); // NOI18N
-        elem.setAttribute("class",action.getClassname()); // NOI18N
-        elem.setAttribute("methodName",action.getMethodName());
+        if (action != null) {
+            elem.setAttribute("id",action.getId()); // NOI18N
+            elem.setAttribute("class",action.getClassname()); // NOI18N
+            elem.setAttribute("methodName",action.getMethodName()); // NOI18N
+        }
         return elem;
     }
     
