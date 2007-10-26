82300905fadc7adb02c6db0da6c1ef01090ff4e3
mkrauskopf <mkrauskopf@netbeans.org>
2007-10-26 10:17:41 +0000

    ruby-debug-ide-0.1.9 was released

--- ruby.debugger/src/org/netbeans/modules/ruby/debugger/Util.java
+++ ruby.debugger/src/org/netbeans/modules/ruby/debugger/Util.java
@@ -66,7 +66,7 @@ public final class Util {
     static final String RUBY_DEBUG_IDE_NAME = "ruby-debug-ide";
     
     /** Required version of ruby-debug-ide gem. */
-    static final String RDEBUG_IDE_VERSION = "0.1.8"; // NOI18N
+    static final String RDEBUG_IDE_VERSION = "0.1.9"; // NOI18N
     
     private static final String RUBY_MIME_TYPE = "text/x-ruby"; // NOI18N
     private static final String ERB_MIME_TYPE = "application/x-httpd-eruby"; // NOI18N
