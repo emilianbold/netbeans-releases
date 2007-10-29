296326af5c0c936905eb67362bc439d1789fcb94
ovk <ovk@netbeans.org>
2007-10-29 16:20:17 +0000

    small check

--- performance/test/qa-functional/src/gui/menu/MainMenu.java
+++ performance/test/qa-functional/src/gui/menu/MainMenu.java
@@ -105,7 +105,7 @@ public class MainMenu extends org.netbeans.performance.test.utilities.Performanc
     }
     
     public void testFileMenu(){
-        WAIT_AFTER_PREPARE = 5000;
+        WAIT_AFTER_PREPARE = 15000;
         testMenu("org.netbeans.core.Bundle","Menu/File");
     }
     
