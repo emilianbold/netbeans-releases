108338daab10ade916ccc2a7a89e7244ef59eb89
vv159170 <vv159170@netbeans.org>
2007-11-07 09:35:19 +0000

    problem test case for IZ#121347: APT is broken on some russian symbols inside string tokens

---
+++ cnd.modelimpl/test/unit/data/org/netbeans/modules/cnd/modelimpl/trace/FileModelTest/0x16.cc
@@ -0,0 +1,8 @@
+int main(int argc, char**argv) {
+   // № 1   in russian is the problem symbol inside string
+   cout <<"Точка № 1  :\n";
+   cout << "Параметр1";
+   // Prints welcome message...
+   cout <<"фывфыв...\n";
+   return 0;
+} 
