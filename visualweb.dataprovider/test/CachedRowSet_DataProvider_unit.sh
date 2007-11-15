49601f51f0067d0748ccad61ecd88c0e52e2315f
jbaker <jbaker@netbeans.org>
2007-11-15 20:14:00 +0000

    Modify CachedRowSetDataProvider test to use database properties file Add new database properties files

--- visualweb.dataprovider/test/CachedRowSet_DataProvider_unit.sh
+++ visualweb.dataprovider/test/CachedRowSet_DataProvider_unit.sh
@@ -7,7 +7,8 @@ export BASE=g:/home/tr61
 echo $BASE
 
 # uncomment when running CachedRowSetTest also - after including test in cfg-unit.xml 
-ant -Dnetbeans.dest.dir="$BASE/nbbuild/netbeans" -Dxtest.file_appserver.properties=$BASE/visualweb/test/data/DefaultDeploymentTargets.properties -Dxtest.file_database.properties=$BASE/visualweb/dataprovider/runtime/test/unit/db_derby.properties -Dxtest.testtype="unit" -Dxtest.testattribs="stable" -Dnetbeans.javacore.noscan=true
+#ant -Dnetbeans.dest.dir="$BASE/nbbuild/netbeans" -Dxtest.file_appserver.properties=$BASE/visualweb/test/data/DefaultDeploymentTargets.properties -Dxtest.file_database.properties=$BASE/visualweb/dataprovider/runtime/test/unit/db_derby.properties -Dxtest.testtype="unit" -Dxtest.testattribs="stable" -Dnetbeans.javacore.noscan=true
+ant -Dnetbeans.dest.dir="$BASE/nbbuild/netbeans"  -Dxtest.testtype="unit" -Dxtest.testattribs="stable" -Dnetbeans.javacore.noscan=true
 
 #ant -Dnetbeans.dest.dir="$BASE/nbbuild/netbeans" -Dxtest.testtype="unit" -Dxtest.testattribs="stable" -Dnetbeans.javacore.noscan=true
 
