2398d01b48ebd989373b922835276ba408d57d6e
ovk <ovk@netbeans.org>
2007-11-15 16:14:00 +0000

    time fixed

--- performance/test/qa-functional/src/gui/window/NewFileDialog.java
+++ performance/test/qa-functional/src/gui/window/NewFileDialog.java
@@ -56,13 +56,13 @@ public class NewFileDialog extends org.netbeans.performance.test.utilities.Perfo
     /** Creates a new instance of NewFileDialog */
     public NewFileDialog(String testName) {
         super(testName);
-        expectedTime = 1821; // 4.1 : 1614, N/A, 1821, 1752, 1182, 1297
+        expectedTime = 1000; // 4.1 : 1614, N/A, 1821, 1752, 1182, 1297
     }
     
     /** Creates a new instance of NewFileDialog */
     public NewFileDialog(String testName, String performanceDataName) {
         super(testName,performanceDataName);
-        expectedTime = 1821; // 4.1 : 1614, N/A, 1821, 1752, 1182, 1297
+        expectedTime = 1000; // 4.1 : 1614, N/A, 1821, 1752, 1182, 1297
     }
     
     public void prepare() {
