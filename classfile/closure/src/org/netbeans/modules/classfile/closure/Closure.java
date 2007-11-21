68117e24cf70cb77659c6c81dc8a55130a7ab78d
tball <tball@netbeans.org>
2007-11-21 23:11:45 +0000

    Added -classpath support to closure tool.

--- classfile/closure/src/org/netbeans/modules/classfile/closure/Closure.java
+++ classfile/closure/src/org/netbeans/modules/classfile/closure/Closure.java
@@ -49,6 +49,9 @@ package org.netbeans.modules.classfile.closure;
 
 import org.netbeans.modules.classfile.*;
 import java.io.*;
+import java.lang.reflect.*;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.*;
 
 /**
@@ -59,60 +62,63 @@ import java.util.*;
  * @author Thomas Ball
  */
 public class Closure {
-    String thisClass;
     Set<String> closure;
-
-    Closure(String spec) {
-        thisClass = spec;
+    ClassLoader loader;
+    
+    Closure(ClassLoader loader) {
+        this.loader = loader;
     }
 
-    void buildClosure(boolean includeJDK, boolean useFileNames)
+    void buildClosure(String[] classes, boolean includeJDK, boolean useFileNames)
       throws IOException {
         if (closure != null)
             return;
         closure = new HashSet<String>();
         Set<String> visited = new HashSet<String>();
         Stack<ClassName> stk = new Stack<ClassName>();
-        ClassName thisCN = ClassName.getClassName(thisClass.replace('.', '/'));
-        stk.push(thisCN);
-        visited.add(thisCN.getExternalName());
+        for (String cls : classes) {
+            ClassName thisCN = ClassName.getClassName(cls.replace('.', '/'));
+            stk.push(thisCN);
+            visited.add(thisCN.getExternalName());
 
-        while (!stk.empty()) {
-            // Add class to closure.
-            ClassName cn = stk.pop();
-            InputStream is = findClassStream(cn.getType());
-	    if (is == null) {
-		System.err.println("couldn't find class: " + 
-                                   cn.getExternalName());
-		continue;
-	    }
-            ClassFile cfile = new ClassFile(is);
-            if (useFileNames)
-                closure.add(cn.getType() + ".class");
-            else
-                closure.add(cfile.getName().getExternalName());
-            
-            ConstantPool pool = cfile.getConstantPool();
-            Iterator refs = pool.getAllClassNames().iterator();
-            while (refs.hasNext()) {
-                ClassName cnRef = (ClassName)refs.next();
-                String cname = cnRef.getExternalName();
-                if (cname.indexOf('[') != -1) {
-                    // skip arrays
-                } else if (!includeJDK && 
-                           (cname.startsWith("java.") || 
-                            cname.startsWith("javax.") ||
-                            cname.startsWith("sun.") ||
-                            cname.startsWith("com.sun.corba") ||
-                            cname.startsWith("com.sun.image") ||
-                            cname.startsWith("com.sun.java.swing") ||
-                            cname.startsWith("com.sun.naming") ||
-                            cname.startsWith("com.sun.security"))) {
-                    // if directed, skip JDK references
-                } else {
-                    boolean isNew = visited.add(cname);
-                    if (isNew)
-                        stk.push(cnRef);
+            while (!stk.empty()) {
+                // Add class to closure.
+                ClassName cn = stk.pop();
+                String classFile = cn.getType() + ".class";
+                InputStream is = loader.getResourceAsStream(classFile);
+                if (is == null) {
+                    System.err.println("couldn't find class: " + 
+                                       cn.getExternalName());
+                    continue;
+                }
+                ClassFile cfile = new ClassFile(is);
+                if (useFileNames)
+                    closure.add(classFile);
+                else
+                    closure.add(cfile.getName().getExternalName());
+
+                ConstantPool pool = cfile.getConstantPool();
+                Iterator refs = pool.getAllClassNames().iterator();
+                while (refs.hasNext()) {
+                    ClassName cnRef = (ClassName)refs.next();
+                    String cname = cnRef.getExternalName();
+                    if (cname.indexOf('[') != -1) {
+                        // skip arrays
+                    } else if (!includeJDK && 
+                               (cname.startsWith("java.") || 
+                                cname.startsWith("javax.") ||
+                                cname.startsWith("sun.") ||
+                                cname.startsWith("com.sun.corba") ||
+                                cname.startsWith("com.sun.image") ||
+                                cname.startsWith("com.sun.java.swing") ||
+                                cname.startsWith("com.sun.naming") ||
+                                cname.startsWith("com.sun.security"))) {
+                        // if directed, skip JDK references
+                    } else {
+                        boolean isNew = visited.add(cname);
+                        if (isNew)
+                            stk.push(cnRef);
+                    }
                 }
             }
         }
@@ -126,12 +132,6 @@ public class Closure {
 
     Iterator dependencies() {
         return closure.iterator();
-    }
-
-    private InputStream findClassStream(String className) {
-        InputStream is = 
-            ClassLoader.getSystemClassLoader().getResourceAsStream(className + ".class");
-        return is;
     }
 
     /**
@@ -150,23 +150,50 @@ public class Closure {
 
         boolean includeJDK = false;
         boolean useFilenames = false;
-        for (int i = 0; i < args.length; i++) {
+        String classpath = null;
+        int i = 0;
+        while (i < args.length) {
             if (args[i].equals("-includejdk"))
                 includeJDK = true;
-            if (args[i].equals("-filenames"))
+            else if (args[i].equals("-filenames"))
                 useFilenames = true;
+            else if (args[i].equals("-classpath") && i+1 < args.length)
+                classpath = args[++i];
             else if (args[i].charAt(0) == '-')
                 usage();
-            else {
-                try {
-                    Closure c = new Closure(args[i]);
-                    c.buildClosure(includeJDK, useFilenames);
-                    c.dumpClosure(System.out);
-                } catch (IOException e) {
-                    System.err.println("error accessing \"" + args[i] + 
-                                       "\": " + e.toString());
-                }
+            else 
+                break;
+            i++;
+        }
+        
+        if (i == args.length)
+            usage();
+        else {
+            try {
+                String[] classes = new String[args.length - i];
+                System.arraycopy(args, i, classes, 0, classes.length);
+                Closure c = new Closure(createLoader(classpath));
+                c.buildClosure(classes, includeJDK, useFilenames);
+                c.dumpClosure(System.out);
+            } catch (IOException e) {
+                System.err.println(e.toString());
             }
+        }
+    }
+    
+    private static ClassLoader createLoader(String classpath) throws IOException {
+        if (classpath == null)
+            return Closure.class.getClassLoader();
+        
+        try {
+            String[] paths = classpath.split(File.pathSeparator);
+            URL[] urls = new URL[paths.length];
+            for (int i = 0; i < paths.length; i++) {
+                urls[i] = new File(paths[i]).toURL();
+            }
+            return new URLClassLoader(urls);
+        } catch (Exception ex) {
+            throw new IOException(ex.toString());
         }
     }
 }
