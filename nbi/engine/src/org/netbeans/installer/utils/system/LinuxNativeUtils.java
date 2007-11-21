6f60bad6420a10c902f81b7378b7213309c72b32
dlipin <dlipin@netbeans.org>
2007-11-21 07:28:36 +0000

    #122040 Installation can`t proceed when the target directory is not in the mount table

--- nbi/engine/src/org/netbeans/installer/utils/system/LinuxNativeUtils.java
+++ nbi/engine/src/org/netbeans/installer/utils/system/LinuxNativeUtils.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- * 
+ *
  * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
- * 
+ *
  * The contents of this file are subject to the terms of either the GNU General
  * Public License Version 2 only ("GPL") or the Common Development and Distribution
  * License("CDDL") (collectively, the "License"). You may not use this file except in
@@ -16,13 +16,13 @@
  * accompanied this code. If applicable, add the following below the License Header,
  * with the fields enclosed by brackets [] replaced by your own identifying
  * information: "Portions Copyrighted [year] [name of copyright owner]"
- * 
+ *
  * Contributor(s):
- * 
+ *
  * The Original Software is NetBeans. The Initial Developer of the Original Software
  * is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun Microsystems, Inc. All
  * Rights Reserved.
- * 
+ *
  * If you wish your version of this file to be governed by only the CDDL or only the
  * GPL Version 2, indicate your decision by adding "[Contributor] elects to include
  * this software in this distribution under the [CDDL or GPL Version 2] license." If
@@ -37,7 +37,11 @@
 package org.netbeans.installer.utils.system;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
 import org.netbeans.installer.utils.FileUtils;
+import org.netbeans.installer.utils.LogManager;
+import org.netbeans.installer.utils.StringUtils;
 import org.netbeans.installer.utils.SystemUtils;
 
 /**
@@ -45,21 +49,22 @@ import org.netbeans.installer.utils.SystemUtils;
  * @author Kirill Sorokin
  */
 public class LinuxNativeUtils extends UnixNativeUtils {
-    public static final String LIBRARY_PREFIX_LINUX = 
-            NATIVE_JNILIB_RESOURCE_SUFFIX + 
+    public static final String LIBRARY_PREFIX_LINUX =
+            NATIVE_JNILIB_RESOURCE_SUFFIX +
             "linux/" ; //NOI18N
-            
+    
     public static final String LIBRARY_I386 =
             "linux.so"; //NO18N
-    public static final String LIBRARY_AMD64 = 
+    public static final String LIBRARY_AMD64 =
             "linux-amd64.so"; //NO18N
     
+    private static final String PROC_MOUNTS_FILE = "/proc/mounts";
     
     public static final String[] FORBIDDEN_DELETING_FILES_LINUX = {};
     
     LinuxNativeUtils() {
-        String library = System.getProperty("os.arch").equals("amd64") ? 
-                    LIBRARY_AMD64 : LIBRARY_I386;
+        String library = System.getProperty("os.arch").equals("amd64") ?
+            LIBRARY_AMD64 : LIBRARY_I386;
         
         loadNativeLibrary(LIBRARY_PREFIX_LINUX + library);
         
@@ -69,12 +74,45 @@ public class LinuxNativeUtils extends UnixNativeUtils {
     public File getDefaultApplicationsLocation() {
         File usrlocal = new File("/usr/local");
         
-        if (usrlocal.exists() && 
-                usrlocal.isDirectory() && 
+        if (usrlocal.exists() &&
+                usrlocal.isDirectory() &&
                 FileUtils.canWrite(usrlocal)) {
             return usrlocal;
         } else {
             return SystemUtils.getUserHomeDirectory();
         }
     }
+    @Override
+    public List<File> getFileSystemRoots() throws IOException {
+        List <File> roots = super.getFileSystemRoots();
+        final File mounts = new File(PROC_MOUNTS_FILE);
+        try {
+            if(FileUtils.exists(mounts) && FileUtils.canRead(mounts)) {
+                List <String> strings = FileUtils.readStringList(mounts);
+                for(int i=0;i<strings.size();i++ ) {
+                    String line = strings.get(i).trim();
+                    final int firstSpaceIndex = line.indexOf(StringUtils.SPACE);
+                    if(firstSpaceIndex!=-1) {
+                        String mountPoint = line.substring(firstSpaceIndex + 1).trim();
+                        final int nextSpaceIndex = mountPoint.indexOf(StringUtils.SPACE);
+                        if(nextSpaceIndex!=-1) {
+                            mountPoint = mountPoint.substring(0, nextSpaceIndex);
+                            if(mountPoint.startsWith(File.separator)) {
+                                final File mountFile = new File(mountPoint);
+                                if(!roots.contains(mountFile)) {
+                                    LogManager.log("... adding mount point from /proc/mounts: " + mountFile);
+                                    roots.add(mountFile);
+                                }
+                            }
+                        }
+                        
+                    }
+                }
+            }
+        } catch (IOException e) {
+            LogManager.log("... cannot read " + mounts, e);
+        }
+        return roots;
+    }
+    
 }
