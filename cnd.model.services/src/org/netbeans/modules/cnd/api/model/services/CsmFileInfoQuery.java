7461c18a7a06b542f5c68ea8042e4b779b1eaca1
sergius <sergius@netbeans.org>
2007-11-22 16:03:14 +0000

    using CsmFileReference infrastructure to get class fields and function definitions highlighting

--- cnd.model.services/src/org/netbeans/modules/cnd/api/model/services/CsmFileInfoQuery.java
+++ cnd.model.services/src/org/netbeans/modules/cnd/api/model/services/CsmFileInfoQuery.java
@@ -23,8 +23,22 @@
  *
  * Contributor(s):
  *
- * Portions Copyrighted 2007 Sun Microsystems, Inc.
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ *
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
  */
+
 package org.netbeans.modules.cnd.api.model.services;
 
 import java.util.Collections;
@@ -82,11 +96,6 @@ public abstract class CsmFileInfoQuery {
      */
     public abstract List<CsmReference> getMacroUsages(CsmFile file);
 
-    /**
-     * @return list of class fields usages in the method's body
-     */
-    public abstract List<CsmReference> getClassFields(CsmFile file);
-
     //
     // Implementation of the default query
     //
@@ -107,10 +116,6 @@ public abstract class CsmFileInfoQuery {
         }
 
         public List<CsmReference> getMacroUsages(CsmFile file) {
-            return Collections.<CsmReference>emptyList();
-        }
-
-        public List<CsmReference> getClassFields(CsmFile file) {
             return Collections.<CsmReference>emptyList();
         }
     } 
