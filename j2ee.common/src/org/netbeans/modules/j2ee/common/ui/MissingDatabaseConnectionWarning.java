b5db814ad31a559033f25cbc01ef91a60fc34e56
jbaker <jbaker@netbeans.org>
2007-12-04 22:05:48 +0000

    121694 deadlock when resolve data sources Fixed.  resetting jButton now done in the EDT

--- j2ee.common/src/org/netbeans/modules/j2ee/common/ui/MissingDatabaseConnectionWarning.java
+++ j2ee.common/src/org/netbeans/modules/j2ee/common/ui/MissingDatabaseConnectionWarning.java
@@ -43,8 +43,6 @@ package org.netbeans.modules.j2ee.common.ui;
 
 import java.awt.Component;
 import java.awt.Dialog;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
 import java.util.Iterator;
 import java.util.Set;
 import javax.swing.AbstractListModel;
@@ -55,8 +53,8 @@ import javax.swing.ListCellRenderer;
 import javax.swing.ListSelectionModel;
 import javax.swing.SwingUtilities;
 import javax.swing.border.Border;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
+import javax.swing.event.ListDataEvent;
+import javax.swing.event.ListDataListener;
 import org.netbeans.api.db.explorer.ConnectionListener;
 import org.netbeans.api.db.explorer.ConnectionManager;
 import org.netbeans.api.db.explorer.JDBCDriver;
@@ -249,13 +247,14 @@ public final class MissingDatabaseConnectionWarning extends JPanel {
     // End of variables declaration//GEN-END:variables
     
     
-    private final class DatasourceListModel extends AbstractListModel implements Runnable, ConnectionListener {
+    private final class DatasourceListModel extends AbstractListModel implements Runnable, ConnectionListener, ListDataListener {
         private final RequestProcessor BROKEN_DATASOURCE_RP = new RequestProcessor("WebLogicalViewProvider.BROKEN_DATASOURCE_RP"); //NOI18N
         private Set<Datasource> datasources;
         
         public DatasourceListModel() {          
-            datasources = BrokenDatasourceSupport.getBrokenDatasources(project);
-            addConnectionListener();
+            datasources = BrokenDatasourceSupport.getBrokenDatasources(project);            
+            addConnectionListener();              
+            addListDataListener(this);
         }
         
         public synchronized int getSize() {
@@ -307,13 +306,8 @@ public final class MissingDatabaseConnectionWarning extends JPanel {
             SwingUtilities.invokeLater(new Runnable() {
                 public void run() {
                     fireContentsChanged(this, 0, datasources.size());
-                }
-            });
-            
-            if (datasources.isEmpty()) {
-                jButtonAddConnection.setEnabled(false);
-                removeConnectionListener();
-            }
+                }                
+            });                                  
         }
         
         // Listen for any connections changed in DB Explorer and refresh model accordingly
@@ -325,9 +319,34 @@ public final class MissingDatabaseConnectionWarning extends JPanel {
         private void addConnectionListener() {
             ConnectionManager.getDefault().addConnectionListener(this);
         }
-        
+                
         private void removeConnectionListener() {
             ConnectionManager.getDefault().removeConnectionListener(this);
+        }
+
+        // ListDataListener methods to implement
+        public void intervalAdded(ListDataEvent e) {
+            cleanup();
+        }
+
+        public void intervalRemoved(ListDataEvent e) {
+            // unused
+        }
+        
+        public void contentsChanged(ListDataEvent e) {
+            cleanup();
+        }
+        
+        private void cleanup() {
+            // If the list of data (datasources) is empty then we're done.  Remove listeners
+            // and disable the Add Connection button.
+            // Otherwise if the list is not empty then listeners are still needed.
+            // Note: if list is not empty the project node will remain badged
+            if (datasources.isEmpty()) {
+                removeConnectionListener();
+                removeListDataListener(this);
+                jButtonAddConnection.setEnabled(false);                
+            }
         }
     }
     
