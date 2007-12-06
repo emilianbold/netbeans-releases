13b11b1c9a0a01821ab1020da24045b97c56ca9d
tor <tor@netbeans.org>
2007-12-06 22:52:06 +0000

    Add an extra hint which checks for retry nodes outside of the rescue node, which as of Ruby 1.9 is illegal

---
+++ ruby.extrahints/test/unit/data/testfiles/retry.rb
@@ -0,0 +1,32 @@
+def stale_session_check!
+  yield
+rescue ArgumentError => argument_error
+  if argument_error.message =~ %r{undefined class/module ([\w:]+)}
+    begin
+      Module.const_missing($1)
+    rescue LoadError, NameError => const_error
+      raise ActionController::SessionRestoreError, <<-end_msg
+Session contains objects whose class definition isn\'t available.
+Remember to require the classes for all objects kept in the session.
+(Original exception: #{const_error.message} [#{const_error.class}])
+      end_msg
+    end
+
+    retry #1
+  else
+    raise
+  end
+end
+
+retry #2
+
+def
+  retry #3
+end
+
+begin
+  retry #4
+rescue
+  retry #5
+end
+
