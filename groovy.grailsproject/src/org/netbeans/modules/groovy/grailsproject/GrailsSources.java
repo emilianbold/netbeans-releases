bfcd7dcafd2336838d8f0f6603946f207deaa8d5
Martin Adamek <martin_adamek@netbeans.org>
2007-12-23 11:19:55 +0000

    Don't display hidden dirs in grails-app in logical view

--- groovy.grailsproject/src/org/netbeans/modules/groovy/grailsproject/GrailsSources.java
+++ groovy.grailsproject/src/org/netbeans/modules/groovy/grailsproject/GrailsSources.java
@@ -102,7 +102,7 @@ public class GrailsSources implements Sources {
         } else if ("scripts".equals(type)) {
             result.add(new Group(projectDir.getFileObject("scripts"), "Scripts", null, null, SourceCategory.VIEWS));
         } else {
-            if(! type.startsWith(".")) { // we have to filter-out hidden directories like .settings etc.
+            if(!type.startsWith(".") && !type.startsWith("grails-app/.")) { // we have to filter-out hidden directories like .settings etc.
                 FileObject fileObject = projectDir.getFileObject(type);
                 if (fileObject != null) {
                     result.add(new Group(fileObject, fileObject.getName(), null, null, SourceCategory.NONE));
