b6d2723eac73fbedc4faa9dc8831439e0aba894c
jjancura <jjancura@netbeans.org>
2008-01-07 22:36:09 +0000

    some predefined tokens registered

--- languages/src/org/netbeans/modules/languages/NBSLanguageReader.java
+++ languages/src/org/netbeans/modules/languages/NBSLanguageReader.java
@@ -126,14 +126,14 @@ public class NBSLanguageReader {
         //HACK adding new token typed does not work because
         // token hierarchy is not updated when TokenProvider fires change
         // so we reserved some default token types at least there
-//        addToken (null, null, "string", null, null);
-//        addToken (null, null, "character", null, null);
-//        addToken (null, null, "identifier", null, null);
-//        addToken (null, null, "whitespace", null, null);
-//        addToken (null, null, "number", null, null);
-//        addToken (null, null, "keyword", null, null);
-//        addToken (null, null, "comment", null, null);
-//        addToken (null, null, "operator", null, null);
+        addToken (null, null, "string", null, null);
+        addToken (null, null, "character", null, null);
+        addToken (null, null, "identifier", null, null);
+        addToken (null, null, "whitespace", null, null);
+        addToken (null, null, "number", null, null);
+        addToken (null, null, "keyword", null, null);
+        addToken (null, null, "comment", null, null);
+        addToken (null, null, "operator", null, null);
         containsTokens = false;
     }
     
@@ -162,7 +162,8 @@ public class NBSLanguageReader {
         return tokenTypes;
     }
     
-    public boolean containsTokens () {
+    public boolean containsTokens () throws ParseException, IOException {
+        if (features == null) readNBS ();
         return containsTokens;
     }
     
