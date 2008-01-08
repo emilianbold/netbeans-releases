c862f61d209e23c20db9f523b7197b586dd7a360
Martin Adamek <martin_adamek@netbeans.org>
2008-01-08 12:46:38 +0000

    Imports cleanup

--- groovy.editor/src/org/netbeans/modules/groovy/editor/parser/GroovySemanticAnalyzer.java
+++ groovy.editor/src/org/netbeans/modules/groovy/editor/parser/GroovySemanticAnalyzer.java
@@ -47,8 +47,6 @@ import java.util.Map;
 import org.codehaus.groovy.ast.ASTNode;
 import org.codehaus.groovy.ast.FieldNode;
 import org.codehaus.groovy.ast.MethodNode;
-import org.codehaus.groovy.ast.expr.ClosureExpression;
-import org.codehaus.groovy.ast.expr.Expression;
 import org.netbeans.api.gsf.ColoringAttributes;
 import org.netbeans.api.gsf.CompilationInfo;
 import org.netbeans.api.gsf.OffsetRange;
@@ -86,6 +84,9 @@ public class GroovySemanticAnalyzer implements SemanticAnalyzer {
     }
 
     public void run(CompilationInfo info) {
+        
+        System.out.println("### GroovySemanticAnalyzer.run: " + info);
+        
         resume();
 
         if (isCancelled()) {
