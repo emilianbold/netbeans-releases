70692a8a53c5eef6bbec42feecc3c9cbcd8da073
tzezula <tzezula@netbeans.org>
2008-01-18 12:59:33 +0000

    #124495:IOException at netbeans 6.0 Startup

--- java.source/src/org/netbeans/modules/java/source/usages/LuceneIndex.java
+++ java.source/src/org/netbeans/modules/java/source/usages/LuceneIndex.java
@@ -554,7 +554,10 @@ class LuceneIndex extends Index {
             } finally {
                 searcher.close();
             }
-        } catch (java.io.FileNotFoundException fnf) {
+        } catch (java.io.IOException ioe) {
+            //The FileNotFoundException is may be thrown if the index is not fully merged,
+            //also IOException can be thrown when index was not closed on exit.
+            //Both can be caused by killing the IDE.
             this.clear();
             return false;
         }
