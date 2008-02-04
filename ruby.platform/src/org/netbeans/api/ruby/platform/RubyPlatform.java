470b5851a18b04eb271abe677e1ae8e91e789ef3
Erno Mononen <emononen@netbeans.org>
2008-02-04 13:21:50 +0100

    equals and hashCode for RubyPlatform

--- ruby.platform/src/org/netbeans/api/ruby/platform/RubyPlatform.java
+++ ruby.platform/src/org/netbeans/api/ruby/platform/RubyPlatform.java
@@ -696,6 +696,28 @@ public final class RubyPlatform {
         return info.getGemHome() != null;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final RubyPlatform other = (RubyPlatform) obj;
+        if (this.id != other.id && (this.id == null || !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + (this.id != null ? this.id.hashCode() : 0);
+        return hash;
+    }
+
     public @Override String toString() {
         return "RubyPlatform[id:" + getID() + ", label:" + getLabel() + ", " + getInterpreter() + "]"; // NOI18N
     }
