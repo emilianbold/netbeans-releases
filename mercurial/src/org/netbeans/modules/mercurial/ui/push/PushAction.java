f146553f381d9fea2f6f48bedd6b9398a1499627
jrice <jrice@netbeans.org>
2008-02-05 17:11:29 +0000

    #126743: Invalid Output when doing Push to repo with nested NB projects

--- mercurial/src/org/netbeans/modules/mercurial/ui/push/PushAction.java
+++ mercurial/src/org/netbeans/modules/mercurial/ui/push/PushAction.java
@@ -205,7 +205,7 @@ public class PushAction extends ContextAction {
 
                 HgUtils.outputMercurialTab(list);
 
-                if (toPrjName == null) {
+                if (toPrjName == null) { 
                     HgUtils.outputMercurialTabInRed(
                             NbBundle.getMessage(PushAction.class,
                             "MSG_PUSH_TO_NONAME", bLocalPush ? HgUtils.stripDoubleSlash(pushPath) : pushPath)); // NOI18N
@@ -214,9 +214,16 @@ public class PushAction extends ContextAction {
                             NbBundle.getMessage(PushAction.class,
                             "MSG_PUSH_TO", toPrjName, bLocalPush ? HgUtils.stripDoubleSlash(pushPath) : pushPath)); // NOI18N
                 }
-                HgUtils.outputMercurialTabInRed(
-                        NbBundle.getMessage(PushAction.class,
-                        "MSG_PUSH_FROM", fromPrjName, root)); // NOI18N
+
+                if (fromPrjName == null ){
+                    HgUtils.outputMercurialTabInRed(
+                            NbBundle.getMessage(PushAction.class,
+                            "MSG_PUSH_FROM_NONAME", root)); // NOI18N
+                } else {
+                    HgUtils.outputMercurialTabInRed(
+                            NbBundle.getMessage(PushAction.class,
+                            "MSG_PUSH_FROM", fromPrjName, root)); // NOI18N
+                }
 
                 boolean bMergeNeeded = HgCommand.isHeadsCreated(list.get(list.size() - 1));
                 boolean bConfirmMerge = false;
@@ -225,11 +232,7 @@ public class PushAction extends ContextAction {
                     if (bNoChanges) {
                         return;
                     }
-                    if (!bLocalPush) {
-                        HgUtils.outputMercurialTabInRed(
-                                NbBundle.getMessage(PushAction.class,
-                                "MSG_PUSH_UPDATE_NEEDED_NONAME", toPrjName, pushPath)); // NOI18N
-                    } else {
+                    if (bLocalPush) {
                         list = HgCommand.doUpdateAll(pushFile, false, null, false);
                         HgUtils.outputMercurialTab(list);
                         if (toPrjName != null) {
