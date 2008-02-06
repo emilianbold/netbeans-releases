0d2aefff366579f5f7ee47cf81ac6a1127b232d9
Nam Nguyen <nnguyen@netbeans.org>
2008-02-05 23:46:04 -0800

    Implemented UI basic for WADL base services

--- websvc.saas.ui/src/org/netbeans/modules/websvc/saas/ui/actions/ViewApiDocAction.java
+++ websvc.saas.ui/src/org/netbeans/modules/websvc/saas/ui/actions/ViewApiDocAction.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- * 
+ *
  * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
- * 
+ *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
  * Development and Distribution License("CDDL") (collectively, the
@@ -20,7 +20,13 @@
  * License Header, with the fields enclosed by brackets [] replaced by
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- * 
+ *
+ * Contributor(s):
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ *
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -31,31 +37,44 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
- * 
- * Contributor(s):
- * 
- * Portions Copyrighted 2008 Sun Microsystems, Inc.
  */
 
-package org.netbeans.modules.websvc.saas.ui.nodes;
+package org.netbeans.modules.websvc.saas.ui.actions;
 
-import org.netbeans.modules.websvc.saas.model.SaasMethod;
-import org.netbeans.modules.websvc.saas.model.WadlSaasMethod;
+import org.openide.nodes.Node;
+import org.openide.util.HelpCtx;
+import org.openide.util.NbBundle;
+import org.openide.util.actions.NodeAction;
 
 /**
  *
- * @author nam
+ * @author  nam
  */
-public class WadlSaasMethodNode extends WadlMethodNode {
-    private SaasMethod saasMethod;
+public class ViewApiDocAction extends NodeAction {
     
-    public WadlSaasMethodNode(WadlSaasMethod saasMethod) {
-        super(saasMethod.getSaas(), saasMethod.getResourcePath(), saasMethod.getWadlMethod());
-        this.saasMethod = saasMethod;
+    /** Creates a new instance of ViewWSDLAction */
+    public ViewApiDocAction() {
+        super();
     }
-
-    @Override
-    public String getDisplayName() {
-        return saasMethod.getName();
+    
+    protected boolean enable(Node[] nodes) {
+        //TODO
+        return false;
+    }
+    
+    public HelpCtx getHelpCtx() {
+        return HelpCtx.DEFAULT_HELP;
+    }
+    
+    public String getName() {
+        return NbBundle.getMessage(ViewWSDLAction.class, "VIEW_API_Action");
+    }
+    
+    protected void performAction(Node[] activatedNodes) {
+        //TODO
+    }
+    
+    public boolean asynchronous() {
+        return true;
     }
 }
