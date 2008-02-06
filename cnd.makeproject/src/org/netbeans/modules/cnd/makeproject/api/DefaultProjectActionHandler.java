56eb4967b26cba8542b4cf76e6efd39dff569d47
Thomas Preisler <thp@netbeans.org>
2008-02-06 10:02:57 -0800

    118572 Terminate running application( or building)

--- cnd.makeproject/src/org/netbeans/modules/cnd/makeproject/api/DefaultProjectActionHandler.java
+++ cnd.makeproject/src/org/netbeans/modules/cnd/makeproject/api/DefaultProjectActionHandler.java
@@ -46,10 +46,13 @@ import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 import java.text.MessageFormat;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.ImageIcon;
 import org.netbeans.api.project.ProjectUtils;
-import org.netbeans.modules.cnd.api.compilers.CompilerSet;
-import org.netbeans.modules.cnd.api.compilers.CompilerSet.CompilerFlavor;
 import org.netbeans.modules.cnd.api.compilers.CompilerSetManager;
 import org.netbeans.modules.cnd.api.execution.ExecutionListener;
 import org.netbeans.modules.cnd.api.execution.NativeExecutor;
@@ -65,6 +68,7 @@ import org.netbeans.modules.cnd.settings.CppSettings;
 import org.openide.DialogDescriptor;
 import org.openide.DialogDisplayer;
 import org.openide.NotifyDescriptor;
+import org.openide.execution.ExecutorTask;
 import org.openide.filesystems.FileObject;
 import org.openide.util.NbBundle;
 import org.openide.util.Utilities;
@@ -108,13 +112,17 @@ public class DefaultProjectActionHandler implements ActionListener {
     
     private static InputOutput mainTab = null;
     private static HandleEvents mainTabHandler = null;
-    private static HashMap<String, Integer> tabMap = new HashMap();
+    private static ArrayList tabNames = new ArrayList();
     
     class HandleEvents implements ExecutionListener {
         private InputOutput reuseTab = null;
         private ProjectActionEvent[] paes;
         private String tabName;
+        private String tabNameSeq;
         int currentAction = 0;
+        ExecutorTask executorTask = null;
+        StopAction sa = null;
+        RerunAction ra = null;
         
         private String getTabName(ProjectActionEvent[] paes) {
             String projectName = ProjectUtils.getInformation(paes[0].getProject()).getName();
@@ -141,16 +149,18 @@ public class DefaultProjectActionHandler implements ActionListener {
         }
         
         private InputOutput getTab() {
-            if (reuseTab != null)
-                return reuseTab;
-            else {
-                InputOutput tab = IOProvider.getDefault().getIO(getTabName(paes[currentAction]), false);
-                try {
-                    tab.getOut().reset();
-                } catch (IOException ioe) {
-                }
-                return tab;
+            return reuseTab;
+        }
+        
+        private InputOutput getIOTab(String tabName) {
+            sa = new StopAction(this);
+            ra = new RerunAction(this);
+            InputOutput tab = IOProvider.getDefault().getIO(tabNameSeq, new Action[] {ra, sa});
+            try {
+                tab.getOut().reset();
+            } catch (IOException ioe) {
             }
+            return tab;
         }
         
         public HandleEvents(ProjectActionEvent[] paes) {
@@ -162,32 +172,46 @@ public class DefaultProjectActionHandler implements ActionListener {
                     mainTab.closeInputOutput();
                     mainTab = null;
                 }
-//                if (mainTab != null && mainTab.isClosed()) {
-//                    mainTabHandler = null;
-//                    mainTab = null;
-//                }
                 tabName = getTabName(paes);
-                Integer i = tabMap.get(tabName);
-                if (i == null) {
-                    i = new Integer(1);
-                    tabMap.put(tabName, i);
-                } else {
-                    tabMap.put(tabName, ++i);
+                tabNameSeq = tabName;
+                if (tabNames.contains(tabName)) {
+                    int seq = 2;
+                    while (true) {
+                        tabNameSeq = tabName + " #" + seq;
+                        if (!tabNames.contains(tabNameSeq)) {
+                            break;
+                        }
+                        seq++;
+                    }
                 }
-                InputOutput tab = IOProvider.getDefault().getIO(tabName, i != 1);
-                try {
-                    tab.getOut().reset();
-                } catch (IOException ioe) {
-                }
+                tabNames.add(tabNameSeq);
+                reuseTab = getIOTab(tabNameSeq);
                 if (mainTabHandler == null) {
-                    reuseTab = tab;
                     mainTab = reuseTab;
                     mainTabHandler = this;
                 }
             }
+            else {
+                tabName = getTabName(paes);
+                tabNameSeq = tabName;
+                reuseTab = getIOTab(tabName);
+            }
+        }
+        
+        public void reset() {
+            currentAction = 0;
+            getTab().closeInputOutput();
+            tabNames.add(tabNameSeq);
+            try {
+                getTab().getOut().reset();
+            } catch (IOException ioe) {
+            }
         }
         
         public void go() {
+            executorTask = null;
+            sa.setEnabled(false);
+            ra.setEnabled(false);
             if (currentAction >= paes.length)
                 return;
             
@@ -304,7 +328,11 @@ public class DefaultProjectActionHandler implements ActionListener {
                     projectExecutor.setExitValueOverride(rcfile);
                 }
                 try {
-                    projectExecutor.execute(getTab());
+                    sa.setEnabled(true);
+                    ra.setEnabled(false);
+                    executorTask = projectExecutor.execute(getTab());
+//                    System.out.println("handleEvents 0 " + this);
+//                    System.out.println("executorTask 0 " + executorTask);
                 } catch (java.io.IOException ioe) {
                 }
             } else if (pae.getID() == ProjectActionEvent.CUSTOM_ACTION) {
@@ -318,14 +346,15 @@ public class DefaultProjectActionHandler implements ActionListener {
             }
         }
         
+        public ExecutorTask getExecutorTask() {
+            return executorTask;
+        }
+        
         public void executionStarted() {
             // Nothing
         }
         
         public void executionFinished(int rc) {
-            Integer i = tabMap.get(tabName);
-            if (i != null)
-                tabMap.put(tabName, --i);
             if (paes[currentAction].getID() == ProjectActionEvent.BUILD || paes[currentAction].getID() == ProjectActionEvent.CLEAN) {
                 // Refresh all files
                 try {
@@ -337,6 +366,9 @@ public class DefaultProjectActionHandler implements ActionListener {
             if (currentAction >= paes.length-1 || rc != 0) {
                 if (mainTabHandler == this)
                     mainTabHandler = null;
+                tabNames.remove(tabNameSeq);
+                sa.setEnabled(false);
+                ra.setEnabled(true);
                 return;
             }
             if (rc == 0) {
@@ -403,6 +435,64 @@ public class DefaultProjectActionHandler implements ActionListener {
                 }
             return true;
         }
+    }
+
+    private static final class StopAction extends AbstractAction {
+        HandleEvents handleEvents;
+
+        public StopAction(HandleEvents handleEvents) {
+            this.handleEvents = handleEvents;
+            //System.out.println("handleEvents 1 " + handleEvents);
+            //setEnabled(false); // initially, until ready
+        }
+
+        @Override
+        public Object getValue(String key) {
+            if (key.equals(Action.SMALL_ICON)) {
+                return new ImageIcon(NativeExecutor.class.getResource("/org/netbeans/modules/cnd/api/execution/resources/stop.png"));
+            } else if (key.equals(Action.SHORT_DESCRIPTION)) {
+                return getString("TargetExecutor.StopAction.stop");
+            } else {
+                return super.getValue(key);
+            }
+        }
+
+        public void actionPerformed(ActionEvent e) {
+            //setEnabled(false); // discourage repeated clicking
+            //System.out.println("handleEvents 2 " + handleEvents);
+            //System.out.println("executorTask 2 " + handleEvents.getExecutorTask());
+            if (handleEvents.getExecutorTask() != null) {
+                handleEvents.getExecutorTask().stop();
+            }
+        }
+
+    }
+
+    private static final class RerunAction extends AbstractAction {
+        HandleEvents handleEvents;
+
+        public RerunAction(HandleEvents handleEvents) {
+            this.handleEvents = handleEvents;
+            System.out.println("handleEvents 1 " + handleEvents);
+        }
+
+        @Override
+        public Object getValue(String key) {
+            if (key.equals(Action.SMALL_ICON)) {
+                return new ImageIcon(NativeExecutor.class.getResource("/org/netbeans/modules/cnd/api/execution/resources/rerun.png"));
+            } else if (key.equals(Action.SHORT_DESCRIPTION)) {
+                return getString("TargetExecutor.RerunAction.rerun");
+            } else {
+                return super.getValue(key);
+            }
+        }
+
+        public void actionPerformed(ActionEvent e) {
+            //setEnabled(false);
+            handleEvents.reset();
+            handleEvents.go();
+        }
+
     }
     
     /** Look up i18n strings here */
