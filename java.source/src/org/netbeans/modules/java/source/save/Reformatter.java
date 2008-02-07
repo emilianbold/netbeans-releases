f2f4ffbe6f46d1878466046b741e99fb4b973ae5
Tomas Zezula <tzezula@netbeans.org>
2008-02-07 19:02:54 +0100

    Deadlock among static initializers

--- java.source/src/org/netbeans/modules/java/source/save/Reformatter.java
+++ java.source/src/org/netbeans/modules/java/source/save/Reformatter.java
@@ -87,7 +87,7 @@ public class Reformatter implements ReformatTask {
                     }
                 }, true);            
             } catch (IOException ioe) {
-                JavaSourceAccessor.INSTANCE.unlockJavaCompiler();
+                JavaSourceAccessor.getINSTANCE().unlockJavaCompiler();
             }
             if (controller == null)
                 return;
@@ -101,7 +101,7 @@ public class Reformatter implements ReformatTask {
         try {
             ClassPath empty = ClassPathSupport.createClassPath(new URL[0]);
             ClasspathInfo cpInfo = ClasspathInfo.create(empty, empty, empty);
-            JavacTaskImpl javacTask = JavaSourceAccessor.INSTANCE.createJavacTask(cpInfo, null, null);
+            JavacTaskImpl javacTask = JavaSourceAccessor.getINSTANCE().createJavacTask(cpInfo, null, null);
             com.sun.tools.javac.util.Context ctx = javacTask.getContext();
             JavaCompiler.instance(ctx).genEndPos = true;
             CompilationUnitTree tree = javacTask.parse(FileObjects.memoryFileObject(text, "")).iterator().next(); //NOI18N
@@ -243,12 +243,12 @@ public class Reformatter implements ReformatTask {
     private class Lock implements ExtraLock {
 
         public void lock() {
-            JavaSourceAccessor.INSTANCE.lockJavaCompiler();
+            JavaSourceAccessor.getINSTANCE().lockJavaCompiler();
         }
 
         public void unlock() {
             controller = null;
-            JavaSourceAccessor.INSTANCE.unlockJavaCompiler();
+            JavaSourceAccessor.getINSTANCE().unlockJavaCompiler();
         }        
     }
     
