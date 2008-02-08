9b931ecdf47c7c87e809fc8b2b1c248645b770d8
Erno Mononen <emononen@netbeans.org>
2008-02-08 12:48:20 +0100

    #106908 related, storing to private.properties

--- ruby.railsprojects/src/org/netbeans/modules/ruby/railsprojects/ui/customizer/RailsProjectProperties.java
+++ ruby.railsprojects/src/org/netbeans/modules/ruby/railsprojects/ui/customizer/RailsProjectProperties.java
@@ -268,7 +268,7 @@ public class RailsProjectProperties extends SharedRubyProjectProperties {
 
         RailsProjectProperties.storePlatform(privateProperties, getPlatform());
         RailsProjectProperties.storeServer(privateProperties, getServer());
-        projectProperties.setProperty(RAILS_ENV, getRailsEnvironment()); // NOI18N
+        privateProperties.setProperty(RAILS_ENV, getRailsEnvironment()); // NOI18N
 
         // Standard store of the properties
         projectGroup.store( projectProperties );        
