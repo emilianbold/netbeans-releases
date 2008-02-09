3a6f821909d062bbd31be1a4a7bf434d808f4d07
Tomas Mysik <tmysik@netbeans.org>
2008-02-09 22:30:26 +0100

    Fixing tests

--- web.jspparser/test/unit/src/org/netbeans/modules/web/jspparser/TestUtil.java
+++ web.jspparser/test/unit/src/org/netbeans/modules/web/jspparser/TestUtil.java
@@ -88,6 +88,9 @@ final class TestUtil {
 
         // module system
         Lookup.getDefault().lookup(ModuleInfo.class);
+
+        // unzip test project
+        TestUtil.getProject(test, "project3");
     }
 
     static FileObject getFileInWorkDir(String path, NbTestCase test) throws Exception {
@@ -119,7 +122,7 @@ final class TestUtil {
     static FileObject getProjectFile(NbTestCase test, String projectFolderName, String filePath) throws Exception {
         Project project = getProject(test, projectFolderName);
         FileObject fo = project.getProjectDirectory().getFileObject(filePath);
-            NbTestCase.assertNotNull("Project file should exist: " + filePath, fo);
+        NbTestCase.assertNotNull("Project file should exist: " + filePath, fo);
 
         return fo;
     }
