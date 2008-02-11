02d3eb8527872a607cd6ba15a02eea608d6f398e
Nam Nguyen <nnguyen@netbeans.org>
2008-02-10 22:40:47 -0800

    - Restructure with WsdlData and WsdlDataManager to have correct module dependency from existing websvc.manager instead of reverse. - Add infrastructure for Transferable and DataFlavor

--- websvc.saas.api/src/org/netbeans/modules/websvc/saas/spi/websvcmgr/WsdlData.java
+++ websvc.saas.api/src/org/netbeans/modules/websvc/saas/spi/websvcmgr/WsdlData.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- *
+ * 
  * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
- *
+ * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
  * Development and Distribution License("CDDL") (collectively, the
@@ -20,13 +20,7 @@
  * License Header, with the fields enclosed by brackets [] replaced by
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- *
- * Contributor(s):
- *
- * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
- * Microsystems, Inc. All Rights Reserved.
- *
+ * 
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -37,15 +31,58 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
+ * 
+ * Contributor(s):
+ * 
+ * Portions Copyrighted 2008 Sun Microsystems, Inc.
  */
 
-package org.netbeans.modules.websvc.saas.spi;
+package org.netbeans.modules.websvc.saas.spi.websvcmgr;
 
-import org.netbeans.modules.websvc.saas.spi.websvcmgr.WsdlServiceProxyDescriptor;
+import java.beans.PropertyChangeListener;
+import java.io.File;
+import org.netbeans.modules.websvc.api.jaxws.wsdlmodel.WsdlService;
 
-
-public interface SaasConsumerExtension {
+/**
+ * For compatibilty with websvc.manager and avoid circular dependency
+ * 
+ * @author nam
+ */
+public interface WsdlData {
     
-    public boolean wsServiceAddedExt(WsdlServiceProxyDescriptor wsMetadataDesc);
-    public boolean wsServiceRemovedExt(WsdlServiceProxyDescriptor wsMetadataDesc);
+    /**
+     * @return id string that identities the WSDL service in persistence in userdir 
+     */
+    String getId();
+    
+    /**
+     * @return URL
+     */
+    String getOriginalWsdlUrl();
+    
+    /**
+     * @return Wsdl service name
+     */
+    String getName();
+    
+    /**
+     * @return path to retrieved WSDL file
+     */
+    String getWsdlFile();
+    
+    /**
+     * @return WSDL service
+     */
+    WsdlService getWsdlService();
+    
+    boolean isReady();
+
+    /**
+     * Returns WSDL Service Proxy Descriptor
+     */
+    WsdlServiceProxyDescriptor getJaxWsDescriptor();
+    WsdlServiceProxyDescriptor getJaxRpcDescriptor();
+    
+    void addPropertyChangeListener(PropertyChangeListener l);
+    void removePropertyChangeListener(PropertyChangeListener l);
 }
--- websvc.saas.api/src/org/netbeans/modules/websvc/saas/spi/websvcmgr/WsdlDataManager.java
+++ websvc.saas.api/src/org/netbeans/modules/websvc/saas/spi/websvcmgr/WsdlDataManager.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- *
+ * 
  * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
- *
+ * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
  * Development and Distribution License("CDDL") (collectively, the
@@ -20,13 +20,7 @@
  * License Header, with the fields enclosed by brackets [] replaced by
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- *
- * Contributor(s):
- *
- * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
- * Microsystems, Inc. All Rights Reserved.
- *
+ * 
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -37,15 +31,38 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
+ * 
+ * Contributor(s):
+ * 
+ * Portions Copyrighted 2008 Sun Microsystems, Inc.
  */
 
-package org.netbeans.modules.websvc.saas.spi;
+package org.netbeans.modules.websvc.saas.spi.websvcmgr;
 
-import org.netbeans.modules.websvc.saas.spi.websvcmgr.WsdlServiceProxyDescriptor;
+import org.netbeans.modules.websvc.saas.*;
 
-
-public interface SaasConsumerExtension {
+/**
+ * Hook to reuse websvc.manager retrieval, compiling and persistence facilityes.
+ * Only to be implemented by websvc.manager.
+ * 
+ * @author nam
+ */
+public interface WsdlDataManager {
     
-    public boolean wsServiceAddedExt(WsdlServiceProxyDescriptor wsMetadataDesc);
-    public boolean wsServiceRemovedExt(WsdlServiceProxyDescriptor wsMetadataDesc);
+    /**
+     * Find or create the WSDL data for the given WSDL URL.
+     * 
+     * @param wsdlUrl
+     * @param serviceName  optional service name; if null return default service
+     * @param synchronuous whether the call is synchronous.
+     * @return WsdlData object, in ready state for consumer editor, if synchronous.
+     */
+    WsdlData getWsdlData(String wsdlUrl, String serviceName, boolean synchronuous);
+    
+    /**
+     * Remove the WSDL data for given WSDL URL from persistence.
+     * @param wsdlUrl
+     */
+  
+    void removeWsdlData(String wsdlUrl, String serviceName);
 }
