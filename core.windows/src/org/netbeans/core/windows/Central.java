63b29bc74b385b943441330436d3bbffd1f581a5
S. Aubrecht <saubrecht@netbeans.org>
2008-02-12 12:42:33 +0100

    123887 - Minimized top component cannot be restored back after undock and dock

--- core.windows/src/org/netbeans/core/windows/Central.java
+++ core.windows/src/org/netbeans/core/windows/Central.java
@@ -1619,7 +1619,8 @@ final class Central implements ControllerHandler {
             for (int i = 0; i < tcs.length; i++) {
                 prevMode = (ModeImpl) wmi.findMode(tcs[i]);
                 tcID = wmi.findTopComponentID(tcs[i]);
-                if (prevMode.getState() == Constants.MODE_STATE_SEPARATED) {
+                if (prevMode.getState() == Constants.MODE_STATE_SEPARATED
+                        || prevMode.getKind() == Constants.MODE_KIND_SLIDING ) {
                     prevMode = model.getModeTopComponentPreviousMode(prevMode, tcID);
                 }
                 prevIndex = prevMode.getOpenedTopComponentsIDs().indexOf( tcID );
