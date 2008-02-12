6456d5847006fbe42e922e79b333a62591bf756f
Tomas Mysik <tmysik@netbeans.org>
2008-02-12 13:38:30 +0100

    Deprecation fixed

--- web.jspparser/src/org/netbeans/modules/web/jsps/parserapi/Node.java
+++ web.jspparser/src/org/netbeans/modules/web/jsps/parserapi/Node.java
@@ -2020,7 +2020,7 @@ public abstract class Node {
 	public boolean isAllSpace() {
 	    boolean isAllSpace = true;
 	    for (int i=0; i<text.length(); i++) {
-		if (!Character.isSpace(text.charAt(i))) {
+		if (!Character.isWhitespace(text.charAt(i))) {
 		    isAllSpace = false;
 		    break;
 		}
