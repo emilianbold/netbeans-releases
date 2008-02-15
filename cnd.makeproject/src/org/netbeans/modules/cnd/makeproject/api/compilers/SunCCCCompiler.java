df0e24d993872232603df0d25e7c63c5548a6533
Thomas Preisler <thp@netbeans.org>
2008-02-15 15:14:44 -0800

    127408 140 useless classes initialized on startup in CND Got rid of MakeProjectModule. General cleanup.

--- cnd.makeproject/src/org/netbeans/modules/cnd/makeproject/api/compilers/SunCCCCompiler.java
+++ cnd.makeproject/src/org/netbeans/modules/cnd/makeproject/api/compilers/SunCCCCompiler.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- * 
+ *
  * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
- * 
+ *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
  * Development and Distribution License("CDDL") (collectively, the
@@ -20,7 +20,13 @@
  * License Header, with the fields enclosed by brackets [] replaced by
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- * 
+ *
+ * Contributor(s):
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ *
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -31,39 +37,27 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
- * 
- * Contributor(s):
- * 
- * Portions Copyrighted 2008 Sun Microsystems, Inc.
  */
 
 package org.netbeans.modules.cnd.makeproject.api.compilers;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.List;
 import org.netbeans.modules.cnd.api.compilers.CompilerSet.CompilerFlavor;
 import org.openide.DialogDisplayer;
-import org.openide.ErrorManager;
 import org.openide.NotifyDescriptor;
 import org.openide.util.NbBundle;
 
-/**
- * A common base class for GNU C and C++  compilers
- * @author vk155633
- */
-public abstract class GNUCCCCompiler extends CCCCompiler {
-
-    private PersistentList systemIncludeDirectoriesList = null;
-    private PersistentList systemPreprocessorSymbolsList = null;
+public abstract class SunCCCCompiler extends CCCCompiler {
+    protected PersistentList systemIncludeDirectoriesList = null;
+    protected PersistentList systemPreprocessorSymbolsList = null;
     
-    public GNUCCCCompiler(CompilerFlavor flavor, int kind, String name, String displayName, String path) {
+    public SunCCCCompiler(CompilerFlavor flavor, int kind, String name, String displayName, String path) {
         super(flavor, kind, name, displayName, path);
     }
-
+    
+    
     @Override
     public boolean setSystemIncludeDirectories(List values) {
         assert values != null;
@@ -105,6 +99,7 @@ public abstract class GNUCCCCompiler extends CCCCompiler {
         return systemIncludeDirectoriesList;
     }
     
+    
     @Override
     public void saveSystemIncludesAndDefines() {
         if (systemIncludeDirectoriesList != null)
@@ -127,7 +122,7 @@ public abstract class GNUCCCCompiler extends CCCCompiler {
     
     protected abstract String getDefaultPath();
     protected abstract String getCompilerStderrCommand();
-    protected abstract String getCompilerStdoutCommand();
+    protected abstract String getCompilerStderrCommand2();
     
     private void getFreshSystemIncludesAndDefines() {
         systemIncludeDirectoriesList = new PersistentList();
@@ -138,11 +133,8 @@ public abstract class GNUCCCCompiler extends CCCCompiler {
         }
         try {
             getSystemIncludesAndDefines(path + getCompilerStderrCommand(), false);
-            getSystemIncludesAndDefines(path + getCompilerStdoutCommand(), true);
-            // a workaround for gcc bug - see http://gcc.gnu.org/ml/gcc-bugs/2006-01/msg00767.html
-            if (!containsMacro(systemPreprocessorSymbolsList, "__STDC__")) { // NOI18N
-                systemPreprocessorSymbolsList.add("__STDC__=1"); // NOI18N
-            }
+            if (getCompilerStderrCommand2() != null)
+                getSystemIncludesAndDefines(path + getCompilerStderrCommand2(), false);
             saveSystemIncludesAndDefines();
         } catch (IOException ioe) {
             System.err.println("IOException " + ioe);
@@ -156,78 +148,5 @@ public abstract class GNUCCCCompiler extends CCCCompiler {
         getFreshSystemIncludesAndDefines();
     }
     
-    private boolean startsWithPath(String line) {
-        line = line.trim();
-        if( line.startsWith("/") ) {  // NOI18N
-            return true;
-        }
-        else if( System.getProperty("os.name").indexOf("Windows") >= 0 ) {
-            if( Character.isLetter(line.charAt(0)) && line.charAt(1) == ':' ) {
-                return true;
-            }
-        }
-        return false;
-    }
-            
-    @Override
-    protected void parseCompilerOutput(InputStream is) {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        
-        try {
-            String line;
-            boolean startIncludes = false;
-            while ((line = reader.readLine()) != null) {
-                //System.out.println(line);
-                if (line.contains("#include <...>")) { // NOI18N
-                    startIncludes = true;
-                    continue;
-                }
-		if (startIncludes) {
-                    if (line.startsWith("End of search") || ! startsWithPath(line)) { // NOI18N
-                        startIncludes = false;
-                        continue;
-                    }
-		}
-                if (startIncludes) {
-                    line = line.trim();
-                    if (getFlavor() == CompilerFlavor.MinGW) {
-                        if (line.toLowerCase().startsWith(getIncludeFilePathPrefix().toLowerCase())) {
-                            line = line.substring(getIncludeFilePathPrefix().length());
-                        }
-                        else if (line.toLowerCase().startsWith("/mingw")) { // NOI18N
-                            line = line.substring(6);
-                        }
-                    }
-                    systemIncludeDirectoriesList.addUnique(normalizePath(getIncludeFilePathPrefix() + line));
-                    if (getIncludeFilePathPrefix().length() > 0 && line.startsWith("/usr/lib")) // NOI18N
-                        systemIncludeDirectoriesList.addUnique(normalizePath(getIncludeFilePathPrefix() + line.substring(4)));
-                    continue;
-                }
-                parseUserMacros(line, systemPreprocessorSymbolsList);
-                if (line.startsWith("#define ")) { // NOI18N
-                    int i = line.indexOf(' ', 8);
-                    if (i > 0) {
-                        String token = line.substring(8, i) + "=" + line.substring(i+1); // NOI18N
-                        systemPreprocessorSymbolsList.add(token);
-                    }
-                }
-            }
-            is.close();
-            reader.close();
-        } catch (IOException ioe) {
-            ErrorManager.getDefault().notify(ErrorManager.WARNING, ioe); // FIXUP
-        }
-    }
-    
-    
-    private void dumpLists() {
-        System.out.println("==================================" + getDisplayName()); // NOI18N
-        for (int i = 0; i < systemIncludeDirectoriesList.size(); i++) {
-            System.out.println("-I" + systemIncludeDirectoriesList.get(i)); // NOI18N
-        }
-        for (int i = 0; i < systemPreprocessorSymbolsList.size(); i++) {
-            System.out.println("-D" + systemPreprocessorSymbolsList.get(i)); // NOI18N
-        }
-    }
     
 }
