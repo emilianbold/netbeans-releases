a3c6c7b8210bcfc880a1fb93d34a3bd365473d47
Kirill Sorokin <ksorokin@netbeans.org>
2008-02-21 14:34:49 +0300

    Moved ActionType and andtracted getType from BpelNodeAction to the API level.

--- bpel.editors.api/src/org/netbeans/modules/bpel/editors/api/nodes/actions/ActionType.java
+++ bpel.editors.api/src/org/netbeans/modules/bpel/editors/api/nodes/actions/ActionType.java
@@ -16,7 +16,7 @@
  * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
-package org.netbeans.modules.bpel.nodes.actions;
+package org.netbeans.modules.bpel.editors.api.nodes.actions;
 
 /**
  *
