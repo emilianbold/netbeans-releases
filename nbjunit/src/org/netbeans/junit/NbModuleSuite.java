1e0c11fb27d57826704bca4d79f4d4af612a5174
Jaroslav Tulach <jtulach@netbeans.org>
2008-02-26 08:12:48 +0100

    Remove imports

--- nbjunit/src/org/netbeans/junit/NbModuleSuite.java
+++ nbjunit/src/org/netbeans/junit/NbModuleSuite.java
@@ -45,7 +45,6 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Method;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
