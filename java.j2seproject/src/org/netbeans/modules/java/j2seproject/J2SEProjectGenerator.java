b5e1d98b7e229330cdf31ccf12dd4e9b67f5a452
Milos Kleint <mkleint@netbeans.org>
2008-02-29 16:53:53 +0100

    add note

--- java.j2seproject/src/org/netbeans/modules/java/j2seproject/J2SEProjectGenerator.java
+++ java.j2seproject/src/org/netbeans/modules/java/j2seproject/J2SEProjectGenerator.java
@@ -323,6 +323,7 @@ public class J2SEProjectGenerator {
         if (!h.isSharableProject()) {
             return; 
         }
+        //TODO: mkleint: this hardwiring of libraries is potencially source of problems
         if (rh.getProjectLibraryManager().getLibrary("junit") == null) {
             rh.copyLibrary(LibraryManager.getDefault().getLibrary("junit")); // NOI18N
         }
