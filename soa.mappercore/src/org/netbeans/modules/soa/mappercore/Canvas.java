bb30f5da8465e7d01f26447fdcb2c071c1f79ebe
AlexanderPermyacov <alex02@netbeans.org>
2008-02-29 17:55:50 +0300

    change color of border in bpel diagram

--- soa.mappercore/src/org/netbeans/modules/soa/mappercore/Canvas.java
+++ soa.mappercore/src/org/netbeans/modules/soa/mappercore/Canvas.java
@@ -205,21 +205,21 @@ public class Canvas extends MapperPanel implements VertexCanvas,
 
     @Override
     protected void printComponent(Graphics g) {
-        System.out.println(getWidth());
-        int w = getWidth();
-        int h = getHeight();
-        getViewport().setSize(getWidth(), getHeight());
-        Rectangle r = getViewport().getViewRect();
-        System.out.println(getWidth());
 //        System.out.println(getWidth());
-//        System.out.println(getHeight());
-        scrollRectToVisible(new Rectangle(0, 0, getWidth() - 1, getHeight() - 1));
-//        System.out.println(getViewport().getViewRect());
-//        Graphics g2 = g.create(r.x, r.y, r.width, r.height);
-//        setGraphViewPositionX(0);
-//                invalidate();
-//        getScrollPane().validate();
-  //      paintImmediately(r);
+//        int w = getWidth();
+//        int h = getHeight();
+//        getViewport().setSize(getWidth(), getHeight());
+//        Rectangle r = getViewport().getViewRect();
+//        System.out.println(getWidth());
+////        System.out.println(getWidth());
+////        System.out.println(getHeight());
+//        scrollRectToVisible(new Rectangle(0, 0, getWidth() - 1, getHeight() - 1));
+////        System.out.println(getViewport().getViewRect());
+////        Graphics g2 = g.create(r.x, r.y, r.width, r.height);
+////        setGraphViewPositionX(0);
+////                invalidate();
+////        getScrollPane().validate();
+//  //      paintImmediately(r);
         paintComponent(g);
  //       setSize(w, h);
         //getViewport().setSize(w - 100, h - 100);
