74b2e1d749d59f3f6074b68f76d441d984a9251f
Vadiraj Deshpande <vadirajvd@netbeans.org>
2008-03-04 16:36:45 +0530

    124994 - Added Hibernate Utility class creation wizard. The utility class can now be created in any java package with any given name. Currently there are two methods to get the session factory, one gets the session factory with the default (hibernate.cfg.xml) config file and another with the input config file.

---
+++ hibernate/src/org/netbeans/modules/hibernate/wizards/HibernateUtilWizard.java
@@ -0,0 +1,217 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * 
+ * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * 
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ * 
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ * 
+ * Contributor(s):
+ * 
+ * Portions Copyrighted 2008 Sun Microsystems, Inc.
+ */
+package org.netbeans.modules.hibernate.wizards;
+
+import java.io.IOException;
+
+import java.util.NoSuchElementException;
+import java.util.Set;
+import java.util.Collections;
+
+import javax.swing.JComponent;
+import java.awt.Component;
+import javax.swing.event.ChangeListener;
+import org.netbeans.api.project.Sources;
+
+
+import org.openide.WizardDescriptor;
+import org.openide.filesystems.FileObject;
+import org.openide.loaders.DataObject;
+import org.openide.loaders.DataFolder;
+import org.openide.util.NbBundle;
+import org.netbeans.api.project.Project;
+import org.netbeans.api.project.ProjectUtils;
+import org.netbeans.api.project.SourceGroup;
+import org.netbeans.spi.java.project.support.ui.templates.JavaTemplates;
+import org.netbeans.spi.project.ui.templates.support.Templates;
+
+/**
+ * This wizard generates the Utility class to get Hibernate Session Factory object
+ * easily. The name of the generated file is kept freeform.
+ * 
+ * @author Vadiraj Deshpande (Vadiraj.Deshpande@Sun.COM)
+ */
+public class HibernateUtilWizard implements WizardDescriptor.InstantiatingIterator {
+
+    private int index;
+    private Project project;
+    private WizardDescriptor wizard;
+    private transient WizardDescriptor.Panel[] panels;
+
+    /**
+     * Creates this wizard.
+     * @return the wizard.
+     */
+    public static HibernateUtilWizard create() {
+        return new HibernateUtilWizard();
+    }
+
+    /**
+     * Initialize panels representing individual wizard's steps and sets
+     * various properties for them influencing wizard appearance.
+     */
+    private WizardDescriptor.Panel[] getPanels() {
+        if (panels == null) {
+            Project p = Templates.getProject(wizard);
+            SourceGroup[] groups = ProjectUtils.getSources(p).getSourceGroups(Sources.TYPE_GENERIC);
+            WizardDescriptor.Panel targetChooser = JavaTemplates.createPackageChooser(p, groups);
+            panels = new WizardDescriptor.Panel[]{
+                targetChooser
+            };
+            String[] steps = createSteps();
+            for (int i = 0; i < panels.length; i++) {
+                Component c = panels[i].getComponent();
+                if (steps[i] == null) {
+                    // Default step name to component name of panel. Mainly
+                    // useful for getting the name of the target chooser to
+                    // appear in the list of steps.
+                    steps[i] = c.getName();
+                }
+                if (c instanceof JComponent) { // assume Swing components
+                    JComponent jc = (JComponent) c;
+                    // Sets step number of a component
+                    jc.putClientProperty("WizardPanel_contentSelectedIndex", new Integer(i)); // NOI18N
+                    // Sets steps names for a panel
+                    jc.putClientProperty("WizardPanel_contentData", steps); // NOI18N
+                    // Turn on subtitle creation on each step
+                    jc.putClientProperty("WizardPanel_autoWizardStyle", Boolean.TRUE); // NOI18N
+                    // Show steps on the left side with the image on the background
+                    jc.putClientProperty("WizardPanel_contentDisplayed", Boolean.TRUE); // NOI18N
+                    // Turn on numbering of all steps
+                    jc.putClientProperty("WizardPanel_contentNumbered", Boolean.TRUE); // NOI18N
+                }
+            }
+        }
+        return panels;
+    }
+
+    /**
+     * Initializes project and source roots.
+     * @param wizard the wizard descriptor.
+     */
+    public void initialize(WizardDescriptor wizard) {
+        this.wizard = wizard;
+        project = Templates.getProject(wizard);
+        FileObject sourceRoot = Util.getSourceRoot(project);        
+        Templates.setTargetFolder(wizard, sourceRoot);      
+    }
+
+    /**
+     * cleans up (does nothing at present)
+     * @param wizard
+     */
+    public void uninitialize(WizardDescriptor wizard) {
+        panels = null;
+    }
+
+    /**
+     * Creates a new Hibernate utility class on the disk.
+     * @return Set containing the newly created file.
+     * @throws java.io.IOException when there's problem duirng file creation.
+     */
+    public Set instantiate() throws IOException {
+        FileObject targetFolder = Templates.getTargetFolder(wizard);
+        DataFolder targetDataFolder = DataFolder.findFolder(targetFolder);
+        String targetName = Templates.getTargetName(wizard);
+        FileObject templateFileObject = Templates.getTemplate(wizard);
+        DataObject templateDataObject = DataObject.find(templateFileObject);
+
+        DataObject newOne = templateDataObject.createFromTemplate(targetDataFolder, targetName);
+
+        return Collections.singleton(newOne.getPrimaryFile());
+    }
+
+    public void previousPanel() {
+        if (!hasPrevious()) {
+            throw new NoSuchElementException();
+        }
+        index--;
+    }
+
+    public void nextPanel() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        index++;
+    }
+
+    public boolean hasPrevious() {
+        return index > 0;
+    }
+
+    public boolean hasNext() {
+        return index < getPanels().length - 1;
+    }
+
+    public String name() {
+        return NbBundle.getMessage(HibernateConfigurationWizard.class, "LBL_UtilWizardTitle");
+    }
+
+    public WizardDescriptor.Panel current() {
+        return getPanels()[index];
+    }
+    // If nothing unusual changes in the middle of the wizard, simply:
+    public final void addChangeListener(ChangeListener l) {
+    }
+
+    public final void removeChangeListener(ChangeListener l) {
+    }
+
+    private String[] createSteps() {
+        String[] beforeSteps = null;
+        Object prop = wizard.getProperty("WizardPanel_contentData"); // NOI18N
+        if (prop != null && prop instanceof String[]) {
+            beforeSteps = (String[]) prop;
+        }
+
+        if (beforeSteps == null) {
+            beforeSteps = new String[0];
+        }
+
+        String[] res = new String[(beforeSteps.length - 1) + panels.length];
+        for (int i = 0; i < res.length; i++) {
+            if (i < (beforeSteps.length - 1)) {
+                res[i] = beforeSteps[i];
+            } else {
+                res[i] = panels[i - beforeSteps.length + 1].getComponent().getName();
+            }
+        }
+        return res;
+    }
+}
