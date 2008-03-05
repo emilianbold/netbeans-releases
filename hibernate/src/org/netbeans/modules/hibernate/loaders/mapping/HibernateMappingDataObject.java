58ecda0d828346d32613a1f3f6033a948fa71dc2
Dongmei Cao <dongmeic@netbeans.org>
2008-03-05 11:37:28 -0800

    Modified not to extend from XmlMultiviewDataObject because we're not going to support Design tab for Hibernate mapping files

--- hibernate/src/org/netbeans/modules/hibernate/loaders/mapping/HibernateMappingDataObject.java
+++ hibernate/src/org/netbeans/modules/hibernate/loaders/mapping/HibernateMappingDataObject.java
@@ -40,59 +40,37 @@
  */
 package org.netbeans.modules.hibernate.loaders.mapping;
 
-import java.awt.Image;
 import java.io.IOException;
-import java.io.StringWriter;
-import java.io.Writer;
-import org.openide.ErrorManager;
+import java.io.OutputStream;
 import org.netbeans.api.xml.cookies.CheckXMLCookie;
 import org.netbeans.api.xml.cookies.ValidateXMLCookie;
-import org.netbeans.core.spi.multiview.MultiViewElement;
-import org.netbeans.modules.hibernate.loaders.HbXmlMultiViewEditorSupport;
-import org.netbeans.modules.hibernate.loaders.mapping.multiview.HibernateMappingToolBarMVElement;
 import org.netbeans.modules.hibernate.mapping.model.HibernateMapping;
 import org.netbeans.modules.hibernate.mapping.model.MyClass;
-import org.netbeans.modules.schema2beans.BaseBean;
-import org.netbeans.modules.schema2beans.Schema2BeansException;
-import org.netbeans.modules.xml.multiview.DesignMultiViewDesc;
-import org.netbeans.modules.xml.multiview.ToolBarMultiViewElement;
-import org.netbeans.modules.xml.multiview.XmlMultiViewDataObject;
-import org.netbeans.modules.xml.multiview.XmlMultiViewDataSynchronizer;
-import org.netbeans.modules.xml.multiview.XmlMultiViewEditorSupport;
 import org.netbeans.spi.xml.cookies.CheckXMLSupport;
 import org.netbeans.spi.xml.cookies.DataObjectAdapters;
 import org.netbeans.spi.xml.cookies.ValidateXMLSupport;
 import org.openide.cookies.EditorCookie;
+import org.openide.filesystems.FileAlreadyLockedException;
 import org.openide.filesystems.FileObject;
 import org.openide.loaders.DataObjectExistsException;
+import org.openide.loaders.MultiDataObject;
 import org.openide.nodes.CookieSet;
 import org.openide.nodes.Node;
 import org.openide.text.DataEditorSupport;
 import org.openide.util.Exceptions;
-import org.openide.util.HelpCtx;
-import org.openide.util.NbBundle;
-import org.openide.util.Utilities;
+import org.openide.util.Lookup;
 
 /**
+ * Represents the Hibernate mapping file
  * 
  * @author Dongmei Cao
  */
-public class HibernateMappingDataObject extends XmlMultiViewDataObject {
+public class HibernateMappingDataObject extends MultiDataObject {
 
-    private static final int TYPE_TOOLBAR = 0;
-    public static final int UPDATE_DELAY = 200;
-    private static final String DESIGN_VIEW_ID = "hibernate_mapping_multiview_design"; // NOI18N
     private HibernateMapping mapping;
-    private ModelSynchronizer modelSynchronizer;
 
     public HibernateMappingDataObject(FileObject pf, HibernateMappingDataLoader loader) throws DataObjectExistsException, IOException {
         super(pf, loader);
-
-        // Make sure to reset the MIME type here. See bug 127051
-        getEditorSupport().setMIMEType(HibernateMappingDataLoader.REQUIRED_MIME);
-
-        // Synchronize between the vew and XML file
-        modelSynchronizer = new ModelSynchronizer(this);
 
         CookieSet cookies = getCookieSet();
         cookies.add((Node.Cookie) DataEditorSupport.create(this, getPrimaryEntry(), cookies));
@@ -101,45 +79,6 @@ public class HibernateMappingDataObject extends XmlMultiViewDataObject {
         cookies.add(checkCookie);
         ValidateXMLCookie validateCookie = new ValidateXMLSupport(in);
         cookies.add(validateCookie);
-        parseDocument();
-    }
-
-    /**
-     * Parses the document.
-     * @return true if document could be parsed (it was valid), false otwherwise.
-     */
-    public boolean parseDocument() {
-        if (mapping == null) {
-            try {
-                mapping = getHibernateMapping();
-            } catch (RuntimeException ex) { // must catch RTE (thrown by schema2beans when document is not valid)
-                //ErrorManager.getDefault().notify(org.openide.ErrorManager.INFORMATIONAL, ex);
-                return false;
-            }
-        } else {
-            try {
-                java.io.InputStream is = getEditorSupport().getInputStream();
-                HibernateMapping newMapping = null;
-                try {
-                    newMapping = HibernateMapping.createGraph(is);
-                } catch (RuntimeException ex) { // must catch RTE (thrown by schema2beans when document is not valid)
-                    //ErrorManager.getDefault().notify(org.openide.ErrorManager.INFORMATIONAL, ex);
-                    return false;
-                }
-                if (newMapping != null) {
-                    try {
-                        mapping.merge(newMapping, BaseBean.MERGE_UPDATE);
-                    } catch (IllegalArgumentException iae) {
-                        //ErrorManager.getDefault().notify(org.openide.ErrorManager.INFORMATIONAL, iae);
-                        return false;
-                    }
-                }
-            } catch (IOException e) {
-                ErrorManager.getDefault().notify(org.openide.ErrorManager.INFORMATIONAL, e);
-                return false;
-            }
-        }
-        return true;
     }
 
     /**
@@ -147,8 +86,22 @@ public class HibernateMappingDataObject extends XmlMultiViewDataObject {
      * 
      */
     public void addMyClass(MyClass myClass) {
-        getHibernateMapping().addMyClass(myClass);
-        modelUpdatedFromUI();
+        OutputStream out = null;
+        try {
+            getHibernateMapping().addMyClass(myClass);
+            out = getPrimaryFile().getOutputStream();
+            getHibernateMapping().write(out);
+        } catch (FileAlreadyLockedException ex) {
+            Exceptions.printStackTrace(ex);
+        } catch (IOException ex) {
+            Exceptions.printStackTrace(ex);
+        } finally {
+            try {
+                out.close();
+            } catch (IOException ex) {
+                Exceptions.printStackTrace(ex);
+            }
+        }
     }
 
     /**
@@ -184,156 +137,14 @@ public class HibernateMappingDataObject extends XmlMultiViewDataObject {
             }
         }
     }
-    
-    /**
-     * Override this method to workaround issue 
-     * http://www.netbeans.org/issues/show_bug.cgi?id=128211
-     */
-    @Override
-    protected synchronized XmlMultiViewEditorSupport getEditorSupport() {
-        if(editorSupport == null) {
-            editorSupport = new HbXmlMultiViewEditorSupport(this);
-            editorSupport.getMultiViewDescriptions();
-        }
-        return editorSupport;
-    }
-
-    protected String getPrefixMark() {
-        return null;
-    }
-
-    public void modelUpdatedFromUI() {
-        setModified(true);
-        modelSynchronizer.requestUpdateData();
-    }
-
-    @Override
-    public boolean isDeleteAllowed() {
-        return true;
-    }
-
-    @Override
-    public boolean isCopyAllowed() {
-        return true;
-    }
-
-    @Override
-    public boolean isMoveAllowed() {
-        return true;
-    }
-
-    @Override
-    protected Image getXmlViewIcon() {
-        return Utilities.loadImage("org/netbeans/modules/hibernate/resources/hibernate-mapping.png");
-    }
-
-    /** 
-     * Enable to focus specific object in Multiview Editor
-     * The default implementation opens the XML View
-     */
-    @Override
-    public void showElement(Object element) {
-        Object target = null;
-        if (element instanceof MyClass) {
-            openView(0);
-            target = element;
-        }
-        if (target != null) {
-            final Object key = target;
-            org.netbeans.modules.xml.multiview.Utils.runInAwtDispatchThread(new Runnable() {
-
-                public void run() {
-                    getActiveMultiViewElement0().getSectionView().openPanel(key);
-                }
-            });
-        }
-    }
-
-    /** 
-     * Enable to get active MultiViewElement object
-     */
-    public ToolBarMultiViewElement getActiveMultiViewElement0() {
-        return (ToolBarMultiViewElement) super.getActiveMultiViewElement();
-    }
-
-    protected DesignMultiViewDesc[] getMultiViewDesc() {
-        // TODO: enable this later when the Design tab is implemented
-        //return new DesignMultiViewDesc[]{new DesignView(this, TYPE_TOOLBAR)};
-        return new DesignMultiViewDesc[0];
-    }
 
     @Override
     protected Node createNodeDelegate() {
         return new HibernateMappingDataNode(this);
     }
-
-    private static class DesignView extends DesignMultiViewDesc {
-
-        private static final long serialVersionUID = 1L;
-        private int type;
-
-        DesignView(HibernateMappingDataObject dObj, int type) {
-            super(dObj, NbBundle.getMessage(HibernateMappingDataObject.class, "LBL_Design"));
-            this.type = type;
-        }
-
-        public MultiViewElement createElement() {
-            HibernateMappingDataObject dObj = (HibernateMappingDataObject) getDataObject();
-            return new HibernateMappingToolBarMVElement(dObj);
-        }
-
-        public Image getIcon() {
-            return Utilities.loadImage("org/netbeans/modules/hibernate/resources/hibernateMapping.gif");
-        }
-
-        public String preferredID() {
-            return DESIGN_VIEW_ID + String.valueOf(type);
-        }
-
-        @Override
-        public HelpCtx getHelpCtx() {
-            //return new HelpCtx(HELP_ID_DESIGN_HIBERNATE_MAPPING); //NOI18N
-            return null;
-        }
-    }
-
-    private class ModelSynchronizer extends XmlMultiViewDataSynchronizer {
-
-        public ModelSynchronizer(XmlMultiViewDataObject dataObject) {
-            super(dataObject, UPDATE_DELAY);
-        }
-
-        protected boolean mayUpdateData(boolean allowDialog) {
-            return true;
-        }
-
-        protected Object getModel() {
-            return getHibernateMapping();
-        }
-
-        protected void reloadModelFromData() {
-            parseDocument();
-        }
-
-        @Override
-        protected void updateDataFromModel(Object model, org.openide.filesystems.FileLock lock, boolean modify) {
-            if (model == null) {
-                return;
-            }
-            try {
-                Writer out = new StringWriter();
-                ((HibernateMapping) model).write(out);
-                out.close();
-                getDataCache().setData(lock, out.toString(), modify);
-            } catch (IOException e) {
-                ErrorManager.getDefault().notify(org.openide.ErrorManager.INFORMATIONAL, e);
-            } catch (Schema2BeansException e) {
-                ErrorManager.getDefault().notify(org.openide.ErrorManager.INFORMATIONAL, e);
-            } finally {
-                if (lock != null) {
-                    lock.releaseLock();
-                }
-            }
-        }
+    
+    @Override
+    public Lookup getLookup() {
+        return getCookieSet().getLookup();
     }
 }
