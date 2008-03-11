3d43bac4db369b1aa0ac7b79b298c598590504b9
Tor Norbye <tor@netbeans.org>
2008-03-10 17:25:04 -0700

    Fix 129036

--- gsf/src/org/netbeans/modules/gsf/ProjectClassPathProvider.java
+++ gsf/src/org/netbeans/modules/gsf/ProjectClassPathProvider.java
@@ -39,7 +39,7 @@
  * made subject to such option by the copyright holder.
  */
 
-package org.netbeans.modules.ruby.rubyproject;
+package org.netbeans.modules.gsf;
 
 import org.netbeans.modules.gsfpath.api.classpath.ClassPath;
 import org.netbeans.api.project.FileOwnerQuery;
