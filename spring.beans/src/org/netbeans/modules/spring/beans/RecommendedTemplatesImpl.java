814526764cc03b2d14a61d658435e2f15af3acca
Andrei Badea <abadea@netbeans.org>
2008-03-13 17:23:37 +0100

    #130020: NullPointerException at org.netbeans.modules.spring.beans.wizards.NewSpringXMLConfigWizardIterator.getConfigFileManager

--- spring.beans/src/org/netbeans/modules/spring/beans/RecommendedTemplatesImpl.java
+++ spring.beans/src/org/netbeans/modules/spring/beans/RecommendedTemplatesImpl.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 2008 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -21,12 +21,6 @@
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
  *
- * Contributor(s):
- *
- * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
- * Microsystems, Inc. All Rights Reserved.
- *
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -37,42 +31,37 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
+ *
+ * Contributor(s):
+ *
+ * Portions Copyrighted 2008 Sun Microsystems, Inc.
  */
 
 package org.netbeans.modules.spring.beans;
 
-import org.netbeans.api.project.Project;
-import org.netbeans.spi.project.LookupProvider;
-import org.openide.util.Lookup;
-import org.openide.util.lookup.Lookups;
+import org.netbeans.spi.project.ui.RecommendedTemplates;
 
 /**
  *
  * @author Andrei Badea
  */
-public class ProjectLookupProvider implements LookupProvider {
+public class RecommendedTemplatesImpl implements RecommendedTemplates {
+
+    private static final String[] SPRING_TYPES = new String[] {
+        "spring-types" // NOI18N
+    };
+
+    private static final String[] SPRING_WEB_TYPES = new String[] {
+        "spring-web-types" // NOI18N
+    };
 
     private final boolean web;
 
-    public static ProjectLookupProvider createNonWeb() {
-        return new ProjectLookupProvider(false);
-    }
-
-    public static ProjectLookupProvider createWeb() {
-        return new ProjectLookupProvider(true);
-    }
-
-    public ProjectLookupProvider(boolean web) {
+    public RecommendedTemplatesImpl(boolean web) {
         this.web = web;
     }
 
-    public Lookup createAdditionalLookup(Lookup baseContext) {
-        Project project = baseContext.lookup(Project.class);
-        if (project == null) {
-            throw new IllegalStateException("Lookup " + baseContext + " does not contain a Project");
-        }
-        return Lookups.fixed(
-                new ProjectSpringScopeProvider(project),
-                new RecommendedTemplatesImpl(web));
+    public String[] getRecommendedTypes() {
+        return web ? SPRING_WEB_TYPES : SPRING_TYPES;
     }
 }
