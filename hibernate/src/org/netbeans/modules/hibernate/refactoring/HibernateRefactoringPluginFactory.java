fa843e1c608df75b90e95096423b216cdf89ba02
Dongmei Cao <dongmeic@netbeans.org>
2008-03-14 12:23:29 -0700

    Initial classes for supporting refactoring - rename and move

---
+++ hibernate/src/org/netbeans/modules/hibernate/refactoring/HibernateRefactoringPluginFactory.java
@@ -0,0 +1,70 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ *
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ */
+
+package org.netbeans.modules.hibernate.refactoring;
+
+import org.netbeans.modules.refactoring.api.AbstractRefactoring;
+import org.netbeans.modules.refactoring.api.MoveRefactoring;
+import org.netbeans.modules.refactoring.api.RenameRefactoring;
+import org.netbeans.modules.refactoring.api.WhereUsedQuery;
+import org.netbeans.modules.refactoring.spi.RefactoringPlugin;
+import org.netbeans.modules.refactoring.spi.RefactoringPluginFactory;
+
+/**
+ * Refactoring plugin factory for refactoring Hibernate mapping files
+ * 
+ * @author Dongmei Cao
+ */
+public class HibernateRefactoringPluginFactory implements RefactoringPluginFactory {
+
+    public RefactoringPlugin createInstance(AbstractRefactoring refactoring) {
+        
+        if (refactoring instanceof WhereUsedQuery) {
+            return null;//new HibernateFindUsagesPlugin((WhereUsedQuery) refactoring);
+        } else if (refactoring instanceof RenameRefactoring) {
+            return new HibernateRenamePlugin((RenameRefactoring)refactoring);
+        } else if (refactoring instanceof MoveRefactoring) {
+            return new HibernateMovePlugin((MoveRefactoring)refactoring);
+        } 
+        
+        return null;
+    }
+}
---
+++ hibernate/src/org/netbeans/modules/hibernate/refactoring/JavaPackageRenameTransaction.java
@@ -0,0 +1,110 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * 
+ * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * 
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ * 
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ * 
+ * Contributor(s):
+ * 
+ * Portions Copyrighted 2008 Sun Microsystems, Inc.
+ */
+
+package org.netbeans.modules.hibernate.refactoring;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Set;
+import org.netbeans.modules.hibernate.mapping.model.HibernateMapping;
+import org.netbeans.modules.hibernate.mapping.model.MyClass;
+import org.openide.ErrorManager;
+import org.openide.filesystems.FileAlreadyLockedException;
+import org.openide.filesystems.FileObject;
+
+/**
+ *
+ * @author Dongmei Cao
+ */
+public class JavaPackageRenameTransaction extends RenameTransaction {
+    
+    public JavaPackageRenameTransaction(Set<FileObject> files, String oldName, String newName) {
+        super(files, oldName, newName);
+    }
+
+    /**
+     * Do the actual changes
+     * 
+     */
+    public void doChanges() {
+        
+        String oldName = getOldName();
+        String newName = getNewName();
+        
+        for (FileObject mappingFileObject : getToBeModifiedFiles()) {
+            
+            OutputStream outs = null;
+            try {
+                InputStream is = mappingFileObject.getInputStream();
+                HibernateMapping hbMapping = HibernateMapping.createGraph(is);
+
+                // Change the package attribute <hibernate-mapping> tag
+                String pkgName = hbMapping.getAttributeValue("package"); //NOI18N
+                if (pkgName.contains(oldName)) {
+                    String newWholePkgName = pkgName.replaceFirst(oldName, newName);
+                    hbMapping.setAttributeValue("package", newWholePkgName);
+                }
+
+                // Change the name attribute <class> tag
+                MyClass[] myClazz = hbMapping.getMyClass();
+                for (int ci = 0; ci < myClazz.length; ci++) {
+                    String clsName = myClazz[ci].getAttributeValue("name"); // NOI18N
+                    if (clsName.startsWith(oldName)) {
+                        String nameBinaryClassName = clsName.replaceFirst(oldName, newName);
+                        myClazz[ci].setAttributeValue("name", nameBinaryClassName); // NOI18N
+                    }
+                }
+                outs = mappingFileObject.getOutputStream();
+                hbMapping.write(outs);
+                
+            } catch (FileAlreadyLockedException ex) {
+                ErrorManager.getDefault().notify(org.openide.ErrorManager.INFORMATIONAL, ex);
+            } catch (IOException ex) {
+                ErrorManager.getDefault().notify(org.openide.ErrorManager.INFORMATIONAL, ex);
+            } finally {
+                try {
+                    outs.close();
+                } catch (IOException ex) {
+                    ErrorManager.getDefault().notify(org.openide.ErrorManager.INFORMATIONAL, ex);
+                }
+            }
+        }
+    }
+}
