835d880589eb279a0d3de344a4663c084792ceb4
Nick N. Krasilnikov <nnnnnk@netbeans.org>
2008-03-17 20:38:39 +0300

    tests for IZ 103901 : Wrong code completion listbox for C++ templates

--- cnd.completion/test/unit/src/org/netbeans/modules/cnd/completion/TemplateCompletionTestCase.java
+++ cnd.completion/test/unit/src/org/netbeans/modules/cnd/completion/TemplateCompletionTestCase.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- *
+ * 
  * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
- *
+ * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
  * Development and Distribution License("CDDL") (collectively, the
@@ -20,13 +20,7 @@
  * License Header, with the fields enclosed by brackets [] replaced by
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- *
- * Contributor(s):
- *
- * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
- * Microsystems, Inc. All Rights Reserved.
- *
+ * 
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -37,39 +31,59 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
+ * 
+ * Contributor(s):
+ * 
+ * Portions Copyrighted 2008 Sun Microsystems, Inc.
  */
 
 package org.netbeans.modules.cnd.completion;
 
-import junit.framework.Test;
-import junit.framework.TestSuite;
-import org.netbeans.modules.cnd.test.BaseTestSuite;
+import org.netbeans.modules.cnd.completion.cplusplus.ext.CompletionBaseTestCase;
 
 /**
  *
- * @author Vladimir Voskresensky
+ * @author Nick Krasilnikov
  */
-public class CCCompletionTest extends BaseTestSuite {
+public class TemplateCompletionTestCase extends CompletionBaseTestCase  {
+
+    /**
+     * Creates a new instance of TemplateCompletionTestCase
+     */
+    public TemplateCompletionTestCase(String testName) {
+        super(testName, true);
+    }
     
-    public CCCompletionTest() {
-        super("C/C++ Completion");
-        
-        this.addTestSuite(CheckModelTestCase.class);
-        this.addTestSuite(CCBasicCompletionTestCase.class);
-        this.addTestSuite(ClassContentTestCase.class);
-        this.addTestSuite(TemplateSpecializationTestCase.class);
-        this.addTestSuite(NamespacesTestCase.class);
-        this.addTestSuite(InheritanceTestCase.class);
-        this.addTestSuite(StaticMembersTestCase.class);
-        this.addTestSuite(FriendTestCase.class);
-        this.addTestSuite(EnumTestCase.class);
-        this.addTestSuite(LibrariesContentTestCase.class);
-        this.addTestSuite(TypedefTestCase.class);
-        this.addTestSuite(TemplateCompletionTestCase.class);
+    public void testTemplates1() throws Exception {
+        super.performTest("template.cc", 37, 5);
+    }
+    
+    public void testTemplates2() throws Exception {
+        super.performTest("template.cc", 37, 5, "t1.");
     }
 
-    public static Test suite() {
-        TestSuite suite = new CCCompletionTest();
-        return suite;
+    public void testTemplates3() throws Exception {
+        super.performTest("template.cc", 37, 5, "T1<1>::");
+    }
+
+    public void testTemplates4() throws Exception {
+        super.performTest("template.cc", 37, 5, "T2<T1<1>>::");
+    }
+
+    public void testTemplates5() throws Exception {
+        super.performTest("template.cc", 37, 5, "T2< T1<1> >::");
+    }
+
+    public void testTemplates6() throws Exception {
+        super.performTest("template.cc", 37, 5, "T2<T1<1>::> t2;",-5);
+    }
+
+    public void testTemplates7() throws Exception {
+        super.performTest("template.cc", 37, 5, "T3<1, T1<1>>::");
+    }
+
+    public void testTemplates8() throws Exception {
+        super.performTest("template.cc", 37, 5, "T3<1, int>::");
     }
 }
+
