b031443e8868eba9d8259e7ede95251a6cacd6d7
Jiri Rechtacek <jrechtacek@netbeans.org>
2008-03-19 20:09:10 +0100

    #130529: IllegalArgumentException when deactivating plugins

--- autoupdate.ui/src/org/netbeans/modules/autoupdate/ui/InstalledTableModel.java
+++ autoupdate.ui/src/org/netbeans/modules/autoupdate/ui/InstalledTableModel.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -24,7 +24,7 @@
  * Contributor(s):
  *
  * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2008 Sun
  * Microsystems, Inc. All Rights Reserved.
  *
  * If you wish your version of this file to be governed by only the CDDL
@@ -129,13 +129,15 @@ public class InstalledTableModel extends UnitCategoryTableModel {
         Unit.Installed u = (Unit.Installed) getUnitAtRow(row);
         assert anValue instanceof Boolean : anValue + " must be instanceof Boolean.";
         boolean beforeMarked = u.isMarked();
-        u.setMarked(! beforeMarked);
-        if (u.isMarked() != beforeMarked) {
-            fireButtonsChange();
-        } else {
-            //TODO: message should contain spec.version
-            String message = getBundle ("NotificationAlreadyPreparedToIntsall", u.getDisplayName ()); // NOI18N
-            DialogDisplayer.getDefault().notify(new NotifyDescriptor.Message(message));
+        if ((Boolean) anValue != beforeMarked) {
+            u.setMarked(! beforeMarked);
+            if (u.isMarked() != beforeMarked) {
+                fireButtonsChange();
+            } else {
+                //TODO: message should contain spec.version
+                String message = getBundle ("NotificationAlreadyPreparedToIntsall", u.getDisplayName ()); // NOI18N
+                DialogDisplayer.getDefault().notify(new NotifyDescriptor.Message(message));
+            }
         }
         
     }
