fbe8fd794801e7296f730cc08e44ec0df2b88b95
Jan Lahoda <jlahoda@netbeans.org>
2008-03-20 17:11:00 +0100

    #103066(partial fix): do not qualify innerclasses.

--- java.source/test/unit/src/org/netbeans/api/java/source/gen/ImportAnalysisTest.java
+++ java.source/test/unit/src/org/netbeans/api/java/source/gen/ImportAnalysisTest.java
@@ -116,6 +116,7 @@ public class ImportAnalysisTest extends GeneratorTest {
         suite.addTest(new ImportAnalysisTest("testImportClashWithJavaLang"));
         suite.addTest(new ImportAnalysisTest("testImportNoClashJavaLang"));
         suite.addTest(new ImportAnalysisTest("testImportNoClashCurrentPackage127486"));
+        suite.addTest(new ImportAnalysisTest("test130479"));
         return suite;
     }
 
@@ -665,6 +666,7 @@ public class ImportAnalysisTest extends GeneratorTest {
                     stats.add(st);
                 }
                 TypeElement list = workingCopy.getElements().getTypeElement("org.netbeans.test.codegen.ImportsTest6");
+                assertNotNull(list);
                 Types types = workingCopy.getTypes();
                 TypeMirror tm = types.getArrayType(types.erasure(list.asType()));
                 stats.add(make.Variable(make.Modifiers(Collections.<Modifier>emptySet()), "s", make.Type(tm), null));
@@ -919,6 +921,31 @@ public class ImportAnalysisTest extends GeneratorTest {
         };
         src.runModificationTask(task).commit();
         assertFiles("testImportNoClashCurrentPackage127486.pass");
+    }
+    
+    public void test130479() throws IOException {
+        testFile = getFile(getSourceDir(), getSourcePckg() + "ImportsTest130479.java");
+        JavaSource src = getJavaSource(testFile);
+        Task<WorkingCopy> task = new Task<WorkingCopy>() {
+
+            public void run(WorkingCopy workingCopy) throws IOException {
+                workingCopy.toPhase(Phase.RESOLVED);
+                CompilationUnitTree cut = workingCopy.getCompilationUnit();
+                TreeMaker make = workingCopy.getTreeMaker();
+                ClassTree clazz = (ClassTree) cut.getTypeDecls().get(0);
+                MethodTree method = (MethodTree) clazz.getMembers().get(2);
+                BlockTree block = method.getBody();
+                final TypeElement foo = workingCopy.getElements().getTypeElement("org.netbeans.test.codegen.ImportsTest130479.Foo");
+                final TypeElement clazzType = workingCopy.getElements().getTypeElement("java.lang.Class");
+                TypeMirror tm = workingCopy.getTypes().getDeclaredType(clazzType, workingCopy.getTypes().erasure(foo.asType()));
+                assertNotNull(foo);
+                Tree type = make.Type(tm);
+                VariableTree vt = make.Variable(make.Modifiers(EnumSet.noneOf(Modifier.class)), "test", type, null);
+                workingCopy.rewrite(block, make.addBlockStatement(block, vt));
+            }
+        };
+        src.runModificationTask(task).commit();
+        assertFiles("testImports130479.pass");
     }
     
     String getSourcePckg() {
