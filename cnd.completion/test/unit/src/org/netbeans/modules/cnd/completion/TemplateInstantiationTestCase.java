d106bf9af31f80118e17af6a6c82989d6e6a9425
Vladimir Voskresensky <vv159170@netbeans.org>
2008-03-21 17:54:48 +0300

    tests for IZ#103462: 'first' and 'second' are missed in Code Completion listbox

--- cnd.completion/test/unit/src/org/netbeans/modules/cnd/completion/TemplateInstantiationTestCase.java
+++ cnd.completion/test/unit/src/org/netbeans/modules/cnd/completion/TemplateInstantiationTestCase.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
- *
+ * 
+ * Copyright 2008 Sun Microsystems, Inc. All rights reserved.
+ * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
  * Development and Distribution License("CDDL") (collectively, the
@@ -20,9 +20,20 @@
  * License Header, with the fields enclosed by brackets [] replaced by
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- *
+ * 
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ * 
  * Contributor(s):
- *
+ * 
  * The Original Software is NetBeans. The Initial Developer of the Original
  * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
  * Microsystems, Inc. All Rights Reserved.
@@ -41,36 +52,50 @@
 
 package org.netbeans.modules.cnd.completion;
 
-import junit.framework.Test;
-import junit.framework.TestSuite;
-import org.netbeans.modules.cnd.test.BaseTestSuite;
+import org.netbeans.modules.cnd.completion.cplusplus.ext.CompletionBaseTestCase;
 
 /**
  *
  * @author Vladimir Voskresensky
  */
-public class CCCompletionTest extends BaseTestSuite {
+public class TemplateInstantiationTestCase extends CompletionBaseTestCase {
     
-    public CCCompletionTest() {
-        super("C/C++ Completion");
-        
-        this.addTestSuite(CheckModelTestCase.class);
-        this.addTestSuite(CCBasicCompletionTestCase.class);
-        this.addTestSuite(ClassContentTestCase.class);
-        this.addTestSuite(TemplateSpecializationTestCase.class);
-        this.addTestSuite(NamespacesTestCase.class);
-        this.addTestSuite(InheritanceTestCase.class);
-        this.addTestSuite(StaticMembersTestCase.class);
-        this.addTestSuite(FriendTestCase.class);
-        this.addTestSuite(EnumTestCase.class);
-        this.addTestSuite(LibrariesContentTestCase.class);
-        this.addTestSuite(TypedefTestCase.class);
-        this.addTestSuite(TemplateCompletionTestCase.class);
-        this.addTestSuite(TemplateInstantiationTestCase.class);
+    public TemplateInstantiationTestCase(String testName) {
+        super(testName, false);
+    }
+    
+    public void test1() throws Exception {
+        super.performTest("instantiation.cc", 8, 12);
     }
 
-    public static Test suite() {
-        TestSuite suite = new CCCompletionTest();
-        return suite;
+    public void test2() throws Exception {
+        super.performTest("instantiation.cc", 9, 11);
     }
+
+    public void test3() throws Exception {
+        super.performTest("instantiation.cc", 10, 15);
+    }
+
+    public void test4() throws Exception {
+        super.performTest("instantiation.cc", 11, 16);
+    }
+
+    public void testFoo1() throws Exception {
+        super.performTest("instantiation.cc", 8, 21);
+        super.performTest("instantiation.cc", 10, 24);
+    }
+
+    public void testFoo2() throws Exception {
+        // when fixed, move to boo1 test for speed up
+        super.performTest("instantiation.cc", 11, 34);
+    }
+
+    public void testBoo1() throws Exception {
+        super.performTest("instantiation.cc", 9, 20);
+    }
+    
+    public void testBoo2() throws Exception {
+        // when fixed, move to boo1 test for speed up
+        super.performTest("instantiation.cc", 12, 36);
+    }    
 }
