85fa5e284e92d19a88a62677d82e03da4adc3152
Egor Ushakov <gorrus@netbeans.org>
2008-04-10 20:44:25 +0400

    post fix for IZ:132427 (NPE in 'Disassembly' tab)

--- cnd.debugger.gdb/src/org/netbeans/modules/cnd/debugger/gdb/disassembly/Disassembly.java
+++ cnd.debugger.gdb/src/org/netbeans/modules/cnd/debugger/gdb/disassembly/Disassembly.java
@@ -54,6 +54,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.logging.Logger;
 import javax.swing.SwingUtilities;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
@@ -111,6 +112,8 @@ public class Disassembly implements PropertyChangeListener, DocumentListener {
     private static final String COMBINED_HEADER="src_and_asm_line={"; // NOI18N
     
     private static FileObject fo = null;
+    
+    private static Logger LOG = Logger.getLogger("gdb.logger"); // NOI18N
     
     public Disassembly(GdbDebugger debugger) {
         this.debugger = debugger;
@@ -255,7 +258,7 @@ public class Disassembly implements PropertyChangeListener, DocumentListener {
         for (Integer idx : regValues.keySet()) {
             String name = regNames.get(idx);
             if (name == null) {
-                assert false : "Unknown register: " + idx; // NOI18N
+                LOG.severe("Unknown register: " + idx); // NOI18N
                 name = String.valueOf(idx);
             }
             res.add(new RegisterValue(name, regValues.get(idx), regModified.contains(idx)));
