5c7a7f71be7a942f7918fc36830ab10212997b73
Erno Mononen <emononen@netbeans.org>
2008-04-11 15:32:59 +0200

    Trivial, removing unused imports

--- ruby.railsprojects/src/org/netbeans/modules/ruby/railsprojects/MigrateAction.java
+++ ruby.railsprojects/src/org/netbeans/modules/ruby/railsprojects/MigrateAction.java
@@ -42,8 +42,6 @@ package org.netbeans.modules.ruby.railsprojects;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
