feee4a37de295c122b82f63c94a6fc5072871bcd
Alexander Kouznetsov <mrkam@netbeans.org>
2008-04-14 22:51:11 +0400

    Fixed New File wizard dialog name

--- jellytools/src/org/netbeans/jellytools/NewFileWizardOperator.java
+++ jellytools/src/org/netbeans/jellytools/NewFileWizardOperator.java
@@ -80,7 +80,7 @@ public class NewFileWizardOperator extends WizardOperator {
     /** Creates new NewFileWizardOperator that can handle it.
      */
     public NewFileWizardOperator() {
-        super(Bundle.getString("org.netbeans.modules.project.ui.Bundle", "LBL_NewFileWizard_Subtitle"));
+        super(Bundle.getStringTrimmed("org.netbeans.modules.project.ui.Bundle", "LBL_NewFileWizard_Title"));        
     }
 
     /** Waits for wizard with given title.
