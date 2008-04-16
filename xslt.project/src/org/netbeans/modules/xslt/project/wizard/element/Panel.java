9aca82d4ef1e76a61c0842456f0398206f5c0462
Vladimir Yaroslavskiy <yaroslavskiy@netbeans.org>
2008-04-16 16:50:23 +0400

    Remove unncessary soa.ui.util

--- xslt.project/src/org/netbeans/modules/xslt/project/wizard/element/Panel.java
+++ xslt.project/src/org/netbeans/modules/xslt/project/wizard/element/Panel.java
@@ -178,15 +178,15 @@ abstract class Panel<T> implements WizardDescriptor.ValidatingPanel<T> {
   }
   
   protected final String i18n(String key) {
-    return org.netbeans.modules.soa.ui.util.UI.i18n(Panel.class, key);
+    return org.netbeans.modules.soa.core.util.UI.i18n(Panel.class, key);
   }
 
   protected final String i18n(String key, String param) {
-    return org.netbeans.modules.soa.ui.util.UI.i18n(Panel.class, key, param);
+    return org.netbeans.modules.soa.core.util.UI.i18n(Panel.class, key, param);
   }
 
   protected final String i18n(String key, String param1, String param2) {
-    return org.netbeans.modules.soa.ui.util.UI.i18n(
+    return org.netbeans.modules.soa.core.util.UI.i18n(
       Panel.class, key, param1, param2);
   }
 
@@ -418,15 +418,15 @@ abstract class Panel<T> implements WizardDescriptor.ValidatingPanel<T> {
   protected static final String EMPTY = ""; // NOI18N
 
   protected static final String NAME_TYPE =
-    org.netbeans.modules.soa.ui.util.UI.i18n(Panel.class,
+    org.netbeans.modules.soa.core.util.UI.i18n(Panel.class,
     "LBL_Service_Type"); // NOI18N
 
   protected static final String NAME_WSDL =
-    org.netbeans.modules.soa.ui.util.UI.i18n(Panel.class,
+    org.netbeans.modules.soa.core.util.UI.i18n(Panel.class,
     "LBL_WSDL_File"); // NOI18N
 
   protected static final String NAME_XSLT =
-    org.netbeans.modules.soa.ui.util.UI.i18n(Panel.class,
+    org.netbeans.modules.soa.core.util.UI.i18n(Panel.class,
     "LBL_XSLT_Configuration"); // NOI18N
 
   public static final String INPUT_FILE = "input.file"; // NOI18N
