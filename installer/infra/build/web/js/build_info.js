d14504372fcb7312b49c32312fa58fc02397d953
Dmitry Lipin <dlipin@netbeans.org>
2008-04-17 20:30:26 +0400

    RC2->FCS branding

--- installer/infra/build/web/js/build_info.js
+++ installer/infra/build/web/js/build_info.js
@@ -46,8 +46,7 @@ var JDK_LATEST_UPDATE = "6";
 var LOAD_OMNITURE_CODE = 1;
 
 function add_download_tabs() {
-	add_download_tab("6.0.1", "http://download.netbeans.org/netbeans/6.0/final/");
-	add_download_tab("6.1 RC2");
+	add_download_tab("6.1");
 	add_download_tab(DEVELOPMENT_TITLE, DEVELOPMENT_BUILDS_LINK);
 	add_download_tab(ARCHIVE_TITLE,ARCHIVE_BUILDS_LINK);
 }
