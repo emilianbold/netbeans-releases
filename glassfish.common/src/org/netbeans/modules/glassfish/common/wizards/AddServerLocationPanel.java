f082d4b6d4a55b5676a4b4f7606ccf47573265b0
pcw <pcw@netbeans.org>
2008-04-24 12:24:54 -0700

    Fixed bug in port detection logic on server install.  last http port was being selected from domain.xml instead of first one.

--- glassfish.common/src/org/netbeans/modules/glassfish/common/wizards/AddServerLocationPanel.java
+++ glassfish.common/src/org/netbeans/modules/glassfish/common/wizards/AddServerLocationPanel.java
@@ -46,9 +46,12 @@ import org.openide.util.HelpCtx;
 import java.awt.Component;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.Level;
@@ -295,6 +298,8 @@ public class AddServerLocationPanel implements WizardDescriptor.Panel, ChangeLis
     private boolean readServerConfiguration(File domainDir) {
         boolean result = false;
         File domainXml = new File(domainDir, DOMAIN_XML_PATH);
+        final Map<String, HttpData> httpMap = new LinkedHashMap<String, HttpData>();
+        
         if (domainXml.exists()) {
             List<TreeParser.Path> pathList = new ArrayList<TreeParser.Path>();
             pathList.add(new TreeParser.Path("/domain/configs/config/http-service/http-listener",
@@ -308,17 +313,19 @@ public class AddServerLocationPanel implements WizardDescriptor.Panel, ChangeLis
                     //   server-name="" blocking-enabled="false" acceptor-threads="1">
                     try {
                         String id = attributes.getValue("id");
-                        int port = Integer.parseInt(attributes.getValue("port"));
-                        
-                        if("admin-listener".equals(id)) {
-                            adminPort = port;
-                        } else {
-                            String secure = attributes.getValue("security-enabled");
-                            if("true".equals(secure)) {
-                                httpsPort = port;
+                        if(id != null && id.length() > 0) {
+                            int port = Integer.parseInt(attributes.getValue("port"));
+                            boolean secure = "true".equals(attributes.getValue("security-enabled"));
+                            boolean enabled = "true".equals(attributes.getValue("enabled"));
+                            if(enabled) {
+                                HttpData data = new HttpData(id, port, secure);
+                                Logger.getLogger("glassfish").log(Level.FINE, " Adding " + data);
+                                httpMap.put(id, data);
                             } else {
-                                httpPort = port;
+                                Logger.getLogger("glassfish").log(Level.FINE, "http-listener " + id + " is not enabled and won't be used.");
                             }
+                        } else {
+                            Logger.getLogger("glassfish").log(Level.FINEST, "http-listener found with no name");
                         }
                     } catch(NumberFormatException ex) {
                         throw new SAXException(ex);
@@ -328,11 +335,76 @@ public class AddServerLocationPanel implements WizardDescriptor.Panel, ChangeLis
             
             try {
                 TreeParser.readXml(domainXml, pathList);
-                result = true;
+                
+                // !PW This probably more convoluted than it had to be, but while
+                // http-listeners are usually named "http-listener-1", "http-listener-2", ...
+                // technically they could be named anything.
+                // 
+                // For now, the logic is as follows:
+                //   admin port is the one named "admin-listener"
+                //   http port is the first non-secure enabled port - typically http-listener-1
+                //   https port is the first secure enabled port - typically http-listener-2
+                // disabled ports are ignored.
+                //
+                HttpData adminData = httpMap.remove("admin-listener");
+                adminPort = adminData != null ? adminData.getPort() : -1;
+                
+                HttpData httpData = null;
+                HttpData httpsData = null;
+                
+                for(HttpData data: httpMap.values()) {
+                    if(data.isSecure()) {
+                        if(httpsData == null) {
+                            httpsData = data;
+                        }
+                    } else {
+                        if(httpData == null) {
+                            httpData = data;
+                        }
+                    }
+                    if(httpData != null && httpsData != null) {
+                        break;
+                    }
+                }
+                
+                httpPort = httpData != null ? httpData.getPort() : -1;
+                httpsPort = httpsData != null ? httpsData.getPort() : -1;
+                result = httpPort != -1;
             } catch(IllegalStateException ex) {
                 Logger.getLogger("glassfish").log(Level.INFO, ex.getLocalizedMessage(), ex);
             }
         }
         return result;
+    }
+    
+    private static class HttpData {
+
+        private final String id;
+        private final int port;
+        private final boolean secure;
+        
+        public HttpData(String id, int port, boolean secure) {
+            this.id = id;
+            this.port = port;
+            this.secure = secure;
+        }
+        
+        public String getId() {
+            return id;
+        }
+
+        public int getPort() {
+            return port;
+        }
+
+        public boolean isSecure() {
+            return secure;
+        }
+        
+        @Override
+        public String toString() {
+            return "{ " + id + ", " + port + ", " + secure + " }";
+        }
+        
     }
 }
