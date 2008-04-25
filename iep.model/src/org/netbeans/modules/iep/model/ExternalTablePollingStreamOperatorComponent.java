032f439bb5c737605595bafb13a3b030507b6248
radval <radval@netbeans.org>
2008-04-25 11:50:55 -0700

    adding external table polling operator specific class.

---
+++ iep.model/src/org/netbeans/modules/iep/model/ExternalTablePollingStreamOperatorComponent.java
@@ -0,0 +1,33 @@
+package org.netbeans.modules.iep.model;
+
+public interface ExternalTablePollingStreamOperatorComponent extends OperatorComponent {
+
+	public static String PROP_POLLING_INTERVAL = "pollingInterval";
+	
+	public static String PROP_POLLING_INTERVAL_TIME_UNIT = "pollingIntervalTimeUnit";
+	
+	public static String PROP_POLLING_RECORD_SIZE = "pollingRecordSize";
+	
+	public static String PROP_DATABASE_JNDI_NAME = "databaseJndiName";
+	
+        public static String PROP_IS_DELETE_RECORDS = "isDeleteRecords";
+        
+	public void setPollingInterval(String pollingInterval);
+        
+    public String getPollingInterval();
+    
+    public void setPollingIntervalTimeUnit(String pollingIntervalTimeUnit);
+    
+    public String getPollingIntervalTimeUnit();
+    
+    public void setPolllingIntervalRecordSize(String recordSize);
+    
+    public void getPolllingIntervalRecordSize();
+    
+    public void setDatabaseJndiName(String databaseJndiName);
+    
+    public String getDatabaseJndiName();
+    
+    
+    
+}
