97f07592951a7a0bd274b42d75f73837d7efd410
pcw <pcw@netbeans.org>
2008-04-25 18:57:32 -0700

    Bias for display of multi-engine apps, in this order - ruby, web, ejb, unknown.

--- glassfish.common/src/org/netbeans/modules/glassfish/common/Commands.java
+++ glassfish.common/src/org/netbeans/modules/glassfish/common/Commands.java
@@ -41,8 +41,10 @@ package org.netbeans.modules.glassfish.common;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -161,7 +163,7 @@ public class Commands {
                     continue;
                 }
                 
-                String engine = getFirstEngine(appAttrs.getValue("nb-engine_value"));
+                String engine = getPreferredEngine(appAttrs.getValue("nb-engine_value"));
                 
                 String name = appAttrs.getValue("nb-name_value");
                 if(name == null || name.length() == 0) {
@@ -192,14 +194,31 @@ public class Commands {
 
         // XXX temporary patch to handle engine descriptions like <web, ejb>
         // until we have better display semantics for such things.
-        private String getFirstEngine(String engineList) {
+        // XXX bias order of list for JavaONE demos.
+        private static final List<String> engineBias = 
+                Arrays.asList(new String [] { "jruby", "web", "ejb" });
+        
+        private String getPreferredEngine(String engineList) {
             String [] engines = engineList.split(",");
-            for(String engine: engines) {
-                if(!skipContainer(engine)) {
-                    return engine;
+            String engine = null;
+            int bias = -1;
+            for(int i = 0; i < engines.length; i++) {
+                if(!skipContainer(engines[i])) {
+                    int newBias = engineBias.indexOf(engines[i]);
+                    if(newBias >= 0 && newBias < bias) {
+                        bias = newBias;
+                    }
+                    if(engine == null) {
+                        engine = engines[i];
+                    }
                 }
             }
-            return "unknown";
+            if(bias != -1) {
+                engine = engineBias.get(bias);
+            } else if(engine == null) {
+                engine = "unknown";
+            }
+            return engine;
         }
 
         /**
