61e81fdd9f9141e6af64e639b0b3a88af095772c
rnithya <rnithya@netbeans.org>
2008-04-28 00:57:47 +0530

    If the table is deleted from collab, the associated dataview should also be closeed.

--- etl.editor/src/org/netbeans/modules/sql/framework/ui/output/dataview/DataOutputPanel.java
+++ etl.editor/src/org/netbeans/modules/sql/framework/ui/output/dataview/DataOutputPanel.java
@@ -333,6 +333,10 @@ public abstract class DataOutputPanel extends JPanel implements ETLOutputPanel {
         queryView = new ResultSetTablePanel(this);
         this.add(queryView, BorderLayout.CENTER);
     }
+    
+    public SQLObject getTable(){
+        return table;
+    }
 
     public abstract void generateResult();
 
@@ -611,6 +615,9 @@ public abstract class DataOutputPanel extends JPanel implements ETLOutputPanel {
                 DialogDisplayer.getDefault().notify(new Message(errorMsg, NotifyDescriptor.INFORMATION_MESSAGE));
             }
             queryView.closeResources(pstmt, conn);
+            if(totalCount <= 0) {
+                totalCount = 1;
+            }
             refreshActionPerformed();
         }
 
