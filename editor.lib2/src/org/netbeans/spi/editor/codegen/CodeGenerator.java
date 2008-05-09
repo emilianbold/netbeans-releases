6748a98bd3fc04fae0050a4e82a286c19eb28ba7
Dusan Balek <dbalek@netbeans.org>
2008-05-09 10:34:08 +0200

    'Insert Code' SPI added - see issue #134239.

--- editor.lib2/src/org/netbeans/spi/editor/codegen/CodeGenerator.java
+++ editor.lib2/src/org/netbeans/spi/editor/codegen/CodeGenerator.java
@@ -39,23 +39,54 @@
  * made subject to such option by the copyright holder.
  */
 
-package org.netbeans.modules.java.editor.codegen;
+package org.netbeans.spi.editor.codegen;
 
-import com.sun.source.util.TreePath;
-import java.io.IOException;
-import javax.swing.text.JTextComponent;
-import org.netbeans.api.java.source.CompilationController;
+import java.util.List;
+import org.openide.util.Lookup;
 
 /**
+ * Interface to be implemented by all generators inserting their code snippets 
+ * into documents using the Insert Code editor action.
  *
  * @author Dusan Balek
+ * @since 1.8
  */
 public interface CodeGenerator {
 
+    /**
+     * Gets the generator's name to be displayed in the popup that appears on
+     * the Insert Code action invocation.
+     * @return non-null name
+     * @since 1.8
+     */
     public String getDisplayName();
-    public void invoke(JTextComponent component);
+
+    /**
+     * Invokes the generator to create the code snippet and insert it into a
+     * document.
+     * @since 1.8
+     */
+    public void invoke();
     
+    /**
+     * Factory creating code generators.<br/> The factory instances are looked up
+     * by the {@link org.netbeans.api.editor.mimelookup.MimeLookup} so they
+     * should be registered in an xml-layer in
+     * <i>Editors/&lt;mime-type&gt;/CodeGenerators</i> directory.
+     * @since 1.8
+     */
     public interface Factory {
-        public Iterable<? extends CodeGenerator> create(CompilationController controller, TreePath path) throws IOException;
+        
+        /**
+         * Creates code generators valid for the particular context.
+         * @param context Contains an instance of
+         * {@link javax.swing.text.JTextComponent} by default. Additonal content
+         * could be added by the {@link CodeGeneratorContextProvider} registered
+         * for the mime type.
+         * @return the list of created code generators. An empty list should be
+         * returned if no generator could be created in the particular context.
+         * @since 1.8
+         */
+        public List<? extends CodeGenerator> create(Lookup context);
     }
 }
