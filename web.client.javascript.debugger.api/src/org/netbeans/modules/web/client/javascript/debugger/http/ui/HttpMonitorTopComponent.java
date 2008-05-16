45601ae62375b64aa64b9b288cc6d5e09145d187
JoelleLam <JoelleLam@netbeans.org>
2008-05-16 14:43:33 -0700

    Adding httpmonitor client side.

--- web.client.javascript.debugger.api/src/org/netbeans/modules/web/client/javascript/debugger/http/ui/HttpMonitorTopComponent.java
+++ web.client.javascript.debugger.api/src/org/netbeans/modules/web/client/javascript/debugger/http/ui/HttpMonitorTopComponent.java
@@ -123,9 +123,6 @@ final class HttpMonitorTopComponent extends TopComponent {
     }
     
     
-
-    
-    
     /** This method is called from within the constructor to
      * initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is
