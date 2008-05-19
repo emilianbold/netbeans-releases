254cb8cb6cc0e766f5953a893cd5de4299854a8f
Vladimir Yaroslavskiy <yaroslavskiy@netbeans.org>
2008-05-19 17:23:18 +0400

    [Issue 135079] It fails to catch incorrect assignments and also time duration comparison

--- bpel.validation/src/org/netbeans/modules/bpel/validation/xpath/Validator.java
+++ bpel.validation/src/org/netbeans/modules/bpel/validation/xpath/Validator.java
@@ -127,14 +127,26 @@ public final class Validator extends BpelValidator implements ValidationVisitor 
 //out("  from based: " + ValidationUtil.getBasedSimpleType(fromType));
 //out("    to based: " + ValidationUtil.getBasedSimpleType(toType));
 
-    if (ValidationUtil.getBasedSimpleType(fromType) != ValidationUtil.getBasedSimpleType(toType)) {
-      addWarning("FIX_TYPE_IN_COPY", copy, getTypeName(fromType), getTypeName(toType)); // NOI18N
+    Component fType = ValidationUtil.getBasedSimpleType(fromType);
+    Component tType = ValidationUtil.getBasedSimpleType(toType);
+
+    if (fType == tType) {
+      return;
+    }
+    String fTypeName = getTypeName(fType);
+    String tTypeName = getTypeName(tType);
+    
+    if (fTypeName.equals("string") && tTypeName.equals("time")) { // NOI18N
+      // # 135079
+      addError("FIX_Time_in_copy", copy); // NOI18N
+    }
+    else {
+      addWarning("FIX_TYPE_IN_COPY", copy, fTypeName, tTypeName); // NOI18N
     }
   }
 
   @Override
-  public void visit(To to)
-  {
+  public void visit(To to) {
     // # 125525
     checkPartnerLink(to);
     // # 131658
