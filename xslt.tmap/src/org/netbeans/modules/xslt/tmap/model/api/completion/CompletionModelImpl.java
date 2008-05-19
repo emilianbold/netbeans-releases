2ef74024df55d843b58dc53adf80d702ccdaa3a4
lativ <lativ@netbeans.org>
2008-05-19 14:56:29 +0400

    fix 135303 - The transformmap source editor should provide schema autocompletion

--- xslt.tmap/src/org/netbeans/modules/xslt/tmap/model/api/completion/CompletionModelImpl.java
+++ xslt.tmap/src/org/netbeans/modules/xslt/tmap/model/api/completion/CompletionModelImpl.java
@@ -2,30 +2,50 @@
  * The contents of this file are subject to the terms of the Common Development
  * and Distribution License (the License). You may not use this file except in
  * compliance with the License.
- * 
+ *
  * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
  * or http://www.netbeans.org/cddl.txt.
- * 
+ *
  * When distributing Covered Code, include this CDDL Header Notice in each file
  * and include the License file at http://www.netbeans.org/cddl.txt.
  * If applicable, add the following below the CDDL Header, with the fields
  * enclosed by brackets [] replaced by your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- * 
+ *
  * The Original Software is NetBeans. The Initial Developer of the Original
  * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
-package org.netbeans.modules.xslt.tmap;
+package org.netbeans.modules.xslt.tmap.model.api.completion;
+
+import org.netbeans.modules.xml.schema.completion.spi.CompletionModelProvider.CompletionModel;
+import org.netbeans.modules.xml.schema.model.SchemaModel;
 
 /**
  *
  * @author Vitaly Bychkov
  * @version 1.0
  */
-public interface TMapConstants {
+public class CompletionModelImpl extends CompletionModel {
 
-    final String TRANSFORMMAP_XML = "transformmap.xml";// NOI18N
-    final String OLD_TRANSFORM_MAP_NS_URI = "http://xml.netbeans.org/schema/transformmap"; // NOI18N
+    private SchemaModel myModel;
     
+    public CompletionModelImpl(SchemaModel model) {
+        myModel = model;
+    }
+
+    @Override
+    public String getSuggestedPrefix() {
+        return "tp";
+    }
+
+    @Override
+    public String getTargetNamespace() {
+        return myModel == null ? null : myModel.getSchema().getTargetNamespace();
+    }
+
+    @Override
+    public SchemaModel getSchemaModel() {
+        return myModel;
+    }
 }
