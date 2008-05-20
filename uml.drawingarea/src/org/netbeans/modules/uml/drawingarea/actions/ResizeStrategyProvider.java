9f1758d38f1602f707c248cdbe3b9ad28574f283
sergeyp <sergeyp@netbeans.org>
2008-05-20 13:45:10 +0400

    fixed an issue also commit commented code for resize to content and different resizing by key modificators

--- uml.drawingarea/src/org/netbeans/modules/uml/drawingarea/actions/ResizeStrategyProvider.java
+++ uml.drawingarea/src/org/netbeans/modules/uml/drawingarea/actions/ResizeStrategyProvider.java
@@ -39,13 +39,21 @@
 
 package org.netbeans.modules.uml.drawingarea.actions;
 
+import java.awt.Rectangle;
 import org.netbeans.api.visual.action.ResizeProvider;
 import org.netbeans.api.visual.action.ResizeStrategy;
+import org.netbeans.api.visual.widget.Widget;
 
 /**
  *
  * @author sp153251
  */
 abstract public class ResizeStrategyProvider implements ResizeStrategy,ResizeProvider {
+
+    abstract Rectangle boundsSuggested(Widget widget, Rectangle originalSceneRectangle, Rectangle rectangle, ControlPoint controlPoint, int modifiersEx);
+
+    abstract void resizingFinished(Widget widget, int modifiersEx);
+
+    abstract void resizingStarted(Widget widget, int modifiersEx);
 
 }
