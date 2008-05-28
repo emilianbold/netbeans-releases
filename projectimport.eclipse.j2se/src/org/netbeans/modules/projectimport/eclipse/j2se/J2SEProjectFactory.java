70ca408a147e97397d4c39bacbc679efecc77a75
David Konecny <dkonecny@netbeans.org>
2008-05-28 15:40:37 +1200

    added j2se and web part of eclipse project import. web part is just sketch with lots of defatuls; j2se works as trunk version

---
+++ projectimport.eclipse.j2se/src/org/netbeans/modules/projectimport/eclipse/j2se/J2SEProjectFactory.java
@@ -0,0 +1,143 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * 
+ * Copyright 2008 Sun Microsystems, Inc. All rights reserved.
+ * 
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ * 
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ * 
+ * Contributor(s):
+ * 
+ * Portions Copyrighted 2008 Sun Microsystems, Inc.
+ */
+
+package org.netbeans.modules.projectimport.eclipse.j2se;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+import org.netbeans.api.project.FileOwnerQuery;
+import org.netbeans.api.project.Project;
+import org.netbeans.api.project.ProjectManager;
+import org.netbeans.modules.java.j2seproject.J2SEProject;
+import org.netbeans.modules.java.j2seproject.J2SEProjectGenerator;
+import org.netbeans.modules.java.j2seproject.J2SEProjectType;
+import org.netbeans.modules.java.j2seproject.ui.customizer.J2SEProjectProperties;
+import org.netbeans.modules.projectimport.eclipse.core.spi.ProjectFactorySupport;
+import org.netbeans.modules.projectimport.eclipse.core.spi.ProjectImportModel;
+import org.netbeans.modules.projectimport.eclipse.core.spi.ProjectTypeUpdater;
+import org.netbeans.spi.project.support.ant.AntProjectHelper;
+import org.netbeans.spi.project.support.ant.EditableProperties;
+import org.openide.filesystems.FileUtil;
+import org.w3c.dom.Element;
+
+/**
+ *
+ */
+public class J2SEProjectFactory implements ProjectTypeUpdater {
+
+    private static final String JAVA_NATURE = "org.eclipse.jdt.core.javanature"; // NOI18N
+
+    public J2SEProjectFactory() {
+    }
+    
+    public boolean canHandle(Set<String> natures) {
+        return natures.contains(JAVA_NATURE);
+    }
+
+    public Project createProject(final ProjectImportModel model, final List<String> importProblems) throws IOException {
+        // calculate nb project location
+        File nbProjectDir = FileUtil.normalizeFile(new File(model.getNetBeansProjectLocation() + "/" + model.getProjectName())); // NOI18N
+        
+        // create basic NB project
+        final AntProjectHelper helper = J2SEProjectGenerator.createProject(
+                nbProjectDir, model.getProjectName(), model.getEclipseSourceRootsAsFileArray(), 
+                model.getEclipseTestSourceRootsAsFileArray(), null, null, null);
+        
+        // get NB project
+        J2SEProject nbProject = (J2SEProject) ProjectManager.getDefault().
+                findProject(FileUtil.toFileObject(
+                FileUtil.normalizeFile(nbProjectDir)));
+        
+        // set labels for source roots
+        ProjectFactorySupport.updateSourceRootLabels(model.getEclipseSourceRoots(), nbProject.getSourceRoots());
+        ProjectFactorySupport.updateSourceRootLabels(model.getEclipseTestSourceRoots(), nbProject.getTestSourceRoots());
+        
+        // TODO: setup include/exclude here
+        
+        // Make sure PCPM knows who owns this (J2SEProject will do the same later on anyway):
+        if (!nbProjectDir.equals(model.getEclipseProjectFolder())) {
+            FileOwnerQuery.markExternalOwner(model.getEclipseProjectFolder().toURI(), nbProject, FileOwnerQuery.EXTERNAL_ALGORITHM_TRANSIENT);
+        }
+        
+        // update project classpath
+        ProjectFactorySupport.updateProjectClassPath(helper, model, importProblems);
+        
+        // set platform used by an Eclipse project
+        if (model.getJavaPlatform() != null) {
+            setExplicitJavaPlatform(helper, model);
+        }
+
+        // store data for project update:
+        ProjectFactorySupport.persistEclipseLink(nbProject, calculateKey(model), model);
+                
+        // save project
+        ProjectManager.getDefault().saveProject(nbProject);
+        return nbProject;
+    }
+
+    private void setExplicitJavaPlatform(final AntProjectHelper helper, final ProjectImportModel model) {
+        Element pcd = helper.getPrimaryConfigurationData(true);
+        Element el = pcd.getOwnerDocument().createElementNS(J2SEProjectType.PROJECT_CONFIGURATION_NAMESPACE, "explicit-platform"); // NOI18N
+        pcd.appendChild(el);
+        helper.putPrimaryConfigurationData(pcd, true);
+        EditableProperties prop = helper.getProperties(AntProjectHelper.PROJECT_PROPERTIES_PATH);
+        String ver = model.getJavaPlatform().getSpecification().getVersion().toString();
+        String normalizedName = (String) model.getJavaPlatform().getProperties().get(
+                    "platform.ant.name"); // NOI18N
+        prop.setProperty(J2SEProjectProperties.JAVAC_SOURCE, ver);
+        prop.setProperty(J2SEProjectProperties.JAVAC_TARGET, ver);
+        prop.setProperty(J2SEProjectProperties.JAVA_PLATFORM, normalizedName);
+        helper.putProperties(AntProjectHelper.PROJECT_PROPERTIES_PATH, prop);
+    }
+
+    public String calculateKey(ProjectImportModel model) {
+        return ProjectFactorySupport.calculateKey(model);
+    }
+
+    public void update(Project project, ProjectImportModel model, String oldKey) throws IOException {
+        String newKey = calculateKey(model);
+        
+        // compare newKey with old one and perform NB project update and optionally eclipse project update
+        
+        ProjectFactorySupport.persistEclipseLink(project, newKey, model);
+    }
+    
+}
---
+++ projectimport.eclipse.web/src/org/netbeans/modules/projectimport/eclipse/web/WebProjectFactory.java
@@ -0,0 +1,171 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * 
+ * Copyright 2008 Sun Microsystems, Inc. All rights reserved.
+ * 
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ * 
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ * 
+ * Contributor(s):
+ * 
+ * Portions Copyrighted 2008 Sun Microsystems, Inc.
+ */
+
+package org.netbeans.modules.projectimport.eclipse.web;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
+import org.netbeans.api.project.FileOwnerQuery;
+import org.netbeans.api.project.Project;
+import org.netbeans.api.project.ProjectManager;
+import org.netbeans.modules.j2ee.deployment.devmodules.api.Deployment;
+import org.netbeans.modules.projectimport.eclipse.core.spi.ProjectFactorySupport;
+import org.netbeans.modules.projectimport.eclipse.core.spi.ProjectImportModel;
+import org.netbeans.modules.projectimport.eclipse.core.spi.ProjectTypeFactory;
+import org.netbeans.modules.web.project.api.WebProjectCreateData;
+import org.netbeans.modules.web.project.api.WebProjectUtilities;
+import org.netbeans.spi.project.support.ant.AntProjectHelper;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileUtil;
+import org.openide.xml.XMLUtil;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+/**
+ *
+ */
+public class WebProjectFactory implements ProjectTypeFactory {
+
+    private static final String WEB_NATURE = "org.eclipse.wst.common.modulecore.ModuleCoreNature"; // NOI18N
+    
+    // TODO: check this one as well
+    private static final String MYECLIPSE_WEB_NATURE = "com.genuitec.eclipse.j2eedt.core.webnature"; // NOI18N
+
+    public WebProjectFactory() {
+    }
+    
+    public boolean canHandle(Set<String> natures) {
+        return natures.contains(WEB_NATURE);
+    }
+
+    public Project createProject(final ProjectImportModel model, final List<String> importProblems) throws IOException {
+        // create nb project location
+        File nbProjectDir = FileUtil.normalizeFile(new File(model.getNetBeansProjectLocation() + "/" + model.getProjectName())); // NOI18N
+        
+        WebContentData webData = parseWebContent(model.getEclipseProjectFolder());
+
+        //
+        //
+        // TODO: most of the values defaulted for now:
+        //
+        //
+        
+        WebProjectCreateData createData = new WebProjectCreateData();
+        createData.setProjectDir(nbProjectDir);
+        createData.setName(model.getProjectName());
+        assert Deployment.getDefault().getServerInstanceIDs().length > 0 : "sorry , for now you have to have at least one server";
+        createData.setServerInstanceID(Deployment.getDefault().getServerInstanceIDs()[0]);
+        createData.setJavaEEVersion("1.5");
+        createData.setSourceLevel("1.5");
+        createData.setJavaPlatformName(model.getJavaPlatform().getDisplayName());
+        createData.setServerLibraryName(null);
+
+        FileObject root = FileUtil.toFileObject(model.getEclipseProjectFolder());
+        createData.setWebModuleFO(root);
+        createData.setSourceFolders(model.getEclipseSourceRootsAsFileArray());
+        createData.setTestFolders(model.getEclipseTestSourceRootsAsFileArray());
+        createData.setContextPath(webData.contextRoot);
+        createData.setDocBase(root.getFileObject(webData.webRoot));
+        createData.setLibFolder(root.getFileObject(webData.webRoot+"/WEB-INF/lib"));
+        createData.setWebInfFolder(root.getFileObject(webData.webRoot+"/WEB-INF"));
+        createData.setLibrariesDefinition(null);
+        createData.setBuildfile("build.xml");
+        
+        AntProjectHelper helper = WebProjectUtilities.importProject(createData);
+        Project nbProject = ProjectManager.getDefault().findProject(helper.getProjectDirectory());
+        
+        // set labels for source roots
+//        ProjectFactorySupport.updateSourceRootLabels(model.getEclipseSourceRoots(), nbProject.getSourceRoots());
+//        ProjectFactorySupport.updateSourceRootLabels(model.getEclipseTestSourceRoots(), nbProject.getTestSourceRoots());
+        
+        // TODO: setup include/exclude here
+        
+        // Make sure PCPM knows who owns this (J2SEProject will do the same later on anyway):
+        if (!nbProjectDir.equals(model.getEclipseProjectFolder())) {
+            FileOwnerQuery.markExternalOwner(model.getEclipseProjectFolder().toURI(), nbProject, FileOwnerQuery.EXTERNAL_ALGORITHM_TRANSIENT);
+        }
+        
+        // update project classpath
+        ProjectFactorySupport.updateProjectClassPath(helper, model, importProblems);
+        
+        // save project
+        ProjectManager.getDefault().saveProject(nbProject);
+        return nbProject;
+    }
+
+    private static WebContentData parseWebContent(File eclipseProject) throws IOException {
+        File f = new File(eclipseProject, ".settings/org.eclipse.wst.common.component"); // NOI18N
+        Document webContent;
+        try {
+            webContent = XMLUtil.parse(new InputSource(f.toURI().toString()), false, true, Util.defaultErrorHandler(), null);
+        } catch (SAXException e) {
+            IOException ioe = (IOException) new IOException(f + ": " + e.toString()).initCause(e);
+            throw ioe;
+        }
+        Element modulesEl = webContent.getDocumentElement();
+        if (!"project-modules".equals(modulesEl.getLocalName())) { // NOI18N
+            return null;
+        }
+        WebContentData data = new WebContentData();
+        Element moduleEl = Util.findElement(modulesEl, "wb-module", null);
+        assert modulesEl != null;
+        for (Element el : Util.findSubElements(moduleEl)) {
+            if ("wb-resource".equals(el.getNodeName())) {
+                if ("/".equals(el.getAttribute("deploy-path"))) {
+                    data.webRoot = el.getAttribute("source-path");
+                }
+            }
+            if ("property".equals(el.getNodeName())) {
+                if ("context-root".equals(el.getAttribute("name"))) {
+                    data.contextRoot = el.getAttribute("value");
+                }
+            }
+        }
+        return data;
+    }
+    
+    private static class WebContentData {
+        private String contextRoot;
+        private String webRoot;
+    }
+}
