004bc12b0042269cfc08488e3a46fe57fc4471dc
Jiri Skrivanek <jskrivanek@netbeans.org>
2008-05-28 13:04:05 +0200

    #109538 - Changed API to allow select subcategory while opening Options dialog.

--- options.api/test/unit/src/org/netbeans/api/options/Subcategory1.java
+++ options.api/test/unit/src/org/netbeans/api/options/Subcategory1.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
- *
+ * 
+ * Copyright 2008 Sun Microsystems, Inc. All rights reserved.
+ * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
  * Development and Distribution License("CDDL") (collectively, the
@@ -20,13 +20,7 @@
  * License Header, with the fields enclosed by brackets [] replaced by
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- *
- * Contributor(s):
- *
- * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2008 Sun
- * Microsystems, Inc. All Rights Reserved.
- *
+ * 
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -37,61 +31,82 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
+ * 
+ * Contributor(s):
+ * 
+ * Portions Copyrighted 2008 Sun Microsystems, Inc.
  */
 
-package org.netbeans.modules.options.advanced;
+package org.netbeans.api.options;
 
-import javax.swing.Icon;
-import javax.swing.ImageIcon;
-import org.netbeans.api.options.OptionsDisplayer;
-import org.netbeans.spi.options.OptionsCategory;
+import java.beans.PropertyChangeListener;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import org.netbeans.spi.options.AdvancedOption;
 import org.netbeans.spi.options.OptionsPanelController;
-import org.openide.util.NbBundle;
-import org.openide.util.Utilities;
-
+import org.openide.util.HelpCtx;
+import org.openide.util.Lookup;
 
 /**
- * Contains information about Advanced Panel, and creates a new
- * instance of it.
- *
- * @author Jan Jancura
  */
-public final class Advanced extends OptionsCategory {
+public class Subcategory1 extends AdvancedOption {
 
-    private OptionsPanelController controller;
-    
-    private static String loc (String key) {
-        return NbBundle.getMessage (Advanced.class, key);
-    }
-
-    private static Icon icon;
+    public static final String DISPLAY_NAME = "Subcategory1 display name";
     
     @Override
-    public Icon getIcon () {
-        if (icon == null)
-            icon = new ImageIcon (
-                Utilities.loadImage 
-                    ("org/netbeans/modules/options/resources/advanced.png")
-            );
-        return icon;
-    }
-    
-    public String getCategoryName () {
-        return loc ("CTL_Advanced_Options");
+    public String getDisplayName() {
+        return DISPLAY_NAME;
     }
 
-    public String getTitle () {
-        return loc ("CTL_Advanced_Options_Title");
-    }
-    
-    public String getDescription () {
-        return loc ("CTL_Advanced_Options_Description");
+    @Override
+    public String getTooltip() {
+        return "Subcategory1 tooltip";
     }
 
-    public OptionsPanelController create () {
-        if (controller == null) {
-            controller = new AdvancedPanelController(OptionsDisplayer.ADVANCED);
-        }
-        return controller;
-    }  
+    @Override
+    public OptionsPanelController create() {
+        return new OptionsPanelController() {
+
+            @Override
+            public void update() {
+            }
+
+            @Override
+            public void applyChanges() {
+            }
+
+            @Override
+            public void cancel() {
+            }
+
+            @Override
+            public boolean isValid() {
+                return true;
+            }
+
+            @Override
+            public boolean isChanged() {
+                return false;
+            }
+
+            @Override
+            public JComponent getComponent(Lookup masterLookup) {
+                return new JLabel();
+            }
+
+            @Override
+            public HelpCtx getHelpCtx() {
+                return null;
+            }
+
+            @Override
+            public void addPropertyChangeListener(PropertyChangeListener l) {
+            }
+
+            @Override
+            public void removePropertyChangeListener(PropertyChangeListener l) {
+            }
+        };
+    }
+
 }
