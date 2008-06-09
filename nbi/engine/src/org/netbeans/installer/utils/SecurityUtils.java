1aff15d2c3763fb418580be8a4af4233314a954a
Dmitry Lipin <dlipin@netbeans.org>
2008-06-09 23:02:48 +0400

    Move show certifice status functionality to SecurityUtils from UiUtils

--- nbi/engine/src/org/netbeans/installer/utils/SecurityUtils.java
+++ nbi/engine/src/org/netbeans/installer/utils/SecurityUtils.java
@@ -44,17 +44,19 @@ import java.security.GeneralSecurityException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
+import java.security.Principal;
 import java.security.cert.CertificateException;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateExpiredException;
 import java.security.cert.CertificateNotYetValidException;
 import java.security.cert.X509Certificate;
+import java.text.DateFormat;
 import java.util.Collections;
 import java.util.Date;
 import java.util.Random;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
-import org.netbeans.installer.utils.UiUtils.CertificateAcceptanceStatus;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -204,7 +206,7 @@ public final class SecurityUtils {
                     timestamp = codeSigners[chainNum].getTimestamp().getTimestamp();
                 }
 
-                CertificateAcceptanceStatus status = UiUtils.showCertificateAcceptanceDialog(
+                CertificateAcceptanceStatus status = showCertificateAcceptanceDialog(
                         certificates,
                         chainStart,
                         chainEnd,
@@ -280,5 +282,185 @@ public final class SecurityUtils {
         }
 
         return false;
+    }
+
+    private static CertificateAcceptanceStatus showCertificateAcceptanceDialog(
+            final Certificate[] certificates,
+            final int chainStart,
+            final int chainEnd,
+            final boolean rootCaIsNotValid,
+            final boolean timeIsNotValid,
+            final Date timestamp,
+            final String description) {
+        if (certificates[chainStart] instanceof X509Certificate && certificates[chainEnd - 1] instanceof X509Certificate) {
+            final X509Certificate firstCert =
+                    (X509Certificate) certificates[chainStart];
+            final X509Certificate lastCert =
+                    (X509Certificate) certificates[chainEnd - 1];
+
+            final Principal subject = firstCert.getSubjectDN();
+            final Principal issuer = lastCert.getIssuerDN();
+
+            // extract subject & issuer's name
+            final String subjectName = extractName(
+                    subject.getName(),
+                    "CN=",
+                    "Unknown Subject");
+            final String issuerName = extractName(
+                    issuer.getName(),
+                    "O=",
+                    "Unknown Issuer");
+
+            // dialog caption
+            String caption = null;
+            String body = "";
+
+            // check if this is the case when both - the root CA and time of
+            // signing is valid:
+            if ((!rootCaIsNotValid) && (!timeIsNotValid)) {
+                caption = StringUtils.format(
+                        "The digital signature of {0} has been verified.",
+                        description);
+
+                body +=
+                        "The digital signature has been validated by a trusted source. " +
+                        "The security certificate was issued by a company that is trusted";
+
+                // for timestamp info, add a message saying that certificate was
+                // valid at the time of signing. And display date of signing.
+                if (timestamp != null) {
+                    // get the right date format for timestamp
+                    final DateFormat df = DateFormat.getDateTimeInstance(
+                            DateFormat.LONG,
+                            DateFormat.LONG);
+                    body += StringUtils.format(
+                            " and was valid at the time of signing on {0}.",
+                            df.format(timestamp));
+                } else {
+                    // add message about valid time of signing:
+                    body +=
+                            ", has not expired and is still valid.";
+                }
+
+                // we should add one more message here - disclaimer we used
+                // to have.  This is to be displayed in the "All trusted"
+                // case in the More Information dialog.
+                body += StringUtils.format(
+                        "Caution: \"{0}\" asserts that this content is safe.  " +
+                        "You should only accept this content if you trust \"{1}\" to make that assertion.",
+                        subjectName,
+                        subjectName);
+            } else {
+                // this is the case when either publisher or time of signing
+                // is invalid - check and add corresponding messages to
+                // appropriate message arrays.
+
+                // If root CA is not valid, add a caption and a message to the
+                // securityAlerts array.
+                if (rootCaIsNotValid) {
+                    // Use different caption text for https and signed content
+                    caption = StringUtils.format(
+                            "The digital signature of {0} cannot be verified.",
+                            description);
+
+                    body += "The digital signature cannot be verified by a trusted source. " +
+                            "Only continue if you trust the origin of the file. " +
+                            "The security certificate was issued by a company that is not trusted.";
+                } else {
+                    caption = StringUtils.format(
+                            "The digital signature of {0} has been verified.",
+                            description);
+
+                    // Same details for both
+                    body += "The security certificate was issued by a company that is trusted.";
+                }
+
+                // now check if time of signing is valid.
+                if (timeIsNotValid) {
+                    // if no warnings yet, add the one that will show with the
+                    // bullet in security warning dialog:
+                    body += "The digital signature was generated with a trusted certificate but has expired or is not yet valid";
+                } else {
+                    // for timestamp info, add a message saying that certificate
+                    // was valid at the time of signing
+                    if (timestamp != null) {
+                        // get the right date format for timestamp
+                        final DateFormat df = DateFormat.getDateTimeInstance(
+                                DateFormat.LONG,
+                                DateFormat.LONG);
+                        body += StringUtils.format(
+                                "The security certificate was valid at the time of signing on {0}.",
+                                df.format(timestamp));
+                    } else {
+                        body += "The security certificate has not expired and is still valid.";
+                    }
+                }
+            }
+
+            String message = StringUtils.format("<html><b>{0}</b><br>" +
+                    "Subject: {1}<br>" +
+                    "Issuer: {2}<br><br>" +
+                    "{3}<br><br>" +
+                    "Click OK to accept the certificate permanently, " +
+                    "No to accept it temporary for this session, " +
+                    "Cancel to reject the certificate.",
+                    caption, subjectName, issuerName, body);
+
+            int option = UiUtils.showYesNoCancelDialog(null, message, JOptionPane.NO_OPTION);
+            if (option == JOptionPane.OK_OPTION) {
+                return CertificateAcceptanceStatus.ACCEPT_PERMANENTLY;
+            } else if (option == JOptionPane.NO_OPTION) {
+                return CertificateAcceptanceStatus.ACCEPT_FOR_THIS_SESSION;
+            } else {
+                return CertificateAcceptanceStatus.DENY;
+            }
+        }
+
+        return CertificateAcceptanceStatus.DENY;
+    }
+
+    private static String extractName(
+            final String nameString,
+            final String prefix,
+            final String defaultValue) {
+        int i = nameString.indexOf(prefix);
+        int j = 0;
+
+        if (i < 0) {
+            return defaultValue;
+        } else {
+            try {
+                // shift to the beginning of the prefix text
+                i = i + prefix.length();
+
+                // check if it begins with a quote
+                if (nameString.charAt(i) == '\"') {
+                    // skip the quote
+                    i = i + 1;
+
+                    // search for another quote
+                    j = nameString.indexOf('\"', i);
+                } else {
+
+                    // no quote, so search for comma
+                    j = nameString.indexOf(',', i);
+                }
+
+                if (j < 0) {
+                    return nameString.substring(i);
+                } else {
+                    return nameString.substring(i, j);
+                }
+            } catch (IndexOutOfBoundsException e) {
+                return defaultValue;
+            }
+        }
+    }
+
+    public static enum CertificateAcceptanceStatus {
+
+        ACCEPT_PERMANENTLY,
+        ACCEPT_FOR_THIS_SESSION,
+        DENY
     }
 }
