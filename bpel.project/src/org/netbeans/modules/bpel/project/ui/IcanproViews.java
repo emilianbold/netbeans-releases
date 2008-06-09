88f092433e511e6421c99a9b3a6fbcb1846c14c1
Vladimir Yaroslavskiy <yaroslavskiy@netbeans.org>
2008-06-09 18:05:29 +0400

    [Issue 126873] Catalog resources management is missing

--- bpel.project/src/org/netbeans/modules/bpel/project/ui/IcanproViews.java
+++ bpel.project/src/org/netbeans/modules/bpel/project/ui/IcanproViews.java
@@ -1,35 +1,56 @@
 /*
- * The contents of this file are subject to the terms of the Common Development
- * and Distribution License (the License). You may not use this file except in
- * compliance with the License.
- * 
- * You can obtain a copy of the License at http://www.netbeans.org/cddl.html
- * or http://www.netbeans.org/cddl.txt.
- * 
- * When distributing Covered Code, include this CDDL Header Notice in each file
- * and include the License file at http://www.netbeans.org/cddl.txt.
- * If applicable, add the following below the CDDL Header, with the fields
- * enclosed by brackets [] replaced by your own identifying information:
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License. When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP. Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- * 
+ *
+ * Contributor(s):
+ *
  * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
  * Microsystems, Inc. All Rights Reserved.
+ *
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
  */
-
-
 package org.netbeans.modules.bpel.project.ui;
 
 import java.awt.Image;
-
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import javax.swing.Action;
-import org.netbeans.api.project.*;
+import org.netbeans.api.project.FileOwnerQuery;
+import org.netbeans.api.project.Project;
+import org.netbeans.api.project.ProjectUtils;
+import org.netbeans.api.project.Sources;
+import org.netbeans.api.project.SourceGroup;
 import org.netbeans.spi.project.ui.support.CommonProjectActions;
-//import org.netbeans.modules.j2ee.common.ui.ServerResourceNode;
-//import org.netbeans.modules.j2ee.dd.api.ejb.DDProvider;
 
 import org.openide.filesystems.FileObject;
 import org.openide.loaders.DataFolder;
@@ -40,13 +61,11 @@ import org.openide.nodes.Node;
 import org.openide.util.NbBundle;
 import org.openide.util.Utilities;
 
-//import org.netbeans.spi.java.project.support.ui.PackageView;
 import org.netbeans.spi.project.support.ant.AntProjectHelper;
 import org.netbeans.spi.project.support.ant.PropertyEvaluator;
 
 import org.netbeans.modules.bpel.project.IcanproProject;
 import org.netbeans.modules.compapp.projects.base.ui.customizer.IcanproProjectProperties;
-// import org.netbeans.modules.websvc.api.webservices.WebServicesView;
 import org.openide.filesystems.FileChangeListener;
 import org.openide.loaders.DataObject;
 import java.util.logging.Logger;
@@ -56,21 +75,19 @@ import javax.swing.event.EventListenerList;
 import org.netbeans.api.queries.VisibilityQuery;
 import org.openide.loaders.ChangeableDataFilter;
 import org.openide.loaders.DataFilter;
+import org.netbeans.modules.bpel.project.ui.catalog.ResourcesNode;
+
 class IcanproViews {
     private static Logger logger = Logger.getLogger(IcanproViews.class.getName());
     
     private static final DataFilter NO_FOLDERS_FILTER = new NoFoldersDataFilter();
     
-    private IcanproViews() {
-    }
+    private IcanproViews() {}
 
     static final class LogicalViewChildren extends Children.Keys implements FileChangeListener {
 
         private static final String KEY_SOURCE_DIR = "srcDir"; // NOI18N
-        private static final String KEY_DOC_BASE = "docBase"; //NOI18N
-        private static final String KEY_EJBS = "ejbKey"; //NOI18N
-        private static final String WEBSERVICES_DIR = "webservicesDir"; // NOI18N 
-        private static final String KEY_SETUP_DIR = "setupDir"; //NOI18N
+        private static final String KEY_RESOURCE_DIR = "resourceDir"; // NOI18N
 
         private AntProjectHelper helper;
         private final PropertyEvaluator evaluator;
@@ -91,41 +108,22 @@ class IcanproViews {
             createNodes();
         }
 
+        @SuppressWarnings("unchecked")
         private void createNodes() {
             List l = new ArrayList();
-            /*
-            l.add(KEY_EJBS);
-            */
-
-            DataFolder docBaseDir = getFolder(IcanproProjectProperties.META_INF);
-            if (docBaseDir != null) {
-                /*
-                l.add(KEY_DOC_BASE);
-                 */
-            }
-
             DataFolder srcDir = getFolder(IcanproProjectProperties.SRC_DIR);
+
             if (srcDir != null) {
                 l.add(KEY_SOURCE_DIR);
             }
+            l.add(KEY_RESOURCE_DIR);
 
-            FileObject setupFolder = getSetupFolder();
-            if (setupFolder != null && setupFolder.isFolder()) {
-                l.add(KEY_SETUP_DIR);
-            }
-/*
-            l.add(WEBSERVICES_DIR);
-*/
             if (l.size() > 0) {
                 setKeys(l);
-            } 
-            
+            }
         }
 
-        private FileObject getSetupFolder() {
-            return projectDir.getFileObject("setup"); //NOI18N
-        }
-
+        @SuppressWarnings("unchecked")
         protected void removeNotify() {
             setKeys(Collections.EMPTY_SET);
             projectDir.removeFileChangeListener(this);
@@ -133,70 +131,46 @@ class IcanproViews {
         }
 
         protected Node[] createNodes(Object key) {
-            Node n = null;
-            if (key == KEY_SOURCE_DIR) {
-                FileObject srcRoot = helper.resolveFileObject(evaluator.getProperty (IcanproProjectProperties.SRC_DIR));
-                Project p = FileOwnerQuery.getOwner (srcRoot);
-                Sources s = ProjectUtils.getSources(p);
-                SourceGroup sgs [] = ProjectUtils.getSources (p).getSourceGroups (IcanproProject.SOURCES_TYPE_ICANPRO);
-		for (int i = 0; i < sgs.length; i++) {
-		    if (sgs [i].contains(srcRoot)) {
-			try {
-                            FileObject folder = sgs[i].getRootFolder();
-			    DataObject dobj = DataObject.find(folder);
-                            n = new RootNode(dobj.getNodeDelegate(), (DataFolder) dobj);
-			} catch (DataObjectNotFoundException ex) {
-			}
-			break;
-		    }
-		}
-            /*
-            } else if (key == KEY_DOC_BASE) {
-                n = new DocBaseNode (getFolder(IcanproProjectProperties.META_INF).getNodeDelegate());
-            } else if (key == KEY_EJBS) {
-                FileObject srcRoot = helper.resolveFileObject(evaluator.getProperty (IcanproProjectProperties.SRC_DIR));
-                Project project = FileOwnerQuery.getOwner (srcRoot);
-                DDProvider provider = DDProvider.getDefault();
-                EjbJarImplementation jp = (EjbJarImplementation) project.getLookup().lookup(EjbJarImplementation.class);
-                org.netbeans.modules.j2ee.dd.api.ejb.EjbJar ejbJar = null;
+          Node node = null;
+          
+          if (key == KEY_SOURCE_DIR) {
+            FileObject srcRoot = helper.resolveFileObject(evaluator.getProperty(IcanproProjectProperties.SRC_DIR));
+            Project p = FileOwnerQuery.getOwner(srcRoot);
+            Sources s = ProjectUtils.getSources(p);
+            SourceGroup sgs [] = ProjectUtils.getSources(p).getSourceGroups(IcanproProject.SOURCES_TYPE_ICANPRO);
+    
+            for (int i = 0; i < sgs.length; i++) {
+              if (sgs [i].contains(srcRoot)) {
                 try {
-                    ejbJar = provider.getDDRoot(jp.getDeploymentDescriptor());
-                } catch (IOException ioe) {
-                    ErrorManager.getDefault().notify(ioe);
+                    FileObject folder = sgs[i].getRootFolder();
+                    DataObject dobj = DataObject.find(folder);
+                    node = new RootNode(dobj.getNodeDelegate(), (DataFolder) dobj);
+                } 
+                catch (DataObjectNotFoundException ex) {
                 }
-                ClassPathProvider cpp = (ClassPathProvider)
-                    project.getLookup().lookup(ClassPathProvider.class);
-                assert cpp != null;
-                ClassPath classPath = cpp.findClassPath(srcRoot, ClassPath.SOURCE);
-                n = new EjbContainerNode(ejbJar, classPath);
-                //Node nws =  new WebServicesNode(ejbJar, classPath);
-                return n == null ? new Node[0] : new Node[] {n};
-            } else if (key == WEBSERVICES_DIR){
-		FileObject 	srcRoot = helper.resolveFileObject(evaluator.getProperty (IcanproProjectProperties.SRC_DIR));
-                WebServicesView webServicesView = WebServicesView.getWebServicesView(srcRoot);
-                if(webServicesView != null)
-		{
-		n = webServicesView.createWebServicesView(srcRoot);
-                }
-	    } else if (key == KEY_SETUP_DIR) {
-                try {
-                    DataObject sdo = DataObject.find(getSetupFolder());
-                    n = new ServerResourceNode(project); // sdo.getNodeDelegate());
-                } catch (org.openide.loaders.DataObjectNotFoundException dnfe) {}
-                */
+                break;
+              }
             }
-            return n == null ? new Node[0] : new Node[] {n};
+          }
+          else if (key == KEY_RESOURCE_DIR) {
+            FileObject srcRoot = helper.resolveFileObject(evaluator.getProperty(IcanproProjectProperties.SRC_DIR));
+            Project project = FileOwnerQuery.getOwner(srcRoot);
+            node = new ResourcesNode(project);
+          }
+          return node == null ? new Node[0] : new Node[] { node };
         }
 
         private DataFolder getFolder(String propName) {
             String propertyValue = evaluator.getProperty (propName);
             if (propertyValue != null ) {
                 FileObject fo = helper.resolveFileObject(evaluator.getProperty (propName));
-                if ( fo != null && fo.isValid()) {
+
+                if (fo != null && fo.isValid()) {
                     try {
                         DataFolder df = DataFolder.findFolder(fo);
                         return df;
-                    }catch (Exception ex) {
+                    }
+                    catch (Exception ex) {
                         logger.fine(ex.getMessage());
                     }
                 }
@@ -204,84 +178,67 @@ class IcanproViews {
             return null;
         }
 
-        // file change events in the project directory
-        public void fileAttributeChanged(org.openide.filesystems.FileAttributeEvent fe) {
-        }
+        public void fileAttributeChanged(org.openide.filesystems.FileAttributeEvent fe) {}
 
-        public void fileChanged(org.openide.filesystems.FileEvent fe) {
-        }
+        public void fileChanged(org.openide.filesystems.FileEvent fe) {}
 
-        public void fileDataCreated(org.openide.filesystems.FileEvent fe) {
-        }
+        public void fileDataCreated(org.openide.filesystems.FileEvent fe) {}
 
-        public void fileDeleted(org.openide.filesystems.FileEvent fe) {
-//            if (!projectDir.isValid()){
-//                    org.netbeans.api.project.ui.OpenProjects.getDefault().close(new Project[] { project });                
-//            }
-
-        }
+        public void fileDeleted(org.openide.filesystems.FileEvent fe) {}
 
         public void fileFolderCreated(org.openide.filesystems.FileEvent fe) {
-            // setup folder could be created
             createNodes();
         }
 
         public void fileRenamed(org.openide.filesystems.FileRenameEvent fe) {
-            // setup folder could be renamed
             createNodes();
         }
     }
-    
+
+    // ----------------------------------------------------- 
     private static final class RootNode extends FilterNode {
-	public RootNode(Node n, DataFolder dataFolder) {
-	    super(n,  dataFolder.createNodeChildren( NO_FOLDERS_FILTER));
-	    disableDelegation(DELEGATE_GET_DISPLAY_NAME|
-		DELEGATE_SET_DISPLAY_NAME|DELEGATE_GET_SHORT_DESCRIPTION|
-		DELEGATE_GET_ACTIONS);
-	    setDisplayName(
-			NbBundle.getMessage(RootNode.class, "LBL_ProcessFiles"));
-	    		
-	}
-	
-	@Override
-	public Action[] getActions(boolean context) {
-	    return new Action[] {
-		CommonProjectActions.newFileAction(),
-                null,
-                org.openide.util.actions.SystemAction.get( org.openide.actions.FileSystemAction.class ),
-                null,
-                org.openide.util.actions.SystemAction.get( org.openide.actions.FindAction.class ),
-                null,
-                org.openide.util.actions.SystemAction.get( org.openide.actions.PasteAction.class ),
-                null,
-                org.openide.util.actions.SystemAction.get( org.openide.actions.ToolsAction.class ),
-	    };
-	}
 
-        public boolean canDestroy() {
-            return false;
-        }
+      public RootNode(Node n, DataFolder dataFolder) {
+          super(n, dataFolder.createNodeChildren(NO_FOLDERS_FILTER));
+          disableDelegation(DELEGATE_GET_DISPLAY_NAME | DELEGATE_SET_DISPLAY_NAME | DELEGATE_GET_SHORT_DESCRIPTION | DELEGATE_GET_ACTIONS);
+          setDisplayName(NbBundle.getMessage(RootNode.class, "LBL_ProcessFiles")); // NOI18N
+      }
+      
+      @Override
+      public Action[] getActions(boolean context) {
+          return new Action[] {
+          CommonProjectActions.newFileAction(),
+                  null,
+                  org.openide.util.actions.SystemAction.get( org.openide.actions.FileSystemAction.class ),
+                  null,
+                  org.openide.util.actions.SystemAction.get( org.openide.actions.FindAction.class ),
+                  null,
+                  org.openide.util.actions.SystemAction.get( org.openide.actions.PasteAction.class ),
+                  null,
+                  org.openide.util.actions.SystemAction.get( org.openide.actions.ToolsAction.class ),
+          };
+      }
 
+      public boolean canDestroy() {
+        return false;
+      }
 
-	
-
-	@Override
-	public boolean canRename() {
-	    return false;
-	}
-	
-	@Override
-	public boolean canCopy() {
-	    return false;
-	}
-	
-	@Override
-	public boolean canCut() {
-	    return false;
-	}
-
+      @Override
+      public boolean canRename() {
+          return false;
+      }
+      
+      @Override
+      public boolean canCopy() {
+          return false;
+      }
+      
+      @Override
+      public boolean canCut() {
+          return false;
+      }
     }
-
+ 
     private static final class DocBaseNode extends FilterNode { 
         private static Image CONFIGURATION_FILES_BADGE = Utilities.loadImage( "org/netbeans/modules/bpel/project/ui/resources/docjar.gif", true ); // NOI18N
 
@@ -341,7 +298,5 @@ class IcanproViews {
         public void removeChangeListener( ChangeListener listener ) {
             ell.remove( ChangeListener.class, listener );
         }
-        
     }
-
 }
