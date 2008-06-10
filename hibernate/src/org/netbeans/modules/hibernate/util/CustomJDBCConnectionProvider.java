f3dc8e1acc678b69bad3f9ff51e4b231a3578d31
Vadiraj Deshpande <vadirajvd@netbeans.org>
2008-06-10 12:42:14 +0530

    Refactoring Custom classloader and Custom JDBC connection provider to util package. These will be used by HQL editor and Reverse Engg. wizard. (and more in future).

--- hibernate/src/org/netbeans/modules/hibernate/util/CustomJDBCConnectionProvider.java
+++ hibernate/src/org/netbeans/modules/hibernate/util/CustomJDBCConnectionProvider.java
@@ -36,7 +36,7 @@
  * 
  * Portions Copyrighted 2008 Sun Microsystems, Inc.
  */
-package org.netbeans.modules.hibernate.service;
+package org.netbeans.modules.hibernate.util;
 
 import java.sql.Connection;
 import java.sql.SQLException;
