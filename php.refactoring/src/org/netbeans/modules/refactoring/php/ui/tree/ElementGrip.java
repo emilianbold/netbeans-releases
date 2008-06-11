6549ab268fb510b6575fa9e5e5c174cefb3252c6
Radek Matous <rmatous@netbeans.org>
2008-06-11 12:51:25 +0200

    new module php.refactoring, deps. on other modules, impl. of find usages

--- php.refactoring/src/org/netbeans/modules/refactoring/php/ui/tree/ElementGrip.java
+++ php.refactoring/src/org/netbeans/modules/refactoring/php/ui/tree/ElementGrip.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- * 
- * Copyright 2008 Sun Microsystems, Inc. All rights reserved.
- * 
+ *
+ * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
  * Development and Distribution License("CDDL") (collectively, the
@@ -20,7 +20,13 @@
  * License Header, with the fields enclosed by brackets [] replaced by
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- * 
+ *
+ * Contributor(s):
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ *
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -31,42 +37,45 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
- * 
- * Contributor(s):
- * 
- * Portions Copyrighted 2008 Sun Microsystems, Inc.
  */
-package org.netbeans.modules.php.editor.parser.astnodes;
+
+package org.netbeans.modules.refactoring.php.ui.tree;
+
+import javax.swing.Icon;
+import org.openide.filesystems.FileObject;
 
 /**
- * Represents a static field access. 
- * <pre>e.g.<pre> MyClass::$a
- * MyClass::$$a[3]
+ *
+ * @author Jan Becicka
+ * @author Tor Norbye
  */
-public class StaticFieldAccess extends StaticDispatch {
-
-    private Variable field;
-
-    public StaticFieldAccess(int start, int end, Identifier className, Variable field) {
-        super(start, end, className);
-        this.field = field;
-    }
-
+public final class ElementGrip {
+    private String toString;
+    private FileObject fileObject;
+    private Icon icon;
+    
     /**
-     * The field of this access
+     * Creates a new instance of ElementGrip
      * 
-     * @return field of this access
      */
-    public Variable getField() {
-        return field;
-    }
-
-    public ASTNode getMember() {
-        return getField();
+    public ElementGrip(String name, FileObject fileObject, Icon icon) {
+        this.toString = name;
+        this.fileObject = fileObject;
+        this.icon = icon;
     }
     
-    @Override
-    public void accept(Visitor visitor) {
-        visitor.visit(this);
-    }            
+    public Icon getIcon() {
+        return icon;
+    }
+    public String toString() {
+        return toString;
+    }
+
+    public ElementGrip getParent() {
+        return ElementGripFactory.getDefault().getParent(this);
+    }
+
+    public FileObject getFileObject() {
+        return fileObject;
+    }
 }
