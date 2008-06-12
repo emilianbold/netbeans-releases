c8e636728c5658a4de0e4889983abbac3178a2f8
Jaroslav Tulach <jtulach@netbeans.org>
2008-06-12 19:06:06 +0200

    Rollback of false part of my commit

--- jellytools/test/qa-functional/src/org/netbeans/jellytools/DocumentsDialogOperatorTest.java
+++ jellytools/test/qa-functional/src/org/netbeans/jellytools/DocumentsDialogOperatorTest.java
@@ -100,22 +100,15 @@ public class DocumentsDialogOperatorTest extends JellyTestCase {
         return suite;
          */
         return (NbTest) NbModuleSuite.create(
-                updateConfig(NbModuleSuite.emptyConfiguration().enableModules(".*").clusters(".*")));
-    }
-    
-    static NbModuleSuite.Configuration updateConfig(NbModuleSuite.Configuration in) {
-        return in.addTest(
-            DocumentsDialogOperatorTest.class, 
-            "testInvoke"
-            "testVerify",
-            "testSelectDocument",
-            "testSelectDocuments",
-            "testGetDescription",
-            "testSaveDocuments",
-            "testCloseDocuments",
-            "testSwitchToDocument"
-        );
-        
+                NbModuleSuite.createConfiguration(DocumentsDialogOperatorTest.class).
+                    addTest("testInvoke").
+                    addTest("testVerify").
+                    addTest("testSelectDocument").
+                    addTest("testSelectDocuments").
+                    addTest("testGetDescription").
+                    addTest("testSaveDocuments").
+                    addTest("testCloseDocuments").
+                    addTest("testSwitchToDocument").enableModules(".*").clusters(".*"));
     }
     
     /** Print out test name. */
