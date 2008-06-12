fede2739c4572810c1bbcd5c6ce48d7b66817ca1
Matthias Schmidt <schmidtm@netbeans.org>
2008-06-12 15:35:56 +0200

    Removing identical method: AstUtilities.getBaseDocument() == NbUtilities.getBaseDocument().

--- groovy.editor/src/org/netbeans/modules/groovy/editor/NbUtilities.java
+++ groovy.editor/src/org/netbeans/modules/groovy/editor/NbUtilities.java
@@ -104,48 +104,6 @@ public class NbUtilities {
 
         return null;
     }
-    
-    public static BaseDocument getBaseDocument(FileObject fileObject, boolean forceOpen) {
-        DataObject dobj;
-
-        try {
-            dobj = DataObject.find(fileObject);
-
-            EditorCookie ec = dobj.getCookie(EditorCookie.class);
-
-            if (ec == null) {
-                throw new IOException("Can't open " + fileObject.getNameExt());
-            }
-
-            Document document;
-
-            if (forceOpen) {
-                document = ec.openDocument();
-            } else {
-                document = ec.getDocument();
-            }
-
-            if (document instanceof BaseDocument) {
-                return ((BaseDocument)document);
-            } else {
-                // Must be testsuite execution
-                try {
-                    Class c = Class.forName("org.netbeans.modules.groovy.editor.test.GroovyTestBase");
-                    if (c != null) {
-                        @SuppressWarnings("unchecked")
-                        java.lang.reflect.Method m = c.getMethod("getDocumentFor", new Class[] { FileObject.class });
-                        return (BaseDocument) m.invoke(null, (Object[])new FileObject[] { fileObject });
-                    }
-                } catch (Exception ex) {
-                    Exceptions.printStackTrace(ex);
-                }
-            }
-        } catch (IOException ioe) {
-            Exceptions.printStackTrace(ioe);
-        }
-
-        return null;
-    }
 
     public static FileObject findFileObject(JTextComponent target) {
         Document doc = target.getDocument();
