600e1ef15628461ada659f84dbc989ac570d65af
jyothivasa <jyothivasa@netbeans.org>
2008-06-11 22:39:27 -0700

    Activity partition orientation is now persisted

--- uml.diagrams/src/org/netbeans/modules/uml/diagrams/nodes/activity/ActivityPartitionWidget.java
+++ uml.diagrams/src/org/netbeans/modules/uml/diagrams/nodes/activity/ActivityPartitionWidget.java
@@ -46,6 +46,7 @@ import java.awt.Paint;
 import java.awt.Rectangle;
 import java.beans.PropertyChangeEvent;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.ResourceBundle;
 import org.netbeans.api.visual.border.BorderFactory;
@@ -64,6 +65,7 @@ import org.netbeans.modules.uml.core.support.umlutils.IElementLocator;
 import org.netbeans.modules.uml.diagrams.nodes.ContainerNode;
 import org.netbeans.modules.uml.diagrams.nodes.UMLNameWidget;
 import org.netbeans.modules.uml.drawingarea.palette.context.DefaultContextPaletteModel;
+import org.netbeans.modules.uml.drawingarea.persistence.NodeWriter;
 import org.netbeans.modules.uml.drawingarea.persistence.PersistenceUtil;
 import org.netbeans.modules.uml.drawingarea.persistence.data.NodeInfo;
 import org.netbeans.modules.uml.drawingarea.util.Util;
@@ -139,7 +141,7 @@ public class ActivityPartitionWidget extends UMLNodeWidget
         partitionPanel.setMinimumSize(new Dimension(100, 85));
         // TODO: need to find a way to figure out the exisiting orientation of sub parttition
         //setOrientation(Orientation.VERTICAL);   
-        populateAllPartitions(partitionElement);
+//        populateAllPartitions(partitionElement);
         mainView.addChild(partitionPanel, 1);
 
         return mainView;
@@ -415,6 +417,9 @@ public class ActivityPartitionWidget extends UMLNodeWidget
         {
             if (elt.getOwner() instanceof IActivity)
             {
+                String or = nodeReader.getProperties().get("Orientation").toString();
+                this.setOrientation(SeparatorWidget.Orientation.valueOf(or));
+                populateAllPartitions((IActivityPartition)elt);
                 super.load(nodeReader);                
             }
             SubPartitionWidget subPart = getSubPartitionWidget((IActivityPartition) elt);
@@ -426,6 +431,15 @@ public class ActivityPartitionWidget extends UMLNodeWidget
                 nodeReader.setPresentationElement(pElt);
             }
         }
+    }
+
+    @Override
+    public void save(NodeWriter nodeWriter)
+    {
+        HashMap map = nodeWriter.getProperties();
+        map.put("Orientation", this.getOrientation().toString());
+        nodeWriter.setProperties(map);
+        super.save(nodeWriter);
     }
 
     private class MainViewWidget extends CustomizableWidget
