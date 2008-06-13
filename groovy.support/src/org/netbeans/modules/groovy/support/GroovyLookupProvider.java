d576c404f4c54063824f05261142f18325bfc09d
Martin Adamek <martin_adamek@netbeans.org>
2008-06-13 12:04:14 +0200

    Run file on Groovy classes in Java SE projects enabled

--- groovy.support/src/org/netbeans/modules/groovy/support/GroovyLookupProvider.java
+++ groovy.support/src/org/netbeans/modules/groovy/support/GroovyLookupProvider.java
@@ -67,8 +67,8 @@ public class GroovyLookupProvider implements LookupProvider {
         }
         List<Object> instances = new ArrayList<Object>(3);
         instances.add(new GroovyProjectExtender(project));
-//        instances.add(LookupMergerSupport.createActionProviderLookupMerger());
-//        instances.add(new GroovyActionProvider(project));
+        instances.add(LookupMergerSupport.createActionProviderLookupMerger());
+        instances.add(new GroovyActionProvider(project));
         instances.add(new GsfClasspathHook(project));
         return Lookups.fixed(instances.toArray(new Object[instances.size()]));
     }
