b345fc4ee342684e66a5af63aa0ae45d9382c94a
Ayub Khan <ayubskhan@netbeans.org>
2008-06-15 02:01:02 -0700

    Fix lookup issue

--- websvc.saas.codegen/src/org/netbeans/modules/websvc/saas/codegen/java/CustomClientJspCodeGenerator.java
+++ websvc.saas.codegen/src/org/netbeans/modules/websvc/saas/codegen/java/CustomClientJspCodeGenerator.java
@@ -47,9 +47,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
 import org.netbeans.modules.websvc.saas.codegen.model.ParameterInfo;
 import org.netbeans.modules.websvc.saas.codegen.java.support.Inflector;
 import org.netbeans.modules.websvc.saas.codegen.util.Util;
+import org.netbeans.modules.websvc.saas.model.CustomSaasMethod;
+import org.netbeans.modules.websvc.saas.model.SaasMethod;
 
 /**
  * Code generator for Accessing Saas services.
@@ -63,6 +66,14 @@ public class CustomClientJspCodeGenerator extends CustomClientServletCodeGenerat
     public CustomClientJspCodeGenerator() throws IOException {
         setDropFileType(Constants.DropFileType.JSP);
         jspSpecialNamesMap.put("page", "page1");
+    }
+
+    @Override
+    public boolean canAccept(SaasMethod method, Document doc) {
+        if (method instanceof CustomSaasMethod && Util.isJsp(doc)) {
+            return true;
+        }
+        return false;
     }
     
     /**
