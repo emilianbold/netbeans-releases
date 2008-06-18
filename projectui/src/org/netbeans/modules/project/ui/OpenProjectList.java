5ee2204c2b5f6128811a3209ef8c3e17cbd6849a
Milan Kubec <mkubec@netbeans.org>
2008-06-18 11:23:30 +0200

    #103644: better handling of setting of main project during asynchronous project opening

--- projectui/src/org/netbeans/modules/project/ui/OpenProjectList.java
+++ projectui/src/org/netbeans/modules/project/ui/OpenProjectList.java
@@ -453,7 +453,11 @@ public final class OpenProjectList {
 	open(projects, openSubprojects, false);
     }
     
-    public void open(final Project[] projects, final boolean openSubprojects, final boolean asynchronously ) {
+    public void open(final Project[] projects, final boolean openSubprojects, final boolean asynchronously) {
+        open(projects, openSubprojects, asynchronously, null);
+    }
+    
+    public void open(final Project[] projects, final boolean openSubprojects, final boolean asynchronously, final Project/*|null*/ mainProject) {
         if (projects.length == 0) {
             //nothing to do:
             return ;
@@ -465,7 +469,7 @@ public final class OpenProjectList {
             if (!EventQueue.isDispatchThread()) { // #89935
                 EventQueue.invokeLater(new Runnable() {
                     public void run() {
-                        open(projects, openSubprojects, asynchronously);
+                        open(projects, openSubprojects, asynchronously, mainProject);
                     }
                 });
                 return;
@@ -492,6 +496,9 @@ public final class OpenProjectList {
 		public void run() {
 		    try {
 			doOpen(projects, openSubprojects, handle, panel);
+                        if (mainProject != null && Arrays.asList(projects).contains(mainProject) && openProjects.contains(mainProject)) {
+                            setMainProject(mainProject);
+                        }
 		    } finally {
 			SwingUtilities.invokeLater(new Runnable() {
 			    public void run() {
@@ -512,6 +519,9 @@ public final class OpenProjectList {
 	    dialog.setVisible(true);
 	} else {
 	    doOpen(projects, openSubprojects, null, null);
+            if (mainProject != null && Arrays.asList(projects).contains(mainProject) && openProjects.contains(mainProject)) {
+                setMainProject(mainProject);
+            }
 	}
         
         long end = System.currentTimeMillis();
