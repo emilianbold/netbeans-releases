879d677de2903055abf995a49440cfe5cb4c5c27
David Van Couvering <davidvc@netbeans.org>
2008-06-18 17:15:11 -0700

    #137600 - Host label was not localized

--- db/src/org/netbeans/modules/db/explorer/dlg/NewConnectionPanel.java
+++ db/src/org/netbeans/modules/db/explorer/dlg/NewConnectionPanel.java
@@ -297,7 +297,7 @@ public class NewConnectionPanel extends ConnectionDialog.FocusablePanel implemen
         org.openide.awt.Mnemonics.setLocalizedText(portLabel, bundle.getString("NewConnectionPort")); // NOI18N
 
         hostLabel.setLabelFor(hostField);
-        org.openide.awt.Mnemonics.setLocalizedText(hostLabel, "&Host:");
+        org.openide.awt.Mnemonics.setLocalizedText(hostLabel, bundle.getString("NewConnectionHost")); // NOI18N
 
         passwordLabel.setLabelFor(passwordField);
         org.openide.awt.Mnemonics.setLocalizedText(passwordLabel, NbBundle.getBundle("org.netbeans.modules.db.resources.Bundle").getString("NewConnectionPassword")); // NOI18N
