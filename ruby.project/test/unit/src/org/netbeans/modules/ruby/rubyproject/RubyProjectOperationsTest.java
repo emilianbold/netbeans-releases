178b3f90625641507bb35b67eb48b38f1c4d0949
Martin Krauskopf <mkrauskopf@netbeans.org>
2008-06-26 16:09:56 +0200

    #138369: Deleting project w/ sources leaves LICENSE file

--- ruby.project/test/unit/src/org/netbeans/modules/ruby/rubyproject/RubyProjectOperationsTest.java
+++ ruby.project/test/unit/src/org/netbeans/modules/ruby/rubyproject/RubyProjectOperationsTest.java
@@ -24,7 +24,7 @@
  * Contributor(s):
  *
  * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2008 Sun
  * Microsystems, Inc. All Rights Reserved.
  *
  * If you wish your version of this file to be governed by only the CDDL
@@ -65,6 +65,7 @@ public class RubyProjectOperationsTest extends RubyProjectTestBase {
             prjDir.getFileObject("nbproject"),
             prjDir.getFileObject("Rakefile"),
             prjDir.getFileObject("README"),
+            prjDir.getFileObject("LICENSE"),
         };
         assertEquals("correct metadata files", Arrays.asList(expectedMetadataFiles), ProjectOperations.getMetadataFiles(project));
         
