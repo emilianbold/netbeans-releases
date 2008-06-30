877414661354b9335d800ca6c14df3c10251ca93
Marian Petras <mpetras@netbeans.org>
2008-06-30 17:14:31 +0200

    removed unused imports, star import replaced with enumeration of individual imports

--- properties/src/org/netbeans/modules/properties/PropertiesLocaleNode.java
+++ properties/src/org/netbeans/modules/properties/PropertiesLocaleNode.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -43,16 +43,10 @@ package org.netbeans.modules.properties;
 
 import java.awt.Component;
 import java.awt.datatransfer.Transferable;
-import java.awt.Dialog;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.IOException;
-import java.text.MessageFormat;
 import java.util.List;
 import javax.swing.Action;
-import javax.swing.JPanel;
 
-import org.openide.actions.*;
 import org.openide.DialogDescriptor;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
@@ -61,6 +55,17 @@ import org.openide.nodes.Node;
 import org.openide.nodes.NodeTransfer;
 import org.openide.NotifyDescriptor;
 import org.openide.DialogDisplayer;
+import org.openide.actions.CopyAction;
+import org.openide.actions.CutAction;
+import org.openide.actions.DeleteAction;
+import org.openide.actions.EditAction;
+import org.openide.actions.FileSystemAction;
+import org.openide.actions.NewAction;
+import org.openide.actions.OpenAction;
+import org.openide.actions.PasteAction;
+import org.openide.actions.PropertiesAction;
+import org.openide.actions.SaveAsTemplateAction;
+import org.openide.actions.ToolsAction;
 import org.openide.util.actions.SystemAction;
 import org.openide.util.datatransfer.NewType;
 import org.openide.util.datatransfer.PasteType;
