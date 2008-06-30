19c363da85f2090f02a5c53d1f2fe2d889278303
Vince Kraemer <vkraemer@netbeans.org>
2008-06-30 09:03:49 -0700

    more test stuff

--- j2ee.sun.appsrv81/test/qa-functional/src/org/netbeans/modules/j2ee/sun/test/Util.java
+++ j2ee.sun.appsrv81/test/qa-functional/src/org/netbeans/modules/j2ee/sun/test/Util.java
@@ -97,13 +97,13 @@ public class Util {
     // SERVER PROPERTIES FOR TESTS
     public static final String _SEP = System.getProperty("file.separator");
     public static final String _DISPLAY_NAME = "Sun Java System Application Server";
-    public static final String _PLATFORM_LOCATION =  System.getProperty("sjsas.server.path");
+    public static final String _PLATFORM_LOCATION = System.getProperty("sjsas.server.path");
     public static final String _INSTALL_LOCATION = _PLATFORM_LOCATION+_SEP+"domains";
     public static final String _DOMAIN = "domain1";
     public static final String _HOST = "localhost";
     public static final String _PORT = getPort(new File(_INSTALL_LOCATION+_SEP+_DOMAIN+_SEP+"config"+_SEP+"domain.xml"));
     public static final String _USER_NAME = "admin";
-    public static final String _PASSWORD =  System.getProperty("sjsas.server.password");
+    public static final String _PASSWORD = System.getProperty("sjsas.server.password");
     public static final String _URL = "["+_PLATFORM_LOCATION+"]deployer:Sun:AppServer::"+_HOST+":"+_PORT;
     
     // SERVER PROPERTIES FOR APP SERVER REGISTRATION
