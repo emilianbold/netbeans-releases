0061f6dc7c9536e08be2c300f286832e409fbfbf
Jesse Glick <jglick@netbeans.org>
2008-07-07 12:11:59 -0400

    Unused imports.

--- java.project/src/org/netbeans/spi/java/project/support/ui/IncludeExcludeVisualizer.java
+++ java.project/src/org/netbeans/spi/java/project/support/ui/IncludeExcludeVisualizer.java
@@ -47,13 +47,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.SortedSet;
 import java.util.TreeSet;
-import java.util.regex.Pattern;
 import javax.swing.JComponent;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import org.netbeans.api.queries.VisibilityQuery;
 import org.netbeans.spi.project.support.ant.PathMatcher;
-import org.openide.util.NbPreferences;
 import org.openide.util.Parameters;
 import org.openide.util.RequestProcessor;
 
