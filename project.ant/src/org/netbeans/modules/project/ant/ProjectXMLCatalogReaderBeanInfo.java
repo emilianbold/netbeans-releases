f6c7a44810897b27f0a75cda3350efba8343f01c
Jesse Glick <jglick@netbeans.org>
2008-07-11 16:22:53 -0400

    Issue #42686: consistent validation of project metadata according to schema. 1. XMLUtil.validate utility method added. 2. XML catalog for project metadata now read from ProjectXMLCatalog in the system filesystem, not the web.    Register http://www.netbeans.org/ns/foo/1 as ProjectXMLCatalog/foo/1.xsd to be found. 3. Validate project.xml/private.xml according to schemas found in ProjectXMLCatalog:    a. When loading a project, if project.xml is invalid then make the load fail.       (Effectively preventing older IDEs from trying to load newer IDEs' projects, if schemas changed.)    b. Validate before saving project.xml/private.xml, to guard against buggy module code.    c. Validate before loading project.xml/private.xml, to guard against e.g. bad user edits or VCS conflict markers.    Note that e.g. <configuration> uses lax subelement processing, so children with no matching schemas will be skipped. 4. Remove logic to display validation errors of freeform project.xml in Output Window.    Users can anyway use A-S-F9 to use the regular Validate action before saving. 5. ant.freeform.ProjectNature.getSchemas removed (natures need no longer implement it).

--- project.ant/src/org/netbeans/modules/project/ant/ProjectXMLCatalogReaderBeanInfo.java
+++ project.ant/src/org/netbeans/modules/project/ant/ProjectXMLCatalogReaderBeanInfo.java
@@ -39,71 +39,21 @@
  * made subject to such option by the copyright holder.
  */
 
-package org.netbeans.modules.project.ui;
+package org.netbeans.modules.project.ant;
 
-import java.awt.Image;
-import java.beans.PropertyChangeListener;
-import java.util.Collections;
-import java.util.Iterator;
-import org.netbeans.modules.xml.catalog.spi.CatalogDescriptor;
-import org.netbeans.modules.xml.catalog.spi.CatalogListener;
-import org.netbeans.modules.xml.catalog.spi.CatalogReader;
-import org.openide.util.NbBundle;
-import org.openide.util.Utilities;
+import java.beans.PropertyDescriptor;
+import java.beans.SimpleBeanInfo;
 
 /**
- * Supplies a catalog which lets users validate against project-related XML schemas.
+ * (Non-)description of {@link ProjectXMLCatalogReader}.
  * @author Jesse Glick
- * @see "issue #49976"
  */
-public class ProjectXMLCatalogReader implements CatalogReader, CatalogDescriptor {
-    
-    private static final String PREFIX = "http://www.netbeans.org/ns/"; // NOI18N
-    private static final String SUFFIX = ".xsd"; // NOI18N
-    
-    /** Default constructor for use from layer. */
-    public ProjectXMLCatalogReader() {}
+public class ProjectXMLCatalogReaderBeanInfo extends SimpleBeanInfo {
 
-    public String resolveURI(String name) {
-        if (name.startsWith(PREFIX)) {
-            return name + SUFFIX;
-        } else {
-            return null;
-        }
+    public ProjectXMLCatalogReaderBeanInfo() {}
+
+    public PropertyDescriptor[] getPropertyDescriptors() {
+        return new PropertyDescriptor[0];
     }
 
-    public String resolvePublic(String publicId) {
-        return null;
-    }
-
-    public String getSystemID(String publicId) {
-        return null;
-    }
-
-    public void removePropertyChangeListener(PropertyChangeListener l) {}
-
-    public void addPropertyChangeListener(PropertyChangeListener l) {}
-
-    public void removeCatalogListener(CatalogListener l) {}
-
-    public void addCatalogListener(CatalogListener l) {}
-
-    public Image getIcon(int type) {
-        return Utilities.loadImage("org/netbeans/modules/project/ui/resources/projectTab.png", true);
-    }
-
-    public void refresh() {}
-
-    public String getShortDescription() {
-        return NbBundle.getMessage(ProjectXMLCatalogReader.class, "HINT_project_xml_schemas");
-    }
-
-    public Iterator getPublicIDs() {
-        return Collections.EMPTY_SET.iterator();
-    }
-
-    public String getDisplayName() {
-        return NbBundle.getMessage(ProjectXMLCatalogReader.class, "LBL_project_xml_schemas");
-    }
-    
 }
