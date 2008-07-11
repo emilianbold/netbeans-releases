81a789c75add55faf59432a0f9e67a1ee7fa9d3f
Tomasz Slota <tslota@netbeans.org>
2008-07-11 15:23:29 +0200

    breaking up some spaghetti code

--- php.editor/src/org/netbeans/modules/php/editor/PHPDocRenderer.java
+++ php.editor/src/org/netbeans/modules/php/editor/PHPDocRenderer.java
@@ -80,205 +80,225 @@ class PHPDocRenderer {
         }
 
         if (element instanceof IndexedElement) {
-            final IndexedElement indexedElement = (IndexedElement) element;
-            StringBuilder description = new StringBuilder();
-            final CCDocHtmlFormatter header = new CCDocHtmlFormatter();
-
-            String location = indexedElement.getFile().isPlatform() ? NbBundle.getMessage(PHPCodeCompletion.class, "PHPPlatform")
-                    : indexedElement.getFilenameUrl();
-
-            header.appendHtml(String.format("<font size=-1>%s</font>", location));
-
-            final StringBuilder phpDoc = new StringBuilder();
-
-            if (indexedElement.getOffset() > -1) {
-                FileObject fo = element.getFileObject();
-                SourceModel model = SourceModelFactory.getInstance().getModel(fo);
-                try {
-
-                    model.runUserActionTask(new CancellableTask<CompilationInfo>() {
-
-                        public void cancel() {
-                        }
-
-                        public void run(CompilationInfo ci) throws Exception {
-                            ParserResult presult = ci.getEmbeddedResults(PHPLanguage.PHP_MIME_TYPE).iterator().next();
-                            Program program = Utils.getRoot(presult.getInfo());
-
-                            if (program != null) {
-                                ASTNode node = Utils.getNodeAtOffset(program, indexedElement.getOffset());
-                                header.appendHtml("<p><font size=+1>"); //NOI18N
-
-                                if (node instanceof FunctionDeclaration) {
-                                    FunctionDeclaration functionDeclaration = (FunctionDeclaration) node;
-                                    String fname = CodeUtils.extractFunctionName(functionDeclaration);
-                                    header.name(ElementKind.METHOD, true);
-                                    header.appendText(fname);
-                                    header.name(ElementKind.METHOD, false);
-                                    header.appendHtml("</font>");
-
-                                    header.parameters(true);
-                                    header.appendText("("); //NOI18N
-                                    int paramCount = functionDeclaration.getFormalParameters().size();
-
-                                    for (int i = 0; i < paramCount; i++) {
-                                        FormalParameter param = functionDeclaration.getFormalParameters().get(i);
-
-                                        if (param.getParameterType() != null) {
-                                            header.type(true);
-                                            header.appendText(param.getParameterType().getName() + " "); //NOI18N
-                                            header.type(false);
-                                        }
-
-                                        header.appendText(CodeUtils.getParamDisplayName(param));
-
-                                        if (param.getDefaultValue() != null) {
-                                            header.type(true);
-                                            header.appendText("=");
-
-                                            if (param.getDefaultValue() instanceof Scalar) {
-                                                Scalar scalar = (Scalar) param.getDefaultValue();
-                                                header.appendText(scalar.getStringValue());
-                                            }
-
-                                            header.type(false);
-                                        }
-
-                                        if (i + 1 < paramCount) {
-                                            header.appendText(", "); //NOI18N
-                                        }
-                                    }
-
-                                    header.appendText(")");
-                                    header.parameters(false);
-
-                                } else {
-                                    header.name(indexedElement.getKind(), true);
-                                    header.appendText(indexedElement.getDisplayName());
-                                    header.name(indexedElement.getKind(), false);
-                                }
-
-                                header.appendHtml("</p><br>"); //NOI18N
-
-                                Comment comment = Utils.getCommentForNode(program, node);
-
-                                if (comment instanceof PHPDocBlock) {
-                                    StringBuilder params = new StringBuilder();
-                                    StringBuilder links = new StringBuilder();
-                                    StringBuilder returnValue = new StringBuilder();
-                                    StringBuilder others = new StringBuilder();
-
-
-                                    PHPDocBlock pHPDocBlock = (PHPDocBlock) comment;
-                                    phpDoc.append(pHPDocBlock.getDescription());
-
-                                    // list PHPDoc tags
-                                    // TODO a better support for PHPDoc tags
-                                    phpDoc.append("<br>\n"); //NOI18N
-
-                                    for (PHPDocTag tag : pHPDocBlock.getTags()) {
-
-                                        switch (tag.getKind()) {
-                                            case PARAM:
-                                                String parts[] = tag.getValue().split("\\s+", 3); //NOI18N
-                                                String paramName,
-                                                 paramType,
-                                                 paramDesc;
-                                                paramName = paramType = paramDesc = ""; //NOI18N
-
-                                                if (parts.length > 0) {
-                                                    paramName = parts[0];
-                                                    if (parts.length > 1) {
-                                                        paramType = parts[1];
-                                                        if (parts.length > 2) {
-                                                            paramDesc = parts[2];
-                                                        }
-                                                    }
-                                                }
-
-                                                String optionalStr = "[optional]"; //NOI18N
-                                                if (paramType.endsWith(optionalStr)) {
-                                                    paramType = paramType.substring(0, paramType.length() - optionalStr.length());
-                                                }
-
-                                                String pline = String.format("<tr><td align=\"right\">%s</td><th  align=\"left\">$%s</th><td>%s</td></tr>\n", //NOI18N
-                                                        paramType, paramName, paramDesc);
-
-                                                params.append(pline);
-                                                break;
-                                            case LINK:
-                                                String lline = String.format("<a href=\"%s\">%s</a><br>\n", //NOI18N
-                                                        tag.getValue(), tag.getValue());
-
-                                                links.append(lline);
-                                                break;
-                                            case RETURN:
-                                                String rparts[] = tag.getValue().split("\\s+", 2); //NOI18N
-
-                                                if (rparts.length > 0) {
-                                                    String type = rparts[0];
-                                                    returnValue.append(String.format("<b>%s:</b> %s<br><br>", //NOI18N
-                                                            NbBundle.getMessage(PHPCodeCompletion.class, "Type"), type));
-
-                                                    if (rparts.length > 1) {
-                                                        String desc = rparts[1];
-                                                        returnValue.append(desc);
-                                                    }
-                                                }
-
-                                                break;
-                                            default:
-                                                String oline = String.format("<tr><th>%s</th><td>%s</td></tr>\n", //NOI18N
-                                                        tag.getKind().toString(), tag.getValue());
-
-                                                others.append(oline);
-                                                break;
-                                        }
-                                    }
-
-
-                                    if (params.length() > 0) {
-                                        phpDoc.append("<h3>"); //NOI18N
-                                        phpDoc.append(NbBundle.getMessage(PHPCodeCompletion.class, "Parameters"));
-                                        phpDoc.append("</h3>\n<table>\n" + params + "</table>\n"); //NOI18N
-                                    }
-
-                                    if (returnValue.length() > 0) {
-                                        phpDoc.append("<h3>"); //NOI18N
-                                        phpDoc.append(NbBundle.getMessage(PHPCodeCompletion.class, "ReturnValue"));
-                                        phpDoc.append("</h3>\n" + returnValue); //NOI18N
-                                    }
-
-                                    if (links.length() > 0) {
-                                        phpDoc.append("<h3>"); //NOI18N
-                                        phpDoc.append(NbBundle.getMessage(PHPCodeCompletion.class, "OnlineDocs"));
-                                        phpDoc.append("</h3>\n" + links); //NOI18N
-                                    }
-
-                                    if (others.length() > 0) {
-                                        phpDoc.append("<table>\n" + others + "</table>\n"); //NOI18N
-                                    }
-                                }
-                            }
-
-                        }
-                    }, true);
-
-                } catch (IOException ex) {
-                    Exceptions.printStackTrace(ex);
-                }
-            }
-
-            if (phpDoc.length() > 0) {
-                description.append(phpDoc);
-            } else {
-                description.append(NbBundle.getMessage(PHPCodeCompletion.class, "PHPDocNotFound"));
-            }
-
-            return header.getText() + description.toString();
+            return documentIndexedElement(info, (IndexedElement) element);
         }
 
         return null;
+    }
+
+    private static String documentIndexedElement(CompilationInfo info, IndexedElement indexedElement) {
+
+        StringBuilder description = new StringBuilder();
+        final CCDocHtmlFormatter header = new CCDocHtmlFormatter();
+
+        String location = indexedElement.getFile().isPlatform() ? NbBundle.getMessage(PHPCodeCompletion.class, "PHPPlatform")
+                : indexedElement.getFilenameUrl();
+
+        header.appendHtml(String.format("<font size=-1>%s</font>", location));
+
+        final StringBuilder phpDoc = new StringBuilder();
+
+        if (indexedElement.getOffset() > -1) {
+            FileObject fo = indexedElement.getFileObject();
+            SourceModel model = SourceModelFactory.getInstance().getModel(fo);
+            try {
+                model.runUserActionTask(new PHPDocExtractor(
+                        header, phpDoc, indexedElement), true);
+
+            } catch (IOException ex) {
+                Exceptions.printStackTrace(ex);
+            }
+        }
+
+        if (phpDoc.length() > 0) {
+            description.append(phpDoc);
+        } else {
+            description.append(NbBundle.getMessage(PHPCodeCompletion.class, "PHPDocNotFound"));
+        }
+
+        return header.getText() + description.toString();
+
+    }
+
+    private static class PHPDocExtractor implements CancellableTask<CompilationInfo> {
+
+        private CCDocHtmlFormatter header;
+        private StringBuilder phpDoc;
+        private IndexedElement indexedElement;
+
+        public PHPDocExtractor(CCDocHtmlFormatter header, StringBuilder phpDoc, IndexedElement indexedElement) {
+            this.header = header;
+            this.phpDoc = phpDoc;
+            this.indexedElement = indexedElement;
+        }
+
+        public void cancel() {
+        }
+
+        private void doFunctionDeclaration(FunctionDeclaration functionDeclaration) {
+            String fname = CodeUtils.extractFunctionName(functionDeclaration);
+            header.name(ElementKind.METHOD, true);
+            header.appendText(fname);
+            header.name(ElementKind.METHOD, false);
+            header.appendHtml("</font>");
+
+            header.parameters(true);
+            header.appendText("("); //NOI18N
+            int paramCount = functionDeclaration.getFormalParameters().size();
+
+            for (int i = 0; i < paramCount; i++) {
+                FormalParameter param = functionDeclaration.getFormalParameters().get(i);
+
+                if (param.getParameterType() != null) {
+                    header.type(true);
+                    header.appendText(param.getParameterType().getName() + " "); //NOI18N
+                    header.type(false);
+                }
+
+                header.appendText(CodeUtils.getParamDisplayName(param));
+
+                if (param.getDefaultValue() != null) {
+                    header.type(true);
+                    header.appendText("=");
+
+                    if (param.getDefaultValue() instanceof Scalar) {
+                        Scalar scalar = (Scalar) param.getDefaultValue();
+                        header.appendText(scalar.getStringValue());
+                    }
+
+                    header.type(false);
+                }
+
+                if (i + 1 < paramCount) {
+                    header.appendText(", "); //NOI18N
+                }
+            }
+
+            header.appendText(")");
+            header.parameters(false);
+        }
+       
+        private void extractPHPDoc(PHPDocBlock pHPDocBlock) {
+            StringBuilder params = new StringBuilder();
+            StringBuilder links = new StringBuilder();
+            StringBuilder returnValue = new StringBuilder();
+            StringBuilder others = new StringBuilder();
+
+            phpDoc.append(pHPDocBlock.getDescription());
+
+            // list PHPDoc tags
+            phpDoc.append("<br>\n"); //NOI18N
+
+            for (PHPDocTag tag : pHPDocBlock.getTags()) {
+
+                switch (tag.getKind()) {
+                    case PARAM:
+                        String parts[] = tag.getValue().split("\\s+", 3); //NOI18N
+                        String paramName,
+                         paramType,
+                         paramDesc;
+                        paramName = paramType = paramDesc = ""; //NOI18N
+
+                        if (parts.length > 0) {
+                            paramName = parts[0];
+                            if (parts.length > 1) {
+                                paramType = parts[1];
+                                if (parts.length > 2) {
+                                    paramDesc = parts[2];
+                                }
+                            }
+                        }
+
+                        String optionalStr = "[optional]"; //NOI18N
+                        if (paramType.endsWith(optionalStr)) {
+                            paramType = paramType.substring(0, paramType.length() - optionalStr.length());
+                        }
+
+                        String pline = String.format("<tr><td align=\"right\">%s</td><th  align=\"left\">$%s</th><td>%s</td></tr>\n", //NOI18N
+                                paramType, paramName, paramDesc);
+
+                        params.append(pline);
+                        break;
+                    case LINK:
+                        String lline = String.format("<a href=\"%s\">%s</a><br>\n", //NOI18N
+                                tag.getValue(), tag.getValue());
+
+                        links.append(lline);
+                        break;
+                    case RETURN:
+                        String rparts[] = tag.getValue().split("\\s+", 2); //NOI18N
+
+                        if (rparts.length > 0) {
+                            String type = rparts[0];
+                            returnValue.append(String.format("<b>%s:</b> %s<br><br>", //NOI18N
+                                    NbBundle.getMessage(PHPCodeCompletion.class, "Type"), type));
+
+                            if (rparts.length > 1) {
+                                String desc = rparts[1];
+                                returnValue.append(desc);
+                            }
+                        }
+
+                        break;
+                    default:
+                        String oline = String.format("<tr><th>%s</th><td>%s</td></tr>\n", //NOI18N
+                                tag.getKind().toString(), tag.getValue());
+
+                        others.append(oline);
+                        break;
+                }
+            }
+
+
+            if (params.length() > 0) {
+                phpDoc.append("<h3>"); //NOI18N
+                phpDoc.append(NbBundle.getMessage(PHPCodeCompletion.class, "Parameters"));
+                phpDoc.append("</h3>\n<table>\n" + params + "</table>\n"); //NOI18N
+            }
+
+            if (returnValue.length() > 0) {
+                phpDoc.append("<h3>"); //NOI18N
+                phpDoc.append(NbBundle.getMessage(PHPCodeCompletion.class, "ReturnValue"));
+                phpDoc.append("</h3>\n" + returnValue); //NOI18N
+            }
+
+            if (links.length() > 0) {
+                phpDoc.append("<h3>"); //NOI18N
+                phpDoc.append(NbBundle.getMessage(PHPCodeCompletion.class, "OnlineDocs"));
+                phpDoc.append("</h3>\n" + links); //NOI18N
+            }
+
+            if (others.length() > 0) {
+                phpDoc.append("<table>\n" + others + "</table>\n"); //NOI18N
+            }
+        }
+
+        public void run(CompilationInfo ci) throws Exception {
+            ParserResult presult = ci.getEmbeddedResults(PHPLanguage.PHP_MIME_TYPE).iterator().next();
+            Program program = Utils.getRoot(presult.getInfo());
+
+            if (program != null) {
+                ASTNode node = Utils.getNodeAtOffset(program, indexedElement.getOffset());
+                header.appendHtml("<p><font size=+1>"); //NOI18N
+
+                if (node instanceof FunctionDeclaration) {
+                    doFunctionDeclaration((FunctionDeclaration) node);
+                } else {
+                    header.name(indexedElement.getKind(), true);
+                    header.appendText(indexedElement.getDisplayName());
+                    header.name(indexedElement.getKind(), false);
+                }
+
+                header.appendHtml("</p><br>"); //NOI18N
+
+                Comment comment = Utils.getCommentForNode(program, node);
+
+                if (comment instanceof PHPDocBlock) {
+                    extractPHPDoc((PHPDocBlock) comment);
+                }
+            }
+
+        }
     }
 }
 
