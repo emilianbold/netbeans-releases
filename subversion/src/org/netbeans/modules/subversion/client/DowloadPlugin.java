b2a480b2c4814f337189a7066c3c73e95148e6f7
Tomas Stupka <tstupka@netbeans.org>
2008-07-15 17:41:48 +0200

    ocate javahl on windows

--- subversion/src/org/netbeans/modules/subversion/client/DowloadPlugin.java
+++ subversion/src/org/netbeans/modules/subversion/client/DowloadPlugin.java
@@ -107,7 +107,7 @@ public class DowloadPlugin implements ActionListener {
                 try {
                     List<UpdateUnit> units = UpdateManager.getDefault().getUpdateUnits(UpdateManager.TYPE.MODULE);
                     for (UpdateUnit u : units) {
-                        if(u.getCodeName().equals("org.netbeans.libs.svnjavahlwin32")) {
+                        if(u.getCodeName().equals(SvnClientFactory.JAVAHL_MODULE_CODE_NAME)) {
 
                             List<UpdateElement> elements = u.getAvailableUpdates();
                             if(elements.size() == 0) {
