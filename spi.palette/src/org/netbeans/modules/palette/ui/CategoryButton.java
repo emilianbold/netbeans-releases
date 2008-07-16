5ef5297e050684f603376ae5f6464ca04b0405e5
S. Aubrecht <saubrecht@netbeans.org>
2008-07-16 17:40:02 +0200

    138493 - [MAC] palette: selected category name is badly visible

--- spi.palette/src/org/netbeans/modules/palette/ui/CategoryButton.java
+++ spi.palette/src/org/netbeans/modules/palette/ui/CategoryButton.java
@@ -161,6 +161,12 @@ class CategoryButton extends JCheckBox implements Autoscroll {
         setToolTipText( category.getShortDescription() );
         getAccessibleContext().setAccessibleName( category.getDisplayName() );
         getAccessibleContext().setAccessibleDescription( category.getShortDescription() );
+        if( isAqua ) {
+            setContentAreaFilled(true);
+            setOpaque(true);
+            setBackground( new Color(0,0,0) );
+            setForeground( new Color(255,255,255) );
+        }
     }
     
     Category getCategory() {
@@ -204,6 +210,7 @@ class CategoryButton extends JCheckBox implements Autoscroll {
         return support;
     }
 
+    @Override
     public Color getBackground() {
         if( isFocusOwner() ) {
             if( isAqua )
@@ -218,12 +225,29 @@ class CategoryButton extends JCheckBox implements Autoscroll {
         }
     }
 
+    @Override
     public Color getForeground() {
         if( isFocusOwner() ) {
             if( isAqua )
                 return UIManager.getColor( "Table.selectionForeground" ); //NOI18N
             return UIManager.getColor( "PropSheet.selectedSetForeground" ); //NOI18N
         } else {
+            if( isAqua ) {
+                Color res = UIManager.getColor("PropSheet.setForeground"); //NOI18N
+
+                if (res == null) {
+                    res = UIManager.getColor("Table.foreground"); //NOI18N
+
+                    if (res == null) {
+                        res = UIManager.getColor("textText");
+
+                        if (res == null) {
+                            res = Color.BLACK;
+                        }
+                    }
+                }
+                return res;
+            }
             return super.getForeground();
         }
     }
