bcbfed660866faebc9369b29e804d3fdcf6211b5
Dafe Simonek <dsimonek@netbeans.org>
2008-07-16 14:44:57 +0200

    #140297: selection behaviour improved, three dot visible again if more results are available

--- spi.quicksearch/src/org/netbeans/modules/quicksearch/SearchResultRender.java
+++ spi.quicksearch/src/org/netbeans/modules/quicksearch/SearchResultRender.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- * 
+ *
  * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
- * 
+ *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
  * Development and Distribution License("CDDL") (collectively, the
@@ -20,7 +20,7 @@
  * License Header, with the fields enclosed by brackets [] replaced by
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- * 
+ *
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -31,9 +31,9 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
- * 
+ *
  * Contributor(s):
- * 
+ *
  * Portions Copyrighted 2008 Sun Microsystems, Inc.
  */
 package org.netbeans.modules.quicksearch;
@@ -61,7 +61,7 @@ import org.openide.util.Utilities;
  * @author Jan Becicka
  */
 class SearchResultRender extends JLabel implements ListCellRenderer {
-    
+
     private static final boolean IS_GTK = "GTK".equals(UIManager.getLookAndFeel().getID()); //NOI18N
 
     private QuickSearchPopup popup;
@@ -86,10 +86,11 @@ class SearchResultRender extends JLabel implements ListCellRenderer {
         if (!(value instanceof ItemResult)) {
             return null;
         }
-        
+
         ItemResult ir = (ItemResult) value;
         List<? extends KeyStroke> shortcut = ir.getShortcut();
-        truncateAndSetTextToLabel(ir.getDisplayName(), resultLabel);
+        resultLabel.setText(ir.getDisplayName());
+        truncateLabel(resultLabel);
         if (shortcut != null && shortcut.size() > 0 && shortcut.get(0) != null) {
             // TBD - display multi shortcuts
             shortcutLabel.setText(getKeyStrokeAsText(shortcut.get(0)));
@@ -189,13 +190,14 @@ class SearchResultRender extends JLabel implements ListCellRenderer {
     /** Truncate text and put "..." at the end if text exceeds given JLabel
      * coordinates - workaround fo JLabel inability to truncate html
      */
-    private static void truncateAndSetTextToLabel (String text, JLabel label) {
+    private static void truncateLabel (JLabel label) {
+        String text = label.getText();
+
         // no need to truncate non html text, JLabel will do it itself
         if (!text.startsWith("<html>")) {
             return;
         }
 
-        label.setText(text);
         int prefWidth = label.getPreferredSize().width;
         int curWidth = label.getWidth();
         if (curWidth > 0 && prefWidth > curWidth) {
