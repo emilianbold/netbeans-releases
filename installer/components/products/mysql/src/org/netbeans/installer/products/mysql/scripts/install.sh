9d6ab5771b75199d496efecfb647106aa7d2ee16
Dmitry Lipin <dlipin@netbeans.org>
2008-07-30 12:36:38 +0400

    Fix basedir and datadir replacement in my.cnf

--- installer/components/products/mysql/src/org/netbeans/installer/products/mysql/scripts/install.sh
+++ installer/components/products/mysql/src/org/netbeans/installer/products/mysql/scripts/install.sh
@@ -106,11 +106,8 @@ DEFAULT_MYSQL_DIR=/usr/local/mysql
 sed -e "s/`escape $DEFAULT_MYSQL_DIR`/`escape $INSTALLDIR`/g" ./my.cnf > ./my.cnf.tmp && mv ./my.cnf.tmp ./my.cnf
 
 #Set basedir and datadir in support-files/mysql.server and my.cnf files
-instesc=`escape "$INSTALLDIR"`
-dataesc=`escape "$INSTALLDIR/data"`
-
-sed -e "s/\[mysqld\]/\[mysqld\]\nbasedir=$instesc\ndatadir=$dataesc\n/g" ./my.cnf > ./my.cnf.tmp && mv ./my.cnf.tmp ./my.cnf
-sed -e "s/^basedir=/basedir=$instesc/g" ./support-files/mysql.server > ./support-files/mysql.server.tmp 
+awk '{ print $i ; if($i=="[mysqld]") { print a ; print b }}' a="basedir = $INSTALLDIR" b="datadir = $INSTALLDIR/data" < ./my.cnf > my.cnf.tmp && mv ./my.cnf.tmp ./my.cnf
+sed -e "s/^basedir=/basedir=`escape $INSTALLDIR`/g" ./support-files/mysql.server > ./support-files/mysql.server.tmp 
 touch -r ./support-files/mysql.server ./support-files/mysql.server.tmp
 mv ./support-files/mysql.server.tmp ./support-files/mysql.server
 
