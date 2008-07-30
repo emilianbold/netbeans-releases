25f958ca0dac64ebe167f7fe5a3a294e3657db7f
Jesse Glick <jglick@netbeans.org>
2008-07-30 14:19:12 -0400

    #142205: fix XML validation errors creating NBMs. Also verify that Info.xml and updates.xml files are valid according to specified DTD.

--- nbbuild/antsrc/org/netbeans/nbbuild/MakeNBM.java
+++ nbbuild/antsrc/org/netbeans/nbbuild/MakeNBM.java
@@ -42,6 +42,8 @@
 package org.netbeans.nbbuild;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -51,6 +53,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URI;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -68,15 +71,22 @@ import java.util.zip.CRC32;
 import java.util.zip.ZipEntry;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import org.apache.tools.ant.AntClassLoader;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.Task;
 import org.apache.tools.ant.taskdefs.Jar;
 import org.apache.tools.ant.taskdefs.SignJar;
+import org.apache.tools.ant.types.Path;
 import org.apache.tools.ant.types.ZipFileSet;
 import org.w3c.dom.DOMImplementation;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.xml.sax.EntityResolver;
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
 
 /** Makes a <code>.nbm</code> (<b>N</b>et<b>B</b>eans <b>M</b>odule) file.
  *
@@ -313,6 +323,7 @@ public class MakeNBM extends Task {
     private ArrayList<String> locales = null;
     private ArrayList<Attributes> moduleAttributes = null;
     private Attributes englishAttr = null;
+    private Path updaterJar;
     
     /** Try to find and create localized info.xml files */
     public void setLocales(String s) {
@@ -427,6 +438,11 @@ public class MakeNBM extends Task {
     
     public ZipFileSet createMain () {
         return (main = new ZipFileSet());
+    }
+
+    /** Fileset for platform/modules/ext/updater.jar, to be used in DTD validation. */
+    public Path createUpdaterJar() {
+        return updaterJar = new Path(getProject());
     }
     
     private Attributes getModuleAttributesForLocale(String locale) throws BuildException {
@@ -717,7 +733,8 @@ public class MakeNBM extends Task {
         }
         
         String pub, sys;
-        if (attr.getValue("AutoUpdate-Show-In-Client") != null || attr.getValue("AutoUpdate-Essential-Module") != null) {
+        if (attr.getValue("AutoUpdate-Show-In-Client") != null || attr.getValue("AutoUpdate-Essential-Module") != null ||
+                attr.getValue("OpenIDE-Module-Recommends") != null || attr.getValue("OpenIDE-Module-Needs") != null) {
             pub = "-//NetBeans//DTD Autoupdate Module Info 2.5//EN";
             sys = "http://www.netbeans.org/dtds/autoupdate-info-2_5.dtd";
         } else if (targetcluster != null && !("".equals(targetcluster))) {
@@ -814,18 +831,10 @@ public class MakeNBM extends Task {
         it = attrNames.iterator();
         while (it.hasNext()) {
             String name = (String) it.next();
-            // Ignore irrelevant attributes (cf. www/www/dtds/autoupdate-catalog-*.dtd
-            //  and www/www/dtds/autoupdate-info-*.dtd):
-            // XXX better would be to enumerate the attrs it *does* recognize!
-            if (!name.startsWith("OpenIDE-Module") && !name.startsWith("AutoUpdate-")) continue;
-            if (name.equals("OpenIDE-Module-Localizing-Bundle")) continue;
-            if (name.equals("OpenIDE-Module-Install")) continue;
-            if (name.equals("OpenIDE-Module-Layer")) continue;
-            if (name.equals("OpenIDE-Module-Description")) continue;
-            if (name.equals("OpenIDE-Module-Package-Dependency-Message")) continue;
-            if (name.equals("OpenIDE-Module-Public-Packages")) continue;
-            if (name.equals("OpenIDE-Module-Friends")) continue;
-            el.setAttribute(name, attr.getValue(name));
+            if (name.matches("OpenIDE-Module(|-(Name|(Specification|Implementation)-Version|(Module|Package|Java|IDE)-Dependencies|" +
+                    "(Short|Long)-Description|Display-Category|Provides|Requires|Recommends|Needs))|AutoUpdate-(Show-In-Client|Essential-Module)")) {
+                el.setAttribute(name, attr.getValue(name));
+            }
         }
         module.appendChild(el);
         // Maybe write out license text.
@@ -835,9 +844,42 @@ public class MakeNBM extends Task {
             el.appendChild(license.getTextNode(doc));
             module.appendChild(el);
         }
+        if (updaterJar != null && updaterJar.size() > 0) {
+            try {
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                XMLUtil.write(doc, baos);
+                validateAgainstAUDTDs(new InputSource(new ByteArrayInputStream(baos.toByteArray())), updaterJar, this);
+            } catch (Exception x) {
+                throw new BuildException("Could not validate Info.xml before writing: " + x, x, getLocation());
+            }
+        } else {
+            log("No updater.jar specified, cannot validate Info.xml against DTD", Project.MSG_WARN);
+        }
         return doc;
     }
-   
+
+    static void validateAgainstAUDTDs(InputSource input, final Path updaterJar, final Task task) throws IOException, SAXException {
+        XMLUtil.parse(input, true, false, new ErrorHandler() {
+            public void warning(SAXParseException exception) throws SAXException {throw exception;}
+            public void error(SAXParseException exception) throws SAXException {throw exception;}
+            public void fatalError(SAXParseException exception) throws SAXException {throw exception;}
+        }, new EntityResolver() {
+            ClassLoader loader = new AntClassLoader(task.getProject(), updaterJar);
+            public InputSource resolveEntity(String publicId, String systemId) throws SAXException, IOException {
+                String remote = "http://www.netbeans.org/dtds/";
+                if (systemId.startsWith(remote)) {
+                    String rsrc = "org/netbeans/updater/resources/" + systemId.substring(remote.length());
+                    URL u = loader.getResource(rsrc);
+                    if (u != null) {
+                        return new InputSource(u.toString());
+                    } else {
+                        task.log(rsrc + " not found in " + updaterJar, Project.MSG_WARN);
+                    }
+                }
+                return null;
+            }
+        });
+    }
 
   /** This returns true if the license should be overridden. */
   protected boolean overrideLicense() {
