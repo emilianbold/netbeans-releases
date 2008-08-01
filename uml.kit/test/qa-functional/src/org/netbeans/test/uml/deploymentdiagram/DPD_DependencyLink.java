7e7cd9385946440e6f8ad0db8b1d53b48aa11d52
Ivan Sidorkin <ivansidorkin@netbeans.org>
2008-08-02 03:40:53 +0400

    Migrated UML tests

--- uml.kit/test/qa-functional/src/org/netbeans/test/uml/deploymentdiagram/DPD_DependencyLink.java
+++ uml.kit/test/qa-functional/src/org/netbeans/test/uml/deploymentdiagram/DPD_DependencyLink.java
@@ -74,7 +74,7 @@ public class DPD_DependencyLink extends UMLTestCase {
     private static String prName = "UMLProjectDPD10";
     private static String dpdName = "DPD";
     
-    private static final String workDir = System.getProperty("xtest.workdir");
+    private static final String workDir = System.getProperty("nbjunit.workdir");
     private static String OUT_LOG_FILE = "";
     private static String ERR_LOG_FILE = "";
     private static PrintStream myOut = null;
