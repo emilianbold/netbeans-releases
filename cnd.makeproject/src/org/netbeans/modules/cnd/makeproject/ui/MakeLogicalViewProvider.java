e0d0fa4f6f706a1b56533b5ec1f563460beb1c04
Sergey Grinev <sergius@netbeans.org>
2008-08-01 19:20:32 +0400

    fixed #142240 Exception appears if user deletes project

--- cnd.makeproject/src/org/netbeans/modules/cnd/makeproject/ui/MakeLogicalViewProvider.java
+++ cnd.makeproject/src/org/netbeans/modules/cnd/makeproject/ui/MakeLogicalViewProvider.java
@@ -1168,7 +1168,7 @@ public class MakeLogicalViewProvider implements LogicalViewProvider {
         }
     }
 
-    private static final int WAIT_DELAY = 50;
+    private static final int WAIT_DELAY = 5000;
     private abstract class BaseMakeViewChildren extends Children.Keys implements ChangeListener {
         private final Folder folder;
 
@@ -1191,8 +1191,12 @@ public class MakeLogicalViewProvider implements LogicalViewProvider {
                     folder.addChangeListener( this );
                     RequestProcessor.getDefault().post(new Runnable() {
                         public void run() {
+                            // between posting this task and running it can be become deleted (see iz #142240)
+                            // TODO: fix workflow instead?
+                            if (project.getProjectDirectory()!=null && project.getProjectDirectory().isValid()) {
                             //System.err.println("ExternalFilesChildren: setting real nodes");
-                            setKeys( getKeys() );
+                                setKeys( getKeys() );
+                            }
                         }
                     }, WAIT_DELAY);
                 }  else {
