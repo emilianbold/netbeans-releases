20f92c2008e4c9e7add918620157db616214d92a
Vita Stejskal <vstejskal@netbeans.org>
2008-08-08 11:54:04 +0200

    #141974: javadoc

--- options.editor/src/org/netbeans/modules/options/editor/spi/PreferencesCustomizer.java
+++ options.editor/src/org/netbeans/modules/options/editor/spi/PreferencesCustomizer.java
@@ -44,17 +44,75 @@ import javax.swing.JComponent;
 import org.openide.util.HelpCtx;
 
 /**
+ * A customizer for editor preferences. This interface can be used for implementing
+ * a UI panel, which allows users to controll editor preferences.
+ *
+ * <p class="nonnormative">This interface is currently used for editor formatting
+ * settings and their customizers supplied by particular language support modules.
+ * The customizers are shown in both the Tools-Options dialog and the project properties
+ * dialog for an individual project.
+ *
+ * <p>Modules can register <code>PreferencesCustomizer.Factory</code> in
+ * <code>OptionsDialog/Editor/Formatting/&lt;mime-type&gt;</code> folder on the
+ * system filesystem. The editor options infrastructure will use registered factories
+ * for creating <code>PreferencesCustomizer</code>s and their UI components, which
+ * it will then show in the appropriate dialog.
  *
  * @author vita
  */
 public interface PreferencesCustomizer {
 
+    /**
+     * Gets an id that will uniquely identify this customizer.
+     *
+     * @return The customizer's identification.
+     */
     String getId();
+
+    /**
+     * Gets the customizer's display name. The display name will be shown to
+     * a user whenever it is needed to refer to this customizer.
+     *
+     * @return The customizer's display name.
+     */
     String getDisplayName();
+
+    /**
+     * Gets the help context for showing help information relevant for this customizer.
+     *
+     * @return The customizer's help context.
+     */
     HelpCtx getHelpCtx();
+
+    /**
+     * Gets the component with UI controls for customizing preferences controlled
+     * by this customizer. The UI controls shoudl treat the <code>Preferences</code>
+     * instance supplied to this customizer as their model. That is it should reflect
+     * any changes done to the <code>preferences</code>' values they are
+     * presenting in the UI and also write any changes done by user back to the
+     * <code>Preferences</code> instance.
+     *
+     * @return The UI component for customizing the preferences.
+     */
     JComponent getComponent();
 
+    /**
+     * A factory for <code>PreferencesCustomizer</code>. The factory is given
+     * an instance of <code>Preferences</code>, which are the subject of customization.
+     * The <code>PreferencesCustomizer</code> created by the factory is expected to
+     * read and write values from the <code>Preferences</code> instance passed to
+     * the {@link Factory#create(java.util.prefs.Preferences)} method.
+     */
     public static interface Factory {
+
+        /**
+         * Creates a new <code>PreferencesCustomizer</code>.
+         *
+         * @param preferences The <code>Preferences</code> instance containing values
+         *   that should be customized.
+         * @return A new <code>PreferencesCustomizer</code> which will provide
+         *   UI for customizing the <code>preferences</code>
+         */
         PreferencesCustomizer create(Preferences preferences);
     }
 }
