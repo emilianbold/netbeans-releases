57416ee282db4a566c99578f88d793b66eea1ce2
Lukas Jungmann <jungi@netbeans.org>
2008-08-11 00:52:26 +0200

    jersey-0.9ea => jersey-0.9ea-latest migration reflected in tests

--- websvc.restkit/test/qa-functional/data/projects/NodesSample/src/java/o/n/m/qa/test/ItemResource.java
+++ websvc.restkit/test/qa-functional/data/projects/NodesSample/src/java/o/n/m/qa/test/ItemResource.java
@@ -9,15 +9,14 @@
 
 package o.n.m.qa.test;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.ProduceMime;
-import javax.ws.rs.ConsumeMime;
 import javax.ws.rs.PUT;
 import javax.ws.rs.GET;
 import javax.ws.rs.DELETE;
-import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 /**
  * REST Web Service
@@ -39,7 +38,7 @@ public class ItemResource {
      * @return an instance of java.lang.String
      */
     @GET
-    @ProduceMime("application/xml")
+    @Produces("application/xml")
     public String getXml(@PathParam("name")
     String name) {
         //TODO return proper representation object
@@ -53,7 +52,7 @@ public class ItemResource {
      * @return an HTTP response with content of the updated or created resource.
      */
     @PUT
-    @ConsumeMime("application/xml")
+    @Consumes("application/xml")
     public void putXml(@PathParam("name")
     String name, String content) {
     }
