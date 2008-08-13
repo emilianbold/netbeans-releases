f3bb4ad69d5114d81c77fc5673536e787b87684e
Jan Lahoda <jlahoda@netbeans.org>
2008-08-13 10:54:14 +0200

    Spelling.

--- java.source/src/org/netbeans/api/java/source/ElementUtilities.java
+++ java.source/src/org/netbeans/api/java/source/ElementUtilities.java
@@ -118,7 +118,7 @@ public final class ElementUtilities {
     
     /**
      * Returns the type element within which this member or constructor
-     * is declared. Does not accept pakages
+     * is declared. Does not accept packages
      * If this is the declaration of a top-level type (a non-nested class
      * or interface), returns null.
      *
@@ -161,10 +161,10 @@ public final class ElementUtilities {
         return delegate.getImplementationOf(method, origin);
     }
     
-    /**Returns true if the given element is syntetic.
+    /**Returns true if the given element is synthetic.
      * 
      *  @param element to check
-     *  @return true if and only if the given element is syntetic, false otherwise
+     *  @return true if and only if the given element is synthetic, false otherwise
      */
     public boolean isSynthetic(Element element) {
         return (((Symbol) element).flags() & Flags.SYNTHETIC) != 0 || (((Symbol) element).flags() & Flags.GENERATEDCONSTR) != 0;
