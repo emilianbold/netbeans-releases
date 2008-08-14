08621485eb2a49680cddb942c95f07f42fb43116
Vladimir Kvashin <vkvashin@netbeans.org>
2008-08-14 23:06:42 +0400

    Moved the test for template inner class dtor definition to the default suite and corrected its golden data

--- cnd.modelimpl/test/unit/src/org/netbeans/modules/cnd/modelimpl/trace/FileModelTest.java
+++ cnd.modelimpl/test/unit/src/org/netbeans/modules/cnd/modelimpl/trace/FileModelTest.java
@@ -420,6 +420,10 @@ public class FileModelTest extends TraceModelTestBase {
         performTest("resolver_typedef_string.cc"); // NOI18N
     }
 
+    public void testTemplateInnerClassDtorDefinition() throws Exception {
+        performTest("template_inner_class_dtor_definition.cc"); // NOI18N
+    }
+
     /////////////////////////////////////////////////////////////////////
     // FAILS
     
@@ -440,10 +444,6 @@ public class FileModelTest extends TraceModelTestBase {
 	    return FileModelTest.class;
 	}
 	
-	public void testTemplateInnerClassDtorDefinition() throws Exception {
-	    performTest("template_inner_class_dtor_definition.cc"); // NOI18N
-	}
-
         @Override
 	protected void postSetUp() {
 	    // init flags needed for file model tests
