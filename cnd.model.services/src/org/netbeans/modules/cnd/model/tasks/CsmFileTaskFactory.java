ea3c53c9842986e1ba39cc4a795ff9df34979fed
Alexander Simon <alexvsimon@netbeans.org>
2008-08-25 18:52:20 +0400

    fixing: IZ#143956:low performance of error and semantic highlighting - do not process one file twice

--- cnd.model.services/src/org/netbeans/modules/cnd/model/tasks/CsmFileTaskFactory.java
+++ cnd.model.services/src/org/netbeans/modules/cnd/model/tasks/CsmFileTaskFactory.java
@@ -43,9 +43,11 @@ package org.netbeans.modules.cnd.model.tasks;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 import org.netbeans.modules.cnd.api.model.CsmChangeEvent;
 import org.netbeans.modules.cnd.api.model.CsmFile;
 import org.netbeans.modules.cnd.api.model.CsmListeners;
@@ -81,7 +83,7 @@ public abstract class CsmFileTaskFactory {
     protected abstract Collection<FileObject> getFileObjects();
     
     protected final void fileObjectsChanged() {
-        final List<FileObject> currentFiles = new ArrayList<FileObject>(getFileObjects());
+        final Set<FileObject> currentFiles = new HashSet<FileObject>(getFileObjects());
         final long id = Math.round(100.0*Math.random());
         final String name = this.getClass().getName();
         if (OpenedEditors.SHOW_TIME) System.err.println("CsmFileTaskFactory: POST worker " + id);
@@ -96,7 +98,7 @@ public abstract class CsmFileTaskFactory {
         });
     }
 
-    private void stateChangedImpl(List<FileObject> currentFiles) {
+    private void stateChangedImpl(Collection<FileObject> currentFiles) {
         Map<CsmFile, Pair> toRemove = new HashMap<CsmFile, Pair>();
         Map<CsmFile, Pair> toAdd = new HashMap<CsmFile, Pair>();
 
