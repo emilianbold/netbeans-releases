10399ca4b9b26f7f6d86a3ced74b74e01171a854
Jesse Glick <jglick@netbeans.org>
2008-08-26 12:54:22 -0400

    #144747: considering entire test to be random.

--- openide.text/test/unit/src/org/openide/text/ReusableEditor2Test.java
+++ openide.text/test/unit/src/org/openide/text/ReusableEditor2Test.java
@@ -57,6 +57,7 @@ import org.openide.util.lookup.Lookups;
  *
  * @author Petr Nejedly, Marek Slama
  */
+@RandomlyFails // #144747
 public class ReusableEditor2Test extends NbTestCase {
     static {
         System.setProperty("org.openide.windows.DummyWindowManager.VISIBLE", "false");
@@ -100,7 +101,6 @@ public class ReusableEditor2Test extends NbTestCase {
      * 4. Open first file with ShowOpenType.REUSE
      * 5. Verify second is closed
      */
-    @RandomlyFails
     public void testReuse() {
         openAndCheck(c1, Line.ShowOpenType.REUSE, Line.ShowVisibilityType.NONE); // 1
         openAndCheck(c2, Line.ShowOpenType.REUSE, Line.ShowVisibilityType.NONE); // 2
