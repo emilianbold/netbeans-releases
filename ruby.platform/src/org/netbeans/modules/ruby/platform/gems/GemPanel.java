5d66e66c424df333e95e6c6239bae2cb2d18a0d7
Martin Krauskopf <mkrauskopf@netbeans.org>
2008-08-28 09:41:31 +0200

    Eat Enter, so the dialog is not closed when user hits the Enter in search field. Filter task is already submitted and will be applied in few ms.

--- ruby.platform/src/org/netbeans/modules/ruby/platform/gems/GemPanel.java
+++ ruby.platform/src/org/netbeans/modules/ruby/platform/gems/GemPanel.java
@@ -590,6 +590,7 @@ public final class GemPanel extends JPanel {
         FormListener formListener = new FormListener();
 
         searchUpdatedText.setColumns(14);
+        searchUpdatedText.addActionListener(formListener);
 
         searchUpdatedLbl.setLabelFor(searchUpdatedText);
         org.openide.awt.Mnemonics.setLocalizedText(searchUpdatedLbl, org.openide.util.NbBundle.getMessage(GemPanel.class, "GemPanel.searchUpdatedLbl.text")); // NOI18N
@@ -679,6 +680,7 @@ public final class GemPanel extends JPanel {
         gemsTab.addTab(org.openide.util.NbBundle.getMessage(GemPanel.class, "GemPanel.updatedPanel.TabConstraints.tabTitle"), updatedPanel); // NOI18N
 
         searchInstText.setColumns(14);
+        searchInstText.addActionListener(formListener);
 
         searchInstLbl.setLabelFor(searchInstText);
         org.openide.awt.Mnemonics.setLocalizedText(searchInstLbl, org.openide.util.NbBundle.getMessage(GemPanel.class, "GemPanel.searchInstLbl.text")); // NOI18N
@@ -760,6 +762,7 @@ public final class GemPanel extends JPanel {
         gemsTab.addTab(org.openide.util.NbBundle.getMessage(GemPanel.class, "GemPanel.installedPanel.TabConstraints.tabTitle"), installedPanel); // NOI18N
 
         searchNewText.setColumns(14);
+        searchNewText.addActionListener(formListener);
 
         searchNewLbl.setLabelFor(searchNewText);
         org.openide.awt.Mnemonics.setLocalizedText(searchNewLbl, org.openide.util.NbBundle.getMessage(GemPanel.class, "GemPanel.searchNewLbl.text")); // NOI18N
@@ -866,7 +869,7 @@ public final class GemPanel extends JPanel {
                     .add(descriptionCheckbox)
                     .add(proxyButton)
                     .add(allVersionsCheckbox))
-                .addContainerGap(463, Short.MAX_VALUE))
+                .addContainerGap(459, Short.MAX_VALUE))
         );
         settingsPanelLayout.setVerticalGroup(
             settingsPanelLayout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
@@ -1002,6 +1005,15 @@ public final class GemPanel extends JPanel {
             }
             else if (evt.getSource() == browseGemHome) {
                 GemPanel.this.browseGemHomeActionPerformed(evt);
+            }
+            else if (evt.getSource() == searchUpdatedText) {
+                GemPanel.this.searchUpdatedTextActionPerformed(evt);
+            }
+            else if (evt.getSource() == searchInstText) {
+                GemPanel.this.searchInstTextActionPerformed(evt);
+            }
+            else if (evt.getSource() == searchNewText) {
+                GemPanel.this.searchNewTextActionPerformed(evt);
             }
         }
     }// </editor-fold>//GEN-END:initComponents
@@ -1174,6 +1186,20 @@ public final class GemPanel extends JPanel {
     private void descriptionCheckboxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_descriptionCheckboxActionPerformed
         RubyPreferences.setFetchGemDescriptions(descriptionCheckbox.isSelected());
     }//GEN-LAST:event_descriptionCheckboxActionPerformed
+
+    private void searchUpdatedTextActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_searchUpdatedTextActionPerformed
+        // Eat Enter, so the dialog is not closed when user hits the Enter in
+        // search field. Filter task is already submitted and will be applied in
+        // few ms.
+    }//GEN-LAST:event_searchUpdatedTextActionPerformed
+
+    private void searchInstTextActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_searchInstTextActionPerformed
+        // see comment in #searchUpdatedTextActionPerformed
+    }//GEN-LAST:event_searchInstTextActionPerformed
+
+    private void searchNewTextActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_searchNewTextActionPerformed
+        // see comment in #searchUpdatedTextActionPerformed
+    }//GEN-LAST:event_searchNewTextActionPerformed
 
     public static File chooseGemRepository(final Component parent) {
         JFileChooser chooser = new JFileChooser();
