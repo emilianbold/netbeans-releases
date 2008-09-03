312d6d2614eb7c581cdc5195c7524a1b8cec4514
Marian Petras <mpetras@netbeans.org>
2008-09-03 21:58:38 +0200

    fixed bug #145463 - "Search results look close to each other"

--- utilities/src/org/netbeans/modules/search/ResultView.java
+++ utilities/src/org/netbeans/modules/search/ResultView.java
@@ -69,6 +69,7 @@ import javax.swing.JToggleButton;
 import javax.swing.JToolBar;
 import javax.swing.JTree;
 import javax.swing.SwingConstants;
+import javax.swing.tree.TreeCellRenderer;
 import javax.swing.tree.TreePath;
 import org.openide.awt.Mnemonics;
 import org.openide.nodes.Node;
@@ -323,7 +324,20 @@ final class ResultView extends TopComponent {
     private static JTree createTree(ResultTreeModel treeModel,
                                     NodeListener nodeListener) {
         JTree tree = new JTree(treeModel);
-        tree.setCellRenderer(new NodeRenderer(false));
+
+        TreeCellRenderer cellRenderer = new NodeRenderer(false);
+        tree.setCellRenderer(cellRenderer);
+        tree.setRowHeight(cellRenderer.getTreeCellRendererComponent(
+                                                tree,       //tree
+                                                treeModel,  //value
+                                                true,       //selected
+                                                true,       //expanded
+                                                false,      //leaf
+                                                0,          //row
+                                                true)       //hasFocus
+                          .getPreferredSize()
+                          .height);
+
         tree.putClientProperty("JTree.lineStyle", "Angled");            //NOI18N
         
         tree.addMouseListener(nodeListener);
