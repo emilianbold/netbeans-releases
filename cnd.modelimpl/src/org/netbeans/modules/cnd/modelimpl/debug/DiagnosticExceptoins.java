d35198ce7f4ae42318eaa6894963e3eb66f9243d
Vladimir Kvashin <vkvashin@netbeans.org>
2008-09-06 02:23:09 +0400

    #142107 smart hdr: modifications upon one more code review + misc minor fixes

--- cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/debug/DiagnosticExceptoins.java
+++ cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/debug/DiagnosticExceptoins.java
@@ -41,6 +41,8 @@
 
 package org.netbeans.modules.cnd.modelimpl.debug;
 
+import org.netbeans.modules.cnd.utils.CndUtils;
+
 /**
  * Allows to get control as soon as an exception occurs
  * in one of the code model threads
@@ -67,29 +69,14 @@ public class DiagnosticExceptoins {
     public static void setHook(Hook aHook) {
 	hook = aHook;
     }
-
-    private static boolean assertions = false;
-    static {
-        try {
-            assert assertions = true;
-        } catch (AssertionError e) {
-            // nothing to do
-        }
-    }
     
     /**
      * This method is called from within catch(...) in code model threads.
      * See Hook.exception description for more details
      */
     public static void register(Throwable thr) {
-        if( assertions ) {
+        if( CndUtils.isDebugMode() ) {
             thr.printStackTrace();
-        } else {
-//            if (thr.getMessage()!=null) {
-//                System.err.println(thr.getMessage());
-//            } else {
-//                System.err.println(thr);
-//            }
         }
 	Hook aHook = hook;
 	if( aHook != null ) {
