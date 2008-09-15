3790217eccb5464c232505670938620400028f3c
Tomas Stupka <tstupka@netbeans.org>
2008-09-15 13:22:32 +0200

    unused import

--- subversion/src/org/netbeans/modules/subversion/ui/properties/SvnProperties.java
+++ subversion/src/org/netbeans/modules/subversion/ui/properties/SvnProperties.java
@@ -42,7 +42,6 @@ package org.netbeans.modules.subversion.ui.properties;
 
 import java.awt.Dialog;
 import java.awt.EventQueue;
-import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
