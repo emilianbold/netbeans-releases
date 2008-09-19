3fdd6173e80b6d5a1a06af8eae4e1cf43d2fc5aa
Tor Norbye <tor@netbeans.org>
2008-09-19 16:38:47 -0700

    147567: Navigate partial without spaces fails Also improve RHTML/embedded test infrastructure

---
+++ ruby.rhtml/test/unit/data/testfiles/app/controllers/admin/overview_controller.rb
@@ -0,0 +1,10 @@
+class OverviewController < ApplicationController
+  def index
+    @enabled, @disabled = @users.partition { |u| u.deleted_at.nil? }
+  end
+  
+  def show
+    @user  = User.new
+  end
+
+end
