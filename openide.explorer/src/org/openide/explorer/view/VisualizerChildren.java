db7c5a27e048c3d742df72b0cbca198472c5d84b
Tomas Holy <t_h@netbeans.org>
2008-09-19 14:52:25 +0200

    #139692: adding assert with event info

--- openide.explorer/src/org/openide/explorer/view/VisualizerChildren.java
+++ openide.explorer/src/org/openide/explorer/view/VisualizerChildren.java
@@ -157,21 +157,43 @@ final class VisualizerChildren extends Object {
 
     final String dumpIndexes(VisualizerNode visNode) {
         StringBuilder sb = new StringBuilder();
-        sb.append("EMPTY: " + (visNode == VisualizerNode.EMPTY) + ", Lazy: " + snapshot.getClass().getName().endsWith("LazySnapshot"));
-        sb.append("\nSeeking for: ").append(visNode.toId());
-        sb.append("\nwith parent: ").append(((VisualizerNode)visNode.getParent()) != null
-                ? ((VisualizerNode)visNode.getParent()).toId() : "null");
-        sb.append("\nSeeking in : ").append(parent != null ? parent.toId() : "null").append("\n");
+        sb.append("EMPTY: " + (visNode == VisualizerNode.EMPTY) + ", Lazy: " // NOI18N
+                + snapshot.getClass().getName().endsWith("LazySnapshot")); // NOI18N
+        sb.append("\nSeeking for: ").append(visNode.toId()); // NOI18N
+        sb.append("\nwith parent: ").append(((VisualizerNode)visNode.getParent()) != null // NOI18N
+                ? ((VisualizerNode)visNode.getParent()).toId() : "null"); // NOI18N
+        sb.append("\nSeeking in : ").append(parent != null ? parent.toId() : "null").append("\n"); // NOI18N
+        addVisNodesInfo(sb);
+        return sb.toString();
+    }
+    
+    private void addVisNodesInfo(StringBuilder sb) {
         for (int i = 0; i < visNodes.size(); i++) {
             VisualizerNode node = (VisualizerNode) visNodes.get(i);
-            sb.append("  ").append(i);
+            sb.append("  ").append(i); // NOI18N
             if (node != null) {
-                sb.append(" = ").append(node.toId());
+                sb.append(" = ").append(node.toId()); // NOI18N
             } else {
-                sb.append(" = null");
+                sb.append(" = null"); // NOI18N
             }
-            sb.append('\n');
+            sb.append('\n'); // NOI18N
+        }        
+    }
+    
+    final String dumpEventInfo(VisualizerEvent ev) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\nEvent: " + ev.getClass().getName()); // NOI18N
+        sb.append("\nOriginal event: " + ev.originalEvent.getClass().getName()); // NOI18N
+        sb.append("\ncurrent vis. nodes:"); // NOI18N
+        addVisNodesInfo(sb);
+        sb.append("\nIndexes: "); // NOI18N
+        int[] arr = ev.getArray();
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(Integer.toString(arr[i]));
+            sb.append(" "); // NOI18N
         }
+        sb.append("\n"); // NOI18N
+        sb.append(ev.originalEvent.toString());
         return sb.toString();
     }
     
@@ -229,6 +251,8 @@ final class VisualizerChildren extends Object {
         if (idxs.length == 0) {
             return;
         }
+        
+        assert visNodes.size() > idxs[idxs.length - 1] : dumpEventInfo(ev);
 
         for (int i = idxs.length - 1; i >= 0; i--) {
             VisualizerNode visNode = visNodes.remove(idxs[i]);
