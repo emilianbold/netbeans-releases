5124e288cd145ae1878ed102793aff3ebc125355
Quy Nguyen <quynguyen@netbeans.org>
2008-09-19 22:13:34 -0700

    Prevent additional source commands when URL content is empty

--- web.client.tools.impl/src/org/netbeans/modules/web/client/javascript/debugger/filesystem/URLFileObject.java
+++ web.client.tools.impl/src/org/netbeans/modules/web/client/javascript/debugger/filesystem/URLFileObject.java
@@ -264,10 +264,6 @@ public class URLFileObject extends FileObject {
                         InputStream is = null;
                         try {
                             is = content.getInputStream();
-                            int available = (is != null) ? is.available() : 0;
-                            if (available <= 0) {
-                                is = null;
-                            }
                         } catch (IOException ex) {
                             is = null;
                         }
