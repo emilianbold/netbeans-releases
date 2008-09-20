9e8b7836579e510f2fccc3e0c80b11410d63c522
Quy Nguyen <quynguyen@netbeans.org>
2008-09-19 21:20:26 -0700

    Fix NPE when source InputStream is null

--- web.client.tools.impl/src/org/netbeans/modules/web/client/javascript/debugger/filesystem/URLFileObject.java
+++ web.client.tools.impl/src/org/netbeans/modules/web/client/javascript/debugger/filesystem/URLFileObject.java
@@ -264,7 +264,7 @@ public class URLFileObject extends FileObject {
                         InputStream is = null;
                         try {
                             is = content.getInputStream();
-                            int available = is.available();
+                            int available = (is != null) ? is.available() : 0;
                             if (available <= 0) {
                                 is = null;
                             }
