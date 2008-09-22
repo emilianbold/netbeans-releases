b2b9a4ee2f1bc73af0bda2dc429678e9a8c7e90c
Tomas Holy <t_h@netbeans.org>
2008-09-22 13:59:57 +0200

    #145376: IllegalArgumentException: A node selection cannot contain nodes not underneath the root

--- openide.explorer/test/unit/src/org/openide/explorer/ExplorerManagerTest.java
+++ openide.explorer/test/unit/src/org/openide/explorer/ExplorerManagerTest.java
@@ -153,13 +153,8 @@ public class ExplorerManagerTest extends NbTestCase
     
     public void testCannotSetNodesNotUnderTheRoot() throws Exception {
         final Node a = new AbstractNode(Children.LEAF);
-        
-        try {
-            em.setSelectedNodes(new Node[] { a });
-            fail("Should throw IllegalArgumentException as the node is not under root");
-        } catch (IllegalArgumentException ex) {
-            // ok, a is not under root
-        }
+        em.setSelectedNodes(new Node[]{a});
+        assertEquals(0, em.getSelectedNodes().length);
     }
     
     
