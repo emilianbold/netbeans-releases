870acfea294804d479b8b32fab3bdf8cb19d951e
Tomas Mysik <tmysik@netbeans.org>
2008-09-26 01:31:57 +0200

    Minor improvement

--- php.project/src/org/netbeans/modules/php/project/connections/RemoteClient.java
+++ php.project/src/org/netbeans/modules/php/project/connections/RemoteClient.java
@@ -695,16 +695,28 @@ public class RemoteClient implements Cancellable {
     }
 
     private void transferFailed(TransferInfo transferInfo, TransferFile file, String reason) {
-        transferInfo.addFailed(file, reason);
-        if (LOGGER.isLoggable(Level.FINE)) {
-            LOGGER.fine("Failed: " + file + ", reason: " + reason);
+        if (!transferInfo.isFailed(file)) {
+            transferInfo.addFailed(file, reason);
+            if (LOGGER.isLoggable(Level.FINE)) {
+                LOGGER.fine("Failed: " + file + ", reason: " + reason);
+            }
+        } else {
+            if (LOGGER.isLoggable(Level.FINE)) {
+                LOGGER.fine("Failed: " + file + ", reason: " + reason + " [ignored, failed already]");
+            }
         }
     }
 
     private void transferIgnored(TransferInfo transferInfo, TransferFile file, String reason) {
-        transferInfo.addIgnored(file, reason);
-        if (LOGGER.isLoggable(Level.FINE)) {
-            LOGGER.fine("Ignored: " + file + ", reason: " + reason);
+        if (!transferInfo.isIgnored(file)) {
+            transferInfo.addIgnored(file, reason);
+            if (LOGGER.isLoggable(Level.FINE)) {
+                LOGGER.fine("Ignored: " + file + ", reason: " + reason);
+            }
+        } else {
+            if (LOGGER.isLoggable(Level.FINE)) {
+                LOGGER.fine("Ignored: " + file + ", reason: " + reason + " [ignored, ignored already]");
+            }
         }
     }
 
