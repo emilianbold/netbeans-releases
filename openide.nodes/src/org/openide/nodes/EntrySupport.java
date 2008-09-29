44537418e169dab2098ca0254db718b0712d31fb
Tomas Holy <t_h@netbeans.org>
2008-09-29 14:52:19 +0200

    isInitialized() should behave more reliably in Default support

--- openide.nodes/src/org/openide/nodes/EntrySupport.java
+++ openide.nodes/src/org/openide/nodes/EntrySupport.java
@@ -124,6 +124,7 @@ abstract class EntrySupport {
         private static final Object LOCK = new Object();
         private static final Logger LOG_GET_ARRAY = Logger.getLogger("org.openide.nodes.Children.getArray"); // NOI18N
         private Thread initThread;
+        private boolean inited = false;
 
 
         public Default(Children ch) {
@@ -131,13 +132,15 @@ abstract class EntrySupport {
         }
 
         public boolean isInitialized() {
-            ChildrenArray arr = array.get();
-            return (arr != null) && arr.isInitialized();
+            return inited;
+            /*ChildrenArray arr = array.get();
+            return (arr != null) && arr.isInitialized();*/
         }
 
         protected List<Node> createSnapshot() {
             return new DefaultSnapshot(getNodes(), array.get());
         }
+
         public final Node[] getNodes() {
             //Thread.dumpStack();
             //System.err.println(off + "getNodes: " + getNode ());
@@ -753,6 +756,7 @@ abstract class EntrySupport {
 
                     // now attach to entrySupport, so when entrySupport == null => we are
                     // not fully initialized!!!!
+                    inited = true;
                     arr.entrySupport = this;
                     class SetAndNotify implements Runnable {
 
@@ -880,6 +884,7 @@ abstract class EntrySupport {
                         // really finalized and not reconstructed
                         mustNotifySetEnties = false;
                         array = EMPTY;
+                        inited = false;
                         children.callRemoveNotify();
                         assert array == EMPTY;
                     }
