025e1cf386df0a0301809cb68284a8405ab2b23e
Petr Pisl <ppisl@netbeans.org>
2008-09-30 15:22:06 +0200

    Removing / from meta tag, because it's not valid html 4.01 code

--- php.project/src/org/netbeans/modules/php/project/ui/resources/emptyWebPagePHP.php
+++ php.project/src/org/netbeans/modules/php/project/ui/resources/emptyWebPagePHP.php
@@ -5,7 +5,7 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
     <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=${project.encoding}"/>
+        <meta http-equiv="Content-Type" content="text/html; charset=${project.encoding}">
         <title></title>
     </head>
     <body>
