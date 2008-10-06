47c1f3973c2b3b77953b4a7eda9560fbc31fd715
Alexander Kouznetsov <mrkam@netbeans.org>
2008-10-07 00:06:12 +0400

    Moved EditorMenuPopup testcase from actions to menus suite

--- performance/languages/test/qa-functional/src/org/netbeans/performance/languages/menus/EditorMenuPopup.java
+++ performance/languages/test/qa-functional/src/org/netbeans/performance/languages/menus/EditorMenuPopup.java
@@ -39,7 +39,7 @@
  * made subject to such option by the copyright holder.
  */
 
-package org.netbeans.performance.languages.actions;
+package org.netbeans.performance.languages.menus;
 
 
 import org.netbeans.jellytools.EditorOperator;
