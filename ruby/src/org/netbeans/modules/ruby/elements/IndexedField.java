2aa268261eff721162b5905be7ef457f143a4447
Tor Norbye <tor@netbeans.org>
2008-10-08 15:56:00 -0700

    141780: Code completion missing classes from MRI Part 2: Ensure that we use the correct Ruby platform when looking up persistent urls, corresponding to the platform manager chosen for the project corresponding to the completion context.

--- ruby/src/org/netbeans/modules/ruby/elements/IndexedField.java
+++ ruby/src/org/netbeans/modules/ruby/elements/IndexedField.java
@@ -29,6 +29,7 @@ package org.netbeans.modules.ruby.elements;
 
 import org.netbeans.modules.gsf.api.ElementKind;
 import org.netbeans.modules.ruby.RubyIndex;
+import org.openide.filesystems.FileObject;
 
 
 /**
@@ -41,15 +42,15 @@ public class IndexedField extends IndexedElement {
     private boolean inherited;
 
     private IndexedField(String name, RubyIndex index, String fileUrl, String fqn,
-        String clz, String require, String attributes, int flags) {
-        super(index, fileUrl, fqn, clz, require, attributes, flags);
+        String clz, String require, String attributes, int flags, FileObject context) {
+        super(index, fileUrl, fqn, clz, require, attributes, flags, context);
         this.name = name;
     }
 
     public static IndexedField create(RubyIndex index, String name, String fqn, String clz,
-        String fileUrl, String require, String attributes, int flags) {
+        String fileUrl, String require, String attributes, int flags, FileObject context) {
         IndexedField m =
-            new IndexedField(name, index, fileUrl, fqn, clz, require, attributes, flags);
+            new IndexedField(name, index, fileUrl, fqn, clz, require, attributes, flags, context);
 
         return m;
     }
