c1cb739605895ede98f5ade3518015b0695f7992
Nick N. Krasilnikov <nnnnnk@netbeans.org>
2008-10-09 14:47:14 +0400

    test 2 for IZ 149485 : parser fails on one particular type of struct initialialization

--- cnd.modelimpl/test/unit/src/org/netbeans/modules/cnd/modelimpl/trace/FileModelTest.java
+++ cnd.modelimpl/test/unit/src/org/netbeans/modules/cnd/modelimpl/trace/FileModelTest.java
@@ -509,6 +509,11 @@ public class FileModelTest extends TraceModelTestBase {
         performTest("struct_initializer.c"); // NOI18N        
     }
 
+    // IZ#149412 : parser doesn't recover after specific structure initialization
+    public void testVariableInitializer() throws Exception {      
+        performTest("variable_initializer.cc"); // NOI18N        
+    }
+
     // IZ#149499 : parser does not support omitting first branch of ? : operator
     public void testTernaryOperator() throws Exception {      
         performTest("ternary_operator.cc"); // NOI18N        
