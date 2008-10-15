86d42b3bc0062fe0b4a670eada642ac25fcc6480
Peter Zavadsky <pzavadsky@netbeans.org>
2008-10-15 10:14:40 -0700

    #148062 Fixing image reference, to be shown in the VW multiview.

--- visualweb.project.jsfloader/src/org/netbeans/modules/visualweb/project/jsfloader/JsfJavaEditorSupport.java
+++ visualweb.project.jsfloader/src/org/netbeans/modules/visualweb/project/jsfloader/JsfJavaEditorSupport.java
@@ -130,7 +130,7 @@ public final class JsfJavaEditorSupport extends DataEditorSupport implements Edi
     private static final int ELEMENT_INDEX_JSP      = 1;
     private static final int ELEMENT_INDEX_JAVA     = 2;
 
-    private static final String ICON_PATH_JSF_JSP = "org/netbeans/modules/visualweb/project/jsfloader/resources/jsfJspObject.gif"; // NOI18N
+    private static final String ICON_PATH_JSF_JSP = "org/netbeans/modules/visualweb/project/jsfloader/resources/jsfJspObject.png"; // NOI18N
 
     private static final Logger logger = Logger.getLogger("org.netbeans.modules.visualweb.project.jsfloader.JsfJavaEditorSupport");
     
