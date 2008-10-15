3e09b33ded7c545d084e5fbe1f816cd7965618b9
Dmitry Lipin <dlipin@netbeans.org>
2008-10-15 23:04:39 +0400

    Use permanent d/l urk for 6.1 link

--- installer/infra/build/web/js/build_info.js
+++ installer/infra/build/web/js/build_info.js
@@ -58,7 +58,7 @@ var BOUNCER_URL = "http://services.netbeans.org/bouncer/index.php";
 var SOURCES_AND_BINARIES_URL = BUILD_LOCATION + "zip/";
 
 function add_download_tabs() {
-	add_download_tab("6.1", "http://download.netbeans.org/netbeans/6.1/final/");
+	add_download_tab("6.1", "http://www.netbeans.org/downloads/");
 	add_download_tab("6.5 RC1");
 	add_download_tab(DEVELOPMENT_TITLE, DEVELOPMENT_BUILDS_LINK);
 	add_download_tab(ARCHIVE_TITLE,ARCHIVE_BUILDS_LINK);
