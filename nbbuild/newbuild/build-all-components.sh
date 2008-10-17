b70211b88f3278a2daf3c9c19493c9cf54a86433
rnovak <rnovak@netbeans.org>
2008-10-17 18:45:39 +0200

    NB binaries will be backup before and after CV tests for further analysis.

--- nbbuild/newbuild/build-all-components.sh
+++ nbbuild/newbuild/build-all-components.sh
@@ -49,7 +49,7 @@ if [ $ERROR_CODE != 0 ]; then
     exit $ERROR_CODE;
 fi
 
-cp -rp nbbuild/netbeans nbbuild/netbeans.orig
+cp -rp nbbuild/netbeans nbbuild/netbeans.before
 
 ###############  Commit validation tests  ##########################
 #cp -r $NB_ALL/nbbuild/netbeans $NB_ALL/nbbuild/netbeans-PRISTINE
@@ -173,6 +173,8 @@ if [ $TEST_CODE = 1 ]; then
     echo "ERROR: At least one of validation tests failed"
     exit 1;
 fi
+
+cp -rp nbbuild/netbeans nbbuild/netbeans.after
 
 #Build UML modules
 ant -Dbuildnum=$BUILDNUM -Dbuildnumber=$BUILDNUMBER -f nbbuild/build.xml rebuild-cluster -Drebuild.cluster.name=nb.cluster.uml -Dbuild.compiler.debuglevel=source,lines
