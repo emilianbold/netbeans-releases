6f67cb3e344cb25e7f92b706c4cd2486660b26e1
Egor Ushakov <gorrus@netbeans.org>
2008-10-29 14:10:21 +0300

    fixed IZ:133134 (Address breakpoint is not shown in the sources)

--- cnd.debugger.gdb/src/org/netbeans/modules/cnd/debugger/gdb/EditorContextBridge.java
+++ cnd.debugger.gdb/src/org/netbeans/modules/cnd/debugger/gdb/EditorContextBridge.java
@@ -130,6 +130,8 @@ public class EditorContextBridge {
                     // do nothing
                 }
             }
+        } else {
+            Disassembly.open();
         }
         return false;
     }
