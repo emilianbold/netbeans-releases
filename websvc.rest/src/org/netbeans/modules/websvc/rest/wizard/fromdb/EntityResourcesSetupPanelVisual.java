372481121c5ccfd9da241210ec675a07b0dffe94
Lukas Jungmann <jungi@netbeans.org>
2008-11-05 23:18:22 +0100

    IZ #151556: [65cat] reflect.UndeclaredThrowableException at $Proxy25.actionPerformed

--- websvc.rest/src/org/netbeans/modules/websvc/rest/wizard/fromdb/EntityResourcesSetupPanelVisual.java
+++ websvc.rest/src/org/netbeans/modules/websvc/rest/wizard/fromdb/EntityResourcesSetupPanelVisual.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -24,7 +24,7 @@
  * Contributor(s):
  *
  * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2008 Sun
  * Microsystems, Inc. All Rights Reserved.
  *
  * If you wish your version of this file to be governed by only the CDDL
@@ -38,17 +38,12 @@
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
  */
-
 package org.netbeans.modules.websvc.rest.wizard.fromdb;
 
-import org.netbeans.modules.websvc.rest.wizard.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.util.ArrayList;
-import java.util.List;
 import javax.swing.ComboBoxModel;
 import javax.swing.JComboBox;
-import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import javax.swing.text.JTextComponent;
 import org.netbeans.api.project.Project;
@@ -56,40 +51,46 @@ import org.netbeans.api.project.ProjectUtils;
 import org.netbeans.api.project.SourceGroup;
 import org.netbeans.modules.websvc.rest.codegen.EntityResourcesGenerator;
 import org.netbeans.modules.websvc.rest.support.SourceGroupSupport;
+import org.netbeans.modules.websvc.rest.wizard.AbstractPanel;
+import org.netbeans.modules.websvc.rest.wizard.SourceGroupUISupport;
+import org.netbeans.modules.websvc.rest.wizard.Util;
+import org.netbeans.modules.websvc.rest.wizard.WizardProperties;
 import org.netbeans.spi.java.project.support.ui.PackageView;
 import org.netbeans.spi.project.ui.templates.support.Templates;
 import org.openide.WizardDescriptor;
 import org.openide.filesystems.FileObject;
+import org.openide.util.ChangeSupport;
 
 /**
  *
  * @author  Pavel Buzek
  */
 public class EntityResourcesSetupPanelVisual extends javax.swing.JPanel implements AbstractPanel.Settings {
-    
+
     private Project project;
-    private List<ChangeListener> listeners;
     private WizardDescriptor wizard;
-    
-    //    private ChangeSupport changeSupport = new ChangeSupport(this);
-    
+    private ChangeSupport changeSupport = new ChangeSupport(this);
+
     /** Creates new form CrudSetupPanel */
     public EntityResourcesSetupPanelVisual(String name) {
         initComponents();
         setName(name);
-        this.listeners = new ArrayList<ChangeListener>();
         resourcePackageComboBox.getEditor().getEditorComponent().addKeyListener(new KeyAdapter() {
+
+            @Override
             public void keyReleased(java.awt.event.KeyEvent evt) {
-                fireChange();
+                changeSupport.fireChange();
             }
         });
         converterPackageComboBox.getEditor().getEditorComponent().addKeyListener(new KeyAdapter() {
+
+            @Override
             public void keyReleased(java.awt.event.KeyEvent evt) {
-                fireChange();
+                changeSupport.fireChange();
             }
         });
     }
-    
+
     /** This method is called from within the constructor to
      * initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is
@@ -127,11 +128,6 @@ public class EntityResourcesSetupPanelVisual extends javax.swing.JPanel implemen
         org.openide.awt.Mnemonics.setLocalizedText(resourcePackageLabel, org.openide.util.NbBundle.getMessage(EntityResourcesSetupPanelVisual.class, "LBL_Package")); // NOI18N
 
         resourcePackageComboBox.setEditable(true);
-        resourcePackageComboBox.addItemListener(new java.awt.event.ItemListener() {
-            public void itemStateChanged(java.awt.event.ItemEvent evt) {
-                resourcePackageComboBoxItemStateChanged(evt);
-            }
-        });
         resourcePackageComboBox.addPropertyChangeListener(new java.beans.PropertyChangeListener() {
             public void propertyChange(java.beans.PropertyChangeEvent evt) {
                 resourcePackageComboBoxPropertyChange(evt);
@@ -142,16 +138,6 @@ public class EntityResourcesSetupPanelVisual extends javax.swing.JPanel implemen
         org.openide.awt.Mnemonics.setLocalizedText(converterPackageLabel, org.openide.util.NbBundle.getMessage(EntityResourcesSetupPanelVisual.class, "LBL_ResourceDir")); // NOI18N
 
         converterPackageComboBox.setEditable(true);
-        converterPackageComboBox.addItemListener(new java.awt.event.ItemListener() {
-            public void itemStateChanged(java.awt.event.ItemEvent evt) {
-                converterPackageComboBoxItemStateChanged(evt);
-            }
-        });
-        converterPackageComboBox.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                converterPackageComboBoxActionPerformed(evt);
-            }
-        });
         converterPackageComboBox.addPropertyChangeListener(new java.beans.PropertyChangeListener() {
             public void propertyChange(java.beans.PropertyChangeEvent evt) {
                 converterPackageComboBoxPropertyChange(evt);
@@ -168,7 +154,7 @@ public class EntityResourcesSetupPanelVisual extends javax.swing.JPanel implemen
                     .add(layout.createSequentialGroup()
                         .add(projectLabel)
                         .add(48, 48, 48)
-                        .add(projectTextField, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 393, Short.MAX_VALUE))
+                        .add(projectTextField, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 422, Short.MAX_VALUE))
                     .add(org.jdesktop.layout.GroupLayout.TRAILING, layout.createSequentialGroup()
                         .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
                             .add(converterPackageLabel, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -220,32 +206,18 @@ public class EntityResourcesSetupPanelVisual extends javax.swing.JPanel implemen
         converterPackageComboBox.getAccessibleContext().setAccessibleName(org.openide.util.NbBundle.getMessage(EntityResourcesSetupPanelVisual.class, "ConverterPackage")); // NOI18N
         converterPackageComboBox.getAccessibleContext().setAccessibleDescription(org.openide.util.NbBundle.getMessage(EntityResourcesSetupPanelVisual.class, "DESC_ConverterPackage")); // NOI18N
     }// </editor-fold>//GEN-END:initComponents
-    
-private void converterPackageComboBoxItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_converterPackageComboBoxItemStateChanged
-
-}//GEN-LAST:event_converterPackageComboBoxItemStateChanged
-
-private void resourcePackageComboBoxItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_resourcePackageComboBoxItemStateChanged
-
-}//GEN-LAST:event_resourcePackageComboBoxItemStateChanged
 
     private void locationComboBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_locationComboBoxActionPerformed
         locationChanged();
     }//GEN-LAST:event_locationComboBoxActionPerformed
 
     private void resourcePackageComboBoxPropertyChange(java.beans.PropertyChangeEvent evt) {//GEN-FIRST:event_resourcePackageComboBoxPropertyChange
-        fireChange();
+        changeSupport.fireChange();
     }//GEN-LAST:event_resourcePackageComboBoxPropertyChange
 
     private void converterPackageComboBoxPropertyChange(java.beans.PropertyChangeEvent evt) {//GEN-FIRST:event_converterPackageComboBoxPropertyChange
-        fireChange();
+        changeSupport.fireChange();
     }//GEN-LAST:event_converterPackageComboBoxPropertyChange
-
-private void converterPackageComboBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_converterPackageComboBoxActionPerformed
-// TODO add your handling code here:
-}//GEN-LAST:event_converterPackageComboBoxActionPerformed
-    
-    
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JComboBox converterPackageComboBox;
     private javax.swing.JLabel converterPackageLabel;
@@ -256,118 +228,105 @@ private void converterPackageComboBoxActionPerformed(java.awt.event.ActionEvent 
     private javax.swing.JComboBox resourcePackageComboBox;
     private javax.swing.JLabel resourcePackageLabel;
     // End of variables declaration//GEN-END:variables
-    
+
     public void addChangeListener(ChangeListener listener) {
-        listeners.add(listener);
+        changeSupport.addChangeListener(listener);
     }
-    
-    private void fireChange() {
-        ChangeEvent event =  new ChangeEvent(this);
-        
-        for (ChangeListener listener : listeners) {
-            listener.stateChanged(event);
-        }
-    }
-    
+
     public boolean valid(WizardDescriptor wizard) {
         AbstractPanel.clearErrorMessage(wizard);
-        SourceGroup[] groups = SourceGroupSupport.getJavaSourceGroups(project);
-        
-        if (groups == null || groups.length < 1) {
+
+        if (getSourceGroup() == null) {
             AbstractPanel.setErrorMessage(wizard, "MSG_NoJavaSourceRoots");
-        } else if (! Util.isValidPackageName(getResourcePackage())) {
+        } else if (!Util.isValidPackageName(getResourcePackage())) {
             AbstractPanel.setErrorMessage(wizard, "MSG_InvalidResourcePackageName");
             return false;
-        } else if (! Util.isValidPackageName(getConverterPackage())) {
+        } else if (!Util.isValidPackageName(getConverterPackage())) {
             AbstractPanel.setErrorMessage(wizard, "MSG_InvalidConverterPackageName");
             return false;
         }
         return true;
     }
-    
-    public SourceGroup getLocationValue() {
-        return (SourceGroup)locationComboBox.getSelectedItem();
-    }
-    
+
     public String getResourcePackage() {
-        return ((JTextComponent)resourcePackageComboBox.getEditor().getEditorComponent()).getText();
+        return ((JTextComponent) resourcePackageComboBox.getEditor().getEditorComponent()).getText();
     }
-    
+
     public String getConverterPackage() {
-        return ((JTextComponent)converterPackageComboBox.getEditor().getEditorComponent()).getText();
+        return ((JTextComponent) converterPackageComboBox.getEditor().getEditorComponent()).getText();
     }
-    
+
     private void setResourcePackage(String text) {
-        ((JTextComponent)resourcePackageComboBox.getEditor().getEditorComponent()).setText(text);
+        ((JTextComponent) resourcePackageComboBox.getEditor().getEditorComponent()).setText(text);
     }
-    
+
     private void setConverterPackage(String text) {
-        ((JTextComponent)converterPackageComboBox.getEditor().getEditorComponent()).setText(text);
+        ((JTextComponent) converterPackageComboBox.getEditor().getEditorComponent()).setText(text);
     }
-    
+
     private void locationChanged() {
         updateSourceGroupPackages();
-        fireChange();
+        changeSupport.fireChange();
     }
-    
+
     public void read(WizardDescriptor settings) {
-        if (project != null) return;
-        
+        if (project != null) {
+            return;
+        }
+
         this.wizard = settings;
-        
+
         project = Templates.getProject(settings);
         projectTextField.setText(ProjectUtils.getInformation(project).getDisplayName());
-        
+
         SourceGroup[] sourceGroups = SourceGroupSupport.getJavaSourceGroups(project);
         SourceGroupUISupport.connect(locationComboBox, sourceGroups);
-        
+
         resourcePackageComboBox.setRenderer(PackageView.listRenderer());
         converterPackageComboBox.setRenderer(PackageView.listRenderer());
         updateSourceGroupPackages();
-        
+
         FileObject targetFolder = Templates.getTargetFolder(settings);
         SourceGroup targetSourceGroup = null;
-        
-        if (targetFolder == null) {
-            targetSourceGroup = getSourceGroup();
-            targetFolder = targetSourceGroup.getRootFolder();
-        } else {
+        String targetPackage = "";
+        if (targetFolder != null) {
+            // set default source group and package cf. targetFolder
             targetSourceGroup = SourceGroupSupport.findSourceGroupForFile(sourceGroups, targetFolder);
-            setSourceGroup(targetSourceGroup);
+            if (targetSourceGroup != null) {
+                locationComboBox.setSelectedItem(targetSourceGroup);
+                targetPackage = SourceGroupSupport.getPackageForFolder(targetSourceGroup, targetFolder);
+            }
         }
-        
-        String targetPackage = SourceGroupSupport.getPackageForFolder(targetSourceGroup, targetFolder);
+
         targetPackage = (targetPackage.length() == 0) ? "" : targetPackage + ".";
         String resourcePackage = targetPackage + EntityResourcesGenerator.RESOURCE_FOLDER;
         setResourcePackage(resourcePackage);
         String converterPackage = targetPackage + EntityResourcesGenerator.CONVERTER_FOLDER;
         setConverterPackage(converterPackage);
-        
+
         addComboBoxListener(resourcePackageComboBox);
         addComboBoxListener(converterPackageComboBox);
     }
-    
+
     public void store(WizardDescriptor settings) {
-        if (Templates.getTargetFolder(settings) == null) {
-            Templates.setTargetFolder(settings, getSourceGroup().getRootFolder());
-        }
-        
         settings.putProperty(WizardProperties.RESOURCE_PACKAGE, getResourcePackage());
         settings.putProperty(WizardProperties.CONVERTER_PACKAGE, getConverterPackage());
+        settings.putProperty(WizardProperties.TARGET_SRC_ROOT, getSourceGroup().getRootFolder());
     }
-    
+
     private void addComboBoxListener(JComboBox comboBox) {
-        JTextComponent text = ((JTextComponent)comboBox.getEditor().getEditorComponent());
+        JTextComponent text = ((JTextComponent) comboBox.getEditor().getEditorComponent());
         text.addKeyListener(new KeyAdapter() {
+
+            @Override
             public void keyReleased(KeyEvent event) {
                 //updatePreview();
             }
         });
     }
-    
-    
+
     private void updateSourceGroupPackages() {
-        SourceGroup sourceGroup = (SourceGroup)locationComboBox.getSelectedItem();
+        SourceGroup sourceGroup = (SourceGroup) locationComboBox.getSelectedItem();
         if (sourceGroup != null) {
             ComboBoxModel model = PackageView.createListView(sourceGroup);
             if (model.getSize() > 0) {
@@ -381,14 +340,8 @@ private void converterPackageComboBoxActionPerformed(java.awt.event.ActionEvent 
             converterPackageComboBox.setModel(model);
         }
     }
-    
-   
-    
+
     private SourceGroup getSourceGroup() {
         return (SourceGroup) locationComboBox.getSelectedItem();
-    }
-    
-    private void setSourceGroup(SourceGroup group) {
-        locationComboBox.setSelectedItem(group);
     }
 }
