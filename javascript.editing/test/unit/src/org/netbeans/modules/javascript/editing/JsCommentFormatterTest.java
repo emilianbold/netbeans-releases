371b33ace2267177ed758f82848d6a0372d4de06
Tor Norbye <tor@netbeans.org>
2008-11-06 17:49:22 -0800

    Tweak

--- javascript.editing/test/unit/src/org/netbeans/modules/javascript/editing/JsCommentFormatterTest.java
+++ javascript.editing/test/unit/src/org/netbeans/modules/javascript/editing/JsCommentFormatterTest.java
@@ -100,7 +100,7 @@ public class JsCommentFormatterTest extends JsTestBase {
 
         List<String> tags = jsComment.getParams();
         List<String> expected = Arrays.asList("");
-
+        jsComment.toHtml();
         assertEquals(expected, tags);
     }
 
