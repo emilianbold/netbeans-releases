78612c0dd2bdbc1b6192d7c1a8acb559b4a52192
Vladimir Voskresensky <vv159170@netbeans.org>
2008-11-12 18:47:12 +0300

    fixing deprecations

--- cnd.editor/src/org/netbeans/modules/cnd/editor/cplusplus/CCKit.java
+++ cnd.editor/src/org/netbeans/modules/cnd/editor/cplusplus/CCKit.java
@@ -52,7 +52,7 @@ import javax.swing.text.TextAction;
 import javax.swing.text.BadLocationException;
 import org.netbeans.api.lexer.InputAttributes;
 import org.netbeans.api.lexer.Language;
-import org.netbeans.api.lexer.Token;
+import org.netbeans.api.lexer.TokenSequence;
 import org.netbeans.cnd.api.lexer.CndLexerUtilities;
 import org.netbeans.cnd.api.lexer.CndTokenUtilities;
 import org.netbeans.cnd.api.lexer.CppTokenId;
@@ -65,7 +65,6 @@ import org.netbeans.editor.BaseAction;
 import org.netbeans.editor.BaseDocument;
 import org.netbeans.editor.BaseKit;
 import org.netbeans.editor.BaseKit.InsertBreakAction;
-import org.netbeans.editor.Formatter;
 import org.netbeans.editor.Syntax;
 import org.netbeans.editor.SyntaxSupport;
 import org.netbeans.editor.SyntaxUpdateTokens;
@@ -164,7 +163,7 @@ public class CCKit extends NbEditorKit {
     /** Create syntax support */
     @Override
     public SyntaxSupport createSyntaxSupport(BaseDocument doc) {
-        SyntaxSupportProvider ss = (SyntaxSupportProvider) Lookup.getDefault().lookup(SyntaxSupportProvider.class);
+        SyntaxSupportProvider ss = Lookup.getDefault().lookup(SyntaxSupportProvider.class);
         SyntaxSupport sup = null;
         if (ss != null) {
             sup = ss.createSyntaxSupport(doc);
@@ -367,21 +366,23 @@ public class CCKit extends NbEditorKit {
                             lastSepOffset = 0;
                         }
                         if (lastSepOffset != -1 && lastSepOffset < dotPos) {
-                            org.netbeans.editor.TokenItem keyword = sup.getTokenChain(lastSepOffset, lBracePos);
-                            while (keyword != null && keyword.getOffset() < lBracePos) {
-                                if (keyword.getTokenID() == CCTokenContext.CLASS ||
-                                        keyword.getTokenID() == CCTokenContext.UNION ||
-                                        keyword.getTokenID() == CCTokenContext.STRUCT ||
-                                        keyword.getTokenID() == CCTokenContext.ENUM) {
-                                    insString = "};"; // NOI18N
-                                    break;
+                            TokenSequence<CppTokenId> cppTokenSequence = CndLexerUtilities.getCppTokenSequence(doc, lBracePos);
+                            cppTokenSequence.move(lBracePos);
+                            while (cppTokenSequence.movePrevious() && cppTokenSequence.offset() >= lastSepOffset) {
+                                switch (cppTokenSequence.token().id()) {
+                                    case CLASS:
+                                    case UNION:
+                                    case STRUCT:
+                                    case ENUM:
+                                        insString = "};"; // NOI18N
+                                        break;
                                 }
-                                keyword = keyword.getNext();
                             }
                         }
-                        doc.insertString(end, insString, null); // NOI18N
+                        doc.insertString(end, "\n" + insString, null); // NOI18N
                         // Lock does not need because method is invoked from BaseKit that already lock indent.
-                        doc.getFormatter().indentNewLine(doc, end);
+                        Indent indent = Indent.get(doc);
+                        indent.reindent(end + 1);
                         caret.setDot(dotPos);
                         return Boolean.TRUE;
                     }
