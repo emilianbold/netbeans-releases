c6789f13ff1b2f44aa2ce3b2b1785b31632b7f5c
Oleg Khokhlov <ovk@netbeans.org>
2008-11-17 19:30:25 +0300

    change in J2SE perf tests #2

---
+++ performance/j2se/test/qa-functional/data/PerformanceTestFoldersData/src/folders/javaFolder1000/AddToFavoritesTest.java
@@ -0,0 +1,125 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ *
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ */
+
+package org.netbeans.performance.j2se.actions;
+
+import org.netbeans.modules.performance.utilities.PerformanceTestCase;
+import org.netbeans.performance.j2se.setup.J2SESetup;
+
+import org.netbeans.jellytools.Bundle;
+import org.netbeans.jellytools.FavoritesOperator;
+import org.netbeans.jellytools.nodes.Node;
+import org.netbeans.jellytools.nodes.SourcePackagesNode;
+import org.netbeans.jemmy.operators.ComponentOperator;
+import org.netbeans.junit.NbTestSuite;
+import org.netbeans.junit.NbModuleSuite;
+
+/**
+ * Tests Add to Favorites.
+ *
+ * @author  mmirilovic@netbeans.org
+ */
+public class AddToFavoritesTest extends PerformanceTestCase {
+
+    protected static String ADD_TO_FAVORITES, REMOVE_FROM_FAVORITES;
+    private String fileProject, filePackage, fileName;
+    private Node addToFavoritesNode;
+    private FavoritesOperator favoritesWindow;
+    
+    
+    /**
+     * Creates a new instance of AddToFavorites
+     * @param testName the name of the test
+     */
+    public AddToFavoritesTest(String testName) {
+        super(testName);
+        expectedTime = WINDOW_OPEN;
+    }
+    
+    /**
+     * Creates a new instance of AddToFavorites
+     * @param testName the name of the test
+     * @param performanceDataName measured values will be saved under this name
+     */
+    public AddToFavoritesTest(String testName, String performanceDataName) {
+        super(testName, performanceDataName);
+        expectedTime = WINDOW_OPEN;
+    }
+
+    public static NbTestSuite suite() {
+        NbTestSuite suite = new NbTestSuite();
+        suite.addTest(NbModuleSuite.create(NbModuleSuite.createConfiguration(J2SESetup.class)
+             .addTest(AddToFavoritesTest.class)
+             .enableModules(".*").clusters(".*")));
+        return suite;
+    }
+
+    public void testAddJavaFile(){
+        fileProject = "PerformanceTestData";
+        filePackage = "org.netbeans.test.performance";
+        fileName = "Main20kB.java";
+        doMeasurement();
+    }
+
+    public void initialize() {
+        ADD_TO_FAVORITES = Bundle.getStringTrimmed("org.openide.actions.Bundle","CTL_Tools") + "|" + Bundle.getStringTrimmed("org.netbeans.modules.favorites.Bundle","ACT_Add"); // Tools|Add to Favorites
+        REMOVE_FROM_FAVORITES = Bundle.getStringTrimmed("org.netbeans.modules.favorites.Bundle","ACT_Remove"); // Remove from Favorites
+    }
+
+    public ComponentOperator open(){
+        addToFavoritesNode.performMenuAction(ADD_TO_FAVORITES);
+        favoritesWindow = new FavoritesOperator();
+        return favoritesWindow;
+    }
+
+    @Override
+    public void close() {
+        favoritesWindow.maximize();
+    	Node n=new Node(favoritesWindow.tree(), fileName);
+        n.performPopupAction(REMOVE_FROM_FAVORITES);
+        favoritesWindow.close();
+    }
+    
+    public void prepare() {
+        addToFavoritesNode = new Node(new SourcePackagesNode(fileProject), filePackage + '|' + fileName);
+    }
+    
+}
---
+++ performance/j2se/test/qa-functional/data/PerformanceTestFoldersData/src/folders/javaFolder1000/OpenRubyProject_2.java
@@ -0,0 +1,132 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ *
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ */
+
+package folders.javaFolder1000;
+
+import junit.framework.Test;
+import org.netbeans.jellytools.WizardOperator;
+import org.netbeans.jellytools.actions.ActionNoBlock;
+import org.netbeans.jemmy.operators.ComponentOperator;
+import org.netbeans.jemmy.operators.JButtonOperator;
+import org.netbeans.jemmy.operators.JTextComponentOperator;
+import org.netbeans.junit.NbModuleSuite;
+import org.netbeans.modules.project.ui.test.ProjectSupport;
+import org.netbeans.modules.performance.utilities.CommonUtilities;
+
+/**
+ *
+ * @author mkhramov@netbeans.org
+ */
+public class OpenRubyProject_2 extends org.netbeans.modules.performance.utilities.PerformanceTestCase {
+    public static final String suiteName="Scripting UI Responsiveness Actions suite";
+    private static String projectName; 
+    private JButtonOperator openButton;
+    private String workdir;    
+    
+    public OpenRubyProject_2(String testName) {
+        super(testName);
+        WAIT_AFTER_OPEN = 20000;
+        workdir = CommonUtilities.getProjectsDir();
+    }
+
+    public OpenRubyProject_2(String testName, String performanceDataName) {
+        super(testName, performanceDataName);
+        WAIT_AFTER_OPEN = 20000;
+        workdir = CommonUtilities.getProjectsDir();
+    }
+    
+    @Override
+    public void initialize() {
+        log("::initialize::");
+        closeAllModal();
+    }
+    
+    @Override
+    public void prepare() {
+        // closeProject should be here so the project is available after the test finished
+        ProjectSupport.closeProject(projectName);
+        new ActionNoBlock("File|Open Project...", null).perform(); //NOI18N
+        WizardOperator opd = new WizardOperator("Open Project"); //NOI18N
+        JTextComponentOperator path = new JTextComponentOperator(opd,1);
+        openButton = new JButtonOperator(opd, "Open Project"); //NOI18N
+        String paths = workdir + projectName;
+        path.setText(paths);        
+    }
+
+    @Override
+    public ComponentOperator open() {
+        openButton.pushNoBlock();
+        return null;
+    }
+    
+    @Override
+    public void close() {
+        log("::close");
+        closeAllModal();
+    }
+
+    public void testOpenRubyProject() {
+        projectName = "RubyPerfTest"; //NO18N
+        expectedTime = 18000;
+        doMeasurement();
+    }
+
+    public void testOpenRailsProject() {
+        projectName = "RailsPerfTest";
+        expectedTime = 18000;
+        doMeasurement();
+    }
+
+    public void testOpenPHPProject() {
+        projectName = "PHPPerfTest";
+        expectedTime = 10000;
+        doMeasurement();
+    }
+
+    public static Test suite() {
+        return NbModuleSuite.create(
+            NbModuleSuite.createConfiguration(OpenRubyProject.class)
+            .enableModules(".*")
+            .clusters(".*")
+            .reuseUserDir(true)
+        );
+    }
+}
