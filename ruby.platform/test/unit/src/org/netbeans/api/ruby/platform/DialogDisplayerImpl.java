a711f5d95466123605fd04a9f5ed0a38d41765a3
Martin Krauskopf <mkrauskopf@netbeans.org>
2008-11-25 15:23:01 +0100

    #153275: Misleading Rails Gem Error After upgrade to NB 6.5 RC 2

--- ruby.platform/test/unit/src/org/netbeans/api/ruby/platform/DialogDisplayerImpl.java
+++ ruby.platform/test/unit/src/org/netbeans/api/ruby/platform/DialogDisplayerImpl.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -24,7 +24,7 @@
  * Contributor(s):
  *
  * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2008 Sun
  * Microsystems, Inc. All Rights Reserved.
  *
  * If you wish your version of this file to be governed by only the CDDL
@@ -39,7 +39,7 @@
  * made subject to such option by the copyright holder.
  */
 
-package org.netbeans.modules.ruby.debugger;
+package org.netbeans.api.ruby.platform;
 
 import java.awt.Dialog;
 import junit.framework.Assert;
