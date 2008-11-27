8c0fc9182f01bc2321134d7a7d6e6c1530b2cc8f
Dmitry Lipin <dlipin@netbeans.org>
2008-11-27 19:23:02 +0300

    Add python link to d/l page : sync with live pages

--- installer/infra/build/web/js/build_info.js
+++ installer/infra/build/web/js/build_info.js
@@ -60,6 +60,7 @@ var SOURCES_AND_BINARIES_URL = "javascript: open_zip_link()";
 
 function add_download_tabs() {
 	add_download_tab("6.5");
+	add_download_tab(PYTHON_TITLE, PYTHON_LINK);
 	add_download_tab(DEVELOPMENT_TITLE, DEVELOPMENT_BUILDS_LINK);
 	add_download_tab(ARCHIVE_TITLE,ARCHIVE_BUILDS_LINK);
 }
