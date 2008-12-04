09a77a62ee5497837ba11ea88a51a29603989042
Vince Kraemer <vkraemer@netbeans.org>
2008-12-03 16:47:12 -0800

    fix output for the 60000 millisec case. should be "1 minute" not "1 minute, "

--- glassfish.common/src/org/netbeans/modules/glassfish/common/wizards/Retriever.java
+++ glassfish.common/src/org/netbeans/modules/glassfish/common/wizards/Retriever.java
@@ -409,7 +409,7 @@ public class Retriever implements Runnable {
                 time %= 60000;
                 builder.append(NbBundle.getMessage(Retriever.class, "TIME_MINUTES", minutes));  //NOI18N
             }
-            if(time >= 1000 || builder.length() > 0) {
+            if(time >= 1000) { //  || builder.length() > 0) {
                 if(builder.length() > 0) {
                     builder.append(separator);
                 }
@@ -418,7 +418,7 @@ public class Retriever implements Runnable {
                 if(seconds > 0) {
                     builder.append(NbBundle.getMessage(Retriever.class, "TIME_SECONDS", seconds));  //NOI18N
                 }
-            } else {
+            } else if (time > 0 && builder.length() < 1) {
                 builder.append(NbBundle.getMessage(Retriever.class, "TIME_MILISECONDS", time));  //NOI18N
             }
         }
