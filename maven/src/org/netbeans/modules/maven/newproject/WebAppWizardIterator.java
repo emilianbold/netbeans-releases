6f0a6217cd0f6781ddbda36f73454f93a0ae52c5
Dafe Simonek <dsimonek@netbeans.org>
2008-12-12 12:29:41 +0100

    #154455: initial implementation of special wizard which allows to set J2EE level, from 1.3 to J2EE 5

--- maven/src/org/netbeans/modules/maven/newproject/WebAppWizardIterator.java
+++ maven/src/org/netbeans/modules/maven/newproject/WebAppWizardIterator.java
@@ -39,34 +39,23 @@
 
 package org.netbeans.modules.maven.newproject;
 
-import org.netbeans.modules.maven.api.archetype.Archetype;
 import java.awt.Component;
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.text.MessageFormat;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Enumeration;
 import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Properties;
 import java.util.Set;
 import javax.swing.JComponent;
-import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
-import org.netbeans.modules.maven.api.FileUtilities;
-import org.netbeans.modules.maven.api.NbMavenProject;
-import org.netbeans.modules.maven.api.execute.RunUtils;
-import org.netbeans.modules.maven.execute.BeanRunConfig;
-import org.netbeans.modules.maven.options.MavenCommandSettings;
 import org.netbeans.api.progress.ProgressHandle;
 import org.netbeans.api.project.Project;
 import org.netbeans.api.project.ProjectManager;
+import org.netbeans.modules.maven.api.FileUtilities;
+import org.netbeans.modules.maven.api.NbMavenProject;
 import org.netbeans.spi.project.ui.support.ProjectChooser;
 import org.openide.WizardDescriptor;
-import org.openide.execution.ExecutorTask;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
 import org.openide.util.Exceptions;
@@ -74,40 +63,30 @@ import org.openide.util.NbBundle;
 
 /**
  *
- *@author mkleint
+ *@author Dafe Simonek
  */
-public class MavenWizardIterator implements WizardDescriptor.ProgressInstantiatingIterator {
+public class WebAppWizardIterator implements WizardDescriptor.ProgressInstantiatingIterator {
     
-    private static final long serialVersionUID = 1L;
-    
-    private static final String USER_DIR_PROP = "user.dir"; //NOI18N
-    static final String PROPERTY_CUSTOM_CREATOR = "customCreator"; //NOI18N
-    private transient int index;
-    private transient WizardDescriptor.Panel[] panels;
-    private transient WizardDescriptor wiz;
-    private final List<ChangeListener> listeners;
+    private int index;
+    private WizardDescriptor.Panel[] panels;
     private ArchetypeProviderImpl ngprovider;
+    private WizardDescriptor wiz;
     
-    public MavenWizardIterator() {
-        listeners = new ArrayList<ChangeListener>();
-    }
+    private WebAppWizardIterator() {}
     
-    public static MavenWizardIterator createIterator() {
-        return new MavenWizardIterator();
+    public static WebAppWizardIterator createIterator() {
+        return new WebAppWizardIterator();
     }
-
     
     private WizardDescriptor.Panel[] createPanels() {
         return new WizardDescriptor.Panel[] {
-            new ChooseWizardPanel(),
-            new BasicWizardPanel(false)
+            new BasicWizardPanel(true)
         };
     }
     
     private String[] createSteps() {
         return new String[] {
-            NbBundle.getMessage(MavenWizardIterator.class, "LBL_CreateProjectStep"),
-            NbBundle.getMessage(MavenWizardIterator.class, "LBL_CreateProjectStep2")
+            NbBundle.getMessage(WebAppWizardIterator.class, "LBL_CreateProjectStep2"),
         };
     }
     
@@ -125,11 +104,11 @@ public class MavenWizardIterator implements WizardDescriptor.ProgressInstantiati
             if (parent != null && parent.exists()) {
                 ProjectChooser.setProjectsFolder(parent);
             }
-            
+
             Set<FileObject> resultSet = new LinkedHashSet<FileObject>();
 //            final Archetype archetype = (Archetype)wiz.getProperty("archetype"); //NOI18N<
             dirF.getParentFile().mkdirs();
-            
+
             handle.progress(NbBundle.getMessage(MavenWizardIterator.class, "PRG_Processing_Archetype"), 2);
             ngprovider.runArchetype(dirF.getParentFile(), wiz);
 //            } else {
@@ -169,114 +148,7 @@ public class MavenWizardIterator implements WizardDescriptor.ProgressInstantiati
             handle.finish();
         }
     }
-    
-    public void initialize(WizardDescriptor wiz) {
-        this.wiz = wiz;
-        ngprovider = new ArchetypeProviderImpl();
-        wiz.putProperty(PROPERTY_CUSTOM_CREATOR, ngprovider);
-        index = 0;
-        panels = createPanels();
-        updateSteps();
-    }
-    
-    public void uninitialize(WizardDescriptor wiz) {
-        this.wiz.putProperty("projdir",null); //NOI18N
-        this.wiz.putProperty("name",null); //NOI18N
-        this.wiz = null;
-        panels = null;
-        listeners.clear();
-    }
-    
-    public String name() {
-        return MessageFormat.format(org.openide.util.NbBundle.getMessage(MavenWizardIterator.class, "NameFormat"),
-                new Object[] {new Integer(index + 1), new Integer(panels.length)});
-    }
-    
-    public boolean hasNext() {
-        return index < panels.length - 1;
-    }
-    
-    public boolean hasPrevious() {
-        return index > 0;
-    }
-    
-    public void nextPanel() {
-        if (!hasNext()) {
-            throw new NoSuchElementException();
-        }
-        index++;
-    }
-    
-    public void previousPanel() {
-        if (!hasPrevious()) {
-            throw new NoSuchElementException();
-        }
-        index--;
-    }
-    
-    public WizardDescriptor.Panel current() {
-        return panels[index];
-    }
-    
-    // If nothing unusual changes in the middle of the wizard, simply:
-    public final void addChangeListener(ChangeListener l) {
-        synchronized (listeners) {
-            listeners.add(l);
-        }
-    }
-    
-    public final void removeChangeListener(ChangeListener l) {
-        synchronized (listeners) {
-            listeners.remove(l);
-        }
-    }
 
-    private void fireChange() {
-        synchronized (listeners) {
-            for (ChangeListener list : listeners) {
-                list.stateChanged(new ChangeEvent(this));
-            }
-        }
-    }
-
-    private int runArchetype(File dirF, String gr, String art, String ver, String pack, Archetype arch) {
-        BeanRunConfig config = new BeanRunConfig();
-        config.setActivatedProfiles(Collections.<String>emptyList());
-        config.setExecutionDirectory(dirF);
-        config.setExecutionName(NbBundle.getMessage(MavenWizardIterator.class, "RUN_Project_Creation"));
-        config.setGoals(Collections.singletonList(MavenCommandSettings.getDefault().getCommand(MavenCommandSettings.COMMAND_CREATE_ARCHETYPE))); //NOI18N
-        Properties props = new Properties();
-        props.setProperty("archetypeArtifactId", arch.getArtifactId()); //NOI18N
-        props.setProperty("archetypeGroupId", arch.getGroupId()); //NOI18N
-        props.setProperty("archetypeVersion", arch.getVersion()); //NOI18N
-        if (arch.getRepository() != null) {
-            props.setProperty("remoteRepositories", arch.getRepository()); //NOI18N
-        }
-        props.setProperty("artifactId", art); //NOI18N
-        props.setProperty("groupId", gr); //NOI18N
-        props.setProperty("version", ver); //NOI18N
-        if (pack != null && pack.trim().length() > 0) {
-            props.setProperty("packageName", pack); //NOI18N
-        }
-        config.setProperties(props);
-        config.setTaskDisplayName(NbBundle.getMessage(MavenWizardIterator.class, "RUN_Project_Creation"));
-        // setup executor now..
-        //hack - we need to setup the user.dir sys property..
-        String oldUserdir = System.getProperty(USER_DIR_PROP); //NOI18N
-        System.setProperty(USER_DIR_PROP, dirF.getAbsolutePath()); //NOI18N
-        try {
-            ExecutorTask task = RunUtils.executeMaven(config); //NOI18N
-            return task.result();
-        } finally {
-            if (oldUserdir == null) {
-                System.getProperties().remove(USER_DIR_PROP); //NOI18N
-            } else {
-                System.setProperty(USER_DIR_PROP, oldUserdir); //NOI18N
-            }
-        }
-        
-    }
-    
     private void addJavaRootFolders(FileObject fo) {
         try {
             Project prj = ProjectManager.getDefault().findProject(fo);
@@ -305,22 +177,22 @@ public class MavenWizardIterator implements WizardDescriptor.ProgressInstantiati
             Exceptions.printStackTrace(ex);
         }
     }
-
-    private void updateSteps() {
+    
+    public void initialize(WizardDescriptor wiz) {
+        index = 0;
+        panels = createPanels();
+        this.wiz = wiz;
         // Make sure list of steps is accurate.
-        String[] steps = new String[panels.length];
-        String[] basicOnes = createSteps();
-        System.arraycopy(basicOnes, 0, steps, 0, basicOnes.length);
+        String[] steps = createSteps();
         for (int i = 0; i < panels.length; i++) {
             Component c = panels[i].getComponent();
-            if (i >= basicOnes.length || steps[i] == null) {
+            if (steps[i] == null) {
                 // Default step name to component name of panel.
                 // Mainly useful for getting the name of the target
                 // chooser to appear in the list of steps.
                 steps[i] = c.getName();
             }
-            if (c instanceof JComponent) {
-                // assume Swing components
+            if (c instanceof JComponent) { // assume Swing components
                 JComponent jc = (JComponent) c;
                 // Step #.
                 jc.putClientProperty("WizardPanel_contentSelectedIndex", new Integer(i)); //NOI18N
@@ -328,6 +200,39 @@ public class MavenWizardIterator implements WizardDescriptor.ProgressInstantiati
                 jc.putClientProperty("WizardPanel_contentData", steps); //NOI18N
             }
         }
+        ngprovider = new ArchetypeProviderImpl();
+        wiz.putProperty(MavenWizardIterator.PROPERTY_CUSTOM_CREATOR, ngprovider);
     }
+    
+    public void uninitialize(WizardDescriptor wiz) {
+        panels = null;
+    }
+    
+    public String name() {
+        return MessageFormat.format(NbBundle.getMessage(WebAppWizardIterator.class, "NameFormat"),
+                new Object[] {new Integer(index + 1), new Integer(panels.length)});
+    }
+    
+    public boolean hasNext() {
+        return false;
+    }
+    
+    public boolean hasPrevious() {
+        return false;
+    }
+    
+    public void nextPanel() {
+    }
+    
+    public void previousPanel() {
+    }
+    
+    public WizardDescriptor.Panel current() {
+        return panels[index];
+    }
+    
+    // If nothing unusual changes in the middle of the wizard, simply:
+    public final void addChangeListener(ChangeListener l) {}
+    public final void removeChangeListener(ChangeListener l) {}
     
 }
