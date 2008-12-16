b14ccb14d8f4283f526c8cb57ce9948d44cad196
Denis Anisimov <ads@netbeans.org>
2008-12-16 12:05:34 +0300

    Fix for #79636 - Screen designer tab traversal

--- vmd.midp/src/org/netbeans/modules/vmd/midp/screen/display/ItemDisplayPresenter.java
+++ vmd.midp/src/org/netbeans/modules/vmd/midp/screen/display/ItemDisplayPresenter.java
@@ -57,6 +57,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.datatransfer.Transferable;
 import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Collections;
@@ -70,6 +72,8 @@ public class ItemDisplayPresenter extends ScreenDisplayPresenter {
     private JPanel panel;
     private JLabel label;
     private JComponent contentComponent;
+
+    private static final JButton BUTTON = new JButton();
     
     public ItemDisplayPresenter() {
         panel = new JPanel() {
@@ -80,6 +84,22 @@ public class ItemDisplayPresenter extends ScreenDisplayPresenter {
         };
         panel.setLayout(new GridBagLayout());
         panel.setOpaque(false);
+
+        // Fix for #79636 - Screen designer tab traversal
+        panel.setInputMap( JComponent.WHEN_FOCUSED, BUTTON.getInputMap());
+        panel.addFocusListener( new FocusAdapter() {
+
+            @Override
+            public void focusGained(FocusEvent e) {
+
+                getComponent().getDocument().getTransactionManager().writeAccess(new Runnable() {
+                    public void run() {
+                        getComponent().getDocument().setSelectedComponents(
+                                "screen", Collections.singleton(getComponent()));   // NOI18N
+                    }
+                });
+            }
+        });
         
         label = new JLabel();
         Font bold = label.getFont().deriveFont(Font.BOLD);
