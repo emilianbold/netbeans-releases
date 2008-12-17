52f7962c3a3cc819825f6ea5bbe37c082ba44bd1
Alexander Simon <alexvsimon@netbeans.org>
2008-12-17 15:20:05 +0300

    fixing IZ#154315:Discovery improvements in NB7.0 - keep selected folder in file chooser between IDE sessions

--- cnd.makeproject/src/org/netbeans/modules/cnd/makeproject/ui/wizards/ImportProjectPanel.java
+++ cnd.makeproject/src/org/netbeans/modules/cnd/makeproject/ui/wizards/ImportProjectPanel.java
@@ -305,14 +305,7 @@ public final class ImportProjectPanel extends JPanel {
     }// </editor-fold>//GEN-END:initComponents
 
     private void browseButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_browseButtonActionPerformed
-        String seed = null;
-        if (projectFolder.getText().length() > 0) {
-            seed = projectFolder.getText();
-        } else if (FileChooser.getCurrectChooserFile() != null) {
-            seed = FileChooser.getCurrectChooserFile().getPath();
-        } else {
-            seed = System.getProperty("user.home"); // NOI18N
-        }
+        String seed = projectFolder.getText();
         JFileChooser fileChooser = new FileChooser(
                 getString("PROJECT_DIR_CHOOSER_TITLE_TXT"), // NOI18N
                 getString("PROJECT_DIR_BUTTON_TXT"), // NOI18N
