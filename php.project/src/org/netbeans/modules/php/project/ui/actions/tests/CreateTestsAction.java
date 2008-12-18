a75f0f9c2c8e1df35bf544e3e1cda08592512eff
Tomas Mysik <tmysik@netbeans.org>
2008-12-18 14:51:59 +0100

    Unit tests can be generated for directories
    Only source directories.

--- php.project/src/org/netbeans/modules/php/project/ui/actions/tests/CreateTestsAction.java
+++ php.project/src/org/netbeans/modules/php/project/ui/actions/tests/CreateTestsAction.java
@@ -41,8 +41,10 @@ package org.netbeans.modules.php.project.ui.actions.tests;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.Integer;
+import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Queue;
 import java.util.Set;
 import java.util.concurrent.ConcurrentLinkedQueue;
@@ -85,6 +87,8 @@ import org.openide.windows.InputOutput;
 public final class CreateTestsAction extends NodeAction {
     private static final long serialVersionUID = 9523829206628824L;
 
+    private static final Logger LOGGER = Logger.getLogger(CreateTestsAction.class.getName());
+
     private static final ExecutionDescriptor EXECUTION_DESCRIPTOR
             = new ExecutionDescriptor().controllable(false).frontWindow(false).inputOutput(InputOutput.NULL);
     private static final RequestProcessor RP = new RequestProcessor("Generate PHP Unit tests", 1); // NOI18N
@@ -109,14 +113,21 @@ public final class CreateTestsAction extends NodeAction {
     }
 
     @Override
-    protected void performAction(Node[] activatedNodes) {
-        String phpUnitPath = PhpOptions.getInstance().getPhpUnit();
+    protected void performAction(final Node[] activatedNodes) {
+        if (activatedNodes.length == 0) {
+            return;
+        }
+        final String phpUnitPath = PhpOptions.getInstance().getPhpUnit();
         if (Utils.validatePhpUnit(phpUnitPath) != null) {
             OptionsDisplayer.getDefault().open(PHPOptionsCategory.PATH_IN_LAYER);
             return;
         }
         // XXX check whether test directory exists
-        RUNNABLES.add(getRunnable(activatedNodes, new PhpUnit(phpUnitPath)));
+        RUNNABLES.add(new Runnable() {
+            public void run() {
+                generateTests(activatedNodes, new PhpUnit(phpUnitPath));
+            }
+        });
         TASK.schedule(0);
     }
 
@@ -125,19 +136,28 @@ public final class CreateTestsAction extends NodeAction {
         if (activatedNodes.length == 0) {
             return false;
         }
+
+        Project onlyOneProjectAllowed = null;
         for (Node node : activatedNodes) {
             FileObject fileObj = getFileObject(node);
             if (fileObj == null) {
                 return false;
             }
 
-            if (!CommandUtils.isPhpFile(fileObj)
-                    /*|| (node.getCookie(DataFolder.class) != null)*/) { // XXX allow for directories underneath sources
+            // only php files or folders allowed
+            if (fileObj.isData()
+                    && !CommandUtils.isPhpFile(fileObj)) {
                 return false;
             }
 
             Project prj = FileOwnerQuery.getOwner(fileObj);
             if (prj == null) {
+                return false;
+            }
+            if (onlyOneProjectAllowed == null) {
+                onlyOneProjectAllowed = prj;
+            } else if (!onlyOneProjectAllowed.equals(prj)) {
+                // tests can be generated only for one project at one time
                 return false;
             }
             PhpProject phpProject = prj.getLookup().lookup(PhpProject.class);
@@ -176,120 +196,144 @@ public final class CreateTestsAction extends NodeAction {
         return fileObj;
     }
 
-    private Runnable getRunnable(final Node[] activatedNodes, final PhpUnit phpUnit) {
-        return new Runnable() {
-            public void run() {
-                Set<FileObject> failed = new HashSet<FileObject>();
-                Set<File> toOpen = new HashSet<File>();
-                boolean error = false;
-                for (Node node : activatedNodes) {
-                    FileObject sourceFo = getFileObject(node);
-                    assert sourceFo != null : "A valid file object not found for node: " + node;
+    static List<FileObject> getFileObjects(final Node[] activatedNodes) {
+        final List<FileObject> files = new ArrayList<FileObject>();
+        for (Node node : activatedNodes) {
+            FileObject fo = getFileObject(node);
+            assert fo != null : "A valid file object not found for node: " + node;
+            files.add(fo);
+        }
+        return files;
+    }
 
-                    final File parent = FileUtil.toFile(sourceFo.getParent());
-                    final File generatedFile = getGeneratedFile(sourceFo, parent);
-                    final File testFile = getTestFile(sourceFo, generatedFile);
-                    if (testFile.isFile()) {
-                        // already exists
-                        toOpen.add(testFile);
-                        continue;
-                    }
+    void generateTests(final Node[] activatedNodes, final PhpUnit phpUnit) {
+        List<FileObject> files = getFileObjects(activatedNodes);
+        assert !files.isEmpty() : "No files for tests?!";
 
-                    ExternalProcessBuilder externalProcessBuilder = new ExternalProcessBuilder(phpUnit.getPhpUnit());
-                    externalProcessBuilder = externalProcessBuilder.workingDirectory(parent);
-                    externalProcessBuilder = externalProcessBuilder.addArgument("--skeleton-test"); // NOI18N
-                    externalProcessBuilder = externalProcessBuilder.addArgument(sourceFo.getName());
-                    ExecutionService service = ExecutionService.newService(externalProcessBuilder, EXECUTION_DESCRIPTOR, null);
-                    Future<Integer> result = service.run();
-                    try {
-                        if (result.get() != 0) {
-                            // test not generated
-                            failed.add(sourceFo);
-                            continue;
-                        }
-                        toOpen.add(moveGeneratedFile(generatedFile, testFile));
-                    } catch (InterruptedException ex) {
-                        Exceptions.printStackTrace(ex);
-                    } catch (ExecutionException ex) {
-                        if (!error) {
-                            error = true;
-                            CommandUtils.processExecutionException(ex);
-                        }
-                        break;
-                    }
-                }
-                if (!failed.isEmpty()) {
-                    StringBuilder sb = new StringBuilder();
-                    for (FileObject file : failed) {
-                        sb.append(file.getNameExt());
-                        sb.append("\n");
-                    }
-                    DialogDisplayer.getDefault().notifyLater(new NotifyDescriptor.Message(
-                            NbBundle.getMessage(CreateTestsAction.class, "MSG_TestNotGenerated", sb.toString()), NotifyDescriptor.WARNING_MESSAGE));
-                }
-
-                Set<File> toRefresh = new HashSet<File>();
-                for (File file : toOpen) {
-                    toRefresh.add(file.getParentFile());
-                    try {
-                        FileObject fo = FileUtil.toFileObject(file);
-                        assert fo != null : "File object not found for " + file;
-                        assert fo.isValid() : "File object not valid for " + file;
-                        DataObject dobj = DataObject.find(fo);
-                        EditorCookie ec = dobj.getCookie(EditorCookie.class);
-                        ec.open();
-                    } catch (DataObjectNotFoundException ex) {
-                        Exceptions.printStackTrace(ex);
-                    }
-                }
-
-                if (!toRefresh.isEmpty()) {
-                    FileUtil.refreshFor(toRefresh.toArray(new File[toRefresh.size()]));
+        final Set<FileObject> proceeded = new HashSet<FileObject>();
+        final Set<FileObject> failed = new HashSet<FileObject>();
+        final Set<File> toOpen = new HashSet<File>();
+        try {
+            for (FileObject fo : files) {
+                generateTest(phpUnit, fo, proceeded, failed, toOpen);
+                Enumeration<? extends FileObject> children = fo.getChildren(true);
+                while (children.hasMoreElements()) {
+                    generateTest(phpUnit, children.nextElement(), proceeded, failed, toOpen);
                 }
             }
+        } catch (ExecutionException ex) {
+            LOGGER.log(Level.INFO, null, ex);
+            CommandUtils.processExecutionException(ex);
+        }
 
-            private File getGeneratedFile(FileObject source, File parent) {
-                return new File(parent, source.getName() + "Test.php"); // NOI18N
+        if (!failed.isEmpty()) {
+            StringBuilder sb = new StringBuilder();
+            for (FileObject file : failed) {
+                sb.append(file.getNameExt());
+                sb.append("\n");
             }
+            DialogDisplayer.getDefault().notifyLater(new NotifyDescriptor.Message(
+                    NbBundle.getMessage(CreateTestsAction.class, "MSG_TestNotGenerated", sb.toString()), NotifyDescriptor.WARNING_MESSAGE));
+        }
 
-            private File getTestFile(FileObject source, File generatedFile) {
-                Project project = FileOwnerQuery.getOwner(source);
-                assert project != null : "Project must be found";
-                final PhpProject phpProject = project.getLookup().lookup(PhpProject.class);
-                assert phpProject != null : "PHP project must be found for " + project;
-
-                FileObject testDirectory = ProjectPropertiesSupport.getTestDirectory(phpProject);
-                FileObject sourcesDirectory = ProjectPropertiesSupport.getSourcesDirectory(phpProject);
-                FileObject targetDirectory = testDirectory != null ? testDirectory : sourcesDirectory;
-                assert targetDirectory != null && targetDirectory.isValid() : "Valid target folder for tests must be found for " + phpProject;
-
-                String relativePath = FileUtil.getRelativePath(sourcesDirectory, source.getParent());
-                assert relativePath != null : String.format("Relative path must be found % and %s", sourcesDirectory, source.getParent());
-
-                return new File(new File(FileUtil.toFile(targetDirectory), relativePath), generatedFile.getName());
+        Set<File> toRefresh = new HashSet<File>();
+        for (File file : toOpen) {
+            assert file.isFile() : "File must be given to open: " + file;
+            toRefresh.add(file.getParentFile());
+            try {
+                FileObject fo = FileUtil.toFileObject(file);
+                assert fo != null : "File object not found for " + file;
+                assert fo.isValid() : "File object not valid for " + file;
+                DataObject dobj = DataObject.find(fo);
+                EditorCookie ec = dobj.getCookie(EditorCookie.class);
+                ec.open();
+            } catch (DataObjectNotFoundException ex) {
+                Exceptions.printStackTrace(ex);
             }
+        }
 
-            private File moveGeneratedFile(File generatedFile, File testFile) {
-                assert generatedFile.isFile() : "Generated files must exist: " + generatedFile;
-                assert !testFile.exists() : "Test file cannot exist: " + testFile;
+        if (!toRefresh.isEmpty()) {
+            FileUtil.refreshFor(toRefresh.toArray(new File[toRefresh.size()]));
+        }
+    }
 
-                // create all the parents
-                try {
-                    FileUtil.createFolder(testFile.getParentFile());
-                } catch (IOException exc) {
-                    // what to do now??
-                    Logger.getLogger(CreateTestsAction.class.getName()).log(Level.WARNING, null, exc);
-                    return generatedFile;
-                }
+    private void generateTest(PhpUnit phpUnit, FileObject sourceFo, Set<FileObject> proceeded, Set<FileObject> failed, Set<File> toOpen) throws ExecutionException {
+        if (sourceFo.isFolder()
+                || !CommandUtils.isPhpFile(sourceFo)
+                || proceeded.contains(sourceFo)) {
+            return;
+        }
+        proceeded.add(sourceFo);
 
-                if (!generatedFile.renameTo(testFile)) {
-                    // what to do now??
-                    return generatedFile;
-                }
-                assert testFile.isFile() : "Test file must exist: " + testFile;
-                // XXX adjust 'require...' in the test file
-                return testFile;
+        final File parent = FileUtil.toFile(sourceFo.getParent());
+        final File generatedFile = getGeneratedFile(sourceFo, parent);
+        final File testFile = getTestFile(sourceFo, generatedFile);
+        if (testFile.isFile()) {
+            // already exists
+            toOpen.add(testFile);
+            return;
+        }
+
+        // test does not exist yet
+        ExternalProcessBuilder externalProcessBuilder = new ExternalProcessBuilder(phpUnit.getPhpUnit());
+        externalProcessBuilder = externalProcessBuilder.workingDirectory(parent);
+        externalProcessBuilder = externalProcessBuilder.addArgument("--skeleton-test"); // NOI18N
+        externalProcessBuilder = externalProcessBuilder.addArgument(sourceFo.getName());
+        ExecutionService service = ExecutionService.newService(externalProcessBuilder, EXECUTION_DESCRIPTOR, null);
+        Future<Integer> result = service.run();
+        try {
+            if (result.get() != 0) {
+                // test not generated
+                failed.add(sourceFo);
+                return;
             }
-        };
+            toOpen.add(moveGeneratedFile(generatedFile, testFile));
+        } catch (InterruptedException ex) {
+            Exceptions.printStackTrace(ex);
+        }
+    }
+
+    private File getGeneratedFile(FileObject source, File parent) {
+        return new File(parent, source.getName() + "Test.php"); // NOI18N
+    }
+
+    // XXX cache the project
+    private File getTestFile(FileObject source, File generatedFile) {
+        Project project = FileOwnerQuery.getOwner(source);
+        assert project != null : "Project must be found";
+        final PhpProject phpProject = project.getLookup().lookup(PhpProject.class);
+        assert phpProject != null : "PHP project must be found for " + project;
+
+        FileObject testDirectory = ProjectPropertiesSupport.getTestDirectory(phpProject);
+        FileObject sourcesDirectory = ProjectPropertiesSupport.getSourcesDirectory(phpProject);
+        FileObject targetDirectory = testDirectory != null ? testDirectory : sourcesDirectory;
+        assert targetDirectory != null && targetDirectory.isValid() : "Valid target folder for tests must be found for " + phpProject;
+
+        String relativePath = FileUtil.getRelativePath(sourcesDirectory, source.getParent());
+        assert relativePath != null : String.format("Relative path must be found % and %s", sourcesDirectory, source.getParent());
+
+        return new File(new File(FileUtil.toFile(targetDirectory), relativePath), generatedFile.getName());
+    }
+
+    private File moveGeneratedFile(File generatedFile, File testFile) {
+        assert generatedFile.isFile() : "Generated files must exist: " + generatedFile;
+        assert !testFile.exists() : "Test file cannot exist: " + testFile;
+
+        // create all the parents
+        try {
+            FileUtil.createFolder(testFile.getParentFile());
+        } catch (IOException exc) {
+            // what to do now??
+            LOGGER.log(Level.WARNING, null, exc);
+            return generatedFile;
+        }
+
+        if (!generatedFile.renameTo(testFile)) {
+            // what to do now??
+            return generatedFile;
+        }
+        assert testFile.isFile() : "Test file must exist: " + testFile;
+        // XXX adjust 'require...' in the test file
+        return testFile;
     }
 }
