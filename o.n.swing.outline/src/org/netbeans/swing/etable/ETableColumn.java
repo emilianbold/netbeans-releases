4e9113f69274bbb7d0e905e1e047774b61fb6797
Jiri Rechtacek <jrechtacek@netbeans.org>
2009-01-15 17:34:42 +0100

    #86793: Need ability to define sorting for the first column

--- o.n.swing.outline/src/org/netbeans/swing/etable/ETableColumn.java
+++ o.n.swing.outline/src/org/netbeans/swing/etable/ETableColumn.java
@@ -109,6 +109,7 @@ public class ETableColumn extends TableColumn implements Comparable<ETableColumn
     
     /** Header renderer created by createDefaultHeaderRenderer. */
     private TableCellRenderer myHeaderRenderer;
+    private Comparator nestedComparator;
     
     /** Creates a new instance of ETableColumn */
     public ETableColumn(ETable table) {
@@ -453,6 +454,28 @@ public class ETableColumn extends TableColumn implements Comparable<ETableColumn
             return new FlippingComparator(new RowComparator(column));
         }
     }
+
+    /** Method allowing to set custom comparator for sorting the rows belonging to
+     * same parent in tree-like part of table. The comparator operates on the types
+     * of the given column, e.g. the class of the given column in table-like part
+     * or node in tree-line part of table.
+     *
+     * @param c comparator or null for using the default comparator
+     * @since 1.3
+     */
+    public void setNestedComparator (Comparator c) {
+        nestedComparator = c;
+    }
+
+    /** Returns comparator for sorting the rows belonging to
+     * same parent in tree-like part of table.
+     *
+     * @return comparator or null if no nested comparator was set and the default comparator will be used
+     * @since 1.3
+     */
+    public Comparator getNestedComparator () {
+        return nestedComparator;
+    }
     
     /**
      * Overriden to return our special header renderer.
@@ -628,7 +651,11 @@ public class ETableColumn extends TableColumn implements Comparable<ETableColumn
             if (obj2 == null) {
                 return 1;
             }
-            if ((obj1 instanceof Comparable) && (obj1.getClass().isAssignableFrom(obj2.getClass()))){
+            
+            // check nested comparator
+            if (getNestedComparator () != null) {
+                return getNestedComparator ().compare (obj1, obj2);
+            } else if ((obj1 instanceof Comparable) && (obj1.getClass().isAssignableFrom(obj2.getClass()))){
                 Comparable c1 = (Comparable) obj1;
                 return c1.compareTo(obj2);
             }
