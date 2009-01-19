a1a0db0dd12e71f38e1129b65bfe1e01e3328ad1
PGebauer <pgebauer@netbeans.org>
2009-01-19 13:36:22 +0100

    The rollback of IZ #154926 bugfix

--- db.dataview/src/org/netbeans/modules/db/dataview/output/SQLExecutionHelper.java
+++ db.dataview/src/org/netbeans/modules/db/dataview/output/SQLExecutionHelper.java
@@ -152,7 +152,7 @@ class SQLExecutionHelper {
                 try {
                     int pos = 1;
                     for (int i = 0; i < insertedRow.length; i++) {
-                        if (insertedRow[i] != null && !insertedRow[i].equals("<DEFAULT>")) {
+                        if (insertedRow[i] != null) {
                             DBReadWriteHelper.setAttributeValue(pstmt, pos++, dataView.getDataViewDBTable().getColumnType(i), insertedRow[i]);
                         }
                     }
