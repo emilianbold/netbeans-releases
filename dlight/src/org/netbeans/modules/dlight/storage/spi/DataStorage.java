b5856b348af503eae86ccf89042e7aee5274e306
Vladimir Kvashin <vkvashin@netbeans.org>
2009-01-22 21:04:12 +0300

    code formatting

--- dlight/src/org/netbeans/modules/dlight/storage/spi/DataStorage.java
+++ dlight/src/org/netbeans/modules/dlight/storage/spi/DataStorage.java
@@ -57,68 +57,68 @@ import java.util.List;
  */
 public abstract class DataStorage {
 
-  private final List<DataTableMetadata> tablesMetadata;
+    private final List<DataTableMetadata> tablesMetadata;
 
-  protected DataStorage() {
-    tablesMetadata = new ArrayList<DataTableMetadata>();
-  }
+    protected DataStorage() {
+        tablesMetadata = new ArrayList<DataTableMetadata>();
+    }
 
-  /**
-   * Checks if storage contains data described by <param>data</param>
-   * @param data data to check in storage. In case <param>data</param>
-   * describes some virtual table and its {@link org.netbeans.modules.dlight.core.storage.model.DataTableMetadata#getSourceTables()} method
-   * returns not empty list, this method will check if all source tables this virtual table(view)
-   * is built from  exists in the storage.
-   * @return <code>true</code> if storage contains <param>data</param>,
-   * <code>false</code> otherwise
-   */
-  public final boolean hasData(DataTableMetadata data) {
-    List<DataTableMetadata> sourceTables = data.getSourceTables();
-    if (sourceTables != null) {
-      for (DataTableMetadata md : tablesMetadata) {
-        for (DataTableMetadata t : sourceTables){
-          if (!t.getName().equals(md.getName())){
-            return false;
-          }
+    /**
+     * Checks if storage contains data described by <param>data</param>
+     * @param data data to check in storage. In case <param>data</param>
+     * describes some virtual table and its {@link org.netbeans.modules.dlight.core.storage.model.DataTableMetadata#getSourceTables()} method
+     * returns not empty list, this method will check if all source tables this virtual table(view)
+     * is built from  exists in the storage.
+     * @return <code>true</code> if storage contains <param>data</param>,
+     * <code>false</code> otherwise
+     */
+    public final boolean hasData(DataTableMetadata data) {
+        List<DataTableMetadata> sourceTables = data.getSourceTables();
+        if (sourceTables != null) {
+            for (DataTableMetadata md : tablesMetadata) {
+                for (DataTableMetadata t : sourceTables) {
+                    if (!t.getName().equals(md.getName())) {
+                        return false;
+                    }
+                }
+            }
+            return true;
         }
-      }
-      return true;
+        for (DataTableMetadata md : tablesMetadata) {
+            if (md.getName().equals(data.getName())) {
+                return true;
+            }
+        }
+        return false;
     }
-    for (DataTableMetadata md : tablesMetadata) {
-      if (md.getName().equals(data.getName())) {
-        return true;
-      }
+
+    /**
+     * Factory method
+     * @return
+     */
+    public abstract DataStorage newInstance();
+
+    public abstract String getID();
+
+    /**
+     * Please be sure this method will return not null for the object
+     * which will be registered in Services (using default public constructor
+     * without parameter)
+     * @return storage types
+     */
+    public abstract List<? extends DataStorageType> getStorageTypes();
+
+    protected abstract boolean createTablesImpl(List<? extends DataTableMetadata> tableMetadatas);
+
+    public abstract void addData(String tableName, List<DataRow> data);
+
+    public boolean supportsType(DataStorageType storageType) {
+        return getStorageTypes() != null && getStorageTypes().contains(storageType);
     }
-    return false;
-  }
 
-  /**
-   * Factory method
-   * @return
-   */
-  public abstract DataStorage newInstance();
-
-  public abstract String getID();
-
-  /**
-   * Please be sure this method will return not null for the object
-   * which will be registered in Services (using default public constructor 
-   * without parameter)
-   * @return storage types 
-   */
-  public abstract List<? extends DataStorageType> getStorageTypes();
-
-  protected abstract boolean createTablesImpl(List<? extends DataTableMetadata> tableMetadatas);
-
-  public abstract void addData(String tableName, List<DataRow> data);
-
-  public boolean supportsType(DataStorageType storageType) {
-    return getStorageTypes() != null && getStorageTypes().contains(storageType);
-  }
-
-  public void createTables(List<? extends DataTableMetadata> tableMetadatas) {
-    if (createTablesImpl(tableMetadatas)) {      
-        tablesMetadata.addAll(tableMetadatas);
-     }
-  }
+    public void createTables(List<? extends DataTableMetadata> tableMetadatas) {
+        if (createTablesImpl(tableMetadatas)) {
+            tablesMetadata.addAll(tableMetadatas);
+        }
+    }
 }
