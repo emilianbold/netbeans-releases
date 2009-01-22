2370bf4f8845a6ffd96ec084d040fcb5070cdc5a
jwinblad <jwinblad@netbeans.org>
2009-01-22 14:12:46 -0800

    Http Monitor Support for Internet Explorer - Comment out enabling since this feature by default since it is in a somewhat experimental state

--- web.client.tools.internetexplorer/nativesrc/HttpMonitoringApp.cpp
+++ web.client.tools.internetexplorer/nativesrc/HttpMonitoringApp.cpp
@@ -1,6 +1,42 @@
-// HttpMonitoringApp.cpp: implementation of the CHttpMonitoringApp class.
-//
-//////////////////////////////////////////////////////////////////////
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2008 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ *
+ * Contributor(s):
+ *      jwinblad <jwinblad@netbeans.org>
+ *
+ * Portions Copyrighted 2008 Sun Microsystems, Inc.
+ */
 
 #include "stdafx.h"
 #include "HttpMonitoringApp.h"
@@ -8,7 +44,6 @@
 #include "XMLTag.h"
 #include "DbgpResponse.h"
 #include <time.h>
-//#include <cstdlib> // for rand()
 #include <atlstr.h>
 #include "base64.h"
 
@@ -311,14 +346,16 @@ STDMETHODIMP CTestSink::OnResponse(
     //}
     //response += _T("\r\n");
 
-    //TODO - response URL
+    //TODO - response URL. Need to figure out how to get the URL 
+	// this is a response for and fill it in here. a blank value 
+	// will crash things.
     msg.addChildTagWithValue(_T("url"), _T("http://"));
     
     //TODO - response ID. this should match the ID from the request.
     // a different ID will clear out the history in netbeans of requests
     msg.addChildTagWithValue(_T("id"), 123);
 
-    //TODO - response Name
+    //TODO - response Name. Appears that it should match url above.
     msg.addChildTagWithValue(_T("name"),_T("http://"));
 
     // Content type: comes as a string formatted such as:
