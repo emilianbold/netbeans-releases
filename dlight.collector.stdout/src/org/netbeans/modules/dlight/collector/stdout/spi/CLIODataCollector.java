63035873fb3f0677c264e78a60409cdc72ca3d05
Andrew Krasny <akrasny@netbeans.org>
2009-01-27 21:51:06 +0300

    - Code Formatting + i18n

--- dlight.collector.stdout/src/org/netbeans/modules/dlight/collector/stdout/spi/CLIODataCollector.java
+++ dlight.collector.stdout/src/org/netbeans/modules/dlight/collector/stdout/spi/CLIODataCollector.java
@@ -42,6 +42,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InterruptedIOException;
+import java.io.Reader;
 import java.nio.channels.Channels;
 import java.nio.channels.ClosedByInterruptException;
 import java.util.ArrayList;
@@ -82,258 +83,288 @@ import org.openide.util.NbBundle;
 /**
  * Command Line output data collector.
  * Implements both {@link org.netbeans.modules.dlight.spi.collectorDataCollector}
- * and {@link org.netbeans.modules.dlight.spi.collector.DataCollector} via invocation of a command-line tool and parsing its output.
+ * and {@link org.netbeans.modules.dlight.spi.collector.DataCollector} via
+ * invocation of a command-line tool and parsing its output.
  */
-public final class CLIODataCollector extends IndicatorDataProvider<CLIODCConfiguration> implements 
-    DataCollector<CLIODCConfiguration> {
+public final class CLIODataCollector
+        extends IndicatorDataProvider<CLIODCConfiguration>
+        implements DataCollector<CLIODCConfiguration> {
 
-  private static final Logger log = DLightLogger.getLogger(CLIODataCollector.class);
-  private String command;
-  private  String argsTemplate;
-  private Thread outProcessingThread;
-  private DataStorage storage;
-  private NativeTask collectorTask;
-  private CLIOParser parser;
-  private List<DataTableMetadata> dataTablesMetadata;
-  private ValidationStatus validationStatus = ValidationStatus.initialStatus;
-  private List<ValidationListener> validationListeners = Collections.synchronizedList(new ArrayList<ValidationListener>());
+    private static final Logger log =
+            DLightLogger.getLogger(CLIODataCollector.class);
+    private String command;
+    private String argsTemplate;
+    private Thread outProcessingThread;
+    private DataStorage storage;
+    private NativeTask collectorTask;
+    private CLIOParser parser;
+    private List<DataTableMetadata> dataTablesMetadata;
+    private ValidationStatus validationStatus = ValidationStatus.initialStatus;
+    private List<ValidationListener> validationListeners =
+            Collections.synchronizedList(new ArrayList<ValidationListener>());
 
-  /**
-   *
-   * @param command command to invoke (without arguments)
-   * @param arguments command arguments
-   * @param parser a {@link org.netbeans.modules.dlight.collector.stdout.api.CLIOParser} to parse command output with
-   * @param dataTablesMetadata describes the tables to store parsed data in
-   */
-  CLIODataCollector(CLIODCConfiguration configuration) {
-    this.command = CLIODCConfigurationAccessor.getDefault().getCommand(configuration);
-    this.argsTemplate = CLIODCConfigurationAccessor.getDefault().getArguments(configuration);
-    this.parser = CLIODCConfigurationAccessor.getDefault().getParser(configuration);
-    this.dataTablesMetadata = CLIODCConfigurationAccessor.getDefault().getDataTablesMetadata(configuration);
-  }
+    /**
+     *
+     * @param command command to invoke (without arguments)
+     * @param arguments command arguments
+     * @param parser a {@link org.netbeans.modules.dlight.collector.stdout.api.CLIOParser}
+     * to parse command output with
+     * @param dataTablesMetadata describes the tables to store parsed data in
+     */
+    CLIODataCollector(CLIODCConfiguration configuration) {
+        CLIODCConfigurationAccessor access =
+                CLIODCConfigurationAccessor.getDefault();
 
-
-
-  
-
-  /**
-   * The types of storage this collector supports
-   * @return returns list of {@link org.netbeans.modules.dlight.core.storage.model.DataStorageType}
-   * data collector can put data into
-   */
-  public Collection<DataStorageType> getSupportedDataStorageTypes() {
-    return Arrays.asList(DataStorageTypeFactory.getInstance().getDataStorageType(SQLDataStorage.SQL_DATA_STORAGE_TYPE));
-  }
-
-  public void init(DataStorage storage, DLightTarget target) {
-    this.storage = storage;
-    log.info("Do INIT for " + storage.toString());
-  }
-
-  protected void processLine(String line) {
-    DataRow dataRow = parser.process(line);
-    if (dataRow != null) {
-      if (dataTablesMetadata != null && !dataTablesMetadata.isEmpty() && storage != null) {
-        storage.addData(dataTablesMetadata.iterator().next().getName()/*"prstat"*/, Arrays.asList(dataRow));
-      }
-      notifyIndicators(Arrays.asList(dataRow));
+        this.command = access.getCommand(configuration);
+        this.argsTemplate = access.getArguments(configuration);
+        this.parser = access.getParser(configuration);
+        this.dataTablesMetadata = access.getDataTablesMetadata(configuration);
     }
-  }
 
- 
+    /**
+     * The types of storage this collector supports
+     * @return returns list of {@link org.netbeans.modules.dlight.core.storage.model.DataStorageType}
+     * data collector can put data into
+     */
+    public Collection<DataStorageType> getSupportedDataStorageTypes() {
+        DataStorageTypeFactory dstf = DataStorageTypeFactory.getInstance();
 
-  protected NativeTask getCollectorTaskFor(DLightTarget target) {
-    String cmd = command;
-    if (target instanceof AttachableTarget) {
-      AttachableTarget at = (AttachableTarget) target;
-      cmd = cmd.concat(" ").concat(argsTemplate.replaceAll("@PID", "" + at.getPID()));
-    } else {
-      cmd = cmd.concat(" " + argsTemplate);
+        return Arrays.asList(
+                dstf.getDataStorageType(SQLDataStorage.SQL_DATA_STORAGE_TYPE));
     }
-    return new NativeTask(target.getExecEnv(), cmd, null);
-  }
 
-  private void targetStarted(DLightTarget target) {
-    resetIndicators();
-    collectorTask = getCollectorTaskFor(target);
+    public void init(DataStorage storage, DLightTarget target) {
+        this.storage = storage;
+        log.info("Do INIT for " + storage.toString()); // NOI18N
+    }
 
-    outProcessingThread = new Thread(new Runnable() {
+    protected void processLine(String line) {
+        DataRow dataRow = parser.process(line);
+        if (dataRow != null) {
+            if (dataTablesMetadata != null &&
+                    !dataTablesMetadata.isEmpty() &&
+                    storage != null) {
 
-      public void run() {
-        try {
-          String line;
-          InputStream is = collectorTask.getInputStream();
+                storage.addData(
+                        dataTablesMetadata.iterator().next().getName(),
+                        Arrays.asList(dataRow));
+            }
 
-          if (is == null) {
+            notifyIndicators(Arrays.asList(dataRow));
+        }
+    }
+
+    protected NativeTask getCollectorTaskFor(DLightTarget target) {
+        String cmd = command + " "; // NOI18N
+
+        if (target instanceof AttachableTarget) {
+            AttachableTarget at = (AttachableTarget) target;
+            cmd += argsTemplate.replaceAll("@PID", "" + at.getPID()); // NOI18N
+        } else {
+            cmd += argsTemplate;
+        }
+
+        return new NativeTask(target.getExecEnv(), cmd, null);
+    }
+
+    private void targetStarted(DLightTarget target) {
+        resetIndicators();
+        collectorTask = getCollectorTaskFor(target);
+
+        outProcessingThread = new Thread(new Runnable() {
+
+            public void run() {
+                try {
+                    String line;
+                    InputStream is = collectorTask.getInputStream();
+
+                    if (is == null) {
+                        return;
+                    }
+                    Reader r = Channels.newReader(
+                            Channels.newChannel(is), "UTF-8"); // NOI18N
+
+                    BufferedReader reader = new BufferedReader(r);
+
+                    while ((line = reader.readLine()) != null) {
+                        processLine(line);
+                    }
+
+                    Thread.sleep(10);
+
+                } catch (InterruptedException ex) {
+                    log.fine(Thread.currentThread().getName() +
+                            " interrupted. Stop it."); // NOI18N
+                } catch (InterruptedIOException ex) {
+                    log.fine(Thread.currentThread().getName() +
+                            " interrupted. Stop it."); // NOI18N
+                } catch (ClosedByInterruptException ex) {
+                    log.fine(Thread.currentThread().getName() +
+                            " interrupted. Stop it."); // NOI18N
+                } catch (IOException ex) {
+                    log.fine(Thread.currentThread().getName() +
+                            " io. Stop it."); // NOI18N
+                }
+            }
+        }, "CLI Data Collector Output Redirector"); // NOI18N
+
+        collectorTask.submit();
+        outProcessingThread.start();
+    }
+
+    private void targetFinished(DLightTarget target) {
+        log.fine("Stopping CLIODataCollector: " + // NOI18N
+                collectorTask.getCommand());
+
+        collectorTask.cancel();
+        outProcessingThread.interrupt();
+    }
+
+    /** {@inheritDoc */
+    public List<DataTableMetadata> getDataTablesMetadata() {
+        return dataTablesMetadata;
+    }
+
+    /** {@inheritDoc */
+    public boolean isAttachable() {
+        return true;
+    }
+
+    /** {@inheritDoc */
+    public String getCmd() {
+        return command;
+    }
+
+    /** {@inheritDoc */
+    public String[] getArgs() {
+        return null;
+    }
+
+    /**
+     * Registers a validation listener
+     * @param listener a validation listener to add
+     */
+    public void addValidationListener(ValidationListener listener) {
+        if (!validationListeners.contains(listener)) {
+            validationListeners.add(listener);
+        }
+    }
+
+    /**
+     * Removes a validation listener
+     * @param listener a listener to remove
+     */
+    public void removeValidationListener(ValidationListener listener) {
+        validationListeners.remove(listener);
+    }
+
+    protected void notifyStatusChanged(
+            final ValidationStatus oldStatus,
+            final ValidationStatus newStatus) {
+
+        if (oldStatus.equals(newStatus)) {
             return;
-          }
-
-          BufferedReader reader = new BufferedReader(Channels.newReader(Channels.newChannel(is), "UTF-8")); // NOI18N
-
-          while ((line = reader.readLine()) != null) {
-            processLine(line);
-          }
-
-          Thread.sleep(10);
-
-        } catch (InterruptedException ex) {
-          log.fine(Thread.currentThread().getName() + " interrupted. Stop it.");
-        } catch (InterruptedIOException ex) {
-          log.fine(Thread.currentThread().getName() + " interrupted. Stop it.");
-        } catch (ClosedByInterruptException ex) {
-          log.fine(Thread.currentThread().getName() + " interrupted. Stop it.");
-        } catch (IOException ex) {
-          log.fine(Thread.currentThread().getName() + " io. Stop it.");
-        //Logger.getLogger(CLIODataCollector.class.getName()).log(Level.SEVERE, null, ex);
-        }
-      }
-    }, "CLI Data Collector Output Redirector");
-
-    collectorTask.submit();
-    outProcessingThread.start();
-  }
-
-  private void targetFinished(DLightTarget target) {
-    log.fine("Stopping CLIODataCollector: " + collectorTask.getCommand());
-    collectorTask.cancel();
-    outProcessingThread.interrupt();
-  }
-
-  /** {@inheritDoc */
-  public List<DataTableMetadata> getDataTablesMetadata() {
-    return dataTablesMetadata;
-  }
-
-  /** {@inheritDoc */
-  public boolean isAttachable() {
-    return true;
-  }
-
-  /** {@inheritDoc */
-  public String getCmd() {
-    return command;
-  }
-
-  /** {@inheritDoc */
-  public String[] getArgs() {
-    return null;
-  }
-
-  /**
-   * Registers a validation listener
-   * @param listener a validation listener to add
-   */
-  public void addValidationListener(ValidationListener listener) {
-    if (!validationListeners.contains(listener)) {
-      validationListeners.add(listener);
-    }
-  }
-
-  /**
-   * Removes a validation listener
-   * @param listener a listener to remove
-   */
-  public void removeValidationListener(ValidationListener listener) {
-    validationListeners.remove(listener);
-  }
-
-  protected void notifyStatusChanged(ValidationStatus oldStatus, ValidationStatus newStatus) {
-      if (oldStatus.equals(newStatus)) {
-          return;
-      }
-      
-    for (ValidationListener validationListener : validationListeners) {
-      validationListener.validationStateChanged(this, oldStatus, newStatus);
-    }
-  }
-
-  private static String loc(String key, Object ... params) {
-    return NbBundle.getMessage(CLIODataCollector.class, key, params);
-  }
-
-  public Future<ValidationStatus> validate(final DLightTarget target) {
-    return DLightExecutorService.service.submit(new Callable<ValidationStatus>() {
-
-      public ValidationStatus call() throws Exception {
-        if (validationStatus.isValid()) {
-          return validationStatus;
         }
 
-        ValidationStatus oldStatus = validationStatus;
-        ValidationStatus newStatus = doValidation(target);
-
-        notifyStatusChanged(oldStatus, newStatus);
-
-        validationStatus = newStatus;
-        return newStatus;
-      }
-    });
-  }
-
-  public void invalidate() {
-    validationStatus = ValidationStatus.initialStatus;
-  }
-
-  private ValidationStatus doValidation(final DLightTarget target) {
-    DLightLogger.assertNonUiThread();
-
-    ValidationStatus result = null;
-    boolean fileExists = false;
-    boolean connected = true;
-
-    try {
-      fileExists = HostInfo.fileExists(target.getExecEnv(), command);
-    } catch (HostNotConnectedException ex) {
-      connected = false;
+        for (ValidationListener validationListener : validationListeners) {
+            validationListener.validationStateChanged(this, oldStatus, newStatus);
+        }
     }
 
-    if (connected) {
-      if (fileExists) {
-        result = ValidationStatus.validStatus;
-      } else {
-        result = ValidationStatus.invalidStatus(
-                loc("ValidationStatus.CommandNotFound", // NOI18N
-                command));
-      }
-    } else {
-      ObservableAction<Boolean> connectAction = target.getExecEnv().getConnectToAction();
-
-      connectAction.addObservableActionListener(new ObservableActionListener<Boolean>() {
-          public void actionCompleted(Action source, Boolean result) {
-              DLightManager.getDefault().revalidateSessions();
-          }
-          public void actionStarted(Action source) {
-          }
-      });
-
-      result = ValidationStatus.unknownStatus(
-              loc("ValidationStatus.HostNotConnected"), // NOI18N
-              connectAction);
+    private static String loc(String key, Object... params) {
+        return NbBundle.getMessage(CLIODataCollector.class, key, params);
     }
 
-    return result;
-  }
+    public Future<ValidationStatus> validate(final DLightTarget target) {
+        Callable<ValidationStatus> validationTask =
+                new Callable<ValidationStatus>() {
 
-  public ValidationStatus getValidationStatus() {
-    return validationStatus;
-  }
+                    public ValidationStatus call() throws Exception {
+                        if (validationStatus.isValid()) {
+                            return validationStatus;
+                        }
 
-  public void targetStateChanged(DLightTarget source, State oldState, State newState) {
-    switch (newState){
-      case RUNNING :
-        targetStarted(source);
-        break;
-      case FAILED:
-        targetFinished(source);
-        break;
-      case TERMINATED:
-        targetFinished(source);
-        break;
-      case DONE:
-        targetFinished(source);
-        break;
+                        ValidationStatus oldStatus = validationStatus;
+                        ValidationStatus newStatus = doValidation(target);
+
+                        notifyStatusChanged(oldStatus, newStatus);
+
+                        validationStatus = newStatus;
+                        return newStatus;
+                    }
+                };
+
+        return DLightExecutorService.service.submit(validationTask);
     }
-  }
-  
-  
+
+    public void invalidate() {
+        validationStatus = ValidationStatus.initialStatus;
+    }
+
+    private ValidationStatus doValidation(final DLightTarget target) {
+        DLightLogger.assertNonUiThread();
+
+        ValidationStatus result = null;
+        boolean fileExists = false;
+        boolean connected = true;
+
+        try {
+            fileExists = HostInfo.fileExists(target.getExecEnv(), command);
+        } catch (HostNotConnectedException ex) {
+            connected = false;
+        }
+
+        if (connected) {
+            if (fileExists) {
+                result = ValidationStatus.validStatus;
+            } else {
+                result = ValidationStatus.invalidStatus(
+                        loc("ValidationStatus.CommandNotFound", // NOI18N
+                        command));
+            }
+        } else {
+            ObservableAction<Boolean> connectAction =
+                    target.getExecEnv().getConnectToAction();
+
+            connectAction.addObservableActionListener(
+                    new ObservableActionListener<Boolean>() {
+
+                        public void actionCompleted(Action source, Boolean result) {
+                            DLightManager.getDefault().revalidateSessions();
+                        }
+
+                        public void actionStarted(Action source) {
+                        }
+                    });
+
+            result = ValidationStatus.unknownStatus(
+                    loc("ValidationStatus.HostNotConnected"), // NOI18N
+                    connectAction);
+        }
+
+        return result;
+    }
+
+    public ValidationStatus getValidationStatus() {
+        return validationStatus;
+    }
+
+    public void targetStateChanged(final DLightTarget source,
+            final State oldState, final State newState) {
+
+        switch (newState) {
+            case RUNNING:
+                targetStarted(source);
+                break;
+            case FAILED:
+                targetFinished(source);
+                break;
+            case TERMINATED:
+                targetFinished(source);
+                break;
+            case DONE:
+                targetFinished(source);
+                break;
+        }
+    }
 }
