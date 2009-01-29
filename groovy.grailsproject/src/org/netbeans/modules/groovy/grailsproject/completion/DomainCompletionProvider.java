d6e232e2f7e784090de41a952c130492e46e6362
phejl <phejl@netbeans.org>
2009-01-29 19:17:36 +0100

    Fixed imports.

--- groovy.grailsproject/src/org/netbeans/modules/groovy/grailsproject/completion/DomainCompletionProvider.java
+++ groovy.grailsproject/src/org/netbeans/modules/groovy/grailsproject/completion/DomainCompletionProvider.java
@@ -39,12 +39,10 @@
 
 package org.netbeans.modules.groovy.grailsproject.completion;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
