bd1e48b080ed8723242d5f1bb11b60b9d7c9fe80
Filip Zamboj <fzamboj@netbeans.org>
2009-01-30 19:09:44 +0100

    At tests stabilization

--- spi.debugger.ui/test/qa-functional/src/org/netbeans/debuggercore/ts/LineBreakpointsHitCountTestSuite.java
+++ spi.debugger.ui/test/qa-functional/src/org/netbeans/debuggercore/ts/LineBreakpointsHitCountTestSuite.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- * 
+ *
  * Copyright 2008 Sun Microsystems, Inc. All rights reserved.
- * 
+ *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
  * Development and Distribution License("CDDL") (collectively, the
@@ -20,7 +20,7 @@
  * License Header, with the fields enclosed by brackets [] replaced by
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- * 
+ *
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -31,16 +31,16 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
- * 
+ *
  * Contributor(s):
- * 
+ *
  * Portions Copyrighted 2008 Sun Microsystems, Inc.
  */
 
 package org.netbeans.debuggercore.ts;
 
 import junit.framework.Test;
-import org.netbeans.debuggercore.LineBreakpointsTest;
+import org.netbeans.debuggercore.LineBreakpointsHitCountTest;
 import org.netbeans.jellytools.JellyTestCase;
 import org.netbeans.junit.NbModuleSuite;
 
@@ -48,17 +48,17 @@ import org.netbeans.junit.NbModuleSuite;
  *
  * @author Peter, Revision Petr Cyhelsky
  */
-public class Breakpoints_LineTestSuite extends JellyTestCase {
-    
-    public Breakpoints_LineTestSuite(String name) {
+public class LineBreakpointsHitCountTestSuite extends JellyTestCase {
+
+    public LineBreakpointsHitCountTestSuite(String name) {
         super(name);
     }
-    
+
     @Override
     protected void setUp() throws Exception {
         System.out.println("### " + getName() + " ###");
     }
-    
+
     public static Test suite() {
 //        String os = System.getProperty("os.name");
 //        String jdk = System.getProperty("java.version");
@@ -66,19 +66,8 @@ public class Breakpoints_LineTestSuite extends JellyTestCase {
 //            return NbModuleSuite.create(NbModuleSuite.emptyConfiguration());
 //        } else {
             return NbModuleSuite.create(NbModuleSuite.emptyConfiguration()
-                .addTest(LineBreakpointsTest.class,
-                    "testLineBreakpointCreation",
-                    "testLineBreakpointFunctionality",
-                    "testLineBreakpointFunctionalityAfterContinue",
-                    "testLineBreakpointFunctionalityInStaticMethod",
-                    "testLineBreakpointFunctionalityInInitializer",
-                    "testLineBreakpointFunctionalityInConstructor",
-                    "testLineBreakpointFunctionalityInInnerClass",
-                    "testLineBreakpointFunctionalityInSecondaryClass",
-                    "testConditionalLineBreakpointFunctionality",
-                    "testLineBreakpointActions",
-                    "testLineBreakpointsValidation"
-                    
+                .addTest(LineBreakpointsHitCountTest.class,
+                        "testLineBreakpointsHitCount"
                 )
             .enableModules(".*").clusters(".*"));
 //        }
