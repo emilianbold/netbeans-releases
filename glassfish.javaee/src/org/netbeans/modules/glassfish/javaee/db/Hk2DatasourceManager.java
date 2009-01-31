a23deb61fb655365baf44f717a70acdd0285904f
vince kraemer <vkraemer@netbeans.org>
2009-01-31 07:44:37 -0800

    resolve some bad practice found bugs

--- glassfish.javaee/src/org/netbeans/modules/glassfish/javaee/db/Hk2DatasourceManager.java
+++ glassfish.javaee/src/org/netbeans/modules/glassfish/javaee/db/Hk2DatasourceManager.java
@@ -493,7 +493,7 @@ public class Hk2DatasourceManager implements DatasourceManager {
         
         UrlData urlData = new UrlData(url);
         if(DbUtil.strEmpty(pool.getUrl())) {
-            matchedSettings = DbUtil.strEquivalent(urlData.getHostName(), pool.getHostName()) &&
+            matchedSettings = DbUtil.strEquivalent(urlData.getHostName(), pool.getHostname()) &&
                     DbUtil.strEquivalent(urlData.getPort(), pool.getPort()) &&
                     DbUtil.strEquivalent(urlData.getDatabaseName(), pool.getDatabaseName()) &&
                     DbUtil.strEquivalent(urlData.getSid(), pool.getSid());
@@ -897,7 +897,7 @@ public class Hk2DatasourceManager implements DatasourceManager {
         
         private final String poolName;
         private final String url;
-        private final String hostName;
+        private final String hostname;
         private final String port;
         private final String databaseName;
         private final String username;
@@ -908,7 +908,7 @@ public class Hk2DatasourceManager implements DatasourceManager {
                 String databaseName, String username, String password, String sid) {
             this.poolName = poolName;
             this.url = url;
-            this.hostName = hostName;
+            this.hostname = hostName;
             this.port = port;
             this.databaseName = databaseName;
             this.username = username;
@@ -928,8 +928,8 @@ public class Hk2DatasourceManager implements DatasourceManager {
             return databaseName;
         }
 
-        public String getHostName() {
-            return hostName;
+        public String getHostname() {
+            return hostname;
         }
 
         public String getPassword() {
