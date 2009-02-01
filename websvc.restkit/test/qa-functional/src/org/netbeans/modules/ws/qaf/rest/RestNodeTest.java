217831824af48b4248623f4bbef6d2ea9c8e77e6
Lukas Jungmann <jungi@netbeans.org>
2009-02-02 00:15:37 +0100

    comment

--- websvc.restkit/test/qa-functional/src/org/netbeans/modules/ws/qaf/rest/RestNodeTest.java
+++ websvc.restkit/test/qa-functional/src/org/netbeans/modules/ws/qaf/rest/RestNodeTest.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2008 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2009 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -34,7 +34,7 @@
  *
  * Contributor(s):
  *
- * Portions Copyrighted 2008 Sun Microsystems, Inc.
+ * Portions Copyrighted 2009 Sun Microsystems, Inc.
  */
 package org.netbeans.modules.ws.qaf.rest;
 
@@ -47,6 +47,8 @@ import org.netbeans.junit.NbModuleSuite;
 
 /**
  * Tests REST node in project logical view
+ *
+ * Duration of this test suite: aprox. 2min
  *
  * @author lukas
  */
