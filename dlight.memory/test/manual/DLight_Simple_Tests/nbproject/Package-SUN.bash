9f18a45ced0a4db938131cdebbd4d1a45553966f
Vladimir Kvashin <vkvashin@netbeans.org>
2009-02-08 22:53:45 +0300

    Added a test project for memory monitor and other gizmo stuff

---
+++ dlight.memory/test/manual/DLight_Simple_Tests/nbproject/Package-SUN.bash
@@ -0,0 +1,72 @@
+#!/bin/bash -x
+
+#
+# Generated - do not edit!
+#
+
+# Macros
+TOP=`pwd`
+PLATFORM=SunStudio-Solaris-x86
+TMPDIR=build/SUN/${PLATFORM}/tmp-packaging
+TMPDIRNAME=tmp-packaging
+OUTPUT_PATH=dist/SUN/${PLATFORM}/profiler_simple_tests
+OUTPUT_BASENAME=profiler_simple_tests
+PACKAGE_TOP_DIR=profilersimpletests/
+
+# Functions
+function checkReturnCode
+{
+    rc=$?
+    if [ $rc != 0 ]
+    then
+        exit $rc
+    fi
+}
+function makeDirectory
+# $1 directory path
+# $2 permission (optional)
+{
+    mkdir -p "$1"
+    checkReturnCode
+    if [ "$2" != "" ]
+    then
+      chmod $2 "$1"
+      checkReturnCode
+    fi
+}
+function copyFileToTmpDir
+# $1 from-file path
+# $2 to-file path
+# $3 permission
+{
+    cp "$1" "$2"
+    checkReturnCode
+    if [ "$3" != "" ]
+    then
+        chmod $3 "$2"
+        checkReturnCode
+    fi
+}
+
+# Setup
+cd "${TOP}"
+mkdir -p dist/SUN/${PLATFORM}/package
+rm -rf ${TMPDIR}
+mkdir -p ${TMPDIR}
+
+# Copy files and create directories and links
+cd "${TOP}"
+makeDirectory ${TMPDIR}/profilersimpletests/bin
+copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
+
+
+# Generate tar file
+cd "${TOP}"
+rm -f dist/SUN/${PLATFORM}/package/profilersimpletests.tar
+cd ${TMPDIR}
+tar -vcf ../../../../dist/SUN/${PLATFORM}/package/profilersimpletests.tar *
+checkReturnCode
+
+# Cleanup
+cd "${TOP}"
+rm -rf ${TMPDIR}
