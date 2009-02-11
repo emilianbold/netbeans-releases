0601bbc5e1c7997e7675cd7648e86d0853625c4d
Vladimir Kvashin <vkvashin@netbeans.org>
2009-02-11 15:29:02 +0300

    #158385 NPE when creating new project

--- cnd.makeproject/src/org/netbeans/modules/cnd/makeproject/api/configurations/MakeConfigurationDescriptor.java
+++ cnd.makeproject/src/org/netbeans/modules/cnd/makeproject/api/configurations/MakeConfigurationDescriptor.java
@@ -207,10 +207,11 @@ public class MakeConfigurationDescriptor extends ConfigurationDescriptor impleme
             }
         }
 //        addSourceFilesFromFolders(sourceFileFolders, false, false, true);
-
-        while (sourceFileFolders.hasNext()) {
-            SourceFolderInfo sourceFolderInfo = sourceFileFolders.next();
-            addSourceFilesFromRoot(getLogicalFolders(), sourceFolderInfo.getFile(), false, true);
+        if (sourceFileFolders != null) {
+            while (sourceFileFolders.hasNext()) {
+                SourceFolderInfo sourceFolderInfo = sourceFileFolders.next();
+                addSourceFilesFromRoot(getLogicalFolders(), sourceFolderInfo.getFile(), false, true);
+            }
         }
         setModified(true);
     }
