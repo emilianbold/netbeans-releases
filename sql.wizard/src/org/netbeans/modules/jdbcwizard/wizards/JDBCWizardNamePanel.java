473f1c3f494de8c76a2f23ca7c2f4f8568d539a3
Jiri Rechtacek <jrechtacek@netbeans.org>
2009-02-12 17:33:14 +0100

    #158464: Wrong extending java.awt.Component by impl of WD.Panel

--- sql.wizard/src/org/netbeans/modules/jdbcwizard/wizards/JDBCWizardNamePanel.java
+++ sql.wizard/src/org/netbeans/modules/jdbcwizard/wizards/JDBCWizardNamePanel.java
@@ -13,13 +13,13 @@
  * "Portions Copyrighted [year] [name of copyright owner]"
  * 
  * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2009 Sun
  * Microsystems, Inc. All Rights Reserved.
  */
 
 /*
  * 
- * Copyright 2005 Sun Microsystems, Inc.
+ * Copyright 2009 Sun Microsystems, Inc.
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -36,18 +36,11 @@
  */
 package org.netbeans.modules.jdbcwizard.wizards;
 
-import java.awt.BorderLayout;
 import java.awt.Component;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
@@ -55,44 +48,18 @@ import javax.swing.event.ChangeListener;
 import org.openide.NotifyDescriptor;
 import org.openide.WizardDescriptor;
 import org.openide.util.HelpCtx;
-import org.openide.util.NbBundle;
 
 /**
  * Accepts input of unique name for new JDBC Collaboration instance.
  * 
  * @author
  */
-public class JDBCWizardNamePanel extends JPanel implements WizardDescriptor.Panel {
-
-    /**
-     * 
-     */
-    private static final long serialVersionUID = 1L;
-
-    class NameFieldKeyAdapter extends KeyAdapter {
-        /**
-         * Overrides default implementation to notify listeners of new collab name value in
-         * associated textfield.
-         * 
-         * @param e KeyEvent to be handled
-         */
-        public void keyReleased(final KeyEvent e) {
-            final String collaborationName = JDBCWizardNamePanel.this.textField.getText();
-
-            if (collaborationName != null && collaborationName.trim().length() != 0) {
-                JDBCWizardNamePanel.this.collabName = collaborationName.trim();
-            } else {
-                JDBCWizardNamePanel.this.collabName = null;
-            }
-
-            JDBCWizardNamePanel.this.fireChangeEvent();
-        }
-    }
+public class JDBCWizardNamePanel implements WizardDescriptor.Panel {
 
     protected String collabName;
 
     /* Set <ChangeListeners> */
-    protected final Set listeners = new HashSet(1);
+    protected final Set<ChangeListener> listeners = new HashSet<ChangeListener>(1);
 
     protected JDBCCollaborationWizard owner;
 
@@ -101,73 +68,13 @@ public class JDBCWizardNamePanel extends JPanel implements WizardDescriptor.Pane
     protected String title;
 
     /**
-     * No-arg constructor for this wizard descriptor.
-     */
-    public JDBCWizardNamePanel() {
-        this.setLayout(new BorderLayout());
-
-        final JPanel outerPanel = new JPanel();
-        outerPanel.setLayout(new GridBagLayout());
-
-        // Top filler panel to absorb 20% of any expansion up and down the page.
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.anchor = GridBagConstraints.PAGE_START;
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.weightx = 1.0;
-        gbc.weighty = 0.2;
-        outerPanel.add(new JPanel(), gbc);
-
-        // Text field label.
-        final JLabel header = new JLabel(NbBundle.getMessage(JDBCWizardNamePanel.class, "LBL_tblwizard_namefield"));
-        gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        gbc.anchor = GridBagConstraints.LINE_START;
-        gbc.fill = GridBagConstraints.NONE;
-        gbc.insets.right = 10;
-        gbc.weightx = 0.0;
-        gbc.weighty = 0.0;
-        outerPanel.add(header, gbc);
-
-        // Text field.
-        this.textField = new JTextField();
-        this.textField.addKeyListener(new NameFieldKeyAdapter());
-
-        gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 2;
-        gbc.anchor = GridBagConstraints.LINE_START;
-        gbc.fill = GridBagConstraints.HORIZONTAL;
-        gbc.weightx = 1.0;
-        gbc.weighty = 0.0;
-        outerPanel.add(this.textField, gbc);
-
-        // Bottom filler panel to absorb 80% of any expansion up and down the page.
-        gbc = new GridBagConstraints();
-        gbc.gridx = 0;
-        gbc.gridy = 3;
-        gbc.anchor = GridBagConstraints.PAGE_START;
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.weightx = 1.0;
-        gbc.weighty = 0.8;
-        outerPanel.add(new JPanel(), gbc);
-
-        this.add(outerPanel, BorderLayout.CENTER);
-    }
-
-    /**
      * Create the wizard panel descriptor, using the given panel title, content panel
      * 
      * @param myOwner JDBCWizard that owns this panel
      * @param panelTitle text to display as panel title
      */
     public JDBCWizardNamePanel(final JDBCCollaborationWizard myOwner, final String panelTitle) {
-        this();
-
         this.title = panelTitle;
-        this.setName(this.title);
         this.owner = myOwner;
     }
 
@@ -187,7 +94,7 @@ public class JDBCWizardNamePanel extends JPanel implements WizardDescriptor.Pane
         Iterator it;
 
         synchronized (this.listeners) {
-            it = new HashSet(this.listeners).iterator();
+            it = new HashSet<ChangeListener>(this.listeners).iterator();
         }
 
         final ChangeEvent ev = new ChangeEvent(this);
@@ -204,7 +111,7 @@ public class JDBCWizardNamePanel extends JPanel implements WizardDescriptor.Pane
      * @see JDBCWizardPanel#getComponent
      */
     public Component getComponent() {
-        return this;
+        return new JDBCWizardNamePanelUI (this, owner, title);
     }
 
     /**
@@ -223,16 +130,6 @@ public class JDBCWizardNamePanel extends JPanel implements WizardDescriptor.Pane
         // Show no Help button for this panel:
         return new HelpCtx(JDBCWizardNamePanel.class);
 
-    }
-
-    /**
-     * Indicates whether current contents of collaboration name textfield correspond to the name of
-     * an existing collaboration in the current project.
-     * 
-     * @return true if textfield contains the name of an existing collab; false otherwise
-     */
-    public boolean isDuplicateCollabName() {
-        return false;
     }
 
     /**
