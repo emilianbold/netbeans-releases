107a08c4dd496d1e8091d6c6d1d895f1ff7fc013
Alexander Simon <alexvsimon@netbeans.org>
2009-02-12 12:40:05 +0300

    noIZ: cleanup unit test trace

--- cnd.completion/test/unit/src/org/netbeans/modules/cnd/completion/includes/CCIncludesAuxTestCase.java
+++ cnd.completion/test/unit/src/org/netbeans/modules/cnd/completion/includes/CCIncludesAuxTestCase.java
@@ -35,33 +35,43 @@ import org.netbeans.modules.cnd.test.BaseTestCase;
  */
 public class CCIncludesAuxTestCase extends BaseTestCase {
 
+    private static final boolean TRACE = false;
+
     /**
      * Creates a new instance of CCIncludesAuxTestCase
      */
     public CCIncludesAuxTestCase(String testName) {
         super(testName);
     }
-    
+
     public void testTextShrinking() throws Exception {
         String text = "/very/long/path/to/include/dir";
         CsmIncludeCompletionItem item = new CsmIncludeCompletionItem(0, 0, text, "on/Unix/system", "", false, true, false);
         String shrinked = item.getRightText(true, "/");
-        System.err.println("shrinked is " + shrinked);
+        if (TRACE) {
+            System.err.println("shrinked is " + shrinked);
+        }
         assertEquals("/very/long.../Unix/system", shrinked);
         text = "C:\\very\\long\\path\\to\\include\\dir";
         item = new CsmIncludeCompletionItem(0, 0, text, "on\\Windows\\system", "", false, true, false);
         shrinked = item.getRightText(true, "\\");
-        System.err.println("shrinked is " + shrinked);
+        if (TRACE) {
+            System.err.println("shrinked is " + shrinked);
+        }
         assertEquals("C:\\very\\long...\\Windows\\system", shrinked);
         text = "C:\\very\\long\\path\\to\\mixed/include/dir";
         item = new CsmIncludeCompletionItem(0, 0, text, "on/Windows//mixed", "", false, true, false);
         shrinked = item.getRightText(true, "\\");
-        System.err.println("shrinked is " + shrinked);
+        if (TRACE) {
+            System.err.println("shrinked is " + shrinked);
+        }
         assertEquals("C:\\very\\long...\\\\mixed", shrinked);
         text = "/very/long/path/to\\include\\mixed\\dir";
         item = new CsmIncludeCompletionItem(0, 0, text, "on/unix/mixed", "", false, true, false);
         shrinked = item.getRightText(true, "/");
-        System.err.println("shrinked is " + shrinked);
+        if (TRACE) {
+            System.err.println("shrinked is " + shrinked);
+        }
         assertEquals("/very/long.../unix/mixed", shrinked);
     }
 }
