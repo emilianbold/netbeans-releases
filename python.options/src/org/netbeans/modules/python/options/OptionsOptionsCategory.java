5650c9dd99eff72e868a877796465755aeb50db4
Tomas Holy <t_h@netbeans.org>
2009-02-13 17:39:19 +0100

    Missing ImageUtilities import in Python cluster

--- python.options/src/org/netbeans/modules/python/options/OptionsOptionsCategory.java
+++ python.options/src/org/netbeans/modules/python/options/OptionsOptionsCategory.java
@@ -5,14 +5,14 @@
 package org.netbeans.modules.python.options;
 
 import javax.swing.Icon;
-import javax.swing.ImageIcon;
 import org.netbeans.spi.options.OptionsCategory;
 import org.netbeans.spi.options.OptionsPanelController;
 import org.openide.util.NbBundle;
-import org.openide.util.Utilities;
+import org.openide.util.ImageUtilities;
 
 public final class OptionsOptionsCategory extends OptionsCategory {
 
+    @Override
     public Icon getIcon() {
         return ImageUtilities.loadImageIcon("org/netbeans/modules/python/options/py_25_32.png", false);
     }
