0a982b6bba07656752b7ea16b1bb9fa5aee7c197
Maria Tishkova <mromashova@netbeans.org>
2009-02-18 14:00:04 +0300

    No IZ: NPE fixed

--- dlight/src/org/netbeans/modules/dlight/spi/impl/TimeEditor.java
+++ dlight/src/org/netbeans/modules/dlight/spi/impl/TimeEditor.java
@@ -66,7 +66,7 @@ public class TimeEditor extends PropertyEditorSupport {
 
     @Override
     public String getAsText() {
-        return ((Time) getValue()).equals(zeroTime) ? "0" : format.format(((Time) getValue()).getNanos() / 1e9);
+        return ((Time) getValue() == null ? "0" : ((Time) getValue()).equals(zeroTime) ? "0" : format.format(((Time) getValue()).getNanos() / 1e9));
     }
 
     @Override
