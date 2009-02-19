3d07b46229ad3f128485a700cc3d1441dcf251f4
Tomas Stupka <tstupka@netbeans.org>
2009-02-19 23:12:19 +0100

    one saved query shold open only one topcomponent

--- bugtracking/src/org/netbeans/modules/bugtracking/ui/query/QueryTopComponent.java
+++ bugtracking/src/org/netbeans/modules/bugtracking/ui/query/QueryTopComponent.java
@@ -360,7 +360,7 @@ final class QueryTopComponent extends TopComponent implements PropertyChangeList
 
     @Override
     protected String preferredID() {
-        return PREFERRED_ID;
+        return query != null && query.getDisplayName() != null ? query.getDisplayName() : PREFERRED_ID;
     }
 
     public void propertyChange(PropertyChangeEvent evt) {
@@ -549,8 +549,13 @@ final class QueryTopComponent extends TopComponent implements PropertyChangeList
                 public void actionPerformed(ActionEvent e) {
                     SwingUtilities.invokeLater(new Runnable() {
                         public void run() {
-                            QueryTopComponent tc = new QueryTopComponent(query);
-                            tc.open();
+                            TopComponent tc = WindowManager.getDefault().findTopComponent(query.getDisplayName());
+                            if(tc == null) {
+                                tc = new QueryTopComponent(query);
+                            }
+                            if(!tc.isOpened()) {
+                                tc.open();
+                            }
                             tc.requestActive();
                         }
                     });
