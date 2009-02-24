713ced3961d4a38d1044c92b4dc9b9c5a19576b6
Jaroslav Tulach <jtulach@netbeans.org>
2009-02-24 14:11:03 +0100

    Fixing NPE in CodeAssistanceOptions.java by reverting MakeProjectType back - e.g. it implements AntBasedProjectType again, yet it does not register itself in the global lookup.

--- cnd.makeproject/src/org/netbeans/modules/cnd/makeproject/MakeProjectType.java
+++ cnd.makeproject/src/org/netbeans/modules/cnd/makeproject/MakeProjectType.java
@@ -41,15 +41,42 @@
 
 package org.netbeans.modules.cnd.makeproject;
 
+import java.io.IOException;
+import org.netbeans.api.project.Project;
+import org.netbeans.spi.project.support.ant.AntBasedProjectType;
+import org.netbeans.spi.project.support.ant.AntProjectHelper;
+
 /**
  * Factory for simple Make projects.
  */
-public final class MakeProjectType {
+public final class MakeProjectType implements AntBasedProjectType {
 
     public static final String TYPE = "org.netbeans.modules.cnd.makeproject"; // NOI18N
+    private static final String PROJECT_CONFIGURATION_NAME = "data"; // NOI18N
     public static final String PROJECT_CONFIGURATION_NAMESPACE = "http://www.netbeans.org/ns/make-project/1"; // NOI18N
+    private static final String PRIVATE_CONFIGURATION_NAME = "data"; // NOI18N
     static final String PRIVATE_CONFIGURATION_NAMESPACE = "http://www.netbeans.org/ns/make-project-private/1"; // NOI18N
     public static final String MAKE_DEP_PROJECTS = "make-dep-projects"; // NOI18N
     public static final String MAKE_DEP_PROJECT = "make-dep-project"; // NOI18N
     public static final String SOURCE_ENCODING_TAG = "sourceEncoding"; // NOI18N
+    
+    /** Do nothing, just a service. */
+    MakeProjectType() {}
+    
+    public String getType() {
+        return TYPE;
+    }
+    
+    public Project createProject(AntProjectHelper helper) throws IOException {
+        return new MakeProject(helper);
+    }
+
+    public String getPrimaryConfigurationDataElementName(boolean shared) {
+        return shared ? PROJECT_CONFIGURATION_NAME : PRIVATE_CONFIGURATION_NAME;
+    }
+    
+    public String getPrimaryConfigurationDataElementNamespace(boolean shared) {
+        return shared ? PROJECT_CONFIGURATION_NAMESPACE : PRIVATE_CONFIGURATION_NAMESPACE;
+    }
+    
 }
