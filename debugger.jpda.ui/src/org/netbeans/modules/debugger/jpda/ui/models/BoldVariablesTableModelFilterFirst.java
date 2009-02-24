23c661ca098891a4483f78282cc5cc0e1858c849
mentlicher <mentlicher@netbeans.org>
2009-02-24 18:16:07 +0100

    #158955 - Make object values non-editable. Because "disabled" color makes values much less readable, foreground color is set to black.

--- debugger.jpda.ui/src/org/netbeans/modules/debugger/jpda/ui/models/BoldVariablesTableModelFilterFirst.java
+++ debugger.jpda.ui/src/org/netbeans/modules/debugger/jpda/ui/models/BoldVariablesTableModelFilterFirst.java
@@ -152,12 +152,14 @@ Constants {
             sb.append ("<font color=");
             sb.append (Integer.toHexString ((color.getRGB () & 0xffffff)));
             sb.append (">");
+        } else {
+            sb.append ("<font color=000000>");
         }
         text = text.replaceAll ("&", "&amp;");
         text = text.replaceAll ("<", "&lt;");
         text = text.replaceAll (">", "&gt;");
         sb.append (text);
-        if (color != null) sb.append ("</font>");
+        /*if (color != null)*/ sb.append ("</font>");
         if (italics) sb.append ("</i>");
         if (bold) sb.append ("</b>");
         sb.append ("</html>");
