875c5524835840938bb9061757fcf1058ca38b82
Vladimir Voskresensky <vv159170@netbeans.org>
2009-02-25 16:55:00 +0300

    IZ#148223: IDE can't recognize overloaded operator&& (support for global operators)

--- cnd.completion/test/unit/src/org/netbeans/modules/cnd/completion/cplusplus/hyperlink/ClassMembersHyperlinkTestCase.java
+++ cnd.completion/test/unit/src/org/netbeans/modules/cnd/completion/cplusplus/hyperlink/ClassMembersHyperlinkTestCase.java
@@ -50,12 +50,12 @@ public class ClassMembersHyperlinkTestCase extends HyperlinkBaseTestCase {
         super(testName);
     }
 
-//    public void testIZ148223() throws Exception {
-//        // IZ#148223: IDE can't recognize overloaded operator&&
-//        performTest("operators_hyperlink.cpp", 65, 21, "operators_hyperlink.cpp", 6, 9); // cc in (a1 && b1).cc()
-//        performTest("operators_hyperlink.cpp", 66, 21, "operators_hyperlink.cpp", 6, 9); // cc in (a1 && e1).cc()
-//        performTest("operators_hyperlink.cpp", 67, 21, "operators_hyperlink.cpp", 14, 9); // c1 in (e1 && d1)->c1()
-//    }
+    public void testIZ148223() throws Exception {
+        // IZ#148223: IDE can't recognize overloaded operator&&
+        performTest("operators_hyperlink.cpp", 65, 21, "operators_hyperlink.cpp", 6, 9); // cc in (a1 && b1).cc()
+        performTest("operators_hyperlink.cpp", 66, 21, "operators_hyperlink.cpp", 6, 9); // cc in (a1 && e1).cc()
+        performTest("operators_hyperlink.cpp", 67, 21, "operators_hyperlink.cpp", 14, 9); // c1 in (e1 && d1)->c1()
+    }
 
     public void testIZ157837() throws Exception {
         // IZ#157837: incorrect deref of function-type fields
