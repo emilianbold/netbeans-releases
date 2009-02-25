699a0a183f049a1a4c3f5205d5355d5782251e29
Vladimir Kvashin <vkvashin@netbeans.org>
2009-02-25 11:39:15 +0300

    fixed test compilation

--- dlight.indicators/test/unit/src/org/netbeans/modules/dlight/indicators/graph/CyclicArrayTest.java
+++ dlight.indicators/test/unit/src/org/netbeans/modules/dlight/indicators/graph/CyclicArrayTest.java
@@ -37,7 +37,7 @@
  * Portions Copyrighted 2008 Sun Microsystems, Inc.
  */
 
-package org.netbeans.dlight.cpu.graph;
+package org.netbeans.modules.dlight.indicators.graph;
 
 import org.netbeans.modules.dlight.indicators.graph.CyclicArray;
 import org.junit.Test;
