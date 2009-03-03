d9a627ce819b353b5087ce746662a980ea2141ba
Marian Petras <mpetras@netbeans.org>
2009-03-03 16:47:28 +0100

    improved IssueFinder

--- bugtracking/test/unit/src/org/netbeans/modules/bugtracking/util/IssueFinderTest.java
+++ bugtracking/test/unit/src/org/netbeans/modules/bugtracking/util/IssueFinderTest.java
@@ -62,55 +62,196 @@ public class IssueFinderTest {
 
         checkNoIssueSpansFound("bug##123456");
         checkNoIssueSpansFound("bug ##123456");
-        checkNoIssueSpansFound("bug# #123456");
         checkNoIssueSpansFound("bug## 123456");
         checkNoIssueSpansFound("bug###123456");
 
         checkNoIssueSpansFound("sbug 123456");
-        checkNoIssueSpansFound("sbug #123456");
         checkNoIssueSpansFound("sbug#123456");
 
-        checkIssueSpans("bug123456", "bug123456");
+        checkNoIssueSpansFound("123456");
+        checkNoIssueSpansFound(" 123456");
+        checkNoIssueSpansFound("  123456");
+        checkNoIssueSpansFound("   123456");
+        checkNoIssueSpansFound("bug123456");
+        checkNoIssueSpansFound("bug#123456");
+        checkNoIssueSpansFound("bug# 123456");
+        checkNoIssueSpansFound("bug#  123456");
+
+        checkIssueSpans("#123456", "#123456");
+        checkIssueSpans("# 123456", "# 123456");
+        checkIssueSpans(" #123456", "#123456");
+        checkIssueSpans("  #123456", "#123456");
+        checkIssueSpans(" # 123456", "# 123456");
+        checkIssueSpans("#  123456", "#  123456");
         checkIssueSpans("bug 123456", "bug 123456");
-        checkIssueSpans("bug#123456", "bug#123456");
         checkIssueSpans("bug  123456", "bug  123456");
-        checkIssueSpans("bug# 123456", "bug# 123456");
         checkIssueSpans("bug #123456", "bug #123456");
         checkIssueSpans("bug   123456", "bug   123456");
         checkIssueSpans("bug  #123456", "bug  #123456");
         checkIssueSpans("bug # 123456", "bug # 123456");
-        checkIssueSpans("bug#  123456", "bug#  123456");
+
+        checkIssueSpans("bug# #123456", "#123456");
+        checkIssueSpans("sbug #123456", "#123456");
+
+        checkIssueSpans("#67888 and #73573", "#67888", "#73573");
+        checkIssueSpans("bugs #67888 and #73573", "#67888", "#73573");
+        checkIssueSpans("issues #67888 and #73573", "#67888", "#73573");
+        checkIssueSpans("bugs #67888, #12345 and #73573", "#67888", "#12345", "#73573");
+
+        checkIssueSpans("#123cdE", "#123cdE");
+        checkIssueSpans("#123CDe", "#123CDe");
+
+        checkIssueSpans("#123cd G", "#123cd");
+        checkIssueSpans("#123cd g", "#123cd");
+        checkNoIssueSpansFound("#123cdG");
+        checkNoIssueSpansFound("#123cdg");
+
+        checkIssueSpans("bug\n123456", "bug\n123456");
+        checkIssueSpans("* bug\n123456", "bug\n123456");
+        checkIssueSpans("* bug\n 123456", "bug\n 123456");
+        checkIssueSpans("* bug\n* 123456", "bug\n* 123456");
+        checkIssueSpans("* bug\n * 123456", "bug\n * 123456");
+        checkIssueSpans("* bug \n * 123456", "bug \n * 123456");
+        checkIssueSpans("bug\n#123456", "bug\n#123456");
+        checkIssueSpans("* bug\n#123456", "bug\n#123456");
+        checkIssueSpans("* bug\n #123456", "bug\n #123456");
+        checkIssueSpans("* bug\n* #123456", "bug\n* #123456");
+        checkIssueSpans("* bug\n * #123456", "bug\n * #123456");
+
+        checkIssueSpans("bug\n\n123456", "bug\n\n123456");
+        checkIssueSpans("* bug\n\n123456", "bug\n\n123456");
+        checkIssueSpans("* bug\n\n 123456", "bug\n\n 123456");
+        checkIssueSpans("* bug\n\n* 123456", "bug\n\n* 123456");
+        checkIssueSpans("* bug\n\n * 123456", "bug\n\n * 123456");
+        checkIssueSpans("* bug \n\n * 123456", "bug \n\n * 123456");
+        checkIssueSpans("bug\n\n#123456", "bug\n\n#123456");
+        checkIssueSpans("* bug\n\n#123456", "bug\n\n#123456");
+        checkIssueSpans("* bug\n\n #123456", "bug\n\n #123456");
+        checkIssueSpans("* bug\n\n* #123456", "bug\n\n* #123456");
+        checkIssueSpans("* bug\n\n * #123456", "bug\n\n * #123456");
+
+        checkIssueSpans("bug\n* \n123456", "bug\n* \n123456");
+        checkIssueSpans("bug\n * \n123456", "bug\n * \n123456");
+
+        checkNoIssueSpansFound("* bug\n *123456");
+        checkNoIssueSpansFound("* bug\n *#123456");
+        checkNoIssueSpansFound("* bug\n *# 123456");
+        checkNoIssueSpansFound("* bug\n *#  123456");
+
+        checkNoIssueSpansFound("bug\n ** \n123456");
+    }
+
+    @Test
+    public void testGetIssueNumber() {
+        testGetIssueNumber("#123456", "123456");
+        testGetIssueNumber("# 123456", "123456");
+        testGetIssueNumber(" #123456", "123456");
+        testGetIssueNumber("  #123456", "123456");
+        testGetIssueNumber(" # 123456", "123456");
+        testGetIssueNumber("#  123456", "123456");
+        testGetIssueNumber("bug 123456", "123456");
+        testGetIssueNumber("bug  123456", "123456");
+        testGetIssueNumber("bug #123456", "123456");
+        testGetIssueNumber("bug   123456", "123456");
+        testGetIssueNumber("bug  #123456", "123456");
+        testGetIssueNumber("bug # 123456", "123456");
+
+        testGetIssueNumber("bug# #123456", "123456");
+        testGetIssueNumber("sbug #123456", "123456");
+
+        testGetIssueNumber("bug #abcdef", "abcdef");
+        testGetIssueNumber("bug #ABCDEF", "ABCDEF");
+
+        testGetIssueNumber("bug # abcdef", "abcdef");
+        testGetIssueNumber("# abcdef", "abcdef");
+    }
+
+    private void checkIssueSpans(String str, String... substr) {
+        checkTestValidity(str != null);
+        checkTestValidity(substr != null);
+
+        int fromIndex = 0;
+
+        int[] expBounds = new int[substr.length * 2];
+        for (int i = 0; i < substr.length; i++) {
+            int lowBound = str.indexOf(substr[i], fromIndex);
+            checkTestValidity(lowBound != -1);
+            int highBound = lowBound + substr[i].length();
+            expBounds[2 * i] = lowBound;
+            expBounds[2 * i + 1] = highBound;
+            fromIndex = highBound;
+        }
+        checkIssueSpans(str, expBounds);
+    }
+
+    private void checkIssueSpans(String str, int... expectedBounds) {
+        if ((expectedBounds == null) || (expectedBounds.length == 0)) {
+            checkNoIssueSpansFound(str);
+            return;
+        }
+
+        checkTestValidity(expectedBounds.length % 2 == 0);
+
+        int[] spans = IssueFinder.getIssueSpans(str);
+        assertNotNull(spans);
+        assertTrue("incorrect bounds detected: "
+                       + "expected: " + printArray(expectedBounds)
+                       + ", real: " + (spans.length == 0 ? "none" : printArray(spans)),
+                   equals(expectedBounds, spans));
     }
 
     private void checkNoIssueSpansFound(String str) {
         checkTestValidity(str != null);
         int[] spans = IssueFinder.getIssueSpans(str);
         assertNotNull(spans);
-        assertTrue("incorrect bounds detected: "
-                       + "no spans expected but got: " + spans,
+        assertTrue("incorrect bounds detected for \"" + str + "\": "
+                       + "no spans expected but got: " + printArray(spans),
                    spans.length == 0);
     }
 
-    private void checkIssueSpans(String str, String substr) {
-        checkTestValidity(str != null);
-        checkTestValidity(substr != null);
-        int lowBound = str.indexOf(substr);
-        checkTestValidity(lowBound != -1);
-        int highBound = lowBound + substr.length();
-        checkIssueSpans(str, lowBound, highBound);
+    private static boolean equals(int[] expected, int[] real) {
+        if ((expected == null) && (real == null)) {
+            return true;
+        }
+
+        if ((expected == null) || (real == null)) {
+            return false;
+        }
+
+        if (expected.length != real.length) {
+            return false;
+        }
+
+        for (int i = 0; i < real.length; i++) {
+            if (real[i] != expected[i]) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
-    private void checkIssueSpans(String str, int expectedLowBound, int expectedHighBound) {
-        int[] spans = IssueFinder.getIssueSpans(str);
-        assertNotNull(spans);
-        assertTrue("incorrect bounds detected: "
-                       + "expected: [" + expectedLowBound + ", " + expectedHighBound + ']'
-                       + ", real: " + spans,
-                   spans.length == 2);
-        assertTrue("incorrect bounds detected: "
-                       + "expected: " + expectedLowBound + ", " + expectedHighBound
-                       + ", real: " + spans[0] + ", " + spans[1],
-                   (spans[0] == expectedLowBound) && (spans[1] == expectedHighBound));
+    private static String printArray(int... arr) {
+        if (arr == null) {
+            return "<null>";
+        }
+
+        if (arr.length == 0) {
+            return "[]";
+        }
+
+        StringBuilder buf = new StringBuilder(arr.length * 4 + 5);
+        buf.append('[');
+        buf.append(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            buf.append(',').append(' ').append(arr[i]);
+        }
+        buf.append(']');
+        return buf.toString();
+    }
+
+    private void testGetIssueNumber(String hyperlinkText, String issueNumber) {
+        assertEquals(issueNumber, IssueFinder.getIssueNumber(hyperlinkText));
     }
 
     private static void checkTestValidity(boolean condition) {
