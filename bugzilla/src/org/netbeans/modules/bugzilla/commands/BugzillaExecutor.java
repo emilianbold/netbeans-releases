f5827d77b4a901ae529fe5ba091dd3385930f085
Tomas Stupka <tstupka@netbeans.org>
2009-03-09 22:45:33 +0100

    preventing NPE

--- bugzilla/src/org/netbeans/modules/bugzilla/commands/BugzillaExecutor.java
+++ bugzilla/src/org/netbeans/modules/bugzilla/commands/BugzillaExecutor.java
@@ -93,12 +93,12 @@ public class BugzillaExecutor {
 
     public static boolean isAuthenticate(CoreException ce) {
         IStatus status = ce.getStatus();
-        return status.getMessage().equals(INVALID_USERNAME_OR_PASSWORD); // XXX
+        return status != null && INVALID_USERNAME_OR_PASSWORD.equals(status.getMessage()); // XXX
     }
 
     public static boolean isNotFound(CoreException ce) {
         IStatus status = ce.getStatus();
-        return status.getMessage().equals(HTTP_ERROR_NOT_FOUND); // XXX
+        return status != null && HTTP_ERROR_NOT_FOUND.equals(status.getMessage()); // XXX
     }
 
     public boolean handleException(CoreException ce) {
