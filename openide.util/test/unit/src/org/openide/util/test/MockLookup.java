1831bbd446b4f1372979b2beaae28e4505846d42
Jaroslav Tulach <jtulach@netbeans.org>
2009-03-10 14:12:46 +0100

    favorites tests updated to new MockLookup architecture

--- openide.util/test/unit/src/org/openide/util/test/MockLookup.java
+++ openide.util/test/unit/src/org/openide/util/test/MockLookup.java
@@ -68,7 +68,6 @@ public class MockLookup extends ProxyLookup {
                 defaultLookup.set(null, null);
             }
             assertEquals(MockLookup.class, Lookup.getDefault().getClass());
-            setInstances();
         } catch (Exception x) {
             throw new ExceptionInInitializerError(x);
         } finally {
@@ -89,7 +88,9 @@ public class MockLookup extends ProxyLookup {
      * and you want to ensure that any users of mock lookup will see the correct default lookup right away,
      * even if they have not yet called {@link #setLookup} or {@link #setInstances}.
      */
-    public static void init() {}
+    public static void init() {
+        setInstances();
+    }
 
     /**
      * Sets the global default lookup with zero or more delegate lookups.
@@ -109,7 +110,7 @@ public class MockLookup extends ProxyLookup {
     public static void setInstances(Object... instances) {
         ClassLoader l = MockLookup.class.getClassLoader();
         if (l != Lookup.getDefault().lookup(ClassLoader.class)) {
-            setLookup(Lookups.metaInfServices(l), Lookups.singleton(l));
+            setLookup(Lookups.fixed(instances), Lookups.metaInfServices(l), Lookups.singleton(l));
         }
         Lookup projects = Lookups.forPath("Services");
         Collection<?> initialize = projects.lookupAll(Object.class);
