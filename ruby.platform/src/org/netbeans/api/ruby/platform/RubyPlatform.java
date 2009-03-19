62ddf348f3e8ff66fa0029d3124e7a89f9baae13
Erno Mononen <emononen@netbeans.org>
2009-03-19 09:52:29 +0100

    Fixed test failure (gems version is now 1.3.1)

--- ruby.platform/src/org/netbeans/api/ruby/platform/RubyPlatform.java
+++ ruby.platform/src/org/netbeans/api/ruby/platform/RubyPlatform.java
@@ -1075,7 +1075,7 @@ public final class RubyPlatform implements Comparable<RubyPlatform> {
             info.libDir = FileUtil.toFile(libDirFO.getFileObject("/1.8")).getAbsolutePath(); // NOI18N
             info.gemHome = FileUtil.toFile(libDirFO.getFileObject("/gems/1.8")).getAbsolutePath(); // NOI18N
             info.gemPath = info.gemHome;
-            info.gemVersion = "1.2.0"; // NOI18N
+            info.gemVersion = "1.3.1"; // NOI18N
             return info;
         }
 
