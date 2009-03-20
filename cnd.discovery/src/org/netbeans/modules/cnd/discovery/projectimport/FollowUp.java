22f611f97a448f73302b371073a8e6711fbcb6bd
Alexander Simon <alexvsimon@netbeans.org>
2009-03-20 15:01:12 +0300

    make follow-up dialog "status of creating new project" ballon

--- cnd.discovery/src/org/netbeans/modules/cnd/discovery/projectimport/FollowUp.java
+++ cnd.discovery/src/org/netbeans/modules/cnd/discovery/projectimport/FollowUp.java
@@ -45,34 +45,53 @@
 
 package org.netbeans.modules.cnd.discovery.projectimport;
 
+import java.awt.Dialog;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.MessageFormat;
 import java.util.Map;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import org.netbeans.modules.cnd.api.model.CsmModel;
+import org.netbeans.modules.cnd.api.model.CsmModelAccessor;
+import org.netbeans.modules.cnd.api.model.CsmProject;
+import org.netbeans.modules.cnd.api.project.NativeFileItem;
 import org.netbeans.modules.cnd.api.project.NativeProject;
 import org.netbeans.modules.cnd.discovery.projectimport.ImportProject.State;
 import org.netbeans.modules.cnd.discovery.projectimport.ImportProject.Step;
 import org.netbeans.modules.cnd.makeproject.api.ui.BrokenIncludes;
+import org.openide.DialogDescriptor;
+import org.openide.DialogDisplayer;
 import org.openide.awt.Notification;
 import org.openide.awt.NotificationDisplayer;
+import org.openide.util.ImageUtilities;
 import org.openide.util.Lookup;
 import org.openide.util.NbBundle;
+import org.openide.util.RequestProcessor;
 
 /**
  *
  * @author Alexander Simon
  */
 public class FollowUp extends JPanel {
-
+    private CsmProject csmProject;
     /** Creates new form FollowUp */
     private FollowUp(ImportProject importer, NativeProject project) {
         initComponents();
+        details.setVisible(false);
+        detailsPane.setVisible(false);
         Map<Step,State> map = importer.getImportResult();
         showState(Step.Project, map.get(Step.Project), projectCreated);
         showState(Step.Configure, map.get(Step.Configure), configureDone);
         showState(Step.MakeClean, map.get(Step.MakeClean), makeClean);
         showState(Step.Make, map.get(Step.Make), make);
         showCodeAssistanceState(map, project);
+        CsmModel model = CsmModelAccessor.getModel();
+        if (model != null  && project != null) {
+            csmProject = model.getProject(project);
+            initDetails();
+        }
     }
 
     private void showState(Step step, State state, JLabel label){
@@ -118,7 +137,7 @@ public class FollowUp extends JPanel {
         }
     }
 
-    private boolean hasBrokenIncludes(NativeProject project) {
+    private static  boolean hasBrokenIncludes(NativeProject project) {
         BrokenIncludes provider = Lookup.getDefault().lookup(BrokenIncludes.class);
         if (provider != null) {
             return provider.isBroken(project);
@@ -126,18 +145,69 @@ public class FollowUp extends JPanel {
         return false;
     }
 
+    private void initDetails() {
+        Object o = csmProject.getPlatformProject();
+        if (o instanceof NativeProject) {
+            NativeProject nativeProject = (NativeProject) o;
+            int sourceFiles = 0;
+            int sourceFilesExcluded = 0;
+            int headerFiles = 0;
+            int headerFilesExcluded = 0;
+            for (NativeFileItem item : nativeProject.getAllFiles()) {
+                switch (item.getLanguage()) {
+                    case C:
+                    case CPP:
+                        sourceFiles++;
+                        if (item.isExcluded()) {
+                            sourceFilesExcluded++;
+                        }
+                        break;
+                    case C_HEADER:
+                        headerFiles++;
+                        if (item.isExcluded()) {
+                            headerFilesExcluded++;
+                        }
+                        break;
+                }
+            }
+            String text = MessageFormat.format(getString("Details.String"), // NOI18N
+                    sourceFilesExcluded, sourceFiles,
+                    headerFilesExcluded, headerFiles);
+            detailsTextPane.setContentType("text/html");
+            detailsTextPane.setText(text);
+            details.setVisible(true);
+            detailsPane.setVisible(true);
+        }
+    }
 
-    public static void showFollowUp(ImportProject importer, NativeProject project) {
-        FollowUp panel = new FollowUp(importer, project);
-        /*Notification notification =*/
-        NotificationDisplayer.getDefault().notify(getString("Dialog_Title"), // NOI18N
-                new ImageIcon(FollowUp.class.getResource("/org/netbeans/modules/cnd/discovery/wizard/resources/configure.png")), // NOI18N
-                panel, panel, NotificationDisplayer.Priority.HIGH);
-        //DialogDescriptor descriptor = new DialogDescriptor(panel, getString("Dialog_Title"), // NOI18N
-        //        true, new Object[]{DialogDescriptor.CLOSED_OPTION}, DialogDescriptor.CLOSED_OPTION,
-        //        DialogDescriptor.DEFAULT_ALIGN, null, null);
-        //Dialog dlg = DialogDisplayer.getDefault().createDialog(descriptor);
-        //dlg.setVisible(true);
+
+    public static void showFollowUp(final ImportProject importer, final NativeProject project) {
+        ActionListener onClickAction = new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                FollowUp panel = new FollowUp(importer, project);
+                DialogDescriptor descriptor = new DialogDescriptor(panel, getString("Dialog_Title"), // NOI18N
+                        true, new Object[]{DialogDescriptor.CLOSED_OPTION}, DialogDescriptor.CLOSED_OPTION,
+                        DialogDescriptor.DEFAULT_ALIGN, null, null);
+                Dialog dlg = DialogDisplayer.getDefault().createDialog(descriptor);
+                dlg.setVisible(true);
+            }
+        };
+        String title;
+        ImageIcon icon;
+        if (hasBrokenIncludes(project)) {
+            title = getString("Configure_Fail"); // NOI18N
+            icon  = ImageUtilities.loadImageIcon("org/netbeans/modules/cnd/discovery/wizard/resources/info.png", false); // NOI18N
+        } else {
+            title = getString("Configure_Success"); // NOI18N
+            icon  = ImageUtilities.loadImageIcon("org/netbeans/modules/cnd/discovery/wizard/resources/check.png", false); // NOI18N
+        }
+        final Notification notification = NotificationDisplayer.getDefault().notify(title, icon,
+                getString("Dialog_Action"), onClickAction, NotificationDisplayer.Priority.HIGH); // NOI18N
+        RequestProcessor.getDefault().post(new Runnable() {
+            public void run() {
+                notification.clear();
+            }
+        }, 15*1000);
     }
 
     /** This method is called from within the constructor to
@@ -155,9 +225,13 @@ public class FollowUp extends JPanel {
         makeClean = new javax.swing.JLabel();
         make = new javax.swing.JLabel();
         codeAssistance = new javax.swing.JLabel();
-        jPanel1 = new javax.swing.JPanel();
+        detailsPane = new javax.swing.JScrollPane();
+        detailsTextPane = new javax.swing.JTextPane();
+        details = new javax.swing.JLabel();
 
+        setEnabled(false);
         setOpaque(false);
+        setPreferredSize(new java.awt.Dimension(320, 280));
         setLayout(new java.awt.GridBagLayout());
 
         projectCreated.setIcon(new javax.swing.ImageIcon(getClass().getResource("/org/netbeans/modules/cnd/discovery/wizard/resources/check.png"))); // NOI18N
@@ -165,6 +239,7 @@ public class FollowUp extends JPanel {
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 8;
+        gridBagConstraints.gridwidth = 2;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         gridBagConstraints.insets = new java.awt.Insets(10, 10, 0, 10);
         add(projectCreated, gridBagConstraints);
@@ -174,6 +249,7 @@ public class FollowUp extends JPanel {
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 10;
+        gridBagConstraints.gridwidth = 2;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
         gridBagConstraints.weightx = 1.0;
@@ -185,6 +261,7 @@ public class FollowUp extends JPanel {
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 12;
+        gridBagConstraints.gridwidth = 2;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
         gridBagConstraints.weightx = 1.0;
@@ -196,6 +273,7 @@ public class FollowUp extends JPanel {
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 14;
+        gridBagConstraints.gridwidth = 2;
         gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
         gridBagConstraints.weightx = 1.0;
@@ -212,19 +290,43 @@ public class FollowUp extends JPanel {
         gridBagConstraints.weightx = 1.0;
         gridBagConstraints.insets = new java.awt.Insets(6, 10, 10, 10);
         add(codeAssistance, gridBagConstraints);
+
+        detailsPane.setBorder(null);
+        detailsPane.setOpaque(false);
+        detailsPane.setPreferredSize(new java.awt.Dimension(100, 100));
+
+        detailsTextPane.setEditable(false);
+        detailsTextPane.setOpaque(false);
+        detailsPane.setViewportView(detailsTextPane);
+
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 20;
-        gridBagConstraints.fill = java.awt.GridBagConstraints.VERTICAL;
+        gridBagConstraints.gridy = 19;
+        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
+        gridBagConstraints.weightx = 1.0;
         gridBagConstraints.weighty = 1.0;
-        add(jPanel1, gridBagConstraints);
+        gridBagConstraints.insets = new java.awt.Insets(0, 10, 0, 10);
+        add(detailsPane, gridBagConstraints);
+
+        details.setLabelFor(detailsTextPane);
+        details.setText(org.openide.util.NbBundle.getMessage(FollowUp.class, "FollowUp.details.text")); // NOI18N
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 18;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.WEST;
+        gridBagConstraints.insets = new java.awt.Insets(0, 10, 0, 10);
+        add(details, gridBagConstraints);
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel codeAssistance;
     private javax.swing.JLabel configureDone;
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel details;
+    private javax.swing.JScrollPane detailsPane;
+    private javax.swing.JTextPane detailsTextPane;
     private javax.swing.JLabel make;
     private javax.swing.JLabel makeClean;
     private javax.swing.JLabel projectCreated;
