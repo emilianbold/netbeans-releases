bc29397526fc551eebbf00f805e16c08c53b5de7
Tomas Stupka <tstupka@netbeans.org>
2009-03-24 19:44:48 +0100

    unused imports

--- bugzilla/src/org/netbeans/modules/bugzilla/query/QueryController.java
+++ bugzilla/src/org/netbeans/modules/bugzilla/query/QueryController.java
@@ -57,7 +57,6 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
