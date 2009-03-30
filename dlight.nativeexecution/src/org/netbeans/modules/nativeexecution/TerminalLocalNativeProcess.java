92ee70a14288b75ce0abac13d4c6acc684552bc9
Andrew Krasny <akrasny@netbeans.org>
2009-03-30 21:22:01 +0400

    #160953 - multiply problems (regressions) running application

--- dlight.nativeexecution/src/org/netbeans/modules/nativeexecution/TerminalLocalNativeProcess.java
+++ dlight.nativeexecution/src/org/netbeans/modules/nativeexecution/TerminalLocalNativeProcess.java
@@ -46,6 +46,7 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
@@ -97,7 +98,7 @@ public final class TerminalLocalNativeProcess extends AbstractNativeProcess {
                 runScript = runScript.replaceAll("\\\\", "/"); // NOI18N
             }
         }
-        
+
         pathConverter = new PathConverter();
 
         dorunScript = runScript;
@@ -163,11 +164,38 @@ public final class TerminalLocalNativeProcess extends AbstractNativeProcess {
 
         ProcessBuilder pb = new ProcessBuilder(command);
 
-        if (isWindows && wDir != null) {
+        if ((isWindows || isMacOS) && wDir != null) {
             pb.directory(new File(wDir));
         }
 
-        MacroMap env = info.getEnvVariables();
+        final MacroMap env = info.getEnvVariables();
+
+        // setup DISPLAY variable for MacOS...
+        if (isMacOS) {
+            ProcessBuilder pb1 = new ProcessBuilder("/bin/sh", "-c", "/bin/echo $DISPLAY"); // NOI18N
+            Process p1 = pb1.start();
+            int status = -1;
+
+            try {
+                status = p1.waitFor();
+            } catch (InterruptedException ex) {
+            }
+
+            String display = null;
+
+            if (status == 0) {
+                BufferedReader br = new BufferedReader(
+                        new InputStreamReader(p1.getInputStream()));
+                display = br.readLine();
+
+            }
+
+            if (display == null || "".equals(display)) { // NOI18N
+                display = ":0.0"; // NOI18N
+            }
+
+            pb.environment().put("DISPLAY", display); // NOI18N
+        }
 
         if (!env.isEmpty()) {
             // TODO: FIXME (?)
@@ -198,15 +226,15 @@ public final class TerminalLocalNativeProcess extends AbstractNativeProcess {
     @Override
     public void cancel() {
         sendSignal(9);
-    };
+    }
 
     private synchronized int sendSignal(int signal) {
         int result = 1;
-        
+
         try {
             ProcessBuilder pb;
             List<String> command = new ArrayList<String>();
-            
+
             if (isWindows) {
                 String shell = HostInfoUtils.getShell(new ExecutionEnvironment());
                 command.add(shell);
@@ -217,7 +245,7 @@ public final class TerminalLocalNativeProcess extends AbstractNativeProcess {
                 command.add("-" + signal); // NOI18N
                 command.add("" + getPID()); // NOI18N
             }
-            
+
             pb = new ProcessBuilder(command);
             Process killProcess = pb.start();
             result = killProcess.waitFor();
