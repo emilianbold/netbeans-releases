6404236d3af8bb64aa68c804ff5591e1a7360636
Karol Harezlak <kharezlak@netbeans.org>
2009-03-31 16:00:17 +0200

    Issue 161119 Inconsistent Delete action for SVG List UI component + refactoring

--- vmd.midpnb/src/org/netbeans/modules/vmd/midpnb/components/svg/form/InstanceRenameSourceAction.java
+++ vmd.midpnb/src/org/netbeans/modules/vmd/midpnb/components/svg/form/InstanceRenameSourceAction.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 2009 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -21,12 +21,6 @@
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
  *
- * Contributor(s):
- *
- * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
- * Microsystems, Inc. All Rights Reserved.
- *
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -37,26 +31,27 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
+ *
+ * Contributor(s):
+ *
+ * Portions Copyrighted 2009 Sun Microsystems, Inc.
  */
-package org.netbeans.modules.vmd.midp.codegen.ui;
+
+package org.netbeans.modules.vmd.midpnb.components.svg.form;
 
 import org.netbeans.modules.vmd.api.model.DesignComponent;
+import org.netbeans.modules.vmd.midp.codegen.ui.InstanceRenameAction;
 
 /**
  *
  * @author ads
  */
-public final class InstanceRenameSourceAction extends InstanceRenameAction {
+ public class InstanceRenameSourceAction extends InstanceRenameAction {
 
-    public static final String PROP_SVGCOMPONENT= "svgButton"; //NOI18N
-
-    @Override
-    public void setComponent(DesignComponent component) {
-        DesignComponent comp = component.readProperty(
-                PROP_SVGCOMPONENT ).getComponent();
-        super.setComponent(component);
-        setActionComponent(comp);
+        @Override
+        public void setComponent(DesignComponent component) {
+            DesignComponent comp = component.readProperty(SVGButtonEventSourceCD.PROP_SVGCOMPONENT).getComponent();
+            super.setComponent(component);
+            setActionComponent(comp);
+        }
     }
-    
-    
-}
---
+++ vmd.midpnb/src/org/netbeans/modules/vmd/midpnb/components/svg/form/MidpSVGActionsSupport.java
@@ -0,0 +1,83 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2009 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ *
+ * Contributor(s):
+ *
+ * Portions Copyrighted 2009 Sun Microsystems, Inc.
+ */
+package org.netbeans.modules.vmd.midpnb.components.svg.form;
+
+import java.util.List;
+import org.netbeans.modules.vmd.api.model.Presenter;
+import org.netbeans.modules.vmd.api.model.presenters.actions.ActionsPresenter;
+import org.netbeans.modules.vmd.api.model.presenters.actions.DeleteAction;
+import org.netbeans.modules.vmd.api.screen.actions.DesignerEditAction;
+import org.netbeans.modules.vmd.midp.actions.GoToSourceAction;
+import org.openide.actions.PropertiesAction;
+import org.openide.util.actions.SystemAction;
+
+/**
+ *
+ * @author karol harezlak
+ */
+public final class MidpSVGActionsSupport {
+
+    private MidpSVGActionsSupport() {
+        throw new IllegalAccessError();
+    }
+
+    public static void addCommonEventSourceActionsPresenters(List<Presenter> presenters,
+            boolean allowEdit, boolean allowGoToSource, boolean allowRename,
+            boolean allowDelete, boolean allowProperties) {
+        if (allowEdit) {
+            presenters.add(ActionsPresenter.create(20, SystemAction.get(DesignerEditAction.class)));
+        }
+        if (allowGoToSource) {
+            presenters.add(ActionsPresenter.create(20, SystemAction.get(GoToSourceAction.class)));
+        }
+        if (allowRename) {
+            presenters.add(ActionsPresenter.create(30, SystemAction.get(InstanceRenameSourceAction.class)));
+        }
+        if (allowDelete) {
+            presenters.add(ActionsPresenter.create(40, SystemAction.get(DeleteAction.class)));
+        }
+        if (allowProperties) {
+            presenters.add(ActionsPresenter.create(60, SystemAction.get(PropertiesAction.class)));
+        }
+    }
+
+   
+}
+
+
