d2536d2462baf508b0bc6ae566054f010d23043d
Thomas Preisler <thp@netbeans.org>
2009-04-02 16:03:53 -0700

    #159623 Add IPS package type support to packaging

--- cnd.makeproject/src/org/netbeans/modules/cnd/makeproject/packaging/IPSPackager.java
+++ cnd.makeproject/src/org/netbeans/modules/cnd/makeproject/packaging/IPSPackager.java
@@ -43,9 +43,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import org.netbeans.modules.cnd.makeproject.api.PackagerInfoElement;
 import org.netbeans.modules.cnd.makeproject.api.PackagerDescriptor;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.StringTokenizer;
@@ -77,15 +75,6 @@ public class IPSPackager implements PackagerDescriptor {
     }
 
     public List<PackagerInfoElement> getDefaultInfoList(MakeConfiguration makeConfiguration, PackagingConfiguration packagingConfiguration) {
-        String defArch;
-        if (makeConfiguration.getPlatform().getValue() == Platform.PLATFORM_SOLARIS_INTEL) {
-            defArch = "i386"; // NOI18N
-        } else if (makeConfiguration.getPlatform().getValue() == Platform.PLATFORM_SOLARIS_SPARC) {
-            defArch = "sparc"; // NOI18N
-        } else {
-            // Anything else ?
-            defArch = "i386"; // NOI18N
-        }
         List<PackagerInfoElement> infoList = new ArrayList<PackagerInfoElement>();
         infoList.add(new PackagerInfoElement(PACKAGER_NAME, "name", packagingConfiguration.getOutputName(), true, true)); // NOI18N
         infoList.add(new PackagerInfoElement(PACKAGER_NAME, "description", "Package description ...", true, true)); // NOI18N
@@ -96,31 +85,31 @@ public class IPSPackager implements PackagerDescriptor {
     public List<String> getOptionalInfoList() {
         List<String> entryComboBox = new ArrayList<String>();
 
-        entryComboBox.add("BASEDIR"); // NOI18N == PackagingConfiguration.TYPE_SVR4_PACKAGE
-        entryComboBox.add("CLASSES"); // NOI18N
-        entryComboBox.add("DESC"); // NOI18N
-        entryComboBox.add("EMAIL"); // NOI18N
-        entryComboBox.add("HOTLINE"); // NOI18N
-        entryComboBox.add("INTONLY"); // NOI18N
-        entryComboBox.add("ISTATES"); // NOI18N
-        entryComboBox.add("MAXINST"); // NOI18N
-        entryComboBox.add("ORDER"); // NOI18N
-        entryComboBox.add("PSTAMP"); // NOI18N
-        entryComboBox.add("RSTATES"); // NOI18N
-        entryComboBox.add("SUNW_ISA"); // NOI18N
-        entryComboBox.add("SUNW_LOC"); // NOI18N
-        entryComboBox.add("SUNW_PKG_DIR"); // NOI18N
-        entryComboBox.add("SUNW_PKG_ALLZONES"); // NOI18N
-        entryComboBox.add("SUNW_PKG_HOLLOW"); // NOI18N
-        entryComboBox.add("SUNW_PKG_THISZONE"); // NOI18N
-        entryComboBox.add("SUNW_PKGLIST"); // NOI18N
-        entryComboBox.add("SUNW_PKGTYPE"); // NOI18N
-        entryComboBox.add("SUNW_PKGVERS"); // NOI18N
-        entryComboBox.add("SUNW_PRODNAME"); // NOI18N
-        entryComboBox.add("SUNW_PRODVERS"); // NOI18N
-        entryComboBox.add("ULIMIT"); // NOI18N
-        entryComboBox.add("VENDOR"); // NOI18N
-        entryComboBox.add("VSTOCK"); // NOI18N
+//        entryComboBox.add("BASEDIR"); // NOI18N == PackagingConfiguration.TYPE_SVR4_PACKAGE
+//        entryComboBox.add("CLASSES"); // NOI18N
+//        entryComboBox.add("DESC"); // NOI18N
+//        entryComboBox.add("EMAIL"); // NOI18N
+//        entryComboBox.add("HOTLINE"); // NOI18N
+//        entryComboBox.add("INTONLY"); // NOI18N
+//        entryComboBox.add("ISTATES"); // NOI18N
+//        entryComboBox.add("MAXINST"); // NOI18N
+//        entryComboBox.add("ORDER"); // NOI18N
+//        entryComboBox.add("PSTAMP"); // NOI18N
+//        entryComboBox.add("RSTATES"); // NOI18N
+//        entryComboBox.add("SUNW_ISA"); // NOI18N
+//        entryComboBox.add("SUNW_LOC"); // NOI18N
+//        entryComboBox.add("SUNW_PKG_DIR"); // NOI18N
+//        entryComboBox.add("SUNW_PKG_ALLZONES"); // NOI18N
+//        entryComboBox.add("SUNW_PKG_HOLLOW"); // NOI18N
+//        entryComboBox.add("SUNW_PKG_THISZONE"); // NOI18N
+//        entryComboBox.add("SUNW_PKGLIST"); // NOI18N
+//        entryComboBox.add("SUNW_PKGTYPE"); // NOI18N
+//        entryComboBox.add("SUNW_PKGVERS"); // NOI18N
+//        entryComboBox.add("SUNW_PRODNAME"); // NOI18N
+//        entryComboBox.add("SUNW_PRODVERS"); // NOI18N
+//        entryComboBox.add("ULIMIT"); // NOI18N
+//        entryComboBox.add("VENDOR"); // NOI18N
+//        entryComboBox.add("VSTOCK"); // NOI18N
 
         return entryComboBox;
     }
@@ -146,7 +135,7 @@ public class IPSPackager implements PackagerDescriptor {
     }
 
     public String getTopDir(MakeConfiguration makeConfiguration, PackagingConfiguration packagingConfiguration) {
-        return packagingConfiguration.findInfoValueName("PKG"); // NOI18N
+        return "/usr"; // NOI18N
     }
 
     public boolean supportsGroupAndOwner() {
@@ -160,7 +149,7 @@ public class IPSPackager implements PackagerDescriptor {
     public static class ScriptWriter implements ShellSciptWriter {
 
         public void writeShellScript(BufferedWriter bw, MakeConfiguration makeConfiguration, PackagingConfiguration packagingConfiguration) throws IOException {
-            writePackagingScriptBodySVR4(bw, makeConfiguration);
+            writePackagingScriptBodyIPS(bw, makeConfiguration);
         }
 
         private List<String> findUndefinedDirectories(PackagingConfiguration packagingConfiguration) {
@@ -186,10 +175,10 @@ public class IPSPackager implements PackagerDescriptor {
                     if (path != null && path.length() > 0) {
                         StringTokenizer tokenizer = new StringTokenizer(path, "/"); // NOI18N
                         while (tokenizer.hasMoreTokens()) {
-                            if (base.length() > 0) {
-                                base += "/"; // NOI18N
-                            }
-                            base += tokenizer.nextToken();
+//                            if (base.length() > 0) {
+//                                base += "/"; // NOI18N
+//                            }
+                            base += "/" + tokenizer.nextToken(); // NOI18N
                             if (!set.contains(base)) {
                                 set.add(base);
                                 list.add(base);
@@ -201,79 +190,153 @@ public class IPSPackager implements PackagerDescriptor {
             return list;
         }
 
-        private void writePackagingScriptBodySVR4(BufferedWriter bw, MakeConfiguration conf) throws IOException {
+        private void writePackagingScriptBodyIPS(BufferedWriter bw, MakeConfiguration conf) throws IOException {
             PackagingConfiguration packagingConfiguration = conf.getPackagingConfiguration();
-            String packageName = packagingConfiguration.findInfoValueName("PKG"); // NOI18N // FIXUP: what is null????
-
-            bw.write("# Create pkginfo and prototype files\n"); // NOI18N
-            bw.write("PKGINFOFILE=${TMPDIR}/pkginfo\n"); // NOI18N
-            bw.write("PROTOTYPEFILE=${TMPDIR}/prototype\n"); // NOI18N
-            bw.write("rm -f $PKGINFOFILE $PROTOTYPEFILE\n"); // NOI18N
-            bw.write("\n"); // NOI18N        
-            bw.write("cd \"${TOP}\"\n"); // NOI18N
-            List<PackagerInfoElement> infoList = packagingConfiguration.getHeaderSubList(PACKAGER_NAME);
-            for (PackagerInfoElement elem : infoList) {
-                bw.write("echo \'" + elem.getName() + "=\"" + packagingConfiguration.expandMacros(elem.getValue()) + "\"\'" + " >> $PKGINFOFILE\n"); // NOI18N
-            }
-            bw.write("\n"); // NOI18N       
-            bw.write("cd \"${TOP}\"\n"); // NOI18N
-            bw.write("echo \"i pkginfo=pkginfo\" >> $PROTOTYPEFILE\n"); // NOI18N
-            bw.write("\n"); // NOI18N     
-            List<String> dirList = findUndefinedDirectories(packagingConfiguration);
-            for (String dir : dirList) {
-                bw.write("echo \"");// NOI18N
-                bw.write("d"); // NOI18N
-                bw.write(" none"); // Classes // NOI18N
-                bw.write(" " + dir); // NOI18N
-                bw.write(" 0" + MakeOptions.getInstance().getDefExePerm()); // NOI18N
-                bw.write(" " + MakeOptions.getInstance().getDefOwner()); // NOI18N
-                bw.write(" " + MakeOptions.getInstance().getDefGroup()); // NOI18N
-                bw.write("\""); // NOI18N
-                bw.write(" >> $PROTOTYPEFILE\n"); // NOI18N
-
-            }
-
-            bw.write("\n"); // NOI18N
             List<PackagerFileElement> fileList = packagingConfiguration.getFiles().getValue();
+
+            bw.write("# Copy files and create directories and links\n"); // NOI18N
             for (PackagerFileElement elem : fileList) {
-                bw.write("echo \"");// NOI18N
-                if (elem.getType() == PackagerFileElement.FileType.DIRECTORY) {
-                    bw.write("d");// NOI18N
-                } else if (elem.getType() == PackagerFileElement.FileType.FILE) {
-                    bw.write("f");// NOI18N
+                bw.write("cd \"${TOP}\"\n"); // NOI18N
+                if (elem.getType() == PackagerFileElement.FileType.FILE) {
+                    String toDir = IpeUtils.getDirName(conf.getPackagingConfiguration().expandMacros(elem.getTo()));
+                    if (toDir != null && toDir.length() >= 0) {
+                        if (toDir.charAt(0) == '/') {
+                            toDir = toDir.substring(1);
+                        }
+                        bw.write("makeDirectory " + "${TMPDIR}/" + toDir + "\n"); // NOI18N
+                    }
+                    bw.write("copyFileToTmpDir \"" + elem.getFrom() + "\" \"${TMPDIR}/" + elem.getTo() + "\" 0" + elem.getPermission() + "\n"); // NOI18N
+                } else if (elem.getType() == PackagerFileElement.FileType.DIRECTORY) {
+                    bw.write("makeDirectory " + " \"${TMPDIR}/" + elem.getTo() + "\"" + " 0" + elem.getPermission() + "\n"); // NOI18N
                 } else if (elem.getType() == PackagerFileElement.FileType.SOFTLINK) {
-                    bw.write("s");// NOI18N
+                    String toDir = IpeUtils.getDirName(elem.getTo());
+                    String toName = IpeUtils.getBaseName(elem.getTo());
+                    if (toDir != null && toDir.length() >= 0) {
+                        if (toDir.charAt(0) == '/') {
+                            toDir = toDir.substring(1);
+                        }
+                        bw.write("makeDirectory " + "\"" + "${TMPDIR}/" + toDir + "\"" + "\n"); // NOI18N
+                    }
+                    bw.write("cd " + "\"" + "${TMPDIR}/" + toDir + "\"" + "\n"); // NOI18N
+                    bw.write("ln -s " + "\"" + elem.getFrom() + "\"" + " " + "\"" + toName + "\"" + "\n"); // NOI18N
+                } else if (elem.getType() == PackagerFileElement.FileType.UNKNOWN) {
+                    // skip ???
                 } else {
                     assert false;
                 }
-                bw.write(" none"); // Classes // NOI18N
-                bw.write(" " + elem.getTo());// NOI18N
-                if (elem.getFrom().length() > 0) {
-                    String from = elem.getFrom();
-                    if (IpeUtils.isPathAbsolute(from)) {
-                        from = IpeUtils.toRelativePath(conf.getBaseDir(), from);
-                    }
-                    bw.write("=" + from);// NOI18N
-                }
-                if (elem.getType() != PackagerFileElement.FileType.SOFTLINK) {
-                    bw.write(" 0" + elem.getPermission());// NOI18N
-                    bw.write(" " + elem.getOwner());// NOI18N
-                    bw.write(" " + elem.getGroup());// NOI18N
-                }
-                bw.write("\""); // NOI18N
-                bw.write(" >> $PROTOTYPEFILE\n"); // NOI18N
+                bw.write("\n"); // NOI18N
             }
             bw.write("\n"); // NOI18N
-            bw.write("# Make package\n"); // NOI18N  
+
+            bw.write("# Create manifest file\n"); // NOI18N
             bw.write("cd \"${TOP}\"\n"); // NOI18N
-            bw.write(packagingConfiguration.getToolValue() + " " + packagingConfiguration.getOptionsValue() + " -o -f $PROTOTYPEFILE -r . -d $TMPDIR\n"); // NOI18N
-            bw.write("checkReturnCode\n"); // NOI18N
-//        bw.write("pkgtrans -s ${TMPDIR} tmp.pkg " + packageName + "\n"); // NOI18N
-//        bw.write("checkReturnCode\n"); // NOI18N
-            bw.write("rm -rf " + packagingConfiguration.getOutputValue() + "/" + packageName + "\n"); // NOI18N
-            bw.write("mv ${TMPDIR}/" + packageName + " " + packagingConfiguration.getOutputValue() + "\n"); // NOI18N
-            bw.write("checkReturnCode\n"); // NOI18N
-            bw.write("echo Solaris SVR4: " + packagingConfiguration.getOutputValue() + "/" + packageName + "\n"); // NOI18N
+            bw.write("CONTROL_FILE=${TMPDIR}/manifest\n"); // NOI18N
+            bw.write("rm -f ${CONTROL_FILE}\n"); // NOI18N
+            bw.write("\n"); // NOI18N
+            bw.write("cd \"${TOP}\"\n"); // NOI18N
+
+            bw.write("echo \'" + "#" + "\' >> ${CONTROL_FILE}\n"); // NOI18N
+            bw.write("echo \'" + "# header" + "\' >> ${CONTROL_FILE}\n"); // NOI18N
+            bw.write("echo \'" + "#" + "\' >> ${CONTROL_FILE}\n"); // NOI18N
+            List<PackagerInfoElement> infoList = packagingConfiguration.getHeaderSubList(PACKAGER_NAME);
+            for (PackagerInfoElement elem : infoList) {
+                String value = elem.getValue();
+                int i = 0;
+                int j = value.indexOf("\\n"); // NOI18N
+                while (j >= 0) {
+                    if (i == 0) {
+                        bw.write("echo \'" + elem.getName() + ": " + value.substring(i, j) + "\' >> ${CONTROL_FILE}\n"); // NOI18N
+                    } else {
+                        bw.write("echo \'" + value.substring(i, j) + "\' >> ${CONTROL_FILE}\n"); // NOI18N
+                    }
+                    i = j + 2;
+                    j = value.indexOf("\\n", i); // NOI18N
+                }
+                if (i < value.length()) {
+                    if (i == 0) {
+                        bw.write("echo \'name=\"" + elem.getName() + "\" value=\"" + value.substring(i) + "\"\' >> ${CONTROL_FILE}\n"); // NOI18N
+                    } else {
+                        bw.write("echo \'" + value.substring(i) + "\' >> ${CONTROL_FILE}\n"); // NOI18N
+                    }
+                }
+            }
+
+            for (String addInfo : packagingConfiguration.getAdditionalInfo().getValue()) {
+                bw.write("echo \"" + addInfo + "\" >> ${CONTROL_FILE}\n"); // NOI18N
+            }
+
+            // Directories
+            bw.write("\n"); // NOI18N
+            bw.write("echo \'" + "#" + "\' >> ${CONTROL_FILE}\n"); // NOI18N
+            bw.write("echo \'" + "# directories" + "\' >> ${CONTROL_FILE}\n"); // NOI18N
+            bw.write("echo \'" + "#" + "\' >> ${CONTROL_FILE}\n"); // NOI18N
+            List<String> dirList = findUndefinedDirectories(packagingConfiguration);
+            for (String dir : dirList) {
+                bw.write("echo \"");// NOI18N
+                bw.write("dir "); // NOI18N
+                bw.write("mode=0" + MakeOptions.getInstance().getDefExePerm() + " "); // NOI18N
+                bw.write("owner=" + MakeOptions.getInstance().getDefOwner() + " "); // NOI18N
+                bw.write("group=" + MakeOptions.getInstance().getDefGroup() + " "); // NOI18N
+                bw.write("path=" + dir); // NOI18N
+                bw.write("\""); // NOI18N
+                bw.write(" >> ${CONTROL_FILE}\n"); // NOI18N
+
+            }
+            for (PackagerFileElement elem : fileList) {
+                if (elem.getType() == PackagerFileElement.FileType.DIRECTORY) {
+                    String path = packagingConfiguration.expandMacros(elem.getTo());
+                    if (path.endsWith("/")) { // NOI18N
+                        path = path.substring(0, path.length() - 1);
+                    }
+                    bw.write("echo \"");// NOI18N
+                    bw.write("dir "); // NOI18N
+                    bw.write("mode=0" + elem.getPermission() + " "); // NOI18N
+                    bw.write("owner=" + elem.getOwner() + " "); // NOI18N
+                    bw.write("group=" + elem.getGroup() + " "); // NOI18N
+                    bw.write("path=" + path); // NOI18N
+                    bw.write("\""); // NOI18N
+                    bw.write(" >> ${CONTROL_FILE}\n"); // NOI18N
+                }
+            }
+
+            // Files
+            bw.write("\n"); // NOI18N
+            bw.write("echo \'" + "#" + "\' >> ${CONTROL_FILE}\n"); // NOI18N
+            bw.write("echo \'" + "# files" + "\' >> ${CONTROL_FILE}\n"); // NOI18N
+            bw.write("echo \'" + "#" + "\' >> ${CONTROL_FILE}\n"); // NOI18N
+            for (PackagerFileElement elem : fileList) {
+                if (elem.getType() == PackagerFileElement.FileType.FILE) {
+                    String path = packagingConfiguration.expandMacros(elem.getTo());
+                    bw.write("echo \"");// NOI18N
+                    bw.write("file "); // NOI18N
+                    bw.write(path.substring(1) + " "); // NOI18N
+                    bw.write("mode=0" + elem.getPermission() + " "); // NOI18N
+                    bw.write("owner=" + elem.getOwner() + " "); // NOI18N
+                    bw.write("group=" + elem.getGroup() + " "); // NOI18N
+                    bw.write("path=" + path); // NOI18N
+                    bw.write("\""); // NOI18N
+                    bw.write(" >> ${CONTROL_FILE}\n"); // NOI18N
+                }
+            }
+
+            // Links
+            bw.write("\n"); // NOI18N
+            bw.write("echo \'" + "#" + "\' >> ${CONTROL_FILE}\n"); // NOI18N
+            bw.write("echo \'" + "# links" + "\' >> ${CONTROL_FILE}\n"); // NOI18N
+            bw.write("echo \'" + "#" + "\' >> ${CONTROL_FILE}\n"); // NOI18N
+            for (PackagerFileElement elem : fileList) {
+                if (elem.getType() == PackagerFileElement.FileType.SOFTLINK) {
+                    String path = packagingConfiguration.expandMacros(elem.getTo());
+                    bw.write("echo \"");// NOI18N
+                    bw.write("hardlink "); // NOI18N
+                    bw.write("path=" + path + " "); // NOI18N
+                    bw.write("target=" + packagingConfiguration.expandMacros(elem.getFrom())); // NOI18N
+                    bw.write("\""); // NOI18N
+                    bw.write(" >> ${CONTROL_FILE}\n"); // NOI18N
+                }
+            }
+
+            bw.write("echo IPS: " + packagingConfiguration.getOutputValue() + "\n"); // NOI18N
             bw.write("\n"); // NOI18N
         }
     }
