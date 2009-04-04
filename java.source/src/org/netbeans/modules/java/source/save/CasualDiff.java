c4b01014c680c4e01514fbf737ffdc7e7867a515
Jan Lahoda <jlahoda@netbeans.org>
2009-04-04 10:40:36 +0200

    #159671: correct rewriting of AssertTree.

--- java.source/src/org/netbeans/modules/java/source/save/CasualDiff.java
+++ java.source/src/org/netbeans/modules/java/source/save/CasualDiff.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2009 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -24,7 +24,7 @@
  * Contributor(s):
  *
  * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2009 Sun
  * Microsystems, Inc. All Rights Reserved.
  *
  * If you wish your version of this file to be governed by only the CDDL
@@ -1097,11 +1097,14 @@ public class CasualDiff {
         // detail
         if (oldT.detail != newT.detail) {
             if (oldT.detail == null) {
+                copyTo(localPointer, condBounds[1]);
+                localPointer = condBounds[1];
                 printer.print(" : ");
                 printer.print(newT.detail);
             } else {
                 int[] detailBounds = getBounds(oldT.detail);
                 if (newT.detail == null) {
+                    copyTo(localPointer, condBounds[1]);
                     localPointer = detailBounds[1];
                 } else {
                     copyTo(localPointer, detailBounds[0]);
