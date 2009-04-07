2655a5e2a2a29f138243be70204390950d344c6c
Vladimir Voskresensky <vv159170@netbeans.org>
2009-04-07 15:23:47 +0400

    clean up

--- cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/csm/deep/StatementBase.java
+++ cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/csm/deep/StatementBase.java
@@ -51,7 +51,6 @@ import java.io.DataOutput;
 import java.io.IOException;
 
 import org.netbeans.modules.cnd.modelimpl.csm.core.*;
-import org.netbeans.modules.cnd.modelimpl.debug.TraceFlags;
 import org.netbeans.modules.cnd.modelimpl.uid.UIDCsmConverter;
 import org.netbeans.modules.cnd.modelimpl.uid.UIDObjectFactory;
 
