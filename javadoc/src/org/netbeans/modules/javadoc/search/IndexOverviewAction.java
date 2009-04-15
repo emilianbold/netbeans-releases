611b7d8e8238149d8f3d965dbc7d6c67871a9fbe
Jan Pokorsky <jpokorsky@netbeans.org>
2009-04-15 15:51:20 +0200

    #121685: added Javadoc References window to handle large reference sets

--- javadoc/src/org/netbeans/modules/javadoc/search/IndexOverviewAction.java
+++ javadoc/src/org/netbeans/modules/javadoc/search/IndexOverviewAction.java
@@ -100,6 +100,8 @@ public final class IndexOverviewAction extends SystemAction implements Presenter
      * is created, it will create submenuitems for each available index.
      */
     private final class IndexMenu extends JMenu implements HelpCtx.Provider, DynamicMenuContent {
+
+        private static final int MAX_ITEMS = 20;
         
         private int itemHash = 0;
         
@@ -151,9 +153,10 @@ public final class IndexOverviewAction extends SystemAction implements Presenter
                 int size = names.size();
                 if (size != indices.size()) throw new IllegalStateException();
                 if (size > 0) {
-                    for (int i = 0; i < size; i++) {
-                        add(new IndexMenuItem((String)names.get(i), (FileObject)indices.get(i)));
+                    for (int i = 0; i < size && i < MAX_ITEMS; i++) {
+                        add(new IndexMenuItem((String) names.get(i), (FileObject) indices.get(i)));
                     }
+                    add(new MoreReferencesMenuItem());
                 } else {
                     JMenuItem dummy = new JMenuItem(NbBundle.getMessage(IndexOverviewAction.class, "CTL_no_indices_found"));
                     dummy.setEnabled(false);
@@ -209,6 +212,26 @@ public final class IndexOverviewAction extends SystemAction implements Presenter
             return IndexOverviewAction.this.getHelpCtx();
         }
         
+    }
+
+    private static final class MoreReferencesMenuItem extends JMenuItem implements ActionListener {
+
+        public MoreReferencesMenuItem() {
+            Mnemonics.setLocalizedText(this, NbBundle.getMessage(IndexOverviewAction.class, "CTL_MORE_INDICES_MenuItem"));
+            addActionListener(this);
+        }
+
+        public void actionPerformed(ActionEvent e) {
+            FileObject fsRef = ReferencesPanel.showInWindow();
+            URL u = null;
+            if (fsRef != null) {
+                u = JavadocURLMapper.findURL(fsRef);
+            }
+            if (u != null) {
+                HtmlBrowser.URLDisplayer.getDefault().showURL(u);
+            }
+        }
+
     }
     
 }
