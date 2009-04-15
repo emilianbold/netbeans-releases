2889ae5c07398e95b13d48aeddacd71325b34e07
Vladimir Kvashin <vkvashin@netbeans.org>
2009-04-16 01:56:50 +0400

    #160540 NullPointerException at org.netbeans.modules.cnd.remote.support.SystemIncludesUtils.run

--- cnd/src/org/netbeans/modules/cnd/api/compilers/CompilerSetProviderFactory.java
+++ cnd/src/org/netbeans/modules/cnd/api/compilers/CompilerSetProviderFactory.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- * 
- * Copyright 2008 Sun Microsystems, Inc. All rights reserved.
- * 
+ *
+ * Copyright 2009 Sun Microsystems, Inc. All rights reserved.
+ *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
  * Development and Distribution License("CDDL") (collectively, the
@@ -20,7 +20,7 @@
  * License Header, with the fields enclosed by brackets [] replaced by
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- * 
+ *
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -31,29 +31,37 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
- * 
+ *
  * Contributor(s):
- * 
- * Portions Copyrighted 2008 Sun Microsystems, Inc.
+ *
+ * Portions Copyrighted 2009 Sun Microsystems, Inc.
  */
 
 package org.netbeans.modules.cnd.api.compilers;
 
-import java.util.List;
+import org.netbeans.modules.cnd.spi.compilers.CompilerSetProviderFactoryImpl;
+import org.netbeans.modules.nativeexecution.api.ExecutionEnvironment;
+import org.openide.util.Lookup;
 
 /**
- * @author gordonp
- * @author vkvashin
+ * An factory for creation CompilerSetProvider instances
+ * @author Vladimir Kvashin
  */
-public interface CompilerSetProvider {
+public class CompilerSetProviderFactory {
+    /**
+     * Creates a new instance of CompilerSetProvider
+     * for the given execution environment
+     * @param execEnv execution environment to create CompilerSetProvider for
+     * @return new CompilerSetProvider instance
+     */
+    public static CompilerSetProvider createNew(ExecutionEnvironment execEnv) {
+        CompilerSetProviderFactoryImpl factory = Lookup.getDefault().lookup(CompilerSetProviderFactoryImpl.class);
+        if (factory == null) {
+            throw new IllegalStateException(CompilerSetProviderFactoryImpl.class.getName() +" not found in lookup"); //NOI18N
+        }
+        return factory.createNew(execEnv);
+    }
 
-    int getPlatform();
-
-    boolean hasMoreCompilerSets();
-
-    String getNextCompilerSetData();
-
-    String[] getCompilerSetData(String path);
-
-    Runnable createCompilerSetDataLoader(List<CompilerSet> sets);
+    private CompilerSetProviderFactory() {
+    }
 }
--- cnd/src/org/netbeans/modules/cnd/spi/compilers/CompilerSetProviderFactoryImpl.java
+++ cnd/src/org/netbeans/modules/cnd/spi/compilers/CompilerSetProviderFactoryImpl.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- * 
- * Copyright 2008 Sun Microsystems, Inc. All rights reserved.
- * 
+ *
+ * Copyright 2009 Sun Microsystems, Inc. All rights reserved.
+ *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
  * Development and Distribution License("CDDL") (collectively, the
@@ -20,7 +20,7 @@
  * License Header, with the fields enclosed by brackets [] replaced by
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- * 
+ *
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -31,29 +31,27 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
- * 
+ *
  * Contributor(s):
- * 
- * Portions Copyrighted 2008 Sun Microsystems, Inc.
+ *
+ * Portions Copyrighted 2009 Sun Microsystems, Inc.
  */
 
-package org.netbeans.modules.cnd.api.compilers;
+package org.netbeans.modules.cnd.spi.compilers;
 
-import java.util.List;
+import org.netbeans.modules.cnd.api.compilers.CompilerSetProvider;
+import org.netbeans.modules.nativeexecution.api.ExecutionEnvironment;
 
 /**
- * @author gordonp
- * @author vkvashin
+ * An SPI for creation CompilerSetProvider instances
+ * @author Vladimir Kvashin
  */
-public interface CompilerSetProvider {
-
-    int getPlatform();
-
-    boolean hasMoreCompilerSets();
-
-    String getNextCompilerSetData();
-
-    String[] getCompilerSetData(String path);
-
-    Runnable createCompilerSetDataLoader(List<CompilerSet> sets);
+public interface CompilerSetProviderFactoryImpl {
+    /**
+     * Creates a new instance of CompilerSetProvider
+     * for the given execution environment
+     * @param execEnv execution environment to create CompilerSetProvider for
+     * @return new CompilerSetProvider instance
+     */
+    public CompilerSetProvider createNew(ExecutionEnvironment execEnv);
 }
