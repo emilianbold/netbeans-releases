bbe1853f43359a53a9f2e8955dc3ee789e55b3ce
Milan Kubec <mkubec@netbeans.org>
2009-04-17 09:37:43 +0200

    #162567: Next won't be allowed when licenses cannot be loaded

--- kenai.ui/src/org/netbeans/modules/kenai/ui/NameAndLicenseWizardPanelGUI.java
+++ kenai.ui/src/org/netbeans/modules/kenai/ui/NameAndLicenseWizardPanelGUI.java
@@ -123,6 +123,8 @@ public class NameAndLicenseWizardPanelGUI extends JPanel {
 
     private String prjNameCheckMessage = null;
 
+    private boolean licensesLoaded = true;
+
     public NameAndLicenseWizardPanelGUI(NameAndLicenseWizardPanel pnl) {
 
         panel = pnl;
@@ -205,10 +207,12 @@ public class NameAndLicenseWizardPanelGUI extends JPanel {
                         model.addElement(license.getDisplayName());
                         licenseList.add(license);
                     }
+                    licensesLoaded = true;
                 } else {
                     // XXX this item needs to be selected
                     model.addElement(NbBundle.getMessage(NameAndLicenseWizardPanel.class,
                             "NameAndLicenseWizardPanelGUI.noLicensesError"));
+                    licensesLoaded = false;
                 }
                 if (!licenseList.isEmpty()) {
                     setLicenses(licenseList);
@@ -671,6 +675,9 @@ public class NameAndLicenseWizardPanelGUI extends JPanel {
             return "Project Description must to be shorter than 500 characters.";
         } else if (prjNameCheckMessage!=null) {
             return prjNameCheckMessage;
+        } else if (!licensesLoaded) {
+            return NbBundle.getMessage(NameAndLicenseWizardPanelGUI.class,
+                    "NameAndLicenseWizardPanelGUI.noLicensesErrMsg");
         }
         return null;
     }
