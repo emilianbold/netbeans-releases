c47bac60b29a5a543f23c6947fd026bc85c96931
Vladimir Kvashin <vkvashin@netbeans.org>
2009-04-21 20:38:25 +0400

    Added an embrionic ServerList test

--- cnd.remote/test/unit/src/org/netbeans/modules/cnd/remote/support/ServerListTestCase.java
+++ cnd.remote/test/unit/src/org/netbeans/modules/cnd/remote/support/ServerListTestCase.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- *
+ * 
  * Copyright 2008 Sun Microsystems, Inc. All rights reserved.
- *
+ * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
  * Development and Distribution License("CDDL") (collectively, the
@@ -20,7 +20,7 @@
  * License Header, with the fields enclosed by brackets [] replaced by
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- *
+ * 
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -31,39 +31,42 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
- *
+ * 
  * Contributor(s):
- *
+ * 
  * Portions Copyrighted 2008 Sun Microsystems, Inc.
  */
+package org.netbeans.modules.cnd.remote.support;
 
-package org.netbeans.modules.cnd.remote;
-
-import junit.framework.Test;
-import junit.framework.TestSuite;
-import org.netbeans.modules.cnd.remote.mapper.MappingsTestCase;
-import org.netbeans.modules.cnd.remote.support.AutarkicRemoteTestCase;
-import org.netbeans.modules.cnd.remote.support.ServerListTestCase;
-import org.netbeans.modules.cnd.remote.support.TransportTestCase;
-import org.netbeans.modules.cnd.test.BaseTestSuite;
+import org.netbeans.modules.cnd.api.remote.ServerList;
+import org.netbeans.modules.cnd.api.remote.ServerRecord;
+import org.netbeans.modules.nativeexecution.api.ExecutionEnvironment;
 
 /**
+ * There hardly is a way to unit test remote operations.
+ * This is just an entry point for manual validation.
  *
  * @author Sergey Grinev
  */
-public class RemoteDevelopmentTest extends BaseTestSuite {
+public class ServerListTestCase extends RemoteTestBase {
 
-    public RemoteDevelopmentTest() {
-        super("Remote Development"); // NOI18N
-        addTestSuite(MappingsTestCase.class);
-        addTestSuite(AutarkicRemoteTestCase.class);
-        addTestSuite(TransportTestCase.class);
-        addTestSuite(ServerListTestCase.class);
+    static {
+//        System.setProperty("cnd.remote.testuserinfo", "rdtest:********@endif.russia");
+//        System.setProperty("cnd.remote.logger.level", "0");
+//        System.setProperty("nativeexecution.support.logger.level", "0");
+    }
+    public ServerListTestCase(String testName) {
+        super(testName);
     }
 
-    public static Test suite() {
-        TestSuite suite = new RemoteDevelopmentTest();
-        return suite;
+    public void testRun() throws Exception {        
+        if (canTestRemote()) {
+            ExecutionEnvironment execEnv = getRemoteExecutionEnvironment();
+            ServerRecord rec = ServerList.addServer(execEnv, false, true);
+            assertNotNull("Null server record", rec);
+            assertEquals(rec.getExecutionEnvironment(), execEnv);
+        } else {
+            System.err.println("Remote tests are not configured."); // to test remote runs
+        }
     }
-
 }
