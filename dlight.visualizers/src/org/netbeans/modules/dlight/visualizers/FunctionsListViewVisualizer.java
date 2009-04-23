ad239f0c5ceb31f4aa66672f9f6bc14760138437
Andrew Krasny <akrasny@netbeans.org>
2009-04-23 22:19:32 +0400

    IZ163518: PARTFIX: Sometimes "No data available yet" appears in detailed view even after it was just displayed

--- dlight.visualizers/src/org/netbeans/modules/dlight/visualizers/FunctionsListViewVisualizer.java
+++ dlight.visualizers/src/org/netbeans/modules/dlight/visualizers/FunctionsListViewVisualizer.java
@@ -322,7 +322,7 @@ public class FunctionsListViewVisualizer extends JPanel implements
         super.removeNotify();
         synchronized (queryLock) {
             if (task != null) {
-                task.cancel(true);
+                task.cancel(false);
                 task = null;
             }
         }
