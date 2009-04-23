860a0a591afab754cc47b8ad43f342d1b30bb689
Thomas Preisler <thp@netbeans.org>
2009-04-23 13:03:53 -0700

    #152168 Cannot run c program in KDE konsole

--- cnd.makeproject/src/org/netbeans/modules/cnd/makeproject/api/runprofiles/RunProfile.java
+++ cnd.makeproject/src/org/netbeans/modules/cnd/makeproject/api/runprofiles/RunProfile.java
@@ -219,11 +219,11 @@ public class RunProfile implements ConfigurationAuxObject {
                 name = getString("TerminalType_KDE"); // NOI18N
                 list.add(name); 
                 termPaths.put(name, termPath);
-                termOptions.put(name, "--notabbar --workdir " + baseDir + " -e \"" + dorun + // NOI18N
+                termOptions.put(name, "--workdir " + baseDir + " -e \"" + dorun + // NOI18N
                         "\" -p \"" + getString("LBL_RunPrompt") + "\" -f \"{0}\" {1} {2}"); // NOI18N
                 if (termPaths.get(def) == null) {
                     termPaths.put(def, termPath);
-                    termOptions.put(def, "--notabbar --workdir " + baseDir + " -e \"" + dorun + // NOI18N
+                    termOptions.put(def, "--workdir " + baseDir + " -e \"" + dorun + // NOI18N
                         "\" -p \"" + getString("LBL_RunPrompt") + "\" -f \"{0}\" {1} {2}"); // NOI18N
                 }
             }
