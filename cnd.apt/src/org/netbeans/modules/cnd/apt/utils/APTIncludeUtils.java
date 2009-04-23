08a24263661ad403f4471129cf5893bc3f96039f
Alexander Simon <alexvsimon@netbeans.org>
2009-04-23 13:01:22 +0400

    fixed IZ#160043:Code assistance does not recognize "framework" bundle structure properly - resolve included file in framework

--- cnd.apt/src/org/netbeans/modules/cnd/apt/utils/APTIncludeUtils.java
+++ cnd.apt/src/org/netbeans/modules/cnd/apt/utils/APTIncludeUtils.java
@@ -52,6 +52,7 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 import org.netbeans.modules.cnd.apt.debug.APTTraceFlags;
 import org.netbeans.modules.cnd.apt.support.ResolvedPath;
+import org.netbeans.modules.cnd.utils.cache.CharSequenceKey;
 
 /**
  *
@@ -94,12 +95,27 @@ public class APTIncludeUtils {
         mapFoldersRef.clear();
     }
     
-    public static ResolvedPath resolveFilePath(Iterator<CharSequence> it, String file, int dirOffset) {
-        while( it.hasNext() ) {
-            CharSequence sysPrefix = it.next();
-            File fileFromPath = new File(new File(sysPrefix.toString()), file);
+    public static ResolvedPath resolveFilePath(Iterator<CharSequence> searchPaths, String includedFile, int dirOffset) {
+        while( searchPaths.hasNext() ) {
+            CharSequence sysPrefix = searchPaths.next();
+            String sysPrefixString = sysPrefix.toString();
+            File fileFromPath = new File(new File(sysPrefixString), includedFile);
             if (!isDirectory(fileFromPath) && exists(fileFromPath)) {
                 return new ResolvedPath(sysPrefix, fileFromPath.getAbsolutePath(), false, dirOffset);
+            } else {
+                int i = includedFile.indexOf('/'); // NOI18N
+                if (i > 0 && sysPrefixString.endsWith("/Frameworks")){ // NOI18N
+                    // possible it is framework include (see IZ#160043)
+                    // #include <GLUT/glut.h>
+                    // header is located in the /System/Library/Frameworks/GLUT.framework/Headers
+                    // system path is /System/Library/Frameworks
+                    // So convert framework path
+                    String fileName = sysPrefixString+"/"+includedFile.substring(0,i)+".framework/Headers"+includedFile.substring(i); // NOI18N
+                    fileFromPath = new File(fileName);
+                    if (!isDirectory(fileFromPath) && exists(fileFromPath)) {
+                        return new ResolvedPath(sysPrefix, fileFromPath.getAbsolutePath(), false, dirOffset);
+                    }
+                }
             }
             dirOffset++;
         }
