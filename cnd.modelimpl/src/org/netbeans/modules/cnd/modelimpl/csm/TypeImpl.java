f163d91e4623a55f803624071bb0b6c639965f26
Vladimir Voskresensky <vv159170@netbeans.org>
2009-04-23 20:10:45 +0400

    fixed IZ#136973: Indeterminate behavior of code completion and hyper link for two structs with same name

--- cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/csm/TypeImpl.java
+++ cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/csm/TypeImpl.java
@@ -78,7 +78,8 @@ public class TypeImpl extends OffsetableBase implements CsmType, SafeClassifierP
     private static final byte FLAGS_TYPE_OF_TYPEDEF = 1 << 0;
     private static final byte FLAGS_REFERENCE = 1 << 1;
     private static final byte FLAGS_CONST = 1 << 2;
-    protected static final int LAST_USED_FLAG_INDEX = 3;
+    private static final byte FLAGS_TYPE_WITH_CLASSIFIER = 1 << 3;
+    protected static final int LAST_USED_FLAG_INDEX = 4;
 
     private final byte pointerDepth;
     private final byte arrayDepth;
@@ -106,6 +107,7 @@ public class TypeImpl extends OffsetableBase implements CsmType, SafeClassifierP
             this._setClassifier(initClassifier(ast));
             this.classifierText = initClassifierText(ast);
         } else {
+            setFlags(FLAGS_TYPE_WITH_CLASSIFIER, true);
             CharSequence typeName = classifier.getName();
             if (typeName == null || typeName.length()==0){
                 this.classifierText = initClassifierText(ast);
@@ -179,7 +181,8 @@ public class TypeImpl extends OffsetableBase implements CsmType, SafeClassifierP
         this.arrayDepth = (byte) type.getArrayDepth();
         setFlags(FLAGS_CONST, type.isConst());
         setFlags(FLAGS_TYPE_OF_TYPEDEF, type.isTypeOfTypedef());
-        
+        setFlags(FLAGS_TYPE_WITH_CLASSIFIER, type.isTypeWithClassifier());
+
         this.classifierUID = type.classifierUID;
         this.qname = type.qname;
         this.classifierText = type.classifierText;
@@ -202,6 +205,7 @@ public class TypeImpl extends OffsetableBase implements CsmType, SafeClassifierP
         if (type instanceof TypeImpl) {
             TypeImpl ti = (TypeImpl) type;
             setFlags(FLAGS_TYPE_OF_TYPEDEF, ti.isTypeOfTypedef());
+            setFlags(FLAGS_TYPE_WITH_CLASSIFIER, ti.isTypeWithClassifier());
             this.classifierUID = ti.classifierUID;
             this.qname = ti.qname;
             this.classifierText = ti.classifierText;
@@ -255,8 +259,12 @@ public class TypeImpl extends OffsetableBase implements CsmType, SafeClassifierP
         return pointerDepth > 0;
     }
 
-    public boolean isTypeOfTypedef() {
+    private boolean isTypeOfTypedef() {
         return hasFlags(FLAGS_TYPE_OF_TYPEDEF);
+    }
+
+    private boolean isTypeWithClassifier() {
+        return hasFlags(FLAGS_TYPE_WITH_CLASSIFIER);
     }
 
     public List<CsmSpecializationParameter> getInstantiationParams() {
@@ -453,7 +461,7 @@ public class TypeImpl extends OffsetableBase implements CsmType, SafeClassifierP
         if (CsmBaseUtilities.isValid(classifier)) {
             // skip
             needToRender = false;
-            if (isTypeOfTypedef() && (parent != null) && !CsmKindUtilities.isBuiltIn(classifier)) {
+            if (!isTypeWithClassifier() && (qname != null) && (parent != null) && !CsmKindUtilities.isBuiltIn(classifier)) {
                 // check visibility of classifier
                 if (!CsmIncludeResolver.getDefault().isObjectVisible(parent.getStartFile(), classifier)) {
                     needToRender = true;
