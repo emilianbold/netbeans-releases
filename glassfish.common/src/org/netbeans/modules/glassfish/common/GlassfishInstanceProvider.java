9fd4dc11695c20060cd66398a09b6dfe6894a9ee
Vince Kraemer <vkraemer@netbeans.org>
2009-04-29 14:10:16 -0700

    #159557: add marker module to provide a similar feature as  the enableExperimentalFeatures property

--- glassfish.common/src/org/netbeans/modules/glassfish/common/GlassfishInstanceProvider.java
+++ glassfish.common/src/org/netbeans/modules/glassfish/common/GlassfishInstanceProvider.java
@@ -88,9 +88,10 @@ public final class GlassfishInstanceProvider implements ServerInstanceProvider {
     static private String PRELUDE_INSTANCES_PATH = "/GlassFish/Instances"; // NOI18N
 
     public static GlassfishInstanceProvider getEe6() {
+        FileObject fo = FileUtil.getConfigFile("GlassFish v3/Enable Experimental Features");
         String v3Root = System.getProperty(EE6_INSTALL_ROOT_PROP);
-        if ("true".equals(System.getProperty(PRELUDE_PROP_ROOT + ENABLE_EXPERIMENTAL_SUFFIX)) || // NOI18N
-               (null != v3Root && v3Root.trim().length() > 0) ) {
+        if ("true".equals(System.getProperty(PRELUDE_PROP_ROOT + ENABLE_EXPERIMENTAL_SUFFIX)) || 
+               (null != v3Root && v3Root.trim().length() > 0) || null != fo) {
             if (ee6Provider == null) {
                 ee6Provider = new GlassfishInstanceProvider(new String[] {EE6_DEPLOYER_FRAGMENT},
                         new String[] {EE6_INSTANCES_PATH},
