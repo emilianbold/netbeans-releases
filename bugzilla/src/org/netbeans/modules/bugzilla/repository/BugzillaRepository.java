134b9fb01f3ca883c7954879d7b5b091811f0fb7
Tomas Stupka <tstupka@netbeans.org>
2009-04-30 11:01:34 +0200

    #164095 - All Issues list contains wrong items

--- bugzilla/src/org/netbeans/modules/bugzilla/repository/BugzillaRepository.java
+++ bugzilla/src/org/netbeans/modules/bugzilla/repository/BugzillaRepository.java
@@ -25,7 +25,7 @@
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
  * under the [CDDL or GPL Version 2] license." If you do not indicate a
- * single choice of license, a recipient has the option to distribute
+ * single choiceget of license, a recipient has the option to distribute
  * your version of this file under either the CDDL, the GPL Version 2 or
  * to extend the choice of license to its licensees as provided above.
  * However, if you add GPL Version 2 code and therefore, elected the GPL
