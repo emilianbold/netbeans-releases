4a7f813a3cec7e5f70b1739e649293bb74676b61
Nick N. Krasilnikov <nnnnnk@netbeans.org>
2009-05-14 21:06:44 +0400

    fixed I18N in CsmCompletionQuery

--- cnd.completion/src/org/netbeans/modules/cnd/completion/cplusplus/ext/CsmCompletionQuery.java
+++ cnd.completion/src/org/netbeans/modules/cnd/completion/cplusplus/ext/CsmCompletionQuery.java
@@ -1988,7 +1988,7 @@ abstract public class CsmCompletionQuery {
                         CsmFileReferences.hasTemplateBasedAncestors(lastType)) {
                     Collection<CsmObject> data = new ArrayList();
                     data.add(new TemplateBasedReferencedObjectImpl());
-                    result = new CsmCompletionResult(component, getBaseDocument(), data, "title", item, endOffset, 0, 0, isProjectBeeingParsed(), contextElement, instantiateTypes);
+                    result = new CsmCompletionResult(component, getBaseDocument(), data, "title", item, endOffset, 0, 0, isProjectBeeingParsed(), contextElement, instantiateTypes); // NOI18N
                 }
             }
 
