7edae0292ab54ac3cb61b394c5426b30b6dc8b11
Alexander Simon <alexvsimon@netbeans.org>
2009-05-14 13:35:02 +0400

    fixed IZ#165014:Regression in org.openide.filesystems.FileUtil.getConfigFile()

--- cnd/src/org/netbeans/modules/cnd/api/compilers/CompilerSetManager.java
+++ cnd/src/org/netbeans/modules/cnd/api/compilers/CompilerSetManager.java
@@ -461,7 +461,7 @@ public class CompilerSetManager {
             return PlatformTypes.PLATFORM_WINDOWS;
         } else if (os.toLowerCase().contains("linux")) { // NOI18N
             return PlatformTypes.PLATFORM_LINUX;
-        } else if (os.toLowerCase().contains("mac")) { // NOI18N
+        } else if (os.toLowerCase().contains("mac") || os.startsWith("Darwin")) { // NOI18N
             return PlatformTypes.PLATFORM_MACOSX;
         } else {
             return PlatformTypes.PLATFORM_GENERIC;
