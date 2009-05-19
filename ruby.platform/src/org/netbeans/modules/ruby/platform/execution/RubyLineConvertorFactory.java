3008e1a66f1debbf9b30f5569ca74b111db2ac8c
pcw <pcw@netbeans.org>
2009-05-19 14:10:28 -0700

    Fixed overaggressive hyperlinking in output window for ruby servers.  Was treating ##:## time fields and url:port combinations as file:line# patterns.

--- ruby.platform/src/org/netbeans/modules/ruby/platform/execution/RubyLineConvertorFactory.java
+++ ruby.platform/src/org/netbeans/modules/ruby/platform/execution/RubyLineConvertorFactory.java
@@ -39,9 +39,14 @@
 package org.netbeans.modules.ruby.platform.execution;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.netbeans.api.extexecution.ExecutionDescriptor.LineConvertorFactory;
+import org.netbeans.api.extexecution.print.ConvertedLine;
 import org.netbeans.api.extexecution.print.LineConvertor;
 import org.netbeans.api.extexecution.print.LineConvertors;
 import org.netbeans.api.extexecution.print.LineConvertors.FileLocator;
@@ -51,6 +56,8 @@ import org.netbeans.api.extexecution.print.LineConvertors.FileLocator;
  * @author Erno Mononen
  */
 public final class RubyLineConvertorFactory implements LineConvertorFactory {
+
+    private static final Logger LOGGER = Logger.getLogger(RubyLineConvertorFactory.class.getName());
 
     private static final String WINDOWS_DRIVE = "(?:\\S{1}:[\\\\/])"; // NOI18N
     private static final String FILE_CHAR = "[^\\s\\[\\]\\:\\\"]"; // NOI18N
@@ -64,6 +71,14 @@ public final class RubyLineConvertorFactory implements LineConvertorFactory {
     // see #157616
     static final Pattern JRUBY_COMPILER = Pattern.compile(":.*:\\s.*?" + STD_SUFFIX); // NOI18N
     static final Pattern RUBY_COMPILER_WIN_MY = Pattern.compile(".*?" + FILE_WIN + SEP + LINE + ROL); // NOI18N
+
+    private static final String TIME_PATTERN = "(\\d{1,2}:\\d{1,2}:\\d{1,2})"; // NOI18N
+    private static final String NUMERIC_URL_PATTERN = "(?:\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}.\\d{1,3})"; // NOI18N
+    private static final String HTTP_URL_PATTERN = "(?:https?://.*?)"; // NOI18N
+    private static final String URL_PORT = "(?::(\\d{1,5}))"; // NOI18N
+    
+    static final Pattern TIME_MATCHER = Pattern.compile(TIME_PATTERN);
+    static final Pattern URL_MATCHER = Pattern.compile("(" + HTTP_URL_PATTERN + "|" + NUMERIC_URL_PATTERN + ")" + URL_PORT);
 
     /* Keeping old one. Get rid of this with more specific recongizers? */
     static final Pattern RUBY_COMPILER_WIN =
@@ -120,6 +135,8 @@ public final class RubyLineConvertorFactory implements LineConvertorFactory {
      */
     public static List<LineConvertor> getStandardConvertors(FileLocator locator) {
         List<LineConvertor> result = new ArrayList<LineConvertor>(4);
+        result.add(new ShortCircuitConvertor(TIME_MATCHER));
+        result.add(new ShortCircuitConvertor(URL_MATCHER));
         result.add(LineConvertors.filePattern(locator, RAILS_RECOGNIZER, null, 1, 2));
         result.add(LineConvertors.filePattern(locator, RUBY_COMPILER_WIN_MY, null, 1, 2));
         result.add(LineConvertors.filePattern(locator, JRUBY_COMPILER, null, 1, 2));
@@ -144,4 +161,45 @@ public final class RubyLineConvertorFactory implements LineConvertorFactory {
         }
         return LineConvertors.proxy(convertorList.toArray(new LineConvertor[convertorList.size()]));
     }
+
+
+    private static class ShortCircuitConvertor implements LineConvertor {
+
+        private final Pattern linePattern;
+
+        public ShortCircuitConvertor(Pattern linePattern) {
+            this.linePattern = linePattern;
+        }
+
+        public List<ConvertedLine> convert(final String line) {
+            // Don't try to match lines that are too long - the java.util.regex library
+            // throws stack exceptions (101234)
+            if (line.length() > 400) {
+                return null;
+            }
+
+            Matcher matcher = linePattern.matcher(line);
+            if(matcher.find()) {
+                if(LOGGER.isLoggable(Level.FINEST)) {
+                    StringBuilder builder = new StringBuilder(line.length());
+                    builder.append("[ ");
+                    for(int i = 1; i < matcher.groupCount(); i++) {
+                        String match = matcher.group(i);
+                        if(match != null) {
+                            if(builder.length() > 2) {
+                                builder.append(", ");
+                            }
+                            builder.append(match);
+                        }
+                    }
+                    builder.append(" ]");
+                    LOGGER.log(Level.FINEST, "ShortCircuitConvertor matched {0}.", builder.toString());
+                }
+                return Collections.<ConvertedLine>singletonList(ConvertedLine.forText(line, null));
+            }
+
+            return null;
+        }
+    }
+
 }
