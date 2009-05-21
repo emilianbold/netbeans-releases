8e58578c717120fd8eeb74d0130c92fe5715211f
Andrew Krasny <akrasny@netbeans.org>
2009-05-21 20:26:51 +0400

    NOIZ: upload simplification

--- dlight.nativeexecution/src/org/netbeans/modules/nativeexecution/api/util/CommonTasksSupport.java
+++ dlight.nativeexecution/src/org/netbeans/modules/nativeexecution/api/util/CommonTasksSupport.java
@@ -38,28 +38,27 @@
  */
 package org.netbeans.modules.nativeexecution.api.util;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.Writer;
 import java.nio.ByteBuffer;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
 import java.nio.channels.WritableByteChannel;
+import java.util.concurrent.Callable;
 import java.util.concurrent.Future;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 import org.netbeans.api.extexecution.ExecutionDescriptor;
-import org.netbeans.api.extexecution.ExecutionDescriptor.InputProcessorFactory;
 import org.netbeans.api.extexecution.ExecutionService;
-import org.netbeans.api.extexecution.input.InputProcessor;
-import org.netbeans.api.extexecution.input.InputProcessors;
 import org.netbeans.modules.nativeexecution.api.ExecutionEnvironment;
 import org.netbeans.modules.nativeexecution.api.NativeProcessBuilder;
 import org.netbeans.modules.nativeexecution.api.NativeProcess;
-import org.netbeans.modules.nativeexecution.api.NativeProcess.State;
 import org.netbeans.modules.nativeexecution.support.InputRedirectorFactory;
+import org.netbeans.modules.nativeexecution.support.NativeTaskExecutorService;
 import org.openide.windows.InputOutput;
 
 /**
@@ -96,75 +95,68 @@ public final class CommonTasksSupport {
             final String dstFileName,
             final int mask, final Writer error) {
 
-        final File localFile = new File(srcFileName);
+        Callable<Integer> uploadTask = new Callable<Integer>() {
 
-        if (!localFile.exists()) {
-            if (error != null) {
+            public Integer call() throws Exception {
+                Integer result = new Integer(-1);
+
+                final File localFile = new File(srcFileName);
+
+                if (!localFile.exists()) {
+                    if (error != null) {
+                        try {
+                            error.append("File " + srcFileName + " not found!"); // NOI18N
+                        } catch (IOException ex) {
+                        }
+                    }
+                    return result;
+                }
+
+                if (!localFile.canRead()) {
+                    if (error != null) {
+                        try {
+                            error.append("File " + srcFileName + " is not readable!"); // NOI18N
+                        } catch (IOException ex) {
+                        }
+                    }
+                    return result;
+                }
+
                 try {
-                    error.append("File " + srcFileName + " not found!"); // NOI18N
+                    CommandLineHelper helper = CommandLineHelper.getInstance(dstExecEnv);
+                    String dstFileNameEscaped = helper.toShellPath(dstFileName);
+
+                    NativeProcessBuilder npb = NativeProcessBuilder.newProcessBuilder(dstExecEnv);
+                    npb.setCommandLine(String.format("cat >%s && chmod 0%03o %s", dstFileNameEscaped, mask, dstFileNameEscaped)); // NOI18N
+                    NativeProcess np = npb.call();
+
+                    OutputStream os = np.getOutputStream();
+                    InputStream es = np.getErrorStream();
+
+                    result = transferFileContent(localFile, os);
+
+                    BufferedReader br = new BufferedReader(new InputStreamReader(es));
+                    String errorLine;
+
+                    while ((errorLine = br.readLine()) != null) {
+                        error.append(errorLine);
+                    }
+
+                    result += np.waitFor();
                 } catch (IOException ex) {
-                }
-            }
-            return null;
-        }
-
-        if (!localFile.canRead()) {
-            if (error != null) {
-                try {
-                    error.append("File " + srcFileName + " is not readable!"); // NOI18N
-                } catch (IOException ex) {
-                }
-            }
-            return null;
-        }
-
-        ChangeListener processListener = new ChangeListener() {
-
-            public void stateChanged(ChangeEvent e) {
-                final Object src = e.getSource();
-
-                if (!(src instanceof NativeProcess)) {
-                    return;
+                    if (error != null) {
+                        error.append(ex.getMessage() == null ? ex.toString() : ex.getMessage()); // NOI18N
+                    }
                 }
 
-                final NativeProcess process = (NativeProcess) src;
-                final State newState = process.getState();
-
-                if (newState == State.RUNNING) {
-                    new CopyRoutine(
-                            process.getOutputStream(),
-                            localFile, mask).start();
-                }
+                return result;
             }
         };
 
-        CommandLineHelper helper = CommandLineHelper.getInstance(dstExecEnv);
-        String dstFileNameEscaped = helper.toShellPath(dstFileName);
-
-        NativeProcessBuilder npb = NativeProcessBuilder.newProcessBuilder(dstExecEnv);
-        npb.setCommandLine(String.format("cat >%s && chmod 0%03o %s", dstFileNameEscaped, mask, dstFileNameEscaped)); // NOI18N
-        npb.addNativeProcessListener(processListener);
-
-        ExecutionDescriptor descriptor =
-                new ExecutionDescriptor().inputOutput(
-                InputOutput.NULL);
-        if (error != null) {
-            descriptor = descriptor.outProcessorFactory(
-                    new InputProcessorFactory() {
-
-                        public InputProcessor newInputProcessor(
-                                InputProcessor defaultProcessor) {
-                            return new FilterInputProcessor(
-                                    InputProcessors.copying(error));
-                        }
-                    });
-        }
-
-        ExecutionService execService = ExecutionService.newService(
-                npb, descriptor, "Upload file " + srcFileName + // NOI18N
+        Future<Integer> result = NativeTaskExecutorService.submit(uploadTask, "Upload file " + srcFileName + // NOI18N
                 " to " + dstExecEnv.toString() + ":" + dstFileName); // NOI18N
 
-        return execService.run();
+        return result;
     }
 
     /**
@@ -293,134 +285,65 @@ public final class CommonTasksSupport {
         return execService.run();
     }
 
-    private static final class CopyRoutine extends Thread {
+    private static int transferFileContent(File srcFile, OutputStream outStream) {
+        WritableByteChannel out = null;
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        boolean interrupted = false;
+        ReadableByteChannel in = null;
+        int result = 1;
 
-        private final OutputStream outStream;
-        private final File srcFile;
-        private final int mask;
+        try {
+            out = Channels.newChannel(outStream);
 
-        public CopyRoutine(OutputStream in, File srcFile, int mask) {
-            this.outStream = in;
-            this.srcFile = srcFile;
-            this.mask = mask;
-        }
+            final FileInputStream fis = new FileInputStream(srcFile);
+            in = Channels.newChannel(fis);
 
-        @Override
-        public void run() {
-            WritableByteChannel out = null;
-            ByteBuffer buffer = ByteBuffer.allocate(1024);
-            boolean interrupted = false;
-            ReadableByteChannel in = null;
-
-            try {
-                out = Channels.newChannel(outStream);
-                long filesize = srcFile.length();
-                int workUnitsLimit = 1;
-                double workUnitFactor = 1;
-                if (filesize > Integer.MAX_VALUE) {
-                    workUnitsLimit = Integer.MAX_VALUE;
-                    workUnitFactor = Integer.MAX_VALUE / filesize;
-                } else {
-                    workUnitsLimit = (int) filesize;
-                    workUnitFactor = 1;
+            while (true) {
+                if (in.read(buffer) <= 0) {
+                    break;
                 }
 
-//                String command = String.format("C0%03o %d %s\n", // NOI18N
-//                        mask, filesize, srcFile.getName());
-//                buffer.put(command.getBytes(), 0, command.length());
-
-                // send a content of srcFile
-                final FileInputStream fis = new FileInputStream(srcFile);
-                in = Channels.newChannel(fis);
-                long sendBytes = 0;
-                int progress = 0;
-
-                while (true) {
-                    int len = in.read(buffer);
-                    if (len <= 0) {
-                        break;
-                    }
-                    buffer.flip();
-                    try {
-                        out.write(buffer);
-                    } catch (Exception ex) {
-                        throw new InterruptedException();
-                    }
-                    buffer.clear();
-                    sendBytes += len;
-                    progress = (int) (sendBytes * workUnitFactor);
-                }
-
-                // Finally write 0 byte
-                buffer.clear();
-                buffer.put((byte) 0);
                 buffer.flip();
-                out.write(buffer);
-            } catch (InterruptedException ie) {
-                interrupted = true;
-            } catch (IOException ex) {
-            } finally {
-                if (in != null) {
-                    try {
-                        in.close();
-                    } catch (IOException ex) {
-                    }
+
+                try {
+                    out.write(buffer);
+                } catch (Exception ex) {
+                    throw new InterruptedException();
                 }
 
-                /* Here I have faced with the following problem:
-                 * If some error occured (permission denied, as an example),
-                 * an attempt to close out leads to JVM crash...
-                 * Interesting thing is that out is not null and even is not
-                 * closed (isOpen() returns true)...
-                 * So this was overcomed using interrupted flag ...
-                 *
-                 * TODO: I don't like this solution.
-                 */
-
-                if (!interrupted && out != null) {
-                    try {
-                        out.close();
-                    } catch (Exception ex) {
-                    }
-                }
-            }
-        }
-    }
-
-    private static class FilterInputProcessor implements InputProcessor {
-
-        private final InputProcessor delegate;
-        private boolean closed;
-
-        public FilterInputProcessor(InputProcessor delegate) {
-            this.delegate = delegate;
-        }
-
-        public void processInput(char[] chars) throws IOException {
-            if (closed) {
-                throw new IllegalStateException("Already closed processor"); // NOI18N
+                buffer.clear();
             }
 
-            StringBuilder sb = new StringBuilder(chars.length);
-            for (char c : chars) {
-                if (c >= 32 || c == 10 || c == 13) {
-                    sb.append(c);
+            result = 0;
+        } catch (InterruptedException ie) {
+            interrupted = true;
+        } catch (IOException ex) {
+        } finally {
+            if (in != null) {
+                try {
+                    in.close();
+                } catch (IOException ex) {
                 }
             }
 
-            delegate.processInput(sb.toString().toCharArray());
-        }
+            /* Here I have faced with the following problem:
+             * If some error occured (permission denied, as an example),
+             * an attempt to close out leads to JVM crash...
+             * Interesting thing is that out is not null and even is not
+             * closed (isOpen() returns true)...
+             * So this was overcomed using interrupted flag ...
+             *
+             * TODO: I don't like this solution.
+             */
 
-        public void reset() throws IOException {
-            if (closed) {
-                throw new IllegalStateException("Already closed processor"); // NOI18N
+            if (!interrupted && out != null) {
+                try {
+                    out.close();
+                } catch (Exception ex) {
+                }
             }
-            delegate.reset();
         }
 
-        public void close() throws IOException {
-            closed = true;
-            delegate.close();
-        }
+        return result;
     }
 }
