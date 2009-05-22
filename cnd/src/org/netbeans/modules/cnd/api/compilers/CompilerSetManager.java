6c387e382d696560c2d952893deafe6c9a95d502
Maria Tishkova <mromashova@netbeans.org>
2009-05-22 18:20:35 +0400

    fixed IZ#162856 IDE does not allow using Sun Studio Express on remote Linux box it is possible now to set several default_locations for the platform

--- cnd/src/org/netbeans/modules/cnd/api/compilers/CompilerSetManager.java
+++ cnd/src/org/netbeans/modules/cnd/api/compilers/CompilerSetManager.java
@@ -547,12 +547,16 @@ public class CompilerSetManager {
      */
     private ArrayList<String> appendDefaultLocations(int platform, ArrayList<String> dirlist) {
         for (ToolchainDescriptor d : ToolchainManager.getImpl().getToolchains(platform)) {
-            Map<String, String> map = d.getDefaultLocations();
+            Map<String, List<String>> map = d.getDefaultLocations();
             if (map != null) {
                 String pname = getPlatformName(platform);
-                String dir = map.get(pname);
-                if (dir != null && !dirlist.contains(dir)) {
-                    dirlist.add(dir);
+                List<String> list = map.get(pname);
+                if (list != null ) {
+                    for (String dir : list){
+                        if (!dirlist.contains(dir)){
+                            dirlist.add(dir);
+                        }
+                    }
                 }
             }
         }
