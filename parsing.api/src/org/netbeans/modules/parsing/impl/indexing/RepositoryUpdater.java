9f5b0469068ac127a928e2da8fa9e2e6d14e138b
Vita Stejskal <vstejskal@netbeans.org>
2009-05-26 14:19:50 +0200

    logging time spent by resolving classpaths dependencies

--- parsing.api/src/org/netbeans/modules/parsing/impl/indexing/RepositoryUpdater.java
+++ parsing.api/src/org/netbeans/modules/parsing/impl/indexing/RepositoryUpdater.java
@@ -1575,6 +1575,7 @@ public final class RepositoryUpdater implements PathRegistryListener, FileChange
             }
 
             updateProgress(NbBundle.getMessage(RepositoryUpdater.class, "MSG_ProjectDependencies")); //NOI18N
+            long tm1 = System.currentTimeMillis();
             if (depCtx == null) {
                 depCtx = new DependenciesContext(scannedRoots2Dependencies, scannedBinaries, useInitialState);
                 final List<URL> newRoots = new LinkedList<URL>();
@@ -1620,6 +1621,10 @@ public final class RepositoryUpdater implements PathRegistryListener, FileChange
                 depCtx.scannedRoots.clear();
                 depCtx.newBinariesToScan.removeAll(depCtx.scannedBinaries);
                 depCtx.scannedBinaries.clear();
+            }
+
+            if (LOGGER.isLoggable(Level.FINE)) {
+                LOGGER.log(Level.FINE, "Resolving dependencies took: {0} ms", System.currentTimeMillis() - tm1); //NOI18N
             }
 
             if (LOGGER.isLoggable(Level.FINE)) {
