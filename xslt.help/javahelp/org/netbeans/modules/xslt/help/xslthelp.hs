13ddfb616de1095776a7279bfce245f706525bfd
ifilippova <ifilippova@netbeans.org>
2009-06-02 20:54:15 +0700

    Fixing issue 166116

--- xslt.help/javahelp/org/netbeans/modules/xslt/help/xslthelp.hs
+++ xslt.help/javahelp/org/netbeans/modules/xslt/help/xslthelp.hs
@@ -1,6 +1,6 @@
 <?xml version='1.0' encoding='ISO-8859-1'?>
 <!--
-    *     Copyright © 2008 Sun Microsystems, Inc. All rights reserved.
+    *     Copyright (c) 2009 Sun Microsystems, Inc. All rights reserved.
     *     Use is subject to license terms.
 --> 
 <!DOCTYPE helpset PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN"
