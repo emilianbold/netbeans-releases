19d9986d1f671d2790c3f8de3571074891ba6639
Vince Kraemer <vkraemer@netbeans.org>
2009-06-02 16:15:53 -0700

    #165190: NPE constructing an error message

--- glassfish.common/src/org/netbeans/modules/glassfish/common/wizards/Retriever.java
+++ glassfish.common/src/org/netbeans/modules/glassfish/common/wizards/Retriever.java
@@ -214,9 +214,9 @@ public class Retriever implements Runnable {
             Logger.getLogger("glassfish").log(Level.FINE, "in == "+in);  //NOI18N
             setDownloadState(STATUS_FAILED, "I/O Exception", ex); // NOI18N
             updateMessage(
-                    in != null ?
+                    connection != null ?
                         NbBundle.getMessage(Retriever.class, "MSG_FileProblem", connection.getURL()) :
-                        NbBundle.getMessage(Retriever.class, "MSG_InvalidUrl", connection.getURL()));
+                        NbBundle.getMessage(Retriever.class, "MSG_InvalidUrl", targetUrl));
         } catch(RuntimeException ex) {
             Logger.getLogger("glassfish").log(Level.FINE, ex.getLocalizedMessage(), ex);  //NOI18N
             setDownloadState(STATUS_FAILED, "Runtime Exception", ex); // NOI18N
