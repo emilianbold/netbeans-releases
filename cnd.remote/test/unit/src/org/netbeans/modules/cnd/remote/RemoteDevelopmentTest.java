f8c8ff5519dc7391e1e866b8c2e7e5ec4f66d1b2
Vladimir Kvashin <vkvashin@netbeans.org>
2009-06-17 18:56:18 +0400

    Tests: moved getting mspecs from .cndtestrc section and trasforming them to environments to the common ancestor

--- cnd.remote/test/unit/src/org/netbeans/modules/cnd/remote/RemoteDevelopmentTest.java
+++ cnd.remote/test/unit/src/org/netbeans/modules/cnd/remote/RemoteDevelopmentTest.java
@@ -39,8 +39,6 @@
 
 package org.netbeans.modules.cnd.remote;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 import org.netbeans.modules.cnd.remote.mapper.MappingsTestCase;
@@ -50,9 +48,6 @@ import org.netbeans.modules.cnd.remote.support.TransportTestCase;
 import org.netbeans.modules.cnd.remote.sync.ScpSyncWorkerTestCase;
 import org.netbeans.modules.cnd.remote.ui.wizard.HostSetupTestCase;
 import org.netbeans.modules.cnd.test.CndBaseTestSuite;
-import org.netbeans.modules.nativeexecution.test.NativeExecutionTestSupport;
-import org.netbeans.modules.nativeexecution.test.RcFile;
-import org.netbeans.modules.nativeexecution.test.RcFile.FormatException;
 
 /**
  *
@@ -82,23 +77,7 @@ public class RemoteDevelopmentTest extends CndBaseTestSuite {
 
 
     private RemoteDevelopmentTest(String name, Class... testClasses) {
-        super(name);
-        try {
-            for (Class testClass : testClasses) {
-                try {
-                    RcFile rcFile = NativeExecutionTestSupport.getRcFile();
-                    for (String platform : rcFile.getKeys("remote.platforms")) {
-                        addTest(testClass, platform);
-                    }
-                } catch (FileNotFoundException ex) {
-                    // rcfile does not exists - no tests to run
-                }
-            }
-        } catch (IOException ex) {
-            addTest(warning("Cannot get execution environment: " + exceptionToString(ex)));
-        } catch (FormatException ex) {
-            addTest(warning("Cannot get execution environment: " + exceptionToString(ex)));
-        }
+        super(name, "remote.platforms", testClasses);
     }
 
     public static Test suite() {
