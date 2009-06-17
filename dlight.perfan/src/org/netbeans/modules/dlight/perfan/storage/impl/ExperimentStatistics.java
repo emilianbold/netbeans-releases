a40b8ccd8584c03ad20c498f66357befd6caf86e
Maria Tishkova <mromashova@netbeans.org>
2009-06-17 12:17:53 +0400

    no IZ: fixed NumberFormatException java.lang.NumberFormatException: For input string: "0.0%" 	at sun.misc.FloatingDecimal.readJavaFormatString(FloatingDecimal.java:1224) 	at java.lang.Double.valueOf(Double.java:447) 	at org.netbeans.modules.dlight.perfan.storage.impl.ExperimentStatistics.parseDouble(ExperimentStatistics.java:150)

--- dlight.perfan/src/org/netbeans/modules/dlight/perfan/storage/impl/ExperimentStatistics.java
+++ dlight.perfan/src/org/netbeans/modules/dlight/perfan/storage/impl/ExperimentStatistics.java
@@ -90,7 +90,9 @@ public final class ExperimentStatistics {
                 if (id.startsWith("User Lock")) { // NOI18N
                     StringTokenizer t = tokenize(s.substring(scidx + 1));
                     _t_usrLock = parseDouble(t.nextToken());
-                    _t_usrLock_p = parseDouble(t.nextToken());
+                    String str_t_usrLock_p = t.nextToken();
+                    str_t_usrLock_p = str_t_usrLock_p.trim().endsWith("%")  ? str_t_usrLock_p.substring(0, str_t_usrLock_p.length() -1) : str_t_usrLock_p;
+                    _t_usrLock_p = parseDouble(str_t_usrLock_p);
                 } else if (id.startsWith("Total Thread Time") || id.startsWith("Total LWP Time")) { // NOI18N
                     _totalThreadTime = parseDouble(s.substring(scidx + 1));
                 } else if (id.startsWith("Duration")) { // NOI18N
