fd1b8af6c9cb31d639ab81977bc3b213b1691699
Andrey Yamkovoy <kaktus@netbeans.org>
2009-06-24 18:26:10 +0400

    Minor changes.

--- j2ee.ejbcore/src/org/netbeans/modules/j2ee/ejbcore/ejb/wizard/session/SessionEJBWizard.java
+++ j2ee.ejbcore/src/org/netbeans/modules/j2ee/ejbcore/ejb/wizard/session/SessionEJBWizard.java
@@ -101,7 +101,7 @@ public final class SessionEJBWizard implements WizardDescriptor.InstantiatingIte
         EjbJar ejbModule = EjbJar.getEjbJar(pkg);
         // TODO: UI - add checkbox for Java EE 5 to create also EJB 2.1 style EJBs
         Profile profile = Profile.fromPropertiesString(ejbModule.getJ2eePlatformVersion());
-        boolean isSimplified = profile.equals(Profile.JAVA_EE_5) || profile.equals(Profile.JAVA_EE_6_FULL);
+        boolean isSimplified = profile.equals(Profile.JAVA_EE_5) || profile.equals(Profile.JAVA_EE_6_FULL) || profile.equals(Profile.JAVA_EE_6_WEB);
         SessionGenerator sessionGenerator = SessionGenerator.create(
                 Templates.getTargetName(wiz), 
                 pkg, 
