4aa5dd795a3918000ee398a3daa2b084430eff8f
pslechta <pslechta@netbeans.org>
2009-06-25 10:15:31 +0200

    web.xml wizard + refactoring

--- web.project/src/org/netbeans/modules/web/project/ui/customizer/WebProjectProperties.java
+++ web.project/src/org/netbeans/modules/web/project/ui/customizer/WebProjectProperties.java
@@ -77,6 +77,7 @@ import org.netbeans.api.project.Project;
 import org.netbeans.api.project.ProjectManager;
 import org.netbeans.api.queries.FileEncodingQuery;
 import org.netbeans.modules.j2ee.common.SharabilityUtility;
+import org.netbeans.modules.j2ee.common.dd.DDHelper;
 import org.netbeans.modules.java.api.common.project.ui.ClassPathUiSupport;
 import org.netbeans.modules.j2ee.common.project.ui.DeployOnSaveUtils;
 import org.netbeans.modules.j2ee.common.project.ui.J2eePlatformUiSupport;
@@ -102,7 +103,6 @@ import org.netbeans.spi.project.support.ant.PropertyUtils;
 import org.netbeans.spi.project.support.ant.ReferenceHelper;
 import org.netbeans.modules.web.project.WebProject;
 import org.netbeans.modules.web.project.WebProjectType;
-import org.netbeans.modules.web.project.api.WebProjectUtilities;
 import org.netbeans.modules.web.project.classpath.ClassPathSupportCallbackImpl;
 
 import org.netbeans.modules.web.spi.webmodule.WebFrameworkProvider;
@@ -458,7 +458,7 @@ final public class WebProjectProperties {
             
             Profile j2eeProfile = Profile.fromPropertiesString(project.getAPIWebModule().getJ2eePlatformVersion());
             if (shouldCreateWebXml()) {
-                WebProjectUtilities.createWebXml(j2eeProfile, true, project.getAPIWebModule().getWebInf());
+                DDHelper.createWebXml(j2eeProfile, true, project.getAPIWebModule().getWebInf());
             }
 
             // extend project with selected frameworks
