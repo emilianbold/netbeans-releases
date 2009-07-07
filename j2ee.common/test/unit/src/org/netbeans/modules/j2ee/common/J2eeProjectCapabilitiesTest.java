dded049e8dcf10d8eb14eca264c3c6579068c531
David Konecny <dkonecny@netbeans.org>
2009-07-08 09:55:11 +1200

    tweaking according to Andrey's comments

--- j2ee.common/test/unit/src/org/netbeans/modules/j2ee/common/J2eeProjectCapabilitiesTest.java
+++ j2ee.common/test/unit/src/org/netbeans/modules/j2ee/common/J2eeProjectCapabilitiesTest.java
@@ -74,26 +74,31 @@ public class J2eeProjectCapabilitiesTest extends NbTestCase {
         assertTrue(cap.isEjb30Supported());
         assertFalse(cap.isEjb31Supported());
         assertFalse(cap.isEjb31LiteSupported());
+
         p = createProject(Profile.JAVA_EE_6_FULL, Type.EJB);
         cap = J2eeProjectCapabilities.forProject(p);
         assertTrue(cap.isEjb30Supported());
         assertTrue(cap.isEjb31Supported());
         assertTrue(cap.isEjb31LiteSupported());
+
         p = createProject(Profile.JAVA_EE_6_WEB, Type.EJB);
         cap = J2eeProjectCapabilities.forProject(p);
         assertFalse(cap.isEjb30Supported());
         assertFalse(cap.isEjb31Supported());
-        assertTrue(cap.isEjb31LiteSupported());
+        assertFalse(cap.isEjb31LiteSupported());
+
         p = createProject(Profile.JAVA_EE_6_WEB, Type.WAR);
         cap = J2eeProjectCapabilities.forProject(p);
         assertFalse(cap.isEjb30Supported());
         assertFalse(cap.isEjb31Supported());
         assertTrue(cap.isEjb31LiteSupported());
+
         p = createProject(Profile.JAVA_EE_6_FULL, Type.WAR);
         cap = J2eeProjectCapabilities.forProject(p);
         assertFalse(cap.isEjb30Supported());
-        assertFalse(cap.isEjb31Supported());
+        assertTrue(cap.isEjb31Supported());
         assertTrue(cap.isEjb31LiteSupported());
+
         p = createProject(Profile.JAVA_EE_5, Type.WAR);
         cap = J2eeProjectCapabilities.forProject(p);
         assertFalse(cap.isEjb30Supported());
