b48cd2ceed6fa43e6fc8d4186b58f215f6faf8ef
Thomas Preisler <thp@netbeans.org>
2009-07-20 14:07:23 -0700

    adding sunstudio tests

--- cnd.makeproject/test/unit/src/org/netbeans/modules/cnd/makeproject/ui/wizards/MakeSampleProjectIteratorTest.java
+++ cnd.makeproject/test/unit/src/org/netbeans/modules/cnd/makeproject/ui/wizards/MakeSampleProjectIteratorTest.java
@@ -179,6 +179,9 @@ public class MakeSampleProjectIteratorTest extends CndBaseTestCase {
 
     @Test
     public void testHelloQtWorld() throws IOException {
+        if (Utilities.getOperatingSystem() == Utilities.OS_SOLARIS) {
+            testSample(SunStudioCompilerSet, "HelloQtWorld", defaultConfs);
+        }
         if (Utilities.getOperatingSystem() == Utilities.OS_LINUX) {
             testSample(GNUCompilerSet, "HelloQtWorld", defaultConfs);
         }
