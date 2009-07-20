afaf3677c48aca85a1d7561796ad3e84fdcb41b7
Tomas Mysik <tmysik@netbeans.org>
2009-07-20 10:49:24 +0200

    Related to #167519: PHPUnit tests not executed if filename doesn't match class name
    PhpDoc information improved.

--- php.project/src/org/netbeans/modules/php/project/ui/resources/PHPUnitSuite.php
+++ php.project/src/org/netbeans/modules/php/project/ui/resources/PHPUnitSuite.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * NetBeans PHPUnit test-suite.
+ *
+ * @package NetBeans
+ */
+
 <#assign licenseFirst = "/* ">
 <#assign licensePrefix = " * ">
 <#assign licenseLast = " */">
@@ -19,9 +25,23 @@
  * added and executed.
  *
  * To run this suite from CLI: phpunit NetBeansSuite.php
+ *
+ * @package NetBeans
  */
 class NetBeansSuite extends PHPUnit_Framework_TestSuite {
 
+    /**
+     * Suite factory.
+     *
+     * This function creates a PHPUnit test-suite,
+     * scans the directory for test-cases,
+     * adds all test-cases found and then returns
+     * a test-suite containing all available tests.
+     *
+     * @access public
+     * @static
+     * @return NetBeansSuite
+     */
     public static function suite() {
         $suite = new NetBeansSuite();
         foreach (self::rglob("*{T,t}est.php", dirname(__FILE__).DIRECTORY_SEPARATOR, GLOB_BRACE) as $file) {
@@ -31,12 +51,18 @@ class NetBeansSuite extends PHPUnit_Framework_TestSuite {
     }
 
     /**
-     * Recursive <a href="http://php.net/manual/en/function.glob.php">glob()</a>.
+     * Recursive {@link http://php.net/manual/en/function.glob.php glob()}.
+     *
+     * @access private
+     * @static
+     *
      * @param  string $pattern the pattern passed to glob(), default is "*"
      * @param  string $path    the path to scan, default is
-     *                         <a href="http://php.net/manual/en/function.getcwd.php">the current working directory</a>
+     *                         {@link http://php.net/manual/en/function.getcwd.php the current working directory}
      * @param  int    $flags   the flags passed to glob()
      * @return array  an array of files in the given path matching the pattern.
+     * @link http://php.net/manual/en/function.glob.php
+     * @link http://php.net/manual/en/function.getcwd.php
      */
     private static function rglob($pattern = '*', $path = '', $flags = 0) {
         $paths = glob($path.'*', GLOB_MARK | GLOB_ONLYDIR | GLOB_NOSORT) or array();
