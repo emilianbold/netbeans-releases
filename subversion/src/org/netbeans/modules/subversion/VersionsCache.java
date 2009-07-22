d8657826ecebdb21ef4960804fa29a7aa235f92e
Marian Petras <mpetras@netbeans.org>
2009-07-22 12:21:22 +0200

    no semantic change - resolved editor warnings about missing curly braces in if-else statements

--- subversion/src/org/netbeans/modules/subversion/VersionsCache.java
+++ subversion/src/org/netbeans/modules/subversion/VersionsCache.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2009 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -24,7 +24,7 @@
  * Contributor(s):
  *
  * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2009 Sun
  * Microsystems, Inc. All Rights Reserved.
  *
  * If you wish your version of this file to be governed by only the CDDL
@@ -132,9 +132,13 @@ public class VersionsCache {
         if (Setup.REVISION_BASE.equals(revision)) {
             try {
                 File svnDir = getMetadataDir(base.getParentFile());
-                if (svnDir == null) return null;
+                if (svnDir == null) {
+                    return null;
+                }
                 File svnBase = new File(svnDir, "text-base/" + base.getName() + ".svn-base");
-                if (!svnBase.exists()) return null;
+                if (!svnBase.exists()) {
+                    return null;
+                }
                 File expanded = new File(svnDir, "text-base/" + base.getName() + ".netbeans-base");
                 if (expanded.canRead() && svnBase.isFile() && expanded.lastModified() >= svnBase.lastModified()) {
                     return expanded;
