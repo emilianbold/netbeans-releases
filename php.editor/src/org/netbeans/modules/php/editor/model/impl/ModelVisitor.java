e11c91ba0fa1d2cc1ef3a2553a8e3b342f219eda
Radek Matous <rmatous@netbeans.org>
2009-07-23 09:39:50 +0200

    printing stripped

--- php.editor/src/org/netbeans/modules/php/editor/model/impl/ModelVisitor.java
+++ php.editor/src/org/netbeans/modules/php/editor/model/impl/ModelVisitor.java
@@ -819,9 +819,7 @@ public final class ModelVisitor extends DefaultTreePathVisitor {
             assert occ != null;
             if (occ.getDeclaration().equals(element)) {
                 retval = occ;
-            } else {
-                System.out.println(occ.getDeclaration().equals(element));
-            }
+            } 
         }
         return retval;
     }
