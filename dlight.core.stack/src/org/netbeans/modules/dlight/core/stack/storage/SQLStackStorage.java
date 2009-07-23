c575d1b7790ccd2061a255b5f341a210f9847c2f
Alexander Simon <alexvsimon@netbeans.org>
2009-07-23 16:37:51 +0400

    do not use l for long constants

--- dlight.core.stack/src/org/netbeans/modules/dlight/core/stack/storage/SQLStackStorage.java
+++ dlight.core.stack/src/org/netbeans/modules/dlight/core/stack/storage/SQLStackStorage.java
@@ -563,7 +563,7 @@ public final class SQLStackStorage {
     private class ExecutorThread extends Thread {
 
         private static final int MAX_COMMANDS = 1000;
-        private static final long SLEEP_INTERVAL = 200l;
+        private static final long SLEEP_INTERVAL = 200L;
         private final LinkedBlockingQueue<Object> queue;
 
         public ExecutorThread() {
