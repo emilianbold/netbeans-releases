12b9d839dea52d783cd4b98d66216e253ab90ec9
Tomas Holy <t_h@netbeans.org>
2009-08-04 09:06:33 +0200

    #168898: Support multiple colors/hyperlinks on single line in OW

--- core.output2/src/org/netbeans/core/output2/OutputPane.java
+++ core.output2/src/org/netbeans/core/output2/OutputPane.java
@@ -41,10 +41,7 @@
 package org.netbeans.core.output2;
 
 import java.awt.event.MouseEvent;
-import javax.swing.text.BadLocationException;
-import javax.swing.text.Segment;
 import org.netbeans.core.output2.ui.AbstractOutputPane;
-
 import javax.swing.*;
 import javax.swing.text.Document;
 import javax.swing.text.PlainDocument;
@@ -68,24 +65,14 @@ class OutputPane extends AbstractOutputPane {
         findOutputTab().documentChanged(this);
     }
 
-    protected void caretEnteredLine(int line) {
-        findOutputTab().caretEnteredLine(line);
+    protected void caretPosChanged(int pos) {
+        findOutputTab().caretPosChanged(pos);
     }
 
-    protected void lineClicked(int line, Point p) {
-        if (!(getDocument() instanceof OutputDocument) || !inLeadingOrTrailingWhitespace(line, p)) {
-            findOutputTab().lineClicked(line);
+    protected void lineClicked(int line, int pos) {
+        if ((getDocument() instanceof OutputDocument)) {
+            findOutputTab().lineClicked(line, pos);
         }
-    }
-    
-    private boolean linePressed (int line, Point p) {
-        boolean result;
-        if (!(getDocument() instanceof OutputDocument) || !inLeadingOrTrailingWhitespace(line, p)) {
-            result = findOutputTab().linePressed (line, p);
-        } else {
-            result = false;
-        }
-        return result;
     }
 
     protected void postPopupMenu(Point p, Component src) {
@@ -93,72 +80,6 @@ class OutputPane extends AbstractOutputPane {
             findOutputTab().postPopupMenu(p, src);
         }
     }
-
-    @Override
-    public void setMouseLine (int line, Point p) {
-        Document doc = getDocument();
-        if (doc instanceof OutputDocument) {
-            boolean link = line != -1 && ((OutputDocument) doc).getLines().isHyperlink(line);
-            if (link && p != null) {
-                //#47256 - Don't set the cursor if the mouse if over
-                //whitespace
-                if (inLeadingOrTrailingWhitespace(line, p)) {
-                    link = false;
-                    line = -1;
-                }
-            }
-            textView.setCursor(link ? Cursor.getPredefinedCursor(Cursor.HAND_CURSOR) :
-                    Cursor.getPredefinedCursor(Cursor.TEXT_CURSOR));
-        }
-        super.setMouseLine(line, p);
-    }
-    
-    private boolean inLeadingOrTrailingWhitespace (int line, Point p) {
-        if (line == -1) {
-            return true;
-        }
-        assert getDocument() instanceof OutputDocument;
-        assert p != null;
-        OutputDocument doc = (OutputDocument) getDocument();
-        int lineStart = doc.getLineStart(line);
-        int lineEnd = doc.getLineEnd(line);
-
-        try {
-            doc.getText (lineStart, lineEnd - lineStart, seg);
-            char curr = seg.first();
-            while (Character.isWhitespace(curr) && curr != Segment.DONE) {
-                lineStart++;
-                curr = seg.next();
-            }
-            curr = seg.last();
-            while (Character.isWhitespace(curr) && curr != Segment.DONE) {
-                lineEnd--;
-                curr = seg.previous();
-            }
-            if (lineEnd <= lineStart) {
-                line = -1;
-            } else {
-                Rectangle startRect = textView.modelToView(lineStart);
-                Rectangle endRect = textView.modelToView(lineEnd);
-                if (p.y >= startRect.y && p.y <= endRect.y && isWrapped()) {
-                    endRect.x = 0;
-                    endRect.width = getWidth();
-                }
-                boolean cursorIsNotOverLeadingOrTrailingWhitespace = 
-                    p.x >= startRect.x && p.y >= startRect.y &&
-                    p.x <= endRect.x + endRect.width &&
-                    p.y <= endRect.y + endRect.height;
-                if (!cursorIsNotOverLeadingOrTrailingWhitespace) {
-                    line = -1;
-                }
-            }
-        } catch (BadLocationException e) {
-            //do nothing
-        }
-        return line == -1;
-    }
-    
-    private Segment seg = new Segment();
     
     /**
      * Only calls super if there are hyperlinks in the document to avoid huge
@@ -169,7 +90,7 @@ class OutputPane extends AbstractOutputPane {
     public void mouseMoved (MouseEvent evt) {
         Document doc = getDocument();
         if (doc instanceof OutputDocument) {
-            if (((OutputDocument) doc).getLines().hasHyperlinks()) {
+            if (((OutputDocument) doc).getLines().hasListeners()) {
                 super.mouseMoved(evt);
             }
         }
@@ -178,17 +99,6 @@ class OutputPane extends AbstractOutputPane {
     @Override
     public void mousePressed(MouseEvent e) {
         super.mousePressed(e);
-        if (e.getSource() == textView && SwingUtilities.isLeftMouseButton(e)) {
-            int pos = textView.viewToModel(e.getPoint());
-            if (pos != -1) {
-                int line = textView.getDocument().getDefaultRootElement().getElementIndex(pos);
-                if (line >= 0) {
-                    if (linePressed(line, e.getPoint())) {
-                        return;
-                    }
-                }
-            }
-        }
         //Refine possibly to focus just what is important..
         findOutputTab().setToFocus((Component)e.getSource());
         findOutputTab().requestActive();
