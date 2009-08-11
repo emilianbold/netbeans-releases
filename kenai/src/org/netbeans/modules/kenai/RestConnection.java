4fe76fa6202434a4fa6fac365d98a19cbe711708
Jan Becicka <jbecicka@netbeans.org>
2009-08-11 14:23:02 +0200

    improved error detection

--- kenai/src/org/netbeans/modules/kenai/RestConnection.java
+++ kenai/src/org/netbeans/modules/kenai/RestConnection.java
@@ -243,7 +243,7 @@ public class RestConnection {
             }
             return response;
         } catch (Exception e) {
-            String errMsg = "Cannot connect to : " + conn.getURL().getHost();
+            String errMsg = "Cannot connect to : " + conn.getURL().getHost() + ", " + e.getMessage();
             try {
                 BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
                 String line;
