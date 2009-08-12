c2e42df0b7a059b9f3f5b170357a05d102591bc4
Maria Tishkova <mromashova@netbeans.org>
2009-08-12 20:54:44 +0400

    IZ#168146 Tree representation for races, stacks are displayed using Stack UI

--- dlight.tha/src/org/netbeans/modules/dlight/tha/THANodeFactory.java
+++ dlight.tha/src/org/netbeans/modules/dlight/tha/THANodeFactory.java
@@ -37,77 +37,14 @@
  * Portions Copyrighted 2009 Sun Microsystems, Inc.
  */
 
-package org.netbeans.modules.dlight.core.stack.ui;
-
-import java.awt.Component;
-import java.awt.Graphics;
-import java.awt.Image;
-import java.util.ArrayList;
-import java.util.List;
-import javax.swing.Icon;
-import org.openide.nodes.AbstractNode;
-import org.openide.nodes.Children;
-import org.openide.util.ImageUtilities;
+package org.netbeans.modules.dlight.tha;
 
 /**
  *
  * @author mt154047
  */
-final class MultipleCallStackRootNode extends AbstractNode{
-    private final List<StackRootNode> children = new ArrayList<StackRootNode>();
-    private final Image  icon = ImageUtilities.icon2Image(new MyIcon());
-
-    MultipleCallStackRootNode() {
-        super(Children.LEAF);
-        setDisplayName("Root");//NOI18N
-    }
-
-
-
-    void add(StackRootNode node){
-        children.add(node);
-        setChildren(Children.LEAF);
-        setChildren(new MultipleCallStackRootChildren(children));
-    }
-
-    void removeAll(){
-        children.clear();
-        setChildren(Children.LEAF);
-    }
-
-//    @Override
-//    public Image getIcon(int type) {
-//        return icon;
-//    }
-//
-//    @Override
-//    public Image getOpenedIcon(int type) {
-//        return getIcon(type);
-//    }
-
-
-
-//    @Override
-//    public String getHtmlDisplayName() {
-//        return "<h2>" + getDisplayName() + "</h2>"; // NOI18N
-//    }
+public interface THANodeFactory<T> {
+    THANode<T> create(T object);
 
     
-
-    class MyIcon implements Icon{
-
-        public void paintIcon(Component c, Graphics g, int x, int y) {
-            
-        }
-
-        public int getIconWidth() {
-            return 10;
-        }
-
-        public int getIconHeight() {
-            return 10;
-        }
-        
-    }
-
 }
