0340bdc9b73eb419aeebe1a5b160a19aa25bd153
Tomas Stupka <tstupka@netbeans.org>
2009-08-14 14:29:10 +0200

    reverting af71688d8080 - chat should be always available

--- subversion/src/org/netbeans/modules/subversion/ui/blame/AnnotationBar.java
+++ subversion/src/org/netbeans/modules/subversion/ui/blame/AnnotationBar.java
@@ -501,7 +501,6 @@ final class AnnotationBar extends JComponent implements Accessible, PropertyChan
                         ku.startChat();
                     }
                 });
-                chatMenu.setEnabled(ku.isOnline());
                 popupMenu.add(chatMenu);
             }
         }
