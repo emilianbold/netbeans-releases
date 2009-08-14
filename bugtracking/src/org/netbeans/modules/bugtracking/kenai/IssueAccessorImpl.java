4906226a51cdb6ea858299d9037fce0087978fbe
joshis <joshis@netbeans.org>
2009-08-14 11:03:22 +0200

    Used issue::open(...) in IssueAccessorImpl

--- bugtracking/src/org/netbeans/modules/bugtracking/kenai/IssueAccessorImpl.java
+++ bugtracking/src/org/netbeans/modules/bugtracking/kenai/IssueAccessorImpl.java
@@ -76,63 +76,11 @@ public class IssueAccessorImpl extends KenaiIssueAccessor {
             public void run() {
                 final Repository repo = KenaiRepositories.getInstance().getRepository(project);
                 handle.finish();
-                final ProgressHandle[] handle = new ProgressHandle[1];
-                handle[0] = ProgressHandleFactory.createHandle(NbBundle.getMessage(Issue.class, "LBL_OPENING_ISSUE", new Object[]{issueID}));
-                handle[0].start();
-                SwingUtilities.invokeLater(new Runnable() {
-
-                    public void run() {
-                        try {
-                            final IssueTopComponent tc = IssueTopComponent.find(issueID);
-                            final Issue issue = tc.getIssue();
-                            if (issue == null) {
-                                tc.initNoIssue();
-                            }
-                            tc.open();
-                            tc.requestActive();
-
-                            BugtrackingManager.getInstance().getRequestProcessor().post(new Runnable() {
-
-                                public void run() {
-                                    try {
-                                        if (issue != null) {
-                                            handle[0].finish();
-                                            handle[0] = ProgressHandleFactory.createHandle(NbBundle.getMessage(Issue.class, "LBL_REFRESING_ISSUE", new Object[]{issueID}));
-                                            handle[0].start();
-                                            issue.refresh();
-                                        } else {
-                                            final Issue refIssue = repo.getIssue(issueID);
-                                            if (refIssue == null) { // probably no issue exists with the issueID
-                                                SwingUtilities.invokeLater(new Runnable() {
-
-                                                    public void run() {
-                                                        tc.close();
-                                                    }
-                                                });
-                                                return;
-                                            }
-                                            SwingUtilities.invokeLater(new Runnable() {
-
-                                                public void run() {
-                                                    tc.setIssue(refIssue);
-                                                }
-                                            });
-                                            try {
-                                                refIssue.setSeen(true);
-                                            } catch (IOException ex) {
-                                                BugtrackingManager.LOG.log(Level.SEVERE, null, ex);
-                                            }
-                                        }
-                                    } finally {
-                                        handle[0].finish();
-                                    }
-                                }
-                            });
-                        } catch (NullPointerException e) {
-                            handle[0].finish();
-                        }
-                    }
-                });
+                try {
+                    Issue.open(repo, issueID);
+                } catch (NullPointerException e) {
+                    //
+                }
             }
         });
     }
