dd3323c6cac8579e714ada4f66a2f018bbcb9dec
Dafe Simonek <dsimonek@netbeans.org>
2009-09-01 14:48:02 +0200

    added generation of scm section, issue mng section, mailing lists section

--- kenai.maven/src/org/netbeans/modules/kenai/maven/KenaiUpdateAction.java
+++ kenai.maven/src/org/netbeans/modules/kenai/maven/KenaiUpdateAction.java
@@ -39,14 +39,17 @@
 package org.netbeans.modules.kenai.maven;
 
 import java.awt.event.ActionEvent;
+import java.util.List;
 import java.util.regex.Pattern;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
 import org.netbeans.modules.kenai.api.KenaiException;
 import org.netbeans.modules.kenai.api.KenaiFeature;
 import org.netbeans.modules.kenai.api.KenaiProject;
-import org.netbeans.modules.kenai.api.KenaiService.Type;
+import org.netbeans.modules.kenai.api.KenaiService;
 import org.netbeans.modules.maven.model.Utilities;
+import org.netbeans.modules.maven.model.pom.IssueManagement;
+import org.netbeans.modules.maven.model.pom.MailingList;
 import org.netbeans.modules.maven.model.pom.POMComponentFactory;
 import org.netbeans.modules.maven.model.pom.POMModel;
 import org.netbeans.modules.maven.model.pom.POMModelFactory;
@@ -144,6 +147,7 @@ public final class KenaiUpdateAction extends AbstractAction implements ContextAw
 
             mProj.setName(kProj.getDisplayName());
             mProj.setDescription(kProj.getDescription());
+            mProj.setURL(kProj.getWebLocation().toExternalForm());
 
             KenaiFeature[] kfs = kProj.getFeatures();
 
@@ -157,15 +161,113 @@ public final class KenaiUpdateAction extends AbstractAction implements ContextAw
                             scm = factory.createScm();
                             mProj.setScm(scm);
                         }
-                        scm.setUrl(kf.getLocation());
+                        updateScm(scm, kf);
+                        break;
+
+                    case ISSUES:
+                        IssueManagement iMng = mProj.getIssueManagement();
+                        if (iMng == null) {
+                            iMng = factory.createIssueManagement();
+                            mProj.setIssueManagement(iMng);
+                        }
+                        updateIssueMng(iMng, kf);
+                        break;
+
+                    case LISTS:
+                        updateLists(mProj, kf, factory, kProj);
+                        break;
+
                 }
             }
-
-
 
         } finally {
             model.endTransaction();
         }
+    }
+
+    private static final String POM_SCM = "scm";
+    private static final String POM_DELIM = ":";
+    private static final String POM_HG = "hg";
+    private static final String POM_SVN = "svn";
+    private static final String POM_EXTERNAL = "???";
+
+    private static void updateScm (Scm scm, KenaiFeature kf) {
+        StringBuilder conn = new StringBuilder();
+        conn.append(POM_SCM);
+        conn.append(POM_DELIM);
+
+        String scmName = kf.getName();
+        if (KenaiService.Names.SUBVERSION.equals(scmName)) {
+            conn.append(POM_SVN);
+        } else if (KenaiService.Names.MERCURIAL.equals(scmName)) {
+            conn.append(POM_HG);
+        } else if (KenaiService.Names.EXTERNAL_REPOSITORY.equals(scmName)) {
+            conn.append(POM_EXTERNAL);
+        }
+        conn.append(POM_DELIM);
+
+        conn.append(kf.getLocation());
+
+        String strConn = conn.toString();
+        scm.setConnection(strConn);
+        scm.setDeveloperConnection(strConn);
+        scm.setUrl(kf.getWebLocation().toExternalForm());
+    }
+
+    private static void updateIssueMng(IssueManagement iMng, KenaiFeature kf) {
+        iMng.setSystem(kf.getService());
+        iMng.setUrl(kf.getLocation());
+    }
+
+    private static final String KENAI_SITE = ".kenai.com";
+    private static final String KENAI_SYMPA = "sympa@";
+    private static final String KENAI_SUBSCRIBE = "?subject=sub%20";
+    private static final String KENAI_UNSUBSCRIBE = "?subject=unsub%20";
+
+    private static void updateLists(Project mProj, KenaiFeature kf, POMComponentFactory factory,
+            KenaiProject kProj) {
+        // does mailing list entry already exist in POM?
+        List<MailingList> mLists = mProj.getMailingLists();
+        MailingList mList = null;
+        if (mLists != null) {
+            for (MailingList ml : mLists) {
+                if (ml.getName() != null && ml.getName().equals(kf.getName())) {
+                    mList = ml;
+                    break;
+                }
+            }
+        }
+
+        if (mList == null) {
+            // no entry in POM, create new one
+            mList = factory.createMailingList();
+            mProj.addMailingList(mList);
+        }
+
+        mList.setName(kf.getName());
+        mList.setArchive(kf.getWebLocation().toExternalForm());
+
+        // post
+        StringBuilder sb = new StringBuilder();
+        sb.append(kf.getName());
+        sb.append('@');
+        sb.append(kProj.getName());
+        sb.append(KENAI_SITE);
+        mList.setPost(sb.toString());
+
+        // subscribe, unsubscribe
+        sb.delete(0, sb.length());
+        sb.append(KENAI_SYMPA);
+        sb.append(kProj.getName());
+        sb.append(KENAI_SITE);
+        StringBuilder sb2 = new StringBuilder();
+        sb2.append(sb);
+        sb.append(KENAI_SUBSCRIBE);
+        sb.append(kf.getName());
+        sb2.append(KENAI_UNSUBSCRIBE);
+        sb2.append(kf.getName());
+        mList.setSubscribe(sb.toString());
+        mList.setUnsubscribe(sb2.toString());
     }
 
 }
