14c65eb75d170a535245731e196b27662a979a8d
Jiri Rechtacek <jrechtacek@netbeans.org>
2009-09-02 16:07:39 +0200

    #170679: java.io.FileNotFoundException

--- derby/src/org/netbeans/modules/derby/RegisterDerby.java
+++ derby/src/org/netbeans/modules/derby/RegisterDerby.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2009 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -24,7 +24,7 @@
  * Contributor(s):
  *
  * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2009 Sun
  * Microsystems, Inc. All Rights Reserved.
  *
  * If you wish your version of this file to be governed by only the CDDL
@@ -216,7 +216,7 @@ public class RegisterDerby implements DatabaseRuntime {
             fileos = new FileOutputStream(derbyProperties);
             derbyProps.store(fileos, NbBundle.getMessage(RegisterDerby.class, "MSG_DerbyPropsFile"));
         } catch (IOException ex) {
-            LOGGER.log(Level.WARNING, null, ex);
+            LOGGER.log(Level.INFO, ex.getLocalizedMessage() + " while createDerbyPropertiesFile into " + derbyProps, ex);
         } finally {
             if (fileos != null) {
                 try {
