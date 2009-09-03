e6d8c40446572fa772787a5862373a033cc903e8
joshis <joshis@netbeans.org>
2009-09-03 16:15:16 +0200

    #169033 - Minor fixed in bundles and UI

--- kenai.ui/src/org/netbeans/modules/kenai/ui/project/kenaiProjectTopComponent.java
+++ kenai.ui/src/org/netbeans/modules/kenai/ui/project/kenaiProjectTopComponent.java
@@ -542,7 +542,7 @@ public final class kenaiProjectTopComponent extends TopComponent implements Prop
 
     private void addSpecificContent() {
         dynamicContentPane.add(NbBundle.getMessage(kenaiProjectTopComponent.class, "MSG_COMMUNICATE"), new ForumsAndMailingListsPanel()); //NOI18N
-        dynamicContentPane.add(NbBundle.getMessage(kenaiProjectTopComponent.class, "MSG_TEST"), new IssuesInformationPanel()); //NOI18N
+        dynamicContentPane.add(NbBundle.getMessage(kenaiProjectTopComponent.class, "MSG_TEST"), new IssuesInformationPanel(instProj)); //NOI18N
         dynamicContentPane.add(NbBundle.getMessage(kenaiProjectTopComponent.class, "MSG_DEVELOP"), new SourcesInformationPanel(mainScrollPane.getVerticalScrollBar())); //NOI18N
     }
 
