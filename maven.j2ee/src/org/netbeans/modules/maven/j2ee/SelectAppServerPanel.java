d43295fa5b141c99aaab28e87e4e68b6dbcf7519
Dafe Simonek <dsimonek@netbeans.org>
2009-09-03 16:47:25 +0200

    allows user to select a module where to store EE server selection - initial version

--- maven.j2ee/src/org/netbeans/modules/maven/j2ee/SelectAppServerPanel.java
+++ maven.j2ee/src/org/netbeans/modules/maven/j2ee/SelectAppServerPanel.java
@@ -39,26 +39,56 @@
 
 package org.netbeans.modules.maven.j2ee;
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.JFileChooser;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+import org.apache.maven.project.MavenProject;
+import org.netbeans.api.project.Project;
+import org.netbeans.api.project.ui.OpenProjects;
 import org.netbeans.modules.j2ee.deployment.devmodules.api.Deployment;
+import org.netbeans.modules.maven.api.NbMavenProject;
+import org.netbeans.modules.maven.model.Utilities;
+import org.netbeans.modules.maven.model.pom.POMModel;
+import org.netbeans.modules.maven.model.pom.POMModelFactory;
+import org.netbeans.modules.xml.xam.ModelSource;
+import org.netbeans.spi.project.ui.LogicalViewProvider;
+import org.openide.DialogDescriptor;
+import org.openide.DialogDisplayer;
 import org.openide.NotificationLineSupport;
+import org.openide.NotifyDescriptor;
+import org.openide.explorer.ExplorerManager;
+import org.openide.explorer.view.ListView;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileUtil;
+import org.openide.nodes.AbstractNode;
+import org.openide.nodes.Children;
+import org.openide.nodes.Node;
 import org.openide.util.NbBundle;
+import org.openide.util.RequestProcessor;
 
 /**
  *
  * @author mkleint
  */
-public class SelectAppServerPanel extends javax.swing.JPanel {
+public class SelectAppServerPanel extends javax.swing.JPanel implements PropertyChangeListener {
     private NotificationLineSupport nls;
+    private FileObject projDir;
 
     /** Creates new form SelectAppServerPanel */
-    public SelectAppServerPanel(boolean showIgnore) {
+    public SelectAppServerPanel(boolean showIgnore, FileObject projDir) {
+        this.projDir = projDir;
         initComponents();
         buttonGroup1.add(rbSession);
         buttonGroup1.add(rbPermanent);
@@ -80,6 +110,8 @@ public class SelectAppServerPanel extends javax.swing.JPanel {
         } else {
             rbIgnore.setVisible(false);
         }
+        updateProjectLbl();
+        rbPermanentStateChanged(null);
     }
 
     String getSelectedServerType() {
@@ -98,6 +130,10 @@ public class SelectAppServerPanel extends javax.swing.JPanel {
 
     boolean isIgnored() {
         return rbIgnore.isSelected();
+    }
+
+    FileObject getPermanentFolder() {
+        return projDir;
     }
 
     private void loadComboModel() {
@@ -134,6 +170,8 @@ public class SelectAppServerPanel extends javax.swing.JPanel {
         rbSession = new javax.swing.JRadioButton();
         rbPermanent = new javax.swing.JRadioButton();
         rbIgnore = new javax.swing.JRadioButton();
+        lblProject = new javax.swing.JLabel();
+        btChange = new javax.swing.JButton();
 
         lblServer.setLabelFor(comServer);
         org.openide.awt.Mnemonics.setLocalizedText(lblServer, org.openide.util.NbBundle.getMessage(SelectAppServerPanel.class, "SelectAppServerPanel.lblServer.text")); // NOI18N
@@ -142,8 +180,24 @@ public class SelectAppServerPanel extends javax.swing.JPanel {
         org.openide.awt.Mnemonics.setLocalizedText(rbSession, org.openide.util.NbBundle.getMessage(SelectAppServerPanel.class, "SelectAppServerPanel.rbSession.text")); // NOI18N
 
         org.openide.awt.Mnemonics.setLocalizedText(rbPermanent, org.openide.util.NbBundle.getMessage(SelectAppServerPanel.class, "SelectAppServerPanel.rbPermanent.text")); // NOI18N
+        rbPermanent.addChangeListener(new javax.swing.event.ChangeListener() {
+            public void stateChanged(javax.swing.event.ChangeEvent evt) {
+                rbPermanentStateChanged(evt);
+            }
+        });
 
         org.openide.awt.Mnemonics.setLocalizedText(rbIgnore, org.openide.util.NbBundle.getBundle(SelectAppServerPanel.class).getString("SelectAppServerPanel.rbIgnore.text")); // NOI18N
+
+        lblProject.setFont(lblProject.getFont().deriveFont(lblProject.getFont().getSize()-1f));
+        org.openide.awt.Mnemonics.setLocalizedText(lblProject, org.openide.util.NbBundle.getMessage(SelectAppServerPanel.class, "SelectAppServerPanel.lblProject.text")); // NOI18N
+
+        org.openide.awt.Mnemonics.setLocalizedText(btChange, org.openide.util.NbBundle.getMessage(SelectAppServerPanel.class, "SelectAppServerPanel.btChange.text")); // NOI18N
+        btChange.setToolTipText(org.openide.util.NbBundle.getMessage(SelectAppServerPanel.class, "SelectAppServerPanel.btChange.toolTipText")); // NOI18N
+        btChange.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btChangeActionPerformed(evt);
+            }
+        });
 
         org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(this);
         this.setLayout(layout);
@@ -151,14 +205,20 @@ public class SelectAppServerPanel extends javax.swing.JPanel {
             layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
             .add(layout.createSequentialGroup()
                 .addContainerGap()
-                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
-                    .add(rbIgnore)
-                    .add(org.jdesktop.layout.GroupLayout.TRAILING, layout.createSequentialGroup()
+                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.TRAILING)
+                    .add(org.jdesktop.layout.GroupLayout.LEADING, layout.createSequentialGroup()
+                        .add(12, 12, 12)
+                        .add(lblProject))
+                    .add(layout.createSequentialGroup()
                         .add(lblServer)
                         .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
                         .add(comServer, 0, 323, Short.MAX_VALUE))
-                    .add(rbSession)
-                    .add(rbPermanent))
+                    .add(org.jdesktop.layout.GroupLayout.LEADING, rbSession)
+                    .add(org.jdesktop.layout.GroupLayout.LEADING, layout.createSequentialGroup()
+                        .add(rbPermanent)
+                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED, 128, Short.MAX_VALUE)
+                        .add(btChange))
+                    .add(org.jdesktop.layout.GroupLayout.LEADING, rbIgnore))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -171,17 +231,77 @@ public class SelectAppServerPanel extends javax.swing.JPanel {
                 .add(18, 18, 18)
                 .add(rbSession)
                 .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
-                .add(rbPermanent)
-                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                    .add(layout.createSequentialGroup()
+                        .add(rbPermanent)
+                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                        .add(lblProject))
+                    .add(btChange))
+                .add(18, 18, 18)
                 .add(rbIgnore)
-                .addContainerGap(org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(20, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void rbPermanentStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_rbPermanentStateChanged
+        boolean isSel = rbPermanent.isSelected();
+        btChange.setEnabled(isSel);
+        lblProject.setEnabled(isSel);
+    }//GEN-LAST:event_rbPermanentStateChanged
+
+    private void btChangeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btChangeActionPerformed
+        JFileChooser fc = new JFileChooser(FileUtil.toFile(projDir));
+        fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+        fc.setControlButtonsAreShown(false);
+        final DialogDescriptor dd = new DialogDescriptor(fc,
+                NbBundle.getMessage(SelectAppServerPanel.class, "TIT_ChooseParent"));
+        final NotificationLineSupport fcNls = dd.createNotificationLineSupport();
+
+        fc.addPropertyChangeListener(new PropertyChangeListener() {
+            public void propertyChange(PropertyChangeEvent evt) {
+                String propName = evt.getPropertyName();
+                if (JFileChooser.SELECTED_FILE_CHANGED_PROPERTY.equals(propName) ||
+                        JFileChooser.DIRECTORY_CHANGED_PROPERTY.equals(propName)) {
+                    Object val = evt.getNewValue();
+                    if (val instanceof File) {
+                        FileObject curFO = FileUtil.toFileObject((File)val);
+                        if (curFO != null) {
+                            if (curFO.getFileObject("pom.xml") != null) {
+                                fcNls.clearMessages();
+                                dd.setValid(true);
+                                return;
+                            }
+                        }
+                    }
+                    fcNls.setErrorMessage(NbBundle.getMessage(
+                            SelectAppServerPanel.class, "ERR_NotMaven"));
+                    dd.setValid(false);
+                }
+            }
+        });
+
+        Object obj = DialogDisplayer.getDefault().notify(dd);
+        if (obj == NotifyDescriptor.OK_OPTION) {
+            File f = fc.getSelectedFile();
+            if (f != null) {
+                System.err.println("file: " + fc.getSelectedFile());
+                projDir = FileUtil.toFileObject(f);
+                updateProjectLbl();
+            }
+        }
+
+    }//GEN-LAST:event_btChangeActionPerformed
+
+    public void propertyChange(PropertyChangeEvent arg0) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btChange;
     private javax.swing.ButtonGroup buttonGroup1;
     private javax.swing.JComboBox comServer;
+    private javax.swing.JLabel lblProject;
     private javax.swing.JLabel lblServer;
     javax.swing.JRadioButton rbIgnore;
     javax.swing.JRadioButton rbPermanent;
@@ -210,6 +330,85 @@ public class SelectAppServerPanel extends javax.swing.JPanel {
         } else {
             nls.clearMessages();
         }
+    }
+
+    private void updateProjectLbl () {
+        FileObject pomFO = projDir.getFileObject("pom.xml"); //NOI18N
+        if (pomFO != null) {
+            ModelSource ms = Utilities.createModelSource(pomFO);
+            POMModel model = POMModelFactory.getDefault().getModel(ms);
+            String name = model.getProject().getName();
+            lblProject.setText("(in " + name + ")");
+        }
+    }
+
+    private static class OpenListPanel extends JPanel implements ExplorerManager.Provider,
+            PropertyChangeListener, Runnable, ActionListener {
+
+        private ListView lv;
+        private ExplorerManager manager;
+        private Project project;
+        private SelectAppServerPanel panel;
+
+        public OpenListPanel(SelectAppServerPanel panel, Project project) {
+            this.panel = panel;
+            this.project = project;
+            lv = new ListView();
+            lv.setDefaultProcessor(this);
+            lv.setPopupAllowed(false);
+            lv.setTraversalAllowed(false);
+            manager = new ExplorerManager();
+            manager.addPropertyChangeListener(this);
+            setLayout(new BorderLayout());
+            add(lv, BorderLayout.CENTER);
+
+            RequestProcessor.getDefault().post(this);
+        }
+
+        public ExplorerManager getExplorerManager() {
+            return manager;
+        }
+
+        public void propertyChange(PropertyChangeEvent evt) {
+            Node[] selNodes = manager.getSelectedNodes();
+            if (selNodes.length == 1) {
+                Project prj = selNodes[0].getLookup().lookup(Project.class);
+                NbMavenProject mav = prj.getLookup().lookup(NbMavenProject.class);
+                MavenProject m = mav.getMavenProject();
+            }
+        }
+
+        /** Loads dependencies outside EQ thread, updates tab state in EQ */
+        public void run() {
+            Project[] prjs = OpenProjects.getDefault().getOpenProjects();
+            final List<Node> toRet = new ArrayList<Node>();
+            for (Project p : prjs) {
+                if (p == project) {
+                    continue;
+                }
+                NbMavenProject mav = p.getLookup().lookup(NbMavenProject.class);
+                if (mav != null) {
+                    LogicalViewProvider lvp = p.getLookup().lookup(LogicalViewProvider.class);
+                    toRet.add(lvp.createLogicalView());
+                }
+            }
+            Children.Array ch = new Children.Array();
+            ch.add(toRet.toArray(new Node[0]));
+            Node root = new AbstractNode(ch);
+            getExplorerManager().setRootContext(root);
+            SwingUtilities.invokeLater(new Runnable() {
+                public void run() {
+                    boolean opEmpty = toRet.isEmpty();
+                    if (opEmpty) {
+                    }
+                }
+            });
+        }
+
+        public void actionPerformed(ActionEvent e) {
+            // empty impl, disables default action
+        }
+
     }
 
 }
