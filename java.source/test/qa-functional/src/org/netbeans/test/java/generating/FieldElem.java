f83d4cf36b747c9e48c84527481bdc5ddaef7d08
Jiri Prox <jiriprox@netbeans.org>
2009-09-07 17:05:48 +0200

    Refactoring tests - moving testcases to one package

--- java.source/test/qa-functional/src/org/netbeans/test/java/generating/FieldElem.java
+++ java.source/test/qa-functional/src/org/netbeans/test/java/generating/FieldElem.java
@@ -44,84 +44,93 @@
  *
  * Created on June 26, 2000, 9:29 AM
  */
+package org.netbeans.test.java.generating;
 
-package org.netbeans.test.java.generating.ConstructorElem;
-
-import org.netbeans.test.java.Common;
-import java.util.Collections;
+import java.util.EnumSet;
+import javax.lang.model.element.Modifier;
 import junit.framework.Test;
+import org.netbeans.test.java.Common;
 import org.netbeans.api.java.source.JavaSource;
 import org.netbeans.junit.*;
 import org.openide.filesystems.FileObject;
 
-/** <B>Java Module General API Test: ConstructorElement</B>
+/** <B>Java Module General API Test: FieldElement</B>
  * <BR><BR><I>What it tests:</I><BR>
- * Creating and handling with ConstructorElement.
+ * Creating and handling with FieldElement.
  * Test is focused on checking of correctness of generated code.
  * <BR><BR><I>How it works:</I><BR>
- * New class is created using DataObject.createFromTemplate() and required constructors
- * are created via API calls.
+ * FieldElements are created and customized using setters and then added using ClassElement.addField() into ClassElement.
  * These actions cause generating of .java code. This code is compared with supposed one.
  * <BR><BR><I>Output:</I><BR>
  * Generated Java code.
  * <BR><BR><I>Possible reasons of failure:</I><BR>
- * <U>Constructors are not inserted properly</U><BR>
- * If there is some constructors in .diff file.
- * <BR><BR><U>Constructors have/return bad properties</U><BR>
+ * <U>Fields are not inserted properly:</U><BR>
+ * If there are some fields in .diff file.
+ * <BR><BR><U>Fields have bad properties (e.g. modifiers, return type)</U><BR>
  * See .diff file to get which ones
  * <BR><BR><U>Bad indentation</U><BR>
- * This is propably not a bug of Java Module. (Editor Bug)
+ * This is probably not a bug of Java Module. (->Editor Bug)
  * In .diff file could be some whitespaces.
- * <BR><BR><U>Exception occured:</U><BR>
+ * <BR><BR><U>Exception occured:</I><BR>
  * See .log file for StackTrace
  *
  * @author Jan Becicka <Jan.Becicka@sun.com>
  */
+public class FieldElem extends org.netbeans.test.java.XRunner {
 
-
-public class ConstructorElem extends org.netbeans.test.java.XRunner {
-    
     public static void main(java.lang.String[] args) {
         junit.textui.TestRunner.run(suite());
     }
-    
-    public ConstructorElem() {
+
+    public FieldElem() {
         super("");
     }
-    
-    public ConstructorElem(java.lang.String testName) {
+
+    public FieldElem(java.lang.String testName) {
         super(testName);
     }
-        
+
     /** "body" of this TestCase
      * @param o SourceElement - target for generating
      * @param log log is used for logging StackTraces
      * @throws Exception
      * @return true if test passed
      * false if failed
-     */    
+     */
     public boolean go(Object o, java.io.PrintWriter log) throws Exception {
-        boolean passed = true;
+
         FileObject fo = (FileObject) o;
-        JavaSource js = JavaSource.forFileObject(fo);    
-        Common.removeConstructors(js);
-        Common.addConstructor(js, Collections.EMPTY_MAP);
-        Common.addConstructor(js, Common.PARS1);
-        Common.addConstructor(js, Common.PARS2);
-        Common.addConstructor(js, Common.PARS3);                       
+        JavaSource js = JavaSource.forFileObject(fo);
+
+        //let's add some fields newField1 .. newField4        
+        int i = 1;
+        EnumSet<Modifier> set = EnumSet.of(Modifier.PUBLIC, Modifier.STATIC);
+        Common.addField(js, Common.getFieldName(i++), set, "boolean");
+
+        set = EnumSet.of(Modifier.PRIVATE, Modifier.STATIC);
+        Common.addField(js, Common.getFieldName(i++), set, "int");
+
+        set = EnumSet.of(Modifier.PROTECTED);
+        Common.addField(js, Common.getFieldName(i++), set, "boolean");
+
+        set = EnumSet.of(Modifier.SYNCHRONIZED, Modifier.PUBLIC);
+        Common.addField(js, Common.getFieldName(i++), set, "float");
+
+        set = EnumSet.of(Modifier.FINAL, Modifier.PUBLIC, Modifier.STATIC);
+        Common.addField(js, Common.getFieldName(i++), set, "String");
         return true;
     }
-    
+
     /**
-     */    
+     */
     protected void setUp() {
         super.setUp();
-        name = "JavaTestSourceConstructorElem";
+        name = "JavaTestSourceFieldElem";
         packageName = "org.netbeans.test.java.testsources";
     }
-    
+
     public static Test suite() {
         return NbModuleSuite.create(
-                NbModuleSuite.createConfiguration(ConstructorElem.class).enableModules(".*").clusters(".*"));
+                NbModuleSuite.createConfiguration(FieldElem.class).enableModules(".*").clusters(".*"));
     }
 }
--- java.source/test/qa-functional/src/org/netbeans/test/java/generating/MethodElem.java
+++ java.source/test/qa-functional/src/org/netbeans/test/java/generating/MethodElem.java
@@ -44,52 +44,56 @@
  *
  * Created on June 26, 2000, 9:29 AM
  */
-package org.netbeans.test.java.generating.FieldElem;
 
+package org.netbeans.test.java.generating;
+
+import org.netbeans.test.java.Common;
 import java.util.EnumSet;
 import javax.lang.model.element.Modifier;
 import junit.framework.Test;
-import org.netbeans.test.java.Common;
 import org.netbeans.api.java.source.JavaSource;
 import org.netbeans.junit.*;
 import org.openide.filesystems.FileObject;
 
-/** <B>Java Module General API Test: FieldElement</B>
+/** <B>Java Module General API Test: MethodElement</B>
  * <BR><BR><I>What it tests:</I><BR>
- * Creating and handling with FieldElement.
+ * Creating and handling with MethodElement.
  * Test is focused on checking of correctness of generated code.
  * <BR><BR><I>How it works:</I><BR>
- * FieldElements are created and customized using setters and then added using ClassElement.addField() into ClassElement.
+ * New class is created using DataObject.createFromTemplate() and also some MethodElements are created.
+ * These are customized using setters and then added using ClassElement.addMethod() into ClassElement.
  * These actions cause generating of .java code. This code is compared with supposed one.
  * <BR><BR><I>Output:</I><BR>
  * Generated Java code.
  * <BR><BR><I>Possible reasons of failure:</I><BR>
- * <U>Fields are not inserted properly:</U><BR>
- * If there are some fields in .diff file.
- * <BR><BR><U>Fields have bad properties (e.g. modifiers, return type)</U><BR>
+ * <U>Methods are not inserted properly</U><BR>
+ * If there are some Interfaces in .diff file.
+ * <BR><BR><U>Methods have/return bad properies</U><BR>
  * See .diff file to get which ones
  * <BR><BR><U>Bad indentation</U><BR>
- * This is probably not a bug of Java Module. (->Editor Bug)
+ * This is probably not a bug of Java Module. (Editor Bug)
  * In .diff file could be some whitespaces.
- * <BR><BR><U>Exception occured:</I><BR>
+ * <BR><BR><I>Exception occured:</I><BR>
  * See .log file for StackTrace
  *
  * @author Jan Becicka <Jan.Becicka@sun.com>
  */
-public class FieldElem extends org.netbeans.test.java.XRunner {
 
+
+public class MethodElem extends org.netbeans.test.java.XRunner {
+    
     public static void main(java.lang.String[] args) {
         junit.textui.TestRunner.run(suite());
     }
-
-    public FieldElem() {
+    
+    public MethodElem() {
         super("");
     }
-
-    public FieldElem(java.lang.String testName) {
+    
+    public MethodElem(java.lang.String testName) {
         super(testName);
     }
-
+    
     /** "body" of this TestCase
      * @param o SourceElement - target for generating
      * @param log log is used for logging StackTraces
@@ -98,39 +102,33 @@ public class FieldElem extends org.netbeans.test.java.XRunner {
      * false if failed
      */
     public boolean go(Object o, java.io.PrintWriter log) throws Exception {
-
+        
+        //        org.openide.src.ClassElement clazz = ((org.openide.src.SourceElement) o).getClasses()[0];
+        boolean passed = true;
         FileObject fo = (FileObject) o;
         JavaSource js = JavaSource.forFileObject(fo);
-
-        //let's add some fields newField1 .. newField4        
-        int i = 1;
-        EnumSet<Modifier> set = EnumSet.of(Modifier.PUBLIC, Modifier.STATIC);
-        Common.addField(js, Common.getFieldName(i++), set, "boolean");
-
-        set = EnumSet.of(Modifier.PRIVATE, Modifier.STATIC);
-        Common.addField(js, Common.getFieldName(i++), set, "int");
-
-        set = EnumSet.of(Modifier.PROTECTED);
-        Common.addField(js, Common.getFieldName(i++), set, "boolean");
-
-        set = EnumSet.of(Modifier.SYNCHRONIZED, Modifier.PUBLIC);
-        Common.addField(js, Common.getFieldName(i++), set, "float");
-
-        set = EnumSet.of(Modifier.FINAL, Modifier.PUBLIC, Modifier.STATIC);
-        Common.addField(js, Common.getFieldName(i++), set, "String");
-        return true;
+        
+        Common.addMethod(js, "method1",Common.PARS1,"void", EnumSet.of(Modifier.PUBLIC,Modifier.STATIC));        
+        Common.addMethod(js, "method1",Common.PARS2,"int", EnumSet.of(Modifier.PRIVATE,Modifier.SYNCHRONIZED));        
+        Common.addMethod(js, "method1",Common.PARS3,"float", EnumSet.of(Modifier.PRIVATE,Modifier.FINAL));        
+        Common.addMethod(js, "method2",Common.PARS1,"double", EnumSet.of(Modifier.PUBLIC,Modifier.STATIC));
+        Common.addMethod(js, "method2",Common.PARS2,"boolean", EnumSet.of(Modifier.PUBLIC,Modifier.STATIC));        
+        Common.addMethod(js, "method2",Common.PARS3,"void", EnumSet.of(Modifier.PUBLIC,Modifier.STATIC));
+        
+        return passed;
     }
-
+    
     /**
      */
     protected void setUp() {
         super.setUp();
-        name = "JavaTestSourceFieldElem";
+        name = "JavaTestSourceMethodElem";
         packageName = "org.netbeans.test.java.testsources";
     }
-
+    
     public static Test suite() {
         return NbModuleSuite.create(
-                NbModuleSuite.createConfiguration(FieldElem.class).enableModules(".*").clusters(".*"));
+                NbModuleSuite.createConfiguration(MethodElem.class).enableModules(".*").clusters(".*"));
     }
+    
 }
