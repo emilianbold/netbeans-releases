90b7814a78a7f04e96cd7d69751b549365b4ab14
michaelnazarov <michaelnazarov@netbeans.org>
2009-09-08 15:15:04 +0400

    automation

--- installer/test/test/org/netbeans/test/installer/TestInstallerAndUninstallerJavaSE.java
+++ installer/test/test/org/netbeans/test/installer/TestInstallerAndUninstallerJavaSE.java
@@ -33,8 +33,9 @@ import org.netbeans.junit.NbTestSuite;
  *
  * @author Mikhail Vaysman
  */
-public class TestInstallerAndUninstallerJavaSE extends NbTestCase {
+public class TestInstallerAndUninstallerJavaSE extends Installer {
 
+    /*
     public TestInstallerAndUninstallerJavaSE() {
         super("Installer test");
     }
@@ -44,21 +45,38 @@ public class TestInstallerAndUninstallerJavaSE extends NbTestCase {
 
         return suite;
     }
+    */
 
     public void testInstaller() {
         TestData data = new TestData(Logger.getLogger("global"));
 
-        Utils.phaseOne(this, data, "javase");
+        Utils.phaseOne(data, "javase");
 
-        Utils.phaseTwo(data);
+        // Pages
+        // Welcome
+        Utils.stepWelcome();
+        // Agreement
+        Utils.stepLicense();
+        // Location
+        Utils.stepSetDir(data, "Install the NetBeans IDE", Utils.NB_DIR_NAME );
+        // Summary
+        Utils.stepInstall(data);
+        //Installation
+        //finish
+        Utils.stepFinish();
+
+
+        //Utils.phaseTwo(data);
 
         Utils.phaseFour(data);
+
+        Utils.phaseFive( data );
 
         //TODO Dir removed test
         //TODO Clean up work dir
     }
 
-    public static void main(String[] args) {
-        junit.textui.TestRunner.run(suite());
-    }
+    //public static void main(String[] args) {
+    //    junit.textui.TestRunner.run(suite());
+    //}
 }
