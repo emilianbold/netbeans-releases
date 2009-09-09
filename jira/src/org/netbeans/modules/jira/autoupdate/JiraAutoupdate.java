01f2b354f254e05518b8e0da231a03d06d5a6b30
Tomas Stupka <tstupka@netbeans.org>
2009-09-09 11:00:20 +0200

    notify even if unable to parse version

--- jira/src/org/netbeans/modules/jira/autoupdate/JiraAutoupdate.java
+++ jira/src/org/netbeans/modules/jira/autoupdate/JiraAutoupdate.java
@@ -132,7 +132,9 @@ public class JiraAutoupdate {
                             return true;
                         }
                     }
-                    return false;
+                    return elements.size() > 0; // looks like we weren't able to
+                                                // parse the version; on the other hand ->
+                                                // there is something so lets be optimistic
                 } else {
                     return false;
                 }
