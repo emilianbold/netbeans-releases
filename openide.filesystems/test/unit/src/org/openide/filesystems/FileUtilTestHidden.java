b6b317a30edb7ac13caca8210a66a93d053f58c2
Jiri Skrivanek <jskrivanek@netbeans.org>
2009-09-09 09:35:29 +0200

    Fixed warnings and formatting.

--- openide.filesystems/test/unit/src/org/openide/filesystems/FileUtilTestHidden.java
+++ openide.filesystems/test/unit/src/org/openide/filesystems/FileUtilTestHidden.java
@@ -38,9 +38,7 @@
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
  */
-
 package org.openide.filesystems;
-
 
 import java.io.File;
 import java.io.IOException;
@@ -50,24 +48,29 @@ import java.util.ArrayList;
 import org.openide.util.Utilities;
 
 public class FileUtilTestHidden extends TestBaseHid {
+
     private FileObject root = null;
 
     protected String[] getResources(String testName) {
         return new String[]{
-            "fileutildir/tofile.txt",
-            "fileutildir/tofileobject.txt",
-            "fileutildir/isParentOf.txt",
-            "fileutildir/fileutildir2/fileutildir3",
-            "fileutildir/fileutildir2/folder/file"                                
-        };
+                    "fileutildir/tofile.txt",
+                    "fileutildir/tofileobject.txt",
+                    "fileutildir/isParentOf.txt",
+                    "fileutildir/fileutildir2/fileutildir3",
+                    "fileutildir/fileutildir2/folder/file"
+                };
     }
 
+    @Override
+    @SuppressWarnings("deprecation")
     protected void setUp() throws Exception {
         super.setUp();
         Repository.getDefault().addFileSystem(testedFS);
         root = testedFS.findResource(getResourcePrefix());
     }
 
+    @Override
+    @SuppressWarnings("deprecation")
     protected void tearDown() throws Exception {
         Repository.getDefault().removeFileSystem(testedFS);
         super.tearDown();
@@ -77,25 +80,29 @@ public class FileUtilTestHidden extends TestBaseHid {
     public FileUtilTestHidden(String name) {
         super(name);
     }
-    
+
     private static class TestListener extends FileChangeAdapter {
+
         boolean wasCalled = false;
+
+        @Override
         public void fileFolderCreated(FileEvent fe) {
             wasCalled = true;
         }
     }
-    
+
     public void testRunAtomicAction() throws Exception {
         final LocalFileSystem lfs = new LocalFileSystem();
         lfs.setRootDirectory(getWorkDir());
-        
+
         final FileSystem defSystem = FileUtil.getConfigRoot().getFileSystem();
         final TestListener l = new TestListener();
         try {
             defSystem.addFileChangeListener(l);
-            lfs.addFileChangeListener(l);            
-            assertFalse(l.wasCalled);            
+            lfs.addFileChangeListener(l);
+            assertFalse(l.wasCalled);
             FileUtil.runAtomicAction(new FileSystem.AtomicAction() {
+
                 public void run() throws IOException {
                     assertFalse(l.wasCalled);
                     assertNull(defSystem.getRoot().getFileObject(getName()));
@@ -104,70 +111,75 @@ public class FileUtilTestHidden extends TestBaseHid {
                     assertNotNull(FileUtil.createFolder(lfs.getRoot(), getName()));
                     assertFalse(l.wasCalled);
                 }
-            });            
+            });
             assertTrue(l.wasCalled);
         } finally {
             defSystem.removeFileChangeListener(l);
-            lfs.removeFileChangeListener(l);            
-        }        
+            lfs.removeFileChangeListener(l);
+        }
     }
-    
 
-    public void testCreateFolder () throws Exception {
-        if (this.testedFS instanceof JarFileSystem) return;
-        
-        assertNotNull(root);        
+    public void testCreateFolder() throws Exception {
+        if (this.testedFS instanceof JarFileSystem) {
+            return;
+        }
+        assertNotNull(root);
         FileObject folder = root.getFileObject("fileutildir");
         FileObject result = FileUtil.createFolder(folder, "fileutildir2/folder/fileutildir4");
         assertNotNull(result);
-        assertSame(result, root.getFileObject("fileutildir/fileutildir2/folder/fileutildir4"));        
+        assertSame(result, root.getFileObject("fileutildir/fileutildir2/folder/fileutildir4"));
     }
-    
-    public void testNormalizeFile () throws Exception {
-       File file = getWorkDir ();
-       file = FileUtil.normalizeFile(file); 
-       
-       File file2 = FileUtil.normalizeFile(file); 
-       assertSame(file, file2);
-       
-       file2 = new File (file, "test/..");
-       file2 = FileUtil.normalizeFile(file); 
-       assertEquals(file2, file);
 
-       if (Utilities.isUnix()) {
-           assertEquals(new File("/"), FileUtil.normalizeFile(new File("/..")));
-           assertEquals(new File("/"), FileUtil.normalizeFile(new File("/../.")));
-       }
+    public void testNormalizeFile() throws Exception {
+        File file = getWorkDir();
+        file = FileUtil.normalizeFile(file);
+
+        File file2 = FileUtil.normalizeFile(file);
+        assertSame(file, file2);
+
+        file2 = new File(file, "test/..");
+        file2 = FileUtil.normalizeFile(file);
+        assertEquals(file2, file);
+
+        if (Utilities.isUnix()) {
+            assertEquals(new File("/"), FileUtil.normalizeFile(new File("/..")));
+            assertEquals(new File("/"), FileUtil.normalizeFile(new File("/../.")));
+        }
     }
-    
-    public void testNormalizeFile2 () throws Exception {
-        if (!Utilities.isWindows()) return;
-        
+
+    public void testNormalizeFile2() throws Exception {
+        if (!Utilities.isWindows()) {
+            return;
+        }
         File rootFile = FileUtil.toFile(root);
-        if (rootFile == null) return;
+        if (rootFile == null) {
+            return;
+        }
         assertTrue(rootFile.exists());
-        
-        File testFile = new File (rootFile, "abc.txt");
+
+        File testFile = new File(rootFile, "abc.txt");
         assertTrue(testFile.createNewFile());
         assertTrue(testFile.exists());
-        
-        File testFile2 = new File (rootFile, "ABC.TXT");
-        assertTrue(testFile2.exists());
-        
-        assertSame(testFile, FileUtil.normalizeFile(testFile));        
-        assertNotSame(testFile2, FileUtil.normalizeFile(testFile2));        
-    }   
-    
-    public void testFindFreeFolderName () throws Exception {
-        if (this.testedFS.isReadOnly()) return;
 
-        String name = FileUtil.findFreeFolderName(root, "fileutildir".toUpperCase());
-        root.createFolder(name);        
+        File testFile2 = new File(rootFile, "ABC.TXT");
+        assertTrue(testFile2.exists());
+
+        assertSame(testFile, FileUtil.normalizeFile(testFile));
+        assertNotSame(testFile2, FileUtil.normalizeFile(testFile2));
     }
-    
+
+    public void testFindFreeFolderName() throws Exception {
+        if (this.testedFS.isReadOnly()) {
+            return;
+        }
+        String name = FileUtil.findFreeFolderName(root, "fileutildir".toUpperCase());
+        root.createFolder(name);
+    }
+
     public void testToFile() throws Exception {
-        if (this.testedFS instanceof JarFileSystem) return;
-        
+        if (this.testedFS instanceof JarFileSystem) {
+            return;
+        }
         assertNotNull(root);
         FileObject testFo = root.getFileObject("fileutildir/tofile.txt");
         assertNotNull(testFo);
@@ -176,28 +188,27 @@ public class FileUtilTestHidden extends TestBaseHid {
         assertNotNull(testFile);
         assertTrue(testFile.exists());
     }
-    
-    
-    public void testIsArchiveFile () throws Exception {
+
+    public void testIsArchiveFile() throws Exception {
         final String base = getWorkDir().toURI().toURL().toExternalForm();
-        URL url = new URL (base + "test.jar");    //NOI18N
-        assertTrue ("test.jar has to be an archive",FileUtil.isArchiveFile (url));  //NOI18N
-        url = new URL (base + ".hidden.jar");   //NOI18N
-        assertTrue (FileUtil.isArchiveFile (url));  //NOI18N
-        url = new URL (base + "folder");    //NOI18N
-        assertFalse ("folder cannot to be an archive", FileUtil.isArchiveFile (url));   //NOI18N
-        url = new URL (base + "folder/");    //NOI18N
-        assertFalse ("folder cannot to be an archive", FileUtil.isArchiveFile (url));   //NOI18N
-        url = new URL (base + ".hidden");   //NOI18N
-        assertFalse (".hidden cannot to be an archive", FileUtil.isArchiveFile (url));  //NOI18N
-        url = new URL (base + ".hidden/");   //NOI18N
-        assertFalse (".hidden cannot to be an archive", FileUtil.isArchiveFile (url));  //NOI18N
+        URL url = new URL(base + "test.jar");    //NOI18N
+        assertTrue("test.jar has to be an archive", FileUtil.isArchiveFile(url));  //NOI18N
+        url = new URL(base + ".hidden.jar");   //NOI18N
+        assertTrue(FileUtil.isArchiveFile(url));  //NOI18N
+        url = new URL(base + "folder");    //NOI18N
+        assertFalse("folder cannot to be an archive", FileUtil.isArchiveFile(url));   //NOI18N
+        url = new URL(base + "folder/");    //NOI18N
+        assertFalse("folder cannot to be an archive", FileUtil.isArchiveFile(url));   //NOI18N
+        url = new URL(base + ".hidden");   //NOI18N
+        assertFalse(".hidden cannot to be an archive", FileUtil.isArchiveFile(url));  //NOI18N
+        url = new URL(base + ".hidden/");   //NOI18N
+        assertFalse(".hidden cannot to be an archive", FileUtil.isArchiveFile(url));  //NOI18N
     }
 
-
-    public void testToFileObject () throws Exception {
-        if (this.testedFS instanceof JarFileSystem) return;
-        
+    public void testToFileObject() throws Exception {
+        if (this.testedFS instanceof JarFileSystem) {
+            return;
+        }
         assertNotNull(root);
         FileObject testFo = root.getFileObject("fileutildir/tofileobject.txt");
         assertNotNull(testFo);
@@ -206,7 +217,7 @@ public class FileUtilTestHidden extends TestBaseHid {
         assertNotNull(rootFile);
         assertTrue(rootFile.exists());
 
-        File testFile = new File (rootFile, "fileutildir/tofileobject.txt");
+        File testFile = new File(rootFile, "fileutildir/tofileobject.txt");
         assertNotNull(testFile);
         assertTrue(testFile.exists());
 
@@ -215,20 +226,23 @@ public class FileUtilTestHidden extends TestBaseHid {
         assertEquals(testFo2, testFo);
     }
 
-    public void testIsParentOf () throws Exception {
-        if (this.testedFS instanceof JarFileSystem) return;
-        
+    public void testIsParentOf() throws Exception {
+        if (this.testedFS instanceof JarFileSystem) {
+            return;
+        }
         final List<FileEvent> events = new ArrayList<FileEvent>();
         assertNotNull(root);
         final FileObject parent = root.getFileObject("fileutildir");
         assertNotNull(parent);
-        
+
         final FileObject child = root.getFileObject("fileutildir/isParentOf.txt");
         assertNotNull(child);
-        
+
         assertTrue(FileUtil.isParentOf(parent, child));
-                
+
         child.addFileChangeListener(new FileChangeAdapter() {
+
+            @Override
             public void fileDeleted(FileEvent fe) {
                 FileObject file = fe.getFile();
                 assertTrue(FileUtil.isParentOf(parent, file));
@@ -236,16 +250,16 @@ public class FileUtilTestHidden extends TestBaseHid {
                 events.add(fe);
             }
         });
-        child.delete();      
-        assertTrue (events.size() == 1);
+        child.delete();
+        assertTrue(events.size() == 1);
         assertNull(root.getFileObject("fileutildir/isParentOf.txt"));
-        
+
     }
 
-
-    public void testIsParentOf3 () throws Exception {
-        if (this.testedFS instanceof JarFileSystem) return;
-        
+    public void testIsParentOf3() throws Exception {
+        if (this.testedFS instanceof JarFileSystem) {
+            return;
+        }
         final List<FileEvent> events = new ArrayList<FileEvent>();
         assertNotNull(root);
         final FileObject[] fileObjects = new FileObject[]{
@@ -253,33 +267,35 @@ public class FileUtilTestHidden extends TestBaseHid {
             root.getFileObject("fileutildir/fileutildir2"),
             root.getFileObject("fileutildir/fileutildir2/fileutildir3")
         };
-        
+
         for (int i = 0; i < fileObjects.length; i++) {
             FileObject fo = fileObjects[i];
-            assertNotNull(fo);            
+            assertNotNull(fo);
         }
-                
-        assertTrue(FileUtil.isParentOf(root, fileObjects[0]));        
+
+        assertTrue(FileUtil.isParentOf(root, fileObjects[0]));
         assertTrue(FileUtil.isParentOf(fileObjects[0], fileObjects[1]));
-        assertTrue(FileUtil.isParentOf(fileObjects[1], fileObjects[2]));        
+        assertTrue(FileUtil.isParentOf(fileObjects[1], fileObjects[2]));
         final FileChangeListener fcl = new FileChangeAdapter() {
+
+            @Override
             public void fileDeleted(FileEvent fe) {
                 FileObject file = fe.getFile();
-                assertNotNull(file.getPath(),file.getParent());
+                assertNotNull(file.getPath(), file.getParent());
                 assertTrue(file.getPath(), FileUtil.isParentOf(root, file));
                 events.add(fe);
             }
         };
         try {
             testedFS.addFileChangeListener(fcl);
-            fileObjects[1].delete();                                
+            fileObjects[1].delete();
             assertTrue(events.size() > 0);
         } finally {
             testedFS.removeFileChangeListener(fcl);
-        }        
+        }
     }
-    
-    public void testGetFileDisplayName ()  throws Exception {        
+
+    public void testGetFileDisplayName() throws Exception {
         final FileObject[] fileObjects = new FileObject[]{
             root,
             root.getFileObject("fileutildir"),
@@ -292,24 +308,24 @@ public class FileUtilTestHidden extends TestBaseHid {
             String displayName = FileUtil.getFileDisplayName(fo);
             File f = FileUtil.toFile(fo);
             if (f != null) {
-                assertEquals(f.getAbsolutePath(), displayName);                
+                assertEquals(f.getAbsolutePath(), displayName);
             } else {
-                FileObject archivFo = FileUtil.getArchiveFile (fo);
+                FileObject archivFo = FileUtil.getArchiveFile(fo);
                 File archiv = (archivFo != null) ? FileUtil.toFile(archivFo) : null;
                 if (archiv != null) {
                     if (fo.isRoot()) {
-                        assertEquals(displayName, archiv.getAbsolutePath());                                                    
+                        assertEquals(displayName, archiv.getAbsolutePath());
                     } else {
-                        assertTrue(displayName.indexOf(archiv.getAbsolutePath()) != -1);    
-                    }                    
+                        assertTrue(displayName.indexOf(archiv.getAbsolutePath()) != -1);
+                    }
                 } else {
                     if (fo.isRoot()) {
-                        assertEquals(displayName, fo.getFileSystem().getDisplayName());                                                                            
+                        assertEquals(displayName, fo.getFileSystem().getDisplayName());
                     } else {
-                        assertTrue(displayName.indexOf(fo.getFileSystem().getDisplayName()) != -1);                                                
-                    }                                        
-                }                
+                        assertTrue(displayName.indexOf(fo.getFileSystem().getDisplayName()) != -1);
+                    }
+                }
             }
-        }                
+        }
     }
 }
