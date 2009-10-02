a27189c7f97927c022aa36190ed36110fb414812
Tomas Stupka <tstupka@netbeans.org>
2009-10-02 16:32:12 +0200

    fix imports

--- bugtracking/src/org/netbeans/modules/bugtracking/kenai/IssueAccessorImpl.java
+++ bugtracking/src/org/netbeans/modules/bugtracking/kenai/IssueAccessorImpl.java
@@ -43,7 +43,6 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Queue;
 import org.netbeans.api.progress.ProgressHandle;
 import org.netbeans.api.progress.ProgressHandleFactory;
 import org.netbeans.modules.bugtracking.BugtrackingManager;
