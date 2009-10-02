d6a4ba26283839057784407a542179b39549975d
Dusan Balek <dbalek@netbeans.org>
2009-10-02 15:14:27 +0200

    Modified to prevent frequent repaints of the code completion popup.

--- editor.completion/src/org/netbeans/modules/editor/completion/CompletionImpl.java
+++ editor.completion/src/org/netbeans/modules/editor/completion/CompletionImpl.java
@@ -1684,8 +1684,7 @@ outer:      for (Iterator it = localCompletionResult.getResultSets().iterator();
                 synchronized (CompletionImpl.this) {
                     if (completionResult != null) {
                         if (!isAllResultsFinished(completionResult.getResultSets())) {
-                            layout.showCompletion(Collections.singletonList(PLEASE_WAIT),
-                                    null, -1, CompletionImpl.this, null, null, 0);
+                            pleaseWaitTimer.restart();
                         }
                     }
                 }
