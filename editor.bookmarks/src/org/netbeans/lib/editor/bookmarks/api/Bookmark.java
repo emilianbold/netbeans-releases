3e3af974ed2de456b91af7856c228443494de2f8
Jan Jancura <jjancura@netbeans.org>
2009-10-05 14:54:36 +0200

    #168738: Navigator jumps to the beginning of the file while parsing is running

--- editor.bookmarks/src/org/netbeans/lib/editor/bookmarks/api/Bookmark.java
+++ editor.bookmarks/src/org/netbeans/lib/editor/bookmarks/api/Bookmark.java
@@ -49,8 +49,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.WeakHashMap;
-import javax.sound.sampled.LineEvent;
-import javax.sound.sampled.LineListener;
 import javax.swing.text.StyledDocument;
 
 import org.netbeans.modules.editor.NbEditorUtilities;
@@ -101,12 +99,12 @@ public final class Bookmark {
         StyledDocument document = (StyledDocument) bookmarkList.getDocument ();
         int lineNumber = NbDocument.findLineNumber (document, offset);
         DataObject dataObject = NbEditorUtilities.getDataObject (document);
-        for (Line line : lineToAnnotation.keySet ()) {
-            if (line.getLineNumber () == lineNumber &&
-                line.getLookup().lookup (DataObject.class).equals (dataObject)
+        for (Line _line : lineToAnnotation.keySet ()) {
+            if (_line.getLineNumber () == lineNumber &&
+                _line.getLookup().lookup (DataObject.class).equals (dataObject)
             ) {
-                this.line = line;
-                this.annotation = lineToAnnotation.get (line);
+                this.line = _line;
+                this.annotation = lineToAnnotation.get (_line);
                 return;
             }
         }
