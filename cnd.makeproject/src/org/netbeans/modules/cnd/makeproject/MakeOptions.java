c6fda3d59ac3c43add200b7a0fbf78b71c811e24
Thomas Preisler <thp@netbeans.org>
2009-10-08 04:47:50 +0400

    #172856 Cann't switch off 'Profile on Run' property on Profiler Tools dialog box

--- cnd.makeproject/src/org/netbeans/modules/cnd/makeproject/MakeOptions.java
+++ cnd.makeproject/src/org/netbeans/modules/cnd/makeproject/MakeOptions.java
@@ -68,6 +68,8 @@ public class MakeOptions extends SharedClassObject implements PropertyChangeList
     public static final String SAVE = "save";  // NOI18N
     // Reuse
     public static final String REUSE = "reuse";  // NOI18N
+    //
+    public static final String SHOW_PROFILING = "showProfiling"; // NOI18N
 
     // Display binary files
     public static final String VIEW_BINARY_FILES = "viewBinaryFiles"; // NOI18N
@@ -177,6 +179,19 @@ public class MakeOptions extends SharedClassObject implements PropertyChangeList
         getPreferences().putBoolean(REUSE, reuse);
         if (oldValue != reuse) {
             firePropertyChange(REUSE, Boolean.valueOf(oldValue), Boolean.valueOf(reuse));
+        }
+    }
+
+    // Show Profiling
+    public boolean getShowProfiling() {
+        return getPreferences().getBoolean(SHOW_PROFILING, true);
+    }
+
+    public void setShowProfiling(boolean reuse) {
+        boolean oldValue = getReuse();
+        getPreferences().putBoolean(SHOW_PROFILING, reuse);
+        if (oldValue != reuse) {
+            firePropertyChange(SHOW_PROFILING, Boolean.valueOf(oldValue), Boolean.valueOf(reuse));
         }
     }
 
