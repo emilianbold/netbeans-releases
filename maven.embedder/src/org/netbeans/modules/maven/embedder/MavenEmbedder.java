f3b51b4532be0568c6e8bce5dded67eef800ffa4
Milos Kleint <mkleint@netbeans.org>
2009-10-08 14:37:40 +0200

    very barebone rewrite of embedder code

--- maven.embedder/src/org/netbeans/modules/maven/embedder/MavenEmbedder.java
+++ maven.embedder/src/org/netbeans/modules/maven/embedder/MavenEmbedder.java
@@ -36,24 +36,160 @@
  *
  * Portions Copyrighted 2009 Sun Microsystems, Inc.
  */
-
 package org.netbeans.modules.maven.embedder;
 
+import java.io.File;
+import java.util.List;
+import org.apache.maven.Maven;
+import org.apache.maven.artifact.InvalidRepositoryException;
 import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.execution.DefaultMavenExecutionResult;
+import org.apache.maven.execution.MavenExecutionRequest;
+import org.apache.maven.execution.MavenExecutionRequestPopulationException;
+import org.apache.maven.execution.MavenExecutionRequestPopulator;
+import org.apache.maven.execution.MavenExecutionResult;
+import org.apache.maven.lifecycle.LifecycleExecutor;
+import org.apache.maven.model.building.ModelBuildingRequest;
+import org.apache.maven.model.io.ModelReader;
+import org.apache.maven.model.io.ModelWriter;
+import org.apache.maven.plugin.BuildPluginManager;
+import org.apache.maven.project.ProjectBuilder;
+import org.apache.maven.project.ProjectBuildingException;
+import org.apache.maven.project.ProjectBuildingRequest;
+import org.apache.maven.project.ProjectBuildingResult;
+import org.apache.maven.repository.RepositorySystem;
+import org.apache.maven.settings.MavenSettingsBuilder;
+import org.apache.maven.settings.validation.SettingsValidationResult;
 import org.codehaus.plexus.PlexusContainer;
+import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
 
 /**
  *
  * @author mkleint
  */
 public class MavenEmbedder {
+    public static final String userHome = System.getProperty( "user.home" );
+    public static final File userMavenConfigurationHome = new File( userHome, ".m2" );
+    public static final File defaultUserLocalRepository = new File( userMavenConfigurationHome, "repository" );
+    public static final File DEFAULT_USER_SETTINGS_FILE = new File( userMavenConfigurationHome, "settings.xml" );
+    public static final File DEFAULT_GLOBAL_SETTINGS_FILE =
+        new File( System.getProperty( "maven.home", System.getProperty( "user.dir", "" ) ), "conf/settings.xml" );
+
+
+
+    private final PlexusContainer plexus;
+    private final Maven maven;
+    private final ProjectBuilder projectBuilder;
+    private final ModelReader modelReader;
+    private final ModelWriter modelWriter;
+    private final RepositorySystem repositorySystem;
+    private final MavenExecutionRequestPopulator populator;
+    private final MavenSettingsBuilder settingsBuilder;
+    private final LifecycleExecutor lifecycleExecutor;
+    private final BuildPluginManager pluginManager;
+    private final EmbedderConfiguration request;
+
+    MavenEmbedder(EmbedderConfiguration req) throws ComponentLookupException {
+        request = req;
+        plexus = req.getContainer();
+        this.maven = plexus.lookup(Maven.class);
+        this.projectBuilder = plexus.lookup(ProjectBuilder.class);
+        this.modelReader = plexus.lookup(ModelReader.class);
+        this.modelWriter = plexus.lookup(ModelWriter.class);
+        this.repositorySystem = plexus.lookup(RepositorySystem.class);
+        this.settingsBuilder = plexus.lookup(MavenSettingsBuilder.class);
+        this.populator = plexus.lookup(MavenExecutionRequestPopulator.class);
+        this.pluginManager = plexus.lookup(BuildPluginManager.class);
+        this.lifecycleExecutor = plexus.lookup(LifecycleExecutor.class);
+    }
 
     public PlexusContainer getPlexusContainer() {
-        throw new UnsupportedOperationException("Not yet implemented");
+        return plexus;
+    }
+
+    public MavenExecutionResult readProject(MavenExecutionRequest request) {
+        File pomFile = request.getPom();
+        MavenExecutionResult result = new DefaultMavenExecutionResult();
+        try {
+            populator.populateDefaults(request);
+            ProjectBuildingRequest configuration = request.getProjectBuildingRequest();
+            configuration.setValidationLevel(ModelBuildingRequest.VALIDATION_LEVEL_MINIMAL);
+            ProjectBuildingResult projectBuildingResult = projectBuilder.build(pomFile, configuration);
+            result.setProject(projectBuildingResult.getProject());
+            result.setArtifactResolutionResult(projectBuildingResult.getArtifactResolutionResult());
+        } catch (ProjectBuildingException ex) {
+            return result.addException(ex);
+        } catch (MavenExecutionRequestPopulationException ex) {
+            return result.addException(ex);
+        }
+        return result;
+    }
+
+    private String getLocalRepositoryPath() {
+        throw new IllegalStateException("not yet implemented");
+//        return getSettings().getLocalRepository();
     }
 
     public ArtifactRepository getLocalRepository() {
-        throw new UnsupportedOperationException("Not yet implemented");
+        try {
+            String localRepositoryPath = getLocalRepositoryPath();
+            if (localRepositoryPath != null) {
+                return repositorySystem.createLocalRepository(new File(localRepositoryPath));
+            }
+            return repositorySystem.createLocalRepository(RepositorySystem.defaultUserLocalRepository);
+        } catch (InvalidRepositoryException ex) {
+            // can't happen
+            throw new IllegalStateException(ex);
+        }
     }
 
+//    public Settings getSettings() {
+//        // MUST NOT use createRequest!
+//
+//        MavenExecutionRequest request = new DefaultMavenExecutionRequest();
+//        if (mavenConfiguration.getGlobalSettingsFile() != null) {
+//            request.setGlobalSettingsFile(new File(mavenConfiguration.getGlobalSettingsFile()));
+//        }
+//        if (mavenConfiguration.getUserSettingsFile() != null) {
+//            request.setUserSettingsFile(new File(mavenConfiguration.getUserSettingsFile()));
+//        }
+//        try {
+//            return settingsBuilder.buildSettings(request);
+//        } catch (IOException ex) {
+//            Exceptions.printStackTrace(ex);
+//        } catch (XmlPullParserException ex) {
+//            Exceptions.printStackTrace(ex);
+//        }
+//    }
+
+//    public Settings buildSettings(String globalSettings, String userSettings) {
+//        MavenExecutionRequest request = createExecutionRequest(null);
+//        request.setGlobalSettingsFile(globalSettings != null ? new File(globalSettings) : null);
+//        request.setUserSettingsFile(userSettings != null ? new File(userSettings) : null);
+//        try {
+//            return settingsBuilder.buildSettings(request);
+//        } catch (IOException ex) {
+//            Exceptions.printStackTrace(ex);
+//        } catch (XmlPullParserException ex) {
+//            Exceptions.printStackTrace(ex);
+//        }
+//    }
+
+    public SettingsValidationResult validateSettings(String settings) {
+        SettingsValidationResult result = new SettingsValidationResult();
+        if (settings != null) {
+            File settingsFile = new File(settings);
+            if (settingsFile.canRead()) {
+                @SuppressWarnings("unchecked")
+                List<String> messages = settingsBuilder.validateSettings(settingsFile).getMessages();
+                for (String message : messages) {
+                    result.addMessage(message);
+                }
+            } else {
+                result.addMessage("Can not read settings file " + settings);
+            }
+        }
+
+        return result;
+    }
 }
