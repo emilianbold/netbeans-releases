e3e8eaee50b2bcb44d9f0772e86d142f8fa887bb
Andrey Yamkovoy <kaktus@netbeans.org>
2009-10-09 18:24:38 +0400

    Fix for #167449 - NullPointerException at org.netbeans.modules.j2ee.ejbcore.ejb.wizard.session.SessionEJBWizard.instantiate

--- j2ee.ejbcore/src/org/netbeans/modules/j2ee/ejbcore/ejb/wizard/mdb/MessageEJBWizard.java
+++ j2ee.ejbcore/src/org/netbeans/modules/j2ee/ejbcore/ejb/wizard/mdb/MessageEJBWizard.java
@@ -100,9 +100,9 @@ public final class MessageEJBWizard implements WizardDescriptor.InstantiatingIte
         
         // TODO: UI - add checkbox for Java EE 5 to create also EJB 2.1 style EJBs
         Profile profile = ejbModule.getJ2eeProfile();
-        boolean isSimplified = profile.equals(Profile.JAVA_EE_5) ||
-                               profile.equals(Profile.JAVA_EE_6_FULL) ||
-                               profile.equals(Profile.JAVA_EE_6_WEB);
+        boolean isSimplified = Profile.JAVA_EE_5.equals(profile) ||
+                               Profile.JAVA_EE_6_FULL.equals(profile) ||
+                               Profile.JAVA_EE_6_WEB.equals(profile);
         MessageGenerator generator = MessageGenerator.create(
                 Templates.getTargetName(wiz),
                 pkg,
