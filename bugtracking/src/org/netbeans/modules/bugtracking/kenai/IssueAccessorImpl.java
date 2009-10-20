8835ce3374aae7b85760d3793b5f70fb5a9b1d55
Tomas Stupka <tstupka@netbeans.org>
2009-10-20 12:51:30 +0200

    fix imports

--- bugtracking/src/org/netbeans/modules/bugtracking/kenai/IssueAccessorImpl.java
+++ bugtracking/src/org/netbeans/modules/bugtracking/kenai/IssueAccessorImpl.java
@@ -40,7 +40,6 @@ package org.netbeans.modules.bugtracking.kenai;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
