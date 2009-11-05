4979b581c956b0a642690c8b5a48a994d214e477
Nick N. Krasilnikov <nnnnnk@netbeans.org>
2009-11-05 22:02:39 +0300

    rolled back IZ#128119 : Error parser doesn't handle linenumber:column correctly

--- cnd.highlight/test/unit/src/org/netbeans/modules/cnd/highlight/error/UnresolvedIdentifierTest.java
+++ cnd.highlight/test/unit/src/org/netbeans/modules/cnd/highlight/error/UnresolvedIdentifierTest.java
@@ -242,11 +242,6 @@ public class UnresolvedIdentifierTest extends ErrorHighlightingBaseTestCase {
         performStaticTest("iz171453.cpp");
     }
 
-    public void testIZ175231() throws Exception {
-        // IZ#175231 : Error parser doesn't handle linenumber:column correctly
-        performStaticTest("iz175231.cpp");
-    }
-
     public void testIZ161565() throws Exception {
         // IZ#161565 : Usage of not-yet-declared function is not highlighted as error
         performStaticTest("iz161565.cpp");
@@ -274,6 +269,11 @@ public class UnresolvedIdentifierTest extends ErrorHighlightingBaseTestCase {
         @Override
         protected Class<?> getTestCaseDataClass() {
             return UnresolvedIdentifierTest.class;
+        }
+
+        public void testIZ175231() throws Exception {
+            // IZ#175231 : Error parser doesn't handle linenumber:column correctly
+            performStaticTest("iz175231.cpp");
         }
     }
 }
