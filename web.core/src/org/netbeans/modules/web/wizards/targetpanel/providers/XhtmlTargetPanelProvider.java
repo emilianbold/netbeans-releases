3e2f4859a8b8612b934935fa9dde0c1eda3bba1d
Denis Anisimov <ads@netbeans.org>
2009-11-07 13:05:54 +0300

    Fix for IZ#120810 - Javascript wizard offers uncomfortable path for new .js

--- web.core/src/org/netbeans/modules/web/wizards/targetpanel/providers/XhtmlTargetPanelProvider.java
+++ web.core/src/org/netbeans/modules/web/wizards/targetpanel/providers/XhtmlTargetPanelProvider.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2009 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -24,7 +24,7 @@
  * Contributor(s):
  *
  * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
  * Microsystems, Inc. All Rights Reserved.
  *
  * If you wish your version of this file to be governed by only the CDDL
@@ -38,56 +38,44 @@
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
  */
-package org.netbeans.modules.web.wizards;
+package org.netbeans.modules.web.wizards.targetpanel.providers;
 
-public class FileType {
+import org.netbeans.modules.target.iterator.spi.TargetPanelProvider;
+import org.netbeans.modules.web.wizards.FileType;
+import org.openide.util.NbBundle;
+import org.openide.util.lookup.ServiceProvider;
 
-    private String name,  suffix;
 
-    private FileType(String name, String suffix) {
-        this.name = name;
-        this.suffix = suffix;
-    }
-
-    @Override
-    public String toString() {
-        return name;
-    }
-
-    public String getSuffix() {
-        return suffix;
-    }
-    public static final FileType SERVLET =
-            new FileType("servlet", "java");
-    public static final FileType FILTER =
-            new FileType("filter", "java");
-    public static final FileType LISTENER =
-            new FileType("listener", "java");
-    public static final FileType JSP =
-            new FileType("jsp", "jsp");
-    public static final FileType JSF =
-            new FileType("jsf", "jsp");
-    public static final FileType JSPDOC =
-            new FileType("jspdoc", "jspx");
-    public static final FileType JSPF =
-            new FileType("jspf", "jspf");
-    public static final FileType TAG =
-            new FileType("tag_file", "tag");
-    public static final FileType TAGLIBRARY =
-            new FileType("tag_library", "tld");
-    public static final FileType TAG_HANDLER =
-            new FileType("tag_handler", "java");
-    public static final FileType HTML =
-            new FileType("html", "html");
-    public static final FileType XHTML =
-            new FileType("xhtml", "xhtml");
-    public static final FileType CSS =
-            new FileType("css", "css");
-    public static final FileType JS =
-            new FileType("javascript","js");
+/**
+ * @author ads
+ *
+ */
+@ServiceProvider(service=TargetPanelProvider.class)
+public class XhtmlTargetPanelProvider extends WebTargetPanelProvider<FileType> {
     
-    public static String IS_XML = "isXml";          // NOI18N
-    public static String IS_SEGMENT = "isSegment";  // NOI18N
-    public static String IS_FACELETS= "isFacelerts";// NOI18N
-} 
+    public XhtmlTargetPanelProvider(){
+        super("xhtml","LBL_XHtmlName"); // NOI18N
+    }
 
+    /* (non-Javadoc)
+     * @see org.netbeans.modules.target.iterator.spi.TargetPanelProvider#getWizardTitle()
+     */
+    public String getWizardTitle() {
+        return NbBundle.getMessage(XhtmlTargetPanelProvider.class, "TITLE_XHTML");// NOI18N
+    }
+    
+    /* (non-Javadoc)
+     * @see org.netbeans.modules.web.wizards.targetpanel.providers.WebTargetPanelProvider#getNewFileName()
+     */
+    public String getNewFileName() {
+        return super.getNewFileName()+"xhtml";//NOI18N
+    }
+
+    /* (non-Javadoc)
+     * @see org.netbeans.modules.target.iterator.spi.TargetPanelProvider#isApplicable(java.lang.Object)
+     */
+    public boolean isApplicable( FileType id ) {
+        return id == FileType.XHTML;
+    }
+
+}
