87ead16a29cb2b4444418caeb219f62ed3f5ba76
joshis <joshis@netbeans.org>
2009-11-09 18:06:28 +0100

    #176253 -  Clear KenaiPopupMenu caches on PROP_URL_CHANGED

--- kenai/src/org/netbeans/modules/kenai/api/KenaiProject.java
+++ kenai/src/org/netbeans/modules/kenai/api/KenaiProject.java
@@ -321,7 +321,7 @@ public final class KenaiProject {
             return m.group(repositoryPatternProjectGroup);
         }
         //hard coded support for external netbeans repositories
-        if (Kenai.getDefault().getUrl().getHost().equals("netbeans.org") && uri.startsWith("https://hg.netbeans.org") || uri.startsWith("http://hg.netbeans.org")) {//NOI18N
+        if (Kenai.getDefault().getUrl().getHost().equals("netbeans.org") && (uri.startsWith("https://hg.netbeans.org") || uri.startsWith("http://hg.netbeans.org"))) {//NOI18N
             return "ide";//NOI18N
         }
         return null;
