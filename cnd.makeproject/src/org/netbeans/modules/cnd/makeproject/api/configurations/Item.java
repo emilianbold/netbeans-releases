9f8ce72b19d6902649b057c3bd96c47f3ddc5164
Vladimir Voskresensky <vv159170@netbeans.org>
2009-11-10 18:30:24 +0300

    fixed IZ#175617 Adding,removing & renaming causes issues - files in physical folders also could be renamed

--- cnd.makeproject/src/org/netbeans/modules/cnd/makeproject/api/configurations/Item.java
+++ cnd.makeproject/src/org/netbeans/modules/cnd/makeproject/api/configurations/Item.java
@@ -86,15 +86,7 @@ public class Item implements NativeFileItem, PropertyChangeListener {
 //        }
         folder = null;
     }
-
-    /**
-     * Rename item.
-     * @param newname new name without suffic or path
-     */
-    public void rename(String newname) {
-        rename(newname, true);
-    }
-
+    
     private void rename(String newname, boolean nameWithoutExtension) {
         if (newname == null || newname.length() == 0 || getFolder() == null) {
             return;
@@ -135,14 +127,11 @@ public class Item implements NativeFileItem, PropertyChangeListener {
             newPath += path.substring(indexDot);
         }
         // Remove old item and insert new with new name
-        moveTo(newPath);
+        renameTo(newPath);
     }
 
-    public void moveTo(String newPath) {
+    private void renameTo(String newPath) {
         Folder f = getFolder();
-        if (f.isDiskFolder()) {
-            return;
-        }
         String oldPath = getAbsPath();
         Item item = new Item(newPath);
         f.addItem(item);
@@ -237,7 +226,7 @@ public class Item implements NativeFileItem, PropertyChangeListener {
                 newPath = IpeUtils.toRelativePath(getFolder().getConfigurationDescriptor().getBaseDir(), newPath);
             }
             newPath = FilePathAdaptor.normalize(newPath);
-            moveTo(newPath);
+            renameTo(newPath);
         }
     }
 
