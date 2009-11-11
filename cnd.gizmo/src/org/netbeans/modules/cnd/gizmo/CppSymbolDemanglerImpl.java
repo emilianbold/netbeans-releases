7627b7266b3da1e873e5e5afbfd1e3c9a2dc92e5
Alexander Simon <alexvsimon@netbeans.org>
2009-11-11 12:23:05 +0300

    fixed IZ#176437 demangler does not work for SunStudio

--- cnd.gizmo/src/org/netbeans/modules/cnd/gizmo/CppSymbolDemanglerImpl.java
+++ cnd.gizmo/src/org/netbeans/modules/cnd/gizmo/CppSymbolDemanglerImpl.java
@@ -283,7 +283,7 @@ public class CppSymbolDemanglerImpl implements CppSymbolDemangler {
 
         final NativeProcessBuilder npb = NativeProcessBuilder.newProcessBuilder(env);
 
-        if (GCPPFILT.equals(demanglerTool) || CPPFILT.equals(demanglerTool)) {
+        if (demanglerTool.indexOf(GCPPFILT) >= 0 || demanglerTool.indexOf(CPPFILT) >=0) {
             StringBuilder cmdline = new StringBuilder();
             cmdline.append(ECHO).append(" \""); // NOI18N
             for (String name : mangledNames) {
