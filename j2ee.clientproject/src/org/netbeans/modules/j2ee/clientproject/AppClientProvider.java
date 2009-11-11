4ef5566df93615550d6dc090b1d83cd4b6565409
phejl <phejl@netbeans.org>
2009-11-11 11:14:12 +0100

    #176475 NPE in ProjectEar$EarResourceChangeReporter.isServerResourceChanged

--- j2ee.clientproject/src/org/netbeans/modules/j2ee/clientproject/AppClientProvider.java
+++ j2ee.clientproject/src/org/netbeans/modules/j2ee/clientproject/AppClientProvider.java
@@ -472,10 +472,13 @@ public final class AppClientProvider extends J2eeModuleProvider
 
         public boolean isServerResourceChanged(long lastDeploy) {
             File resDir = getResourceDirectory();
-            if (resDir != null) {
-                for (File file : resDir.listFiles()) {
-                    if (file.lastModified() > lastDeploy) {
-                        return true;
+            if (resDir != null && resDir.exists() && resDir.isDirectory()) {
+                File[] children = resDir.listFiles();
+                if (children != null) {
+                    for (File file : children) {
+                        if (file.lastModified() > lastDeploy) {
+                            return true;
+                        }
                     }
                 }
             }
