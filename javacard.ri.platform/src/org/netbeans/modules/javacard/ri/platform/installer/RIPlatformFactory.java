0b07c3cf245529983527859776db5be12b91fbfd
Anki Nelaturu <ankinelaturu@netbeans.org>
2009-11-17 10:21:03 -0800

    Partial changes for nosuspend fix did not go in earlier change set properly. Doing remaining now.

--- javacard.ri.platform/src/org/netbeans/modules/javacard/ri/platform/installer/RIPlatformFactory.java
+++ javacard.ri.platform/src/org/netbeans/modules/javacard/ri/platform/installer/RIPlatformFactory.java
@@ -199,7 +199,7 @@ final class RIPlatformFactory implements Mutex.ExceptionAction<FileObject> {
         if (!canInstall(platformProps)) {
             throw new IOException (NbBundle.getMessage(RIPlatformFactory.class,
                     "ERR_TOO_OLD", //NOI18N
-                    platformProps.get(JavacardPlatformKeyNames.PLATFORM_JAVACARD_SPECIFICATION_VERSION),
+                    platformProps.get(JavacardPlatformKeyNames.PLATFORM_JAVACARD_VERSION),
                     MINIMUM_SUPPORTED_VERSION)); //NOI18N
         }
         //Translate UNIX-style relative paths into platform-specific absolute
