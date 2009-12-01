5676fd5ce6c4aa875c1368ef6cb4e0b183078d39
Alexander Simon <alexvsimon@netbeans.org>
2009-12-01 16:43:55 +0300

    - add execute in dir method - do not remove new lines from output

--- dlight.nativeexecution/src/org/netbeans/modules/nativeexecution/api/util/ProcessUtils.java
+++ dlight.nativeexecution/src/org/netbeans/modules/nativeexecution/api/util/ProcessUtils.java
@@ -137,9 +137,14 @@ public final class ProcessUtils {
 
         try {
             String line;
+            boolean first = true;
 
             while ((line = br.readLine()) != null) {
+                if (!first) {
+                    result.append('\n');
+                }
                 result.append(line);
+                first = false;
             }
         } finally {
             if (br != null) {
@@ -222,6 +227,10 @@ public final class ProcessUtils {
 
     public static ExitStatus execute(final ExecutionEnvironment execEnv, final String executable, final String... args) {
         return execute(NativeProcessBuilder.newProcessBuilder(execEnv).setExecutable(executable).setArguments(args));
+    }
+
+    public static ExitStatus executeInDir(final String workingDir, final ExecutionEnvironment execEnv,  final String executable, final String... args) {
+        return execute(NativeProcessBuilder.newProcessBuilder(execEnv).setExecutable(executable).setArguments(args).setWorkingDirectory(workingDir));
     }
 
     /**
