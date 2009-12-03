53b9ffe28c749cf560966a39ec85a7faa9e73a45
Tomas Holy <t_h@netbeans.org>
2009-12-03 17:35:19 +0100

    #178057: TableView does not show popup menu on a selected read only cell

--- openide.explorer/src/org/openide/explorer/view/TableView.java
+++ openide.explorer/src/org/openide/explorer/view/TableView.java
@@ -625,6 +625,9 @@ public class TableView extends JScrollPane {
             if (o instanceof Node.Property) { // && (e == null || e instanceof KeyEvent)) {
                 //Toggle booleans without instantiating an editor
                 Node.Property p = (Node.Property)o;
+                if (!p.canWrite()) {
+                    return false;
+                }
                 if (p.getValueType() == Boolean.class || p.getValueType() == Boolean.TYPE) {
                     PropertiesRowModel.toggleBooleanProperty(p);
                     Rectangle r = getCellRect(row, column, true);
