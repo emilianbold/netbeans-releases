ce00de1507073e9b2cd543ef1a1d9c4f8dd6c590
Ondrej Vrabec <ovrabec@netbeans.org>
2009-12-04 11:01:43 +0100

    Files in conflict displayed in Versioning output view even after resolving conflicts

--- subversion/src/org/netbeans/modules/subversion/ui/update/UpdateResultsTable.java
+++ subversion/src/org/netbeans/modules/subversion/ui/update/UpdateResultsTable.java
@@ -88,6 +88,10 @@ class UpdateResultsTable implements MouseListener, ListSelectionListener, Ancest
     
     private String [] tableColumns; 
     private TableSorter sorter;
+    /**
+     * File is in conflict and is not a directory
+     */
+    private static final int ACTION_CONFLICTED_FILE = FileUpdateInfo.ACTION_CONFLICTED | FileUpdateInfo.ACTION_TYPE_FILE;
 
     /**
      * Defines labels for Versioning view table columns.
@@ -272,7 +276,7 @@ class UpdateResultsTable implements MouseListener, ListSelectionListener, Ancest
     }
     private static ActionEvaluator conflictEvaluator = new ActionEvaluator() {
         public boolean isAction(FileUpdateInfo info) {
-            return (info.getAction() & FileUpdateInfo.ACTION_CONFLICTED & FileUpdateInfo.ACTION_TYPE_FILE) != 0; 
+            return (info.getAction() & ACTION_CONFLICTED_FILE) == ACTION_CONFLICTED_FILE;
         }
     };
     private static ActionEvaluator notDeletedEvaluator = new ActionEvaluator() {
@@ -419,9 +423,9 @@ class UpdateResultsTable implements MouseListener, ListSelectionListener, Ancest
                 FileUpdateInfo fui = (FileUpdateInfo) node.getLookup().lookup(FileUpdateInfo.class);
                 if(fui != null) {                    
                     int action = fui.getAction();
-                    if((action & FileUpdateInfo.ACTION_CONFLICTED & FileUpdateInfo.ACTION_TYPE_FILE) != 0 && 
+                    if((action & ACTION_CONFLICTED_FILE) == ACTION_CONFLICTED_FILE &&
                        fui.getFile().equals(changedFile) && 
-                       ( (newFileInfo.getStatus() & newFileInfo.STATUS_VERSIONED_CONFLICT) == 0) ) 
+                       ( (newFileInfo.getStatus() & FileInformation.STATUS_VERSIONED_CONFLICT) == 0) )
                     {
                         action &= ~FileUpdateInfo.ACTION_CONFLICTED;
                         action |=  FileUpdateInfo.ACTION_CONFLICTED_RESOLVED;
