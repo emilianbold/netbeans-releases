f8f4e3e699d9b2f9a18d38e947178a4cbd54626e
Tomas Stupka <tstupka@netbeans.org>
2009-12-15 15:34:35 +0100

    minor cleanup Issue #146634 - add VQ to the versioning api

--- versioning/test/unit/src/org/netbeans/modules/versioning/spi/testvcs/TestVCSVisibilityQuery.java
+++ versioning/test/unit/src/org/netbeans/modules/versioning/spi/testvcs/TestVCSVisibilityQuery.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2009 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 2009 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -21,12 +21,6 @@
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
  *
- * Contributor(s):
- *
- * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
- * Microsystems, Inc. All Rights Reserved.
- *
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -37,63 +31,28 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
+ *
+ * Contributor(s):
+ *
+ * Portions Copyrighted 2009 Sun Microsystems, Inc.
  */
-package org.netbeans.modules.versioning.spi.testvcs;
 
-import org.netbeans.modules.versioning.spi.VersioningSystem;
-import org.netbeans.modules.versioning.spi.VCSInterceptor;
-import org.netbeans.modules.versioning.spi.VCSAnnotator;
+package org.netbeans.modules.versioning.spi.testvcs;
 
 import java.io.File;
 import org.netbeans.modules.versioning.spi.VCSVisibilityQuery;
 
 /**
- * Test versioning system.
- * 
- * @author Maros Sandor
+ *
+ * @author Tomas Stupka
  */
-@org.openide.util.lookup.ServiceProvider(service=org.netbeans.modules.versioning.spi.VersioningSystem.class)
-public class TestVCS extends VersioningSystem {
+public class TestVCSVisibilityQuery extends VCSVisibilityQuery {
 
-    private static TestVCS instance;
-    private VCSInterceptor interceptor;
-    private VCSAnnotator annotator;
-    private VCSVisibilityQuery vq;
-
-    public static final String VERSIONED_FOLDER_SUFFIX = "-test-versioned";
-
-    public static TestVCS getInstance() {
-        return instance;
-    }
-    
-    public TestVCS() {
-        instance = this;
-        interceptor = new TestVCSInterceptor();
-        annotator = new TestVCSAnnotator();
-        vq = new TestVCSVisibilityQuery();
-    }
-
-    public File getTopmostManagedAncestor(File file) {
-        File topmost = null;
-        for (; file != null; file = file.getParentFile()) {
-            if (file.getName().endsWith(VERSIONED_FOLDER_SUFFIX)) {
-                topmost = file;
-            }
-        }
-        return topmost;
-    }
-
-    public VCSInterceptor getVCSInterceptor() {
-        return interceptor;
-    }
-
-    public VCSAnnotator getVCSAnnotator() {
-        return annotator;
-    }
+    public static final String INVISIBLE_FILE_SUFFIX = "invisible";
 
     @Override
-    public VCSVisibilityQuery getVisibilityQuery() {
-        return vq;
-}
+    public boolean isVisible(File file) {
+        return !file.getName().endsWith(INVISIBLE_FILE_SUFFIX);
+    }
 
 }
