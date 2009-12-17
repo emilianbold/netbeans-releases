b27c6c677af6646c315fe0b38c8d9d80f5f35c1b
Tomas Stupka <tstupka@netbeans.org>
2009-12-17 17:29:14 +0100

    and fix one more broken import

--- bugtracking/src/org/netbeans/modules/bugtracking/ui/nodes/RepositoryNode.java
+++ bugtracking/src/org/netbeans/modules/bugtracking/ui/nodes/RepositoryNode.java
@@ -43,7 +43,6 @@ import java.awt.Image;
 import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
-import org.netbeans.modules.bugtracking.BugtrackingManager;
 import org.netbeans.modules.bugtracking.RepositoriesSupport;
 import org.netbeans.modules.bugtracking.spi.Repository;
 import org.netbeans.modules.bugtracking.ui.issue.IssueAction;
@@ -54,6 +53,7 @@ import org.openide.NotifyDescriptor;
 import org.openide.nodes.AbstractNode;
 import org.openide.nodes.Children;
 import org.openide.util.NbBundle;
+import org.openide.util.RequestProcessor;
 import org.openide.util.actions.SystemAction;
 
 /**
