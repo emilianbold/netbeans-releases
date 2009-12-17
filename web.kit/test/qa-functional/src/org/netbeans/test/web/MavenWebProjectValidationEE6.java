92be129809a6d6a32c90837b139306acbc43b03e
Jindrich Sedek <jsedek@netbeans.org>
2009-12-17 11:58:41 +0100

    fixing web kit tests

--- web.kit/test/qa-functional/src/org/netbeans/test/web/MavenWebProjectValidationEE6.java
+++ web.kit/test/qa-functional/src/org/netbeans/test/web/MavenWebProjectValidationEE6.java
@@ -1,8 +1,8 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- *
- * Copyright 1997-2009 Sun Microsystems, Inc. All rights reserved.
- *
+ * 
+ * Copyright 2008 Sun Microsystems, Inc. All rights reserved.
+ * 
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
  * Development and Distribution License("CDDL") (collectively, the
@@ -20,13 +20,7 @@
  * License Header, with the fields enclosed by brackets [] replaced by
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- *
- * Contributor(s):
- *
- * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
- * Microsystems, Inc. All Rights Reserved.
- *
+ * 
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -37,66 +31,55 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
+ * 
+ * Contributor(s):
+ * 
+ * Portions Copyrighted 2008 Sun Microsystems, Inc.
  */
+
 package org.netbeans.test.web;
 
-import java.io.File;
-import java.io.IOException;
 import junit.framework.Test;
-import org.netbeans.jellytools.nodes.Node;
-import org.netbeans.jellytools.ProjectsTabOperator;
-import org.netbeans.jellytools.actions.RedeployProjectAction;
+import org.netbeans.jellytools.modules.j2ee.J2eeTestCase;
 import org.netbeans.junit.NbModuleSuite;
 
 /**
+ *
+ * @author Jindrich Sedek
  */
-public class WebProjectValidationEE5 extends WebProjectValidation {
+public class MavenWebProjectValidationEE6 extends MavenWebProjectValidation {
 
     static {
-        PROJECT_NAME = "WebJ2EE5Project"; // NOI18N
+        PROJECT_NAME = "WebMavenProject";
+        PROJECT_LOCATION = new MavenWebProjectValidation().getWorkDirPath();
     }
 
-    /** Need to be defined because of JUnit */
-    public WebProjectValidationEE5(String name) {
+    public MavenWebProjectValidationEE6(String name) {
         super(name);
     }
 
-    /** Need to be defined because of JUnit */
-    public WebProjectValidationEE5() {
-        super();
+    public MavenWebProjectValidationEE6() {
+        this("MavenWebProjectValidation");
     }
 
     public static Test suite() {
-        NbModuleSuite.Configuration conf = NbModuleSuite.createConfiguration(WebProjectValidationEE5.class);
-        conf = addServerTests(Server.GLASSFISH_V3, conf,
-              "testPreconditions", "testNewWebProject", "testRedeployProject",
+        NbModuleSuite.Configuration conf = NbModuleSuite.createConfiguration(MavenWebProjectValidationEE6.class);
+        conf = addServerTests(J2eeTestCase.Server.GLASSFISH_V3, conf,
+              "testPreconditions", "testNewMavenWebProject", "testRegisterTomcat",
               "testNewJSP", "testNewJSP2", "testNewServlet", "testNewServlet2",
-              "testCompileAllJSP", "testCompileJSP",
-              "testCleanAndBuildProject", "testRunProject", "testRunJSP", "testViewServlet",
-              "testRunServlet", "testCreateTLD", "testCreateTagHandler", "testRunTag",
-              "testNewHTML", "testRunHTML", "testNewSegment", "testNewDocument",
+              "testCleanAndBuildProject", "testRunProject", "testRunJSP", //"testViewServlet",
+              "testRunServlet","testCreateTLD", "testCreateTagHandler",
+              "testRunTag","testNewHTML", "testRunHTML",
+              "testNewSegment", "testNewDocument",
+//              "testJSPNavigator", "testHTMLNavigator",
               "testStopServer", "testStartServer", "testBrowserSettings", "testFinish"
                );
         conf = conf.enableModules(".*").clusters(".*");
-        return NbModuleSuite.create(conf); 
+        return NbModuleSuite.create(conf);
     }
 
     @Override
     protected String getEEVersion() {
-        return JAVA_EE_5;
-    }
-
-    public void testRedeployProject() throws IOException {
-        Node rootNode = new ProjectsTabOperator().getProjectRootNode(PROJECT_NAME);
-        Util.cleanStatusBar();
-        new RedeployProjectAction().perform(rootNode);
-        waitBuildSuccessful();
-        logAndCloseOutputs();
-    }
-
-    //********************************************************
-    public String getProjectFolder(String project) {
-        return PROJECT_LOCATION + File.separator + project;
+        return JAVA_EE_6;
     }
 }
-
