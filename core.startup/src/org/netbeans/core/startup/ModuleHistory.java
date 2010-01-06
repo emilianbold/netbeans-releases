31cb3658419577993cbc5a84b741326a808e6c66
Jesse Glick <jglick@netbeans.org>
2010-01-06 13:13:12 -0500

    Issue 113341: pick up the newest copy of a module if found in multiple clusters. Issue 36701: permit InstalledFileLocator to return multiple results. Also removing some complexity from ModuleList and config/Modules/*.xml: * no need to store release or specversion attributes * drop support for very old "origin" attributes * remove deprecated processing of ModuleInstall serialized forms * do not bother calling deprecated ModuleInstall.installed or .updated methods * get rid of most of ModuleHistory (maybe delete entirely)

--- core.startup/src/org/netbeans/core/startup/ModuleHistory.java
+++ core.startup/src/org/netbeans/core/startup/ModuleHistory.java
@@ -47,26 +47,13 @@ package org.netbeans.core.startup;
 // and ModuleInfo-related things). Should be possible to use without
 // the rest of core.
 
-import org.openide.modules.SpecificationVersion;
-
 /** Representation of the history of the module.
- * This includes information such as: whether the module
- * has been installed before and now just needs to be restored
- * (or in fact where it was installed); the previous version
- * of the module, in case it needs to be upgraded.
- * Used for communication
- * between the module lister and the module installer.
  * @author Jesse Glick
  */
-
+// XXX no longer useful, could probably delete, and deprecate Module.history
 public final class ModuleHistory {
     
     private final String jar;
-    private int oldMajorVers;
-    private SpecificationVersion oldSpecVers;
-    private boolean upgraded;
-    private byte[] installerState;
-    private boolean installerStateChanged = false;
     
     /** Create a module history with essential information.
      * You also need to specify a relative or absolute JAR name.
@@ -74,10 +61,6 @@ public final class ModuleHistory {
     public ModuleHistory(String jar) {
         assert jar != null;
         this.jar = jar;
-        upgraded = false;
-        oldMajorVers = -1;
-        oldSpecVers = null;
-        installerState = null;
     }
     
     /**
@@ -86,68 +69,6 @@ public final class ModuleHistory {
      */
     String getJar() {
         return jar;
-    }
-    
-    /** True if this module has been installed before. */
-    boolean isPreviouslyInstalled() {
-        return upgraded;
-    }
-    
-    /** The old major version of the module,
-     * before an upgrade.
-     * -1 if unspecified, or it has never been installed before.
-     */
-    int getOldMajorVersion() {
-        return oldMajorVers;
-    }
-    
-    /** The old specification version of the module,
-     * before an upgrade.
-     * null if unspecified, or it has never been installed before.
-     */
-    SpecificationVersion getOldSpecificationVersion() {
-        return oldSpecVers;
-    }
-    
-    /** Signal that a module has been previously installed,
-     * marking it as a possible candidate for upgrade.
-     */
-    void upgrade(int oldMajorVersion, SpecificationVersion oldSpecificationVersion) {
-        upgraded = true;
-        oldMajorVers = oldMajorVersion;
-        oldSpecVers = oldSpecificationVersion;
-    }
-    
-    /** Get the stored state of the ModuleInstall, if any.
-     * Currently this would be a serialized bytestream.
-     * null if unknown or there was no stored state.
-     */
-    byte[] getInstallerState() {
-        return installerState;
-    }
-    
-    /** Set the stored state of the ModuleInstall.
-     * This may be null to indicate that no state
-     * needs to be stored. Otherwise it would currently
-     * be a serialized bytestream.
-     */
-    void setInstallerState(byte[] state) {
-        if (installerState != null && state != null) {
-            installerStateChanged = true;
-        }
-        installerState = state;
-    }
-    
-    /** True if the state of the installer has changed dynamically. */
-    boolean getInstallerStateChanged() {
-        return installerStateChanged;
-    }
-    
-    /** Reset history after an uninstall. */
-    void resetHistory() {
-        upgraded = false;
-        installerState = null;
-        installerStateChanged = false;
     }
     
 }
