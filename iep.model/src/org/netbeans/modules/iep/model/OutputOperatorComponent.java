5118e3683833d9d9e05c77f491aa884a62af5c53
Jesse Glick <jglick@netbeans.org>
2010-01-12 15:14:43 -0500

    Continuation of 24e6c46d4f70: removing more soa-related modules from sources.

--- iep.model/src/org/netbeans/modules/iep/model/OutputOperatorComponent.java
+++
@@ -1,8 +0,0 @@
-package org.netbeans.modules.iep.model;
-
-public interface OutputOperatorComponent extends OperatorComponent, MultiWSDLComponentReference {
-
-    public static String PROP_WS_OUTPUT_KEY = "wsOutput";
-    
-    boolean isWebServiceOutput();
-}
