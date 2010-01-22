0df3729c7034fc24be2c94c8260305b180ca9764
Tomas Pavek <tpavek@netbeans.org>
2010-01-22 17:38:06 +0100

    #179795: attempt to make the search field text apear centered vertically

--- kenai.ui/src/org/netbeans/modules/kenai/ui/SearchField.java
+++ kenai.ui/src/org/netbeans/modules/kenai/ui/SearchField.java
@@ -131,7 +131,8 @@ public class SearchField extends JPanel implements ActionListener {
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 2;
         gridBagConstraints.gridy = 0;
-        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.CENTER;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
         gridBagConstraints.weightx = 1.0;
         gridBagConstraints.weighty = 1.0;
         gridBagConstraints.insets = new java.awt.Insets(0, 0, 0, 2);
