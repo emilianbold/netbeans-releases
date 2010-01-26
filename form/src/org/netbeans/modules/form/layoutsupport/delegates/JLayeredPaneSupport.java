0582d455915d69d44c8c56eb33ce776d1f83c36d
Jan Stola <jstola@netbeans.org>
2010-01-26 11:41:48 +0100

    Using System.arraycopy instead of manual array copy.

--- form/src/org/netbeans/modules/form/layoutsupport/delegates/JLayeredPaneSupport.java
+++ form/src/org/netbeans/modules/form/layoutsupport/delegates/JLayeredPaneSupport.java
@@ -310,9 +310,7 @@ public class JLayeredPaneSupport extends AbsoluteLayoutSupport {
                     { // disabling this method due to limited persistence
                     } // capabilities (compatibility with previous versions)
                 };
-
-            for (int i=0; i < props.length; i++)
-                layeredProps[i+1] = props[i];
+            System.arraycopy(props, 0, layeredProps, 1, props.length);
 
             return layeredProps;
         }
