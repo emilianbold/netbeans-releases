9376a0a0b139b02ad0e9af2079491283eb82961d
Petr Pisl <ppisl@netbeans.org>
2010-02-04 15:24:34 +0100

    issue #176224 - wrong formatting in multiline arrays inside function calls

--- php.editor/src/org/netbeans/modules/php/editor/indent/IndentLevelCalculator.java
+++ php.editor/src/org/netbeans/modules/php/editor/indent/IndentLevelCalculator.java
@@ -169,7 +169,10 @@ public class IndentLevelCalculator extends DefaultTreePathVisitor {
             ArrayElement lastElem = node.getElements().get(node.getElements().size() - 1);
             int end = lastElem.getEndOffset();
 
-            if (!PARENTS_WITHOUT_CONT_INDENT.contains(parentClass)){
+            if ((parentClass == FunctionInvocation.class
+		    && ((FunctionInvocation)getPath().get(0)).getParameters().size() > 1)
+		    || !PARENTS_WITHOUT_CONT_INDENT.contains(parentClass)){
+//	    if (!PARENTS_WITHOUT_CONT_INDENT.contains(parentClass)){
                 addIndentLevel(start, itemsInArrayDeclarationSize);
                 addIndentLevel(end, -1 * itemsInArrayDeclarationSize);
             }
@@ -185,6 +188,17 @@ public class IndentLevelCalculator extends DefaultTreePathVisitor {
     @Override
     public void visit(InfixExpression node) {
         Class parentClass = getPath().get(0).getClass();
+
+        if (!PARENTS_WITHOUT_CONT_INDENT.contains(parentClass)){
+            indentContinuationWithinStatement(node);
+        }
+
+        super.visit(node);
+    }
+
+    @Override
+    public void visit(FunctionInvocation node) {
+	Class parentClass = getPath().get(0).getClass();
 
         if (!PARENTS_WITHOUT_CONT_INDENT.contains(parentClass)){
             indentContinuationWithinStatement(node);
