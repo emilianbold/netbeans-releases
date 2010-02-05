9b31feea1c95a06b8d1d033a529d2158f72f344d
Petr Pisl <ppisl@netbeans.org>
2010-02-05 21:20:59 +0100

    Adding foramat options of spaces around operators.

--- php.editor/src/org/netbeans/modules/php/editor/indent/WSTransformer.java
+++ php.editor/src/org/netbeans/modules/php/editor/indent/WSTransformer.java
@@ -44,16 +44,13 @@ import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.TreeSet;
-import org.netbeans.api.editor.EditorUtilities;
 import org.netbeans.api.lexer.Token;
 import org.netbeans.api.lexer.TokenSequence;
 import org.netbeans.api.lexer.TokenUtilities;
-import org.netbeans.editor.Utilities;
 import org.netbeans.modules.editor.indent.spi.Context;
 import org.netbeans.modules.php.editor.lexer.LexUtilities;
 import org.netbeans.modules.php.editor.lexer.PHPTokenId;
 import org.netbeans.modules.php.editor.parser.astnodes.ASTNode;
-import org.netbeans.modules.php.editor.parser.astnodes.ArrayCreation;
 import org.netbeans.modules.php.editor.parser.astnodes.Block;
 import org.netbeans.modules.php.editor.parser.astnodes.CatchClause;
 import org.netbeans.modules.php.editor.parser.astnodes.ClassDeclaration;
@@ -90,7 +87,7 @@ class WSTransformer extends DefaultTreePathVisitor {
     private Collection<CodeRange> unbreakableRanges = new TreeSet<CodeRange>();
     private Collection<Integer> breakPins = new LinkedList<Integer>();
 
-    private final Collection<PHPTokenId> WS_AND_COMMENT_TOKENS = Arrays.asList(PHPTokenId.PHPDOC_COMMENT_START,
+    private final List<PHPTokenId> WS_AND_COMMENT_TOKENS = Arrays.asList(PHPTokenId.PHPDOC_COMMENT_START,
             PHPTokenId.PHPDOC_COMMENT_END, PHPTokenId.PHPDOC_COMMENT, PHPTokenId.WHITESPACE,
             PHPTokenId.PHP_COMMENT_START, PHPTokenId.PHP_COMMENT_END, PHPTokenId.PHP_COMMENT,
             PHPTokenId.PHP_LINE_COMMENT);
@@ -103,7 +100,17 @@ class WSTransformer extends DefaultTreePathVisitor {
     private final Collection<PHPTokenId> NO_BREAK_B4_TKNS = Arrays.asList(PHPTokenId.PHP_CLOSETAG,
             PHPTokenId.PHP_ELSE, PHPTokenId.PHP_ELSEIF, PHPTokenId.PHP_ELSE, PHPTokenId.PHP_CATCH,
             PHPTokenId.PHP_WHILE);
-    
+
+    private final List<String> ASSIGN_OPERATORS =  Arrays.asList(
+	    "=", ".=", "=.", "+=", "=+", "=-", "-=" //NOI18N
+	    );
+    private final List<String> BINARY_OPERATORS =  Arrays.asList(
+	    "+", "-", "*", "/", "<", ">", "<>", "<=", ">=", "==", "===", //NOI18N
+	    "%", "&", "|", "^", "~", "<<", ">>", "!=", "!==", ".", "&&", "||"
+	    );
+    private final List<String> UNARY_OPERATOS = Arrays.asList(
+	    "++", "--", "!" //NOI8N
+	    );
     // keep information, whether the function declaration is visited directly or from method declaration
     private boolean isMethod;
 
@@ -111,6 +118,28 @@ class WSTransformer extends DefaultTreePathVisitor {
         this.context = context;
         isMethod = false;
     }
+
+    @Override
+    public void visit(Program node) {
+	super.visit(node);
+	TokenSequence<PHPTokenId> ts = tokenSequence(node.getStartOffset());
+        Token<? extends PHPTokenId> token;
+
+	while (ts.moveNext()) {
+	    token = LexUtilities.findNextToken(ts, Arrays.asList(PHPTokenId.PHP_TOKEN, PHPTokenId.PHP_OPERATOR));
+	    String text = token.text().toString();
+	    if (ASSIGN_OPERATORS.contains(text)) {
+			checkSpaceAroundToken(ts, CodeStyle.get(context.document()).spaceAroundAssignOps());
+	    }
+	    else if (BINARY_OPERATORS.contains(text)) {
+		checkSpaceAroundToken(ts, CodeStyle.get(context.document()).spaceAroundBinaryOps());
+	    }
+	    else if (UNARY_OPERATOS.contains(text)) {
+		checkSpaceAroundToken(ts, CodeStyle.get(context.document()).spaceAroundUnaryOps());
+	    }
+	}
+    }
+
 
     @Override
     public void visit(Block node) {
@@ -492,8 +521,6 @@ class WSTransformer extends DefaultTreePathVisitor {
         super.visit(node);
     }
 
-
-
     private void checkSpaceBetweenCurlyCloseAndToken(int offset, boolean insertSpace,
              final List<PHPTokenId> beforeTokens) {
         TokenSequence<PHPTokenId> ts = tokenSequence(offset);
@@ -519,11 +546,26 @@ class WSTransformer extends DefaultTreePathVisitor {
         }
     }
 
+    private void checkSpaceAroundToken(TokenSequence<PHPTokenId>ts, boolean insertSpace) {
+	int offset = ts.offset();
+	replaceSpaceBeforeToken(ts, insertSpace, null);
+	ts.move(offset);
+	ts.moveNext();
+
+	if (ts.moveNext()) {
+	    LexUtilities.findNext(ts, WS_AND_COMMENT_TOKENS);
+	    replaceSpaceBeforeToken(ts, insertSpace, null);
+	    ts.move(offset);
+	    ts.moveNext();
+	}
+    }
+
     private void replaceSpaceBeforeToken(TokenSequence<PHPTokenId> ts, boolean space,
             final List<PHPTokenId> afterTokens) {
         if (ts.movePrevious()) {
             Token<? extends PHPTokenId> token = ts.token();
-            if ((afterTokens.contains(token.id())
+            if (((afterTokens == null && token.id() != PHPTokenId.WHITESPACE)
+		    || (afterTokens != null && afterTokens.contains(token.id()))
                     || token.id() == PHPTokenId.PHP_COMMENT_END) && space) {
                 replacements.add(new Replacement(ts.offset() + token.length(), 0, " ")); //NOI18N
             } else if (token.id() == PHPTokenId.WHITESPACE && countOfNewLines(token.text()) == 0) {
@@ -974,4 +1016,5 @@ class WSTransformer extends DefaultTreePathVisitor {
         FIELD,
         UNKNOWN;
     }
+    
 }
--- php.editor/src/org/netbeans/modules/php/editor/indent/ui/FmtSpaces.java
+++ php.editor/src/org/netbeans/modules/php/editor/indent/ui/FmtSpaces.java
@@ -252,11 +252,11 @@ public class FmtSpaces extends JPanel implements TreeCellRenderer, MouseListener
                 new Item(spaceBeforeSwitchParen)
                 ),
     
-//            new Item("AroundOperators",                         // NOI18N
-//                new Item(spaceAroundUnaryOps),
-//                new Item(spaceAroundBinaryOps),
-//                new Item(spaceAroundTernaryOps),
-//                new Item(spaceAroundAssignOps) ),
+            new Item("AroundOperators",                         // NOI18N
+                new Item(spaceAroundUnaryOps),
+                new Item(spaceAroundBinaryOps),
+                new Item(spaceAroundTernaryOps),
+                new Item(spaceAroundAssignOps) ),
             
             new Item("BeforeLeftBraces",                        // NOI18N
                 new Item(spaceBeforeClassDeclLeftBrace),
