7ff736ac1480f60a1b0f540ab350a02b37369411
Radek Matous <rmatous@netbeans.org>
2010-02-08 12:38:25 +0100

    #178133 - 6.8RC1 (With PHP) becomes slow for no obvious reason

--- php.editor/src/org/netbeans/modules/php/editor/model/impl/FunctionScopeImpl.java
+++ php.editor/src/org/netbeans/modules/php/editor/model/impl/FunctionScopeImpl.java
@@ -63,7 +63,7 @@ import org.netbeans.modules.php.editor.parser.astnodes.Variable;
  */
 class FunctionScopeImpl extends ScopeImpl implements FunctionScope, VariableNameFactory {
     private List<? extends Parameter> paremeters;
-    String returnType;
+    volatile String returnType;
 
     //new contructors
     FunctionScopeImpl(Scope inScope, FunctionDeclarationInfo info, String returnType) {
@@ -130,9 +130,14 @@ class FunctionScopeImpl extends ScopeImpl implements FunctionScope, VariableName
 
     public Collection<? extends TypeScope> getReturnTypes(boolean resolve) {
         Collection<TypeScope> retval = Collections.<TypeScope>emptyList();
-        if (returnType != null && returnType.length() > 0) {
-            retval = new ArrayList<TypeScope>();
-            for (String typeName : returnType.split("\\|")) {//NOI18N
+        String types = null;
+        synchronized(this) {
+            types = returnType;
+        }
+        if (types != null && types.length() > 0) {
+            boolean evaluate = types.indexOf("@") != -1;//NOI18N
+            retval = new HashSet<TypeScope>();
+            for (String typeName : types.split("\\|")) {//NOI18N
                 if (typeName.trim().length() > 0) {
                     if (resolve && typeName.contains("@")) {//NOI18N
                         try {
@@ -147,13 +152,19 @@ class FunctionScopeImpl extends ScopeImpl implements FunctionScope, VariableName
                     }
                 }
             }
-            returnType = null;//NOI18N
-            for (TypeScope typeScope : retval) {
-                if (returnType == null) {
-                    returnType = typeScope.getNamespaceName().append(typeScope.getName()).toString();
-                } else {
-                    returnType += "|"+typeScope.getNamespaceName().append(typeScope.getName()).toString();//NOI18N
+            if (evaluate) {
+                StringBuilder sb = new StringBuilder();
+                for (TypeScope typeScope : retval) {
+                    if (sb.length() != 0 ) {
+                        sb.append("|");//NOI18N
+                    }
+                    sb.append(typeScope.getNamespaceName().append(typeScope.getName()).toString());
                 }
+                synchronized(this) {
+                    if (types.equals(returnType)) {
+                        returnType = sb.toString();
+                    }
+                }                
             }
         }
         return retval;
