166612924e64f1b7a05a3062cdcef4e2485929e0
Jan Jancura <jjancura@netbeans.org>
2010-03-08 15:28:38 +0100

    #181275: odd error highlight in quick search

--- spellchecker.apimodule/src/org/netbeans/modules/spellchecker/api/Spellchecker.java
+++ spellchecker.apimodule/src/org/netbeans/modules/spellchecker/api/Spellchecker.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2007 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 2010 Sun Microsystems, Inc. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common
@@ -21,12 +21,6 @@
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
  *
- * Contributor(s):
- *
- * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
- * Microsystems, Inc. All Rights Reserved.
- *
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -37,41 +31,47 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
+ *
+ * Contributor(s):
+ *
+ * Portions Copyrighted 2010 Sun Microsystems, Inc.
  */
-package org.netbeans.modules.spellchecker;
 
+package org.netbeans.modules.spellchecker.api;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import javax.swing.text.JTextComponent;
-import org.netbeans.lib.editor.util.swing.DocumentUtilities;
-import org.netbeans.modules.spellchecker.api.Spellchecker;
-import org.netbeans.spi.editor.highlighting.HighlightsLayer;
-import org.netbeans.spi.editor.highlighting.HighlightsLayerFactory;
-import org.netbeans.spi.editor.highlighting.ZOrder;
-import org.netbeans.spi.editor.highlighting.support.OffsetsBag;
+
+import org.openide.util.Exceptions;
+import org.openide.util.Lookup;
+
 
 /**
  *
- * @author Jan Lahoda
+ * @author hanz
  */
-public class SpellcheckerHighlightLayerFactory implements HighlightsLayerFactory {
-    
-    public SpellcheckerHighlightLayerFactory() {
-    }
-    
-    public HighlightsLayer[] createLayers(Context ctx) {
-        OffsetsBag bag = getBag(ctx.getComponent());
-        return new HighlightsLayer[] {
-            HighlightsLayer.create(SpellcheckerHighlightLayerFactory.class.getName(), ZOrder.SYNTAX_RACK, false, bag),
-        };
-    }
-    
-    public static synchronized OffsetsBag getBag(JTextComponent component) {
-        OffsetsBag bag = (OffsetsBag) component.getClientProperty(SpellcheckerHighlightLayerFactory.class);
-        Spellchecker.register (component);
-        if (bag == null) {
-            component.putClientProperty(SpellcheckerHighlightLayerFactory.class, bag = new OffsetsBag(component.getDocument()));
+public class Spellchecker {
+
+    public static void register (
+        JTextComponent          textComponent
+    ) {
+        ClassLoader systemClassLoader = Lookup.getDefault ().lookup (ClassLoader.class);
+        try {
+            Class componentPeerClass = systemClassLoader.loadClass ("org.netbeans.modules.spellchecker.ComponentPeer");
+            Method assureInstalledMethod = componentPeerClass.getMethod ("assureInstalled", Class.forName ("javax.swing.text.JTextComponent"));
+            assureInstalledMethod.invoke (null, textComponent);
+        } catch (IllegalAccessException ex) {
+            Exceptions.printStackTrace (ex);
+        } catch (IllegalArgumentException ex) {
+            Exceptions.printStackTrace (ex);
+        } catch (InvocationTargetException ex) {
+            Exceptions.printStackTrace (ex);
+        } catch (ClassNotFoundException ex) {
+        } catch (NoSuchMethodException ex) {
+            Exceptions.printStackTrace (ex);
+        } catch (SecurityException ex) {
+            Exceptions.printStackTrace (ex);
         }
-        
-        return bag;
     }
-    
 }
