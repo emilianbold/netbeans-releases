4c44560aa40f5b311d568039eb93996cf6dedb88
Ondrej Vrabec <ovrabec@netbeans.org>
2010-03-09 14:22:51 +0100

    Issue #181528 - Open changed files in editor to prevent loosing changes

--- subversion/src/org/netbeans/modules/subversion/ui/diff/MultiDiffPanel.java
+++ subversion/src/org/netbeans/modules/subversion/ui/diff/MultiDiffPanel.java
@@ -287,13 +287,27 @@ public class MultiDiffPanel extends javax.swing.JPanel implements ActionListener
     }
 
     public SaveCookie[] getSaveCookies (boolean ommitOpened) {
+        EditorCookie[] editorCookiesCopy = getEditorCookies(ommitOpened);
+        SaveCookie[] saveCookies = getSaveCookies(setups, editorCookiesCopy);
+        return saveCookies;
+    }
+
+    public EditorCookie[] getEditorCookies (boolean ommitOpened) {
         EditorCookie[] editorCookiesCopy = copyArray(editorCookies);
         DiffUtils.cleanThoseUnmodified(editorCookiesCopy);
         if (ommitOpened) {
             DiffUtils.cleanThoseWithEditorPaneOpen(editorCookiesCopy);
         }
-        SaveCookie[] saveCookies = getSaveCookies(setups, editorCookiesCopy);
-        return saveCookies;
+        int count = 0, length = editorCookiesCopy.length;
+        EditorCookie[] editorCookiesShorten = new EditorCookie[length];
+        for (int i = 0; i < length; i++) {
+            EditorCookie editorCookie = editorCookiesCopy[i];
+            if (editorCookie == null) {
+                continue;
+            }
+            editorCookiesShorten[count++] = editorCookie;
+        }
+        return CollectionUtils.shortenArray(editorCookiesShorten, count);
     }
 
     private static SaveCookie[] getSaveCookies(Setup[] setups,
