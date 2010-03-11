bdb876dc565bfd8490987ed28bc40139c409b1e2
Jan Lahoda <jlahoda@netbeans.org>
2010-03-11 14:21:13 +0100

    Improving the StaticNonFinalUsedInInitialization hint.

--- java.hints/src/org/netbeans/modules/java/hints/StaticNonFinalUsedInInitialization.java
+++ java.hints/src/org/netbeans/modules/java/hints/StaticNonFinalUsedInInitialization.java
@@ -39,18 +39,16 @@
 
 package org.netbeans.modules.java.hints;
 
+import com.sun.source.tree.AssignmentTree;
+import com.sun.source.tree.BlockTree;
 import com.sun.source.tree.IdentifierTree;
 import com.sun.source.tree.Tree;
-import com.sun.source.tree.Tree.Kind;
+import com.sun.source.tree.VariableTree;
 import com.sun.source.util.TreePath;
-import java.util.Map;
 import javax.lang.model.element.Element;
-import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.Modifier;
 import org.netbeans.api.java.source.CompilationInfo;
-import org.netbeans.modules.java.hints.errors.Utilities;
 import org.netbeans.modules.java.hints.jackpot.code.spi.Hint;
-import org.netbeans.modules.java.hints.jackpot.code.spi.TriggerPattern;
 import org.netbeans.modules.java.hints.jackpot.code.spi.TriggerTreeKind;
 import org.netbeans.modules.java.hints.jackpot.spi.HintContext;
 import org.netbeans.modules.java.hints.jackpot.spi.support.ErrorDescriptionFactory;
@@ -72,7 +70,7 @@ public class StaticNonFinalUsedInInitialization {
         CompilationInfo info = ctx.getInfo();
 
         Element e = info.getTrees().getElement(ctx.getPath());
-        if (e == null) {
+        if (e == null || !e.getKind().isField()) {
             return null;
         }
         if (!e.getModifiers().contains(Modifier.STATIC)) {
@@ -81,10 +79,46 @@ public class StaticNonFinalUsedInInitialization {
         if (e.getModifiers().contains(Modifier.FINAL)) {
             return null;
         }
+        TreePath previous = ctx.getPath();
+        TreePath current  = previous.getParentPath();
 
-        TreePath method = Utilities.findEnclosingMethodOrConstructor(ctx, ctx.getPath());
-        if (method != null) {
-            return null;
+        OUTER: while (true) {
+            if (current == null) {
+                return null;
+            }
+            switch (current.getLeaf().getKind()) {
+                case CLASS:
+                case METHOD:
+                    return null;
+                case BLOCK:
+                    if (((BlockTree) current.getLeaf()).isStatic()) {
+                        break OUTER;
+                    }
+                    break;
+                case VARIABLE:
+                    if (((VariableTree) current.getLeaf()).getInitializer() != previous.getLeaf()) {
+                        return null;//TODO: most likely not needed
+                    }
+                    
+                    Element el = info.getTrees().getElement(current);
+
+                    if (el != null && el.getKind().isField()) {
+                        if (el.getModifiers().contains(Modifier.STATIC)) {
+                            break OUTER;
+                        } else {
+                            break;
+                        }
+                    }
+                    break;
+                case ASSIGNMENT:
+                    if (((AssignmentTree) current.getLeaf()).getExpression() != previous.getLeaf()) {
+                        return null;
+                    }
+                    break;
+            }
+
+            previous = current;
+            current = current.getParentPath();
         }
 
         return ErrorDescriptionFactory.forName(ctx, it,
--- java.hints/test/unit/src/org/netbeans/modules/java/hints/StaticNonFinalUsedInInitializationTest.java
+++ java.hints/test/unit/src/org/netbeans/modules/java/hints/StaticNonFinalUsedInInitializationTest.java
@@ -48,7 +48,7 @@ import org.netbeans.modules.java.hints.jackpot.code.spi.TestBase;
 public class StaticNonFinalUsedInInitializationTest extends TestBase {
 
     public StaticNonFinalUsedInInitializationTest(String name) {
-        super(name, LeakingThisInConstructor.class);
+        super(name, StaticNonFinalUsedInInitialization.class);
     }
 
     public void testDoNotReport() throws Exception {
@@ -60,14 +60,40 @@ public class StaticNonFinalUsedInInitializationTest extends TestBase {
                             "}"
                             );
     }
+    public void testDoNotReport2() throws Exception {
+        performAnalysisTest("test/Test.java",
+                            "package test;\n" +
+                            "public class Test {\n" +
+                            "    private static int A = 5;\n" +
+                            "    public int B = A + 10;\n" +
+                            "    public int C;\n" +
+                            "    {\n" +
+                            "        C = A + 10;" +
+                            "    }\n" +
+                            "}"
+                            );
+    }
     public void testReportIt() throws Exception {
         performAnalysisTest("test/Test.java",
                             "package test;\n" +
                             "public class Test {\n" +
                             "    static int A = 5;\n" +
                             "    static int B = A + 10;\n" +
-                            "}" //,
-                            // "2:39-2:43:verifier:Static non-final variable used during initialization"
+                            "}",
+                            "3:19-3:20:verifier:StaticNonFinalUsedInInitialization"
+                            );
+    }
+    public void testReportIt2() throws Exception {
+        performAnalysisTest("test/Test.java",
+                            "package test;\n" +
+                            "public class Test {\n" +
+                            "    static int A = 5;\n" +
+                            "    static int B;\n" +
+                            "    static {\n" +
+                            "        B = A + 10;\n" +
+                            "    }\n" +
+                            "}",
+                            "5:12-5:13:verifier:StaticNonFinalUsedInInitialization"
                             );
     }
 }
