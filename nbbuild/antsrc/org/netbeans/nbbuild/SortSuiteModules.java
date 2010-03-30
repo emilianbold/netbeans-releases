4c7c31511e41422b566deb56d6d9ca6180e203d4
Jesse Glick <jglick@netbeans.org>
2010-03-30 14:10:26 -0400

    Restoring full 73a95afa3f40 (contrary to d8b568f9c6e0) to make only one pass for NBMs. Individual problems are probably now fixed. Also sorting kit modules better to avoid passes. Also building autoupdate.services right off so DTD validation can work.

--- nbbuild/antsrc/org/netbeans/nbbuild/SortSuiteModules.java
+++ nbbuild/antsrc/org/netbeans/nbbuild/SortSuiteModules.java
@@ -147,10 +147,10 @@ public class SortSuiteModules extends Task {
                 throw new BuildException("Malformed project file " + projectXml, getLocation());
             }
             for (Element dep : XMLUtil.findSubElements(depsEl)) {
-                if (ParseProjectXml.findNBMElement(dep, "build-prerequisite") == null) {
-                    // Note that this does not prevent "...will first try to build..." from ParseProjectXml,
-                    // since that builds transitive runtime dependencies first. Not clear if SortSuiteModules
-                    // needs an option to sort also by runtime-only dependencies.
+                if (ParseProjectXml.findNBMElement(dep, "build-prerequisite") == null &&
+                    // Just build-prerequisite would not prevent "...will first try to build..." from ParseProjectXml,
+                    // since that builds transitive runtime dependencies (e.g. from *.kit) first.
+                    ParseProjectXml.findNBMElement(dep, "run-dependency") == null) {
                     continue;
                 }
                 Element cnbEl2 = ParseProjectXml.findNBMElement(dep, "code-name-base");
