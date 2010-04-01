b3a28d088767d7920908c6c03a8f118802e4738b
Vince Kraemer <vkraemer@netbeans.org>
2010-04-01 12:57:40 -0700

    fix for #183188 not quite right...

--- glassfish.common/src/org/netbeans/modules/glassfish/common/nodes/actions/UndeployModuleAction.java
+++ glassfish.common/src/org/netbeans/modules/glassfish/common/nodes/actions/UndeployModuleAction.java
@@ -100,8 +100,6 @@ public class UndeployModuleAction extends NodeAction {
                 });
             }
         }
-        undeployer.shutdown();
-        undeployer = null;
     }
 
     @Override
