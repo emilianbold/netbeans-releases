30161c91067c51a074895175cabaaf7124f30dc7
Vladimir Voskresensky <vv159170@netbeans.org>
2010-04-22 12:56:52 +0400

    disable WARNING messages for FileUtil logger to not break err output in tests

--- cnd/test/unit/src/org/netbeans/modules/cnd/test/CndBaseTestCase.java
+++ cnd/test/unit/src/org/netbeans/modules/cnd/test/CndBaseTestCase.java
@@ -191,6 +191,7 @@ public abstract class CndBaseTestCase extends NativeExecutionBaseTestCase {
         }
         
         Logger.getLogger("org.netbeans.modules.editor.settings.storage.Utils").setLevel(Level.SEVERE);
+        Logger.getLogger("org.openide.filesystems.FileUtil").setLevel(Level.SEVERE);
         System.setProperty("cnd.mode.unittest", "true");
         System.setProperty("SUNW_NO_UPDATE_NOTIFY", "true");
         List<Class<?>> list = new ArrayList<Class<?>>();
