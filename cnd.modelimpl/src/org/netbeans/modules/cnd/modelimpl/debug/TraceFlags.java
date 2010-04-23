47699d8e4e80d356f60616b76d93faec647d1f63
Vladimir Voskresensky <vv159170@netbeans.org>
2010-04-23 15:27:49 +0400

    use flags

--- cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/debug/TraceFlags.java
+++ cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/debug/TraceFlags.java
@@ -53,7 +53,7 @@ import org.netbeans.modules.cnd.debug.DebugUtils;
  * @author Vladimir Kvashim
  */
 public class TraceFlags {
-    public static volatile boolean TRACE_182342_BUG = false;
+    public static volatile boolean TRACE_182342_BUG = Boolean.getBoolean("cnd.modelimpl.trace182342");
 
     public static final boolean TRACE_CPU_CPP = false;
     public static final boolean TRACE_PARSER_QUEUE_DETAILS = Boolean.getBoolean("cnd.parser.queue.trace.details"); // NOI18N
