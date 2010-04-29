2d92226215b67254afac37be1c56b5836dd1cc89
Vladimir Voskresensky <vv159170@netbeans.org>
2010-04-29 11:12:50 +0400

    test for #184085 -  Bit fields show up in multiple places in code completion

---
+++ cnd.completion/test/unit/data/org/netbeans/modules/cnd/completion/NonGlobalCompletionTestCase/iz184085.cc
@@ -0,0 +1,14 @@
+struct {
+   char      len;
+   struct {
+      int    type:3;
+      int    attribute:3;
+      int    direction:2;
+   } bitfields;
+   short     time;
+   char      extra[16];
+} err_tmp;
+
+void check184085() {
+        // insert code here;
+}
