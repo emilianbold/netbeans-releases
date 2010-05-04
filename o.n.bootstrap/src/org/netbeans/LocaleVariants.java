ffd2b34af789cd88e7407bda22972eea1271fe3b
Jaroslav Tulach <jtulach@netbeans.org>
2010-05-04 12:55:48 +0200

    #185303: Putting LocaleVariants specific methods into their own class. As a result Jesse can use other methods in Util without executing the LocaleVariants static initializer.

---
+++ o.n.bootstrap/src/org/netbeans/LocaleVariants.java
@@ -0,0 +1,277 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 1997-2009 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ *
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ *
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ */
+
+package org.netbeans;
+
+import java.io.*;
+import java.util.*;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.openide.modules.InstalledFileLocator;
+import org.openide.util.NbBundle;
+import org.openide.util.Utilities;
+
+final class LocaleVariants implements Stamps.Updater {
+    private static final Logger err = Util.err;
+    
+    private static Map<File,List<FileWithSuffix>> map;
+    private static Locale mapLocale;
+    private static LocaleVariants UPDATER;
+
+    // Prevent accidental subclassing.
+    private LocaleVariants() {
+    }
+
+    static {
+        InputStream stream = Stamps.getModulesJARs().asStream("localeVariants");
+        if (stream != null) try {
+            DataInputStream is = new DataInputStream(stream);
+            String locale = is.readUTF();
+            mapLocale = Locale.getDefault().toString().equals(locale) ? Locale.getDefault() : null;
+            Map<File,List<FileWithSuffix>> tmp = new HashMap<File, List<FileWithSuffix>>();
+            for (;;) {
+                String file = is.readUTF();
+                if (file.length() == 0) {
+                    break;
+                }
+                List<FileWithSuffix> arr = new ArrayList<FileWithSuffix>();
+                for (;;) {
+                    FileWithSuffix fws = FileWithSuffix.read(is);
+                    if (fws == null) {
+                        break;
+                    }
+                    arr.add(fws);
+                }
+                tmp.put(new File(file), arr);
+            }
+            is.close();
+            map = tmp;
+        } catch (IOException ex) {
+            err.log(Level.WARNING, "Cannot read localeVariants cache", ex);
+        }
+
+        if (map == null) {
+            map = new HashMap<File, List<FileWithSuffix>>();
+        }
+    }
+
+    public void flushCaches(DataOutputStream os) throws IOException {
+        synchronized (map) {
+            os.writeUTF(mapLocale.toString());
+            for (Map.Entry<File, List<FileWithSuffix>> entry : map.entrySet()) {
+                os.writeUTF(entry.getKey().getPath());
+                for (FileWithSuffix fws : entry.getValue()) {
+                    fws.write(os);
+                }
+                os.writeUTF("");
+            }
+            os.writeUTF("");
+        }
+    }
+
+    public void cacheReady() {
+    }
+    static void clearCaches() {
+        synchronized (map) {
+            map.clear();
+        }
+    }
+
+    /**
+     * Find existing locale variants of f, in search order.
+     */
+    static List<File> findLocaleVariantsOf(File f) {
+        List<FileWithSuffix> result = findLocaleVariantsWithSuffixesOf(f);
+        List<File> l = new ArrayList<File>(result.size());
+        for (FileWithSuffix fws : result) {
+            l.add(fws.file);
+        }
+        return l;
+    }
+
+    static final class FileWithSuffix {
+        public final File file;
+        public final String suffix;
+        FileWithSuffix(File file, String suffix) {
+            this.file = file;
+            this.suffix = suffix;
+        }
+
+        static FileWithSuffix read(DataInputStream is) throws IOException {
+            String path = is.readUTF();
+            if (path.length() == 0) {
+                return null;
+            }
+            String suffix = is.readUTF();
+            return new FileWithSuffix(new File(path), suffix);
+        }
+
+        void write(DataOutputStream os) throws IOException {
+            os.writeUTF(file.getPath());
+            os.writeUTF(suffix);
+        }
+    }
+    /**
+     * Find existing locale variants of f, in search order.
+     */
+    static List<FileWithSuffix> findLocaleVariantsWithSuffixesOf(File f) {
+        List<FileWithSuffix> res;
+        synchronized (map) {
+            if (mapLocale != Locale.getDefault()) {
+                map.clear();
+                mapLocale = Locale.getDefault();
+            }
+            res = map.get(f);
+            if (res != null) {
+                return res;
+            }
+        }
+
+        if (! f.isFile()) {
+            res = Collections.emptyList();
+        } else {
+            String logicalDir = null;
+            {
+                // #34069: we have to consider that e.g. modules/locale/foo_branding.jar might be
+                // located in a different root of ${netbeans.dirs}, so need to use IFL. Here the
+                // logical path would be "modules/foo.jar" for the base module.
+                String logicalPath = findLogicalPath(f);
+                if (logicalPath != null) {
+                    int slash = logicalPath.lastIndexOf('/');
+                    if (slash != -1) {
+                        logicalDir = logicalPath.substring(0, slash + 1) + "locale/"; // NOI18N
+                    } else {
+                        logicalDir = "locale/"; // NOI18N
+                    }
+                }
+            }
+            List<FileWithSuffix> l = new ArrayList<FileWithSuffix>(7);
+            String nameExt = f.getName();
+            int idx = nameExt.lastIndexOf('.'); // NOI18N
+            String name, ext;
+            if (idx != -1) {
+                name = nameExt.substring(0, idx);
+                ext = nameExt.substring(idx);
+            } else {
+                name = nameExt;
+                ext = ""; // NOI18N
+            }
+            if (logicalDir != null) {
+                for (String suffix : getLocalizingSuffixesFast()) {
+                    String path = logicalDir + name + suffix + ext;
+                    File v = InstalledFileLocator.getDefault().locate(path, null, false);
+                    if (v != null) {
+                        l.add(new FileWithSuffix(v, suffix));
+                    }
+                }
+            } else {
+                File dir = new File(f.getParentFile(), "locale"); // NOI18N
+                if (dir.exists()) {
+                    for (String suffix : getLocalizingSuffixesFast()) {
+                        File v = new File(dir, name + suffix + ext);
+                        if (v.isFile()) {
+                            l.add(new FileWithSuffix(v, suffix));
+                        }
+                    }
+                }
+            }
+            res = l;
+        }
+        synchronized (map) {
+            map.put(f, res);
+        }
+        if (UPDATER == null) {
+            UPDATER = new LocaleVariants();
+            Stamps.getModulesJARs().scheduleSave(UPDATER, "localeVariants", false);
+        }
+        return res;
+    }
+    
+    /** Similar to {@link NbBundle#getLocalizingSuffixes} but optimized.
+     * @since JST-PENDING: Called from InstalledFileLocatorImpl
+     */
+    static synchronized String[] getLocalizingSuffixesFast() {
+        if (suffixes == null ||
+                Locale.getDefault() != lastLocale ||
+                !Utilities.compareObjects(NbBundle.getBranding(), lastBranding)) {
+            List<String> _suffixes = new ArrayList<String>();
+            Iterator<String> it = NbBundle.getLocalizingSuffixes();
+            while (it.hasNext()) {
+                _suffixes.add(it.next());
+            }
+            suffixes = _suffixes.toArray(new String[_suffixes.size()]);
+            lastLocale = Locale.getDefault();
+            lastBranding = NbBundle.getBranding();
+        }
+        return suffixes;
+    }
+    private static String[] suffixes = null;
+    private static Locale lastLocale = null;
+    private static String lastBranding = null;
+
+    /**
+     * Find a path such that InstalledFileLocator.getDefault().locate(path, null, false)
+     * yields the given file. Only guaranteed to work in case the logical path is a suffix of
+     * the file's absolute path (after converting path separators); otherwise there is no
+     * general way to invert locate(...) so this heuristic may fail. However for the IFL
+     * implementation used in a plain NB installation (i.e.
+     * org.netbeans.core.modules.InstalledFileLocatorImpl), this condition will in fact hold.
+     * @return the inverse of locate(...), or null if there is no such path
+     * @see "#34069"
+     */
+    private static String findLogicalPath(File f) {
+        InstalledFileLocator l = InstalledFileLocator.getDefault();
+        String path = f.getName();
+        File parent = f.getParentFile();
+        while (parent != null) {
+            File probe = l.locate(path, null, false);
+            //System.err.println("Util.fLP: f=" + f + " parent=" + parent + " probe=" + probe + " f.equals(probe)=" + f.equals(probe));
+            if (f.equals(probe)) {
+                return path;
+            }
+            path = parent.getName() + '/' + path;
+            parent = parent.getParentFile();
+        }
+        return null;
+    }
+
+}
