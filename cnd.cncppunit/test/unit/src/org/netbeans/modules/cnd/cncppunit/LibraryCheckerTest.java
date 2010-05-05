a071471eb9a588d89ed9f1befa0f5c53a5e971b7
Alexey Vladykin <alexey_vladykin@netbeans.org>
2010-05-05 19:11:00 +0400

    Fixing #185506 provide warning about missing cunit/cppunit library in the new test wizard - implemented LibraryChecker service

---
+++ cnd.cncppunit/test/unit/src/org/netbeans/modules/cnd/cncppunit/LibraryCheckerTest.java
@@ -0,0 +1,96 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2010 Sun Microsystems, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Sun designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Sun in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ *
+ * Contributor(s):
+ *
+ * Portions Copyrighted 2010 Sun Microsystems, Inc.
+ */
+package org.netbeans.modules.cnd.cncppunit;
+
+import org.junit.Test;
+import org.netbeans.modules.cnd.api.toolchain.AbstractCompiler;
+import org.netbeans.modules.cnd.api.toolchain.CompilerSet;
+import org.netbeans.modules.cnd.api.toolchain.CompilerSetManager;
+import org.netbeans.modules.cnd.api.toolchain.PredefinedToolKind;
+import org.netbeans.modules.cnd.remote.support.RemoteTestBase;
+import org.netbeans.modules.cnd.test.CndBaseTestSuite;
+import org.netbeans.modules.nativeexecution.api.ExecutionEnvironment;
+import org.netbeans.modules.nativeexecution.api.ExecutionEnvironmentFactory;
+import org.netbeans.modules.nativeexecution.test.ForAllEnvironments;
+import static org.junit.Assert.*;
+
+/**
+ * @author Alexey Vladykin
+ */
+public class LibraryCheckerTest extends RemoteTestBase {
+
+    public LibraryCheckerTest(String name) {
+        super(name);
+    }
+
+    public LibraryCheckerTest(String name, ExecutionEnvironment execEnv) {
+        super(name, execEnv);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static junit.framework.Test suite() {
+        return new CndBaseTestSuite(LibraryCheckerTest.class);
+    }
+
+    @Test
+    public void testIsLibraryAvailableLocal() throws Exception {
+        doTestIsLibraryAvailable(ExecutionEnvironmentFactory.getLocal());
+    }
+
+    @Test
+    @ForAllEnvironments(section = "remote.platforms")
+    public void testIsLibraryAvailable() throws Exception {
+        doTestIsLibraryAvailable(getTestExecutionEnvironment());
+    }
+
+    private void doTestIsLibraryAvailable(ExecutionEnvironment execEnv) throws Exception {
+        setupHost(execEnv);
+        CompilerSetManager compilerSetManager = CompilerSetManager.get(execEnv);
+        assertFalse(compilerSetManager.isPending());
+        for (CompilerSet compilerSet : compilerSetManager.getCompilerSets()) {
+            AbstractCompiler cCompiler = (AbstractCompiler) compilerSet.getTool(PredefinedToolKind.CCompiler);
+            assertTrue(LibraryChecker.isLibraryAvailable("rt", cCompiler));
+            assertFalse(LibraryChecker.isLibraryAvailable("foo", cCompiler));
+
+            AbstractCompiler cppCompiler = (AbstractCompiler) compilerSet.getTool(PredefinedToolKind.CCCompiler);
+            assertTrue(LibraryChecker.isLibraryAvailable("rt", cppCompiler));
+            assertFalse(LibraryChecker.isLibraryAvailable("foo", cppCompiler));
+        }
+    }
+}
