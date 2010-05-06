56a82bb56051d18c77c23d27fd74daee46eb6a4c
Jan Becicka <jbecicka@netbeans.org>
2010-05-06 11:32:33 +0200

    Backed out changeset caa7095d7d91

--- kenai.ui/src/org/netbeans/modules/kenai/ui/spi/UIUtils.java
+++ kenai.ui/src/org/netbeans/modules/kenai/ui/spi/UIUtils.java
@@ -142,7 +142,6 @@ public final class UIUtils {
             }
             kenai.login(uname, password,
                     force ? true : goOnline);
-            Utilities.isChatSupported(kenai);
         } catch (KenaiException ex) {
             return false;
         }
