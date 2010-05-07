c02ff22f8d299f1962466d5a0a35041c61dec24f
Ondrej Vrabec <ovrabec@netbeans.org>
2010-05-07 10:33:11 +0200

    Issue #185536 - [69cat] OutOfMemoryError after VCS-Diff counting diffs mainly on-demand

--- versioning.system.cvss/src/org/netbeans/modules/versioning/system/cvss/ui/actions/diff/MultiDiffPanel.java
+++ versioning.system.cvss/src/org/netbeans/modules/versioning/system/cvss/ui/actions/diff/MultiDiffPanel.java
@@ -477,6 +477,11 @@ class MultiDiffPanel extends javax.swing.JPanel implements ActionListener, Versi
         DiffController view = null;
         
         if (currentIndex != -1) {
+            if (dpt != null) {
+                prepareTask.cancel();
+                dpt.setTableIndex(currentIndex);
+                prepareTask.schedule(100);
+            }
             currentModelIndex = showingFileTable() ? fileTable.getModelIndex(currentIndex) : 0;
             view = setups[currentModelIndex].getView();
 
@@ -516,6 +521,7 @@ class MultiDiffPanel extends javax.swing.JPanel implements ActionListener, Versi
                 }
             } else {
                 diffView = new NoContentPanel(NbBundle.getMessage(MultiDiffPanel.class, "MSG_DiffPanel_NoContent"));
+                displayDiffView();
             }            
             lookup.setData(fileObj, observableEditorCookie, diffView.getActionMap());
         } else {
@@ -795,19 +801,26 @@ class MultiDiffPanel extends javax.swing.JPanel implements ActionListener, Versi
     private class DiffPrepareTask implements Runnable {
         
         private final Setup[] prepareSetups;
+        private int tableIndex; // index of a row in the table - viewIndex, needs to be translated to modelIndex
 
         public DiffPrepareTask(Setup [] prepareSetups) {
             this.prepareSetups = prepareSetups;
+            this.tableIndex = 0;
         }
 
         @Override
         public void run() {
             try {
-                for (int i = 0; i < prepareSetups.length; i++) {
-                    if (prepareSetups != setups) return;
+                int[] indexes = prepareIndexesToRefresh();
+                for (int i : indexes) {
+                    if (prepareSetups != setups || Thread.interrupted()) return;
+                    int modelIndex = fileTable == null ? i : fileTable.getModelIndex(i);
+                    if (prepareSetups[modelIndex].getView() != null) {
+                        continue;
+                    }
                     try {
-                        prepareSetups[i].initSources(group);  // slow network I/O
-                        final int fi = i;
+                        prepareSetups[modelIndex].initSources(group);  // slow network I/O
+                        final int fi = modelIndex;
                         StreamSource ss1 = prepareSetups[fi].getFirstSource();
                         StreamSource ss2 = prepareSetups[fi].getSecondSource();
                         final DiffController view = DiffController.createEnhanced(ss1, ss2);  // possibly executing slow external diff
@@ -815,10 +828,10 @@ class MultiDiffPanel extends javax.swing.JPanel implements ActionListener, Versi
                         if (Thread.interrupted()) {
                             return;
                         }
+                        prepareSetups[fi].setView(view);
                         SwingUtilities.invokeLater(new Runnable() {
                             @Override
                             public void run() {
-                                prepareSetups[fi].setView(view);
                                 if (prepareSetups != setups) {
                                     return;
                                 }
@@ -837,6 +850,26 @@ class MultiDiffPanel extends javax.swing.JPanel implements ActionListener, Versi
             } finally {
                 executed();
             }
+        }
+
+        private int[] prepareIndexesToRefresh () {
+            int min = Math.max(0, tableIndex - 2);
+            int max = Math.min(prepareSetups.length - 1, tableIndex + 2);
+            int[] indexes = new int[max - min + 1];
+            // adding tableIndex, tableIndex - 1, tableIndex + 1, tableIndex - 2, tableIndex + 2, etc.
+            for (int i = tableIndex, j = tableIndex + 1, k = 0; i >= min || j <= max; --i, ++j) {
+                if (i >= min) {
+                    indexes[k++] = i;
+                }
+                if (j <= max) {
+                    indexes[k++] = j;
+                }
+            }
+            return indexes;
+        }
+
+        private void setTableIndex(int index) {
+            this.tableIndex = index;
         }
     }
 
