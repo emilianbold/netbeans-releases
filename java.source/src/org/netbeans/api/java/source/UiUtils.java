dc3a523627fd01e13e57205da4921dcadd995f4a
Tomas Zezula <tzezula@netbeans.org>
2010-05-13 08:30:34 +0200

    #185495:Unable to navigate to a big file

--- java.source/src/org/netbeans/api/java/source/UiUtils.java
+++ java.source/src/org/netbeans/api/java/source/UiUtils.java
@@ -60,6 +60,8 @@ import javax.swing.SwingUtilities;
 import javax.swing.text.StyledDocument;
 import org.netbeans.modules.java.source.pretty.VeryPretty;
 import org.netbeans.modules.java.ui.Icons;
+import org.openide.DialogDisplayer;
+import org.openide.NotifyDescriptor;
 import org.openide.cookies.EditorCookie;
 import org.openide.cookies.LineCookie;
 import org.openide.cookies.OpenCookie;
@@ -69,6 +71,8 @@ import org.openide.text.Line;
 import org.openide.text.Line.ShowOpenType;
 import org.openide.text.Line.ShowVisibilityType;
 import org.openide.text.NbDocument;
+import org.openide.util.NbBundle;
+import org.openide.util.UserQuestionException;
 
 /** This class contains various methods bound to visualization of Java model 
  * elements. It was formerly included under SourceUtils
@@ -348,8 +352,21 @@ public final class  UiUtils {
             EditorCookie ec = od.getLookup().lookup(EditorCookie.class);
             LineCookie lc = od.getLookup().lookup(LineCookie.class);
             
-            if (ec != null && lc != null && offset != -1) {                
-                StyledDocument doc = ec.openDocument();                
+            if (ec != null && lc != null && offset != -1) {
+                StyledDocument doc = null;
+                try {
+                    doc = ec.openDocument();
+                } catch (UserQuestionException uqe) {
+                    final Object value = DialogDisplayer.getDefault().notify(
+                            new NotifyDescriptor.Confirmation(uqe.getLocalizedMessage(),
+                            NbBundle.getMessage(UiUtils.class, "TXT_Question"),
+                            NotifyDescriptor.YES_NO_OPTION));
+                    if (value != NotifyDescriptor.YES_OPTION) {
+                        return false;
+                    }
+                    uqe.confirmed();
+                    doc = ec.openDocument();
+                }
                 if (doc != null) {
                     int line = NbDocument.findLineNumber(doc, offset);
                     int lineOffset = NbDocument.findLineOffset(doc, line);
