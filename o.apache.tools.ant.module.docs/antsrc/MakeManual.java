a219be2f170d15e55b9bbc45d536f3d74c131c4a
Jesse Glick <jglick@netbeans.org>
2010-05-18 12:52:41 -0400

    Issue #185375: upgrade Ant to 1.8.1. (transplanted from c665f3cc2f10fd0efb36a80fb38adc4d00518afe)

--- o.apache.tools.ant.module.docs/antsrc/MakeManual.java
+++ o.apache.tools.ant.module.docs/antsrc/MakeManual.java
@@ -156,7 +156,7 @@ public class MakeManual extends Task {
                 id += "." + anchor;
             }
         }
-        String title = linktext != null ? linktext : "Ant 1.8.0 Manual";
+        String title = linktext != null ? linktext : "Ant 1.8.1 Manual";
         String log = "";
         for (int i = 0; i < depth; i++) {
             log += "\t";
