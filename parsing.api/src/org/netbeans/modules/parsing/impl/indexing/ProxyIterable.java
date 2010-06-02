e2325cfa470cf1ba8d8d978197e42933d427c4c2
Tomas Zezula <tzezula@netbeans.org>
2010-06-02 18:59:53 +0200

    #187051:Too much time spent in the iteration over the indexedIndexables in LI.store() (transplanted from 0da5348f1db4c0ab6a7cc4653fdd3a6ba40e106c)

--- parsing.api/src/org/netbeans/modules/parsing/impl/indexing/ProxyIterable.java
+++ parsing.api/src/org/netbeans/modules/parsing/impl/indexing/ProxyIterable.java
@@ -45,8 +45,11 @@ package org.netbeans.modules.parsing.impl.indexing;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Iterable over list of lists
@@ -57,19 +60,30 @@ final class ProxyIterable<T> implements Iterable<T> {
 
     private final Collection<? extends Iterable<? extends T>> delegates;
     private final boolean allowDuplicates;
+    private final AtomicReference<List<T>> cache;
 
     public ProxyIterable(Collection<? extends Iterable<? extends T>> delegates) {
-        this(delegates, true);
+        this(delegates, true, false);
     }
 
     public ProxyIterable(Collection<? extends Iterable<? extends T>> delegates, boolean allowDuplicates) {
+        this(delegates, allowDuplicates, false);
+    }
+
+    public ProxyIterable(Collection<? extends Iterable<? extends T>> delegates, boolean allowDuplicates, boolean cacheValues) {
         assert delegates != null;
         this.delegates = delegates;
         this.allowDuplicates = allowDuplicates;
+        this.cache = cacheValues ? new AtomicReference<List<T>>() : null;
     }
 
     public Iterator<T> iterator() {
-        return new ProxyIterator<T>(delegates.iterator(), allowDuplicates);
+        final List<T> _cache = cache == null ? null : cache.get();
+        if (_cache != null) {
+            return _cache.iterator();
+        } else {
+            return new ProxyIterator<T>(delegates.iterator(), allowDuplicates, cache);
+        }
     }
 
     @Override
@@ -81,24 +95,38 @@ final class ProxyIterable<T> implements Iterable<T> {
 
         private final Iterator<? extends Iterable< ? extends T>> iterables;
         private final Set<T> seen;
-        
+        private final AtomicReference<List<T>> cacheRef;
+        private final List<T> cache;
         private Iterator<? extends T> currentIterator;
         private T currentObject;
 
-        public ProxyIterator(Iterator<? extends Iterable< ? extends T>> iterables, boolean allowDuplicates) {
+        public ProxyIterator(
+                Iterator<? extends Iterable< ? extends T>> iterables,
+                boolean allowDuplicates,
+                final AtomicReference<List<T>> cacheRef) {
             assert iterables != null;
             this.iterables = iterables;
             this.seen = allowDuplicates ? null : new HashSet<T>();
+            this.cacheRef = cacheRef;
+            this.cache = this.cacheRef == null ? null : new LinkedList<T>();
         }
 
+        @Override
         public boolean hasNext() {
-            return getCurrent(false) != null;
+            final boolean result = getCurrent(false) != null;
+            if (!result && this.cacheRef != null) {
+                this.cacheRef.compareAndSet(null, cache);
+            }
+            return result;
         }
 
+        @Override
         public T next() {
             T curObj =  getCurrent(true);
             if (curObj == null) {
                 throw new NoSuchElementException();
+            } else if (cache != null) {
+                cache.add(curObj);
             }
             return curObj;
         }
--- parsing.api/test/unit/src/org/netbeans/modules/parsing/impl/indexing/ProxyIterableTest.java
+++ parsing.api/test/unit/src/org/netbeans/modules/parsing/impl/indexing/ProxyIterableTest.java
@@ -247,4 +247,283 @@ public class ProxyIterableTest extends NbTestCase {
         assertEquals(Integer.valueOf(4), it.next());
         assertFalse(it.hasNext());
     }
+
+    public void testEmptyCached() {
+        ProxyIterable<Integer> pi = new ProxyIterable<Integer>(Collections.<Collection<? extends Integer>>emptySet(),true,true);
+        //First run
+        assertFalse(pi.iterator().hasNext());
+        try {
+            pi.iterator().next();
+            assertTrue(false);
+        } catch (NoSuchElementException e) {}
+        //Second run
+        assertFalse(pi.iterator().hasNext());
+        try {
+            pi.iterator().next();
+            assertTrue(false);
+        } catch (NoSuchElementException e) {}
+    }
+
+    public void testSingleListCached() {
+        List<Integer> l1 = new LinkedList<Integer>();
+        l1.add(1);
+        l1.add(2);
+        List<List<? extends Integer>> toAdd = new LinkedList<List<? extends Integer>>();
+        toAdd.add(l1);
+        ProxyIterable<Integer> pi = new ProxyIterable<Integer>(toAdd,true,true);
+        //First run
+        Iterator<? extends Integer> it = pi.iterator();
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(1), it.next());
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(2), it.next());
+        assertFalse(it.hasNext());
+        //Second run
+        it = pi.iterator();
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(1), it.next());
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(2), it.next());
+        assertFalse(it.hasNext());
+    }
+
+    public void testMultipleListsCached() {
+        List<Integer> l1 = new LinkedList<Integer>();
+        l1.add(1);
+        l1.add(2);
+        List<Integer> l2 = new LinkedList<Integer>();
+        l2.add(3);
+        l2.add(4);
+        List<Integer> l3 = new LinkedList<Integer>();
+        l3.add(5);
+        l3.add(6);
+        List<List<? extends Integer>> toAdd = new LinkedList<List<? extends Integer>>();
+        toAdd.add(l1);
+        toAdd.add(l2);
+        toAdd.add(l3);
+        ProxyIterable<Integer> pi = new ProxyIterable<Integer>(toAdd, true, true);
+        //First run
+        Iterator<? extends Integer> it = pi.iterator();
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(1), it.next());
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(2), it.next());
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(3), it.next());
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(4), it.next());
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(5), it.next());
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(6), it.next());
+        assertFalse(it.hasNext());
+        //First run
+        it = pi.iterator();
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(1), it.next());
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(2), it.next());
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(3), it.next());
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(4), it.next());
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(5), it.next());
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(6), it.next());
+        assertFalse(it.hasNext());
+    }
+
+    public void testEmptyListCached() {
+        List<Integer> l1 = new LinkedList<Integer>();
+        l1.add(1);
+        l1.add(2);
+        List<Integer> l2 = new LinkedList<Integer>();
+        List<Integer> l3 = new LinkedList<Integer>();
+        l3.add(5);
+        l3.add(6);
+        List<List<? extends Integer>> toAdd = new LinkedList<List<? extends Integer>>();
+        toAdd.add(l1);
+        toAdd.add(l2);
+        toAdd.add(l3);
+        ProxyIterable<Integer> pi = new ProxyIterable<Integer>(toAdd,true,true);
+        //First run
+        Iterator<? extends Integer> it = pi.iterator();
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(1), it.next());
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(2), it.next());
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(5), it.next());
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(6), it.next());
+        assertFalse(it.hasNext());
+        //Second run
+        it = pi.iterator();
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(1), it.next());
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(2), it.next());
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(5), it.next());
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(6), it.next());
+        assertFalse(it.hasNext());
+    }
+
+    public void testDuplicatesCached() {
+        List<Integer> l1 = new LinkedList<Integer>();
+        l1.add(1);
+        l1.add(2);
+        List<Integer> l2 = new LinkedList<Integer>();
+        l2.add(1);
+        l2.add(2);
+        List<Integer> l3 = new LinkedList<Integer>();
+        l3.add(1);
+        l3.add(2);
+        List<List<? extends Integer>> toAdd = new LinkedList<List<? extends Integer>>();
+        toAdd.add(l1);
+        toAdd.add(l2);
+        toAdd.add(l3);
+        ProxyIterable<Integer> pi = new ProxyIterable<Integer>(toAdd,true,true);
+        //First run
+        Iterator<? extends Integer> it = pi.iterator();
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(1), it.next());
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(2), it.next());
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(1), it.next());
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(2), it.next());
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(1), it.next());
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(2), it.next());
+        assertFalse(it.hasNext());
+        //Second run
+        it = pi.iterator();
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(1), it.next());
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(2), it.next());
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(1), it.next());
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(2), it.next());
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(1), it.next());
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(2), it.next());
+        assertFalse(it.hasNext());
+    }
+
+    public void testNoDuplicatesCached() {
+        List<Integer> l1 = new LinkedList<Integer>();
+        l1.add(1);
+        l1.add(2);
+        List<Integer> l2 = new LinkedList<Integer>();
+        l2.add(1);
+        l2.add(2);
+        List<Integer> l3 = new LinkedList<Integer>();
+        l3.add(1);
+        l3.add(2);
+        List<List<? extends Integer>> toAdd = new LinkedList<List<? extends Integer>>();
+        toAdd.add(l1);
+        toAdd.add(l2);
+        toAdd.add(l3);
+        ProxyIterable<Integer> pi = new ProxyIterable<Integer>(toAdd, false, true);
+        //First run
+        Iterator<? extends Integer> it = pi.iterator();
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(1), it.next());
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(2), it.next());
+        assertFalse(it.hasNext());
+        //First second
+        it = pi.iterator();
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(1), it.next());
+        assertTrue(it.hasNext());
+        assertEquals(Integer.valueOf(2), it.next());
+        assertFalse(it.hasNext());
+    }
+
+    public void testUnusualCached() {
+        List<Integer> l1 = new LinkedList<Integer>();
+        l1.add(1);
+        l1.add(2);
+        List<Integer> l2 = new LinkedList<Integer>();
+        l2.add(3);
+        l2.add(4);
+        List<Integer> l3 = new LinkedList<Integer>();
+        l3.add(2);
+        l3.add(3);
+        List<List<? extends Integer>> toAdd = new LinkedList<List<? extends Integer>>();
+        toAdd.add(l1);
+        toAdd.add(l2);
+        toAdd.add(l3);
+        ProxyIterable<Integer> pi = new ProxyIterable<Integer>(toAdd,true,true);
+        //First run
+        Iterator<? extends Integer> it = pi.iterator();
+        for(int i = 0; i < 20; i++) {
+            assertTrue("i=" + i, it.hasNext());
+        }
+        assertEquals(Integer.valueOf(1), it.next());
+        assertEquals(Integer.valueOf(2), it.next());
+        assertEquals(Integer.valueOf(3), it.next());
+        assertEquals(Integer.valueOf(4), it.next());
+        assertEquals(Integer.valueOf(2), it.next());
+        assertEquals(Integer.valueOf(3), it.next());
+        assertFalse(it.hasNext());
+        //First run
+        it = pi.iterator();
+        for(int i = 0; i < 20; i++) {
+            assertTrue("i=" + i, it.hasNext());
+        }
+        assertEquals(Integer.valueOf(1), it.next());
+        assertEquals(Integer.valueOf(2), it.next());
+        assertEquals(Integer.valueOf(3), it.next());
+        assertEquals(Integer.valueOf(4), it.next());
+        assertEquals(Integer.valueOf(2), it.next());
+        assertEquals(Integer.valueOf(3), it.next());
+        assertFalse(it.hasNext());
+    }
+
+    public void testUnusualNoDuplicatesCached() {
+        List<Integer> l1 = new LinkedList<Integer>();
+        l1.add(1);
+        l1.add(2);
+        List<Integer> l2 = new LinkedList<Integer>();
+        l2.add(3);
+        l2.add(4);
+        List<Integer> l3 = new LinkedList<Integer>();
+        l3.add(2);
+        l3.add(3);
+        List<List<? extends Integer>> toAdd = new LinkedList<List<? extends Integer>>();
+        toAdd.add(l1);
+        toAdd.add(l2);
+        toAdd.add(l3);
+        ProxyIterable<Integer> pi = new ProxyIterable<Integer>(toAdd, false,true);
+        //First run
+        Iterator<? extends Integer> it = pi.iterator();
+        for(int i = 0; i < 20; i++) {
+            assertTrue("i=" + i, it.hasNext());
+        }
+        assertEquals(Integer.valueOf(1), it.next());
+        assertEquals(Integer.valueOf(2), it.next());
+        assertEquals(Integer.valueOf(3), it.next());
+        assertEquals(Integer.valueOf(4), it.next());
+        assertFalse(it.hasNext());
+        //Second run
+        it = pi.iterator();
+        for(int i = 0; i < 20; i++) {
+            assertTrue("i=" + i, it.hasNext());
+        }
+        assertEquals(Integer.valueOf(1), it.next());
+        assertEquals(Integer.valueOf(2), it.next());
+        assertEquals(Integer.valueOf(3), it.next());
+        assertEquals(Integer.valueOf(4), it.next());
+        assertFalse(it.hasNext());
+    }
 }
