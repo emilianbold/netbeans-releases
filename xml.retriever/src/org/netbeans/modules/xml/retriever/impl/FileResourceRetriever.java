ed13722ad789cdce0643da4d849d94bdb15f02f2
Vladimir Yaroslavskiy <yaroslavskiy@netbeans.org>
2010-06-11 11:30:34 +0400

    [Bug #182518] NPE from FileResourceRetriever.retrieveDocument

--- xml.retriever/src/org/netbeans/modules/xml/retriever/impl/FileResourceRetriever.java
+++ xml.retriever/src/org/netbeans/modules/xml/retriever/impl/FileResourceRetriever.java
@@ -41,16 +41,6 @@
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
  */
-
-/*
- * FileResourceRetriever.java
- *
- * Created on January 9, 2006, 10:47 PM
- *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
- */
-
 package org.netbeans.modules.xml.retriever.impl;
 
 import java.io.File;
@@ -63,14 +53,9 @@ import java.util.HashMap;
 import org.netbeans.modules.xml.retriever.*;
 
 /**
- *
  * @author girix
  */
 public class FileResourceRetriever implements ResourceRetriever{
-    
-    /** Creates a new instance of FileResourceRetriever */
-    public FileResourceRetriever() {
-    }
     
     public boolean accept(String baseAddr, String currentAddr) throws URISyntaxException {
         
@@ -86,17 +71,21 @@ public class FileResourceRetriever implements ResourceRetriever{
                     return true;
             }
         }
-        
-        
         return false;
-        
     }
     
     long streamLength = 0;
+
     public HashMap<String, InputStream> retrieveDocument(String baseAddress, String documentAddress) throws IOException,URISyntaxException{
-        URI currURI = new URI(getEffectiveAddress(baseAddress, documentAddress));
+        String address = getEffectiveAddress(baseAddress, documentAddress);
+
+        if (address == null) {
+            return new HashMap<String, InputStream>();
+        }
+        URI currURI = new URI(address);
         HashMap<String, InputStream> result = null;
         File curFile = new File(currURI);
+    
         if(curFile.isFile()){
             InputStream is = new FileInputStream(curFile);
             result = new HashMap<String, InputStream>();
@@ -114,6 +103,9 @@ public class FileResourceRetriever implements ResourceRetriever{
     }
     
     public String getEffectiveAddress(String baseAddress, String documentAddress) throws IOException, URISyntaxException {
+//System.out.println();
+//System.out.println("baseAddress: " + baseAddress);
+//System.out.println("documentAddress: " + documentAddress);
         URI currURI = new URI(documentAddress);
         if(currURI.isAbsolute()){
             //abs file URI
@@ -126,6 +118,7 @@ public class FileResourceRetriever implements ResourceRetriever{
             }else{
                 //neither the current URI nor the base URI are absoulte. So, can not resolve this
                 //path
+//System.out.println("    return null");
                 return null;
             }
         }
