ebaef50bee0d894c5531a5a8e6122cc7fbb8dae4
alexvsimon <alexvsimon@netbeans.org>
2010-06-15 22:33:55 +0400

    fix i18n

--- cnd.toolchain/src/org/netbeans/modules/cnd/toolchain/compilerset/CompilerSetManagerImpl.java
+++ cnd.toolchain/src/org/netbeans/modules/cnd/toolchain/compilerset/CompilerSetManagerImpl.java
@@ -809,8 +809,8 @@ public final class CompilerSetManagerImpl extends CompilerSetManager {
         }
     }
 
-    private static final String ALIAS_SunStudio = "SunStudio";
-    private static final String ALIAS_OracleSolarisStudio = "OracleSolarisStudio";
+    private static final String ALIAS_SunStudio = "SunStudio"; // NOI18N
+    private static final String ALIAS_OracleSolarisStudio = "OracleSolarisStudio"; // NOI18N
     private static final String[] KNOWN_STUDIO = new String[] {
         "OracleSolarisStudio_12.2", // NOI18N
         "OracleSolarisStudioExpress", // NOI18N
