715a4eecf63159c2532e540c5be1aeb487160d7e
mkuchtiak <mkuchtiak@netbeans.org>
2010-06-16 11:16:18 +0200

    #187597, #187598 : enable JSR109 deployment on WebLogic, use WebLogic Tester capability to test web services (transplanted from 3b6f4e0e58a375816db4a4383d0ca119c76e7095)

--- websvc.wsstack.jaxws/src/org/netbeans/modules/websvc/wsstack/jaxws/tomcat/TomcatJaxWsStack.java
+++ websvc.wsstack.jaxws/src/org/netbeans/modules/websvc/wsstack/jaxws/tomcat/TomcatJaxWsStack.java
@@ -231,15 +231,15 @@ public class TomcatJaxWsStack implements WSStackImplementation<JaxWs> {
         return new JaxWs.UriDescriptor() {
 
             public String getServiceUri(String applicationRoot, String serviceName, String portName, boolean isEjb) {
-                return applicationRoot+"/"+serviceName; //NOI18N
+                return (applicationRoot.length()>0 ? applicationRoot+"/" : "")+serviceName; //NOI18N
             }
 
             public String getDescriptorUri(String applicationRoot, String serviceName, String portName, boolean isEjb) {
                 return getServiceUri(applicationRoot, serviceName, portName, isEjb)+"?wsdl"; //NOI18N
             }
             
-            public String getTesterPageUri(String applicationRoot, String serviceName, String portName, boolean isEjb) {
-                return applicationRoot+"/"+serviceName; //NOI18N
+            public String getTesterPageUri(String host, String port, String applicationRoot, String serviceName, String portName, boolean isEjb) {
+                return "http://"+host+":"+port+"/"+getServiceUri(applicationRoot, serviceName, portName, isEjb); //NOI18N
             }
             
         };
