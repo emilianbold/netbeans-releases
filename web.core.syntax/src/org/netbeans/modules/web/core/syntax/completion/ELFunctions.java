5996fcfa7724e1e47a806d19c29097cd6868ae46
Tomasz Slota <tslota@netbeans.org>
2010-07-29 13:19:18 +0800

    #188866: NPE at ELFunctions.getFunctions()

--- web.core.syntax/src/org/netbeans/modules/web/core/syntax/completion/ELFunctions.java
+++ web.core.syntax/src/org/netbeans/modules/web/core/syntax/completion/ELFunctions.java
@@ -129,7 +129,7 @@ public class ELFunctions {
     public static List<Function> getFunctions(JspSyntaxSupport sup, String start){
         List<Function> functions = new ArrayList<Function>();
         JspParserAPI.ParseResult result = JspUtils.getCachedParseResult(sup.getFileObject(), false, false);
-        if (result != null) {
+        if (result != null && result.getPageInfo() != null) {
             Map<?,?> libraries = result.getPageInfo().getTagLibraries();
             Map<?,?> prefixes = result.getPageInfo().getJspPrefixMapper();
             Iterator<?> iter = prefixes.keySet().iterator();
