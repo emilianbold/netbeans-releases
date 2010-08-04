63d753851a85b1e0f393eaacde60860c7a06213d
Jesse Glick <jglick@netbeans.org>
2010-08-04 07:10:15 -0400

    Also skipping python cluster for deprecatedModulesAreDisabled, since it still uses GSF.

--- o.n.core/test/qa-functional/src/org/netbeans/core/validation/ValidateModulesTest.java
+++ o.n.core/test/qa-functional/src/org/netbeans/core/validation/ValidateModulesTest.java
@@ -82,7 +82,7 @@ public class ValidateModulesTest extends NbTestCase {
         TestSuite suite = new TestSuite();
         suite.addTest(new ValidateModulesTest("clusterVersions"));
         suite.addTest(NbModuleSuite.create(NbModuleSuite.createConfiguration(ValidateModulesTest.class).addTest("deprecatedModulesAreDisabled").
-                clusters("(?!extra$).*").enableModules(".*").honorAutoloadEager(true).gui(false).enableClasspathModules(false)));
+                clusters("(?!(extra|python)$).*").enableModules(".*").honorAutoloadEager(true).gui(false).enableClasspathModules(false)));
         suite.addTest(NbModuleSuite.create(NbModuleSuite.createConfiguration(ValidateModulesTest.class).
                 clusters(".*").enableModules(".*").honorAutoloadEager(true).gui(false).enableClasspathModules(false)));
         suite.addTest(NbModuleSuite.create(NbModuleSuite.createConfiguration(ValidateModulesTest.class).
@@ -188,6 +188,7 @@ public class ValidateModulesTest extends NbTestCase {
 
     public void deprecatedModulesAreDisabled() {
         Set<String> cnbs = new TreeSet<String>();
+        StringBuilder problems = new StringBuilder();
         for (Module m : Main.getModuleSystem().getManager().getModules()) {
             if ("true".equals(m.getAttribute("OpenIDE-Module-Deprecated"))) {
                 String cnb = m.getCodeNameBase();
@@ -196,10 +197,16 @@ public class ValidateModulesTest extends NbTestCase {
                     continue;
                 }
                 cnbs.add(cnb);
-                assertFalse(cnb + " is deprecated and should not be enabled", m.isEnabled());
+                if (m.isEnabled()) {
+                    problems.append('\n').append(cnb).append(" is deprecated and should not be enabled");
+                }
             }
         }
-        System.out.println("Deprecated modules all correctly disabled: " + cnbs);
+        if (problems.length() > 0) {
+            fail("Some deprecated modules are in use" + problems);
+        } else {
+            System.out.println("Deprecated modules all correctly disabled: " + cnbs);
+        }
     }
 
     private static Set<Manifest> loadManifests() throws Exception {
