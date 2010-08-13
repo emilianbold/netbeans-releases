5cf87a4887d09ecc724dd198f4341201bb2da910
Jesse Glick <jglick@netbeans.org>
2010-08-13 15:44:26 -0400

    #189318: Maven: "Downloading ..." processes cannot be canceled

--- maven/src/org/netbeans/modules/maven/api/NbMavenProject.java
+++ maven/src/org/netbeans/modules/maven/api/NbMavenProject.java
@@ -188,7 +188,7 @@ public final class NbMavenProject {
                     AggregateProgressHandle hndl = AggregateProgressFactory.createHandle(NbBundle.getMessage(NbMavenProject.class, "Progress_Download"),
                             new ProgressContributor[] {
                                 AggregateProgressFactory.createProgressContributor("zaloha") },  //NOI18N
-                            null, null);
+                            ProgressTransferListener.cancellable(), null);
 
                     boolean ok = true;
                     try {
@@ -203,6 +203,7 @@ public final class NbMavenProject {
                             Exception ex = (Exception)res.getExceptions().get(0);
                             StatusDisplayer.getDefault().setStatusText(NbBundle.getMessage(NbMavenProject.class, "MSG_Failed", ex.getLocalizedMessage()));
                         }
+                    } catch (ThreadDeath d) { // download interrupted
                     } finally {
                         hndl.finish();
                         ProgressTransferListener.clearAggregateHandle();
@@ -366,7 +367,7 @@ public final class NbMavenProject {
                 }
                 String label = javadoc ? NbBundle.getMessage(NbMavenProject.class, "Progress_Javadoc") : NbBundle.getMessage(NbMavenProject.class, "Progress_Source");
                 AggregateProgressHandle handle = AggregateProgressFactory.createHandle(label,
-                        contribs, null, null);
+                        contribs, ProgressTransferListener.cancellable(), null);
                 handle.start();
                 try {
                     ProgressTransferListener.setAggregateHandle(handle);
@@ -375,6 +376,7 @@ public final class NbMavenProject {
                         downloadOneJavadocSources(online, contribs[index], project, a, javadoc);
                         index++;
                     }
+                } catch (ThreadDeath d) { // download interrupted
                 } finally {
                     handle.finish();
                     ProgressTransferListener.clearAggregateHandle();
