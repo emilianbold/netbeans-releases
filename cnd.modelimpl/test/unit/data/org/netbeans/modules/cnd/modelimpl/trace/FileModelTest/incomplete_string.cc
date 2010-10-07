7e6931aeb8aeec8f6b7473b804288c3e1a818eb0
Vladimir Voskresensky <vv159170@netbeans.org>
2010-10-07 16:56:56 +0400

    fixed #114788 -  unbalanced quote in inactive code break model for file

--- cnd.modelimpl/test/unit/data/org/netbeans/modules/cnd/modelimpl/trace/FileModelTest/incomplete_string.cc
+++ cnd.modelimpl/test/unit/data/org/netbeans/modules/cnd/modelimpl/trace/FileModelTest/incomplete_string.cc
@@ -3,3 +3,19 @@ char* v = "S  ;
 #endif
 
 int foo_incomplete_string();
+
+
+#if 0
+ it's a dead block with incomplete char literal
+
+#endif
+
+int foo_incomplete_char() {
+ char c = 'asdfasdf
+ c++;
+}
+
+int foo_incomplete_char_ok() {
+
+}
+
