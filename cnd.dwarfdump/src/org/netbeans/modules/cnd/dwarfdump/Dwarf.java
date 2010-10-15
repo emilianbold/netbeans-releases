905d5dd417ca166cad8630c5a89db44cd0d2dda7
Alexander Simon <alexvsimon@netbeans.org>
2010-10-15 15:59:42 +0400

    fixing Bug #189050 easy import doesn't work on MacOSX

--- cnd.dwarfdump/src/org/netbeans/modules/cnd/dwarfdump/Dwarf.java
+++ cnd.dwarfdump/src/org/netbeans/modules/cnd/dwarfdump/Dwarf.java
@@ -319,6 +319,7 @@ public class Dwarf {
                     if (!currentList.hasNext()) {
                         continue;
                     }
+                    break;
                 } else {
                     currentList = null;
                     return;
