f546e0a8ba30b930e9b219795abbe72181a4ff64
Alexander Simon <alexvsimon@netbeans.org>
2010-10-27 12:09:01 +0400

    fixed Bug #190205 'Go To Type' does not find 'enum' CpuArch using camelcase string 'CpuA'

--- cnd.gotodeclaration/src/org/netbeans/modules/cnd/gotodeclaration/type/CppTypeProvider.java
+++ cnd.gotodeclaration/src/org/netbeans/modules/cnd/gotodeclaration/type/CppTypeProvider.java
@@ -68,7 +68,6 @@ public class CppTypeProvider implements TypeProvider {
         return NbBundle.getMessage(CppTypeProvider.class, "TYPE_PROVIDER_DISPLAY_NAME"); // NOI18N
     }
 
-//    public List<? extends TypeDescriptor> getTypeNames(Project project, String text, SearchType type) {
     @Override
     public void computeTypeNames(Context context, Result res) {
         isCancelled = false;
@@ -78,14 +77,8 @@ public class CppTypeProvider implements TypeProvider {
 	
 	if( TRACE ) System.err.printf("CppTypeProvider.getTypeNames(%s, %s, %s)\n", project, text, type);
 	
-	
-	
-	CsmSelect.CsmFilter filter = org.netbeans.modules.cnd.gotodeclaration.matcher.NameMatcherFactory.createNameFilter(text, type);
+	CsmSelect.CsmFilter filter = createTypeFilter();
 	NameMatcher matcher = NameMatcherFactory.createNameMatcher(text, type);
-	if( filter == null) {
-	    return;
-	}
-        
 	if( project == null ) {
             Collection<CsmProject> csmProjects = CsmModelAccessor.getModel().projects();
 	    if( ! csmProjects.isEmpty() ) {
@@ -104,8 +97,7 @@ public class CppTypeProvider implements TypeProvider {
                 }
                 res.addResult(new ArrayList<TypeDescriptor>(result));
 	    }
-	}
-	else {
+	} else {
 	    Set<TypeDescriptor> result = new HashSet<TypeDescriptor>();
             CsmProject csmProject = CsmModelAccessor.getModel().getProject(project);
 	    processProject(csmProject, result, filter, matcher);
@@ -174,9 +166,11 @@ public class CppTypeProvider implements TypeProvider {
             case CLASS:
             case UNION:
             case STRUCT:
-                CsmClass cls = (CsmClass) decl;
-                result.add(createTypeDescriptor(cls));
-                if( ! isCancelled ) {
+                if(!isCancelled) {
+                    CsmClass cls = (CsmClass) decl;
+                    if (matcher.accept(decl.getName().toString())) {
+                        result.add(createTypeDescriptor(cls));
+                    }
                     for( CsmMember member : cls.getMembers() ) {
                         if( ! isCancelled ) {
                             if (matcher.accept(member.getName().toString())) {
@@ -188,27 +182,21 @@ public class CppTypeProvider implements TypeProvider {
                 break;
             case ENUM:
             case TYPEDEF:
-                result.add(createTypeDescriptor((CsmClassifier) decl));
-                break;
-            case BUILT_IN:
-            case ENUMERATOR:
-            case MACRO:
-            case VARIABLE:
-            case VARIABLE_DEFINITION:
-            case FUNCTION:
-            case FUNCTION_DEFINITION:
-            case TEMPLATE_SPECIALIZATION:
-            case ASM:
-            case TEMPLATE_DECLARATION:
-            case NAMESPACE_DEFINITION:
-            case NAMESPACE_ALIAS:
-            case USING_DIRECTIVE:
-            case USING_DECLARATION:
-            case CLASS_FORWARD_DECLARATION:
-            case CLASS_FRIEND_DECLARATION:
-            case FUNCTION_FRIEND:
-            case FUNCTION_FRIEND_DEFINITION:
+                if(!isCancelled) {
+                    if (matcher.accept(decl.getName().toString())) {
+                        result.add(createTypeDescriptor((CsmClassifier) decl));
+                    }
+                }
                 break;
         }
     }    
+
+    private CsmSelect.CsmFilter createTypeFilter() {
+        return CsmSelect.getFilterBuilder().createKindFilter(
+                CsmDeclaration.Kind.CLASS,
+                CsmDeclaration.Kind.STRUCT,
+                CsmDeclaration.Kind.UNION,
+                CsmDeclaration.Kind.ENUM,
+                CsmDeclaration.Kind.TYPEDEF);
+    }
 }
