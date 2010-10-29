56cd9fceefa5cc7d6101e59cd7e149ed54d34d7d
Ondrej Vrabec <ovrabec@netbeans.org>
2010-10-29 11:55:22 +0200

    resolving editor warnings

--- versioning/src/org/netbeans/modules/versioning/VersioningMainMenu.java
+++ versioning/src/org/netbeans/modules/versioning/VersioningMainMenu.java
@@ -63,14 +63,17 @@ import java.util.*;
  */
 public class VersioningMainMenu extends AbstractAction implements DynamicMenuContent {
 
+    @Override
     public void actionPerformed(ActionEvent e) {
         // does nothing, this is a popup menu
     }
 
+    @Override
     public JComponent[] getMenuPresenters() {
         return createMenu();
     }
 
+    @Override
     public JComponent[] synchMenuPresenters(JComponent[] items) {
         return createMenu();
     }
@@ -119,6 +122,7 @@ public class VersioningMainMenu extends AbstractAction implements DynamicMenuCon
         final JMenu menu = new JMenu();
         Mnemonics.setLocalizedText(menu, Utils.getMenuLabel(system));
         menu.addMenuListener(new MenuListener() {
+            @Override
             public void menuSelected(MenuEvent e) {
                 if (menu.getItemCount() != 0) return;
                 // context should be cached while the menu is displayed
@@ -126,9 +130,11 @@ public class VersioningMainMenu extends AbstractAction implements DynamicMenuCon
                 constructMenu(menu, system, ctx);
             }
     
+            @Override
             public void menuDeselected(MenuEvent e) {
             }
     
+            @Override
             public void menuCanceled(MenuEvent e) {
             }
         });
@@ -164,6 +170,7 @@ public class VersioningMainMenu extends AbstractAction implements DynamicMenuCon
     }
 
     static final class ByDisplayNameComparator implements Comparator<VersioningSystem> {
+        @Override
         public int compare(VersioningSystem a, VersioningSystem b) {
             return Utils.getDisplayName(a).compareTo(Utils.getDisplayName(b));
         }
