f1734ad98fbcc7ab5b2df4900728b15511b4301a
Vladimir Voskresensky <vv159170@netbeans.org>
2010-10-30 20:13:48 +0400

    fixed 191446 -  no code assistance for elementes #include'ed in namespace body

---
+++ cnd.completion/test/unit/data/org/netbeans/modules/cnd/completion/cplusplus/hyperlink/BasicHyperlinkTestCase/iz191446.h
@@ -0,0 +1,12 @@
+
+#ifndef PROJECT_TEST_H_Test191446
+#define    PROJECT_TEST_H_Test191446
+
+class Test191446{
+    public:
+        Test191446(){}
+        ~Test191446(){}
+        int tester(){ return 3;}
+};
+
+#endif
