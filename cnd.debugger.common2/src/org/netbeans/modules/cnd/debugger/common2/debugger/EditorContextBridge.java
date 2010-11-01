d274489ae8f35f407f4327eb3bc494d13a888a02
Egor Ushakov <gorrus@netbeans.org>
2010-11-01 13:16:47 +0300

    fixed #191251 (Unable to set breakpoint in undocked document)

--- cnd.debugger.common2/src/org/netbeans/modules/cnd/debugger/common2/debugger/EditorContextBridge.java
+++ cnd.debugger.common2/src/org/netbeans/modules/cnd/debugger/common2/debugger/EditorContextBridge.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright 2010 Oracle and/or its affiliates. All rights reserved.
  *
  * Oracle and Java are registered trademarks of Oracle and/or its affiliates.
  * Other names may be trademarks of their respective owners.
@@ -24,12 +24,6 @@
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
  *
- * Contributor(s):
- *
- * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
- * Microsystems, Inc. All Rights Reserved.
- *
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -40,59 +34,52 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
+ *
+ * Contributor(s):
+ *
+ * Portions Copyrighted 2010 Sun Microsystems, Inc.
  */
 
-package org.netbeans.modules.cnd.debugger.common2.debugger.actions;
+package org.netbeans.modules.cnd.debugger.common2.debugger;
 
-import org.netbeans.spi.debugger.ActionsProviderSupport;
-import org.netbeans.spi.debugger.ContextProvider;
-
-import org.netbeans.modules.cnd.debugger.common2.debugger.NativeDebugger;
-import org.netbeans.modules.cnd.debugger.common2.debugger.DebuggerManager;
-import org.netbeans.modules.cnd.debugger.common2.debugger.State;
-import org.netbeans.modules.cnd.debugger.common2.debugger.StateListener;
+import java.beans.PropertyChangeListener;
+import org.netbeans.modules.cnd.utils.MIMENames;
+import org.netbeans.spi.debugger.ui.EditorContextDispatcher;
+import org.openide.filesystems.FileObject;
 
 /**
- * Common code for all ActionProvider's in this package.
+ *
+ * @author Egor Ushakov
  */
+public class EditorContextBridge {
+    private static final EditorContextDispatcher contextDispatcher = EditorContextDispatcher.getDefault();
 
-abstract class NativeActionsProvider
-    extends ActionsProviderSupport implements StateListener {
-
-    private ContextProvider ctx;
-
-    public NativeActionsProvider(ContextProvider ctx) {
-	this.ctx = ctx;
-	if (getDebugger() != null)
-	    getDebugger().addStateListener(this);
+    public static String getCurrentURL() {
+        return contextDispatcher.getCurrentURLAsString();
     }
 
-    // interface ActionsProvider
-    // override ActionsProvider
-    @Override
-    public void postAction (final Object action,
-			    final Runnable actionPerformedNotifier) {
-	// Comes on the eventQ
-	// The default implementation of postAction calls doAction on an RP.
-	// We call it in the eventQ.
-	try {
-	    doAction(action);
-	} finally {
-	    actionPerformedNotifier.run();
-	}
-    }
-    protected NativeDebugger getDebugger() {
-	// TODO: how do we know this returns the debugger for 
-	// the current session?
-	if (ctx != null)
-	    return ctx.lookupFirst(null, NativeDebugger.class);
-	else {
-	    NativeDebugger nd = DebuggerManager.get().currentDebugger();
-	    return (nd != null) ? nd : null;
-	}
+    public static String getCurrentFilePath() {
+        FileObject currentFile = contextDispatcher.getCurrentFile();
+        if (currentFile != null) {
+            return currentFile.getPath();
+        }
+        return "";
     }
 
-    protected DebuggerManager manager() {
-	return DebuggerManager.get();
+    public static int getCurrentLineNumber() {
+        return contextDispatcher.getCurrentLineNumber();
+    }
+    
+    public static String getCurrentMIMEType() {
+        FileObject fo = contextDispatcher.getCurrentFile();
+        return fo != null ? fo.getMIMEType() : ""; // NOI18N
+    }
+
+    public static void addPropertyChangeListener(PropertyChangeListener l) {
+        contextDispatcher.addPropertyChangeListener(MIMENames.C_MIME_TYPE, l);
+        contextDispatcher.addPropertyChangeListener(MIMENames.CPLUSPLUS_MIME_TYPE, l);
+        contextDispatcher.addPropertyChangeListener(MIMENames.HEADER_MIME_TYPE, l);
+        contextDispatcher.addPropertyChangeListener(MIMENames.ASM_MIME_TYPE, l);
+        contextDispatcher.addPropertyChangeListener(MIMENames.FORTRAN_MIME_TYPE, l);
     }
 }
