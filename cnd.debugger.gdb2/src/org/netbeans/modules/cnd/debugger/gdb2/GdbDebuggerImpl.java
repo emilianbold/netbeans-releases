c9737debee7d9eb12e463de3aefd343ed8e9187b
Egor Ushakov <gorrus@netbeans.org>
2010-11-01 13:43:16 +0300

    fixing #191466 (AssertionError on close IDE)

--- cnd.debugger.gdb2/src/org/netbeans/modules/cnd/debugger/gdb2/GdbDebuggerImpl.java
+++ cnd.debugger.gdb2/src/org/netbeans/modules/cnd/debugger/gdb2/GdbDebuggerImpl.java
@@ -642,11 +642,7 @@ public final class GdbDebuggerImpl extends NativeDebuggerImpl
     }
 
     public void shutDown() {
-	/* LATER postKill doesn't quite work
-	// dbx side does something a bit more specialized but should it?
 	postKill();
-	*/
-	assert false : "shutDown(): NOT IMPLEMENTED";
     }
 
     public final void stepInto() {
