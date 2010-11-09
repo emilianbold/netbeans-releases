8dc10220cdff3d3547f147410eb52e6275c21a91
David Konecny <dkonecny@netbeans.org>
2010-11-09 13:41:04 +1300

    #Bug 159110 - Web Project - Properties, Sources - Label column is not attached to the right boarder

--- j2ee.ejbjarproject/src/org/netbeans/modules/j2ee/ejbjarproject/ui/customizer/CustomizerSources.java
+++ j2ee.ejbjarproject/src/org/netbeans/modules/j2ee/ejbjarproject/ui/customizer/CustomizerSources.java
@@ -45,27 +45,19 @@
 package org.netbeans.modules.j2ee.ejbjarproject.ui.customizer;
 
 import org.netbeans.modules.java.api.common.project.ui.customizer.SourceRootsUi;
-import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.io.File;
 import java.nio.charset.Charset;
-import java.nio.charset.CharsetDecoder;
-import java.nio.charset.CharsetEncoder;
-import java.util.logging.Logger;
-import javax.swing.DefaultComboBoxModel;
 import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
-import javax.swing.JLabel;
-import javax.swing.JList;
-import javax.swing.ListCellRenderer;
 import javax.swing.UIManager;
 import javax.swing.event.ListDataEvent;
 import javax.swing.event.ListDataListener;
-import javax.swing.plaf.UIResource;
 import org.netbeans.api.queries.CollocationQuery;
+import org.netbeans.modules.j2ee.common.Util;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
 import org.openide.util.HelpCtx;
@@ -168,6 +160,8 @@ public class CustomizerSources extends javax.swing.JPanel implements HelpCtx.Pro
             }
         });
         
+        Util.initTwoColumnTableVisualProperties(this, sourceRoots);
+        Util.initTwoColumnTableVisualProperties(this, testRoots);
         this.uiProperties = uiProperties;
     }
     
