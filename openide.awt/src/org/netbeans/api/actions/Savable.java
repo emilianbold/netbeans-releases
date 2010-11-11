8db6be383fa172261cc5c84bbaf5194c2a9b1d8f
Jaroslav Tulach <jtulach@netbeans.org>
2010-11-11 11:57:33 +0100

    Initial proposal for Savable and Savables

--- openide.awt/src/org/netbeans/api/actions/Savable.java
+++ openide.awt/src/org/netbeans/api/actions/Savable.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright 2010 Oracle and/or its affiliates. All rights reserved.
  *
  * Oracle and Java are registered trademarks of Oracle and/or its affiliates.
  * Other names may be trademarks of their respective owners.
@@ -24,12 +24,6 @@
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
  *
- * Contributor(s):
- *
- * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
- * Microsystems, Inc. All Rights Reserved.
- *
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -40,16 +34,27 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
+ *
+ * Contributor(s):
+ *
+ * Portions Copyrighted 2010 Sun Microsystems, Inc.
  */
-package org.openide.cookies;
 
-import org.netbeans.api.actions.Savable;
-import org.openide.nodes.Node;
+package org.netbeans.api.actions;
 
+import java.io.IOException;
+import org.netbeans.spi.actions.Savables;
 
-/** The cookie for the save operation.
-*
-* @author Dafe Simonek
-*/
-public interface SaveCookie extends Node.Cookie, Savable {
+/** Context interface that represents ability to save. To get best
+ * interaction with the system, it is preferable to use {@link Savables}
+ * to create instances of this interface rather than implementing it 
+ * directly.
+ *
+ * @author Jaroslav Tulach <jtulach@netbeans.org>
+ */
+public interface Savable {
+    /** Invoke the save operation.
+     * @throws IOException if the object could not be saved
+     */
+    public void save() throws IOException;
 }
--- openide.awt/src/org/netbeans/spi/actions/Savables.java
+++ openide.awt/src/org/netbeans/spi/actions/Savables.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright 2010 Oracle and/or its affiliates. All rights reserved.
  *
  * Oracle and Java are registered trademarks of Oracle and/or its affiliates.
  * Other names may be trademarks of their respective owners.
@@ -24,12 +24,6 @@
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
  *
- * Contributor(s):
- *
- * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
- * Microsystems, Inc. All Rights Reserved.
- *
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -40,16 +34,43 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
+ *
+ * Contributor(s):
+ *
+ * Portions Copyrighted 2010 Sun Microsystems, Inc.
  */
-package org.openide.cookies;
 
+package org.netbeans.spi.actions;
+
+import java.util.Set;
+import java.util.concurrent.Callable;
 import org.netbeans.api.actions.Savable;
-import org.openide.nodes.Node;
 
+/** Default implementation of {@link Savable} interface. 
+ *
+ * @author Jaroslav Tulach <jtulach@netbeans.org>
+ */
+public final class Savables {
+    private Savables() {
+    }
+    
+    public static Savable create(
+        Object identity, 
+        Callable<Void> callback,
+        CharSequence displayName,
+        CharSequence saveMessage
+    ) {
+        return null;
+    }
 
-/** The cookie for the save operation.
-*
-* @author Dafe Simonek
-*/
-public interface SaveCookie extends Node.Cookie, Savable {
+    public static void destroy(Object identity) {
+    }
+    
+    public static Set<Savable> findPendingSavables() {
+        return null;
+    }
+    
+    public static String findDisplayName(Savable savable) {
+        return null;
+    }
 }
