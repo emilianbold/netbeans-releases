c212ede56bed76e91beaa742e588131ccbc28985
Vladimir Voskresensky <vv159170@netbeans.org>
2010-11-22 20:59:41 +0300

    more trace

--- cnd.modelimpl/test/unit/src/org/netbeans/modules/cnd/modelimpl/impl/services/SelectTestBase.java
+++ cnd.modelimpl/test/unit/src/org/netbeans/modules/cnd/modelimpl/impl/services/SelectTestBase.java
@@ -116,6 +116,7 @@ public abstract class SelectTestBase extends ModelImplBaseTestCase {
                 final CsmFile containingFile = func.getContainingFile();
                 boolean ok = iter.hasNext();
                 if (!ok) {
+                    System.err.println("ERROR FOR: " + decl + "\n\tUIN=" + decl.getUniqueName() + "\n\tFQN="+qName);
                     // more trace
                     if (dumpProjectContainer && project instanceof ProjectBase) {
                         dumpProjectContainer = false;
