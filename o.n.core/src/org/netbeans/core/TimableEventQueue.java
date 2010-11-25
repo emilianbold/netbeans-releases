334db0b52dac6a9ee287315ccd6c8f744caebbe7
Jaroslav Tulach <jtulach@netbeans.org>
2010-11-25 14:58:08 +0100

    More of #192427: Don't deal with main window in the test, consider it showing

--- o.n.core/src/org/netbeans/core/TimableEventQueue.java
+++ o.n.core/src/org/netbeans/core/TimableEventQueue.java
@@ -98,14 +98,19 @@ implements Runnable {
     private volatile ActionListener stoppable;
     private volatile boolean isWaitCursor;
     static volatile Thread eq;
-    private final Frame mainWindow = WindowManager.getDefault().getMainWindow();
+    private final Frame mainWindow;
 
-    public TimableEventQueue() {
+    TimableEventQueue(Frame f) {
+        this.mainWindow = f;
         TIMEOUT = RP.create(this);
         TIMEOUT.setPriority(Thread.MIN_PRIORITY);
     }
 
     static void initialize() {
+        initialize(null, true);
+    }
+    static void initialize(final Frame f, final boolean defaultWindow) {
+        
         // #28536: make sure a JRE bug does not prevent the event queue from having
         // the right context class loader
         // and #35470: do it early, before any module-loaded AWT code might run
@@ -116,11 +121,15 @@ implements Runnable {
             Mutex.EVENT.writeAccess (new Mutex.Action<Void>() {
                 @Override
                 public Void run() {
+                    Frame use = f;
+                    if (defaultWindow && use == null) {
+                        use = WindowManager.getDefault().getMainWindow();
+                    }
                     ClassLoader scl = Lookup.getDefault().lookup(ClassLoader.class);
                     if (scl != null) {
                         Thread.currentThread().setContextClassLoader(scl);
                     }
-                    Toolkit.getDefaultToolkit().getSystemEventQueue().push(new TimableEventQueue());
+                    Toolkit.getDefaultToolkit().getSystemEventQueue().push(new TimableEventQueue(use));
                     LOG.fine("Initialization done");
                     return null;
                 }
@@ -178,10 +187,14 @@ implements Runnable {
         }
         return;
     }
+    
+    private boolean isShowing() {
+        return mainWindow == null || mainWindow.isShowing();
+    }
 
     private boolean tick(String name) {
         start = System.currentTimeMillis();
-        if (start >= ignoreTill && mainWindow.isShowing()) {
+        if (start >= ignoreTill && isShowing()) {
             LOG.log(Level.FINEST, "tick, schedule a timer for {0}", name);
             TIMEOUT.schedule(QUANTUM);
             return true;
