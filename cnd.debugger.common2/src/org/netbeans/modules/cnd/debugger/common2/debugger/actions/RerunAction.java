c4ef5adae818e7b800f47800e7721db1fd527fde
Vladimir Voskresensky <vv159170@netbeans.org>
2010-12-07 18:14:42 +0300

    fixing #191731 -  Too many CND classes loaded for JavaSE project

--- cnd.debugger.common2/src/org/netbeans/modules/cnd/debugger/common2/debugger/actions/RerunAction.java
+++ cnd.debugger.common2/src/org/netbeans/modules/cnd/debugger/common2/debugger/actions/RerunAction.java
@@ -81,10 +81,13 @@ public class RerunAction extends CallableSystemAction implements StateListener {
 
     // interface SystemAction
     public String getName() {
-	if (DebuggerManager.isStandalone() || !DebuggerOption.RUN_AUTOSTART.isEnabled(DebuggerManager.get().globalOptions()))
+        // removed call to DebuggerManager.isStandalone() to prevent loading of class
+        // standalone tool rebrands LBL_RerunProcess and set text of LBL_RunProcess
+	if (!DebuggerOption.RUN_AUTOSTART.isEnabled(DebuggerManager.get().globalOptions())) {
 	    return Catalog.get("LBL_RunProcess"); // NOI18N
-	else
+        } else {
 	    return Catalog.get("LBL_RerunProcess"); // NOI18N
+        }
     }
     
 
