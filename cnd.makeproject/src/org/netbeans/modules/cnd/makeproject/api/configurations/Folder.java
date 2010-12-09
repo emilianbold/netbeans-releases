0f9ca18d491f6cc25864105b883dc4571f535203
Vladimir Kvashin <vkvashin@netbeans.org>
2010-12-09 23:45:13 +0300

    better diagnostics for #191913 -  Exception: null folder file object

--- cnd.makeproject/src/org/netbeans/modules/cnd/makeproject/api/configurations/Folder.java
+++ cnd.makeproject/src/org/netbeans/modules/cnd/makeproject/api/configurations/Folder.java
@@ -143,8 +143,8 @@ public class Folder implements FileChangeListener, ChangeListener {
         String AbsRootPath = CndPathUtilitities.toAbsolutePath(configurationDescriptor.getBaseDir(), rootPath);
         AbsRootPath = RemoteFileUtil.normalizeAbsolutePath(AbsRootPath, getProject());
         FileObject folderFile = RemoteFileUtil.getFileObject(AbsRootPath, getProject());
-        CndUtils.assertNotNull(folderFile, "null folder file object"); //NOI18N
         if (folderFile == null) {
+            log.log(Level.INFO, "Null file object; folder kind: {0}, path: {1}", new Object[] { kind, AbsRootPath }); //NOI18N
             return;
         }
 
