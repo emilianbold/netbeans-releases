ecc27dcbf1f57a58f7d66463da5c58d178ef34b9
Alexander Simon <alexvsimon@netbeans.org>
2010-12-15 17:13:31 +0300

    fix unit tests

--- cnd.discovery/test/unit/src/org/netbeans/modules/cnd/discovery/project/MakeProjectTestBase.java
+++ cnd.discovery/test/unit/src/org/netbeans/modules/cnd/discovery/project/MakeProjectTestBase.java
@@ -69,7 +69,6 @@ import org.netbeans.modules.cnd.api.project.NativeProject;
 import org.netbeans.modules.nativeexecution.api.util.Path;
 import org.netbeans.modules.cnd.discovery.projectimport.ImportProject;
 import org.netbeans.modules.cnd.makeproject.MakeOptions;
-import org.netbeans.modules.cnd.makeproject.MakeProjectTypeImpl;
 import org.netbeans.modules.cnd.makeproject.api.wizards.WizardConstants;
 import org.netbeans.modules.cnd.modelimpl.csm.core.ModelImpl;
 import org.netbeans.modules.cnd.modelimpl.repository.RepositoryUtils;
@@ -110,19 +109,10 @@ public abstract class MakeProjectTestBase extends CndBaseTestCase { //extends Nb
     @Override
     protected void setUp() throws Exception {
         super.setUp();
-        //MockServices.setServices(MakeProjectType.class);
         MakeOptions.getInstance().setFixUnresolvedInclude(false);
         startupModel();
     }
 
-    @Override
-    protected List<Class<?>> getServices() {
-        List<Class<?>> list = new ArrayList<Class<?>>();
-        list.add(MakeProjectTypeImpl.class);
-        list.addAll(super.getServices());
-        return list;
-    }
- 
     @Override
     protected void setUpMime() {
         // setting up MIME breaks other services
