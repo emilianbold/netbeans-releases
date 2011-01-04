1fc2ec00a97715f5a12107f74169d912fd1e8a4f
vince kraemer <vkraemer@netbeans.org>
2011-01-04 12:37:02 -0800

    #178145 : MessageDestinations are not displayed in the multiview editor for sun-application-client.xml

--- j2ee.sun.ddui/src/org/netbeans/modules/j2ee/sun/ddloaders/multiview/jms/MessageDestinationGroupNode.java
+++ j2ee.sun.ddui/src/org/netbeans/modules/j2ee/sun/ddloaders/multiview/jms/MessageDestinationGroupNode.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright 1997-2011 Oracle and/or its affiliates. All rights reserved.
  *
  * Oracle and Java are registered trademarks of Oracle and/or its affiliates.
  * Other names may be trademarks of their respective owners.
@@ -94,7 +94,7 @@ public class MessageDestinationGroupNode extends NamedBeanGroupNode {
         } else if(sunEjbJar != null) {
             EnterpriseBeans eb = sunEjbJar.getEnterpriseBeans();
             destinations = (eb != null) ? eb.getMessageDestination() : null;
-        } else if(sunWebApp != null) {
+        } else if(sunAppClient != null) {
             destinations = sunAppClient.getMessageDestination();
         }
         return destinations;
