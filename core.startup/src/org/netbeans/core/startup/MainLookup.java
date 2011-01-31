d4296488c71ebf070681443101dee05f755d0522
Jesse Glick <jglick@netbeans.org>
2011-01-31 17:03:49 -0500

    Just print a stack trace in case MISL is modified incompatibly - ought to be fixed ASAP anyway.

--- core.startup/src/org/netbeans/core/startup/MainLookup.java
+++ core.startup/src/org/netbeans/core/startup/MainLookup.java
@@ -45,10 +45,10 @@
 package org.netbeans.core.startup;
 
 import java.lang.reflect.Field;
-import org.netbeans.JaveleonModule;
 import org.netbeans.Module;
 import org.openide.modules.ModuleInfo;
 import org.openide.modules.Modules;
+import org.openide.util.Exceptions;
 import org.openide.util.Lookup;
 import org.openide.util.lookup.AbstractLookup;
 import org.openide.util.lookup.InstanceContent;
@@ -108,11 +108,8 @@ public final class MainLookup extends ProxyLookup {
                     Field field = look.getClass().getDeclaredField("loader");
                     field.setAccessible(true);
                     field.set(look, classLoader);
-                } catch (Exception ex) {
-                    // OK the field have been removed from the class
-                    // just replace the MetaInfLookup instance then
-                    // warn user about this though.
-                    System.err.println("[JAVELOEN WARNING]: had to replace the META-INF/services lookup. Any attempt to lookup an already looked up class or interface will give you a completely fresh implementation instance!");
+                } catch (Exception x) {
+                    Exceptions.printStackTrace(x);
                 }
             }
         }
@@ -131,7 +128,7 @@ public final class MainLookup extends ProxyLookup {
             classLoader = nue;
             MainLookup l = (MainLookup)Lookup.getDefault();
             Lookup[] delegates = l.getLookups();
-            Lookup[] newDelegates = delegates.clone();          
+            Lookup[] newDelegates = delegates.clone();
             // Replace classloader.
             newDelegates[0] = Lookups.metaInfServices(classLoader);
             newDelegates[1] = Lookups.singleton(classLoader);
