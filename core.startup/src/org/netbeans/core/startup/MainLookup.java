e002da35d2b673ebac2116fa232e1679492d06d0
allangregersen <allangregersen@netbeans.org>
2011-02-07 08:57:42 +0100

    No need to iterate through all lookups when we know that MetaInfServices is at index 0 always. At least other code rely on this assumption as well.

--- core.startup/src/org/netbeans/core/startup/MainLookup.java
+++ core.startup/src/org/netbeans/core/startup/MainLookup.java
@@ -93,26 +93,26 @@ public final class MainLookup extends ProxyLookup {
     }
     
     /**
+     * This method is specific to Javeleon, and used only
+     * by Javeleon.
      * Persisting all previously looked up
      * service instances from META-INF/services.
      * All new lookups will ask the new system class loader.
-     */    
+     */
     static void systemClassLoaderChangedForJaveleon(ClassLoader nue) {
         classLoader = nue;
         MainLookup l = (MainLookup)Lookup.getDefault();
         Lookup[] newDelegates = l.getLookups().clone();
         
-        for (Lookup look : newDelegates) {
-            if (look.getClass().getName().contains("MetaInfServicesLookup")) {
-                try {
-                    Field field = look.getClass().getDeclaredField("loader");
-                    field.setAccessible(true);
-                    field.set(look, classLoader);
-                } catch (Exception x) {
-                    Exceptions.printStackTrace(x);
-                }
+        // The MetaInf/services lookup instance is always placed at index 0.
+        Lookup metaServicesLookup = newDelegates[0];
+            try {
+                Field field = metaServicesLookup.getClass().getDeclaredField("loader");
+                field.setAccessible(true);
+                field.set(metaServicesLookup, classLoader);
+            } catch (Exception x) {
+                Exceptions.printStackTrace(x);
             }
-        }
         newDelegates[1] = Lookups.singleton(classLoader);
         l.setLookups(newDelegates);
     }
