812f4af77e5e5399890fc3028a824f8318dbe9a2
Vladimir Voskresensky <vv159170@netbeans.org>
2011-02-09 13:29:34 +0300

    more trace

--- cnd.apt/src/org/netbeans/modules/cnd/apt/support/IncludeDirEntry.java
+++ cnd.apt/src/org/netbeans/modules/cnd/apt/support/IncludeDirEntry.java
@@ -62,6 +62,8 @@ import org.openide.util.Exceptions;
  * @author Vladimir Voskresensky
  */
 public final class IncludeDirEntry {
+    private final static boolean EXTRA_TRACE_FAILED_INCLUDES = Boolean.getBoolean("cnd.apt.extra.trace.failed.includes"); // NOI18N
+    
     static {
         SupportAPIAccessor.register(new AccessorImpl());
     }
@@ -115,6 +117,8 @@ public final class IncludeDirEntry {
                     } catch (FileStateInvalidException ex) {
                         Exceptions.printStackTrace(ex);
                     }
+                } else if (EXTRA_TRACE_FAILED_INCLUDES) {
+                    System.err.println("NOT EXISTING IncludeDirEntry " + dir + "File.exists()=" + new File(dir).exists()); // NOI18N
                 }
                 CharSequence asCharSeq = FilePathCache.getManager().getString(dir);
                 out = new IncludeDirEntry(exists, framework, entryFS, asCharSeq);
