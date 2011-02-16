3f7c192e78c3b623a2586bd38959e95e6d5784bf
Alexander Simon <alexvsimon@netbeans.org>
2011-02-16 20:11:29 +0300

    fix name of reparse menu item

--- cnd.modelui/src/org/netbeans/modules/cnd/modelui/actions/ReparseProjectAction.java
+++ cnd.modelui/src/org/netbeans/modules/cnd/modelui/actions/ReparseProjectAction.java
@@ -43,9 +43,11 @@
 package org.netbeans.modules.cnd.modelui.actions;
 
 import java.util.Collection;
+import javax.swing.JMenuItem;
 import org.netbeans.modules.cnd.api.model.CsmModelAccessor;
 import org.netbeans.modules.cnd.api.model.CsmProject;
 import org.openide.util.NbBundle;
+import org.openide.util.actions.Presenter.Menu;
         
 /**
  * @author Vladimir Voskresensky
@@ -58,10 +60,14 @@ public final class ReparseProjectAction extends ProjectActionBase {
 
     @Override
     public String getName() {
+        final Collection<CsmProject> projects = getCsmProjects(getActivatedNodes());
+        System.err.println("getName " + projects);
+        if (projects != null && projects.size() > 1) {
+            return NbBundle.getMessage(getClass(), "CTL_ReparseProjects", projects.size()); //NOI18N
+        }
         return NbBundle.getMessage(getClass(), "CTL_ReparseProject"); //NOI18N
     }
 
-    
     @Override
     protected void performAction(Collection<CsmProject> csmProjects) {
         CsmModelAccessor.getModel().scheduleReparse(csmProjects);
