1ca3aa98ab7565e50f75277b8afa9ae71d132d83
Jesse Glick <jglick@netbeans.org>
2011-02-23 19:52:42 -0500

    Removing references to visualweb.

--- nbbuild/newbuild/init.sh
+++ nbbuild/newbuild/init.sh
@@ -110,10 +110,8 @@ mkdir -p $LOGS
 #LOGS
 IDE_BUILD_LOG=$LOGS/$BASENAME-build-ide.log
 MOBILITY_BUILD_LOG=$LOGS/$BASENAME-build-mobility.log
-VISUALWEB_BUILD_LOG=$LOGS/$BASENAME-build-visualweb.log
 NBMS_BUILD_LOC=$LOGS/$BASENAME-build-nbms.log
 SCP_LOG=$LOGS/$BASENAME-scp.log
 MAC_LOG=$LOGS/$BASENAME-native_mac.log
 MAC_LOG_NEW=$LOGS/$BASENAME-native_mac_new.log
 INSTALLER_LOG=$LOGS/$BASENAME-installers.log
-VISUALWEB_SANITY_LOG=$LOGS/$BASENAME-sanity-visualweb.log
