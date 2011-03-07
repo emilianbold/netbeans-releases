22f96010019ddf428decb9ad16abb8cad59338ce
Jesse Glick <jglick@netbeans.org>
2011-03-07 17:38:34 -0500

    Another Aether NPE like #195045, #190588, and others. Providing a utility method to fix this more simply. Cannot be fully automatic since it must be done in the active thread.

--- maven.embedder/src/org/netbeans/modules/maven/embedder/NBRepositoryModelResolver.java
+++ maven.embedder/src/org/netbeans/modules/maven/embedder/NBRepositoryModelResolver.java
@@ -98,6 +98,7 @@ class NBRepositoryModelResolver
     @Override
     public ModelSource resolveModel(String groupId, String artifactId, String version)
             throws UnresolvableModelException {
+        embedder.setUpLegacySupport();
         Artifact artifactParent = embedder.lookupComponent(RepositorySystem.class).createProjectArtifact(groupId, artifactId, version);
         try {
             embedder.resolve(artifactParent, remoteRepositories, embedder.getLocalRepository());
