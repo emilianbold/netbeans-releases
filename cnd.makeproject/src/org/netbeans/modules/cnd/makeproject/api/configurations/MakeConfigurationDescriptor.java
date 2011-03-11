aa85f00eccabc3f7a8057bc9552f213b6b7587b3
Alexander Simon <alexvsimon@netbeans.org>
2011-03-11 16:10:09 +0300

    fixed Bug #196587 performance regression in NativeProject.findFileItem().

--- cnd.makeproject/src/org/netbeans/modules/cnd/makeproject/api/configurations/MakeConfigurationDescriptor.java
+++ cnd.makeproject/src/org/netbeans/modules/cnd/makeproject/api/configurations/MakeConfigurationDescriptor.java
@@ -496,7 +496,7 @@ public final class MakeConfigurationDescriptor extends ConfigurationDescriptor i
     }
 
     public Item findItemByFileObject(FileObject fileObject) {
-        return findItemByPathImpl(fileObject.getPath());
+        return findProjectItemByPath(fileObject.getPath());
     }
 
     public Item findProjectItemByPath(String path) {
