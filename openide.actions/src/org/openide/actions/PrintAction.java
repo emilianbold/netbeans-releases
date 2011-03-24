648c06fe1e04ad5c62af8e2c25391cb039782383
Jesse Glick <jglick@netbeans.org>
2011-03-24 14:31:51 -0400

    Reverting accidental change made by anebuzelsky in 01a18eb459a2.

--- openide.actions/src/org/openide/actions/PrintAction.java
+++ openide.actions/src/org/openide/actions/PrintAction.java
@@ -71,6 +71,8 @@ public class PrintAction extends CookieAction {
 
             if (pc != null) {
                 pc.print();
+            }
+        }
     }
 
     protected boolean asynchronous() {
