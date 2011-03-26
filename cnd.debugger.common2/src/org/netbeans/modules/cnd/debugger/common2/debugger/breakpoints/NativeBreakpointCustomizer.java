23590dbe0a1c60b4c12adaaa92e649ba2c694e45
Egor Ushakov <gorrus@netbeans.org>
2011-03-26 11:07:17 +0300

    fixed #195529 (right-click on breakpoint shows disabled properties)

--- cnd.debugger.common2/src/org/netbeans/modules/cnd/debugger/common2/debugger/breakpoints/NativeBreakpointCustomizer.java
+++ cnd.debugger.common2/src/org/netbeans/modules/cnd/debugger/common2/debugger/breakpoints/NativeBreakpointCustomizer.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright 2011 Oracle and/or its affiliates. All rights reserved.
  *
  * Oracle and Java are registered trademarks of Oracle and/or its affiliates.
  * Other names may be trademarks of their respective owners.
@@ -23,13 +23,7 @@
  * License Header, with the fields enclosed by brackets [] replaced by
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- *
- * Contributor(s):
- *
- * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
- * Microsystems, Inc. All Rights Reserved.
- *
+ * 
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -40,79 +34,54 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
+ * 
+ * Contributor(s):
+ * 
+ * Portions Copyrighted 2008 Sun Microsystems, Inc.
  */
 
 package org.netbeans.modules.cnd.debugger.common2.debugger.breakpoints;
 
-import java.lang.ref.Reference;
-import java.lang.ref.WeakReference; 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.beans.Customizer;
 
 import javax.swing.JComponent;
+import javax.swing.JPanel;
 
-import org.netbeans.spi.debugger.ui.BreakpointType;
 import org.netbeans.spi.debugger.ui.Controller;
 
-abstract public class NativeBreakpointType extends BreakpointType {
-
-    private final static String CATEGORY_NAME = "native"; // NOI18N
-
-    /**
-     * Factory method.
-     */
-
-    public abstract NativeBreakpoint newInstance(int flags);
-
-    private Reference<BreakpointPanel> customizerRef = 
-	new WeakReference<BreakpointPanel>(null);
-
-    // interface BreakpointType
-    public JComponent getCustomizer() {
-	BreakpointPanel customizer = getCustomizer(null);
-	customizerRef = new WeakReference<BreakpointPanel>(customizer);
-	return customizer;
+/**
+ *
+ * @author Egor Ushakov
+ */
+public class NativeBreakpointCustomizer extends JPanel implements Customizer, Controller {
+    
+    private JComponent c;
+    
+    public void setObject(Object bean) {
+        if (!(bean instanceof NativeBreakpoint)) {
+            throw new IllegalArgumentException(bean.toString());
+        }
+        init((NativeBreakpoint) bean);
     }
     
-    // interface BreakpointType
-    @Override
-    public Controller getController() {
-	// OLD BreakpointPanel customizer = getCustomizer(null);
-	BreakpointPanel customizer = customizerRef.get();
-	if (customizer != null)
-	    return customizer.getController();
-	else
-	    return null;
+    private void init(NativeBreakpoint b) {
+        c = b.getBreakpointType().getCustomizer(b.makeEditableCopy());
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.weightx = 1.0;
+        gbc.weighty = 1.0;
+        add(c, gbc);
     }
 
-    /**
-     * The following abstract getCustomizer() is not the same as 
-     * BreakpointType.getCustomizer()!
-     *
-     * If 'bpt' is non-null the returned component is used for editing (i.e.
-     * CustomizeBreakpoint actions)
-     *
-     * If 'bpt' is null the returned component is used for creation of
-     * new breakpoints.
-     */
-
-    abstract public BreakpointPanel getCustomizer(NativeBreakpoint editableBreakpoint);
-
-    // interface BreakpointType
-    public final String getCategoryDisplayName() {
-	    return CATEGORY_NAME;
-    } 
-
-    static boolean isOurs(String category) {
-	return CATEGORY_NAME.equals(category);
+    public boolean ok() {
+        return ((ControllerProvider) c).getController().ok();
     }
 
-    /**
-     * Only one subclass of ours, FunctionBreakpointType, overrides this
-     * to return true;.
-     */
-
-    // interface BreakpointType
-    public boolean isDefault() {
-	return false;
-    } 
+    public boolean cancel() {
+        return ((ControllerProvider) c).getController().cancel();
+    }
 
 }
