15c436681548ef0c71a9dba49341dd4e97c2d7d8
Egor Ushakov <gorrus@netbeans.org>
2011-04-20 14:19:02 +0400

    fixing 7036869 (various problems in disassembly mode) - context menu actions

--- cnd.debugger.common2/src/org/netbeans/modules/cnd/debugger/common2/debugger/actions/GoToSourceAction.java
+++ cnd.debugger.common2/src/org/netbeans/modules/cnd/debugger/common2/debugger/actions/GoToSourceAction.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright 2011 Oracle and/or its affiliates. All rights reserved.
  *
  * Oracle and Java are registered trademarks of Oracle and/or its affiliates.
  * Other names may be trademarks of their respective owners.
@@ -24,12 +24,6 @@
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
  *
- * Contributor(s):
- *
- * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2007 Sun
- * Microsystems, Inc. All Rights Reserved.
- *
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -40,25 +34,81 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
+ *
+ * Contributor(s):
+ *
+ * Portions Copyrighted 2011 Sun Microsystems, Inc.
  */
+package org.netbeans.modules.cnd.debugger.common2.debugger.actions;
 
-package org.netbeans.modules.cnd.debugger.common2.debugger.assembly;
-
-import org.netbeans.modules.cnd.debugger.common2.debugger.breakpoints.NativeBreakpoint;
+import org.netbeans.modules.cnd.debugger.common2.debugger.DebuggerManager;
+import org.netbeans.modules.cnd.debugger.common2.debugger.NativeDebugger;
+import org.netbeans.modules.cnd.debugger.common2.debugger.NativeDebuggerImpl;
+import org.netbeans.modules.cnd.debugger.common2.debugger.State;
 import org.netbeans.modules.cnd.debugger.common2.debugger.StateListener;
+import org.openide.util.HelpCtx;
+import org.openide.util.actions.CallableSystemAction;
+import org.openide.util.lookup.ServiceProvider;
 
-public interface Controller {
-    public void setBreakpoint(String address, boolean set);
-    public void toggleBreakpoint(String address);
-    public void updateBreakpoint(long address, NativeBreakpoint bpt, boolean set);
-    public void enableBreakpoint(long address, NativeBreakpoint bpt, boolean enable);
-    public void addStateListenerInst(StateListener sl);
-    public void removeStateListenerInst(StateListener sl);
+/**
+ *
+ * @author Egor Ushakov
+ */
+@ServiceProvider(service=StateListener.class)
+public class GoToSourceAction extends CallableSystemAction implements StateListener {
 
-    // ask for disassembly for current visiting location
-    public void requestDis(boolean withSource);
+    private final String menu_name;
 
-    // ask for disassembly for the specified range (raw ... no srclines)
-    public void requestDis(String start, int count, boolean withSource);
+    public GoToSourceAction() {
+	menu_name=Catalog.get("Dis_ACT_Go_To_Source"); // NOI18N
+    }
 
+    // interface SystemAction
+    public String getName() {
+	return menu_name;
+    }
+
+    // interface SystemAction
+    @Override
+    protected void initialize() {
+        super.initialize();
+        setEnabled(false);
+    }
+
+    // interface StateListener
+    public void update(State state) {
+        boolean enable;
+        if (!state.isLoaded) {
+            enable = false;
+        } else {
+            enable = state.isListening();
+        }
+
+        setEnabled(enable);
+    }
+    
+    // interface CallableSystemAction
+    public void performAction() {
+        NativeDebugger debugger = DebuggerManager.get().currentDebugger();
+	if (debugger instanceof NativeDebuggerImpl) {
+            ((NativeDebuggerImpl)debugger).showCurrentSource();
+        }
+    }
+
+    // interface CallableSystemAction
+    @Override
+    public boolean asynchronous() {
+	return false;
+    }
+
+    // interface SystemAction
+    public HelpCtx getHelpCtx() {
+        return null;
+    }
+
+    // interface SystemAction
+    @Override
+    protected String iconResource() {
+        return null;
+    }
 }
