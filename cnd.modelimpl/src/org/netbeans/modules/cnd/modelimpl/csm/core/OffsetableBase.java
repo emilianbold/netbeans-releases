3ec212d323ca6581edf3f0963962074a50b45d4a
Nikolay Krasilnikov <nnnnnk@netbeans.org>
2011-05-18 13:50:38 +0400

    added isDebugMode before assertTrueInConsole (transplanted from 641ba5a0ee03482037517175d56699b100c03f92)

--- cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/csm/core/OffsetableBase.java
+++ cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/csm/core/OffsetableBase.java
@@ -80,7 +80,9 @@ public class OffsetableBase implements CsmOffsetable, Disposable {
         // Parameters.notNull("file can not be null", file); // NOI18N
         this.fileUID = UIDCsmConverter.fileToUID(file);
         this.fileRef = null;// to prevent error with "final"
-        CndUtils.assertTrueInConsole(start <= end, "end < start for " + file.getAbsolutePath() + ":[" + start + "-" + end + "]"); // NOI18N
+        if(CndUtils.isDebugMode()) {
+            CndUtils.assertTrueInConsole(start <= end, "end < start for " + file.getAbsolutePath() + ":[" + start + "-" + end + "]"); // NOI18N
+        }
         if(end < start) {
             end = start;
         }
