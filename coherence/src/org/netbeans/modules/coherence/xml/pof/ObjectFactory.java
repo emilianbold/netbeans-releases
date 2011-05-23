5c0a2c3fe314f9707be102b1b2128c903308d5fc
David Konecny <dkonecny@netbeans.org>
2011-05-24 11:30:43 +1200

    automatic conversion of end line character (CRLF to LF) failed on these files so I'm fixing them individually

--- coherence/src/org/netbeans/modules/coherence/xml/pof/ObjectFactory.java
+++ coherence/src/org/netbeans/modules/coherence/xml/pof/ObjectFactory.java
@@ -41,191 +41,95 @@
  */
 package org.netbeans.modules.coherence.xml.pof;
 
-
-
 import javax.xml.bind.annotation.XmlRegistry;
 
-
-
-
-
 /**
-
  * This object contains factory methods for each 
-
  * Java content interface and Java element interface 
-
  * generated in the org.netbeans.modules.coherence.xml.pof package. 
-
  * <p>An ObjectFactory allows you to programatically 
-
  * construct new instances of the Java representation 
-
  * for XML content. The Java representation of XML 
-
  * content can consist of schema derived interfaces 
-
  * and classes representing the binding of schema 
-
  * type definitions, element declarations and model 
-
  * groups.  Factory methods for each of these are 
-
  * provided in this class.
-
  * 
-
  */
-
 @XmlRegistry
-
 public class ObjectFactory {
 
 
-
-
-
     /**
-
      * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: org.netbeans.modules.coherence.xml.pof
-
      * 
-
      */
-
     public ObjectFactory() {
-
     }
 
-
-
     /**
-
      * Create an instance of {@link UserType }
-
      * 
-
      */
-
     public UserType createUserType() {
-
         return new UserType();
-
     }
 
-
-
     /**
-
      * Create an instance of {@link Serializer }
-
      * 
-
      */
-
     public Serializer createSerializer() {
-
         return new Serializer();
-
     }
 
-
-
     /**
-
      * Create an instance of {@link InitParams }
-
      * 
-
      */
-
     public InitParams createInitParams() {
-
         return new InitParams();
-
     }
 
-
-
     /**
-
      * Create an instance of {@link InitParam }
-
      * 
-
      */
-
     public InitParam createInitParam() {
-
         return new InitParam();
-
     }
 
-
-
     /**
-
      * Create an instance of {@link PofConfig }
-
      * 
-
      */
-
     public PofConfig createPofConfig() {
-
         return new PofConfig();
-
     }
 
-
-
     /**
-
      * Create an instance of {@link UserTypeList }
-
      * 
-
      */
-
     public UserTypeList createUserTypeList() {
-
         return new UserTypeList();
-
     }
 
-
-
     /**
-
      * Create an instance of {@link DefaultSerializer }
-
      * 
-
      */
-
     public DefaultSerializer createDefaultSerializer() {
-
         return new DefaultSerializer();
-
     }
-
-
 
     /**
-
      * Create an instance of {@link Include }
-
      * 
-
      */
-
     public Include createInclude() {
-
         return new Include();
-
     }
-
-
 
 }
-
