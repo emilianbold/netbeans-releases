19736678b4f8153a39f9eae3bb87175cf0eeddf7
Maria Tishkova <mromashova@netbeans.org>
2011-05-26 18:08:33 +0400

    noi18n (transplanted from 295836299655c6de6cceaff1edf026055b9133c9)

--- dlight.core.stack/src/org/netbeans/modules/dlight/core/stack/storage/impl/SQLStackDataStorage.java
+++ dlight.core.stack/src/org/netbeans/modules/dlight/core/stack/storage/impl/SQLStackDataStorage.java
@@ -847,7 +847,7 @@ public class SQLStackDataStorage implements ProxyDataStorage, StackDataStorage2,
                             if (moduleOffset != null) {
                                 qname.append('+').append(moduleOffset);
                             }else{
-                                qname.append('+').append("0x0");
+                                qname.append('+').append("0x0");//NOI18N
                             }
                             qname.append('`');
                         }
@@ -857,7 +857,7 @@ public class SQLStackDataStorage implements ProxyDataStorage, StackDataStorage2,
                         if (offset > 0) {
                             qname.append("+0x").append(Long.toHexString(offset)); // NOI18N
                         }else{
-                            qname.append('+').append("0x0");
+                            qname.append('+').append("0x0");//NOI18N
                         }
 
                         if (srcFile != null) {
