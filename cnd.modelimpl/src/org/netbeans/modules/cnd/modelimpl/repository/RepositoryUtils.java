fb2eb1c758e7cf6985221bb0f3dc2b7d7d116ac1
Vladimir Kvashin <vkvashin@netbeans.org>
2011-05-31 16:10:25 +0400

    fixed #198735 projects with remote development host are always reparsed on connection to host

--- cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/repository/RepositoryUtils.java
+++ cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/repository/RepositoryUtils.java
@@ -88,7 +88,7 @@ public final class RepositoryUtils {
     /**
      * the version of the persistency mechanism
      */
-    private static int CURRENT_VERSION_OF_PERSISTENCY = 110;
+    private static int CURRENT_VERSION_OF_PERSISTENCY = 111;
 
     /** Creates a new instance of RepositoryUtils */
     private RepositoryUtils() {
