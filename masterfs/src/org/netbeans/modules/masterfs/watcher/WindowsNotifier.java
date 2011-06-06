28e9971c9822054c9bc6cc361301c1ea2a032bd3
Jaroslav Tulach <jtulach@netbeans.org>
2011-06-06 15:06:18 +0200

    #199186: Convert slashes to backslashes

--- masterfs/src/org/netbeans/modules/masterfs/watcher/WindowsNotifier.java
+++ masterfs/src/org/netbeans/modules/masterfs/watcher/WindowsNotifier.java
@@ -295,7 +295,7 @@ public class WindowsNotifier extends Notifier<Void> {
 
         if (path.length() < 3 ) throw new IOException("wrong path: " + path);
 
-        String root = path.substring(0, 3);
+        String root = path.substring(0, 3).replace('/', '\\');
         if (root.charAt(1) != ':' || root.charAt(2) != '\\') {
             throw new IOException("wrong path: " + path);
         }
