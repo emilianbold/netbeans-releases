b8e701fb5729824fd447a7053103dc95f8f3d05e
Tomas Stupka <tstupka@netbeans.org>
2011-06-08 18:23:34 +0200

    fix priority regression introduced in #148b26148960

--- versioning/src/org/netbeans/modules/versioning/VersioningManager.java
+++ versioning/src/org/netbeans/modules/versioning/VersioningManager.java
@@ -200,6 +200,7 @@ public class VersioningManager implements PropertyChangeListener, LookupListener
     
     private void init() {
         try {
+            prioritiesSet = anyPrioritySet();
             systemsLookupResult.addLookupListener(this);
             refreshVersioningSystems();
             filesystemInterceptor.init(this);
@@ -233,6 +234,7 @@ public class VersioningManager implements PropertyChangeListener, LookupListener
 
             // inline loadVersioningSystems(systems);
             versioningSystems.addAll(systems);
+            if(prioritiesSet) sortVersioningSystems(); 
             for (VersioningSystem system : versioningSystems) {
                 if (localHistory == null && Utils.isLocalHistory(system)) {
                     localHistory = system;
@@ -245,6 +247,13 @@ public class VersioningManager implements PropertyChangeListener, LookupListener
         flushFileOwnerCache();
         refreshDiffSidebars(null);
         VersioningAnnotationProvider.refreshAllAnnotations();
+    }
+
+    void sortVersioningSystems() {
+        synchronized(versioningSystems) {
+            Collections.sort(versioningSystems, new ByPriorityComparator());
+            Collections.reverse(versioningSystems); // systems with higher priority should be at the end of the list, see getOwner logic
+        }
     }
 
     InterceptionListener getInterceptionListener() {
@@ -580,5 +589,30 @@ public class VersioningManager implements PropertyChangeListener, LookupListener
         } finally {
             LOG.log(Level.FINE, "needsLocalHistory method [{0}] returns {1}", new Object[] {methodName, ret});
         }
+    }
+
+    /**
+     * Sorts versioning systems according to their priority. Systems with lower value (higher priority) will be stated before those with higher value (lower priority) in the given list.
+     */
+    private static final class ByPriorityComparator implements Comparator<VersioningSystem> {
+        @Override
+        public int compare(VersioningSystem s1, VersioningSystem s2) {
+            return Utils.getPriority(s1).compareTo(Utils.getPriority(s2));
+        }
+    }    
+    
+    private boolean prioritiesSet = false;
+    private boolean anyPrioritySet() {
+        Set<Object> keys = System.getProperties().keySet();
+        for (Object k : keys) {
+            if(k instanceof String) {
+                if(k.toString().startsWith("versioning.") && 
+                   k.toString().endsWith(".priority")) 
+                {
+                    return true;
+                }      
+            }
+        }
+        return false;
     }
 }
