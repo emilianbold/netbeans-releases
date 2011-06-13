b2901d5879ae9a143b5f4b6679c62f79e5a27177
Petr Pisl <ppisl@netbeans.org>
2011-06-13 17:16:42 +0200

    #199298 - AssertionError: The same token (index: 3938 - WHITESPACE, format tokens: 6961) was precessed before. Please report this to help fix issue 188809. (transplanted from bc6b007d39f3576c42fdb74b5360ecd86be9e16f)

--- php.editor/test/unit/src/org/netbeans/modules/php/editor/indent/PHPFormatterTest.java
+++ php.editor/test/unit/src/org/netbeans/modules/php/editor/indent/PHPFormatterTest.java
@@ -2254,6 +2254,10 @@ public class PHPFormatterTest extends PHPTestBase {
         reformatFileContents("testfiles/formatting/issue197304_01.php", options);
     }
 
+    public void testIssue199298_01() throws Exception {
+	HashMap<String, Object> options = new HashMap<String, Object>(FmtOptions.getDefaults());
+        reformatFileContents("testfiles/formatting/issue199298_01.php", options);
+    }
     
     private void reformatFileContents(String file) throws Exception {
         reformatFileContents(file, new IndentPrefs(2, 2));
