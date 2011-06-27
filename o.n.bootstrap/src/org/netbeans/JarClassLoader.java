cf6bd5cea107c108dc97ee7b72ee7e5cc87779b5
Jaroslav Tulach <jtulach@netbeans.org>
2011-06-27 09:41:07 +0200

    #192465: Dump more info about content of disk

--- o.n.bootstrap/src/org/netbeans/JarClassLoader.java
+++ o.n.bootstrap/src/org/netbeans/JarClassLoader.java
@@ -478,6 +478,24 @@ public class JarClassLoader extends ProxyClassLoader {
                                             opened(JarClassLoader.JarSource.this, "ziperror");
                                             continue;
                                         }
+                                        File f = file;
+                                        for (;;) {
+                                            if (f == null) {
+                                                LOGGER.log(Level.INFO, "file {0} is null. # of retries {1}", new Object[] {f, retry}); // NOI18N
+                                                break;
+                                            }
+                                            if (f.exists()) {
+                                                LOGGER.log(Level.WARNING, "file {0} exists. # of retries {1}", new Object[] {f, retry}); // NOI18N
+                                                if (f.isDirectory()) {
+                                                    LOGGER.log(Level.INFO, "{0} is directory and contains: {1}", new Object[]{f, Arrays.toString(f.list())}); // NOI18N
+                                                } else {
+                                                    LOGGER.log(Level.INFO, "{0} isDirectory: {1}, isFile: {2}", new Object[]{f, f.isDirectory(), f.isFile()}); // NOI18N
+                                                }
+                                                break;
+                                            }
+                                            LOGGER.log(Level.WARNING, "{0} does not exist, # of retries {1}", new Object[] { f, retry }); // NOI18N
+                                            f = f.getParentFile();
+                                        }
                                         throw zip;
                                     }
                                 }
