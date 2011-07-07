f76c54b5cf2c5df7507b5f9af373d6377f92bdeb
Jiri Sedlacek <jis@netbeans.org>
2011-07-07 13:32:12 +0200

    Fixed missing support for profiling NetBeans module suites

--- profiler.nbimpl/src/org/netbeans/modules/profiler/nbimpl/project/JavaProjectContentsSupportProvider.java
+++ profiler.nbimpl/src/org/netbeans/modules/profiler/nbimpl/project/JavaProjectContentsSupportProvider.java
@@ -61,6 +61,7 @@ import org.openide.filesystems.FileObject;
                             @ProjectType(id="org-netbeans-modules-java-j2seproject"),
                             @ProjectType(id="org-netbeans-modules-ant-freeform", position=1203),
                             @ProjectType(id="org-netbeans-modules-apisupport-project"),
+                            @ProjectType(id="org-netbeans-modules-apisupport-project-suite"),
                             @ProjectType(id="org-netbeans-modules-j2ee-ejbjarproject"),
                             @ProjectType(id="org-netbeans-modules-web-project"),
                             @ProjectType(id="org-netbeans-modules-maven")},
