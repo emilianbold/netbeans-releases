eb7b3af6d67f932a978e4e3b4d66aacdf5d9546d
Tomas Zezula <tzezula@netbeans.org>
2011-07-19 16:12:36 +0200

    Clean up and javadoc.

--- java.source/src/org/netbeans/api/java/source/SourceUtils.java
+++ java.source/src/org/netbeans/api/java/source/SourceUtils.java
@@ -247,7 +247,19 @@ public class SourceUtils {
 	return (TypeElement)ec;
     }
 
-    public static String[] getJVMSignature(final ElementHandle<?> handle) {
+    /**
+     * Returns an array containing the JVM signature of the {@link ElementHandle}.
+     * @param handle to obtain the JVM signature for.
+     * @return an array containing the JVM signature. The signature depends on
+     * the {@link ElementHandle}'s {@link ElementKind}. For class or package
+     * it returns a single element array containing the class (package) binary
+     * name (JLS section 13.1). For field (method) it returns three element array
+     * containing owner class binary name (JLS section 13.1) in the first element,
+     * field (method) name in the second element and JVM type (JVM method formal
+     * parameters (JVMS section 2.10.1)) in the third element.
+     */
+    @NonNull
+    public static String[] getJVMSignature(@NonNull final ElementHandle<?> handle) {
         return ElementHandleAccessor.INSTANCE.getVMSignature(handle);
     }
     
