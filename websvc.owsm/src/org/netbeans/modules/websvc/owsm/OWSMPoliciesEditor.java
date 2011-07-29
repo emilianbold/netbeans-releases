6ff51605404b22e811cfba9a3004ec27f1d428e0
Denis Anisimov <ads@netbeans.org>
2011-07-29 19:24:23 +0400

    Changes in two areas: - OWSM Policies support for Web Services for WL ( completed ) - Basic UI for WS Client wizard ( creation from WS Saas )

--- websvc.owsm/src/org/netbeans/modules/websvc/owsm/OWSMPoliciesEditor.java
+++ websvc.owsm/src/org/netbeans/modules/websvc/owsm/OWSMPoliciesEditor.java
@@ -44,22 +44,47 @@ package org.netbeans.modules.websvc.owsm;
 
 import java.awt.FlowLayout;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import javax.lang.model.element.Element;
+import javax.lang.model.element.Name;
+import javax.lang.model.element.TypeElement;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
+import org.netbeans.api.java.source.JavaSource;
+import org.netbeans.api.java.source.Task;
+import org.netbeans.api.java.source.TreeMaker;
+import org.netbeans.api.java.source.TreeUtilities;
+import org.netbeans.api.java.source.WorkingCopy;
 import org.netbeans.modules.javaee.specs.support.api.JaxWsPoliciesSupport;
 import org.netbeans.modules.websvc.api.wseditor.InvalidDataException;
 import org.netbeans.modules.websvc.api.wseditor.WSEditor;
+import org.openide.DialogDisplayer;
+import org.openide.NotifyDescriptor;
 import org.openide.filesystems.FileObject;
 import org.openide.nodes.Node;
 import org.openide.util.Lookup;
 import org.openide.util.NbBundle;
+
+import com.sun.source.tree.AnnotationTree;
+import com.sun.source.tree.ClassTree;
+import com.sun.source.tree.CompilationUnitTree;
+import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.ModifiersTree;
+import com.sun.source.tree.NewArrayTree;
+import com.sun.source.tree.Tree;
+import java.io.IOException;
+import org.netbeans.api.project.FileOwnerQuery;
+import org.netbeans.api.project.Project;
 
 
 /**
@@ -90,7 +115,8 @@ class OWSMPoliciesEditor implements WSEditor {
             return component;
         }
         List<String> securityPolicies = filterSecurityPolicies( );
-        return new PoliciesVisualPanel( securityPolicies );
+        myPanel = new PoliciesVisualPanel( securityPolicies , myFileObject );
+        return myPanel;
     }
 
     /* (non-Javadoc)
@@ -106,17 +132,28 @@ class OWSMPoliciesEditor implements WSEditor {
      */
     @Override
     public void save( Node node ) {
-        // TODO Auto-generated method stub
-
+        if ( node ==null ){
+            return;
+        }
+        if ( SwingUtilities.isEventDispatchThread() ){
+            PoliciesVisualPanel.JAVA_PROCESSOR.post( new Runnable() {
+                
+                @Override
+                public void run() {
+                    doSave();
+                }
+            });
+        }
+        else {
+            doSave();
+        }
     }
-
+    
     /* (non-Javadoc)
      * @see org.netbeans.modules.websvc.api.wseditor.WSEditor#cancel(org.openide.nodes.Node)
      */
     @Override
     public void cancel( Node node ) {
-        // TODO Auto-generated method stub
-
     }
 
     /* (non-Javadoc)
@@ -125,6 +162,135 @@ class OWSMPoliciesEditor implements WSEditor {
     @Override
     public String getDescription() {
         return NbBundle.getMessage( OWSMPoliciesEditor.class, "TXT_PanelDescription");  // NOI18N
+    }
+    
+    private void doSave(){
+        if ( myPanel == null ){
+            return;
+        }
+        if ( myPanel.getWsFqn() == null ){
+            return;
+        }
+        JavaSource javaSource = JavaSource.forFileObject(myFileObject );
+        if ( javaSource == null ) {
+            NotifyDescriptor descriptor = new NotifyDescriptor.Message( 
+                    NbBundle.getMessage(OWSMPoliciesEditor.class, "ERR_NoJava"),    // NOI18N
+                    NotifyDescriptor.ERROR_MESSAGE);
+            DialogDisplayer.getDefault().notify( descriptor );
+            return;
+        }
+        final List<String> policyIds = myPanel.getPolicyIds();
+        List<String> fqns = new ArrayList<String>(2);
+        fqns.add( PoliciesVisualPanel.OWSM_SECURITY_POLICY);
+        if ( policyIds.size() >1 ){
+            Project project = FileOwnerQuery.getOwner(myFileObject);
+            fqns.add(PoliciesVisualPanel.OWSM_SECURITY_POLICIES);
+            mySupport.extendsProjectClasspath(project, fqns);
+        }
+        try {
+            javaSource.runModificationTask(new Task<WorkingCopy>() {
+
+                @Override
+                public void run( WorkingCopy workingCopy ) throws Exception {
+                    workingCopy.toPhase(JavaSource.Phase.ELEMENTS_RESOLVED);
+                    String wsFqn = myPanel.getWsFqn();
+                    
+                    CompilationUnitTree cu = workingCopy.getCompilationUnit();
+                    ClassTree wsClassTree = null;
+                    if (cu != null) {
+                        List<? extends Tree> decls = cu.getTypeDecls();
+                        for (Tree decl : decls) {
+                            if (!TreeUtilities.CLASS_TREE_KINDS.contains(decl
+                                    .getKind()))
+                            {
+                                continue;
+                            }
+
+                            ClassTree classTree = (ClassTree) decl;
+                            Element element = workingCopy.getTrees()
+                                    .getElement(
+                                            workingCopy.getTrees().getPath(cu,
+                                                    classTree));
+                            if (element instanceof TypeElement) {
+                                Name className = ((TypeElement) element)
+                                        .getQualifiedName();
+                                if (className.contentEquals(wsFqn)) {
+                                    wsClassTree = classTree;
+                                }
+                            }
+                        }
+                    }
+                    if (wsClassTree == null) {
+                        return;
+                    }
+                    rewriteAnnotations(policyIds, workingCopy, wsClassTree);
+                }
+
+            }).commit();
+        }
+        catch (IOException e) {
+            Logger.getLogger( OWSMPoliciesEditor.class.getName() ).log( 
+                    Level.INFO, null, e );
+        }
+    }
+    
+    private void rewriteAnnotations( final List<String> policyIds,
+            WorkingCopy workingCopy, ClassTree wsClassTree )
+    {
+        TreeMaker maker = workingCopy.getTreeMaker();
+
+        ModifiersTree modifiers = wsClassTree.getModifiers();
+        List<? extends AnnotationTree> annotations = modifiers.getAnnotations();
+        List<AnnotationTree> clearedTrees = new ArrayList<AnnotationTree>( 
+                annotations.size() );
+        for (AnnotationTree annotationTree : annotations) {
+            Tree annotationType = annotationTree.getAnnotationType();
+            Element element = workingCopy.getTrees().getElement( workingCopy.getTrees().getPath(
+                    workingCopy.getCompilationUnit() , annotationType ));
+            if ( element.equals( workingCopy.getElements().getTypeElement( 
+                    PoliciesVisualPanel.OWSM_SECURITY_POLICIES)) || element.
+                        equals( workingCopy.getElements().getTypeElement( 
+                            PoliciesVisualPanel.OWSM_SECURITY_POLICY)))
+            {
+                continue;
+            }
+            clearedTrees.add( annotationTree );
+        }
+        
+        modifiers = maker.Modifiers( modifiers, clearedTrees );
+
+        AnnotationTree newAnnotation = null;
+        if (policyIds.size() > 1) {
+            List<ExpressionTree> idList = new ArrayList<ExpressionTree>( policyIds.size() );
+            for (String id : policyIds) {
+                AnnotationTree annotationTree = createPolicyAnnotation(
+                        maker, id);
+                idList.add(annotationTree);
+            }
+            NewArrayTree newArray = maker.NewArray( workingCopy.getTrees().getTree(
+                    workingCopy.getElements().getTypeElement( PoliciesVisualPanel.OWSM_SECURITY_POLICY)), 
+                    Collections.<ExpressionTree>emptyList(), idList );
+            newAnnotation = maker.Annotation(
+                    maker.QualIdent(PoliciesVisualPanel.OWSM_SECURITY_POLICIES),
+                    Collections.singletonList( maker.Assignment( 
+                            maker.Identifier(PoliciesVisualPanel.VALUE), newArray )));
+        }
+        else if ( policyIds.size() == 1){
+            newAnnotation = createPolicyAnnotation(maker, policyIds.get(0));
+        }
+
+        if (newAnnotation != null) {
+            modifiers = maker.addModifiersAnnotation(modifiers, newAnnotation);
+        }
+        workingCopy.rewrite(wsClassTree.getModifiers(), modifiers);
+    }
+    
+    private AnnotationTree createPolicyAnnotation( TreeMaker maker , String id) {
+        ExpressionTree idTree = maker.Assignment(maker.Identifier(
+                PoliciesVisualPanel.URI), maker.Literal(id));
+        return maker.Annotation(
+                maker.QualIdent(PoliciesVisualPanel.OWSM_SECURITY_POLICY), 
+                Collections.singletonList( idTree ) );
     }
     
     private List<String> filterSecurityPolicies() {
@@ -142,6 +308,7 @@ class OWSMPoliciesEditor implements WSEditor {
     private static Set<String> SECURITY_POLICIES = new LinkedHashSet<String>();
     private JaxWsPoliciesSupport mySupport;
     private FileObject myFileObject;
+    private PoliciesVisualPanel myPanel;
 
     static {
         SECURITY_POLICIES.add("binding_authorization_denyall_policy");      // NOI18N
