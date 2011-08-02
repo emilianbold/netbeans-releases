22a4d4311c6224c572b142becda40e4e4cf88647
Tomas Hurka <thurka@netbeans.org>
2011-08-02 14:11:41 +0200

    temporary disable CPU sampling UI

--- profiler.stp/src/org/netbeans/modules/profiler/stp/CPUSettingsBasicPanel.java
+++ profiler.stp/src/org/netbeans/modules/profiler/stp/CPUSettingsBasicPanel.java
@@ -215,9 +215,10 @@ public class CPUSettingsBasicPanel extends DefaultSettingsPanel implements Actio
     }
 
     public void setProfilingType(int profilingType) {
-        sampleAppRadio.setSelected(profilingType == ProfilingSettings.PROFILE_CPU_SAMPLING);
-        profileAppRadio.setSelected(profilingType == ProfilingSettings.PROFILE_CPU_ENTIRE ||
-                                       profilingType == ProfilingSettings.PROFILE_CPU_PART);
+//        sampleAppRadio.setSelected(profilingType == ProfilingSettings.PROFILE_CPU_SAMPLING);
+//        profileAppRadio.setSelected(profilingType == ProfilingSettings.PROFILE_CPU_ENTIRE ||
+//                                       profilingType == ProfilingSettings.PROFILE_CPU_PART);
+        profileAppRadio.setSelected(true);  // FIXXX - remove once sampling is implemented
         stopwatchRadio.setSelected(profilingType == ProfilingSettings.PROFILE_CPU_STOPWATCH);
     }
 
@@ -321,10 +322,11 @@ public class CPUSettingsBasicPanel extends DefaultSettingsPanel implements Actio
 
         // entireAppRadio
         sampleAppRadio = new JRadioButton();
+        sampleAppRadio.setEnabled(false); // FIXXX - remove once sampling is implemented
         org.openide.awt.Mnemonics.setLocalizedText(sampleAppRadio, SAMPLE_APP_RADIO_TEXT);
         sampleAppRadio.setToolTipText(STP_SAMPLEAPP_TOOLTIP);
         sampleAppRadio.setOpaque(false);
-        sampleAppRadio.setSelected(true);
+//        sampleAppRadio.setSelected(true); // FIXXX - uncomment once sampling is implemented
         cpuModeRadios.add(sampleAppRadio);
         sampleAppRadio.addChangeListener(new ChangeListener() {
                 public void stateChanged(ChangeEvent e) {
