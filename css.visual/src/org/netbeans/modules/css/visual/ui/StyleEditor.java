5a7a9f1b96be561c61d6bf517e4b4d5696b8b70f
Marek Fukala <mfukala@netbeans.org>
2011-08-03 14:42:14 +0200

    #200599 - Stylebuilder doesn't generate does generate only one property for "all"-margin/padding/border

--- css.visual/src/org/netbeans/modules/css/visual/ui/StyleEditor.java
+++ css.visual/src/org/netbeans/modules/css/visual/ui/StyleEditor.java
@@ -61,14 +61,12 @@ import org.netbeans.modules.css.visual.CssRuleContent;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
-import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.Logger;
 import javax.swing.Icon;
 import javax.swing.JPanel;
 import org.netbeans.modules.css.visual.api.CssRuleContext;
 import org.openide.util.Exceptions;
-import org.openide.util.RequestProcessor;
 
 /**
  * Super class for all Style editors
@@ -148,13 +146,13 @@ abstract public class StyleEditor extends JPanel {
 
     protected void startAggregatedEventsSession() {
         if(!IN_PROPERTY_VALUES_INITIALIZATION.get()) {
-//            CssEditorSupport.getDefault().firstAggregatedEventWillFire();
+            CssEditorSupport.getDefault().firstAggregatedEventWillFire();
         }
     }
 
     protected void closeAggregatedEventsSession() {
         if(!IN_PROPERTY_VALUES_INITIALIZATION.get()) {
-//            CssEditorSupport.getDefault().lastAggregatedEventFired();
+            CssEditorSupport.getDefault().lastAggregatedEventFired();
         }
     }
     
