5b0808c3c8dd372e8d1c18380a1daa223b94fd82
Jan Lahoda <jlahoda@netbeans.org>
2011-08-25 11:21:09 +0200

    #196508: ReadWriteUtils.convertFromNewlines takes start/end, not start/length.

--- editor.lib/src/org/netbeans/editor/BaseDocument.java
+++ editor.lib/src/org/netbeans/editor/BaseDocument.java
@@ -1532,7 +1532,7 @@ public class BaseDocument extends AbstractDocument implements AtomicLockDocument
             }
             CharSequence docText = (CharSequence) getProperty(CharSequence.class);
             // Skip extra '\n' (added by AbstractDocument convention) at the end of char sequence
-            ReadWriteBuffer buffer = ReadWriteUtils.convertFromNewlines(docText, pos, len, lineSeparator);
+            ReadWriteBuffer buffer = ReadWriteUtils.convertFromNewlines(docText, pos, pos + len, lineSeparator);
             ReadWriteUtils.write(writer, buffer);
             writer.flush();
         } finally {
