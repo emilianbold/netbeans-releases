c77c5a7f56af321ffc7f674d31e543f98d8b47df
Ondrej Brejla <obrejla@netbeans.org>
2011-08-31 17:02:30 +0200

    Removing whitespaces

--- php.editor/src/org/netbeans/modules/php/editor/index/Signature.java
+++ php.editor/src/org/netbeans/modules/php/editor/index/Signature.java
@@ -23,7 +23,7 @@
  * License Header, with the fields enclosed by brackets [] replaced by
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- * 
+ *
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -34,9 +34,9 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
- * 
+ *
  * Contributor(s):
- * 
+ *
  * Portions Copyrighted 2008 Sun Microsystems, Inc.
  */
 
@@ -50,7 +50,7 @@ import org.netbeans.api.annotations.common.NonNull;
  * @author Marek Fukala
  */
 public class Signature {
-        //shared array for better performance, 
+        //shared array for better performance,
         //access is supposed from one thread so perf
         //shouldn't degrade due to synchronization
         private static int[] SHARED;
@@ -64,7 +64,7 @@ public class Signature {
         public static Signature get(String signature) {
             return new Signature(signature);
         }
-        
+
         private Signature(String signature) {
             this.signature = signature;
             this.positions = parseSignature(signature);
@@ -76,16 +76,16 @@ public class Signature {
 
             return signature.substring(positions[index], index == positions.length - 1 ? signature.length() : positions[index + 1] - 1);
         }
-        
+
         public int integer(int index) {
             String item = string(index);
             if(item != null && item.length() > 0) {
                 return Integer.parseInt(item);
             } else {
                 return -1;
-            }                    
+            }
         }
-        
+
         private static int[] parseSignature(String signature) {
             synchronized(SHARED) {
                 int count = 0;
@@ -96,10 +96,10 @@ public class Signature {
                 }
                 count++; //include the first zero
                 int[] a = new int[count];
-                System.arraycopy(SHARED, 0, a, 0, count); 
+                System.arraycopy(SHARED, 0, a, 0, count);
                 return a;
             }
         }
-        
+
     }
 
