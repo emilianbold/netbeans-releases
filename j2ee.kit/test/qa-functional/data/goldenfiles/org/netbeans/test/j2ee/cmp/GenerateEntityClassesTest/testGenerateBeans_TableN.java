dadbac64b8f9692cb4f215289bfb009cb85d52b8
Jiri Skrivanek <jskrivanek@netbeans.org>
2011-09-09 12:14:55 +0200

    Golden files fixed after change of import statements formatting.

--- j2ee.kit/test/qa-functional/data/goldenfiles/org/netbeans/test/j2ee/cmp/GenerateEntityClassesTest/testGenerateBeans_TableN.java
+++ j2ee.kit/test/qa-functional/data/goldenfiles/org/netbeans/test/j2ee/cmp/GenerateEntityClassesTest/testGenerateBeans_TableN.java
@@ -7,16 +7,7 @@ package test;
 import java.io.Serializable;
 import java.math.BigInteger;
 import java.util.Date;
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Lob;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.*;
 
 /**
  *
