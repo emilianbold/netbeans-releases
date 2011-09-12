760f8f2d39444fc17722288b94266d49ac7eda3f
Adam Senk <asenk@netbeans.org>
2011-09-12 16:01:48 +0200

    QA Functional test update

--- form/test/qa-functional/src/org/netbeans/qa/form/visualDevelopment/AddComponents_AWT.java
+++ form/test/qa-functional/src/org/netbeans/qa/form/visualDevelopment/AddComponents_AWT.java
@@ -54,9 +54,9 @@ import org.netbeans.jellytools.actions.*;
 import org.netbeans.junit.ide.ProjectSupport;
 import org.netbeans.qa.form.*;
 import java.io.*;
+import java.util.ArrayList;
 import junit.framework.Test;
 import org.netbeans.junit.NbModuleSuite;
-
 
 /**
  *<P>
@@ -85,41 +85,40 @@ import org.netbeans.junit.NbModuleSuite;
  * 20 April 2011 WORKS
  */
 public class AddComponents_AWT extends ExtJellyTestCase {
-    
+
     public String FILE_NAME = "clear_Frame";
     public String PACKAGE_NAME = "data";
     public String DATA_PROJECT_NAME = "SampleProject";
     public String FRAME_ROOT = "[Frame]";
-    
     public MainWindowOperator mainWindow;
     public ProjectsTabOperator pto;
     public Node formnode;
-    
+
     public AddComponents_AWT(String testName) {
         super(testName);
     }
-    
-    
-     @Override
+
+    @Override
     public void setUp() throws IOException {
         openDataProjects(_testProjectName);
     }
-    
+
     public static Test suite() {
         return NbModuleSuite.create(NbModuleSuite.createConfiguration(AddComponents_AWT.class).addTest(
                 "testAddAndCompile",
-                "testJavaFile",
-                "testFormFile"
+                "testJavaFile"//,
+                //"testFormFile"
                 ).clusters(".*").enableModules(".*").gui(true));
 
     }
+
     /** Run test.
      */
     public void testAddAndCompile() {
         String categoryName = "AWT";
-        
+
         System.out.println(">>>" + this.getWorkDirPath() + "<<<");
-        
+
         mainWindow = MainWindowOperator.getDefault();
         pto = new ProjectsTabOperator();
         sleep(300);
@@ -129,17 +128,17 @@ public class AddComponents_AWT extends ExtJellyTestCase {
         formnode = new Node(prn, "Source Packages|" + PACKAGE_NAME + "|" + FILE_NAME);
         formnode.select();
         log("Form node selected.");
-        
+
         EditAction editAction = new EditAction();
         editAction.perform(formnode);
         log("Source Editor window opened.");
-        
+
         OpenAction openAction = new OpenAction();
         openAction.perform(formnode);
         log("Form Editor window opened.");
-        
+
         // store all component names from the category in the Vector
-        Vector componentNames = new Vector();
+
         ComponentPaletteOperator palette = new ComponentPaletteOperator();
         palette.collapseBeans();
         //        palette.collapseLayouts();
@@ -148,24 +147,35 @@ public class AddComponents_AWT extends ExtJellyTestCase {
         palette.collapseSwingMenus();
         palette.collapseSwingWindows();
         palette.expandAWT();
-        String[] componentList = {"Label", "Button", "Text Field", "Text Area", "Checkbox", "Choice", "List", "Scrollbar", "Scroll Pane", "Panel", "Canvas", "Menu Bar", "Popup Menu"};
-        for (int i=0;i < componentList.length; i++) {
-            log("Adding " + componentList[i]);
-            componentNames.addElement(componentList[i]);
-        }
+
         ComponentInspectorOperator cio = new ComponentInspectorOperator();
-        Node inspectorRootNode = new Node(cio.treeComponents(), FRAME_ROOT);
-        inspectorRootNode.select();
-        inspectorRootNode.expand();
-        // add all beans from Palette Category to form
-        Action popupAddFromPaletteAction;
-        for(int i = 0; i < componentNames.size(); i++){
-            String itemPath = "Add From Palette|AWT|" + componentNames.elementAt(i).toString();
-            log("Running " + itemPath);
-            popupAddFromPaletteAction = new Action(null, itemPath);
-            popupAddFromPaletteAction.perform(inspectorRootNode);
-        }
-        
+        cio.freezeNavigatorAndRun(new Runnable() {
+
+            @Override
+            public void run() {
+                ArrayList componentNames = new ArrayList();
+                String[] componentList = {"Label", "Button", "Text Field", "Text Area", "Checkbox", "Choice", "List", "Scrollbar", "Scroll Pane", "Panel", "Canvas", "Menu Bar", "Popup Menu"};
+                for (int i = 0; i < componentList.length; i++) {
+                    log("Adding " + componentList[i]);
+                    componentNames.add(componentList[i]);
+                }
+
+                ComponentInspectorOperator cio = new ComponentInspectorOperator();
+                Node inspectorRootNode = new Node(cio.treeComponents(), FRAME_ROOT);
+                inspectorRootNode.select();
+                inspectorRootNode.expand();
+                // add all beans from Palette Category to form
+
+                for (int i = 0; i < componentNames.size(); i++) {
+                    String itemPath = "Add From Palette|AWT|" + componentNames.remove(0).toString();
+                    log("Running " + itemPath);
+
+                    runPopupOverNode(itemPath, inspectorRootNode);
+                }
+            }
+        });
+
+
         log("All components from Component Palette : " + categoryName + " - were added to " + FILE_NAME);
         log("Try to save the form.");
         new org.netbeans.jemmy.EventTool().waitNoEvent(1000);
@@ -174,54 +184,89 @@ public class AddComponents_AWT extends ExtJellyTestCase {
         saveAction = new Action("File|Save", null);
         saveAction.perform();
     }
-    
+
     /** Run test.
      */
     public void testFormFile() {
         compareFileByExt("form");
     }
-    
+
     /** Run test.
      */
     public void testJavaFile() {
-        compareFileByExt("java");
+        //compareFileByExt("java");
+
+        mainWindow = MainWindowOperator.getDefault();
+        pto = new ProjectsTabOperator();
+        sleep(300);
+        ProjectRootNode prn = pto.getProjectRootNode(DATA_PROJECT_NAME);
+        sleep(300);
+        prn.select();
+        formnode = new Node(prn, "Source Packages|" + PACKAGE_NAME + "|" + FILE_NAME);
+        formnode.select();
+        log("Form node selected.");
+
+        FormDesignerOperator designer = new FormDesignerOperator(FILE_NAME);
+        OpenAction openAction = new OpenAction();
+        openAction.perform(formnode);
+        ArrayList<String> lines = new ArrayList<String>();
+
+        lines.add("label1 = new java.awt.Label();");
+        lines.add("button1 = new java.awt.Button();");
+        lines.add("textField1 = new java.awt.TextField();");
+        lines.add("textArea1 = new java.awt.TextArea();");
+        lines.add("checkbox1 = new java.awt.Checkbox();");
+        lines.add("choice1 = new java.awt.Choice();");
+        lines.add("list1 = new java.awt.List();");
+
+
+        lines.add("private java.awt.Button button1;");
+        lines.add("private java.awt.Checkbox checkbox1;");
+        lines.add("private java.awt.Choice choice1;");
+        lines.add("private java.awt.Label label1;");
+        lines.add("private java.awt.List list1;");
+        lines.add("private java.awt.TextArea textArea1;");
+        lines.add("private java.awt.TextField textField1;");
+
+        findInCode(lines, designer);
     }
-    
+
     private void compareFileByExt(String fileExt) {
         String refSourceFilePath = getDataDir().getAbsolutePath() + File.separatorChar
-                + DATA_PROJECT_NAME +  File.separatorChar + "src" + File.separatorChar
+                + DATA_PROJECT_NAME + File.separatorChar + "src" + File.separatorChar
                 + PACKAGE_NAME + File.separatorChar + FILE_NAME + "." + fileExt;
         log("refSourceFilePath:" + refSourceFilePath);
-        
+
         try {
-            getRef().print( VisualDevelopmentUtil.readFromFile(refSourceFilePath) );
+            getRef().print(VisualDevelopmentUtil.readFromFile(refSourceFilePath));
         } catch (Exception e) {
             fail("Fail during creating ref file: " + e.getMessage());
         }
-        
-        String javaVersionPrefix = VisualDevelopmentUtil.JAVA_VERSION.substring(0,3);
+
+        String javaVersionPrefix = VisualDevelopmentUtil.JAVA_VERSION.substring(0, 3);
         String passFileName = this.getName() + "_" + javaVersionPrefix + ".pass";
         log("passFileName: " + passFileName);
-        
-        compareReferenceFiles(this.getName()+".ref", passFileName, this.getName()+".diff");
+
+        compareReferenceFiles(this.getName() + ".ref", passFileName, this.getName() + ".diff");
     }
 
     /** Run test.
      */
-    public void testCloseDataProject(){
+    public void testCloseDataProject() {
         closeDataProject();
         //        EditorWindowOperator ewo = new EditorWindowOperator();
         //        ewo.closeDiscard();
     }
-    
-    public void closeDataProject(){
+
+    public void closeDataProject() {
         ProjectSupport.closeProject(DATA_PROJECT_NAME);
         log("SampleProject closed.");
     }
-    
+
     static void sleep(int ms) {
-        try {Thread.sleep(ms);} catch (Exception e) {}
+        try {
+            Thread.sleep(ms);
+        } catch (Exception e) {
+        }
     }
-    
- 
 }
