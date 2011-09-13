3ddf5573a0b400e0901960779f714323a6eb0ddb
Petr Cyhelsky <cyhelsky@netbeans.org>
2011-09-13 08:42:40 +0200

    performance testing -cold startup scripts + netbeans.conf update

--- performance/hudson/coldstart.sh
+++ performance/hudson/coldstart.sh
@@ -1,4 +1,5 @@
 #!bash -x
+#if test ! -e /space/hudsonserver/master 
 env
 #sanitize any orphaned JUnitTestRunners
 jps | grep JUnitTestRunner | cut -d' ' -f1 | xargs kill -9
@@ -11,17 +12,34 @@ project_root=$reposdir/ergonomics
 export project_root=`cygpath -m $project_root`
 netbeans_dest=$reposdir/netbeans
 export netbeans_dest=`cygpath -m $netbeans_dest`
+filename=`ls "$reposdir"/zip`
+echo "filename=$filename"
+pnum=`gawk -v s=${filename} 'BEGIN {print substr(s,24,12)}'`
+echo "pnum=$pnum"
+echo -n ${pnum}>$reposdir/build.number
+
+#update repository
+cd  $project_root
+hg pull -u
+
 # copy netbeans.conf to netbeans dir
-cp -f $performance/hudson/netbeans.conf $netbeans_dest/etc/
+cp -f $project_root/performance/hudson/netbeans.conf $netbeans_dest/etc/
+
 # delete all netbeans userdirs
 rm -rf $HOME/.netbeans
+
 # start netbeans first time
 /home/hudson/scripts/nb_start.bat
+
 # start nb second time + open project
 /home/hudson/scripts/nb_open_pr.bat
+
 # start netbeans third time
 /home/hudson/scripts/nb_start.bat
+
 #start script dealing with post-startup run
 /home/hudson/scripts/preparecold.bat
+
 # restart 
 /home/hudson/scripts/restart.bat
+#fi
