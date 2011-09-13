94375acf1a2fafe57e09b7799adbff97aa5a7a3f
Jaroslav Havlin <jhavlin@netbeans.org>
2011-09-13 11:48:05 +0200

    #197822 - When I set NetBeans as the default editor for files and I double click to open them, the editor will open but the file will not

--- utilities/test/unit/src/org/netbeans/modules/openfile/OpenCLITest.java
+++ utilities/test/unit/src/org/netbeans/modules/openfile/OpenCLITest.java
@@ -45,19 +45,22 @@
 package org.netbeans.modules.openfile;
 
 import java.awt.Component;
+import java.awt.Frame;
+import java.awt.Image;
+import java.beans.PropertyChangeListener;
 import java.io.File;
+import java.util.Set;
+import javax.swing.Action;
 import org.netbeans.api.sendopts.CommandLine;
 import org.netbeans.junit.MockServices;
 import org.netbeans.junit.NbTestCase;
-import org.openide.explorer.ExplorerManager;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
 import org.openide.nodes.Node;
 import org.openide.nodes.NodeAcceptor;
 import org.openide.nodes.NodeOperation;
 import org.openide.util.UserCancelException;
-import org.openide.windows.TopComponent;
-import org.openide.windows.WindowManager;
+import org.openide.windows.*;
 
 /** 
  *
@@ -70,18 +73,22 @@ public class OpenCLITest extends NbTestCase {
         super(testName);
     }
     
+    @Override
     protected boolean runInEQ() {
         return true;
     }
     
+    @Override
     protected void setUp() throws Exception {
         dir = new File(getWorkDir(), "tstdir");
         dir.mkdirs();
         
-        MockServices.setServices(MockNodeOperation.class);
+        MockServices.setServices(MockWindowManager.class,
+                MockNodeOperation.class);
         MockNodeOperation.explored = null;
     }
 
+    @Override
     protected void tearDown() throws Exception {
     }
     
@@ -99,28 +106,182 @@ public class OpenCLITest extends NbTestCase {
     public static final class MockNodeOperation extends NodeOperation {
         public static Node explored;
         
+        @Override
         public boolean customize(Node n) {
             fail("No customize");
             return false;
         }
 
+        @Override
         public void explore(Node n) {
             assertNull("No explore before", explored);
             explored = n;
         }
 
+        @Override
         public void showProperties(Node n) {
             fail("no props");
         }
 
+        @Override
         public void showProperties(Node[] n) {
             fail("no props");
         }
 
+        @Override
         public Node[] select(String title, String rootTitle, Node root, NodeAcceptor acceptor, Component top) throws UserCancelException {
             fail("no select");
             return null;
         }
         
+    }
+
+    public static final class MockWindowManager extends WindowManager {
+
+        @Override
+        public void invokeWhenUIReady(Runnable runnable) {
+            // run immediatelly
+            runnable.run();
+        }
+
+        @Override
+        public Mode findMode(String name) {
+            throw unsupp();
+        }
+
+        @Override
+        public Mode findMode(TopComponent tc) {
+            throw unsupp();
+        }
+
+        @Override
+        public Set<? extends Mode> getModes() {
+            throw unsupp();
+        }
+
+        @Override
+        public Frame getMainWindow() {
+            throw unsupp();
+        }
+
+        @Override
+        public void updateUI() {
+            throw unsupp();
+        }
+
+        @Override
+        protected Component createTopComponentManager(TopComponent c) {
+            throw unsupp();
+        }
+
+        @Override
+        public Workspace createWorkspace(String name, String displayName) {
+            throw unsupp();
+        }
+
+        @Override
+        public Workspace findWorkspace(String name) {
+            throw unsupp();
+        }
+
+        @Override
+        public Workspace[] getWorkspaces() {
+            throw unsupp();
+        }
+
+        @Override
+        public void setWorkspaces(Workspace[] workspaces) {
+            throw unsupp();
+        }
+
+        @Override
+        public Workspace getCurrentWorkspace() {
+            throw unsupp();
+        }
+
+        @Override
+        public TopComponentGroup findTopComponentGroup(String name) {
+            throw new UnsupportedOperationException("Not supported yet.");
+        }
+
+        @Override
+        public void addPropertyChangeListener(PropertyChangeListener l) {
+            throw unsupp();
+        }
+
+        @Override
+        public void removePropertyChangeListener(PropertyChangeListener l) {
+            throw unsupp();
+        }
+
+        @Override
+        protected void topComponentOpen(TopComponent tc) {
+            throw unsupp();
+        }
+
+        @Override
+        protected void topComponentClose(TopComponent tc) {
+            throw unsupp();
+        }
+
+        @Override
+        protected void topComponentRequestActive(TopComponent tc) {
+            throw unsupp();
+        }
+
+        @Override
+        protected void topComponentRequestVisible(TopComponent tc) {
+            throw unsupp();
+        }
+
+        @Override
+        protected void topComponentDisplayNameChanged(TopComponent tc, String displayName) {
+            throw unsupp();
+        }
+
+        @Override
+        protected void topComponentHtmlDisplayNameChanged(TopComponent tc, String htmlDisplayName) {
+            throw unsupp();
+        }
+
+        @Override
+        protected void topComponentToolTipChanged(TopComponent tc, String toolTip) {
+            throw unsupp();
+        }
+
+        @Override
+        protected void topComponentIconChanged(TopComponent tc, Image icon) {
+            throw unsupp();
+        }
+
+        @Override
+        protected void topComponentActivatedNodesChanged(TopComponent tc,
+                Node[] activatedNodes) {
+            throw unsupp();
+        }
+
+        @Override
+        protected boolean topComponentIsOpened(TopComponent tc) {
+            throw unsupp();
+        }
+
+        @Override
+        protected Action[] topComponentDefaultActions(TopComponent tc) {
+            throw unsupp();
+        }
+
+        @Override
+        protected String topComponentID(TopComponent tc, String preferredID) {
+            throw unsupp();
+        }
+
+        @Override
+        public TopComponent findTopComponent(String tcID) {
+            throw unsupp();
+        }
+
+        private UnsupportedOperationException unsupp() {
+            return new UnsupportedOperationException("Not supported yet.");
+        }
     }
 }
