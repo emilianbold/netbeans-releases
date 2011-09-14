149d9c3a56a0b366c6e4afac394680502e5d701f
Miloslav Metelka <mmetelka@netbeans.org>
2011-09-14 10:22:41 +0200

    Removed extra debugging code.

--- editor.lib2/src/org/netbeans/modules/editor/lib2/view/ViewUpdates.java
+++ editor.lib2/src/org/netbeans/modules/editor/lib2/view/ViewUpdates.java
@@ -238,8 +238,6 @@ public final class ViewUpdates implements DocumentListener, EditorViewFactoryLis
 
     @Override
     public void insertUpdate(DocumentEvent evt) {
-        if (Integer.valueOf(55).equals(docView.getTextComponent().getClientProperty("id")))
-            evt = evt; // Place breakpoint here to track update for particular VH
         clearIncomingEvent(evt);
         if (docView.lock()) {
             docView.checkDocumentLockedIfLogging();
