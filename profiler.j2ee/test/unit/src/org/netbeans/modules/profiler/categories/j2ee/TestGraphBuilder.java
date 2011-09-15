d7bbd2cba9c9c02538a0757dce49699f14e169a4
Tomas Hurka <thurka@netbeans.org>
2011-09-15 16:17:03 +0200

    profiler.j2ee tests are broken after bugfix #202121, set CPUCCTNodeFactory explicitly in doStartup()

--- profiler.j2ee/test/unit/src/org/netbeans/modules/profiler/categories/j2ee/TestGraphBuilder.java
+++ profiler.j2ee/test/unit/src/org/netbeans/modules/profiler/categories/j2ee/TestGraphBuilder.java
@@ -43,8 +43,10 @@
  */
 package org.netbeans.modules.profiler.categories.j2ee;
 
+import org.netbeans.lib.profiler.ProfilerClient;
 import org.netbeans.lib.profiler.results.RuntimeCCTNode;
 import org.netbeans.lib.profiler.results.cpu.CPUCallGraphBuilder;
+import org.netbeans.lib.profiler.results.cpu.cct.CPUCCTNodeFactory;
 
 
 /**
@@ -56,5 +58,10 @@ public class TestGraphBuilder extends CPUCallGraphBuilder {
     
     protected RuntimeCCTNode getAppRootNode() {
         return super.getAppRootNode();
+    }
+
+    protected void doStartup(ProfilerClient profilerClient) {
+        super.doStartup(profilerClient);
+        setFactory(new CPUCCTNodeFactory(isCollectingTwoTimeStamps()));
     }
 }
