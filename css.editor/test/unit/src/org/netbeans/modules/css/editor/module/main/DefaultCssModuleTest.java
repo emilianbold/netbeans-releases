cd0a1d38da9942f17b3585eb666e5d70775837fa
Marek Fukala <mfukala@netbeans.org>
2011-09-29 14:55:55 +0200

    adding missing CSS2.1 property definitions for border-collapse and border-spacing

--- css.editor/test/unit/src/org/netbeans/modules/css/editor/module/main/DefaultCssModuleTest.java
+++ css.editor/test/unit/src/org/netbeans/modules/css/editor/module/main/DefaultCssModuleTest.java
@@ -44,6 +44,7 @@ package org.netbeans.modules.css.editor.module.main;
 import org.netbeans.modules.css.editor.module.CssModuleSupport;
 import org.netbeans.modules.css.editor.module.spi.HelpResolver;
 import org.netbeans.modules.css.editor.module.spi.Property;
+import org.netbeans.modules.css.editor.properties.parser.PropertyModel;
 
 /**
  *
@@ -51,29 +52,141 @@ import org.netbeans.modules.css.editor.module.spi.Property;
  */
 public class DefaultCssModuleTest extends CssModuleTestBase {
 
+    //parsed from http://www.w3.org/TR/CSS2/propidx.html
+    //(Cascading Style Sheets Level 2 Revision 1 (CSS 2.1) Specification)
+    private static final String[] ALL_CSS21_PROPERTIES = new String[]{
+        "azimuth",
+        "background-attachment",
+        "background-color",
+        "background-image",
+        "background-position",
+        "background-repeat",
+        "background",
+        "border-collapse",
+        "border-color",
+        "border-spacing",
+        "border-style",
+        "border-top", "border-right", "border-bottom", "border-left",
+        "border-top-color", "border-right-color", "border-bottom-color", "border-left-color",
+        "border-top-style", "border-right-style", "border-bottom-style", "border-left-style",
+        "border-top-width", "border-right-width", "border-bottom-width", "border-left-width",
+        "border-width",
+        "border",
+        "bottom",
+        "caption-side",
+        "clear",
+        "clip",
+        "color",
+        "content",
+        "counter-increment",
+        "counter-reset",
+        "cue-after",
+        "cue-before",
+        "cue",
+        "cursor",
+        "direction",
+        "display",
+        "elevation",
+        "empty-cells",
+        "float",
+        "font-family",
+        "font-size",
+        "font-style",
+        "font-variant",
+        "font-weight",
+        "font",
+        "height",
+        "left",
+        "letter-spacing",
+        "line-height",
+        "list-style-image",
+        "list-style-position",
+        "list-style-type",
+        "list-style",
+        "margin-right", "margin-left",
+        "margin-top", "margin-bottom",
+        "margin",
+        "max-height",
+        "max-width",
+        "min-height",
+        "min-width",
+        "orphans",
+        "outline-color",
+        "outline-style",
+        "outline-width",
+        "outline",
+        "overflow",
+        "padding-top", "padding-right", "padding-bottom", "padding-left",
+        "padding",
+        "page-break-after",
+        "page-break-before",
+        "page-break-inside",
+        "pause-after",
+        "pause-before",
+        "pause",
+        "pitch-range",
+        "pitch",
+        "play-during",
+        "position",
+        "quotes",
+        "richness",
+        "right",
+        "speak-header",
+        "speak-numeral",
+        "speak-punctuation",
+        "speak",
+        "speech-rate",
+        "stress",
+        "table-layout",
+        "text-align",
+        "text-decoration",
+        "text-indent",
+        "text-transform",
+        "top",
+        "unicode-bidi",
+        "vertical-align",
+        "visibility",
+        "voice-family",
+        "volume",
+        "white-space",
+        "widows",
+        "width",
+        "word-spacing",
+        "z-index"
+    };
+    
     public DefaultCssModuleTest(String name) {
         super(name);
     }
-    
+
     @Override
     protected void setUp() throws Exception {
         super.setUp();
         //allow the InstalledFilesLocator to work
         System.setProperty("netbeans.dirs", System.getProperty("cluster.path.final"));//NOI18N
     }
-    
+
     public void testDocumentation() {
         HelpResolver resolver = CssModuleSupport.getHelpResolver();
         assertNotNull(resolver);
-        
+
         Property color = CssModuleSupport.getProperty("color");
         assertNotNull(color);
-        
+
         String help = resolver.getHelp(color);
         assertNotNull(help);
         assertTrue(help.length() > 0);
-        
-        System.out.println(help);
+
+//        System.out.println(help);
     }
-    
+
+    public void testAllCSS21PropertiesAreAvailable() {
+        for(String propName : ALL_CSS21_PROPERTIES) {
+            Property prop = CssModuleSupport.getProperty(propName);
+            assertNotNull(String.format("No such property %s found!", propName), prop);
+            
+            PropertyModel model = CssModuleSupport.getPropertyModel(propName);
+            assertNotNull(model);
+        }
+    }
 }
