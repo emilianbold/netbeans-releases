e7d8db6122804147887e97ea568305a41e1b61a6
Ondrej Brejla <obrejla@netbeans.org>
2011-10-04 14:29:54 +0200

    Removing whitespaces

--- php.editor/src/org/netbeans/modules/php/editor/codegen/TableGenerator.java
+++ php.editor/src/org/netbeans/modules/php/editor/codegen/TableGenerator.java
@@ -124,7 +124,7 @@ public class TableGenerator implements CodeGenerator {
 
     private String findConnVariableInScope() {
         final List<String> connVariables = new ArrayList<String>();
-        
+
         try {
             ParserManager.parse(Collections.singleton(Source.create(component.getDocument())), new UserTask() {
 
