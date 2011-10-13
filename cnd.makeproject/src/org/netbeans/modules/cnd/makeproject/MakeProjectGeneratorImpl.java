138253724a9728ca259b5f9df5f92b7d347ef6c8
Alexander Simon <alexvsimon@netbeans.org>
2011-10-13 17:59:08 +0400

    fixing Bug #203507 Can't create project in Samba Remote mode (WindowsXP->Solaris) - work around of Bug #203636

--- cnd.makeproject/src/org/netbeans/modules/cnd/makeproject/MakeProjectGeneratorImpl.java
+++ cnd.makeproject/src/org/netbeans/modules/cnd/makeproject/MakeProjectGeneratorImpl.java
@@ -80,6 +80,7 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.netbeans.modules.cnd.makeproject.api.ProjectGenerator.ProjectParameters;
 import org.netbeans.modules.cnd.makeproject.api.ProjectSupport;
+import org.netbeans.modules.cnd.makeproject.ui.wizards.MakeSampleProjectGenerator;
 import org.netbeans.modules.cnd.spi.remote.RemoteSyncFactory;
 import org.netbeans.modules.cnd.utils.CndPathUtilitities;
 import org.netbeans.modules.cnd.utils.CndUtils;
@@ -345,7 +346,9 @@ public class MakeProjectGeneratorImpl {
     }
 
     private static FileObject createProjectDir(ProjectParameters prjParams) throws IOException {
-        FileObject dirFO = FileUtil.createFolder(prjParams.getSourceFileSystem().getRoot(), prjParams.getProjectFolderPath());
+        String projectFolderPath = prjParams.getProjectFolderPath();
+        MakeSampleProjectGenerator.workAroundBug203507(projectFolderPath);
+        FileObject dirFO = FileUtil.createFolder(prjParams.getSourceFileSystem().getRoot(), projectFolderPath);
         //File dir = prjParams.getProjectFolder();
         //if (!dir.exists()) {
         //    //Refresh before mkdir not to depend on window focus
