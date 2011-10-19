17fd3bf3447c8ab4b020cc66913b0bfe689d7fd8
Egor Ushakov <gorrus@netbeans.org>
2011-10-19 14:14:23 +0400

    fixed #203916 (Rerun does not update PID)

--- cnd.debugger.gdb2/src/org/netbeans/modules/cnd/debugger/gdb2/GdbDebuggerImpl.java
+++ cnd.debugger.gdb2/src/org/netbeans/modules/cnd/debugger/gdb2/GdbDebuggerImpl.java
@@ -1165,6 +1165,9 @@ public final class GdbDebuggerImpl extends NativeDebuggerImpl
 
 	    breakStartCmd.chain(runCmd, breakMainCmd);
 	    breakMainCmd.chain(runCmd, runCmd);
+            
+            // need to clear PID, see IZ 203916
+            session().setPid(-1);
 
             // _start does not work on MacOS
             if (getHost().getPlatform() == Platform.MacOSX_x86) {
