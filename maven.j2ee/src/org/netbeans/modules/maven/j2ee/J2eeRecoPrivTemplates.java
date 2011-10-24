1df43a5889b70af3b2d7403d6dacc1b2f031625c
Martin Janicek <mjanicek@netbeans.org>
2011-10-24 10:39:02 +0200

    #204136 - NullPointerException at org.netbeans.modules.maven.j2ee.J2eeRecoPrivTemplates.getPrivilegedTemplates

--- maven.j2ee/src/org/netbeans/modules/maven/j2ee/J2eeRecoPrivTemplates.java
+++ maven.j2ee/src/org/netbeans/modules/maven/j2ee/J2eeRecoPrivTemplates.java
@@ -285,11 +285,13 @@ public class J2eeRecoPrivTemplates implements RecommendedTemplates, PrivilegedTe
                 if (Profile.JAVA_EE_6_WEB.equals(p) || Profile.JAVA_EE_6_FULL.equals(p)) {
                     ArrayList<String> toRet = new ArrayList<String>(Arrays.asList(WEB_PRIVILEGED_NAMES_6));
                     J2eeProjectCapabilities cap = J2eeProjectCapabilities.forProject(project);
-                    if (cap.isEjb31Supported()) {
-                        toRet.addAll(Arrays.asList(WEB_PRIVILEGED_NAMES_EE6_FULL));
-                    }
-                    if (cap.isEjb31LiteSupported()) {
-                        toRet.addAll(Arrays.asList(WEB_PRIVILEGED_NAMES_EE6_WEB));
+                    if (cap != null) {
+                        if (cap.isEjb31Supported()) {
+                            toRet.addAll(Arrays.asList(WEB_PRIVILEGED_NAMES_EE6_FULL));
+                        }
+                        if (cap.isEjb31LiteSupported()) {
+                            toRet.addAll(Arrays.asList(WEB_PRIVILEGED_NAMES_EE6_WEB));
+                        }
                     }
                     return toRet.toArray(new String[0]);
                 }
