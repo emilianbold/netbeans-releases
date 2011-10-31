b54b87a792e03424bd95b77aa73d71c1ecc1a934
Marek Fukala <mfukala@netbeans.org>
2011-10-31 13:40:44 +0100

    #196724 - Missing DIR attribute value

--- html.editor/src/org/netbeans/modules/html/editor/completion/AttrValuesCompletion.java
+++ html.editor/src/org/netbeans/modules/html/editor/completion/AttrValuesCompletion.java
@@ -100,6 +100,9 @@ public abstract class AttrValuesCompletion {
                 "index", "glossary", "copyright", "chapter", "section", "subsection",
                 "appendix", "help","bookmark"});
     
+    private static final ValueCompletion<HtmlCompletionItem> DIR_SUPPORT =
+            new ValuesSetSupport(new String[]{"ltr", "rtl"});
+    
     static {
         //TODO uff, such long list ... redo it so it resolves according to the DTD attribute automatically
         //mixed with html5 content...
@@ -176,6 +179,8 @@ public abstract class AttrValuesCompletion {
         putSupport("input", "type", INPUT_TYPE_SUPPORT); //NOI18N
         
         putSupport(null, "rel", LINK_TYPES_SUPPORT);
+        
+        putSupport(null, "dir", DIR_SUPPORT);
     }
 
     private static void putSupport(String tag, String attr, ValueCompletion<HtmlCompletionItem> support) {
