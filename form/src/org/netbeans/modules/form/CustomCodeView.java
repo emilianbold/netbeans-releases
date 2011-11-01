d8827e249e7ca851ddae1ee5b0cd6467d57aa408
Jan Stola <jstola@netbeans.org>
2011-11-01 14:14:52 +0100

    204477: Code customizer for swing component does not resize correctly

--- form/src/org/netbeans/modules/form/CustomCodeView.java
+++ form/src/org/netbeans/modules/form/CustomCodeView.java
@@ -1154,7 +1154,7 @@ class CustomCodeView extends javax.swing.JPanel {
                     .addComponent(selectComponentLabel)
                     .addComponent(componentCombo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(renameButton))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(initCodeLabel)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 348, Short.MAX_VALUE)
