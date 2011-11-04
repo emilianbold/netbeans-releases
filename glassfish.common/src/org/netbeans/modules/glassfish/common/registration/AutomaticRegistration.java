604c157fb37d738bb4255373ffe7c4ce43b7903c
vince kraemer <vkraemer@netbeans.org>
2011-11-04 12:49:22 -0700

    #204642 : fix name to match actual version being installed

--- glassfish.common/src/org/netbeans/modules/glassfish/common/registration/AutomaticRegistration.java
+++ glassfish.common/src/org/netbeans/modules/glassfish/common/registration/AutomaticRegistration.java
@@ -112,7 +112,7 @@ public class AutomaticRegistration {
             // ee6wc
             config = "GlassFishEE6WC/Instances";
             deployer = "deployer:gfv3ee6wc";
-            defaultDisplayName = "GlassFish Server 3.1";
+            defaultDisplayName = "GlassFish Server 3.1.1";
         } else if(new File(glassfishHome, "lib/schemas/web-app_3_0.xsd").exists()) {
             // ee6
             config = "GlassFishEE6/Instances";
