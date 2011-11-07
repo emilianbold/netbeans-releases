210bcca0325e7c0712fae8e29fe8e6813ed4a383
Jesse Glick <jglick@netbeans.org>
2011-11-07 16:15:40 -0500

    Attempting to fix various memory leaks.

--- maven.grammar/src/org/netbeans/modules/maven/navigator/POMModelPanel.java
+++ maven.grammar/src/org/netbeans/modules/maven/navigator/POMModelPanel.java
@@ -51,6 +51,8 @@ import java.awt.event.KeyEvent;
 import java.beans.PropertyVetoException;
 import java.io.File;
 import java.io.IOException;
+import java.lang.ref.Reference;
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -128,7 +130,7 @@ public class POMModelPanel extends javax.swing.JPanel implements ExplorerManager
     
     private BeanTreeView treeView;
     private DataObject current;
-    private JTextComponent currentComponent;
+    private Reference<JTextComponent> currentComponent;
     private int currentDot = -1;
     private RequestProcessor.Task caretTask = RequestProcessor.getDefault().create(new Runnable() {
         public void run() {
@@ -288,8 +290,9 @@ public class POMModelPanel extends javax.swing.JPanel implements ExplorerManager
         if (current != null) {
             current.getPrimaryFile().removeFileChangeListener(adapter);
         }
-        if (currentComponent != null) {
-            currentComponent.removeCaretListener(this);
+        JTextComponent cc = currentComponent != null ? currentComponent.get() : null;
+        if (cc != null) {
+            cc.removeCaretListener(this);
         }
     }
     
@@ -402,7 +405,7 @@ public class POMModelPanel extends javax.swing.JPanel implements ExplorerManager
                         JTextComponent component = panes[0];
                         component.removeCaretListener(POMModelPanel.this);
                         component.addCaretListener(POMModelPanel.this);
-                        currentComponent = component;
+                        currentComponent = new WeakReference<JTextComponent>(component);
                     }
                 }
             } );
@@ -600,7 +603,8 @@ public class POMModelPanel extends javax.swing.JPanel implements ExplorerManager
     }
 
     public void caretUpdate(CaretEvent e) {
-        if (e.getSource() != currentComponent) {
+        JTextComponent cc = currentComponent != null ? currentComponent.get() : null;
+        if (e.getSource() != cc) {
             ((JTextComponent)e.getSource()).removeCaretListener(this);
             //just a double check we do't get a persistent leak here..
             return;
