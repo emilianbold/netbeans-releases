4d19cd057eaf07e3ef753792da35e39dae483b1f
Jesse Glick <jglick@netbeans.org>
2011-11-10 16:51:16 -0500

    #177609: [68cat] Run Main Class not invoked directly after saving

--- maven/src/org/netbeans/modules/maven/execute/DefaultReplaceTokenProvider.java
+++ maven/src/org/netbeans/modules/maven/execute/DefaultReplaceTokenProvider.java
@@ -269,10 +269,8 @@ public class DefaultReplaceTokenProvider implements ReplaceTokenProvider, Action
                     for (int i = 0; i < grp.length; i++) {
                         String relPath = FileUtil.getRelativePath(grp[i].getRootFolder(), fo);
                         if (relPath != null) {
-                            if (!SourceUtils.isScanInProgress()) {
-                                if (SourceUtils.isMainClass(relPath.replaceFirst("[.]java$", "").replace('/', '.'), ClasspathInfo.create(fo), true)) {
-                                    return action + ".main";//NOI18N
-                                }
+                            if (SourceUtils.isMainClass(relPath.replaceFirst("[.]java$", "").replace('/', '.'), ClasspathInfo.create(fo), true)) {
+                                return action + ".main";//NOI18N
                             }
                         }
                     }
