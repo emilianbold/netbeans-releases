500412d1197d5440011b05f87a4aa92cddf01141
Jesse Glick <jglick@netbeans.org>
2011-11-17 19:52:14 -0500

    #202831: Move Maven Repositories view under Services tab

---
+++ maven.repository/src/org/netbeans/modules/maven/repository/M2RepositoryBrowser.java
@@ -0,0 +1,202 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2010 Oracle and/or its affiliates. All rights reserved.
+ *
+ * Oracle and Java are registered trademarks of Oracle and/or its affiliates.
+ * Other names may be trademarks of their respective owners.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ *
+ * Contributor(s):
+ *
+ * Portions Copyrighted 2008 Sun Microsystems, Inc.
+ */
+package org.netbeans.modules.maven.repository;
+
+import java.awt.event.ActionEvent;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import org.netbeans.api.core.ide.ServicesTabNodeRegistration;
+import org.netbeans.modules.maven.indexer.api.QueryRequest;
+import org.netbeans.modules.maven.indexer.api.RepositoryIndexer;
+import org.netbeans.modules.maven.indexer.api.RepositoryInfo;
+import org.netbeans.modules.maven.indexer.api.RepositoryPreferences;
+import static org.netbeans.modules.maven.repository.Bundle.*;
+import org.netbeans.modules.maven.repository.register.RepositoryRegisterUI;
+import org.openide.DialogDescriptor;
+import org.openide.DialogDisplayer;
+import org.openide.NotifyDescriptor;
+import org.openide.nodes.AbstractNode;
+import org.openide.nodes.ChildFactory;
+import org.openide.nodes.Children;
+import org.openide.nodes.Node;
+import org.openide.util.ChangeSupport;
+import org.openide.util.NbBundle.Messages;
+import org.openide.util.RequestProcessor;
+import org.openide.util.Union2;
+
+@ServicesTabNodeRegistration(name="M2RepositoryBrowser", displayName="#CTL_M2RepositoryBrowserTopComponent", shortDescription="#HINT_M2RepositoryBrowserTopComponent", iconResource=M2RepositoryBrowser.ICON_PATH, position=431)
+@Messages({
+    "CTL_M2RepositoryBrowserTopComponent=Maven Repositories",
+    "HINT_M2RepositoryBrowserTopComponent=Displays contents of local and remote Apache Maven repositories and permits them to be searched and indexed."
+})
+public final class M2RepositoryBrowser extends AbstractNode {
+
+    static final String ICON_PATH = "org/netbeans/modules/maven/repository/MavenRepoBrowser.png"; //NOI18N
+
+    private M2RepositoryBrowser() {
+        super(Children.create(new RootNodes(), true));
+        setName("M2RepositoryBrowser");
+        setDisplayName(CTL_M2RepositoryBrowserTopComponent());
+        setShortDescription(HINT_M2RepositoryBrowserTopComponent());
+        setIconBaseWithExtension(ICON_PATH);
+    }
+
+    @Override public Action[] getActions(boolean context) {
+        return new Action[] {
+            new SearchAction(),
+            new AddAction()
+        };
+    }
+
+    private static class AddAction extends AbstractAction {
+        AddAction() {
+            super(LBL_Add_Repo());
+        }
+        @Override public void actionPerformed(ActionEvent e) {
+    final RepositoryRegisterUI rrui = new RepositoryRegisterUI();
+    rrui.getAccessibleContext().setAccessibleDescription(LBL_Add_Repo());
+    DialogDescriptor dd = new DialogDescriptor(rrui, LBL_Add_Repo());
+    dd.setClosingOptions(new Object[]{
+                rrui.getButton(),
+                DialogDescriptor.CANCEL_OPTION
+            });
+    dd.setOptions(new Object[]{
+                rrui.getButton(),
+                DialogDescriptor.CANCEL_OPTION
+            });
+    Object ret = DialogDisplayer.getDefault().notify(dd);
+    if (rrui.getButton() == ret) {
+        final  RepositoryInfo info;
+        try {
+            info = rrui.getRepositoryInfo();
+        } catch (URISyntaxException x) {
+            DialogDisplayer.getDefault().notify(new NotifyDescriptor.Message(x.getLocalizedMessage(), NotifyDescriptor.ERROR_MESSAGE));
+            return;
+        }
+        RepositoryPreferences.getInstance().addOrModifyRepositoryInfo(info);
+        RequestProcessor.getDefault().post(new Runnable() {
+                @Override public void run() {
+                    RepositoryIndexer.indexRepo(info);
+                }
+            });
+    }
+        }
+    }
+
+    private static class SearchAction extends AbstractAction {
+        @Messages("LBL_REPO_Find=Find")
+        SearchAction() {
+            super(LBL_REPO_Find());
+        }
+        @Messages("TIT_Find_In_Repositories=Find in Repositories")
+        @Override public void actionPerformed(ActionEvent e) {
+    final FindInRepoPanel pnl = new FindInRepoPanel();
+    pnl.getAccessibleContext().setAccessibleDescription(TIT_Find_In_Repositories());
+    final DialogDescriptor dd = new DialogDescriptor(pnl, TIT_Find_In_Repositories());
+    pnl.attachDesc(dd);
+    Object ret = DialogDisplayer.getDefault().notify(dd);
+    if (ret == DialogDescriptor.OK_OPTION) {
+        synchronized (searches) {
+            searches.add(new QueryRequest(pnl.getQuery(), RepositoryPreferences.getInstance().getRepositoryInfos(), new Observer() {
+                @Override public void update(Observable o, Object arg) {/* unused, real observer added later */}
+            }));
+        }
+        cs.fireChange();
+    }
+        }
+    }
+
+    private static final List<QueryRequest> searches = new ArrayList<QueryRequest>();
+    static void remove(QueryRequest search) {
+        synchronized (searches) {
+            searches.remove(search);
+        }
+        cs.fireChange();
+    }
+    private static final ChangeSupport cs = new ChangeSupport(M2RepositoryBrowser.class);
+    public static void addChangeListener(ChangeListener l) {
+        cs.addChangeListener(l);
+    }
+    public static void removeChangeListener(ChangeListener l) {
+        cs.removeChangeListener(l);
+    }
+
+    private static class RootNodes extends ChildFactory.Detachable<Union2<RepositoryInfo,QueryRequest>> implements ChangeListener {
+        @Override protected boolean createKeys(List<Union2<RepositoryInfo,QueryRequest>> toPopulate) {
+            for (RepositoryInfo info : RepositoryPreferences.getInstance().getRepositoryInfos()) {
+                toPopulate.add(Union2.<RepositoryInfo,QueryRequest>createFirst(info));
+            }
+            synchronized (searches) {
+                for (QueryRequest search : searches) {
+                    toPopulate.add(Union2.<RepositoryInfo,QueryRequest>createSecond(search));
+                }
+            }
+            return true;
+        }
+        @Override protected Node createNodeForKey(Union2<RepositoryInfo,QueryRequest> key) {
+            if (key.hasFirst()) {
+                return new RepositoryNode(key.first());
+            } else {
+                return new FindResultsNode(key.second());
+            }
+        }
+        @Override protected void addNotify() {
+            RepositoryPreferences.getInstance().addChangeListener(this);
+            addChangeListener(this);
+        }
+        @Override protected void removeNotify() {
+            RepositoryPreferences.getInstance().removeChangeListener(this);
+            removeChangeListener(this);
+        }
+        @Override public void stateChanged(ChangeEvent e) {
+            refresh(false);
+        }
+    }
+
+}
