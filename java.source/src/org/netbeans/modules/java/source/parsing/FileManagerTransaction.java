7647a9f50c98c9c1d2906aeb692eca8441ae42cf
Svata Dedic <sdedic@netbeans.org>
2011-12-20 21:03:32 +0100

    Partial prototype for transactional storage

--- java.source/src/org/netbeans/modules/java/source/parsing/FileManagerTransaction.java
+++ java.source/src/org/netbeans/modules/java/source/parsing/FileManagerTransaction.java
@@ -41,15 +41,25 @@
  */
 package org.netbeans.modules.java.source.parsing;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import javax.tools.JavaFileObject;
 import org.netbeans.api.annotations.common.NonNull;
+import org.netbeans.api.annotations.common.NullAllowed;
+import org.netbeans.modules.java.preprocessorbridge.spi.JavaFileFilterImplementation;
 import org.netbeans.modules.java.source.indexing.TransactionContext;
 import org.netbeans.modules.java.source.util.Iterators;
 
@@ -65,10 +75,32 @@ public abstract class FileManagerTransaction extends TransactionContext.Service 
 
     @NonNull
     abstract Iterable<JavaFileObject> filter (
+        @NonNull String packageName,
         @NonNull Iterable<JavaFileObject> files);
 
+    /**
+     * Creates fileobject suitable for output. 
+     * @param file file to produce
+     * @param root root directory for the package structure
+     * @param filter output filter
+     * @param encoding desired file encoding
+     * @return 
+     */
+    abstract JavaFileObject  createFileObject(final @NonNull File file, final @NonNull File root,
+        final @NullAllowed JavaFileFilterImplementation filter, final @NullAllowed Charset encoding);
 
-
+    /**
+     * Looks up a FileObject suitable for reading. The method MAY return {@code null}, if the FileObject
+     * is not part of the transaction.
+     * 
+     * @param dirName
+     * @param relativeName
+     * @return 
+     */
+    JavaFileObject  readFileObject(String dirName, String relativeName) {
+        return null;
+    }
+    
     public static FileManagerTransaction writeBack() {
         return new WriteBack();
     }
@@ -80,15 +112,97 @@ public abstract class FileManagerTransaction extends TransactionContext.Service 
     public static FileManagerTransaction read() {
         return new Read();
     }
+    
+    static class JFOContent {
+        JavaFileObject  jfo;
+        byte[] content;
+
+        public JFOContent(JavaFileObject jfo, byte[] content) {
+            this.jfo = jfo;
+            this.content = content;
+        }
+        
+        
+    }
+    
+    /**
+     * Simple storage for cached file content, which can be flushed from time to time
+     * when e.g. memory is exhausted, or too much content is generated.
+     */
+    static class Storage {
+        Map<String, Map<File, CachedFileObject>> contentCache = new HashMap<String, Map<File, CachedFileObject>>();
+        
+        void addFile(String packageName, CachedFileObject fo) {
+            
+            Map<File, CachedFileObject> dirContent = contentCache.get(packageName);
+            if (dirContent == null) {
+                dirContent = new HashMap<File, CachedFileObject>();
+                contentCache.put(packageName, dirContent);
+            }
+            dirContent.put(toFile(fo), fo);
+        }
+        
+        Collection<File>  listDir(String dir) {
+            Map<File, CachedFileObject> content = contentCache.get(dir);
+            return content == null ? Collections.<File>emptyList() : content.keySet();
+        }
+        
+        /**
+         * This method makes a copy of the storage
+         * @param dir
+         * @return 
+         */
+        Collection<JavaFileObject> getFileObjects(String dir) {
+            Map<File, CachedFileObject> content = contentCache.get(dir);
+            return new ArrayList<JavaFileObject>(content.values());
+        }
+        
+        void clear() {
+            contentCache.clear();
+        }
+        
+        /**
+         * Flushes contents of the storage
+         * @throws IOException 
+         */
+        void flush() throws IOException {
+            for (Map<File, CachedFileObject> dirContent : contentCache.values()) {
+                for (CachedFileObject cfo : dirContent.values()) {
+                    cfo.flush();
+                }
+            }
+            contentCache.clear();
+        }
+        
+        CachedFileObject getFileObject(String dir, String file) {
+            Map<File, CachedFileObject> content = contentCache.get(dir);
+            for (Map.Entry<File, CachedFileObject> en : content.entrySet()) {
+                if (file.equals(en.getKey().getName())) {
+                    return en.getValue();
+                }
+            }
+            return null;
+        }
+    }
+    
+    /**
+     * Helper that extracts j.o.File from the JFO instance
+     * @param o JFO instance
+     * @return the File represented by the JFO
+     */
+    private static File toFile(JavaFileObject o) {
+        File f = ((FileObjects.FileBase)o).f;
+        return f;
+    }
 
     private static class WriteBack extends FileManagerTransaction {
 
         private final Set<File> deleted;
-        private final Map<File,byte[]> written;
+        private final Storage   storage;
 
         private WriteBack () {
             deleted = new HashSet<File>();
-            written = new HashMap<File, byte[]>();
+            storage = new Storage();
         }
 
         @Override
@@ -99,21 +213,54 @@ public abstract class FileManagerTransaction extends TransactionContext.Service 
 
         @Override
         @NonNull
-        Iterable<JavaFileObject> filter(Iterable<JavaFileObject> files) {
+        Iterable<JavaFileObject> filter(String packageName, Iterable<JavaFileObject> files) {
+            final Collection<File>    added = storage.listDir(packageName);
+
             Iterable<JavaFileObject> res = files;
-            if (!deleted.isEmpty()) {
-                res = Iterators.filter (
+            if (deleted.isEmpty() && added.isEmpty()) {
+                return res;
+            }
+            
+            if (added.isEmpty()) {
+                // just filter out the deleted files
+                return Iterators.filter (
                     res,
                     new Comparable<JavaFileObject>() {
                         public int compareTo(@NonNull final JavaFileObject o) {
-                            final File f = ((FileObjects.FileBase)o).f;
+                            final File f = toFile(o);
                             return deleted.contains(f) ? 0 : -1;
                         }
                     }
                 );
             }
-            //Todo: written
-            return res;
+            Collection<JavaFileObject> toAdd = storage.getFileObjects(packageName);
+            Collection<Iterable<JavaFileObject>> chain = new ArrayList<Iterable<JavaFileObject>>(2);
+            chain.add(toAdd);
+            chain.add(Iterators.filter (
+                    res,
+                    new Comparable<JavaFileObject>() {
+                        public int compareTo(@NonNull final JavaFileObject o) {
+                            final File f = toFile(o);
+                            // we must also remove the added files, so the Iterator does not contain duplicates
+                            return deleted.contains(f) || added.contains(f) ? 0 : -1;
+                        }
+                    }
+            ));
+            return Iterators.chained(chain);
+        }
+        
+        JavaFileObject  createFileObject(final @NonNull File file, final @NonNull File root,
+            final @NullAllowed JavaFileFilterImplementation filter, final @NullAllowed Charset encoding) {
+            final String[] pkgNamePair = FileObjects.getFolderAndBaseName(
+                    FileObjects.getRelativePath(root,file),File.separatorChar);
+
+            String pname = FileObjects.convertFolder2Package(pkgNamePair[0], File.separatorChar);
+            CachedFileObject cfo = new CachedFileObject(
+                    this, file, 
+                    pname, pkgNamePair[1], filter, encoding);
+            
+            storage.addFile(pname, cfo);
+            return cfo;
         }
 
         @Override
@@ -122,14 +269,7 @@ public abstract class FileManagerTransaction extends TransactionContext.Service 
                 for (File f : deleted) {
                     f.delete();
                 }
-                for (Map.Entry<File,byte[]> e : written.entrySet()) {
-                    final FileOutputStream out = new FileOutputStream(e.getKey());
-                    try {
-                        out.write(e.getValue());
-                    } finally {
-                        out.close();
-                    }
-                }
+                storage.flush();
             } finally {
                 clean();
             }
@@ -142,7 +282,12 @@ public abstract class FileManagerTransaction extends TransactionContext.Service 
 
         private void clean() {
             deleted.clear();
-            written.clear();
+            storage.clear();
+        }
+
+        @Override
+        JavaFileObject readFileObject(String dirName, String relativeName) {
+            return storage.getFileObject(dirName, relativeName);
         }
     }
 
@@ -156,7 +301,7 @@ public abstract class FileManagerTransaction extends TransactionContext.Service 
 
         @Override
         @NonNull
-        Iterable<JavaFileObject> filter(@NonNull final Iterable<JavaFileObject> files) {
+        Iterable<JavaFileObject> filter(String packageName, @NonNull final Iterable<JavaFileObject> files) {
             return files;
         }
 
@@ -169,6 +314,10 @@ public abstract class FileManagerTransaction extends TransactionContext.Service 
             throw new UnsupportedOperationException("RollBack is unsupported"); //NOI18N
         }
 
+        @Override
+        JavaFileObject createFileObject(File file, File root, JavaFileFilterImplementation filter, Charset encoding) {
+            return FileObjects.fileFileObject(file, root, filter, encoding);
+        }
     }
 
     private static class Read extends FileManagerTransaction {
@@ -180,7 +329,7 @@ public abstract class FileManagerTransaction extends TransactionContext.Service 
 
         @Override
         @NonNull
-        Iterable<JavaFileObject> filter(@NonNull final Iterable<JavaFileObject> files) {
+        Iterable<JavaFileObject> filter(String packageName, @NonNull final Iterable<JavaFileObject> files) {
             return files;
         }
 
@@ -194,5 +343,86 @@ public abstract class FileManagerTransaction extends TransactionContext.Service 
             throw new UnsupportedOperationException ("RollBack not supported, read-only."); //NOi18N
         }
 
+        @Override
+        JavaFileObject createFileObject(File file, File root, JavaFileFilterImplementation filter, Charset encoding) {
+            return FileObjects.fileFileObject(file, root, filter, encoding);
+        }
+
+    }
+    
+    /**
+     * File object, whose contents is cached in memory and eventually flushed to the disk.
+     * When flushed, methods will delegate to a plain regular FileObject over j.o.File,
+     * so dangling references still work
+     */
+    static class CachedFileObject extends FileObjects.FileBase {
+        static final byte[] NOTHING = new byte[0];
+        
+        byte[] content = NOTHING;
+        WriteBack   writer;
+        FileObjects.FileBase    delegate;
+        
+        public CachedFileObject(WriteBack wb, File file, String pkgName, String name, JavaFileFilterImplementation filter, Charset encoding) {
+            super(file, pkgName, name, filter, encoding);
+            this.writer = wb;
+        }
+        
+        static File getRootFile(File startFrom, String pkgName) {
+            int index;
+            
+            while ((index = pkgName.indexOf('.')) != -1) {
+                startFrom = startFrom.getParentFile();
+            }
+            return startFrom;
+        }
+        
+        void flush() throws IOException {
+            final FileOutputStream out = new FileOutputStream(f);
+            try {
+                out.write(content);
+                free();
+            } finally {
+                out.close();
+            }
+        }
+        
+        void free() {
+            writer = null;
+            delegate = (FileObjects.FileBase)FileObjects.fileFileObject(getFile(), getRootFile(getFile(), getPackage()), filter, encoding);
+        }
+
+        @Override
+        public boolean delete() {
+            if (delegate != null) {
+                return delegate.delete();
+            } else {
+                writer.delete(toFile(this));
+                return true;
+            }
+        }
+
+        @Override
+        public InputStream openInputStream() throws IOException {
+            if (delegate != null) {
+                return delegate.openInputStream();
+            } else {
+                return new ByteArrayInputStream(content);
+            }
+        }
+
+        @Override
+        public OutputStream openOutputStream() throws IOException {
+            if (delegate != null) {
+                return delegate.openOutputStream();
+            } else {
+                return new ByteArrayOutputStream() {
+                    @Override
+                    public void close() throws IOException {
+                        super.close();
+                        content = toByteArray();
+                    }
+                };
+            }
+        }
     }
 }
