1091674cc4092fed9065894a17d71a090850bbb4
Ralph Benjamin Ruijs <ralphbenjamin@netbeans.org>
2012-01-16 12:01:56 +0100

    Issue #207179 - NullPointerException at org.netbeans.modules.refactoring.java.RefactoringUtils.findEnclosingClass

--- refactoring.java/src/org/netbeans/modules/refactoring/java/ui/MoveMembersUI.java
+++ refactoring.java/src/org/netbeans/modules/refactoring/java/ui/MoveMembersUI.java
@@ -72,7 +72,7 @@ public class MoveMembersUI implements RefactoringUI {
         this.selectedElements = selectedElement;
         this.ic = new InstanceContent();
         this.refactoring = new MoveRefactoring(new AbstractLookup(ic));
-        refactoring.getContext().add(new JavaMoveMembersProperties());
+        refactoring.getContext().add(new JavaMoveMembersProperties(selectedElement));
     }
 
     @Override
@@ -103,12 +103,14 @@ public class MoveMembersUI implements RefactoringUI {
         ic.set(handles, null);
         TreePathHandle target = panel.getTarget();
         refactoring.setTarget(target == null? Lookup.EMPTY:Lookups.fixed(target));
-        JavaMoveMembersProperties properties = new JavaMoveMembersProperties();
+        JavaMoveMembersProperties properties = refactoring.getContext().lookup(JavaMoveMembersProperties.class);
+        if(properties == null) {
+            refactoring.getContext().add(properties = new JavaMoveMembersProperties(selectedElements));
+        }
         properties.setVisibility(panel.getVisibility());
         properties.setDelegate(panel.getDelegate());
         properties.setUpdateJavaDoc(panel.getUpdateJavaDoc());
         properties.setAddDeprecated(panel.getDeprecated());
-        refactoring.getContext().add(properties);
         return checkOnly? refactoring.fastCheckParameters() : refactoring.checkParameters();
     }
 
