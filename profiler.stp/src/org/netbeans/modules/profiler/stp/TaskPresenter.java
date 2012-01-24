78fcca55234cf63928ff37e72a431cccfa2d8b71
Jiri Sedlacek <jis@netbeans.org>
2012-01-24 15:14:41 +0100

    Fixed #207629 by reverting d351230ef663

--- profiler.stp/src/org/netbeans/modules/profiler/stp/TaskPresenter.java
+++ profiler.stp/src/org/netbeans/modules/profiler/stp/TaskPresenter.java
@@ -163,7 +163,7 @@ public class TaskPresenter implements TaskChooser.Item {
         private JScrollPane configurationsScrollPane;
         private TPHyperlinkTextArea createCustom;
         private int activeSettingsIndex = -1; // valid only when popup menu is opened!
-        private int selectedSettingsIndex = -1;
+        private int selectedSettingsIndex = 0;
 
         //~ Constructors ---------------------------------------------------------------------------------------------------------
 
@@ -177,7 +177,6 @@ public class TaskPresenter implements TaskChooser.Item {
         public void setProfilingSettings(ArrayList<ProfilingSettings> profilingSettings) {
             resetProfilingSettings();
             this.profilingSettings.addAll(profilingSettings);
-            selectedSettingsIndex = 0;
             updateItems();
         }
 
@@ -194,7 +193,7 @@ public class TaskPresenter implements TaskChooser.Item {
         }
 
         public void resetProfilingSettings() {
-            selectedSettingsIndex = -1;
+            selectedSettingsIndex = 0;
             profilingSettings.clear();
         }
 
