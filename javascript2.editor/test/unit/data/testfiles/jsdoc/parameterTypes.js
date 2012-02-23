a76ec8c7949e8f467898cdadedca1f918308ab75
Martin Fousek <marfous@netbeans.org>
2012-02-23 17:05:02 +0100

    Included missing file for unit testing

---
+++ javascript2.editor/test/unit/data/testfiles/jsdoc/parameterTypes.js
@@ -0,0 +1,24 @@
+/**
+ * @param {String} userName
+ */
+function line1(userName){}
+
+/**
+ * @param {String|Number} product
+ */
+function line2(product){}
+
+/**
+ * @param {String} [accessLevel] accessLevel is optional
+ */
+function line3(accessLevel){}
+
+/**
+ * @param {String} [accessLevel="author"] accessLevel is optional
+ */
+function line4(accessLevel){}
+
+/**
+ * @param accessLevel
+ */
+function line5(accessLevel){}
