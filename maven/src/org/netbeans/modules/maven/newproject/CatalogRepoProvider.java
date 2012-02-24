065963687c1a4b3aa1fba8dcedfb042ba6a0d84a
Milos Kleint <mkleint@netbeans.org>
2012-02-24 12:48:14 +0100

    #206644 instead of requiring indexes to be present locally to retrieve the archetypes, rely on the archetype catalogs on servers to achieve the same and only download the catalog. Cached locally by netbeans, updated daily (if the archetype wizard is used)

--- maven/src/org/netbeans/modules/maven/newproject/CatalogRepoProvider.java
+++ maven/src/org/netbeans/modules/maven/newproject/CatalogRepoProvider.java
@@ -44,14 +44,35 @@ package org.netbeans.modules.maven.newproject;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.repository.ArtifactRepository;
+import org.apache.maven.artifact.repository.ArtifactRepositoryFactory;
+import org.apache.maven.artifact.repository.ArtifactRepositoryPolicy;
+import org.apache.maven.artifact.repository.layout.ArtifactRepositoryLayout;
+import org.apache.maven.repository.MirrorSelector;
 import org.apache.maven.repository.RepositorySystem;
+import org.apache.maven.settings.Mirror;
+import org.apache.maven.settings.Proxy;
+import org.apache.maven.settings.Server;
+import org.apache.maven.settings.crypto.DefaultSettingsDecryptionRequest;
+import org.apache.maven.settings.crypto.SettingsDecrypter;
+import org.apache.maven.settings.crypto.SettingsDecryptionResult;
+import org.apache.maven.wagon.ConnectionException;
+import org.apache.maven.wagon.Wagon;
+import org.apache.maven.wagon.WagonException;
+import org.apache.maven.wagon.authentication.AuthenticationException;
+import org.apache.maven.wagon.authentication.AuthenticationInfo;
+import org.apache.maven.wagon.proxy.ProxyInfo;
+import org.apache.maven.wagon.repository.Repository;
+import org.codehaus.plexus.component.repository.exception.ComponentLookupException;
+import org.codehaus.plexus.util.FileUtils;
 import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.JDOMException;
@@ -59,10 +80,11 @@ import org.jdom.Namespace;
 import org.jdom.input.SAXBuilder;
 import org.netbeans.modules.maven.api.archetype.Archetype;
 import org.netbeans.modules.maven.api.archetype.ArchetypeProvider;
-import org.netbeans.modules.maven.indexer.api.NBVersionInfo;
+import org.netbeans.modules.maven.embedder.EmbedderFactory;
+import org.netbeans.modules.maven.embedder.MavenEmbedder;
+import org.netbeans.modules.maven.indexer.api.RepositoryInfo;
 import org.netbeans.modules.maven.indexer.api.RepositoryPreferences;
-import org.netbeans.modules.maven.indexer.api.RepositoryQueries;
-import org.netbeans.modules.maven.indexer.api.RepositoryUtil;
+import org.openide.modules.Places;
 import org.openide.util.NbCollections;
 import org.openide.util.lookup.ServiceProvider;
 
@@ -89,9 +111,17 @@ public abstract class CatalogRepoProvider implements ArchetypeProvider {
     protected abstract String repository();
 
     public @Override List<Archetype> getArchetypes() {
+        try {
+            return getArchetypes(file(), repository());
+        } catch (IOException ex) {
+            LOG.log(Level.INFO, null, ex);
+            return Collections.emptyList();
+        }
+    }
+    
+    static List<Archetype> getArchetypes(URL file, String repository) {
         List<Archetype> toRet = new ArrayList<Archetype>();
         try {
-            URL file = file();
             if (file == null) {
                 return toRet;
             }
@@ -115,7 +145,7 @@ public abstract class CatalogRepoProvider implements ArchetypeProvider {
                         if (repo != null) {
                             archetype.setRepository(repo);
                         } else {
-                            archetype.setRepository(repository());
+                            archetype.setRepository(repository);
                         }
                         if (desc != null) {
                             archetype.setDescription(desc);
@@ -136,7 +166,7 @@ public abstract class CatalogRepoProvider implements ArchetypeProvider {
     public static class LocalCatalogRepoProvider extends CatalogRepoProvider {
 
         @Override protected URL file() throws IOException {
-            File f = new File(RepositorySystem.userMavenConfigurationHome, "archetype-catalog.xml");
+            File f = new File(RepositorySystem.userMavenConfigurationHome, "archetype-catalog.xml"); //NOI18N
             return f.isFile() ? f.toURI().toURL() : null;
         }
 
@@ -146,28 +176,153 @@ public abstract class CatalogRepoProvider implements ArchetypeProvider {
 
     }
 
-    @ServiceProvider(service=ArchetypeProvider.class, position=200)
-    public static class DefaultCatalogRepoProvider extends CatalogRepoProvider {
+    // one day period for updating downloaded/cached archetype catalogs
+    private static final long ARCHETYPE_TIMEOUT = 1000 * 60 * 60 * 24; // * 7;
+    
+    @ServiceProvider(service=ArchetypeProvider.class, position=300)
+    public static class RemoteCatalogProvider implements ArchetypeProvider {
 
-        @Override protected URL file() throws IOException {
-            List<NBVersionInfo> versions = RepositoryQueries.getVersions("org.apache.maven.archetype", "archetype-common", Collections.singletonList(RepositoryPreferences.getInstance().getLocalRepository())); // NOI18N
-            if (versions.isEmpty()) {
-                return null;
+        @Override
+        public List<Archetype> getArchetypes() {
+            File root = Places.getCacheSubdirectory("mavenarchetypes"); //NOI18N
+            ArrayList<Archetype> toRet = new ArrayList<Archetype>();
+            MavenEmbedder embedder = EmbedderFactory.getOnlineEmbedder();
+            SettingsDecryptionResult settings = embedder.lookupComponent(SettingsDecrypter.class).decrypt(new DefaultSettingsDecryptionRequest(embedder.getSettings()));
+            
+            for (RepositoryInfo info : RepositoryPreferences.getInstance().getRepositoryInfos()) {
+                if (info.isRemoteDownloadable()) {
+                    File catalog = new File(new File( root, info.getId()), "archetype-catalog.xml"); //NOI18N
+                    boolean download = false;
+                    if (!catalog.exists()) {
+                        download = true;
+                    } else {
+                        long lastM = catalog.lastModified();
+                        if (lastM == 0) {
+                            download = true;
+                        } else if (lastM - System.currentTimeMillis() > ARCHETYPE_TIMEOUT) {
+                            download = true;
+                        }
+                    }
+                    
+                    if (download) {
+                        download(info.getId(), info.getRepositoryUrl(), catalog, settings, embedder);
+                    }
+                    
+                    if (catalog.exists()) {
+                        try {
+                            toRet.addAll(CatalogRepoProvider.getArchetypes(catalog.toURI().toURL(), info.getRepositoryUrl()));
+                        } catch (MalformedURLException ex) {
+                            LOG.log(Level.INFO, null, ex);
+                        }
+                    }
+                }
             }
-            // any need to sort?
-            NBVersionInfo newest = versions.get(0);
-            Artifact art = RepositoryUtil.createArtifact(newest);
-            File jar = art.getFile();
-            if (!jar.isFile()) {
-                return null;
-            }
-            return new URL("jar:" + jar.toURI() + "!/archetype-catalog.xml"); // NOI18N
+            
+            return toRet;
         }
 
-        @Override protected String repository() {
-            return null;
-        }
+        private void download(String id, String url, File catalog, SettingsDecryptionResult settings, MavenEmbedder embedder) {
+            String baseurl = url;
+            String repoId = id;
+            try {
+                catalog.getParentFile().mkdirs();
+                AuthenticationInfo wagonAuth = null;
+//        //this comes from maven-compat
+                MirrorSelector mrri = embedder.getPlexus().lookup(MirrorSelector.class);
 
+                // handle mirroring
+                ArtifactRepositoryLayout layout = (ArtifactRepositoryLayout) embedder.getPlexus().lookup(ArtifactRepositoryLayout.ROLE, "default"); //NOI18N
+                ArtifactRepositoryFactory arf = embedder.getPlexus().lookup(ArtifactRepositoryFactory.class);
+
+                //what is the update policy?
+                ArtifactRepositoryPolicy policy = new ArtifactRepositoryPolicy(true, ArtifactRepositoryPolicy.UPDATE_POLICY_DAILY, ArtifactRepositoryPolicy.UPDATE_POLICY_DAILY);
+                ArtifactRepository art = arf.createArtifactRepository(repoId, baseurl, layout, policy, policy);
+                Mirror mirr = mrri.getMirror(art, embedder.getSettings().getMirrors());
+                if (mirr != null) {
+                    baseurl = mirr.getUrl();
+                    repoId = mirr.getId();
+                }
+
+                for (Server server : settings.getServers()) {
+                    if (repoId.equals(server.getId())) {
+                        wagonAuth = new AuthenticationInfo();
+                        wagonAuth.setUserName(server.getUsername());
+                        wagonAuth.setPassword(server.getPassword());
+                        wagonAuth.setPassphrase(server.getPassphrase());
+                        wagonAuth.setPrivateKey(server.getPrivateKey());
+                        break;
+                    }
+                }
+                String protocol = URI.create(baseurl).getScheme();
+                ProxyInfo wagonProxy = null;
+                for (Proxy proxy : settings.getProxies()) {
+                    if (proxy.isActive()) {
+                        wagonProxy = new ProxyInfo();
+                        wagonProxy.setHost(proxy.getHost());
+                        wagonProxy.setPort(proxy.getPort());
+                        wagonProxy.setNonProxyHosts(proxy.getNonProxyHosts());
+                        wagonProxy.setUserName(proxy.getUsername());
+                        wagonProxy.setPassword(proxy.getPassword());
+                        wagonProxy.setType(protocol);
+                        break;
+                    }
+                }
+                Wagon wagon = embedder.getPlexus().lookup(Wagon.class, protocol);
+                assert wagon != null;
+
+                Repository repository = new Repository(repoId, baseurl);
+
+                try {
+
+                    // when working in the context of Maven, the WagonManager is already
+                    // populated with proxy information from the Maven environment
+
+                    if (wagonAuth != null) {
+                        if (wagonProxy != null) {
+                            wagon.connect(repository, wagonAuth, wagonProxy);
+                        } else {
+                            wagon.connect(repository, wagonAuth);
+                        }
+                    } else {
+                        if (wagonProxy != null) {
+                            wagon.connect(repository, wagonProxy);
+                        } else {
+                            wagon.connect(repository);
+                        }
+                    }
+
+                    File temp = File.createTempFile("maven", "catalog"); //NOI18N
+                    try {
+                        wagon.get("archetype-catalog.xml", temp); //NOI18N
+                        //only overwrite the old file or create file if the content is there.
+                        if (temp.exists() && temp.length() > 0) {
+                            FileUtils.copyFile(temp, catalog);
+                            temp.delete();
+                        }
+                    } finally {
+                        wagon.disconnect();
+                    }
+
+                } catch (AuthenticationException ex) {
+                    String msg = "Authentication exception connecting to " + repository;  //NOI18N
+                    throw new IOException(msg, ex);
+                } catch (WagonException ex) {
+                    String msg = "Wagon exception connecting to " + repository;  //NOI18N
+                    throw new IOException(msg, ex);
+                } finally {
+                    try {
+                        wagon.disconnect();
+                    } catch (ConnectionException ex) {
+                        String msg = "Wagon exception disconnecting from " + repository; //NOI18N
+                        throw new IOException(msg, ex);
+                    }
+                }
+            } catch (ComponentLookupException ex) {
+                LOG.log(Level.FINE, null, ex);
+            } catch (IOException io) {
+                LOG.log(Level.INFO, null, io);
+            }
+
+        }
     }
-
 }
