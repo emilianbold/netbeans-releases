0de8e0e36f9f226a48c72f4ef7591f4b7f091048
Denis Anisimov <ads@netbeans.org>
2012-02-28 16:26:31 +0400

    Chrome plugin installation check code is implemented. Installation code is following.

--- extbrowser/src/org/netbeans/modules/extbrowser/ExtBrowserImpl.java
+++ extbrowser/src/org/netbeans/modules/extbrowser/ExtBrowserImpl.java
@@ -163,6 +163,9 @@ public abstract class ExtBrowserImpl extends HtmlBrowser.Impl {
         if ( extBrowserFactory instanceof FirefoxBrowser ){
             return BrowserId.FIREFOX;
         }
+        /*else if ( extBrowserFactory instanceof ChromeBrowser ){
+            return BrowserId.CHROME;
+        }*/
         return null;
     }
     
