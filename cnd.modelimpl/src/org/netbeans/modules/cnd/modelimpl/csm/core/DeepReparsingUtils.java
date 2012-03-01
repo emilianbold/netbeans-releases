d3bae33837923e6f287f92c25559e5e5bb130f64
Vladimir Voskresensky <vv159170@netbeans.org>
2012-03-01 21:13:23 +0400

    fixing #187567 [code model] Support for incremental+partial parsing -- introduce file signature

--- cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/csm/core/DeepReparsingUtils.java
+++ cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/csm/core/DeepReparsingUtils.java
@@ -61,6 +61,7 @@ import org.netbeans.modules.cnd.api.model.services.CsmFileInfoQuery;
 import org.netbeans.modules.cnd.api.project.NativeFileItem;
 import org.netbeans.modules.cnd.apt.support.APTDriver;
 import org.netbeans.modules.cnd.apt.support.APTPreprocHandler;
+import org.netbeans.modules.cnd.modelimpl.content.file.FileContentSignature;
 import org.netbeans.modules.cnd.modelimpl.content.project.GraphContainer.ParentFiles;
 import org.netbeans.modules.cnd.modelimpl.debug.DiagnosticExceptoins;
 import org.netbeans.modules.cnd.modelimpl.debug.TraceFlags;
@@ -98,8 +99,11 @@ public final class DeepReparsingUtils {
             LOG.log(Level.INFO, "OPTIMISTIC partialReparseOnChangedFile {0}", fileImpl.getAbsolutePath());
             project.markAsParsingPreprocStates(fileImpl.getAbsolutePath());
             fileImpl.markReparseNeeded(false);
+            FileContentSignature prevSig = fileImpl.getSignature();
             ParserQueue.instance().add(fileImpl, Collections.singleton(FileImpl.DUMMY_STATE),
-                    ParserQueue.Position.HEAD, false, ParserQueue.FileAction.NOTHING);
+                    ParserQueue.Position.IMMEDIATE, false, ParserQueue.FileAction.NOTHING);
+            FileContentSignature newSig = fileImpl.getSignature();
+            LOG.log(Level.INFO, "check signatures for {0} : {1}", new Object[]{fileImpl.getAbsolutePath(), newSig.equals(prevSig)});
             return true;
         }
         return false;
