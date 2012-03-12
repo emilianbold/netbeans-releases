f18ba145035219f5701c76b919c16d44bff95784
igor_nikiforov <igor_nikiforov@netbeans.org>
2012-03-12 16:10:22 +0400

    Bug 207619 "Remote FS should pass FS TCK" partial fix.

--- dlight.remote.impl/test/unit/src/org/netbeans/modules/remote/test/RemoteFSTCKTest.java
+++ dlight.remote.impl/test/unit/src/org/netbeans/modules/remote/test/RemoteFSTCKTest.java
@@ -65,25 +65,13 @@ public class RemoteFSTCKTest extends RemoteFSTCKTestCase {
     public static Test suite() {
         NbTestSuite suite = new NbTestSuite();
         suite.addTestSuite(FileSystemTestHid.class);
-        suite.addTestSuite(FileObjectTestHid_.class);
+        suite.addTestSuite(FileObjectTestHid.class);
         // it seems AttributesTestHidden does not belong to FS TCK
         //suite.addTestSuite(AttributesTestHidden.class);
         suite.addTestSuite(URLMapperTestHidden.class);
         suite.addTestSuite(FileUtilTestHidden.class);
         suite.addTestSuite(TempFileObjectTestHid.class);
         return new RemoteFSTCKTest(suite);
-    }
-    
-    public static class FileObjectTestHid_ extends FileObjectTestHid {
+    }    
 
-        public FileObjectTestHid_(String testName) {
-            super(testName);
-        }
-
-        @RandomlyFails
-        @Override
-        public void testBigFileAndAsString() throws Exception {
-            super.testBigFileAndAsString();
-        }
-    }
 }
