17d448502cda7eaae9c1e7c862a996d19838ff9a
Martin Fousek <marfous@netbeans.org>
2012-03-19 13:53:51 +0100

    Refactoring

--- web.jsf.richfaces/src/org/netbeans/modules/web/jsf/richfaces/Richfaces4Implementation.java
+++ web.jsf.richfaces/src/org/netbeans/modules/web/jsf/richfaces/Richfaces4Implementation.java
@@ -44,15 +44,7 @@ package org.netbeans.modules.web.jsf.richfaces;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.EnumSet;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
+import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.prefs.Preferences;
@@ -91,25 +83,23 @@ public class Richfaces4Implementation implements JsfComponentImplementation {
     private static final String richfacesUiPom ="https://repository.jboss.org/nexus/content/groups/public-jboss/org/richfaces/ui/richfaces-components-ui/4.2.0.Final/richfaces-components-ui-4.2.0.Final.pom";
     private static final String richfacesCorePom ="https://repository.jboss.org/nexus/content/groups/public-jboss/org/richfaces/core/richfaces-core-impl/4.2.0.Final/richfaces-core-impl-4.2.0.Final.pom";
 
-    public static final Map<String, String> RF_LIBRARIES = new HashMap<String, String>();
+    public static final Set<String> RF_LIBRARIES = new HashSet<String>();
     public static final Map<String, String> RF_DEPENDENCIES = new HashMap<String, String>();
 
     public static final String PREF_RICHFACES_NODE = "richfaces"; //NOI18N
     public static final String PREF_RICHFACES_LIBRARY = "base-library"; //NOI18N
 
+    static {
+        RF_LIBRARIES.add("org.richfaces.application.Module"); //NOI18N
+        RF_LIBRARIES.add("org.richfaces.application.ServiceLoader"); //NOI18N
+        RF_LIBRARIES.add("org.richfaces.el.ValueDescriptor"); //NOI18N
+        RF_LIBRARIES.add("org.richfaces.el.ValueReference"); //NOI18N
+        RF_DEPENDENCIES.put("com.google.common.base.Functions", "guava.jar"); //NOI18N
+        RF_DEPENDENCIES.put("org.w3c.css.sac.Parser", "sac.jar"); //NOI18N
+        RF_DEPENDENCIES.put("com.steadystate.css.parser.ParseException", "cssparser.jar"); //NOI18N
+    }
 
     public Richfaces4Implementation() {
-        if (RF_LIBRARIES.isEmpty()) {
-            RF_LIBRARIES.put("org.richfaces.application.Module", "richfaces-core-api-4.0.0.Final.jar"); //NOI18N
-            RF_LIBRARIES.put("org.richfaces.application.ServiceLoader", "richfaces-core-impl-4.0.0.Final.jar"); //NOI18N
-            RF_LIBRARIES.put("org.richfaces.el.ValueDescriptor", "richfaces-components-api-4.0.0.Final.jar"); //NOI18N
-            RF_LIBRARIES.put("org.richfaces.el.ValueReference", "richfaces-components-ui-4.0.0.Final.jar"); //NOI18N
-        }
-        if (RF_DEPENDENCIES.isEmpty()) {
-            RF_DEPENDENCIES.put("com.google.common.base.Functions", "guava.jar"); //NOI18N
-            RF_DEPENDENCIES.put("org.w3c.css.sac.Parser", "sac.jar"); //NOI18N
-            RF_DEPENDENCIES.put("com.steadystate.css.parser.ParseException", "cssparser.jar"); //NOI18N
-        }
     }
 
     @Override
@@ -233,9 +223,10 @@ public class Richfaces4Implementation implements JsfComponentImplementation {
     @Override
     public boolean isInWebModule(org.netbeans.modules.web.api.webmodule.WebModule webModule) {
         ClassPath classpath = ClassPath.getClassPath(webModule.getDocumentBase(), ClassPath.COMPILE);
-        Set<Entry<String, String>> entrySet = RF_LIBRARIES.entrySet();
-        for (Entry<String, String> entry : entrySet) {
-            if (classpath.findResource(entry.getKey().replace('.', '/') + ".class") == null) { //NOI18N
+        Iterator<String> iterator = RF_LIBRARIES.iterator();
+        while (iterator.hasNext()) {
+            String libraryName = iterator.next();
+            if (classpath.findResource(libraryName.replace('.', '/') + ".class") == null) { //NOI18N
                 return false;
             }
         }
