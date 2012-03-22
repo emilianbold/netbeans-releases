88f33d60ea0a7c58d16fc415b34de0ca6bd2fefc
Jaroslav Havlin <jhavlin@netbeans.org>
2012-03-22 13:28:28 +0100

    IgnoreListPanel uses JTable

--- api.search/src/org/netbeans/modules/search/IgnoreListPanel.java
+++ api.search/src/org/netbeans/modules/search/IgnoreListPanel.java
@@ -11,14 +11,16 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.regex.Pattern;
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.AbstractTableModel;
 import org.netbeans.api.search.RegexpUtil;
 import org.netbeans.api.search.SearchScopeOptions;
+import org.netbeans.modules.search.ui.UiUtils;
 import org.openide.awt.Mnemonics;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
-import org.openide.util.Exceptions;
 import org.openide.util.NbBundle;
-
 
 /**
  *
@@ -26,9 +28,6 @@ import org.openide.util.NbBundle;
  */
 public final class IgnoreListPanel extends javax.swing.JPanel {
 
-    static final String SIMPLE_PREFIX = "s: ";                          //NOI18N
-    static final String REGEXP_PREFIX = "x: ";                          //NOI18N
-    static final String FILE_PREFIX = "f: ";                            //NOI18N
     private IgnoredListModel ignoreListModel;
     private JFileChooser jFileChooser;
 
@@ -40,19 +39,25 @@ public final class IgnoreListPanel extends javax.swing.JPanel {
         initComponents();
         setMnemonics();
         updateEnabledButtons();
+        table.getSelectionModel().addListSelectionListener(
+                new ListSelectionListener() {
+                    @Override
+                    public void valueChanged(ListSelectionEvent e) {
+                        updateEnabledButtons();
+                    }
+                });
     }
 
     private void updateEnabledButtons() {
-        int cnt = list.getSelectedIndices().length;
+        int cnt = table.getSelectedRows().length;
         btnDelete.setEnabled(cnt > 0);
 
         boolean editable = false;
-        if (cnt == 1 && list.getSelectedValue() != null) {
-            String value = list.getSelectedValue().toString();
-            if (value.startsWith(SIMPLE_PREFIX)
-                    || value.startsWith(REGEXP_PREFIX)) {
-                editable = true;
-            }
+        int index = table.getSelectedRow();
+        if (cnt == 1 && index >= 0) {
+            IgnoreListItem ili = ignoreListModel.list.get(index);
+            editable = ili.type == ItemType.PATTERN
+                    || ili.type == ItemType.REGEXP;
         }
         btnEdit.setEnabled(editable);
     }
@@ -71,8 +76,8 @@ public final class IgnoreListPanel extends javax.swing.JPanel {
         btnDelete = new javax.swing.JButton();
         btnClose = new javax.swing.JButton();
         btnEdit = new javax.swing.JButton();
-        jScrollPane2 = new javax.swing.JScrollPane();
-        list = new javax.swing.JList();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        table = new javax.swing.JTable();
 
         btnBrowse.setText("Add Folder...");
         btnBrowse.addActionListener(new java.awt.event.ActionListener() {
@@ -109,13 +114,8 @@ public final class IgnoreListPanel extends javax.swing.JPanel {
             }
         });
 
-        list.setModel(ignoreListModel);
-        list.addListSelectionListener(new javax.swing.event.ListSelectionListener() {
-            public void valueChanged(javax.swing.event.ListSelectionEvent evt) {
-                listValueChanged(evt);
-            }
-        });
-        jScrollPane2.setViewportView(list);
+        table.setModel(ignoreListModel);
+        jScrollPane1.setViewportView(table);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -123,8 +123,8 @@ public final class IgnoreListPanel extends javax.swing.JPanel {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 288, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 331, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(btnDelete, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addComponent(btnEdit, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
@@ -138,7 +138,6 @@ public final class IgnoreListPanel extends javax.swing.JPanel {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane2)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(btnBrowse, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -148,18 +147,16 @@ public final class IgnoreListPanel extends javax.swing.JPanel {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(btnDelete)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 53, Short.MAX_VALUE)
-                        .addComponent(btnClose)))
+                        .addComponent(btnClose))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
                 .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
 
-    private void listValueChanged(javax.swing.event.ListSelectionEvent evt) {//GEN-FIRST:event_listValueChanged
-        updateEnabledButtons();
-    }//GEN-LAST:event_listValueChanged
-
     private void btnDeleteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnDeleteActionPerformed
-        for (Object o : list.getSelectedValues()) {
-            ignoreListModel.remove(o);
+        for (int i : table.getSelectedRows()) {
+            IgnoreListItem ili = ignoreListModel.list.get(i);
+            ignoreListModel.remove(ili);
         }
     }//GEN-LAST:event_btnDeleteActionPerformed
 
@@ -178,7 +175,7 @@ public final class IgnoreListPanel extends javax.swing.JPanel {
                     JFileChooser.FILES_AND_DIRECTORIES);
             jFileChooser.setMultiSelectionEnabled(true);
         }
-        int showOpenDialog = jFileChooser.showOpenDialog(list);
+        int showOpenDialog = jFileChooser.showOpenDialog(table);
         if (showOpenDialog == JFileChooser.APPROVE_OPTION) {
             File[] selected = jFileChooser.getSelectedFiles();
             if (selected != null) {
@@ -202,14 +199,14 @@ public final class IgnoreListPanel extends javax.swing.JPanel {
     }//GEN-LAST:event_btnPatternActionPerformed
 
     private void btnEditActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEditActionPerformed
-        final String val = list.getSelectedValue().toString();
-        boolean regex = val.startsWith(REGEXP_PREFIX);
+        final IgnoreListItem ili =
+                ignoreListModel.list.get(table.getSelectedRow());
+        boolean regex = ili.type == ItemType.REGEXP;
         PatternSandbox.openDialog(new PatternSandbox.PathPatternComposer(
-                val.substring(REGEXP_PREFIX.length()), regex) {
-
+                ili.value, regex) {
             @Override
             protected void onApply(String pattern, boolean regexp) {
-                ignoreListModel.remove(val);
+                ignoreListModel.remove(ili);
                 ignoreListModel.addPattern(pattern, regexp);
             }
         }, btnEdit);
@@ -220,8 +217,8 @@ public final class IgnoreListPanel extends javax.swing.JPanel {
     private javax.swing.JButton btnDelete;
     private javax.swing.JButton btnEdit;
     private javax.swing.JButton btnPattern;
-    private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JList list;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable table;
     // End of variables declaration//GEN-END:variables
 
     public static void main(String[] args) {
@@ -250,20 +247,103 @@ public final class IgnoreListPanel extends javax.swing.JPanel {
         return NbBundle.getMessage(IgnoreListPanel.class, key);
     }
 
-    class IgnoredListModel extends AbstractListModel {
+    enum ItemType {
 
-        List<String> list;
+        FILE("f: "), FOLDER("f: "), REGEXP("x: "),
+        PATTERN("s: "), INVALID("i: ");
+        private String typePrefix;
+
+        private ItemType(String typePrefix) {
+            this.typePrefix = typePrefix;
+        }
+
+        public String getTypePrefix() {
+            return typePrefix;
+        }
+
+        public boolean isTypeOf(String s) {
+            return s.startsWith(typePrefix);
+        }
+    }
+
+    static class IgnoreListItem {
+
+        private ItemType type;
+        private String value;
+
+        private IgnoreListItem(String string) {
+            if (ItemType.PATTERN.isTypeOf(string)) {
+                this.type = ItemType.PATTERN;
+                this.value = string.substring(3);
+            } else if (ItemType.REGEXP.isTypeOf(string)) {
+                this.type = ItemType.REGEXP;
+                this.value = string.substring(3);
+            } else if (ItemType.FILE.isTypeOf(string)) {
+                String path = string.substring(3);
+                File f = new File(path);
+                if (!f.exists()) {
+                    this.type = ItemType.INVALID;
+                    return;
+                } else if (f.isDirectory()) {
+                    this.type = ItemType.FOLDER;
+                } else {
+                    this.type = ItemType.FILE;
+                }
+                this.value = path;
+            } else {
+                type = ItemType.INVALID;
+                return;
+            }
+        }
+
+        private IgnoreListItem(ItemType type, String value) {
+            this.type = type;
+            this.value = value;
+        }
+
+        public String toString() {
+            return type.getTypePrefix() + value;
+        }
+
+        static IgnoreListItem fromString(String string) {
+            return new IgnoreListItem(string);
+        }
+
+        static IgnoreListItem forFile(File f) {
+            ItemType type;
+            if (!f.exists()) {
+                type = ItemType.INVALID;
+            } else if (f.isDirectory()) {
+                type = ItemType.FOLDER;
+            } else {
+                type = ItemType.FILE;
+            }
+            return new IgnoreListItem(type, f.getAbsolutePath());
+        }
+
+        static IgnoreListItem forRegexp(String regexp) {
+            return new IgnoreListItem(ItemType.REGEXP, regexp);
+        }
+
+        static IgnoreListItem forPattern(String pattern) {
+            return new IgnoreListItem(ItemType.PATTERN, pattern);
+        }
+    }
+
+    class IgnoredListModel extends AbstractTableModel {
+
+        List<IgnoreListItem> list;
 
         public void remove(Object o) {
             int index = list.indexOf(o);
             list.remove(index);
-            this.fireIntervalRemoved(this, index, index + 1);
+            fireTableRowsDeleted(index, index);
             persist();
         }
 
         public void addFile(File f) {
-            list.add(FILE_PREFIX + f.getAbsolutePath());
-            fireIntervalAdded(this, list.size() - 1, list.size());
+            list.add(IgnoreListItem.forFile(f));
+            fireTableRowsInserted(list.size() - 1, list.size());
             persist();
         }
 
@@ -276,37 +356,80 @@ public final class IgnoreListPanel extends javax.swing.JPanel {
         }
 
         public void addSimplePatter(String p) {
-            list.add(SIMPLE_PREFIX + p);
-            fireIntervalAdded(this, list.size() - 1, list.size());
+            list.add(IgnoreListItem.forPattern(p));
+            fireTableRowsInserted(list.size() - 1, list.size());
             persist();
         }
 
         public void addRegularExpression(String x) {
-            list.add(REGEXP_PREFIX + x);
-            fireIntervalAdded(this, list.size() - 1, list.size());
+            list.add(IgnoreListItem.forRegexp(x));
+            fireTableRowsInserted(list.size() - 1, list.size());
             persist();
         }
 
         public IgnoredListModel() {
             List<String> orig = FindDialogMemory.getDefault().getIgnoreList();
-            list = new ArrayList<String>(orig.size());
-            list.addAll(orig);
-        }
-
-        @Override
-        public int getSize() {
-            return list.size();
-        }
-
-        @Override
-        public String getElementAt(int index) {
-            return list.get(index);
+            list = new ArrayList<IgnoreListItem>(orig.size());
+            for (String s : orig) {
+                IgnoreListItem ili = IgnoreListItem.fromString(s);
+                if (ili.type != ItemType.INVALID) {
+                    list.add(ili);
+                }
+            }
         }
 
         public void persist() {
             List<String> copy = new ArrayList<String>(list.size());
-            copy.addAll(list);
+            for (IgnoreListItem ili : list) {
+                copy.add(ili.toString());
+            }
             FindDialogMemory.getDefault().setIgnoreList(copy);
+        }
+
+        @Override
+        public int getRowCount() {
+            return list.size();
+        }
+
+        @Override
+        public int getColumnCount() {
+            return 2;
+        }
+
+        @Override
+        public Object getValueAt(int rowIndex, int columnIndex) {
+            IgnoreListItem ili = list.get(rowIndex);
+            if (columnIndex == 0) {
+                String msg;
+                switch (ili.type) {
+                    case FILE:
+                        msg = "IgnoreListPanel.type.file";              //NOI18N
+                        break;
+                    case FOLDER:
+                        msg = "IgnoreListPanel.type.folder";            //NOI18N
+                        break;
+                    case PATTERN:
+                        msg = "IgnoreListPanel.type.pattern";           //NOI18N
+                        break;
+                    case REGEXP:
+                        msg = "IgnoreListPanel.type.regexp";            //NOI18N
+                        break;
+                    default:
+                        msg = "IgnoreListPanel.type.invalid";           //NOI18N
+                }
+                return UiUtils.getText(msg);
+            } else {
+                return ili.value;
+            }
+        }
+
+        @Override
+        public String getColumnName(int column) {
+            if (column == 0) {
+                return UiUtils.getText("IgnoreListPanel.item.type");    //NOI18N
+            } else {
+                return UiUtils.getText("IgnoreListPanel.item.value");   //NOI18N
+            }
         }
     }
 
@@ -333,19 +456,18 @@ public final class IgnoreListPanel extends javax.swing.JPanel {
         public IgnoreListManager(List<String> ignoreList) {
             items = new LinkedList<IgnoredItemDefinition>();
             for (String s : ignoreList) {
-                try {
-                    if (s.startsWith(SIMPLE_PREFIX)) {
-                        String p = s.substring(SIMPLE_PREFIX.length());
-                        items.add(new IgnoredPatternDefinition(p));
-                    } else if (s.startsWith(REGEXP_PREFIX)) {
-                        String x = s.substring(REGEXP_PREFIX.length());
-                        items.add(new IgnoredRegexpDefinition(x));
-                    } else if (s.startsWith(FILE_PREFIX)) {
-                        String f = s.substring(FILE_PREFIX.length());
-                        items.add(new IgnoredDirDefinition(f));
-                    }
-                } catch (Exception e) {
-                    Exceptions.printStackTrace(e);
+                IgnoreListItem ili = IgnoreListItem.fromString(s);
+                switch (ili.type) {
+                    case PATTERN:
+                        items.add(new IgnoredPatternDefinition(ili.value));
+                        break;
+                    case REGEXP:
+                        items.add(new IgnoredRegexpDefinition(ili.value));
+                        break;
+                    case FOLDER:
+                    case FILE:
+                        items.add(new IgnoredDirDefinition(ili.value));
+                        break;
                 }
             }
         }
@@ -403,7 +525,7 @@ public final class IgnoreListPanel extends javax.swing.JPanel {
 
             @Override
             boolean isIgnored(FileObject obj) {
-                return FileUtil.isParentOf(dir, obj);
+                return FileUtil.isParentOf(dir, obj) || obj.equals(dir);
             }
         }
     }
