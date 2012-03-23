40a7cde55326199765778d31eb857bd78e08ff2c
Jiri Rechtacek <jrechtacek@netbeans.org>
2012-03-23 22:44:45 +0100

    just a detail

--- nbbuild/newbuild/build-nbi.sh
+++ nbbuild/newbuild/build-nbi.sh
@@ -158,7 +158,7 @@ if [ -z $DONT_SIGN_INSTALLER ]; then
 fi
 
 cd $DIST
-bash ${SCRIPTS_DIR}/files-info.sh bundles zip zip/moduleclusters
+bash ${SCRIPTS_DIR}/files-info.sh bundles bundles/jdk zip zip/moduleclusters
 ERROR_CODE=$?
 if [ $ERROR_CODE != 0 ]; then
     echo "ERROR: $ERROR_CODE - Counting of MD5 sums and size failed"
