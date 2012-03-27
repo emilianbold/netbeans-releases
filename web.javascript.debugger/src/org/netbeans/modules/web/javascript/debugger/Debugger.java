1737da8ec2ec9f39ca1ea7233b21e9cb7e7fae63
David Konecny <dkonecny@netbeans.org>
2012-03-28 06:29:50 +1300

    #210202 - Debugging does not work on Mac OS X

--- web.javascript.debugger/src/org/netbeans/modules/web/javascript/debugger/Debugger.java
+++ web.javascript.debugger/src/org/netbeans/modules/web/javascript/debugger/Debugger.java
@@ -149,7 +149,12 @@ public class Debugger {
             urlToDebug = reformatFileURL(urlToDebug);
             List<? extends WipBrowser.WipTabConnector> tabs = browser.getTabs(backend);
             for (WipBrowser.WipTabConnector tabConnector : tabs) {
-                if (tabConnector.getUrl().equals(urlToDebug)) {
+                String tabUrl = tabConnector.getUrl();
+                // #210202
+                if (tabUrl.startsWith("file://localhost/")) {
+                    tabUrl = tabUrl.replace("file://localhost/", "file:///");
+                }
+                if (tabUrl.equals(urlToDebug)) {
                     BrowserDebugger.log(Bundle.ConnectionEstablished());
                     listener = new TabListener();
                     debugListener = listener.getDebugEventListenerImpl();
