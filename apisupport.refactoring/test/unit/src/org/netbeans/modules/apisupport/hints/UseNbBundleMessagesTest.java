4f1ea236205e3b741223cf63039e471183992eea
Jesse Glick <jglick@netbeans.org>
2012-04-10 20:58:07 -0400

    Adding tests.

--- apisupport.refactoring/test/unit/src/org/netbeans/modules/apisupport/hints/UseNbBundleMessagesTest.java
+++ apisupport.refactoring/test/unit/src/org/netbeans/modules/apisupport/hints/UseNbBundleMessagesTest.java
@@ -64,6 +64,77 @@ public class UseNbBundleMessagesTest {
                 assertWarnings("3:41-3:51:warning:" + UseNbBundleMessages_error_text());
     }
 
+    @Test public void nonConstClass() throws Exception {
+        HintTest.create().classpath(cp()).
+                input("package test;\n" +
+                       "class Test {\n" +
+                       "    String m() {\n" +
+                       "        return org.openide.util.NbBundle.getMessage(getClass(), \"somekey\");\n" +
+                       "    }\n" +
+                       "}\n").
+                input("test/Bundle.properties", "somekey=text\n", false).
+                run(UseNbBundleMessages.class).
+                findWarning("3:41-3:51:warning:" + UseNbBundleMessages_only_class_const()).
+                assertFixes();
+    }
+
+    @Test public void wrongClass() throws Exception {
+        HintTest.create().classpath(cp()).
+                input("package test;\n" +
+                       "class Test {\n" +
+                       "    class Nested {\n" +
+                       "        String m() {\n" +
+                       "            return org.openide.util.NbBundle.getMessage(Nested.class, \"somekey\");\n" +
+                       "        }\n" +
+                       "    }\n" +
+                       "}\n").
+                input("test/Bundle.properties", "somekey=text\n", false).
+                run(UseNbBundleMessages.class).
+                findWarning("4:45-4:55:warning:" + UseNbBundleMessages_wrong_class_name("Test")).
+                assertFixes();
+    }
+
+    @Test public void nonConstKey() throws Exception {
+        HintTest.create().classpath(cp()).
+                input("package test;\n" +
+                       "class Test {\n" +
+                       "    String m(String which) {\n" +
+                       "        return org.openide.util.NbBundle.getMessage(Test.class, which + \"key\");\n" +
+                       "    }\n" +
+                       "}\n").
+                input("test/Bundle.properties", "somekey=text\n", false).
+                run(UseNbBundleMessages.class).
+                findWarning("3:41-3:51:warning:" + UseNbBundleMessages_only_string_const()).
+                assertFixes();
+    }
+
+    @Test public void noSuchBundle() throws Exception {
+        HintTest.create().classpath(cp()).
+                input("package test;\n" +
+                       "class Test {\n" +
+                       "    String m() {\n" +
+                       "        return org.openide.util.NbBundle.getMessage(Test.class, \"somekey\");\n" +
+                       "    }\n" +
+                       "}\n").
+                run(UseNbBundleMessages.class).
+                findWarning("3:41-3:51:warning:" + UseNbBundleMessages_no_such_bundle("test/Bundle.properties")).
+                assertFixes();
+    }
+
+    @Test public void noSuchKey() throws Exception {
+        HintTest.create().classpath(cp()).
+                input("package test;\n" +
+                       "class Test {\n" +
+                       "    String m() {\n" +
+                       "        return org.openide.util.NbBundle.getMessage(Test.class, \"otherkey\");\n" +
+                       "    }\n" +
+                       "}\n").
+                input("test/Bundle.properties", "somekey=text\n", false).
+                run(UseNbBundleMessages.class).
+                findWarning("3:41-3:51:warning:" + UseNbBundleMessages_no_such_key("otherkey")).
+                assertFixes();
+    }
+
     @Test public void simpleFix() throws Exception {
         HintTest.create().classpath(cp()).
                 input("package test;\n" +
@@ -90,6 +161,148 @@ public class UseNbBundleMessagesTest {
                        "    }\n" +
                        "}\n");
     }
+
+    @Test public void onConstructor() throws Exception {
+        HintTest.create().classpath(cp()).
+                input("package test;\n" +
+                       "class Test {\n" +
+                       "    Test() {\n" +
+                       "        String k = org.openide.util.NbBundle.getMessage(Test.class, \"k\");\n" +
+                       "    }\n" +
+                       "}\n").
+                input("test/Bundle.properties", "k=v\n", false).
+                run(UseNbBundleMessages.class).
+                findWarning("3:45-3:55:warning:" + UseNbBundleMessages_error_text()).
+                applyFix().
+                assertVerbatimOutput("test/Bundle.properties", "").
+                assertOutput("package test;\n" +
+                       "import org.openide.util.NbBundle.Messages;\n" +
+                       "import static test.Bundle.*;\n" +
+                       "class Test {\n" +
+                       "    @Messages(\"k=v\")\n" +
+                       "    Test() {\n" +
+                       "        String k = k();\n" +
+                       "    }\n" +
+                       "}\n");
+    }
+
+    @Test public void onField() throws Exception {
+        HintTest.create().classpath(cp()).
+                input("package test;\n" +
+                       "class Test {\n" +
+                       "    private static final String s = org.openide.util.NbBundle.getMessage(Test.class, \"k\");\n" +
+                       "}\n").
+                input("test/Bundle.properties", "k=v\n", false).
+                run(UseNbBundleMessages.class).
+                findWarning("2:62-2:72:warning:" + UseNbBundleMessages_error_text()).
+                applyFix().
+                assertVerbatimOutput("test/Bundle.properties", "").
+                assertOutput("package test;\n" +
+                       "import org.openide.util.NbBundle.Messages;\n" +
+                       "import static test.Bundle.*;\n" +
+                       "class Test {\n" +
+                       "    @Messages(\"k=v\")\n" +
+                       "    private static final String s = k();\n" +
+                       "}\n");
+    }
+
+    @Test public void onClass() throws Exception {
+        HintTest.create().classpath(cp()).
+                input("package test;\n" +
+                       "class Test {\n" +
+                       "    static {\n" +
+                       "        String k = org.openide.util.NbBundle.getMessage(Test.class, \"k\");\n" +
+                       "    }\n" +
+                       "}\n").
+                input("test/Bundle.properties", "k=v\n", false).
+                run(UseNbBundleMessages.class).
+                findWarning("3:45-3:55:warning:" + UseNbBundleMessages_error_text()).
+                applyFix().
+                assertVerbatimOutput("test/Bundle.properties", "").
+                assertOutput("package test;\n" +
+                       "import org.openide.util.NbBundle.Messages;\n" +
+                       "import static test.Bundle.*;\n" +
+                       "@Messages(\"k=v\")\n" +
+                       "class Test {\n" +
+                       "    static {\n" +
+                       "        String k = k();\n" +
+                       "    }\n" +
+                       "}\n");
+    }
+
+    @Test public void insideAnonymous() throws Exception {
+        HintTest.create().classpath(cp()).
+                input("package test;\n" +
+                       "class Test {\n" +
+                       "    Object o = new Object() {\n" +
+                       "        public String toString() {\n" +
+                       "            return org.openide.util.NbBundle.getMessage(Test.class, \"k\");\n" +
+                       "        }\n" +
+                       "    };\n" +
+                       "}\n").
+                input("test/Bundle.properties", "k=v\n", false).
+                run(UseNbBundleMessages.class).
+                findWarning("4:45-4:55:warning:" + UseNbBundleMessages_error_text()).
+                applyFix().
+                assertVerbatimOutput("test/Bundle.properties", "").
+                assertOutput("package test;\n" +
+                       "import org.openide.util.NbBundle.Messages;\n" +
+                       "import static test.Bundle.*;\n" +
+                       "class Test {\n" +
+                       "    @Messages(\"k=v\")\n" +
+                       "    Object o = new Object() {\n" +
+                       "        public String toString() {\n" +
+                       "            return k();\n" +
+                       "        }\n" +
+                       "    };\n" +
+                       "}\n");
+    }
+
+    @Test public void insideLocal() throws Exception {
+        HintTest.create().classpath(cp()).
+                input("package test;\n" +
+                       "class Test {\n" +
+                       "    void m() {\n" +
+                       "        class Local {\n" +
+                       "            class MoreLocal {\n" +
+                       "                String s = org.openide.util.NbBundle.getMessage(Test.class, \"k\");\n" +
+                       "            }\n" +
+                       "        }\n" +
+                       "    }\n" +
+                       "}\n").
+                input("test/Bundle.properties", "k=v\n", false).
+                run(UseNbBundleMessages.class).
+                findWarning("5:53-5:63:warning:" + UseNbBundleMessages_error_text()).
+                applyFix().
+                assertVerbatimOutput("test/Bundle.properties", "").
+                assertOutput("package test;\n" +
+                       "import org.openide.util.NbBundle.Messages;\n" +
+                       "import static test.Bundle.*;\n" +
+                       "class Test {\n" +
+                       "    @Messages(\"k=v\")\n" +
+                       "    void m() {\n" +
+                       "        class Local {\n" +
+                       "            class MoreLocal {\n" +
+                       "                String s = k();\n" +
+                       "            }\n" +
+                       "        }\n" +
+                       "    }\n" +
+                       "}\n");
+    }
+
+    // XXX to be tested:
+    // - @Something(displayName="#key") on class
+    // - @Something(displayName="#key") on method
+    // - @Something(displayName="#key") on package-info.java
+    // - Messages, Bundle already imported
+    // - add to @Messages("firstkey=...")
+    // - add to @Messages({"firstkey=...", "secondkey=..."})
+    // - some enclosing element already has definition, just need to use
+    // - message formats
+    // - comments preceding key in bundle
+    // - other keys in bundle left behind
+    // - unusual characters (not identifier chars) in key
+    // - unusual characters (\n etc.) in value
 
     /* XXX pending #209375
     @Test public void sequentialFixes() throws Exception {
