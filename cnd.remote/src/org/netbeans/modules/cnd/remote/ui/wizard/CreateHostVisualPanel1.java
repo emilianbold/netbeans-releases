50ad4b12333cdbb59d947961880f4f1431a905b0
Andrew Krasny <akrasny@netbeans.org>
2012-04-11 17:50:28 +0400

    Bug 210401 - Usability problems with "New Remote Build Host" wizard.

--- cnd.remote/src/org/netbeans/modules/cnd/remote/ui/wizard/CreateHostVisualPanel1.java
+++ cnd.remote/src/org/netbeans/modules/cnd/remote/ui/wizard/CreateHostVisualPanel1.java
@@ -42,86 +42,72 @@
 package org.netbeans.modules.cnd.remote.ui.wizard;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
+import javax.swing.Icon;
 import javax.swing.JPanel;
-import javax.swing.JTable;
 import javax.swing.SwingUtilities;
-import javax.swing.event.AncestorEvent;
-import javax.swing.event.AncestorListener;
+import javax.swing.UIManager;
 import javax.swing.event.ChangeListener;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
-import org.netbeans.api.progress.ProgressHandleFactory;
+import org.netbeans.modules.cnd.remote.ui.networkneighbour.NetworkNeighbourhoodList;
 import org.openide.util.NbBundle;
 
-/*package*/ final class CreateHostVisualPanel1 extends JPanel {
+/*
+ * package
+ */ final class CreateHostVisualPanel1 extends JPanel {
 
-    private final HostsListTableModel tableModel = new HostsListTableModel();
+    private final NetworkNeighbourhoodList networkNeighbourhoodList;
+    private final ChangeListener listener;
     private final CreateHostData data;
 
     public CreateHostVisualPanel1(CreateHostData data, final ChangeListener listener) {
         this.data = data;
+        this.listener = listener;
+
         initComponents();
         lblUser.setVisible(data.isManagingUser());
         textUser.setVisible(data.isManagingUser());
-        textPort.setText(Integer.toString(22));
-        textHostname.getDocument().addDocumentListener(new DocumentListener() {
 
-            @Override
-            public void insertUpdate(DocumentEvent e) {
-                listener.stateChanged(null);
-            }
+        proxySettingsPanel.setVisible(false);
+        cbProxySettings.setSelected(false);
+        cbProxySettings.setIcon(getCollapsedIcon());
 
-            @Override
-            public void removeUpdate(DocumentEvent e) {
-                listener.stateChanged(null);
-            }
+        networkNeighbourhoodList = new NetworkNeighbourhoodList();
+        serversListPanel.add(networkNeighbourhoodList, BorderLayout.CENTER);
+        hostSelectionPanel.attach(networkNeighbourhoodList);
 
-            @Override
-            public void changedUpdate(DocumentEvent e) {
-                listener.stateChanged(null);
-            }
-        });
-        pbarStatusPanel.removeAll();
-        pbarStatusPanel.add(ProgressHandleFactory.createProgressComponent(tableModel.getProgressHandle()), BorderLayout.CENTER);
-        pbarStatusPanel.validate();
+        hostSelectionPanel.addChangeListener(listener);
 
-        addAncestorListener(new AncestorListener() {
-            @Override
-            public void ancestorAdded(AncestorEvent event) {
-                tableModel.start(new Runnable() {
-                    @Override
-                    public void run() {
-                        pbarStatusPanel.setVisible(false);
-                    }
-                });
-            }
-            @Override
-            public void ancestorRemoved(AncestorEvent event) {
-                tableModel.stop();
-            }
-            @Override
-            public void ancestorMoved(AncestorEvent event) {
-            }
-        });
         addFocusListener(new FocusAdapter() {
+
             @Override
             public void focusGained(FocusEvent e) {
                 SwingUtilities.invokeLater(new Runnable() {
+
                     @Override
                     public void run() {
-                        textHostname.requestFocus();
+                        hostSelectionPanel.requestFocus();
                     }
                 });
             }
         });
     }
 
+    private void fireChange() {
+        if (listener != null) {
+            listener.stateChanged(null);
+        }
+    }
+
+    @Override
+    public void addNotify() {
+        super.addNotify();
+        fireChange();
+    }
+
     void init() {
         textUser.setText(data.getUserName());
-        textPort.setText(Integer.toString(data.getPort()));
+        hostSelectionPanel.set(data.getHostName(), data.getPort());
     }
 
     @Override
@@ -130,117 +116,84 @@ import org.openide.util.NbBundle;
     }
 
     public String getHostname() {
-        return textHostname.getText().trim();
+        return hostSelectionPanel.getHostname();
     }
 
     public String getUser() {
         return textUser.getText().trim();
     }
-    
+
     @Override
     public void setEnabled(boolean enabled) {
         super.setEnabled(enabled);
         textUser.setEnabled(enabled);
-        textHostname.setEnabled(enabled);
-        textHostname.setEditable(enabled);
-        textPort.setEnabled(enabled);
-        textPort.setEditable(enabled);
+        hostSelectionPanel.setEnabled(enabled);
+        proxySettingsPanel.setEnabled(enabled);
+        cbProxySettings.setEnabled(enabled);
         lblUser.setEnabled(enabled);
-        lblHostName.setEnabled(enabled);
         lblNeighbouthood.setEnabled(enabled);
-        lblPort.setEnabled(enabled);
-        tableHostsList.setEnabled(enabled);
+        serversListPanel.setEnabled(enabled);
     }
 
-    public Integer getPort() {
-        if ("".equals(textPort.getText())) { // NOI18N
-            return 22;
-        }
-
-        try {
-            return Integer.valueOf(Integer.parseInt(textPort.getText().trim()));
-        } catch(NumberFormatException e) {
-            //return Integer.valueOf(ExecutionEnvironmentFactory.DEFAULT_PORT);
-            return null;
-        }
+    public int getPort() {
+        return hostSelectionPanel.getPort();
     }
 
-    /** This method is called from within the constructor to
-     * initialize the form.
-     * WARNING: Do NOT modify this code. The content of this method is
-     * always regenerated by the Form Editor.
+    /**
+     * This method is called from within the constructor to initialize the form.
+     * WARNING: Do NOT modify this code. The content of this method is always
+     * regenerated by the Form Editor.
      */
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        lblHostName = new javax.swing.JLabel();
-        textHostname = new javax.swing.JTextField();
-        lblNeighbouthood = new javax.swing.JLabel();
-        jScrollPane2 = new javax.swing.JScrollPane();
-        tableHostsList = new HostTable();
-        pbarStatusPanel = new javax.swing.JPanel();
-        lblPort = new javax.swing.JLabel();
-        textPort = new org.netbeans.modules.cnd.remote.ui.wizard.PortTextField();
         lblUser = new javax.swing.JLabel();
         textUser = new javax.swing.JTextField();
+        hostSelectionPanel = new org.netbeans.modules.cnd.remote.ui.networkneighbour.HostSelectionPanel();
+        lblNeighbouthood = new javax.swing.JLabel();
+        serversListPanel = new javax.swing.JPanel();
+        cbProxySettings = new javax.swing.JCheckBox();
+        proxySettingsPanel = new org.netbeans.modules.cnd.remote.ui.proxysettings.ProxySettingsPanel();
 
         setPreferredSize(new java.awt.Dimension(534, 409));
         setRequestFocusEnabled(false);
 
-        lblHostName.setLabelFor(textHostname);
-        org.openide.awt.Mnemonics.setLocalizedText(lblHostName, org.openide.util.NbBundle.getMessage(CreateHostVisualPanel1.class, "CreateHostVisualPanel1.lblHostName.text")); // NOI18N
-
-        textHostname.setText(org.openide.util.NbBundle.getMessage(CreateHostVisualPanel1.class, "CreateHostVisualPanel1.textHostname.text")); // NOI18N
-
-        lblNeighbouthood.setLabelFor(tableHostsList);
-        org.openide.awt.Mnemonics.setLocalizedText(lblNeighbouthood, org.openide.util.NbBundle.getMessage(CreateHostVisualPanel1.class, "CreateHostVisualPanel1.lblNeighbouthood.text")); // NOI18N
-
-        tableHostsList.setModel(tableModel);
-        tableHostsList.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                tableHostsListMouseClicked(evt);
-            }
-        });
-        jScrollPane2.setViewportView(tableHostsList);
-        tableHostsList.getColumnModel().getColumn(0).setHeaderValue(org.openide.util.NbBundle.getMessage(CreateHostVisualPanel1.class, "CreateHostVisualPanel1.tableHostsList.columnModel.title0")); // NOI18N
-        tableHostsList.getColumnModel().getColumn(1).setHeaderValue(org.openide.util.NbBundle.getMessage(CreateHostVisualPanel1.class, "CreateHostVisualPanel1.tableHostsList.columnModel.title1")); // NOI18N
-
-        pbarStatusPanel.setMaximumSize(new java.awt.Dimension(2147483647, 10));
-        pbarStatusPanel.setMinimumSize(new java.awt.Dimension(100, 10));
-        pbarStatusPanel.setLayout(new java.awt.BorderLayout());
-
-        lblPort.setLabelFor(textPort);
-        org.openide.awt.Mnemonics.setLocalizedText(lblPort, org.openide.util.NbBundle.getMessage(CreateHostVisualPanel1.class, "CreateHostVisualPanel1.lblPort.text")); // NOI18N
-
-        textPort.setText(org.openide.util.NbBundle.getMessage(CreateHostVisualPanel1.class, "CreateHostVisualPanel1.textPort.text")); // NOI18N
-
+        lblUser.setLabelFor(textUser);
         org.openide.awt.Mnemonics.setLocalizedText(lblUser, org.openide.util.NbBundle.getMessage(CreateHostVisualPanel1.class, "CreateHostVisualPanel1.lblUser.text")); // NOI18N
 
         textUser.setText(org.openide.util.NbBundle.getMessage(CreateHostVisualPanel1.class, "CreateHostVisualPanel1.textUser.text")); // NOI18N
+
+        lblNeighbouthood.setLabelFor(serversListPanel);
+        org.openide.awt.Mnemonics.setLocalizedText(lblNeighbouthood, org.openide.util.NbBundle.getMessage(CreateHostVisualPanel1.class, "CreateHostVisualPanel1.lblNeighbouthood.text")); // NOI18N
+
+        serversListPanel.setLayout(new java.awt.BorderLayout());
+
+        org.openide.awt.Mnemonics.setLocalizedText(cbProxySettings, org.openide.util.NbBundle.getMessage(CreateHostVisualPanel1.class, "CreateHostVisualPanel1.cbProxySettings.text")); // NOI18N
+        cbProxySettings.setFocusPainted(false);
+        cbProxySettings.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cbProxySettingsActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(pbarStatusPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 534, Short.MAX_VALUE)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(lblNeighbouthood)
+                .addGap(0, 0, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jScrollPane2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 534, Short.MAX_VALUE)
-                    .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
-                        .addComponent(lblNeighbouthood)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED))
+                    .addComponent(hostSelectionPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(serversListPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(lblUser, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(lblHostName))
+                        .addComponent(lblUser, javax.swing.GroupLayout.PREFERRED_SIZE, 61, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(textUser, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 329, Short.MAX_VALUE)
-                            .addComponent(textHostname, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 329, Short.MAX_VALUE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(lblPort)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(textPort, javax.swing.GroupLayout.PREFERRED_SIZE, 68, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(textUser, javax.swing.GroupLayout.DEFAULT_SIZE, 330, Short.MAX_VALUE)
+                        .addGap(119, 119, 119))
+                    .addComponent(proxySettingsPanel, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(cbProxySettings, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -250,74 +203,42 @@ import org.openide.util.NbBundle;
                     .addComponent(lblUser)
                     .addComponent(textUser, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(textHostname, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(lblHostName)
-                    .addComponent(lblPort)
-                    .addComponent(textPort, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(hostSelectionPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(6, 6, 6)
                 .addComponent(lblNeighbouthood)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 229, Short.MAX_VALUE)
-                .addGap(18, 18, 18)
-                .addComponent(pbarStatusPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 13, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addComponent(serversListPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 271, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(cbProxySettings)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(proxySettingsPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 36, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
-    private void tableHostsListMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tableHostsListMouseClicked
-        // TODO add your handling code here:
-        if (evt.getClickCount() == 2) {
-            int row = tableHostsList.rowAtPoint(evt.getPoint());
-            textHostname.setText(((HostsListTableModel) tableHostsList.getModel()).getHostName(row));
-        }
-}//GEN-LAST:event_tableHostsListMouseClicked
-
+    private void cbProxySettingsActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbProxySettingsActionPerformed
+        proxySettingsPanel.setVisible(cbProxySettings.isSelected());
+        cbProxySettings.setIcon(cbProxySettings.isSelected() ? getExpandedIcon() : getCollapsedIcon());
+    }//GEN-LAST:event_cbProxySettingsActionPerformed
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JLabel lblHostName;
+    private javax.swing.JCheckBox cbProxySettings;
+    private org.netbeans.modules.cnd.remote.ui.networkneighbour.HostSelectionPanel hostSelectionPanel;
     private javax.swing.JLabel lblNeighbouthood;
-    private javax.swing.JLabel lblPort;
     private javax.swing.JLabel lblUser;
-    private javax.swing.JPanel pbarStatusPanel;
-    private javax.swing.JTable tableHostsList;
-    private javax.swing.JTextField textHostname;
-    private org.netbeans.modules.cnd.remote.ui.wizard.PortTextField textPort;
+    private org.netbeans.modules.cnd.remote.ui.proxysettings.ProxySettingsPanel proxySettingsPanel;
+    private javax.swing.JPanel serversListPanel;
     private javax.swing.JTextField textUser;
     // End of variables declaration//GEN-END:variables
+    static final boolean isGtk = "GTK".equals(UIManager.getLookAndFeel().getID()); //NOI18N
 
-    private class HostTable extends JTable {
+    static Icon getExpandedIcon() {
+        return UIManager.getIcon(isGtk ? "Tree.gtk_expandedIcon" : "Tree.expandedIcon"); //NOI18N
+    }
 
-        private final Color stdBackground;
-        private final Color stdForeground;
-        private final Color stdGrid;
-        private final Color stdHeader;
+    static Icon getCollapsedIcon() {
+        return UIManager.getIcon(isGtk ? "Tree.gtk_collapsedIcon" : "Tree.collapsedIcon"); //NOI18N
+    }
 
-        public HostTable() {
-            stdBackground = getBackground();
-            stdForeground = getForeground();
-            stdGrid = getGridColor();
-            stdHeader = getTableHeader().getForeground();
-        }
-
-        @Override
-        public void setEnabled(boolean enabled) {
-            super.setEnabled(enabled);
-            if (enabled) {
-                setForeground(stdForeground);
-                setBackground(stdBackground);
-                setGridColor(stdGrid);
-                getTableHeader().setForeground(stdHeader);
-            } else {
-                Color back = textPort.getBackground();
-                Color fore = Color.lightGray; // textPort.getForeground();
-                setBackground(back);
-                setForeground(fore);
-                setGridColor(fore);
-                getTableHeader().setForeground(fore);
-            }
-            invalidate();
-            repaint();
-        }
+    boolean isProxyValid() {
+        return proxySettingsPanel.isValidState();
     }
 }
-
