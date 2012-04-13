609267926c1b97e258126f22e93093976a519190
Vladimir Voskresensky <vv159170@netbeans.org>
2012-04-13 19:08:06 +0400

    comments and log

--- cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/csm/core/DeepReparsingUtils.java
+++ cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/csm/core/DeepReparsingUtils.java
@@ -260,7 +260,7 @@ public final class DeepReparsingUtils {
      */
     public static void reparseOnPropertyChanged(Collection<NativeFileItem> items, ProjectBase changedProject, boolean invalidateLibs) {
         if (TRACE) {
-            LOG.log(Level.INFO, "reparseOnPropertyChanged {0}", toString(items));
+            LOG.log(Level.INFO, "reparseOnPropertyChanged {0}{1}", new Object[] {invalidateLibs ? "With Invalidating Libs " : "", toString(items)});
         }        
         try {
             ParserQueue.instance().onStartAddingProjectFiles(changedProject);
@@ -307,6 +307,7 @@ public final class DeepReparsingUtils {
                     LOG.log(Level.INFO, "reparseOnPropertyChanged invalidates all libraries for {0}", changedProject);
                 }
                 // invalide libraries when asked but after deep reparsing activity
+                // because this activity uses information about project dependency and library dependencies
                 assert (changedProject instanceof ProjectImpl): "should be ProjectImpl: " + changedProject;
                 LibraryManager.getInstance().onProjectPropertyChanged(changedProject);
             }
