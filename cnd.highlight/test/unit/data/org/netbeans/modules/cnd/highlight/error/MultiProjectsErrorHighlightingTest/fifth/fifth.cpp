bd6d72df3310b98ac7f0edbb72b60c7bd9ad0247
Vladimir Voskresensky <vv159170@netbeans.org>
2012-04-13 20:58:34 +0400

    failing test for #210898 - incorrect content of system includes after reopening projects => unresolved identifiers in dependent projects

---
+++ cnd.highlight/test/unit/data/org/netbeans/modules/cnd/highlight/error/MultiProjectsErrorHighlightingTest/fifth/fifth.cpp
@@ -0,0 +1,13 @@
+
+#define MY_CPLUS_PLUS
+
+#include "lib_header.h"
+
+MyClass202433_One var;
+
+int main202433_Fifth() {
+    var.foo();
+    MyClass202433_Other var2;
+    var2.boo();
+    return 1;
+}
--- cnd.highlight/test/unit/data/org/netbeans/modules/cnd/highlight/error/MultiProjectsErrorHighlightingTest/first/first.cpp
+++ cnd.highlight/test/unit/data/org/netbeans/modules/cnd/highlight/error/MultiProjectsErrorHighlightingTest/first/first.cpp
@@ -2,5 +2,9 @@
 #include "lib_header.h"
 
 int main202433_First() {
+    MyClass202433_Yet_Other var;
+    var.boo2();
+    MyClass202433_Two var2;
+    var2.boo();
    return pGlobal202433->field1; 
 }
