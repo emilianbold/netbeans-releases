3e7243fe1ec78e9e00378d605b1e555936c8be07
S. Aubrecht <saubrecht@netbeans.org>
2012-04-23 16:41:29 +0200

    #211470 - invalid tab index

--- core.windows/src/org/netbeans/core/windows/view/ui/tabcontrol/tabbedpane/NBTabbedPaneController.java
+++ core.windows/src/org/netbeans/core/windows/view/ui/tabcontrol/tabbedpane/NBTabbedPaneController.java
@@ -259,7 +259,7 @@ public class NBTabbedPaneController {
         if( index > container.getTabCount() )
             index = -1;
         container.add( tabData.getComponent(), index );
-        index = container.indexOf( tabData.getComponent() );
+        index = container.indexOfComponent( tabData.getComponent() );
         container.setTabComponentAt( index, new CloseableTabComponent( tabData.getIcon(),
                 tabData.getText(), true, tabData.getTooltip(), container, controller ) );
     }
