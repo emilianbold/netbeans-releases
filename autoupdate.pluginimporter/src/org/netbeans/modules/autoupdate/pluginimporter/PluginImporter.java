75fa54253496e9e00295f0dd0c29bbe0d16bf8ca
Jiri Rechtacek <jrechtacek@netbeans.org>
2012-05-11 09:20:52 +0200

    #198780: NullPointerException at java.util.StringTokenizer.<init>

--- autoupdate.pluginimporter/src/org/netbeans/modules/autoupdate/pluginimporter/PluginImporter.java
+++ autoupdate.pluginimporter/src/org/netbeans/modules/autoupdate/pluginimporter/PluginImporter.java
@@ -133,7 +133,7 @@ public class PluginImporter {
             SpecificationVersion remoteSpec = null;
             if (remoteUnit != null && ! remoteUnit.getAvailableUpdates ().isEmpty ()) {
                 remoteElement = remoteUnit.getAvailableUpdates ().get (0);
-                remoteSpec = new SpecificationVersion (remoteElement.getSpecificationVersion ());
+                remoteSpec = remoteElement.getSpecificationVersion() == null ? null : new SpecificationVersion(remoteElement.getSpecificationVersion());
             }
 
             if (unit.getInstalled () != null) {
