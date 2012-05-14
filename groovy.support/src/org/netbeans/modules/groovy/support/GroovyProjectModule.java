cc02d84653e6d4aa55142a34ff602e9d69b3a337
Martin Janicek <mjanicek@netbeans.org>
2012-05-14 18:01:12 +0200

    Registration for context menu actions also in popup menu

--- groovy.support/src/org/netbeans/modules/groovy/support/GroovyProjectModule.java
+++ groovy.support/src/org/netbeans/modules/groovy/support/GroovyProjectModule.java
@@ -49,6 +49,7 @@ import org.netbeans.spi.project.ActionProvider;
 import org.netbeans.spi.project.ui.support.FileSensitiveActions;
 import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
 import org.openide.awt.ActionRegistration;
 import org.openide.util.NbBundle.Messages;
 
@@ -63,7 +64,7 @@ public class GroovyProjectModule {
     @Messages("LBL_CompileFile_Action=Compile File")
     @ActionID(id = "org.netbeans.modules.groovy.support.GroovyProjectModule.compile", category = "Groovy")
     @ActionRegistration(lazy = false, displayName = "#LBL_CompileFile_Action")
-    @ActionReference(path = "Loaders/text/x-groovy/Actions", position = 550, separatorBefore = 530)
+    @ActionReference(path = "Loaders/text/x-groovy/Actions", position = 550)
     public static Action compile() {
         return FileSensitiveActions.fileCommandAction(
                 ActionProvider.COMMAND_COMPILE_SINGLE,
@@ -74,7 +75,10 @@ public class GroovyProjectModule {
     @Messages("LBL_RunFile_Action=Run File")
     @ActionID(id = "org.netbeans.modules.groovy.support.GroovyProjectModule.run", category = "Groovy")
     @ActionRegistration(lazy = false, displayName = "#LBL_RunFile_Action")
-    @ActionReference(path = "Loaders/text/x-groovy/Actions", position = 560)
+    @ActionReferences(value = {
+        @ActionReference(path = "Loaders/text/x-groovy/Actions", position = 560),
+        @ActionReference(path = "Editors/text/x-groovy/Popup", position = 810, separatorBefore = 800)
+    })
     public static Action run() {
         return FileSensitiveActions.fileCommandAction(
                 ActionProvider.COMMAND_RUN_SINGLE,
@@ -85,7 +89,10 @@ public class GroovyProjectModule {
     @Messages("LBL_DebugFile_Action=Debug File")
     @ActionID(id = "org.netbeans.modules.groovy.support.GroovyProjectModule.debug", category = "Groovy")
     @ActionRegistration(lazy = false, displayName = "#LBL_DebugFile_Action")
-    @ActionReference(path = "Loaders/text/x-groovy/Actions", position = 570)
+    @ActionReferences(value = {
+        @ActionReference(path = "Loaders/text/x-groovy/Actions", position = 570),
+        @ActionReference(path = "Editors/text/x-groovy/Popup", position = 820)
+    })
     public static Action debug() {
         return FileSensitiveActions.fileCommandAction(
                 ActionProvider.COMMAND_DEBUG_SINGLE,
@@ -96,7 +103,10 @@ public class GroovyProjectModule {
     @Messages("LBL_TestFile_Action=Test File")
     @ActionID(id = "org.netbeans.modules.groovy.support.GroovyProjectModule.test", category = "Groovy")
     @ActionRegistration(lazy = false, displayName = "#LBL_TestFile_Action")
-    @ActionReference(path = "Loaders/text/x-groovy/Actions", position = 580)
+    @ActionReferences(value = {
+        @ActionReference(path = "Loaders/text/x-groovy/Actions", position = 580),
+        @ActionReference(path = "Editors/text/x-groovy/Popup", position = 830)
+    })
     public static Action test() {
         return FileSensitiveActions.fileCommandAction(
                 ActionProvider.COMMAND_TEST_SINGLE,
@@ -107,7 +117,10 @@ public class GroovyProjectModule {
     @Messages("LBL_DebugTestFile_Action=Debug Test File")
     @ActionID(id = "org.netbeans.modules.groovy.support.GroovyProjectModule.debug.test", category = "Groovy")
     @ActionRegistration(lazy = false, displayName = "#LBL_DebugTestFile_Action")
-    @ActionReference(path = "Loaders/text/x-groovy/Actions", position = 590)
+    @ActionReferences(value = {
+        @ActionReference(path = "Loaders/text/x-groovy/Actions", position = 590),
+        @ActionReference(path = "Editors/text/x-groovy/Popup", position = 840)
+    })
     public static Action debugTest() {
         return FileSensitiveActions.fileCommandAction(
                 ActionProvider.COMMAND_DEBUG_TEST_SINGLE,
