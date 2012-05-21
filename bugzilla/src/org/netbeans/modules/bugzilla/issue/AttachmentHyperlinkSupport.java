06a7be2d250a5a6ea594603adabc204a499ce44d
Tomas Stupka <tstupka@netbeans.org>
2012-05-21 15:16:07 +0200

    Issue #212623 - Links to attachements are broken

--- bugzilla/src/org/netbeans/modules/bugzilla/issue/AttachmentHyperlinkSupport.java
+++ bugzilla/src/org/netbeans/modules/bugzilla/issue/AttachmentHyperlinkSupport.java
@@ -53,17 +53,30 @@ import java.util.Collection;
 class AttachmentHyperlinkSupport {
 
     private static final String PREFIX = "Created an attachment (id=";  //NOI18N
+    private static final String PREFIX2 = "Created attachment ";  //NOI18N
     private static final int EQUAL_SIGN_POSITION = PREFIX.lastIndexOf('=');
-    private static final int ATTACHMENT_HYPERLINK_START_FALLBACK
-                             = PREFIX.indexOf("attachment");            //NOI18N
+    private static final int ATTACHMENT_HYPERLINK_START_FALLBACK  = PREFIX.indexOf("attachment");            //NOI18N
+    private static final int ATTACHMENT_HYPERLINK_START_FALLBACK2  = PREFIX2.indexOf("attachment");            //NOI18N
     //private static final Pattern pattern = Pattern.compile(
     //                 "([0-9]++)\\)[^\\r\\n]*+(?:[\\r\\n]++(.*+))?+"); //NOI18N
 
     private static final char CR = '\r';
     private static final char LF = '\n';
 
-    static int[] findBoundaries(String text,
-                                Collection<String> knownIds) {
+    static Attachement findAttachment(String text, Collection<String> knownIds) {
+        int[] boundaries = findAttachment1(text, knownIds);
+        if(boundaries == null) {
+            return findAttachment2(text, knownIds);
+        } else {
+            Attachement b = new Attachement();
+            b.idx1 = boundaries[0];
+            b.idx2 = boundaries[1];
+            b.id = getAttachmentId(text);
+            return b;
+        }
+    }
+    
+    private static int[] findAttachment1(String text, Collection<String> knownIds) {
         if ((knownIds != null) && knownIds.isEmpty()) {
             return null;
         }
@@ -82,31 +95,7 @@ class AttachmentHyperlinkSupport {
                     int idEndIndex = index;
                     if (isKnownId(text.substring(idStartIndex, idEndIndex),
                                   knownIds)) {
-                        do {
-                            index++;
-                        } while ((index < length) && isNotNewline(text.charAt(index)));
-                        if (index < length) {       //at newline
-
-                            /* skip just one newline */
-                            if (text.charAt(index) == CR) {
-                                index++;
-                            }
-                            if ((index < length) && (text.charAt(index) == LF)) {
-                                index++;
-                            }
-                            while ((index < length) && isSpace(text.charAt(index))) {
-                                index++;
-                            }
-                            if ((index < length) && isNotNewline(text.charAt(index))) {   //at printable
-                                int descriptionStart = index;
-                                do {
-                                    index++;
-                                } while ((index < length) && isNotNewline(text.charAt(index)));
-                                return new int[] {descriptionStart, index};
-                            }
-                        }
-                        return new int[] {ATTACHMENT_HYPERLINK_START_FALLBACK,
-                                          idEndIndex + 1};
+                        return findBoundaries(index, text, idEndIndex + 1, ATTACHMENT_HYPERLINK_START_FALLBACK);
                     }
                 }
             }
@@ -114,12 +103,69 @@ class AttachmentHyperlinkSupport {
         return null;
     }
 
-    static String getAttachmentId(String commentText) {
+    private static Attachement findAttachment2(String text, Collection<String> knownIds) {
+        if (knownIds == null || knownIds.isEmpty()) {
+            return null;
+        }
+
+        for (String id : knownIds) {
+            String prefixId = PREFIX2 + id;
+            if(!text.startsWith(prefixId)) {
+                continue;
+            }
+            
+            int[] boundaries = findBoundaries(prefixId.length() - 1, text, prefixId.length(), ATTACHMENT_HYPERLINK_START_FALLBACK2);
+            if(boundaries != null) {
+                Attachement b = new Attachement();
+                b.idx1 = boundaries[0];
+                b.idx2 = boundaries[1];
+                b.id = id;
+                return b;
+            }
+        }
+        return null;
+    }    
+    
+    private static int[] findBoundaries(int index, String text, int idEndIndex, int fallback) {
+        final int length = text.length();
+        do {
+            index++;
+        } while ((index < length) && isNotNewline(text.charAt(index)));
+        if (index < length) {       //at newline
+
+            /* skip just one newline */
+            if (text.charAt(index) == CR) {
+                index++;
+            }
+            if ((index < length) && (text.charAt(index) == LF)) {
+                index++;
+            }
+            while ((index < length) && isSpace(text.charAt(index))) {
+                index++;
+            }
+            if ((index < length) && isNotNewline(text.charAt(index))) {   //at printable
+                int descriptionStart = index;
+                do {
+                    index++;
+                } while ((index < length) && isNotNewline(text.charAt(index)));
+                return new int[] {descriptionStart, index};
+            }
+        }
+        return new int[] {fallback, idEndIndex};
+    }
+
+    private static String getAttachmentId(String commentText) {
         int closingBracketPos = commentText.indexOf(')', PREFIX.length() + 1);
         assert closingBracketPos != -1;
         return new String(commentText.substring(PREFIX.length(), closingBracketPos));
     }
 
+    static class Attachement {
+        int idx1;
+        int idx2;
+        String id;
+    }
+    
     private static boolean isKnownId(String id,
                                      Collection<String> knownIds) {
         if (knownIds == null) {
