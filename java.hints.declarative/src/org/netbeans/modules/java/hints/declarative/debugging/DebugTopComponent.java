83cb7bda54290b240ae03e7a4315069d6216f557
Jan Lahoda <jlahoda@netbeans.org>
2012-05-28 18:07:19 +0200

    #185294: initialize the editor pane on background.

--- java.hints.declarative/src/org/netbeans/modules/java/hints/declarative/debugging/DebugTopComponent.java
+++ java.hints.declarative/src/org/netbeans/modules/java/hints/declarative/debugging/DebugTopComponent.java
@@ -42,10 +42,14 @@ import java.awt.Component;
 import java.awt.Rectangle;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.concurrent.Callable;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.DefaultListCellRenderer;
+import javax.swing.JEditorPane;
 import javax.swing.JList;
+import javax.swing.SwingUtilities;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.Document;
 import javax.swing.text.EditorKit;
@@ -61,26 +65,62 @@ import org.netbeans.modules.java.hints.declarative.Utilities;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
 import org.openide.util.Exceptions;
+import org.openide.util.NbBundle.Messages;
+import org.openide.util.RequestProcessor;
 
 @ConvertAsProperties(dtd = "-//org.netbeans.modules.jackpot30.file.debugging//Debug//EN",
 autostore = false)
+@Messages("DN_Loading=Loading...")
 public final class DebugTopComponent extends TopComponent {
 
+    private static final RequestProcessor WORKER = new RequestProcessor(DebugTopComponent.class.getName(), 1, false, false);
+    
     private static DebugTopComponent instance;
     /** path to the icon used by the component and its open action */
 //    static final String ICON_PATH = "SET/PATH/TO/ICON/HERE";
     private static final String PREFERRED_ID = "DebugTopComponent";
 
+    //@GuardedBy(AWT)
+    private JEditorPane code;
+    private boolean initialized;
+    
     public DebugTopComponent() {
         initComponents();
         setName(NbBundle.getMessage(DebugTopComponent.class, "CTL_DebugTopComponent"));
         setToolTipText(NbBundle.getMessage(DebugTopComponent.class, "HINT_DebugTopComponent"));
 //        setIcon(ImageUtilities.loadImage(ICON_PATH, true));
         
-        code.setEditorKit(MimeLookup.getLookup("text/x-javahints").lookup(EditorKit.class));
-        Document doc = code.getEditorKit().createDefaultDocument();
-        doc.putProperty("mimeType", "text/x-javahints");
-        code.setDocument(doc);
+        WORKER.post(new Runnable() {
+            @Override public void run() {
+                final EditorKit kit = MimeLookup.getLookup("text/x-javahints").lookup(EditorKit.class);
+                
+                if (kit instanceof Callable) {
+                    try {
+                        ((Callable) kit).call();
+                    } catch (Exception ex) {
+                        Exceptions.printStackTrace(ex);
+                    }
+                }
+                
+                SwingUtilities.invokeLater(new Runnable() {
+                    @Override public void run() {
+                        code = new javax.swing.JEditorPane();
+                        code.setContentType("text/x-javahints");
+                        code.setEditorKit(kit);
+                        Document doc = code.getEditorKit().createDefaultDocument();
+                        doc.putProperty("mimeType", "text/x-javahints");
+                        code.setDocument(doc);
+                        editorHolder.setViewportView(code);
+                        
+                        initialized = true;
+                        
+                        synchronized (DebugTopComponent.this) {
+                            DebugTopComponent.this.notifyAll();
+                        }
+                    }
+                });
+            }
+        });
     }
 
     /** This method is called from within the constructor to
@@ -92,8 +132,7 @@ public final class DebugTopComponent extends TopComponent {
     private void initComponents() {
 
         files = new javax.swing.JComboBox();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        code = new javax.swing.JEditorPane();
+        editorHolder = new javax.swing.JScrollPane();
 
         files.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
         files.setRenderer(new FileRenderer());
@@ -103,18 +142,15 @@ public final class DebugTopComponent extends TopComponent {
             }
         });
 
-        code.setContentType("text/x-javahints");
-        jScrollPane1.setViewportView(code);
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(files, javax.swing.GroupLayout.Alignment.TRAILING, 0, 376, Short.MAX_VALUE)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 376, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(editorHolder)
+                    .addComponent(files, 0, 376, Short.MAX_VALUE))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -123,14 +159,18 @@ public final class DebugTopComponent extends TopComponent {
                 .addContainerGap()
                 .addComponent(files, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 246, Short.MAX_VALUE)
+                .addComponent(editorHolder, javax.swing.GroupLayout.DEFAULT_SIZE, 246, Short.MAX_VALUE)
                 .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
 
     private void filesActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_filesActionPerformed
         synchronized (this) {
-            FileObject selected = (FileObject) files.getSelectedItem();
+            Object selectedObj = files.getSelectedItem();
+            
+            if (!(selectedObj instanceof FileObject)) return ;
+            
+            FileObject selected = (FileObject) selectedObj;
             String spec = Utilities.readFile(selected);
 
             code.setText(spec);
@@ -150,9 +190,8 @@ public final class DebugTopComponent extends TopComponent {
     }//GEN-LAST:event_filesActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JEditorPane code;
+    private javax.swing.JScrollPane editorHolder;
     private javax.swing.JComboBox files;
-    private javax.swing.JScrollPane jScrollPane1;
     // End of variables declaration//GEN-END:variables
 
     private Collection<? extends HintWrapper> hints = Collections.emptyList();
@@ -161,8 +200,15 @@ public final class DebugTopComponent extends TopComponent {
         return hints;
     }
 
-    public Document getDocument() {
-        //XXX: threading
+    public synchronized Document getDocument() {
+        //XXX: threading - called off AWT
+        while (!initialized) {
+            try {
+                wait();
+            } catch (InterruptedException ex) {
+                Logger.getLogger(DebugTopComponent.class.getName()).log(Level.FINE, null, ex);
+            }
+        }
         return code.getDocument();
     }
 
@@ -206,24 +252,48 @@ public final class DebugTopComponent extends TopComponent {
         return TopComponent.PERSISTENCE_ALWAYS;
     }
 
+    private long openCount = 0;
+    
     @Override
     public void componentOpened() {
-        DefaultComboBoxModel dlm = new DefaultComboBoxModel();
+        DefaultComboBoxModel loadingModel = new DefaultComboBoxModel();
+        
+        loadingModel.addElement(Bundle.DN_Loading());
+        
+        files.setModel(loadingModel);
+        files.setSelectedIndex(0);
+        
+        final long startingCount = ++openCount;
+        
+        WORKER.post(new Runnable() {
+            @Override public void run() {
+                final DefaultComboBoxModel dlm = new DefaultComboBoxModel();
 
-        for (FileObject file : DeclarativeHintRegistry.findAllFiles()) {
-            dlm.addElement(file);
-        }
+                for (FileObject file : DeclarativeHintRegistry.findAllFiles()) {
+                    dlm.addElement(file);
+                }
 
-        files.setModel(dlm);
-        if (dlm.getSize()>0)
-            files.setSelectedIndex(0);
+                SwingUtilities.invokeLater(new Runnable() {
+                    @Override public void run() {
+                        if (startingCount != openCount) return ;
+                        
+                        files.setModel(dlm);
+                        if (dlm.getSize()>0)
+                            files.setSelectedIndex(0);
+                    }
+                });
+            }
+        });
     }
 
     @Override
     public synchronized void componentClosed() {
         files.setModel(new DefaultComboBoxModel());
-        code.setText("");
+        if (code != null) {
+            code.setText("");
+        }
         hints = Collections.emptyList();
+        initialized = false;
     }
 
     void writeProperties(java.util.Properties p) {
