01c9b80c55467b78ea282111340e705c6e7459f2
Jan Peska <JPESKA@netbeans.org>
2012-05-28 11:22:33 +0200

    Add New Category action to the Add Task To Category dialog.

--- tasks.ui/src/org/netbeans/modules/tasks/ui/DashboardTopComponent.java
+++ tasks.ui/src/org/netbeans/modules/tasks/ui/DashboardTopComponent.java
@@ -62,6 +62,8 @@ import org.openide.awt.ActionID;
 import org.openide.awt.ActionReference;
 import org.openide.windows.WindowManager;
 import java.util.List;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.SwingUtilities;
 import org.netbeans.modules.bugtracking.api.RepositoryManager;
 import org.netbeans.modules.tasks.ui.dashboard.CategoryNode;
@@ -77,9 +79,7 @@ iconBase = "org/netbeans/modules/tasks/ui/resources/dashboard.png",
 persistenceType = TopComponent.PERSISTENCE_ALWAYS)
 @TopComponent.Registration(mode = "explorer", openAtStartup = true)
 @ActionID(category = "Window", id = "org.netbeans.modules.tasks.ui.DashboardTopComponent")
-@ActionReference(path = "Menu/Window"
- , position = 501
- )
+@ActionReference(path = "Menu/Window", position = 501)
 @TopComponent.OpenActionRegistration(displayName = "#CTL_DashboardAction",
 preferredID = "DashboardTopComponent")
 @NbBundle.Messages({
@@ -95,7 +95,6 @@ public final class DashboardTopComponent extends TopComponent {
     private FilterDocumentListener filterListener;
     private CategoryNameDocumentListener categoryNameListener;
     private Timer filterTimer;
-    private Timer cateregoryNameTimer;
     private ActiveTaskPanel activeTaskPanel;
     private final GridBagConstraints activeTaskConstrains;
     private FilterPanel filterPanel;
@@ -108,8 +107,6 @@ public final class DashboardTopComponent extends TopComponent {
         setName(NbBundle.getMessage(DashboardTopComponent.class, "CTL_DashboardTopComponent")); //NOI18N
         filterTimer = new Timer(500, new FilterTimerListener());
         filterTimer.stop();
-        cateregoryNameTimer = new Timer(200, new CategoryNameTimerListener());
-        cateregoryNameTimer.stop();
         activeTaskConstrains = new GridBagConstraints(0, 1, 2, 1, 1.0, 0, GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL, new Insets(0, 3, 0, 0), 0, 0);
         componentAdapter = new ComponentAdapter() {
             @Override
@@ -185,7 +182,8 @@ public final class DashboardTopComponent extends TopComponent {
     /**
      * Gets default instance. Do not use directly: reserved for *.settings files
      * only, i.e. deserialization routines; otherwise you could get a
-     * non-deserialized instance. To obtain the singleton instance, use {@link #findInstance}.
+     * non-deserialized instance. To obtain the singleton instance, use
+     * {@link #findInstance}.
      */
     @Override
     public void componentOpened() {
@@ -206,7 +204,6 @@ public final class DashboardTopComponent extends TopComponent {
 
         //load data after the component is displayed
         SwingUtilities.invokeLater(new Runnable() {
-
             @Override
             public void run() {
                 dashboard.loadData();
@@ -260,15 +257,16 @@ public final class DashboardTopComponent extends TopComponent {
         }
     }
 
-    public void createCategory() {
+    public Category createCategory() {
         categoryNamePanel = new CategoryNamePanel(NbBundle.getMessage(DashboardTopComponent.class, "LBL_CreateCatNameLabel"), ""); //NOI18N
 
         boolean confirm = showCategoryNameDialog(categoryNamePanel, NbBundle.getMessage(DashboardTopComponent.class, "LBL_CreateCatTitle")); //NOI18N
         if (confirm) {
-            //TODO fire event to category listeners
             Category category = new Category(categoryNamePanel.getCategoryName());
             DashboardViewer.getInstance().addCategory(category);
+            return category;
         }
+        return null;
     }
 
     public void renameCategory(Category category) {
@@ -276,7 +274,6 @@ public final class DashboardTopComponent extends TopComponent {
 
         boolean confirm = showCategoryNameDialog(categoryNamePanel, NbBundle.getMessage(DashboardTopComponent.class, "LBL_RenameCatTitle")); //NOI18N
         if (confirm) {
-            //TODO fire event to category listeners
             DashboardViewer.getInstance().renameCategory(category, categoryNamePanel.getCategoryName());
         }
     }
@@ -291,7 +288,7 @@ public final class DashboardTopComponent extends TopComponent {
                 NotifyDescriptor.OK_OPTION);
         categoryNameDialog.setValid(false);
         if (categoryNameListener == null) {
-            categoryNameListener = new CategoryNameDocumentListener(cateregoryNameTimer);
+            categoryNameListener = new CategoryNameDocumentListener();
         }
         panel.addDocumentListener(categoryNameListener);
         boolean confirm = DialogDisplayer.getDefault().notify(categoryNameDialog) == NotifyDescriptor.OK_OPTION;
@@ -317,10 +314,7 @@ public final class DashboardTopComponent extends TopComponent {
 
         if (DialogDisplayer.getDefault().notify(nd) == NotifyDescriptor.OK_OPTION) {
             Category category = picker.getChosenCategory();
-            for (TaskNode taskNode : taskNodes) {
-                //TODO add all task at once
-                DashboardViewer.getInstance().addTaskToCategory(taskNode, category);
-            }
+            DashboardViewer.getInstance().addTaskToCategory(category, taskNodes);
         }
 
     }
@@ -364,26 +358,6 @@ public final class DashboardTopComponent extends TopComponent {
         }
     }
 
-    private class CategoryNameTimerListener implements ActionListener {
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            if (e.getSource() == cateregoryNameTimer) {
-                cateregoryNameTimer.stop();
-                if (categoryNamePanel.getCategoryName().isEmpty()) {
-                    categoryNamePanel.setErrorText(NbBundle.getMessage(DashboardTopComponent.class, "LBL_CatNameErrEmpty")); //NOI18N
-                    categoryNameDialog.setValid(false);
-                } else if (!DashboardViewer.getInstance().isCategoryNameUnique(categoryNamePanel.getCategoryName())) {
-                    categoryNamePanel.setErrorText(NbBundle.getMessage(DashboardTopComponent.class, "LBL_CatNameErrUnique")); //NOI18N
-                    categoryNameDialog.setValid(false);
-                } else {
-                    categoryNamePanel.setErrorText("");
-                    categoryNameDialog.setValid(true);
-                }
-            }
-        }
-    }
-
     private class FilterDocumentListener implements DocumentListener {
 
         private Timer timer;
@@ -410,28 +384,36 @@ public final class DashboardTopComponent extends TopComponent {
 
     private class CategoryNameDocumentListener implements DocumentListener {
 
-        private Timer timer;
 
-        public CategoryNameDocumentListener(Timer timer) {
-            this.timer = timer;
+        public CategoryNameDocumentListener() {
         }
 
         @Override
         public void insertUpdate(DocumentEvent e) {
-            categoryNameDialog.setValid(false);
-            timer.restart();
+            checkCategoryName();
         }
 
         @Override
         public void removeUpdate(DocumentEvent e) {
-            categoryNameDialog.setValid(false);
-            timer.restart();
+            checkCategoryName();
         }
 
         @Override
         public void changedUpdate(DocumentEvent e) {
-            categoryNameDialog.setValid(false);
-            timer.restart();
+            checkCategoryName();
+        }
+
+        private void checkCategoryName(){
+            if (categoryNamePanel.getCategoryName().isEmpty()) {
+                categoryNamePanel.setErrorText(NbBundle.getMessage(DashboardTopComponent.class, "LBL_CatNameErrEmpty")); //NOI18N
+                categoryNameDialog.setValid(false);
+            } else if (!DashboardViewer.getInstance().isCategoryNameUnique(categoryNamePanel.getCategoryName())) {
+                categoryNamePanel.setErrorText(NbBundle.getMessage(DashboardTopComponent.class, "LBL_CatNameErrUnique")); //NOI18N
+                categoryNameDialog.setValid(false);
+            } else {
+                categoryNamePanel.setErrorText("");
+                categoryNameDialog.setValid(true);
+            }
         }
     }
 }
