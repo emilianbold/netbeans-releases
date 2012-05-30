4abb9abcc4f18bca0688c8b6bca126483a2c8cb8
S. Aubrecht <saubrecht@netbeans.org>
2012-05-30 14:33:00 +0200

    move all toolbar related classes to a separate package

--- web.browser.api/src/org/netbeans/modules/web/browser/api/WebBrowserPane.java
+++ web.browser.api/src/org/netbeans/modules/web/browser/api/WebBrowserPane.java
@@ -42,16 +42,14 @@
 package org.netbeans.modules.web.browser.api;
 
 import java.awt.Component;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.net.URL;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
-import javax.swing.*;
+import javax.swing.SwingUtilities;
 import org.netbeans.core.HtmlBrowserComponent;
-import org.netbeans.modules.web.browser.spi.Zoomable;
+import org.netbeans.modules.web.browser.ui.DeveloperToolbar;
 import org.openide.awt.HtmlBrowser;
 import org.openide.awt.HtmlBrowser.Factory;
 import org.openide.util.Lookup;
@@ -71,7 +69,7 @@ public final class WebBrowserPane {
     private HtmlBrowserComponent topComponent;
     private boolean wrapEmbeddedBrowserInTopComponent;
     private boolean createTopComponent = false;
-    private JToolBar developerToolbar;
+    private DeveloperToolbar devToolbar;
     
 //    WebBrowserPane(HtmlBrowserComponent comp) {
 //        this(comp.getBrowserImpl(), null, false, comp);
@@ -131,7 +129,7 @@ public final class WebBrowserPane {
     
     private synchronized HtmlBrowserComponent getTopComponent() {
         if (topComponent == null && createTopComponent) {
-            developerToolbar = new JToolBar();
+            devToolbar = DeveloperToolbar.create();
 
             // below constructor sets some TopComponent properties and needs
             // to be therefore called in AWT thread:
@@ -140,7 +138,7 @@ public final class WebBrowserPane {
                 @Override
                 protected void componentOpened() {
                     super.componentOpened();
-                    initDevToolbar();
+                    devToolbar.intialize( getLookup() );
                 }
 
                 @Override
@@ -151,7 +149,7 @@ public final class WebBrowserPane {
 
                 @Override
                 protected HtmlBrowser createBrowser( Factory factory, boolean showToolbar, boolean showStatus ) {
-                    return new HtmlBrowser( factory, showToolbar, showStatus, developerToolbar );
+                    return new HtmlBrowser( factory, showToolbar, showStatus, devToolbar.getComponent() );
                 }
 
             };
@@ -305,55 +303,6 @@ public final class WebBrowserPane {
         for (WebBrowserPaneListener listener : listeners) {
             listener.browserEvent(new WebBrowserRunningStateChangedEvent(this));
         }
-    }
-
-    private void initDevToolbar() {
-        developerToolbar.setFloatable( false );
-        developerToolbar.setFocusable( false );
-
-        DefaultComboBoxModel zoomModel = new DefaultComboBoxModel();
-        zoomModel.addElement( "200%" ); //NOI18N
-        zoomModel.addElement( "150%" ); //NOI18N
-        zoomModel.addElement( "100%" ); //NOI18N
-        zoomModel.addElement( "75%" ); //NOI18N
-        zoomModel.addElement( "50%" ); //NOI18N
-        final JComboBox comboZoom = new JComboBox(zoomModel);
-        comboZoom.setEditable( true );
-        if( comboZoom.getEditor().getEditorComponent() instanceof JTextField )
-            ((JTextField)comboZoom.getEditor().getEditorComponent()).setColumns( 4 );
-        comboZoom.setSelectedItem( "100%" ); //NOI18N
-        comboZoom.addItemListener( new ItemListener() {
-
-            @Override
-            public void itemStateChanged( ItemEvent e ) {
-                if( e.getStateChange() == ItemEvent.DESELECTED )
-                    return;
-                String newZoom = zoom( comboZoom.getSelectedItem().toString() );
-                comboZoom.setSelectedItem( newZoom );
-            }
-        });
-        comboZoom.setEnabled( null != topComponent.getLookup().lookup( Zoomable.class ) );
-        developerToolbar.add( comboZoom );
-    }
-
-    private String zoom( String zoomFactor ) {
-        if( null == topComponent || zoomFactor.trim().isEmpty() )
-            return null;
-
-        Zoomable zoomable = topComponent.getLookup().lookup( Zoomable.class );
-        if( null == zoomable )
-            return null;
-        try {
-            zoomFactor = zoomFactor.replaceAll( "\\%", ""); //NOI18N
-            zoomFactor = zoomFactor.trim();
-            double zoom = Double.parseDouble( zoomFactor );
-            zoom = Math.abs( zoom )/100;
-            zoomable.zoom( zoom );
-            return (int)(100*zoom) + "%";
-        } catch( NumberFormatException nfe ) {
-            //ignore
-        }
-        return null;
     }
     
     /**
---
+++ web.browser.api/src/org/netbeans/modules/web/browser/ui/DeveloperToolbar.java
@@ -0,0 +1,130 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2012 Oracle and/or its affiliates. All rights reserved.
+ *
+ * Oracle and Java are registered trademarks of Oracle and/or its affiliates.
+ * Other names may be trademarks of their respective owners.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ *
+ * Contributor(s):
+ *
+ * Portions Copyrighted 2012 Sun Microsystems, Inc.
+ */
+package org.netbeans.modules.web.browser.ui;
+
+import java.awt.Component;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
+import javax.swing.JToolBar;
+import org.netbeans.modules.web.browser.spi.Zoomable;
+import org.openide.util.Lookup;
+
+/**
+ * Toolbar with web-developer tools.
+ * 
+ * @author S. Aubrecht
+ */
+public class DeveloperToolbar {
+
+    private final JToolBar bar;
+    private Lookup context;
+
+    private DeveloperToolbar() {
+        bar = new JToolBar();
+        bar.setFloatable( false );
+        bar.setFocusable( false );
+    }
+
+    public static DeveloperToolbar create() {
+        return new DeveloperToolbar();
+    }
+
+    public Component getComponent() {
+        return bar;
+    }
+
+    public void intialize( Lookup context ) {
+        this.context = context;
+
+        //ZOOM combo box
+        DefaultComboBoxModel zoomModel = new DefaultComboBoxModel();
+        zoomModel.addElement( "200%" ); //NOI18N
+        zoomModel.addElement( "150%" ); //NOI18N
+        zoomModel.addElement( "100%" ); //NOI18N
+        zoomModel.addElement( "75%" ); //NOI18N
+        zoomModel.addElement( "50%" ); //NOI18N
+        final JComboBox comboZoom = new JComboBox(zoomModel);
+        comboZoom.setEditable( true );
+        if( comboZoom.getEditor().getEditorComponent() instanceof JTextField )
+            ((JTextField)comboZoom.getEditor().getEditorComponent()).setColumns( 4 );
+        comboZoom.setSelectedItem( "100%" ); //NOI18N
+        comboZoom.addItemListener( new ItemListener() {
+
+            @Override
+            public void itemStateChanged( ItemEvent e ) {
+                if( e.getStateChange() == ItemEvent.DESELECTED )
+                    return;
+                String newZoom = zoom( comboZoom.getSelectedItem().toString() );
+                comboZoom.setSelectedItem( newZoom );
+            }
+        });
+        comboZoom.setEnabled( null != getLookup().lookup( Zoomable.class ) );
+        bar.add( comboZoom );
+    }
+
+    private Lookup getLookup() {
+        return null == context ? Lookup.EMPTY : context;
+    }
+
+    private String zoom( String zoomFactor ) {
+        if( zoomFactor.trim().isEmpty() )
+            return null;
+
+        Zoomable zoomable = getLookup().lookup( Zoomable.class );
+        if( null == zoomable )
+            return null;
+        try {
+            zoomFactor = zoomFactor.replaceAll( "\\%", ""); //NOI18N
+            zoomFactor = zoomFactor.trim();
+            double zoom = Double.parseDouble( zoomFactor );
+            zoom = Math.abs( zoom )/100;
+            zoomable.zoom( zoom );
+            return (int)(100*zoom) + "%";
+        } catch( NumberFormatException nfe ) {
+            //ignore
+        }
+        return null;
+    }
+}
