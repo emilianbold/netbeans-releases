f8df12b2e479b436b1953ff6a37b816cec5958d7
Tomas Stupka <tstupka@netbeans.org>
2012-06-04 17:55:41 +0200

    Issue #213501 - hyperlinks missing in svn history summary

--- jira/src/org/netbeans/modules/jira/issue/JiraIssueFinder.java
+++ jira/src/org/netbeans/modules/jira/issue/JiraIssueFinder.java
@@ -373,7 +373,8 @@ public class JiraIssueFinder extends IssueFinder {
         }
 
         private static boolean isPrjKeyChar(int c) {
-            return (c > 0x20) && (c <= 0xff) && (c != ',');
+            return (c > 0x20) && (c <= 0xff) && 
+                   (isPunctuationAllowed() ? (c != ',') : !isPunct(c)); 
         }
 
         private static boolean isDigit(int c) {
@@ -426,6 +427,11 @@ public class JiraIssueFinder extends IssueFinder {
 
             return true;
         }
+
+        private static boolean isPunctuationAllowed() {
+            return !"true".equals(System.getProperty("org.netbeans.modules.jira.noPunctuationInIssueKey", "false"));
+        }
+
 
     }
 
