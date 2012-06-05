d5b1ab8f5f0c8e8a888d66a385870c0bdfebe8e3
Alexander Simon <alexvsimon@netbeans.org>
2012-06-05 14:36:31 +0400

    fixed Bug #213328 - Navigator is empty for standalone files

--- cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/csm/core/CsmStandaloneFileProviderImpl.java
+++ cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/csm/core/CsmStandaloneFileProviderImpl.java
@@ -45,6 +45,7 @@ import java.io.File;
 import java.io.PrintWriter;
 import java.nio.BufferUnderflowException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -63,9 +64,11 @@ import org.netbeans.modules.cnd.api.model.CsmProject;
 import org.netbeans.modules.cnd.api.model.services.CsmStandaloneFileProvider;
 import org.netbeans.modules.cnd.api.project.DefaultSystemSettings;
 import org.netbeans.modules.cnd.api.project.NativeFileItem;
+import org.netbeans.modules.cnd.api.project.NativeFileItem.LanguageFlavor;
 import org.netbeans.modules.cnd.api.project.NativeFileItemSet;
 import org.netbeans.modules.cnd.api.project.NativeProject;
 import org.netbeans.modules.cnd.api.project.NativeProjectItemsListener;
+import org.netbeans.modules.cnd.api.project.NativeProjectRegistry;
 import org.netbeans.modules.cnd.modelimpl.debug.DiagnosticExceptoins;
 import org.netbeans.modules.cnd.modelimpl.trace.NativeProjectProvider;
 import org.netbeans.modules.cnd.modelutil.CsmUtilities;
@@ -330,9 +333,20 @@ public class CsmStandaloneFileProviderImpl extends CsmStandaloneFileProvider {
                 return null;
             }
             NativeFileItemSet set = dao.getLookup().lookup(NativeFileItemSet.class);
-            if (set == null || !set.isEmpty()) {
+            if (set == null) {
                 // it does not matter, what is there in the set! - see #185599, #185629
                 return null;
+            }
+            NativeFileItem itemPrototype = null;
+            if (!set.isEmpty()) {
+                Iterator<NativeFileItem> iterator = set.getItems().iterator();
+                Collection<NativeProject> openProjects = NativeProjectRegistry.getDefault().getOpenProjects();
+                while(iterator.hasNext()) {
+                    itemPrototype = iterator.next();
+                    if (openProjects.contains(itemPrototype.getNativeProject())) {
+                        return null;
+                    }
+                }
             }
             CsmModel model = ModelImpl.instance();
             List<FSPath> sysIncludes = new ArrayList<FSPath>();
@@ -340,7 +354,15 @@ public class CsmStandaloneFileProviderImpl extends CsmStandaloneFileProvider {
             List<String> sysMacros = new ArrayList<String>();
             List<String> usrMacros = new ArrayList<String>();
             List<String> undefinedMacros = new ArrayList<String>();
-            NativeFileItem.Language lang = NativeProjectProvider.getLanguage(file, dao);
+            NativeFileItem.Language lang;
+            LanguageFlavor flavor;
+            if (itemPrototype != null) {
+                lang = itemPrototype.getLanguage();
+                flavor = itemPrototype.getLanguageFlavor();
+            } else {
+                lang = NativeProjectProvider.getLanguage(file, dao);
+                flavor = LanguageFlavor.UNKNOWN;
+            }
             NativeProject prototype = null;            
             for (CsmProject csmProject : model.projects()) {
                 Object p = csmProject.getPlatformProject();
@@ -373,18 +395,22 @@ public class CsmStandaloneFileProviderImpl extends CsmStandaloneFileProvider {
                 fs = CndFileUtils.getLocalFileSystem();
             }
             NativeProjectImpl impl = new NativeProjectImpl(file, sysIncludes, usrIncludes, sysMacros, usrMacros, undefinedMacros);
-            if (prototype == null) {
-                // Some default implementation should be provided.
-                sysIncludes.addAll(CndFileUtils.toFSPathList(fs, DefaultSystemSettings.getDefault().getSystemIncludes(lang, impl)));
-                sysMacros.addAll(DefaultSystemSettings.getDefault().getSystemMacros(lang, impl));
-            } else {
+            if (itemPrototype != null) {
+                sysIncludes.addAll(itemPrototype.getSystemIncludePaths());
+                sysMacros.addAll(itemPrototype.getSystemMacroDefinitions());
+                usrIncludes.addAll(itemPrototype.getUserIncludePaths());
+                usrMacros.addAll(itemPrototype.getUserMacroDefinitions());
+            } else if (prototype != null) {
                 sysIncludes.addAll(prototype.getSystemIncludePaths());
                 sysMacros.addAll(prototype.getSystemMacroDefinitions());
                 usrIncludes.addAll(prototype.getUserIncludePaths());
                 usrMacros.addAll(prototype.getUserMacroDefinitions());
+            } else  {
+                sysIncludes.addAll(CndFileUtils.toFSPathList(fs, DefaultSystemSettings.getDefault().getSystemIncludes(lang, impl)));
+                sysMacros.addAll(DefaultSystemSettings.getDefault().getSystemMacros(lang, impl));
             }
             impl.checkPaths();
-            impl.addFile(file);
+            impl.addFile(file, lang, flavor);
             set.add(impl.findFileItem(file));
             return impl;
         }
@@ -425,10 +451,8 @@ public class CsmStandaloneFileProviderImpl extends CsmStandaloneFileProvider {
             }
         }
         
-        private void addFile(FileObject file) {
-            DataObject dobj = NativeProjectProvider.getDataObject(file);
-            NativeFileItem.Language lang = NativeProjectProvider.getLanguage(file, dobj);
-            NativeFileItemImpl item = new NativeFileItemImpl(file, this, lang);
+        private void addFile(FileObject file, NativeFileItem.Language lang, LanguageFlavor flavor) {
+            NativeFileItemImpl item = new NativeFileItemImpl(file, this, lang, flavor);
             //TODO: put item in loockup of DataObject
             // registerItemInDataObject(dobj, item);
             this.files.add(item);
@@ -538,12 +562,14 @@ public class CsmStandaloneFileProviderImpl extends CsmStandaloneFileProvider {
         private final FileObject fileObject;
         private final NativeProjectImpl project;
         private final NativeFileItem.Language lang;
+        private final NativeFileItem.LanguageFlavor flavor;
 
-        public NativeFileItemImpl(FileObject file, NativeProjectImpl project, NativeFileItem.Language language) {
+        public NativeFileItemImpl(FileObject file, NativeProjectImpl project, NativeFileItem.Language language, NativeFileItem.LanguageFlavor flavor) {
 
             this.project = project;
             this.fileObject = file;
             this.lang = language;
+            this.flavor = flavor;
         }
 
         @Override
@@ -604,7 +630,7 @@ public class CsmStandaloneFileProviderImpl extends CsmStandaloneFileProvider {
 
         @Override
         public NativeFileItem.LanguageFlavor getLanguageFlavor() {
-            return NativeFileItem.LanguageFlavor.UNKNOWN;
+            return flavor;
         }
 
         @Override
