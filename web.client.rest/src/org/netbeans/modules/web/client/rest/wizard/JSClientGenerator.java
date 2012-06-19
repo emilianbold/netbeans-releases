15abcfc7c3982a9e19652dc89cc9195f87114f6b
Denis Anisimov <ads@netbeans.org>
2012-06-19 10:40:30 +0400

    Basic generation based on specific GET methods.

--- web.client.rest/src/org/netbeans/modules/web/client/rest/wizard/JSClientGenerator.java
+++ web.client.rest/src/org/netbeans/modules/web/client/rest/wizard/JSClientGenerator.java
@@ -45,10 +45,12 @@ package org.netbeans.modules.web.client.rest.wizard;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.logging.Level;
@@ -58,18 +60,29 @@ import javax.lang.model.element.AnnotationMirror;
 import javax.lang.model.element.AnnotationValue;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ExecutableElement;
+import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
 import javax.lang.model.element.VariableElement;
 import javax.lang.model.type.DeclaredType;
+import javax.lang.model.type.TypeKind;
 import javax.lang.model.type.TypeMirror;
 import javax.lang.model.util.ElementFilter;
+import javax.swing.text.BadLocationException;
 
 import org.netbeans.api.java.source.CompilationController;
 import org.netbeans.api.java.source.JavaSource;
 import org.netbeans.api.java.source.Task;
 import org.netbeans.api.java.source.JavaSource.Phase;
+import org.netbeans.api.project.FileOwnerQuery;
+import org.netbeans.api.project.Project;
+import org.netbeans.editor.BaseDocument;
+import org.netbeans.modules.editor.indent.api.Indent;
 import org.netbeans.modules.websvc.rest.model.api.RestServiceDescription;
+import org.netbeans.modules.websvc.rest.spi.RestSupport;
+import org.openide.cookies.EditorCookie;
 import org.openide.filesystems.FileObject;
+import org.openide.loaders.DataObject;
+import org.openide.loaders.DataObjectNotFoundException;
 
 
 /**
@@ -78,6 +91,8 @@ import org.openide.filesystems.FileObject;
  */
 class JSClientGenerator {
     
+    private static final String SLASH = "/";                        // NOI18N
+
     private static final Logger LOG = Logger.getLogger( JSClientGenerator.class.getName()); 
     
     private static final String PATH = "javax.ws.rs.Path";           // NOI18N
@@ -90,6 +105,10 @@ class JSClientGenerator {
     private static final String CONSUMES = "javax.ws.rs.Consumes";   // NOI18N
     private static final String JSON = "application/json";           // NOI18N
     
+    private static final String XML_ROOT_ELEMENT = 
+        "javax.xml.bind.annotation.XmlRootElement";                  // NOI18N
+    private static final String ID = "javax.persistence.Id";         // NOI18N
+    
     private JSClientGenerator(RestServiceDescription description){
         myDescription = description;
     }
@@ -99,12 +118,12 @@ class JSClientGenerator {
         return new JSClientGenerator(description);
     }
 
-    public void generate( FileObject jsFile) {
+    public void generate( final FileObject jsFile) {
         FileObject restSource = myDescription.getFile();
         if ( restSource == null ){
             return;
         }
-        StringBuilder builder = new StringBuilder("$(function(){\n");
+        myContent = new StringBuilder("$(function(){\n");
         JavaSource javaSource = JavaSource.forFileObject( restSource);
         final String restClass = myDescription.getClassName();
         Task<CompilationController> task = new Task<CompilationController>(){
@@ -142,8 +161,13 @@ class JSClientGenerator {
                     }
                 }
                 
-                handleRestMethods(controller , getMethods, postMethods, 
+                try {
+                    handleRestMethods(controller , getMethods, postMethods, 
                         putMethods, deleteMethods);
+                }
+                catch( IOException e ){
+                    LOG.log(Level.WARNING , null ,e );
+                }
             }
 
         };
@@ -160,14 +184,56 @@ class JSClientGenerator {
         catch (ExecutionException e) {
             LOG.log(Level.INFO , null ,e );
         }
-        builder.append("});");
+        
+        myContent.append("});");
+        
+
+        try {
+            DataObject jsDo = DataObject.find(jsFile);
+            EditorCookie cookie = jsDo.getCookie(EditorCookie.class);
+            cookie.open();
+            final BaseDocument document = (BaseDocument) cookie.openDocument();
+
+            final Indent indent = Indent.get(document);
+            indent.lock();
+            try {
+                document.runAtomic(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        int position = document.getLength();
+                        try {
+                            document.insertString(position,
+                                    myContent.toString(), null);
+
+                            indent.reindent(0, document.getLength());
+                        }
+                        catch (BadLocationException e) {
+                            LOG.log(Level.WARNING, null, e);
+                        }
+                    }
+                });
+            }
+            finally {
+                indent.unlock();
+            }
+            cookie.saveDocument();
+
+        }
+        catch (DataObjectNotFoundException e) {
+            LOG.log(Level.WARNING, null, e);
+        }
+        catch (IOException e) {
+            LOG.log(Level.WARNING, null, e);
+        }
+        
     }
     
     private void handleRestMethods( CompilationController controller,  
             List<ExecutableElement> getMethods,
             List<ExecutableElement> postMethods,
             List<ExecutableElement> putMethods,
-            List<ExecutableElement> deleteMethods )
+            List<ExecutableElement> deleteMethods ) throws IOException
     {
         Map<String,ExecutableElement> noParamGetMethods = 
             new HashMap<String, ExecutableElement>();
@@ -183,6 +249,9 @@ class JSClientGenerator {
             AnnotationMirror annotation = getAnnotation(method, PATH);
             String path = getValue( annotation );
             if ( parameters.size() == 0 ){
+                if ( path == null ){
+                    path = "";
+                }
                 noParamGetMethods.put( path, method );
                 continue;
             }
@@ -195,6 +264,7 @@ class JSClientGenerator {
             String pathNoParam = removeParamTemplate( path , getValue(annotation));
             oneParamGetMethods.put(pathNoParam, method );
         }
+        Map<String,String> fqn2Path = new HashMap<String, String>();
         for(Entry<String,ExecutableElement> entry : noParamGetMethods.entrySet()){
             String path = entry.getKey();
             ExecutableElement method = entry.getValue();
@@ -202,14 +272,207 @@ class JSClientGenerator {
             TypeMirror returnType = method.getReturnType();
             TypeMirror entityCollectionType = getCollectionType(returnType, controller);
             if ( entityCollectionType == null ){
-                // collection of entities
+                // just plain entity
+                
             }
             else {
-                // just plain entity
+                // collection of entities
+                Element entityType = controller.getTypes().asElement(entityCollectionType);
+                if ( entityType instanceof TypeElement ){
+                    String fqn = ((TypeElement)entityType).getQualifiedName().toString();
+                    fqn2Path.put(fqn, path);
+                }
+            }
+        }
+        for(Entry<String,ExecutableElement> entry : oneParamGetMethods.entrySet()){
+            String path = entry.getKey();
+            ExecutableElement method = entry.getValue();
+            
+            TypeMirror returnType = method.getReturnType();
+            Element returnElement = controller.getTypes().asElement( returnType );
+            if ( returnElement instanceof TypeElement ){
+                // TODO: return type could be a promitive type. How it should be handled ?
+                if ( getAnnotation( returnElement, XML_ROOT_ELEMENT) == null ){
+                    /* TODO : here is only @XmlRootElement annotated elements
+                     * are considered as JSON serializable ( that's true for
+                     * NB generated entities ) but there could be probably
+                     * other ways to serialize ( read/write REST providers )
+                     * POJO classes     
+                     */
+                    continue;
+                }
+                String fqn = ((TypeElement)returnElement).getQualifiedName().toString();
+                String collectionPath = fqn2Path.get(fqn);
+                generateBackendModel( (TypeElement)returnElement , path , 
+                        collectionPath, controller );
             }
         }
     }
     
+    private void generateBackendModel( TypeElement entity, String path,
+            String collectionPath, CompilationController controller ) throws IOException
+    {
+        String fqn = entity.getQualifiedName().toString();
+        String name = entity.getSimpleName().toString();
+        String modelName = suggestModelName(name );
+        
+        myContent.append("\n// Model for ");                    // NOI18N
+        if ( name.equals(modelName)){
+            myContent.append( name );
+        }
+        else {
+            myContent.append( fqn );
+        }
+        myContent.append(" entity\n");                          // NOI18N
+        
+        myContent.append("window.");
+        myContent.append(modelName);
+        myContent.append(" = Backbone.Model.extend({\n");       // NOI18N
+        myContent.append("urlRoot : \"");                       // NOI18N
+        myContent.append( getUrl( path ));
+        myContent.append("\"");                                 // NOI18N
+        String parsedData = parse(entity, controller);
+        if ( parsedData != null ){
+            myContent.append(",\n");                              // NOI18N
+            myContent.append(parsedData);
+        }
+        myContent.append("\n});\n\n");                          // NOI18N
+        
+        if ( collectionPath == null){
+            return;
+        }
+        myContent.append("\n// Collection class for ");          // NOI18N
+        if ( name.equals(modelName)){
+            myContent.append( name );
+        }
+        else {
+            myContent.append( entity.getQualifiedName().toString() );
+        }
+        myContent.append(" entities\n");                         // NOI18N
+        myContent.append("window.");
+        myContent.append(modelName);
+        myContent.append("Collection");                         // NOI18N
+        myContent.append(" = Backbone.Collection.extend({\n");  // NOI18N
+        myContent.append("model: ");                            // NOI18N
+        myContent.append(modelName);
+        myContent.append(",\nurl : \"");                        // NOI18N
+        myContent.append( getUrl( collectionPath ));
+        myContent.append("\"\n"); 
+        myContent.append("});\n\n");                             // NOI18N
+    }
+
+    private String parse( TypeElement entity, CompilationController controller ) {
+        /*
+         *  parse entity and generate attributes:
+         *  1) idAttribute
+         *  2) primitive attributes if any
+         *  3) do not include attributes with complex type  
+         */
+        List<VariableElement> fields = ElementFilter.fieldsIn(entity.getEnclosedElements());
+        VariableElement id = null;
+        for (VariableElement field : fields) {
+            if ( getAnnotation(field, ID) != null ){
+                TypeMirror fieldType = field.asType();
+                if ( fieldType.getKind().isPrimitive() ){
+                    id = field;
+                    break;
+                }
+                Element fieldTypeElement = controller.getTypes().asElement(fieldType);
+                TypeElement stringElement = controller.getElements().
+                    getTypeElement(String.class.getName());
+                if ( stringElement != null && stringElement.equals( fieldTypeElement)){
+                    id = field;
+                    break;
+                }
+                
+                PackageElement pack = controller.getElements().getPackageOf(
+                        fieldTypeElement);
+                if ( pack.getQualifiedName().contentEquals("java.lang")){      // NOI18N
+                    try {
+                        if ( controller.getTypes().unboxedType(fieldType) != null ){
+                            id = field;
+                            break;
+                        }
+                    }
+                    catch(IllegalArgumentException e){
+                        // just skip field
+                    }
+                }
+            }
+        }
+        StringBuilder builder = new StringBuilder();
+        if ( id != null ){
+            String idAttr = id.getSimpleName().toString();
+            builder.append("idAttribute : '");                          // NO18N
+            builder.append(idAttr);
+            builder.append("'");                                        // NO18N
+        }
+        
+        
+        if ( builder.length() >0 ){
+            return builder.toString();
+        }
+        else {
+            return null;
+        }
+    }
+
+    private String getUrl( String relativePath ) throws IOException {
+        Project project = FileOwnerQuery.getOwner(myDescription.getFile());
+        RestSupport restSupport = project.getLookup().lookup(RestSupport.class);
+        String applicationPath = restSupport.getApplicationPath();
+        String uri = myDescription.getUriTemplate();
+        
+        if (applicationPath == null) {
+            applicationPath = uri;
+        }
+        else {
+            applicationPath = addUrlPath(applicationPath, uri);
+        }
+        applicationPath = addUrlPath(applicationPath, relativePath);
+        
+        return addUrlPath(restSupport.getContextRootURL(),applicationPath);            
+    }
+
+    private String addUrlPath( String path, String uri ) {
+        if (uri.startsWith(SLASH)) {
+            if (path.endsWith(SLASH)) {
+                path = path + uri.substring(1);
+            }
+            else {
+                path = path + uri;
+            }
+        }
+        else {
+            if (path.endsWith(SLASH)) {
+                path = path + uri;
+            }
+            else {
+                path = path + SLASH + uri;
+            }
+        }
+        return path;
+    }
+
+    private String suggestModelName( String name ) {
+        if ( myEntities.contains(name)){
+            String newName ;
+            int index =1;
+            while( true ){
+                newName = name+index;
+                if ( !myEntities.contains(newName)){
+                    myEntities.add(newName);
+                    return newName;
+                }
+                index++;
+            }
+        }
+        else {
+            myEntities.add(name);
+        }
+        return name;
+    }
+
     private String removeParamTemplate( String path, String param ) {
         int index = path.indexOf('{');
         String template = path;
@@ -271,6 +534,9 @@ class JSClientGenerator {
     }
     
     private String getValue( AnnotationMirror annotation ){
+        if ( annotation == null ){
+            return null;
+        }
         Map<? extends ExecutableElement, ? extends AnnotationValue> elementValues = 
             annotation.getElementValues();
         for (Entry<? extends ExecutableElement, ? extends AnnotationValue> entry : 
@@ -359,5 +625,7 @@ class JSClientGenerator {
     }
     
     private RestServiceDescription myDescription;
+    private StringBuilder myContent;
+    private Set<String> myEntities  = new HashSet<String>();
 
 }
