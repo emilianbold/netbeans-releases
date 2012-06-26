463c10183576a5098ab589f3950596098d4b7177
Tomas Stupka <tstupka@netbeans.org>
2012-06-26 12:36:43 +0200

    Issue #214689 - incorrect background for Attach log file

--- bugzilla/src/org/netbeans/modules/bugzilla/issue/IssuePanel.java
+++ bugzilla/src/org/netbeans/modules/bugzilla/issue/IssuePanel.java
@@ -1855,6 +1855,7 @@ public class IssuePanel extends javax.swing.JPanel implements Scrollable {
         assignToDefaultCheckBox.setBackground(javax.swing.UIManager.getDefaults().getColor("TextArea.background"));
         org.openide.awt.Mnemonics.setLocalizedText(assignToDefaultCheckBox, org.openide.util.NbBundle.getMessage(IssuePanel.class, "IssuePanel.assignToDefaultCheckBox.text")); // NOI18N
 
+        attachLogCheckBox.setBackground(javax.swing.UIManager.getDefaults().getColor("TextArea.background"));
         org.openide.awt.Mnemonics.setLocalizedText(attachLogCheckBox, org.openide.util.NbBundle.getMessage(IssuePanel.class, "IssuePanel.attachLogCheckBox.text")); // NOI18N
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
