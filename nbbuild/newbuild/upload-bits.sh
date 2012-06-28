5ff96787281842ca406899041167c1b95a2c5b1c
Antonin Nebuzelsky <anebuzelsky@netbeans.org>
2012-06-28 16:21:17 +0200

    7.2 FCS branding.

--- nbbuild/newbuild/upload-bits.sh
+++ nbbuild/newbuild/upload-bits.sh
@@ -5,7 +5,7 @@ set -x
 DIRNAME=`dirname $0`
 cd ${DIRNAME}
 TRUNK_NIGHTLY_DIRNAME=`pwd`
-export BUILD_DESC=7.2rc1
+export BUILD_DESC=7.2
 source init.sh
 
 ssh -p 222 $DIST_SERVER mkdir -p $DIST_SERVER_PATH/.$DATESTAMP
