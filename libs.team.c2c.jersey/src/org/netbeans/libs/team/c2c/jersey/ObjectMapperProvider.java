90c5ab266c2c7358af0a9a9cd92e3a7f62c7008d
Jaroslav Tulach <jtulach@netbeans.org>
2012-07-18 16:17:18 +0200

    Ignore unknown properties. Probably wiser in case ODS adds some new format - the deserialization will not fail.

--- libs.team.c2c.jersey/src/org/netbeans/libs/team/c2c/jersey/ObjectMapperProvider.java
+++ libs.team.c2c.jersey/src/org/netbeans/libs/team/c2c/jersey/ObjectMapperProvider.java
@@ -3,10 +3,12 @@ package org.netbeans.libs.team.c2c.jersey;
 import javax.ws.rs.ext.ContextResolver;
 import javax.ws.rs.ext.Provider;
 import org.codehaus.jackson.annotate.JsonAutoDetect;
+import org.codehaus.jackson.annotate.JsonTypeInfo;
 import org.codehaus.jackson.map.DeserializationConfig;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.SerializationConfig.Feature;
 import org.codehaus.jackson.map.introspect.VisibilityChecker;
+import org.codehaus.jackson.map.jsontype.impl.StdTypeResolverBuilder;
 
 /**
  *
@@ -32,6 +34,7 @@ public class ObjectMapperProvider implements ContextResolver<ObjectMapper> {
 
         ObjectMapper result = new ObjectMapper();
         result.configure(Feature.INDENT_OUTPUT, true);
+        result.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         result.setVisibilityChecker(VisibilityChecker.Std.defaultInstance().withFieldVisibility(JsonAutoDetect.Visibility.ANY));
 //        result.setDeserializationConfig(result.getDeserializationConfig().with(DeserializationConfig.Feature.UNWRAP_ROOT_VALUE));
 
