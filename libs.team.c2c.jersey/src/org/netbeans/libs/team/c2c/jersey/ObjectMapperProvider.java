d6417f93649f26e9cf1ead6004df4f773b6ccef0
Jaroslav Tulach <jtulach@netbeans.org>
2012-07-18 16:32:28 +0200

    NetBeans own connector to deal with Oracle Developer Services via Jersey

--- libs.team.c2c.jersey/src/org/netbeans/libs/team/c2c/jersey/ObjectMapperProvider.java
+++ libs.team.c2c.jersey/src/org/netbeans/libs/team/c2c/jersey/ObjectMapperProvider.java
@@ -3,41 +3,32 @@ package org.netbeans.libs.team.c2c.jersey;
 import javax.ws.rs.ext.ContextResolver;
 import javax.ws.rs.ext.Provider;
 import org.codehaus.jackson.annotate.JsonAutoDetect;
-import org.codehaus.jackson.annotate.JsonTypeInfo;
 import org.codehaus.jackson.map.DeserializationConfig;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.SerializationConfig.Feature;
 import org.codehaus.jackson.map.introspect.VisibilityChecker;
-import org.codehaus.jackson.map.jsontype.impl.StdTypeResolverBuilder;
+import org.openide.util.lookup.ServiceProvider;
 
 /**
  *
  * @author japod
  */
+@ServiceProvider(service=ContextResolver.class)
 @Provider
-public class ObjectMapperProvider implements ContextResolver<ObjectMapper> {
-
-    final ObjectMapper defaultObjectMapper;
+public final class ObjectMapperProvider implements ContextResolver<ObjectMapper> {
+    private final ObjectMapper defaultObjectMapper;
 
     public ObjectMapperProvider() {
-        defaultObjectMapper = createDefaultMapper();
-    }
-
-    @Override
-    public ObjectMapper getContext(Class<?> type) {
-
-        return defaultObjectMapper;
-    }
-
-
-    private static ObjectMapper createDefaultMapper() {
-
         ObjectMapper result = new ObjectMapper();
         result.configure(Feature.INDENT_OUTPUT, true);
         result.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         result.setVisibilityChecker(VisibilityChecker.Std.defaultInstance().withFieldVisibility(JsonAutoDetect.Visibility.ANY));
 //        result.setDeserializationConfig(result.getDeserializationConfig().with(DeserializationConfig.Feature.UNWRAP_ROOT_VALUE));
+        defaultObjectMapper = result;
+    }
 
-        return result;
+    @Override
+    public ObjectMapper getContext(Class<?> type) {
+        return defaultObjectMapper;
     }
 }
