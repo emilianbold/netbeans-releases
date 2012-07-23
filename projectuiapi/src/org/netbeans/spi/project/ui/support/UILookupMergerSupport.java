d8bd077c290f9dfdf112ab55d22f78acd939770b
Tomas Zezula <tzezula@netbeans.org>
2012-07-23 18:45:47 +0200

    #215628:Provide generic API and UI for solving project metadata problems

--- projectuiapi/src/org/netbeans/spi/project/ui/support/UILookupMergerSupport.java
+++ projectuiapi/src/org/netbeans/spi/project/ui/support/UILookupMergerSupport.java
@@ -45,15 +45,26 @@
 
 package org.netbeans.spi.project.ui.support;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.lang.ref.Reference;
+import java.lang.ref.WeakReference;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.LinkedHashSet;
 import java.util.Set;
+import org.netbeans.api.annotations.common.NonNull;
 import org.netbeans.modules.project.uiapi.ProjectOpenedTrampoline;
 import org.netbeans.spi.project.LookupMerger;
 import org.netbeans.spi.project.ui.PrivilegedTemplates;
 import org.netbeans.spi.project.ui.ProjectOpenedHook;
+import org.netbeans.spi.project.ui.ProjectProblemsProvider;
 import org.netbeans.spi.project.ui.RecommendedTemplates;
 import org.openide.util.Lookup;
+import org.openide.util.Parameters;
+import org.openide.util.WeakListeners;
 
 /**
  * Factory class for creation of {@link org.netbeans.spi.project.LookupMerger} instances.
@@ -98,6 +109,20 @@ public final class UILookupMergerSupport {
     public static LookupMerger<ProjectOpenedHook> createProjectOpenHookMerger(ProjectOpenedHook defaultInstance) {
         return new OpenMerger(defaultInstance);
     }
+
+
+    /**
+     * Create a {@link org.netbeans.spi.project.LookupMerger} instance
+     * for {@link org.netbeans.spi.project.ui.ProjectProblemsProvider}. The merger
+     * collects all {@link org.netbeans.spi.project.ui.ProjectProblemsProvider.ProjectProblem}s
+     * from {@link org.netbeans.spi.project.ui.ProjectProblemsProvider}s registered in the
+     * project lookup.
+     * @return instance to include in project lookup
+     * @since  1.60
+     */
+    public static LookupMerger<ProjectProblemsProvider> createProjectProblemsProviderMerger() {
+        return new ProjectProblemsProviderMerger();
+    }
     
     private static class PrivilegedMerger implements LookupMerger<PrivilegedTemplates> {
         public Class<PrivilegedTemplates> getMergeableClass() {
@@ -134,6 +159,20 @@ public final class UILookupMergerSupport {
             return new OpenHookImpl(defaultInstance, lookup);
         }
         
+    }
+
+    private static class ProjectProblemsProviderMerger implements LookupMerger<ProjectProblemsProvider> {
+
+        @Override
+        public Class<ProjectProblemsProvider> getMergeableClass() {
+            return ProjectProblemsProvider.class;
+        }
+
+        @Override
+        public ProjectProblemsProvider merge(Lookup lookup) {
+            return new ProjectProblemsProviderImpl(lookup);
+        }
+
     }
     
     private static class PrivilegedTemplatesImpl implements PrivilegedTemplates {
@@ -215,5 +254,61 @@ public final class UILookupMergerSupport {
         
     }
     
-    
+    private static class ProjectProblemsProviderImpl implements ProjectProblemsProvider, PropertyChangeListener {
+
+        private final Lookup lkp;
+        private final PropertyChangeSupport support;
+        //@GuardedBy("this")
+        private Iterable<? extends Reference<ProjectProblemsProvider>> providers;
+
+
+        ProjectProblemsProviderImpl(@NonNull final Lookup lkp) {
+            Parameters.notNull("lkp", lkp); //NOI18N
+            this.lkp = lkp;
+            this.support = new PropertyChangeSupport(this);
+        }
+
+        @Override
+        public void addPropertyChangeListener(PropertyChangeListener listener) {
+            Parameters.notNull("listener", listener);   //NOI18N
+            support.addPropertyChangeListener(listener);
+        }
+
+        @Override
+        public void removePropertyChangeListener(PropertyChangeListener listener) {
+            Parameters.notNull("listener", listener);   //NOI18N
+            support.removePropertyChangeListener(listener);
+        }
+
+        @Override
+        public Collection<? extends ProjectProblem> getProblems() {
+            final Collection<ProjectProblem> problems = new LinkedHashSet<ProjectProblem>();
+            for (Reference<ProjectProblemsProvider> providerRef : getProviders()) {
+                final ProjectProblemsProvider provider = providerRef.get();
+                if (provider != null) {
+                    problems.addAll(provider.getProblems());
+                }
+            }
+            return Collections.unmodifiableCollection(problems);
+        }
+
+        @Override
+        public void propertyChange(PropertyChangeEvent evt) {
+            if (PROP_PROBLEMS.equals(evt.getPropertyName())) {
+                support.firePropertyChange(PROP_PROBLEMS,null,null);
+            }
+        }
+
+        private synchronized Iterable<? extends Reference<ProjectProblemsProvider>> getProviders() {
+            if (providers == null) {
+                final Collection<Reference<ProjectProblemsProvider>> newProviders = new LinkedHashSet<Reference<ProjectProblemsProvider>>();
+                for (ProjectProblemsProvider provider : lkp.lookupAll(ProjectProblemsProvider.class)) {
+                    provider.addPropertyChangeListener(WeakListeners.propertyChange(this, provider));
+                    newProviders.add(new WeakReference<ProjectProblemsProvider>(provider));
+                }
+                providers = newProviders;
+            }
+            return providers;
+        }
+    }
 }
