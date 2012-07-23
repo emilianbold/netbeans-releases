97a442dbd2676276572e5883e2a50278fd81e579
Libor Fischmeistr <lfischmeistr@netbeans.org>
2012-07-23 16:51:24 +0200

    #215069: Standalone FX SDK preferred over JDK integrated (transplanted from 10a681d83267565439d2e33bc78d2e2468b10854)

--- installer/engine/src/org/netbeans/installer/utils/applications/JavaFXUtils.java
+++ installer/engine/src/org/netbeans/installer/utils/applications/JavaFXUtils.java
@@ -38,25 +38,31 @@
  */
 package org.netbeans.installer.utils.applications;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.netbeans.installer.utils.FileUtils;
 import org.netbeans.installer.utils.LogManager;
+import org.netbeans.installer.utils.StringUtils;
 import org.netbeans.installer.utils.SystemUtils;
 import org.netbeans.installer.utils.exceptions.NativeException;
 import org.netbeans.installer.utils.helper.Platform;
 import org.netbeans.installer.utils.helper.PlatformConstants;
 import org.netbeans.installer.utils.helper.Version;
 import org.netbeans.installer.utils.system.WindowsNativeUtils;
-import static org.netbeans.installer.utils.system.windows.WindowsRegistry.HKLM;
 import org.netbeans.installer.utils.system.windows.WindowsRegistry;
+import static org.netbeans.installer.utils.system.windows.WindowsRegistry.HKLM;
 
 public class JavaFXUtils {
     /////////////////////////////////////////////////////////////////////////////////
     // Static    
 
-    public static String getJavaFXSDKInstallationPath (Platform platform) {
+    public static String getJavaFXSDKInstallationPath (Platform platform) {        
         return getFXRegistryValue(platform, FXSDK_KEY, FX_SDK_HOME_PATH);
     }
 
-    public static String getJavaFXRuntimeInstallationPath (Platform platform) {
+    public static String getJavaFXRuntimeInstallationPath (Platform platform) {     
         return getFXRegistryMaxValue(platform, FXRUNTIME_INSTALLATION_KEY);
     }
 
@@ -128,6 +134,75 @@ public class JavaFXUtils {
             LogManager.log(e);
         }
         return result;
+    }
+    
+    public static boolean jdkContainsJavaFX(File jdkLocation) {
+        File javaFXSDK = new File(jdkLocation, "lib" + File.separator + "ant-javafx.jar");
+        if (!javaFXSDK.exists())
+            return false;
+        
+        File javaFXRuntimeFolder = new File(jdkLocation, "jre" + File.separator + "lib");
+        
+        File javaFXRuntimeDeply = new File(javaFXRuntimeFolder, "deploy.jar");
+        if (!javaFXRuntimeDeply.exists())
+            return false;
+        
+        File javaFXRuntimeJavaws = new File(javaFXRuntimeFolder, "javaws.jar");
+        if (!javaFXRuntimeJavaws.exists())
+            return false;
+        
+        File javaFXRuntimeJfxrt = new File(javaFXRuntimeFolder, "jfxrt.jar");
+        if (!javaFXRuntimeJfxrt.exists())
+            return false;
+        
+        File javaFXRuntimePlugin = new File(javaFXRuntimeFolder, "plugin.jar");
+        if (!javaFXRuntimePlugin.exists())
+            return false;
+        
+        return true;
+    }
+    
+    /**
+     * Registers JavaFX into NetBeans IDE.
+     * 
+     * CAN BE CALLED AFTER IDE IS INSTALLED!!!
+     * 
+     * @param nbLocation
+     * @param sdkLocation
+     * @param reLocation
+     * @return
+     * @throws IOException 
+     */
+    public static boolean registerJavaFX(File nbLocation, File sdkLocation, File reLocation) throws IOException {
+        File javaExe = JavaUtils.getExecutable(new File(System.getProperty("java.home")));
+        String [] cp = {
+            "platform/core/core.jar",
+            "platform/lib/boot.jar",
+            "platform/lib/org-openide-modules.jar",
+            "platform/core/org-openide-filesystems.jar",
+            "platform/lib/org-openide-util.jar",
+            "platform/lib/org-openide-util-lookup.jar",
+            "javafx/modules/org-netbeans-modules-javafx2-platform.jar"
+        };
+        for(String c : cp) {
+            File f = new File(nbLocation, c);
+            if(!FileUtils.exists(f)) {
+                LogManager.log("... cannot find jar required for JavaFX integration: " + f);
+                return false;
+            }
+        }
+        String mainClass = "org.netbeans.modules.javafx2.platform.registration.AutomaticRegistration";
+        List <String> commands = new ArrayList <String> ();
+        File nbCluster = new File(nbLocation, "nb");
+        commands.add(javaExe.getAbsolutePath());
+        commands.add("-cp");
+        commands.add(StringUtils.asString(cp, File.pathSeparator));
+        commands.add(mainClass);        
+        commands.add(nbCluster.getAbsolutePath());     
+        commands.add(sdkLocation.getAbsolutePath());
+        commands.add(reLocation.getAbsolutePath());
+        
+        return SystemUtils.executeCommand(nbLocation, commands.toArray(new String[]{})).getErrorCode() == 0;
     }
 
     /////////////////////////////////////////////////////////////////////////////////
