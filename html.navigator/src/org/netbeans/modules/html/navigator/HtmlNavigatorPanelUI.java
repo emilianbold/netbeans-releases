276f7ccfdb2774762e9cf7d0bd3770cb59f78ebe
Marek Fukala <mfukala@netbeans.org>
2012-08-02 14:40:40 +0200

    Progress on the merged html view. Now shows remote content not present in the static page. The code is not completed, it is very buggy!!!

--- html.navigator/src/org/netbeans/modules/html/navigator/HtmlNavigatorPanelUI.java
+++ html.navigator/src/org/netbeans/modules/html/navigator/HtmlNavigatorPanelUI.java
@@ -217,7 +217,12 @@ public class HtmlNavigatorPanelUI extends JPanel implements ExplorerManager.Prov
     }
     
     public void selectElementNode(final int offset) {
-        final Node match = getRootNode().getNodeForOffset(offset);
+        HtmlNode root = getRootNode();
+        if(root == null) {
+            return ;
+        }
+        
+        final Node match = root.getNodeForOffset(offset);
         if(match == null) {
             return ;
         }
--- html.navigator/src/org/netbeans/modules/html/navigator/HtmlNode.java
+++ html.navigator/src/org/netbeans/modules/html/navigator/HtmlNode.java
@@ -44,8 +44,6 @@
 package org.netbeans.modules.html.navigator;
 
 import java.awt.Image;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
@@ -71,58 +69,82 @@ import org.openide.util.ImageUtilities;
 public class HtmlNode extends AbstractNode {
 
     private static final Image ICON = ImageUtilities.loadImage("org/netbeans/modules/html/navigator/resources/html_element.png"); //NOI18N
-    
-    private HtmlElementDescription element;
     private HtmlNavigatorPanelUI ui;
     private FileObject fileObject; // For the root description
-
     private Action openAction;
     private Action highlightInBrowserAction;
-    
     private Node domCounterpart;
+    //static description
+    private HtmlElementDescription element;
+    //dynamic description
+    private WebKitNodeDescription webKitNodeDescription;
 
     public HtmlNode(HtmlElementDescription element, HtmlNavigatorPanelUI ui, FileObject fileObject) {
-        super(element.isLeaf()
-                ? Children.LEAF
-                : new ElementChildren(element.getChildren(), ui, fileObject));
+//        super(element.isLeaf()
+//                ? Children.LEAF
+//                : new ElementChildren(ui, fileObject));
+        super(new ElementChildren(ui, fileObject));
+
         this.element = element;
         this.ui = ui;
         this.fileObject = fileObject;
-        
+
         openAction = new OpenAction(element);
         highlightInBrowserAction = new HighlightInBrowserAction(element, ui);
-        
-        if(ui.getPageModel() != null) {
+
+        getElementChildren().setStaticKeys(element.getChildren());
+
+        if (ui.getPageModel() != null) {
             domCounterpart = Utils.findNode(ui.getPageModel().getDocumentNode(), element);
+            if (domCounterpart != null) {
+                webKitNodeDescription = WebKitNodeDescription.forNode(domCounterpart);
+                Collection<? extends Description> dynamicKeys = webKitNodeDescription.getChildren();
+                getElementChildren().setDynamicKeys(dynamicKeys);
+            }
         }
         highlightInBrowserAction.setEnabled(domCounterpart != null);
+
+        getElementChildren().resetKeys();
+
     }
-    
+
+    private ElementChildren getElementChildren() {
+        Children ch = getChildren();
+        return ch == Children.LEAF ? null : (ElementChildren) getChildren();
+    }
+
     //recursively refreshes the DOM counterpart status
     void refreshDOMStatus() {
         Node match = Utils.findNode(ui.getPageModel().getDocumentNode(), element);
-        if(domCounterpart == null && match != null || domCounterpart != null && match == null) {
+        if (domCounterpart == null && match != null || domCounterpart != null && match == null) {
             String oldHtmlDisplayName = getHtmlDisplayName();
             domCounterpart = match;
             fireDisplayNameChange(oldHtmlDisplayName, getHtmlDisplayName());
             highlightInBrowserAction.setEnabled(domCounterpart != null);
+
+
         }
-        
-        for(Node child : getChildren().getNodes()) {
-            ((HtmlNode)child).refreshDOMStatus();
+
+        webKitNodeDescription = domCounterpart == null ? null : WebKitNodeDescription.forNode(domCounterpart);
+        getElementChildren().setDynamicKeys(webKitNodeDescription == null ? Collections.<Description>emptyList() : webKitNodeDescription.getChildren());
+        getElementChildren().resetKeys();
+
+        for (Node child : getChildren().getNodes(true)) {
+            if (child instanceof HtmlNode) {
+                ((HtmlNode) child).refreshDOMStatus();
+            }
         }
     }
-    
+
     @Override
     public PropertySet[] getPropertySets() {
-        if(getElement().getType() == ElementType.OPEN_TAG) {
+        if (getElement().getElementType() == ElementType.OPEN_TAG) {
             final AtomicReference<HtmlElementProperties.PropertiesPropertySet> pset_ref = new AtomicReference<HtmlElementProperties.PropertiesPropertySet>();
             try {
                 getElement().runTask(new HtmlElementDescription.Task() {
-
                     @Override
                     public void run(HtmlParserResult result) {
-                        OpenTag openTag = (OpenTag)getElement().resolve(result);
+                        OpenTag openTag = (OpenTag) getElement().resolve(result);
                         HtmlElementProperties.PropertiesPropertySet pset = new HtmlElementProperties.PropertiesPropertySet(result, openTag);
                         pset_ref.set(pset);
                     }
@@ -131,12 +153,12 @@ public class HtmlNode extends AbstractNode {
                 Exceptions.printStackTrace(ex);
             }
             HtmlElementProperties.PropertiesPropertySet pset = pset_ref.get();
-            if(pset == null) {
+            if (pset == null) {
                 return super.getPropertySets();
             } else {
                 return new PropertySet[]{pset};
             }
-            
+
         } else {
             return super.getPropertySets();
         }
@@ -150,28 +172,28 @@ public class HtmlNode extends AbstractNode {
     @Override
     public String getHtmlDisplayName() {
         StringBuilder b = new StringBuilder();
-        if(domCounterpart != null) {
+        if (domCounterpart != null) {
             b.append("<b>");
         }
         b.append(getDisplayName());
-        if(domCounterpart != null) {
+        if (domCounterpart != null) {
             b.append("</b>");
         }
-        if(getElement().getIdAttr() != null) {
+        if (getElement().getIdAttr() != null) {
             b.append("&nbsp;");
             b.append("<font color=\"#777777\">");
             b.append("id=");
             b.append(getElement().getIdAttr());
             b.append("</font>");
         }
-        if(getElement().getClassAttr() != null) {
+        if (getElement().getClassAttr() != null) {
             b.append("&nbsp;");
             b.append("<font color=\"#777777\">");
             b.append("class=");
             b.append(getElement().getClassAttr());
             b.append("</font>");
         }
-        
+
         return b.toString();
     }
 
@@ -184,8 +206,7 @@ public class HtmlNode extends AbstractNode {
     public Image getOpenedIcon(int type) {
         return ICON;
     }
-    
-    
+
     @Override
     public Action[] getActions(boolean context) {
 
@@ -233,7 +254,6 @@ public class HtmlNode extends AbstractNode {
 //            }
 //        }
 //    }
-
     public HtmlNode getNodeForOffset(int offset) {
         if (getElement().getFrom() > offset) {
             return null;
@@ -245,12 +265,14 @@ public class HtmlNode extends AbstractNode {
         if (ch instanceof ElementChildren) {
             Node[] children = ch.getNodes();
             for (int i = 0; i < children.length; i++) {
-                HtmlNode c = (HtmlNode) children[i];
-                long start = c.getElement().getFrom();
-                if (start <= offset) {
-                    long end = c.getElement().getTo();
-                    if (end >= offset) {
-                        return c.getNodeForOffset(offset);
+                if (children[i] instanceof HtmlNode) {
+                    HtmlNode c = (HtmlNode) children[i];
+                    long start = c.getElement().getFrom();
+                    if (start <= offset) {
+                        long end = c.getElement().getTo();
+                        if (end >= offset) {
+                            return c.getNodeForOffset(offset);
+                        }
                     }
                 }
             }
@@ -267,62 +289,63 @@ public class HtmlNode extends AbstractNode {
     }
 
     private void updateRecursively(HtmlElementDescription newDescription, List<Node> nodesToExpand, List<Node> nodesToExpandRec) {
-        Children ch = getChildren();
+        ElementChildren ch = getElementChildren();
 
-        //If a node that was a LEAF now has children the child type has to be changed from Children.LEAF
-        //to ElementChildren to be able to hold the new child data
-        if (!(ch instanceof ElementChildren) && !newDescription.isLeaf()) {
-            ch = new ElementChildren(Collections.<HtmlElementDescription>emptyList(), ui, fileObject);
-            setChildren(ch);
-        }
-
-        if (ch instanceof ElementChildren) {
-            Set<HtmlElementDescription> oldSubs = new HashSet<HtmlElementDescription>(element.getChildren());
+//        //If a node that was a LEAF now has children the child type has to be changed from Children.LEAF
+//        //to ElementChildren to be able to hold the new child data
+//        if (!(ch instanceof ElementChildren) && !newDescription.isLeaf()) {
+//            ch = new ElementChildren(ui, fileObject);
+//            setChildren(ch);
+//        }
 
 
-            // Create a hashtable which maps StructureItem to node.
-            // We will then identify the nodes by the description. The trick is 
-            // that the new and old description are equal and have the same hashcode
-            Node[] nodes = ch.getNodes(true);
-            HashMap<HtmlElementDescription, HtmlNode> oldD2node = new HashMap<HtmlElementDescription, HtmlNode>();
-            for (Node node : nodes) {
+        Set<HtmlElementDescription> oldSubs = new HashSet<HtmlElementDescription>(element.getChildren());
+
+        // Create a hashtable which maps StructureItem to node.
+        // We will then identify the nodes by the description. The trick is 
+        // that the new and old description are equal and have the same hashcode
+        Node[] nodes = ch.getNodes(true);
+        HashMap<HtmlElementDescription, HtmlNode> oldD2node = new HashMap<HtmlElementDescription, HtmlNode>();
+        for (Node node : nodes) {
+            if (node instanceof HtmlNode) {
                 oldD2node.put(((HtmlNode) node).element, (HtmlNode) node);
             }
+        }
 
-            List<HtmlElementDescription> newDescriptionChildren = newDescription.getChildren();
-            // Now refresh keys
-            ((ElementChildren) ch).resetKeys(newDescriptionChildren);
+        List<HtmlElementDescription> newDescriptionChildren = newDescription.getChildren();
+        // Now refresh keys
+        ch.setStaticKeys(newDescription.getChildren());
+        ch.resetKeys();
 
+        // Reread nodes
+        nodes = ch.getNodes(true);
 
-            // Reread nodes
-            nodes = ch.getNodes(true);
-
-            for (HtmlElementDescription newSub : newDescriptionChildren ) {
-                HtmlNode node = oldD2node.get(newSub);
-                if (node != null) { 
-                    //the node already existed in the old node children
-                    if (!oldSubs.contains(newSub)) {
-                        nodesToExpand.add(node);
-                    }
-                    // update the node recursively
-                    node.updateRecursively(newSub, nodesToExpand, nodesToExpandRec); 
-                } else { 
-                    // a new node
-                    for (Node newNode : nodes) {
-                        if (newNode instanceof HtmlNode && ((HtmlNode) newNode).getElement() == newSub) {
-                            //recursively expand the new nodes
-                            nodesToExpandRec.add(newNode);
-                            break;
-                        }
+        for (HtmlElementDescription newSub : newDescriptionChildren) {
+            HtmlNode node = oldD2node.get(newSub);
+            if (node != null) {
+                //the node already existed in the old node children
+                if (!oldSubs.contains(newSub)) {
+                    nodesToExpand.add(node);
+                }
+                // update the node recursively
+                node.updateRecursively(newSub, nodesToExpand, nodesToExpandRec);
+            } else {
+                // a new node
+                for (Node newNode : nodes) {
+                    if (newNode instanceof HtmlNode && ((HtmlNode) newNode).getElement() == newSub) {
+                        //recursively expand the new nodes
+                        nodesToExpandRec.add(newNode);
+                        break;
                     }
                 }
             }
-            
         }
+
+
 
         String oldHtml = element.getName();
         String descHtml = newDescription.getName();
-        
+
         if (oldHtml != null && !oldHtml.equals(descHtml)) {
             // Different headers => we need to fire displayname change
             fireDisplayNameChange(oldHtml, descHtml);
@@ -339,26 +362,45 @@ public class HtmlNode extends AbstractNode {
         return fileObject;
     }
 
-    private static final class ElementChildren extends Children.Keys<HtmlElementDescription> {
+    private static class ElementChildren extends Children.Keys<Description> {
 
         private HtmlNavigatorPanelUI ui;
         private FileObject fileObject;
+        private Collection<? extends Description> staticKeys = new HashSet<Description>();
+        private Collection<? extends Description> dynamicKeys = new HashSet<Description>();
 
-        public ElementChildren(Collection<HtmlElementDescription> descriptions, HtmlNavigatorPanelUI ui, FileObject fileObject) {
-            resetKeys(descriptions);
+        public ElementChildren(HtmlNavigatorPanelUI ui, FileObject fileObject) {
             this.ui = ui;
             this.fileObject = fileObject;
         }
 
         @Override
-        protected Node[] createNodes(HtmlElementDescription key) {
-            return new Node[]{new HtmlNode(key, ui, fileObject)};
+        protected Node[] createNodes(Description key) {
+            switch (key.getType()) {
+                case Description.STATIC_NODE:
+                    return new Node[]{new HtmlNode((HtmlElementDescription) key, ui, fileObject)};
+                case Description.DYNAMIC_NODE:
+                    return new Node[]{new WebKitNode((WebKitNodeDescription) key)};
+                default:
+                    return null;
+            }
         }
 
-        void resetKeys(Collection<HtmlElementDescription> descriptions) {
-            setKeys(descriptions);
+        void setStaticKeys(Collection<? extends Description> staticDescriptions) {
+            staticKeys = staticDescriptions;
+        }
+
+        void setDynamicKeys(Collection<? extends Description> dynamicDescriptions) {
+            dynamicKeys = dynamicDescriptions;
+        }
+
+        void resetKeys() {
+            Set<Description> set = new HashSet<Description>();
+            //static keys have precendence
+            set.addAll(staticKeys);
+            set.addAll(dynamicKeys);
+
+            setKeys(set);
         }
     }
-
-    
 }
