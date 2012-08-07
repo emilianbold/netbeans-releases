415a5571d1f3e08af5d4990b04dd687421d40509
Martin Janicek <mjanicek@netbeans.org>
2012-08-07 15:50:06 +0200

    Constructor calls also in find type usages

--- groovy.refactoring/src/org/netbeans/modules/groovy/refactoring/findusages/impl/FindTypeUsages.java
+++ groovy.refactoring/src/org/netbeans/modules/groovy/refactoring/findusages/impl/FindTypeUsages.java
@@ -50,6 +50,7 @@ import org.codehaus.groovy.ast.ModuleNode;
 import org.codehaus.groovy.ast.Parameter;
 import org.codehaus.groovy.ast.PropertyNode;
 import org.codehaus.groovy.ast.expr.ClassExpression;
+import org.codehaus.groovy.ast.expr.ConstructorCallExpression;
 import org.codehaus.groovy.ast.expr.DeclarationExpression;
 import org.codehaus.groovy.ast.stmt.ForStatement;
 import org.netbeans.modules.csl.api.ElementKind;
@@ -141,6 +142,14 @@ public class FindTypeUsages extends AbstractFindUsages {
                 addIfEquals(param);
             }
             super.visitConstructorOrMethod(method, isConstructor);
+        }
+
+        @Override
+        public void visitConstructorCallExpression(ConstructorCallExpression call) {
+            if (isEquals(call.getType())) {
+                usages.add(call);
+            }
+            super.visitConstructorCallExpression(call);
         }
 
         @Override
