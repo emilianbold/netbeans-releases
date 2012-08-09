c0ff4405613ec15bd400d78e6df752eca299edcc
Martin Fousek <marfous@netbeans.org>
2012-08-09 14:22:37 +0200

    revert of the #47f82a1f20d2 changeset

--- javascript2.editor/src/org/netbeans/modules/javascript2/editor/model/impl/ModelVisitor.java
+++ javascript2.editor/src/org/netbeans/modules/javascript2/editor/model/impl/ModelVisitor.java
@@ -48,11 +48,7 @@ import com.oracle.nashorn.parser.TokenType;
 import java.util.*;
 import org.netbeans.modules.csl.api.Modifier;
 import org.netbeans.modules.csl.api.OffsetRange;
-import org.netbeans.modules.javascript2.editor.doc.DocumentationUtils;
-import org.netbeans.modules.javascript2.editor.doc.api.JsDocumentationSupport;
 import org.netbeans.modules.javascript2.editor.doc.spi.DocParameter;
-import org.netbeans.modules.javascript2.editor.doc.spi.JsComment;
-import org.netbeans.modules.javascript2.editor.doc.spi.JsDocumentationHolder;
 import org.netbeans.modules.javascript2.editor.lexer.LexUtilities;
 import org.netbeans.modules.javascript2.editor.model.*;
 import org.netbeans.modules.javascript2.editor.parser.JsParserResult;
@@ -482,7 +478,7 @@ public class ModelVisitor extends PathNodeVisitor {
                 List<Type> types = parserResult.getDocumentationHolder().getReturnType(functionNode);
                 if (types != null && !types.isEmpty()) {
                     for(Type type : types) {
-                        fncScope.addReturnType(new TypeUsageImpl(type.getType(), type.getOffset(), true));
+                        fncScope.addReturnType(new TypeUsageImpl(type.getType(), -1, true));
                     }
                 }
                 if (fncScope.areReturnTypesEmpty()) {
@@ -915,17 +911,6 @@ public class ModelVisitor extends PathNodeVisitor {
             scope = scope.getInScope();
         }
         if (property != null) {
-
-            // occurence in the doc
-            JsComment comment = JsDocumentationSupport.getCommentForOffset(parserResult, property.getOffset());
-            if (comment != null) {
-                for (DocParameter docParameter : comment.getParameters()) {
-                    if (docParameter.getParamName().getName().equals(iNode.getName())) {
-                        ((JsObjectImpl)property).addOccurrence(DocumentationUtils.getOffsetRange(docParameter));
-                    }
-                }
-            }
-
             ((JsObjectImpl)property).addOccurrence(ModelUtils.documentOffsetRange(parserResult, iNode.getStart(), iNode.getFinish()));
         } else {
             // it's a new global variable?
