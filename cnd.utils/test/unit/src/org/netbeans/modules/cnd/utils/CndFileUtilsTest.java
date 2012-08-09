0e11248df98c0fa78729345e0cdc9272446ca73e
Egor Ushakov <gorrus@netbeans.org>
2012-08-09 20:01:52 +0400

    first simple test for files Flags updates

--- cnd.utils/test/unit/src/org/netbeans/modules/cnd/utils/CndFileUtilsTest.java
+++ cnd.utils/test/unit/src/org/netbeans/modules/cnd/utils/CndFileUtilsTest.java
@@ -41,16 +41,22 @@ import java.io.File;
 import java.io.IOException;
 import junit.framework.Assert;
 import org.junit.Test;
+import org.netbeans.junit.NbTestCase;
 import org.netbeans.modules.cnd.utils.cache.CndFileUtils;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
-import org.openide.util.Utilities;
 
 /**
  *
  * @author vv159170
  */
-public class CndFileUtilsTest {
+public class CndFileUtilsTest extends NbTestCase {
+
+    public CndFileUtilsTest(String name) {
+        super(name);
+        System.setProperty("cnd.modelimpl.assert.notfound", "true");
+    }
+    
     @Test
     public void testLocalUrlToFileObject() throws IOException {
         File temp = File.createTempFile("urlToFileObject", ".txt");
@@ -78,5 +84,40 @@ public class CndFileUtilsTest {
         Assert.assertNull(CndFileUtils.urlToFileObject("file:/" + escapedPath));
         Assert.assertEquals(fo, CndFileUtils.urlToFileObject("file://" + escapedPath));
         temp.delete();
+    }
+    
+    @Test
+    public void testFlagsUpdates() throws IOException {
+        clearWorkDir();
+        FileObject workDirFO = FileUtil.toFileObject(getWorkDir());
+        int i = 0, MAX_ITER = 100;
+        while (i++ < MAX_ITER) {
+            // create folders and files
+            FileObject parent = workDirFO.createFolder("parent");
+            assertTrue(CndFileUtils.isExistingDirectory(CndFileUtils.getLocalFileSystem(), parent.getPath()));
+            
+            FileObject child = parent.createFolder("child");
+            assertTrue(CndFileUtils.isExistingDirectory(CndFileUtils.getLocalFileSystem(), child.getPath()));
+            assertTrue(CndFileUtils.isExistingDirectory(CndFileUtils.getLocalFileSystem(), parent.getPath()));
+            
+            FileObject file = parent.createData("test", "c");
+            assertTrue(CndFileUtils.isExistingFile(file.getPath()));
+            assertTrue(CndFileUtils.isExistingDirectory(CndFileUtils.getLocalFileSystem(), child.getPath()));
+            assertTrue(CndFileUtils.isExistingDirectory(CndFileUtils.getLocalFileSystem(), parent.getPath()));
+            
+            // delete folders and files
+            file.delete();
+            assertFalse(CndFileUtils.isExistingFile(file.getPath()));
+            assertTrue(CndFileUtils.isExistingDirectory(CndFileUtils.getLocalFileSystem(), child.getPath()));
+            assertTrue(CndFileUtils.isExistingDirectory(CndFileUtils.getLocalFileSystem(), parent.getPath()));
+            
+            child.delete();
+            assertFalse(CndFileUtils.isExistingDirectory(CndFileUtils.getLocalFileSystem(), child.getPath()));
+            assertTrue(CndFileUtils.isExistingDirectory(CndFileUtils.getLocalFileSystem(), parent.getPath()));
+            
+            parent.delete();
+            assertFalse(CndFileUtils.isExistingDirectory(CndFileUtils.getLocalFileSystem(), parent.getPath()));
+            assertFalse(CndFileUtils.isExistingDirectory(CndFileUtils.getLocalFileSystem(), child.getPath()));
+        }
     }
 }
