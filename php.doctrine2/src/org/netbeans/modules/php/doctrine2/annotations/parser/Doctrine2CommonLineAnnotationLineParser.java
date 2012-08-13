7b5d7f144f7241e5ab1942ce4003dea901cb2f3d
Ondrej Brejla <obrejla@netbeans.org>
2012-08-13 17:09:50 +0200

    Common line parser handles inline annotations and typed params in lines without leading annotation.

--- php.doctrine2/src/org/netbeans/modules/php/doctrine2/annotations/parser/Doctrine2CommonLineAnnotationLineParser.java
+++ php.doctrine2/src/org/netbeans/modules/php/doctrine2/annotations/parser/Doctrine2CommonLineAnnotationLineParser.java
@@ -41,6 +41,7 @@
  */
 package org.netbeans.modules.php.doctrine2.annotations.parser;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
@@ -53,15 +54,22 @@ import org.netbeans.modules.php.spi.annotation.AnnotationParsedLine;
  *
  * @author Ondrej Brejla <obrejla@netbeans.org>
  */
-public class Doctrine2InlineAnnotationLineParser implements AnnotationLineParser {
+public class Doctrine2CommonLineAnnotationLineParser implements AnnotationLineParser {
 
-    private static final AnnotationLineParser INSTANCE = new Doctrine2InlineAnnotationLineParser();
+    private static final AnnotationLineParser INSTANCE = new Doctrine2CommonLineAnnotationLineParser();
 
     private static final Set<String> INLINE_ANNOTATIONS = new HashSet<String>();
     static {
         INLINE_ANNOTATIONS.add("Index"); //NOI18N
         INLINE_ANNOTATIONS.add("UniqueConstraint"); //NOI18N
         INLINE_ANNOTATIONS.add("JoinColumn"); //NOI18N
+    }
+
+    private static final Set<String> TYPED_PARAMETERS = new HashSet<String>();
+    static {
+        TYPED_PARAMETERS.add("targetDocument"); //NOI18N
+        TYPED_PARAMETERS.add("repositoryClass"); //NOI18N
+        TYPED_PARAMETERS.add("targetEntity"); //NOI18N
     }
 
     @AnnotationLineParser.Registration(position=700)
@@ -72,9 +80,11 @@ public class Doctrine2InlineAnnotationLineParser implements AnnotationLineParser
     @Override
     public AnnotationParsedLine parse(String line) {
         AnnotationParsedLine result = null;
-        final Map<OffsetRange, String> extractInlineTypes = AnnotationUtils.extractInlineAnnotations(line, INLINE_ANNOTATIONS);
-        if (!extractInlineTypes.isEmpty()) {
-            result = new AnnotationParsedLine.ParsedLine("", extractInlineTypes, line.trim());
+        Map<OffsetRange, String> types = new HashMap<OffsetRange, String>();
+        types.putAll(AnnotationUtils.extractInlineAnnotations(line, INLINE_ANNOTATIONS));
+        types.putAll(AnnotationUtils.extractTypesFromParameters(line, TYPED_PARAMETERS));
+        if (!types.isEmpty()) {
+            result = new AnnotationParsedLine.ParsedLine("", types, line.trim());
         }
         return result;
     }
