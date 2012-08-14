7c04d8426d9ba8176b60a251e236df04b5d9b2e0
mentlicher <mentlicher@netbeans.org>
2012-08-14 13:53:31 +0200

    Adding a utility method toHTML().

--- web.javascript.debugger/src/org/netbeans/modules/web/javascript/debugger/ViewModelSupport.java
+++ web.javascript.debugger/src/org/netbeans/modules/web/javascript/debugger/ViewModelSupport.java
@@ -18,6 +18,7 @@
  */
 package org.netbeans.modules.web.javascript.debugger;
 
+import java.awt.Color;
 import java.util.Collection;
 import java.util.concurrent.CopyOnWriteArrayList;
 
@@ -31,6 +32,8 @@ import org.netbeans.spi.viewmodel.ModelEvent.TreeChanged;
  *
  */
 public abstract class ViewModelSupport {
+    
+    private CopyOnWriteArrayList<ModelListener> myListeners;
     
     protected ViewModelSupport() {
         myListeners = new CopyOnWriteArrayList<ModelListener>();
@@ -67,5 +70,36 @@ public abstract class ViewModelSupport {
     }
     
     
-    private CopyOnWriteArrayList<ModelListener> myListeners;
+    public static String toHTML (
+        String text,
+        boolean bold,
+        boolean italics,
+        Color color
+    ) {
+        if (text == null) return null;
+        if (text.length() > 6 && text.substring(0, 6).equalsIgnoreCase("<html>")) {
+            return text; // Already HTML
+        }
+        StringBuffer sb = new StringBuffer ();
+        sb.append ("<html>");
+        if (bold) sb.append ("<b>");
+        if (italics) sb.append ("<i>");
+        if (color != null) {
+            sb.append ("<font color=");
+            sb.append (Integer.toHexString ((color.getRGB () & 0xffffff)));
+            sb.append (">");
+        } else {
+            sb.append ("<font color=000000>");
+        }
+        text = text.replaceAll ("&", "&amp;");
+        text = text.replaceAll ("<", "&lt;");
+        text = text.replaceAll (">", "&gt;");
+        sb.append (text);
+        /*if (color != null)*/ sb.append ("</font>");
+        if (italics) sb.append ("</i>");
+        if (bold) sb.append ("</b>");
+        sb.append ("</html>");
+        return sb.toString ();
+    }
+    
 }
