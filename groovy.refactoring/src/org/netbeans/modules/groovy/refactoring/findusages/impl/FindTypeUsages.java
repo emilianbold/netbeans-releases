492a21e7e79a69b1fd75d967327e4c6d62ed7498
Martin Janicek <mjanicek@netbeans.org>
2012-08-15 15:09:19 +0200

    Find type usages for generics

--- groovy.refactoring/src/org/netbeans/modules/groovy/refactoring/findusages/impl/FindTypeUsages.java
+++ groovy.refactoring/src/org/netbeans/modules/groovy/refactoring/findusages/impl/FindTypeUsages.java
@@ -45,6 +45,7 @@ package org.netbeans.modules.groovy.refactoring.findusages.impl;
 import org.codehaus.groovy.ast.ASTNode;
 import org.codehaus.groovy.ast.ClassNode;
 import org.codehaus.groovy.ast.FieldNode;
+import org.codehaus.groovy.ast.GenericsType;
 import org.codehaus.groovy.ast.MethodNode;
 import org.codehaus.groovy.ast.ModuleNode;
 import org.codehaus.groovy.ast.Parameter;
@@ -57,6 +58,7 @@ import org.codehaus.groovy.ast.stmt.ForStatement;
 import org.netbeans.modules.csl.api.ElementKind;
 import org.netbeans.modules.groovy.editor.api.ASTUtils.FakeASTNode;
 import org.netbeans.modules.groovy.editor.api.ElementUtils;
+import org.netbeans.modules.groovy.editor.api.FindTypeUtils;
 import org.netbeans.modules.groovy.refactoring.GroovyRefactoringElement;
 
 /**
@@ -165,8 +167,16 @@ public class FindTypeUsages extends AbstractFindUsages {
         }
 
         private void addIfEquals(ASTNode node) {
+            final ClassNode type = ElementUtils.getType(node);
             if (isEquals(node)) {
-                usages.add(new FakeASTNode(ElementUtils.getType(node), ElementUtils.getTypeName(node)));
+                usages.add(new FakeASTNode(type, ElementUtils.getTypeName(node)));
+            }
+
+            final GenericsType[] genericTypes = type.getGenericsTypes();
+            if (genericTypes != null && genericTypes.length > 0) {
+                for (GenericsType genericType : genericTypes) {
+                    addIfEquals(genericType.getType());
+                }
             }
         }
 
