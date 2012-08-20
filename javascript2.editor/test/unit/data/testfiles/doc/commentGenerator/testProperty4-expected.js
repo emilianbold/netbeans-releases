08c152630190574f4b6848c2737212751950dc95
Martin Fousek <marfous@netbeans.org>
2012-08-20 08:17:47 +0200

    added several tests against comment generation to cover better property and object comments

---
+++ javascript2.editor/test/unit/data/testfiles/doc/commentGenerator/testProperty4-expected.js
@@ -0,0 +1,18 @@
+ShapeFactory.prototype = {
+
+    cokoliv: {
+        /**
+         * 
+         * @param {type} asf
+         * @return undefined^
+         */
+        ahoj: function(asf) {
+        }
+    },
+
+    propert: 5,
+
+    createShape: function(sdd) {
+        return new Shape();
+    }
+};
