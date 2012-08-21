d2cbc219ac29763bde60780d7b2f203e3c779f08
Tomas Mysik <tmysik@netbeans.org>
2012-08-21 10:49:58 +0200

    Backout of c29d74bca098
    Better fix will be used.

--- web.clientproject/src/org/netbeans/modules/web/clientproject/ui/wizard/ClientSideProjectWizardIterator.java
+++ web.clientproject/src/org/netbeans/modules/web/clientproject/ui/wizard/ClientSideProjectWizardIterator.java
@@ -290,10 +290,6 @@ public final class ClientSideProjectWizardIterator implements WizardDescriptor.P
                 ClientSideProjectUtilities.initializeProject(projectHelper);
                 siteRootDir = ClientSideProjectUtilities.getSiteRootFolder(projectHelper);
                 assert siteRootDir != null;
-                FileObject fo = ClientSideProjectUtilities.createBlankIndexHtml(siteRootDir);
-                if (fo != null) {
-                    files.add(fo);
-                }
              }
 
             // js libs
