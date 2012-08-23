3feade7ae08f3d1cd81ffa34abd98ed6b3a44a26
Ondrej Brejla <obrejla@netbeans.org>
2012-08-23 09:34:43 +0200

    Fixed imports.

--- php.twig/src/org/netbeans/modules/php/twig/editor/completion/TwigCompletionItem.java
+++ php.twig/src/org/netbeans/modules/php/twig/editor/completion/TwigCompletionItem.java
@@ -50,7 +50,6 @@ import org.netbeans.modules.csl.api.ElementHandle;
 import org.netbeans.modules.csl.api.ElementKind;
 import org.netbeans.modules.csl.api.HtmlFormatter;
 import org.netbeans.modules.csl.api.Modifier;
-import org.netbeans.modules.php.twig.editor.completion.TwigFunction;
 import org.openide.util.ImageUtilities;
 import org.openide.util.NbBundle;
 
