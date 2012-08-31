65d7770a86f1613c61697a0fa612d11cb40cf111
Tomas Mysik <tmysik@netbeans.org>
2012-08-31 07:17:38 +0200

    Fire change event also when js libraries changes

--- web.clientproject/src/org/netbeans/modules/web/clientproject/ui/JavaScriptLibrarySelection.java
+++ web.clientproject/src/org/netbeans/modules/web/clientproject/ui/JavaScriptLibrarySelection.java
@@ -179,6 +179,23 @@ public class JavaScriptLibrarySelection extends JPanel {
         assert EventQueue.isDispatchThread();
         selectedLibrariesList.setModel(selectedLibrariesListModel);
         selectedLibrariesList.setCellRenderer(new SelectedLibraryRenderer(selectedLibrariesList.getCellRenderer()));
+        selectedLibrariesListModel.addListDataListener(new ListDataListener() {
+            @Override
+            public void intervalAdded(ListDataEvent e) {
+                dataChanged();
+            }
+            @Override
+            public void intervalRemoved(ListDataEvent e) {
+                dataChanged();
+            }
+            @Override
+            public void contentsChanged(ListDataEvent e) {
+                dataChanged();
+            }
+            private void dataChanged() {
+                fireChangeEvent();
+            }
+        });
     }
 
     private void initLibrariesButtons() {
