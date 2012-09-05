0fe18f0227d9fe6255e9ce72feca20166310a0d4
Tomas Kraus <TomasKraus@netbeans.org>
2012-09-05 16:24:24 +0200

    Bug# 217727 - Cannot register GlassFish 3.1.2.2 - removed duplicated code

--- glassfish.common/src/org/netbeans/modules/glassfish/spi/ServerUtilities.java
+++ glassfish.common/src/org/netbeans/modules/glassfish/spi/ServerUtilities.java
@@ -57,7 +57,6 @@ import org.netbeans.api.server.ServerInstance;
 import org.netbeans.modules.glassfish.common.GlassfishInstanceProvider;
 import org.netbeans.modules.glassfish.common.ServerDetails;
 import org.netbeans.modules.glassfish.common.wizards.GlassfishWizardProvider;
-import org.netbeans.modules.glassfish.common.wizards.ServerWizardIterator;
 import org.netbeans.spi.server.ServerInstanceProvider;
 import org.openide.WizardDescriptor.InstantiatingIterator;
 import org.openide.filesystems.FileObject;
@@ -187,23 +186,13 @@ public final class ServerUtilities {
 //    }
     
     /**
-     * Returns an instance of the AddServerWizard for this server plugin.
-     * 
-     * @return instance of the AddServerWizard for this server.
+     * Creates an iterator for a wizard to instantiate server objects.
+     * <p/>
+     * @return Server wizard iterator initialized with supported GlassFish
+     * server versions.
      */
     public InstantiatingIterator getAddInstanceIterator() {
-        return new ServerWizardIterator(new ServerDetails[] { 
-            ServerDetails.GLASSFISH_SERVER_3_1_1,
-            ServerDetails.GLASSFISH_SERVER_3_1_2,
-            ServerDetails.GLASSFISH_SERVER_4_0,
-            ServerDetails.GLASSFISH_SERVER_3_0_1, 
-            ServerDetails.GLASSFISH_SERVER_3_1,
-            ServerDetails.GLASSFISH_SERVER_3, 
-        },
-                new ServerDetails[] { 
-            ServerDetails.GLASSFISH_SERVER_3_1_2,
-            ServerDetails.GLASSFISH_SERVER_3_0_1, 
-        });
+        return ServerDetails.getInstantiatingIterator();
     }
     
     /**
--- glassfish.javaee/src/org/netbeans/modules/glassfish/javaee/Hk2OptionalFactory.java
+++ glassfish.javaee/src/org/netbeans/modules/glassfish/javaee/Hk2OptionalFactory.java
@@ -54,6 +54,7 @@ import javax.enterprise.deploy.spi.DeploymentManager;
 import javax.enterprise.deploy.spi.factories.DeploymentFactory;
 import javax.swing.event.ChangeListener;
 import org.netbeans.api.server.ServerInstance;
+import org.netbeans.modules.glassfish.common.ServerDetails;
 import org.netbeans.modules.glassfish.javaee.db.Hk2DatasourceManager;
 import org.netbeans.modules.glassfish.javaee.ide.FastDeploy;
 import org.netbeans.modules.glassfish.spi.GlassfishModule;
@@ -206,7 +207,7 @@ public class Hk2OptionalFactory extends OptionalDeploymentManagerFactory {
         private ServerUtilities su;
 
         public J2eeInstantiatingIterator(ServerUtilities su) {
-            this.delegate = su.getAddInstanceIterator();
+            this.delegate = ServerDetails.getInstantiatingIterator();
             this.su = su;
         }
 
