68c3cc93cc2af1c28fbb11209e5fa95937b7a7fd
Martin Fousek <marfous@netbeans.org>
2012-09-14 09:36:35 +0200

    #218294 - WARNING [org.openide.util.RequestProcessor]: Too many org.netbeans.modules.php.smarty.SmartyPhpFrameworkProvider$1 (3) in shared RequestProcessor; create your own

--- php.smarty/src/org/netbeans/modules/php/smarty/SmartyPhpFrameworkProvider.java
+++ php.smarty/src/org/netbeans/modules/php/smarty/SmartyPhpFrameworkProvider.java
@@ -68,6 +68,8 @@ import org.openide.util.RequestProcessor;
  */
 public final class SmartyPhpFrameworkProvider extends PhpFrameworkProvider {
 
+    private static final RequestProcessor RP = new RequestProcessor(SmartyPhpFrameworkProvider.class);
+
     private static final Logger LOGGER = Logger.getLogger(SmartyPhpFrameworkProvider.class.getName());
 
     /** Preferences property if the given {@link PhpModule} contains Smarty framework or not. */
@@ -161,7 +163,7 @@ public final class SmartyPhpFrameworkProvider extends PhpFrameworkProvider {
             return true;
         }
 
-        RequestProcessor.getDefault().post(new Runnable() {
+        RP.post(new Runnable() {
             @Override
             public void run() {
                 FileObject sourceDirectory = phpModule.getSourceDirectory();
