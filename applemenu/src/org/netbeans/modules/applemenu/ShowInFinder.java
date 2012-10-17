930665d34d3f50b5f03ee07c9cc0d63613616315
Tomas Zezula <tzezula@netbeans.org>
2012-10-17 09:59:09 +0200

    #220242:Select in Folder does not work on Mountain Lion

--- applemenu/src/org/netbeans/modules/applemenu/ShowInFinder.java
+++ applemenu/src/org/netbeans/modules/applemenu/ShowInFinder.java
@@ -44,16 +44,11 @@ package org.netbeans.modules.applemenu;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.text.MessageFormat;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
 import org.openide.loaders.DataObject;
 
 import org.openide.awt.ActionRegistration;
@@ -63,8 +58,6 @@ import org.openide.awt.ActionID;
 import org.openide.awt.StatusDisplayer;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
-import org.openide.util.Exceptions;
-import org.openide.util.Lookup;
 import org.openide.util.NbBundle;
 
 /**
@@ -78,11 +71,8 @@ id = "org.netbeans.modules.applemenu.ShowInFinder")
     @ActionReference(path = "UI/ToolActions/Files", position = 1001)
 })
 public final class ShowInFinder implements ActionListener {
-    
-    private static final String APPLE_SCRIPT = "AppleScriptEngine";  //NOI18N
-    private static final String SCRIPT = "org/netbeans/modules/applemenu/resources/select_file.scpt";    //NOI18N
+        
     private static final Logger LOG = Logger.getLogger(ShowInFinder.class.getName());
-    private static volatile String script;
 
     private final DataObject context;
 
@@ -106,63 +96,19 @@ public final class ShowInFinder implements ActionListener {
             LOG.log(Level.INFO, "Ignoring non local file: {0}", FileUtil.getFileDisplayName(fobj)); //NOI18N
             StatusDisplayer.getDefault().setStatusText(NbBundle.getMessage(ShowInFinder.class, "TXT_NoLocalFile"));
             return;
-        }
-        final File folder = file.getParentFile();
-        if (folder == null) {
-            LOG.log(Level.INFO, "Ignoring file with no parent: {0}", FileUtil.getFileDisplayName(fobj)); //NOI18N
-            StatusDisplayer.getDefault().setStatusText(NbBundle.getMessage(ShowInFinder.class, "TXT_NoLocalFile"));
-            return;
-        }
-        final String _script = getScript();
-        if (_script == null) {
-            LOG.log(Level.INFO, "Cannot load apple scipt program"); //NOI18N
-            return;
-        }
-        final ClassLoader cl = Lookup.getDefault().lookup(ClassLoader.class);
-        if (cl == null) {
-            LOG.log(Level.INFO, "No ClassLoader to load AppleScriptEngine"); //NOI18N
-            return;
-        }
-        final ScriptEngineManager scriptEngineFactory = new ScriptEngineManager (cl);
-        final ScriptEngine appleScriptEngine = scriptEngineFactory.getEngineByName(APPLE_SCRIPT);
-        if (appleScriptEngine == null) {
-            LOG.log(Level.INFO, "No AppleScriptEngine found"); //NOI18N
-            return;
-        }
+        }                
         try {
-            appleScriptEngine.eval(
-                MessageFormat.format(
-                    _script,
-                    folder.getAbsolutePath(),
-                    file.getAbsolutePath()));
-        } catch (ScriptException ex) {
-            Exceptions.printStackTrace(ex);
+            final Class<?> fmClz = Class.forName("com.apple.eio.FileManager");
+            final Method m = fmClz.getDeclaredMethod("revealInFinder", File.class);
+            m.invoke(null, file);
+        } catch (ClassNotFoundException e) {
+            LOG.log(Level.FINE, "Cannot load com.apple.eio.FileManager class.");    //NOI18N
+        } catch (NoSuchMethodException e) {
+            LOG.log(Level.FINE, "No method revealInFinder(java.io.File) in the com.apple.eio.FileManager class.");    //NOI18N
+        } catch (InvocationTargetException e) {
+            LOG.log(Level.FINE, "Cannot invoke method com.apple.eio.FileManager.revealInFinder(java.io.File).");    //NOI18N
+        } catch (IllegalAccessException e) {
+            LOG.log(Level.FINE, "The method com.apple.eio.FileManager.revealInFinder(java.io.File) is not accessible"); //NOI18N
         }
-    }
-    
-    private static String getScript() {
-        String _script = script;
-        if (_script != null) {
-            return _script;
-        }
-        try {
-            final ClassLoader cl = ShowInFinder.class.getClassLoader();
-            final BufferedReader in = new BufferedReader ( new InputStreamReader (cl.getResourceAsStream(SCRIPT), "UTF-8"));    //NOI18N
-            try {
-                final StringBuilder builder = new StringBuilder();
-                int len;
-                char[] buffer = new char[512];
-                while ((len=in.read(buffer,0,buffer.length))>0) {
-                    builder.append(buffer, 0, len);
-                }
-                _script = builder.toString();
-            } finally {
-                in.close();
-            }
-        } catch (IOException ioe) {
-            Exceptions.printStackTrace(ioe);
-        }
-        script = _script;
-        return _script;
-    }
+    }        
 }
