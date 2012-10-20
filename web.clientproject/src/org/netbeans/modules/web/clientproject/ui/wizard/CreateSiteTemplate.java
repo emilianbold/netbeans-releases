8863492df23c2deb76abdd2d82ac3faa038cc8bc
Tomas Mysik <tmysik@netbeans.org>
2012-10-20 11:25:59 +0200

    #219243 - Save as Template not reflecting state

--- web.clientproject/src/org/netbeans/modules/web/clientproject/ui/wizard/CreateSiteTemplate.java
+++ web.clientproject/src/org/netbeans/modules/web/clientproject/ui/wizard/CreateSiteTemplate.java
@@ -43,6 +43,7 @@ package org.netbeans.modules.web.clientproject.ui.wizard;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.EventQueue;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -65,6 +66,7 @@ import org.openide.DialogDisplayer;
 import org.openide.NotifyDescriptor;
 import org.openide.WizardDescriptor;
 import org.openide.WizardDescriptor.Panel;
+import org.openide.awt.StatusDisplayer;
 import org.openide.explorer.ExplorerManager;
 import org.openide.explorer.view.CheckableNode;
 import org.openide.explorer.view.OutlineView;
@@ -341,11 +343,11 @@ public class CreateSiteTemplate extends javax.swing.JPanel implements ExplorerMa
         }
     }
     
-    private static class WizardIterator implements WizardDescriptor.InstantiatingIterator {
+    private static class WizardIterator implements WizardDescriptor.BackgroundInstantiatingIterator {
 
-        private WizardPanel panel;
-        private ClientSideProject p;
-        private ChangeSupport sup = new ChangeSupport(this);
+        private final WizardPanel panel;
+        private final ClientSideProject p;
+        private final ChangeSupport sup = new ChangeSupport(this);
 
         public WizardIterator(ClientSideProject p) {
             this.p = p;
@@ -358,9 +360,15 @@ public class CreateSiteTemplate extends javax.swing.JPanel implements ExplorerMa
                 }
             });
         }
-        
+
+        @NbBundle.Messages({
+            "# {0} - template name",
+            "CreateSiteTemplate.info.templateCreated=Template {0} successfully created."
+        })
         @Override
         public Set instantiate() throws IOException {
+            assert !EventQueue.isDispatchThread();
+            // threading model of WizardPanel is broken and cannot be properly used
             String name = panel.comp.getTemplateName();
             if (!name.endsWith(".zip")) { //NOI18N
                 name += ".zip"; //NOI18N
@@ -373,6 +381,7 @@ public class CreateSiteTemplate extends javax.swing.JPanel implements ExplorerMa
                 }
             }
             createZipFile(f, p, panel.comp.manager.getRootContext());
+            StatusDisplayer.getDefault().setStatusText(Bundle.CreateSiteTemplate_info_templateCreated(name));
             return null;
         }
 
