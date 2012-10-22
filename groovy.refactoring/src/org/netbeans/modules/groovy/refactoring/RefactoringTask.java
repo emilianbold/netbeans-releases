abe667a01b498b7db36ce969842248759f4d05d3
Martin Janicek <mjanicek@netbeans.org>
2012-10-22 14:57:42 +0200

    Additional fix for 220571

--- groovy.refactoring/src/org/netbeans/modules/groovy/refactoring/RefactoringTask.java
+++ groovy.refactoring/src/org/netbeans/modules/groovy/refactoring/RefactoringTask.java
@@ -145,6 +145,8 @@ public abstract class RefactoringTask extends UserTask implements Runnable {
             final RefactoringElement element = createRefactoringElement(path, findingNode, kind);
             if (element != null && element.getName() != null && element.getFileObject() != null) {
                 ui = createRefactoringUI(element, start, end, parserResult);
+            } else {
+                throw new IllegalStateException("RefactoringElement isn't initiated correctly!"); // NOI18N
             }
         }
 
