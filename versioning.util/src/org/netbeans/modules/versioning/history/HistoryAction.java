6b37171708695ea7577fc3e23a372f7077aeef53
Tomas Stupka <tstupka@netbeans.org>
2012-10-25 22:18:41 +0200

    Issue #220820 - NullPointerException at org.netbeans.modules.versioning.history.HistoryAction.getHistoryEntry

--- versioning.util/src/org/netbeans/modules/versioning/history/HistoryAction.java
+++ versioning.util/src/org/netbeans/modules/versioning/history/HistoryAction.java
@@ -104,8 +104,14 @@ public abstract class HistoryAction extends NodeAction {
         return name;
     }
     
-    protected HistoryEntry getHistoryEntry() {
-        Collection<? extends Node> nodes = getContext().lookupAll(Node.class);
+    protected String getRevisionShort() {
+        Lookup ctx = getContext();
+        if(ctx == null) {
+            // this is strange, but there seem to be cases when the context wasn't set yet.
+            // see also issue #220820
+            return null;
+        }
+        Collection<? extends Node> nodes = ctx.lookupAll(Node.class);
         HistoryEntry he = null;
         for(Node node : nodes) {
             he = node.getLookup().lookup(VCSHistoryProvider.HistoryEntry.class);
@@ -114,7 +120,7 @@ public abstract class HistoryAction extends NodeAction {
             }
         }
         assert he != null;
-        return he;
+        return he.getRevisionShort();
     }
 
     @Override
