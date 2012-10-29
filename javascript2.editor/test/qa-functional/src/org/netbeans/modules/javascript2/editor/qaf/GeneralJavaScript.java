18521724997b475df561ababbfce050234045cb4
Vladimir Riha <vriha@netbeans.org>
2012-10-29 13:28:06 +0100

    qa test - refactoring

--- javascript2.editor/test/qa-functional/src/org/netbeans/modules/javascript2/editor/qaf/GeneralJavaScript.java
+++ javascript2.editor/test/qa-functional/src/org/netbeans/modules/javascript2/editor/qaf/GeneralJavaScript.java
@@ -65,13 +65,13 @@ import org.openide.util.Exceptions;
  */
 public class GeneralJavaScript extends JellyTestCase {
 
-    static final String PHP_CATEGORY_NAME = "PHP";
-    static final String PHP_PROJECT_NAME = "PHP Application";
-    static final String OTHER_CATEGORY_NAME = "Other";
+    public static final String PHP_CATEGORY_NAME = "PHP";
+    public static final String PHP_PROJECT_NAME = "PHP Application";
+    public static final String OTHER_CATEGORY_NAME = "Other";
     protected EventTool evt;
     public static String currentFile = "";
     public final String TEST_BASE_NAME = "js2_";
-    public int name_iterator = 0;
+    public static int NAME_ITERATOR = 0;
 
     public GeneralJavaScript(String arg0) {
         super(arg0);
