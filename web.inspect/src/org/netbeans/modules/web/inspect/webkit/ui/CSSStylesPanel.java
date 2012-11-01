226494be126dca50137dc5b8f0b8e5a804e035a5
Jan Stola <jstola@netbeans.org>
2012-11-01 15:27:39 +0100

    Avoiding potential StringIndexOutOfBoundsException exception.

--- web.inspect/src/org/netbeans/modules/web/inspect/webkit/ui/CSSStylesPanel.java
+++ web.inspect/src/org/netbeans/modules/web/inspect/webkit/ui/CSSStylesPanel.java
@@ -464,10 +464,14 @@ public class CSSStylesPanel extends JPanel implements PageModel.CSSStylesView {
          * @return {@code true} when the property uses star or underscore hack.
          */
         private boolean isIEHackIgnoredByWebKit(Property property, Snapshot snapshot) {
+            boolean isHack = false;
             String styleSheetText = snapshot.getText().toString();
             int startOffset = property.getStartOffset();
-            char c = styleSheetText.charAt(startOffset-1);
-            return (c == '_' || c == '*');
+            if (startOffset != -1) {
+                char c = styleSheetText.charAt(startOffset-1);
+                isHack = (c == '_' || c == '*');
+            }
+            return isHack;
         }
 
     }
