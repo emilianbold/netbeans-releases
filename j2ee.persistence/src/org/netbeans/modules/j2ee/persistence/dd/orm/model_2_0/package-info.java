b49f5694986c205e3f63c92234e3c47c57ffa24a
Petr Hejl <phejl@netbeans.org>
2012-11-03 21:24:30 +0100

    Suppressing warnings about same interface name for generated classes.

--- j2ee.persistence/src/org/netbeans/modules/j2ee/persistence/dd/orm/model_2_0/package-info.java
+++ j2ee.persistence/src/org/netbeans/modules/j2ee/persistence/dd/orm/model_2_0/package-info.java
@@ -52,6 +52,7 @@
     removeUnreferencedNodes=true,
     java5=true
 )
+@org.netbeans.api.annotations.common.SuppressWarnings("NM_SAME_SIMPLE_NAME_AS_INTERFACE") // justification="Generated implementation classes"
 package org.netbeans.modules.j2ee.persistence.dd.orm.model_2_0;
 
 import org.netbeans.modules.schema2beans.Schema2Beans;
