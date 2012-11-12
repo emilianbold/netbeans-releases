8ce88ebff4ead79c9138cd4c7d6723dd864b62fc
Vladimir Kvashin <vkvashin@netbeans.org>
2012-11-12 15:22:03 +0400

    fixed remote test failure: link's createData() and createFolder() returned wrong file objects (delegates children instead of wrapped LinkChild)

--- dlight.remote.impl/src/org/netbeans/modules/remote/impl/fs/RemoteLinkBase.java
+++ dlight.remote.impl/src/org/netbeans/modules/remote/impl/fs/RemoteLinkBase.java
@@ -260,8 +260,7 @@ public abstract class RemoteLinkBase extends RemoteFileObjectBase implements Fil
     protected RemoteFileObject createFolderImpl(String name, RemoteFileObjectBase orig) throws IOException {
         RemoteFileObjectBase delegate = getDelegate();
         if (delegate != null) {
-            // TODO return right object
-            return delegate.createFolderImpl(name, orig);
+            return wrapFileObject(delegate.createFolderImpl(name, orig), null);
         } else {
             throw fileNotFoundException("create a folder in"); //NOI18N
         }
@@ -271,8 +270,7 @@ public abstract class RemoteLinkBase extends RemoteFileObjectBase implements Fil
     protected RemoteFileObject createDataImpl(String name, String ext, RemoteFileObjectBase orig) throws IOException {
         RemoteFileObjectBase delegate = getDelegate();
         if (delegate != null) {
-            // TODO return right object
-            return delegate.createDataImpl(name, ext, orig);
+            return wrapFileObject(delegate.createDataImpl(name, ext, orig), null);
         } else {
             throw fileNotFoundException("create a file in"); //NOI18N
         }
