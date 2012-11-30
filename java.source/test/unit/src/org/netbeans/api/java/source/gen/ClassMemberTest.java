10d8095c15d63d8b59d029b466006036cfaf2cc4
Jan Lahoda <jlahoda@netbeans.org>
2012-11-30 16:36:38 +0100

    Ensuring the correct length is assumed for constructors when their name did not change

--- java.source/test/unit/src/org/netbeans/api/java/source/gen/ClassMemberTest.java
+++ java.source/test/unit/src/org/netbeans/api/java/source/gen/ClassMemberTest.java
@@ -1787,6 +1787,44 @@ public class ClassMemberTest extends GeneratorTestMDRCompat {
         assertEquals(golden, res);
     }
 
+    public void testAddConstructorParam() throws Exception {
+        testFile = new File(getWorkDir(), "Test.java");
+        TestUtilities.copyStringToFile(testFile,
+            "package hierbas.del.litoral;\n" +
+            "\n" +
+            "public class Test {\n" +
+            "    public Test() {\n" +
+            "    }\n" +
+            "}\n"
+            );
+        String golden =
+            "package hierbas.del.litoral;\n" +
+            "\n" +
+            "public class Test {\n" +
+            "    public Test(int i) {\n" +
+            "    }\n" +
+            "}\n";
+
+        JavaSource src = getJavaSource(testFile);
+        Task<WorkingCopy> task = new Task<WorkingCopy>() {
+
+            public void run(WorkingCopy workingCopy) throws Exception {
+                workingCopy.toPhase(Phase.RESOLVED); // is it neccessary?
+                CompilationUnitTree cut = workingCopy.getCompilationUnit();
+                TreeMaker make = workingCopy.getTreeMaker();
+                ClassTree clazz = (ClassTree) cut.getTypeDecls().get(0);
+                MethodTree constr = (MethodTree) clazz.getMembers().get(0);
+                VariableTree param = make.Variable(make.Modifiers(EnumSet.noneOf(Modifier.class)), "i", make.PrimitiveType(TypeKind.INT), null);
+                MethodTree nueConstr = make.addMethodParameter(constr, param);
+                workingCopy.rewrite(constr, nueConstr);
+            }
+        };
+        src.runModificationTask(task).commit();
+        String res = TestUtilities.copyFileToString(testFile);
+        System.err.println(res);
+        assertEquals(golden, res);
+    }
+
     String getGoldenPckg() {
         return "";
     }
