56761a8642eea8f907b024b8bc5c71463416b6f9
ivan <ivan@netbeans.org>
2012-12-05 01:12:23 -0800

    207965 - Terminal incorrectly displays prompt string         Preparation for introduction of xterm emulation.

--- lib.terminalemulator/src/org/netbeans/lib/terminalemulator/InterpProtoANSI.java
+++ lib.terminalemulator/src/org/netbeans/lib/terminalemulator/InterpProtoANSI.java
@@ -47,7 +47,301 @@ package org.netbeans.lib.terminalemulator;
  */
 public class InterpProtoANSI extends InterpDumb {
 
+    protected static class Ascii {
+        public static final char ESC = 27;
+        public static final char CTRL_N = 14;   // ASCII SO/ShiftOut
+        public static final char CTRL_O = 15;   // ASCII SI/ShiftIn
+    }
+    
     protected static class InterpTypeProtoANSI extends InterpTypeDumb {
+	protected final Actor act_reset_number = new ACT_RESET_NUMBER();
+	protected final Actor act_remember_digit = new ACT_REMEMBER_DIGIT();
+
+	protected final State st_esc = new State("esc");	// NOI18N
+	protected final State st_esc_lb = new State("esc_lb");	// NOI18N
+
+	protected InterpTypeProtoANSI() {
+	    st_base.setAction((char) 27, st_esc, new ACT_TO_ESC());
+	    st_base.setAction(Ascii.CTRL_N, st_base, new ACT_AS());
+	    st_base.setAction(Ascii.CTRL_O, st_base, new ACT_AE());
+
+	    st_esc.setRegular(st_esc, act_regular);
+	    st_esc.setAction('M', st_base, new ACT_M());
+	    st_esc.setAction('c', st_base, new ACT_FULL_RESET());
+	    st_esc.setAction('[', st_esc_lb, act_reset_number);
+
+	    st_esc_lb.setRegular(st_esc_lb, act_regular);
+	    for (char c = '0'; c <= '9'; c++)
+		st_esc_lb.setAction(c, st_esc_lb, act_remember_digit);
+	    st_esc_lb.setAction(';', st_esc_lb, new ACT_PUSH_NUMBER());
+	    st_esc_lb.setAction('A', st_base, new ACT_UP());
+	    st_esc_lb.setAction('B', st_base, new ACT_DO());
+	    st_esc_lb.setAction('C', st_base, new ACT_ND());
+	    st_esc_lb.setAction('D', st_base, new ACT_BC());
+	    st_esc_lb.setAction('H', st_base, new ACT_HO());
+	    st_esc_lb.setAction('J', st_base, new ACT_J());
+	    st_esc_lb.setAction('K', st_base, new ACT_K());
+	    st_esc_lb.setAction('L', st_base, new ACT_AL());
+	    st_esc_lb.setAction('M', st_base, new ACT_DL());
+	    st_esc_lb.setAction('P', st_base, new ACT_DC());
+	    st_esc_lb.setAction('@', st_base, new ACT_IC());
+	    st_esc_lb.setAction('h', st_base, new ACT_SM());
+	    st_esc_lb.setAction('m', st_base, new ACT_ATTR());
+	    st_esc_lb.setAction('n', st_base, new ACT_DSR());
+	    st_esc_lb.setAction('r', st_base, new ACT_MARGIN());
+        }
+
+	static final class ACT_TO_ESC implements Actor {
+	    @Override
+	    public String action(AbstractInterp ai, char c) {
+		InterpDumb i = (InterpDumb) ai;
+		i.ctl_sequence = "";	// NOI18N
+		return null;
+	    }
+	}
+
+        static final class ACT_AS implements Actor {
+	    @Override
+            public String action(AbstractInterp ai, char c) {
+                ai.ops.op_as();
+                return null;
+            }
+        }
+        
+        static final class ACT_AE implements Actor {
+	    @Override
+            public String action(AbstractInterp ai, char c) {
+                ai.ops.op_ae();
+                return null;
+            }
+        }
+
+	static class ACT_RESET_NUMBER implements Actor {
+	    @Override
+	    public String action(AbstractInterp ai, char c) {
+		ai.resetNumber();
+		return null;
+	    }
+	};
+
+	static final class ACT_REMEMBER_DIGIT implements Actor {
+	    @Override
+	    public String action(AbstractInterp ai, char c) {
+		ai.remember_digit(c);
+		return null;
+	    }
+	};
+
+	static final class ACT_M implements Actor {
+            @Override
+	    public String action(AbstractInterp ai, char c) {
+		ai.ops.op_up(1);
+		return null;
+	    }
+	}
+
+	static final class ACT_FULL_RESET implements Actor {
+	    @Override
+	    public String action(AbstractInterp ai, char c) {
+		ai.ops.op_full_reset();
+		return null;
+	    }
+	}
+
+	static final class ACT_PUSH_NUMBER implements Actor {
+	    @Override
+	    public String action(AbstractInterp ai, char c) {
+		if (!ai.pushNumber())
+		    return "ACT PUSH_NUMBER";	// NOI18N
+		return null;
+	    }
+	}
+
+	static final class ACT_UP implements Actor {
+	    @Override
+	    public String action(AbstractInterp ai, char c) {
+		if (ai.noNumber())
+		    ai.ops.op_up(1);
+		else
+		    ai.ops.op_up(ai.numberAt(0));
+		return null;
+	    }
+	}
+
+	static final class ACT_DO implements Actor {
+	    @Override
+	    public String action(AbstractInterp ai, char c) {
+		if (ai.noNumber())
+		    ai.ops.op_do(1);
+		else
+		    ai.ops.op_do(ai.numberAt(0));
+		return null;
+	    }
+	}
+
+	static final class ACT_ND implements Actor {
+	    @Override
+	    public String action(AbstractInterp ai, char c) {
+		if (ai.noNumber())
+		    ai.ops.op_nd(1);
+		else
+		    ai.ops.op_nd(ai.numberAt(0));
+		return null;
+	    }
+	}
+
+	static final class ACT_BC implements Actor {
+	    @Override
+	    public String action(AbstractInterp ai, char c) {
+		if (ai.noNumber())
+		    ai.ops.op_bc(1);
+		else
+		    ai.ops.op_bc(ai.numberAt(0));
+		return null;
+	    }
+	}
+
+	static final class ACT_HO implements Actor {
+	    @Override
+	    public String action(AbstractInterp ai, char c) {
+		if (ai.noNumber()) {
+		    ai.ops.op_ho();
+		} else {
+		    ai.ops.op_cm(ai.numberAt(0), ai.numberAt(1));// row, col
+		} 
+		return null;
+	    }
+	}
+
+	static final class ACT_J implements Actor {
+	    @Override
+	    public String action(AbstractInterp ai, char c) {
+		if (ai.noNumber()) {
+		    ai.ops.op_cd();
+		} else {
+		    int count = ai.numberAt(0);
+		    if (count == 1) {
+			return "ACT J: count of 1 not supported";	// NOI18N
+		    } else if (count == 2) {
+			ai.ops.op_cl();
+		    } 
+		} 
+		return null;
+	    }
+	}
+
+	static final class ACT_K implements Actor {
+	    @Override
+	    public String action(AbstractInterp ai, char c) {
+		if (ai.noNumber()) {
+		    ai.ops.op_ce();
+		} else {
+		    int count = ai.numberAt(0);
+		    if (count == 1) {
+			return "ACT K: count of 1 not supported";	// NOI18N
+		    } else if (count == 2) {
+			return "ACT K: count of 2 not supported";	// NOI18N
+		    } 
+		} 
+		return null;
+	    }
+	}
+
+	static final class ACT_AL implements Actor {
+	    @Override
+	    public String action(AbstractInterp ai, char c) {
+		if (ai.noNumber()) {
+		    ai.ops.op_al(1);
+		} else {
+		    ai.ops.op_al(ai.numberAt(0));
+		} 
+		return null;
+	    }
+	}
+
+	static final class ACT_DL implements Actor {
+	    @Override
+	    public String action(AbstractInterp ai, char c) {
+		if (ai.noNumber()) {
+		    ai.ops.op_dl(1);
+		} else {
+		    ai.ops.op_dl(ai.numberAt(0));
+		} 
+		return null;
+	    }
+	}
+
+	static final class ACT_DC implements Actor {
+	    @Override
+	    public String action(AbstractInterp ai, char c) {
+		if (ai.noNumber())
+		    ai.ops.op_dc(1);
+		else
+		    ai.ops.op_dc(ai.numberAt(0));
+		return null;
+	    }
+	}
+
+	static final class ACT_IC implements Actor {
+	    @Override
+	    public String action(AbstractInterp ai, char c) {
+		if (ai.noNumber())
+		    ai.ops.op_ic(1);
+		else
+		    ai.ops.op_ic(ai.numberAt(0));
+		return null;
+	    }
+	}
+
+	static final class ACT_SM implements Actor {
+	    @Override
+	    public String action(AbstractInterp ai, char c) {
+		if (ai.noNumber())
+		    ai.ops.op_set_mode(1);
+		else
+		    ai.ops.op_set_mode(ai.numberAt(0));
+		return null;
+	    }
+	}
+
+	static final class ACT_ATTR implements Actor {
+	    @Override
+	    public String action(AbstractInterp ai, char c) {
+		// set graphics modes (bold, reverse video etc)
+		if (ai.noNumber()) {
+		    ai.ops.op_attr(0);	// reset everything
+		} else {
+		    for (int n = 0; n <= ai.nNumbers(); n++)
+			ai.ops.op_attr(ai.numberAt(n));
+		}
+		return null;
+	    }
+	}
+
+	static final class ACT_DSR implements Actor {
+	    // Device Status Report
+	    @Override
+	    public String action(AbstractInterp ai, char c) {
+		if (ai.noNumber()) {
+		    ai.ops.op_status_report(5);	// reset everything
+		} else {
+		    ai.ops.op_status_report(ai.numberAt(0));
+		}
+		return null;
+	    }
+	}
+
+	static final class ACT_MARGIN implements Actor {
+	    @Override
+	    public String action(AbstractInterp ai, char c) {
+		if (ai.noNumber())
+		    ai.ops.op_margin(0, 0);
+		else
+		    ai.ops.op_margin(ai.numberAt(0), ai.numberAt(1));
+		return null;
+	    }
+	}
+
     }
 
     private InterpTypeProtoANSI type;
