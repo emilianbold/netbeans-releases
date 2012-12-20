dae0dfbb894895c3fdb10648e190bb2ae8d61b6f
Denis Anisimov <ads@netbeans.org>
2012-12-20 22:05:54 +0400

    FindBugs warning corrections.

--- websvc.restapi/src/org/netbeans/modules/websvc/rest/model/impl/SubResourceLocatorImpl.java
+++ websvc.restapi/src/org/netbeans/modules/websvc/rest/model/impl/SubResourceLocatorImpl.java
@@ -57,7 +57,6 @@ public class SubResourceLocatorImpl extends RestMethodDescriptionImpl
         implements SubResourceLocator {
     
     private String uriTemplate;
-    private String resourceType;
     
     public SubResourceLocatorImpl(ExecutableElement methodElement) {
         super(methodElement);
@@ -70,7 +69,7 @@ public class SubResourceLocatorImpl extends RestMethodDescriptionImpl
     }
     
     public String getResourceType() {
-        return resourceType;
+        return null;
     }
     
     public Status refresh(Element element) {
