88b7739cb587a1d89cc803b3f9b61c9f2fde5a99
Petr Pisl <ppisl@netbeans.org>
2012-12-20 15:39:09 +0100

    #224041 - Calling methods of window's properties marked window as global not declared variable

--- javascript2.editor/test/unit/src/org/netbeans/modules/javascript2/editor/hint/JsGlobalIsNotDeclaredTest.java
+++ javascript2.editor/test/unit/src/org/netbeans/modules/javascript2/editor/hint/JsGlobalIsNotDeclaredTest.java
@@ -66,4 +66,8 @@ public class JsGlobalIsNotDeclaredTest extends HintTestBase {
     public void testIssue224040() throws Exception {
         checkHints(this, createRule(), "testfiles/hints/issue224040.js", null);
     }
+    
+    public void testIssue224041() throws Exception {
+        checkHints(this, createRule(), "testfiles/hints/issue224041.js", null);
+    }
 }
