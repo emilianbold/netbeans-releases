5cbcc309f6be9efb725f59c40dba6d06f230f2cf
Denis Anisimov <ads@netbeans.org>
2012-12-29 15:40:30 +0400

    REST client wizard refactoring to allow generate code using JAX-RS 2.0 API along with Jersey 1.X.

---
+++ websvc.rest/src/org/netbeans/modules/websvc/rest/client/JaxRsGenerationStrategy.java
@@ -0,0 +1,431 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ *
+ * Oracle and Java are registered trademarks of Oracle and/or its affiliates.
+ * Other names may be trademarks of their respective owners.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * Contributor(s):
+ * The Original Software is NetBeans. The Initial Developer of the Original
+ * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
+ * Microsystems, Inc. All Rights Reserved.
+ *
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ */
+package org.netbeans.modules.websvc.rest.client;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import javax.lang.model.element.Modifier;
+import javax.lang.model.element.TypeElement;
+
+import org.netbeans.api.java.source.TreeMaker;
+import org.netbeans.api.java.source.WorkingCopy;
+import org.netbeans.modules.websvc.rest.client.ClientJavaSourceHelper.HttpMimeType;
+import org.netbeans.modules.websvc.rest.client.ClientJavaSourceHelper.PathFormat;
+import org.netbeans.modules.websvc.rest.model.api.HttpMethod;
+import org.netbeans.modules.websvc.rest.support.JavaSourceHelper;
+import org.netbeans.modules.websvc.saas.model.WadlSaasMethod;
+import org.netbeans.modules.websvc.saas.model.oauth.Metadata;
+
+import com.sun.source.tree.ClassTree;
+import com.sun.source.tree.ExpressionTree;
+import com.sun.source.tree.MethodTree;
+import com.sun.source.tree.ModifiersTree;
+import com.sun.source.tree.Tree;
+import com.sun.source.tree.TypeParameterTree;
+import com.sun.source.tree.VariableTree;
+
+
+/**
+ * 
+ * JAX-RS 2.X client generation strategy
+ * ( Jersey 2.X is used in specific cases ).
+ * @author ads
+ *
+ */
+class JaxRsGenerationStrategy extends ClientGenerationStrategy {
+
+    /* (non-Javadoc)
+     * @see org.netbeans.modules.websvc.rest.client.ClientGenerationStrategy#generateFields(org.netbeans.api.java.source.TreeMaker, org.netbeans.api.java.source.WorkingCopy, com.sun.source.tree.ClassTree, java.lang.String, org.netbeans.modules.websvc.rest.client.Security)
+     */
+    @Override
+    ClassTree generateFields( TreeMaker maker, WorkingCopy copy,
+            ClassTree classTree, String resourceURI, Security security )
+    {
+     // add 3 fields
+        ModifiersTree fieldModif =  maker.Modifiers(Collections.<Modifier>singleton(Modifier.PRIVATE));
+        Tree typeTree = JavaSourceHelper.createTypeTree(copy, "javax.ws.rs.client.WebTarget"); //NOI18N
+        VariableTree fieldTree = maker.Variable(fieldModif, "webTarget", typeTree, null); //NOI18N
+        ClassTree modifiedClass = maker.addClassMember(classTree, fieldTree);
+
+        fieldModif =  maker.Modifiers(Collections.<Modifier>singleton(Modifier.PRIVATE));
+        typeTree = JavaSourceHelper.createTypeTree(copy, "javax.ws.rs.client.Client"); //NOI18N
+        fieldTree = maker.Variable(fieldModif, "client", typeTree, null); //NOI18N
+        modifiedClass = maker.addClassMember(modifiedClass, fieldTree);
+
+        Set<Modifier> modifiersSet = new HashSet<Modifier>();
+        modifiersSet.add(Modifier.PRIVATE);
+        modifiersSet.add(Modifier.STATIC);
+        modifiersSet.add(Modifier.FINAL);
+        fieldModif =  maker.Modifiers(modifiersSet);
+        typeTree = maker.Identifier("String"); //NOI18N
+
+        String baseUri = resourceURI;
+        if (security.isSSL() && resourceURI.startsWith("http:")) { //NOI18N
+            baseUri = "https:"+resourceURI.substring(5); //NOI18N
+        }
+        fieldTree = maker.Variable(fieldModif, "BASE_URI", typeTree, maker.Literal(baseUri)); //NOI18N
+        modifiedClass = maker.addClassMember(modifiedClass, fieldTree);      
+        return modifiedClass;
+    }
+
+    /* (non-Javadoc)
+     * @see org.netbeans.modules.websvc.rest.client.ClientGenerationStrategy#generateConstructor(org.netbeans.api.java.source.TreeMaker, org.netbeans.api.java.source.WorkingCopy, com.sun.source.tree.ClassTree, org.netbeans.modules.websvc.rest.client.ClientJavaSourceHelper.PathFormat, org.netbeans.modules.websvc.rest.client.Security)
+     */
+    @Override
+    MethodTree generateConstructor( TreeMaker maker, WorkingCopy copy,
+            ClassTree classTree, PathFormat pf, Security security )
+    {
+        ModifiersTree methodModifier = maker.Modifiers(
+                Collections.<Modifier>singleton(Modifier.PUBLIC));
+        boolean isSubresource = (pf.getArguments().length>0);
+
+        List<VariableTree> paramList = new ArrayList<VariableTree>();
+        if (isSubresource) {
+            for (String arg : pf.getArguments()) {
+                Tree argTypeTree = maker.Identifier("String"); //NOI18N
+                ModifiersTree fieldModifier = maker.Modifiers(
+                        Collections.<Modifier>emptySet());
+                VariableTree argFieldTree = maker.Variable(fieldModifier, 
+                        arg, argTypeTree, null); //NOI18N
+                paramList.add(argFieldTree);
+            }
+        }
+
+        String resURI = null; //NOI18N
+        String subresourceExpr = ""; //NOI18N
+        if (isSubresource) {
+            subresourceExpr = "    String resourcePath = "+
+                    getPathExpression(pf)+";";                      //NOI18N
+            resURI = "resourcePath";                                //NOI18N
+        } else {
+            resURI = getPathExpression(pf); //NOI18N
+        }
+
+        // XXX: requires Jersey API ( not just JAX-RS )  
+        String SSLExpr = security.isSSL() ?
+            "// SSL configuration\n" + //NOI18N
+            "client.setProperty(" +
+            "org.glassfish.jersey.client.ClientProperties.SSL_CONFIG, " + //NOI18N
+            "new org.glassfish.jersey.client.SslConfig(getHostnameVerifier(), getSSLContext()));": //NOI18N
+            ""; //NOI18N
+
+        String body =
+                "{"+                                                            //NOI18N
+                "   client = "+"javax.ws.rs.client.ClientFactory.newClient();"+ //NOI18N
+                SSLExpr+        
+                subresourceExpr +
+                ("\"\"".equals(resURI) ?
+                "   webTarget = client.target(BASE_URI);" : //NOI18N
+                "   webTarget = client.target(BASE_URI).path("+resURI+");") + //NOI18N
+                "}"; //NOI18N
+        return maker.Constructor (
+                methodModifier,
+                Collections.<TypeParameterTree>emptyList(),
+                paramList,
+                Collections.<ExpressionTree>emptyList(),
+                body);
+    }
+
+    /* (non-Javadoc)
+     * @see org.netbeans.modules.websvc.rest.client.ClientGenerationStrategy#generateSubresourceMethod(org.netbeans.api.java.source.TreeMaker, org.netbeans.api.java.source.WorkingCopy, com.sun.source.tree.ClassTree, org.netbeans.modules.websvc.rest.client.ClientJavaSourceHelper.PathFormat)
+     */
+    @Override
+    MethodTree generateSubresourceMethod( TreeMaker maker, WorkingCopy copy,
+            ClassTree classTree, PathFormat pf )
+    {
+        String body =
+                "{"+ //NOI18N
+                "   String resourcePath = "+getPathExpression(pf)+";"+ //NOI18N
+                "   webTarget = client.target(BASE_URI).path(resourcePath);"+ //NOI18N
+                "}"; //NOI18N
+        ModifiersTree methodModifier = maker.Modifiers(
+                Collections.<Modifier>singleton(Modifier.PUBLIC));
+        
+        List<VariableTree> paramList = new ArrayList<VariableTree>();
+        for (String arg : pf.getArguments()) {
+            Tree argTypeTree = maker.Identifier("String"); // NOI18N
+            ModifiersTree fieldModifier = maker.Modifiers(Collections
+                    .<Modifier> emptySet());
+            VariableTree argFieldTree = maker.Variable(fieldModifier, arg,
+                    argTypeTree, null); // NOI18N
+            paramList.add(argFieldTree);
+        }
+        return maker.Method(methodModifier,
+                "setResourcePath", // NOI18N
+                JavaSourceHelper.createTypeTree(copy, "void"), // NOI18N
+                Collections.<TypeParameterTree> emptyList(), paramList,
+                Collections.<ExpressionTree> emptyList(), body, null); // NOI18N
+    }
+
+    
+
+    /* (non-Javadoc)
+     * @see org.netbeans.modules.websvc.rest.client.ClientGenerationStrategy#generateClose(org.netbeans.api.java.source.TreeMaker, org.netbeans.api.java.source.WorkingCopy)
+     */
+    @Override
+    MethodTree generateClose( TreeMaker maker, WorkingCopy copy ) {
+        ModifiersTree methodModifier = maker.Modifiers(
+                Collections.<Modifier>singleton(Modifier.PUBLIC));
+        return maker.Method (
+                methodModifier,
+                "close", //NOI18N
+                JavaSourceHelper.createTypeTree(copy, "void"), //NOI18N
+                Collections.<TypeParameterTree>emptyList(),
+                Collections.<VariableTree>emptyList(),
+                Collections.<ExpressionTree>emptyList(),
+                "{"+ //NOI18N
+                "   client.close();"+ //NOI18N
+                "}", //NOI18N
+                null); 
+    }
+
+    /* (non-Javadoc)
+     * @see org.netbeans.modules.websvc.rest.client.ClientGenerationStrategy#generateBasicAuth(org.netbeans.api.java.source.TreeMaker, org.netbeans.api.java.source.WorkingCopy, java.util.List)
+     */
+    @Override
+    MethodTree generateBasicAuth( TreeMaker maker, WorkingCopy copy,
+            List<VariableTree> authParams )
+    {
+        ModifiersTree methodModifier = maker.Modifiers(
+                Collections.<Modifier>singleton(Modifier.PUBLIC));
+        String body =
+                "{"+ //NOI18N
+                "   client.register(new org.glassfish.jersey.client.filter.HTTPBasicAuthFilter(username, password));"+ //NOI18N
+                "}"; //NOI18N
+        return maker.Method(methodModifier,
+                "setUsernamePassword", // NOI18N
+                JavaSourceHelper.createTypeTree(copy, "void"), // NOI18N
+                Collections.<TypeParameterTree> emptyList(), authParams,
+                Collections.<ExpressionTree> emptyList(), body, null); // NOI18N
+    }
+    
+    @Override
+    MethodTree generateHttpPOSTMethod( WorkingCopy copy, HttpMethod httpMethod,
+            HttpMimeType requestMimeType, boolean multipleMimeTypes )
+    {
+        String methodPrefix = httpMethod.getType().toLowerCase();
+        String responseType = httpMethod.getReturnType();
+        String path = httpMethod.getPath();
+        String methodName = httpMethod.getName() + 
+                (multipleMimeTypes ? "_"+requestMimeType.name() : ""); //NOI18N
+
+        TreeMaker maker = copy.getTreeMaker();
+        ModifiersTree methodModifier = maker.Modifiers(
+                Collections.<Modifier>singleton(Modifier.PUBLIC));
+        ModifiersTree paramModifier = maker.Modifiers(Collections.<Modifier>emptySet());
+
+        VariableTree classParam = null;
+        ExpressionTree responseTree = null;
+        String bodyParam1 = ""; //NOI18N
+        String ret = ""; //NOI18N
+        List<TypeParameterTree> typeParams =  
+                Collections.<TypeParameterTree>emptyList();
+        
+        // create param list
+
+        List<VariableTree> paramList = new ArrayList<VariableTree>();
+        String bodyParam = "";
+        if (requestMimeType != null) {
+            if (requestMimeType == HttpMimeType.FORM) {
+                // PENDING
+            } else {
+                VariableTree objectParam = maker.Variable(paramModifier, 
+                        "requestEntity", maker.Identifier("Object"), null); //NOI18N
+                paramList.add(objectParam);
+                bodyParam= "requestEntity"; //NOI18N
+            }
+        }
+        
+        if ("javax.ws.rs.core.Response".equals(responseType)) {         //NOI18N
+            TypeElement clientResponseEl = copy.getElements().getTypeElement(
+                    "javax.ws.rs.core.Response");        //NOI18N
+            ret = "return "; //NOI18N
+            responseTree = (clientResponseEl == null ?
+                copy.getTreeMaker().Identifier("javax.ws.rs.core.Response") : // NOI18N
+                copy.getTreeMaker().QualIdent(clientResponseEl));
+            bodyParam1 = (clientResponseEl == null ?
+                "javax.ws.rs.core.Response.class" :      //NOI18N
+                "Response.class");                                //NOI18N
+        } 
+        else if ("void".equals(responseType)) {                         //NOI18N
+            responseTree = maker.Identifier("void");                    //NOI18N
+        } 
+        else if (String.class.getName().equals(responseType)) {            
+            responseTree = maker.Identifier("String");                  //NOI18N
+            ret = "return "; //NOI18N
+            bodyParam1="String.class";                                  //NOI18N
+        } 
+        else {
+            responseTree = maker.Identifier("T");                       //NOI18N
+            ret = "return ";                                            //NOI18N
+            bodyParam1="responseType";                                  //NOI18N
+            classParam = maker.Variable(paramModifier, "responseType", 
+                    maker.Identifier("Class<T>"), null);                //NOI18N
+            typeParams = Collections.<TypeParameterTree>singletonList(
+                    maker.TypeParameter("T", Collections.<ExpressionTree>emptyList()));
+        }
+
+        if (classParam != null) {
+            paramList.add(classParam);
+       
+        }
+        
+        if ( bodyParam.length() >0 && bodyParam1.length() >0 ){
+            bodyParam +=",";
+        }
+
+        // throws
+        ExpressionTree throwsTree = JavaSourceHelper.createTypeTree(copy, 
+                "javax.ws.rs.client.ClientException");     //NOI18N
+
+        if (path.length() == 0) {
+            // body
+            String body =
+                "{"+ 
+                    (requestMimeType == null ?
+                        "   "+ret+"webTarget.request()."+methodPrefix+"("+
+                        bodyParam +bodyParam1+");" :                    //NOI18N
+                        "   "+ret+"webTarget.request("+
+                            requestMimeType.getMediaType()+")."+methodPrefix+
+                            "("+bodyParam+bodyParam1+");") +               //NOI18N
+                "}";                                                        //NOI18N
+            return maker.Method (
+                    methodModifier,
+                    methodName,
+                    responseTree,
+                    typeParams,
+                    paramList,
+                    Collections.singletonList(throwsTree),
+                    body,
+                    null); 
+        } else {
+            // add path params to param list
+            PathFormat pf = getPathFormat(path);
+            for (String arg : pf.getArguments()) {
+                Tree typeTree = maker.Identifier("String");                 //NOI18N
+                ModifiersTree fieldModifier = maker.Modifiers(
+                        Collections.<Modifier>emptySet());
+                VariableTree fieldTree = maker.Variable(fieldModifier, arg, 
+                        typeTree, null); 
+                paramList.add(fieldTree);
+            }
+            // body
+            String body =
+                    "{"+ //NOI18N
+                        (requestMimeType == null ?
+                            "   "+ret+"webTarget.path("+getPathExpression(pf)+
+                            ").request()."+methodPrefix+"("+
+                                bodyParam +bodyParam1+");" :  //NOI18N
+                            "   "+ret+"webTarget.path("+getPathExpression(pf)+
+                            ").request("+requestMimeType.getMediaType()+")."+
+                            methodPrefix+"("+bodyParam+bodyParam1+");") +  //NOI18N
+                    "}"; //NOI18N
+            return maker.Method (
+                    methodModifier,
+                    methodName,
+                    responseTree,
+                    typeParams,
+                    paramList,
+                    Collections.<ExpressionTree>singletonList(throwsTree),
+                    body,
+                    null); //NOI18N
+        }
+    }
+
+    @Override
+    Collection<? extends MethodTree> generateHttpGETMethod( WorkingCopy copy,
+            HttpMethod httpMethod, HttpMimeType mimeType,
+            boolean multipleMimeTypes )
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    MethodTree generateHttpGETMethod( WorkingCopy copy,
+            WadlSaasMethod saasMethod, HttpMimeType mimeType,
+            boolean multipleMimeTypes, HttpParams httpParams, Security security )
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    MethodTree generateHttpPOSTMethod( WorkingCopy copy,
+            WadlSaasMethod saasMethod, HttpMimeType mimeType,
+            boolean multipleMimeTypes, HttpParams httpParams, Security security )
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    MethodTree generateFormMethod( TreeMaker maker, WorkingCopy copy ) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    MethodTree generateOptionalFormMethod( TreeMaker maker, WorkingCopy copy ) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    ClassTree generateOAuthMethods( String projectType, WorkingCopy copy,
+            ClassTree modifiedClass, Metadata oauthMetadata )
+    {
+        /*
+         *  There is no OAuth functionality available in Jersey 2.X
+         *  So this strategy should not be used when OAuth is required. 
+         */
+        assert false;
+        return modifiedClass;
+    }
+
+}
