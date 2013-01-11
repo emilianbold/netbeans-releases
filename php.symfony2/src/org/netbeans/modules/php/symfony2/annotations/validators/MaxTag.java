15fe29ef848e65e3a6d2c563964542f7a9cc7db0
Tomas Mysik <tmysik@netbeans.org>
2013-01-11 07:47:22 +0100

    #216786 cont'd - Missing Validators annotations
    Number Constraints.
    (transplanted from 0ea912a8d05077837d78f4c197b7f02975dc3200)

--- php.symfony2/src/org/netbeans/modules/php/symfony2/annotations/validators/MaxTag.java
+++ php.symfony2/src/org/netbeans/modules/php/symfony2/annotations/validators/MaxTag.java
@@ -41,62 +41,33 @@
  */
 package org.netbeans.modules.php.symfony2.annotations.validators;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import org.netbeans.modules.csl.api.HtmlFormatter;
 import org.netbeans.modules.php.spi.annotation.AnnotationCompletionTag;
-import org.netbeans.modules.php.spi.annotation.AnnotationCompletionTagProvider;
 import org.openide.util.NbBundle;
 
-// http://symfony.com/doc/current/book/validation.html#validation-constraints
-public class Symfony2ValidatorsAnnotationsProvider extends AnnotationCompletionTagProvider {
+public class MaxTag extends AnnotationCompletionTag {
 
-    @NbBundle.Messages("Symfony2ValidatorsAnnotationsProvider.name=Symfony2 Validators")
-    public Symfony2ValidatorsAnnotationsProvider() {
-        super("Symfony2 Validators Annotations", // NOI18N
-                Bundle.Symfony2ValidatorsAnnotationsProvider_name(),
-                null);
+    public MaxTag() {
+        super("Max", // NOI18N
+                "@Max(limit=${limit}, message=\"${message}\", invalidMessage=\"${invalidMessage}\")", // NOI18N
+                NbBundle.getMessage(MaxTag.class, "MaxTag.documentation"));
     }
 
     @Override
-    public List<AnnotationCompletionTag> getFunctionAnnotations() {
-        return Collections.emptyList();
-    }
-
-    @Override
-    public List<AnnotationCompletionTag> getTypeAnnotations() {
-        return Collections.emptyList();
-    }
-
-    @Override
-    public List<AnnotationCompletionTag> getFieldAnnotations() {
-        return getValidatorTags();
-    }
-
-    @Override
-    public List<AnnotationCompletionTag> getMethodAnnotations() {
-        return getValidatorTags();
-    }
-
-    private List<AnnotationCompletionTag> getValidatorTags() {
-        return Arrays.<AnnotationCompletionTag>asList(
-                new NotBlankTag(),
-                new BlankTag(),
-                new NotNullTag(),
-                new NullTag(),
-                new TrueTag(),
-                new FalseTag(),
-                new TypeTag(),
-                new EmailTag(),
-                new MinLengthTag(),
-                new MaxLengthTag(),
-                new LengthTag(),
-                new UrlTag(),
-                new RegexTag(),
-                new IpTag(),
-                new MaxTag(),
-                new MinTag(),
-                new RangeTag());
+    public void formatParameters(HtmlFormatter formatter) {
+        formatter.appendText("(limit="); //NOI18N
+        formatter.parameters(true);
+        formatter.appendText("limit"); //NOI18N
+        formatter.parameters(false);
+        formatter.appendText(", message="); //NOI18N
+        formatter.parameters(true);
+        formatter.appendText("\"message\""); //NOI18N
+        formatter.parameters(false);
+        formatter.appendText(", invalidMessage="); //NOI18N
+        formatter.parameters(true);
+        formatter.appendText("\"invalidMessage\""); //NOI18N
+        formatter.parameters(false);
+        formatter.appendText(")"); //NOI18N
     }
 
 }
