6c5f1974754e927755bb9370c73c25d0ebcf5742
Alexander Simon <alexvsimon@netbeans.org>
2013-01-24 12:29:27 +0400

    fixed Bug #225270 Wring implementation of MakeBasedProjectFactorySingleton.public Result isProject2(FileObject projectDirectory)

--- cnd.makeproject/src/org/netbeans/modules/cnd/makeproject/MakeBasedProjectFactorySingleton.java
+++ cnd.makeproject/src/org/netbeans/modules/cnd/makeproject/MakeBasedProjectFactorySingleton.java
@@ -113,7 +113,7 @@ public final class MakeBasedProjectFactorySingleton implements ProjectFactory2 {
             return null;
         }
         try {
-            Document projectXml = loadProjectXml(projectDirectory);
+            Document projectXml = loadProjectXml(projectFile);
             if (projectXml != null) {
                 Element typeEl = XMLUtil.findElement(projectXml.getDocumentElement(), "type", PROJECT_NS); // NOI18N
                 if (typeEl != null) {
