fdac98590179d9da7ed6d1fd015486851b0c3271
Ondrej Brejla <obrejla@netbeans.org>
2013-02-06 21:41:05 +0100

    Fixed FB issue.

--- php.editor/src/org/netbeans/modules/php/editor/verification/HintRule.java
+++ php.editor/src/org/netbeans/modules/php/editor/verification/HintRule.java
@@ -70,7 +70,7 @@ public abstract class HintRule implements CaretSensitiveRule, InvokableRule<Hint
         return currentLineBounds == OffsetRange.NONE || hintOffsetRange.overlaps(currentLineBounds);
     }
 
-    private synchronized OffsetRange getLineBounds(BaseDocument doc) {
+    private OffsetRange getLineBounds(BaseDocument doc) {
         if (lineBounds == null) {
             lineBounds = VerificationUtils.createLineBounds(caretOffset, doc);
         }
