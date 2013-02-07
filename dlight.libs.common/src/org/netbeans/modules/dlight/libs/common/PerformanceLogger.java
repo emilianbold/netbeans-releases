e39cbbad8134a1ae1471a728b7681f9ae9398903
Alexander Simon <alexvsimon@netbeans.org>
2013-02-07 19:06:13 +0400

    fixing Bug #225299 Detect C/C++ specific performance problem - implement post parsing analyzer

--- dlight.libs.common/src/org/netbeans/modules/dlight/libs/common/PerformanceLogger.java
+++ dlight.libs.common/src/org/netbeans/modules/dlight/libs/common/PerformanceLogger.java
@@ -106,6 +106,7 @@ public class PerformanceLogger {
         private final long start;
         private final long cpuTime;
         private final long userTime;
+        private final long threadID;
 
         private PerformaceAction(String id, Object source) {
             this.id = id;
@@ -115,14 +116,17 @@ public class PerformanceLogger {
                 if (CPU_TIME_AVAILABLE) {
                     cpuTime = threadMXBean.getCurrentThreadCpuTime();
                     userTime = threadMXBean.getCurrentThreadUserTime();
+                    threadID = Thread.currentThread().getId();
                 } else {
-                    cpuTime = 0;
-                    userTime = 0;
+                    cpuTime = -1;
+                    userTime = -1;
+                    threadID = -1;
                 }
             } else {
                 start = 0;
-                cpuTime = 0;
-                userTime = 0;
+                cpuTime = -1;
+                userTime = -1;
+                threadID = -1;
             }
         }
         //</editor-fold>
@@ -330,8 +334,15 @@ public class PerformanceLogger {
             long delta = System.nanoTime() - action.start;
             Runtime runtime = Runtime.getRuntime();
             long usedMemeory = runtime.totalMemory() - runtime.freeMemory();
-            long cpuTime = 0;
-            long userTime = 0;
+            long cpuTime;
+            long userTime;
+            if (CPU_TIME_AVAILABLE && action.cpuTime != -1 && action.userTime != -1 && action.threadID >= 0) {
+                cpuTime = threadMXBean.getThreadCpuTime(action.threadID) - action.cpuTime;
+                userTime = threadMXBean.getThreadUserTime(action.threadID) - action.userTime;
+            } else {
+                cpuTime = 0;
+                userTime = 0;
+            }
             PerformanceEvent event = new PerformanceEventImpl(action.id, action.source, action.start, delta, cpuTime, userTime, usedMemeory, new Object[0]);
             lineLock.writeLock().lock();
             try {
