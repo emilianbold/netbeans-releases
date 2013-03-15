5fd8ee8df451d9bbd5cb303b5c0fed747acc8915
Marek Fukala <mfukala@netbeans.org>
2013-03-15 16:27:18 +0100

    improved cp vars find usages, find usages for cp mixins works

--- css.prep/src/org/netbeans/modules/css/prep/refactoring/CPWhereUsedQueryPlugin.java
+++ css.prep/src/org/netbeans/modules/css/prep/refactoring/CPWhereUsedQueryPlugin.java
@@ -60,7 +60,9 @@ import org.netbeans.modules.refactoring.api.Problem;
 import org.netbeans.modules.refactoring.api.WhereUsedQuery;
 import org.netbeans.modules.refactoring.spi.RefactoringElementsBag;
 import org.netbeans.modules.refactoring.spi.RefactoringPlugin;
+import org.netbeans.modules.web.common.api.DependencyType;
 import org.openide.filesystems.FileObject;
+import org.openide.util.Exceptions;
 import org.openide.util.Lookup;
 
 /**
@@ -96,14 +98,16 @@ public class CPWhereUsedQueryPlugin implements RefactoringPlugin {
                 case cp_variable:
                     findVariables(context, elements);
                     break;
-                    
+
                 case cp_mixin_name:
-                    //todo implement
+                    findMixins(context, elements);
                     break;
             }
 
         } catch (Exception /*IOException, ParseException*/ ex) {
-            return new Problem(true, ex.getLocalizedMessage());
+            Exceptions.printStackTrace(ex);
+            String msg = ex.getLocalizedMessage();
+            return new Problem(true, msg != null ? msg : ex.toString());
         }
         return null; //no problem
     }
@@ -111,16 +115,22 @@ public class CPWhereUsedQueryPlugin implements RefactoringPlugin {
     private void findVariables(RefactoringElementContext context, RefactoringElementsBag elements) throws IOException, ParseException {
         String varName = context.getElementName();
         int offset = context.getCaret();
-        
+
         CPModel model = CPModel.getModel(context.getParserResult());
         //find a variable the user tries to refactor on
         CPElement currentVar = model.getVariableAtOffset(offset);
-        if(currentVar == null) {
+        if (currentVar == null) {
             //shouldn't happen
-            return ;
+            return;
         }
+        
+        //
+        //XXX the algorithm is still erroneous as it doesn't expect a global 
+        //or local variable to be hidden by another local variable declaration
+        //
+        
         boolean useIndexForCurrentFile = false;
-        switch(currentVar.getType()) {
+        switch (currentVar.getType()) {
             case VARIABLE_GLOBAL_DECLARATION:
                 //if it's a global variable, then use just index data even for current file
                 useIndexForCurrentFile = true;
@@ -128,29 +138,73 @@ public class CPWhereUsedQueryPlugin implements RefactoringPlugin {
             case VARIABLE_USAGE:
                 //add all visible variable with the same name in the current file
                 Collection<CPElement> visibleVars = model.getVariables(offset);
-                for(CPElement vvar : visibleVars) {
-                    if(vvar.getName().equals(varName)) {
+                for (CPElement vvar : visibleVars) {
+                    if (vvar.getName().equals(varName)) {
                         WhereUsedElement elem = WhereUsedElement.create(context.getFileObject(), vvar.getName(), vvar.getRange(), ElementKind.VARIABLE);
                         elements.add(refactoring, elem);
                     }
                 }
                 break;
+            case VARIABLE_DECLARATION_MIXIN_PARAMS:
+            case VARIABLE_LOCAL_DECLARATION:
+                //just find local usages in this file
+                OffsetRange scope = currentVar.getScope();
+                for (CPElement var : model.getVariables()) {
+                    if (var.getName().equals(varName)) {
+                        if (scope.containsInclusive(var.getRange().getStart())) {
+                            WhereUsedElement elem = WhereUsedElement.create(context.getFileObject(), var.getName(), var.getRange(), ElementKind.VARIABLE);
+                            elements.add(refactoring, elem);
+                        }
+                    }
+                }
+                return; //exit -- just local items, do not run the code below searching over related files
+
+            default:
+                return; //unsupported type
         }
-        
-        
+
         //all files linked from the base file with their CP models
-        Map<FileObject, CPCssIndexModel> indexModels = CPUtils.getIndexModels(context.getFileObject(), useIndexForCurrentFile);
+        Map<FileObject, CPCssIndexModel> indexModels = CPUtils.getIndexModels(context.getFileObject(), DependencyType.REFERRING_AND_REFERRED, !useIndexForCurrentFile);
         for (Map.Entry<FileObject, CPCssIndexModel> entry : indexModels.entrySet()) {
             CPCssIndexModel im = entry.getValue();
             FileObject file = entry.getKey();
+            CPModel cpModel = CPModel.getModel(file);
             for (CPElementHandle var : im.getVariables()) {
-                if (var.getType() == CPElementType.VARIABLE_GLOBAL_DECLARATION && var.getName().equals(varName)) {
-                    CPElement cpElement = var.resolve(CPModel.getModel(file));
+                switch (var.getType()) {
+                    case VARIABLE_USAGE:
+                    case VARIABLE_GLOBAL_DECLARATION:
+                        if (var.getName().equals(varName)) {
+                            CPElement cpElement = var.resolve(cpModel);
+                            if (cpElement != null) {
+                                OffsetRange elementRange = cpElement.getRange();
+                                WhereUsedElement elem = WhereUsedElement.create(file, varName, elementRange, ElementKind.VARIABLE);
+                                elements.add(refactoring, elem);
+
+                            }
+                        }
+                        break;
+                }
+            }
+        }
+    }
+
+    private void findMixins(RefactoringElementContext context, RefactoringElementsBag elements) throws IOException, ParseException {
+        String mixinName = context.getElementName();
+
+        //all files linked from the base file with their CP models
+        Map<FileObject, CPCssIndexModel> indexModels = CPUtils.getIndexModels(context.getFileObject(), DependencyType.REFERRING_AND_REFERRED, false);
+        for (Map.Entry<FileObject, CPCssIndexModel> entry : indexModels.entrySet()) {
+            CPCssIndexModel im = entry.getValue();
+            FileObject file = entry.getKey();
+            CPModel cpModel = CPModel.getModel(file);
+            for (CPElementHandle var : im.getMixins()) {
+                //change both declarations and usages
+                if (var.getName().equals(mixinName)) {
+                    CPElement cpElement = var.resolve(cpModel);
                     if (cpElement != null) {
                         OffsetRange elementRange = cpElement.getRange();
-                        WhereUsedElement elem = WhereUsedElement.create(file, varName, elementRange, ElementKind.VARIABLE);
+                        WhereUsedElement elem = WhereUsedElement.create(file, mixinName, elementRange, ElementKind.METHOD);
                         elements.add(refactoring, elem);
-                        
                     }
                 }
             }
@@ -158,17 +212,17 @@ public class CPWhereUsedQueryPlugin implements RefactoringPlugin {
     }
 
     @Override
-        public Problem fastCheckParameters() {
+    public Problem fastCheckParameters() {
         return null;
     }
 
     @Override
-        public Problem checkParameters() {
+    public Problem checkParameters() {
         return null;
     }
 
     @Override
-        public void cancelRequest() {
+    public void cancelRequest() {
         cancelled = true;
     }
 }
