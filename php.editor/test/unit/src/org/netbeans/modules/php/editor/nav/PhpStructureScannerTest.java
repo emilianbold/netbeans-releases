840b6835dd10da12802d50c45cd64ade810f25a4
Ondrej Brejla <obrejla@netbeans.org>
2013-03-22 14:26:55 +0100

    Try to fix failing tests.
    Devided index dependent tests from simple AST based tests.

--- php.editor/test/unit/src/org/netbeans/modules/php/editor/nav/PhpStructureScannerTest.java
+++ php.editor/test/unit/src/org/netbeans/modules/php/editor/nav/PhpStructureScannerTest.java
@@ -42,32 +42,11 @@
 
 package org.netbeans.modules.php.editor.nav;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
-import org.netbeans.api.java.classpath.ClassPath;
-import org.netbeans.modules.csl.api.HtmlFormatter;
-import org.netbeans.modules.csl.api.StructureItem;
-import org.netbeans.modules.parsing.api.ParserManager;
-import org.netbeans.modules.parsing.api.ResultIterator;
-import org.netbeans.modules.parsing.api.Source;
-import org.netbeans.modules.parsing.api.UserTask;
-import org.netbeans.modules.php.editor.parser.PHPParseResult;
-import org.netbeans.modules.php.editor.parser.ParserTestBase;
-import org.netbeans.modules.php.editor.parser.TestHtmlFormatter;
-import org.netbeans.modules.php.project.api.PhpSourcePath;
-import org.netbeans.spi.java.classpath.support.ClassPathSupport;
-import org.openide.filesystems.FileObject;
-import org.openide.filesystems.FileUtil;
-
 /**
  *
  * @author Petr Pisl
  */
-public class PhpStructureScannerTest extends ParserTestBase{
+public class PhpStructureScannerTest extends PhpNavigatorTestBase{
 
     public PhpStructureScannerTest(String testName) {
         super(testName);
@@ -143,14 +122,6 @@ public class PhpStructureScannerTest extends ParserTestBase{
         performTest("structure/issue170712");
     }
 
-    public void testDeprecatedDeclarations() throws Exception {
-        performTest("structure/deprecatedDeclarations");
-    }
-
-    public void testDeprecatedTypes() throws Exception {
-        performTest("structure/deprecatedTypes");
-    }
-
     public void testFoldingMethod() throws Exception {
         checkFolds("testfiles/parser/foldingMethod.php");
     }
@@ -181,79 +152,6 @@ public class PhpStructureScannerTest extends ParserTestBase{
 
     public void testIssue216088() throws Exception {
         checkFolds("testfiles/parser/issue216088.php");
-    }
-
-    @Override
-    protected String getTestResult(String filename) throws Exception {
-        StringBuilder sb = new StringBuilder();
-        FileObject testFile = getTestFile("testfiles/" + filename + ".php");
-
-        Source testSource = getTestSource(testFile);
-
-        final PhpStructureScanner instance = new PhpStructureScanner();
-        final List<StructureItem> result = new ArrayList<StructureItem>();
-        ParserManager.parse(Collections.singleton(testSource), new UserTask() {
-
-            @Override
-            public void run(ResultIterator resultIterator) throws Exception {
-                PHPParseResult info = (PHPParseResult)resultIterator.getParserResult();
-                if (info != null) {
-                    result.addAll(instance.scan(info));
-                }
-            }
-        });
-        Comparator<StructureItem> comparator = new Comparator<StructureItem>() {
-            @Override
-            public int compare(StructureItem o1, StructureItem o2) {
-                long position1 = o1.getPosition();
-                long position2 = o2.getPosition();
-                return (int) (position1 - position2);
-            }
-        };
-        Collections.sort(result,comparator);
-
-        for (StructureItem structureItem : result) {
-            Collections.sort(structureItem.getNestedItems(),comparator);
-            sb.append(printStructureItem(structureItem, 0));
-            sb.append("\n");
-        }
-        return sb.toString();
-    }
-
-    private String printStructureItem(StructureItem structureItem, int indent) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(indent(indent));
-        sb.append(structureItem.getName());
-        sb.append(" [");
-        sb.append(structureItem.getPosition());
-        sb.append(", ");
-        sb.append(structureItem.getEndPosition());
-        sb.append("] : ");
-        HtmlFormatter formatter = new TestHtmlFormatter() ;
-        sb.append(structureItem.getHtml(formatter));
-        for (StructureItem item : structureItem.getNestedItems()) {
-            sb.append("\n");
-            sb.append(printStructureItem(item, indent+1));
-        }
-        return sb.toString();
-    }
-
-    private String indent(int indent) {
-        String text = "|-";
-        for (int i = 0; i < indent; i++  ) {
-            text = text + "-";
-        }
-        return text;
-    }
-
-    @Override
-    protected Map<String, ClassPath> createClassPathsForTest() {
-        return Collections.singletonMap(
-            PhpSourcePath.SOURCE_CP,
-            ClassPathSupport.createClassPath(new FileObject[] {
-                FileUtil.toFileObject(new File(getDataDir(), "/testfiles/structure"))
-            })
-        );
     }
 
 }
