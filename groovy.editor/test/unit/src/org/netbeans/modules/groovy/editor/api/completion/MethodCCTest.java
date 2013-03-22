6915bdfcf1d8daf655ac38d91f3398d0ff70b180
Martin Janicek <mjanicek@netbeans.org>
2013-03-22 11:24:36 +0100

    #205951 - CC shows wrong leftShift return type in tests

--- groovy.editor/test/unit/src/org/netbeans/modules/groovy/editor/api/completion/MethodCCTest.java
+++ groovy.editor/test/unit/src/org/netbeans/modules/groovy/editor/api/completion/MethodCCTest.java
@@ -170,17 +170,12 @@ public class MethodCCTest extends GroovyCCTestBase {
         checkCompletion(BASE + "CompletionNoDot1.groovy", "        n^", false);
     }
 
-    // For some reason there is a StringBuilder as a return type for leftShift method
-    // This should be fixed, but it's not a top priority - see issue #205951
-
-    /*
     public void testCompletionNoPrefixString1() throws Exception {
-        checkCompletion(getTestFolderPath() + "CompletionNoPrefixString1.groovy", "println \"Hello $name!\".^", false);
+        checkCompletion(BASE + "CompletionNoPrefixString1.groovy", "println \"Hello $name!\".^", false);
     }
 
     public void testCompletionNoPrefixString2() throws Exception {
-        checkCompletion(getTestFolderPath() + "CompletionNoPrefixString2.groovy", "def name='Petr'.^", false);
+        checkCompletion(BASE + "CompletionNoPrefixString2.groovy", "def name='Petr'.^", false);
     }
-    */
 }
 
