82b6996cfba676764a4d1093164063951d2b082e
Martin Janicek <mjanicek@netbeans.org>
2013-03-27 10:57:36 +0100

    #227946 - [cc] Support for @Delegate transformation

---
+++ groovy.editor/test/unit/data/testfiles/completion/transformations/delegate1/Delegate1.groovy
@@ -0,0 +1,10 @@
+class Showcase {
+    @Delegate Delegation delegation
+    
+    void normal() {
+        
+    }
+}
+
+Showcase showcase = new Showcase();
+showcase.
---
+++ groovy.editor/test/unit/data/testfiles/completion/transformations/delegate1/Delegation.groovy
@@ -0,0 +1,4 @@
+
+public interface Delegation {
+    void showMe();
+}
