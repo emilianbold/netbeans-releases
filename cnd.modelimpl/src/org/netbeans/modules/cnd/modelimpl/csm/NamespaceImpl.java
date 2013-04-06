9672eb79643470132f9d943ad05977a730c3403a
Vladimir Voskresensky <vv159170@netbeans.org>
2013-04-07 02:24:39 +0400

    fix double check (use volatile)

--- cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/csm/NamespaceImpl.java
+++ cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/csm/NamespaceImpl.java
@@ -608,18 +608,19 @@ public class NamespaceImpl implements CsmNamespace, MutableDeclarationsContainer
         return _getProject();
     }
     
-    private CsmUID<CsmNamespace> uid = null;
+    private volatile CsmUID<CsmNamespace> uid = null;
     @Override
     public final CsmUID<CsmNamespace> getUID() {
         CsmUID<CsmNamespace> out = uid;
         if (out == null) {
             synchronized (this) {
                 if (uid == null) {
-                    uid = out = createUID();
+                    uid = createUID();
                 }
             }
+            return uid;
         }
-        return uid;
+        return out;
     }   
     
     protected CsmUID<CsmNamespace> createUID() {
