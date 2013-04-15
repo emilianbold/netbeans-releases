075bf581a7307aee1db3dd057e6715f761eac08d
Petr Pisl <ppisl@netbeans.org>
2013-04-15 10:57:36 +0200

    Adding comment

--- javascript2.editor/src/org/netbeans/modules/javascript2/editor/index/JsIndex.java
+++ javascript2.editor/src/org/netbeans/modules/javascript2/editor/index/JsIndex.java
@@ -64,6 +64,11 @@ public class JsIndex {
     private static final Logger LOG = Logger.getLogger(JsIndex.class.getName());
     private final QuerySupport querySupport;
     public static final String FIELD_BASE_NAME = "bn"; //NOI18N
+    /**
+     * In this field is in the lucene also coded, whether the object is anonymous (last char is 'A')
+     * or normal object (last cahr is 'O'). If someone needs to access this field
+     * directly, then has to be count with this.
+     */
     public static final String FIELD_FQ_NAME = "fqn"; //NOI18N
     public static final String FIELD_OFFSET = "offset"; //NOI18N
     public static final String FIELD_ASSIGNMENS = "assign"; //NOI18N
