c2a78a727f50386dbab1a692101afc06366a08a6
Milutin Kristofic <mkristofic@netbeans.org>
2013-04-16 14:56:06 +0200

    #217163 - There is no way to define deleted word interceptor/behavior

--- csl.api/src/org/netbeans/modules/csl/core/CslCamelCaseInterceptor.java
+++ csl.api/src/org/netbeans/modules/csl/core/CslCamelCaseInterceptor.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 1997-2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright 2013 Oracle and/or its affiliates. All rights reserved.
  *
  * Oracle and Java are registered trademarks of Oracle and/or its affiliates.
  * Other names may be trademarks of their respective owners.
@@ -24,12 +24,6 @@
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
  *
- * Contributor(s):
- *
- * The Original Software is NetBeans. The Initial Developer of the Original
- * Software is Sun Microsystems, Inc. Portions Copyright 1997-2006 Sun
- * Microsystems, Inc. All Rights Reserved.
- *
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -40,33 +34,57 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
- */
-
-package org.netbeans.modules.editor.java;
-
-import javax.swing.Action;
-import javax.swing.text.BadLocationException;
-import javax.swing.text.JTextComponent;
-
-/**
  *
- * @author Sandip V. Chitale (Sandip.Chitale@Sun.Com)
+ * Contributor(s):
+ *
+ * Portions Copyrighted 2013 Sun Microsystems, Inc.
  */
-/* package */ class NextCamelCasePosition extends AbstractCamelCasePosition {
+package org.netbeans.modules.csl.core;
 
-    public NextCamelCasePosition(Action originalAction) {
-        this(JavaKit.nextCamelCasePosition, originalAction);
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import org.netbeans.api.editor.mimelookup.MimePath;
+import org.netbeans.api.editor.mimelookup.MimeRegistration;
+import org.netbeans.api.editor.mimelookup.MimeRegistrations;
+import org.netbeans.modules.csl.api.KeystrokeHandler;
+import org.netbeans.modules.csl.api.UiUtils;
+import org.netbeans.spi.editor.typinghooks.CamelCaseInterceptor;
+import org.netbeans.spi.editor.typinghooks.TypedBreakInterceptor;
+
+public class CslCamelCaseInterceptor implements CamelCaseInterceptor {
+
+    @Override
+    public boolean beforeChange(MutableContext context) throws BadLocationException {
+        return false;
     }
 
-    protected NextCamelCasePosition(String name, Action originalAction) {
-        super(name, originalAction);
+    @Override
+    public void change(MutableContext context) throws BadLocationException {
+        int offset = context.getOffset();
+        Document doc = context.getDocument();
+
+        KeystrokeHandler bc = UiUtils.getBracketCompletion(doc, offset);
+        if (bc != null) {
+            int nextOffset = bc.getNextWordOffset(doc, offset, context.isBackward());
+            context.setNextWordOffset(nextOffset);
+        }
     }
 
-    protected int newOffset(JTextComponent textComponent) throws BadLocationException {
-        return CamelCaseOperations.nextCamelCasePosition(textComponent);
+    @Override
+    public void afterChange(MutableContext context) throws BadLocationException {
     }
 
-    protected void moveToNewOffset(JTextComponent textComponent, int offset) throws BadLocationException {
-        textComponent.setCaretPosition(offset);
+    @Override
+    public void cancelled(MutableContext context) {
+    }
+
+    @MimeRegistrations(value = {
+        @MimeRegistration(mimeType = "", service = CamelCaseInterceptor.Factory.class)})
+    public static class Factory implements CamelCaseInterceptor.Factory {
+
+        @Override
+        public CamelCaseInterceptor createCamelCaseInterceptor(MimePath mimePath) {
+            return new CslCamelCaseInterceptor();
+        }
     }
 }
