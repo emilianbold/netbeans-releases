1bdcd99f6fbe64adb1f357cab9be79bb435d255a
Jaroslav Tulach <jtulach@netbeans.org>
2013-04-23 16:00:28 +0200

    #228700: Fix and test by Jaroslav Havlin: Ignore \f

--- openide.loaders/src/org/openide/loaders/DefaultDataObject.java
+++ openide.loaders/src/org/openide/loaders/DefaultDataObject.java
@@ -263,7 +263,7 @@ final class DefaultDataObject extends MultiDataObject implements OpenCookie {
                     try {
                         int len = is.read (arr);
                         for (int i = 0; i < len; i++) {
-                            if (arr[i] >= 0 && arr[i] <= 31 && arr[i] != '\n' && arr[i] != '\r' && arr[i] != '\t') {
+                            if (arr[i] >= 0 && arr[i] <= 31 && arr[i] != '\n' && arr[i] != '\r' && arr[i] != '\t' && arr[i] != '\f') {
                                 return;
                             }
                         }
