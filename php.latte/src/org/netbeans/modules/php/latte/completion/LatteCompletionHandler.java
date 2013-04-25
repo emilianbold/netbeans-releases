2d309ef6bb4f4531747ea6d2419bacd1c989269f
Ondrej Brejla <obrejla@netbeans.org>
2013-04-25 12:23:57 +0200

    Added macro and helper items for CC.

--- php.latte/src/org/netbeans/modules/php/latte/completion/LatteCompletionHandler.java
+++ php.latte/src/org/netbeans/modules/php/latte/completion/LatteCompletionHandler.java
@@ -41,7 +41,9 @@
  */
 package org.netbeans.modules.php.latte.completion;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import javax.swing.text.Document;
@@ -54,12 +56,92 @@ import org.netbeans.modules.csl.api.ElementHandle;
 import org.netbeans.modules.csl.api.ParameterInfo;
 import org.netbeans.modules.csl.spi.DefaultCompletionResult;
 import org.netbeans.modules.csl.spi.ParserResult;
+import org.netbeans.modules.php.latte.completion.LatteElement.Parameter;
 
 /**
  *
  * @author Ondrej Brejla <obrejla@netbeans.org>
  */
 public class LatteCompletionHandler implements CodeCompletionHandler {
+    private static final Set<LatteElement> MACROS = new HashSet<>();
+    static {
+        MACROS.add(LatteElement.Factory.create("link", "link ${Presenter}:${action}")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("plink", "plink ${Presenter}:${action}")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("continueIf", "continueIf ${true}")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("breakIf", "breakif ${true}")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("if", "if ${true}")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("else")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("elseif", "elseif ${true}")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("ifset", "ifset $${var}")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("ifset block", "ifset #${block}")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("elseifset", "elseifset $${var}")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("ifCurrent", "ifCurrent ${Presenter}:${action}")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("for", "for ${init}; ${cond}; ${exec}")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("foreach", "foreach $${array} as $${item}")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("while", "while ${true}")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("include", "include '${file.latte}'")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("include block", "include #{block}'")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("extends", "extends '${latte.file}")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("layout", "layout '${latte.file}")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("control", "control ${name}")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("cache","cache $${key}")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("snippet", "snippet ${name}")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("attr")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("block", "block #${name}")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("define", "define #${name}")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("includeblock", "includeblock '${latte.file}'")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("contentType", "contentType $${type}")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("status", "status $${code}")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("capture", "capture $${var}")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("assign")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("default", "default $${name} = ${value}")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("var", "var $${name} = ${value}")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("dump", "dump $${var}")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("syntax", "syntax ${mode}")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("use", "use ${Class}")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("form", "form ${name}")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("label", "label ${name}")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("input", "input ${name}")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("debugbreak", "debugbreak ${cond}")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("l")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("r")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("first")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("last")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("sep")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("_")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("!")); //NOI18N
+        MACROS.add(LatteElement.Factory.create("!_")); //NOI18N
+    }
+
+    private static final Set<LatteElement> HELPERS = new HashSet<>();
+    static {
+        HELPERS.add(LatteElement.Factory.create("truncate", Arrays.asList(new Parameter[] {new Parameter("length"), new Parameter("append", "'…'")}))); //NOI18N
+        HELPERS.add(LatteElement.Factory.create("substr", Arrays.asList(new Parameter[] {new Parameter("offset"), new Parameter("length", "stringLegth")}))); //NOI18N
+        HELPERS.add(LatteElement.Factory.create("trim", Arrays.asList(new Parameter[] {new Parameter("charlist", " \\t\\n\\r\\0\\x0B\\xC2\\xA0")}))); //NOI18N
+        HELPERS.add(LatteElement.Factory.create("striptags")); //NOI18N
+        HELPERS.add(LatteElement.Factory.create("strip")); //NOI18N
+        HELPERS.add(LatteElement.Factory.create("webalize", Arrays.asList(new Parameter[] {new Parameter("charlist", "NULL"), new Parameter("lower", "true")}))); //NOI18N
+        HELPERS.add(LatteElement.Factory.create("toAscii")); //NOI18N
+        HELPERS.add(LatteElement.Factory.create("indent", Arrays.asList(new Parameter[] {new Parameter("level", "1"), new Parameter("char", "'\\t'")}))); //NOI18N
+        HELPERS.add(LatteElement.Factory.create("replace", Arrays.asList(new Parameter[] {new Parameter("search"), new Parameter("replace", "''")}))); //NOI18N
+        HELPERS.add(LatteElement.Factory.create("replaceRE", Arrays.asList(new Parameter[] {new Parameter("pattern"), new Parameter("replace", "''")}))); //NOI18N
+        HELPERS.add(LatteElement.Factory.create("padLeft", Arrays.asList(new Parameter[] {new Parameter("length"), new Parameter("pad", "' '")}))); //NOI18N
+        HELPERS.add(LatteElement.Factory.create("padRight", Arrays.asList(new Parameter[] {new Parameter("length"), new Parameter("pad", "' '")}))); //NOI18N
+        HELPERS.add(LatteElement.Factory.create("repeat", Arrays.asList(new Parameter[] {new Parameter("count")}))); //NOI18N
+        HELPERS.add(LatteElement.Factory.create("implode", Arrays.asList(new Parameter[] {new Parameter("glue", "''")}))); //NOI18N
+        HELPERS.add(LatteElement.Factory.create("nl2br")); //NOI18N
+        HELPERS.add(LatteElement.Factory.create("lower")); //NOI18N
+        HELPERS.add(LatteElement.Factory.create("upper")); //NOI18N
+        HELPERS.add(LatteElement.Factory.create("firstLower")); //NOI18N
+        HELPERS.add(LatteElement.Factory.create("capitalize")); //NOI18N
+        HELPERS.add(LatteElement.Factory.create("date", Arrays.asList(new Parameter[] {new Parameter("'format'")}))); //NOI18N
+        HELPERS.add(LatteElement.Factory.create("number", Arrays.asList(new Parameter[] {new Parameter("decimals", "0"), new Parameter("decPoint", "'.'")}))); //NOI18N
+        HELPERS.add(LatteElement.Factory.create("bytes", Arrays.asList(new Parameter[] {new Parameter("precision", "2")}))); //NOI18N
+        HELPERS.add(LatteElement.Factory.create("dataStream", Arrays.asList(new Parameter[] {new Parameter("mimetype", "NULL")}))); //NOI18N
+        HELPERS.add(LatteElement.Factory.create("url")); //NOI18N
+        HELPERS.add(LatteElement.Factory.create("length")); //NOI18N
+        HELPERS.add(LatteElement.Factory.create("null")); //NOI18N
+    }
 
     @Override
     public CodeCompletionResult complete(CodeCompletionContext context) {
