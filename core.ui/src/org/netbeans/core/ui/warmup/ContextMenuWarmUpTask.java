526f552ba1faa9059925eec0a09a50c518d0b22f
Jiri Rechtacek <jrechtacek@netbeans.org>
2013-04-26 21:30:43 +0200

    fix of broken apisupport.ant builds (see NB-Core-Build 10054-65)

--- core.ui/src/org/netbeans/core/ui/warmup/ContextMenuWarmUpTask.java
+++ core.ui/src/org/netbeans/core/ui/warmup/ContextMenuWarmUpTask.java
@@ -45,6 +45,7 @@
 package org.netbeans.core.ui.warmup;
 
 import java.awt.EventQueue;
+import java.util.logging.LogManager;
 
 import javax.swing.Action;
 import javax.swing.JMenuItem;
@@ -72,7 +73,9 @@ public final class ContextMenuWarmUpTask implements Runnable {
         assert EventQueue.isDispatchThread();
         // For first context menu.
         org.openide.actions.ActionManager.getDefault().getContextActions();
-        JMenuItem mi = new javax.swing.JMenuItem();
+        synchronized (LogManager.getLogManager()) {
+            JMenuItem mi = new javax.swing.JMenuItem();
+        }
         warmUpToolsPopupMenuItem();
     }
 
