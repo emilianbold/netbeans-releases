3f44b4f45df1adacb92fef1c29cdbd46051d5599
Ondrej Brejla <obrejla@netbeans.org>
2013-05-06 10:51:50 +0200

    Added basic Neon completion handler.

---
+++ languages.neon/src/org/netbeans/modules/languages/neon/NeonCompletionHandler.java
@@ -0,0 +1,120 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2013 Oracle and/or its affiliates. All rights reserved.
+ *
+ * Oracle and Java are registered trademarks of Oracle and/or its affiliates.
+ * Other names may be trademarks of their respective owners.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ *
+ * Contributor(s):
+ *
+ * Portions Copyrighted 2013 Sun Microsystems, Inc.
+ */
+package org.netbeans.modules.languages.neon;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+import javax.swing.text.Document;
+import javax.swing.text.JTextComponent;
+import org.netbeans.modules.csl.api.CodeCompletionContext;
+import org.netbeans.modules.csl.api.CodeCompletionHandler;
+import org.netbeans.modules.csl.api.CodeCompletionResult;
+import org.netbeans.modules.csl.api.CompletionProposal;
+import org.netbeans.modules.csl.api.ElementHandle;
+import org.netbeans.modules.csl.api.ParameterInfo;
+import org.netbeans.modules.csl.spi.DefaultCompletionResult;
+import org.netbeans.modules.csl.spi.ParserResult;
+
+/**
+ *
+ * @author Ondrej Brejla <obrejla@netbeans.org>
+ */
+public class NeonCompletionHandler implements CodeCompletionHandler {
+    private static final Collection<Character> AUTOPOPUP_STOP_CHARS = new TreeSet<>(
+            Arrays.asList('=', ';', '+', '-', '*', '/', '%', '(', ')', '[', ']', '{', '}', '?', ' ', '\t', '\n'));
+
+    @Override
+    public CodeCompletionResult complete(CodeCompletionContext context) {
+        final List<CompletionProposal> completionProposals = new ArrayList<>();
+        return new DefaultCompletionResult(completionProposals, false);
+    }
+
+    @Override
+    public String document(ParserResult info, ElementHandle element) {
+        return null;
+    }
+
+    @Override
+    public ElementHandle resolveLink(String link, ElementHandle originalHandle) {
+        return null;
+    }
+
+    @Override
+    public String getPrefix(ParserResult info, int caretOffset, boolean upToOffset) {
+        return "";
+    }
+
+    @Override
+    public QueryType getAutoQuery(JTextComponent component, String typedText) {
+        QueryType result = QueryType.ALL_COMPLETION;
+        if (typedText.length() == 0) {
+            result = QueryType.NONE;
+        } else {
+            char lastChar = typedText.charAt(typedText.length() - 1);
+            if (AUTOPOPUP_STOP_CHARS.contains(Character.valueOf(lastChar))) {
+                result = QueryType.STOP;
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public String resolveTemplateVariable(String variable, ParserResult info, int caretOffset, String name, Map parameters) {
+        return "";
+    }
+
+    @Override
+    public Set<String> getApplicableTemplates(Document doc, int selectionBegin, int selectionEnd) {
+        return null;
+    }
+
+    @Override
+    public ParameterInfo parameters(ParserResult info, int caretOffset, CompletionProposal proposal) {
+        return ParameterInfo.NONE;
+    }
+
+}
