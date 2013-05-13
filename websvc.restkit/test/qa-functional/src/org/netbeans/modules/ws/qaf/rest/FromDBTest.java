feda5ff72ef783cf4669b605ee52bf09b5b88dc5
Jiri Skrivanek <jskrivanek@netbeans.org>
2013-05-13 09:19:10 +0100

    Wait for wizard is closed.

--- websvc.restkit/test/qa-functional/src/org/netbeans/modules/ws/qaf/rest/FromDBTest.java
+++ websvc.restkit/test/qa-functional/src/org/netbeans/modules/ws/qaf/rest/FromDBTest.java
@@ -91,6 +91,7 @@ public class FromDBTest extends CRUDTest {
         jcbo.clearText();
         jcbo.typeText(getRestPackage() + ".service"); //NOI18N
         wo.btFinish().pushNoBlock();
+        wo.waitClosed();
         String generationTitle = Bundle.getStringTrimmed("org.netbeans.modules.j2ee.persistence.wizard.fromdb.Bundle", "TXT_EntityClassesGeneration");
         waitDialogClosed(generationTitle, 180000); // wait 3 minutes
         new EventTool().waitNoEvent(1500);
