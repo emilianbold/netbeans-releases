8c5b184f24e1f60b56f0219ed7951e943f258a5c
Tomas Zezula <tzezula@netbeans.org>
2013-05-16 15:02:16 +0200

    J2SEDeployActionProvider done.

--- java.j2sedeploy/src/org/netbeans/modules/java/j2sedeploy/J2SEDeployActionProvider.java
+++ java.j2sedeploy/src/org/netbeans/modules/java/j2sedeploy/J2SEDeployActionProvider.java
@@ -43,19 +43,31 @@ package org.netbeans.modules.java.j2sedeploy;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.atomic.AtomicBoolean;
+import org.apache.tools.ant.module.api.support.ActionUtils;
 import org.netbeans.api.annotations.common.NonNull;
 import org.netbeans.api.annotations.common.NullAllowed;
 import org.netbeans.api.project.Project;
 import org.netbeans.api.project.ProjectManager;
 import org.netbeans.modules.java.j2seproject.api.J2SEPropertyEvaluator;
+import org.netbeans.spi.project.ActionProgress;
 import org.netbeans.spi.project.ActionProvider;
 import org.netbeans.spi.project.LookupProvider;
 import org.netbeans.spi.project.ProjectServiceProvider;
 import org.netbeans.spi.project.support.ant.PropertyEvaluator;
+import org.openide.DialogDisplayer;
+import org.openide.NotifyDescriptor;
+import org.openide.execution.ExecutorTask;
+import org.openide.filesystems.FileObject;
+import org.openide.util.Exceptions;
 import org.openide.util.Lookup;
+import org.openide.util.NbBundle;
 import org.openide.util.Parameters;
 import org.openide.util.RequestProcessor;
+import org.openide.util.Task;
+import org.openide.util.TaskListener;
 
 /**
  *
@@ -70,6 +82,13 @@ public class J2SEDeployActionProvider implements ActionProvider {
     public static final String COMMAND_PACKAGE_IMAGE = "package-image";           //NOI18N
     public static final String COMMAND_PACKAGE_ALL = "package-all";               //NOI18N
     public static final String PROP_NATIVE_BUILDING = "native.bundling.enabled";    //NOI18N
+    public static final String BUILD_NATIVE_SCRIPT_PATH = "nbproject/build-native.xml"; //NOI18N
+
+    private static final String TARGET_BUILD_NATIVE = "build-native";               //NOI18N
+    private static final String PACKAGE_TYPE = "native.package.type";               //NOI18N
+    private static final String PACKAGE_TYPE_INSTALLERS = "installers";             //NOI18N
+    private static final String PACKAGE_TYPE_IMAGE = "image";                       //NOI18N
+    private static final String PACKAGE_TYPE_ALL = "all";                           //NOI18N
 
     private static final RequestProcessor RP = new RequestProcessor(J2SEDeployActionProvider.class);
 
@@ -90,15 +109,78 @@ public class J2SEDeployActionProvider implements ActionProvider {
         };
     }
 
+    @NbBundle.Messages("LBL_No_Build_XML_Found=The project does not have a {0} build script.")
     @Override
     public void invokeAction(String command, Lookup context) throws IllegalArgumentException {
-        
+        final Project prj = context.lookup(Project.class);
+        if (prj == null) {
+            throw new IllegalArgumentException(String.format(
+                "The context %s has no Project.",   //NOI18N
+                context));
+        }
+        final FileObject fo = prj.getProjectDirectory();
+        final FileObject buildScript = fo.getFileObject(BUILD_NATIVE_SCRIPT_PATH);
+        if (buildScript == null || !buildScript.isValid()) {
+            NotifyDescriptor nd = new NotifyDescriptor.Message(
+                Bundle.LBL_No_Build_XML_Found(BUILD_NATIVE_SCRIPT_PATH),
+                NotifyDescriptor.WARNING_MESSAGE);
+            DialogDisplayer.getDefault().notify(nd);
+            return;
+        }
+        final ActionProgress listener = ActionProgress.start(context);
+        boolean success = false;
+        try {
+            final Properties p = new Properties();
+            p.setProperty(PACKAGE_TYPE, toPackageType(command));
+            final ExecutorTask task = ActionUtils.runTarget(
+                buildScript,
+                new String[] {TARGET_BUILD_NATIVE},
+                p);
+            task.addTaskListener(new TaskListener() {
+                @Override
+                public void taskFinished(Task _tmp) {
+                    listener.finished(task.result() == 0);
+                }
+            });
+            success = true;
+        } catch (IOException ex) {            
+            Exceptions.printStackTrace(ex);
+        } finally {
+            if (!success) {
+                listener.finished(false);
+            }
+        }
     }
 
     @Override
     public boolean isActionEnabled(String command, Lookup context) throws IllegalArgumentException {
-        return listener.isEnabled();
-    }    
+        return supportsCommand(command) &&
+               listener.getProject().equals(context.lookup(Project.class)) &&
+               listener.isEnabled();
+    }
+
+    private boolean supportsCommand (@NonNull final String command) {
+        for (String supportedCommand : getSupportedActions()) {
+            if (supportedCommand.equals(command)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @NonNull
+    private static String toPackageType(@NonNull final String command) {
+        switch (command) {
+            case COMMAND_PACKAGE_ALL:
+                return PACKAGE_TYPE_ALL;
+            case COMMAND_PACKAGE_IMAGE:
+                return PACKAGE_TYPE_IMAGE;
+            case COMMAND_PACKAGE_INSTALLERS:
+                return PACKAGE_TYPE_INSTALLERS;
+            default:
+                throw new IllegalArgumentException(command);
+        }
+    }
 
     private static final class Listener implements Runnable, PropertyChangeListener {
 
@@ -149,6 +231,11 @@ public class J2SEDeployActionProvider implements ActionProvider {
                 cachedEnabled = res = isTrue(eval.getProperty(PROP_NATIVE_BUILDING));
             }
             return res;
+        }
+
+        @NonNull
+        Project getProject() {
+            return prj;
         }
 
         private static boolean isTrue(@NullAllowed String value) {
