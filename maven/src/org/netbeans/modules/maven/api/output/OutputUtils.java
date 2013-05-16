ed452de5fa19fd82f9241f369452689b38d1e8bd
Jaroslav Havlin <jhavlin@netbeans.org>
2013-05-16 15:41:29 +0200

    Fix regexp for matching stacktrace lines

--- maven/src/org/netbeans/modules/maven/api/output/OutputUtils.java
+++ maven/src/org/netbeans/modules/maven/api/output/OutputUtils.java
@@ -65,7 +65,7 @@ import static org.netbeans.modules.maven.api.output.Bundle.*;
  * @author mkleint
  */
 public final class OutputUtils {
-    public static final Pattern linePattern = Pattern.compile("(?:\\[catch\\])?\\sat (.*)\\((Native Method|(.*)\\.java\\:(\\d+))\\)"); //NOI18N
+    public static final Pattern linePattern = Pattern.compile("(?:\\[catch\\])?\\sat (.*)\\((?:Native Method|(.*)\\.java)\\:(\\d+)\\)"); //NOI18N
  
     /** Creates a new instance of OutputUtils */
     private OutputUtils() {
@@ -74,7 +74,7 @@ public final class OutputUtils {
     public static OutputListener matchStackTraceLine(String line, ClassPath classPath) {
         Matcher match = linePattern.matcher(line);
         OutputListener list = null;
-        if (match.matches() && match.groupCount() == 4) {
+        if (match.matches() && match.groupCount() == 3) {
             String method = match.group(1);
             String file = match.group(2);
             String lineNum = match.group(3);
