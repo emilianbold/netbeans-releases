f248befbc946f81feb3b5349070ae9c1d19cc05c
Svata Dedic <sdedic@netbeans.org>
2013-05-17 16:12:36 +0200

    Implemented detection of deadlock and offending indexer for scan cancel

--- parsing.api/src/org/netbeans/modules/parsing/impl/indexing/RepositoryUpdater.java
+++ parsing.api/src/org/netbeans/modules/parsing/impl/indexing/RepositoryUpdater.java
@@ -2935,7 +2935,7 @@ public final class RepositoryUpdater implements PathRegistryListener, PropertyCh
                         new FileObjectCrawler(rootFo, checkTimeStamps, entry, getCancelRequest(), getSuspendStatus()) : // rescan the whole root (no timestamp check)
                         new FileObjectCrawler(rootFo, files.toArray(new FileObject[files.size()]), checkTimeStamps, entry, getCancelRequest(), getSuspendStatus()); // rescan selected files (no timestamp check)
                     if (lctx != null) {
-                        lctx.noteRootScanning(root);
+                        lctx.noteRootScanning(root, true);
                     }
                     long t = System.currentTimeMillis();
                     final List<Indexable> resources = crawler.getResources();
@@ -3531,7 +3531,7 @@ public final class RepositoryUpdater implements PathRegistryListener, PropertyCh
 //            updateProgress(root);
             LogContext lctx = getLogContext();
             if (lctx != null) {
-                lctx.noteRootScanning(root);
+                lctx.noteRootScanning(root, false);
             }
             try {
                 final List<Indexable> indexables = new ArrayList<Indexable>();
@@ -3639,7 +3639,7 @@ public final class RepositoryUpdater implements PathRegistryListener, PropertyCh
                 LogContext lctx = getLogContext();
                 
                 if (lctx != null) {
-                    lctx.noteRootScanning(root);
+                    lctx.noteRootScanning(root, false);
                 }
                 this.updateProgress(root, true);
                 try {
@@ -3809,7 +3809,7 @@ public final class RepositoryUpdater implements PathRegistryListener, PropertyCh
                 }                
                 LogContext lctx = getLogContext();
                 if (lctx != null) {
-                    lctx.noteRootScanning(root);
+                    lctx.noteRootScanning(root, false);
                 }
                 this.updateProgress(root, true);
                 try {
@@ -4664,7 +4664,7 @@ public final class RepositoryUpdater implements PathRegistryListener, PropertyCh
             final BitSet startedIndexers = new BitSet(contexts.size());
             LogContext lctx = getLogContext();
             if (lctx != null) {
-                lctx.noteRootScanning(root);
+                lctx.noteRootScanning(root, false);
             }
             try {
                 createBinaryContexts(root, binaryIndexers, contexts);
@@ -4753,7 +4753,7 @@ public final class RepositoryUpdater implements PathRegistryListener, PropertyCh
                     LogContext lctx = getLogContext();
                     try {
                         if (lctx != null) {
-                            lctx.noteRootScanning(source);
+                            lctx.noteRootScanning(source, false);
                         }
                         if (scanSource (source, ctx.fullRescanSourceRoots.contains(source), ctx.sourcesForBinaryRoots.contains(source), indexers, outOfDateFiles, deletedFiles, recursiveListenersTime)) {
                             ctx.scannedRoots.add(source);
