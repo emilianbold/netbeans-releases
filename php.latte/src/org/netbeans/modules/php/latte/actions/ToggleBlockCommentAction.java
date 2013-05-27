5483eb7e0e835493018eb2fd3a0d382cc6b96240
Ondrej Brejla <obrejla@netbeans.org>
2013-05-27 15:54:55 +0200

    #230261 - Toggle comment action doesn't work in .latte files
    Second part - Latte commenting and uncommenting when the caret is inside Latte top tokens.

--- php.latte/src/org/netbeans/modules/php/latte/actions/ToggleBlockCommentAction.java
+++ php.latte/src/org/netbeans/modules/php/latte/actions/ToggleBlockCommentAction.java
@@ -42,11 +42,20 @@
 package org.netbeans.modules.php.latte.actions;
 
 import java.awt.event.ActionEvent;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.text.BadLocationException;
 import javax.swing.text.JTextComponent;
+import org.netbeans.api.lexer.Token;
+import org.netbeans.api.lexer.TokenSequence;
 import org.netbeans.editor.BaseAction;
+import org.netbeans.editor.BaseDocument;
+import org.netbeans.editor.Utilities;
 import org.netbeans.editor.ext.ExtKit;
 import org.netbeans.modules.csl.api.CslActions;
+import org.netbeans.modules.php.latte.lexer.LatteTopTokenId;
+import org.netbeans.modules.php.latte.utils.LatteLexerUtils;
 
 /**
  *
@@ -57,6 +66,8 @@ public class ToggleBlockCommentAction extends BaseAction {
     private static final Logger LOGGER = Logger.getLogger(ToggleBlockCommentAction.class.getName());
     private static final String FORCE_COMMENT = "force-comment"; //NOI18N
     private static final String FORCE_UNCOMMENT = "force-uncomment"; //NOI18N
+    private static final String COMMENT_DELIMITER_PART = "*"; //NOI18N
+    private static final int COMMENT_DELIMITER_PART_LENGTH = COMMENT_DELIMITER_PART.length(); //NOI18N
 
     public ToggleBlockCommentAction() {
         super(ExtKit.toggleCommentAction);
@@ -64,8 +75,132 @@ public class ToggleBlockCommentAction extends BaseAction {
 
     @Override
     public void actionPerformed(ActionEvent evt, JTextComponent target) {
-        LOGGER.info("TOGGLE COMMENT - not implemented for Latte parts");
-        performDefaultAction(evt, target);
+        if (target != null) {
+            if (!target.isEditable() || !target.isEnabled() || !(target.getDocument() instanceof BaseDocument)) {
+                target.getToolkit().beep();
+                return;
+            }
+            final int caretOffset = Utilities.isSelectionShowing(target) ? target.getSelectionStart() : target.getCaretPosition();
+            final BaseDocument baseDocument = (BaseDocument) target.getDocument();
+            final AtomicBoolean processedByLatte = new AtomicBoolean(false);
+            baseDocument.runAtomic(new Runnable() {
+
+                @Override
+                public void run() {
+                    performLatteAction(baseDocument, caretOffset, processedByLatte);
+                }
+            });
+            if (!processedByLatte.get()) {
+                performDefaultAction(evt, target);
+            }
+        }
+    }
+
+    private void performLatteAction(BaseDocument baseDocument, int caretOffset, AtomicBoolean processedByLatte) {
+        TokenSequence<? extends LatteTopTokenId> topTs = LatteLexerUtils.getLatteTopTokenSequence(baseDocument, caretOffset);
+        if (topTs != null) {
+            topTs.move(caretOffset);
+            topTs.moveNext();
+            topTs.movePrevious();
+            Token<? extends LatteTopTokenId> token = topTs.token();
+            if (token != null) {
+                LatteTopTokenId tokenId = token.id();
+                if (tokenId == LatteTopTokenId.T_LATTE_COMMENT || tokenId == LatteTopTokenId.T_LATTE_COMMENT_DELIMITER) {
+                    uncomment(baseDocument, topTs);
+                    processedByLatte.set(true);
+                } else if (tokenId == LatteTopTokenId.T_LATTE || tokenId == LatteTopTokenId.T_LATTE_DELIMITER || tokenId == LatteTopTokenId.T_LATTE_ERROR) {
+                    comment(baseDocument, topTs, processedByLatte);
+                }
+            }
+        } else {
+            processedByLatte.set(false);
+        }
+    }
+
+    private void comment(BaseDocument baseDocument, TokenSequence<? extends LatteTopTokenId> topTs, AtomicBoolean processedByLatte) {
+        if (moveToOpeningDelimiter(topTs)) {
+            int start = topTs.offset() + topTs.token().length();
+            if (moveToClosingDelimiter(topTs)) {
+                int end = topTs.offset() + COMMENT_DELIMITER_PART_LENGTH;
+                try {
+                    baseDocument.insertString(start, COMMENT_DELIMITER_PART, null);
+                    baseDocument.insertString(end, COMMENT_DELIMITER_PART, null);
+                } catch (BadLocationException ex) {
+                    LOGGER.log(Level.WARNING, null, ex);
+                }
+                processedByLatte.set(true);
+            }
+        }
+    }
+
+    private static boolean moveToOpeningDelimiter(TokenSequence<? extends LatteTopTokenId> topTs) {
+        boolean result = false;
+        while (topTs.movePrevious()) {
+            Token<? extends LatteTopTokenId> token = topTs.token();
+            if (token == null || token.id() == LatteTopTokenId.T_HTML) {
+                topTs.moveNext();
+                token = topTs.token();
+                if (token != null && token.id() == LatteTopTokenId.T_LATTE_DELIMITER) {
+                    result = true;
+                } else {
+                    result = false;
+                }
+                break;
+            } else {
+                continue;
+            }
+        }
+        return result;
+    }
+
+    private static boolean moveToClosingDelimiter(TokenSequence<? extends LatteTopTokenId> topTs) {
+        boolean result = false;
+        while (topTs.moveNext()) {
+            Token<? extends LatteTopTokenId> token = topTs.token();
+            if (token == null || token.id() == LatteTopTokenId.T_HTML) {
+                topTs.movePrevious();
+                token = topTs.token();
+                if (token != null && token.id() == LatteTopTokenId.T_LATTE_DELIMITER) {
+                    result = true;
+                } else {
+                    result = false;
+                }
+                break;
+            } else {
+                continue;
+            }
+        }
+        return result;
+    }
+
+    private void uncomment(BaseDocument baseDocument, TokenSequence<? extends LatteTopTokenId> topTs) {
+        moveToOpeningCommentDelimiter(topTs);
+        int start = topTs.offset() + topTs.token().length() - COMMENT_DELIMITER_PART_LENGTH;
+        moveToClosingCommentDelimiter(topTs);
+        int end = topTs.offset() - COMMENT_DELIMITER_PART_LENGTH;
+        try {
+            baseDocument.remove(start, COMMENT_DELIMITER_PART_LENGTH);
+            baseDocument.remove(end, COMMENT_DELIMITER_PART_LENGTH);
+        } catch (BadLocationException ex) {
+            LOGGER.log(Level.WARNING, null, ex);
+        }
+    }
+
+    private static void moveToOpeningCommentDelimiter(TokenSequence<? extends LatteTopTokenId> topTs) {
+        while (topTs.movePrevious()) {
+            Token<? extends LatteTopTokenId> token = topTs.token();
+            if (token != null && (token.id() == LatteTopTokenId.T_LATTE_COMMENT || token.id() == LatteTopTokenId.T_LATTE_COMMENT_DELIMITER)) {
+                continue;
+            } else {
+                topTs.moveNext();
+                break;
+            }
+        }
+    }
+
+    private static void moveToClosingCommentDelimiter(TokenSequence<? extends LatteTopTokenId> topTs) {
+        topTs.moveNext(); // T_LATTE_COMMENT - content
+        topTs.moveNext(); // T_LATTE_COMMENT_DELIMITER - closing
     }
 
     private void performDefaultAction(ActionEvent evt, JTextComponent target) {
