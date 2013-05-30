7703df7c94363358948979ae72ea9ff75d216733
mentlicher <mentlicher@netbeans.org>
2013-05-30 14:34:47 +0200

    #230241: Colors are read from UIManager. When not defined, try to use either light or dark colors, based on the tree background.

--- o.n.swing.plaf/src/org/netbeans/swing/plaf/metal/DarkMetalTheme.java
+++ o.n.swing.plaf/src/org/netbeans/swing/plaf/metal/DarkMetalTheme.java
@@ -146,6 +146,13 @@ public class DarkMetalTheme extends DefaultMetalTheme {
         UIManager.put( "nb.formdesigner.gap.fixed.color", new Color(112,112,112) ); //NOI18N
         UIManager.put( "nb.formdesigner.gap.resizing.color", new Color(116,116,116) ); //NOI18N
         UIManager.put( "nb.formdesigner.gap.min.color", new Color(104,104,104) ); //NOI18N
+
+        // debugger
+        UIManager.put( "nb.debugger.debugging.currentThread", new Color(30, 80, 28) ); //NOI18N
+        UIManager.put( "nb.debugger.debugging.highlightColor", new Color(40, 60, 38) ); //NOI18N
+        UIManager.put( "nb.debugger.debugging.BPHits", new Color(65, 65, 0)); //NOI18N
+        UIManager.put( "nb.debugger.debugging.bars.BPHits", new Color(120, 120, 25)); //NOI18N
+        UIManager.put( "nb.debugger.debugging.bars.currentThread", new Color(40, 100, 35)); //NOI18N
     }
 
     @Override
