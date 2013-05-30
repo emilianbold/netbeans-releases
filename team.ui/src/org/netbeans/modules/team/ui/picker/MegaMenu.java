fc1298ce7686e585b69b6b503af63768a0b25417
S. Aubrecht <saubrecht@netbeans.org>
2013-05-30 16:14:37 +0200

    initial selection in mega menu

--- team.ui/src/org/netbeans/modules/team/ui/picker/MegaMenu.java
+++ team.ui/src/org/netbeans/modules/team/ui/picker/MegaMenu.java
@@ -53,6 +53,7 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import org.netbeans.modules.team.ui.TeamServerManager;
 import org.netbeans.modules.team.ui.spi.TeamServer;
+import org.netbeans.modules.team.ui.util.treelist.ListNode;
 
 /**
  *
@@ -119,5 +120,9 @@ public class MegaMenu {
             PopupWindow.hidePopup();
             show( invoker );
         }
+    }
+
+    public void setInitialSelection( ListNode selNode ) {
+        selModel.setInitialSelection( selNode );
     }
 }
