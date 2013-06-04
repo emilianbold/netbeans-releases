c0cd5a4e75afe3a8aea0680732a4f133f2ef38e0
Milos Kleint <mkleint@netbeans.org>
2013-06-04 09:14:03 +0200

    #227101 fix tests that expect synchronous project reload

--- maven/src/org/netbeans/modules/maven/NbMavenProjectImpl.java
+++ maven/src/org/netbeans/modules/maven/NbMavenProjectImpl.java
@@ -447,6 +447,11 @@ public final class NbMavenProjectImpl implements Project {
     public void fireProjectReload() {
         //#227101 not only AWT and project read/write mutex has to be checked, there are some additional more
         //complex scenarios that can lead to deadlock. Just give up and always fire changes in separate RP.
+        if (Boolean.getBoolean("test.reload.sync")) {
+            reloadTask.run();
+            //for tests just do sync reload, even though silly, even sillier is to attempt to sync the threads..
+            return;
+        }
         reloadTask.schedule(0); //asuming here that schedule(0) will move the scheduled task in the queue if not yet executed
     }
 
