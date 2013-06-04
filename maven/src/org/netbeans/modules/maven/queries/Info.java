60923fceefa920e33470df9533107261adfbeb2c
Milos Kleint <mkleint@netbeans.org>
2013-06-04 13:12:25 +0200

    #230466 only remove the external listeners when our listener list shrinks to zero, not when external code is calling it repeatedly on empty list.

--- maven/src/org/netbeans/modules/maven/queries/Info.java
+++ maven/src/org/netbeans/modules/maven/queries/Info.java
@@ -173,7 +173,8 @@ public final class Info implements ProjectInformation, PropertyChangeListener {
         return project;
     }
     
-    @Override public void addPropertyChangeListener(PropertyChangeListener listener) {
+    @Override 
+    public synchronized void addPropertyChangeListener(PropertyChangeListener listener) {
         if (!pcs.hasListeners(null)) {
             project.getLookup().lookup(NbMavenProject.class).addPropertyChangeListener(this);
             preferenceChangeListener = new PreferenceChangeListener() {
@@ -190,9 +191,11 @@ public final class Info implements ProjectInformation, PropertyChangeListener {
         pcs.addPropertyChangeListener(listener);
     }
     
-    @Override public void removePropertyChangeListener(PropertyChangeListener listener) {
+    @Override 
+    public synchronized void removePropertyChangeListener(PropertyChangeListener listener) {
+        boolean had = pcs.hasListeners(null);
         pcs.removePropertyChangeListener(listener);
-        if (!pcs.hasListeners(null)) {
+        if (had && !pcs.hasListeners(null)) {
             project.getLookup().lookup(NbMavenProject.class).removePropertyChangeListener(this);
             NbPreferences.forModule(Info.class).removePreferenceChangeListener(preferenceChangeListener);
         }
