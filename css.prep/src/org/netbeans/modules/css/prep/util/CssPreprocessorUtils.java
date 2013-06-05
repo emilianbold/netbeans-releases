1a31e61340d7842b1bec2e868ec0c1d204f30638
Marek Fukala <mfukala@netbeans.org>
2013-06-05 10:12:52 +0200

    #228645 - removing unneeded indexer/index

--- css.prep/src/org/netbeans/modules/css/prep/util/CssPreprocessorUtils.java
+++ css.prep/src/org/netbeans/modules/css/prep/util/CssPreprocessorUtils.java
@@ -52,7 +52,6 @@ import java.util.logging.Logger;
 import java.util.regex.Pattern;
 import org.netbeans.api.annotations.common.CheckForNull;
 import org.netbeans.api.project.Project;
-import org.netbeans.modules.css.prep.CPIndex;
 import org.netbeans.modules.css.prep.CssPreprocessorType;
 import org.netbeans.modules.css.prep.preferences.CssPreprocessorPreferences;
 import org.netbeans.modules.web.common.api.CssPreprocessors;
@@ -112,18 +111,6 @@ public final class CssPreprocessorUtils {
     private static boolean askUser(String title, String question) {
         Object result = DialogDisplayer.getDefault().notify(new NotifyDescriptor.Confirmation(question, title, NotifyDescriptor.YES_NO_OPTION));
         return result == NotifyDescriptor.YES_OPTION;
-    }
-
-    public static boolean hasAnyFilesForCompiling(Project project, CssPreprocessorType fileType) {
-        try {
-            Collection<FileObject> files = CPIndex.get(project).findFiles(fileType);
-            LOGGER.log(Level.FINE, "Project {0} contains {1} {2} files", new Object[] {project.getProjectDirectory(), files.size(), fileType});
-            return !files.isEmpty();
-        } catch (IOException ex) {
-            LOGGER.log(Level.WARNING, null, ex);
-        }
-        // presume we have some files for processing
-        return true;
     }
 
     public static String encodeMappings(List<Pair<String, String>> mappings) {
