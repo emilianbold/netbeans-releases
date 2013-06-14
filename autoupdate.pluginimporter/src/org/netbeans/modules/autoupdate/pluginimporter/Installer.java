268af65ee5fea6c100a109a3f6c7daf2abf2daf4
Antonin Nebuzelsky <anebuzelsky@netbeans.org>
2013-06-14 14:26:21 +0200

    import settings and plugins also from 7.3.1

--- autoupdate.pluginimporter/src/org/netbeans/modules/autoupdate/pluginimporter/Installer.java
+++ autoupdate.pluginimporter/src/org/netbeans/modules/autoupdate/pluginimporter/Installer.java
@@ -74,7 +74,7 @@ public class Installer extends ModuleInstall {
     private static final List<String> VERSION_TO_CHECK =
             Arrays.asList (".netbeans/7.1.2", ".netbeans/7.1.1", ".netbeans/7.1", ".netbeans/7.0", ".netbeans/6.9"); //NOI18N
     private static final List<String> NEWER_VERSION_TO_CHECK =
-            Arrays.asList ("7.3", "7.2.1", "7.2"); //NOI18N
+            Arrays.asList ("7.3.1", "7.3", "7.2.1", "7.2"); //NOI18N
     private static final String IMPORTED = "imported"; // NOI18N
 
     @Override
