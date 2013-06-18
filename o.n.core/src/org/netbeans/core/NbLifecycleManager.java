779af09f314165c0bcde4860835dbf18a3fd3c73
Jaroslav Tulach <jtulach@netbeans.org>
2013-06-18 14:49:57 +0200

    #230452: onExit is supposed to be guarded by NbLifecycleManager.class. Thus isExitingMethod should be synchronized. (transplanted from 3442ccc85b7bc5fb43be490b9f64b80a658c6074)

--- o.n.core/src/org/netbeans/core/NbLifecycleManager.java
+++ o.n.core/src/org/netbeans/core/NbLifecycleManager.java
@@ -218,7 +218,7 @@ public final class NbLifecycleManager extends LifecycleManager {
         }
     }
     
-    public static boolean isExiting() {
+    public static synchronized boolean isExiting() {
         return onExit != null;
     }
 
