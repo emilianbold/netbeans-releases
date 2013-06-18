853c4e1f393c7d81392d52b8491203ee7c477245
Jaroslav Tulach <jtulach@netbeans.org>
2013-06-18 14:47:29 +0200

    #230042: Make sure value of cdl does not mutate inside of finishExitState method (transplanted from f1042cdb6e32feff39efe0524cab3204e5e7faf3)

--- o.n.core/src/org/netbeans/core/NbLifecycleManager.java
+++ o.n.core/src/org/netbeans/core/NbLifecycleManager.java
@@ -144,15 +144,15 @@ public final class NbLifecycleManager extends LifecycleManager {
         }
     }
     
-    private void finishExitState(CountDownLatch[] cdl, boolean clean) {
+    private void finishExitState(CountDownLatch cdl, boolean clean) {
         LOG.log(Level.FINE, "finishExitState {0} clean: {1}", new Object[]{Thread.currentThread(), clean});
         if (EventQueue.isDispatchThread()) {
             boolean prev = isExitOnEventQueue;
             if (!prev) {
                 isExitOnEventQueue = true;
                 try {
-                    LOG.log(Level.FINE, "waiting in EDT: {0} own: {1}", new Object[]{onExit, cdl[0]});
-                    if (cdl[0].await(5, TimeUnit.SECONDS)) {
+                    LOG.log(Level.FINE, "waiting in EDT: {0} own: {1}", new Object[]{onExit, cdl});
+                    if (cdl.await(5, TimeUnit.SECONDS)) {
                         LOG.fine("wait is over, return");
                         return;
                     }
@@ -171,16 +171,16 @@ public final class NbLifecycleManager extends LifecycleManager {
                 isExitOnEventQueue = prev;
             }
         }
-        LOG.log(Level.FINE, "About to block on {0}", cdl[0]);
+        LOG.log(Level.FINE, "About to block on {0}", cdl);
         try {
-            cdl[0].await();
+            cdl.await();
         } catch (InterruptedException ex) {
             LOG.log(Level.FINE, null, ex);
         } finally {
             if (clean) {
-                LOG.log(Level.FINE, "Cleaning {0} own {1}", new Object[] { onExit, cdl[0] });
+                LOG.log(Level.FINE, "Cleaning {0} own {1}", new Object[] { onExit, cdl });
                 synchronized (NbLifecycleManager.class) {
-                    assert cdl[0] == onExit;
+                    assert cdl == onExit;
                     onExit = null;
                 }
             }
@@ -202,12 +202,12 @@ public final class NbLifecycleManager extends LifecycleManager {
         try {
             CountDownLatch[] cdl = { null };
             if (blockForExit(cdl)) {
-                finishExitState(cdl, false);
+                finishExitState(cdl[0], false);
                 return;
             }
             NbLifeExit action = new NbLifeExit(0, status, cdl[0]);
             Mutex.EVENT.readAccess(action);
-            finishExitState(cdl, true);
+            finishExitState(cdl[0], true);
         } catch (Error | RuntimeException ex) {
             LOG.log(Level.SEVERE, "Error during shutdown", ex);
             throw ex;
