5d06e0881d45bb1aae9ea47de3b8826bd997be1a
Jan Becicka <jbecicka@netbeans.org>
2013-07-11 11:50:51 +0200

    Issue #231881 - Hard to run project from different platform

--- cordova/src/org/netbeans/modules/cordova/CordovaPerformer.java
+++ cordova/src/org/netbeans/modules/cordova/CordovaPerformer.java
@@ -102,7 +102,7 @@ public class CordovaPerformer implements BuildPerformer {
     
     private final RequestProcessor RP = new RequestProcessor(CordovaPerformer.class.getName(), 10);
 
-    private final int BUILD_SCRIPT_VERSION = 10;
+    private final int BUILD_SCRIPT_VERSION = 11;
     
     public static CordovaPerformer getDefault() {
         return Lookup.getDefault().lookup(CordovaPerformer.class);
