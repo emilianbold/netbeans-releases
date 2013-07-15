6dad946c391f80dde78395ac6db5fc4bba0d3f1c
Tomas Mysik <tmysik@netbeans.org>
2013-07-15 09:02:27 +0200

    #232583 - detect --sourcemap option availability on sass compiler and use it

--- css.prep/src/org/netbeans/modules/css/prep/sass/SassExecutable.java
+++ css.prep/src/org/netbeans/modules/css/prep/sass/SassExecutable.java
@@ -44,22 +44,30 @@ package org.netbeans.modules.css.prep.sass;
 import java.awt.EventQueue;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.ExecutionException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.netbeans.api.annotations.common.CheckForNull;
 import org.netbeans.api.extexecution.ExecutionDescriptor;
+import org.netbeans.api.extexecution.input.InputProcessor;
+import org.netbeans.api.extexecution.input.InputProcessors;
+import org.netbeans.api.extexecution.input.LineProcessor;
 import org.netbeans.modules.css.prep.options.CssPrepOptions;
 import org.netbeans.modules.css.prep.util.ExternalExecutable;
 import org.netbeans.modules.css.prep.util.ExternalExecutableValidator;
 import org.netbeans.modules.css.prep.util.FileUtils;
 import org.netbeans.modules.css.prep.util.InvalidExternalExecutableException;
 import org.netbeans.modules.css.prep.util.UiUtils;
+import org.netbeans.modules.web.common.api.Version;
 import org.openide.filesystems.FileUtil;
 import org.openide.util.NbBundle;
 import org.openide.windows.IOProvider;
+import org.openide.windows.InputOutput;
 
 /**
  * Class representing <tt>sass</tt> command line tool.
@@ -72,8 +80,15 @@ public final class SassExecutable {
     public static final String EXECUTABLE_LONG_NAME = EXECUTABLE_NAME + FileUtils.getScriptExtension(true, false);
 
     private static final String DEBUG_PARAM = "--debug-info"; // NOI18N
+    private static final String SOURCEMAP_PARAM = "--sourcemap"; // NOI18N
+    private static final String VERSION_PARAM = "--version"; // NOI18N
 
     private static final File TMP_DIR = new File(System.getProperty("java.io.tmpdir")); // NOI18N
+
+    private static final Version MINIMAL_VERSION_WITH_SOURCEMAP = Version.fromDottedNotationWithFallback("3.3.0"); // NOI18N
+
+    // version of the compiler set in ide options
+    private static volatile Version version;
 
     private final String sassPath;
 
@@ -100,6 +115,40 @@ public final class SassExecutable {
     @NbBundle.Messages("Sass.executable.label=Sass executable")
     public static String validate(String path) {
         return ExternalExecutableValidator.validateCommand(path, Bundle.Sass_executable_label());
+    }
+
+    public static void resetVersion() {
+        version = null;
+    }
+
+    @CheckForNull
+    private static Version getVersion() {
+        assert !EventQueue.isDispatchThread();
+        if (version != null) {
+            return version;
+        }
+        VersionOutputProcessorFactory versionOutputProcessorFactory = new VersionOutputProcessorFactory();
+        try {
+            SassExecutable sassExecutable = getDefault();
+            sassExecutable.getExecutable("Sass version") // NOI18N
+                    .additionalParameters(Collections.singletonList(VERSION_PARAM))
+                    .runAndWait(getSilentDescriptor(), versionOutputProcessorFactory, "Detecting Sass version..."); // NOI18N
+            String detectedVersion = versionOutputProcessorFactory.getVersion();
+            if (detectedVersion != null) {
+                version = Version.fromDottedNotationWithFallback(detectedVersion);
+                return version;
+            }
+        } catch (CancellationException ex) {
+            // cancelled, cannot happen
+            assert false;
+        } catch (ExecutionException ex) {
+            LOGGER.log(Level.INFO, null, ex);
+        } catch (InvalidExternalExecutableException ex) {
+            // cannot happen
+            LOGGER.log(Level.WARNING, null, ex);
+            assert false;
+        }
+        return null;
     }
 
     @NbBundle.Messages("Sass.compile=Sass (compile)")
@@ -149,18 +198,76 @@ public final class SassExecutable {
                 .postExecution(postTask);
     }
 
+    private static ExecutionDescriptor getSilentDescriptor() {
+        return new ExecutionDescriptor()
+                .inputOutput(InputOutput.NULL)
+                .inputVisible(false)
+                .frontWindow(false)
+                .showProgress(false);
+    }
+
     private List<String> getParameters(File inputFile, File outputFile) {
         List<String> params = new ArrayList<>();
         // debug
         boolean debug = CssPrepOptions.getInstance().getSassDebug();
         if (debug) {
             params.add(DEBUG_PARAM);
+            Version installedVersion = getVersion();
+            if (installedVersion != null
+                    && installedVersion.isAboveOrEqual(MINIMAL_VERSION_WITH_SOURCEMAP)) {
+                params.add(SOURCEMAP_PARAM);
+            }
         }
         // input
         params.add(inputFile.getAbsolutePath());
         // output
         params.add(outputFile.getAbsolutePath());
         return params;
+    }
+
+    //~ Inner classes
+
+    static final class VersionOutputProcessorFactory implements ExecutionDescriptor.InputProcessorFactory {
+
+        // simply catch all numbers and dots only
+        static final Pattern VERSION_PATTERN = Pattern.compile("Sass\\s+(\\d+(\\.\\d+)*)", Pattern.CASE_INSENSITIVE); // NOI18N
+
+        private volatile String version;
+
+
+        @Override
+        public InputProcessor newInputProcessor(InputProcessor defaultProcessor) {
+            return InputProcessors.bridge(new LineProcessor() {
+
+                @Override
+                public void processLine(String line) {
+                    assert version == null : version + " :: " + line;
+                    version = parseVersion(line);
+                }
+
+                @Override
+                public void reset() {
+                }
+
+                @Override
+                public void close() {
+                }
+
+            });
+        }
+
+        public String getVersion() {
+            return version;
+        }
+
+        static String parseVersion(String line) {
+            Matcher matcher = VERSION_PATTERN.matcher(line);
+            if (matcher.find()) {
+                return matcher.group(1);
+            }
+            return null;
+        }
+
     }
 
 }
