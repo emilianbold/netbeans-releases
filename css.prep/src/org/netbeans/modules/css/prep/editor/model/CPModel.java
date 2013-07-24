48b00b35689212b602092afca70a275046ac9ea7
Marek Fukala <mfukala@netbeans.org>
2013-07-24 09:54:06 +0200

    #233283 - do not access CssParserResult outside of parsing task - making CPModel fully initialized in the constructor so can be safely held and used outside of parsing task.

--- css.prep/src/org/netbeans/modules/css/prep/editor/model/CPModel.java
+++ css.prep/src/org/netbeans/modules/css/prep/editor/model/CPModel.java
@@ -59,6 +59,7 @@ import org.netbeans.modules.css.lib.api.NodeVisitor;
 import org.netbeans.modules.css.prep.editor.CPType;
 import org.netbeans.modules.parsing.api.ParserManager;
 import org.netbeans.modules.parsing.api.ResultIterator;
+import org.netbeans.modules.parsing.api.Snapshot;
 import org.netbeans.modules.parsing.api.Source;
 import org.netbeans.modules.parsing.api.UserTask;
 import org.netbeans.modules.parsing.spi.ParseException;
@@ -89,9 +90,6 @@ public class CPModel {
         "@warn",
         "@while"
     });
-    private CssParserResult result;
-//    private Set<Element> vars;
-    private CPType cpType;
 
     public static CPModel getModel(CssParserResult result) {
         CPModel curr = result.getProperty(CPModel.class);
@@ -104,8 +102,6 @@ public class CPModel {
 
     /**
      * Will run parsing task!.
-     *
-     * XXX CPModel shouldn't be exposed outside of the parsing task :-(
      *
      * @param file
      * @return
@@ -128,39 +124,61 @@ public class CPModel {
 
         return model_ref.get();
     }
+    private CPType cpType;
+    private final Snapshot snapshot;
+    private final Collection<CPElement> variables, mixins;
 
     private CPModel(CssParserResult result) {
-        this.result = result;
+        snapshot = result.getSnapshot();
+        variables = findVariables(result);
+        mixins = findMixins(result);
+        cpType = findPreprocessorType();
     }
 
-    public FileObject getFile() {
-        return result.getSnapshot().getSource().getFileObject();
+    /**
+     * Gets all variables from this file's model.
+     *
+     * @return
+     */
+    public Collection<CPElement> getVariables() {
+        return variables;
+    }
+
+    public Collection<CPElement> getMixins() {
+        return mixins;
     }
 
     public CPType getPreprocessorType() {
-        if (cpType == null) {
-            String fileMimetype = topLevelSnapshotMimetype != null
-                    ? topLevelSnapshotMimetype //unit tests - fileless snapshots
-                    : result.getSnapshot().getSource().getFileObject().getMIMEType();
-            if (fileMimetype == null) {
-                cpType = CPType.NONE;
-            } else {
-                switch (fileMimetype) {
-                    case "text/less":
-                        //NOI18N
-                        cpType = CPType.LESS;
-                        break;
-                    case "text/scss":
-                        //NOI18N
-                        cpType = CPType.SCSS;
-                        break;
-                    default:
-                        cpType = CPType.NONE;
-                        break;
-                }
+        return cpType;
+    }
+
+    public FileObject getFile() {
+        return snapshot.getSource().getFileObject();
+    }
+
+    private CPType findPreprocessorType() {
+        CPType type;
+        String fileMimetype = topLevelSnapshotMimetype != null
+                ? topLevelSnapshotMimetype //unit tests - fileless snapshots
+                : snapshot.getSource().getFileObject().getMIMEType();
+        if (fileMimetype == null) {
+            type = CPType.NONE;
+        } else {
+            switch (fileMimetype) {
+                case "text/less":
+                    //NOI18N
+                    type = CPType.LESS;
+                    break;
+                case "text/scss":
+                    //NOI18N
+                    type = CPType.SCSS;
+                    break;
+                default:
+                    type = CPType.NONE;
+                    break;
             }
         }
-        return cpType;
+        return type;
     }
 
     /**
@@ -194,6 +212,7 @@ public class CPModel {
 
     /**
      * Returns a variable at the given position if there's any.
+     *
      * @param offset
      * @return found variable or null
      */
@@ -212,13 +231,13 @@ public class CPModel {
      *
      * @return
      */
-    public Collection<CPElement> getVariables() {
+    private Collection<CPElement> findVariables(CssParserResult result) {
         final Collection<CPElement> vars = new ArrayList<>();
         NodeVisitor visitor = new NodeVisitor() {
             private boolean in_cp_variable_declaration, in_block_control, in_block;
             private Stack<OffsetRange> contexts = new Stack<>();
             private Collection<CPElement> elementsAwaitingBlockNode = new ArrayList<>();
-            
+
             @Override
             public boolean visit(Node node) {
 
@@ -227,13 +246,13 @@ public class CPModel {
                     case declarations:
                         OffsetRange range = new OffsetRange(node.from(), node.to());
                         contexts.push(range);
-                        
+
                         //set scope to the elements preceeding this block node but defining its scope as the block content
-                        for(CPElement e : elementsAwaitingBlockNode) {
+                        for (CPElement e : elementsAwaitingBlockNode) {
                             e.setScope(range);
                         }
                         elementsAwaitingBlockNode.clear();
-                        
+
                         //the declarations node represents a content of a code block
                         in_block = true;
 
@@ -282,7 +301,7 @@ public class CPModel {
                         OffsetRange variableRange = new OffsetRange(node.from(), node.to());
                         CPElement element = new CPElement(handle, variableRange, scope);
 
-                        switch(type) {
+                        switch (type) {
                             case VARIABLE_DECLARATION_IN_BLOCK_CONTROL:
                                 //scope is null as the variable is declared in sass control before the actual block node
                                 //just remember the element and we will set the proper block scope later during parsing
@@ -318,8 +337,8 @@ public class CPModel {
     }
 
     //XXX mixin usages!
-    public Collection<CPElement> getMixins() {
-        final Collection<CPElement> mixins = new ArrayList<>();
+    private Collection<CPElement> findMixins(CssParserResult result) {
+        final Collection<CPElement> items = new ArrayList<>();
         NodeVisitor visitor = new NodeVisitor() {
             @Override
             public boolean visit(Node node) {
@@ -331,10 +350,10 @@ public class CPModel {
                             OffsetRange variableRange = new OffsetRange(mixin_name.from(), mixin_name.to());
                             OffsetRange scope = null; //TODO implement!
                             CPElement element = new CPElement(handle, variableRange, scope);
-                            mixins.add(element);
+                            items.add(element);
                         }
                         break;
-                        
+
                     case cp_mixin_call:
                         mixin_name = NodeUtil.getChildByType(node, NodeType.cp_mixin_name);
                         if (mixin_name != null) {
@@ -342,10 +361,10 @@ public class CPModel {
                             OffsetRange variableRange = new OffsetRange(mixin_name.from(), mixin_name.to());
                             OffsetRange scope = null; //TODO implement!
                             CPElement element = new CPElement(handle, variableRange, scope);
-                            mixins.add(element);
+                            items.add(element);
                         }
                         break;
-                        
+
                     default:
                         //visit children
                         List<Node> children = node.children();
@@ -360,7 +379,7 @@ public class CPModel {
             }
         };
         visitor.visit(result.getParseTree());
-        return mixins;
+        return items;
     }
 
     public Collection<String> getMixinNames() {
