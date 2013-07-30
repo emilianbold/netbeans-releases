5872aa0b17a93bc1fcc26ab46cfa804d391a7ca2
Petr Pisl <ppisl@netbeans.org>
2013-07-30 21:04:19 +0200

    #233237 - Parameter is visible in navigator as type

--- javascript2.editor/test/unit/src/org/netbeans/modules/javascript2/editor/JsStructureScannerTest.java
+++ javascript2.editor/test/unit/src/org/netbeans/modules/javascript2/editor/JsStructureScannerTest.java
@@ -431,4 +431,8 @@ public class JsStructureScannerTest extends JsTestBase {
     public void testIssue228289() throws Exception {
         checkStructure("testfiles/structure/issue228289.js");
     }
+    
+    public void testIssue233237() throws Exception {
+        checkStructure("testfiles/structure/issue233237.js");
+    }
 }
