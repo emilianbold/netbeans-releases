5b2ba1d3913698716a8110746d25adcc95e7df56
Milan Kuchtiak <mkuchtiak@netbeans.org>
2013-08-06 11:46:38 +0200

    #223718 sample rewritten to Backbone library

---
+++ websvc.rest.samples/CustomerDB/src/java/entities/service/MicroMarketFacadeREST.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities.service;
+
+import entities.MicroMarket;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+/**
+ *
+ * @author milan
+ */
+@Stateless
+@Path("entities.micromarket")
+public class MicroMarketFacadeREST extends AbstractFacade<MicroMarket> {
+    @PersistenceContext(unitName = "CustomerDBPU")
+    private EntityManager em;
+
+    public MicroMarketFacadeREST() {
+        super(MicroMarket.class);
+    }
+
+    @POST
+    @Override
+    @Consumes({"application/xml", "application/json"})
+    public void create(MicroMarket entity) {
+        super.create(entity);
+    }
+
+    @PUT
+    @Override
+    @Consumes({"application/xml", "application/json"})
+    public void edit(MicroMarket entity) {
+        super.edit(entity);
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void remove(@PathParam("id") String id) {
+        super.remove(super.find(id));
+    }
+
+    @GET
+    @Path("{id}")
+    @Produces({"application/xml", "application/json"})
+    public MicroMarket find(@PathParam("id") String id) {
+        return super.find(id);
+    }
+
+    @GET
+    @Override
+    @Produces({"application/xml", "application/json"})
+    public List<MicroMarket> findAll() {
+        return super.findAll();
+    }
+
+    @GET
+    @Path("{from}/{to}")
+    @Produces({"application/xml", "application/json"})
+    public List<MicroMarket> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
+        return super.findRange(new int[]{from, to});
+    }
+
+    @GET
+    @Path("count")
+    @Produces("text/plain")
+    public String countREST() {
+        return String.valueOf(super.count());
+    }
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+    
+}
