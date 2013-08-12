9e1d04a067b2a648ca3a99202536aba2046eaf4e
Jan Becicka <jbecicka@netbeans.org>
2013-08-12 21:03:05 +0200

    Improved build script to fail if ipa not created properly

--- cordova/src/org/netbeans/modules/cordova/CordovaPerformer.java
+++ cordova/src/org/netbeans/modules/cordova/CordovaPerformer.java
@@ -109,7 +109,7 @@ public class CordovaPerformer implements BuildPerformer {
     
     private final RequestProcessor RP = new RequestProcessor(CordovaPerformer.class.getName(), 10);
 
-    private final int BUILD_SCRIPT_VERSION = 33;
+    private final int BUILD_SCRIPT_VERSION = 34;
     
     public static CordovaPerformer getDefault() {
         return Lookup.getDefault().lookup(CordovaPerformer.class);
