18b1115e2dfaa97d88cae471d89954ad95f22dc4
Tomas Pavek <tpavek@netbeans.org>
2013-08-13 19:46:42 +0200

    backport of fix of #225537 - more

--- form/src/org/netbeans/modules/form/layoutsupport/griddesigner/GridDesigner.java
+++ form/src/org/netbeans/modules/form/layoutsupport/griddesigner/GridDesigner.java
@@ -56,6 +56,7 @@ import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
