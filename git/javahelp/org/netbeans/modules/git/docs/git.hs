7d067d3eb95c9c2ab9a054ba9c3a90e3efac10c2
kganfiel <kganfiel@netbeans.org>
2013-08-22 11:42:37 +0200

    remove index and search elements from hs file for javahelp

--- git/javahelp/org/netbeans/modules/git/docs/git.hs
+++ git/javahelp/org/netbeans/modules/git/docs/git.hs
@@ -12,16 +12,5 @@
         <type>javax.help.TOCView</type>
         <data>git-toc.xml</data>
     </view>
-    <view mergetype="javax.help.AppendMerge">
-        <name>Index</name>
-        <label>Index</label>
-        <type>javax.help.IndexView</type>
-        <data>git-idx.xml</data>
-    </view>
-    <view>
-        <name>Search</name>
-        <label>Search</label>
-        <type>javax.help.SearchView</type>
-        <data engine="com.sun.java.help.search.DefaultSearchEngine">JavaHelpSearch</data>
-    </view>
+    
 </helpset>
