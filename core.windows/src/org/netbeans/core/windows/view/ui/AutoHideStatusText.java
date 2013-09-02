37d553485e9280f5701182ee665b8f456c27c542
S. Aubrecht <saubrecht@netbeans.org>
2013-09-02 11:02:44 +0200

    #235264  - move the status line when the main window is resized

--- core.windows/src/org/netbeans/core/windows/view/ui/AutoHideStatusText.java
+++ core.windows/src/org/netbeans/core/windows/view/ui/AutoHideStatusText.java
@@ -45,6 +45,8 @@ import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Rectangle;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.BorderFactory;
@@ -92,6 +94,13 @@ final class AutoHideStatusText implements ChangeListener, Runnable {
                     text = null;
                     run();
                 }
+            }
+        });
+
+        frame.addComponentListener( new ComponentAdapter() {
+            @Override
+            public void componentResized( ComponentEvent e ) {
+                run();
             }
         });
     }
