6b18fb240c250f7ad58619a1a025411a0462128d
Jan Taus <jtau8042@netbeans.org>
2013-09-17 14:45:55 +0200

    support for limited module build needed by JDeveloper

--- nbbuild/newbuild/pack-all-components.sh
+++ nbbuild/newbuild/pack-all-components.sh
@@ -71,13 +71,16 @@ pack_all_components()
     cd $NB_ALL/nbbuild
 
     #Pack the distributions
-    ant zip-cluster-config -Dcluster.config=full -Dzip.name=$DIST_DIR/zip/$NAME.zip || exit 1
-
-    #ant zip-cluster-config -Dcluster.config=platform -Dzip.name=$DIST_DIR/zip/$NAME-platform.zip || exit 1
-    ant zip-cluster-config -Dcluster.config=basic -Dzip.name=$DIST_DIR/zip/$NAME-javase.zip || exit 1
-    ant zip-cluster-config -Dcluster.config=standard -Dzip.name=$DIST_DIR/zip/$NAME-javaee.zip || exit 1
-    ant zip-cluster-config -Dcluster.config=php -Dzip.name=$DIST_DIR/zip/$NAME-php.zip || exit 1
-    ant zip-cluster-config -Dcluster.config=cnd -Dzip.name=$DIST_DIR/zip/$NAME-cpp.zip || exit 1
+    if [ -z $JDEV_BUILD ]; then
+	ant zip-cluster-config -Dcluster.config=full -Dzip.name=$DIST_DIR/zip/$NAME.zip || exit 1
+        #ant zip-cluster-config -Dcluster.config=platform -Dzip.name=$DIST_DIR/zip/$NAME-platform.zip || exit 1
+	ant zip-cluster-config -Dcluster.config=basic -Dzip.name=$DIST_DIR/zip/$NAME-javase.zip || exit 1
+	ant zip-cluster-config -Dcluster.config=standard -Dzip.name=$DIST_DIR/zip/$NAME-javaee.zip || exit 1
+	ant zip-cluster-config -Dcluster.config=php -Dzip.name=$DIST_DIR/zip/$NAME-php.zip || exit 1
+	ant zip-cluster-config -Dcluster.config=cnd -Dzip.name=$DIST_DIR/zip/$NAME-cpp.zip || exit 1
+    else
+	ant zip-cluster-config -Dcluster.config=java -Dzip.name=$DIST_DIR/zip/$NAME.zip || exit 1
+    fi
 
     cd $NB_ALL/nbbuild/netbeans
 
