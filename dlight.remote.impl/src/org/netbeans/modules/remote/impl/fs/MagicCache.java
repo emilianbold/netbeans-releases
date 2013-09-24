dc618ba752545320c7e3fe48476e7bc47fdfe5be
Vladimir Kvashin <vkvashin@netbeans.org>
2013-09-24 17:20:20 +0400

    code formatting (transplanted from eb119ecb68d9a09fcc11e103f9122ec154203335)

--- dlight.remote.impl/src/org/netbeans/modules/remote/impl/fs/MagicCache.java
+++ dlight.remote.impl/src/org/netbeans/modules/remote/impl/fs/MagicCache.java
@@ -41,7 +41,6 @@
  */
 package org.netbeans.modules.remote.impl.fs;
 
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -50,15 +49,12 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.Callable;
 import org.netbeans.modules.nativeexecution.api.NativeProcessBuilder;
 import org.netbeans.modules.nativeexecution.api.util.ConnectionManager;
 import org.netbeans.modules.nativeexecution.api.util.ProcessUtils;
-import org.netbeans.modules.nativeexecution.api.util.ProcessUtils.ExitStatus;
 import org.netbeans.modules.nativeexecution.api.util.RemoteStatistics;
 import org.openide.filesystems.FileUtil;
 import org.openide.util.Exceptions;
