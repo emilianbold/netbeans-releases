673610b9f2f7b461d96cb4f7b569bfa7901e5819
Petr Hejl <phejl@netbeans.org>
2013-09-24 14:30:10 +0200

    #231992 - No documentation for Knockout functions displayed in JavaScript code completion

--- javascript2.editor/test/unit/src/org/netbeans/modules/javascript2/editor/model/impl/ModelTest.java
+++ javascript2.editor/test/unit/src/org/netbeans/modules/javascript2/editor/model/impl/ModelTest.java
@@ -203,7 +203,7 @@ public class ModelTest extends ModelTestBase {
         FileObject fo = getTestFile("testfiles/model/person.js.model");
         BufferedReader reader = new BufferedReader(new InputStreamReader(fo.getInputStream()));
         try {
-            Collection<JsObject> obj = Model.readModel(reader, null, null);
+            Collection<JsObject> obj = Model.readModel(reader, null, null, null);
             assertEquals(1, obj.size());
 
             final StringWriter sw = new StringWriter();
