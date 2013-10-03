0af9f8c7613d552860efbf1ff5c1ac033be57b75
Jan Peska <JPESKA@netbeans.org>
2013-10-03 14:40:08 +0200

    Add scheduling to the issue tooltip

--- jira/src/org/netbeans/modules/jira/issue/NbJiraIssue.java
+++ jira/src/org/netbeans/modules/jira/issue/NbJiraIssue.java
@@ -1162,13 +1162,24 @@ public class NbJiraIssue extends AbstractNbTaskWrapper {
             if (resolution != null && !resolution.trim().isEmpty()) {
                 status += "/" + resolution; //NOI18N
             }
+            String scheduledLabel = NbBundle.getMessage(NbJiraIssue.class, "CTL_Issue_Scheduled_Title"); //NOI18N
+            String scheduled = "---";
+
+            String dueLabel = NbBundle.getMessage(NbJiraIssue.class, "CTL_Issue_Due_Title"); //NOI18N
+            String due = "---";
+
+            String estimateLabel = NbBundle.getMessage(NbJiraIssue.class, "CTL_Issue_Estimate_Title_Short"); //NOI18N
+            String estimate = "---";
 
             String fieldTable = "<table>" //NOI18N
-                + "<tr><td><b>" + priorityLabel + ":</b></td><td><img src=\"" + priorityIcon + "\">&nbsp;" + priority + "</td><td style=\"padding-left:25px;\"><b>" + typeLabel + ":</b></td><td>" + type + "</td></tr>" //NOI18N
-                + "<tr><td><b>" + projectLabel + ":</b></td><td>" + project + "</td><td style=\"padding-left:25px;\"><b>" + componentLabel + ":</b></td><td>" + component + "</td></tr>" //NOI18N
-                + "<tr><td><b>" + assigneeLabel + ":</b></td><td colspan=\"3\">" + assignee + "</td></tr>" //NOI18N
-                + "<tr><td><b>" + statusLabel + ":</b></td><td colspan=\"3\">" + status + "</td></tr>" //NOI18N
-                + "</table>"; //NOI18N
+                    + "<tr><td><b>" + priorityLabel + ":</b></td><td><img src=\"" + priorityIcon + "\">&nbsp;" + priority + "</td><td style=\"padding-left:25px;\"><b>" + typeLabel + ":</b></td><td>" + type + "</td></tr>" //NOI18N
+                    + "<tr><td><b>" + projectLabel + ":</b></td><td>" + project + "</td><td style=\"padding-left:25px;\"><b>" + componentLabel + ":</b></td><td>" + component + "</td></tr>" //NOI18N
+                    + "<tr><td><b>" + assigneeLabel + ":</b></td><td colspan=\"3\">" + assignee + "</td></tr>" //NOI18N
+                    + "<tr><td><b>" + statusLabel + ":</b></td><td colspan=\"3\">" + status + "</td></tr>" //NOI18N
+                    + "<tr><td><b>" + scheduledLabel + ":</b></td><td colspan=\"3\">" + scheduled + "</td></tr>" //NOI18N
+                    + "<tr><td><b>" + dueLabel + ":</b></td><td>" + due + "</td>" //NOI18N
+                    + "<td style=\"padding-left:25px;\"><b>" + estimateLabel + ":</b></td><td>" + estimate + "</td></tr>" //NOI18N
+                    + "</table>"; //NOI18N
             sb.append("<hr>"); //NOI18N
             sb.append(fieldTable);
         }
