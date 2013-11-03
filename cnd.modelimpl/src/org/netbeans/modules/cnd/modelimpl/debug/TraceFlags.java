e907919e4d97777168427dde37925c7e209b39ad
Vladimir Voskresensky <vv159170@netbeans.org>
2013-11-02 20:23:24 -0400

    disabling cache to see other numbers with disabled cache (transplanted from b1e88c6c6f4429a0aad82a5ce6b7444f80aa2e98)

--- cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/debug/TraceFlags.java
+++ cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/debug/TraceFlags.java
@@ -90,7 +90,7 @@ public class TraceFlags {
     public static final boolean TRACE_CACHE = DebugUtils.getBoolean("cnd.trace.cache", false); // NOI18N
     public static final boolean USE_AST_CACHE = DebugUtils.getBoolean("cnd.use.ast.cache", false); // NOI18N
     public static final boolean CACHE_SKIP_APT_VISIT = DebugUtils.getBoolean("cnd.cache.skip.apt.visit", false); // NOI18N
-    public static final boolean CACHE_FILE_STATE = DebugUtils.getBoolean("cnd.cache.file.state", true); // NOI18N
+    public static final boolean CACHE_FILE_STATE = DebugUtils.getBoolean("cnd.cache.file.state", false); // NOI18N
     public static final boolean USE_WEAK_MEMORY_CACHE = DebugUtils.getBoolean("cnd.cache.key.object", true); // NOI18N
     public static final boolean APT_FILE_CACHE_ENTRY = DebugUtils.getBoolean("cnd.apt.cache.entry", true); //NOI18N
 
