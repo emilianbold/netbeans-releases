648046448e6b359328dfd990c494be7276a4b263
Alexander Simon <alexvsimon@netbeans.org>
2013-11-06 16:28:13 +0400

    fixing Bug #235601 Poor parsing scalability - cache include resolver results, switch cache on (transplanted from 0f03b8f9d7861b96f189dbed45fc508b8b06224e)

--- cnd.apt/src/org/netbeans/modules/cnd/apt/debug/APTTraceFlags.java
+++ cnd.apt/src/org/netbeans/modules/cnd/apt/debug/APTTraceFlags.java
@@ -67,7 +67,7 @@ public interface APTTraceFlags {
     public static final boolean TRACE_APT_CACHE = Boolean.getBoolean("cnd.apt.cache.hits"); // NOI18N
     public static final boolean USE_SOFT_APT_CACHE = DebugUtils.getBoolean("cnd.apt.cache.soft", true); // NOI18N
 
-    public static final boolean USE_INCLIDE_RESOLVER_CACHE = DebugUtils.getBoolean("cnd.apt.include.resolver.cache", false); // NOI18N
+    public static final boolean USE_INCLIDE_RESOLVER_CACHE = DebugUtils.getBoolean("cnd.apt.include.resolver.cache", true); // NOI18N
 
     public static final boolean USE_APT_TEST_TOKEN = Boolean.getBoolean("cnd.apt.apttoken"); // NOI18N
 
