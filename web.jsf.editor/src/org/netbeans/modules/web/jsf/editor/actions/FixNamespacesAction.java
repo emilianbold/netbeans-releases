9e595e23856dd354a5f538afd138d3aaed1502a4
Martin Fousek <marfous@netbeans.org>
2013-11-06 14:43:56 +0100

    Remove unused NS option stored in preferences

--- web.jsf.editor/src/org/netbeans/modules/web/jsf/editor/actions/FixNamespacesAction.java
+++ web.jsf.editor/src/org/netbeans/modules/web/jsf/editor/actions/FixNamespacesAction.java
@@ -48,12 +48,12 @@ import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.prefs.Preferences;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.SwingUtilities;
 import javax.swing.text.JTextComponent;
 import org.netbeans.api.editor.EditorActionRegistration;
-import org.netbeans.api.html.lexer.HTMLTokenId;
 import org.netbeans.api.progress.ProgressUtils;
 import org.netbeans.editor.BaseAction;
 import static org.netbeans.editor.BaseAction.MAGIC_POSITION_RESET;
@@ -72,6 +72,7 @@ import org.openide.DialogDisplayer;
 import org.openide.util.Exceptions;
 import org.openide.util.HelpCtx;
 import org.openide.util.NbBundle;
+import org.openide.util.NbPreferences;
 import org.openide.util.RequestProcessor;
 
 /**
@@ -92,6 +93,10 @@ public class FixNamespacesAction extends BaseAction {
 
     private static final long serialVersionUID = 1L;
     private static final RequestProcessor RP = new RequestProcessor(FixNamespacesAction.class);
+    private static final String PREFERENCES_NODE_KEY = FixNamespacesAction.class.getName();
+    private static final String KEY_REMOVE_UNUSED_NS = "remove.unused.namespaces"; //NOI18N
+    private static final boolean REMOVE_UNUSED_NS_DEFAULT = true;
+
     static final String ACTION_NAME = "fix-namespaces"; //NOI18N
 
     public FixNamespacesAction() {
@@ -106,11 +111,7 @@ public class FixNamespacesAction extends BaseAction {
             final UserTask task = new UserTask() {
                 @Override
                 public void run(ResultIterator ri) throws Exception {
-                    ResultIterator resultIterator = WebUtils.getResultIterator(ri, "text/html");
-                    if (resultIterator == null) {
-                        return;
-                    }
-                    Parser.Result parserResult = resultIterator.getParserResult();
+                    Parser.Result parserResult = getHtmlParserResult(ri);
                     if (parserResult instanceof HtmlParserResult) {
                         HtmlParserResult htmlParserResult = (HtmlParserResult) parserResult;
                         if (cancel.get()) {
@@ -126,7 +127,7 @@ public class FixNamespacesAction extends BaseAction {
                                 importData.set(data);
                             }
                         } else {
-                            performFixNamespaces(htmlParserResult, data, data.getDefaultVariants(), true);
+                            performFixNamespaces(htmlParserResult, data, data.getDefaultVariants(), isRemoveUnusedNs());
                         }
                     }
                 }
@@ -154,6 +155,15 @@ public class FixNamespacesAction extends BaseAction {
         }
     }
 
+    private static Parser.Result getHtmlParserResult(ResultIterator ri) throws ParseException {
+        ResultIterator resultIterator = WebUtils.getResultIterator(ri, "text/html");
+        if (resultIterator == null) {
+            return null;
+        } else {
+            return resultIterator.getParserResult();
+        }
+    }
+
     private static void performFixNamespaces(
             final HtmlParserResult parserResult,
             final ImportData importData,
@@ -167,13 +177,25 @@ public class FixNamespacesAction extends BaseAction {
         return namespaceProcessor.computeImportData();
     }
 
+    private static Preferences getPreferences() {
+        return NbPreferences.forModule(FixNamespacesAction.class).node(PREFERENCES_NODE_KEY);
+    }
+
+    private static boolean isRemoveUnusedNs() {
+        return getPreferences().getBoolean(KEY_REMOVE_UNUSED_NS, REMOVE_UNUSED_NS_DEFAULT);
+    }
+
+    private static void setRemoveUnusedNs(final boolean removeUnusedUses) {
+        getPreferences().putBoolean(KEY_REMOVE_UNUSED_NS, removeUnusedUses);
+    }
+
     @NbBundle.Messages({
         "LBL_Ok=Ok",
         "LBL_Cancel=Cancel"
     })
     private static void showFixNamespacesDialog(final JTextComponent target, final ImportData importData) {
         final FixDuplicateImportStmts panel = new FixDuplicateImportStmts();
-        panel.initPanel(importData, true);
+        panel.initPanel(importData, isRemoveUnusedNs());
         final JButton ok = new JButton(Bundle.LBL_Ok());
         final JButton cancel = new JButton(Bundle.LBL_Cancel());
         final AtomicBoolean stop = new AtomicBoolean();
@@ -192,7 +214,7 @@ public class FixNamespacesAction extends BaseAction {
                             ParserManager.parse(Collections.singleton(Source.create(target.getDocument())), new UserTask() {
                                 @Override
                                 public void run(ResultIterator resultIterator) throws Exception {
-                                    Parser.Result parserResult = resultIterator.getParserResult();
+                                    Parser.Result parserResult = getHtmlParserResult(resultIterator);
                                     if (parserResult instanceof HtmlParserResult) {
                                         SwingUtilities.invokeLater(new Runnable() {
                                             @Override
@@ -211,6 +233,8 @@ public class FixNamespacesAction extends BaseAction {
                         } catch (ParseException ex) {
                             Exceptions.printStackTrace(ex);
                         }
+
+                        setRemoveUnusedNs(removeUnusedNamespaces);
                         SwingUtilities.invokeLater(new Runnable() {
                             @Override
                             public void run() {
--- web.jsf.editor/src/org/netbeans/modules/web/jsf/editor/actions/NamespaceProcessor.java
+++ web.jsf.editor/src/org/netbeans/modules/web/jsf/editor/actions/NamespaceProcessor.java
@@ -44,7 +44,6 @@ package org.netbeans.modules.web.jsf.editor.actions;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
