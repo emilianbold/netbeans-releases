e35c2e0f99979024ad4e18bbfe3943362ec86909
Vladimir Kvashin <vkvashin@netbeans.org>
2013-11-19 19:38:40 +0400

    fixing unit test (transplanted from 603b8ed4b8175bdd5abab4026a559d80d9c31d27)

--- dlight.remote.impl/test/unit/src/org/netbeans/modules/remote/impl/fs/RefreshTestCase_IZ_210125.java
+++ dlight.remote.impl/test/unit/src/org/netbeans/modules/remote/impl/fs/RefreshTestCase_IZ_210125.java
@@ -85,7 +85,7 @@ public class RefreshTestCase_IZ_210125 extends RemoteFileTestBase {
             FileObject fileFO1 = FileUtil.createData(dirFO1, "file_1");
             String dirPath1 = dirFO1.getPath();
             String filePath1 = fileFO1.getPath();
-            RemoteFileSystemManager.getInstance().getFileSystem(execEnv).getRefreshManager().testWaitLastRefreshFinished();
+            //RemoteFileSystemManager.getInstance().getFileSystem(execEnv).getRefreshManager().testWaitLastRefreshFinished();
             RemoteFileSystemManager.getInstance().resetFileSystem(execEnv);
             sleep(100); // just in case
             ProcessUtils.ExitStatus res = ProcessUtils.execute(execEnv, "sh", "-c", "rm -rf " + dirPath1);
@@ -97,7 +97,7 @@ public class RefreshTestCase_IZ_210125 extends RemoteFileTestBase {
             assertNotNull("Null remote file system", fs);
             rootFO = fs.getRoot();
             ConnectionManager.getInstance().connectTo(execEnv);
-            RemoteFileSystemManager.getInstance().getFileSystem(execEnv).getRefreshManager().testWaitLastRefreshFinished();
+            //RemoteFileSystemManager.getInstance().getFileSystem(execEnv).getRefreshManager().testWaitLastRefreshFinished();
             baseFO = getFileObject(baseDir);
             dirFO1 = rootFO.getFileObject(dirPath1);
             fileFO1 = rootFO.getFileObject(filePath1);
@@ -105,7 +105,7 @@ public class RefreshTestCase_IZ_210125 extends RemoteFileTestBase {
                 // wait 4 sec, ask each half second
                 for (int i = 0; i < 8; i++) {
                     sleep(500);
-                    RemoteFileSystemManager.getInstance().getFileSystem(execEnv).getRefreshManager().testWaitLastRefreshFinished();
+                    //RemoteFileSystemManager.getInstance().getFileSystem(execEnv).getRefreshManager().testWaitLastRefreshFinished();
                     dirFO1 = rootFO.getFileObject(dirPath1);
                     fileFO1 = rootFO.getFileObject(filePath1);
                     if (dirFO1 == null && fileFO1 == null) {
