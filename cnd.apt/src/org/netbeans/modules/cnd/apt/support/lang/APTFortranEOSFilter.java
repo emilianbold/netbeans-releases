76888bef64b0bedb6e5e659beb654a95fdd232a0
Maria Dalmatova <mromashova@netbeans.org>
2013-11-20 12:27:24 +0400

    changeset:   275991:6b4be8c6d539 branch:      release74 user:        Alexander Simon <alexvsimon@netbeans.org> summary:     fix some findbugs (transplanted from e3828b6ff85a6d0b0dd42bed1f11f4ad99673d7b)

--- cnd.apt/src/org/netbeans/modules/cnd/apt/support/lang/APTFortranEOSFilter.java
+++ cnd.apt/src/org/netbeans/modules/cnd/apt/support/lang/APTFortranEOSFilter.java
@@ -68,7 +68,7 @@ final class APTFortranEOSFilter implements APTLanguageFilter {
         return new FilterStream(origStream);
     }
 
-    private final class FilterStream implements TokenStream {
+    private static final class FilterStream implements TokenStream {
         private TokenStream orig;
         private Token currentToken;
         boolean newLine = false;
