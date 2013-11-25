de875b15fd899ffe74929f80081232f68f611944
Tomas Mysik <tmysik@netbeans.org>
2013-11-25 09:23:54 +0100

    Coding style, threading

--- php.api.phpmodule/src/org/netbeans/modules/php/api/ui/options/FrameworksPanel.java
+++ php.api.phpmodule/src/org/netbeans/modules/php/api/ui/options/FrameworksPanel.java
@@ -42,6 +42,8 @@
 package org.netbeans.modules.php.api.ui.options;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.EventQueue;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.text.Collator;
@@ -50,37 +52,45 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Enumeration;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 import javax.swing.DefaultListModel;
+import javax.swing.GroupLayout;
 import javax.swing.JComponent;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.LayoutStyle;
+import javax.swing.ListCellRenderer;
+import javax.swing.ListSelectionModel;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+import org.netbeans.api.annotations.common.CheckForNull;
 import org.netbeans.modules.php.api.util.UiUtils;
 import org.netbeans.spi.options.AdvancedOption;
 import org.netbeans.spi.options.OptionsPanelController;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
 import org.openide.util.Lookup;
-import org.openide.util.NbBundle;
 
 /**
  * Visual panel for Frameworks and Tools options.
- * 
+ *
  * @author S. Aubrecht
  */
 @org.netbeans.api.annotations.common.SuppressWarnings("SE_BAD_FIELD_STORE")
-@OptionsPanelController.Keywords(keywords={"php"},
-        location=UiUtils.OPTIONS_PATH, tabTitle="#LBL_FrameworksTabTitle")
-final class FrameworksPanel extends javax.swing.JPanel {
+@OptionsPanelController.Keywords(
+        keywords = "php",
+        location = UiUtils.OPTIONS_PATH, tabTitle = "#LBL_FrameworksTabTitle")
+final class FrameworksPanel extends JPanel {
 
     private final Lookup masterLookup;
     private final FrameworksOptionsPanelController masterController;
-    private final ArrayList<AdvancedOption> options;
+    private final List<AdvancedOption> options;
     private final Map<AdvancedOption, OptionsPanelController> option2controller;
-    private final Map<AdvancedOption, JComponent> option2panel;
+    // @GuardedBy("EDT")
     private final Map<AdvancedOption, List<String>> option2keywords;
     private final PropertyChangeListener changeListener = new PropertyChangeListener() {
         @Override
@@ -89,25 +99,36 @@ final class FrameworksPanel extends javax.swing.JPanel {
         }
     };
 
-    FrameworksPanel(FrameworksOptionsPanelController masterController, Lookup masterLookup, 
-            ArrayList<AdvancedOption> options) {
+    volatile int selectedCategoryIndex = 0;
+
+
+    FrameworksPanel(FrameworksOptionsPanelController masterController, Lookup masterLookup, List<AdvancedOption> options) {
+        assert EventQueue.isDispatchThread();
         this.masterLookup = masterLookup;
         this.masterController = masterController;
+        sortOptions(options);
+        this.options = new CopyOnWriteArrayList<>(options);
+        option2controller = new ConcurrentHashMap<>(options.size() * 2);
+        option2keywords = new HashMap<>(options.size() * 2);
         initComponents();
-        this.options = new ArrayList<>(options);
+        init();
+    }
+
+    private void sortOptions(List<AdvancedOption> options) {
         final Collator collator = Collator.getInstance();
-        Collections.sort(this.options, new Comparator<AdvancedOption>() {
+        Collections.sort(options, new Comparator<AdvancedOption>() {
             @Override
             public int compare(AdvancedOption o1, AdvancedOption o2) {
                 return collator.compare(o1.getDisplayName(), o2.getDisplayName());
             }
         });
-        option2controller = new HashMap<>(options.size());
-        option2panel = new HashMap<>(options.size());
-        option2keywords = new HashMap<>(options.size());
-        DefaultListModel model = new DefaultListModel();
-        for( AdvancedOption ao : this.options ) {
-            model.addElement( ao.getDisplayName() );
+    }
+
+    private void init() {
+        assert EventQueue.isDispatchThread();
+        DefaultListModel<String> model = new DefaultListModel<>();
+        for (AdvancedOption ao : options) {
+            model.addElement(ao.getDisplayName());
         }
         listCategories.setModel(model);
         listCategories.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
@@ -116,8 +137,14 @@ final class FrameworksPanel extends javax.swing.JPanel {
                 switchPanel();
             }
         });
-        listCategories.setSelectedIndex(0);
-        listCategories.setVisibleRowCount(Math.min(model.getSize(), 20));
+        listCategories.setSelectedIndex(selectedCategoryIndex);
+        listCategories.setCellRenderer(new NameListCellRenderer(listCategories.getCellRenderer()));
+        listCategories.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                selectedCategoryIndex = listCategories.getSelectedIndex();
+            }
+        });
     }
 
     /**
@@ -127,173 +154,190 @@ final class FrameworksPanel extends javax.swing.JPanel {
      */
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
-        java.awt.GridBagConstraints gridBagConstraints;
 
-        lblCategories = new javax.swing.JLabel();
-        scrollCategories = new javax.swing.JScrollPane();
-        listCategories = new javax.swing.JList();
-        panelContent = new javax.swing.JPanel();
+        scrollCategories = new JScrollPane();
+        listCategories = new JList<String>();
+        panelContent = new JPanel();
 
-        setLayout(new java.awt.GridBagLayout());
-
-        org.openide.awt.Mnemonics.setLocalizedText(lblCategories, NbBundle.getBundle(FrameworksPanel.class).getString("FrameworksPanel.lblCategories.text")); // NOI18N
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 0;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        add(lblCategories, gridBagConstraints);
-
-        listCategories.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
+        listCategories.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         scrollCategories.setViewportView(listCategories);
 
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 1;
-        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        gridBagConstraints.insets = new java.awt.Insets(5, 0, 0, 0);
-        add(scrollCategories, gridBagConstraints);
+        panelContent.setLayout(new BorderLayout());
 
-        panelContent.setLayout(new java.awt.BorderLayout());
-        gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 1;
-        gridBagConstraints.gridy = 0;
-        gridBagConstraints.gridheight = 3;
-        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
-        gridBagConstraints.weightx = 1.0;
-        gridBagConstraints.weighty = 1.0;
-        gridBagConstraints.insets = new java.awt.Insets(0, 20, 0, 0);
-        add(panelContent, gridBagConstraints);
+        GroupLayout layout = new GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(scrollCategories, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(panelContent, GroupLayout.DEFAULT_SIZE, 147, Short.MAX_VALUE)
+                .addGap(0, 0, 0))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(0, 0, 0)
+                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                    .addComponent(scrollCategories, GroupLayout.DEFAULT_SIZE, 300, Short.MAX_VALUE)
+                    .addComponent(panelContent, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+        );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel lblCategories;
-    private javax.swing.JList listCategories;
-    private javax.swing.JPanel panelContent;
-    private javax.swing.JScrollPane scrollCategories;
+    private JList<String> listCategories;
+    private JPanel panelContent;
+    private JScrollPane scrollCategories;
     // End of variables declaration//GEN-END:variables
 
+    @CheckForNull
     OptionsPanelController getSelectedController() {
-        int selIndex = listCategories.getSelectedIndex();
-        if( selIndex < 0 )
-            return null;
         AdvancedOption option = getSelectedOption();
-        if( null == option )
+        if (option == null) {
             return null;
+        }
         return option2controller.get(option);
     }
-    
+
+    @CheckForNull
     private AdvancedOption getSelectedOption() {
-        int selIndex = listCategories.getSelectedIndex();
-        if( selIndex < 0 )
+        if (selectedCategoryIndex < 0) {
             return null;
-        return options.get(selIndex);
+        }
+        return options.get(selectedCategoryIndex);
     }
-    
-    
-    private void switchPanel() {
+
+    void switchPanel() {
+        assert EventQueue.isDispatchThread();
         panelContent.removeAll();
-        
+
         AdvancedOption selOption = getSelectedOption();
-        if( null != selOption ) {
+        if (selOption != null) {
             OptionsPanelController controller = getController(selOption);
             controller.update();
         }
         OptionsPanelController selection = getSelectedController();
-        if( null != selection ) {
+        if (selection != null) {
             panelContent.add(selection.getComponent(Lookup.EMPTY), BorderLayout.CENTER);
         }
         panelContent.invalidate();
         panelContent.revalidate();
         panelContent.repaint();
     }
-    
-    private OptionsPanelController getController( AdvancedOption option ) {
+
+    private OptionsPanelController getController(AdvancedOption option) {
+        assert EventQueue.isDispatchThread();
         OptionsPanelController controller = option2controller.get(option);
-        if( null == controller ) {
+        if (controller == null) {
             controller = option.create();
+            // comply with javadoc
+            controller.getComponent(masterLookup);
             option2controller.put(option, controller);
-            JComponent panel = controller.getComponent(masterLookup);
-            option2panel.put(option, panel);
             controller.addPropertyChangeListener(changeListener);
         }
         return controller;
     }
-    
-    void handleSearch( List<String> matchedKeywords ) {
-        for( AdvancedOption option : options ) {
+
+    void handleSearch(List<String> matchedKeywords) {
+        assert EventQueue.isDispatchThread();
+        for (AdvancedOption option : options) {
             List<String> keywords = option2keywords.get(option);
-            if( null == keywords ) {
-                keywords = loadKeywords( option );
+            if (keywords == null) {
+                keywords = loadKeywords(option);
                 option2keywords.put(option, keywords);
             }
-            for( String kw : matchedKeywords ) {
-                if( keywords.contains(kw) ) {
+            for (String kw : matchedKeywords) {
+                if (keywords.contains(kw)) {
                     setSelecteOption(option);
                     return;
                 }
             }
         }
     }
-    
-    private List<String> loadKeywords( AdvancedOption option ) {
+
+    private List<String> loadKeywords(AdvancedOption option) {
         OptionsPanelController controller = getController(option);
         JComponent panel = controller.getComponent(masterLookup);
-        String id = "OptionsDialog/Keywords/" + panel.getClass().getName(); //NOI18N
-        ArrayList<String> res = new ArrayList<>(20);
+        String id = "OptionsDialog/Keywords/" + panel.getClass().getName(); // NOI18N
+        List<String> res = new ArrayList<>(20);
         FileObject keywordsFO = FileUtil.getConfigFile(id);
-        if( null != keywordsFO ) {
+        if (keywordsFO != null) {
             Enumeration<String> attributes = keywordsFO.getAttributes();
-            while(attributes.hasMoreElements()) {
+            while (attributes.hasMoreElements()) {
                 String attribute = attributes.nextElement();
-                if(attribute.startsWith("keywords")) { //NOI18N
+                if (attribute.startsWith("keywords")) { // NOI18N
                     String word = keywordsFO.getAttribute(attribute).toString();
                     res.add(word.toUpperCase());
                 }
             }
         }
-        
         return res;
     }
 
     void update() {
-        for( OptionsPanelController controller : option2controller.values() ) {
+        for (OptionsPanelController controller : option2controller.values()) {
             controller.update();
         }
     }
 
     void applyChanges() {
-        for( OptionsPanelController controller : option2controller.values() ) {
+        for (OptionsPanelController controller : option2controller.values()) {
             controller.applyChanges();
         }
     }
 
     void cancel() {
-        for( OptionsPanelController controller : option2controller.values() ) {
+        for (OptionsPanelController controller : option2controller.values()) {
             controller.cancel();
         }
     }
 
     boolean isControllerValid() {
-        for( OptionsPanelController controller : option2controller.values() ) {
-            if( !controller.isValid() )
+        for (OptionsPanelController controller : option2controller.values()) {
+            if (!controller.isValid()) {
                 return false;
+            }
         }
         return true;
     }
 
     boolean isChanged() {
-        for( OptionsPanelController controller : option2controller.values() ) {
-            if( controller.isChanged())
+        for (OptionsPanelController controller : option2controller.values()) {
+            if (controller.isChanged()) {
                 return true;
+            }
         }
         return false;
     }
 
     void setSelecteOption(AdvancedOption option) {
         int index = options.indexOf(option);
-        if( index >= 0 )
+        if (index >= 0) {
             listCategories.setSelectedIndex(index);
+        }
     }
+
+    //~ Inner classes
+
+    private static final class NameListCellRenderer implements ListCellRenderer<String> {
+
+        private final ListCellRenderer<? super String> defaultCellRenderer;
+
+
+        NameListCellRenderer(ListCellRenderer<? super String> defaultCellRenderer) {
+            assert defaultCellRenderer != null;
+            this.defaultCellRenderer = defaultCellRenderer;
+        }
+
+        @Override
+        public Component getListCellRendererComponent(JList<? extends String> list, String value, int index, boolean isSelected, boolean cellHasFocus) {
+            assert EventQueue.isDispatchThread();
+            // is there a better way to simply add a padding?
+            value += "   "; // NOI18N
+            return defaultCellRenderer.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus);
+        }
+
+    }
+
 }
