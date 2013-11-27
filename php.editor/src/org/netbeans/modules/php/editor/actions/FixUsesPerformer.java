4828b47d256896e11a868d3f010b7fe8396dedf3
Ondrej Brejla <obrejla@netbeans.org>
2013-11-27 10:57:44 +0100

    #238828 - IndexOutOfBoundsException: Index: 5, Size: 4 (transplanted from be279a82dd3394ee3354d98ad07c02655e17144e)

--- php.editor/src/org/netbeans/modules/php/editor/actions/FixUsesPerformer.java
+++ php.editor/src/org/netbeans/modules/php/editor/actions/FixUsesPerformer.java
@@ -164,6 +164,9 @@ public class FixUsesPerformer {
         for (int i = 0; i < selections.size(); i++) {
             List<UsedNamespaceName> usedNamespaceNames = new ArrayList<>();
             ItemVariant baseVariant = selections.get(i);
+            if (!baseVariant.canBeUsed()) {
+                continue;
+            }
             for (int j = i + 1; j < selectionsCopy.size(); j++) {
                 ItemVariant testedVariant = selectionsCopy.get(j);
                 if (baseVariant.equals(testedVariant) && !itemsToRemove.contains(j)) {
