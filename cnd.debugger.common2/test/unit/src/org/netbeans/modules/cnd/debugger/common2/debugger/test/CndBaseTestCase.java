99dc67a43a851fbb5619bc6ce860e49b0938945b
Alexander Simon <alexvsimon@netbeans.org>
2013-11-28 12:26:15 +0400

    fix unit tests: minimize output (transplanted from cd9611ccb30752b60c49cdb2648dd043d5b184f4)

--- cnd.debugger.common2/test/unit/src/org/netbeans/modules/cnd/debugger/common2/debugger/test/CndBaseTestCase.java
+++ cnd.debugger.common2/test/unit/src/org/netbeans/modules/cnd/debugger/common2/debugger/test/CndBaseTestCase.java
@@ -89,6 +89,8 @@ import org.netbeans.junit.NbTestCase;
  */
 public abstract class CndBaseTestCase extends NbTestCase {
 
+    private Logger logger5;
+    private Logger logger4;
     static {
         // Setting netbeans.dirs makes installedFileLocator work properly
         File[] clusters = findClusters();
@@ -181,6 +183,11 @@ public abstract class CndBaseTestCase extends NbTestCase {
         Logger.getLogger("org.netbeans.modules.editor.settings.storage.Utils").setLevel(Level.SEVERE);
         System.setProperty("cnd.mode.unittest", "true");
         System.setProperty("SUNW_NO_UPDATE_NOTIFY", "true");
+        logger4 = Logger.getLogger("org.netbeans.modules.settings.RecognizeInstanceObjects");
+        logger4.setLevel(Level.SEVERE);
+        logger5 = Logger.getLogger("org.netbeans.ui.indexing");
+        logger5.setLevel(Level.SEVERE);
+        System.setProperty("RepositoryUpdate.increasedLogLevel", "SEVERE");
         List<Class> list = new ArrayList<Class>();
 //        list.add(MockMimeLookup.class);
         for(Class cls : getServises()){
