d170f9c2c8c81f561573b0a9260c5ab934eef30d
Petr Hejl <phejl@netbeans.org>
2013-11-29 15:49:35 +0100

    Fixing foreach as alias.

--- html.knockout/src/org/netbeans/modules/html/knockout/KOTemplateContext.java
+++ html.knockout/src/org/netbeans/modules/html/knockout/KOTemplateContext.java
@@ -61,111 +61,11 @@ import org.openide.util.Pair;
  */
 public class KOTemplateContext {
 
-    private static final String NAME_PROPERTY = "name"; // NOI18N
-
-    private static final String DATA_PROPERTY = "data"; // NOI18N
-
-    private static final String FOREACH_PROPERTY = "foreach"; // NOI18N
-
-    private static final String AS_PROPERTY = "as"; // NOI18N
-
     private final LinkedList<StackItem> scripts = new LinkedList<>();
 
     private boolean isScriptStart;
 
     private boolean isId;
-
-    public static TemplateDescriptor getTemplateDescriptor(Snapshot snapshot, TokenSequence<? extends JsTokenId> ts) {
-        if (ts == null) {
-            return null;
-        }
-
-        ts.moveStart();
-        ts.moveNext();
-        String name = null;
-        String alias = null;
-        String data = null;
-        boolean forEach = false;
-        Token<? extends JsTokenId> token = LexUtilities.findNextNonWsNonComment(ts);
-        if (token.id() == JsTokenId.BRACKET_LEFT_CURLY) {
-            while ((token = findNext(ts, JsTokenId.IDENTIFIER, false)) != null) {
-                String text = token.text().toString();
-                if ((NAME_PROPERTY.equals(text) || AS_PROPERTY.equals(text)) && ts.moveNext()) { // NOI18N
-                    token = LexUtilities.findNextNonWsNonComment(ts);
-                    if (token.id() == JsTokenId.OPERATOR_COLON && ts.moveNext()) {
-                        token = LexUtilities.findNextNonWsNonComment(ts);
-                        if (token.id() == JsTokenId.STRING_BEGIN && ts.moveNext()) {
-                            token = LexUtilities.findNextNonWsNonComment(ts);
-                            if (token.id() == JsTokenId.STRING) {
-                                if (NAME_PROPERTY.equals(text)) { // NOI18N
-                                    name = token.text().toString();
-                                } else {
-                                    alias = token.text().toString();
-                                }
-                            }
-                        }
-                    }
-                } else if ((DATA_PROPERTY.equals(text) || FOREACH_PROPERTY.equals(text)) && ts.moveNext()) { // NOI18N
-                    token = LexUtilities.findNextNonWsNonComment(ts);
-                    if (token.id() == JsTokenId.OPERATOR_COLON && ts.moveNext()) {
-                        LexUtilities.findNextNonWsNonComment(ts);
-                        int start = ts.offset();
-                        token = findNext(ts, JsTokenId.OPERATOR_COMMA, true);
-                        if (token != null) {
-                            data = snapshot.getText().subSequence(start, ts.offset()).toString().trim();
-                            forEach = FOREACH_PROPERTY.equals(text);
-                        }
-                    }
-                }
-                if (token == null || token.id() != JsTokenId.OPERATOR_COMMA) {
-                    findNext(ts, JsTokenId.OPERATOR_COMMA, false);
-                }
-            }
-            if (name != null && data != null) {
-                return new TemplateDescriptor(name, data, forEach, forEach ? alias : null);
-            }
-        }
-
-        return null;
-    }
-
-    private static Token<? extends JsTokenId> findNext(TokenSequence<? extends JsTokenId> ts, JsTokenId toFind, boolean findEnd) {
-        LinkedList<JsTokenId> stack = new LinkedList<>();
-        while (ts.moveNext()) {
-            Token<? extends JsTokenId> token = LexUtilities.findNextNonWsNonComment(ts);
-            JsTokenId id = token.id();
-            switch (id) {
-                case BRACKET_LEFT_BRACKET:
-                case BRACKET_LEFT_CURLY:
-                case BRACKET_LEFT_PAREN:
-                    stack.push(id);
-                    break;
-                case BRACKET_RIGHT_BRACKET:
-                    if (stack.isEmpty() || stack.pop() != JsTokenId.BRACKET_LEFT_BRACKET) {
-                        return null;
-                    }
-                    break;
-                case BRACKET_RIGHT_CURLY:
-                    if (stack.isEmpty() && findEnd) {
-                        return token;
-                    }
-                    if (stack.isEmpty() || stack.pop() != JsTokenId.BRACKET_LEFT_CURLY) {
-                        return null;
-                    }
-                    break;
-                case BRACKET_RIGHT_PAREN:
-                    if (stack.isEmpty() || stack.pop() != JsTokenId.BRACKET_LEFT_PAREN) {
-                        return null;
-                    }
-                    break;
-                default:
-                    if (toFind == id && stack.isEmpty()) {
-                        return token;
-                    }
-            }
-        }
-        return null;
-    }
 
     public Pair<Boolean, String> process(@NonNull Token<HTMLTokenId> token) {
         switch (token.id()) {
@@ -230,40 +130,6 @@ public class KOTemplateContext {
         scripts.clear();
         isScriptStart = false;
         isId = false;
-    }
-
-    public static class TemplateDescriptor {
-
-        private final String name;
-
-        private final String data;
-
-        private final boolean isForEach;
-
-        private final String alias;
-
-        public TemplateDescriptor(String name, String data, boolean isForEach, String alias) {
-            this.name = name;
-            this.data = data;
-            this.isForEach = isForEach;
-            this.alias = alias;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public String getData() {
-            return data;
-        }
-
-        public boolean isIsForEach() {
-            return isForEach;
-        }
-
-        public String getAlias() {
-            return alias;
-        }
     }
 
     public static class TemplateUsage {
