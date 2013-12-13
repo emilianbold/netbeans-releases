36caba3186dff48e43ddf25e990bdd5f8c2d66ff
Theofanis Oikonomou <theofanis@netbeans.org>
2013-12-13 13:16:22 +0100

    #239462 - ArrayIndexOutOfBoundsException: 1

--- gsf.testrunner/src/org/netbeans/modules/gsf/testrunner/api/TestsuiteNode.java
+++ gsf.testrunner/src/org/netbeans/modules/gsf/testrunner/api/TestsuiteNode.java
@@ -441,7 +441,7 @@ public class TestsuiteNode extends AbstractNode {
      * @return {@code true} if the currently used JDK version is 8, otherwise {@code false}.
      */
     private static boolean isJDK8() {
-        return Integer.parseInt(System.getProperty("java.version").split(".")[1]) == 8;
+        return Integer.parseInt(System.getProperty("java.version").split("\\.")[1]) == 8;
     }
 
 }
