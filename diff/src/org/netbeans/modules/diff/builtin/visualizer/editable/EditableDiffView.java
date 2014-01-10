bcfe874228e1fa9a839e4b9b7da282b524fb4183
Ondrej Vrabec <ovrabec@netbeans.org>
2014-01-10 11:21:40 +0100

    #240168 - OutOfMemoryError: Java heap space share string instances as long as possible until the computing finishes

--- diff/src/org/netbeans/modules/diff/builtin/visualizer/editable/EditableDiffView.java
+++ diff/src/org/netbeans/modules/diff/builtin/visualizer/editable/EditableDiffView.java
@@ -1157,6 +1157,7 @@ public class EditableDiffView extends DiffControllerImpl implements DiffView, Do
                                 open = DialogDisplayer.getDefault().notify(desc).equals(NotifyDescriptor.OK_OPTION);
                             }
                             if (open) {
+                                LOG.log(Level.INFO, "User acepted UQE: {0}", fo.getPath()); //NOI18N
                                 ex.confirmed();
                                 sdoc = ec.openDocument();
                             } else {
