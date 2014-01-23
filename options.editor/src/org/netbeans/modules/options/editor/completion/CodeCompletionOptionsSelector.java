74c6a678adebd852a77352a5f917bf62043a099d
Theofanis Oikonomou <theofanis@netbeans.org>
2014-01-23 16:27:51 +0100

    Task #240234 - Inconsistent activation/deactivation of Apply button from Editor->Code Completion->All Languages options panel

--- options.editor/src/org/netbeans/modules/options/editor/completion/CodeCompletionOptionsSelector.java
+++ options.editor/src/org/netbeans/modules/options/editor/completion/CodeCompletionOptionsSelector.java
@@ -104,6 +104,18 @@ public final class CodeCompletionOptionsSelector {
         pcs.removePropertyChangeListener(l);
     }
 
+    String getSavedValue(String mimeType, String key) {
+        PreferencesCustomizer prefsCustomizer = getCustomizer(mimeType);
+        if (prefsCustomizer != null) {
+            Lookup l = Lookups.forPath(CODE_COMPLETION_CUSTOMIZERS_FOLDER + mimeType);
+            PreferencesCustomizer.CustomCustomizer customizer = l.lookup(PreferencesCustomizer.CustomCustomizer.class);
+            if (customizer != null) {
+                return customizer.getSavedValue(prefsCustomizer, key);
+            }
+        }
+        return null;
+    }
+
     private PreferencesCustomizer getCustomizer(String mimeType) {
         PreferencesCustomizer customizer = allCustomizers.get(mimeType);
         if (customizer == null) {
