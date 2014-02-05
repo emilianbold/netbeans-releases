0851a8c61c67c5237da8583b8ea7ab761c59e154
mentlicher <mentlicher@netbeans.org>
2014-02-05 15:33:59 +0100

    #238902: Make the deactivated breakpoints to look like deactivated.

--- web.javascript.debugger/src/org/netbeans/modules/web/javascript/debugger/breakpoints/WebBreakpointsActiveService.java
+++ web.javascript.debugger/src/org/netbeans/modules/web/javascript/debugger/breakpoints/WebBreakpointsActiveService.java
@@ -46,14 +46,12 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 import java.net.URL;
-import org.netbeans.api.debugger.DebuggerEngine;
 import org.netbeans.api.debugger.DebuggerManager;
 import org.netbeans.api.debugger.DebuggerManagerAdapter;
-import org.netbeans.api.debugger.LazyDebuggerManagerListener;
+import org.netbeans.api.debugger.Session;
 import org.netbeans.modules.javascript2.debug.breakpoints.JSBreakpointsInfo;
 import org.netbeans.modules.web.webkit.debugging.api.Debugger;
 import org.netbeans.modules.web.webkit.debugging.api.dom.Node;
-import org.netbeans.spi.debugger.DebuggerServiceRegistration;
 import org.netbeans.spi.debugger.ui.EditorContextDispatcher;
 import org.openide.filesystems.FileObject;
 import org.openide.util.Utilities;
@@ -64,58 +62,26 @@ import org.openide.util.lookup.ServiceProvider;
  * @author Martin
  */
 @ServiceProvider(service = JSBreakpointsInfo.class)
-@DebuggerServiceRegistration(types=LazyDebuggerManagerListener.class)
-public class WebBreakpointsActiveService extends DebuggerManagerAdapter
-                                         implements JSBreakpointsInfo {
+public class WebBreakpointsActiveService implements JSBreakpointsInfo {
 
-    private boolean active = true;
+    private volatile boolean active = true;
     private final PropertyChangeSupport pcs = new PropertyChangeSupport(this);
     
-    @Override
-    public String[] getProperties() {
-        return new String[] { DebuggerManager.PROP_DEBUGGER_ENGINES }; 
-    }
-
-    @Override
-    public void engineAdded(DebuggerEngine engine) {
-        Debugger debugger = engine.lookupFirst(null, Debugger.class);
-        if (debugger != null) {
-            debugger.addPropertyChangeListener(this);
-            boolean a = debugger.areBreakpointsActive();
-            if (a != active) {
-                active = a;
-                fireChange();
-            }
-        }
-    }
-
-    @Override
-    public void engineRemoved(DebuggerEngine engine) {
-        Debugger debugger = engine.lookupFirst(null, Debugger.class);
-        if (debugger != null) {
-            debugger.removePropertyChangeListener(this);
-            if (true != active) {
-                active = true;
-                fireChange();
-            }
-        }
+    public WebBreakpointsActiveService() {
+        SessionActiveListener sal = new SessionActiveListener();
+        DebuggerManager.getDebuggerManager().addDebuggerListener(DebuggerManager.PROP_CURRENT_SESSION, sal);
     }
     
-    @Override
-    public void propertyChange(PropertyChangeEvent evt) {
-        String propertyName = evt.getPropertyName();
-        if (Debugger.PROP_BREAKPOINTS_ACTIVE.equals(propertyName)) {
-            boolean a = (Boolean) evt.getNewValue();
-            if (a != active) {
-                active = a;
-                fireChange();
-            }
-        }
-    }
-
     @Override
     public boolean areBreakpointsActivated() {
         return active;
+    }
+    
+    private void setActive(boolean active) {
+        if (this.active != active) {
+            this.active = active;
+            pcs.firePropertyChange(PROP_BREAKPOINTS_ACTIVE, !active, active);
+        }
     }
     
     @Override
@@ -153,8 +119,51 @@ public class WebBreakpointsActiveService extends DebuggerManagerAdapter
         pcs.removePropertyChangeListener(l);
     }
     
-    private void fireChange() {
-        pcs.firePropertyChange(PROP_BREAKPOINTS_ACTIVE, null, active);
-    }
+    private class SessionActiveListener extends DebuggerManagerAdapter {
+        
+        private Debugger currentDebugger;
+        
+        public SessionActiveListener() {
+            currentDebugger = getCurrentDebugger();
+            if (currentDebugger != null) {
+                active = currentDebugger.areBreakpointsActive();
+            }
+        }
 
+        @Override
+        public void propertyChange(PropertyChangeEvent evt) {
+            String propertyName = evt.getPropertyName();
+            if (DebuggerManager.PROP_CURRENT_SESSION.equals(propertyName)) {
+                Debugger newDebugger = getCurrentDebugger();
+                synchronized (this) {
+                    if (currentDebugger != null) {
+                        currentDebugger.removePropertyChangeListener(this);
+                    }
+                    currentDebugger = newDebugger;
+                }
+                if (newDebugger != null) {
+                    setActive(newDebugger.areBreakpointsActive());
+                } else {
+                    setActive(true);
+                }
+            }
+            if (Debugger.PROP_BREAKPOINTS_ACTIVE.equals(propertyName)) {
+                setActive(((Debugger) evt.getSource()).areBreakpointsActive());
+            }
+        }
+        
+        private Debugger getCurrentDebugger() {
+            Session s = DebuggerManager.getDebuggerManager().getCurrentSession();
+            if (s != null) {
+                Debugger debugger = s.lookupFirst(null, Debugger.class);
+                if (debugger != null) {
+                    debugger.addPropertyChangeListener(this);
+                }
+                return debugger;
+            } else {
+                return null;
+            }
+        }
+
+    }
 }
