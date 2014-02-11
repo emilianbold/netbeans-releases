addca17827053019e9ba94980203a4c164de3da6
Martin Fousek <marfous@netbeans.org>
2014-02-11 15:10:10 +0100

    Java7 syntax in TplIndenter

--- php.smarty/src/org/netbeans/modules/php/smarty/editor/indent/TplIndenter.java
+++ php.smarty/src/org/netbeans/modules/php/smarty/editor/indent/TplIndenter.java
@@ -106,7 +106,7 @@ public class TplIndenter extends AbstractIndenter<TplTopTokenId> {
 
     @Override
     protected void reset() {
-        stack = new Stack<TplStackItem>();
+        stack = new Stack<>();
         preservedLineIndentation = -1;
     }
 
@@ -194,7 +194,7 @@ public class TplIndenter extends AbstractIndenter<TplTopTokenId> {
     protected List<IndentCommand> getLineIndent(IndenterContextData<TplTopTokenId> context,
             List<IndentCommand> preliminaryNextLineIndent) throws BadLocationException {
         Stack<TplStackItem> blockStack = getStack();
-        List<IndentCommand> iis = new ArrayList<IndentCommand>();
+        List<IndentCommand> iis = new ArrayList<>();
         getIndentFromState(iis, true, context.getLineStartOffset());
 
         JoinedTokenSequence<TplTopTokenId> ts = context.getJoinedTokenSequences();
