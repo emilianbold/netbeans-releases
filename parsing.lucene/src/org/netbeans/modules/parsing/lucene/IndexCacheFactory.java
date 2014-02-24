16ba3d2db2b1f6c5445c7c0e26d32b439f846e92
Tomas Zezula <tzezula@netbeans.org>
2014-02-24 16:43:23 +0100

    #239570:OutOfMemoryError: Java heap space

--- parsing.lucene/src/org/netbeans/modules/parsing/lucene/IndexCacheFactory.java
+++ parsing.lucene/src/org/netbeans/modules/parsing/lucene/IndexCacheFactory.java
@@ -42,8 +42,11 @@
 
 package org.netbeans.modules.parsing.lucene;
 
-import java.net.URL;
+import java.net.URI;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
+import org.netbeans.api.annotations.common.NonNull;
+import org.openide.util.Parameters;
 import org.openide.util.Utilities;
 
 /**
@@ -53,22 +56,78 @@ import org.openide.util.Utilities;
 class IndexCacheFactory {
     
     private static final Logger LOG = Logger.getLogger(IndexCacheFactory.class.getName());
+    private static final String PROP_CACHE_SIZE = "java.index.size";    //NOI18N
     private static final IndexCacheFactory instance = new IndexCacheFactory();
-    private final LRUCache<URL, Evictable> cache;
+    private final RAMContoller ramController;
+    private final LRUCache<URI, Evictable> nioCache;
+    private final LRUCache<URI, Evictable> ramCache;
 
     private IndexCacheFactory() {
-        this.cache = new LRUCache<URL, Evictable>(new DefaultPolicy());
+        this.ramController = new RAMContoller();
+        this.nioCache = new LRUCache<>(new NIOPolicy());
+        this.ramCache = new LRUCache<>(new RAMPolicy(ramController));
     }
 
-    public LRUCache<URL,Evictable> getCache() {
-        return cache;
+    @NonNull
+    LRUCache<URI,Evictable> getNIOCache() {
+        return nioCache;
+    }
+
+    @NonNull
+    LRUCache<URI,Evictable> getRAMCache() {
+        return ramCache;
+    }
+
+    @NonNull
+    RAMContoller getRAMController() {
+        return ramController;
     }
 
     public static IndexCacheFactory getDefault() {
         return instance;
     }
 
-    private static class DefaultPolicy implements EvictionPolicy<URL,Evictable> {
+    static final class RAMContoller {
+        private static final float DEFAULT_CACHE_SIZE = 0.05f;
+        private static final long maxCacheSize = getCacheSize();
+        private final AtomicLong currentCacheSize = new AtomicLong();
+
+
+        public long acquire(final long bytes) {
+            return currentCacheSize.addAndGet(bytes);
+        }
+
+        public long release(final long bytes) {
+            return currentCacheSize.addAndGet(~bytes + 1);
+        }
+
+        public boolean isFull() {
+            return currentCacheSize.get() > maxCacheSize;
+        }
+
+        public boolean shouldLoad(final long bytes) {
+            //Todo: Preffer NMAP to RAM
+            return bytes < maxCacheSize;
+        }
+
+        private static long getCacheSize() {
+            float per = -1.0f;
+            final String propVal = System.getProperty(PROP_CACHE_SIZE);
+            if (propVal != null) {
+                try {
+                    per = Float.parseFloat(propVal);
+                } catch (NumberFormatException nfe) {
+                    //Handled below
+                }
+            }
+            if (per<0) {
+                per = DEFAULT_CACHE_SIZE;
+            }
+            return (long) (per * Runtime.getRuntime().maxMemory());
+        }
+    }
+
+    private static final class NIOPolicy implements EvictionPolicy<URI,Evictable> {
         private static final int DEFAULT_SIZE = 400;
         private static final boolean NEEDS_REMOVE =  Boolean.getBoolean("IndexCache.force") || (Utilities.isUnix() && !Utilities.isMac());  //NOI18N
         private static final int MAX_SIZE;
@@ -86,9 +145,27 @@ class IndexCacheFactory {
             LOG.fine("NEEDS_REMOVE: " + NEEDS_REMOVE +" MAX_SIZE: " + MAX_SIZE);    //NOI18N
         }
 
-        public boolean shouldEvict(int size, URL key, Evictable value) {
+        @Override
+        public boolean shouldEvict(int size, URI key, Evictable value) {
             return NEEDS_REMOVE && size>MAX_SIZE;
         }
     }
 
+    private static final class RAMPolicy implements EvictionPolicy<URI, Evictable> {
+
+        private final RAMContoller controller;
+
+        RAMPolicy(@NonNull final RAMContoller controller) {
+            Parameters.notNull("controller", controller);   //NOI18N
+            this.controller = controller;
+        }
+
+        @Override
+        public boolean shouldEvict(int size, URI key, Evictable value) {
+            return controller.isFull();
+        }
+
+
+        
+    }
 }
