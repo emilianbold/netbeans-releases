06ece2601655866bf9ae707c18154ef0dd28d201
Petr Kudryavtsev <petrk@netbeans.org>
2014-03-13 18:36:37 +0400

    Fixed #242861 - Wrong offsets of friend declaration

--- cnd.modelimpl/test/unit/src/org/netbeans/modules/cnd/modelimpl/trace/FileModelTest3.java
+++ cnd.modelimpl/test/unit/src/org/netbeans/modules/cnd/modelimpl/trace/FileModelTest3.java
@@ -81,6 +81,10 @@ public class FileModelTest3 extends TraceModelTestBase {
     public void testBug242674() throws Exception {
         performTest("bug242674.cpp"); // NOI18N
     }    
+    
+    public void testBug242861() throws Exception {
+        performTest("bug242861.cpp");
+    }
 
     @Override
     protected Class<?> getTestCaseDataClass() {
