586d97de44b4666527b611effeab56efb28bc05a
Petr Kudryavtsev <petrk@netbeans.org>
2014-04-21 12:54:22 +0400

    Fixed #243940 - inaccuracy tests (cpp11): forward declaration of enumerations

---
+++ cnd.modelimpl/test/unit/data/org/netbeans/modules/cnd/modelimpl/trace/FileModelCpp11Test/bug243940.cpp
@@ -0,0 +1,11 @@
+namespace bug243940 {
+    enum class E1_243940;
+    enum class E2_243940 : int;
+    enum class E3_243940 : short;
+    enum E4_243940 : int;
+    enum E5_243940 : short;
+    enum E6;
+    enum {
+        XXX_243940
+    };
+}
