844c70154523936517b76fa7cce994bdce7d156f
Petr Kudryavtsev <petrk@netbeans.org>
2014-04-22 15:31:16 +0400

    Additional fix for #243940 - inaccuracy tests (cpp11): forward declaration of enumerations -- fixed enum qualified id started with :: token

--- cnd.modelimpl/test/unit/data/org/netbeans/modules/cnd/modelimpl/trace/FileModelCpp11Test/bug243940.cpp
+++ cnd.modelimpl/test/unit/data/org/netbeans/modules/cnd/modelimpl/trace/FileModelCpp11Test/bug243940.cpp
@@ -4,8 +4,13 @@ namespace bug243940 {
     enum class E3_243940 : short;
     enum E4_243940 : int;
     enum E5_243940 : short;
-    enum E6;
+    namespace N243940 {
+        enum class E6_243940;
+    }    
     enum {
         XXX_243940
     };
+    enum N243940::E6_243940 e6_1_243940; //ok
+    enum ::bug243940::N243940::E6_243940 e6_2_243940; //ok    
 }
+
