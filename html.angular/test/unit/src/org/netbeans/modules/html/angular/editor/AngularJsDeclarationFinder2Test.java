0270b9f3ce90f97c558d096cbb77a11fa7f94051
Petr Pisl <ppisl@netbeans.org>
2014-04-24 15:02:21 +0200

    Find bugs and removing unused imports

--- html.angular/test/unit/src/org/netbeans/modules/html/angular/editor/AngularJsDeclarationFinder2Test.java
+++ html.angular/test/unit/src/org/netbeans/modules/html/angular/editor/AngularJsDeclarationFinder2Test.java
@@ -129,7 +129,7 @@ public class AngularJsDeclarationFinder2Test extends JsCodeCompletionBase {
     public void testIssue243888_08() throws Exception {
         checkDeclaration("angularTestProject/public_html/issue243888.html", "<span ng-bind=\"printAt^tempt\"></span>", "controllers243888.js", 788);
     }
-
+    
     @Override
     protected void checkDeclaration(String relFilePath, String caretLine, String file, int offset) throws Exception {
          
