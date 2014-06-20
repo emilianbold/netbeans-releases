5ba653b1b43c672891e6bb4d939d34417c34d6dd
Vladimir Riha <vriha@netbeans.org>
2014-06-20 09:25:09 +0200

    tests for go to declaration to external file

--- javascript2.requirejs/test/qa-functional/data/SimpleRequire/public_html/js/app/mytest.js
+++ javascript2.requirejs/test/qa-functional/data/SimpleRequire/public_html/js/app/mytest.js
@@ -3,5 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
+define(["external", "app/newFunction"], function(external, nf){
+    
+});
 
