c0082698e70d3b48cb1cd0ff1bd69735aeac40fd
Ondrej Vrabec <ovrabec@netbeans.org>
2014-06-26 08:49:36 +0200

    Fix commit time - a test's failing. Commit's timestamp should be acquired from the authorship of the commit so it complies with CLI e.g. after a rebase.

--- libs.git/src/org/netbeans/libs/git/GitRevisionInfo.java
+++ libs.git/src/org/netbeans/libs/git/GitRevisionInfo.java
@@ -131,11 +131,13 @@ public final class GitRevisionInfo {
      * @return time this commit was created in milliseconds.
      */
     public long getCommitTime () {
-        PersonIdent committer = revCommit.getCommitterIdent();
-        if (committer == null) {
+        // must be indeed author, that complies with CLI
+        // committer time is different after rebase
+        PersonIdent author = revCommit.getAuthorIdent();
+        if (author == null) {
             return (long) revCommit.getCommitTime() * 1000;
         } else {
-            return committer.getWhen().getTime();
+            return author.getWhen().getTime();
         }
     }
 
