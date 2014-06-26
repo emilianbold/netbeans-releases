830184b305189877e358b7eca93ff16dee8ce6a1
Ondrej Brejla <obrejla@netbeans.org>
2014-06-26 11:08:14 +0200

    Hints.

--- php.editor/test/unit/src/org/netbeans/modules/php/editor/parser/ASTPHP5ParserTest.java
+++ php.editor/test/unit/src/org/netbeans/modules/php/editor/parser/ASTPHP5ParserTest.java
@@ -511,7 +511,7 @@ public class ASTPHP5ParserTest extends ParserTestBase {
     @Override
     protected String getTestResult(String filename) throws Exception {
         File testFile = new File(getDataDir(), "testfiles/" + filename + ".php");
-        StringBuffer result = new StringBuffer();
+        StringBuilder result = new StringBuilder();
         String content = PHPLexerUtils.getFileContent(testFile);
         ASTPHP5Scanner scanner = new ASTPHP5Scanner(new StringReader(content));
 
@@ -521,7 +521,7 @@ public class ASTPHP5ParserTest extends ParserTestBase {
         do {
             symbol = scanner.next_token();
             result.append("        <token id='").append(Utils.getASTScannerTokenName(symbol.sym)).append("' start='");
-            result.append(symbol.left).append("' end='").append(symbol.right + "'>\n");
+            result.append(symbol.left).append("' end='").append(symbol.right).append("'>\n");
             result.append("            <text>");
             result.append(PHPLexerUtils.getXmlStringValue(content.substring(symbol.left, symbol.right)));
             result.append("</text>\n");
