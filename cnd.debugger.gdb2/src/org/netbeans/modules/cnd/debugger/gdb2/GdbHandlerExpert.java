bdf5775d1bc7eb5299b124ef2360d3cd45ac10c3
Nikolay Koldunov <henk89@netbeans.org>
2014-07-01 15:53:02 +0400

    Fixed #244006 - Line breakpoints does not work inside c:/cygwin64 folder

--- cnd.debugger.gdb2/src/org/netbeans/modules/cnd/debugger/gdb2/GdbHandlerExpert.java
+++ cnd.debugger.gdb2/src/org/netbeans/modules/cnd/debugger/gdb2/GdbHandlerExpert.java
@@ -239,8 +239,6 @@ public class GdbHandlerExpert implements HandlerExpert {
 
 	    String fileLine;
 	    if (file != null && file.length() > 0) {
-                // IZs 169200 & 174479 (send internal path for cygwin)
-                file = debugger.fmap().worldToEngine(file);
 		fileLine = file + ":" + line;	// NOI18N
 	    } else {
 		fileLine = "" + line;		// NOI18N
