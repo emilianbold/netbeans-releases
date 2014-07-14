b62209331510763b5b84c7bbc6d179e08b80d17c
Maria Dalmatova <mromashova@netbeans.org>
2014-07-14 19:32:26 +0400

    CallGraphAction which allows to update UI after non EDT action is invoked

--- cnd.callgraph/src/org/netbeans/modules/cnd/callgraph/api/ui/CallGraphAction.java
+++ cnd.callgraph/src/org/netbeans/modules/cnd/callgraph/api/ui/CallGraphAction.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright 2010 Oracle and/or its affiliates. All rights reserved.
+ * Copyright 2014 Oracle and/or its affiliates. All rights reserved.
  *
  * Oracle and Java are registered trademarks of Oracle and/or its affiliates.
  * Other names may be trademarks of their respective owners.
@@ -23,7 +23,7 @@
  * License Header, with the fields enclosed by brackets [] replaced by
  * your own identifying information:
  * "Portions Copyrighted [year] [name of copyright owner]"
- * 
+ *
  * If you wish your version of this file to be governed by only the CDDL
  * or only the GPL Version 2, indicate your decision by adding
  * "[Contributor] elects to include this software in this distribution
@@ -34,33 +34,46 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
- * 
+ *
  * Contributor(s):
- * 
- * Portions Copyrighted 2008 Sun Microsystems, Inc.
+ *
+ * Portions Copyrighted 2014 Sun Microsystems, Inc.
  */
 
 package org.netbeans.modules.cnd.callgraph.api.ui;
 
-import java.util.Collection;
-import javax.swing.JPanel;
-import org.netbeans.modules.cnd.callgraph.api.Call;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+import javax.swing.SwingUtilities;
+import org.openide.util.RequestProcessor;
 
 /**
  *
- * @author Alexander Simon
+ * @author mtishkov
  */
-public interface CallGraphUI {
-    /**
-     * if method returns true the call graph shows tree and graph views.
-     * if method returns false the call graph shows only tree view.
-     * @return 
-     */
-    boolean showGraph();
+abstract public class CallGraphAction extends AbstractAction{
+    private final RequestProcessor RP = new RequestProcessor("CallGraphAction", 1);
+    private final CallGraphActionEDTRunnable runnable;
     
-    Catalog getCatalog();
     
-    JPanel getContextPanel(Call call);    
+    public CallGraphAction (CallGraphActionEDTRunnable runnable){
+        this.runnable = runnable;
+    }
     
-    Collection<CallGraphAction> getActions(CallGraphActionEDTRunnable runnable);
+    abstract public void doNonEDTAction();
+
+    @Override
+    public final void actionPerformed(ActionEvent e) {
+        RP.post(new Runnable() {
+
+            @Override
+            public void run() {
+                doNonEDTAction();
+                SwingUtilities.invokeLater(runnable);
+            }
+            
+        });
+    }
+    
+    
 }
