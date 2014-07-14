956e3c1d6b8e95df9e734605d4e9d7e455eed496
Tomas Mysik <tmysik@netbeans.org>
2014-07-14 11:08:57 +0200

    #245633 - Unify karma configuration errors reporting

--- javascript.karma/src/org/netbeans/modules/javascript/karma/exec/KarmaServer.java
+++ javascript.karma/src/org/netbeans/modules/javascript/karma/exec/KarmaServer.java
@@ -63,10 +63,10 @@ import org.netbeans.modules.javascript.karma.coverage.CoverageProcessor;
 import org.netbeans.modules.javascript.karma.coverage.CoverageWatcher;
 import org.netbeans.modules.javascript.karma.preferences.KarmaPreferences;
 import org.netbeans.modules.javascript.karma.run.KarmaRunInfo;
+import org.netbeans.modules.javascript.karma.ui.KarmaErrorsDialog;
 import org.netbeans.modules.web.browser.api.BrowserSupport;
 import org.netbeans.modules.web.browser.api.BrowserUISupport;
 import org.netbeans.modules.web.clientproject.api.jstesting.Coverage;
-import org.openide.awt.StatusDisplayer;
 import org.openide.filesystems.FileChangeAdapter;
 import org.openide.filesystems.FileChangeListener;
 import org.openide.filesystems.FileEvent;
@@ -137,7 +137,7 @@ public final class KarmaServer implements PropertyChangeListener {
         if (server != null) {
             started = true;
         } else {
-            StatusDisplayer.getDefault().setStatusText(Bundle.KarmaServer_start_error());
+            KarmaErrorsDialog.getInstance().show(Bundle.KarmaServer_start_error());
         }
         addCoverageListener();
         addConfigFileListener();
