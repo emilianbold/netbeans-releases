aa4554cdb569c771c8ecdd06ca081446aaea1f3d
Petr Hejl <phejl@netbeans.org>
2014-08-27 16:01:20 +0200

    Disable remote debugging for now.

--- j2ee.weblogic9/src/org/netbeans/modules/j2ee/weblogic9/optional/WLStartServer.java
+++ j2ee.weblogic9/src/org/netbeans/modules/j2ee/weblogic9/optional/WLStartServer.java
@@ -112,7 +112,7 @@ public final class WLStartServer extends StartServer {
         if (!isRunning()) {
             return false;
         }
-        return true;
+        return !dm.isRemote();
     }
 
     @Override
