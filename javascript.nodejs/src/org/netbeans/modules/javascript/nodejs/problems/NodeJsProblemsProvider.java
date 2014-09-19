5c20ab8acd870ed2c74400ad210d065e73a363ca
Tomas Mysik <tmysik@netbeans.org>
2014-09-19 12:39:46 +0200

    Add project problems for node.js

---
+++ javascript.nodejs/src/org/netbeans/modules/javascript/nodejs/problems/NodeJsProblemsProvider.java
@@ -0,0 +1,213 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright 2014 Oracle and/or its affiliates. All rights reserved.
+ *
+ * Oracle and Java are registered trademarks of Oracle and/or its affiliates.
+ * Other names may be trademarks of their respective owners.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ *
+ * Contributor(s):
+ *
+ * Portions Copyrighted 2014 Sun Microsystems, Inc.
+ */
+package org.netbeans.modules.javascript.nodejs.problems;
+
+import java.awt.EventQueue;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import org.netbeans.api.project.Project;
+import org.netbeans.modules.javascript.nodejs.exec.NodeExecutable;
+import org.netbeans.modules.javascript.nodejs.options.NodeJsOptionsValidator;
+import org.netbeans.modules.javascript.nodejs.platform.NodeJsSupport;
+import org.netbeans.modules.javascript.nodejs.preferences.NodeJsPreferencesValidator;
+import org.netbeans.modules.javascript.nodejs.util.FileUtils;
+import org.netbeans.modules.javascript.nodejs.util.ValidationResult;
+import org.netbeans.modules.web.common.api.Version;
+import org.netbeans.spi.project.ProjectServiceProvider;
+import org.netbeans.spi.project.ui.ProjectProblemResolver;
+import org.netbeans.spi.project.ui.ProjectProblemsProvider;
+import org.netbeans.spi.project.ui.support.ProjectProblemsProviderSupport;
+import org.openide.util.NbBundle;
+
+@ProjectServiceProvider(service = ProjectProblemsProvider.class, projectType = "org-netbeans-modules-web-clientproject") // NOI18N
+public final class NodeJsProblemsProvider implements ProjectProblemsProvider {
+
+    final ProjectProblemsProviderSupport problemsProviderSupport = new ProjectProblemsProviderSupport(this);
+    final Project project;
+    private final PropertyChangeListener nodeJsSupportListener = new NodeJsSupportListener();
+
+    // @GuardedBy("this")
+    private NodeJsSupport nodeJsSupport;
+
+
+    public NodeJsProblemsProvider(Project project) {
+        assert project != null;
+        this.project = project;
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        problemsProviderSupport.addPropertyChangeListener(listener);
+    }
+
+    @Override
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        problemsProviderSupport.removePropertyChangeListener(listener);
+    }
+
+    @NbBundle.Messages({
+        "# {0} - message",
+        "NodeJsProblemProvider.error=Node.js: {0}",
+    })
+    @Override
+    public Collection<? extends ProjectProblem> getProblems() {
+        return problemsProviderSupport.getProblems(new ProjectProblemsProviderSupport.ProblemsCollector() {
+            @Override
+            public Collection<? extends ProjectProblem> collectProblems() {
+                if (!getNodeJsSupport().getPreferences().isEnabled()) {
+                    return Collections.emptyList();
+                }
+                Collection<ProjectProblemsProvider.ProjectProblem> currentProblems = new ArrayList<>();
+                checkOptions(currentProblems);
+                checkPreferences(currentProblems);
+                checkNode(currentProblems);
+                return currentProblems;
+            }
+        });
+    }
+
+    synchronized NodeJsSupport getNodeJsSupport() {
+        if (nodeJsSupport == null) {
+            nodeJsSupport = NodeJsSupport.forProject(project);
+            nodeJsSupport.addPropertyChangeListener(nodeJsSupportListener);
+        }
+        return nodeJsSupport;
+    }
+
+    void checkOptions(Collection<ProjectProblem> currentProblems) {
+        ValidationResult validationResult = new NodeJsOptionsValidator()
+                .validate()
+                .getResult();
+        if (validationResult.isFaultless()) {
+            return;
+        }
+        String message = validationResult.getFirstErrorMessage();
+        if (message == null) {
+            message = validationResult.getFirstWarningMessage();
+        }
+        assert message != null : "Message should be found for invalid options";
+        message = Bundle.NodeJsProblemProvider_error(message);
+        ProjectProblem problem = ProjectProblem.createError(
+                message,
+                message,
+                new OptionsProblemResolver());
+        currentProblems.add(problem);
+    }
+
+    void checkPreferences(Collection<ProjectProblem> currentProblems) {
+        ValidationResult validationResult = new NodeJsPreferencesValidator()
+                .validate(project)
+                .getResult();
+        if (validationResult.isFaultless()) {
+            return;
+        }
+        String message = validationResult.getFirstErrorMessage();
+        if (message == null) {
+            message = validationResult.getFirstWarningMessage();
+        }
+        assert message != null : "Message should be found for invalid preferences: " + project.getProjectDirectory().getNameExt();
+        message = Bundle.NodeJsProblemProvider_error(message);
+        ProjectProblem problem = ProjectProblem.createError(
+                message,
+                message,
+                new CustomizerProblemResolver(project));
+        currentProblems.add(problem);
+    }
+
+    @NbBundle.Messages({
+        "NodeJsProblemProvider.node.version=Unknown version",
+        "# {0} - version",
+        "NodeJsProblemProvider.node.sources=Missing sources for version {0}",
+    })
+    void checkNode(Collection<ProjectProblem> currentProblems) {
+        NodeExecutable node = NodeExecutable.forProject(project, false);
+        if (node == null) {
+            // already handled
+            return;
+        }
+        if (EventQueue.isDispatchThread()
+                && !node.hasVersion()) {
+            // avoid ui flickering
+            return;
+        }
+        Version version = node.getVersion();
+        if (version == null) {
+            String message = Bundle.NodeJsProblemProvider_error(Bundle.NodeJsProblemProvider_node_version());
+            ProjectProblem problem = ProjectProblem.createError(
+                    message,
+                    message,
+                    getNodeResolver());
+            currentProblems.add(problem);
+            return;
+        }
+        if (!FileUtils.hasNodeSources(version)) {
+            String message = Bundle.NodeJsProblemProvider_error(Bundle.NodeJsProblemProvider_node_sources(version.toString()));
+            ProjectProblem problem = ProjectProblem.createError(
+                    message,
+                    message,
+                    getNodeResolver());
+            currentProblems.add(problem);
+        }
+    }
+
+    private ProjectProblemResolver getNodeResolver() {
+        if (getNodeJsSupport().getPreferences().isDefaultNode()) {
+            return new OptionsProblemResolver();
+        }
+        return new CustomizerProblemResolver(project);
+    }
+
+    //~ Inner classes
+
+    private final class NodeJsSupportListener implements PropertyChangeListener {
+
+        @Override
+        public void propertyChange(PropertyChangeEvent evt) {
+            problemsProviderSupport.fireProblemsChange();
+        }
+
+    }
+
+}
