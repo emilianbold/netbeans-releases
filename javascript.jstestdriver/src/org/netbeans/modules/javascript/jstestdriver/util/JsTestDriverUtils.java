2ca45f05c5486705d2568148adb6c4439e4997f7
Theofanis Oikonomou <theofanis@netbeans.org>
2014-10-10 15:21:19 +0200

    Task #219022 - failed JS unit test should be a node with clickable stacktrace

--- javascript.jstestdriver/src/org/netbeans/modules/javascript/jstestdriver/util/JsTestDriverUtils.java
+++ javascript.jstestdriver/src/org/netbeans/modules/javascript/jstestdriver/util/JsTestDriverUtils.java
@@ -43,12 +43,23 @@
 package org.netbeans.modules.javascript.jstestdriver.util;
 
 import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.netbeans.api.annotations.common.CheckForNull;
 import org.netbeans.api.project.Project;
 import org.netbeans.modules.javascript.jstestdriver.preferences.JsTestDriverPreferences;
+import org.openide.cookies.EditorCookie;
+import org.openide.cookies.LineCookie;
+import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileUtil;
+import org.openide.loaders.DataObject;
+import org.openide.loaders.DataObjectNotFoundException;
+import org.openide.text.Line;
+import org.openide.util.Mutex;
 
 public final class JsTestDriverUtils {
+
+    static final Logger LOGGER = Logger.getLogger(JsTestDriverUtils.class.getName());
 
     private JsTestDriverUtils() {
     }
@@ -74,6 +85,57 @@ public final class JsTestDriverUtils {
             return config;
         }
         return null;
+    }
+
+    /**
+     * Opens the file and optionally set cursor to the line. This action is always run in AWT thread.
+     * @param file path of a file to open
+     * @param line line of a file to set cursor to, {@code -1} if no specific line is needed
+     * @param column column within a line of a file to set cursor to, {@code -1} if no specific column is needed
+     */
+    public static void openFile(File file, int line, int column) {
+        assert file != null;
+
+        FileObject fileObject = FileUtil.toFileObject(FileUtil.normalizeFile(file));
+        if (fileObject == null) {
+            LOGGER.log(Level.INFO, "FileObject not found for {0}", file);
+            return;
+        }
+
+        DataObject dataObject;
+        try {
+            dataObject = DataObject.find(fileObject);
+        } catch (DataObjectNotFoundException ex) {
+            LOGGER.log(Level.INFO, "DataObject not found for {0}", file);
+            return;
+        }
+
+        if (line == -1) {
+            // simply open file
+            EditorCookie ec = dataObject.getLookup().lookup(EditorCookie.class);
+            ec.open();
+            return;
+        }
+
+        // open at specific line
+        LineCookie lineCookie = dataObject.getLookup().lookup(LineCookie.class);
+        if (lineCookie == null) {
+            LOGGER.log(Level.INFO, "LineCookie not found for {0}", file);
+            return;
+        }
+        Line.Set lineSet = lineCookie.getLineSet();
+        try {
+            final Line currentLine = lineSet.getCurrent(line - 1);
+            final int col = column == -1 ? -1 : column - 1;
+            Mutex.EVENT.readAccess(new Runnable() {
+                @Override
+                public void run() {
+                    currentLine.show(Line.ShowOpenType.OPEN, Line.ShowVisibilityType.FOCUS, col);
+                }
+            });
+        } catch (IndexOutOfBoundsException exc) {
+            LOGGER.log(Level.FINE, null, exc);
+        }
     }
 
 }
