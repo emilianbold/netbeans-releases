bcdcc3e2f12b6ca6c1a9b4f7bf868051598f5692
Tomas Mysik <tmysik@netbeans.org>
2014-10-21 17:31:52 +0200

    Sync project name change to package.json

--- javascript.nodejs/src/org/netbeans/modules/javascript/nodejs/platform/NodeJsPlatformProvider.java
+++ javascript.nodejs/src/org/netbeans/modules/javascript/nodejs/platform/NodeJsPlatformProvider.java
@@ -46,14 +46,17 @@ import java.beans.PropertyChangeListener;
 import java.net.URL;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.json.simple.JSONObject;
 import org.netbeans.api.annotations.common.StaticResource;
 import org.netbeans.api.project.Project;
 import org.netbeans.modules.javascript.nodejs.file.PackageJson;
 import org.netbeans.modules.javascript.nodejs.preferences.NodeJsPreferences;
 import org.netbeans.modules.javascript.nodejs.ui.Notifications;
 import org.netbeans.modules.javascript.nodejs.ui.customizer.NodeJsRunPanel;
+import org.netbeans.modules.javascript.nodejs.util.StringUtils;
 import org.netbeans.modules.web.clientproject.api.BadgeIcon;
 import org.netbeans.modules.web.clientproject.api.platform.PlatformProviders;
 import org.netbeans.modules.web.clientproject.spi.CustomizerPanelImplementation;
@@ -63,6 +66,7 @@ import org.netbeans.spi.project.ActionProvider;
 import org.openide.util.ImageUtilities;
 import org.openide.util.NbBundle;
 import org.openide.util.lookup.ServiceProvider;
+
 
 @ServiceProvider(service = PlatformProviderImplementation.class, path = PlatformProviders.PLATFORM_PATH, position = 100)
 public final class NodeJsPlatformProvider implements PlatformProviderImplementation, PropertyChangeListener {
@@ -146,16 +150,10 @@ public final class NodeJsPlatformProvider implements PlatformProviderImplementat
         String propertyName = event.getPropertyName();
         if (PROP_ENABLED.equals(propertyName)) {
             NodeJsSupport.forProject(project).getPreferences().setEnabled((boolean) event.getNewValue());
+        } else if (PROP_PROJECT_NAME.equals(propertyName)) {
+            projectNameChanged(project, (String) event.getNewValue());
         } else if (PROP_RUN_CONFIGURATION.equals(propertyName)) {
-            Object activeRunConfig = event.getNewValue();
-            boolean runEnabled = false;
-            for (CustomizerPanelImplementation panel : getRunCustomizerPanels(project)) {
-                if (panel.getIdentifier().equals(activeRunConfig)) {
-                    runEnabled = true;
-                    break;
-                }
-            }
-            NodeJsSupport.forProject(project).getPreferences().setRunEnabled(runEnabled);
+            runConfigurationChanged(project, event.getNewValue());
         }
     }
 
@@ -186,17 +184,59 @@ public final class NodeJsPlatformProvider implements PlatformProviderImplementat
         if (!packageJson.exists()) {
             return;
         }
-        JSONObject content = packageJson.getContent();
+        Map<String, Object> content = packageJson.getContent();
         if (content == null) {
             // some error
             return;
         }
         Object engines = content.get("engines"); // NOI18N
-        if (engines instanceof JSONObject) {
-            if (((JSONObject) engines).containsKey("node")) { // NOI18N
+        if (engines instanceof Map) {
+            if (((Map<String, Object>) engines).containsKey("node")) { // NOI18N
                 Notifications.notifyNodeJsDetected(project);
             }
         }
+    }
+
+    private void projectNameChanged(Project project, String newName) {
+        String projectName = project.getProjectDirectory().getNameExt();
+        NodeJsSupport nodeJsSupport = NodeJsSupport.forProject(project);
+        if (!nodeJsSupport.getPreferences().isEnabled()) {
+            LOGGER.log(Level.FINE, "Project name change ignored, node.js not enabled in project {0}", projectName);
+            return;
+        }
+        PackageJson packageJson = nodeJsSupport.getPackageJson();
+        if (!packageJson.exists()) {
+            LOGGER.log(Level.FINE, "Project name change ignored, package.json not exist in project {0}", projectName);
+            return;
+        }
+        LOGGER.log(Level.FINE, "Processing project name change in project {0}", projectName);
+        Map<String, Object> content = packageJson.getContent();
+        if (content == null) {
+            LOGGER.log(Level.FINE, "Project name change ignored, package.json has no content in project {0}", projectName);
+            return;
+        }
+        if (!StringUtils.hasText(newName)) {
+            LOGGER.log(Level.FINE, "Project name change ignored, new name is empty in project {0}", projectName);
+            return;
+        }
+        String name = (String) content.get(PackageJson.NAME);
+        if (Objects.equals(name, newName)) {
+            LOGGER.log(Level.FINE, "Project name change ignored, new name same as current name in package.json in project {0}", projectName);
+            return;
+        }
+        content.put(PackageJson.NAME, newName);
+        packageJson.setContent(content);
+    }
+
+    private void runConfigurationChanged(Project project, Object activeRunConfig) {
+        boolean runEnabled = false;
+        for (CustomizerPanelImplementation panel : getRunCustomizerPanels(project)) {
+            if (panel.getIdentifier().equals(activeRunConfig)) {
+                runEnabled = true;
+                break;
+            }
+        }
+        NodeJsSupport.forProject(project).getPreferences().setRunEnabled(runEnabled);
     }
 
 }
