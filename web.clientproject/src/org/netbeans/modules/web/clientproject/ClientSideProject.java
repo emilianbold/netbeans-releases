47c47b039bd179d898acffc470243aa0f088ec15
Tomas Mysik <tmysik@netbeans.org>
2014-10-31 11:34:38 +0100

    #247949 - Find in files does not find an occurrence in build file

--- web.clientproject/src/org/netbeans/modules/web/clientproject/ClientSideProject.java
+++ web.clientproject/src/org/netbeans/modules/web/clientproject/ClientSideProject.java
@@ -888,14 +888,21 @@ public class ClientSideProject implements Project {
         }
 
         private FileObject[] getRoots() {
-            List<FileObject> roots = new ArrayList<FileObject>();
-            addRoots(roots, project.getSiteRootFolder(), project.getTestsFolder(false));
+            List<FileObject> roots = new ArrayList<>();
+            FileObject projectDir = project.getProjectDirectory();
+            roots.add(projectDir);
+            addRoots(roots, projectDir, project.getSourcesFolder(), project.getSiteRootFolder(), project.getTestsFolder(false));
             return roots.toArray(new FileObject[roots.size()]);
         }
 
-        private void addRoots(List<FileObject> result, FileObject... roots) {
+        /**
+         * Add extra roots, skip the ones that are underneath the project
+         * directory (already included in search).
+         */
+        private void addRoots(List<FileObject> result, FileObject projectDir, FileObject... roots) {
             for (FileObject root : roots) {
-                if (root != null) {
+                if (root != null
+                        && !FileUtil.isParentOf(projectDir, root)) {
                     result.add(root);
                 }
             }
