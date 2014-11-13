c432ea88a535224ee66caf02fc750630adcfda66
Libor Fischmeistr <lfischmeistr@netbeans.org>
2014-11-13 10:43:12 +0100

    #247763: NullPointerException in NbProxySelector constructor

--- core.network/src/org/netbeans/core/network/proxy/NbProxySelector.java
+++ core.network/src/org/netbeans/core/network/proxy/NbProxySelector.java
@@ -76,7 +76,7 @@ public final class NbProxySelector extends ProxySelector {
     public NbProxySelector() {
         original = ProxySelector.getDefault();
         LOG.log(Level.FINE, "java.net.useSystemProxies has been set to {0}", useSystemProxies());
-        if (original.getClass().getName().equals(DEFAULT_PROXY_SELECTOR_CLASS_NAME) || original == null) {
+        if (original == null || original.getClass().getName().equals(DEFAULT_PROXY_SELECTOR_CLASS_NAME)) {
             NetworkProxyReloader.reloadNetworkProxy();
         }
         ProxySettings.addPreferenceChangeListener(new ProxySettingsListener());
