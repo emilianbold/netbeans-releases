c58f8270ffd373b5bd6c57174e01dfb0e5357c78
Petr Pisl <ppisl@netbeans.org>
2015-02-03 15:51:08 +0100

    Marking callback functions. Adding special icon, filtering out from the code completion and storing the functions into the index

--- javascript2.editor/src/org/netbeans/modules/javascript2/editor/model/impl/ModelVisitor.java
+++ javascript2.editor/src/org/netbeans/modules/javascript2/editor/model/impl/ModelVisitor.java
@@ -833,6 +833,7 @@ public class ModelVisitor extends PathNodeVisitor {
                                 JsTokenId.JAVASCRIPT_MIME_TYPE, null);
                         parentObject.addProperty(callBackFunction.getName(), callBackFunction);
                         callBackFunction.setDocumentation(Documentation.create(comment.getDocumentation()));
+                        callBackFunction.setJsKind(JsElement.Kind.CALLBACK);
                         List<DocParameter> docParameters = comment.getParameters();
                         for (DocParameter docParameter: docParameters) {
                             ParameterObject parameter = new ParameterObject(callBackFunction, docParameter.getParamName(), JsTokenId.JAVASCRIPT_MIME_TYPE, null);
