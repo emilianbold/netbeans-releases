cc8a067e0ba3892b7c7cc3c0afe587d7f7c12df3
Petr Pisl <ppisl@netbeans.org>
2015-02-03 14:38:33 +0100

    Provide documentation for the callback function

--- javascript2.editor/src/org/netbeans/modules/javascript2/editor/model/impl/ModelVisitor.java
+++ javascript2.editor/src/org/netbeans/modules/javascript2/editor/model/impl/ModelVisitor.java
@@ -832,6 +832,7 @@ public class ModelVisitor extends PathNodeVisitor {
                                 new OffsetRange(callBack.getOffset(), callBack.getOffset() + callBack.getType().length()),
                                 JsTokenId.JAVASCRIPT_MIME_TYPE, null);
                         parentObject.addProperty(callBackFunction.getName(), callBackFunction);
+                        callBackFunction.setDocumentation(Documentation.create(comment.getDocumentation()));
                         List<DocParameter> docParameters = comment.getParameters();
                         for (DocParameter docParameter: docParameters) {
                             ParameterObject parameter = new ParameterObject(callBackFunction, docParameter.getParamName(), JsTokenId.JAVASCRIPT_MIME_TYPE, null);
