0a1e36fdcc722673030c1ac2789b194863f64f90
Petr Pisl <ppisl@netbeans.org>
2015-02-20 10:19:42 +0100

    #250547 - Braces directly after css selector marked as error

--- javascript2.jade/test/unit/src/org/netbeans/modules/javascript2/jade/editor/lexer/JadeLexerTest.java
+++ javascript2.jade/test/unit/src/org/netbeans/modules/javascript2/jade/editor/lexer/JadeLexerTest.java
@@ -268,6 +268,10 @@ public class JadeLexerTest extends CslTestBase {
         checkLexer("testfiles/lexer/issue250531.jade");
     }
     
+    public void testIssue250547() throws Exception {
+        checkLexer("testfiles/lexer/issue250547.jade");
+    }
+    
     public void testIssue250567_01() throws Exception {
         checkLexer("testfiles/lexer/issue250567_01.jade");
     }
