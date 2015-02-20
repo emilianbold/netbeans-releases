1fff54cdd15a98fa1fdecbfb798adbb457f77485
Vladimir Riha <vriha@netbeans.org>
2015-02-20 16:18:46 +0100

    fixed tearDown for tests in suites

--- javascript2.nodejs/test/qa-functional/src/org/netbeans/modules/javascript2/nodejs/cc/ExportsModuleTest.java
+++ javascript2.nodejs/test/qa-functional/src/org/netbeans/modules/javascript2/nodejs/cc/ExportsModuleTest.java
@@ -95,6 +95,7 @@ public class ExportsModuleTest extends GeneralNodeJs {
 
     public void openProject() throws Exception {
         startTest();
+        GeneralNodeJs.currentLine = 0;
         JemmyProperties.setCurrentTimeout("ActionProducer.MaxActionTime", 180000);
         openDataProjects("SimpleNode");
         evt.waitNoEvent(2000);
--- javascript2.nodejs/test/qa-functional/src/org/netbeans/modules/javascript2/nodejs/cc/MEPropertyTest.java
+++ javascript2.nodejs/test/qa-functional/src/org/netbeans/modules/javascript2/nodejs/cc/MEPropertyTest.java
@@ -75,6 +75,7 @@ public class MEPropertyTest extends GeneralNodeJs {
 
     public void openProject() throws Exception {
         startTest();
+        GeneralNodeJs.currentLine = 0;
         JemmyProperties.setCurrentTimeout("ActionProducer.MaxActionTime", 180000);
         openDataProjects("SimpleNode");
         evt.waitNoEvent(2000);
