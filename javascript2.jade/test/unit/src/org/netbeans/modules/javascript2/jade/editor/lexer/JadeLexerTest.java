1b0d7de6e97dc508b48de4fe921931cd9e787bd5
Petr Pisl <ppisl@netbeans.org>
2015-02-20 11:44:51 +0100

    #250539 - HTML element not highlighted

--- javascript2.jade/test/unit/src/org/netbeans/modules/javascript2/jade/editor/lexer/JadeLexerTest.java
+++ javascript2.jade/test/unit/src/org/netbeans/modules/javascript2/jade/editor/lexer/JadeLexerTest.java
@@ -296,6 +296,10 @@ public class JadeLexerTest extends CslTestBase {
         checkLexer("testfiles/lexer/issue250543.jade");
     }
     
+    public void testIssue250539() throws Exception {
+        checkLexer("testfiles/lexer/issue250539.jade");
+    }
+    
     private void checkLexer(final String filePath) throws Exception {
         Source testSource = getTestSource(getTestFile(filePath));
         Snapshot snapshot = testSource.createSnapshot();
