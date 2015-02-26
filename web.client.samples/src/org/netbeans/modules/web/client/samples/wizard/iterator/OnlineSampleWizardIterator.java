3fb55d0d0ca2ef7dd1e09afc7c2416b17025a389
Tomas Mysik <tmysik@netbeans.org>
2015-02-26 14:05:06 +0100

    #250725 - NullPointerException at org.netbeans.modules.masterfs.filebasedfs.fileobjects.FolderObj.getFileObject

--- web.client.samples/src/org/netbeans/modules/web/client/samples/wizard/iterator/OnlineSampleWizardIterator.java
+++ web.client.samples/src/org/netbeans/modules/web/client/samples/wizard/iterator/OnlineSampleWizardIterator.java
@@ -122,7 +122,8 @@ public abstract class OnlineSampleWizardIterator extends AbstractWizardIterator 
         }
         final FileObject projectDirFO = FileUtil.toFileObject(projectDir);
 
-        CreateProjectProperties props = new CreateProjectProperties(projectDirFO, name);
+        CreateProjectProperties props = new CreateProjectProperties(projectDirFO, name)
+                .setStartFile("index.html"); // NOI18N
 
         OnlineSiteTemplate siteTemplate = getSiteTemplate();
         if (siteTemplate != null) {
@@ -141,9 +142,12 @@ public abstract class OnlineSampleWizardIterator extends AbstractWizardIterator 
 
         FileObject siteRoot = project.getProjectDirectory().getFileObject(props.getSiteRootFolder());
         assert siteRoot != null;
-        FileObject startFile = siteRoot.getFileObject(props.getStartFile());
+        String startFile = props.getStartFile();
         if (startFile != null) {
-            files.add(startFile);
+            FileObject startFileFo = siteRoot.getFileObject(startFile);
+            if (startFileFo != null) {
+                files.add(startFileFo);
+            }
         }
 
         File parent = projectDir.getParentFile();
