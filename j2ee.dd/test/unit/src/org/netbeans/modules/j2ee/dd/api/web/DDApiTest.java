cfb03edd40a9fbd5d7ad475e2f606f3a68ae119d
Petr Hejl <phejl@netbeans.org>
2015-02-26 11:50:42 +0100

    Fixing failing tests.

--- j2ee.dd/test/unit/src/org/netbeans/modules/j2ee/dd/api/web/DDApiTest.java
+++ j2ee.dd/test/unit/src/org/netbeans/modules/j2ee/dd/api/web/DDApiTest.java
@@ -137,6 +137,9 @@ public class DDApiTest extends NbTestCase {
     }
     
     public void test_InitParams() {
+        // unfortunately the test depends on actions made by other tests
+        test_Servlet();
+
         System.out.println("Testing init-params, context-params");
         Servlet s = (Servlet)webApp.findBeanByName("Servlet","ServletName",SERVLET_NAME);
         assertTrue("Servlet "+SERVLET_NAME+" not found", null != s);
@@ -181,6 +184,9 @@ public class DDApiTest extends NbTestCase {
     }
     
     public void test_Description() {
+        // unfortunately the test depends on actions made by other tests
+        test_InitParams();
+
         System.out.println("Testing description, description for locales");
         Servlet s = (Servlet)webApp.findBeanByName("Servlet","ServletName",SERVLET_NAME);
         assertTrue("Servlet "+SERVLET_NAME+" not found", null != s);
@@ -266,6 +272,10 @@ public class DDApiTest extends NbTestCase {
     }
     
     public void test_Result() {
+        // unfortunately the test depends on actions made by other tests
+        test_Description();
+        test_addBean();
+
         System.out.println("Comparing result with golden file");
         
         String testDataDirS = System.getProperty("test.data.dir");
@@ -306,7 +316,7 @@ public class DDApiTest extends NbTestCase {
     private static boolean initialized;
     
     @Override
-        protected void setUp() throws Exception {
+    protected void setUp() throws Exception {
         super.setUp();
         System.out.println("setUp() .......................");
         
@@ -338,4 +348,15 @@ public class DDApiTest extends NbTestCase {
         assertTrue("WebApp object not found", null != webApp);
         
     }
+
+    @Override
+    protected void tearDown() throws Exception {
+        if (initialized && fo != null) {
+            fo.delete();
+            fo = null;
+            webApp = null;
+        }
+        super.tearDown();
+    }
+
 }
