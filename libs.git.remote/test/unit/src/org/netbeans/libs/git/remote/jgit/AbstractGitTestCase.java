d0eb97b8e0533f0cd460c069a40f8aefd43f44bd
Alexander Simon <alexvsimon@netbeans.org>
2015-02-27 16:43:24 +0300

    fixing Bug #249744 Support Git VCS for full remote project - fix empty user name in unit tests org.netbeans.libs.git.remote.GitException: #git --no-pager commit --status -m commit --
    *** Please tell me who you are.
    
    Run
    
      git config --global user.email "you@example.com"
      git config --global user.name "Your Name"
    
    to set your account's default identity.
    Omit --global to set the identity only in this repository.
    
    fatal: empty ident  <hudson@enum.(none)> not allowed

--- libs.git.remote/test/unit/src/org/netbeans/libs/git/remote/jgit/AbstractGitTestCase.java
+++ libs.git.remote/test/unit/src/org/netbeans/libs/git/remote/jgit/AbstractGitTestCase.java
@@ -188,6 +188,16 @@ public class AbstractGitTestCase extends NbTestCase {
     private void initializeRepository() throws Exception {
         runExternally(repositoryLocation, Arrays.asList("init"));
         runExternally(wc.getParentFile(), Arrays.asList("clone", repositoryLocation.getPath(), wc.getName()));
+
+        List<String> res = runExternally(wc.getParentFile(), Arrays.asList("config", "--global", "--get", "user.name"));
+        if (res.size() == 0 || res.get(0).isEmpty()) {
+            runExternally(wc.getParentFile(), Arrays.asList("config", "--global", "user.name", "Your Name"));
+        }
+        res = runExternally(wc.getParentFile(), Arrays.asList("config", "--global", "--get", "user.email"));
+        if (res.size() == 0 || res.get(0).isEmpty()) {
+            runExternally(wc.getParentFile(), Arrays.asList("config", "--global", "user.email", "you@example.com"));
+        }
+        
         localRepository = new JGitRepository(wc);
     }
 
