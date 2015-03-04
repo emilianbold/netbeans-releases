d0b567b4dca46d62912e531ed8e8490d31fae604
Petr Pisl <ppisl@netbeans.org>
2015-03-04 14:25:12 +0100

    #250860 - The platin text is treated as whitespace in lexer - another case

--- javascript2.jade/test/unit/src/org/netbeans/modules/javascript2/jade/editor/lexer/JadeLexerTest.java
+++ javascript2.jade/test/unit/src/org/netbeans/modules/javascript2/jade/editor/lexer/JadeLexerTest.java
@@ -328,6 +328,10 @@ public class JadeLexerTest extends CslTestBase {
         checkLexer("testfiles/lexer/issue250860.jade");
     }
     
+    public void testIssue250860_01() throws Exception {
+        checkLexer("testfiles/lexer/issue250860_01.jade");
+    }
+    
     private void checkLexer(final String filePath) throws Exception {
         Source testSource = getTestSource(getTestFile(filePath));
         Snapshot snapshot = testSource.createSnapshot();
