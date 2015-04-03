c154da2db3925d8b1d29f42ecf394c240d0fd726
Petr Pisl <ppisl@netbeans.org>
2015-04-03 10:33:39 +0200

    #250322 - Completion for core modules does not work in nested scope

--- javascript2.nodejs/test/unit/src/org/netbeans/modules/javascript2/nodejs/editor/NodeJSCodeCompletionTest.java
+++ javascript2.nodejs/test/unit/src/org/netbeans/modules/javascript2/nodejs/editor/NodeJSCodeCompletionTest.java
@@ -276,6 +276,14 @@ public class NodeJSCodeCompletionTest extends JsCodeCompletionBase {
         checkCompletion("TestNavigation/public_html/js/issue250338/issue250338_03.js", "require('../cc01/complexModule').^", false);
     }
     
+    public void testIssue250322_01() throws Exception {
+        checkCompletion("TestNavigation/public_html/js/issue250322.js", "ht.^; // cc here", false);
+    }
+    
+    public void testIssue250322_02() throws Exception {
+        checkCompletion("TestNavigation/public_html/js/issue250322.js", "fs.^; // cc here", false);
+    }
+    
     @Override
     protected Map<String, ClassPath> createClassPathsForTest() {
         List<FileObject> cpRoots = new LinkedList<FileObject>();
