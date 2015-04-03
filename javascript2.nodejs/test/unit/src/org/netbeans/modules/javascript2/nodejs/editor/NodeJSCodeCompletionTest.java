5198ade9ca8e2c3c9ea1370943690fc4895be06f
Petr Pisl <ppisl@netbeans.org>
2015-04-03 10:26:58 +0200

    #250338 - Code completion for anonymous module in inner scope

--- javascript2.nodejs/test/unit/src/org/netbeans/modules/javascript2/nodejs/editor/NodeJSCodeCompletionTest.java
+++ javascript2.nodejs/test/unit/src/org/netbeans/modules/javascript2/nodejs/editor/NodeJSCodeCompletionTest.java
@@ -264,6 +264,18 @@ public class NodeJSCodeCompletionTest extends JsCodeCompletionBase {
         checkCompletion("TestNavigation/public_html/js/issue250603/issue250603.js", "if (this.^)", false);
     }
     
+    public void testIssue250338_01() throws Exception {
+        checkCompletion("TestNavigation/public_html/js/issue250338/issue250338_01.js", "require('../cc01/complexModule').^", false);
+    }
+    
+    public void testIssue250338_02() throws Exception {
+        checkCompletion("TestNavigation/public_html/js/issue250338/issue250338_02.js", "require('../cc01/complexModule').^", false);
+    }
+    
+    public void testIssue250338_03() throws Exception {
+        checkCompletion("TestNavigation/public_html/js/issue250338/issue250338_03.js", "require('../cc01/complexModule').^", false);
+    }
+    
     @Override
     protected Map<String, ClassPath> createClassPathsForTest() {
         List<FileObject> cpRoots = new LinkedList<FileObject>();
