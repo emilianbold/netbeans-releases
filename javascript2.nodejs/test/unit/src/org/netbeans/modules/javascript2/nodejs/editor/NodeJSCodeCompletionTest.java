aa08cc7691bad44131a8a1d59e46a53f1d5d92a1
Petr Pisl <ppisl@netbeans.org>
2015-04-03 10:09:13 +0200

    #250603 - No code completion for this inside object literal

--- javascript2.nodejs/test/unit/src/org/netbeans/modules/javascript2/nodejs/editor/NodeJSCodeCompletionTest.java
+++ javascript2.nodejs/test/unit/src/org/netbeans/modules/javascript2/nodejs/editor/NodeJSCodeCompletionTest.java
@@ -260,6 +260,10 @@ public class NodeJSCodeCompletionTest extends JsCodeCompletionBase {
         checkCompletion("TestNavigation/public_html/js/issue249439/test249439.js", "wn.^create();", false);
     }
     
+    public void testIssue250603_01() throws Exception {
+        checkCompletion("TestNavigation/public_html/js/issue250603/issue250603.js", "if (this.^)", false);
+    }
+    
     @Override
     protected Map<String, ClassPath> createClassPathsForTest() {
         List<FileObject> cpRoots = new LinkedList<FileObject>();
