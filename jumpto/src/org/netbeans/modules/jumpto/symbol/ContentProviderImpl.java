a159c23f51a06923cf1d7082ea7e0ea488d99b07
Tomas Zezula <tzezula@netbeans.org>
2015-04-24 11:03:26 +0200

    #252066: Highlighting does not work for Go To Type andGo To Symbol

--- jumpto/src/org/netbeans/modules/jumpto/symbol/ContentProviderImpl.java
+++ jumpto/src/org/netbeans/modules/jumpto/symbol/ContentProviderImpl.java
@@ -111,6 +111,14 @@ final class ContentProviderImpl implements GoToPanel.ContentProvider {
                     protected String getItemValue(@NonNull final SymbolDescriptor item) {
                         return item.getSymbolName();
                     }
+                    @Override
+                    protected void update(@NonNull final SymbolDescriptor item) {
+                        String searchText = getSearchText();
+                        if (searchText == null) {
+                            searchText = "";    //NOI18N
+                        }
+                        SymbolProviderAccessor.DEFAULT.setHighlightText(item, searchText);
+                    }
                 };
             }
         });
