35ae6ace7ac690716a3a0892d51f211b664d296c
Alexander Simon <alexvsimon@netbeans.org>
2015-06-04 15:11:08 +0300

    reformat configurations

--- cnd.discovery/tools/BuildTrace/nbproject/Makefile-variables.mk
+++ cnd.discovery/tools/BuildTrace/nbproject/Makefile-variables.mk
@@ -6,22 +6,6 @@
 CND_BASEDIR=`pwd`
 CND_BUILDDIR=build
 CND_DISTDIR=dist
-# SunOS-x86 configuration
-CND_PLATFORM_SunOS-x86=OracleSolarisStudio-Solaris-x86
-CND_ARTIFACT_DIR_SunOS-x86=dist/SunOS-x86/OracleSolarisStudio-Solaris-x86
-CND_ARTIFACT_NAME_SunOS-x86=libBuildTrace.so
-CND_ARTIFACT_PATH_SunOS-x86=dist/SunOS-x86/OracleSolarisStudio-Solaris-x86/libBuildTrace.so
-CND_PACKAGE_DIR_SunOS-x86=dist/SunOS-x86/OracleSolarisStudio-Solaris-x86/package
-CND_PACKAGE_NAME_SunOS-x86=libBuildTrace.so.tar
-CND_PACKAGE_PATH_SunOS-x86=dist/SunOS-x86/OracleSolarisStudio-Solaris-x86/package/libBuildTrace.so.tar
-# SunOS-x86_64 configuration
-CND_PLATFORM_SunOS-x86_64=OracleSolarisStudio-Solaris-x86
-CND_ARTIFACT_DIR_SunOS-x86_64=dist/SunOS-x86_64/OracleSolarisStudio-Solaris-x86
-CND_ARTIFACT_NAME_SunOS-x86_64=libBuildTrace.so
-CND_ARTIFACT_PATH_SunOS-x86_64=dist/SunOS-x86_64/OracleSolarisStudio-Solaris-x86/libBuildTrace.so
-CND_PACKAGE_DIR_SunOS-x86_64=dist/SunOS-x86_64/OracleSolarisStudio-Solaris-x86/package
-CND_PACKAGE_NAME_SunOS-x86_64=libBuildTrace.so.tar
-CND_PACKAGE_PATH_SunOS-x86_64=dist/SunOS-x86_64/OracleSolarisStudio-Solaris-x86/package/libBuildTrace.so.tar
 # SunOS-sparc configuration
 CND_PLATFORM_SunOS-sparc=OracleSolarisStudio-Solaris-Sparc
 CND_ARTIFACT_DIR_SunOS-sparc=dist/SunOS-sparc/OracleSolarisStudio-Solaris-Sparc
@@ -38,6 +22,30 @@ CND_ARTIFACT_PATH_SunOS-sparc_64=dist/SunOS-sparc_64/OracleSolarisStudio-Solaris
 CND_PACKAGE_DIR_SunOS-sparc_64=dist/SunOS-sparc_64/OracleSolarisStudio-Solaris-Sparc/package
 CND_PACKAGE_NAME_SunOS-sparc_64=libBuildTrace.so.tar
 CND_PACKAGE_PATH_SunOS-sparc_64=dist/SunOS-sparc_64/OracleSolarisStudio-Solaris-Sparc/package/libBuildTrace.so.tar
+# SunOS-x86 configuration
+CND_PLATFORM_SunOS-x86=OracleSolarisStudio-Solaris-x86
+CND_ARTIFACT_DIR_SunOS-x86=dist/SunOS-x86/OracleSolarisStudio-Solaris-x86
+CND_ARTIFACT_NAME_SunOS-x86=libBuildTrace.so
+CND_ARTIFACT_PATH_SunOS-x86=dist/SunOS-x86/OracleSolarisStudio-Solaris-x86/libBuildTrace.so
+CND_PACKAGE_DIR_SunOS-x86=dist/SunOS-x86/OracleSolarisStudio-Solaris-x86/package
+CND_PACKAGE_NAME_SunOS-x86=libBuildTrace.so.tar
+CND_PACKAGE_PATH_SunOS-x86=dist/SunOS-x86/OracleSolarisStudio-Solaris-x86/package/libBuildTrace.so.tar
+# SunOS-x86_64 configuration
+CND_PLATFORM_SunOS-x86_64=OracleSolarisStudio-Solaris-x86
+CND_ARTIFACT_DIR_SunOS-x86_64=dist/SunOS-x86_64/OracleSolarisStudio-Solaris-x86
+CND_ARTIFACT_NAME_SunOS-x86_64=libBuildTrace.so
+CND_ARTIFACT_PATH_SunOS-x86_64=dist/SunOS-x86_64/OracleSolarisStudio-Solaris-x86/libBuildTrace.so
+CND_PACKAGE_DIR_SunOS-x86_64=dist/SunOS-x86_64/OracleSolarisStudio-Solaris-x86/package
+CND_PACKAGE_NAME_SunOS-x86_64=libBuildTrace.so.tar
+CND_PACKAGE_PATH_SunOS-x86_64=dist/SunOS-x86_64/OracleSolarisStudio-Solaris-x86/package/libBuildTrace.so.tar
+# SunOS-Previse configuration
+CND_PLATFORM_SunOS-Previse=OracleSolarisStudio-Solaris-x86
+CND_ARTIFACT_DIR_SunOS-Previse=dist/SunOS-Previse/OracleSolarisStudio-Solaris-x86
+CND_ARTIFACT_NAME_SunOS-Previse=libBuildTrace.so
+CND_ARTIFACT_PATH_SunOS-Previse=dist/SunOS-Previse/OracleSolarisStudio-Solaris-x86/libBuildTrace.so
+CND_PACKAGE_DIR_SunOS-Previse=dist/SunOS-Previse/OracleSolarisStudio-Solaris-x86/package
+CND_PACKAGE_NAME_SunOS-Previse=libBuildTrace.so.tar
+CND_PACKAGE_PATH_SunOS-Previse=dist/SunOS-Previse/OracleSolarisStudio-Solaris-x86/package/libBuildTrace.so.tar
 # Linux-x86 configuration
 CND_PLATFORM_Linux-x86=OracleSolarisStudio-Linux-x86
 CND_ARTIFACT_DIR_Linux-x86=dist/Linux-x86/OracleSolarisStudio-Linux-x86
@@ -54,6 +62,14 @@ CND_ARTIFACT_PATH_Linux-x86_64=dist/Linux-x86_64/OracleSolarisStudio-Linux-x86/l
 CND_PACKAGE_DIR_Linux-x86_64=dist/Linux-x86_64/OracleSolarisStudio-Linux-x86/package
 CND_PACKAGE_NAME_Linux-x86_64=libBuildTrace.so.tar
 CND_PACKAGE_PATH_Linux-x86_64=dist/Linux-x86_64/OracleSolarisStudio-Linux-x86/package/libBuildTrace.so.tar
+# Linux-sparc_64 configuration
+CND_PLATFORM_Linux-sparc_64=GNU-Linux-x86
+CND_ARTIFACT_DIR_Linux-sparc_64=dist/Linux-sparc_64/GNU-Linux-x86
+CND_ARTIFACT_NAME_Linux-sparc_64=libBuildTrace.so
+CND_ARTIFACT_PATH_Linux-sparc_64=dist/Linux-sparc_64/GNU-Linux-x86/libBuildTrace.so
+CND_PACKAGE_DIR_Linux-sparc_64=dist/Linux-sparc_64/GNU-Linux-x86/package
+CND_PACKAGE_NAME_Linux-sparc_64=libBuildTrace.so.tar
+CND_PACKAGE_PATH_Linux-sparc_64=dist/Linux-sparc_64/GNU-Linux-x86/package/libBuildTrace.so.tar
 # MacOSX-x86 configuration
 CND_PLATFORM_MacOSX-x86=GNU-MacOSX
 CND_ARTIFACT_DIR_MacOSX-x86=dist/MacOSX-x86/GNU-MacOSX
@@ -70,22 +86,6 @@ CND_ARTIFACT_PATH_MacOSX-x86_64=dist/MacOSX-x86_64/GNU-Solaris-x86/libBuildTrace
 CND_PACKAGE_DIR_MacOSX-x86_64=dist/MacOSX-x86_64/GNU-Solaris-x86/package
 CND_PACKAGE_NAME_MacOSX-x86_64=libBuildTrace.so.tar
 CND_PACKAGE_PATH_MacOSX-x86_64=dist/MacOSX-x86_64/GNU-Solaris-x86/package/libBuildTrace.so.tar
-# Linux-sparc_64 configuration
-CND_PLATFORM_Linux-sparc_64=GNU-Linux-x86
-CND_ARTIFACT_DIR_Linux-sparc_64=dist/Linux-sparc_64/GNU-Linux-x86
-CND_ARTIFACT_NAME_Linux-sparc_64=libBuildTrace.so
-CND_ARTIFACT_PATH_Linux-sparc_64=dist/Linux-sparc_64/GNU-Linux-x86/libBuildTrace.so
-CND_PACKAGE_DIR_Linux-sparc_64=dist/Linux-sparc_64/GNU-Linux-x86/package
-CND_PACKAGE_NAME_Linux-sparc_64=libBuildTrace.so.tar
-CND_PACKAGE_PATH_Linux-sparc_64=dist/Linux-sparc_64/GNU-Linux-x86/package/libBuildTrace.so.tar
-# SunOS-Previse configuration
-CND_PLATFORM_SunOS-Previse=OracleSolarisStudio-Solaris-x86
-CND_ARTIFACT_DIR_SunOS-Previse=dist/SunOS-Previse/OracleSolarisStudio-Solaris-x86
-CND_ARTIFACT_NAME_SunOS-Previse=libBuildTrace.so
-CND_ARTIFACT_PATH_SunOS-Previse=dist/SunOS-Previse/OracleSolarisStudio-Solaris-x86/libBuildTrace.so
-CND_PACKAGE_DIR_SunOS-Previse=dist/SunOS-Previse/OracleSolarisStudio-Solaris-x86/package
-CND_PACKAGE_NAME_SunOS-Previse=libBuildTrace.so.tar
-CND_PACKAGE_PATH_SunOS-Previse=dist/SunOS-Previse/OracleSolarisStudio-Solaris-x86/package/libBuildTrace.so.tar
 #
 # include compiler specific variables
 #
