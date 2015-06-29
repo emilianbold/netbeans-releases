8aa3d0413735b1d80001e11504400b60c3acceb2
Tomas Zezula <tzezula@netbeans.org>
2015-06-29 14:22:35 +0200

    Clean up.

--- parsing.api/src/org/netbeans/modules/parsing/spi/LowMemoryWatcher.java
+++ parsing.api/src/org/netbeans/modules/parsing/spi/LowMemoryWatcher.java
@@ -42,73 +42,41 @@
 
 package org.netbeans.modules.parsing.spi;
 
-import java.lang.management.ManagementFactory;
-import java.lang.management.MemoryMXBean;
-import java.lang.management.MemoryUsage;
-import java.util.concurrent.Callable;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.openide.util.Exceptions;
-
 /**
  * A service providing information about
  * low memory condition.
- * 
+ *
  * @since 1.2
  * @author Tomas Zezula
  */
 public final class LowMemoryWatcher {
 
-    private static final Logger LOG = Logger.getLogger(LowMemoryWatcher.class.getName());
-    private static final long LOGGER_RATE = Integer.getInteger(
-            String.format("%s.logger_rate",LowMemoryWatcher.class.getName()),   //NOI18N
-            1000);   //1s
-
-    
     //@GuardedBy("LowMemoryWatcher.class")
     private static LowMemoryWatcher instance;
 
-    private final Callable<Boolean> strategy;
-    private final AtomicBoolean testEnforcesLowMemory = new AtomicBoolean();    
+    private final org.netbeans.modules.parsing.lucene.support.LowMemoryWatcher delegate;
 
     private LowMemoryWatcher () {
-        this.strategy = new DefaultStrategy();
+        this.delegate = org.netbeans.modules.parsing.lucene.support.LowMemoryWatcher.getInstance();
     }
-    
+
     /**
      * Returns true if the application is in low memory condition.
      * This information can be used by batch file processing.
      * @return true if nearly whole memory is used
      */
     public boolean isLowMemory () {
-        if (testEnforcesLowMemory.get()) {
-            return true;
-        }
-        try {
-            return strategy.call();
-        } catch (Exception e) {
-            Exceptions.printStackTrace(e);
-            return false;
-        }
+        return delegate.isLowMemory();
     }
-    
+
     /**
      * Tries to free memory.
      * @since 2.12
      */
     public void free() {
-        final Runtime rt = Runtime.getRuntime();
-        rt.gc();
-        rt.runFinalization();
-        rt.gc();
-        rt.gc();
+        delegate.free();
     }
 
-    /*test*/ void setLowMemory(final boolean lowMemory) {
-        this.testEnforcesLowMemory.set(lowMemory);
-    }
-    
     /**
      * Returns an instance of {@link LowMemoryWatcher}
      * @return the {@link LowMemoryWatcher}
@@ -119,46 +87,4 @@ public final class LowMemoryWatcher {
         }
         return instance;
     }
-
-
-    private static class DefaultStrategy implements Callable<Boolean> {
-
-        private static final float heapLimit = 0.8f;
-        private final MemoryMXBean memBean;
-        private volatile long lastTime;
-
-        DefaultStrategy() {
-            this.memBean = ManagementFactory.getMemoryMXBean();
-            assert this.memBean != null;
-        }
-
-        @Override
-        public Boolean call() throws Exception {
-            if (this.memBean != null) {
-                final MemoryUsage usage = this.memBean.getHeapMemoryUsage();
-                if (usage != null) {
-                    long used = usage.getUsed();
-                    long max = usage.getMax();
-                    final boolean res = used > max * heapLimit;
-                    if (LOG.isLoggable(Level.FINEST)) {
-                        final long now = System.currentTimeMillis();
-                        if (now - lastTime > LOGGER_RATE) {
-                            LOG.log(
-                                Level.FINEST,
-                                "Max memory: {0}, Used memory: {1}, Low memory condition: {2}", //NOI18N
-                                new Object[]{
-                                    max,
-                                    used,
-                                    res
-                                });
-                            lastTime = now;
-                        }
-                    }
-                    return res;
-                }
-            }
-            return false;
-        }
-    }
-    
 }
