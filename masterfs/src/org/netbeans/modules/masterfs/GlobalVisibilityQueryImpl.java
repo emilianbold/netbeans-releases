04d11d8f0a049e0dd7841e2809f1f11dbe0c7ac7
Jaroslav Havlin <jhavlin@netbeans.org>
2015-07-20 10:13:48 +0200

    #246521: Show .dotted files (by default)

--- masterfs/src/org/netbeans/modules/masterfs/GlobalVisibilityQueryImpl.java
+++ masterfs/src/org/netbeans/modules/masterfs/GlobalVisibilityQueryImpl.java
@@ -52,6 +52,7 @@ import javax.swing.event.ChangeListener;
 import java.util.prefs.Preferences;
 import java.util.regex.Pattern;
 import org.netbeans.spi.queries.VisibilityQueryImplementation2;
+import org.openide.filesystems.FileUtil;
 import org.openide.util.ChangeSupport;
 import org.openide.util.NbPreferences;
 import org.openide.util.lookup.ServiceProvider;
@@ -78,7 +79,18 @@ public class GlobalVisibilityQueryImpl implements VisibilityQueryImplementation2
      * Keep it synchronized with IgnoredFilesPreferences.PROP_IGNORED_FILES
      */ 
     private static final String PROP_IGNORED_FILES = "IgnoredFiles"; // NOI18N
+    private static final String PROP_IGNORE_HIDDEN_FILES_IN_USER_HOME
+            = "IgnoreHiddenFilesInUserHome";                           // NOI18N
     private Pattern ignoreFilesPattern = null;
+
+    private boolean ignoreHiddenInHome = true;
+    private boolean ignoreHiddenInHomeInitialized = false;
+
+    private FileObject home = null;
+    private String homePath = null;
+    private boolean homeInitialized = false;
+
+    private PreferenceChangeListener preferencesListener = null;
 
     /** Default instance for lookup. */
     public GlobalVisibilityQueryImpl() {
@@ -89,11 +101,21 @@ public class GlobalVisibilityQueryImpl implements VisibilityQueryImplementation2
     }
     
     public boolean isVisible(FileObject file) {
-        return isVisible(file.getNameExt());
+        String name = file.getNameExt();
+        if (isIgnoreHiddenInHome() && isHidden(name) && isInHomeFolder(file)) {
+            return false;
+        } else {
+            return isVisible(name);
+        }
     }
     
     public boolean isVisible(File file) {
-        return isVisible(file.getName());
+        String name = file.getName();
+        if (isIgnoreHiddenInHome() && isHidden(name) && isInHomeFolder(file)) {
+            return false;
+        } else {
+            return isVisible(name);
+        }
     }
     
 
@@ -118,6 +140,15 @@ public class GlobalVisibilityQueryImpl implements VisibilityQueryImplementation2
         cs.removeChangeListener(l);
     }
 
+    private boolean isIgnoreHiddenInHome() {
+        if (!ignoreHiddenInHomeInitialized) {
+            ignoreHiddenInHome = getPreferences().getBoolean(
+                    PROP_IGNORE_HIDDEN_FILES_IN_USER_HOME, true);
+            ignoreHiddenInHomeInitialized = true;
+        }
+        return ignoreHiddenInHome;
+    }
+
     private Pattern getIgnoreFilesPattern() {
         if (ignoreFilesPattern == null) {
             String ignoredFiles = getIgnoredFiles();
@@ -128,17 +159,81 @@ public class GlobalVisibilityQueryImpl implements VisibilityQueryImplementation2
 
     protected String getIgnoredFiles() {
         // \.(cvsignore|svn|DS_Store) is covered by ^\..*$
-        String retval = getPreferences().get(PROP_IGNORED_FILES, "^(CVS|SCCS|vssver.?\\.scc|#.*#|%.*%|_svn)$|~$|^\\.(?!(htaccess|git.+|hgignore|npmignore|jshintrc|bowerrc|travis\\.yml)$).*$");//NOI18N;
-        getPreferences().addPreferenceChangeListener(new PreferenceChangeListener() {
-            public void preferenceChange(PreferenceChangeEvent evt) {
-                if (PROP_IGNORED_FILES.equals(evt.getKey())) {
-                    ignoreFilesPattern = null;
-                    cs.fireChange();
-                }
-                
+        String retval = getPreferences().get(PROP_IGNORED_FILES, "^(CVS|SCCS|vssver.?\\.scc|#.*#|%.*%|_svn)$|~$|^\\.(git|hg|svn|cache|DS_store)$|^Thumbs.db$");//NOI18N;
+        PreferenceChangeListener listenerToAdd;
+        synchronized (this) {
+            if (preferencesListener == null) {
+                preferencesListener = new PreferenceChangeListener() {
+                    @Override
+                    public void preferenceChange(PreferenceChangeEvent evt) {
+                        if (PROP_IGNORED_FILES.equals(evt.getKey())) {
+                            ignoreFilesPattern = null;
+                            cs.fireChange();
+                        } else if (PROP_IGNORE_HIDDEN_FILES_IN_USER_HOME.equals(
+                                evt.getKey())) {
+                            ignoreHiddenInHomeInitialized = false;
+                            cs.fireChange();
+                        }
+                    }
+                };
+                listenerToAdd = preferencesListener;
+            } else {
+                listenerToAdd = null;
             }
-        });                
+        }
+        if (listenerToAdd != null) {
+            getPreferences().addPreferenceChangeListener(listenerToAdd);
+        }
         return retval;
     }
 
+    /**
+     * Check if the file is hidden in Unix file systems.
+     *
+     * @param String File name.
+     * @return True if file name starts with a dot.
+     */
+    private boolean isHidden(String fileName) {
+        return fileName.startsWith(".");                                //NOI18N
+    }
+
+    /**
+     * Check if the file is located directly in user's home folder.
+     *
+     * @param fo The FileObject.
+     * @return True if this is a file located directly in user's home folder.
+     */
+    private boolean isInHomeFolder(FileObject fo) {
+        if (!homeInitialized) {
+            initializeHome();
+        }
+        FileObject parent = fo.getParent();
+        return parent != null && parent == home;
+    }
+
+    /**
+     * Check if the file is located directly in user's home folder.
+     *
+     * @param f The file.
+     * @return True if this is a file located directly in user's home folder.
+     */
+    private boolean isInHomeFolder(File f) {
+        if (!homeInitialized) {
+            initializeHome();
+        }
+        String parentPath = f.getParent();
+        return parentPath != null && parentPath.equals(homePath);
+    }
+
+    /**
+     * Initialize variables holding the user directory.
+     */
+    private void initializeHome() {
+        String homeRaw = System.getProperty("user.home");               //NOI18N
+        if (homeRaw != null) {
+            homePath = FileUtil.normalizePath(homeRaw);
+            home = FileUtil.toFileObject(new File(homePath));
+        }
+        homeInitialized = true;
+    }
 }
