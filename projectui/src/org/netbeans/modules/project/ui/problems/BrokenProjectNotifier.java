4b1afa66e416f6c734a3d8d832fd88548d1ca651
Tomas Zezula <tzezula@netbeans.org>
2015-08-11 18:19:37 +0200

    #252824:org.netbeans.modules.masterfs.filebasedfs.utils.FileChangedManager.exists: LowPerformance took 31089 ms.

--- projectui/src/org/netbeans/modules/project/ui/problems/BrokenProjectNotifier.java
+++ projectui/src/org/netbeans/modules/project/ui/problems/BrokenProjectNotifier.java
@@ -43,8 +43,11 @@ package org.netbeans.modules.project.ui.problems;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.Queue;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
@@ -112,36 +115,44 @@ public class BrokenProjectNotifier {
             @NonNull final Project[] newOpened) {
         final Set<Project> justAdded = new HashSet<Project>(Arrays.asList(newOpened));
         justAdded.removeAll(Arrays.asList(oldOpened));
-        LOG.log(Level.FINE, "New projects to check: {0}", justAdded);   //NOI18N
-        for (Project project : justAdded) {
-            final ProjectProblemsProvider ppp = project.getLookup().lookup(ProjectProblemsProvider.class);
-            if (ppp != null) {
-                for (ProjectProblemsProvider.ProjectProblem problem : ppp.getProblems()) {
-                    if (problem.getSeverity() == ProjectProblemsProvider.Severity.ERROR) {
-                        scheduleAlert(project);
-                        break;
+        if (!justAdded.isEmpty()) {
+            LOG.log(Level.FINE, "New projects to check: {0}", justAdded);   //NOI18N
+            NOTIFIER.execute(new Runnable(){
+                @Override
+                public void run() {
+                    final Queue<Project> toCheck = new ArrayDeque<Project>();
+                    for (Project project : justAdded) {
+                        final ProjectProblemsProvider ppp = project.getLookup().lookup(ProjectProblemsProvider.class);
+                        if (ppp != null) {
+                            for (ProjectProblemsProvider.ProjectProblem problem : ppp.getProblems()) {
+                                if (problem.getSeverity() == ProjectProblemsProvider.Severity.ERROR) {
+                                    toCheck.offer(project);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    if (!toCheck.isEmpty()) {
+                        scheduleAlert(toCheck);
                     }
                 }
-            }
+            });
         }
     }
 
-    private void scheduleAlert(@NonNull final Project prj) {
-        LOG.log(Level.FINE, "Scheduling alert for project: {0}", prj);  //NOI18N
-        NOTIFIER.post(new Runnable() {
-            @Override
-            public void run() {
-                final Future<Project[]> projects = OpenProjects.getDefault().openProjects();
-                try {
-                    projects.get();
-                } catch (ExecutionException ex) {
-                    Exceptions.printStackTrace(ex);
-                } catch (InterruptedException ie) {
-                    Exceptions.printStackTrace(ie);
-                }
-                ProjectProblems.showAlert(prj);
-            }
-        });
+    private void scheduleAlert(@NonNull final Collection<? extends Project> prjs) {
+        final Future<Project[]> projects = OpenProjects.getDefault().openProjects();
+        try {
+            projects.get();
+        } catch (ExecutionException ex) {
+            Exceptions.printStackTrace(ex);
+        } catch (InterruptedException ie) {
+            Exceptions.printStackTrace(ie);
+        }
+        for (Project prj : prjs) {
+            LOG.log(Level.FINE, "Showing alert for project: {0}", prjs);  //NOI18N
+            ProjectProblems.showAlert(prj);
+        }
     }
 
     public static BrokenProjectNotifier getInstnace() {
