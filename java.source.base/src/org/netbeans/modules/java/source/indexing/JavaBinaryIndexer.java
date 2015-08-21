010b4ad211c6eedd43f50da7b294cb4f6d5cae3c
Dusan Balek <dbalek@netbeans.org>
2015-08-21 18:06:33 +0200

    Modified to prevent AssertionErrors.

--- java.source.base/src/org/netbeans/modules/java/source/indexing/JavaBinaryIndexer.java
+++ java.source.base/src/org/netbeans/modules/java/source/indexing/JavaBinaryIndexer.java
@@ -200,11 +200,7 @@ public class JavaBinaryIndexer extends BinaryIndexer {
         final JavacTaskImpl jt = JavacParser.createJavacTask(cpInfo, new DevNullDiagnosticListener(), null, null, null, null, null, null);
         TreeLoader.preRegister(jt.getContext(), cpInfo, true);
         //Force JTImpl.prepareCompiler to get JTImpl into Context
-        try {
-            jt.parse(new JavaFileObject[0]);
-        } catch (IOException ex) {
-            Exceptions.printStackTrace(ex);
-        }
+        jt.enter();
         TypeElement jc = (TypeElement) ((JavacElements)jt.getElements()).getTypeElementByBinaryName(fqn);
         if (jc != null) {
             List<ExecutableElement> methods = ElementFilter.methodsIn(jt.getElements().getAllMembers(jc));
