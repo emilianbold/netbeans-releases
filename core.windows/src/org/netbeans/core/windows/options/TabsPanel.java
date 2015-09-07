7da29d635f65e4d87f4b0ad873ad37390e380543
Theofanis Oikonomou <theofanis@netbeans.org>
2015-09-07 18:11:00 +0200

    fixing typo Task #255041 - Options: Look & Feel tab disabled after search for "look"

--- core.windows/src/org/netbeans/core/windows/options/TabsPanel.java
+++ core.windows/src/org/netbeans/core/windows/options/TabsPanel.java
@@ -52,7 +52,7 @@ import org.netbeans.spi.options.OptionsPanelController;
 import org.openide.util.NbBundle;
 import org.openide.util.NbPreferences;
 
-@OptionsPanelController.Keywords(keywords={"KW_TabsOptions"}, location="Appearance", tabTitle="#Tabs_DisplayName")
+@OptionsPanelController.Keywords(keywords={"#KW_TabsOptions"}, location="Appearance", tabTitle="#Tabs_DisplayName")
 public class TabsPanel extends javax.swing.JPanel {
 
     protected final TabsOptionsPanelController controller;
