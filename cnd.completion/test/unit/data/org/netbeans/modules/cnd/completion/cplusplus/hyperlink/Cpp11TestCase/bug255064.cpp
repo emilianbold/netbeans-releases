ba7ce015de1dc7eb44f47c7b5a8fc23ca933c268
Petr Kudryavtsev <petrk@netbeans.org>
2015-09-08 13:28:09 +0300

    Additional fix for #255064 - Forward strongly typed enum hides its definition -- test case

---
+++ cnd.completion/test/unit/data/org/netbeans/modules/cnd/completion/cplusplus/hyperlink/Cpp11TestCase/bug255064.cpp
@@ -0,0 +1,12 @@
+namespace bug255064 {
+    enum class BLABLA255064 : int {
+        ABC255064
+    };
+
+    enum class BLABLA255064 : int;
+
+    int main255064() {
+        BLABLA255064 var = BLABLA255064::ABC255064;
+        return 0;
+    }     
+}
