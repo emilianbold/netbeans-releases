e4083bff2df5689b02285c106b4954e78182932b
Nikolay Koldunov <henk89@netbeans.org>
2015-09-09 19:08:36 +0300

    Fixed #255145 - DebugTarget.cloneRecord() should clone arguments too

--- cnd.debugger.common2/src/org/netbeans/modules/cnd/debugger/common2/debugger/debugtarget/DebugTarget.java
+++ cnd.debugger.common2/src/org/netbeans/modules/cnd/debugger/common2/debugger/debugtarget/DebugTarget.java
@@ -321,6 +321,7 @@ public final class DebugTarget implements Record {
 	clone.setHostName(this.getHostName());
 	clone.setCorefile(this.getCorefile());
 	clone.setEngine(this.getEngine());
+        clone.setUnparsedArgs(this.getUnparsedArgs());
 
 	return clone;
     }
