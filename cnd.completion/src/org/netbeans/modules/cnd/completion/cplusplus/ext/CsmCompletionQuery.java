5943d812676a0bfd2d9c36328b02db7c6fc07b57
Petr Kudryavtsev <petrk@netbeans.org>
2015-09-17 16:58:33 +0300

    Additional fix for #244177 - Unresolved decltype inside decltype -- fixed issue with additional test case

--- cnd.completion/src/org/netbeans/modules/cnd/completion/cplusplus/ext/CsmCompletionQuery.java
+++ cnd.completion/src/org/netbeans/modules/cnd/completion/cplusplus/ext/CsmCompletionQuery.java
@@ -384,11 +384,13 @@ abstract public class CsmCompletionQuery {
                     csmFile = CsmUtilities.getCsmFile(baseDocument, true, false);
                 }
 
+                final boolean processAsExpression = CsmKindUtilities.isExpression(expression);
+                
                 CsmCompletionTokenProcessor tp;
 
                 CsmScope passedScope = null;
 
-                if (CsmKindUtilities.isExpression(expression)) {
+                if (processAsExpression) {
                     passedScope = ((CsmExpression) expression).getScope();
                     tp = processTokensInExpression((CsmExpression) expression, task.isFindTypeTask());
                 } else {
@@ -399,7 +401,7 @@ abstract public class CsmCompletionQuery {
                     try {
                         pushResolveContext(CsmResolveContext.create(csmFile, startOffset));
 
-                        CsmCompletionExpression exp = tp.getResultExp();
+                        CsmCompletionExpression exp = getResultExpression(tp, processAsExpression, task.isFindTypeTask());
 
                         Context ctx = getResolvedContext(
                                 null,
@@ -744,8 +746,8 @@ abstract public class CsmCompletionQuery {
                 CppTokenId.LPAREN.fixedText() + expression.getExpandedText().toString() + CppTokenId.RPAREN.fixedText() :
                 expression.getExpandedText().toString();
 
-        int exprStartOffset = expression.getStartOffset();
-        int exprEndOffset = exprStartOffset + expressionText.length();
+        int exprStartOffset = expression.getStartOffset() - (keepWholeAst ? 1 : 0);
+        int exprEndOffset = exprStartOffset + expressionText.length() + (keepWholeAst ? 1 : 0);
 
         final CsmCompletionTokenProcessor tp = new CsmCompletionTokenProcessor(exprEndOffset, exprStartOffset);
         TokenHierarchy<String> hi = TokenHierarchy.create(
@@ -776,6 +778,17 @@ abstract public class CsmCompletionQuery {
         }
 
         return tp;
+    }
+    
+    private CsmCompletionExpression getResultExpression(CsmCompletionTokenProcessor tp, boolean processedAsExpression, boolean keepWholeAst) {
+        if (processedAsExpression && keepWholeAst) {
+            // See implementation of processTokensInExpression
+            CsmCompletionExpression resultExp = tp.getResultExp();
+            if (resultExp != null && resultExp.getParameterCount() == 1 && resultExp.getExpID() == CsmCompletionExpression.PARENTHESIS) {
+                return resultExp.getParameter(0);
+            }
+        }
+        return tp.getResultExp();
     }
 
     abstract protected boolean isProjectBeeingParsed(boolean openingSource);
@@ -1452,9 +1465,10 @@ abstract public class CsmCompletionQuery {
         private CsmType findExpressionType(final CsmOffsetable expression) {
             if (expression != null && !antiLoop.contains(expression)) {
 
+                final boolean processAsExpression = CsmKindUtilities.isExpression(expression);
                 CsmCompletionTokenProcessor tp;
 
-                if (CsmKindUtilities.isExpression(expression)) {
+                if (processAsExpression) {
                     tp = processTokensInExpression((CsmExpression) expression, true);
                 } else {
                     long docVersion = DocumentUtilities.getDocumentVersion(getBaseDocument());
@@ -1463,7 +1477,7 @@ abstract public class CsmCompletionQuery {
 
                 if (!checkErrorTokenState(tp)) {
                     antiLoop.add(expression);
-                    CsmCompletionExpression exp = tp.getResultExp();
+                    CsmCompletionExpression exp = getResultExpression(tp, processAsExpression, true);
                     return resolveType(exp);
                 }
             }
