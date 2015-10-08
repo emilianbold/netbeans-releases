00446ea495cfbfa05692c6624e6dbbadec5a0341
Tomas Mysik <tmysik@netbeans.org>
2015-10-08 11:04:22 +0200

    One more change related to spaceship operator

--- php.editor/src/org/netbeans/modules/php/editor/parser/PHP5ErrorHandlerImpl.java
+++ php.editor/src/org/netbeans/modules/php/editor/parser/PHP5ErrorHandlerImpl.java
@@ -338,6 +338,7 @@ public class PHP5ErrorHandlerImpl implements PHP5ErrorHandler {
                 case ASTPHP5Symbols.T_INTERFACE : text = "interface"; break; //NOI18N
                 case ASTPHP5Symbols.T_SWITCH : text = "switch"; break; //NOI18N
                 case ASTPHP5Symbols.T_IS_GREATER_OR_EQUAL : text = ">="; break; //NOI18N
+                case ASTPHP5Symbols.T_SPACESHIP : text = "<=>"; break; //NOI18N
                 case ASTPHP5Symbols.T_OPEN_RECT : text = "["; break; //NOI18N
                 case ASTPHP5Symbols.T_CURLY_OPEN_WITH_DOLAR : text = "{$"; break; //NOI18N
                 case ASTPHP5Symbols.T_FINAL : text = "final"; break; //NOI18N
