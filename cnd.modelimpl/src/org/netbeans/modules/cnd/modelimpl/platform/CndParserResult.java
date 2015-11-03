13a129f166c9e2e960442446abef1a5b2d4d19bb
Alexander Simon <alexvsimon@netbeans.org>
2015-11-03 19:19:39 +0300

    fixed Bug #256285 Project with existing code lost C++11 flag after opening (transplanted from d47493e7235a152215f2bcedb9373c274e376e46)

--- cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/platform/CndParserResult.java
+++ cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/platform/CndParserResult.java
@@ -42,25 +42,44 @@
 
 package org.netbeans.modules.cnd.modelimpl.platform;
 
+import javax.swing.text.Document;
+import org.netbeans.api.lexer.TokenHierarchy;
+import org.netbeans.api.lexer.TokenHierarchyEvent;
+import org.netbeans.api.lexer.TokenHierarchyEventType;
+import org.netbeans.api.lexer.TokenHierarchyListener;
 import org.netbeans.modules.cnd.api.model.CsmFile;
 import org.netbeans.modules.parsing.api.Snapshot;
 import org.netbeans.modules.parsing.spi.Parser.Result;
+import org.openide.util.WeakListeners;
 
 /**
  *
  * @author Alexander Simon
  */
-public class CndParserResult  extends Result {
+public class CndParserResult  extends Result implements TokenHierarchyListener {
 
     private final CsmFile file;
     private final long fileVersion;
     private final long docVersion;
+    private boolean invalid = false;
 
     /*package*/CndParserResult(CsmFile file, Snapshot snapshot, long fileVersion, long docVersion) {
         super(snapshot);
         this.file = file;
         this.fileVersion = fileVersion;
         this.docVersion = docVersion;
+        // when snapshot is document based we'd like to be sensitive to
+        // rebuild of TokenHierarchy, i.e. when language flavor of document
+        // is changed and document is relexed;
+        // or DocumentLanguageFlavorProvider.addProperty took long time
+        // and Parsing API was invoked on the document with default language flavor
+        final Document doc = snapshot.getSource().getDocument(false);
+        if (doc != null) {
+            TokenHierarchy<Document> hierarchy = TokenHierarchy.get(doc);
+            if (hierarchy != null) {
+                hierarchy.addTokenHierarchyListener(WeakListeners.create(TokenHierarchyListener.class, CndParserResult.this, hierarchy));
+            }
+        }
     }
 
     @Override
@@ -77,5 +96,16 @@ public class CndParserResult  extends Result {
     
     /*package*/ long getDocumentVersion() {
         return docVersion;
+    }
+
+    /*package*/ boolean isInvalid() {
+        return invalid;
+    }
+
+    @Override
+    public void tokenHierarchyChanged(TokenHierarchyEvent evt) {
+        if (evt.type() == TokenHierarchyEventType.REBUILD) {
+            this.invalid = true;
+        }
     }
 }
