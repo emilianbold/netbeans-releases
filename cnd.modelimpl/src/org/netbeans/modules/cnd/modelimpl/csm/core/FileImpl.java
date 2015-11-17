8f4f267bf799fb395a4b90a0c3cba442d2d58c39
Alexander Simon <alexvsimon@netbeans.org>
2015-11-17 16:57:01 +0300

    fixed Bug 216611 Change in include directive causes wrong macro hyperlink (transplanted from 68de5a3328c67a68aa77bb021f317579248ac66e)

--- cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/csm/core/FileImpl.java
+++ cnd.modelimpl/src/org/netbeans/modules/cnd/modelimpl/csm/core/FileImpl.java
@@ -1533,13 +1533,14 @@ public final class FileImpl implements CsmFile,
         parser.init(this, filteredTokenStream, parseParams.callback);
 
         parseResult = parser.parse(parseParams.lazyCompound ? CsmParser.ConstructionKind.TRANSLATION_UNIT : CsmParser.ConstructionKind.TRANSLATION_UNIT_WITH_COMPOUND);
-        
+        PreprocHandler.State ppOrigState = ppState;
+        PreprocHandler.State ppUsedState = parseParams.tsp.getPreprocHandlerStartState();
         if (!cacheTokens) {
             pcState = parseParams.tsp.release();
         }
 
         assert pcState != null;
-        startProject.setParsedPCState(this, ppState, pcState);
+        startProject.setParsedPCState(this, ppOrigState, ppUsedState, pcState);
 
         if (emptyAstStatictics) {
             time = System.currentTimeMillis() - time;
