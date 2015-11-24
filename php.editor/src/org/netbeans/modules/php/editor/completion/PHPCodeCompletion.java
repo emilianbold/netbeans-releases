c09a1d6891bf952cebc757b7c75f90fca6db7ec7
Tomas Mysik <tmysik@netbeans.org>
2015-11-24 15:57:26 +0100

    Add "yield from" to code-completion

--- php.editor/src/org/netbeans/modules/php/editor/completion/PHPCodeCompletion.java
+++ php.editor/src/org/netbeans/modules/php/editor/completion/PHPCodeCompletion.java
@@ -192,6 +192,7 @@ public class PHPCodeCompletion implements CodeCompletionHandler2 {
         PHP_KEYWORDS.put("xor", KeywordCompletionType.ENDS_WITH_SPACE); //NOI18N
         PHP_KEYWORDS.put("finally", KeywordCompletionType.ENDS_WITH_CURLY_BRACKETS); //NOI18N
         PHP_KEYWORDS.put("yield", KeywordCompletionType.CURSOR_BEFORE_ENDING_SEMICOLON); //NOI18N
+        PHP_KEYWORDS.put("yield from", KeywordCompletionType.ENDS_WITH_SPACE); //NOI18N
     }
     private static final String[] PHP_LANGUAGE_CONSTRUCTS_WITH_QUOTES = {
         "echo", "include", "include_once", "require", "require_once", "print" // NOI18N
