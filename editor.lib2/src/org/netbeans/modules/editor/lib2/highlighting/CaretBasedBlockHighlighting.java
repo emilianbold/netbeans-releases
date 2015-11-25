ab9e6a5c243e1f64f5d6745979c83b0191856f60
Ralph Benjamin Ruijs <ralphbenjamin@netbeans.org>
2015-11-25 13:00:31 +0100

    Update CaretBasedHighlighting to show the selection of all carets

--- editor.lib2/src/org/netbeans/modules/editor/lib2/highlighting/CaretBasedBlockHighlighting.java
+++ editor.lib2/src/org/netbeans/modules/editor/lib2/highlighting/CaretBasedBlockHighlighting.java
@@ -64,18 +64,19 @@ import javax.swing.text.JTextComponent;
 import javax.swing.text.Position;
 import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
+import org.netbeans.api.editor.CaretInfo;
+import org.netbeans.api.editor.EditorCaret;
 import org.netbeans.api.editor.mimelookup.MimeLookup;
 import org.netbeans.api.editor.mimelookup.MimePath;
 import org.netbeans.api.editor.settings.AttributesUtilities;
 import org.netbeans.api.editor.settings.FontColorNames;
 import org.netbeans.api.editor.settings.FontColorSettings;
-import org.netbeans.lib.editor.util.swing.BlockCompare;
 import org.netbeans.modules.editor.lib2.DocUtils;
 import org.netbeans.modules.editor.lib2.RectangularSelectionUtils;
 import org.netbeans.spi.editor.highlighting.HighlightsChangeEvent;
 import org.netbeans.spi.editor.highlighting.HighlightsChangeListener;
+import org.netbeans.spi.editor.highlighting.HighlightsContainer;
 import org.netbeans.spi.editor.highlighting.HighlightsSequence;
-import org.netbeans.spi.editor.highlighting.support.AbstractHighlightsContainer;
 import org.netbeans.spi.editor.highlighting.support.PositionsBag;
 import org.openide.util.Lookup;
 import org.openide.util.LookupEvent;
@@ -87,7 +88,7 @@ import org.openide.util.WeakListeners;
  * 
  * @author Vita Stejskal
  */
-public abstract class CaretBasedBlockHighlighting extends AbstractHighlightsContainer implements ChangeListener, PropertyChangeListener {
+public abstract class CaretBasedBlockHighlighting implements HighlightsContainer, ChangeListener, PropertyChangeListener {
 
     // -J-Dorg.netbeans.modules.editor.lib2.highlighting.CaretBasedBlockHighlighting.level=FINE
     private static final Logger LOG = Logger.getLogger(CaretBasedBlockHighlighting.class.getName());
@@ -103,8 +104,7 @@ public abstract class CaretBasedBlockHighlighting extends AbstractHighlightsCont
     private final boolean extendsEOL;
     private final boolean extendsEmptyLine;
     
-    private Position currentBlockStart;
-    private Position currentBlockEnd;
+    private final PositionsBag selectionsBag;
     
     private AttributeSet attribs;
     
@@ -122,6 +122,8 @@ public abstract class CaretBasedBlockHighlighting extends AbstractHighlightsCont
         
         // Hook up the component
         this.component = component;
+        
+        selectionsBag = new PositionsBag(component.getDocument(), false);
     }
     
     private void init() {
@@ -156,31 +158,18 @@ public abstract class CaretBasedBlockHighlighting extends AbstractHighlightsCont
             inited = true;
             init();
         }
+        
+        return selectionsBag.getHighlights(startOffset, endOffset);
+    }
 
-        Position blockStart;
-        Position blockEnd;
-        synchronized (this) {
-            blockStart = currentBlockStart;
-            blockEnd = currentBlockEnd;
-        }
-        if (blockStart != null && blockEnd != null &&
-            endOffset >= blockStart.getOffset() && startOffset <= blockEnd.getOffset())
-        {
-            if (LOG.isLoggable(Level.FINE)) {
-                LOG.fine("Queried for highlights in <" //NOI18N
-                    + startOffset + ", " + endOffset + ">, returning <" //NOI18N
-                    + positionToString(blockStart) + ", " + positionToString(blockEnd) + ">" //NOI18N
-                    + ", layer=" + s2s(this) + '\n'); //NOI18N
-            }
+    @Override
+    public void addHighlightsChangeListener(HighlightsChangeListener listener) {
+        selectionsBag.addHighlightsChangeListener(listener);
+    }
 
-            return new SimpleHighlightsSequence(
-                Math.max(blockStart.getOffset(), startOffset), 
-                Math.min(blockEnd.getOffset(), endOffset), 
-                getAttribs()
-            );
-        } else {
-            return HighlightsSequence.EMPTY;
-        }
+    @Override
+    public void removeHighlightsChangeListener(HighlightsChangeListener listener) {
+        selectionsBag.removeHighlightsChangeListener(listener);
     }
 
     // ------------------------------------------------
@@ -215,7 +204,7 @@ public abstract class CaretBasedBlockHighlighting extends AbstractHighlightsCont
         updateLineInfo(true);
     }
 
-    protected abstract Position [] getCurrentBlockPositions(Document document);
+    protected abstract PositionsBag getCurrentBlockPositions(Document document);
     
     // ------------------------------------------------
     // private implementation
@@ -224,78 +213,12 @@ public abstract class CaretBasedBlockHighlighting extends AbstractHighlightsCont
     private final void updateLineInfo(boolean fire) {
         ((AbstractDocument) component.getDocument()).readLock();
         try {
-            Position newStart;
-            Position newEnd;
-            Position changeStart;
-            Position changeEnd;
-            Position[] newBlock = getCurrentBlockPositions(component.getDocument());
+            PositionsBag newPositions = getCurrentBlockPositions(component.getDocument());
             synchronized (this) {
-                if (newBlock != null) {
-                    newStart = newBlock[0];
-                    newEnd = newBlock[1];
-                    if (currentBlockStart == null) { // not valid yet
-                        if (newStart.getOffset() < newEnd.getOffset()) { // Valid non-empty block
-                            changeStart = newStart;
-                            changeEnd = newEnd;
-                        } else {
-                            changeStart = null; // Invalid or empty block => no change
-                            changeEnd = null;
-                        }
-                    } else { // Valid current start and end blocks
-                        // Compare new block to old one
-                        BlockCompare compare = BlockCompare.get(
-                                newStart.getOffset(), newEnd.getOffset(),
-                                currentBlockStart.getOffset(), currentBlockEnd.getOffset());
-                        if (compare.invalidX()) { // newStart > newEnd
-                            changeStart = null; // No change
-                            changeEnd = null;
-                        } else if (compare.equal()) { // Same blocks
-                            changeStart = null; // No firing
-                            changeEnd = null;
-                        } else if (compare.equalStart()) {
-                            if (compare.containsStrict()) {
-                                changeStart = currentBlockEnd;
-                                changeEnd = newEnd;
-                            } else {
-                                assert (compare.insideStrict());
-                                changeStart = newEnd;
-                                changeEnd = currentBlockEnd;
-                            }
-                        } else if (compare.equalEnd()) {
-                            if (compare.containsStrict()) {
-                                changeStart = newStart;
-                                changeEnd = currentBlockStart;
-                            } else {
-                                assert (compare.insideStrict());
-                                changeStart = currentBlockStart;
-                                changeEnd = newStart;
-                            }
-                        } else {
-                            changeStart = (compare.lowerStart()) ? newStart : currentBlockStart;
-                            changeEnd = (compare.lowerEnd()) ? currentBlockEnd : newEnd;
-                        }
-                    }
-
+                if (newPositions != null) {
+                    selectionsBag.setHighlights(newPositions);
                 } else { // newBlock is null => selection removed
-                    newStart = null;
-                    newEnd = null;
-                    changeStart = currentBlockStart;
-                    changeEnd = currentBlockEnd;
-                }
-
-                if (LOG.isLoggable(Level.FINE)) {
-                    LOG.fine("Caret selection block changed from [" //NOI18N
-                            + positionToString(currentBlockStart) + ", " + positionToString(currentBlockEnd) + "] to [" //NOI18N
-                            + positionToString(newStart) + ", " + positionToString(newEnd) + "]" //NOI18N
-                            + ", layer=" + s2s(this) + '\n'); //NOI18N
-                }
-                currentBlockStart = newStart;
-                currentBlockEnd = newEnd;
-            }
-
-            if (changeStart != null) {
-                if (fire) {
-                    fireHighlightsChange(changeStart.getOffset(), changeEnd.getOffset());
+                    selectionsBag.clear();
                 }
             }
         } finally {
@@ -356,46 +279,6 @@ public abstract class CaretBasedBlockHighlighting extends AbstractHighlightsCont
     private static String s2s(Object o) {
         return o == null ? "null" : o.getClass().getSimpleName() + "@" + Integer.toHexString(System.identityHashCode(o)); //NOI18N
     }
-
-    public static final class SimpleHighlightsSequence implements HighlightsSequence {
-        
-        private int startOffset;
-        private int endOffset;
-        private AttributeSet attribs;
-        
-        private boolean end = false;
-        
-        public SimpleHighlightsSequence(int startOffset, int endOffset, AttributeSet attribs) {
-            this.startOffset = startOffset;
-            this.endOffset = endOffset;
-            this.attribs = attribs;
-        }
-
-        @Override
-        public boolean moveNext() {
-            if (!end) {
-                end = true;
-                return true;
-            } else {
-                return false;
-            }
-        }
-
-        @Override
-        public int getStartOffset() {
-            return startOffset;
-        }
-
-        @Override
-        public int getEndOffset() {
-            return endOffset;
-        }
-
-        @Override
-        public AttributeSet getAttributes() {
-            return attribs;
-        }
-    } // End of SimpleHighlightsSequence
     
     public static final class CaretRowHighlighting extends CaretBasedBlockHighlighting {
         
@@ -405,27 +288,46 @@ public abstract class CaretBasedBlockHighlighting extends AbstractHighlightsCont
             super(component, FontColorNames.CARET_ROW_COLORING, true, false);
         }
         
-        protected Position[] getCurrentBlockPositions(Document document) {
+        @Override
+        protected PositionsBag getCurrentBlockPositions(Document document) {
             Caret caret = caret();
+            PositionsBag selections = null;
             if (document != null && caret != null) {
-                int caretOffset = caret.getDot();
-                try {
-                    int startOffset = DocUtils.getRowStart(document, caretOffset, 0);
-                    int endOffset = DocUtils.getRowEnd(document, caretOffset);
+                selections = new PositionsBag(document);
+                if(caret instanceof EditorCaret) {
+                    EditorCaret editorCaret = (EditorCaret) caret;
+                    List<CaretInfo> carets = editorCaret.getAllCarets();
+                    for (CaretInfo c : carets) {
+                        int caretOffset = c.getDot();
+                        try {
+                            int startOffset = DocUtils.getRowStart(document, caretOffset, 0);
+                            int endOffset = DocUtils.getRowEnd(document, caretOffset);
+                            // include the new-line character or the end of the document
+                            endOffset++;
+                            Position startPos = document.createPosition(startOffset);
+                            Position endPos = document.createPosition(endOffset);
+                            selections.addHighlight(startPos, endPos, getAttribs());
+                        } catch(BadLocationException e) {
+                            LOG.log(Level.WARNING, e.getMessage(), e);
+                        }
+                    }
+                } else {
+                    int caretOffset = caret.getDot();
+                    try {
+                        int startOffset = DocUtils.getRowStart(document, caretOffset, 0);
+                        int endOffset = DocUtils.getRowEnd(document, caretOffset);
 
-                    // include the new-line character or the end of the document
-                    endOffset++;
-
-                    return new Position [] {
-                        document.createPosition(startOffset),
-                        document.createPosition(endOffset),
-                    };
-                } catch (BadLocationException e) {
-                    LOG.log(Level.WARNING, e.getMessage(), e);
+                        // include the new-line character or the end of the document
+                        endOffset++;
+                        Position startPos = document.createPosition(startOffset);
+                        Position endPos = document.createPosition(endOffset);
+                        selections.addHighlight(startPos, endPos, getAttribs());
+                    } catch (BadLocationException e) {
+                        LOG.log(Level.WARNING, e.getMessage(), e);
+                    }
                 }
             }
-
-            return null;
+            return selections;
         }
     } // End of CaretRowHighlighting class
     
@@ -445,43 +347,62 @@ public abstract class CaretBasedBlockHighlighting extends AbstractHighlightsCont
         }
     
         @Override
-        protected Position[] getCurrentBlockPositions(Document document) {
+        protected PositionsBag getCurrentBlockPositions(Document document) {
             Caret caret = caret();
+            PositionsBag selections = null;
             if (document != null && caret != null) {
-                int caretOffset = caret.getDot();
-                int markOffset = caret.getMark();
+                selections = new PositionsBag(document);
+                if(caret instanceof EditorCaret) {
+                    EditorCaret editorCaret = (EditorCaret) caret;
+                    for (CaretInfo caretInfo : editorCaret.getAllCarets()) {
+                        int caretOffset = caretInfo.getDot();
+                        int markOffset = caretInfo.getMark();
 
-                if (caretOffset != markOffset) {
-                    try {
-                        return new Position [] {
-                            document.createPosition(Math.min(caretOffset, markOffset)),
-                            document.createPosition(Math.max(caretOffset, markOffset)),
-                        };
-                    } catch (BadLocationException e) {
-                        LOG.log(Level.WARNING, e.getMessage(), e);
+                        if (caretOffset != markOffset) {
+                            try {
+                                Position start = document.createPosition(Math.min(caretOffset, markOffset));
+                                Position end = document.createPosition(Math.max(caretOffset, markOffset));
+                                selections.addHighlight(start, end, getAttribs());
+                            } catch (BadLocationException e) {
+                                LOG.log(Level.WARNING, e.getMessage(), e);
+                            }
+                        }
+                    }
+                } else {
+                    int caretOffset = caret.getDot();
+                    int markOffset = caret.getMark();
+
+                    if (caretOffset != markOffset) {
+                        try {
+                            Position start = document.createPosition(Math.min(caretOffset, markOffset));
+                            Position end = document.createPosition(Math.max(caretOffset, markOffset));
+                            selections.addHighlight(start, end, getAttribs());
+                        } catch (BadLocationException e) {
+                            LOG.log(Level.WARNING, e.getMessage(), e);
+                        }
                     }
                 }
             }
             
-            return null;
+            return selections;
         }
 
-        @Override
-        public HighlightsSequence getHighlights(int startOffset, int endOffset) {
-            if (!RectangularSelectionUtils.isRectangularSelection(component())) { // regular selection
-                return super.getHighlights(startOffset, endOffset);
-            } else { // rectangular selection
-                return (rectangularSelectionBag != null)
-                        ? (rectangularSelectionBag.getHighlights(startOffset, endOffset))
-                        : HighlightsSequence.EMPTY;
-            }
-        }
+//        @Override
+//        public HighlightsSequence getHighlights(int startOffset, int endOffset) {
+//            if (!RectangularSelectionUtils.isRectangularSelection(component())) { // regular selection
+//                return super.getHighlights(startOffset, endOffset);
+//            } else { // rectangular selection
+//                return (rectangularSelectionBag != null)
+//                        ? (rectangularSelectionBag.getHighlights(startOffset, endOffset))
+//                        : HighlightsSequence.EMPTY;
+//            }
+//        }
 
         @Override
         public void propertyChange(PropertyChangeEvent evt) {
             super.propertyChange(evt);
             if (RectangularSelectionUtils.getRectangularSelectionProperty().equals(evt.getPropertyName())) {
-                fireHighlightsChange(0, component().getDocument().getLength());
+//                fireHighlightsChange(0, component().getDocument().getLength());
             }
         }
 
@@ -508,7 +429,7 @@ public abstract class CaretBasedBlockHighlighting extends AbstractHighlightsCont
                     }
                     // Fire change at once
                     if (hlChangeStartOffset != -1) {
-                        fireHighlightsChange(hlChangeStartOffset, hlChangeEndOffset);
+//                        fireHighlightsChange(hlChangeStartOffset, hlChangeEndOffset);
                         hlChangeStartOffset = -1;
                     }
                 }
