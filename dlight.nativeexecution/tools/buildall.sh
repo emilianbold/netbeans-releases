38a03a65d83c71224b953cbd1828dccb17348e02
Ilia Gromov <ilia@netbeans.org>
2015-12-01 18:14:42 +0400

    additional fix Bug #256898 - dlight.nativeexecution Makefile support for different compilers/flags (transplanted from a167ece411eb90b0d8806fee9dd99cadb5aeff75)

--- dlight.nativeexecution/tools/buildall.sh
+++ dlight.nativeexecution/tools/buildall.sh
@@ -1,20 +1,59 @@
-64BITS=64
+#/bin/bash
 
-sh build.sh COMPILER=~/demo/sources/solarisstudiodev/bin/cc CFLAGS_EXTRA="-xinstrument=datarace" CF_COMMON=""
+if [ "$1" = "--solaris" ]; then
+	SOLARIS=1
+fi
+
+MAKE=`which gmake || which make`
+
+if [ "x$SOLARIS" != "x" ]; then
+	DISCOVER=/path/to/discover
+	args='CC=/path/to/cc NOSTRIP=1 CFLAGS_EXTRA=-O3 64BITS=64 CF_COMMON=""'
+else
+	CC=gcc
+	args='64BITS=64'
+fi
+
+sh build.sh clean
 
 cd pty
-gmake CC=~/demo/sources/solarisstudiodev/bin/cc CFLAGS_EXTRA="-xinstrument=datarace" CF_COMMON="" 64BITS="64"
+rm -rf dist
 cd ..
 
 cd killall
-gmake CC=~/demo/sources/solarisstudiodev/bin/cc CFLAGS_EXTRA="-xinstrument=datarace" CF_COMMON="" 64BITS="64"
+rm -rf dist
 cd ..
 
 cd unbuffer
-gmake CC=~/demo/sources/solarisstudiodev/bin/cc CFLAGS_EXTRA="-xinstrument=datarace" CF_COMMON="" 64BITS="64"
+rm -rf dist
 cd ..
 
-BUILD_ALL=buildall/
+
+sh build.sh $args
+
+cd pty
+${MAKE} $args
+cd ..
+
+cd killall
+${MAKE} $args
+cd ..
+
+cd unbuffer
+${MAKE} $args
+cd ..
+
+BUILD_ALL=buildall
 
 mkdir -p $BUILD_ALL
-find ../release/bin/nativeexecution/ unbuffer/dist/ pty/dist killall/dist -type f -exec cp {} $BUILD_ALL \;
+cd $BUILD_ALL
+rm -f *
+cd ..
+
+find "../release/bin/nativeexecution/" "unbuffer/dist/" "pty/dist" "killall/dist" -not -name "*.sh" -type f -exec cp {} $BUILD_ALL \;
+
+if [ "x$SOLARIS" != "x" ]; then
+	find "$BUILD_ALL" -type f -exec sh -c "$DISCOVER"' -v -w $0.%p.txt $0' {} \;
+fi
+
+find $BUILD_ALL -type f | xargs file
