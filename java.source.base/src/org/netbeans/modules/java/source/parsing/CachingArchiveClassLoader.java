e5db8ad05da931834bef615ce066f9f287d47f3d
Tomas Zezula <tzezula@netbeans.org>
2015-12-07 15:07:21 +0100

    Disabling lmf for now.

--- java.source.base/src/org/netbeans/modules/java/source/parsing/CachingArchiveClassLoader.java
+++ java.source.base/src/org/netbeans/modules/java/source/parsing/CachingArchiveClassLoader.java
@@ -106,7 +106,10 @@ public final class CachingArchiveClassLoader extends ClassLoader {
                             }
                             return defineClass(
                                     name,
-                                    com.sun.tools.hc.LambdaMetafactory.translateClassFile(buffer,0,len),
+                                    buffer,
+                                    //Todo:
+                                    //-buffer
+                                    //+com.sun.tools.hc.LambdaMetafactory.translateClassFile(buffer,0,len),
                                     0,
                                     len);
                         } catch (FileNotFoundException fnf) {
