5897198f0e0ced2e3965590e79b30d61f55690cf
Vladimir Kvashin <vkvashin@netbeans.org>
2015-12-28 17:02:59 +0300

    additional fix for #256919 - fix issues discovered in rfs_controller and rfs_preload - source changes

--- dlight.remote.impl/tools/fs_server/src/fs_server.c
+++ dlight.remote.impl/tools/fs_server/src/fs_server.c
@@ -90,7 +90,7 @@ static int refresh_sleep = 1;
 
 #define FS_SERVER_MAJOR_VERSION 1
 #define FS_SERVER_MID_VERSION 7
-#define FS_SERVER_MINOR_VERSION 5
+#define FS_SERVER_MINOR_VERSION 6
 
 typedef struct fs_entry {
     int /*short?*/ name_len;
