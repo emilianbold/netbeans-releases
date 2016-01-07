b06526d1916f422f909e64ceef1c70b3997419cc
Tomas Zezula <tzezula@netbeans.org>
2016-01-07 14:36:54 +0100

    Comment is valid even on line continuation.

--- docker.editor/src/org/netbeans/modules/docker/editor/lexer/DockerfileLexer.java
+++ docker.editor/src/org/netbeans/modules/docker/editor/lexer/DockerfileLexer.java
@@ -65,7 +65,8 @@ import static org.netbeans.spi.lexer.LexerInput.EOF;
  */
 final class DockerfileLexer implements Lexer<DockerfileTokenId> {
     private static final int STATE_NEW_LINE = 0;
-    private static final int STATE_ONBUILD = STATE_NEW_LINE + 1;
+    private static final int STATE_CONT_LINE = STATE_NEW_LINE + 1;
+    private static final int STATE_ONBUILD = STATE_CONT_LINE + 1;
     private static final int STATE_REST = STATE_ONBUILD + 1;
     private static final int STATE_ESCAPE = STATE_REST + 1;
 
@@ -114,9 +115,10 @@ final class DockerfileLexer implements Lexer<DockerfileTokenId> {
             switch (c) {
                 case '\r':
                 case '\n':
-                    if (currentState != STATE_ESCAPE) {
-                        newStateHolder[0] = STATE_NEW_LINE;
-                    }
+                    newStateHolder[0] =
+                            currentState == STATE_ESCAPE ?
+                            STATE_CONT_LINE :
+                            STATE_NEW_LINE;
                 case '\t':
                 case 0x0b:
                 case '\f':
@@ -147,7 +149,8 @@ final class DockerfileLexer implements Lexer<DockerfileTokenId> {
                     newStateHolder[0] = STATE_REST;
                     return token(DockerfileTokenId.COMMA);
                 case '#':   //NOI18N
-                    if (currentState == STATE_NEW_LINE) {
+                    if (currentState == STATE_NEW_LINE ||
+                        currentState == STATE_CONT_LINE) {
                         while (true) {
                             switch (nextChar()) {
                                 case '\r': consumeNewline();
@@ -251,9 +254,9 @@ final class DockerfileLexer implements Lexer<DockerfileTokenId> {
             switch (c) {
                 case '\r':
                 case '\n':
-                    if (currentState != STATE_ESCAPE) {
-                        stateHolder[0] = STATE_NEW_LINE;
-                    }
+                    stateHolder[0] = currentState == STATE_ESCAPE ?
+                            STATE_CONT_LINE:
+                            STATE_NEW_LINE;
                     break;
                 case '\t':
                 case 0x0b:
@@ -363,7 +366,7 @@ final class DockerfileLexer implements Lexer<DockerfileTokenId> {
 
     private static boolean isIdentifierStart(final int c, final int currentState) {
         return Character.isJavaIdentifierStart(c) ||
-            (currentState != STATE_NEW_LINE && c == '#');   //NOI18N
+            (c == '#' && currentState != STATE_NEW_LINE && currentState != STATE_CONT_LINE);   //NOI18N
     }
 
     private static boolean isIdentifierPart(final int c, int currentState) {
@@ -371,7 +374,7 @@ final class DockerfileLexer implements Lexer<DockerfileTokenId> {
                 c == '.' || //NOI18N
                 c == '-' || //NOI18N
                 c == '_' || //NOI18N
-                (currentState != STATE_NEW_LINE && c == '#');   //NOI18N
+                (c == '#' && currentState != STATE_NEW_LINE && currentState != STATE_CONT_LINE);   //NOI18N
     }
 
     private static boolean isSpaceOnLine(int c) {
