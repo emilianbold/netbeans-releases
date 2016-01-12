868b33cf6aa8f62637a2863b8e7307a54230385e
Petr Kudryavtsev <petrk@netbeans.org>
2016-01-12 16:07:34 +0300

    Fixed #257028 - std::locale and unresolved identifier

---
+++ cnd.completion/test/unit/data/org/netbeans/modules/cnd/completion/cplusplus/hyperlink/LibrariesContentHyperlinkTestCase/src/bug257028.cpp
@@ -0,0 +1,11 @@
+#include <bug257028_1.h>
+#include <bug257028_2.h>
+
+namespace bug257028 {
+    using namespace outer257028;
+
+    void foo257028() {
+        BBB257028 b;
+        b.boo().foo();
+    }  
+}
