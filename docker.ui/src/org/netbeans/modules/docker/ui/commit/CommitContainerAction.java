996b6e34bb90344a3e9f9a366d1b986601d3a97c
Petr Hejl <phejl@netbeans.org>
2016-02-01 15:16:54 +0100

    Report errors to user.

--- docker.ui/src/org/netbeans/modules/docker/ui/commit/CommitContainerAction.java
+++ docker.ui/src/org/netbeans/modules/docker/ui/commit/CommitContainerAction.java
@@ -51,6 +51,7 @@ import org.netbeans.modules.docker.api.DockerException;
 import org.netbeans.modules.docker.api.DockerAction;
 import org.openide.DialogDescriptor;
 import org.openide.DialogDisplayer;
+import org.openide.NotifyDescriptor;
 import org.openide.awt.Mnemonics;
 import org.openide.nodes.Node;
 import org.openide.util.HelpCtx;
@@ -118,8 +119,10 @@ public class CommitContainerAction extends NodeAction {
                     DockerAction facade = new DockerAction(container.getInstance());
                     facade.commit(container, repository, tag, author, message, pause);
                 } catch (DockerException ex) {
-                    // FIXME inform user
                     LOGGER.log(Level.INFO, null, ex);
+                    String msg = ex.getLocalizedMessage();
+                    NotifyDescriptor desc = new NotifyDescriptor.Message(msg, NotifyDescriptor.ERROR_MESSAGE);
+                    DialogDisplayer.getDefault().notify(desc);
                 } finally {
                     handle.finish();
                 }
