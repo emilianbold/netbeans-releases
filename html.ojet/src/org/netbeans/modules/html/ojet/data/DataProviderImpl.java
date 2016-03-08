026f95281994b364a64138ec6fa38663cf68f745
Petr Pisl <ppisl@netbeans.org>
2016-03-08 15:23:09 +0100

    Updating OJET Documentation for version 2.0.0

--- html.ojet/src/org/netbeans/modules/html/ojet/data/DataProviderImpl.java
+++ html.ojet/src/org/netbeans/modules/html/ojet/data/DataProviderImpl.java
@@ -62,7 +62,7 @@ public class DataProviderImpl extends DataProvider {
     private static final String zipFolder = "docs";
     private static final String ZIP_PREFIX = "ojetdocs-";
     private static final String ZIP_EXTENSION = ".zip";
-    protected static final String DEFAULT_VERSION = "1.1.2";
+    protected static final String DEFAULT_VERSION = "2.0.0";
     private static final HashMap<String, DataItemImpl.DataItemComponent> data = new HashMap();
     private static FileObject docRoot = null;
     private static String currentVersion;
