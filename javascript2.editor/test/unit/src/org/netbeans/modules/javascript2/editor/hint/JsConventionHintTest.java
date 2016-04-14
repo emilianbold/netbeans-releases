b2ec5e2798c5b2f11b30f83d34f477ea6c6852fb
Petr Hejl <phejl@netbeans.org>
2016-04-14 11:29:36 +0200

    Adding test case.

--- javascript2.editor/test/unit/src/org/netbeans/modules/javascript2/editor/hint/JsConventionHintTest.java
+++ javascript2.editor/test/unit/src/org/netbeans/modules/javascript2/editor/hint/JsConventionHintTest.java
@@ -76,6 +76,10 @@ public class JsConventionHintTest extends HintTestBase {
         return new DuplicatePropertyName();
     }
     
+    public void testClassDeclaration01() throws Exception {
+        checkHints(this, createSemicolonHint(), "testfiles/hints/classDeclaration.js", null);
+    }
+    
     public void testSemicolonAssignment() throws Exception {
         checkHints(this, createSemicolonHint(), "testfiles/coloring/assignments01.js", null);
     }
