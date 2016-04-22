4f847d6b4e42c74b7b873383bb9dde6b3a0670a7
Nikolay Koldunov <henk89@netbeans.org>
2016-04-22 18:03:56 +0300

    Fixed #23153208 - Pin watches support for DBX

---
+++ cnd.debugger.dbx/src/org/netbeans/modules/cnd/debugger/dbx/DbxPinWatchValueProvider.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.netbeans.modules.cnd.debugger.dbx;
+
+import org.netbeans.modules.cnd.debugger.common2.debugger.NativePinWatchValueProvider;
+import org.netbeans.spi.debugger.ContextProvider;
+import org.netbeans.spi.debugger.DebuggerServiceRegistration;
+import org.netbeans.spi.debugger.ui.PinWatchUISupport;
+
+/**
+ *
+ * @author Nikolay Koldunov
+ */
+@DebuggerServiceRegistration(path = "netbeans-DbxDebuggerEngine", types = PinWatchUISupport.ValueProvider.class)
+public class DbxPinWatchValueProvider extends NativePinWatchValueProvider {
+
+    public DbxPinWatchValueProvider(ContextProvider lookupProvider) {
+        super(lookupProvider);
+    }
+}
