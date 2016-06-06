d3e2026e3114fae54923f3918146955779f4c5a3
Tomas Stupka <tstupka@netbeans.org>
2016-06-06 18:12:15 +0200

    Added copyright headers. [16]

--- performance/sparrow/src/org/netbeans/performance/spi/AbstractLogFile.java
+++ performance/sparrow/src/org/netbeans/performance/spi/AbstractLogFile.java
@@ -1,4 +1,43 @@
 /*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2002, 2016 Oracle and/or its affiliates. All rights reserved.
+ *
+ * Oracle and Java are registered trademarks of Oracle and/or its affiliates.
+ * Other names may be trademarks of their respective owners.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ *
+ * Contributor(s):
+ */
+/*
  * AbstractLogFile.java
  *
  * Created on October 8, 2002, 12:57 PM
@@ -9,7 +48,7 @@ import java.io.*;
 import org.netbeans.performance.spi.*;
 import java.util.Iterator;
 /** Convenience implementation of LogFile which extends AbstractDataAggregation.
- *  Subclassers should be able to simply implement parse(), processValue() and 
+ *  Subclassers should be able to simply implement parse(), processValue() and
  *  call addElement() to add new LogElements created during the parse process.
  *
  * @author  Tim Boudreau
@@ -17,12 +56,12 @@ import java.util.Iterator;
 public abstract class AbstractLogFile extends AbstractDataAggregation implements LogFile, Named {
     private String filename;
     /**Temporary variable for the full text of the file.  Implementors should
-     * null this value at the end of the parse() method, since it can be a 
+     * null this value at the end of the parse() method, since it can be a
      * large amount of data. */
     protected String fullText=null;
     /**Date format used by the Ant script to log times.*/
     public static final String NBLOG_DATEFORMAT = "yyyy.MM.dd hh:mm:ss:S z";
-    
+
     /** Creates a new instance of AbstractLogFile */
     protected AbstractLogFile(String filename) throws DataNotFoundException {
         this.filename = filename;
