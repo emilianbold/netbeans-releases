42ea53635087ebfb64a162b80602dd4eaecaafc6
Tomas Mysik <tmysik@netbeans.org>
2016-06-09 07:47:51 +0200

    Add template for PHP trait

---
+++ php.project/src/org/netbeans/modules/php/project/ui/resources/emptyTraitPHP.php
@@ -0,0 +1,17 @@
+<?php
+<#assign licenseFirst = "/* ">
+<#assign licensePrefix = " * ">
+<#assign licenseLast = " */">
+<#include "${project.licensePath}">
+
+<#if namespace?? && namespace?length &gt; 0>
+namespace ${namespace};
+</#if>
+
+/**
+ *
+ * @author ${user}
+ */
+trait ${name} {
+    //put your code here
+}
