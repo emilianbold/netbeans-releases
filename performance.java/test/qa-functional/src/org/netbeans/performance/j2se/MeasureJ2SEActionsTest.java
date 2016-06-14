23da54c6802c7e356c196bf8ce2a183ff797863d
Tomas Stupka <tstupka@netbeans.org>
2016-06-14 16:33:46 +0200

    fixed typo in license

--- performance.java/test/qa-functional/src/org/netbeans/performance/j2se/MeasureJ2SEActionsTest.java
+++ performance.java/test/qa-functional/src/org/netbeans/performance/j2se/MeasureJ2SEActionsTest.java
@@ -11,7 +11,7 @@
  * Development and Distribution License("CDDL") (collectively, the
  * "License"). You may not use this file except in compliance with the
  * License. You can obtain a copy of the License at
- * http:www.netbeans.org/cddl-gplv2.html
+ * http://www.netbeans.org/cddl-gplv2.html
  * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
  * specific language governing permissions and limitations under the
  * License.  When distributing the software, include this License Header
