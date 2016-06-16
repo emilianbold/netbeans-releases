20381a930c80e26ecae5edb1b538a27c5a9eaa0a
Tomas Stupka <tstupka@netbeans.org>
2016-06-16 16:49:07 +0200

    fixed minor typos in license headers

--- web.jsf.navigation/src/org/netbeans/modules/web/jsf/navigation/graph/actions/LinkCreateProvider.java
+++ web.jsf.navigation/src/org/netbeans/modules/web/jsf/navigation/graph/actions/LinkCreateProvider.java
@@ -40,7 +40,8 @@
  * However, if you add GPL Version 2 code and therefore, elected the GPL
  * Version 2 license, then the option applies only if the new code is
  * made subject to such option by the copyright holder.
- *
+ */
+/*
  * LinkCreateProvider.java
  *
  * Created on January 29, 2007, 12:55 PM
