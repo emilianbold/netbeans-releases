628a0c6f50bfc2a321da8a7b4934aaab96b53804
Dusan Balek <dbalek@netbeans.org>
2016-06-30 15:05:42 +0200

    Issue #258473 - Incorrect code completion for Outer.this with Prefer Static Imports - fixed.

--- java.editor/src/org/netbeans/modules/editor/java/JavaCompletionItem.java
+++ java.editor/src/org/netbeans/modules/editor/java/JavaCompletionItem.java
@@ -3390,7 +3390,7 @@ public abstract class JavaCompletionItem implements CompletionItem {
                                 if (cancel.get()) {
                                     return;
                                 }
-                                if (CodeStyle.getDefault(doc).preferStaticImports()) {
+                                if (CodeStyle.getDefault(doc).preferStaticImports() && !"this".equals(memberName) && !"super".equals(memberName)) {
                                     Element e = getElementHandle().resolve(copy);
                                     if (e != null) {
                                         copy.rewrite(copy.getCompilationUnit(), GeneratorUtilities.get(copy).addImports(copy.getCompilationUnit(), Collections.singleton(e)));
