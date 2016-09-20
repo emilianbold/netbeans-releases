75918fd0b358444cd2001f3dff066dda42c0a9ce
Tomas Zezula <tzezula@netbeans.org>
2016-09-20 14:52:59 +0200

    Do not call JavaRunner when project has custom update.

--- java.api.common/src/org/netbeans/modules/java/api/common/project/BaseActionProvider.java
+++ java.api.common/src/org/netbeans/modules/java/api/common/project/BaseActionProvider.java
@@ -245,6 +245,10 @@ public abstract class BaseActionProvider implements ActionProvider {
     abstract protected Set<String> getJavaModelActions();
 
     abstract protected boolean isCompileOnSaveEnabled();
+    
+    protected boolean isCompileOnSaveUpdate() {
+        return isCompileOnSaveEnabled();
+    }
 
     protected void setServerExecution(boolean serverExecution) {
         this.serverExecution = serverExecution;
@@ -432,6 +436,7 @@ public abstract class BaseActionProvider implements ActionProvider {
         final String[] userPropertiesFile = new String[]{verifyUserPropertiesFile()};
 
         final boolean isCompileOnSaveEnabled = isCompileOnSaveEnabled();
+        final boolean isCompileOnSaveUpdate = isCompileOnSaveUpdate();
         final AtomicReference<Thread> caller = new AtomicReference<Thread>(Thread.currentThread());
         final AtomicBoolean called = new AtomicBoolean(false);
         // XXX prefer to call just if and when actually starting target, but that is hard to calculate here
@@ -604,7 +609,7 @@ public abstract class BaseActionProvider implements ActionProvider {
                 if (targetNames.length == 0) {
                     targetNames = null;
                 }
-                if (isCompileOnSaveEnabled && !NO_SYNC_COMMANDS.contains(command2execute)) {
+                if (isCompileOnSaveUpdate && !NO_SYNC_COMMANDS.contains(command2execute)) {
                     p.put("nb.wait.for.caches", "true");
                 }
                 final Callback cb = getCallback();
@@ -661,7 +666,7 @@ public abstract class BaseActionProvider implements ActionProvider {
         }
         final Action action = new Action();
 
-        if (getJavaModelActions().contains(command) || (isCompileOnSaveEnabled && getScanSensitiveActions().contains(command))) {
+        if (getJavaModelActions().contains(command) || (isCompileOnSaveUpdate && getScanSensitiveActions().contains(command))) {
             //Always have to run with java model
             ScanDialog.runWhenScanFinished(action, commandName(command));
         }
@@ -1502,7 +1507,7 @@ public abstract class BaseActionProvider implements ActionProvider {
             return true;
         }   
         if (   Arrays.asList(getActionsDisabledForQuickRun()).contains(command)
-            && isCompileOnSaveEnabled()
+            && isCompileOnSaveUpdate()
             && !allowAntBuild()) {
             return false;
         }
