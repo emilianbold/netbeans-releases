4c0b7231fbef918a95ca74d2ca16193feae60cdd
Tomas Zezula <tzezula@netbeans.org>
2016-11-02 18:52:54 +0100

    Exact PFM.C.E.hasLocation

--- java.source.base/src/org/netbeans/modules/java/source/parsing/ProxyFileManager.java
+++ java.source.base/src/org/netbeans/modules/java/source/parsing/ProxyFileManager.java
@@ -57,7 +57,6 @@ import java.util.ConcurrentModificationException;
 import java.util.HashMap;
 import java.util.IdentityHashMap;
 import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
@@ -984,9 +983,6 @@ public final class ProxyFileManager implements JavaFileManager {
             }
 
             boolean hasLocation() {
-                if (filter == null) {
-                    return true;
-                }
                 return get().length > 0;
             }
 
