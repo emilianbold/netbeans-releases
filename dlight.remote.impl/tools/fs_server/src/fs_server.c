2d2562efe98a9d2fc26be953154c19b43a194947
Vladimir Kvashin <vkvashin@netbeans.org>
2016-12-01 02:00:13 +0300

    Fixing tests (#268926 - Remote File System should support DeleteOnExit) - don't truncate error log - fs_server source changes

--- dlight.remote.impl/tools/fs_server/src/fs_server.c
+++ dlight.remote.impl/tools/fs_server/src/fs_server.c
@@ -99,7 +99,7 @@ bool redirect_err_flag = false;
 
 #define FS_SERVER_MAJOR_VERSION 1
 #define FS_SERVER_MID_VERSION 12
-#define FS_SERVER_MINOR_VERSION 4
+#define FS_SERVER_MINOR_VERSION 5
 
 typedef struct fs_entry {
     int /*short?*/ name_len;
@@ -2087,21 +2087,21 @@ static void shutdown() {
 static void log_header(int argc, char* argv[]) {
     if (log_flag) {
         log_open("log") ;
-        log_print("\n--------------------------------------\nfs_server version %d.%d.%d (%s %s) started on ",
+        log_and_err_print("\n--------------------------------------\nfs_server version %d.%d.%d (%s %s) started on ",
                 FS_SERVER_MAJOR_VERSION, FS_SERVER_MID_VERSION, FS_SERVER_MINOR_VERSION, __DATE__, __TIME__);
         time_t t = time(NULL);
         struct tm *tt = localtime(&t);
         if (tt) {
-            log_print("%d/%02d/%02d at %02d:%02d:%02d\n",
+            log_and_err_print("%d/%02d/%02d at %02d:%02d:%02d\n",
                     tt->tm_year+1900, tt->tm_mon + 1, tt->tm_mday,
                     tt->tm_hour, tt->tm_min, tt->tm_sec);
         } else {
-            log_print("<error getting time: %s>\n", strerror(errno));
+            log_and_err_print("<error getting time: %s>\n", strerror(errno));
         }
         for (int i = 0; i < argc; i++) {
-            log_print("%s ", argv[i]);
+            log_and_err_print("%s ", argv[i]);
         }
-        log_print("\n");
+        log_and_err_print("\n");
     }
 }
 
