cf90f2299e7c09a75f392dee7e48fa6cf423a8c6
PGebauer <pgebauer@netbeans.org>
2016-11-30 16:45:57 +0100

    Mac OS X installers swithed off temporarily.

--- nbbuild/newbuild/build-nbi.sh
+++ nbbuild/newbuild/build-nbi.sh
@@ -58,52 +58,52 @@ fi
 OUTPUT_DIR="$DIST/installers"
 export OUTPUT_DIR
 
-if [ ! -z $NATIVE_MAC_MACHINE ] && [ ! -z $MAC_PATH ]; then
-   ssh $NATIVE_MAC_MACHINE rm -rf $MAC_PATH/installer
-   ERROR_CODE=$?
-   if [ $ERROR_CODE != 0 ]; then
-       echo "ERROR: $ERROR_CODE - Connection to MAC machine $NATIVE_MAC_MACHINE failed, can't remove old scripts"
-       exit $ERROR_CODE;
-   fi
-   ssh $NATIVE_MAC_MACHINE mkdir -p $MAC_PATH/installer
-   cd $NB_ALL
-   gtar c installer/mac | ssh $NATIVE_MAC_MACHINE "( cd $MAC_PATH; tar x )"
+# if [ ! -z $NATIVE_MAC_MACHINE ] && [ ! -z $MAC_PATH ]; then
+#    ssh $NATIVE_MAC_MACHINE rm -rf $MAC_PATH/installer
+#    ERROR_CODE=$?
+#    if [ $ERROR_CODE != 0 ]; then
+#        echo "ERROR: $ERROR_CODE - Connection to MAC machine $NATIVE_MAC_MACHINE failed, can't remove old scripts"
+#        exit $ERROR_CODE;
+#    fi
+#    ssh $NATIVE_MAC_MACHINE mkdir -p $MAC_PATH/installer
+#    cd $NB_ALL
+#    gtar c installer/mac | ssh $NATIVE_MAC_MACHINE "( cd $MAC_PATH; tar x )"
+# 
+#     cd $NB_ALL/l10n
+#     gtar c src/*/other/installer/mac/* | ssh $NATIVE_MAC_MACHINE "( cd $MAC_PATH; tar x)"
+#     cd $NB_ALL
 
-    cd $NB_ALL/l10n
-    gtar c src/*/other/installer/mac/* | ssh $NATIVE_MAC_MACHINE "( cd $MAC_PATH; tar x)"
-    cd $NB_ALL
-
-   ssh $NATIVE_MAC_MACHINE rm -rf $MAC_PATH/zip/* 
-   ERROR_CODE=$?
-   if [ $ERROR_CODE != 0 ]; then
-       echo "ERROR: $ERROR_CODE - Connection to MAC machine $NATIVE_MAC_MACHINE failed, can't remove old bits"
-       exit $ERROR_CODE;
-   fi
-   ssh $NATIVE_MAC_MACHINE mkdir -p $MAC_PATH/zip/moduleclusters
-   ls $DIST/zip/moduleclusters | grep -v "all-in-one" | xargs -I {} scp -q -v $DIST/zip/moduleclusters/{} $NATIVE_MAC_MACHINE:$MAC_PATH/zip/moduleclusters/
-
-   ERROR_CODE=$?
-   if [ $ERROR_CODE != 0 ]; then
-       echo "ERROR: $ERROR_CODE - Connection to MAC machine $NATIVE_MAC_MACHINE failed, can't put the zips"
-       exit $ERROR_CODE;
-   fi
-
-   # Run new builds
-   sh $NB_ALL/installer/mac/newbuild/init.sh | ssh $NATIVE_MAC_MACHINE "cat > $MAC_PATH/installer/mac/newbuild/build-private.sh"
-   ssh $NATIVE_MAC_MACHINE chmod a+x $MAC_PATH/installer/mac/newbuild/build.sh
-
-   if [ ! -z $SIGNING_PASSWORD ] ; then
-       UNLOCK_COMMAND="security unlock-keychain -p $SIGNING_PASSWORD ;"
-   else
-       SIGNING_IDENTITY=0
-   fi
-
-   BASE_COMMAND="$MAC_PATH/installer/mac/newbuild/build.sh $MAC_PATH $BASENAME_PREFIX $BUILDNUMBER $BUILD_NBJDK7 $BUILD_NBJDK8 '"$SIGNING_IDENTITY"' $LOCALES"
-   
-   ssh $NATIVE_MAC_MACHINE "$UNLOCK_COMMAND $BASE_COMMAND" > $MAC_LOG_NEW 2>&1 &
-   REMOTE_MAC_PID=$!
-
-fi
+#    ssh $NATIVE_MAC_MACHINE rm -rf $MAC_PATH/zip/* 
+#    ERROR_CODE=$?
+#    if [ $ERROR_CODE != 0 ]; then
+#        echo "ERROR: $ERROR_CODE - Connection to MAC machine $NATIVE_MAC_MACHINE failed, can't remove old bits"
+#        exit $ERROR_CODE;
+#    fi
+#    ssh $NATIVE_MAC_MACHINE mkdir -p $MAC_PATH/zip/moduleclusters
+#    ls $DIST/zip/moduleclusters | grep -v "all-in-one" | xargs -I {} scp -q -v $DIST/zip/moduleclusters/{} $NATIVE_MAC_MACHINE:$MAC_PATH/zip/moduleclusters/
+# 
+#    ERROR_CODE=$?
+#    if [ $ERROR_CODE != 0 ]; then
+#        echo "ERROR: $ERROR_CODE - Connection to MAC machine $NATIVE_MAC_MACHINE failed, can't put the zips"
+#        exit $ERROR_CODE;
+#    fi
+# 
+#    # Run new builds
+#    sh $NB_ALL/installer/mac/newbuild/init.sh | ssh $NATIVE_MAC_MACHINE "cat > $MAC_PATH/installer/mac/newbuild/build-private.sh"
+#    ssh $NATIVE_MAC_MACHINE chmod a+x $MAC_PATH/installer/mac/newbuild/build.sh
+# 
+#    if [ ! -z $SIGNING_PASSWORD ] ; then
+#        UNLOCK_COMMAND="security unlock-keychain -p $SIGNING_PASSWORD ;"
+#    else
+#        SIGNING_IDENTITY=0
+#    fi
+# 
+#    BASE_COMMAND="$MAC_PATH/installer/mac/newbuild/build.sh $MAC_PATH $BASENAME_PREFIX $BUILDNUMBER $BUILD_NBJDK7 $BUILD_NBJDK8 '"$SIGNING_IDENTITY"' $LOCALES"
+#    
+#    ssh $NATIVE_MAC_MACHINE "$UNLOCK_COMMAND $BASE_COMMAND" > $MAC_LOG_NEW 2>&1 &
+#    REMOTE_MAC_PID=$!
+# 
+# fi
 
 cd $NB_ALL/installer/infra/build
 
@@ -137,28 +137,28 @@ fi
 mv $OUTPUT_DIR/* $DIST
 rmdir $OUTPUT_DIR
 
-#Check if Mac installer was OK, 10 "BUILD SUCCESSFUL" messages should be in Mac log
-if [ ! -z $NATIVE_MAC_MACHINE ] && [ ! -z $MAC_PATH ]; then
-
-    IS_NEW_MAC_FAILED=`cat $MAC_LOG_NEW | grep "BUILD FAILED" | wc -l | tr " " "\n" | grep -v '^$'`
-    IS_NEW_MAC_CONNECT=`cat $MAC_LOG_NEW | grep "Connection timed out" | wc -l | tr " " "\n" | grep -v '^$'`
-
-    if [ $IS_NEW_MAC_FAILED -eq 0 ] && [ $IS_NEW_MAC_CONNECT -eq 0 ]; then
-        #copy the bits back
-        mkdir -p $DIST/bundles
-
-        rsync -avz -e ssh $NATIVE_MAC_MACHINE:$MAC_PATH/installer/mac/newbuild/dist_en/* $DIST/bundles
-        ERROR_CODE=$?
-        if [ $ERROR_CODE != 0 ]; then
-            echo "ERROR: $ERROR_CODE - Connection to MAC machine $NATIVE_MAC_MACHINE failed, can't get installers"
-            exit $ERROR_CODE;
-        fi
-    else
-        tail -100 $MAC_LOG_NEW
-        echo "ERROR: - Native Mac Installers build failed"
-        exit 1;
-    fi
-fi
+# #Check if Mac installer was OK, 10 "BUILD SUCCESSFUL" messages should be in Mac log
+# if [ ! -z $NATIVE_MAC_MACHINE ] && [ ! -z $MAC_PATH ]; then
+# 
+#     IS_NEW_MAC_FAILED=`cat $MAC_LOG_NEW | grep "BUILD FAILED" | wc -l | tr " " "\n" | grep -v '^$'`
+#     IS_NEW_MAC_CONNECT=`cat $MAC_LOG_NEW | grep "Connection timed out" | wc -l | tr " " "\n" | grep -v '^$'`
+# 
+#     if [ $IS_NEW_MAC_FAILED -eq 0 ] && [ $IS_NEW_MAC_CONNECT -eq 0 ]; then
+#         #copy the bits back
+#         mkdir -p $DIST/bundles
+# 
+#         rsync -avz -e ssh $NATIVE_MAC_MACHINE:$MAC_PATH/installer/mac/newbuild/dist_en/* $DIST/bundles
+#         ERROR_CODE=$?
+#         if [ $ERROR_CODE != 0 ]; then
+#             echo "ERROR: $ERROR_CODE - Connection to MAC machine $NATIVE_MAC_MACHINE failed, can't get installers"
+#             exit $ERROR_CODE;
+#         fi
+#     else
+#         tail -100 $MAC_LOG_NEW
+#         echo "ERROR: - Native Mac Installers build failed"
+#         exit 1;
+#     fi
+# fi
 
 ###################################################################
 #
