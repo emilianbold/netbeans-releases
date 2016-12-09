3050498b5d0ee3fae4fd8b34343fe637b22d2b91
Jaroslav Tulach <jtulach@netbeans.org>
2016-12-09 14:17:45 +0100

    Honor http_proxy and https_proxy environment variables

--- nbbuild/antsrc/org/netbeans/nbbuild/extlibs/DownloadBinaries.java
+++ nbbuild/antsrc/org/netbeans/nbbuild/extlibs/DownloadBinaries.java
@@ -54,12 +54,20 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.math.BigInteger;
 import java.net.HttpURLConnection;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import org.apache.tools.ant.BuildException;
 import org.apache.tools.ant.DirectoryScanner;
 import org.apache.tools.ant.Project;
@@ -282,8 +290,7 @@ public class DownloadBinaries extends Task {
     private boolean downloadFromServer(URL url, String cacheName, File destination, String expectedHash) 
     throws IOException {
         log("Downloading: " + url);
-        URLConnection conn = url.openConnection();
-        conn.connect();
+        URLConnection conn = openConnection(url);
         int code = HttpURLConnection.HTTP_OK;
         if (conn instanceof HttpURLConnection) {
             code = ((HttpURLConnection) conn).getResponseCode();
@@ -325,6 +332,72 @@ public class DownloadBinaries extends Task {
             is.close();
         }
         return true;
+    }
+
+    private URLConnection openConnection(final URL url) throws IOException {
+        final URLConnection[] conn = { null };
+        final CountDownLatch connected = new CountDownLatch(1);
+        ExecutorService connectors = Executors.newFixedThreadPool(3);
+        connectors.submit(new Runnable() {
+            public void run() {
+                String httpProxy = System.getenv("http_proxy");
+                if (httpProxy != null) {
+                    try {
+                        URI uri = new URI(httpProxy);
+                        InetSocketAddress address = InetSocketAddress.createUnresolved(uri.getHost(), uri.getPort());
+                        Proxy proxy = new Proxy(Proxy.Type.HTTP, address);
+                        URLConnection test = url.openConnection(proxy);
+                        test.connect();
+                        conn[0] = test;
+                        connected.countDown();
+                    } catch (IOException ex) {
+                        log(ex, Project.MSG_ERR);
+                    } catch (URISyntaxException ex) {
+                        log(ex, Project.MSG_ERR);
+                    }
+                }
+            }
+        });
+        connectors.submit(new Runnable() {
+            public void run() {
+                String httpProxy = System.getenv("https_proxy");
+                if (httpProxy != null) {
+                    try {
+                        URI uri = new URI(httpProxy);
+                        InetSocketAddress address = InetSocketAddress.createUnresolved(uri.getHost(), uri.getPort());
+                        Proxy proxy = new Proxy(Proxy.Type.HTTP, address);
+                        URLConnection test = url.openConnection(proxy);
+                        test.connect();
+                        conn[0] = test;
+                        connected.countDown();
+                    } catch (IOException ex) {
+                        log(ex, Project.MSG_ERR);
+                    } catch (URISyntaxException ex) {
+                        log(ex, Project.MSG_ERR);
+                    }
+                }
+            }
+        });
+        connectors.submit(new Runnable() {
+            public void run() {
+                try {
+                    URLConnection test = url.openConnection();
+                    test.connect();
+                    conn[0] = test;
+                    connected.countDown();
+                } catch (IOException ex) {
+                    log(ex, Project.MSG_ERR);
+                }
+            }
+        });
+        try {
+            connected.await(5, TimeUnit.SECONDS);
+        } catch (InterruptedException ex) {
+        }
+        if (conn[0] == null) {
+            throw new BuildException("Cannot connect to " + url);
+        }
+        return conn[0];
     }
 
     private String hash(File f) {
