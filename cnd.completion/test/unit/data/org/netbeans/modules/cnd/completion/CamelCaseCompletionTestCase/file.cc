decae5e7101a9913ed60e561dccad8f3df183035
Ilia Gromov <ilia@netbeans.org>
2017-02-01 17:07:15 +0300

    Fixed #269708 - Allow camel case completion

---
+++ cnd.completion/test/unit/data/org/netbeans/modules/cnd/completion/CamelCaseCompletionTestCase/file.cc
@@ -0,0 +1,42 @@
+void functionAaa() {
+}
+
+void functionBbb() {
+}
+
+void functionAaaExtra() {
+}
+
+//////////////////////
+
+long GetId() {
+    return 0;
+}
+
+long getIdentificator() {
+    return 0;
+}
+
+//////////////////////
+
+int add_simple(int a, int b) {
+    return a + b
+}
+
+int add_complicated(int a, int b) {
+    return 42;
+}
+
+long add_simple_long(int a, int b) {
+    return a + b;
+}
+
+void add_somethingToSomething(int a, int b) {
+}
+
+void UID(){
+}
+
+int main() {
+    // <- insert code here
+}
