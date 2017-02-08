a25c79054f0f3432f29970687a0bcb71d45c79d4
Tomas Zezula <tzezula@netbeans.org>
2017-02-08 18:23:31 +0100

    ClassPathProviderMerger propagates ClassPath flags.

--- java.project/src/org/netbeans/spi/java/project/support/ClassPathProviderMerger.java
+++ java.project/src/org/netbeans/spi/java/project/support/ClassPathProviderMerger.java
@@ -47,14 +47,18 @@ import java.beans.PropertyChangeSupport;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import org.netbeans.api.annotations.common.NonNull;
 import org.netbeans.api.java.classpath.ClassPath;
 import org.netbeans.spi.java.classpath.ClassPathFactory;
 import org.netbeans.spi.java.classpath.ClassPathImplementation;
 import org.netbeans.spi.java.classpath.ClassPathProvider;
 import org.netbeans.spi.java.classpath.FilteringPathResourceImplementation;
+import org.netbeans.spi.java.classpath.FlaggedClassPathImplementation;
 import org.netbeans.spi.java.classpath.PathResourceImplementation;
 import org.netbeans.spi.project.LookupMerger;
 import org.openide.filesystems.FileObject;
@@ -127,12 +131,14 @@ final class ClassPathProviderMerger implements LookupMerger<ClassPathProvider> {
      *  The proxy is designed to be used as a union of class paths.
      *  E.g. to be able to easily iterate or listen on all design resources = sources + compile resources
      */
-    class ProxyClassPathImplementation implements ClassPathImplementation {
+    class ProxyClassPathImplementation implements FlaggedClassPathImplementation {
 
-        private PathResourceImplementation[] classPaths;
+        private ProxyFilteringCPI[] classPaths;
         private List<PathResourceImplementation> resourcesCache;
+        private Set<ClassPath.Flag> flagsCache;
         private ArrayList<PropertyChangeListener> listeners;
         private LookupListener lookupList;
+        private PropertyChangeListener flagsListener;
         private Lookup.Result<ClassPathProvider> providers;
         private ClassPathProvider mainProvider;
         private FileObject file;
@@ -147,40 +153,75 @@ final class ClassPathProviderMerger implements LookupMerger<ClassPathProvider> {
             providers = context.lookupResult(ClassPathProvider.class);
 
             checkProviders();
-            lookupList = new LookupListener() {
-                public void resultChanged(LookupEvent ev) {
-                    checkProviders();
+            lookupList = (ev) -> {
+                checkProviders();
+            };
+            flagsListener = (ev) -> {
+                if (ProxyFilteringCPI.PROP_CP_FLAGS.equals(ev.getPropertyName())) {
+                    synchronized (this) {
+                        this.flagsCache = null;
+                    }
+                    firePropertyChange(new PropertyChangeEvent(this, PROP_FLAGS, null, null));
                 }
             };
             providers.addLookupListener(lookupList);
-            
         }
-        
+
         boolean hasAnyResults() {
             return hasAny;
         }
-        
+
         private void checkProviders() {
-            hasAny = false;
-            List<PathResourceImplementation> impls = new ArrayList<PathResourceImplementation>();
+            boolean hasAnyVote = false;
+            final List<ProxyFilteringCPI> impls = new ArrayList<>();
             ClassPath mainResult = mainProvider.findClassPath(file, type);
             if (mainResult != null) {
-                hasAny = true;
-                impls.add(getClassPathImplementation(mainResult));
+                hasAnyVote = true;
+                impls.add(new ProxyFilteringCPI(mainResult));
             }
-            
             for (ClassPathProvider prvd : providers.allInstances()) {
                 ClassPath path = prvd.findClassPath(file, type);
                 if (path != null) {
-                    impls.add(getClassPathImplementation(path));
-                    hasAny = true;
+                    hasAnyVote = true;
+                    impls.add(new ProxyFilteringCPI(path));
                 }
             }
             synchronized (this) {
-                this.classPaths = impls.toArray(new PathResourceImplementation[impls.size()]);
+                this.hasAny = hasAnyVote;
+                if (this.classPaths != null) {
+                    for (ProxyFilteringCPI pr : this.classPaths) {
+                        pr.removePropertyChangeListener(flagsListener);
+                    }
+                }
+                this.classPaths = impls.toArray(new ProxyFilteringCPI[impls.size()]);
+                for (ProxyFilteringCPI pr : this.classPaths) {
+                    pr.addPropertyChangeListener(flagsListener);
+                }
+                //Clean the cache
+                resourcesCache = null;
+                flagsCache = null;
             }
-            PropertyChangeEvent ev = new PropertyChangeEvent(this, ClassPathImplementation.PROP_RESOURCES, null, null);
-            firePropertyChange(ev);
+            firePropertyChange(new PropertyChangeEvent(this, PROP_RESOURCES, null, null));
+            firePropertyChange(new PropertyChangeEvent(this, PROP_FLAGS, null, null));
+        }
+
+        @Override
+        public Set<ClassPath.Flag> getFlags() {
+            synchronized (this) {
+                if (this.flagsCache != null) {
+                    return this.flagsCache;
+                }
+            }
+            final Set<ClassPath.Flag> flags = EnumSet.noneOf(ClassPath.Flag.class);
+            for (ProxyFilteringCPI impl : classPaths) {
+                flags.addAll(impl.getClassPathFlags());
+            }
+            synchronized (this) {
+                if (this.flagsCache == null) {
+                    flagsCache = Collections.unmodifiableSet(flags);
+                }
+                return this.flagsCache;
+            }
         }
 
         public List<? extends PathResourceImplementation> getResources() {
@@ -232,29 +273,26 @@ final class ClassPathProviderMerger implements LookupMerger<ClassPathProvider> {
         }
 
         private void firePropertyChange(PropertyChangeEvent event) {
-                PropertyChangeListener[] _listeners;
-                synchronized (this) {
-                    resourcesCache = null;    //Clean the cache
-                    if (listeners == null) {
-                        return;
-                    }
-                    _listeners = listeners.toArray(new PropertyChangeListener[ProxyClassPathImplementation.this.listeners.size()]);
+            PropertyChangeListener[] _listeners;
+            synchronized (this) {
+                if (listeners == null) {
+                    return;
                 }
-                for (PropertyChangeListener l : _listeners) {
-                    l.propertyChange(event);
-                }
+                _listeners = listeners.toArray(new PropertyChangeListener[ProxyClassPathImplementation.this.listeners.size()]);
+            }
+            for (PropertyChangeListener l : _listeners) {
+                l.propertyChange(event);
+            }
         }
     }
-    
-    static FilteringPathResourceImplementation getClassPathImplementation(ClassPath path) {
-        return new ProxyFilteringCPI(path);
-    }
-    
+
     private static class ProxyFilteringCPI implements FilteringPathResourceImplementation, PropertyChangeListener {
+        static final String PROP_CP_FLAGS = "classPathFlags";   //NOI18N
         private final ClassPath classpath;
         private final PropertyChangeSupport changeSupport;
 
-        private ProxyFilteringCPI(final ClassPath path) {
+        private ProxyFilteringCPI(
+                final ClassPath path) {
             assert path != null;
             this.classpath = path;
             this.changeSupport = new PropertyChangeSupport(this);
@@ -295,12 +333,16 @@ final class ClassPathProviderMerger implements LookupMerger<ClassPathProvider> {
             final String propName = evt.getPropertyName();
             if (ClassPath.PROP_ENTRIES.equals(propName)) {
                 this.changeSupport.firePropertyChange(PROP_ROOTS, null, null);
-            }
-            else if (ClassPath.PROP_INCLUDES.equals(propName)) {
+            } else if (ClassPath.PROP_INCLUDES.equals(propName)) {
                 this.changeSupport.firePropertyChange(PROP_INCLUDES, null, null);
+            } else if (ClassPath.PROP_FLAGS.equals(propName)) {
+                this.changeSupport.firePropertyChange(PROP_CP_FLAGS, null, null);
             }
         }
-        
+
+        @NonNull
+        Set<ClassPath.Flag> getClassPathFlags() {
+            return classpath.getFlags();
+        }
     }
-            
 }
