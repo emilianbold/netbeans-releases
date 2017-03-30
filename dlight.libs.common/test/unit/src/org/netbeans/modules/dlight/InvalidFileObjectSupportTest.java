c2581a6f0c990493e2da0d7e6170f59470ca9705
Vladimir Kvashin <vkvashin@netbeans.org>
2017-03-30 20:21:32 +0300

    test for #258416 - AssertionError: nbfs://nbhost/org.netbea.... (transplanted from c38319da61697ace5575157b97251839c607bc18)

--- dlight.libs.common/test/unit/src/org/netbeans/modules/dlight/InvalidFileObjectSupportTest.java
+++ dlight.libs.common/test/unit/src/org/netbeans/modules/dlight/InvalidFileObjectSupportTest.java
@@ -47,16 +47,27 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 import java.io.File;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLStreamHandlerFactory;
 import org.openide.filesystems.FileObject;
 import org.openide.filesystems.FileSystem;
 import org.openide.filesystems.FileUtil;
+import org.openide.filesystems.URLMapper;
+import org.openide.util.Lookup;
 
 /**
  *
  * @author Vladimir Kvashin
  */
 public class InvalidFileObjectSupportTest {
-    
+
+    static {        
+        // otherwise we get java.net.MalformedURLException: unknown protocol
+        // even if we register via @URLStreamHandlerRegistration annotation
+        URL.setURLStreamHandlerFactory(Lookup.getDefault().lookup(URLStreamHandlerFactory.class));
+    }    
+
     @Test
     public void testInvalidFileObject() throws Exception {
         File file = File.createTempFile("qwe", "asd");
@@ -72,7 +83,9 @@ public class InvalidFileObjectSupportTest {
             file.delete();
         }
         FileObject invalidFo1 = InvalidFileObjectSupport.getInvalidFileObject(fs, path);
+        URI uri1 = invalidFo1.toURI(); // just to check that there is no assertions
         FileObject invalidFo2 = InvalidFileObjectSupport.getInvalidFileObject(fs, path);
+        URI uri2 = invalidFo2.toURI(); // just to check that there is no assertions
         assertTrue(invalidFo1 == invalidFo2);
         assertFalse(invalidFo1.isValid());
         assertEquals(origFo.getName(), invalidFo1.getName());
