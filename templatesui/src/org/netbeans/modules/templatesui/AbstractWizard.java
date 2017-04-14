a48dda4e4d98eca826b9217093106c9689ad8471
Jaroslav Tulach <jtulach@netbeans.org>
2017-04-14 12:05:58 +0200

    Always fill in our steps, even for non HTML panels

--- templatesui/src/org/netbeans/modules/templatesui/AbstractWizard.java
+++ templatesui/src/org/netbeans/modules/templatesui/AbstractWizard.java
@@ -205,7 +205,19 @@ implements WizardDescriptor.InstantiatingIterator<WizardDescriptor> {
     
     @Override
     public WizardDescriptor.Panel<WizardDescriptor> current() {
-        return getPanels().get(index);
+        WizardDescriptor.Panel<WizardDescriptor> ret = getPanels().get(index);
+        if (ret.getComponent() != p) {
+            if (ret.getComponent() instanceof JComponent) {
+                JComponent update = (JComponent) ret.getComponent();
+                update.putClientProperty(WizardDescriptor.PROP_CONTENT_DATA,
+                    p.getClientProperty(WizardDescriptor.PROP_CONTENT_DATA)
+                );
+                update.putClientProperty(WizardDescriptor.PROP_CONTENT_SELECTED_INDEX,
+                    p.getClientProperty(WizardDescriptor.PROP_CONTENT_SELECTED_INDEX)
+                );
+            }
+        }
+        return ret;
     }
 
     @NbBundle.Messages({
