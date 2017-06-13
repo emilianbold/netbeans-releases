7b55b38d7482ac93368e231b4380949e47d673e2
Maria Dalmatova <mromashova@netbeans.org>
2017-06-13 13:08:20 +0300

    updated  UI: implemented pinned tooltip with navigation, code snippet and details in bottom window

--- cnd.diagnostics.clank/src/org/netbeans/modules/cnd/diagnostics/clank/ui/ClankErrorPathDetailsProviderImpl.java
+++ cnd.diagnostics.clank/src/org/netbeans/modules/cnd/diagnostics/clank/ui/ClankErrorPathDetailsProviderImpl.java
@@ -39,14 +39,36 @@
  */
 package org.netbeans.modules.cnd.diagnostics.clank.ui;
 
-import java.util.Collections;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import org.netbeans.modules.cnd.diagnostics.clank.ui.codesnippet.ClankDiagnosticsDetailsTopComponent;
+import javax.swing.JEditorPane;
 import javax.swing.SwingUtilities;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.JTextComponent;
 import org.clang.tools.services.ClankDiagnosticInfo;
+import org.netbeans.api.editor.EditorRegistry;
+import org.netbeans.api.editor.document.LineDocument;
+import org.netbeans.api.editor.document.LineDocumentUtils;
+import org.netbeans.editor.EditorUI;
+import org.netbeans.editor.Utilities;
+import org.netbeans.editor.ext.ToolTipSupport;
+import org.netbeans.modules.cnd.api.model.CsmFile;
+import org.netbeans.modules.cnd.api.model.services.CsmFileInfoQuery;
 import org.netbeans.modules.cnd.diagnostics.clank.ClankCsmErrorInfo;
-import org.netbeans.modules.cnd.diagnostics.clank.ClankDiagnosticsAnalyzerImpl;
 import org.netbeans.modules.cnd.diagnostics.clank.ClankErrorPathDetailsProvider;
-import org.openide.util.Utilities;
-import org.openide.util.lookup.Lookups;
+import org.netbeans.modules.cnd.diagnostics.clank.impl.ClankCsmErrorInfoAccessor;
+import org.netbeans.modules.cnd.diagnostics.clank.ui.tooltip.ToolTipUI;
+import org.netbeans.modules.cnd.diagnostics.clank.ui.tooltip.ViewFactory;
+import org.netbeans.modules.cnd.diagnostics.clank.ui.views.DiagnosticsAnnotationProvider;
+import org.netbeans.modules.cnd.modelutil.CsmUtilities;
+import org.netbeans.modules.cnd.utils.cache.CndFileUtils;
+import org.openide.filesystems.FileObject;
+import org.openide.filesystems.FileStateInvalidException;
+import org.openide.filesystems.FileSystem;
+import org.openide.util.Exceptions;
+import org.openide.util.RequestProcessor;
 import org.openide.util.lookup.ServiceProvider;
 
 /**
@@ -58,19 +80,79 @@ public class ClankErrorPathDetailsProviderImpl implements ClankErrorPathDetailsP
 
     @Override
     public void implement(final ClankCsmErrorInfo errorInfo) {
+
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                //provider2Hints
-                ClankDiagnosticsDetailsTopComponent details = ClankDiagnosticsDetailsTopComponent.findInstance();
+                final ClankDiagnosticsDetailsTopComponent details = ClankDiagnosticsDetailsTopComponent.findInstance();
                 details.setData(errorInfo);
-//                ClankDiagnosticsAnalyzerImpl analyzer = new ClankDiagnosticsAnalyzerImpl
-//                details.setData(Lookups.fixed(Utilities.actionsGlobalContext().lookupAll(Object.class).toArray(new Object[0])), 
-//                         new AnalysisResult(provider2Hints, errorsToProjects, analyzerId2Description, Collections.EMPTY_LIST));
                 details.open();
                 details.requestActive();
+                final PropertyChangeListener listener = new PropertyChangeListener() {
+                    @Override
+                    public void propertyChange(PropertyChangeEvent evt) {
+                        if (DiagnosticsAnnotationProvider.DIAGNOSTIC_CHANGED.equals(evt.getPropertyName())) {
+                            ClankDiagnosticInfo note = (ClankDiagnosticInfo) evt.getNewValue();
+                            details.setSelectedNode(note);
+                        }
+                    }
+                };
+                RequestProcessor.getDefault().post(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+
+                            CsmFile csmErrorFile = ClankCsmErrorInfoAccessor.getDefault().getCsmFile(errorInfo);
+                            FileSystem fSystem = csmErrorFile.getFileObject().getFileSystem();
+                            DiagnosticsAnnotationProvider.clearAll();
+                            for (ClankDiagnosticInfo note : ClankCsmErrorInfoAccessor.getDefault().getDelegate(errorInfo).notes()) {
+                                FileObject fo = CndFileUtils.toFileObject(fSystem, note.getSourceFileName());
+                                CsmFile csmNoteFile = CsmUtilities.getCsmFile(fo, true, false);
+                                JTextComponent textComponent = EditorRegistry.findComponent(CsmUtilities.getDocument(csmNoteFile));
+                                JEditorPane editorPane = null;
+                                //JEditorPane editorPane =
+                                if (textComponent instanceof JEditorPane) {
+                                    editorPane = ((JEditorPane) textComponent);
+                                }
+                                final JEditorPane ep = editorPane;
+                                Document document = ep.getDocument();
+                                if (ep == null) {
+                                    return;
+                                }
+                                int offset = note.getEndOffset();
+                                if (document instanceof LineDocument) {
+                                    offset = LineDocumentUtils.getLineEnd((LineDocument) document, offset);
+                                }
+                                final int locOffset = offset;
+                                final int[] lineColumnByOffset = CsmFileInfoQuery.getDefault().getLineColumnByOffset(csmNoteFile, note.getStartOffset());
+                                //find last symbol at the line
+
+                                SwingUtilities.invokeLater(new Runnable() {
+                                    public void run() {
+                                        EditorUI eui = Utilities.getEditorUI(ep);
+                                        if (eui == null) {
+                                            return;
+                                        }
+                                        ToolTipUI noteTooltip = ViewFactory.getDefault().createToolTip(note.getMessage(),
+                                                null,
+                                                new ToolTipUI.Pinnable(note.getMessage(), lineColumnByOffset[0] - 1, note),
+                                                listener);
+                                        noteTooltip.pin(ep, locOffset);
+                                    }
+                                });
+                            }
+                        } catch (FileStateInvalidException ex) {
+                            Exceptions.printStackTrace(ex);
+                        } catch (BadLocationException ex) {
+                            Exceptions.printStackTrace(ex);
+                        }
+
+                    }
+                });
+
             }
         });
+
     }
 
 }
