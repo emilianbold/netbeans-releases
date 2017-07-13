4dff87e7c1449f58bbf0be5cc8f4fe4c04b3b41c
Libor Fischmeistr <lfischmeistr@netbeans.org>
2017-07-13 16:53:55 +0200

    #270896 - Update JDK to 8u141 - signing single pks using Oracle signing server

--- installer/mac/newbuild/build.sh
+++ installer/mac/newbuild/build.sh
@@ -43,14 +43,14 @@ set -x -e
 echo Given parameters: $1 $2 $3 $4 $5 $6
 
 if [ -z "$1" ] || [ -z "$2" ]|| [ -z "$3" ] || [ -z "$4" ] || [ -z "$5" ] || [ -z "$6" ] || [ -z "$7" ]; then
-    echo "usage: $0 zipdir prefix buildnumber build_jdk7 build_jdk8 signing_identity [nb_locales]"
+    echo "usage: $0 zipdir prefix buildnumber build_jdk7 build_jdk8 mac_sign_client mac_sign_user mac_sign_guid codesignbureau_credfile [nb_locales]"
     echo ""
     echo "zipdir is the dir which contains the zip/modulclusters
     echo "prefix is the distro filename prefix, e.g. netbeans-hudson-trunk in netbeans-hudson-trunk-2464"
     echo "buildnumber is the distro buildnumber, e.g. 2464 in netbeans-hudson-trunk-2464"
     echo "build_jdk7 is 1 if bundle jdk7 are required and 0 if not"
     echo "build_jdk8 is 1 if bundle jdk8 are required and 0 if not"
-    echo "signing_identity is a digital identity for signing the OS X installer or 0 if not signing"
+    echo "mac_sign_client, mac_sign_user, mac_sign_guid, codesignbureau_credfile are required if the packages are to be signed, 0 if not"
     echo "nb_locales is the string with the list of locales
     exit 1
 fi
@@ -60,9 +60,12 @@ prefix=$2
 buildnumber=$3
 build_jdk7=$4
 build_jdk8=$5
-signing_identity=$6
-if [ -n "$7" ] ; then
-  nb_locales=",$7"
+mac_sign_client=$6
+mac_sign_user=$7
+mac_sign_guid=$8
+codesignbureau_credfile=$9
+if [ -n "${10}" ] ; then
+  nb_locales=",${10}"
 fi
 
 basename=`dirname "$0"`
@@ -87,10 +90,16 @@ if [ -z $en_build ] ; then
     en_build=1
 fi
 
-if [ 0 = "${signing_identity}" ] ; then
-    signing_identity=0
+if [ -z $mac_sign_client || 0 -eq $mac_sign_client || -z $mac_sign_user || 0 -eq $mac_sign_user || -z $mac_sign_guid || 0 -eq $mac_sign_guid || -z $codesignbureau_credfile || 0 -eq $codesignbureau_credfile ] ; then
+    mac_sign_client=0
+    mac_sign_user=0
+    mac_sign_guid=0
+    codesignbureau_credfile=0
+else
+    export CODESIGNBUREAU_CREDFILE=$codesignbureau_credfile
 fi
 
 rm -rf "$basename"/dist_en
-ant -f $basename/build.xml $target -Dlocales=$nb_locales -Dcommon.name=$commonname -Dprefix=$prefix -Dbuildnumber=$buildnumber  -Dsigning_identity="${signing_identity}" -Dbuild.jdk7=$build_jdk7 -Dbuild.jdk8=$build_jdk8 -Dgf_builds_host=$GLASSFISH_BUILDS_HOST -Djre_builds_host=$JRE_BUILDS_HOST -Djdk_builds_host=$JDK_BUILDS_HOST -Djre_builds_path=$JRE_BUILDS_PATH -Djdk7_builds_path=$JDK7_BUILDS_PATH -Djdk8_builds_path=$JDK8_BUILDS_PATH -Dbinary_cache_host=$BINARY_CACHE_HOST -Dmac.sign.client=$MAC_SIGN_CLIENT -Dmac.sign.user=$MAC_SIGN_USER -Dmac.sign.giud=$MAC_SIGN_GUID
+
+ant -f $basename/build.xml $target -Dlocales=$nb_locales -Dcommon.name=$commonname -Dprefix=$prefix -Dbuildnumber=$buildnumber -Dmac.sign.client=$mac_sign_client -Dmac.sign.user=$mac_sign_user -Dmac.sign.guid=$mac_sign_guid -Dbuild.jdk7=$build_jdk7 -Dbuild.jdk8=$build_jdk8 -Dgf_builds_host=$GLASSFISH_BUILDS_HOST -Djre_builds_host=$JRE_BUILDS_HOST -Djdk_builds_host=$JDK_BUILDS_HOST -Djre_builds_path=$JRE_BUILDS_PATH -Djdk7_builds_path=$JDK7_BUILDS_PATH -Djdk8_builds_path=$JDK8_BUILDS_PATH -Dbinary_cache_host=$BINARY_CACHE_HOST
 mv -f "$basename"/dist "$basename"/dist_en
