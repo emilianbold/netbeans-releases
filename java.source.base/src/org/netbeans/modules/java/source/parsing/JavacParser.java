138acacc4a85b1a80e6a629c9f5f27c564cd7130
Tomas Zezula <tzezula@netbeans.org>
2017-07-28 16:23:33 +0200

    Fixed broken incremental reparse.

--- java.source.base/src/org/netbeans/modules/java/source/parsing/JavacParser.java
+++ java.source.base/src/org/netbeans/modules/java/source/parsing/JavacParser.java
@@ -1390,7 +1390,10 @@ public class JavacParser extends Parser {
         }
     }
 
-    private void checkSourceModification(final SourceModificationEvent evt) {
+    private void checkSourceModification(SourceModificationEvent evt) {
+        if (evt instanceof SourceModificationEvent.Composite) {
+            evt = ((SourceModificationEvent.Composite)evt).getWriteEvent();
+        }
         if (evt != null && evt.sourceChanged()) {
             Pair<DocPositionRegion,MethodTree> changedMethod = null;
             if (supportsReparse) {
