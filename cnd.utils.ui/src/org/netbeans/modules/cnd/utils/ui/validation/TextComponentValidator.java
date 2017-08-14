614903809eabeab31ff28db7894f43f36a624a8a
Ilia Gromov <ilia@netbeans.org>
2017-08-14 17:21:00 +0300

    Fixing #271055 - Provide Developer VMs in ODCS plugin Validators

---
+++ cnd.utils.ui/src/org/netbeans/modules/cnd/utils/ui/validation/TextComponentValidator.java
@@ -0,0 +1,99 @@
+/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright (c) 2017 Oracle and/or its affiliates. All rights reserved.
+ *
+ * Oracle and Java are registered trademarks of Oracle and/or its affiliates.
+ * Other names may be trademarks of their respective owners.
+ *
+ * The contents of this file are subject to the terms of either the GNU
+ * General Public License Version 2 only ("GPL") or the Common
+ * Development and Distribution License("CDDL") (collectively, the
+ * "License"). You may not use this file except in compliance with the
+ * License. You can obtain a copy of the License at
+ * http://www.netbeans.org/cddl-gplv2.html
+ * or nbbuild/licenses/CDDL-GPL-2-CP. See the License for the
+ * specific language governing permissions and limitations under the
+ * License.  When distributing the software, include this License Header
+ * Notice in each file and include the License file at
+ * nbbuild/licenses/CDDL-GPL-2-CP.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the GPL Version 2 section of the License file that
+ * accompanied this code. If applicable, add the following below the
+ * License Header, with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ *
+ * If you wish your version of this file to be governed by only the CDDL
+ * or only the GPL Version 2, indicate your decision by adding
+ * "[Contributor] elects to include this software in this distribution
+ * under the [CDDL or GPL Version 2] license." If you do not indicate a
+ * single choice of license, a recipient has the option to distribute
+ * your version of this file under either the CDDL, the GPL Version 2 or
+ * to extend the choice of license to its licensees as provided above.
+ * However, if you add GPL Version 2 code and therefore, elected the GPL
+ * Version 2 license, then the option applies only if the new code is
+ * made subject to such option by the copyright holder.
+ *
+ * Contributor(s):
+ */
+package org.netbeans.modules.cnd.utils.ui.validation;
+
+import java.util.function.Predicate;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.JTextComponent;
+import org.openide.NotifyDescriptor;
+
+/**
+ *
+ * @author Ilia Gromov
+ */
+public class TextComponentValidator {
+
+    private final Validator validator;
+    private final NotifyDescriptor nd;
+
+    public TextComponentValidator(NotifyDescriptor nd) {
+        this.validator = new Validator();
+        this.nd = nd;
+    }
+
+    public void addTextComponentRule(JTextComponent textComponent, Predicate<String> rule) {
+        textComponent.getDocument().addDocumentListener(new DocListener(nd));
+
+        validator.addValidationRule(() -> textComponent.getText(), rule);
+    }
+
+    private class DocListener implements DocumentListener {
+
+        private final NotifyDescriptor nd;
+
+        public DocListener(NotifyDescriptor nd) {
+            this.nd = nd;
+        }
+
+        @Override
+        public void insertUpdate(DocumentEvent e) {
+            fireUpdate(e);
+        }
+
+        @Override
+        public void removeUpdate(DocumentEvent e) {
+            fireUpdate(e);
+        }
+
+        @Override
+        public void changedUpdate(DocumentEvent e) {
+            fireUpdate(e);
+        }
+
+        public Validator getValidator() {
+            return validator;
+        }
+
+        private void fireUpdate(DocumentEvent e) {
+            nd.setValid(validator.isValid());
+        }
+    }
+}
