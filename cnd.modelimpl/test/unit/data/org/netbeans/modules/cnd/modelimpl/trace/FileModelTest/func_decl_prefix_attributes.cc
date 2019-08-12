f8f9237450e03fba4daf6441bcb6ea0a02d87fac
Geertjan Wielenga <geertjan@netbeans.org>
2019-08-12 21:34:33 +0200

    Donation 4: Removing data files without licenses

--- cnd.modelimpl/test/unit/data/org/netbeans/modules/cnd/modelimpl/trace/FileModelTest/func_decl_prefix_attributes.cc
+++
@@ -1,8 +0,0 @@
-// this is simplified exaple from GL library (gl.h) for gcc version >= 3.3 
-#  define GLAPI __attribute__((visibility("default")))
-#  define GLAPIENTRY
-
-GLAPI void GLAPIENTRY glFunction( int red, int green, int blue );
-
-// example from IZ#149506
-__attribute__((format(printf,0,1))) static void foo_149506() {}
